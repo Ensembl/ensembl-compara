d384b7327b4024e6c3901526d7ef3d6f9501640a
Web Admin <w3adm@sanger.ac.uk>
2001-04-02 09:46:04 +0000

    protein based snp glyphset for Emmanuel rmp

---
+++ modules/Bio/EnsEMBL/GlyphSet/prot_snp.pm
@@ -0,0 +1,66 @@
+package Bio::EnsEMBL::GlyphSet::prot_snp;
+use strict;
+use vars qw(@ISA);
+use lib "..";
+use Bio::EnsEMBL::GlyphSet;
+@ISA = qw(Bio::EnsEMBL::GlyphSet);
+use Bio::EnsEMBL::Glyph::Rect;
+use Bio::EnsEMBL::Glyph::Text;
+use Bio::EnsEMBL::Glyph::Composite;
+
+sub _init {
+    my ($this, $protein, $Config) = @_;
+
+    my $y          = 0;
+    my $h          = 4;
+    my $highlights = $this->highlights();
+   
+#Draw the protein
+    my $length = $protein->length();
+
+    my $xp = 0;
+    my $wp = $length;
+
+    print STDERR "PROT VERSION001\n";
+
+   
+    my $colour = $Config->get($Config->script(), 'prot_snp','col');
+
+    my $rect = new Bio::EnsEMBL::Glyph::Rect({
+			'x'        => $xp,
+			'y'        => $y,
+			'width'    => $wp,
+			'height'   => $h,
+			'id'       => $protein->id(),
+			'colour'   => $colour,
+			'zmenu' => {
+			    'caption' => $protein->id(),
+			},
+		    });
+    
+#    push @{$this->{'glyphs'}}, $rect;   
+    $this->push($rect);
+    
+   
+}
+1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
