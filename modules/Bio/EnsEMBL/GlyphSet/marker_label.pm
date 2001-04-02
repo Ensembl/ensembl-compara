461ba455db160fc476b3cd24fc488903caf12e3b
Web Admin <w3adm@sanger.ac.uk>
2001-04-02 19:52:46 +0000

    new glyphset providing labels for map markers

---
+++ modules/Bio/EnsEMBL/GlyphSet/marker_label.pm
@@ -0,0 +1,62 @@
+package Bio::EnsEMBL::GlyphSet::marker_label;
+use strict;
+use vars qw(@ISA);
+use Bio::EnsEMBL::GlyphSet;
+use Bio::EnsEMBL::Glyph::Rect;
+use Bio::EnsEMBL::Glyph::Intron;
+use Bio::EnsEMBL::Glyph::Text;
+use Bio::EnsEMBL::Glyph::Composite;
+use Bump;
+
+@ISA = qw(Bio::EnsEMBL::GlyphSet);
+
+sub _init {
+    my $self = shift;
+    my $VirtualContig = $self->{'container'};
+    my $Config = $self->{'config'};
+    
+    my $pix_per_bp  = $Config->transform->{'scalex'};
+    my $bitmap_length = int($VirtualContig->length * $pix_per_bp);
+    my @bitmap;
+
+    return unless ($self->strand() == -1);
+
+    my $feature_colour 	= $Config->get($Config->script(),'marker','col');
+    my $fontname = "Tiny";
+    my ($w,$h) = $Config->texthelper->px2bp($fontname);
+    $w = $Config->texthelper->width($fontname);
+
+    foreach my $f ($VirtualContig->get_landmark_MarkerFeatures()){
+	my $fid = $f->id();
+	my $bp_textwidth = $w * length("$fid ");
+	my $glyph = new Bio::EnsEMBL::Glyph::Text({
+		'x'	    => $f->start(),
+		'y'	    => 0,
+		'height'    => $Config->texthelper->height($fontname),
+		'font'	    => $fontname,
+		'colour'    => $feature_colour,
+		'absolutey' => 1,
+		'text'	    => $fid,
+		});
+
+	##############
+    	# bump-tastic
+	#
+    	my $bump_start = int($glyph->x() * $pix_per_bp);
+	$bump_start    = 0 if ($bump_start < 0);
+
+    	my $bump_end = $bump_start + $bp_textwidth;
+    	next if $bump_end > $bitmap_length;
+    	my $row = &Bump::bump_row(      
+	    $bump_start,
+	    $bump_end,
+	    $bitmap_length,
+	    \@bitmap
+    	);
+
+    	$glyph->y($glyph->y() + (1.2 * $row * $h));
+    	$self->push($glyph);
+    }	
+}
+
+1;
