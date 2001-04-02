c60ad3b426b57d919733d8498ee3e8ed59b91b9f
Web Admin <w3adm@sanger.ac.uk>
2001-04-02 16:19:17 +0000

    Bumping Rewritten Uses pixels, not basepairs Transcript now conforms

--- modules/Bio/EnsEMBL/GlyphSet/transcript.pm
+++ modules/Bio/EnsEMBL/GlyphSet/transcript.pm
@@ -15,9 +15,9 @@ sub init_label {
     my ($this) = @_;
 
     my $label = new Bio::EnsEMBL::Glyph::Text({
-	'text'      => 'Transcript',
-	'font'      => 'Small',
-	'absolutey' => 1,
+    'text'      => 'Transcript',
+    'font'      => 'Small',
+    'absolutey' => 1,
     });
     $this->label($label);
 }
@@ -26,22 +26,24 @@ sub _init {
     my ($self) = @_;
 
     my $VirtualContig = $self->{'container'};
-    my $Config = $self->{'config'};
+    my $Config      = $self->{'config'};
 
-	my $vcid			= $VirtualContig->id();
-	my $scriptname		= $Config->script();
-    my $y             	= 0;
-    my $h             	= 8;
-    my $highlights    	= $self->highlights();
-    my @bitmap			= undef;
-    my $im_width      	= $Config->image_width();
-    my $length 			= $VirtualContig->length();
-    my $colour        	= $Config->get($scriptname,'transcript','unknown');
-    my $type          	= $Config->get($scriptname,'transcript','src');
-    my @allgenes      	= ();
-	
+    my $vcid        = $VirtualContig->id();
+    my $scriptname  = $Config->script();
+    my $y	    = 0;
+    my $h	    = 8;
+    my $highlights  = $self->highlights();
+    my @bitmap      = undef;
+    my $im_width    = $Config->image_width();
+    my $colour      = $Config->get($scriptname,'transcript','unknown');
+    my $type        = $Config->get($scriptname,'transcript','src');
+    my @allgenes    = ();
+    
+    my $pix_per_bp  = $Config->transform->{'scalex'};
+    my $bitmap_length = int($VirtualContig->length * $pix_per_bp);
+    
     foreach my $vg ($VirtualContig->get_all_VirtualGenes()){
-	push (@allgenes, $vg->gene());
+    push (@allgenes, $vg->gene());
     }
 
     if ($type eq 'all'){
@@ -52,176 +54,176 @@ sub _init {
     }
 
     GENE: for my $eg (@allgenes) {
-	my $vgid = $eg->id();
-    	my $hi_colour = $Config->get($scriptname,'transcript','hi') if(defined $highlights && $highlights =~ /\|$vgid\|/);
+    my $vgid = $eg->id();
+        my $hi_colour = $Config->get($scriptname,'transcript','hi') if(defined $highlights && $highlights =~ /\|$vgid\|/);
 
-	TRANSCRIPT: for my $transcript ($eg->each_Transcript()) {
+    TRANSCRIPT: for my $transcript ($eg->each_Transcript()) {
 
-    	#########
-    	# test transcript strand
-    	#
-    	my $tstrand = $transcript->strand_in_context($vcid);
-    	next TRANSCRIPT if($tstrand != $self->strand());
+        #########
+        # test transcript strand
+        #
+        my $tstrand = $transcript->strand_in_context($vcid);
+        next TRANSCRIPT if($tstrand != $self->strand());
 
-    	#########
-    	# set colour for transcripts and test if we're highlighted or not
-    	# 
-    	my @dblinks = ();
-    	my $id = $transcript->id();
-	    my $gene_name;
-	    my ($hugo, $swisslink, $sptrembllink);
-	    eval {
-			@dblinks = $transcript->each_DBLink();
+        #########
+        # set colour for transcripts and test if we're highlighted or not
+        # 
+        my @dblinks = ();
+        my $id = $transcript->id();
+        my $gene_name;
+        my ($hugo, $swisslink, $sptrembllink);
+        eval {
+            @dblinks = $transcript->each_DBLink();
 
-			foreach my $DB_link ( @dblinks ){
-		    	if( $DB_link->database eq 'HUGO') {
-					$hugo = $DB_link;
-					last;
-		    	}
-		    	if( $DB_link->database =~ /SWISS/o ) {
-					$swisslink = $DB_link;
-		    	}
-		    	if( $DB_link->database eq 'SPTREMBL') {
-					$sptrembllink = $DB_link;
-		    	}
-			}
+            foreach my $DB_link ( @dblinks ){
+                if( $DB_link->database eq 'HUGO') {
+                    $hugo = $DB_link;
+                    last;
+                }
+                if( $DB_link->database =~ /SWISS/o ) {
+                    $swisslink = $DB_link;
+                }
+                if( $DB_link->database eq 'SPTREMBL') {
+                    $sptrembllink = $DB_link;
+                }
+            }
 
-			if( $hugo ) {
-		    	$id = $hugo->primary_id;
-			} elsif ( $swisslink ) {
-		    	$id = $swisslink->primary_id;
-			} elsif ( $sptrembllink ) {
-		    	$id = $sptrembllink->primary_id;
-			}  
+            if( $hugo ) {
+                $id = $hugo->primary_id;
+            } elsif ( $swisslink ) {
+                $id = $swisslink->primary_id;
+            } elsif ( $sptrembllink ) {
+                $id = $sptrembllink->primary_id;
+            }  
         };
 
-	    if (@dblinks){
-			$colour = $Config->get($scriptname,'transcript','known');
-	    } else {
-			$colour = $Config->get($scriptname,'transcript','unknown');
-	    }
-	    if ($eg->{'_is_external'}){
-			$colour = $Config->get($scriptname,'transcript','ext');
-	    }
-		
-	    my $tid = $transcript->id();
-		my $Composite = new Bio::EnsEMBL::Glyph::Composite({
-		'zmenu'  => {
-		    'caption'     => $id,
-		    'More information'     			=> "/perl/geneview?transcript=$id",
-		    'Peptide sequence (FASTA)'		=> "/perl/dumpview?type=peptide&id=$tid",
-		    'Supporting evidence'      		=> "/perl/transview?transcript=$tid",
-		    'cDNA sequence' 				=> "/perl/dumpview?type=cdna&id=$tid",
-		},
-	    });
+        if (@dblinks){
+            $colour = $Config->get($scriptname,'transcript','known');
+        } else {
+            $colour = $Config->get($scriptname,'transcript','unknown');
+        }
+        if ($eg->{'_is_external'}){
+            $colour = $Config->get($scriptname,'transcript','ext');
+        }
+        
+        my $tid = $transcript->id();
+        my $Composite = new Bio::EnsEMBL::Glyph::Composite({
+        'zmenu'  => {
+            'caption'			=> $id,
+            'More information'          => "/perl/geneview?transcript=$id",
+            'Peptide sequence (FASTA)'  => "/perl/dumpview?type=peptide&id=$tid",
+            'Supporting evidence'       => "/perl/transview?transcript=$tid",
+            'cDNA sequence'             => "/perl/dumpview?type=cdna&id=$tid",
+	    },
+        });
 
-	    my @exons = $transcript->each_Exon_in_context($vcid);
+        my @exons = $transcript->each_Exon_in_context($vcid);
 
-	    my ($start_screwed, $end_screwed);
-	    if($tstrand != -1) {
-			$start_screwed = $transcript->is_start_exon_in_context($vcid);
-			$end_screwed   = $transcript->is_end_exon_in_context($vcid);
-	    } else {
-			$end_screwed   = $transcript->is_start_exon_in_context($vcid);
-			$start_screwed = $transcript->is_end_exon_in_context($vcid);
-			@exons = reverse @exons;
-	    }
+        my ($start_screwed, $end_screwed);
+        if($tstrand != -1) {
+            $start_screwed = $transcript->is_start_exon_in_context($vcid);
+            $end_screwed   = $transcript->is_end_exon_in_context($vcid);
+        } else {
+            $end_screwed   = $transcript->is_start_exon_in_context($vcid);
+            $start_screwed = $transcript->is_end_exon_in_context($vcid);
+            @exons = reverse @exons;
+        }
 
-	    my $start_exon = $exons[0];
-	    my $end_exon   = $exons[-1];
+        my $start_exon = $exons[0];
+        my $end_exon   = $exons[-1];
 
-	    my $previous_endx;
+        my $previous_endx;
 
-	    #########
-	    # draw anything trailing off the beginning
-	    #
-	    if(defined $start_screwed && $start_screwed == 0) {
-		my $clip = new Bio::EnsEMBL::Glyph::Line({
-		    'x'         => 0,
-		    'y'         => $y+int($h/2),
-		    'width'     => $start_exon->start(),
-		    'height'    => 0,
-		    'absolutey' => 1,
-		    'colour'    => $colour,
-		    'dotted'    => 1,
-		});
-		$Composite->push($clip);
-		$previous_endx = $start_exon->end();
-	    }
+        #########
+        # draw anything trailing off the beginning
+        #
+        if(defined $start_screwed && $start_screwed == 0) {
+        my $clip = new Bio::EnsEMBL::Glyph::Line({
+            'x'         => 0,
+            'y'         => $y+int($h/2),
+            'width'     => $start_exon->start(),
+            'height'    => 0,
+            'absolutey' => 1,
+            'colour'    => $colour,
+            'dotted'    => 1,
+        });
+        $Composite->push($clip);
+        $previous_endx = $start_exon->end();
+        }
 
-    	EXON: for my $exon (@exons) {
-		#########
-		# otherwise we're on the VC and everything's ok
-		#
-		my $x = $exon->start();
-		my $w = $exon->end() - $x;
+        EXON: for my $exon (@exons) {
+        #########
+        # otherwise we're on the VC and everything's ok
+        #
+        my $x = $exon->start();
+        my $w = $exon->end() - $x;
 
-		my $rect = new Bio::EnsEMBL::Glyph::Rect({
-		    'x'         => $x,
-		    'y'         => $y,
-		    'width'     => $w,
-		    'height'    => $h,
-		    'colour'    => $colour,
-		    'absolutey' => 1,
-		});
+        my $rect = new Bio::EnsEMBL::Glyph::Rect({
+            'x'         => $x,
+            'y'         => $y,
+            'width'     => $w,
+            'height'    => $h,
+            'colour'    => $colour,
+            'absolutey' => 1,
+        });
 
-		my $intron = new Bio::EnsEMBL::Glyph::Intron({
-		    'x'         => $previous_endx,
-		    'y'         => $y,
-		    'width'     => ($x - $previous_endx),
-		    'height'    => $h,
-		    #'id'        => $exon->id(),
-		    'colour'    => $colour,
-		    'absolutey' => 1,
-		    'strand'    => $tstrand,
-		}) if(defined $previous_endx);
+        my $intron = new Bio::EnsEMBL::Glyph::Intron({
+            'x'         => $previous_endx,
+            'y'         => $y,
+            'width'     => ($x - $previous_endx),
+            'height'    => $h,
+            #'id'        => $exon->id(),
+            'colour'    => $colour,
+            'absolutey' => 1,
+            'strand'    => $tstrand,
+        }) if(defined $previous_endx);
 
-		$Composite->push($rect);
-		$Composite->push($intron);
+        $Composite->push($rect);
+        $Composite->push($intron);
 
-		$previous_endx = $exon->end();
-	    }
+        $previous_endx = $exon->end();
+        }
 
-	    #########
-	    # draw anything trailing off the end
-	    #
-	    if(defined $end_screwed && $end_screwed == 0) {
-		my $clip = new Bio::EnsEMBL::Glyph::Line({
-		    'x'         => $previous_endx,
-		    'width'     => $length - $previous_endx,
-		    'y'         => $y+int($h/2),
-		    'height'    => 0,
-		    'colour'    => $colour,
-		    'absolutey' => 1,
-		    'dotted'    => 1,
-		});
-		$Composite->push($clip);
-	    }
+        #########
+        # draw anything trailing off the end
+        #
+        if(defined $end_screwed && $end_screwed == 0) {
+        my $clip = new Bio::EnsEMBL::Glyph::Line({
+            'x'         => $previous_endx,
+            'width'     => $VirtualContig->length() - $previous_endx,
+            'y'         => $y+int($h/2),
+            'height'    => 0,
+            'colour'    => $colour,
+            'absolutey' => 1,
+            'dotted'    => 1,
+        });
+        $Composite->push($clip);
+        }
 
 
-	    #########
-	    # bump it baby, yeah!
-	    # bump-nology!
-	    #
-	    my $bump_start = $Composite->x();
-	    $bump_start = 0 if ($bump_start < 0);
+        #########
+        # bump it baby, yeah!
+        # bump-nology!
+        #
+        my $bump_start = int($Composite->x * $pix_per_bp);
+        $bump_start = 0 if ($bump_start < 0);
 
-	    my $bump_end = $bump_start + ($Composite->width());
-	    next if $bump_end > $length;
+        my $bump_end = $bump_start + int($Composite->width * $pix_per_bp);
+        if ($bump_end > $bitmap_length){$bump_end = $bitmap_length};
 
-	    my $row = &Bump::bump_row(      
-		$bump_start,
-		$bump_end,
-		$length,
-		\@bitmap
-	    );
+        my $row = &Bump::bump_row(      
+	    $bump_start,
+	    $bump_end,
+	    $bitmap_length,
+	    \@bitmap
+        );
 
-	    #########
-	    # shift the composite container by however much we're bumped
-	    #
-	    $Composite->y($Composite->y() + (1.5 * $row * $h * -$tstrand));
-	    $self->push($Composite);
-	}
+        #########
+        # shift the composite container by however much we're bumped
+        #
+        $Composite->y($Composite->y() + (1.5 * $row * $h * -$tstrand));
+        $self->push($Composite);
+    }
     }
 }
 
