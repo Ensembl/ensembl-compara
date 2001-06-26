85a8ca7573d13d34efd32bf10b90e9da07606f0d
Emmanuel Mongin <mongin@sanger.ac.uk>
2001-06-26 19:49:23 +0000

    Completly changed intron.pl, don't know if its gonna work

--- modules/Bio/EnsEMBL/GlyphSet/intron.pm
+++ modules/Bio/EnsEMBL/GlyphSet/intron.pm
@@ -1,4 +1,4 @@
-package Bio::EnsEMBL::GlyphSet::intron;
+package Bio::EnsEMBL::GlyphSet::pfam;
 use strict;
 use vars qw(@ISA);
 use Bio::EnsEMBL::GlyphSet;
@@ -6,12 +6,14 @@ use Bio::EnsEMBL::GlyphSet;
 use Bio::EnsEMBL::Glyph::Rect;
 use Bio::EnsEMBL::Glyph::Text;
 use Bio::EnsEMBL::Glyph::Composite;
+use ColourMap;
+use Bump;
 
 sub init_label {
     my ($self) = @_;
 
     my $label = new Bio::EnsEMBL::Glyph::Text({
-	'text'      => 'introns',
+	'text'      => 'Intron',
 	'font'      => 'Small',
 	'absolutey' => 1,
     });
@@ -20,70 +22,91 @@ sub init_label {
 
 sub _init {
     my ($self) = @_;
+    my %hash;
+    my $caption       = "Intron";
+    my @bitmap        = undef;
+    my $protein       = $self->{'container'};
+    my $Config        = $self->{'config'};
+    my $pix_per_bp    = $Config->transform->{'scalex'};
+    my $bitmap_length = int($self->{'container'}->length() * $pix_per_bp);
+    my $y             = 0;
+    my $h             = 4;
+    my $cmap          = new ColourMap;
+    my $black         = $cmap->id_by_name('black');
+    my $red           = $cmap->id_by_name('red');
+    my $font          = "Small";
+    my $colour        = $Config->get('intron','col');
+    my ($fontwidth,
+	$fontheight)  = $Config->texthelper->px2bp($font);
 
-    my $protein = $self->{'container'};
-    my $Config = $self->{'config'}; 
+    my @pf_feat = $protein->get_all_IntronFeatures();
+    foreach my $feat(@pf_feat) {
+	push(@{$hash{$feat->feature2->seqname}},$feat);
+    }
     
-    my $y          = 0;
-    my $h          = 4;
-    my $highlights = $self->highlights();
-    my $key = "Intron";
-    
-    my $x = 0;
-    my $w = 0;
-
-    my $colour = $Config->get( 'intron','col');
-
-    my @introns = $protein->each_Intron_feature();
-
-    if (@introns) {
-	my $composite = new Bio::EnsEMBL::Glyph::Composite({
-	    'id'    => $key,
+    foreach my $key (keys %hash) {
+	my @row  = @{$hash{$key}};
+			
+	my $Composite = new Bio::EnsEMBL::Glyph::Composite({
+	    'x'     => $row[0]->feature1->start(),
+	    'y'     => $y,
 	    'zmenu' => {
-		'caption'  => $key,
+		'caption' => "Intron"
 	    },
 	});
-	my $colour = $Config->get( 'prints','col');
-	
-	foreach my $int (@introns) {
-	    my $x      = $int->feature1->start();
-	    my $w      = $int->feature1->end() - $x;
-	    my $id     = $int->feature2->seqname();
-	    my $start  = $int->feature2->start();
-	    my $end    = $int->feature2->end();
-	    my $length = $end - $start;
-	    
+
+	my $pfsave;
+	my ($minx, $maxx);
+		
+	foreach my $pf (@row) {
+	    my $x  = $pf->feature1->start();
+	    $minx  = $x if ($x < $minx || !defined($minx));
+	    my $w  = $pf->feature1->end() - $x;
+	    $maxx  = $pf->feature1->end() if ($pf->feature1->end() > $maxx || !defined($maxx));
+	    my $id = $pf->feature2->seqname();
+
 	    my $rect = new Bio::EnsEMBL::Glyph::Rect({
 		'x'        => $x,
 		'y'        => $y,
 		'width'    => $w,
 		'height'   => $h,
-		'id'       => $id,
 		'colour'   => $colour,
 	    });
-	    $composite->push($rect) if(defined $rect);
+	    $Composite->push($rect);
+	    $pfsave = $pf;
 	}
-	$self->push($composite);
+
+	#########
+	# add a domain linker
+	#
+	my $rect = new Bio::EnsEMBL::Glyph::Rect({
+	    'x'         => $minx,
+	    'y'         => $y + 2,
+	    'width'     => $maxx - $minx,
+	    'height'    => 0,
+	    'colour'    => $colour,
+	    'absolutey' => 1,
+	});
+	$Composite->push($rect);
+
+	my $desc = $pfsave->idesc();
+
+	my $text = new Bio::EnsEMBL::Glyph::Text({
+	    'font'   => $font,
+	    'text'   => $desc,
+	    'x'      => $row[0]->feature1->start(),
+	    'y'      => $h + 1,
+	    'height' => $fontheight,
+	    'width'  => $fontwidth * length($desc),
+	    'colour' => $black,
+	});
+	$Composite->push($text);
+	
+	$self->push($Composite);
     }
+
 }
+
 1;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
