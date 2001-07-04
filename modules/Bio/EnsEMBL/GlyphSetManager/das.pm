940c0f37f125ecaef52292132bf0d41ad41d9ea5
Web Admin <w3adm@sanger.ac.uk>
2001-07-04 14:22:28 +0000

    Added das manager to allow for the inclusion of dynamically generated glyphsets

---
+++ modules/Bio/EnsEMBL/GlyphSetManager/das.pm
@@ -0,0 +1,67 @@
+package Bio::EnsEMBL::GlyphSetManager::das;
+use strict;
+use Bio::EnsEMBL::GlyphSetManager;
+use Bio::EnsEMBL::GlyphSet::das;
+use vars qw(@ISA);
+@ISA = qw(Bio::EnsEMBL::GlyphSetManager);
+use Data::Dumper;
+use ExternalDAS;
+
+##
+## 2001/07/03	js5		Added external DAS source code
+## 2001/07/04	js5		Added sub add_glyphset to remove duplication in code in init!
+##
+my $species_defs = $ENV{'ENSEMBL_SPECIES'} . qq(_Defs);
+eval "require $species_defs";
+if ($@){ die "Can't use ${species_defs}.pm - $@\n"; }
+
+sub init {
+    my ($self) = @_;
+
+    $self->label("Das Sources");
+
+    my $Config = $self->{'config'};
+    my @das_source_names = keys %{$species_defs->ENSEMBL_INTERNAL_DAS_SOURCES};
+    #########
+    # apply parallelisation here |
+    #                            V
+    #
+    for my $das_source_name (@das_source_names) {
+		next unless( $Config->get($das_source_name,'on') eq 'on' );
+		my $extra_config = $species_defs->ENSEMBL_INTERNAL_DAS_SOURCES->{$das_source_name};
+		$extra_config->{'name'} = $das_source_name;
+		$self->add_glyphset( $extra_config );
+	}
+	my $ext_das = new ExternalDAS();
+	$ext_das->get_sources();
+
+    for my $das_source_name ( keys %{$ext_das->{'data'}} ) {
+		next unless( $Config->get("extdas_$das_source_name",'on') eq 'on' );
+		my $extra_config 		    = $ext_das->{'data'}->{$das_source_name};
+		$extra_config->{'name'} 	= "extdas_$das_source_name";
+		$extra_config->{'url'} 		= "http://$extra_config->{'URL'}/das";
+		$self->add_glyphset( $extra_config );		
+	}	
+}
+
+sub add_glyphset {
+	my ($self,$extra_config) = @_;	
+		
+	my $das_glyphset;
+	eval {
+		$das_glyphset = new Bio::EnsEMBL::GlyphSet::das(
+			$self->{'container'},
+			$self->{'config'},
+			$self->{'highlights'},
+			$self->{'strand'},
+		$extra_config);
+	};
+							   
+	if($@) {
+		print STDERR "DAS GLYPHSET $extra_config->{'name'} failed\n";
+	} else {
+		push @{$self->{'glyphsets'}}, $das_glyphset;
+	}
+}
+
+1;
