3bea5d80bf3bf07e4fd6e15d8d906fba14d4299f
Web Admin <w3adm@sanger.ac.uk>
2001-07-04 14:16:54 +0000

    Modified das glyphset to include extra link text if this is available

--- modules/Bio/EnsEMBL/GlyphSet/das.pm
+++ modules/Bio/EnsEMBL/GlyphSet/das.pm
@@ -9,7 +9,7 @@ use SiteDefs;
 
 sub init_label {
     my ($self) = @_;
-	return if( defined $self->{'config'}->{'_no_label'} );
+    return if( defined $self->{'config'}->{'_no_label'} );
     my $label = new Bio::EnsEMBL::Glyph::Text({
 	'text'      => $self->{'extras'}->{'caption'},
 	'font'      => 'Small',
@@ -23,34 +23,42 @@ sub _init {
     return unless ($self->strand() == -1);
 	
     my $Config         	= $self->{'config'};
-    my $feature_colour 	= $Config->get($self->das_name(), 'col');
+    my $feature_colour 	= $Config->get($self->das_name(), 'col') || $Config->colourmap()->id_by_name('contigblue1');
     my $vc 		= $self->{'container'};
     my $length 		= $vc->length();
     
     my @features = $vc->get_all_ExternalFeatures();
+    my $link_text = $self->{'extras'}->{'linktext'} || 'Additional info';
     foreach my $f(@features){
-	next unless ($f->primary_tag() eq "das" && $f->source_tag() eq $self->{'extras'}->{'dsn'});
-	
-	my $id     = $f->das_id();
-#	my $type   = $f->das_name();
-#	my $dsn    = $f->das_dsn();
-#	my $source = $f->source_tag();
-#	my $strand = $f->strand();
-	
-	my $glyph = new Bio::EnsEMBL::Glyph::Rect({
+		next unless ($f->primary_tag() eq "das" && $f->source_tag() eq $self->{'extras'}->{'dsn'});
+		my $id     = $f->das_id();
+		#	my $type   = $f->das_name();
+		#	MY $dsn    = $f->das_dsn();
+		#	my $source = $f->source_tag();
+		#	my $strand = $f->strand();
+
+		my $zmenu = {
+                	'caption'         => $self->{'extras'}->{'label'},
+                	"DAS source info" => $self->{'extras'}->{'url'}
+        	};
+		# JS5: If we have an ID then we can add this to the Zmenu and
+		#      also see if we can make a link to any additional information
+		#      about the source.
+		if($id && $id ne 'null') {
+            	$zmenu->{$link_text} = $self->{'extras'}->{'linkURL'}.$id
+			if($self->{'extras'}->{'linkURL'});
+	    	$zmenu->{$id} = '';
+		}
+		my $glyph = new Bio::EnsEMBL::Glyph::Rect({
             'x'      	=> $f->start(),
-	    'y'      	=> 0,
-	    'width'  	=> $f->end()-$f->start(),
-	    'height' 	=> 8,
-	    'colour' 	=> $feature_colour,
-	    'absolutey' => 1,
-            'zmenu'     => {
-                'caption' 	  => $self->{'extras'}->{'label'},
-                $id 		  => "",
-                "DAS source info" => $self->{'extras'}->{'url'},
-	    }
-	});
-	$self->push($glyph);
+	    	'y'      	=> 0,
+	    	'width'  	=> $f->end()-$f->start(),
+	    	'height' 	=> 8,
+	    	'colour' 	=> $feature_colour,
+	    	'absolutey' => 1,
+            'zmenu'     => $zmenu
+		});
+		$self->push($glyph);
     }
 }
 
