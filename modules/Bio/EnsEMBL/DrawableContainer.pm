7163c6eb534a5e7a9060e1020b40d272fb5f3f03
Web Admin <w3adm@sanger.ac.uk>
2001-07-04 14:19:45 +0000

    Added additional code to account for External das sources

--- modules/Bio/EnsEMBL/DrawableContainer.pm
+++ modules/Bio/EnsEMBL/DrawableContainer.pm
@@ -2,8 +2,8 @@ package Bio::EnsEMBL::DrawableContainer;
 use Bio::Root::RootI;
 use strict;
 use vars qw(@ISA);
-use Bio::EnsEMBL::Utils::Eprof qw(eprof_start eprof_end);
 use Bio::EnsEMBL::Glyph::Rect;
+use Bio::EnsEMBL::GlyphSetManager::das;
 
 @ISA = qw(Bio::Root::RootI);
 
@@ -37,117 +37,137 @@ sub new {
     #########
     # loop over all the glyphsets the user wants:
     #
-
-    my @subsections = $Config->subsections();
-
-    my @order = sort { $Config->get($a, 'pos') <=> $Config->get($b, 'pos') } @subsections;
-
-    &eprof_start('glyphset_creation');
-
-    #########
-    # instantiate our list of glyphsets
-    #
     for my $strand (@strands_to_show) {
-      my @tmp;
 
-      if($strand == 1) {
-	@tmp = reverse @order;
-      } else {
-        @tmp = @order;
-      }
-
-      for my $row (@tmp) {
-	#########
-	# skip this row if user has it turned off
-	#
-	next if ($Config->get($row, 'on') eq "off");
-	next if ($Config->get($row, 'str') eq "r" && $strand != -1);
-	next if ($Config->get($row, 'str') eq "f" && $strand != 1);
-
-	#########
-	# create a new glyphset for this row
-	#
-	my $classname = qq(Bio::EnsEMBL::GlyphSet::$row);
-
-	#########
-	# require & import the package
-	#
-	eval "require $classname";
-
-	if($@) {
-	    print STDERR qq(DrawableContainer::new failed to require $classname: $@\n);
-	    next;
+	my $tmp_glyphset_store = {};
+	for my $row ($Config->subsections()) {
+	    #########
+	    # skip this row if user has it turned off
+	    #
+	    next unless ($Config->get($row, 'on') eq "on");
+	    next if ($Config->get($row, 'str') eq "r" && $strand != -1);
+	    next if ($Config->get($row, 'str') eq "f" && $strand != 1);
+	    
+	    if(substr($row, 0, 4) ne "das_") {
+		#########
+		# create a new glyphset for this row
+		#
+		my $classname = qq(Bio::EnsEMBL::GlyphSet::$row);
+		
+		#########
+		# require & import the package
+		#
+		eval "require $classname";
+		
+		if($@) {
+		    print STDERR qq(DrawableContainer::new failed to require $classname: $@\n);
+		    next;
+		}
+		$classname->import();
+		
+		#########
+		# generate a set for both strands
+		#
+                my $GlyphSet;
+		eval {
+			$GlyphSet = new $classname($Container, $Config, $highlights, $strand);
+		};
+		if($@) {
+			print STDERR "GLYPHSET $classname failed\n";
+                } else {
+			$tmp_glyphset_store->{$Config->get($row, 'pos')} = $GlyphSet;
+		}
+	    }
 	}
-	$classname->import();
 
 	#########
-	# generate a set for both strands
+	# install the glyphset managers
 	#
-	my $GlyphSet = new $classname($Container, $Config, $highlights, $strand);
+	my $DasManager = new Bio::EnsEMBL::GlyphSetManager::das($Container, $Config, $highlights, $strand);
+	my $das_offset = 200;
+	for my $glyphset ($DasManager->glyphsets()) {
+	    my $row = $glyphset->das_name();
 
-	push @{$self->{'glyphsets'}}, $GlyphSet;
-      }
+	    #########
+	    # skip this row if user has it turned off
+	    #
+	    next unless ($Config->get($row, 'on') eq "on");
+		
+	    $tmp_glyphset_store->{$Config->get($row, 'pos')||$das_offset++} = $glyphset;
+	}
+
+	#########
+	# sort out the resulting mess
+	#
+	my @tmp = ();
+
+	my @keys = keys %{$tmp_glyphset_store};
+	@keys = sort { $a <=> $b } @keys;
+
+	for my $key (@keys) {
+	    push @tmp, $tmp_glyphset_store->{$key};
+	}
+	@tmp = reverse @tmp if($strand == 1);
+	push @{$self->{'glyphsets'}}, @tmp;
     }
-
+	
     #########
     # calculate real scaling here
     # 
     my $spacing = $self->{'spacing'};
-
+    
     #########
     # calculate the maximum label width (plus margin)
     #
     my $label_length_px = 0;
-
+    
     for my $glyphset (@{$self->{'glyphsets'}}) {
 	next if(!defined $glyphset->label());
-
+	
 	my $chars  = length($glyphset->label->text());
 	my $pixels = $chars * $Config->texthelper->width($glyphset->label->font());
 	
 	$label_length_px = $pixels if($pixels > $label_length_px);
-
+	
 	#########
 	# just for good measure:
 	#
 	$glyphset->label->width($label_length_px);
     }
-
+    
     #########
     # add spacing before and after labels
     #
     $label_length_px += $spacing * 2;
-
+    
+    print STDERR "\nLABEL LENGTH: $label_length_px\n\n";
     #########
     # calculate scaling factors
     #
     my $pseudo_im_width = $Config->image_width() - $label_length_px - $spacing;
-
+    
     #########
     # set scaling factor for base-pairs -> pixels
     #
     my $scalex = $pseudo_im_width / $Config->container_width();
     $Config->{'transform'}->{'scalex'} = $scalex;
-
-
-    print STDERR qq(my config is $Config and my image_width = ), $Config->image_width(), qq(\n);
-
+    
     #########
     # set scaling factor for 'absolutex' coordinates -> real pixel coords
     #
     $Config->{'transform'}->{'absolutescalex'} = $pseudo_im_width / $Config->image_width();
-
+    
     #########
     # because our text label starts are < 0, translate everything back onto the canvas
     #
     my $extra_translation = $label_length_px;
     $Config->{'transform'}->{'translatex'} += $extra_translation;
-
+    
     for my $glyphset (@{$self->{'glyphsets'}}) {
 	next if(!defined $glyphset->label());
 	$glyphset->label->x(-($extra_translation - $spacing) / $scalex);
     }
-
+    
     #########
     # pull out alternating background colours for this script
     #
@@ -156,27 +176,27 @@ sub new {
 	'0' => $Config->get('_settings', 'bgcolour1') || $white,
 	'1' => $Config->get('_settings', 'bgcolour2') || $white,
     };
-
+    
     my $bgcolour_flag;
     $bgcolour_flag = 1 if($$bgcolours{0} ne $$bgcolours{1});
-
+    
     #########
     # go ahead and do all the database work
     #
     my $yoffset = $spacing;
     my $iteration = 0;
     for my $glyphset (@{$self->{'glyphsets'}}) {
-
+	
 	#########
 	# load everything from the database
 	#
 	$glyphset->_init();
-
+	
 	#########
 	# don't waste any more time on this row if there's nothing in it
 	#
 	next if(scalar @{$glyphset->{'glyphs'}} == 0);
-
+	
 	#########
 	# remove any whitespace at the top of this row
 	#
@@ -223,8 +243,6 @@ sub new {
 	$iteration ++;
     }
 
-    &eprof_end('glyphset_creation');
-
     return $self;
 }
 
@@ -253,9 +271,7 @@ sub render {
     #########
     # big, shiny, rendering 'GO' button
     #
-    &eprof_start(qq(renderer_creation_$type));
     my $renderer = $renderer_type->new($self->{'config'}, $self->{'vc'}, $self->{'glyphsets'});
-    &eprof_end(qq(renderer_creation_$type));
 
     return $renderer->canvas();
 }
