ad97bb70b4930757ff31b2032bb687264e434fe2
Web Admin <w3adm@sanger.ac.uk>
2001-09-19 10:43:14 +0000

    White space tidy

--- modules/Bio/EnsEMBL/GlyphSet_feature.pm
+++ modules/Bio/EnsEMBL/GlyphSet_feature.pm
@@ -10,11 +10,11 @@ use Bump;
 
 sub init_label {
     my ($self) = @_;
-	return if( defined $self->{'config'}->{'_no_label'} );
+    return if( defined $self->{'config'}->{'_no_label'} );
     my $label = new Bio::EnsEMBL::Glyph::Text({
-	'text'      => $self->my_label(),
-	'font'      => 'Small',
-	'absolutey' => 1,
+        'text'      => $self->my_label(),
+        'font'      => 'Small',
+        'absolutey' => 1,
     });
     $self->label($label);
 }
@@ -32,8 +32,10 @@ sub features {
 sub zmenu {
     my ($self, $id ) = @_;
 
-    return { 'caption' => "Unknown",
-	     "$id"     => "You must write your own zmenu call" };
+    return {
+        'caption' => "Unknown",
+        "$id"     => "You must write your own zmenu call"
+    };
 }
 
 sub _init {
@@ -58,86 +60,77 @@ sub _init {
     my @glyphs;
 
     foreach my $f ($self->features){
-	unless ( $id{$f->id()} ) {
-	    $id{$f->id()} = [];
-	}
-	push(@{$id{$f->id()}}, $f );
+        $id{$f->id()} = [] unless $id{$f->id()};
+        push(@{$id{$f->id()}}, $f );
     }
-($type, 'dep');
 
     my @glyphs;
 
-    foreach my $f ($self->features){
-	unless ( $id{$f->id()} ) {
-	    $id{$f->id()} = [];
-	}
-	push(@{$id{$f->id()}}, $f );
-    }
     foreach my $i (keys %id){
-	@{$id{$i}} =  sort {$a->start() <=> $b->start() } @{$id{$i}};
-	my $j = 1;
-	
-	my $has_origin = undef;
-	my $estid = $i;
-	$estid =~ s/(.*?)\.\d+/$1/;
-	
-	my $Composite = new Bio::EnsEMBL::Glyph::Composite({
-	    'zmenu'     => $self->zmenu( $i )
-	});
-	foreach my $f (@{$id{$i}}){
-	    unless (defined $has_origin){
-		$Composite->x($f->start());
-		$Composite->y(0);
-		$has_origin = 1;
-	    }
-	    my $glyph = new Bio::EnsEMBL::Glyph::Rect({
-		'x'      	=> $f->start(),
-		'y'      	=> 0,
-		'width'  	=> $f->length(),
-		'height' 	=> $h,
-		'colour' 	=> $feature_colour,
-		'absolutey' => 1,
-		'_feature' 	=> $f, 
-	    });
-	    $Composite->push($glyph);
-	    $j++;
-	}
-	
-	if ($dep > 0){ # we bump
-	    my $bump_start = int($Composite->x() * $pix_per_bp);
-	    $bump_start = 0 if ($bump_start < 0);
-	    
-	    my $bump_end = $bump_start + ($Composite->width() * $pix_per_bp);
-            if ($bump_end > $bitmap_length){$bump_end = $bitmap_length};
-	    my $row = &Bump::bump_row(
-				      $bump_start,
-				      $bump_end,
-				      $bitmap_length,
-				      \@bitmap
-				      );
-	    
-	    next if ($row > $dep);
-	    $Composite->y($Composite->y() + (1.5 * $row * $h * -$strand));
-	    
-	    # if we are bumped && on a large contig then draw frames around features....
-	    $Composite->bordercolour($feature_colour) unless ($small_contig);
-	}
-	push @glyphs ,$Composite;
-	if(exists $highlights{$i}) {
+        @{$id{$i}} =  sort {$a->start() <=> $b->start() } @{$id{$i}};
+        my $j = 1;
+    
+        my $has_origin = undef;
+        my $estid = $i;
+        $estid =~ s/(.*?)\.\d+/$1/;
+    
+        my $Composite = new Bio::EnsEMBL::Glyph::Composite({
+            'zmenu'     => $self->zmenu( $i )
+        });
+        foreach my $f (@{$id{$i}}){
+            unless (defined $has_origin){
+                $Composite->x($f->start());
+                $Composite->y(0);
+                $has_origin = 1;
+            }
             my $glyph = new Bio::EnsEMBL::Glyph::Rect({
-		'x'		=> $Composite->x() - 1/$pix_per_bp,
-		'y'		=> $Composite->y()-1,
-		'width'		=> $Composite->width() + 2/$pix_per_bp,
-		'height'	=> $h + 2,
-		'colour'	=> $hi_colour,
-		'absolutey' => 1,
+                'x'          => $f->start(),
+                'y'          => 0,
+                'width'      => $f->length(),
+                'height'     => $h,
+                'colour'     => $feature_colour,
+                'absolutey' => 1,
+                '_feature'     => $f, 
+            });
+            $Composite->push($glyph);
+            $j++;
+        }
+    
+        if ($dep > 0){ # we bump
+            my $bump_start = int($Composite->x() * $pix_per_bp);
+            $bump_start = 0 if ($bump_start < 0);
+            
+            my $bump_end = $bump_start + ($Composite->width() * $pix_per_bp);
+            $bump_end = $bitmap_length if ($bump_end > $bitmap_length);
+            my $row = &Bump::bump_row(
+                $bump_start,
+                $bump_end,
+                $bitmap_length,
+                \@bitmap
+            );
+
+            next if ($row > $dep);
+            $Composite->y($Composite->y() + (1.5 * $row * $h * -$strand));
+        
+            # if we are bumped && on a large contig then draw frames around features....
+            $Composite->bordercolour($feature_colour) unless ($small_contig);
+        }
+        push @glyphs ,$Composite;
+        if(exists $highlights{$i}) {
+            my $glyph = new Bio::EnsEMBL::Glyph::Rect({
+                'x'        => $Composite->x() - 1/$pix_per_bp,
+                'y'        => $Composite->y()-1,
+                'width'        => $Composite->width() + 2/$pix_per_bp,
+                'height'    => $h + 2,
+                'colour'    => $hi_colour,
+                'absolutey' => 1,
             });
             $self->push($glyph);
-	}
+        }
     }
 
     foreach ( @glyphs ) {
-	$self->push($_);
+        $self->push($_);
     }
 }
 
