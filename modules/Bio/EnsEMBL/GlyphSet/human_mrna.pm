9a8751f956e406b8a8fa84aba21a216bd8c78cfc
Web Admin <w3adm@sanger.ac.uk>
2002-01-10 16:37:12 +0000

    new feature tracks

---
+++ modules/Bio/EnsEMBL/GlyphSet/human_mrna.pm
@@ -0,0 +1,23 @@
+package Bio::EnsEMBL::GlyphSet::human_mrna;
+use strict;
+use vars qw(@ISA);
+use Bio::EnsEMBL::GlyphSet_feature;
+
+@ISA = qw(Bio::EnsEMBL::GlyphSet_feature);
+
+sub my_label { return "Human mRNA"; }
+
+sub features {
+    my ($self) = @_;
+    return $self->{'container'}->get_all_SimilarityFeatures_above_score("human_mrna",80,$self->glob_bp);
+}
+
+sub href {
+    my ($self, $id ) = @_;
+    return $self->{'config'}->{'ext_url'}->get_url('EMBL',$id);
+}
+sub zmenu {
+    my ($self, $id ) = @_;
+    return { 'caption' => "$id", "EMBL: $id" => $self->href( $id ) };
+}
+1;
---
+++ modules/Bio/EnsEMBL/GlyphSet/human_swall.pm
@@ -0,0 +1,27 @@
+package Bio::EnsEMBL::GlyphSet::human_swall;
+use strict;
+use vars qw(@ISA);
+use Bio::EnsEMBL::GlyphSet_feature;
+
+@ISA = qw(Bio::EnsEMBL::GlyphSet_feature);
+
+sub my_label { return "Human swall"; }
+
+sub features {
+    my ($self) = @_;
+    return $self->{'container'}->get_all_SimilarityFeatures_above_score(
+        "human_swall", 80, $self->glob_bp
+    );
+}
+
+sub href {
+    my ( $self, $id ) = @_;
+    return $self->{'config'}->{'ext_url'}->get_url( $id=~/^NP/ ? 'REFSEQPROTEIN' : 'SWISS', $id );
+}
+
+sub zmenu {
+    my ($self, $id ) = @_;
+    $id =~ s/(.*)\.\d+/$1/o;
+    return { 'caption' => "$id", "Protein homology" => $self->href( $id ) };
+}
+1;
