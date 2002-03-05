e2895965c75e6cf53b428e4d98ecbf7c535ce0f1
Web Admin <w3adm@sanger.ac.uk>
2002-03-05 10:44:21 +0000

    made expression link disappear if no expression DB

--- modules/Bio/EnsEMBL/GlyphSet/transcript_lite.pm
+++ modules/Bio/EnsEMBL/GlyphSet/transcript_lite.pm
@@ -49,17 +49,20 @@ sub zmenu {
     my ($self, $vt) = @_;
     my $vtid = $vt->{'stable_id'};
     my $id   = $vt->{'synonym'} eq '' ? $vtid : $vt->{'synonym'};
-    return {
+    my $zmenu = {
         'caption'                       => $id,
         "00:Transcr:$vtid"              => "",
         "01:(Gene:$vt->{'gene'})"       => "",
         '03:Transcript information'     => "/$ENV{'ENSEMBL_SPECIES'}/geneview?gene=$vt->{'gene'}",
         '04:Protein information'        => "/$ENV{'ENSEMBL_SPECIES'}/protview?peptide=".$vt->{'translation'},
         '05:Supporting evidence'        => "/$ENV{'ENSEMBL_SPECIES'}/transview?transcript=$vtid",
-        '06:Expression information'     => "/$ENV{'ENSEMBL_SPECIES'}/sageview?alias=$vt->{'gene'}",
         '07:Protein sequence (FASTA)'   => "/$ENV{'ENSEMBL_SPECIES'}/exportview?tab=fasta&type=feature&ftype=peptide&id=$vtid",
         '08:cDNA sequence'              => "/$ENV{'ENSEMBL_SPECIES'}/exportview?tab=fasta&type=feature&ftype=cdna&id=$vtid",
     };
+    my $DB = EnsWeb::species_defs->databases;
+    $zmenu->{'06:Expression information'}
+      = "/$ENV{'ENSEMBL_SPECIES'}/sageview?alias=$vt->{'gene'}" if $DB->{'ENSEMBL_EXPRESSION'};
+    return $zmenu;
 }
 
 sub text_label {
