abc28445ce9577456a49628bb8954c721080e2fe
Abel Ureta-Vidal <abel@sanger.ac.uk>
2002-03-07 11:12:45 +0000

    changes to do ->prepare only once. Update doc and sql query file in consequence

--- scripts/dumps/dump_by_stepped_align_id.pl
+++ scripts/dumps/dump_by_stepped_align_id.pl
@@ -4,15 +4,42 @@ use strict;
 use Getopt::Long;
 use Bio::EnsEMBL::Compara::DBSQL::DBAdaptor;
 
-my $host   = 'ecs1b.sanger.ac.uk';
-my $dbname = 'abel_compara_human_mouse';
+$| = 1;
+
+my $usage = "
+Usage: $0 [options] SQL_query_file
+
+Dump from compara database, following the SQL query in SQL_query_file
+which must contain one and only one 'align_id=?' statement.
+
+ -help show this menu
+ -h    compara database hostname
+ -d    compara database name
+ -u    database user name (default: ensro)
+ -o    output file name
+
+";
+
+my $help = 0;
+my $host;#   = 'ecs1b.sanger.ac.uk';
+my $dbname;# = 'abel_compara_humanNCBI28_mouse';
 my $dbuser = 'ensro';
 my $output;
 
-GetOptions('host:s' => \$host,
-	   'dbname:s' => \$dbname,
-	   'dbuser:s' => \$dbuser,
+GetOptions('help' => \$help,
+	   'h=s' => \$host,
+	   'd=s' => \$dbname,
+	   'u=s' => \$dbuser,
 	   'o:s' => \$output);
+
+die $usage if ($help);
+if (! defined $host) {
+  warn "\n Must specify a host with -h\n";
+  die $usage;
+} elsif (! defined $dbname) {
+  warn "\n Must specify a database with -d\n";
+  die $usage;
+}
 
 my $db = new Bio::EnsEMBL::Compara::DBSQL::DBAdaptor(-host => $host,
 						     -dbname => $dbname,
@@ -34,27 +61,36 @@ if (! defined $output) {
   $output = \*OUTPUT;
 }
 
+print STDERR "Opening and reading SQL file...";
+
 open SQL, "$sql_file" || die "could not open $sql_file; $!\n";
 my $sql_query = <SQL>;
 close SQL;
 chomp $sql_query;
 
+print STDERR "Done\n";
+
 my $ga = $db->get_GenomicAlignAdaptor();
 
-foreach my $gid ( $ga->list_align_ids() ) {
+print STDERR "Preparing SQL query...";
 
-  my $local_sql_query = $sql_query;
-  $local_sql_query =~ s/<align_id>/$gid/g;
+my $sth = $db->prepare("$sql_query");
 
-  my $sth = $db->prepare("$local_sql_query");
-  unless ($sth->execute()) {
+print STDERR "Done\n";
+print STDERR "Getting list of align_ids and print in STDOUT...";
+
+foreach my $gid ($ga->list_align_ids()) {
+
+  unless ($sth->execute($gid)) {
     $db->throw("Failed execution of a select query");
   }
-
+  
   while (my @columns = $sth->fetchrow_array()) {
     print $output join("\t",@columns),"\n";
   }
-
+  
 }
+
+print STDERR "Done\n";
 
 close $output;
