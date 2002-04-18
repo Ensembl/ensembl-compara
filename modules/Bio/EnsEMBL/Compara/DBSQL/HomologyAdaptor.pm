ab7b09a7ea336cea5118ca1490294e3bdd162ddf
Stephen Searle <searle@sanger.ac.uk>
2002-04-18 13:24:30 +0000

    Added list_stable_ids_from_species to retrieve all the stable ids from a specified species.
    Fixed get_homologues_by_species_transcript to return all the homologues
    rather than just the first

--- modules/Bio/EnsEMBL/Compara/DBSQL/HomologyAdaptor.pm
+++ modules/Bio/EnsEMBL/Compara/DBSQL/HomologyAdaptor.pm
@@ -31,10 +31,51 @@ sub get_homologues_by_species_transcript  {
              where gd.genome_db_id=grm.genome_db_id and gr.gene_relationship_id=grm.gene_relationship_id 
              and gd.name='$species' and grm.member_stable_id='$gene' group by grm.gene_relationship_id";
 
-    my @transcripts=$self->_get_genes_by_relationship_internal_id($hspecies,$self->_get_relationship($q));
+
+    my @relationshipids = $self->_get_relationships($q);
+
+    my @transcripts;
+    foreach my $rel (@relationshipids) {
+      push @transcripts, $self->_get_genes_by_relationship_internal_id($hspecies,$rel);
+    }
+
+    return @transcripts;
+
+}                               
 
 
+=head2 list_stable_ids_from_species
 
+ Title   : list_stable_ids_from_species
+ Usage   : $db->list_stable_ids_from_species('Homo_sapiens')
+ Function: Find all the stable ids in the gene_relationship_member table 
+           from a specified species 
+ Example :
+ Returns : array from transcript stable ids
+ Args    : species
+
+=cut
+
+sub list_stable_ids_from_species  {
+
+    my ($self,$species)=@_;
+
+    my $q ="select grm.member_stable_id 
+            from gene_relationship_member grm,genome_db gd 
+            where gd.genome_db_id=grm.genome_db_id and gd.name='$species'";
+
+    
+    my @transcripts;
+
+    my $sth = $self->prepare($q);
+    $sth->execute();
+
+    my $id;
+    while (($id) = $sth->fetchrow_array) {
+      push (@transcripts,$id);
+    }
+
+    return @transcripts;
 }                               
 
 
@@ -51,22 +92,35 @@ sub  _get_genes_by_relationship_internal_id {
 
 }
 
-
-
-
 sub _get_gene_stable_ids {
     my ($self,$q)=@_;
 
     $q = $self->prepare($q);
     $q->execute();
 
-    return $q->fetchrow_array;
-
-
-
+    my @transcripts;
+    my $id;
+    while (($id) = $q->fetchrow_array) {
+      push (@transcripts,$id);
+    }
+    return @transcripts;
+    #return $q->fetchrow_array;
 }
 
 
+sub _get_relationships {
+    my ($self,$q)=@_;
+
+    $q = $self->prepare($q);
+    $q->execute();
+
+    my @ids;
+    my $id;
+    while (($id) = $q->fetchrow_array) {
+      push (@ids,$id);
+    }
+    return @ids;
+}
 
 
 sub _get_relationship {
