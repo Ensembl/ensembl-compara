ec9ff0e24d766ea52ef9b326d3b135326507a15e
James Stalker <jws@sanger.ac.uk>
2002-05-20 14:15:08 +0000

    Haplotype and snplotype(tm) drawing code

---
+++ modules/Bio/EnsEMBL/GlyphSet/haplotype.pm
@@ -0,0 +1,49 @@
+package Bio::EnsEMBL::GlyphSet::haplotype;
+use strict;
+use EnsWeb;
+use vars qw(@ISA);
+use Bio::EnsEMBL::GlyphSet_simple;
+use Bio::EnsEMBL::DBSQL::DBAdaptor;
+use Bio::EnsEMBL::ExternalData::Haplotype::HaplotypeAdaptor;
+@ISA = qw(Bio::EnsEMBL::GlyphSet_simple);
+
+sub my_label { return "Haplotypes"; }
+
+sub features {
+    my ($self) = @_;
+    print STDERR "Getting adaptor!!!\n";
+    unless( $self->{'config'}->{'_hap_adaptor'} ) {
+        my $db_details = EnsWeb::species_defs->databases;
+        print STDERR "DB: ".$db_details->{'ENSEMBL_HAPLOTYPE'}{'NAME'}."\n";
+        print STDERR "UN: ".$db_details->{'ENSEMBL_HAPLOTYPE'}{'USER'}."\n";
+        print STDERR "PW: ".$db_details->{'ENSEMBL_HAPLOTYPE'}{'PASS'}."\n";
+        print STDERR "HT: ".$db_details->{'ENSEMBL_HAPLOTYPE'}{'HOST'}."\n";
+        print STDERR "PT: ".$db_details->{'ENSEMBL_HAPLOTYPE'}{'PORT'}."\n";
+        my $dbad = Bio::EnsEMBL::DBSQL::DBAdaptor->new( 
+            -dbname => $db_details->{'ENSEMBL_HAPLOTYPE'}{'NAME'},
+            -user   => $db_details->{'ENSEMBL_HAPLOTYPE'}{'USER'},
+            -pass   => $db_details->{'ENSEMBL_HAPLOTYPE'}{'PASS'},
+            -host   => $db_details->{'ENSEMBL_HAPLOTYPE'}{'HOST'},
+            -port   => $db_details->{'ENSEMBL_HAPLOTYPE'}{'PORT'},
+        );
+        $self->{'config'}->{'_hap_adaptor'} = Bio::EnsEMBL::ExternalData::Haplotype::HaplotypeAdaptor->new( $dbad );
+    }
+    print STDERR "Getting data!!!\n";
+    return $self->{'container'}->get_Haplotypes_start_end(
+        $self->{'config'}->{'_hap_adaptor'}
+    );
+}
+
+sub href {
+    my ($self, $f ) = @_;
+    return "/$ENV{'ENSEMBL_SPECIES'}/haploview?haplotype=".$f->id;
+}
+sub zmenu {
+    my ($self, $f ) = @_;
+    return { 
+        'caption'        => $f->id,
+	'Haplotype info' => $self->href($f)
+    };
+}
+
+1;
