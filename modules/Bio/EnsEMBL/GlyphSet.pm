cb7c532afcf295e0083f928d94da8b9b933c6b99
Web Admin <w3adm@sanger.ac.uk>
2003-01-20 12:00:09 +0000

    added CIGAR line code drawer

--- modules/Bio/EnsEMBL/GlyphSet.pm
+++ modules/Bio/EnsEMBL/GlyphSet.pm
@@ -67,4 +67,57 @@ sub zoom_zmenu {
     };
 }
 
+sub draw_cigar_feature {
+  my( $self, $Composite, $f, $h, $feature_colour, $delete_colour ) = @_;
+## Find the 5' end of the feature.. (start if on forward strand of forward feature....)
+  my $S = (my $O = $self->strand ) == 1 ? $f->start : $f->end;
+  my $length = $self->{'container'}->length;
+
+  my @delete;
+
+## Parse the cigar string, splitting up into an array
+## like ('10M','2I','30M','I','M','20M','2D','2020M');
+## original string - "10M2I30MIM20M2D2020M"
+  foreach( $f->cigar_string=~/(\d*[MDI])/g ) {
+## Split each of the {number}{Letter} entries into a pair of [ {number}, {letter} ] 
+## representing length and feature type ( 'M' -> 'Match/mismatch', 'I' -> Insert, 'D' -> Deletion )
+## If there is no number convert it to [ 1, {letter} ] as no-number implies a single base pair...
+    my ($l,$type) = /^(\d+)([MDI])/ ? ($1,$2):(1,$_);
+## If it is a D (this is a deletion) and so we note it as a feature between the end
+## of the current and the start of the next feature...
+##                      ( current start, current start - ORIENTATION )
+## otherwise it is an insertion or match/mismatch
+##  we compute next start sa ( current start, next start - ORIENTATION ) 
+##  next start is current start + (length of sub-feature) * ORIENTATION 
+    my $s = $S;
+    my $e = ( $S += ( $type eq 'D' ? 0 : $l*$O ) ) - $O;
+## If a match/mismatch - draw box....
+    if($type eq 'M') {
+      ($s,$e) = ($e,$s) if $s>$e;      ## Sort out flipped features...
+      next if $e < 1 || $s > $length;  ## Skip if all outside the box...
+      $s = 1       if $s<1;            ## Trim to area of box...
+      $e = $length if $e>$length;
+      $Composite->push(new Sanger::Graphics::Glyph::Rect({
+        'x'          => $s-1,            'y'          => 0, 
+        'width'      => $e-$s+1,         'height'     => $h,
+        'colour'     => $feature_colour, 'absolutey'  => 1,
+      }));
+## If a deletion temp store it so that we can draw after all matches....
+    } elsif($type eq 'D') {
+      ($s,$e) = ($e,$s) if $s<$e;
+      next if $e < 1 || $s > $length;  ## Skip if all outside box
+      push @delete, $e;
+    }
+  }
+
+## Draw deletion markers....
+  foreach (@delete) {
+    $Composite->push(new Sanger::Graphics::Glyph::Rect({
+      'x'          => $_,              'y'          => 0, 
+      'width'      => 0,               'height'     => $h,
+      'colour'     => $delete_colour,  'absolutey'  => 1,
+    }));
+  }
+}
+
 1;
