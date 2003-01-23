fd034053f528e51a1d541c44f1ecb455582361be
Web Admin <w3adm@sanger.ac.uk>
2003-01-23 10:01:40 +0000

    added xref type genes to vert. genes

--- modules/Bio/EnsEMBL/GlyphSet/Vgenes.pm
+++ modules/Bio/EnsEMBL/GlyphSet/Vgenes.pm
@@ -31,26 +31,43 @@ sub _init {
     my $Config = $self->{'config'};
     my $chr      = $self->{'container'}->{'chr'};
    	my $genes_col = $Config->get( 'Vgenes','col_genes' );
+   	my $xref_col  = $Config->get( 'Vgenes','col_xref' );
    	my $known_col = $Config->get( 'Vgenes','col_known' );
 	
 	
     my $known_genes = $self->{'container'}->{'da'}->get_density_per_chromosome_type($chr,'kngene');
+    my $xref_genes = $self->{'container'}->{'da'}->get_density_per_chromosome_type($chr,'xrefgene');
     my $genes = $self->{'container'}->{'da'}->get_density_per_chromosome_type($chr,'gene');
 
     return unless $known_genes->size() && $genes->size();
 
-	my $Hscale_factor = $known_genes->{'_biggest_value'} / $genes->{'_biggest_value'};
 
    	$genes->scale_to_fit( $Config->get( 'Vgenes', 'width' ) );
 	$genes->stretch(0);
+	my $Hscale_factor = $known_genes->{'_biggest_value'} / $genes->{'_biggest_value'};
    	$known_genes->scale_to_fit( $Config->get( 'Vgenes', 'width' ) * $Hscale_factor );	
 	$known_genes->stretch(0);
+        warn( "XX: $known_genes->{'_biggest_value'} - $genes->{'_biggest_value'} - $xref_genes->{'_biggest_value'} :XX");
+	$Hscale_factor = $xref_genes->{'_biggest_value'} / $genes->{'_biggest_value'};
+   	$xref_genes->scale_to_fit( $Config->get( 'Vgenes', 'width' ) * $Hscale_factor );	
+	$xref_genes->stretch(0);
 		
 
 	my @genes = $genes->get_binvalues();
+	my @xrefs = $xref_genes->get_binvalues();
 	my @known_genes = $known_genes->get_binvalues();	
 
     foreach (@genes){
+		my $xref = shift @xrefs;	
+	    my $g_x = new Sanger::Graphics::Glyph::Rect({
+			'x'      => $xref->{'chromosomestart'},
+			'y'      => 0,
+			'width'  => $xref->{'chromosomeend'}-$_->{'chromosomestart'},
+			'height' => $xref->{'scaledvalue'},
+			'colour' => $xref_col,
+			'absolutey' => 1,
+		});
+	    $self->push($g_x);
 		my $known_gene = shift @known_genes;	
 	    my $g_x = new Sanger::Graphics::Glyph::Rect({
 			'x'      => $known_gene->{'chromosomestart'},
