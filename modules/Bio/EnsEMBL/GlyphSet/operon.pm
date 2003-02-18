fb685bbbec810c194cd8245cffda7e2f456c1533
Web Admin <w3adm@sanger.ac.uk>
2003-02-18 17:25:56 +0000

    new tracks / renamed tracks

---
+++ modules/Bio/EnsEMBL/GlyphSet/operon.pm
@@ -0,0 +1,29 @@
+package Bio::EnsEMBL::GlyphSet::operon;
+use strict;
+use vars qw(@ISA);
+use Bio::EnsEMBL::GlyphSet_simple;
+@ISA = qw(Bio::EnsEMBL::GlyphSet_simple);
+
+sub my_label { return "Operon"; }
+
+sub my_helplink { return "markers"; }
+
+sub features {
+    my ($self) = @_;
+    return $self->{'container'}->get_all_SimpleFeatures('operon', 25);
+}
+
+sub zmenu {
+  my ($self, $f ) = @_;
+  
+  my $score = $f->score();
+  my $start = $self->{'container'}->chr_start() + $f->start() - 1;
+  my $end   = $self->{'container'}->chr_start() + $f->end() - 1;
+
+  return {
+        'caption' => 'CPG data island',
+        "01:Score: $score" => '',
+        "02:bp: $start-$end" => ''
+    };
+}
+1;
---
+++ modules/Bio/EnsEMBL/GlyphSet/other_cdna.pm
@@ -0,0 +1,42 @@
+package Bio::EnsEMBL::GlyphSet::other_cdna;
+use strict;
+use vars qw(@ISA);
+use Bio::EnsEMBL::GlyphSet_feature;
+
+@ISA = qw(Bio::EnsEMBL::GlyphSet_feature);
+
+sub my_label { return "Other cDNAs"; }
+
+sub features {
+    my ($self) = @_;
+    return $self->{'container'}->get_all_DnaAlignFeatures('other_cdna', 80);
+    return $T;
+}
+
+sub colour {
+   my( $self, $id ) = @_;
+   return $self->{'colours'}->{
+     $id =~/(NM_\d+)/ ? 'REFSEQ' : ( /(RO|ZX|PX|ZA|PL)\d{5}[A-Z]\d{2}/ ? 'RIKEN' : 'EMBL' ) 
+   }
+}
+sub href {
+    my ($self, $id ) = @_;
+    if ($id =~ /^(NM_\d+)/){
+      return $self->{'config'}->{'ext_url'}->get_url('REFSEQ', $1);
+    }
+    if( $id =~ /(RO|ZX|PX|ZA|PL)\d{5}[A-Z]\d{2}/ ) {
+      return $self->{'config'}->{'ext_url'}->get_url('RIKEN', $id);
+    }
+    return $self->{'config'}->{'ext_url'}->get_url('EMBL',$id);
+}
+sub zmenu {
+  my ($self, $id ) = @_;
+  if ($id =~ /^(NM_\d+)/){
+    return { 'caption' => "$id", "REFSEQ: $id" => $self->href($id) };
+  }
+  if( $id =~ /(RO|ZX|PX|ZA|PL)\d{5}[A-Z]\d{2}/ ) {
+    return { 'caption' => "$id", "RIKEN: $id" => $self->href($id) };
+  }
+  return { 'caption' => "$id", "EMBL: $id" => $self->href($id) };
+}
+1;
