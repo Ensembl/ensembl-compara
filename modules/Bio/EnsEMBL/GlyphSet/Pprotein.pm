913dbd46cb3184457e247b69e164f45632172745
Brian Gibbins <bg2@sanger.ac.uk>
2003-04-15 13:10:08 +0000

    add markup for  splice site on prot glyph

--- modules/Bio/EnsEMBL/GlyphSet/Pprotein.pm
+++ modules/Bio/EnsEMBL/GlyphSet/Pprotein.pm
@@ -5,6 +5,7 @@ use Bio::EnsEMBL::GlyphSet;
 @ISA = qw(Bio::EnsEMBL::GlyphSet);
 use Sanger::Graphics::Glyph::Rect;
 use Sanger::Graphics::Glyph::Text;
+use EnsEMBL::Web::GeneTrans::support;
 
 sub init_label {
     my ($self) = @_;
@@ -19,25 +20,41 @@ sub init_label {
 
 sub _init {
     my ($self) = @_;
-
     my $protein = $self->{'container'};
+	my $transcript = $protein->transcript();
+	my $pep_splice = &EnsEMBL::Web::GeneTrans::support::prot_splice_site($transcript);
     my $Config  = $self->{'config'};
-    my $y       = 0;
-    my $h       = 4;
-    my $colour  = $Config->get('Pprotein','col');
+    my $x 		= 0;
+	my $y       = 0;
+    my $h       = 4; 
+	my $flip = 0;
+	my @colours  = ($Config->get('Pprotein','col1'), $Config->get('Pprotein','col2'));
+	my $start_phase = 1;
 
-    my $rect = new Sanger::Graphics::Glyph::Rect({
-	'x'        => 0,
-	'y'        => $y,
-	'width'    => $protein->length(),
-	'height'   => $h,
-	'id'       => $protein->id(),
-	'colour'   => $colour,
-    });
-    
+	for my $exon_offset (sort { $a <=> $b } keys %$pep_splice){
+	   my $colour = $colours[$flip];
+	   my $exon_id = $pep_splice->{$exon_offset}{'exon'};
+ 	   my $rect = new Sanger::Graphics::Glyph::Rect({
+		'x'        => $x,
+		'y'        => $y,
+		'width'    => $exon_offset - $x,
+		'height'   => $h,
+		'id'       => $protein->id(),
+		'colour'   => $colour,
+		'zmenu' => {
+			'caption' => "Splice Information",
+			"00:Exon: $exon_id" => "",
+			"01:Start Phase: $start_phase" => "",
+			'02:End Phase: '. ($pep_splice->{$exon_offset}{'phase'} +1) => "",
+			'03:Length: '.($exon_offset - $x)  => "", },
+    	});
+  #/$ENV{'ENSEMBL_SPECIES'}/exonview?exon=$exon_id&db=$db
     $self->push($rect);
+	$x = $exon_offset ;
+	$start_phase = ($pep_splice->{$exon_offset}{'phase'} +1) ;
+	$flip = 1-$flip;
+	}
 }
-
 1;
 
 
