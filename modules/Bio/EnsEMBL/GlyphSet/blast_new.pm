da2378523477708bd4043ce2b285b06941d4f119
Web Admin <w3adm@sanger.ac.uk>
2003-04-25 13:21:29 +0000

    new blast track!

---
+++ modules/Bio/EnsEMBL/GlyphSet/blast_new.pm
@@ -0,0 +1,28 @@
+package Bio::EnsEMBL::GlyphSet::blast_new;
+use strict;
+use vars qw(@ISA);
+use Bio::EnsEMBL::GlyphSet_feature;
+
+@ISA = qw(Bio::EnsEMBL::GlyphSet_feature);
+
+sub my_label { return "Blast hits"; }
+
+sub features {
+    my ($self) = @_;
+    return map {
+       /BLAST_NEW:(.*)/? $self->{'container'}->get_all_SearchFeatures($1):()
+    } $self->highlights;
+}
+
+sub href {
+    my ( $self, $id ) = @_;
+    return undef;
+ #   return $self->{'config'}->{'ext_url'}->get_url( 'SRS_PROTEIN', $id );
+}
+
+sub zmenu {
+    my ($self, $id ) = @_;
+    # $id =~ s/(.*)\.\d+/$1/o;
+    return { 'caption' => "$id" }; #, "Protein homology" => $self->href( $id ) };
+}
+1;
