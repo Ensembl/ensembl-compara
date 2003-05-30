2d2e557965f7caabfd23159310cd624c8f47ccc5
Web Admin <w3adm@sanger.ac.uk>
2003-05-30 09:33:41 +0000

    added zfish drawing code

---
+++ modules/Bio/EnsEMBL/GlyphSet/zfish_est.pm
@@ -0,0 +1,25 @@
+package Bio::EnsEMBL::GlyphSet::zfish_est;
+use strict;
+use vars qw(@ISA);
+use Bio::EnsEMBL::GlyphSet_feature;
+
+@ISA = qw(Bio::EnsEMBL::GlyphSet_feature);
+
+sub my_label { return "Zebrafish ESTs"; }
+
+sub features {
+    my ($self) = @_;
+    return $self->{'container'}->get_all_SimilarityFeatures( "zfish_EST", 80, $self->glob_bp);
+}
+
+sub href {
+    my ( $self, $id ) = @_;
+    return $self->ID_URL( 'EMBL', $id );
+}
+
+sub zmenu {
+    my ($self, $id ) = @_;
+    $id =~ s/(^[^\.]+)\..*/$1/;
+    return { 'caption' => "EST $id", "$id" => $self->href( $id ) };
+}
+1;
