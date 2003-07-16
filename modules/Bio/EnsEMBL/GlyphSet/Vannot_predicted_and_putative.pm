b85d4ce0877cd5a64ac140ae9bea4634f1696ecb
Patrick Meidl <pm2@sanger.ac.uk>
2003-07-16 15:12:12 +0000

    removed obsolete use of SiteDefs

--- modules/Bio/EnsEMBL/GlyphSet/Vannot_predicted_and_putative.pm
+++ modules/Bio/EnsEMBL/GlyphSet/Vannot_predicted_and_putative.pm
@@ -7,7 +7,6 @@ use Sanger::Graphics::Glyph::Rect;
 use Sanger::Graphics::Glyph::Poly;
 use Sanger::Graphics::Glyph::Text;
 use Sanger::Graphics::Glyph::Line;
-use SiteDefs;
 
 sub init_label {
     my ($self) = @_;
