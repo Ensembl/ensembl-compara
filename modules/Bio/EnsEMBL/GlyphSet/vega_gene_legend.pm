e44d21b6e220898eecf73f8ba467e879dda1e532
Patrick Meidl <pm2@sanger.ac.uk>
2003-08-12 15:36:33 +0000

    changed help link to use the javascript function hw()

--- modules/Bio/EnsEMBL/GlyphSet/vega_gene_legend.pm
+++ modules/Bio/EnsEMBL/GlyphSet/vega_gene_legend.pm
@@ -18,12 +18,10 @@ sub init_label {
         'text'      => 'Gene legend',
         'font'      => 'Small',
         'absolutey' => 1,
-         'href'      => qq[javascript:X=window.open(\'/$ENV{'ENSEMBL_SPECIES'}/helpview?se=1&kw=${SPECIES_SHORT}_gene_classification\',\'helpview\',\'height=400,width=500,left=100,screenX=100,top=100,screenY=100,resizable,scrollbars=yes\');X.focus();void(0)],
-
+        'href'      => qq[javascript:X=hw('$ENV{'ENSEMBL_SPECIES'}','${SPECIES_SHORT}_gene_classification','')],
         'zmenu'     => {
-            'caption'                     => 'HELP',
-            "01:Track information..."     =>
-qq[javascript:X=window.open(\\\'/$ENV{'ENSEMBL_SPECIES'}/helpview?se=1&kw=${SPECIES_SHORT}_gene_classification\\\',\\\'helpview\\\',\\\'height=400,width=500,left=100,screenX=100,top=100,screenY=100,resizable,scrollbars=yes\\\');X.focus();void(0)]
+          'caption'                     => 'HELP',
+          "01:Track information..."     => qq[javascript:X=hw(\\'$ENV{'ENSEMBL_SPECIES'}\\',\\'${SPECIES_SHORT}_gene_classification\\',\\'\\')]
         }
     });
     $self->label($label);
