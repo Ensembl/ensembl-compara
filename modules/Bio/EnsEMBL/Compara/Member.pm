3443dff944879b3bd5f9fc73977a75e56679b1b3
Abel Ureta-Vidal <abel@sanger.ac.uk>
2003-09-02 14:26:20 +0000

    Does not inherite anymore from Bio::EnsEMBL::Compara::BaseRelation, so made some modifications as consequence

--- modules/Bio/EnsEMBL/Compara/Member.pm
+++ modules/Bio/EnsEMBL/Compara/Member.pm
@@ -1,28 +1,137 @@
 package Bio::EnsEMBL::Compara::Member;
 
 use strict;
-use Bio::EnsEMBL::Compara::BaseRelation;
+use Bio::EnsEMBL::Root;
 
-our @ISA = qw(Bio::EnsEMBL::Compara::BaseRelation);
-
-=head2 new
-
-=cut
+our @ISA = qw(Bio::EnsEMBL::Root);
 
 sub new {
   my ($class, @args) = @_;
-
   my $self = $class->SUPER::new(@args);
 
   if (scalar @args) {
-    my ($taxon_id, $genome_db_id) = $self->_rearrange([qw(TAXON_ID GENOME_DB_ID)], @args);
-
+    #do this explicitly.
+    my ($dbid, $stable_id, $description, $source_id, $source_name, $adaptor, $taxon_id, $genome_db_id) = $self->_rearrange([qw(DBID STABLE_ID DESCRIPTION SOURCE_ID SOURCE_NAME ADAPTOR TAXON_ID GENOME_DB_ID)], @args);
+    
+    $dbid && $self->dbID($dbid);
+    $stable_id && $self->stable_id($stable_id);
+    $description && $self->description($description);
+    $source_id && $self->source_id($source_id);
+    $source_name && $self->source_id($source_name);
+    $adaptor && $self->adaptor($adaptor);
     $taxon_id && $self->taxon_id($taxon_id);
     $genome_db_id && $self->genome_db_id($genome_db_id);
   }
   
   return $self;
 }   
+
+=head2 new_fast
+
+  Arg [1]    : hash reference $hashref
+  Example    : none
+  Description: This is an ultra fast constructor which requires knowledge of
+               the objects internals to be used.
+  Returntype : 
+  Exceptions : none
+  Caller     : 
+
+=cut
+
+sub new_fast {
+  my ($class, $hashref) = @_;
+
+  return bless $hashref, $class;
+}
+
+=head2 dbID
+
+  Arg [1]    : int $dbID (optional)
+  Example    : 
+  Description: 
+  Returntype : 
+  Exceptions : 
+  Caller     : 
+
+=cut
+
+sub dbID {
+  my $self = shift;
+  $self->{'_dbID'} = shift if(@_);
+  return $self->{'_dbID'};
+}
+
+=head2 stable_id
+
+  Arg [1]    : string $stable_id (optional)
+  Example    : 
+  Description: 
+  Returntype : 
+  Exceptions : 
+  Caller     : 
+
+=cut
+
+sub stable_id {
+  my $self = shift;
+  $self->{'_stable_id'} = shift if(@_);
+  return $self->{'_stable_id'};
+}
+
+=head2 description
+
+  Arg [1]    : string $description (optional)
+  Example    : 
+  Description: 
+  Returntype : string
+  Exceptions : 
+  Caller     : 
+
+=cut
+
+sub description {
+  my $self = shift;
+  $self->{'_description'} = shift if(@_);
+  return $self->{'_description'};
+}
+
+=head2 source_id
+
+=cut
+
+sub source_id {
+  my $self = shift;
+  $self->{'_source_id'} = shift if (@_);
+  return $self->{'_source_id'};
+}
+
+=head2 source_name
+
+=cut
+
+sub source_name {
+  my $self = shift;
+  $self->{'_source_name'} = shift if (@_);
+  return $self->{'_source_name'};
+}
+
+=head2 adaptor
+
+  Arg [1]    : string $adaptor (optional)
+               corresponding to a perl module
+  Example    : 
+  Description: 
+  Returntype : 
+  Exceptions : 
+  Caller     : 
+
+=cut
+
+sub adaptor {
+  my $self = shift;
+  $self->{'_adaptor'} = shift if(@_);
+  return $self->{'_adaptor'};
+}
 
 =head2 chr_name
 
