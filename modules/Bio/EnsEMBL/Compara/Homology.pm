fc0202ae4a6afe06e7d88f8ffadef2184ab01df3
Abel Ureta-Vidal <abel@sanger.ac.uk>
2003-09-05 10:13:47 +0000

    Deleted new and type methods, first is inherited and the second is obsolete before even being used :)

--- modules/Bio/EnsEMBL/Compara/Homology.pm
+++ modules/Bio/EnsEMBL/Compara/Homology.pm
@@ -5,49 +5,5 @@ use Bio::EnsEMBL::Compara::BaseRelation;
 
 our @ISA = qw(Bio::EnsEMBL::Compara::BaseRelation);
 
-=head2 new
-
-  Arg [1]    : 
-  Example    : 
-  Description: 
-  Returntype : Bio::EnsEMBL::Compara::Homology (but without members; caller has to fill using
-               add_Member_Attribute)
-  Exceptions : 
-  Caller     : 
-
-=cut
-
-sub new {
-  my($class,@args) = @_;
-  
-  my $self = $class->SUPER::new(@args);
-  
-  if (scalar @args) {
-     #do this explicitly.
-     my ($type) = $self->_rearrange([qw(TYPE)], @args);
-      
-      $type && $self->type($type);
-  }
-  
-  return $self;
-}   
-
-=head2 type
-
-  Arg [1]    : 
-  Example    : 
-  Description: 
-  Returntype : 
-  Exceptions : 
-  Caller     : 
-
-=cut
-
-sub type {
-  my $self = shift;
-  $self->{'_type'} = shift if(@_);
-  return $self->{'_type'};
-}
-
 1;
 
