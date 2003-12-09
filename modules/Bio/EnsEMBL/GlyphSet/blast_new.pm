ae295c5f7eb11b8fbb7424eb8a50153464b155ac
Web Admin <w3adm@sanger.ac.uk>
2003-12-09 13:49:35 +0000

    Better zmenu and track suppression

--- modules/Bio/EnsEMBL/GlyphSet/blast_new.pm
+++ modules/Bio/EnsEMBL/GlyphSet/blast_new.pm
@@ -20,36 +20,60 @@ sub href {
     #   return $self->ID_URL( 'SRS_PROTEIN', $id );
 }
 
+# Overload SUPER::_init to suppress track if BLAST_NEW not in highlights param
+sub _init{
+  my $self = shift;
+  map{
+    /BLAST_NEW:(.*)/ && return $self->SUPER::_init(@_);
+  } $self->highlights;
+  return undef;
+}
+
 sub zmenu {
   my $self = shift;
   my $zmenu = {};
 
-  $zmenu->{caption} = shift || 'UNKNOWN!';
+  my $id = shift || 'UNKNOWN!';
 
   my $boxes     = shift || [];
   my $first_box = $boxes->[0] || [];
   my $feature   = $first_box->[2];
-  
+
   if( $feature ){
+    my $caption = '';
     if( $feature->has_tag('qname') ){ 
-      my $qstring = '';
-      $qstring.= ($feature->each_tag_value('qname'))[0].":";
-      $qstring.= ($feature->each_tag_value('qstart'))[0] ."-";
-      $qstring.= ($feature->each_tag_value('qend'))[0];
-      $zmenu->{"00:Qry: $qstring"} = '';
+      my $qname  = ($feature->each_tag_value('qname'))[0] || 'UNKNOWN!';
+      my $qstart = ( $feature->has_tag('qstart') ? 
+		     ($feature->each_tag_value('qstart'))[0] : '' );
+      my $qend   = ( $feature->has_tag('qend') ? 
+		     ($feature->each_tag_value('qend'))[0] : '' );
+      my $qstrand= ( $feature->has_tag('qstrand') ? 
+		     ($feature->each_tag_value('qstrand'))[0] : '' );
+
+      $caption .= $qname ." vs. ";
+      $zmenu->{ "00:Qry: $qstart-$qend(". ( $qstrand<1 ? '-)' : '+)' ) } = '';
     }
     if( $feature->has_tag('hname') ){ 
-      my $hstring = '';
-      $hstring.= ($feature->each_tag_value('hname'))[0].":";
-      $hstring.= ($feature->each_tag_value('hstart'))[0] ."-";
-      $hstring.= ($feature->each_tag_value('hend'))[0];
-      $zmenu->{"01:Hit: $hstring"} = '';
+      my $hname = ($feature->each_tag_value('hname'))[0] || 'UNKNOWN!';
+      my $hstart = ( $feature->has_tag('hstart') ? 
+		     ($feature->each_tag_value('hstart'))[0] : '' );
+      my $hend   = ( $feature->has_tag('hend') ? 
+		     ($feature->each_tag_value('hend'))[0] : '' );
+      my $hstrand= ( $feature->has_tag('hstrand') ? 
+		     ($feature->each_tag_value('hstrand'))[0] : '' );
+
+      $caption .= $hname;
+      $zmenu->{ "00:Hit: $hstart-$hend(". ( $hstrand<1 ? '-)' : '+)' ) } = '';
     }
+    $zmenu->{caption} = $caption;
     $zmenu->{"02:Score:     ". $feature->score} = '';
     $zmenu->{"03:PercentID: ". $feature->percent_id} ='';
     $zmenu->{"04:Length:    ". $feature->length } = '';
     my $ev = $feature->p_value;
     if( defined( $ev ) ){ $zmenu->{"05:P-value: $ev"} = '' };
+  }
+  else{
+    $zmenu->{caption} = $id;
   }
 
   return $zmenu;
