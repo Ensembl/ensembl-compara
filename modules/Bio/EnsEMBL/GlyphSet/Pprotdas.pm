fd78de5f81073ffb5e08701b985b5eb8e97a0e28
Will Spooner <whs@sanger.ac.uk>
2004-01-08 10:25:21 +0000

    Protein DAS drawing code

---
+++ modules/Bio/EnsEMBL/GlyphSet/Pprotdas.pm
@@ -0,0 +1,153 @@
+package Bio::EnsEMBL::GlyphSet::Pprotdas;
+use strict;
+use vars qw(@ISA);
+use Bio::EnsEMBL::GlyphSet;
+@ISA = qw(Bio::EnsEMBL::GlyphSet);
+use Sanger::Graphics::Glyph::Composite;
+use Sanger::Graphics::Glyph::Rect;
+use Sanger::Graphics::Glyph::Text;
+use Sanger::Graphics::ColourMap;
+use Sanger::Graphics::Bump;
+use Data::Dumper;
+use ExtURL;
+
+
+sub init_label {
+    my ($self) = @_;
+    return if( defined $self->{'config'}->{'_no_label'} );
+    my $label = $self->{'extras'} && $self->{'extras'}->{'name'};
+    $label ||= 'ProteinDAS';
+
+    my $label = new Sanger::Graphics::Glyph::Text
+      ( { 'text'      => $label,
+	  'font'      => 'Small',
+	  'absolutey' => 1 });
+
+    $self->label($label);
+
+    return 1;
+}
+
+
+
+sub _init {
+
+    my ($self) = @_;
+    my %hash;
+    my $caption       = $self->managed_name || "GeneDAS";
+    my @bitmap        = undef;
+    my $protein       = $self->{'container'};
+    my $Config        = $self->{'config'};
+    my $pix_per_bp    = $Config->transform->{'scalex'};
+    my $bitmap_length = int($self->{'container'}->length() * $pix_per_bp);
+    my $y             = 0;
+    my $h             = 4;
+    my $cmap          = new Sanger::Graphics::ColourMap;
+    my $black         = 'black';
+    my $red           = 'red';
+    my $font          = "Small";
+    my $colour        = $Config->get('Ppfam','col');
+    my ($fontwidth,
+	$fontheight)  = $Config->texthelper->px2bp($font);
+
+    my $das_feat_ref = $self->{extras}->{features};
+    ref( $das_feat_ref ) eq 'ARRAY' || 
+      ( warn("No feature array for ProteinDAS track") &&  return );
+    my @das_feats = @$das_feat_ref;
+
+    foreach my $feat(@das_feats) {
+	push(@{$hash{$feat->das_id}},$feat);
+    }
+
+    foreach my $key (keys %hash) {
+	my @row  = @{$hash{$key}};
+	my $desc = $row[0]->das_id();
+		
+	# Zmenu
+	my $Composite = new Sanger::Graphics::Glyph::Composite
+	  ({
+	    'x'     => $row[0]->das_start(),
+	    'y'     => $y,
+	    'href'  => $row[0]->das_link(),
+	    'zmenu' => {
+			'caption' => $row[0]->das_type(),
+			$key      => $row[0]->das_link() || undef,
+		       },
+	   });
+
+	# Boxes
+	my $pfsave;
+	my ($minx, $maxx);
+	foreach my $pf (@row) {
+	    my $x  = $pf->das_start();
+	    $minx  = $x if ($x < $minx || !defined($minx));
+	    my $w  = $pf->das_end() - $x;
+	    $maxx  = $pf->das_end() if ($pf->das_end() > $maxx || !defined($maxx));
+	    my $id = $pf->das_id();
+
+	    my $rect = new Sanger::Graphics::Glyph::Rect({
+		'x'        => $x,
+		'y'        => $y,
+		'width'    => $w,
+		'height'   => $h,
+		'colour'   => $colour,
+	    });
+	    $Composite->push($rect);
+	    $pfsave = $pf;
+	}
+
+	my $rect = new Sanger::Graphics::Glyph::Rect({
+	    'x'         => $minx,
+	    'y'         => $y + 2,
+	    'width'     => $maxx - $minx,
+	    'height'    => 0,
+	    'colour'    => $colour,
+	    'absolutey' => 1,
+	});
+	$Composite->push($rect);
+
+	# Label - disabled for now
+	if( 0 ){
+	    my $desc = $pfsave->das_id() || $key;
+	    my $text = new Sanger::Graphics::Glyph::Text
+	      ({
+		'font'   => $font,
+		'text'   => $desc,
+		'x'      => $row[0]->das_start(),
+		'y'      => $h + 1,
+		'height' => $fontheight,
+		'width'  => $fontwidth * length($desc),
+		'colour' => $black,
+	       });
+	    #$Composite->push($text);
+	}
+
+	if ($Config->get('Ppfam', 'dep') > 0){ # we bump
+            my $bump_start = int($Composite->x() * $pix_per_bp);
+            $bump_start = 0 if ($bump_start < 0);
+	    
+            my $bump_end = $bump_start + int($Composite->width()*$pix_per_bp);
+            if ($bump_end > $bitmap_length){$bump_end = $bitmap_length};
+            my $row = & Sanger::Graphics::Bump::bump_row(
+				      $bump_start,
+				      $bump_end,
+				      $bitmap_length,
+				      \@bitmap
+				      );
+            $Composite->y($Composite->y() + (1.5 * $row * ($h + $fontheight)));
+        }
+	
+	$self->push($Composite);
+    }
+
+}
+
+
+sub managed_name{
+    my ($self) = @_;
+    return $self->{'extras'}->{'name'}
+}
+
+
+1;
+
---
+++ modules/Bio/EnsEMBL/GlyphSetManager/Pprotdas.pm
@@ -0,0 +1,72 @@
+package Bio::EnsEMBL::GlyphSetManager::Pprotdas;
+
+use strict;
+use Sanger::Graphics::GlyphSetManager;
+use Bio::EnsEMBL::GlyphSet::Pprotdas;
+use vars qw(@ISA);
+@ISA = qw(Sanger::Graphics::GlyphSetManager);
+
+sub init {
+    my ($self) = @_;
+
+    my $Config = $self->{'config'};
+    my $protein = $self->{'container'};
+
+    # Get features. The data structure returned is YUK!
+    my $feat_container = $protein->get_all_DASFeatures();
+    ref( $feat_container ) ne 'HASH' and return; # Sanity check
+
+    # Examine the data structure, and create glyphs accordingly
+    my %feats_by_glyph;
+    foreach my $dsn( keys( %$feat_container ) ){
+	my $sub_feat_container = $feat_container->{$dsn};
+	ref( $sub_feat_container ) ne 'ARRAY' and next; # Another sanity check
+	my $feat_ref = $sub_feat_container->[1];
+	if( ref( $feat_ref ) ne 'ARRAY' or ! scalar @$feat_ref ){ next }
+
+	# OK - we now have a list of DASSeqFeature objs. Sort them per-glyph
+	foreach my $feat( @$feat_ref ){
+
+	    # Skip protein-wide features (GeneDAS - tabulated elsewhere )
+	    # GeneDAS Identified by DAS segment id eq DAS feature id
+	    my $id = $feat->das_id();
+	    if( $feat->das_segment_id() =~ /^$id/ ){ next }
+
+	    # Push feature onto appropriate glyph key (by feature type)
+	    my $type = $dsn . '_' . $feat->das_type_id() || 'UNKNOWN';
+	    $feats_by_glyph{$type} ||= [];
+	    push @{$feats_by_glyph{$type}}, $feat
+	}
+    }
+
+    foreach my $das_source_name( keys %feats_by_glyph ) {
+	my $extra_config = {};
+	$extra_config->{'name'}     = $das_source_name;
+	$extra_config->{'features'} = $feats_by_glyph{$das_source_name};
+	$self->add_glyphset( $extra_config );
+    }
+    return 1;
+}
+
+sub add_glyphset {
+    my ($self,$config) = @_;	
+    my $glyphset;
+    eval {
+	$glyphset = new Bio::EnsEMBL::GlyphSet::Pprotdas
+	  ( $self->{'container'},
+	    $self->{'config'},
+	    $self->{'highlights'},
+	    $self->{'strand'},
+	    $config );
+    };
+
+    if($@) {
+	print STDERR "DAS GLYPHSET $config->{'name'} failed\n";
+	return undef();
+    }
+
+    push @{$self->{'glyphsets'}}, $glyphset;
+    return 1;
+}
+
+1;
