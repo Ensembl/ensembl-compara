ada2cf38ea911d4367dd546b7e11a4298afc9760
Jessica Severin <jessica@sanger.ac.uk>
2004-02-23 17:39:04 +0000

    comparaDumpGenes.pl now figures out the name for the fasta file from the genome_db table, so one specifies a -fastdir where the files are to be dumped.  Also added logic so that if neither a genome_db_id nor a subset_id is specified it will dump all peptide sets for all species loaded into compara into the fastadir.  The locations of the fasta files are stored in subset.dump_loc

--- scripts/pipeline/comparaDumpGenes.pl
+++ scripts/pipeline/comparaDumpGenes.pl
@@ -3,10 +3,11 @@
 use strict;
 use DBI;
 use Getopt::Long;
+use Bio::EnsEMBL::Compara::DBSQL::DBAdaptor;
 
 my $conf_file;
-my ($help, $host, $user, $pass, $dbname, $port, $conf_file, $adaptor, $subset_id);
-my ($genome_db_id, $prefix, $fastafile);
+my ($help, $host, $user, $pass, $dbname, $port, $compara_conf, $adaptor, $subset_id);
+my ($genome_db_id, $prefix, $fastadir);
 
 GetOptions('help' => \$help,
            'host=s' => \$host,
@@ -14,16 +15,16 @@ GetOptions('help' => \$help,
            'pass=s' => \$pass,
            'dbname=s' => \$dbname,
 	   'port=i' => \$port,
-           'conf=s' => \$conf_file,
+           'compara=s' => \$compara_conf,
            'genome_db_id=i' => \$genome_db_id,
            'subset_id=i' => \$subset_id,
 	   'prefix=s' => \$prefix,
-	   'fasta=s' => \$fastafile
+	   'fastadir=s' => \$fastadir
 	  );
 	  
 	  
-if(-e $conf_file) {	  
-  my %conf = %{do $conf_file};
+if(-e $compara_conf) {
+  my %conf = %{do $compara_conf};
 
   $host = $conf{'host'};
   $port = $conf{'port'};
@@ -38,40 +39,70 @@ unless(defined($host) and defined($user) and defined($dbname)) {
   print "\nERROR : must specify host, user, and database to connect to compara\n\n";
   usage(); 
 }
-unless(defined($fastafile) and ($fastafile =~ /^\//)) { 
-  print "\nERROR : must specify an full output path for the fasta file\n\n";
+unless(defined($fastadir) and ($fastadir =~ /^\//)) { 
+  print "\nERROR : must specify an full output path for the fasta directory\n\n";
   usage(); 
 }
-unless(defined($genome_db_id) or defined($subset_id)) { help(); }
 
 
-my $dsn = "DBI:mysql:database=$dbname;host=$host;port=$port";
+my $compara_db = new Bio::EnsEMBL::Compara::DBSQL::DBAdaptor(-host => $host,
+							     -user => $user,
+							     -pass => $pass,
+							     -dbname => $dbname);
 
-my $dbh = DBI->connect("$dsn",$user,$pass) 
-  || die "Database connection not made: $DBI::errstr";
-  
+#
+# if neither genome_db_id or subset_id specified, dump for all genomes
+#
+
+unless(defined($genome_db_id) or defined($subset_id)) { 
+  my $genomedbAdaptor = $compara_db->get_GenomeDBAdaptor();
+
+  my @genomedbArray = @{$genomedbAdaptor->fetch_all()};
+  my $count = $#genomedbArray;
+  print("fetched $count different genome_db from compara\n");
+
+  foreach my $genome_db (@genomedbArray) {
+    my $genome_db_id = $genome_db->dbID();  
+
+    $subset_id = getSubsetIdForGenomeDBId($compara_db, $genome_db_id);
+    if(defined($subset_id)) {
+      my $fastafile = getFastaNameForGenomeDBID($compara_db, $genome_db_id);
+      dumpFastaForSubset($compara_db, $subset_id, $fastafile);
+    }
+    print("\n");
+  }
+  exit(0);
+}
+
+
+#
+# if genome_db_id specified dump for that species, 
+# otherwise $subset_id is defined from options
+#
+
 if(defined($genome_db_id)) {
-  my @subsetIds = @{ getSubsetIdsForGenomeDBId($dbh) };
-
-  if($#subsetIds > 0) {
-    die ("ERROR in Compara DB: more than 1 subset of longest peptides defined for genome_db_id = $genome_db_id\n");
-  }
-  if($#subsetIds < 0) {
-    die ("ERROR in Compara DB: no subset of longest peptides defined for genome_db_id = $genome_db_id\n");
-  }
-  $subset_id = $subsetIds[0];
+  $subset_id = getSubsetIdForGenomeDBId($compara_db, $genome_db_id);
 }
 
 unless(defined($subset_id)) {
   die "ERROR : must specify a compara subset to dump fasta from\n";
 }
-dumpFastaForSubset($dbh, $subset_id);
+
+my $fastafile = getFastaNameForSubsetID($compara_db, $subset_id);
+dumpFastaForSubset($compara_db, $subset_id, $fastafile);
+
+#$dbh->disconnect();
 
 
-$dbh->disconnect();
 exit(0);
 
 
+
+#######################
+#
+# subroutines
+#
+#######################
 
 sub usage {
   print "comparaDumpGenes.pl -pass {-compara | -host -user -dbname} {-genome_db_id | -subset_id } [options]\n";
@@ -85,16 +116,29 @@ sub usage {
   print "  -dbname <name>    : use database <name> to connect to compara DB\n";
   print "  -genome_db_id <#> : dump member associated with genome_db_id\n";
   print "  -subset_id <#>    : dump member associated with subset_id\n";
-  print "  -fasta <path>     : dump fasta to file location\n";
+  print "  -fastadir <path>  : dump fasta into directory\n";
   print "  -prefix <string>  : use <string> as prefix for sequence names in fasta file\n";
   print "comparaDumpGenes.pl v1.0\n";
   
   exit(1);  
 }
 
+=head3
+if(defined($genome_db_id)) {
+  my @subsetIds = @{ getSubsetIdsForGenomeDBId($compara_db) };
 
-sub getSubsetIdsForGenomeDBId {
-  my ($dbh) = @_;
+  if($#subsetIds > 0) {
+    die ("ERROR in Compara DB: more than 1 subset of longest peptides defined for genome_db_id = $genome_db_id\n");
+  }
+  if($#subsetIds < 0) {
+    die ("ERROR in Compara DB: no subset of longest peptides defined for genome_db_id = $genome_db_id\n");
+  }
+  $subset_id = $subsetIds[0];
+}
+=cut 
+
+sub getSubsetIdForGenomeDBId {
+  my ($dbh, $genome_db_id) = @_;
   
   my @subsetIds = ();
   my $subset_id;
@@ -111,18 +155,80 @@ sub getSubsetIdsForGenomeDBId {
   $sth->bind_columns( undef, \$subset_id );
 
   while( $sth->fetch() ) {
-    print("found subset_id = $subset_id\n");
+    print("found subset_id = $subset_id for genome_db_id = $genome_db_id\n");
     push @subsetIds, $subset_id;
   }
   
   $sth->finish();
-  print
-  return \@subsetIds;
+
+  if($#subsetIds > 0) {
+    warn ("Compara DB: more than 1 subset of longest peptides defined for genome_db_id = $genome_db_id\n");
+  }
+  if($#subsetIds < 0) {
+    warn ("Compara DB: no subset of longest peptides defined for genome_db_id = $genome_db_id\n");
+  }
+
+  return $subsetIds[0];
+}
+
+
+sub getFastaNameForSubsetID {
+  my ($dbh, $subset_id) = @_;
+  
+  my($name, $species, $assembly);
+  
+  
+  my $sql = "SELECT genome_db.name, genome_db.assembly " .
+            "FROM subset_member, member, genome_db " .
+	    "WHERE subset_member.subset_id='$subset_id' ".
+	    "AND member.member_id=subset_member.member_id ". 
+	    "AND member.genome_db_id=genome_db.genome_db_id ".
+	    "GROUP BY genome_db.genome_db_id;";
+  my $sth = $dbh->prepare( $sql );
+  $sth->execute();
+
+  $sth->bind_columns( undef, \$species, \$assembly );
+
+  while( $sth->fetch() ) {
+    $species =~ s/\s+/_/g;
+    $name = $fastadir . "/" . $species . "_" . $assembly . ".fasta";
+    $name =~ s/\/\//\//g;
+    print("name = '$name'\n");
+  }
+  
+  $sth->finish();
+  return $name;
+}
+
+
+sub getFastaNameForGenomeDBID {
+  my ($dbh, $genomeDBID) = @_;
+  
+  my($name, $species, $assembly);
+  
+  
+  my $sql = "SELECT genome_db.name, genome_db.assembly " .
+            "FROM genome_db " .
+	    "WHERE genome_db.genome_db_id=$genomeDBID;";
+  my $sth = $dbh->prepare( $sql );
+  $sth->execute();
+
+  $sth->bind_columns( undef, \$species, \$assembly );
+
+  while( $sth->fetch() ) {
+    $species =~ s/\s+/_/g;
+    $name = $fastadir . "/" . $species . "_" . $assembly . ".fasta";
+    $name =~ s/\/\//\//g;
+    print("name = '$name'\n");
+  }
+  
+  $sth->finish();
+  return $name;
 }
 
 
 sub dumpFastaForSubset {
-  my($dbh, $subset_id) = @_;
+  my($dbh, $subset_id, $fastafile) = @_;
   
   my $sql = "SELECT member.stable_id, member.description, sequence.sequence " .
             "FROM member, sequence, subset_member " .
@@ -155,4 +261,6 @@ sub dumpFastaForSubset {
   $sth = $dbh->prepare("UPDATE subset SET dump_loc = ? WHERE subset_id = ?");
   $sth->execute($fastafile, $subset_id);
   
+  print("Prepare fasta file as blast database\n");
+  system("setdb $fastafile");
 }
