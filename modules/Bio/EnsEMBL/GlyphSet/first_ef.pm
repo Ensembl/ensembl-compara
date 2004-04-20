dbd1706d601d7cbebe98dae59ef1e380a70fc826
Fiona Cunningham <fiona@ebi.ac.uk>
2004-04-20 13:07:48 +0000

    added new track First EF to contigview

---
+++ modules/Bio/EnsEMBL/GlyphSet/first_ef.pm
@@ -0,0 +1,38 @@
+package Bio::EnsEMBL::GlyphSet::eponine;
+use strict;
+use vars qw(@ISA);
+use Bio::EnsEMBL::GlyphSet_simple;
+@ISA = qw(Bio::EnsEMBL::GlyphSet_simple);
+
+sub squish { return 1; }
+sub my_label { return "FirstEF"; }
+
+sub my_description { return "First Exon Finder<br />&nbsp;start sites"; }
+
+sub my_helplink { return "markers"; }
+
+sub features {
+    my ($self) = @_;
+
+    return $self->{'container'}->get_all_SimpleFeatures('FirstEF');
+}
+
+sub href {
+    my ($self, $f ) = @_;
+    return undef;
+}
+
+sub zmenu {
+    my ($self, $f ) = @_;
+    
+    my $score = $f->score();
+    my $start = $f->start() + $self->{'container'}->start() - 1;
+    my $end   = $f->end()   + $self->{'container'}->start() - 1;
+
+    return {
+        'caption'                                     => 'FirstEF',
+        "01:Score: $score"                            => '',
+        "02:bp: $start-$end"                          => ''
+    };
+}
+1;
