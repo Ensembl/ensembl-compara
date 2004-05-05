7ed80398cebd6ddc9dd32eaa947a7db9850b3670
Steve Trevanion <st3@sanger.ac.uk>
2004-05-05 15:40:45 +0000

    added POD and modified to work with new schema

--- modules/Bio/EnsEMBL/GlyphSet/polyA_signal.pm
+++ modules/Bio/EnsEMBL/GlyphSet/polyA_signal.pm
@@ -1,24 +1,84 @@
+=head1 NAME
+
+Bio::EnsEMBL::GlyphSet::polyA_signal.pm
+GlyphSet to draw polyA sites
+
+=head1 DESCRIPTION
+
+Displays polyA sites stored as simple features
+
+=head1 LICENCE
+
+This code is distributed under an Apache style licence:
+Please see http://www.ensembl.org/code_licence.html for details
+
+=head1 AUTHOR
+
+Steve Trevanion <st3@sanger.ac.uk>
+
+=head1 CONTACT
+
+Post questions to the EnsEMBL development list ensembl-dev@ebi.ac.uk
+
+=cut
+
 package Bio::EnsEMBL::GlyphSet::polyA_signal;
 use strict;
 use vars qw(@ISA);
 use Bio::EnsEMBL::GlyphSet_simple;
 @ISA = qw(Bio::EnsEMBL::GlyphSet_simple);
 
+
+=head2 my_label
+
+  Arg[1]      : none
+  Example     : my $label = $self->my_label;
+  Description : returns the label for the track (displayed track name)
+  Return type : String - track label
+  Exceptions  : none
+  Caller      : $self->init_label()
+
+=cut
+
 sub my_label { return "PolyA signals"; }
 
-sub my_helplink { return "markers"; }
+
+=head2 features
+
+  Arg[1]      : none 
+  Example     : my $f = $self->features;
+  Description : this function does the data fetching from the core database
+  Return type : listref of Bio::EnsEMBL::DnaDnaAlignFeature objects
+  Exceptions  : none
+  Caller      : $self->_init()
+
+=cut
 
 sub features {
     my ($self) = @_;
     return $self->{'container'}->get_all_SimpleFeatures('polyA_signal', 0);
 }
 
+
+=head2 zmenu
+
+  Arg[1]      : feature ID
+  Arg[2]      : a listref of Bio::EnsEMBL::DnaDnaAlignFeature objects
+  Example     : my $zmenu = $self->zmenu($id, $feature_array);
+  Description : creates the zmenu (context menu) for the glyphset. Returns a
+                hashref describing the zmenu entries and properties
+  Return type : hashref
+  Exceptions  : none
+  Caller      : 
+
+=cut
+
 sub zmenu {
   my ($self, $f ) = @_;
   
   my $score = $f->score();
-  my $start = $self->{'container'}->chr_start() + $f->start() - 1;
-  my $end   = $self->{'container'}->chr_start() + $f->end() - 1;
+  my $start = $self->{'container'}->start() + $f->start() - 1;
+  my $end   = $self->{'container'}->start() + $f->end() - 1;
 
   return {
         'caption' => 'polyA signal',
