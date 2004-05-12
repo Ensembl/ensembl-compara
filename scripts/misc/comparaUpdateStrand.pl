bba586ac39d3510c49cb4b1a2ee4abe679e67cb4
Jessica Severin <jessica@sanger.ac.uk>
2004-05-12 14:47:14 +0000

    updates the member.chr_strand by connecting to core databases. Works incrementally by fetching blocks of members with chr_strand=0 which is undefined and updates the block.

---
+++ scripts/misc/comparaUpdateStrand.pl
@@ -0,0 +1,170 @@
+#!/usr/local/ensembl/bin/perl -w
+
+use strict;
+use DBI;
+use Getopt::Long;
+use Bio::EnsEMBL::Compara::DBSQL::DBAdaptor;
+use Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor;
+use Bio::EnsEMBL::Pipeline::Analysis;
+use Bio::EnsEMBL::Pipeline::Rule;
+use Bio::EnsEMBL::Compara::GenomeDB;
+use Bio::EnsEMBL::DBLoader;
+use Bio::EnsEMBL::Compara::Homology;
+use Bio::EnsEMBL::Compara::Attribute;
+use Bio::EnsEMBL::Compara::Hive::Extensions;
+
+
+
+# ok this is a hack, but I'm going to pretend I've got an object here
+# by creating a blessed hash ref and passing it around like an object
+# this is to avoid using global variables in functions, and to consolidate
+# the globals into a nice '$self' package
+my $self = bless {};
+
+$self->{'compara_conf'} = {};
+$self->{'compara_conf'}->{'-user'} = 'ensro';
+$self->{'compara_conf'}->{'-port'} = 3306;
+
+$self->{'speciesList'} = ();
+
+my $conf_file;
+my ($help, $host, $user, $pass, $dbname, $port, $adaptor);
+
+GetOptions('help'     => \$help,
+           'conf=s'   => \$conf_file,
+           'dbhost=s' => \$host,
+           'dbport=i' => \$port,
+           'dbuser=s' => \$user,
+           'dbpass=s' => \$pass,
+           'dbname=s' => \$dbname,
+           'gdb=i'    => \$self->{'genome_db_id'},
+          );
+
+if ($help) { usage(); }
+
+parse_conf($self, $conf_file);
+
+if($host)   { $self->{'compara_conf'}->{'-host'}   = $host; }
+if($port)   { $self->{'compara_conf'}->{'-port'}   = $port; }
+if($dbname) { $self->{'compara_conf'}->{'-dbname'} = $dbname; }
+if($user)   { $self->{'compara_conf'}->{'-user'}   = $user; }
+if($pass)   { $self->{'compara_conf'}->{'-pass'}   = $pass; }
+
+
+unless(defined($self->{'compara_conf'}->{'-host'})
+       and defined($self->{'compara_conf'}->{'-user'})
+       and defined($self->{'compara_conf'}->{'-dbname'}))
+{
+  print "\nERROR : must specify host, user, and database to connect to compara\n\n";
+  usage(); 
+}
+
+unless(defined($self->{'genome_db_id'})) {
+  print "\nERROR : must specify genome_db_id\n\n";
+  usage();
+}
+
+$self->{'comparaDBA'}  = new Bio::EnsEMBL::Compara::DBSQL::DBAdaptor(%{$self->{'compara_conf'}});
+$self->{'pipelineDBA'} = new Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor(-DBCONN => $self->{'comparaDBA'});
+
+update_strand($self, $self->{'genome_db_id'});
+
+exit(0);
+
+
+#######################
+#
+# subroutines
+#
+#######################
+
+sub usage {
+  print "comparaUpdateStrand.pl [options]\n";
+  print "  -help                  : print this help\n";
+  print "  -conf <path>           : config file describing compara, templates, and external genome databases\n";
+  print "  -dbhost <machine>      : compara mysql database host <machine>\n";
+  print "  -dbport <port#>        : compara mysql port number\n";
+  print "  -dbname <name>         : compara mysql database <name>\n";
+  print "  -dbuser <name>         : compara mysql connection user <name>\n";
+  print "  -dbpass <pass>         : compara mysql connection password\n";
+  print "  -gdb <int>             : genome_db_id for genomeDB\n";
+  print "comparaUpdateStrand.pl v1.1\n";
+  
+  exit(1);  
+}
+
+
+sub parse_conf {
+  my $self      = shift;
+  my $conf_file = shift;
+
+  if($conf_file and (-e $conf_file)) {
+    #read configuration file from disk
+    my @conf_list = @{do $conf_file};
+
+    foreach my $confPtr (@conf_list) {
+      #print("HANDLE type " . $confPtr->{TYPE} . "\n");
+      if($confPtr->{TYPE} eq 'COMPARA') {
+        $self->{'compara_conf'} = $confPtr;
+      }
+      if($confPtr->{TYPE} eq 'BLAST_TEMPLATE') {
+        $self->{'analysis_template'} = $confPtr;
+      }
+      if($confPtr->{TYPE} eq 'SPECIES') {
+        push @{$self->{'speciesList'}}, $confPtr;
+      }
+    }
+  }
+}
+
+
+
+sub update_strand {
+  my $self         = shift;
+  my $genome_db_id = shift;
+
+  my $db = $self->{'comparaDBA'};
+
+  my $genome_db      = $db->get_GenomeDBAdaptor->fetch_by_dbID($genome_db_id);
+  my $coreDBA        = $genome_db->connect_to_genome_locator();
+  my $geneDBA        = $coreDBA->get_GeneAdaptor();
+  my $transcriptDBA = $coreDBA->get_TranscriptAdaptor();
+
+  my $sth = $db->prepare("SELECT member.member_id, member.stable_id, source.source_name " .
+                         " FROM member, source" .
+                         " WHERE member.source_id=source.source_id".
+                         " AND member.chr_strand=0".
+                         " AND member.genome_db_id=$genome_db_id".
+                         " LIMIT 5 ");
+  $sth->execute();
+
+  my ($member_id, $stable_id, $source);
+  my $memberCount=0;
+  $sth->bind_columns( \$member_id, \$stable_id, \$source );
+
+  while( $sth->fetch() ) {
+    $memberCount++;
+    my $chr_strand = '0';
+    if($source eq 'ENSEMBLGENE') {
+      my $gene = $geneDBA->fetch_by_stable_id($stable_id);
+      $chr_strand = $gene->seq_region_strand;
+    }
+    if($source eq 'ENSEMBLPEP') {
+      my $transcript = $transcriptDBA->fetch_by_translation_stable_id($stable_id);
+      $chr_strand = $transcript->seq_region_strand;
+    }
+
+    if($chr_strand ne '0') {
+      my $sth_mem = $db->prepare("UPDATE member SET chr_strand=? WHERE member_id=?");
+      $sth_mem->execute($chr_strand, $member_id);
+      $sth_mem->finish;
+      print("converted chr_strand to '$chr_strand' for member_id=$member_id\n");
+    }
+    else {
+      warn("did get valid chr_strand\n");
+    }
+
+  }
+  $sth->finish();
+  return $memberCount;
+}
