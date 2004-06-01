14a48f19662e3d4ca78d7317d69c008ccddbaa4d
Jessica Severin <jessica@sanger.ac.uk>
2004-06-01 15:25:28 +0000

    removed old code and changed code to allow switching off of pipeline configuration

--- scripts/pipeline/comparaLoadGenomes.pl
+++ scripts/pipeline/comparaLoadGenomes.pl
@@ -60,7 +60,7 @@ if(%analysis_template and (not(-d $analysis_template{'fasta_dir'}))) {
 my $self = bless {};
 
 $self->{'comparaDBA'}  = new Bio::EnsEMBL::Compara::DBSQL::DBAdaptor(%compara_conf);
-$self->{'pipelineDBA'} = new Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor(-DBCONN => $self->{'comparaDBA'});
+#$self->{'pipelineDBA'} = new Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor(-DBCONN => $self->{'comparaDBA'});
 
 my $analysis = $self->prepareGenomeAnalysis();
 
@@ -134,7 +134,7 @@ sub prepareGenomeAnalysis
       -input_id_type   => 'genome_db_id',
       -module          => 'Bio::EnsEMBL::Compara::RunnableDB::Dummy'
     );
-  $self->{'pipelineDBA'}->get_AnalysisAdaptor()->store($submit_analysis);
+  $self->{'comparaDBA'}->get_AnalysisAdaptor()->store($submit_analysis);
 
   return $submit_analysis  
     unless($analysis_template{fasta_dir});
@@ -145,12 +145,14 @@ sub prepareGenomeAnalysis
       -input_id_type   => 'genome_db_id',
       -module          => 'Bio::EnsEMBL::Compara::RunnableDB::GenomeLoadMembers'
     );
-  $self->{'pipelineDBA'}->get_AnalysisAdaptor()->store($load_analysis);
+  $self->{'comparaDBA'}->get_AnalysisAdaptor()->store($load_analysis);
 
-  my $rule = Bio::EnsEMBL::Pipeline::Rule->new('-goalAnalysis'=>$load_analysis);
-  $rule->add_condition($submit_analysis->logic_name());
-  unless(checkIfRuleExists($self->{'pipelineDBA'}, $rule)) {
-    $self->{'pipelineDBA'}->get_RuleAdaptor->store($rule);
+  if(defined($self->{'pipelineDBA'})) {
+    my $rule = Bio::EnsEMBL::Pipeline::Rule->new('-goalAnalysis'=>$load_analysis);
+    $rule->add_condition($submit_analysis->logic_name());
+    unless(checkIfRuleExists($self->{'pipelineDBA'}, $rule)) {
+      $self->{'pipelineDBA'}->get_RuleAdaptor->store($rule);
+    }
   }
   my $simplerule = Bio::EnsEMBL::Hive::SimpleRule->new(
       '-condition_analysis' => $submit_analysis,
@@ -165,12 +167,14 @@ sub prepareGenomeAnalysis
       -module          => 'Bio::EnsEMBL::Compara::RunnableDB::GenomeDumpFasta',
       -parameters      => 'fasta_dir=>'.$analysis_template{fasta_dir}.',',
     );
-  $self->{'pipelineDBA'}->get_AnalysisAdaptor()->store($dumpfasta_analysis);
+  $self->{'comparaDBA'}->get_AnalysisAdaptor()->store($dumpfasta_analysis);
 
-  $rule = Bio::EnsEMBL::Pipeline::Rule->new('-goalAnalysis'=>$dumpfasta_analysis);
-  $rule->add_condition($load_analysis->logic_name());
-  unless(checkIfRuleExists($self->{'pipelineDBA'}, $rule)) {
-    $self->{'pipelineDBA'}->get_RuleAdaptor->store($rule);
+  if(defined($self->{'pipelineDBA'})) {
+    my $rule = Bio::EnsEMBL::Pipeline::Rule->new('-goalAnalysis'=>$dumpfasta_analysis);
+    $rule->add_condition($load_analysis->logic_name());
+    unless(checkIfRuleExists($self->{'pipelineDBA'}, $rule)) {
+      $self->{'pipelineDBA'}->get_RuleAdaptor->store($rule);
+    }
   }
   $simplerule = Bio::EnsEMBL::Hive::SimpleRule->new(
       '-condition_analysis' => $load_analysis,
@@ -185,12 +189,14 @@ sub prepareGenomeAnalysis
       -module          => 'Bio::EnsEMBL::Compara::RunnableDB::CreateBlastRules',
       -parameters      => 'fasta_dir=>'.$analysis_template{fasta_dir}.',',
     );
-  $self->{'pipelineDBA'}->get_AnalysisAdaptor()->store($blastrules_analysis);
+  $self->{'comparaDBA'}->get_AnalysisAdaptor()->store($blastrules_analysis);
 
-  $rule = Bio::EnsEMBL::Pipeline::Rule->new('-goalAnalysis'=>$blastrules_analysis);
-  $rule->add_condition($dumpfasta_analysis->logic_name());
-  unless(checkIfRuleExists($self->{'pipelineDBA'}, $rule)) {
-    $self->{'pipelineDBA'}->get_RuleAdaptor->store($rule);
+  if(defined($self->{'pipelineDBA'})) {
+    my $rule = Bio::EnsEMBL::Pipeline::Rule->new('-goalAnalysis'=>$blastrules_analysis);
+    $rule->add_condition($dumpfasta_analysis->logic_name());
+    unless(checkIfRuleExists($self->{'pipelineDBA'}, $rule)) {
+      $self->{'pipelineDBA'}->get_RuleAdaptor->store($rule);
+    }
   }
   $simplerule = Bio::EnsEMBL::Hive::SimpleRule->new(
       '-condition_analysis' => $dumpfasta_analysis,
@@ -203,7 +209,7 @@ sub prepareGenomeAnalysis
   my $blast_template = new Bio::EnsEMBL::Pipeline::Analysis(%analysis_template);
   $blast_template->logic_name("blast_template");
   $blast_template->input_id_type('MemberPep');
-  eval { $self->{'pipelineDBA'}->get_AnalysisAdaptor()->store($blast_template); };
+  eval { $self->{'comparaDBA'}->get_AnalysisAdaptor()->store($blast_template); };
 
   return $submit_analysis;
 }
@@ -347,7 +353,7 @@ sub submitGenome
         0              #save runtime NO (ie do insert)
       );
       print("  stored genome_db_id in input_id_analysis\n") if($verbose);
-  };
+  } if(defined($self->{'pipelineDBA'}));
 
   $self->{'comparaDBA'}->get_AnalysisJobAdaptor->create_new_job(
       -input_id       => $input_id,
@@ -376,327 +382,14 @@ sub prepareMemberPepAnalyses
       -parameters      => "genome_db_id=>".$species->{'genome_db'}->dbID(), #"subset_id=>".$subset->dbID().
       -input_id_type   => 'MemberPep'
     );
-  $self->{'pipelineDBA'}->get_AnalysisAdaptor()->store($submitpep_analysis);
+  $self->{'comparaDBA'}->get_AnalysisAdaptor()->store($submitpep_analysis);
 
 
   my $blast_analysis = new Bio::EnsEMBL::Pipeline::Analysis(%analysis_template);
   $blast_analysis->logic_name("blast_" . $species->{'genome_db'}->dbID(). "_". $species->{'genome_db'}->assembly());
   $blast_analysis->input_id_type('MemberPep');
-  $self->{'pipelineDBA'}->get_AnalysisAdaptor()->store($blast_analysis);
+  $self->{'comparaDBA'}->get_AnalysisAdaptor()->store($blast_analysis);
 
 }
 
 
-
-=head3
-if(defined($genome_db_id)) {
-  my @subsetIds = @{ getSubsetIdsForGenomeDBId($comparaDBA) };
-
-  if($#subsetIds > 0) {
-    die ("ERROR in Compara DB: more than 1 subset of longest peptides defined for genome_db_id = $genome_db_id\n");
-  }
-  if($#subsetIds < 0) {
-    die ("ERROR in Compara DB: no subset of longest peptides defined for genome_db_id = $genome_db_id\n");
-  }
-  $subset_id = $subsetIds[0];
-}
-=cut
-
-
-=head4
-sub getSubsetIdForGenomeDBId {
-  my ($dbh, $genome_db_id) = @_;
-
-  my @subsetIds = ();
-  my $subset_id;
-
-  my $sql = "SELECT distinct subset.subset_id " .
-            "FROM member, subset, subset_member " .
-            "WHERE subset.subset_id=subset_member.subset_id ".
-            "AND subset.description like '%longest%' ".
-            "AND member.member_id=subset_member.member_id ".
-            "AND member.genome_db_id=$genome_db_id;";
-  my $sth = $dbh->prepare( $sql );
-  $sth->execute();
-
-  $sth->bind_columns( undef, \$subset_id );
-
-  while( $sth->fetch() ) {
-    print("found subset_id = $subset_id for genome_db_id = $genome_db_id\n");
-    push @subsetIds, $subset_id;
-  }
-
-  $sth->finish();
-
-  if($#subsetIds > 0) {
-    warn ("Compara DB: more than 1 subset of longest peptides defined for genome_db_id = $genome_db_id\n");
-  }
-  if($#subsetIds < 0) {
-    warn ("Compara DB: no subset of longest peptides defined for genome_db_id = $genome_db_id\n");
-  }
-
-  return $subsetIds[0];
-}
-
-
-sub getGenomeDBIdForSubsetId{
-  my ($dbh, $subset_id) = @_;
-  
-  my @genomeIds = ();
-  my $genome_db_id;
-
-  my $sql = "SELECT distinct member.genome_db_id " .
-            "FROM member, subset_member " .
-	    "WHERE subset_member.subset_id=$subset_id  ".
-	    "AND member.member_id=subset_member.member_id;";
-  my $sth = $dbh->prepare( $sql );
-  $sth->execute();
-
-  $sth->bind_columns( undef, \$genome_db_id );
-
-  while( $sth->fetch() ) {
-    print("found genome_db_id = $genome_db_id for subset_id = $subset_id\n");
-    push @genomeIds, $genome_db_id;
-  }
-  
-  $sth->finish();
-
-  if($#genomeIds > 0) {
-    warn ("Compara DB: more than 1 subset of longest peptides defined for genome_db_id = $genome_db_id\n");
-  }
-  if($#genomeIds < 0) {
-    warn ("Compara DB: no subset of longest peptides defined for genome_db_id = $genome_db_id\n");
-  }
-
-  return $genomeIds[0];
-}
-
-
-sub getFastaNameForSubsetID {
-  my ($dbh, $subset_id) = @_;
-  
-  my($name, $species, $assembly);
-
-
-  my $sql = "SELECT genome_db.name, genome_db.assembly " .
-            "FROM subset_member, member, genome_db " .
-	    "WHERE subset_member.subset_id='$subset_id' ".
-	    "AND member.member_id=subset_member.member_id ".
-	    "AND member.genome_db_id=genome_db.genome_db_id ".
-	    "GROUP BY genome_db.genome_db_id;";
-  my $sth = $dbh->prepare( $sql );
-  $sth->execute();
-
-  $sth->bind_columns( undef, \$species, \$assembly );
-
-  while( $sth->fetch() ) {
-    $species =~ s/\s+/_/g;
-    $name = $fastadir . "/" . $species . "_" . $assembly . ".fasta";
-    $name =~ s/\/\//\//g;
-    print("name = '$name'\n");
-  }
-
-  $sth->finish();
-  return $name;
-}
-
-
-sub getFastaNameForGenomeDBID {
-  my ($dbh, $genomeDBID) = @_;
-  
-  my($name, $species, $assembly);
-  
-
-  my $sql = "SELECT genome_db.name, genome_db.assembly " .
-            "FROM genome_db " .
-	    "WHERE genome_db.genome_db_id=$genomeDBID;";
-  my $sth = $dbh->prepare( $sql );
-  $sth->execute();
-
-  $sth->bind_columns( undef, \$species, \$assembly );
-
-  while( $sth->fetch() ) {
-    $species =~ s/\s+/_/g;
-    $name = $fastadir . "/" . $species . "_" . $assembly . ".fasta";
-    $name =~ s/\/\//\//g;
-    print("name = '$name'\n");
-  }
-
-  $sth->finish();
-  return $name;
-}
-
-
-sub dumpFastaForSubset {
-  my($dbh, $subset_id, $fastafile) = @_;
-
-  my $sql = "SELECT member.stable_id, member.description, sequence.sequence " .
-            "FROM member, sequence, subset_member " .
-	    "WHERE subset_member.subset_id = $subset_id ".
-	    "AND member.member_id=subset_member.member_id ".
-	    "AND member.sequence_id=sequence.sequence_id " .
-	    "GROUP BY member.member_id ORDER BY member.stable_id;";
-
-  open FASTAFILE, ">$fastafile"
-    or die "Could open $fastafile for output\n";
-  print("writing fasta to loc '$fastafile'\n");
-
-  my $sth = $dbh->prepare( $sql );
-  $sth->execute();
-
-  my ($stable_id, $description, $sequence);
-  $sth->bind_columns( undef, \$stable_id, \$description, \$sequence );
-
-  while( $sth->fetch() ) {
-    $sequence =~ s/(.{72})/$1\n/g;
-    print FASTAFILE ">$stable_id $description\n$sequence\n";
-  }
-  close(FASTAFILE);
-
-  $sth->finish();
-
-  #
-  # update this subset_id's  subset.dump_loc with the full path of this dumped fasta file
-  #
-
-  $sth = $dbh->prepare("UPDATE subset SET dump_loc = ? WHERE subset_id = ?");
-  $sth->execute($fastafile, $subset_id);
-
-  #addAnalysisForSubset($dbh, $subset_id, $fastafile);
-  SubmitSubsetForAnalysis($subset_id, $fastafile);
-
-  print("Prepare fasta file as blast database\n");
-  system("setdb $fastafile");
-}
-
-
-sub taxonIDForSubsetID {
-  my ($dbh, $subset_id) = @_;
-
-  my $taxon_id = undef;
-
-
-  my $sql = "SELECT distinct member.taxon_id " .
-            "FROM subset_member, member " .
-	    "WHERE subset_member.subset_id='$subset_id' ".
-	    "AND member.member_id=subset_member.member_id;";
-  my $sth = $dbh->prepare( $sql );
-  $sth->execute();
-
-  $sth->bind_columns( undef, \$taxon_id );
-
-  if( $sth->fetch() ) {
-    print("taxon_id = '$taxon_id'\n");
-  }
-  $sth->finish();
-  return $taxon_id;
-}
-
-
-sub addAnalysisForSubset {
-  my($dbh, $subset_id, $fastafile) = @_;
-
-  unless(defined($analysis_conf)) { return; }
-  print("read analysis template from '$analysis_conf'\n");
-
-  my %analconf = %{do $analysis_conf};
-
-  my $analysisAdaptor = $dbh->get_AnalysisAdaptor();
-
-  my $taxon_id = taxonIDForSubsetID($dbh, $subset_id);
-  my $logic_name = $analconf{logic_name} . "_$taxon_id";
-
-  my $analysis = Bio::EnsEMBL::Pipeline::Analysis->new(
-      -db              => "subset_id=$subset_id",
-      -db_file         => $fastafile,
-      -db_version      => '1',
-      -logic_name      => $logic_name,
-      -program         => $analconf{program},
-      -program_version => $analconf{program_version},
-      -program_file    => $analconf{program_file},
-      -gff_source      => $analconf{gff_source},
-      -gff_feature     => $analconf{gff_feature},
-      -module          => $analconf{module},
-      -module_version  => $analconf{module_version},
-      -parameters      => $analconf{parameters},
-      -created         => $analconf{created}
-    );
-
-  $analysisAdaptor->store($analysis);  
-} 
-
-sub SubmitSubsetForAnalysis {
-  my($comparaDBA, $pipelineDBA, $subset) = @_;
-
-  print("\nSubmitSubsetForAnalysis\n");
-
-  my $sicDBA = $pipelineDBA->get_StateInfoContainer;
-
-  my $genome = $comparaDBA->get_GenomeDBAdaptor()->fetch_by_dbID($subset->{genome_db_id});
-  my $logic_name = "SubmitPep_" . $genome->assembly();
-
-  my $analysis = Bio::EnsEMBL::Pipeline::Analysis->new(
-      -db              => "subset_id=" . $subset->dbID().";genome_db_id=".$genome->dbID,
-      -db_file         => $subset->dump_loc(),
-      -db_version      => '1',
-      -logic_name      => $logic_name,
-      -input_id_type   => 'MemberPep'
-    );
-
-  $pipelineDBA->get_AnalysisAdaptor()->store($analysis);
-
-  #my $host = hostname();
-  print("store using sic\n");
-  my $errorCount=0;
-  my $tryCount=0;
-  eval {
-    foreach my $member_id (@{$subset->member_id_list()}) {
-      eval {
-        $tryCount++;
-        $sicDBA->store_input_id_analysis($member_id, #input_id
-                                         $analysis,
-                                         'earth', #execution_host
-                                         0 #save runtime NO (ie do insert)
-                                        );
-      };
-      if($@) {
-        $errorCount++;
-        if($errorCount>42 && ($errorCount/$tryCount > 0.95)) {
-          die("too many repeated failed insert attempts, assume will continue for durration. ACK!!\n");
-        }
-      } # should handle the error, but ignore for now
-      if($tryCount>=5) { last; }
-    }
-  };
-  print("CREATED all input_id_analysis\n");
-
-
-  #
-  # now add the 'blast' analysis
-  #
-  my $logic_name = "blast_" . $genome->assembly();
-  my $analysis = Bio::EnsEMBL::Pipeline::Analysis->new(
-      -db              => "subset_id=" . $subset->dbID().";genome_db_id=".$genome->dbID,
-      -db_file         => $subset->dump_loc(),
-      -db_version      => '1',
-      -logic_name      => $logic_name,
-      -input_id_type   => 'MemberPep'
-    );
-
-  $pipelineDBA->get_AnalysisAdaptor()->store($analysis);
-
-
-
-  my $logic_name = "blast_" . $species1Ptr->{abrev};
-  print("build analysis $logic_name\n");
-  my %analParams = %analysis_template;
-  $analParams{'-logic_name'}    = $logic_name;
-  $analParams{'-input_id_type'} = $species1Ptr->{condition}->input_id_type();
-  $analParams{'-db'}            = $species2Ptr->{abrev};
-  $analParams{'-db_file'}       = $species2Ptr->{condition}->db_file();
-  my $analysis = new Bio::EnsEMBL::Pipeline::Analysis(%analParams);
-  $db->get_AnalysisAdaptor->store($analysis);
-
-
-}
-=cut
