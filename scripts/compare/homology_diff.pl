8ee4de79e72a54da5f5e77302895002b128fedee
Jessica Severin <jessica@sanger.ac.uk>
2004-08-02 17:10:06 +0000

    updated to deal with new homology types.  switched options to use two urls rather than a conf file.  Output conversions in a matrix form in addition to old output.

--- scripts/compare/homology_diff.pl
+++ scripts/compare/homology_diff.pl
@@ -22,7 +22,7 @@ use Bio::EnsEMBL::Pipeline::Analysis;
 use Bio::EnsEMBL::Pipeline::Rule;
 use Bio::EnsEMBL::Compara::GenomeDB;
 use Bio::EnsEMBL::DBLoader;
-
+use Bio::EnsEMBL::Hive::URLFactory;
 
 # ok this is a hack, but I'm going to pretend I've got an object here
 # by creating a blessed hash ref and passing it around like an object
@@ -30,25 +30,19 @@ use Bio::EnsEMBL::DBLoader;
 # the globals into a nice '$self' package
 my $self = bless {};
 
-$self->{'compara_conf'} = {};
-$self->{'compara_conf'}->{'-user'} = 'ensro';
-$self->{'compara_conf'}->{'-port'} = 3306;
-
-$self->{'compara_ref_conf'} = {};
-$self->{'compara_ref_conf'}->{'-user'} = 'ensro';
-$self->{'compara_ref_conf'}->{'-port'} = 3306;
-
-my $conf_file;
-my ($help, $host, $user, $pass, $dbname, $port, $adaptor);
 $self->{'compara_ref_hash'}    = {};
 $self->{'compara_ref_missing'} = {};
-$self->{'compara_ref_dups'} = {};
+$self->{'compara_new_hash'} = {};
+$self->{'conversion_hash'} = {};
+$self->{'allTypes'} = {};
 
 $self->{'refDups'} = 0;
 $self->{'newDups'} = 0;
 
+my $help;
 my $sameBRH=0;
 my $sameRHS=0;
+my $BRH2BRH=0;  #changed BRH sub type (eg BRH to BRH_MULTI)
 my $BRH2RHS=0;
 my $RHS2BRH=0;
 my $countAdds=0;
@@ -58,39 +52,33 @@ my $BRHCount=0;
 my $RHSCount=0;
 my $newBRH=0;
 my $newRHS=0;
+my $url     = undef;
+my $url_ref = undef;
 
 GetOptions('help'     => \$help,
-           'conf=s'   => \$conf_file,
-           'dbhost=s' => \$host,
-           'dbport=i' => \$port,
-           'dbuser=s' => \$user,
-           'dbpass=s' => \$pass,
-           'dbname=s' => \$dbname,
+           'url1=s'   => \$url_ref,
+           'url2=s'   => \$url,
            'gdb1=i'   => \$self->{'genome_db_id_1'},
            'gdb2=i'   => \$self->{'genome_db_id_2'},
           );
 
 if ($help) { usage(); }
 
-parse_conf($self, $conf_file);
-
-if($host)   { $self->{'compara_conf'}->{'-host'}   = $host; }
-if($port)   { $self->{'compara_conf'}->{'-port'}   = $port; }
-if($dbname) { $self->{'compara_conf'}->{'-dbname'} = $dbname; }
-if($user)   { $self->{'compara_conf'}->{'-user'}   = $user; }
-if($pass)   { $self->{'compara_conf'}->{'-pass'}   = $pass; }
-
-
-unless(defined($self->{'compara_conf'}->{'-host'})
-       and defined($self->{'compara_conf'}->{'-user'})
-       and defined($self->{'compara_conf'}->{'-dbname'}))
-{
-  print "\nERROR : must specify host, user, and database to connect to compara\n\n";
+unless($url and $url_ref) {
+  print "\nERROR : must specify url for 2 compara databases\n\n";
   usage();
 }
 
-$self->{'comparaDBA'}  = new Bio::EnsEMBL::Compara::DBSQL::DBAdaptor(%{$self->{'compara_conf'}});
-$self->{'refComparaDBA'}  = new Bio::EnsEMBL::Compara::DBSQL::DBAdaptor(%{$self->{'compara_ref_conf'}});
+$self->{'comparaDBA'}    = Bio::EnsEMBL::Hive::URLFactory->fetch($url, 'compara');
+$self->{'refComparaDBA'} = Bio::EnsEMBL::Hive::URLFactory->fetch($url_ref, 'compara');
+
+unless($self->{'comparaDBA'} and $self->{'refComparaDBA'} and
+       $self->{'comparaDBA'}->isa('Bio::EnsEMBL::Compara::DBSQL::DBAdaptor') and
+       $self->{'refComparaDBA'}->isa('Bio::EnsEMBL::Compara::DBSQL::DBAdaptor'))
+{
+  print "\nERROR : must specify must specify url for 2 compara databases\n\n";
+  usage();
+}
 
 loadReferenceCompara($self);
 
@@ -102,17 +90,18 @@ print("  ", $self->{'refDups'}, " duplicate homologies for cleanup\n");
 print("new compara\n  $new_homology_count total homologies\n");
 print("  ", $self->{'newDups'}, " duplicate homologies for cleanup\n");
 
-printf("%1.1f%% same BRH ($sameBRH/$BRHCount)\n", scalar($sameBRH/$BRHCount*100.0)) if($BRHCount>0);
-printf("%1.1f%% same RHS ($sameRHS/$RHSCount)\n", scalar($sameRHS/$RHSCount*100.0)) if($RHSCount>0);
-print("$BRH2RHS converted BRH20 -> RHS21\n");
-print("$RHS2BRH converted RHS20 -> BRH21\n");
-print("$newBRH new BRH Homologies\n");
-print("$newRHS new RHS Homologies\n");
-print(scalar(keys(%{$self->{'compara_ref_missing'}})) . " lost homologies\n");
+print("differences\n");
+printf("%6.1f%% same BRH ($sameBRH/$BRHCount)\n", scalar($sameBRH/$BRHCount*100.0)) if($BRHCount>0);
+printf("%6.1f%% same RHS ($sameRHS/$RHSCount)\n", scalar($sameRHS/$RHSCount*100.0)) if($RHSCount>0);
+printf("%7d switched BRH subtype\n", $BRH2BRH);
+printf("%7d converted BRH -> RHS\n", $BRH2RHS);
+printf("%7d converted RHS -> BRH\n", $RHS2BRH);
+printf("%7d new BRH Homologies\n", $newBRH);
+printf("%7d new RHS Homologies\n", $newRHS);
 
-foreach my $key (keys(%{$self->{'compara_ref_missing'}})) {
-  print("  $key => " . $self->{'compara_ref_missing'}->{$key} . "\n");
-}
+print_conversion_stats($self);
+print_missing_stats($self);
+
 exit(0);
 
 
@@ -123,17 +112,13 @@ exit(0);
 #######################
 
 sub usage {
-  print "comparaDumpAllPeptides.pl [options]\n";
+  print "homology_diff.pl [options]\n";
   print "  -help                  : print this help\n";
-  print "  -conf <path>           : config file describing compara, templates, and external genome databases\n";
-  print "  -dbhost <machine>      : compara mysql database host <machine>\n";
-  print "  -dbport <port#>        : compara mysql port number\n";
-  print "  -dbname <name>         : compara mysql database <name>\n";
-  print "  -dbuser <name>         : compara mysql connection user <name>\n";
-  print "  -dbpass <pass>         : compara mysql connection password\n";
+  print "  -url1 <str>            : url of reference compara DB\n";
+  print "  -url2 <str>            : url of compara DB \n";
   print "  -gdb1 <int>            : genome_db_id of first genome\n";
   print "  -gdb2 <int>            : genome_db_id of second genome\n";
-  print "comparaDumpAllPeptides.pl v1.1\n";
+  print "homology_diff.pl v1.1\n";
 
   exit(1);
 }
@@ -200,7 +185,7 @@ sub loadReferenceCompara
             " AND h.homology_id=hm2.homology_id AND hm2.member_id=m2.member_id ".
             " AND m2.genome_db_id= ". $self->{'genome_db_id_2'};
             #" ORDER BY  m1.stable_id, m2.stable_id";
-  my $sth = $self->{'refComparaDBA'}->prepare( $sql );
+  my $sth = $self->{'refComparaDBA'}->dbc->prepare( $sql );
   $sth->execute();
   my ($gene_id1, $gene_id2, $type);
   $sth->bind_columns(\$gene_id1, \$gene_id2, \$type );
@@ -217,6 +202,11 @@ sub loadReferenceCompara
 
     $self->{'compara_ref_hash'}->{$key} = $type;
     $self->{'compara_ref_missing'}->{$key} = $type;
+    $self->{'allTypes'}->{$type} = $type;
+
+    $self->{'conversion_hash'}->{$type} = {'total'=>0} unless($self->{'conversion_hash'}->{$type});
+    my $count = $self->{'conversion_hash'}->{$type}->{'total'};
+    $self->{'conversion_hash'}->{$type}->{'total'} = $count+1;
   }
 }
 
@@ -234,40 +224,102 @@ sub compareComparas
             " AND h.homology_id=hm2.homology_id AND hm2.member_id=m2.member_id ".
             " AND m2.genome_db_id= ". $self->{'genome_db_id_2'};
             #" ORDER BY  m1.stable_id, m2.stable_id";
-  my $sth = $self->{'comparaDBA'}->prepare( $sql );
+  my $sth = $self->{'comparaDBA'}->dbc->prepare( $sql );
   $sth->execute();
-  my ($gene_id1, $gene_id2, $type21, $homology_id);
-  $sth->bind_columns(\$gene_id1, \$gene_id2, \$type21, \$homology_id );
+  my ($gene_id1, $gene_id2, $new_type, $homology_id);
+  $sth->bind_columns(\$gene_id1, \$gene_id2, \$new_type, \$homology_id );
   while($sth->fetch()) {
     my $key = $gene_id1 ."_". $gene_id2;
     my $ref_type = $self->{'compara_ref_hash'}->{$key};
 
-    if($self->{'compara_ref_dups'}->{$key}) {
+    if($self->{'compara_new_hash'}->{$key}) {
       $self->{'newDups'}++;
-      print("new_compara duplicate '$key' already as ".$self->{'compara_ref_hash'}->{$key}."\n");
+      print("new_compara duplicate '$key' as $new_type and ",
+            $self->{'compara_new_hash'}->{$key},
+            "\n");
     }
-    $self->{'compara_ref_dups'}->{$key} =
+    $self->{'compara_new_hash'}->{$key} = $new_type;
+
+    $self->{'allTypes'}->{$new_type} = $new_type;
+
+    #my $count = $self->{'allTypes'}->{$new_type};
+    #$count=0 unless($count);
+    #$self->{'allTypes'}->{$new_type} = $count+1;
 
 
-
-    
-    #print("check compara21 key='$key' '$type21' vs '$type20'\n");
+    #print("check compara21 key='$key' '$new_type' vs '$type20'\n");
     $new_homology_count++;
-    #$BRHCount++ if($type21 eq 'BRH');
-    #$RHSCount++ if($type21 eq 'RHS');
+    #$BRHCount++ if($new_type eq 'BRH');
+    #$RHSCount++ if($new_type eq 'RHS');
     if($ref_type) {
-      #print("check compara21 key='$key' '$type21' vs '$type20'\n");
-      $sameBRH++ if(($ref_type eq 'BRH') and ($type21 eq 'BRH'));
-      $sameRHS++ if(($ref_type eq 'RHS') and ($type21 eq 'RHS'));
-      $BRH2RHS++ if(($ref_type eq 'BRH') and ($type21 eq 'RHS'));
-      $RHS2BRH++ if(($ref_type eq 'RHS') and ($type21 eq 'BRH'));
+      #print("check compara21 key='$key' '$new_type' vs '$type20'\n");
+      $sameBRH++ if(($ref_type =~ /^BRH/) and ($ref_type eq $new_type));
+      $sameRHS++ if(($ref_type =~ /^RHS/) and ($ref_type eq $new_type));
+
+      my $count = $self->{'conversion_hash'}->{$ref_type}->{$new_type};
+      $count=0 unless($count);
+      $self->{'conversion_hash'}->{$ref_type}->{$new_type} = $count+1;
+      
+      $BRH2BRH++ if(($ref_type =~ /^BRH/) and ($new_type =~ /^BRH/) and ($ref_type ne $new_type));
+      $BRH2RHS++ if(($ref_type =~ /^BRH/) and ($new_type eq 'RHS'));
+      $RHS2BRH++ if(($ref_type eq 'RHS') and ($new_type =~ /^BRH/));
 
       delete $self->{'compara_ref_missing'}->{$key};
     }
     else {
-      $newBRH++ if($type21 eq 'BRH');
-      $newRHS++ if($type21 eq 'RHS');
+      $newBRH++ if($new_type =~ /^BRH/);
+      $newRHS++ if($new_type =~ /^RHS/);
     }
   }
+}
+
+
+sub print_conversion_stats
+{
+  my $self = shift;
+  my %typeCount;
+  my @allTypes = sort(keys(%{$self->{'allTypes'}}));
+
+  printf("%10s ", "old/new");
+  foreach my $new_type (@allTypes) {
+    printf("%10s ", $new_type);
+  }
+  printf("%10s\n",'old_total');
+  
+  foreach my $ref_type (@allTypes) {
+    my $convHash = $self->{'conversion_hash'}->{$ref_type};
+    printf("%10s ", $ref_type);
+    foreach my $new_type (@allTypes) {
+      my $count = $self->{'conversion_hash'}->{$ref_type}->{$new_type};
+      $count=0 unless($count);
+      printf("%10d ", $count);
+    }
+    my $count = $self->{'conversion_hash'}->{$ref_type}->{'total'};
+    $count=0 unless($count);
+    printf("%10d\n", $count);
+  }
+}
+
+
+sub print_missing_stats
+{
+  my $self = shift;
+  my %typeCount;
+
+  print(scalar(keys(%{$self->{'compara_ref_missing'}})) . " lost homologies\n");
+
+  foreach my $key (keys(%{$self->{'compara_ref_missing'}})) {
+    my $type = $self->{'compara_ref_missing'}->{$key};
+    my $count = $typeCount{$type};
+    $count=0 unless($count);
+    $typeCount{$type} = $count+1;
+    #print("  $key => " . $self->{'compara_ref_missing'}->{$key} . "\n");
+  }
+  
+  foreach my $type (keys(%typeCount)) {
+    print("  $type ", $typeCount{$type}, "\n");
+  }
+
+      
 }
 
