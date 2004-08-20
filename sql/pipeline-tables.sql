1769a5c78e281cbc52e78ba47af11523426fbd81
Javier Herrero <jh7@sanger.ac.uk>
2004-08-20 09:47:53 +0000

    Merged from main trunk

--- sql/pipeline-tables.sql
+++ sql/pipeline-tables.sql
@@ -4,15 +4,15 @@ CREATE TABLE subset (
  description    varchar(50),
  dump_loc       varchar(255),
 
- PRIMARY KEY (subset_id)
+ PRIMARY KEY (subset_id),
+ UNIQUE (description)
 );
 
 CREATE TABLE subset_member (
  subset_id   int(10) NOT NULL,
  member_id   int(10) NOT NULL,
 
- KEY(subset_id),
- KEY(member_id)
+ UNIQUE subset_member_id (subset_id, member_id)
 );
 
 
@@ -23,6 +23,7 @@ CREATE TABLE member_gene_peptide(
  peptide_member_id    int(10) NOT NULL,
 
  UNIQUE (gene_member_id, peptide_member_id),
+ KEY(peptide_member_id)
 );
 
 
@@ -46,100 +47,66 @@ CREATE TABLE genome_db_extn (
 
 ------------------------------------------------------------------------------------
 --
--- Table structure for table 'peptide_align_feature'
+-- Table structure for table 'genome_db_stats'
 --
--- overview: This tables stores the raw HSP local alignment results
---           of peptide to peptide alignments returned by a BLAST run
---           it is translated from a FeaturePair object
+-- overview: This tables statistics related to whole genomes
+--           The type of data analyzed is described in the 'data_type' field
 -- semantics:
--- peptide_align_feature_id  - internal id
--- qmember_id                - member.member_id of query peptide
--- hmember_id                - member.member_id of hit peptide
--- qgenome_db_id             - genome_db_id of query peptide (for query optimization)
--- hgenome_db_id             - genome_db_id of hit peptide (for query optimization)
--- qstart                    - start pos in query peptide sequence
--- qend                      - end  pos in query peptide sequence
--- hstart                    - start pos in hit peptide sequence
--- hend                      - end  pos in hit peptide sequence
--- score                     - blast score for this HSP
--- evalue                    - blast evalue for this HSP
--- align_length              - alignment length of HSP
--- identical_matches         - blast HSP match score
--- positive_matches          - blast HSP positive score
--- perc_ident                - percent identical matches in the HSP length
--- perc_pos                  - precent positive matches in the HSP length
--- cigar_line                - cigar string coding the actual alignment
+--   genome_db_id - link to genome_db table id
+--   data_type    - descriptor of type e.g. intergenic
+--   (rest of columns are standard statistical values calculated from the
+--    dataset defined by 'data_type')
 
-CREATE TABLE peptide_align_feature (
+CREATE TABLE genome_db_stats (
+    genome_db_id    int(10) NOT NULL default '0',
+    data_type       varchar(20) NOT NULL,
+    count           int(10) NOT NULL,
+    mean            double NOT NULL default '0',
+    median          double NOT NULL default '0',
+    mode            double NOT NULL,
+    stddev          double NOT NULL,
+    variance        double NOT NULL,
+    min             double NOT NULL default '0',
+    max             double NOT NULL default '0',
+    overlap_count   int(10) NOT NULL default '0',
 
-  peptide_align_feature_id  int(10) unsigned NOT NULL auto_increment,
-  qmember_id                int(10) NOT NULL,
-  hmember_id                int(10) NOT NULL,
-  qgenome_db_id             int(10) NOT NULL,
-  hgenome_db_id             int(10) NOT NULL,
-  analysis_id               int(10) NOT NULL,
-  qstart                    int(10) DEFAULT '0' NOT NULL,
-  qend                      int(10) DEFAULT '0' NOT NULL,
-  hstart                    int(11) DEFAULT '0' NOT NULL,
-  hend                      int(11) DEFAULT '0' NOT NULL,
-  score                     double(16,4) DEFAULT '0.0000' NOT NULL,
-  evalue                    varchar(20),
-  align_length              int(10),
-  identical_matches         int(10),
-  perc_ident                int(10),
-  positive_matches          int(10),
-  perc_pos                  int(10),
-  hit_rank                  int(10),
-  cigar_line                mediumtext,
-
-  PRIMARY KEY (peptide_align_feature_id),
-  KEY qmember_id  (qmember_id),
-  KEY hmember_id  (hmember_id)
-
+    UNIQUE KEY genome_db_id_type (genome_db_id, data_type)
 );
+
 
 
 ------------------------------------------------------------------------------------
 --
--- Table structure for table 'analysis'
+-- Table structure for table 'dnafrag_chunk'
+--
+-- overview: This tables holds chunking information so that these
+--           DNA chunks can be identified (stable_id) and cached
 --
 -- semantics:
--- analysis_id - internal id
--- created   - date to distinguish newer and older versions off the
---             same analysis. Not well maintained so far.
--- logic_name  string to identify the analysis. Used mainly inside pipeline.
--- db, db_version, db_file
---  - db should be a database name, db version the version of that db
---    db_file the file system location of that database,
---    probably wiser to generate from just db and configurations
--- program, program_version,program_file
---  - The binary used to create a feature. Similar semantic to above
--- module, module_version
---  - Perl module names (RunnableDBS usually) executing this analysis.
--- parameters a paramter string which is processed by the perl module
--- gff_source, gff_feature
---  - how to make a gff dump from features with this analysis
+--   dnafrag_chunk_id - primary key
+--   dnafrag_id       - foreign key link to dnafrag table
+--   stable_id        - unique string identifying chunk (to be used to
+--                      extract this sequence from a flat file)
+--                      (ie subset.dump_loc file linked via subset_dnafrag_chunk)
+--   seq_start        - start offset in dnafrag
+--   seq_end          - end offset in dnafrag
+--   sequence_id      - optional use (store chunk sequence in DB)
+                       
+CREATE TABLE dnafrag_chunk (
+  dnafrag_chunk_id    int(10) NOT NULL DEFAULT '0',
+  dnafrag_id          int(10) NOT NULL DEFAULT '0',
+  stable_id           varchar(40) NOT NULL,
+  seq_start           int(10) unsigned NOT NULL DEFAULT '0',
+  seq_end             int(10) unsigned NOT NULL DEFAULT '0',
 
-CREATE TABLE analysis (
+  PRIMARY KEY (dnafrag_chunk_id),
+  UNIQUE      (stable_id)
+);
 
-  analysis_id                 int(10) unsigned DEFAULT '0' NOT NULL auto_increment,
-  created                     datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
-  logic_name                  varchar(40) not null,
-  db                          varchar(120),
-  db_version                  varchar(40),
-  db_file                     varchar(120),
-  program                     varchar(80),
-  program_version             varchar(40),
-  program_file                varchar(80),
-  parameters                  varchar(255),
-  module                      varchar(80),
-  module_version              varchar(40),
-  gff_source                  varchar(40),
-  gff_feature                 varchar(40),
+CREATE TABLE subset_dnafrag_chunk (
+ subset_id          int(10) NOT NULL,
+ dnafrag_chunk_id   int(10) NOT NULL,
 
-  PRIMARY KEY (analysis_id),
-  KEY logic_name_idx( logic_name ),
-  UNIQUE(logic_name)
-
+ UNIQUE(subset_id, dnafrag_chunk_id)
 );
 
