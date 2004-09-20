4b7285abac3f52c32ef266339449835a356b0ac0
Abel Ureta-Vidal <abel@sanger.ac.uk>
2004-09-20 08:59:09 +0000

    Updated to new compara schema and code clean up at the same time

--- scripts/synteny/LoadComparaDb.pl
+++ scripts/synteny/LoadComparaDb.pl
@@ -1,139 +1,98 @@
 #!/usr/local/ensembl/bin/perl -w
 
 use strict;
+use Bio::EnsEMBL::Registry;
 use Getopt::Long;
-use Bio::EnsEMBL::Compara::DBSQL::DBAdaptor;
 use Bio::EnsEMBL::Compara::DnaFrag;
-
+use Bio::EnsEMBL::Compara::MethodLinkSpeciesSet;
 
 my $usage = "
-
-$0 -host ecs2.internal.sanger.ac.uk -dbuser ecs2dadmin -dbpass xxxx -port 3353 -dbname ensembl_compara_13_1 \
--conf_file /nfs/acari/abel/src/ensembl_main/ensembl-compara/modules/Bio/EnsEMBL/Compara/Compara.conf
--genome_db_id1 1 genome_db_id2 2
-
-";
+$0
+--help                print this menu
+--dbname string
+[--reg_conf string]
+--qy string
+--tg string
+\n";
 
 my $help = 0;
-my ($host,$dbname,$dbuser,$dbpass,$conf_file);
-my ($genome_db_id1,$genome_db_id2);
+my ($dbname,$qy_species, $tg_species, $reg_conf);
 my $method_link_type = "SYNTENY";
-my $port ='3352';
 
 GetOptions('help' => \$help,
-	   'host=s' => \$host,
-	   'dbuser=s' => \$dbuser,
-	   'dbpass=s' => \$dbpass,
-	   'port=i' => \$port,
 	   'dbname=s' => \$dbname,
-	   'conf_file=s' => \$conf_file,
-	   'genome_db_id1=i' => \$genome_db_id1,
-	   'genome_db_id2=i' => \$genome_db_id2);
+	   'reg_conf=s' => \$reg_conf,
+	   'qy=i' => \$qy_species,
+	   'tg=i' => \$tg_species);
+
+$|=1;
 
 if ($help) {
   print $usage;
   exit 0;
 }
 
-my $db = new Bio::EnsEMBL::Compara::DBSQL::DBAdaptor(-conf_file => $conf_file,
-						     -host => $host,
-						     -dbname => $dbname,
-						     -port => $port,
-						     -user => $dbuser,
-						     -pass => $dbpass);
+Bio::EnsEMBL::Registry->load_all($reg_conf);
 
-my $gdb_adaptor = $db->get_GenomeDBAdaptor;
-my $genome_db1 = $gdb_adaptor->fetch_by_dbID($genome_db_id1);
-my $genome_db2 = $gdb_adaptor->fetch_by_dbID($genome_db_id2);
+my $dbc = Bio::EnsEMBL::Registry->get_DBAdaptor($dbname, 'compara')->dbc;
 
+my $gdba = Bio::EnsEMBL::Registry->get_adaptor($dbname, 'compara', 'GenomeDB');
+my $dfa = Bio::EnsEMBL::Registry->get_adaptor($dbname, 'compara', 'DnaFrag');
+my $mlssa = Bio::EnsEMBL::Registry->get_adaptor($dbname, 'compara', 'Bio::EnsEMBL::Compara::MethodLinkSpeciesSet');
 
-my $dnafrag_adaptor = $db->get_DnaFragAdaptor;
+my $qy_binomial = Bio::EnsEMBL::Registry->get_adaptor($qy_species,'core','MetaContainer')->get_Species->binomial;
+my $tg_binomial = Bio::EnsEMBL::Registry->get_adaptor($tg_species,'core','MetaContainer')->get_Species->binomial;
 
-my $dbadaptor1 = $db->get_db_adaptor($genome_db1->name,$genome_db1->assembly);
-my @chromosomes1 = @{$dbadaptor1->get_SliceAdaptor->fetch_all('toplevel')};
-my %chromosomes1;
+my $qy_gdb = $gdba->fetch_by_name_assembly($qy_binomial);
+my $tg_gdb = $gdba->fetch_by_name_assembly($tg_binomial);
+my $mlss = new Bio::EnsEMBL::Compara::MethodLinkSpeciesSet;
+$mlss->method_link_type($method_link_type);
+$mlss->species([$qy_gdb, $tg_gdb]);
+$mlssa->store($mlss);
 
-foreach my $chr (@chromosomes1) {
-  $chromosomes1{$chr->seq_region_name} = $chr;
+my $qy_sa = Bio::EnsEMBL::Registry->get_adaptor($qy_species, 'core', 'Slice');
+my %qy_slices;
+foreach my $qy_slice (@{$qy_sa->fetch_all('toplevel')}) {
+  $qy_slices{$qy_slice->name} = $qy_slice;
 }
 
-my $dbadaptor2 = $db->get_db_adaptor($genome_db2->name,$genome_db2->assembly);
-my @chromosomes2 = @{$dbadaptor2->get_SliceAdaptor->fetch_all('toplevel')};
-my %chromosomes2;
-
-foreach my $chr (@chromosomes2) {
-  $chromosomes2{$chr->seq_region_name} = $chr;
+my $tg_sa = Bio::EnsEMBL::Registry->get_adaptor($tg_species, 'core', 'Slice');
+my %tg_slices;
+foreach my $tg_slice (@{$tg_sa->fetch_all('toplevel')}) {
+  $tg_slices{$tg_slice->name} = $tg_slice;
 }
 
-my $sth_method_link = $db->prepare("SELECT method_link_id FROM method_link WHERE type = ?");
-$sth_method_link->execute($method_link_type);
-my ($method_link_id) = $sth_method_link->fetchrow_array();
-
-unless (defined $method_link_id) {
-  warn "There is no type $method_link_type in the method_link table of compara db.
-EXIT 1";
-  exit 1;
-}
-
-my $sth_method_link_species = $db->prepare("
-SELECT ml.method_link_id 
-FROM method_link_species mls1, method_link_species mls2, method_link ml 
-WHERE mls1.method_link_id = ml.method_link_id AND 
-      mls2.method_link_id = ml.method_link_id AND 
-      mls1.genome_db_id = ? AND
-      mls2.genome_db_id = ? AND
-      mls1.species_set = mls2.species_set AND
-      ml.method_link_id = ?");
-
-$sth_method_link_species->execute($genome_db_id1,$genome_db_id2,$method_link_id);
-my ($already_stored) = $sth_method_link_species->fetchrow_array();
-
-unless (defined $already_stored) {
-  $sth_method_link_species = $db->prepare("select max(species_set) from method_link_species where method_link_id = ?");
-  $sth_method_link_species->execute($method_link_id);
-  my ($max_species_set) = $sth_method_link_species->fetchrow_array();
-
-  $max_species_set = 0 unless (defined $max_species_set);
-  
-  $sth_method_link_species = $db->prepare("insert into method_link_species (method_link_id,species_set,genome_db_id) values (?,?,?)");
-  $sth_method_link_species->execute($method_link_id,$max_species_set + 1,$genome_db_id1);
-  $sth_method_link_species->execute($method_link_id,$max_species_set + 1,$genome_db_id2);
-}
-
-
-my $sth_synteny_region = $db->prepare("insert into synteny_region (rel_orientation) values (?)");
-my $sth_dnafrag_region = $db->prepare("insert into dnafrag_region (synteny_region_id,dnafrag_id,seq_start,seq_end) values (?,?,?,?)");
+my $sth_synteny_region = $dbc->prepare("insert into synteny_region (rel_orientation) values (?)");
+my $sth_dnafrag_region = $dbc->prepare("insert into dnafrag_region (synteny_region_id,dnafrag_id,seq_start,seq_end) values (?,?,?,?)");
 
 my $line_number = 1;
 
 while (defined (my $line = <>) ) {
   chomp $line;
   if ($line =~ /^(\S+)\t.*\t.*\t(\d+)\t(\d+)\t.*\t(-1|1)\t.*\t(\S+)\t(\d+)\t(\d+)$/) {#####This will need to be changed
-    my ($chr1,$start1,$end1,$rel,$chr2,$start2,$end2) = ($1,$2,$3,$4,$5,$6,$7);
+    my ($qy_chr,$qy_start,$qy_end,$rel,$tg_chr,$tg_start,$tg_end) = ($1,$2,$3,$4,$5,$6,$7);
     
-    my $dnafrag1 = new Bio::EnsEMBL::Compara::DnaFrag;
-    $dnafrag1->name($chr1);
-    $dnafrag1->genomedb($genome_db1);
-    $dnafrag1->type($chromosomes1{$chr1}->coord_system->name);
-    $dnafrag1->start(1);
-    $dnafrag1->end($chromosomes1{$chr1}->length);
+    my $qy_dnafrag = new Bio::EnsEMBL::Compara::DnaFrag;
+    $qy_dnafrag->name($qy_chr);
+    $qy_dnafrag->genomedb($qy_gdb);
+    $qy_dnafrag->coord_system_name($qy_slices{$qy_chr}->coord_system->name);
+    $qy_dnafrag->length($qy_slices{$qy_chr}->seq_region_length);
  
-    $dnafrag_adaptor->store_if_needed($dnafrag1);
+    $dfa->store_if_needed($qy_dnafrag);
     
-    my $dnafrag2 = new Bio::EnsEMBL::Compara::DnaFrag;
-    $dnafrag2->name($chr2);
-    $dnafrag2->genomedb($genome_db2);
-    $dnafrag2->type($chromosomes2{$chr2}->coord_system->name);
-    $dnafrag2->start(1);
-    $dnafrag2->end($chromosomes2{$chr2}->length);
-    $dnafrag_adaptor->store_if_needed($dnafrag2);
+    my $tg_dnafrag = new Bio::EnsEMBL::Compara::DnaFrag;
+    $tg_dnafrag->name($tg_chr);
+    $tg_dnafrag->genomedb($tg_gdb);
+    $tg_dnafrag->coord_system_name($tg_slices{$tg_chr}->coord_system->name);
+    $tg_dnafrag->end($tg_slices{$tg_chr}->req_region_length);
+    $dfa->store_if_needed($tg_dnafrag);
 
-# print STDERR "1: $chr1, 2: $chr2, end1: " .$dnafrag1->end.", end2: ". $dnafrag2->end."\n";
+# print STDERR "1: $qy_chr, 2: $tg_chr, qy_end: " .$qy_dnafrag->end.", tg_end: ". $tg_dnafrag->end."\n";
   
     $sth_synteny_region->execute($rel);
     my $synteny_region_id = $sth_synteny_region->{'mysql_insertid'};
-    $sth_dnafrag_region->execute($synteny_region_id,$dnafrag1->dbID,$start1,$end1);
-    $sth_dnafrag_region->execute($synteny_region_id,$dnafrag2->dbID,$start2,$end2);
+    $sth_dnafrag_region->execute($synteny_region_id,$qy_dnafrag->dbID,$qy_start,$qy_end);
+    $sth_dnafrag_region->execute($synteny_region_id,$tg_dnafrag->dbID,$tg_start,$tg_end);
     print STDERR "synteny region line number $line_number loaded\n";
     $line_number++;
   } else {
