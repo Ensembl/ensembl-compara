575c40c930fa00e2a35ed2b339663ce579df5eb1
Abel Ureta-Vidal <abel@sanger.ac.uk>
2004-10-18 14:05:22 +0000

    Don't need anymore uniprot description file and redundancy description file. Get the data directly from compara

--- scripts/family/parse_mcl.pl
+++ scripts/family/parse_mcl.pl
@@ -2,188 +2,149 @@
 
 use strict;
 use Getopt::Long;
-use IO::File;
-use Bio::EnsEMBL::Compara::DBSQL::DBAdaptor;
+use Bio::EnsEMBL::Registry;
 use Bio::EnsEMBL::Compara::Family;
-use Bio::EnsEMBL::Compara::Member;
-use Bio::EnsEMBL::Compara::Taxon;
 use Bio::EnsEMBL::Compara::Attribute;
-use Bio::EnsEMBL::Slice;
 
 $| = 1;
 
 my $usage = "
-Usage: $0 options mcl_file index_file desc_file redundancy_file
+Usage: $0 options tab_file mcl_file
 
 i.e.
 
 $0 
 
 Options:
--host 
 -dbname family dbname
--dbuser
--dbpass
 -prefix family stable id prefix (default: ENSF)
 -offset family id numbering start (default:1)
 
 \n";
 
 my $help = 0 ;
-my $family_source_name = "ENSEMBL_FAMILIES";
+my $method_link_type = "FAMILY";
 my $family_prefix = "ENSF";
 my $family_offset = 1;
-my $host;
-my $port = "";
 my $dbname;
-my $dbuser;
-my $dbpass;
-my $conf_file;
+my $reg_conf;
 
 GetOptions('help' => \$help,
-	   'host=s' => \$host,
-	   'port=i' => \$port,
 	   'dbname=s' => \$dbname,
-	   'dbuser=s' => \$dbuser,
-	   'dbpass=s' => \$dbpass,
 	   'prefix=s' => \$family_prefix,
 	   'offset=i' => \$family_offset,
-           'conf_file=s' => \$conf_file);
+           'reg_conf=s' => \$reg_conf);
 
 if ($help) {
   print $usage;
   exit 0;
 }
 
-unless (scalar @ARGV == 4) {
-  print "Need 4 arguments\n";
+unless (scalar @ARGV == 2) {
+  print "Need 2 arguments\n";
   print $usage;
   exit 0;
 }
 
-my ($mcl_file,$index_file,$desc_file, $redunfile) = @ARGV;
+my ($tab_file, $mcl_file) = @ARGV;
+
+# Take values from ENSEMBL_REGISTRY environment variable or from ~/.ensembl_init
+# if no reg_conf file is given.
+Bio::EnsEMBL::Registry->load_all($reg_conf);
+
 
 my @clusters;
-my %seqinfo;
-my %member_index;
-my %redun_hash;
+my %stable_id2source_name;
+my %tab_index2stable_id;
+my %redundant_sequence_id2stable_ids;
 
-my $db;
+my $dbc = Bio::EnsEMBL::Registry->get_DBAdaptor($dbname,'compara')->dbc;
+my $fa = Bio::EnsEMBL::Registry->get_adaptor($dbname,'compara','Family');
+my $ma = Bio::EnsEMBL::Registry->get_adaptor($dbname,'compara','Member');
+my $gdba = Bio::EnsEMBL::Registry->get_adaptor($dbname,'compara','GenomeDB');
+my $mlssa = Bio::EnsEMBL::Registry->get_adaptor($dbname,'compara','MethodLinkSpeciesSet');
 
-if (defined $conf_file) {
-  $db = new Bio::EnsEMBL::Compara::DBSQL::DBAdaptor(-host   => $host,
-                                                    -port   => $port,
-                                                    -user   => $dbuser,
-                                                    -pass   => $dbpass,
-                                                    -dbname => $dbname,
-                                                    -conf_file => $conf_file);
-} else {
-  $db = new Bio::EnsEMBL::Compara::DBSQL::DBAdaptor(-host   => $host,
-                                                    -port   => $port,
-                                                    -user   => $dbuser,
-                                                    -pass   => $dbpass,
-                                                    -dbname => $dbname);
-}
 
-my $fa = $db->get_FamilyAdaptor;
-my $gdb = $db->get_GenomeDBAdaptor;
-my %genomedbs;
-foreach my $genomedb (@{$gdb->fetch_all}) {
-  $genomedbs{$genomedb->taxon_id} = $genomedb;
-}
+my $mlss = new Bio::EnsEMBL::Compara::MethodLinkSpeciesSet;
+$mlss->species_set(\@{$gdba->fetch_all});
+$mlss->method_link_type($method_link_type);
+$mlssa->store($mlss);
 
-print STDERR "Reading index file...";
+print STDERR "Reading tab file...";
 
-if ($index_file =~ /\.gz/) {
-  open INDEX, "gunzip -c $index_file|" ||
-    die "$index_file: $!";
-} else {
-  open INDEX, $index_file ||
-    die "$index_file: $!";
-}
-my $max_member_index;
+open TAB, $tab_file ||
+  die "$tab_file: $!";
 
-while (<INDEX>) {
+while (<TAB>) {
   if (/^(\S+)\s+(\S+)/) {
-    my ($index,$seqid) = ($1,$2);
-    $member_index{$index} = $seqid;
-    $seqinfo{$seqid}{'index'} = $index;
-    unless (defined $max_member_index) {
-      $max_member_index = $index;
-    } elsif ($index > $max_member_index) {
-      $max_member_index = $index;
-    }
+    my ($index,$stable_id) = ($1,$2);
+    $tab_index2stable_id{$index} = $stable_id;
   } else {
-    warn "$index_file has not the expected format
+    warn "$tab_file has not the expected format
 EXIT 1\n";
     exit 1;
   }
 }
-close INDEX
-  || die "$index_file: $!";
+close TAB
+  || die "$tab_file: $!";
 
 print STDERR "Done\n";
 
-print STDERR "Reading description file...";
+print STDERR "Getting source_name from database...";
 
-if ($desc_file =~ /\.gz/) {
-  open DESC, "gunzip -c $desc_file|" || 
-    die "$desc_file: $!"; 
-} else {
-  open DESC, $desc_file ||
-    die "$desc_file: $!";
+my $sql = "select stable_id,source_name,description from member where source_name in ('SWISSPROT','SPTREMBL','ENSEMBLPEP')";
+my $sth = $dbc->prepare($sql);
+$sth->execute;
+
+my ($stable_id,$source_name,$description);
+
+$sth->bind_columns(\$stable_id,\$source_name,\$description);
+
+while ($sth->fetch) {
+    $description = "" unless (defined $description);
+    $stable_id2source_name{$stable_id} = $source_name;
 }
 
-while (<DESC>) {
-  if (/^(.*)\t(.*)\t(.*)\t(.*)$/) {
-    my ($type,$seqid,$desc,$taxon) = ($1,$2,$3,$4);
-    if(!$taxon || !$seqid) {
-      warn("taxon or seqid not defined, skipping description:\n".
-           "\t[$type]\t[$seqid]\t\[$desc]\t[$taxon]\n");
-      next;
-    }
-    $desc = "" unless (defined $desc);
-    $seqinfo{$seqid}{'type'} = $type;
-    $seqinfo{$seqid}{'description'} = $desc;
-    $seqinfo{$seqid}{'taxon'} = $taxon;
-    unless (defined $seqinfo{$seqid}{'index'}) {
-      $max_member_index++;
-      $seqinfo{$seqid}{'index'} = $max_member_index;
-    }
-  } else {
-    warn "$desc_file has not the expected format
-EXIT 2\n";
-    exit 2;
+$sth->finish;
+
+#foreach my $stable_id (keys %stable_id2source_name) {
+#  print $stable_id," ";
+#  print join(" ", $stable_id2source_name{$stable_id}),"\n";
+#}
+
+print STDERR "Done\n";
+
+print STDERR "Getting redundancy information from the database...";
+
+$sql = "select sequence_id,count(*) as count from member where source_name in ('SWISSPROT','SPTREMBL','ENSEMBLPEP') group by sequence_id having count>1";
+$sth = $dbc->prepare($sql);
+$sth->execute;
+
+my ($sequence_id,$count);
+$sth->bind_columns(\$sequence_id,\$count);
+
+my $sql2 = "select stable_id from member where sequence_id = ?";
+my $sth2 = $dbc->prepare($sql2);
+
+while ( $sth->fetch() ) {
+  $sth2->execute($sequence_id);
+  my ($stable_id);
+  $sth2->bind_columns(\$stable_id);
+  while ( $sth2->fetch() ) {
+    push @{$redundant_sequence_id2stable_ids{$sequence_id}},$stable_id;
   }
 }
 
-close DESC
-  || die "$desc_file: $!";
+$sth2->finish;
+$sth->finish;
+
+#foreach my $sequence_id (keys %redundant_sequence_id2stable_ids) {
+#  print $sequence_id," ";
+#  print join(" ", @{$redundant_sequence_id2stable_ids{$sequence_id}}),"\n";
+#}
 
 print STDERR "Done\n";
 
-print STDERR "Reading redundancies file...";
-if ($redunfile =~ /\.gz/) {
-  open REDUN, "gunzip -c $redunfile|" ||
-    die "$redunfile: $!";
-} else {
-  open REDUN, $redunfile ||
-    die "$redunfile: $!";
-}
-
-while (<REDUN>) {
-  chomp;
-  my @tab = split;
-  my $refid = shift @tab;
-  foreach my $id (@tab) {
-    next if ($id eq $refid);
-    $redun_hash{$refid}{$id} = 1;
-  }
-}
-
-close REDUN;
-
-print STDERR "Done\n";
 
 print STDERR "Reading mcl file...";
 if ($mcl_file =~ /\.gz/) {
@@ -217,145 +178,78 @@ close MCL ||
 
 print STDERR "Done\n";
 
-print STDERR "Loading clusters in family db\n";
+print STDERR "Loading clusters in compara\n";
 
 # starting to use the Family API here to load in a family database
-# still print out description for each entries in order to determinate 
-# a consensus description
-
-my $max_cluster_index;
+# still print out description for each SWISSPROT/SPTREMBL entries in the family
+# in order to determinate a consensus description
 
 foreach my $cluster (@clusters) {
   my ($cluster_index, @cluster_members) = split /\s+/,$cluster;
-  print STDERR "Loading cluster $cluster_index...";
 
-  unless (defined $max_cluster_index) {
-    $max_cluster_index = $cluster_index;
-  } elsif ($cluster_index > $max_cluster_index) {
-    $max_cluster_index = $cluster_index;
+  my $popped = pop @cluster_members;
+  if ($popped ne "\$") {
+    die "problem in the mcl parsing in clustar id $cluster_index\n";
   }
 
-  my $family_stable_id = sprintf ("$family_prefix%011.0d",$cluster_index + $family_offset);
+  print STDERR "Loading cluster $cluster_index...";
+
+  my $stable_id = sprintf ("$family_prefix%011.0d",$cluster_index + $family_offset);
   my $family = Bio::EnsEMBL::Compara::Family->new_fast
     ({
-      '_stable_id' => $family_stable_id,
-      '_source_name' => $family_source_name,
+      '_stable_id' => $stable_id,
+      '_method_link_species_set' => $mlss,
       '_description' => "NULL",
       '_description_score' => 0
      });
   
-  foreach my $member_idx (@cluster_members) {
-    last if ($member_idx =~ /^\$$/);
-    my $seqid = $member_index{$member_idx};
+  foreach my $tab_idx (@cluster_members) {
+    my $seqid = $tab_index2stable_id{$tab_idx};
 
     unless($seqid) {
-      warn("no seqid defined for member [$member_idx]\n");
+      warn("no seqid defined for member [$tab_idx]\n");
       next;
     }
 
-    if(!$seqinfo{$seqid}) {
-      warn("no seqinfo defined for [$seqid]\n");
-      next;
-    }
-   
-    if(!$seqinfo{$seqid}{'taxon'}) {
-      warn("taxon is not defined for seqid [$seqid]");
-      if($seqinfo{$seqid}) {
-         map {warn( $_ . '=>' . $seqinfo{$seqid}{$_})} keys %{$seqinfo{$seqid}};
-      }
+    if(!$stable_id2source_name{$seqid}) {
+      warn("no stable_id2source_name defined for [$seqid]\n");
       next;
     }
 
-    my $taxon_hash = parse_taxon($seqinfo{$seqid}{'taxon'});
-    my @classification = split(':',$taxon_hash->{'taxon_classification'});
-    my $taxon = new Bio::EnsEMBL::Compara::Taxon->new(-classification=>\@classification);
-    $taxon->common_name($taxon_hash->{'taxon_common_name'});
-    $taxon->sub_species($taxon_hash->{'taxon_sub_species'});
-    $taxon->ncbi_taxid($taxon_hash->{'taxon_id'});
-
-    my $member_source =  uc $seqinfo{$seqid}{'type'};
-    my $member = $db->get_MemberAdaptor->fetch_by_source_stable_id($member_source, $seqid);
+    my $member_source =  uc $stable_id2source_name{$seqid};
+    my $member = $ma->fetch_by_source_stable_id($member_source, $seqid);
     unless($member) {
-      $member = Bio::EnsEMBL::Compara::Member->new_fast
-        ({'_stable_id' => $seqid,
-          '_version' => 0,
-          '_taxon_id' => $taxon->ncbi_taxid,
-          '_taxon' => $taxon,
-          '_description' => $seqinfo{$seqid}{'description'},
-          '_source_name' => uc $seqinfo{$seqid}{'type'},
-          '_genome_db_id' => "NULL",
-          '_chr_name' => "NULL",
-          '_chr_start' => "NULL",
-          '_chr_end' => "NULL",
-          '_sequence' => "NULL"});
-
-      if ($member->source_name eq "ENSEMBLPEP" ||
-          $member->source_name eq "ENSEMBLGENE") {
-        #get genome_db_id
-        my $genomedb = $genomedbs{$member->taxon_id};
-        $member->genome_db_id($genomedb->dbID);
-        #get chr_name, chr_start, chr_end
-        my $core_db = $genomedb->connect_to_genome_locator;
-        my $GeneAdaptor = $core_db->get_GeneAdaptor;
-        my $TranscriptAdaptor = $core_db->get_TranscriptAdaptor;
-        my $gene;
-        my $transcript;
-
-        if ($member->source_name eq "ENSEMBLPEP") {
-          $transcript = $TranscriptAdaptor->fetch_by_translation_stable_id($member->stable_id);
-          $transcript->transform('toplevel');
-          $member->chr_name($transcript->slice->seq_region_name);
-          $member->chr_start($transcript->coding_region_start);
-          $member->chr_end($transcript->coding_region_end);
-          $member->sequence($transcript->translate->seq);
-          $member->version($transcript->translation->version);
-        
-        } 
-        elsif ($member->source_name eq "ENSEMBLGENE") {
-          $gene = $GeneAdaptor->fetch_by_stable_id($member->stable_id);
-          
-          unless (defined $gene) {
-            print STDERR $member->stable_id," ",$member->source_name," ",$member->taxon_id," is undef!!!\n";
-            die;
-          }
-          $gene->transform('toplevel');
-          $member->chr_name($gene->slice->seq_region_name);
-          $member->chr_start($gene->seq_region_start);
-          $member->chr_end($gene->seq_region_end);
-          $member->version($gene->version);
-        }
-      }
+      die "member does not exist in the database";
     }
-
+    
     my $attribute = new Bio::EnsEMBL::Compara::Attribute;
     $attribute->cigar_line("NULL");
-    
+
     $family->add_Member_Attribute([$member, $attribute]);
   }
 
   my $dbID = $fa->store($family);
+#  my $dbID = 1;
 
   foreach my $member_attribute (@{$family->get_all_Member_Attribute}) {
     my ($member,$attribute) = @{$member_attribute};
-    print $member->source_name,"\t$dbID\t",$member->stable_id,"\t",$seqinfo{$member->stable_id}{'description'},"\n";
-    $seqinfo{$member->stable_id}{'printed'} = 1;
-    if (defined $redun_hash{$member->stable_id}) {
-      foreach my $member_stable_id (keys %{$redun_hash{$member->stable_id}}) {
-        print uc($seqinfo{$member_stable_id}{'type'}),"\t$dbID\t$member_stable_id\t",$seqinfo{$member_stable_id}{'description'},"\n";
-        $seqinfo{$member_stable_id}{'printed'} = 1;
+    if ($member->source_name eq 'SWISSPROT' || $member->source_name eq 'SPTREMBL') {
+      print $member->source_name,"\t$dbID\t",$member->stable_id,"\t",$member->description,"\n";
+    }
+
+    if (defined $redundant_sequence_id2stable_ids{$member->sequence_id}) {
+      foreach my $stable_id (@{$redundant_sequence_id2stable_ids{$member->sequence_id}}) {
+        next if ($stable_id eq $member->stable_id);
+        next unless ($stable_id2source_name{$stable_id} eq 'SWISSPROT' || $stable_id2source_name{$stable_id} eq 'SPTREMBL');
+        my $rd_member = $ma->fetch_by_source_stable_id($stable_id2source_name{$stable_id}, $stable_id);
+        print $rd_member->source_name,"\t$dbID\t",$rd_member->stable_id,"\t",$rd_member->description,"\n";
       }
     } 
   }
+
   print STDERR "Done\n";
+#  last;
 }
 
 print STDERR "END\n";
 
-sub parse_taxon {
-  my ($str) = @_;
-
-  $str=~s/=;/=NULL;/g;
-  my %taxon = map {split '=',$_} split';',$str;
-
-  return \%taxon;
-}
