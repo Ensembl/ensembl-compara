6225298240db1d15e5ffb943dc485f9c22143040
Web Admin <w3adm@sanger.ac.uk>
2004-11-02 12:57:50 +0000

    new schema for microarrays

--- modules/Bio/EnsEMBL/GlyphSet/generic_microarray.pm
+++ modules/Bio/EnsEMBL/GlyphSet/generic_microarray.pm
@@ -6,27 +6,15 @@ use Bio::EnsEMBL::GlyphSet_feature;
 
 sub my_label { 
   my $self = shift;
-  my $key = '_my_label';
-  if( ! exists( $self->{$key} ) ){
-    my $misc_set_code = $self->my_config('FEATURES') || 
-      die( "The FEATURES key of UserConfig must be a misc_set_code" );
-    my $db_adaptor = $self->{'container'}->adaptor->db;
-    my $msa_adaptor = $db_adaptor->get_MiscSetAdaptor;
-    my $misc_set = $msa_adaptor->fetch_by_code( $misc_set_code ) ||
-      die( "The misc_set_code $misc_set_code is not found in the DB" );
-    $self->{$key} = $misc_set->name;
-  }
-  return $self->{$key};
+  return $self->my_config('caption') || 'Affy microarray';
 }
 
 ## Retrieve all MiscFeatures from the misc_set table of the database
 ## corresponding to the misc_set_code (UserConfig FEATURES key)
 
 sub features {
-    my ($self) = @_;
-    my $misc_set_code = $self->my_config('FEATURES') || 
-      die( "The FEATURES key of UserConfig must be a misc_set_code" );
-    return $self->{'container'}->get_all_MiscFeatures( $misc_set_code );
+  my ($self) = @_;
+  return $self->{'container'}->get_all_AffyFeatures( $self->my_config('FEATURES') );
 }
 
 ## Return the image label and the position of the label
@@ -36,41 +24,30 @@ sub features {
 sub image_label {
   my ($self, $f ) = @_;
   return '';
-  #return( $f->get_scalar_attribute('name') );
 }
 
 ## Link back to this page centred on the map fragment
 
 sub href {
-  my ($self, $id, $feature_data ) = @_;
-  my $f = $feature_data->[0][2];
-  $f || return '';
-  my $tmpl = "fastaview?faid=microarray&id=%s";
-  return sprintf( $tmpl, $f->dbID );;
+  my ($self, $id, $fd ) = @_;
+  my $f = $fd->[0][2];
+  my $tmpl = "/%s/featureview?type=AffyProbe&id=%s";
+  return sprintf( "/%s/featureview?type=AffyProbe&id=%s", $self->{container}{_config_file_name_}, $f->probeset );
 }
 
 ## Create the zmenu...
 ## Include each accession id separately
 sub zmenu {
-    my ($self, $id, $feat_data ) = @_;
-    my $f = $feat_data->[0][2];
-    #my $zmenu = {};
-    my $zmenu = { 'caption' => 'Reporter Data' };
-    my $href = $self->href($id, $feat_data);
-    if( $href ){ $zmenu->{"01:details..."} = $href }
-    my $i = 1;
-    foreach my $attrib( @{$f->get_all_Attributes} ){
-      $i++;
-      my $key = sprintf( "%2.2d:%s:%s", $i, $attrib->name, $attrib->value );
-      $zmenu->{$key} = '';
-    }
-    return $zmenu;
+  my ($self, $id, $fd ) = @_;
+  my $f = $fd->[0][2];
+  return {
+    'caption' => "Affy feature: ".$f->probeset,
+    'Probe set details: ' => $self->href( $id, $fd )
+  };
 }
-
-## 
 sub feature_group{
   my( $self, $f ) = @_;
-  return $f->get_scalar_attribute('compositeName');
+  return $f->probeset();
 }
 
 
