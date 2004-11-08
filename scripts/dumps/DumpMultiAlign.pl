4dafd7e4e88c5e5ebb82cd8ed1f8015b32e1572a
Javier Herrero <jh7@sanger.ac.uk>
2004-11-08 18:14:05 +0000

    Uses Bio::EnsEMBL::REgistry and Bio::EnsEMBL::Compara::GenomicAlignBlocks

--- scripts/dumps/DumpMultiAlign.pl
+++ scripts/dumps/DumpMultiAlign.pl
@@ -1,201 +1,310 @@
 #!/usr/local/ensembl/bin/perl -w
-# get a slice from the commandline arguments
-# get genomicaligns from compara for that slice
-# try to get slices for target regions
-# pass them in the print option from GenomicAlign object
+
+my $description = q{
+###########################################################################
+##
+## PROGRAM DumpMultiAlign.pl
+##
+## AUTHORS
+##    Abel Ureta-Vidal (abel@ebi.ac.uk)
+##    Javier Herrero (jherrero@ebi.ac.uk)
+##
+## COPYRIGHT
+##    This modules is part of the Ensembl project http://www.ensembl.org
+##
+## DESCRIPTION
+##    This script dumps (pairwise or multiple) genomic alignments from
+##    an EnsEMBL Compara Database.
+##
+###########################################################################
+
+};
+
+=head1 NAME
+
+DumpMultiAlign.pl
+
+=head1 AUTHORS
+
+ Abel Ureta-Vidal (abel@ebi.ac.uk)
+ Javier Herrero (jherrero@ebi.ac.uk)
+
+=head1 COPYRIGHT
+
+This modules is part of the Ensembl project http://www.ensembl.org
+
+=head1 DESCRIPTION
+
+This script dumps (pairwise or multiple) genomic alignments from
+an EnsEMBL Compara Database.
+
+=head1 SYNOPSIS
+
+perl DumpMultiAlign.pl --species human --seq_region 14 --seq_region_start 75000000
+    --seq_region_end 75010000 --alignment_type BLASTZ_NET --set_of_species human:mouse
+
+perl DumpMultiAlign.pl
+    [--reg_conf registry_configuration_file]
+    [--dbname compara_db_name]
+    [--species species]
+    [--coord_system coordinates_name]
+    --seq_region region_name
+    --seq_region_start start
+    --seq_region_end end
+    [--alignment_type method_link_name]
+    [--set_of_species species1:species2:species3:...]
+    [--output_format clustalw|fasta|...]
+    [--output_file filename]
+
+=head1 OPTIONS
+
+=head2 GETTING HELP
+
+=over
+
+=item B<[--help]>
+
+  Prints help message and exits.
+
+=back
+
+=head2 GENERAL CONFIGURATION
+
+=over
+
+=item B<[--reg_conf registry_configuration_file]>
+
+the Bio::EnsEMBL::Registry configuration file. If none given,
+the one set in ENSEMBL_REGISTRY will be used if defined, if not
+~/.ensembl_init will be used.
+
+=item B<[--dbname compara_db_name]>
+  
+the name of compara DB in the registry_configuration_file or any
+of its aliases. Uses "compara" by default.
+
+=back
+
+=head2 SPECIFYING THE QUERY SLICE
+
+=over
+
+=item B<[--species species]>
+  
+Query species. Default is "human"
+
+=item B<[--coord_system coordinates_name]>
+  
+Query coordinate system. Default is "chromosome"
+
+=item B<--seq_region region_name>
+  
+Query region name, i.e. the chromosome name
+
+=item B<--seq_region_start start>
+
+=item B<--seq_region_end end>
+
+=back
+
+=head2 SPECIFYING THE ALIGNMENT TYPE
+
+=over
+
+=item B<[--alignment_type method_link_name]>
+  
+The type of alignment. Default is "BLASTZ_NET"
+
+=item B<[--set_of_species species1:species2:species3:...]>
+  
+The list of species used to get those alignments. Default is
+"human:mouse". The names should correspond to the name of the
+core database in the registry_configuration_file or any of its
+aliases
+
+=back
+
+=head2 OUTPUT
+
+=over
+
+=item B<[--output_format clustalw|fasta|...]>
+  
+The type of output you want. "clustalw" is the default.
+
+=item B<[--output_file filename]>
+  
+The name of the output file. By default the output is the
+standard output
+
+=back
+
+=cut
+
+my $usage = qq{
+perl DumpMultiAlign.pl
+  Getting help:
+    [--help]
+  
+  General configuration:
+    [--reg_conf registry_configuration_file]
+        the Bio::EnsEMBL::Registry configuration file. If none given,
+        the one set in ENSEMBL_REGISTRY will be used if defined, if not
+        ~/.ensembl_init will be used.
+    [--dbname compara_db_name]
+        the name of compara DB in the registry_configuration_file or any
+        of its aliases. Uses "compara" by default.
+
+  For the query slice:
+    [--species species]
+        Query species. Default is "human"
+    [--coord_system coordinates_name]
+        Query coordinate system. Default is "chromosome"
+    --seq_region region_name
+        Query region name, i.e. the chromosome name
+    --seq_region_start start
+    --seq_region_end end
+
+  For the alignments:
+    [--alignment_type method_link_name]
+        The type of alignment. Default is "BLASTZ_NET"
+    [--set_of_species species1:species2:species3:...]
+        The list of species used to get those alignments. Default is
+        "human:mouse". The names should correspond to the name of the
+        core database in the registry_configuration_file or any of its
+        aliases
+
+  Ouput:
+    [--output_format clustalw|fasta|...]
+        The type of output you want. "clustalw" is the default.
+    [--output_file filename]
+        The name of the output file. By default the output is the
+        standard output
+};
 
 use strict;
-use Getopt::Long;
-use Bio::EnsEMBL::Compara::DBSQL::DBAdaptor;
-use Bio::EnsEMBL::DBSQL::DBAdaptor;
+use Bio::EnsEMBL::Registry;
+use Bio::EnsEMBL::Utils::Exception qw(throw);
+use Bio::SimpleAlign;
 use Bio::AlignIO;
 use Bio::LocatableSeq;
+use Getopt::Long;
 
-# specify connection to compara
-# specify species chromsome start end
-
-
-# get species DBAdaptor from Compara
-# get all the other species that are there in Compara
-# get DBAdaptors for them.
-
-unless ( @ARGV ) {
-  usage();
-  exit();
-}
-
-my ( $host, $user, $port, $dbname, $seq_region, $seq_region_start, $seq_region_end,$species, $conf_file );
-my $pass = "";
-my $alignment_type = "BLASTZ_NET";
-my $output_format = "fasta";
+my $reg_conf;
+my $dbname = "compara";
+my $species = "human";
 my $coord_system = "chromosome";
+my $seq_region = "14";
+my $seq_region_start = 75000000;
+my $seq_region_end = 75010000;
+my $alignment_type = "BLASTZ_NET";
+my $set_of_species = "human:mouse";
+my $output_file = undef;
+my $output_format = "clustalw";
+my $help;
 
 GetOptions(
-	   "host=s" => \$host,
-	   "user=s" => \$user,
-	   "pass=s" => \$pass,
-	   "port=i" => \$port,
-	   "dbname=s" => \$dbname,
-	   "seq_region=s" => \$seq_region,
-	   "seq_region_start=i" => \$seq_region_start,
-	   "seq_region_end=i" => \$seq_region_end,
-	   "alignment_type=s" => \$alignment_type,
-           "output_format=s" => \$output_format,
-           "coord_system=s" => \$coord_system,
-	   "species=s" => \$species,
-	   "conf_file=s" => \$conf_file
-	  );
-
-# change this to use supplied compara database
-
-my $compara = new Bio::EnsEMBL::Compara::DBSQL::DBAdaptor(-host => $host,
-                                                          -port => $port,
-							  -user => $user,
-							  -dbname => $dbname,
-							  -pass => $pass,
-							  -conf_file => $conf_file);
-
-my $all_genome_dbs = $compara->get_GenomeDBAdaptor()->fetch_all();
-
-# there is a fetch_all on genome_DB, but by time of writing this,
-#  compara wasnt filled and configured properly
-
-my ( $primary_species ) = grep { $_->name() eq $species } @$all_genome_dbs;
-my ( @other_species ) = grep { $_->name() ne $species } @$all_genome_dbs;
-
-my @wanted_other_species;
-
-foreach my $other_species (@other_species) {
-  my $name = $other_species->name;
-  my $assembly = $other_species->assembly;
-  if (defined $compara->get_db_adaptor($name,$assembly)) {
-    push @wanted_other_species,$other_species;
-  }
-}
-
-@other_species = @wanted_other_species;
-
-my $primary_slice = $primary_species->db_adaptor()->get_SliceAdaptor()->
-  fetch_by_region("toplevel",$seq_region, $seq_region_start, $seq_region_end);
-my $primary_seq = $primary_slice->seq();
-my @other_seq;
-
-
-my $dnafrags = $compara->get_DnaFragAdaptor()->fetch_all_by_GenomeDB_region
-  ( 
-   $primary_species,
-   $coord_system,
-   $seq_region,
-   $seq_region_start,
-   $seq_region_end
+    "help" => \$help,
+    "reg_conf=s" => \$reg_conf,
+    "dbname=s" => \$dbname,
+    "species=s" => \$species,
+    "coord_system=s" => \$coord_system,
+    "seq_region=s" => \$seq_region,
+    "seq_region_start=i" => \$seq_region_start,
+    "seq_region_end=i" => \$seq_region_end,
+    "alignment_type=s" => \$alignment_type,
+    "set_of_species=s" => \$set_of_species,
+    "output_format=s" => \$output_format,
+    "output_file=s" => \$output_file,
   );
 
-my $gaa = $compara->get_GenomicAlignAdaptor;
+# Print Help and exit
+if ($help) {
+  print $description, $usage;
+  exit(0);
+}
 
-my @out = ();
-my $count = 0;
-my $alignments_found = 0;
+if ($output_file) {
+  open(STDOUT, ">$output_file") or die("Cannot open $output_file");
+}
 
-for( my $i_species = 0; $i_species<=$#other_species; $i_species++ ) {
-  my $qy_gdb = $other_species[$i_species];
-  $other_seq[$i_species] = "." x length( $primary_seq );
+# Configure the Bio::EnsEMBL::Registry
+# Uses $reg_conf if supllied. Uses ENV{ENSMEBL_REGISTRY} instead if defined. Uses ~/.ensembl_init
+# if all the previous fail.
+Bio::EnsEMBL::Registry->load_all($reg_conf);
 
-  foreach my $df (@$dnafrags) {
-    #caclulate coords relative to start of dnafrag
-    my $df_start = $seq_region_start - $df->start + 1;
-    my $df_end   = $seq_region_end   - $df->start + 1;
-  
-    #constrain coordinates so they are completely within the dna frag
-    my $len = $df->end - $df->start + 1;
-    $df_start = ($df_start < 1)  ? 1 : $df_start;
-    $df_end   = ($df_end > $len) ? $len : $df_end;
+# Getting all the Bio::EnsEMBL::Compara::GenomeDB objects
+my $genome_dbs;
+my $genome_db_adaptor = Bio::EnsEMBL::Registry->get_adaptor($dbname, 'compara', 'GenomeDB');
+throw("Registry configuration file has no data for connecting to <$dbname>")
+    if (!$genome_db_adaptor);
+foreach my $this_species (split(":", $set_of_species)) {
+  my $this_meta_container_adaptor = Bio::EnsEMBL::Registry->get_adaptor(
+      $this_species, 'core', 'MetaContainer');
+  throw("Registry configuration file has no data for connecting to <$this_species>")
+      if (!$this_meta_container_adaptor);
+  my $this_binomial_id = $this_meta_container_adaptor->get_Species->binomial;
+  # Fetch Bio::EnsEMBL::Compara::GenomeDB object
+  my $genome_db = $genome_db_adaptor->fetch_by_name_assembly($this_binomial_id);
+  # Add Bio::EnsEMBL::Compara::GenomeDB object to the list
+  push(@$genome_dbs, $genome_db);
+}
 
-    #fetch all alignments in the region we are interested in
-    my $genomic_aligns = $gaa->fetch_all_by_DnaFrag_GenomeDB($df,
-							     $qy_gdb,
-							     $df_start,
-							     $df_end,
-							     $alignment_type
-							    );
-    
-    for my $align ( @$genomic_aligns ) {
+# Getting Bio::EnsEMBL::Compara::MethodLinkSpeciesSet obejct
+my $method_link_species_set_adaptor = Bio::EnsEMBL::Registry->get_adaptor(
+    $dbname, 'compara', 'MethodLinkSpeciesSet');
+my $method_link_species_set = $method_link_species_set_adaptor->fetch_by_method_link_type_GenomeDBs(
+        $alignment_type, $genome_dbs);
+throw("The database do not contain any $alignment_type data for $set_of_species!")
+    if (!$method_link_species_set);
 
-      my ($fake,$aligned_string) = @{$align->alignment_strings("NO_SEQ","FIX_SEQ")};
-      if( ! $aligned_string ) { next } else {$alignments_found = 1; }
+# Fetching the query Slice:
+my $slice_adaptor = Bio::EnsEMBL::Registry->get_adaptor($species, 'core', 'Slice');
+throw("Registry configuration file has no data for connecting to <$species>")
+    if (!$slice_adaptor);
+my $query_slice = $slice_adaptor->fetch_by_region('toplevel', $seq_region, $seq_region_start, $seq_region_end);
+throw("No Slice can be created with coordinates $seq_region:$seq_region_start-$seq_region_end")
+    if (!$query_slice);
 
-      # now put it in the other_seq_array
-      my ( $p_start, $p_end );
-      $p_start = $align->consensus_start() + $align->consensus_dnafrag->start() - $primary_slice->start();
-      $p_end = $align->consensus_end() + $align->consensus_dnafrag->start() - $primary_slice->start();
+# Fetching all the GenomicAlignBlock corresponding to this Slice:
+my $genomic_align_block_adaptor = Bio::EnsEMBL::Registry->get_adaptor(
+    $dbname, 'compara', 'GenomicAlignBlock');
+my $genomic_align_blocks =
+    $genomic_align_block_adaptor->fetch_all_by_MethodLinkSpeciesSet_Slice(
+        $method_link_species_set, $query_slice);
 
-      # put the align string into result
-      $count++;
-      
-      substr( $other_seq[$i_species], $p_start-1, length( $aligned_string ), $aligned_string );
-    }
+my $all_aligns;
+foreach my $this_genomic_align_block (@$genomic_align_blocks) {
+  my $simple_align = Bio::SimpleAlign->new();
+  $simple_align->id("GAB#".$this_genomic_align_block->dbID);
+  $simple_align->score($this_genomic_align_block->score);
+
+  foreach my $this_genomic_align (@{$this_genomic_align_block->get_all_GenomicAligns}) {
+    my $seq_name = $this_genomic_align->dnafrag->genome_db->name;
+    $seq_name =~ s/(.)\w* (.)\w*/$1$2/;
+    $seq_name .= $this_genomic_align->dnafrag->name;
+    my $aligned_sequence = $this_genomic_align->aligned_sequence;
+    my $seq = Bio::LocatableSeq->new(
+            -SEQ    => $aligned_sequence,
+            -START  => $this_genomic_align->dnafrag_start,
+            -END    => $this_genomic_align->dnafrag_end,
+            -ID     => $seq_name,
+            -STRAND => $this_genomic_align->dnafrag_strand
+        );
+    $simple_align->add_seq($seq);
   }
+  push(@$all_aligns, $simple_align);
 }
 
-if( ! $alignments_found ) {
-  print STDERR "Sorry, no alignments found in the specified region.\n";
-  exit;
-} else {
-  print STDERR "$count alignments done\n";
+my $alignIO = Bio::AlignIO->newFh(
+        -interleaved => 0,
+        -fh => \*STDOUT,
+        -format => $output_format,
+        -idlength => 10
+    );
+  
+foreach my $this_align (@$all_aligns) {
+  print $alignIO $this_align;
 }
-
-my $alignIO = Bio::AlignIO->newFh(-interleaved => 0,
-                                  -fh => \*STDOUT,
-                                  -format => $output_format,
-                                  -idlength => 20);
-
-my $sa = Bio::SimpleAlign->new();
-
-
-my $seq = Bio::LocatableSeq->new(-SEQ    => $primary_seq,
-                                 -START  => 1,
-                                 -END    => length($primary_seq),
-                                 -ID     => $primary_species->name(),
-                                 -STRAND => 0);
-
-$sa->add_seq($seq);
-
-for( my $i_species = 0; $i_species<=$#other_species; $i_species++ ) {
-  my $seq=$other_seq[$i_species];
-  my $locseq = Bio::LocatableSeq->new(-SEQ    => $seq,
-                                 -START  => 1,
-                                 -END    => length($seq),
-                                 -ID     => $other_species[$i_species]->name(),
-                                 -STRAND => 0);
-  $sa->add_seq($locseq);
-}
-
-print $alignIO $sa;
 
 exit;
-
-
-sub usage {
-  print STDERR <<EOF
-
-Usage: DumpMultiAlign.pl options
-  where options should be 
-  
-  -host -h 
-  -user -u 
-  -pass -pa
-  -port -po
-  -dbname -d
-  -conf_file filename
-  to specify compara database connection
-  
-  -seq_region
-  -seq_region_start
-  -seq_region_end
-  -alignment_type e.g. WGA (default WGA)
-  -species -sp
-
-  to specify the region you want to dump with alignments to other
-  available species.
-
-EOF
-}
