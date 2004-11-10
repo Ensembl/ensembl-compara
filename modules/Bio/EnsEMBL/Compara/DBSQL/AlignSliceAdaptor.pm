aa8e386590d19bc82ae741e8c73bcdab517a4d99
Javier Herrero <jh7@sanger.ac.uk>
2004-11-10 15:37:20 +0000

    Added: extra checks before fetching data Added: use cache whenever possible

--- modules/Bio/EnsEMBL/Compara/DBSQL/AlignSliceAdaptor.pm
+++ modules/Bio/EnsEMBL/Compara/DBSQL/AlignSliceAdaptor.pm
@@ -15,9 +15,19 @@ sub new {
   return $self;
 }
 
-
 sub fetch_by_Slice_MethodLinkSpeciesSet {
   my ($self, $reference_slice, $method_link_species_set) = @_;
+
+
+  throw("[$reference_slice] is not a Bio::EnsEMBL::Slice")
+      unless ($reference_slice and ref($reference_slice) and
+          $reference_slice->isa("Bio::EnsEMBL::Slice"));
+  throw("[$method_link_species_set] is not a Bio::EnsEMBL::Compara::MethodLinkSpeciesSet")
+      unless ($method_link_species_set and ref($method_link_species_set) and
+          $method_link_species_set->isa("Bio::EnsEMBL::Compara::MethodLinkSpeciesSet"));
+  # Use cache whenever possible
+  my $key = $reference_slice->name.":".$method_link_species_set->dbID;
+  return $self->{'_cache'}->{$key} if (defined($self->{'_cache'}->{$key}));
 
   my $genomic_align_block_adaptor = $self->db->get_GenomicAlignBlockAdaptor;
   my $genomic_align_blocks = $genomic_align_block_adaptor->fetch_all_by_MethodLinkSpeciesSet_Slice(
@@ -30,6 +40,7 @@ sub fetch_by_Slice_MethodLinkSpeciesSet {
           -reference_Slice => $reference_slice,
           -Genomic_Align_Blocks => $genomic_align_blocks,
       );
+  $self->{'_cache'}->{$key} = $align_slice;
 
   return $align_slice;
 }
