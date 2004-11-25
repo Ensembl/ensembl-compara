bd4976934d1e4fdb6c4a65874347337f3d2e113a
Javier Herrero <jh7@sanger.ac.uk>
2004-11-25 18:01:33 +0000

    Some (hardly needed) code cleaning have been done. The script can save all the toplevel dnafrag at the beginning It can deal with UCSC special chromosomes, using the golden path data for mapping them on top level EnsEMBL seq_region (= Compara DnaFrags)

--- scripts/gen_malign/store_multiz_alignment.pl
+++ scripts/gen_malign/store_multiz_alignment.pl
@@ -75,17 +75,18 @@ hard-coded and thus will get out-to-date at some stage.
 
 use strict;
 use Bio::EnsEMBL::Registry;
-use Bio::EnsEMBL::Utils::Exception qw( throw warning info );
+use Bio::EnsEMBL::Utils::Exception qw( throw warning info verbose );
 use Bio::EnsEMBL::Compara::DBSQL::DBAdaptor;
 use Bio::EnsEMBL::Compara::DBSQL::GenomeDBAdaptor;
 use Bio::EnsEMBL::Compara::DBSQL::DnaFragAdaptor;
 use Bio::EnsEMBL::Compara::DBSQL::GenomicAlignAdaptor;
 use Bio::EnsEMBL::Compara::GenomicAlign;
+use Bio::EnsEMBL::Mapper;
 use Bio::EnsEMBL::Compara::DnaFrag;
 use Bio::EnsEMBL::Utils::Exception qw( verbose );
 use Getopt::Long;
 
-#verbose("INFO");
+verbose("WARNING");
 
 ###############################################################################
 ##  CONFIGURATION VARIABLES:
@@ -103,14 +104,14 @@ my $ucsc_2_ensembl = {
       "canFam1" => {'name' => "Canis familiaris", 'assembly' => "BROADD1"},
       "fr1"     => {'name' => "Fugu rubripes", 'assembly' => "FUGU2"},
       "danRer1"     => {'name' => "Danio rerio", 'assembly' => "ZFISH3"},
-
-      "hg16" => {'name' => "Homo sapiens", 'assembly' => "NCBI34"},
-      "hg17" => {'name' => "Homo sapiens", 'assembly' => "NCBI35"},
-      "Mm3"  => {'name' => "Mus musculus", 'assembly' => "NCBIM32"},
-      "Mm5"  => {'name' => "Mus musculus", 'assembly' => "NCBIM33"},
-      "Rn3"  => {'name' => "Rattus norvegicus", 'assembly' => "RGSC3.1"},
-      "Gg2" => {'name' => "Gallus gallus", 'assembly' => "WASHUC1"},
-      "Pt1" => {'name' => "Pan troglodytes", 'assembly' => "CHIMP1"},
+# 
+#       "hg16" => {'name' => "Homo sapiens", 'assembly' => "NCBI34"},
+#       "hg17" => {'name' => "Homo sapiens", 'assembly' => "NCBI35"},
+#       "Mm3"  => {'name' => "Mus musculus", 'assembly' => "NCBIM32"},
+#       "Mm5"  => {'name' => "Mus musculus", 'assembly' => "NCBIM33"},
+#       "Rn3"  => {'name' => "Rattus norvegicus", 'assembly' => "RGSC3.1"},
+#       "Gg2" => {'name' => "Gallus gallus", 'assembly' => "WASHUC1"},
+#       "Pt1" => {'name' => "Pan troglodytes", 'assembly' => "CHIMP1"},
     };
 ###############################################################################
 
@@ -139,6 +140,7 @@ my $force = 0;
 my $score_threshold;
 my $sequences_threshold;
 my $check_sequences = 1;
+my $load_dnafrags = 0;
 
 
 GetOptions(
@@ -152,6 +154,7 @@ GetOptions(
     'score=i' => \$score_threshold,
     'min_seq=i' => \$sequences_threshold,
     'check_sequences!' => \$check_sequences,
+    'load_dnafrags!' => \$load_dnafrags,
   );
 
 if ($help) {
@@ -161,6 +164,8 @@ if ($help) {
 
 Bio::EnsEMBL::Registry->load_all($reg_conf);
 
+
+## Read directory and get the list of multiz files
 if (!$multiz_dir or !opendir(MULTIZ_DIR, $multiz_dir)) {
   print "ERROR: Cannot open <$multiz_dir> directory!\n", $usage;
   exit(1);
@@ -168,6 +173,7 @@ if (!$multiz_dir or !opendir(MULTIZ_DIR, $multiz_dir)) {
 my @multiz_files = grep { /\.maf/ } readdir(MULTIZ_DIR);
 closedir(MULTIZ_DIR);
 
+## Get Compara adaptors
 my $genome_db_adaptor = Bio::EnsEMBL::Registry->get_adaptor($dbname, 'compara', 'GenomeDB');
 die "Cannot get adaptor for '$dbname', 'compara', 'GenomeDB'" if (!$genome_db_adaptor);
 my $dnafrag_adaptor = Bio::EnsEMBL::Registry->get_adaptor($dbname, 'compara', 'DnaFrag');
@@ -189,6 +195,7 @@ if (!$species_string and $multiz_dir =~ /mz((\w\w[\d]+)+)/) {
   $species_string =~ s/(\d)(\w)/$1_$2/g;
 }
 
+## Store (or get) the MethodLinkSpeciesSet object
 if (!$species_string) {
   print "ERROR: Species not defined and cannot be guessed from mavid directory name!\n", $usage;
   exit(1);
@@ -215,6 +222,9 @@ foreach my $this_species (@species) {
     next if ($resp =~ /^y/i);
     exit(1);
   }
+  if ($load_dnafrags) {
+    store_all_dnafrags($dnafrag_adaptor, $genome_db->{$this_species});
+  }
   push (@$species_set, $genome_db->{$this_species});
 }
 
@@ -223,19 +233,22 @@ my $method_link_species_set = new Bio::EnsEMBL::Compara::MethodLinkSpeciesSet(
         -method_link_type => "MULTIZ",
         -species_set => $species_set
     );
+
+# This method stores the new method_link_species_set and return the obejct with a new dbID. IF
+# the object already exists on the DB, it returns the obejct with the existing dbID
 $method_link_species_set = $method_link_species_set_adaptor->store($method_link_species_set);
 
 foreach my $multiz_file (@multiz_files) {
-  my @all_these_genomic_aligns; # array of Bio::EnsEMBL::Compara::GenomicAlign objects to store as a single multiple alignment
-
+  ## Open file, decompressing it on the fly if needed
   if ($multiz_file =~ /\.gz$/) {
     open(MULTIZ, "gunzip -c $multiz_dir/$multiz_file |") || die;
   } elsif ($multiz_file =~ /\.bz2$/) {
     open(MULTIZ, "bzcat $multiz_dir/$multiz_file |");
   } else {
-    open(MULTIZ, $multiz_dir/$multiz_file);
+    open(MULTIZ, "$multiz_dir/$multiz_file");
   }
 
+  my @all_these_genomic_aligns; # array of Bio::EnsEMBL::Compara::GenomicAlign objects to store as a single multiple alignment
   while (<MULTIZ>) {
     ## For all lines that are not "s" lines (parts of the multiple alignment)
     if (!/^s\s+([^\.]+)\.chr(\S+)\s+(\d+)\s+(\d+)\s+([\+\-])\s+\d+\s+(.+)$/) {
@@ -304,49 +317,9 @@ foreach my $multiz_file (@multiz_files) {
       }
     }
     
-    my $dnafrag;
-    if (defined($genome_db->{$species})) {
-      $dnafrag = get_this_dnafrag($genome_db->{$species}, undef, $chromosome);
-    }
-
-    ## Deal with unknown dnafrags
-    if (!$dnafrag) {
-      $print_multiple_alignment =~ s/\n$/  **NOT AVAILABLE**\n/;;
-      if ($force) {
-        print "IGNORING: Chromosome $species.chr$chromosome\n";
-        $all_warnings{"Chromosome $species.chr$chromosome"}++;
-        next;
-      } elsif ($skip) {
-        $all_warnings{"Chromosome $species.chr$chromosome"}++;
-        $malign_warning .= "- $species.chr$chromosome (".$all_warnings{"Chromosome $species.chr$chromosome"}.")-";
-        next;
-      } else {
-        throw ("Cannot fetch DnaFrag for ".$ucsc_2_ensembl->{$species}->{'name'}.", chromosome $chromosome\n".
-            $print_multiple_alignment);
-      }
-    }
-    
-    my ($dnafrag_start, $dnafrag_end);
-    if ($strand == 1) {
-      $dnafrag_start = $start_pos + 1;
-      $dnafrag_end = $start_pos + $length;
-    } elsif ($strand == -1) {
-      $dnafrag_start = $dnafrag->length - $start_pos - $length + 1;
-      $dnafrag_end = $dnafrag->length - $start_pos;
-    } else {
-      throw("Cannot understand strand $strand");
-    }
-
-    my $genomic_align = new Bio::EnsEMBL::Compara::GenomicAlign (
-            -adaptor => $genomic_align_adaptor,
-            -dnafrag => $dnafrag,
-            -dnafrag_start => $dnafrag_start,
-            -dnafrag_end => $dnafrag_end,
-            -dnafrag_strand => $strand,
-            -aligned_sequence => $aligned_sequence,
-            -group_id => 0,
-            -level_id => 1
-        );
+    my $genomic_align = get_this_genomic_align($species, $chromosome, $start_pos, $length, $strand,
+          $aligned_sequence);
+    next if (!$genomic_align);
 
     if ($check_sequences) {
       my $db_sequence = uc($genomic_align->dnafrag->slice->subseq(
@@ -355,17 +328,35 @@ foreach my $multiz_file (@multiz_files) {
               $genomic_align->dnafrag_strand
           ));
       my $multiz_sequence = uc($genomic_align->original_sequence);
+      my $err_str = 
+          "Error while retrieving sequence ".
+          $ucsc_2_ensembl->{$species}->{'name'}.
+          ", chromosome $chromosome [". $genomic_align->dnafrag->name.":".
+          $genomic_align->dnafrag_start. "-". $genomic_align->dnafrag_end. "] ".
+          (($strand == 1)?"(+)":"(-)")." -- (MULTIZ start:$start_pos length:$length\n".
+          " DATABS: ". substr($db_sequence, 0, 10). "..".
+          substr($db_sequence, -11). "\n".
+          " MULTIZ: ". substr($multiz_sequence, 0, 10). "..".
+          substr($multiz_sequence, -11);
       if ($db_sequence ne $multiz_sequence) {
-        my $err_str = 
-            "Error while retrieving sequence ".
-            $ucsc_2_ensembl->{$species}->{'name'}.
-            ", chromosome $chromosome [". $dnafrag_start. "-". $dnafrag_end. "] ".
-            (($strand == 1)?"(+)":"(-)")." -- (MULTIZ start:$start_pos length:$length\n".
-            " DATABS: ". substr($db_sequence, 0, 10). "..".
-            substr($db_sequence, -11). "\n".
-            " MULTIZ: ". substr($multiz_sequence, 0, 10). "..".
-            substr($multiz_sequence, -11);
-        throw $err_str;
+        my $ga_start = $genomic_align->dnafrag_start;
+        my $ga_end = $genomic_align->dnafrag_end;
+        $genomic_align->dnafrag_strand($genomic_align->dnafrag_strand);
+        $genomic_align->dnafrag_start($genomic_align->dnafrag->slice->end - $ga_end + 1);
+        $genomic_align->dnafrag_end($genomic_align->dnafrag->slice->end - $ga_start + 1);
+        $db_sequence = uc($genomic_align->dnafrag->slice->subseq(
+                $genomic_align->dnafrag_start,
+                $genomic_align->dnafrag_end,
+                $genomic_align->dnafrag_strand
+            ));
+        if ($db_sequence eq $multiz_sequence) {
+          warning("Reversing ".$genomic_align->dnafrag->slice->name);
+        }
+      }
+      if ($db_sequence ne $multiz_sequence) {
+        warning($err_str);
+        <STDIN>;
+        next;
       }
     }
     push(@all_these_genomic_aligns, $genomic_align);
@@ -413,10 +404,8 @@ exit(0);
 
 =head2 get_this_dnafrag
 
-  Arg [1]    : Bio::EnsEMBL::Compara::GenomeDB $genome_db
-               The GenomeDB object corresponding to this dnafrag.
-  Arg[2]     : string $fragment_type
-  Arg[3]     : string $fragment_name
+  Arg [1]    : string $species
+  Arg[2]     : string $fragment_name
   Example    : get_this_danfrag($human_db, 'chromosome', '17');
   Description: Returns the corresponding DnaFrag object.
   Returntype : Bio::EnsEMBL::Compara::DnaFrag
@@ -424,20 +413,217 @@ exit(0);
 
 =cut
 
-sub get_this_dnafrag {
-  my ($genome_db, $fragment_type, $fragment_name) = @_;
+my $dnafrag_mapper;
+my $dnafrag_length;
+sub get_this_genomic_align {
+  my ($species, $name, $start_pos, $length, $strand, $aligned_sequence) = @_;
 
-  my $dnafrags = $dnafrag_adaptor->fetch_all_by_GenomeDB_region($genome_db, $fragment_type, $fragment_name);
-  my $dnafrag = undef;
-  foreach my $this_dnafrag (@$dnafrags) {
-    if ((!defined($fragment_type) or ($this_dnafrag->coord_system_name eq $fragment_type))
-        and $this_dnafrag->name eq $fragment_name) {
-      $dnafrag = $this_dnafrag;
-      last;
+  my ($dnafrag_start, $dnafrag_end);
+  my $dnafrag = $dnafrag_adaptor->fetch_by_GenomeDB_and_name($genome_db->{$species}, $name);
+  if (!$dnafrag) {
+    ## This chromosome does not exist in EnsEMBL, we have to map it using the golden path
+    if (!defined($dnafrag_mapper->{"${species}_${name}"})) {
+      $dnafrag_mapper->{"${species}_${name}"} = get_this_dnafrag_mapper($species, $name);
+    }
+    if (!defined($dnafrag_length->{"${species}_${name}"})) {
+      $dnafrag_length->{"${species}_${name}"} = get_this_ucsc_chromosome_length($species, $name);
+    }
+
+    my @mapped_objects;
+    if ($strand == -1) {
+      @mapped_objects = $dnafrag_mapper->{"${species}_${name}"}->map_coordinates(
+              $name,
+              $dnafrag_length->{"${species}_${name}"}-($start_pos + $length - 1),
+              $dnafrag_length->{"${species}_${name}"}-($start_pos),
+              $strand,
+              'chromosome');
+    } else {
+      @mapped_objects = $dnafrag_mapper->{"${species}_${name}"}->map_coordinates(
+              $name,
+              ($start_pos + 1),
+              ($start_pos + $length),
+              $strand,
+              'chromosome');
+    }
+
+    if (scalar(@mapped_objects) == 1 and $mapped_objects[0]->isa("Bio::EnsEMBL::Mapper::Coordinate")) {
+      $dnafrag = $dnafrag_adaptor->fetch_by_GenomeDB_and_name(
+              $genome_db->{$species},
+              $mapped_objects[0]->id
+          );
+      if (!$dnafrag) {
+        ## DnaFrags are not in the compara DB. Try to project on toplevel coord system
+        my $slice_adaptor = $genome_db->{$species}->db_adaptor->get_SliceAdaptor;
+        my $slice = ($slice_adaptor->fetch_by_region(undef, $mapped_objects[0]->id)
+                or $slice_adaptor->fetch_by_region(undef, $mapped_objects[0]->id.".1"));
+        my $projection_segments;
+        $projection_segments = $slice->project('toplevel') if (defined($slice));
+        if ($projection_segments and scalar(@$projection_segments) == 1) {
+          my $projected_start = $projection_segments->[0]->from_start;
+          my $projected_end = $projection_segments->[0]->from_end;
+          my $projected_slice = $projection_segments->[0]->to_Slice;
+          warning("$projected_start != ", $projected_slice->start)
+              if ($projected_start != $projected_slice->start);
+          warning("$projected_end != ", $projected_slice->end)
+              if ($projected_end != $projected_slice->end);
+          $dnafrag = $dnafrag_adaptor->fetch_by_GenomeDB_and_name(
+                  $genome_db->{$species},
+                  $projected_slice->seq_region_name
+              );
+          $dnafrag_start = $mapped_objects[0]->start + $projected_slice->start - $projected_start;
+          $dnafrag_end = $mapped_objects[0]->end + $projected_slice->end - $projected_end;
+        } else {
+          $all_warnings{"Mapping $species.".$mapped_objects[0]->id. " on $species.chr$name"}++;
+          $malign_warning .= "- $species.chr$name (".$mapped_objects[0]->id.") -";
+          return undef;
+        }
+      } else {
+        $dnafrag_start = $mapped_objects[0]->start;
+        $dnafrag_end = $mapped_objects[0]->end;
+      }
+    }
+  } else {
+    if ($strand == 1) {
+      $dnafrag_start = $start_pos + 1;
+      $dnafrag_end = $start_pos + $length;
+    } elsif ($strand == -1) {
+      $dnafrag_start = $dnafrag->length - $start_pos - $length + 1;
+      $dnafrag_end = $dnafrag->length - $start_pos;
+    } else {
+      throw("Cannot understand strand $strand");
     }
   }
   
-  #returns null if the dnafrag does not exist in the database
-  return $dnafrag;
+  ## Deal with unknown dnafrags
+  if (!$dnafrag) {
+    $print_multiple_alignment =~ s/\n$/  **NOT AVAILABLE**\n/;;
+    if ($force) {
+      print "IGNORING: Chromosome $species.chr$name\n";
+      $all_warnings{"Chromosome $species.chr$name"}++;
+      return undef;
+    } elsif ($skip) {
+      $all_warnings{"Chromosome $species.chr$name"}++;
+      $malign_warning .= "- $species.chr$name -";
+      return undef;
+    } else {
+      throw ("Cannot fetch DnaFrag for ".$ucsc_2_ensembl->{$species}->{'name'}.", chromosome $name\n".
+          $print_multiple_alignment);
+    }
+  }
+    
+  my $genomic_align = new Bio::EnsEMBL::Compara::GenomicAlign (
+          -adaptor => $genomic_align_adaptor,
+          -dnafrag => $dnafrag,
+          -dnafrag_start => $dnafrag_start,
+          -dnafrag_end => $dnafrag_end,
+          -dnafrag_strand => $strand,
+          -aligned_sequence => $aligned_sequence,
+          -group_id => 0,
+          -level_id => 1
+      );
+
+  return $genomic_align;
 }
 
+
+=head2 get_this_ucsc_chromosome_length
+
+ Arg[1]      : string $short_UCSC_species_name
+ Arg[2]      : string $chromosome_name
+ Example     : get_this_ucsc_chromosome_length("galGal2", "chrX");
+ Description : This function reads the chromInfo file corresponding to this
+               species and returns the length of the choosen chromosome
+ ReturnType  : integer
+ Exception   : return 0 if the data is not found
+ Caller      : methodname
+
+=cut
+
+sub get_this_ucsc_chromosome_length {
+  my ($species, $name) = @_;
+  my $dnafrag_mapper;
+
+  ## Get the length of the chromosome
+  my $chromosome_length = qx"fgrep 'chr$name' $multiz_dir/${species}_chromInfo.txt";
+  if (defined($chromosome_length)) {
+    ($chromosome_length) = ($chromosome_length =~ /^\S+\s+(\d+)/);
+  }
+  $chromosome_length = 0 if (!defined($chromosome_length));
+
+  return $chromosome_length;
+}
+
+
+=head2 get_this_dnafrag_mapper
+
+ Arg[1]      : string $short_UCSC_species_name
+ Arg[2]      : string $chromosome_name
+ Example     : get_this_dnafrag_mapper("galGal2", "chrX");
+ Description : This function reads the golden path file corresponding to this
+               species and creates a Bio::EnsEMBL::Mapper object with this
+               assembly
+ ReturnType  : Bio::EnsEMBL::Mapper object
+ Exception   : return undef if file cannot be opened
+ Exception   : return undef if file is not understood
+ Caller      : methodname
+
+=cut
+
+sub get_this_dnafrag_mapper {
+  my ($species, $name) = @_;
+  my $dnafrag_mapper;
+
+  open(GOLD, "$multiz_dir/${species}_gold.txt") or return undef;
+  $dnafrag_mapper = new Bio::EnsEMBL::Mapper('frag', 'chromosome');
+  while (<GOLD>) {
+    next if (/^#/);
+    if (!/^\d+\s+(\S+)\s+(\d+)\s+(\d+)\s+\d+\s+\w\s+(\S+)\s+(\d+)\s+(\d+)\s+([\-|\+|\.])$/) {
+      throw ("Wrong File format: $multiz_dir/${species}_gold.txt\n$_\n");
+      return undef;
+    }
+    my ($chr, $chr_start, $chr_end, $frag, $frag_start, $frag_end, $frag_strand) =
+        ($1, $2, $3, $4, $5, $6, $7);
+    $frag_strand = "+" if ($frag_strand ne "-");
+    $chr =~ s/^chr//;
+    next if ($chr ne $name);
+    if (($frag_end - $frag_start + 1) == ($chr_end - $chr_start)) {
+      $frag_start--;
+    }
+    $dnafrag_mapper->add_map_coordinates(
+            $frag, $frag_start+1, $frag_end, $frag_strand."1",
+            $chr, $chr_start+1, $chr_end);
+  }
+  close(GOLD);
+
+  return $dnafrag_mapper;
+}
+
+
+=head2 store_all_dnafrags
+
+ Arg[1]      : Bio::EnsEMBL::Compara::DBSQL::DnaFragAdaptor $dnafrag_adaptor
+ Arg[2]      : Bio::EnsEMBL::Compara::GenomeDB $genome_db
+ Example     : store_all_dnafrags($dnafrag_adaptor, $human_genome_db);
+ Description : This function stores (if needed) all the dnafrags corresponding
+               to tolevel slices in the corresponding core DB.
+ Exception   : none
+ Caller      : methodname
+
+=cut
+
+sub store_all_dnafrags {
+  my ($dnafrag_adaptor, $genome_db) = @_;
+
+  my $slice_adaptor = $genome_db->db_adaptor->get_SliceAdaptor;
+
+  my $all_slices = $slice_adaptor->fetch_all('toplevel');
+  foreach my $slice (@$all_slices) {
+    my $new_dnafrag = new Bio::EnsEMBL::Compara::DnaFrag(
+            -length => $slice->seq_region_length,
+            -name => $slice->seq_region_name,
+            -genome_db => $genome_db,
+            -coord_system_name => $slice->coord_system->name
+        );
+    $dnafrag_adaptor->store_if_needed($new_dnafrag);
+  }
+}
