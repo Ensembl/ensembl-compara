26469f5505fcf6282b2f727e545dc8cf9deebb36
Steve Trevanion <st3@sanger.ac.uk>
2004-12-20 10:04:57 +0000

    port from vega-26-dev

--- modules/Bio/EnsEMBL/GlyphSet/est2genome.pm
+++ modules/Bio/EnsEMBL/GlyphSet/est2genome.pm
@@ -9,7 +9,7 @@ sub my_label { return "ESTs"; }
 
 sub features {
     my ($self) = @_;
-    return $self->{'container'}->get_all_SimilarityFeatures( "est2genome", 80);
+    return $self->{'container'}->get_all_DnaAlignFeatures( "est2genome", 80);
 }
 
 sub href {
@@ -19,7 +19,14 @@ sub href {
 
 sub zmenu {
     my ($self, $id ) = @_;
-    $id =~ s/(^[^\.]+)\..*/$1/;
-    return { 'caption' => "EST $id", "$id" => $self->href( $id ) };
+    (my $stripped_id = $id) =~ s/(^[^\.]+)\..*/$1/;
+    my $zmenu = { 'caption' => "EST $stripped_id", "$stripped_id" => $self->href( $stripped_id ) };
+
+    my $extra_URL  = "/@{[$self->{container}{_config_file_name_}]}/featureview?type=DnaAlignFeature&id=$id";
+    #$extra_URL .= "&db=".$self->my_config('DATABASE') if $self->my_config('DATABASE');
+    $zmenu->{ 'View all hits' } = $extra_URL;
+    return $zmenu;
 }
+
 1;
+
--- modules/Bio/EnsEMBL/GlyphSet/est2genome_all.pm
+++ modules/Bio/EnsEMBL/GlyphSet/est2genome_all.pm
@@ -5,8 +5,8 @@ use warnings;
 no warnings 'uninitialized';
 
 use vars qw(@ISA);
-use Bio::EnsEMBL::GlyphSet_feature;
-@ISA = qw(Bio::EnsEMBL::GlyphSet_feature);
+use Bio::EnsEMBL::GlyphSet::est2genome;
+@ISA = qw(Bio::EnsEMBL::GlyphSet::est2genome);
 
 sub my_label { return "ESTs"; }
 
@@ -17,14 +17,5 @@ sub features {
            $self->{'container'}->get_all_DnaAlignFeatures("est2genome_other", 80);
 }
 
-sub href {
-    my ( $self, $id ) = @_;
-    return $self->{'config'}->{'exturl'}->get_url( 'EMBL', $id );
-}
-
-sub zmenu {
-    my ($self, $id ) = @_;
-    $id =~ s/(^[^\.]+)\..*/$1/;
-    return { 'caption' => "EST $id", "$id" => $self->href( $id ) };
-}
 1;
+
