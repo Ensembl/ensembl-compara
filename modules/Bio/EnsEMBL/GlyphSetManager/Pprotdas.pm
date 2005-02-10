71fb8f117e4ccc0c2af574d5b496429a9c027fb4
Eugene Kulesha <ek@ebi.ac.uk>
2005-02-10 09:32:09 +0000

    enabled display of ensembl_location DAS sources in protview

--- modules/Bio/EnsEMBL/GlyphSetManager/Pprotdas.pm
+++ modules/Bio/EnsEMBL/GlyphSetManager/Pprotdas.pm
@@ -17,7 +17,8 @@ sub init {
   my %authorities = map{$_->name => $_->authority} @das_adaptors;
 
   # Get features. The data structure returned is YUK!
-  my $feat_container = $translation->get_all_DASFeatures();
+  my $feat_container = $translation->get_all_DAS_Features();
+
   ref( $feat_container ) ne 'HASH' and return; # Sanity check
 
   # Set the order flag. This ensures glyphsets are drawn in the order 
@@ -27,37 +28,46 @@ sub init {
   # Examine the data structure, and create glyphs accordingly
   my $user_confkey;
   foreach my $das_confkey( keys( %$feat_container ) ){
-
     $user_confkey = 'genedas_'.$das_confkey;
+ 
+    my $sub_feat_container = $feat_container->{$das_confkey};
+    ref( $sub_feat_container ) ne 'ARRAY' and next; # Another sanity check
+    my $feat_ref = $sub_feat_container->[1];
 
-    # Add a separator (top)
-    $self->add_glyphset_separator
-      ({ 
+	 
+    # OK - we now have a list of DASSeqFeature objs. Sort them per-glyph
+    my %feats_by_glyphset;
+	 my $skipped_features = 0;
+    foreach my $feat( @$feat_ref ){
+
+      # Skip protein-wide features (GeneDAS - tabulated elsewhere )
+      # GeneDAS Identified by DAS segment id eq DAS feature id
+#      if( $feat->das_segment->ref() eq $feat->das_id() ) { next; }	
+		  my $type = $feat->das_type_id() || 'UNKNOWN';
+
+		  if ( ($feat->das_type_id() =~ /^(contig|component|karyotype)$/i) || ( $feat->das_type_id() =~ /^(contig|component|karyotype):/i) || (! $feat->das_end() )) {
+				$skipped_features = 1;
+				next;
+		  }
+
+		my $fend = $feat->das_end();
+
+      $feats_by_glyphset{$type} ||= [];
+      push @{$feats_by_glyphset{$type}}, $feat
+    }
+
+    if (! scalar keys %feats_by_glyphset) {
+		  next if ($skipped_features);
+		  $feats_by_glyphset{'No annotation'} = [] 
+    };
+
+   # Add a separator (top)
+    $self->add_glyphset_separator ({ 
         name=>"DAS $das_confkey",
         confkey=>$user_confkey,
         authority=>$authorities{$das_confkey},
         order  => sprintf("%05d", $self->{order} -- )
        });
-
-    my $sub_feat_container = $feat_container->{$das_confkey};
-    ref( $sub_feat_container ) ne 'ARRAY' and next; # Another sanity check
-    my $feat_ref = $sub_feat_container->[1];
-
-    # OK - we now have a list of DASSeqFeature objs. Sort them per-glyph
-    my %feats_by_glyphset;
-    foreach my $feat( @$feat_ref ){
-
-      # Skip protein-wide features (GeneDAS - tabulated elsewhere )
-      # GeneDAS Identified by DAS segment id eq DAS feature id
-      if( $feat->das_segment->ref() eq $feat->das_id() ) { next; }	
-
-      my $type = $feat->das_type_id() || 'UNKNOWN';
-      $feats_by_glyphset{$type} ||= [];
-      push @{$feats_by_glyphset{$type}}, $feat
-    }
-    if( ! scalar keys %feats_by_glyphset ){ 
-      $feats_by_glyphset{'No annotation'} = [] 
-    };
 
     foreach my $das_track( keys %feats_by_glyphset ) {
       my $extra_config = {};
@@ -67,7 +77,7 @@ sub init {
       $extra_config->{'order'}    = sprintf( "%05d", $self->{order} -- );
       $self->add_glyphset( $extra_config );
     }
-  }
+}
 
     # Add a separator (bottom)
   #$self->add_glyphset_separator({ name=>'', 
