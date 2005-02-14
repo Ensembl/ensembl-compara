dd8f2af24ea8b668a6b47537a754bb44f9e36c19
Jessica Severin <jessica@sanger.ac.uk>
2005-02-14 10:14:43 +0000

    added check in method new_from_transcript for transcripts with defined sequence but which is zero length

--- modules/Bio/EnsEMBL/Compara/Member.pm
+++ modules/Bio/EnsEMBL/Compara/Member.pm
@@ -160,6 +160,9 @@ sub new_from_transcript {
     if ($seq_string =~ /^X+$/) {
       warn("X+ in sequence from translation " . $transcript->translation->stable_id."\n");
     }
+    elsif (length($seq_string) == 0) {
+      warn("zero length sequence from translation " . $transcript->translation->stable_id."\n");
+    }
     else {
       #$seq_string =~ s/(.{72})/$1\n/g;
       $self->sequence($seq_string);
