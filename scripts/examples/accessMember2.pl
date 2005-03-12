ba9de63f0d1e8ea67e25ea374273aed1ecd5f5e2
Jessica Severin <jessica@sanger.ac.uk>
2005-03-12 16:47:58 +0000

    example which fetches a gene_member, gets all its peptide_members (translated transcripts) and prints them out with seq_length (which is dynamically loaded)

---
+++ scripts/examples/accessMember2.pl
@@ -0,0 +1,28 @@
+#!/usr/local/ensembl/bin/perl -w
+
+use strict;
+use Getopt::Long;
+use Bio::EnsEMBL::Registry;
+use Bio::EnsEMBL::Compara::DBSQL::DBAdaptor;
+
+my $reg_conf = shift;
+die("must specify registry conf file on commandline\n") unless($reg_conf);
+Bio::EnsEMBL::Registry->load_all($reg_conf);
+
+# get compara DBAdaptor
+my $comparaDBA = Bio::EnsEMBL::Registry-> get_DBAdaptor('compara', 'compara');
+
+my $ma = $comparaDBA->get_MemberAdaptor;
+my $gene_member = $ma->fetch_by_source_stable_id("ENSEMBLGENE", "ENSG00000060069");
+$gene_member->print_member;
+
+my $members = $gene_member->get_all_peptide_Members;
+printf("fetched %d peptides (translated transcripts) for gene\n", scalar(@$members));
+
+foreach my $m2 (@{$members}) {
+  printf("seq_length = %d", $m2->seq_length);
+  $m2->print_member;
+}
+
+exit(0);
+
