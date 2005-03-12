8c26079cc475eb9c97f12e7d3479b1561084efe0
Jessica Severin <jessica@sanger.ac.uk>
2005-03-12 11:37:39 +0000

    moved into compara from my personal testing directory a set of working examples that use a Registry and use the compara API

---
+++ scripts/examples/acessGenomeDB.pl
@@ -0,0 +1,31 @@
+#!/usr/local/ensembl/bin/perl -w
+
+use strict;
+use Getopt::Long;
+use Bio::EnsEMBL::Registry;
+use Bio::EnsEMBL::Compara::DBSQL::DBAdaptor;
+
+my $reg_conf = shift;
+die("must specify registry conf file on commandline\n") unless($reg_conf);
+Bio::EnsEMBL::Registry->load_all($reg_conf);
+
+# get compara DBAdaptor
+my $comparaDBA = Bio::EnsEMBL::Registry-> get_DBAdaptor('compara', 'compara');
+
+# get GenomeDB for human
+my $humanGDB = $comparaDBA->get_GenomeDBAdaptor-> fetch_by_registry_name("human");
+
+# get DBAdaptor for Human ensembl core database
+my $human_core_DBA = $humanGDB->db_adaptor;
+
+# print some info
+printf("COMPARA %s : %s : %s\n   %s\n", $humanGDB->name, $humanGDB->assembly, $humanGDB->genebuild, 
+    join("_", reverse($humanGDB->taxon->classification)));
+
+my $species_name = $human_core_DBA->get_MetaContainer->get_Species->binomial;
+my $species_assembly = $human_core_DBA->get_CoordSystemAdaptor->fetch_all->[0]->version;
+my $species_genebuild = $human_core_DBA->get_MetaContainer->get_genebuild;
+printf("CORE    %s : %s : %s\n", $species_name, $species_assembly, $species_genebuild);
+
+exit(0);
+
---
+++ scripts/examples/checkComparaRegistryConf.pl
@@ -0,0 +1,48 @@
+#!/usr/local/ensembl/bin/perl -w
+
+use strict;
+use Getopt::Long;
+use Bio::EnsEMBL::Registry;
+use Bio::EnsEMBL::Compara::DBSQL::DBAdaptor;
+use Bio::SimpleAlign;
+
+my $reg_conf = shift;
+die("must specify registry conf file on commandline\n") unless($reg_conf);
+
+Bio::EnsEMBL::Registry->load_all($reg_conf);
+
+my $comparaDBA  = Bio::EnsEMBL::Registry->get_DBAdaptor('compara', 'compara');
+
+print($comparaDBA, "\n");
+
+my $genomes = $comparaDBA->get_GenomeDBAdaptor->fetch_all;
+foreach my $genomeDB (@{$genomes}) {
+  printf("genome_db(%d)\n", $genomeDB->dbID);
+  printf("  compara(%d) %s : %s : %s\n",
+     $genomeDB->taxon_id, $genomeDB->name, $genomeDB->assembly, $genomeDB->genebuild);
+
+  die("ERROR::  db_adaptor not connected to an ensembl-core") unless($genomeDB->db_adaptor);
+
+  my $meta = $genomeDB->db_adaptor->get_MetaContainer;
+
+  my $taxon_id = $meta->get_taxonomy_id;
+  my $taxon = $meta->get_Species;
+
+  my $genome_name = $taxon->binomial;
+  my ($cs) = @{$genomeDB->db_adaptor->get_CoordSystemAdaptor->fetch_all()};
+  my $assembly = $cs->version;
+  my $genebuild = $meta->get_genebuild;
+
+  printf("  core   (%d) %s : %s : %s\n", $taxon_id, $genome_name, $assembly, $genebuild);
+
+  print("=== MISMATCH ===\n\n")
+  unless(($genomeDB->taxon_id == $taxon_id) and
+         ($genomeDB->name eq $genome_name) and
+         ($genomeDB->assembly eq $assembly) and
+         ($genomeDB->genebuild eq $genebuild));
+
+}
+
+
+exit(0);
+
