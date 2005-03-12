06426ef71365e1bb1be362bea628d8ef5cdece1c
Jessica Severin <jessica@sanger.ac.uk>
2005-03-12 18:18:20 +0000

    simple script to grab a DnaFrag object and then use it to get the core Slice and then do something with the slice (subslice, get_all_Genes, loop & print)

---
+++ scripts/examples/accessDnaFrag.pl
@@ -0,0 +1,33 @@
+#!/usr/local/ensembl/bin/perl -w
+
+use strict;
+use Getopt::Long;
+use Bio::EnsEMBL::Registry;
+use Bio::EnsEMBL::Compara::DBSQL::DBAdaptor;
+
+my $reg_conf = shift;
+die("must specify registry conf file on commandline\n") unless($reg_conf);
+Bio::EnsEMBL::Registry->load_all($reg_conf);
+
+# get compara DBAdaptor
+my $comparaDBA = Bio::EnsEMBL::Registry->get_DBAdaptor('compara', 'compara');
+
+# get GenomeDB for human
+my $humanGDB = $comparaDBA->get_GenomeDBAdaptor->fetch_by_registry_name("human");
+
+# get dnafrag for human chr 18
+my $dnafrag = $comparaDBA->get_DnaFragAdaptor->
+     fetch_by_GenomeDB_and_name($humanGDB, '18');
+
+# get its core Slice and subslice it
+my $slice = $dnafrag->slice;
+$slice = $slice->sub_Slice(75000000,76000000,1);
+
+# the the genes on the subslice and print
+my $genes = $slice->get_all_Genes;
+foreach my $gene (@{$genes}) {
+  printf("%s\n", $gene->stable_id);
+}
+
+exit(0);
+
