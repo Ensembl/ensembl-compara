97f6868c3ee3ccbf704bd36a4f895759340523db
Jessica Severin <jessica@sanger.ac.uk>
2005-03-21 21:22:11 +0000

    added explicit use statements for each object used in this example so students know which perldocs to check. Also changed to use Registry::get_adaptor to find MemberAdaptor to show both the old and new ways to get the adaptors

--- scripts/examples/getHomology.pl
+++ scripts/examples/getHomology.pl
@@ -2,9 +2,15 @@
 
 use strict;
 use Getopt::Long;
-use Bio::EnsEMBL::Registry;
-use Bio::EnsEMBL::Compara::DBSQL::DBAdaptor;
 use Time::HiRes qw { time };
+use Bio::EnsEMBL::Registry;
+
+use Bio::EnsEMBL::Compara::DBSQL::MemberAdaptor;
+use Bio::EnsEMBL::Compara::DBSQL::HomologyAdaptor;
+use Bio::EnsEMBL::Compara::Homology;
+use Bio::EnsEMBL::Compara::Member;
+use Bio::EnsEMBL::Compara::Attribute;
+
 
 my $reg_conf = shift;
 die("must specify registry conf file on commandline\n") unless($reg_conf);
@@ -12,16 +18,16 @@ Bio::EnsEMBL::Registry->load_all($reg_conf);
 
 ###########################
 # 
-# advanced example which uses a recursive approach
-# to build single linkage clusters within a species set
+# example to get all orthologues which 
+# contain a specified gene
 #
 ###########################
 
 # get compara DBAdaptor
 my $comparaDBA = Bio::EnsEMBL::Registry-> get_DBAdaptor('compara', 'compara');
 
-my $MA = $comparaDBA->get_MemberAdaptor;
-my $gene_member = $MA->fetch_by_source_stable_id("ENSEMBLGENE", "ENSG00000060069");
+my $memberAdaptor = Bio::EnsEMBL::Registry->get_adaptor('compara', 'compara', 'Member');
+my $gene_member = $memberAdaptor->fetch_by_source_stable_id("ENSEMBLGENE", "ENSG00000060069");
 
 my $homologies = $comparaDBA-> get_HomologyAdaptor-> 
        fetch_all_by_Member_method_link_type($gene_member, 'ENSEMBL_ORTHOLOGUES');
