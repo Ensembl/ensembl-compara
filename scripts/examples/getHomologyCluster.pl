e2fa13928f2b44483a2f0ee38d710b175952cb7f
Jessica Severin <jessica@sanger.ac.uk>
2005-03-21 21:53:50 +0000

    removed confusing hash of species so now grab cluster for all species changed starting gene to a DNA polymerase subunit ENSG00000014138 added member_set to track unique members in this cluster (and restrict search)

--- scripts/examples/getHomologyCluster.pl
+++ scripts/examples/getHomologyCluster.pl
@@ -2,9 +2,14 @@
 
 use strict;
 use Getopt::Long;
-use Bio::EnsEMBL::Registry;
-use Bio::EnsEMBL::Compara::DBSQL::DBAdaptor;
 use Time::HiRes qw { time };
+
+use Bio::EnsEMBL::Registry;
+use Bio::EnsEMBL::Compara::DBSQL::MemberAdaptor;
+use Bio::EnsEMBL::Compara::DBSQL::HomologyAdaptor;
+use Bio::EnsEMBL::Compara::Homology;
+use Bio::EnsEMBL::Compara::Member;
+use Bio::EnsEMBL::Compara::Attribute;
 
 my $reg_conf = shift;
 die("must specify registry conf file on commandline\n") unless($reg_conf);
@@ -13,35 +18,32 @@ Bio::EnsEMBL::Registry->load_all($reg_conf);
 ###########################
 # 
 # advanced example which uses a recursive approach
-# to build single linkage clusters within a species set
+# to build single linkage clusters within the orthologues
+# by starting at a specific gene and following the links
 #
 ###########################
 
 # get compara DBAdaptor
 my $comparaDBA = Bio::EnsEMBL::Registry-> get_DBAdaptor('compara', 'compara');
 
-# build a species set with a hash that is easy to test against 
-my $human = $comparaDBA->get_GenomeDBAdaptor->fetch_by_registry_name("human");
-my $mouse = $comparaDBA->get_GenomeDBAdaptor->fetch_by_registry_name("mouse");
-my $chicken = $comparaDBA->get_GenomeDBAdaptor->fetch_by_registry_name("chicken");
-my $species_set = {};
-$species_set->{$human->dbID} = 1;
-$species_set->{$mouse->dbID} = 1;
-$species_set->{$chicken->dbID} = 1;
-
 
 my $MA = $comparaDBA->get_MemberAdaptor;
-my $gene_member = $MA->fetch_by_source_stable_id("ENSEMBLGENE", "ENSG00000060069");
+my $gene_member = $MA->fetch_by_source_stable_id("ENSEMBLGENE", "ENSG00000014138");
 
 my $start = time();
 my $ortho_set = {};
-get_orthologue_cluster($gene_member, $species_set, $ortho_set, 0);
+my $member_set = {};
+get_orthologue_cluster($gene_member, $ortho_set, $member_set, 0);
 
 printf("cluster has %d links\n", scalar(keys(%{$ortho_set})));
+printf("cluster has %d genes\n", scalar(keys(%{$member_set})));
 printf("%1.3f msec\n", 1000.0*(time() - $start));
 
 foreach my $homology (values(%{$ortho_set})) {
   $homology->print_homology;
+}
+foreach my $member (values(%{$member_set})) {
+  $member->print_member;
 }
 
 
@@ -50,11 +52,14 @@ exit(0);
 
 sub get_orthologue_cluster {
   my $gene = shift;
-  my $species_set = shift;
   my $ortho_set = shift;
+  my $member_set = shift;
   my $debug = shift;
 
+  return if($member_set->{$gene->dbID});
+
   $gene->print_member("query gene\n") if($debug);
+  $member_set->{$gene->dbID} = $gene;
 
   my $homologies = $comparaDBA->get_HomologyAdaptor->fetch_by_Member($gene);
   printf("fetched %d homologies\n", scalar(@$homologies)) if($debug);
@@ -67,12 +72,10 @@ sub get_orthologue_cluster {
       my ($member, $attribute) = @{$member_attribute};
       next if($member->dbID == $gene->dbID); #skip query gene
       $member->print_member if($debug);
-      next unless($species_set->{$member->genome_db_id});
-      print("YES in species set\n") if($debug);
 
       printf("adding homology_id %d to cluster\n", $homology->dbID) if($debug);
       $ortho_set->{$homology->dbID} = $homology;
-      get_orthologue_cluster($member, $species_set, $ortho_set, $debug);
+      get_orthologue_cluster($member, $ortho_set, $member_set, $debug);
     }
   }
   printf("done with search query %s\n", $gene->stable_id) if($debug);
