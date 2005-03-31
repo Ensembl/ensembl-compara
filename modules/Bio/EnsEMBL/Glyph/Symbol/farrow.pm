94c562084e56a7b0cb39be23ac2977afaa2a37c4
James Stalker <jws@sanger.ac.uk>
2005-03-31 14:38:17 +0000

    Now simple wrapper subclasses of anchored_arrow

--- modules/Bio/EnsEMBL/Glyph/Symbol/farrow.pm
+++ modules/Bio/EnsEMBL/Glyph/Symbol/farrow.pm
@@ -4,50 +4,25 @@ Bio::EnsEMBL::Glyph::Symbol::farrow
 
 =head1 DESCRIPTION
 
-A collection of drawing-code glyphs to represent a forward-pointing arrow.
+Thin wrapper subclass around anchored_arrow, forcing forward orientation, and
+setting full for default bar_style.
 
 =cut
 
 package Bio::EnsEMBL::Glyph::Symbol::farrow;
 use strict;
-use Sanger::Graphics::Glyph::Poly;
 
-sub draw {
-    my $self = shift;
-    my $style = $self->style;
-    my $feature = $self->feature;
+use vars qw(@ISA);
+use Bio::EnsEMBL::Glyph::Symbol::anchored_arrow;
+@ISA = qw(Bio::EnsEMBL::Glyph::Symbol::anchored_arrow);
 
-    my $rowheight = $feature->{'row_height'};
-    my $start = $feature->{'start'};
-    my $end = $feature->{'end'};
-    my $pix_per_bp = $feature->{'pix_per_bp'};
-    my $y_offset = $feature->{'y_offset'};
-    
-    my $linecolour = $style->{'fgcolor'};
-    my $fillcolour = $style->{'bgcolor'} || $style->{'colour'};
-
-    my $height = $style->{'height'};
-
-    my $slope = $height/2/$pix_per_bp;
-    my $points = ( $end - $start + 1 > $slope ) ?
-        [
-          $start - 1,    $y_offset,
-          $start - 1,    $y_offset + $height,
-          $end - $slope, $y_offset + $height,
-          $end,          $y_offset + $height/2,
-          $end - $slope, $y_offset
-        ] : [
-          $start-1,      $y_offset,
-          $start-1,      $y_offset + $height,
-          $end,          $y_offset + $height/2
-        ];
-    return new Sanger::Graphics::Glyph::Poly({
-        'points'    => $points,
-	'colour'     => $fillcolour,
-	'bordercolour' => $linecolour,
-        'absolutey' => 1
-    });
-
+sub default_bar_style {
+    return 'full';
 }
+
+sub orientation {
+    1;
+}
+
 
 1;
--- modules/Bio/EnsEMBL/Glyph/Symbol/rarrow.pm
+++ modules/Bio/EnsEMBL/Glyph/Symbol/rarrow.pm
@@ -4,55 +4,25 @@ Bio::EnsEMBL::Glyph::Symbol::rarrow
 
 =head1 DESCRIPTION
 
-A collection of drawing-code glyphs to represent a reverse-pointing arrow.
+Thin wrapper subclass around anchored_arrow, forcing reverse orientation, and
+setting full for default bar_style.
 
 =cut
 
 package Bio::EnsEMBL::Glyph::Symbol::rarrow;
 use strict;
-use Sanger::Graphics::Glyph::Poly;
 
 use vars qw(@ISA);
-use Bio::EnsEMBL::Glyph::Symbol;
-@ISA = qw(Bio::EnsEMBL::Glyph::Symbol);
+use Bio::EnsEMBL::Glyph::Symbol::anchored_arrow;
+@ISA = qw(Bio::EnsEMBL::Glyph::Symbol::anchored_arrow);
 
-sub draw {
-    my $self = shift;
-    my $style = $self->style;
-    my $feature = $self->feature;
-
-    my $rowheight = $feature->{'row_height'};
-    my $start = $feature->{'start'};
-    my $end = $feature->{'end'};
-    my $pix_per_bp = $feature->{'pix_per_bp'};
-    my $y_offset = $feature->{'y_offset'};
-    
-    my $linecolour = $style->{'fgcolor'};
-    my $fillcolour = $style->{'bgcolor'} || $style->{'colour'};
-
-    my $height = $style->{'height'};
-
-    my $slope = $height/2/$pix_per_bp;
-
-    my $points = ( $end - $start + 1 > $slope ) ?
-    [
-      $end,                $y_offset,
-      $end,                $y_offset + $height,
-      $start - 1 + $slope, $y_offset + $height,
-      $start - 1,          $y_offset + $height/2,
-      $start - 1 + $slope, $y_offset
-    ] : [
-      $end,                $y_offset,
-      $end,                $y_offset + $height,
-      $start-1,            $y_offset + $height/2
-    ];
-    return new Sanger::Graphics::Glyph::Poly({
-        'points'    => $points,
-	'colour'     => $fillcolour,
-	'bordercolour' => $linecolour,
-        'absolutey' => 1
-    });
-
+sub default_bar_style {
+    return 'full';
 }
+
+sub orientation {
+    -1;
+}
+
 
 1;
