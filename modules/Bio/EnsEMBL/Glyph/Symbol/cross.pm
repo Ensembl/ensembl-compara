0d52cf4d2db4d5e901daff2efdbaaf569a658601
James Stalker <jws@sanger.ac.uk>
2005-03-31 21:15:42 +0000

    old rugged DAS symbol

---
+++ modules/Bio/EnsEMBL/Glyph/Symbol/cross.pm
@@ -0,0 +1,61 @@
+=head1 NAME
+
+Bio::EnsEMBL::Glyph::Symbol::cross
+
+=head1 DESCRIPTION
+
+A collection of drawing-code glyphs to represent a cross.
+
+=cut
+
+package Bio::EnsEMBL::Glyph::Symbol::cross;
+use strict;
+use Sanger::Graphics::Glyph::Line;
+
+use vars qw(@ISA);
+use Bio::EnsEMBL::Glyph::Symbol;
+@ISA = qw(Bio::EnsEMBL::Glyph::Symbol);
+
+sub draw {
+    my $self = shift;
+    my $style = $self->style;
+    my $feature = $self->feature;
+
+    my $y_offset = $feature->{'y_offset'};
+    my $pix_per_bp = $feature->{'pix_per_bp'};
+    
+    # Point feature, so just use start
+    my $start = $feature->{'start'};
+   
+    my $linecolour = $style->{'fgcolor'};
+    my $fillcolour = $style->{'bgcolor'} || $style->{'colour'};
+    $linecolour ||= $fillcolour;
+
+    my $height = $style->{'height'};
+    my $width =  $style->{'linewidth'} || $height;
+    $width /= $pix_per_bp;  # remember, x in bp, y in pixels
+
+    my $x = $start -1 -$width/2;
+    my $y = $y_offset + $height/2;
+
+    return (new Sanger::Graphics::Glyph::Line({
+	    'x'          => $start -1 -$width/2,
+	    'y'          => $y_offset + $height/2,
+	    'width'      => $width,
+	    'height'     => 0,
+	    'colour'     => $linecolour,
+	    'absolutey' => 1
+	}),
+	new Sanger::Graphics::Glyph::Line({
+	    'x'          => $start-1,
+	    'y'          => $y_offset,
+	    'width'      => 0,
+	    'height'     => $height,
+	    'colour'     => $linecolour,
+	    'absolutey' => 1
+	})
+	);
+}
+
+
+1;
