e77645bd7fe64b07c1365c5d2bed24b8e401943a
Jessica Severin <jessica@sanger.ac.uk>
2005-04-27 13:23:17 +0000

    changed tree PRIMARY to node_id, added method_link_species_set_id to protein_tree_member

--- sql/protein_tree.sql
+++ sql/protein_tree.sql
@@ -6,21 +6,21 @@
 -- overview: for importing from the ncbi taxonomy taxdump.tar nodes.dmp file
 --           tables stores the nodes in a tree formated DB
 -- semantics:
---      nestedset_id          -- PRIMARY node id 
+--      node_id               -- PRIMARY node id 
 --      parent_id             -- parent node id
 --      root_id               -- to quickly isolated nodes of the different rooted tree sets
 --      left_index            -- for fast nested set searching
 --      right_index           -- for fast nested set searching
 
 CREATE TABLE protein_tree_nodes (
-  nestedset_id                    int(10) unsigned NOT NULL auto_increment, # unique internal id
+  node_id                         int(10) unsigned NOT NULL auto_increment, # unique internal id
   parent_id                       int(10) unsigned NOT NULL,
   root_id                         int(10) unsigned NOT NULL,
   left_index                      int(10) NOT NULL,
   right_index                     int(10) NOT NULL,
   distance_to_parent              double default 1.0 NOT NULL,
   
-  PRIMARY KEY (nestedset_id),
+  PRIMARY KEY (node_id),
   KEY (parent_id),
   KEY (root_id)
 );
@@ -33,18 +33,19 @@ CREATE TABLE protein_tree_nodes (
 -- overview:
 --   to allow certain nodes to have aligned members attached to them   
 -- semantics:
---    nestedset_id             -- the id of node associated with this name
+--    node_id                  -- the id of node associated with this name
 --    member_id                -- link to member.member_id in many-1 relation (single member per node)
 --    cigar_line               -- (synonym, common name, ...)
 
 CREATE TABLE protein_tree_member (
-  nestedset_id                int(10) unsigned NOT NULL,
+  node_id                     int(10) unsigned NOT NULL,
   member_id                   int(10) unsigned NOT NULL, 
+  method_link_species_set_id  int(10) unsigned NOT NULL,
   cigar_line                  mediumtext,
   cigar_start                 int(10),
   cigar_end                   int(10),
 
-  UNIQUE (nestedset_id),
+  UNIQUE (node_id),
   KEY (member_id)
 );
 
@@ -58,16 +59,16 @@ CREATE TABLE protein_tree_member (
 --    to allow for naming of nodes.  Useful for tagging multiple root nodes
 --    Allows for many-many relations
 -- semantics:
---    nestedset_id             -- the id of node associated with this name
---    name                     -- name itself
---    name_class               -- (synonym, common name, ...)
+--    node_id             -- the id of node associated with this name
+--    name                -- name itself
+--    name_class          -- (synonym, common name, ...)
 
 CREATE TABLE protein_tree_names (
-  nestedset_id                int(10) unsigned NOT NULL,
-  name                        varchar(255),
-  name_class                  varchar(50),
+  node_id                int(10) unsigned NOT NULL,
+  name                   varchar(255),
+  name_class             varchar(50),
 
-  KEY (nestedset_id),
+  KEY (node_id),
   KEY (name)
 );
 
