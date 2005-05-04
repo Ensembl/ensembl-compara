58ebfdb4393132e2edd82649f8bb7790625a388e
Jessica Severin <jessica@sanger.ac.uk>
2005-05-04 15:48:48 +0000

    new registry for release30 new script to output cDNA alignments of all the homologies of a species pair

---
+++ scripts/examples/getHomologyAligns.pl
@@ -0,0 +1,43 @@
+#!/usr/local/ensembl/bin/perl -w
+
+use strict;
+use Getopt::Long;
+use Bio::EnsEMBL::Registry;
+use Bio::EnsEMBL::Compara::DBSQL::DBAdaptor;
+use Bio::AlignIO;
+use Time::HiRes qw { time };
+
+my $reg_conf = shift;
+die("must specify registry conf file on commandline\n") unless($reg_conf);
+Bio::EnsEMBL::Registry->load_all($reg_conf);
+
+# get compara DBAdaptor
+my $comparaDBA = Bio::EnsEMBL::Registry-> get_DBAdaptor('compara', 'compara');
+
+my $mlss = $comparaDBA->get_MethodLinkSpeciesSetAdaptor->
+    fetch_by_method_link_type_genome_db_ids('ENSEMBL_ORTHOLOGUES',[1,2]);
+
+my $species_names = '';
+foreach my $gdb (@{$mlss->species_set}) {
+  $species_names .= $gdb->dbID.".".$gdb->name."  ";
+}
+printf("mlss(%d) %s : %s\n", $mlss->dbID, $mlss->method_link_type, $species_names);
+
+my $start = time();
+my $homology_list = $comparaDBA->get_HomologyAdaptor->
+    fetch_all_by_MethodLinkSpeciesSet($mlss);
+printf("%1.3f sec\n", (time() - $start));
+printf("fetched %d homologies\n", scalar(@{$homology_list}));
+
+foreach my $homology (@{$homology_list}) {
+  my $sa = $homology->get_SimpleAlign("cdna");
+  my $alignIO = Bio::AlignIO->newFh(-interleaved => 0,
+                                    -fh => \*STDOUT,
+                                    -format => "phylip",
+	                      -idlength => 20);
+
+ print $alignIO $sa;
+}
+
+exit(0);
+
