5db2e82069eb034cdef0e511b32232925f8c1fd2
Fiona Cunningham <fiona@ebi.ac.uk>
2005-09-30 15:37:04 +0000

    ths script needs to give blast a 'code' name so links to it can be turned off in archive site

--- modules/EnsEMBL/Web/Document/Configure.pm
+++ modules/EnsEMBL/Web/Document/Configure.pm
@@ -13,8 +13,9 @@ sub common_menu_items {
 ## Check blastview is available...
   if( 1 ) {
     $doc->menu->add_entry( 'whattodo',
+      'code' => 'blast',
       'href' => "/$species_m/blastview",
-      'text'=>'Run a BLAST search'
+      'text' => 'Run a BLAST search',
     );
   }
 ## Check Search is available...
