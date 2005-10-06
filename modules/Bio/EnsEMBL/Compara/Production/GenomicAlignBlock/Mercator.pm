47968133558b044ec9a8ad189694f891a8714823
Abel Ureta-Vidal <abel@sanger.ac.uk>
2005-10-06 15:37:44 +0000

    Modules for the whole genome multiple alignment pipeline

---
+++ modules/Bio/EnsEMBL/Compara/Production/GenomicAlignBlock/Mercator.pm
@@ -0,0 +1,405 @@
+#
+# You may distribute this module under the same terms as perl itself
+#
+# POD documentation - main docs before the code
+
+=pod 
+
+=head1 NAME
+
+Bio::EnsEMBL::Compara::Production::GenomicAlignBlock::Mercator
+
+=cut
+
+=head1 SYNOPSIS
+
+=cut
+
+=head1 DESCRIPTION
+
+
+=cut
+
+=head1 CONTACT
+
+Abel Ureta-Vidal <abel@ebi.ac.uk>
+
+=cut
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object methods. 
+Internal methods are usually preceded with a _
+
+=cut
+
+package Bio::EnsEMBL::Compara::Production::GenomicAlignBlock::Mercator;
+
+use strict;
+use Bio::EnsEMBL::Analysis::Runnable::Mercator;
+use Bio::EnsEMBL::Compara::DnaFragRegion;
+use Bio::EnsEMBL::Compara::Production::DBSQL::DBAdaptor;;
+use Bio::EnsEMBL::Utils::Exception;
+use Time::HiRes qw(time gettimeofday tv_interval);
+
+use Bio::EnsEMBL::Hive::Process;
+our @ISA = qw(Bio::EnsEMBL::Hive::Process);
+
+my $WORKDIR; # global variable holding the path to the working directory where output will be written
+
+=head2 fetch_input
+
+    Title   :   fetch_input
+    Usage   :   $self->fetch_input
+    Function:   Fetches input data for repeatmasker from the database
+    Returns :   none
+    Args    :   none
+
+=cut
+
+sub fetch_input {
+  my( $self) = @_;
+
+  #create a Compara::DBAdaptor which shares the same DBI handle
+  #with $self->db (Hive DBAdaptor)
+  $self->{'comparaDBA'} = Bio::EnsEMBL::Compara::Production::DBSQL::DBAdaptor->new(-DBCONN=>$self->db->dbc);
+
+  $self->strict_map(1);
+
+  $self->get_params($self->parameters);
+  $self->get_params($self->input_id);
+
+#  throw("Missing dna_collection_name") unless($self->dna_collection_name);
+
+  return 1;
+}
+
+sub run
+{
+  my $self = shift;
+  $self->dumpMercatorFiles;
+#  return 1;
+  print "genome_db_ids: ",$self->genome_db_ids,"\n";
+
+  unless (defined $self->output_dir) {
+    $WORKDIR = $self->worker_temp_directory;
+    mkdir("$WORKDIR/output_dir", 0777);
+    $self->output_dir("$WORKDIR/output_dir");
+  } else {
+    if (! -e $self->output_dir) {
+      mkdir($self->output_dir, 0777);
+    }
+  }
+
+  print "output_dir: ",$self->output_dir,"\n";
+  my $runnable = new Bio::EnsEMBL::Analysis::Runnable::Mercator
+    (-input_dir => $self->input_dir,
+     -output_dir => $self->output_dir,
+     -genome_names => $self->genome_db_ids,
+     -analysis => $self->analysis);
+  $self->{'_runnable'} = $runnable;
+  print "sr1 #: ",scalar @{$self->{'_runnable'}->output},"\n";
+  $runnable->run_analysis;
+#  $self->output($runnable->output);
+#  rmdir($runnable->workdir) if (defined $runnable->workdir);
+}
+
+sub write_output {
+  my ($self) = @_;
+
+  my %run_ids2synteny_and_constraints;
+  my $synteny_region_ids = $self->store_synteny(\%run_ids2synteny_and_constraints);
+  my $output_id = "{method_link_species_set_id=>".$self->method_link_species_set->dbID;
+  $output_id .= ",tree_file=>\'" . $self->tree_file . "\'";
+  foreach my $sr_id (@{$synteny_region_ids}) {
+    $self->dataflow_output_id($output_id . ",synteny_region_id=>$sr_id}");
+  }
+
+#  if ($self->mavid_constraints) {
+#    $self->store_mavid_constraints(\%run_ids2synteny_and_constraints);
+#  }
+
+  return 1;
+}
+
+sub store_synteny {
+  my ($self, $run_ids2synteny_and_constraints) = @_;
+
+  my $mlssa = $self->{'comparaDBA'}->get_MethodLinkSpeciesSetAdaptor;
+  my $sra = $self->{'comparaDBA'}->get_SyntenyRegionAdaptor;
+  my $dfa = $self->{'comparaDBA'}->get_DnaFragAdaptor;
+  my $gdba = $self->{'comparaDBA'}->get_GenomeDBAdaptor;
+
+  my @genome_dbs;
+  foreach my $gdb_id (@{$self->genome_db_ids}) {
+    my $gdb = $gdba->fetch_by_dbID($gdb_id);
+    push @genome_dbs, $gdb;
+  }
+  my $mlss = new Bio::EnsEMBL::Compara::MethodLinkSpeciesSet
+    (-method_link_type => "SYNTENY",
+     -species_set => \@genome_dbs);
+  $mlssa->store($mlss);
+  $self->method_link_species_set($mlss);
+
+  my $synteny_region_ids;
+  my %dnafrag_hash;
+  foreach my $sr (@{$self->{'_runnable'}->output}) {
+    my $synteny_region = new Bio::EnsEMBL::Compara::SyntenyRegion
+      (-method_link_species_set_id => $mlss->dbID);
+    my $run_id;
+    foreach my $dfr (@{$sr}) {
+      my ($gdb_id, $seq_region_name, $start, $end, $strand);
+      ($run_id, $gdb_id, $seq_region_name, $start, $end, $strand) = @{$dfr};
+      next if ($seq_region_name eq 'NA' && $start eq 'NA' && $end eq 'NA' && $strand eq 'NA');
+      my $dnafrag = $dnafrag_hash{$gdb_id."_".$seq_region_name};
+      unless (defined $dnafrag) {
+        $dnafrag = $dfa->fetch_by_GenomeDB_and_name($gdb_id, $seq_region_name);
+        $dnafrag_hash{$gdb_id."_".$seq_region_name} = $dnafrag;
+      }
+      $strand = ($strand eq "+")?1:-1;
+      my $dnafrag_region = new Bio::EnsEMBL::Compara::DnaFragRegion
+        (-dnafrag_id => $dnafrag->dbID,
+         -dnafrag_start => $start+1, # because half-open coordinate system
+         -dnafrag_end => $end,
+         -dnafrag_strand => $strand);
+      $synteny_region->add_child($dnafrag_region);
+    }
+    $sra->store($synteny_region);
+    push @{$synteny_region_ids}, $synteny_region->dbID;
+    push @{$run_ids2synteny_and_constraints->{$run_id}}, $synteny_region->dbID;
+    $synteny_region->release;
+  }
+
+  return $synteny_region_ids;
+}
+
+sub store_mavid_contraints {
+  my ($self, $run_ids2synteny_and_constraints) = @_;
+  
+  my $mlssa = $self->{'comparaDBA'}->get_MethodLinkSpeciesSetAdaptor;
+  my $sra = $self->{'comparaDBA'}->get_SyntenyRegionAdaptor;
+  my $dfa = $self->{'comparaDBA'}->get_DnaFragAdaptor;
+  my $gdba = $self->{'comparaDBA'}->get_GenomeDBAdaptor;
+  my $pafa = $self->{'comparaDBA'}->get_PeptideAlignAdaptor;
+
+  my @genome_dbs;
+  foreach my $gdb_id (@{$self->genome_db_ids}) {
+    my $gdb = $gdba->fetch_by_dbID($gdb_id);
+    push @genome_dbs, $gdb;
+  }
+  my $mlss = new Bio::EnsEMBL::Compara::MethodLinkSpeciesSet
+    (-method_link_type => "MAVID_CONSTRAINTS",
+     -species_set => \@genome_dbs);
+  $mlssa->store($mlss);
+
+  my $pairwisehits_file = $self->output_dir . "/pairwisehits";
+
+  open F, $pairwisehits_file ||
+    throw("Can't open $pairwisehits_file\n");
+
+  my %hash;
+  while (<F>) {
+    my ($run_id, $gdb_id1, $member_id1, $gdb_id2, $member_id2) = split;
+    my ($paf) = sort {$b->score <=> $a->score} @{$pafa->fetch_all_by_qmember_id_hmember_id($member_id1,$member_id2)};
+    # here we need to recalculate peptide coordinates to genomic coordinates....
+    # and match with a hash to the corresponding synteny_region_id
+    # push @{$run_ids2synteny_and_constraints}, $synteny_region->dbID;
+  }
+  close F;
+#  my $output = [ values %hash ];
+#  print "scalar output", scalar @{$output},"\n";
+#  print "No synteny regions found" if (scalar @{$output} == 0);
+#  $self->output($output);
+}
+
+##########################################
+#
+# getter/setter methods
+# 
+##########################################
+
+#sub dna_collection_name {
+#  my $self = shift;
+#  $self->{'_dna_collection_name'} = shift if(@_);
+#  return $self->{'_dna_collection_name'};
+#}
+
+sub input_dir {
+  my $self = shift;
+  $self->{'_input_dir'} = shift if(@_);
+  return $self->{'_input_dir'};
+}
+
+sub output_dir {
+  my $self = shift;
+  $self->{'_output_dir'} = shift if(@_);
+  return $self->{'_output_dir'};
+}
+
+sub genome_db_ids {
+  my $self = shift;
+  $self->{'_genome_db_ids'} = shift if(@_);
+  return $self->{'_genome_db_ids'};
+}
+
+sub cutoff_score {
+  my $self = shift;
+  $self->{'_cutoff_score'} = shift if(@_);
+  return $self->{'_cutoff_score'};
+}
+
+sub cutoff_evalue {
+  my $self = shift;
+  $self->{'_cutoff_evalue'} = shift if(@_);
+  return $self->{'_cutoff_evalue'};
+}
+
+sub strict_map {
+  my $self = shift;
+  $self->{'_strict_map'} = shift if(@_);
+  return $self->{'_strict_map'};
+}
+
+sub mavid_constraints {
+  my $self = shift;
+  $self->{'_mavid_constraints'} = shift if(@_);
+  return $self->{'_mavid_constraints'};
+}
+
+sub method_link_species_set {
+  my $self = shift;
+  $self->{'_method_link_species_set'} = shift if(@_);
+  return $self->{'_method_link_species_set'};
+}
+
+sub tree_file {
+  my $self = shift;
+  $self->{'_tree_file'} = shift if(@_);
+  return $self->{'_tree_file'};
+}
+
+##########################################
+#
+# internal methods
+#
+##########################################
+
+sub get_params {
+  my $self         = shift;
+  my $param_string = shift;
+
+  return unless($param_string);
+  print("parsing parameter string : ",$param_string,"\n");
+
+  my $params = eval($param_string);
+  return unless($params);
+  if(defined($params->{'input_dir'})) {
+    $self->input_dir($params->{'input_dir'});
+  }
+  if(defined($params->{'output_dir'})) {
+    $self->input_dir($params->{'output_dir'});
+  }
+  if(defined($params->{'gdb_ids'})) {
+    print "gdb_ids :",$params->{'gdb_ids'},"\n";
+    $self->genome_db_ids($params->{'gdb_ids'});
+  }
+  if(defined($params->{'cutoff_score'})) {
+    $self->cutoff_score($params->{'cutoff_score'});
+  }
+  if(defined($params->{'cutoff_evalue'})) {
+    $self->cutoff_evalue($params->{'cutoff_evalue'});
+  }
+  if(defined($params->{'strict_map'})) {
+    $self->strict_map($params->{'strict_map'});
+  }
+  if(defined($params->{'mavid_constraints'})) {
+    $self->mavid_constraints($params->{'mavid_constraints'});
+  }
+  if(defined($params->{'tree_file'})) {
+    $self->tree_file($params->{'tree_file'});
+  }
+  return 1;
+}
+
+sub dumpMercatorFiles {
+  my $self = shift;
+
+#  $self->{'comparaDBA'}->dbc->disconnect_when_inactive(1);
+  
+  my $starttime = time();
+  unless (defined $self->input_dir) {
+    $WORKDIR = $self->worker_temp_directory;
+    mkdir("$WORKDIR/input_dir", 0777);
+    $self->input_dir("$WORKDIR/input_dir");
+  } else {
+    if (! -e $self->input_dir) {
+      mkdir($self->input_dir, 0777);
+    }
+  }
+
+  my $dfa = $self->{'comparaDBA'}->get_DnaFragAdaptor;
+  my $gdba = $self->{'comparaDBA'}->get_GenomeDBAdaptor;
+  my $ma = $self->{'comparaDBA'}->get_MemberAdaptor;
+  my $ssa = $self->{'comparaDBA'}->get_SubsetAdaptor;
+
+  foreach my $gdb_id (@{$self->genome_db_ids}) {
+    my $gdb = $gdba->fetch_by_dbID($gdb_id);
+    print "gdb id: ",$gdb->dbID,"\n";
+    my $file = $self->input_dir . "/$gdb_id.chroms";
+    open F, ">$file";
+    foreach my $df (@{$dfa->fetch_all_by_GenomeDB_region($gdb)}) {
+      print F $df->name . "\t" . $df->length,"\n";
+    }
+    close F;
+    my $ss = $ssa->fetch_by_set_description("gdb:".$gdb->dbID ." ". $gdb->name . ' coding exons');
+    print $ss," :",$ss->dbID,"\n";
+    $file = $self->input_dir . "/$gdb_id.anchors";
+    open F, ">$file";
+    foreach my $member (@{$ma->fetch_by_subset_id($ss->dbID)}) {
+      my $strand = "+";
+      $strand = "-" if ($member->chr_strand == -1);
+      print F $member->dbID . "\t" .
+        $member->chr_name ."\t" .
+          $strand . "\t" .
+            ($member->chr_start - 1) ."\t" .
+              $member->chr_end ."\n";
+    }
+    close F;
+  }
+  my $sql = "select qmember_id,hmember_id,score,evalue from peptide_align_feature where qgenome_db_id = ? and hgenome_db_id = ? and hit_rank=1";
+  my $sth = $self->{'comparaDBA'}->dbc->prepare($sql);
+  my ($qmember_id,$hmember_id,$score,$evalue);
+  my @genome_db_ids = @{$self->genome_db_ids};
+  print "old genome_db array: ",$self->genome_db_ids,"\n";
+  print "new genome_db array: ",\@genome_db_ids,"\n";
+  while (my $gdb_id1 = shift @genome_db_ids) {
+    print "old genome_db array after shift: ",$self->genome_db_ids,"\n";
+    foreach my $gdb_id2 (@genome_db_ids) {
+      my $file = $self->input_dir . "/$gdb_id1" . "-$gdb_id2.hits";
+      open F, ">$file";
+      $sth->execute($gdb_id1, $gdb_id2);
+      $sth->bind_columns( \$qmember_id,\$hmember_id,\$score,\$evalue);
+      my %pair_seen = ();
+      while ($sth->fetch()) {
+        next if ($pair_seen{$qmember_id . "_" . $hmember_id});
+        next if (defined $self->cutoff_score && $score < $self->cutoff_score);
+        next if (defined $self->cutoff_evalue && $evalue > $self->cutoff_evalue);
+        print F "$qmember_id\t$hmember_id\t" . int($score). "\t$evalue\n";
+        $pair_seen{$qmember_id . "_" . $hmember_id} = 1;
+      }
+      close F;
+    }
+  }
+
+  if($self->debug){printf("%1.3f secs to dump nib for \"%s\" collection\n", (time()-$starttime), $self->collection_name);}
+
+#  $self->{'comparaDBA'}->dbc->disconnect_when_inactive(0);
+
+  return 1;
+}
+
+
+
+
+
+1;
---
+++ modules/Bio/EnsEMBL/Compara/Production/GenomicAlignBlock/Mlagan.pm
@@ -0,0 +1,319 @@
+#
+# You may distribute this module under the same terms as perl itself
+#
+# POD documentation - main docs before the code
+
+=pod 
+
+=head1 NAME
+
+Bio::EnsEMBL::Compara::Production::GenomicAlignBlock::Mlagan
+
+=cut
+
+=head1 SYNOPSIS
+
+=cut
+
+=head1 DESCRIPTION
+
+
+=cut
+
+=head1 CONTACT
+
+Abel Ureta-Vidal <abel@ebi.ac.uk>
+
+=cut
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object methods. 
+Internal methods are usually preceded with a _
+
+=cut
+
+package Bio::EnsEMBL::Compara::Production::GenomicAlignBlock::Mlagan;
+
+use strict;
+use Bio::EnsEMBL::Analysis::Runnable::Mlagan;
+use Bio::EnsEMBL::Compara::DnaFragRegion;
+use Bio::EnsEMBL::Compara::Production::DBSQL::DBAdaptor;;
+use Bio::EnsEMBL::Utils::Exception;
+use Bio::EnsEMBL::Compara::NestedSet;
+use Bio::EnsEMBL::Compara::Graph::NewickParser;
+use Time::HiRes qw(time gettimeofday tv_interval);
+
+use Bio::EnsEMBL::Hive::Process;
+our @ISA = qw(Bio::EnsEMBL::Hive::Process);
+
+$| = 1;
+
+my $WORKDIR; # global variable holding the path to the working directory where output will be written
+
+=head2 fetch_input
+
+    Title   :   fetch_input
+    Usage   :   $self->fetch_input
+    Function:   Fetches input data for repeatmasker from the database
+    Returns :   none
+    Args    :   none
+
+=cut
+
+sub fetch_input {
+  my( $self) = @_;
+
+  $self->{'comparaDBA'} = Bio::EnsEMBL::Compara::Production::DBSQL::DBAdaptor->new(-DBCONN=>$self->db->dbc);
+
+  $self->get_params($self->parameters);
+  $self->get_params($self->input_id);
+
+
+  return 1;
+}
+
+sub run
+{
+  my $self = shift;
+  $self->dumpFasta;
+
+  my $runnable = new Bio::EnsEMBL::Analysis::Runnable::Mlagan
+    (-workdir => $WORKDIR,
+     -fasta_files => $self->fasta_files,
+     -tree_string => $self->tree_string,
+     -analysis => $self->analysis);
+  $self->{'_runnable'} = $runnable;
+  $runnable->run_analysis;
+#  $self->global_cleanup;
+}
+
+sub write_output {
+  my ($self) = @_;
+
+  my $mlssa = $self->{'comparaDBA'}->get_MethodLinkSpeciesSetAdaptor;
+  my $mlss = $mlssa->fetch_by_dbID($self->method_link_species_set_id);
+  my $gaba = $self->{'comparaDBA'}->get_GenomicAlignBlockAdaptor;
+  foreach my $gab (@{$self->{'_runnable'}->output}) {
+    foreach my $ga (@{$gab->genomic_align_array}) {
+#      $ga->method_link_species_set_id($self->method_link_species_set_id);
+      $ga->method_link_species_set($mlss);
+      my $dfr = $self->{'_dnafrag_regions'}{$ga->dnafrag_id};
+      $ga->dnafrag_id($dfr->dnafrag_id);
+      $ga->dnafrag($dfr->dnafrag);
+      $ga->dnafrag_start($dfr->dnafrag_start);
+      $ga->dnafrag_end($dfr->dnafrag_end);
+      $ga->dnafrag_strand($dfr->dnafrag_strand);
+      $ga->level_id(1);
+      $dfr->release;
+      unless (defined $gab->length) {
+        $gab->length(length($ga->aligned_sequence));
+      }
+    }
+#    $gab->method_link_species_set_id($self->method_link_species_set_id);
+    $gab->method_link_species_set($mlss);
+    $gaba->store($gab);
+  }
+  return 1;
+}
+
+##########################################
+#
+# getter/setter methods
+# 
+##########################################
+
+#sub input_dir {
+#  my $self = shift;
+#  $self->{'_input_dir'} = shift if(@_);
+#  return $self->{'_input_dir'};
+#}
+
+sub synteny_region_id {
+  my $self = shift;
+  $self->{'_synteny_region_id'} = shift if(@_);
+  return $self->{'_synteny_region_id'};
+}
+
+sub fasta_files {
+  my $self = shift;
+
+  $self->{'_fasta_files'} = [] unless (defined $self->{'_fasta_files'});
+
+  if (@_) {
+    my $value = shift;
+    push @{$self->{'_fasta_files'}}, $value;
+  }
+
+  return $self->{'_fasta_files'};
+}
+
+sub tree_file {
+  my $self = shift;
+  $self->{'_tree_file'} = shift if(@_);
+  return $self->{'_tree_file'};
+}
+
+sub tree_string {
+  my $self = shift;
+  $self->{'_tree_string'} = shift if(@_);
+  return $self->{'_tree_string'};
+}
+
+sub parameters {
+  my $self = shift;
+  $self->{'_parameters'} = shift if(@_);
+  return $self->{'_parameters'};
+}
+
+sub method_link_species_set_id {
+  my $self = shift;
+  $self->{'_method_link_species_set_id'} = shift if(@_);
+  return $self->{'_method_link_species_set_id'};
+}
+
+##########################################
+#
+# internal methods
+#
+##########################################
+
+sub get_params {
+  my $self         = shift;
+  my $param_string = shift;
+
+  return unless($param_string);
+  print("parsing parameter string : ",$param_string,"\n");
+
+  my $params = eval($param_string);
+  return unless($params);
+
+  if(defined($params->{'synteny_region_id'})) {
+    $self->synteny_region_id($params->{'synteny_region_id'});
+  }
+  if(defined($params->{'method_link_species_set_id'})) {
+    $self->method_link_species_set_id($params->{'method_link_species_set_id'});
+  }
+  if(defined($params->{'tree_file'})) {
+    $self->tree_file($params->{'tree_file'});
+  }
+
+  return 1;
+}
+
+sub dumpFasta {
+  my $self = shift;
+
+#  $self->{'comparaDBA'}->dbc->disconnect_when_inactive(1);
+
+  $WORKDIR = $self->worker_temp_directory;
+
+  my $sra = $self->{'comparaDBA'}->get_SyntenyRegionAdaptor;
+
+  my $sr = $sra->fetch_by_dbID($self->synteny_region_id);
+
+  my $idx = 1;
+#  print "Dumping1\n";
+  foreach my $dfr (@{$sr->children}) {  
+#    print "Dumping2\n";
+    my $file = $WORKDIR . "/seq" . $idx . ".fa";
+    my $masked_file = $WORKDIR . "/seq" . $idx . ".fa.masked";
+    $idx++;
+
+    open F, ">$file" || throw("Couldn't open $file");
+    open MF, ">$masked_file" || throw("Couldn't open $masked_file");
+
+    # WARNING this is a hack. It won't work at all on self comparisons!!!
+    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    $self->{'_dnafrag_regions'}{$dfr->dnafrag_id} = $dfr;
+    $dfr->retain;
+    $dfr->disavow_parent;
+    my $slice = $dfr->slice;
+    print F ">" . $dfr->dnafrag_id . "\n";
+    print MF ">" . $dfr->dnafrag_id . "\n";
+    my $seq = $slice->seq;
+    $seq =~ s/(.{80})/$1\n/g;
+    chomp $seq;
+    print F $seq,"\n";
+    $seq = $slice->get_repeatmasked_seq->seq;
+    $seq =~ s/(.{80})/$1\n/g;
+    chomp $seq;
+    print MF $seq,"\n";
+  
+    close F;
+    close MF;
+
+    push @{$self->fasta_files}, $file;
+  }
+#  print "Dumping3\n";
+#  $self->{'comparaDBA'}->dbc->disconnect_when_inactive(0);
+  $sr->release_tree;
+#  print "Dumping4\n";
+  if ($self->tree_file) {
+    $self->dumpTreeFile;
+  }
+  return 1;
+}
+
+sub dumpTreeFile {
+  my $self = shift;
+  my $tree_file = $self->tree_file;
+  open F, $tree_file || throw("Can not open $tree_file");
+  my $newick = "";
+  while (<F>) {
+    chomp;
+    if (/^\s*(.*)\s*$/) {
+      $newick .= $1;
+    }
+  }
+  close F;
+  print "newick: ",$newick,"\n";
+  my $tree = Bio::EnsEMBL::Compara::Graph::NewickParser::parse_newick_into_tree($newick);
+  print "tree_string1: ",$tree->newick_simple_format,"\n";
+  $tree->print_tree;
+  $self->update_node_names($tree);
+  $tree_file = $WORKDIR . "/tree_file";
+  open F, ">$tree_file";
+  print F $tree->newick_simple_format,"\n";
+  close F;
+  $tree->print_tree;
+  my $tree_string = $tree->newick_simple_format;
+  print "tree_string2: $tree_string\n";
+  $tree_string =~ s/:\d+\.\d+//g;
+  $tree_string =~ s/[,;]/ /g;
+  print "tree_string3: $tree_string\n";
+  $self->tree_string($tree_string);
+  $tree->release_tree;
+  $self->tree_file($tree_file);
+#  die;
+  return $tree_file;
+}
+
+sub update_node_names {
+  my $self = shift;
+  my $tree = shift;
+  my %gdb_id2dfr;
+  foreach my $dfr (values %{$self->{'_dnafrag_regions'}}) {
+    $gdb_id2dfr{$dfr->dnafrag->genome_db->dbID} = $dfr->dnafrag_id;
+  }
+
+  foreach my $leaf (@{$tree->get_all_leaves}) {
+    if (defined $gdb_id2dfr{$leaf->name}) {
+      $leaf->name($gdb_id2dfr{$leaf->name});
+    } else {
+#      $leaf->parent->remove_child($leaf);
+#      $leaf->parent->retain->remove_child($leaf);
+      $leaf->disavow_parent;
+      $tree->minimize_tree;
+    }
+  }
+#  $tree->minimize_tree;
+  if ($tree->get_child_count == 1) {
+    my $child = $tree->children->[0];
+    $child->parent->merge_children($child);
+    $child->disavow_parent;
+  }
+  return $tree;
+}
+
+1;
