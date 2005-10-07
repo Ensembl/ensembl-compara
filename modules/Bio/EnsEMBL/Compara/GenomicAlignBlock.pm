022c201c474317d3e35ce473f40c5e8e413db565
Javier Herrero <jh7@sanger.ac.uk>
2005-10-07 13:44:55 +0000

    Bug fix: Restrict correctly the GenomicAlignBlock when the reference GenomicAlign is defined in the reverse strand.

--- modules/Bio/EnsEMBL/Compara/GenomicAlignBlock.pm
+++ modules/Bio/EnsEMBL/Compara/GenomicAlignBlock.pm
@@ -813,8 +813,8 @@ sub length {
     if (defined($self->{'adaptor'}) and defined($self->dbID)) {
       # ...from the database, using the dbID of the Bio::Ensembl::Compara::GenomicAlignBlock object
       $self->adaptor->retrieve_all_direct_attributes($self);
-    } elsif (@{$self->get_all_GenomicAligns} and $self->get_all_GenomicAligns->[0]->aligned_sequence) {
-      $self->{'length'} = CORE::length($self->get_all_GenomicAligns->[0]->aligned_sequence);
+    } elsif (@{$self->get_all_GenomicAligns} and $self->get_all_GenomicAligns->[0]->aligned_sequence("+FAKE_SEQ")) {
+      $self->{'length'} = CORE::length($self->get_all_GenomicAligns->[0]->aligned_sequence("+FAKE_SEQ"));
     }
   }
   
@@ -1404,6 +1404,10 @@ sub restrict_between_reference_positions {
   ## Skip if no restriction is needed. Return original object!
   return $self if ($excess_at_the_start < 0 and $excess_at_the_end < 0);
 
+  my $negative_strand = ($reference_genomic_align->dnafrag_strand == -1);
+  $self->reverse_complement() if ($negative_strand);
+
+  ## Create a new Bio::EnsEMBL::Compara::GenomicAlignBlock object
   foreach my $this_genomic_align (@{$self->get_all_GenomicAligns}) {
     my $new_genomic_align = new Bio::EnsEMBL::Compara::GenomicAlign(
           -method_link_species_set => $this_genomic_align->method_link_species_set,
@@ -1535,6 +1539,8 @@ sub restrict_between_reference_positions {
       }
     }
   }
+
+  $self->reverse_complement() if ($negative_strand);
 
   throw("Reference GenomicAlign not found!") if (!$genomic_align_block->get_all_GenomicAligns->[0]);
   return $genomic_align_block;
