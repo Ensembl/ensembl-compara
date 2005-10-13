22949594232760511bffb556a87c052c0b141463
Web Admin <w3adm@sanger.ac.uk>
2005-10-13 16:23:13 +0000

    patterncolour support rmp

--- modules/Bio/EnsEMBL/Glyph/Symbol/box.pm
+++ modules/Bio/EnsEMBL/Glyph/Symbol/box.pm
@@ -17,31 +17,32 @@ use Bio::EnsEMBL::Glyph::Symbol;
 @ISA = qw(Bio::EnsEMBL::Glyph::Symbol);
 
 sub draw {
-    my $self = shift;
-    my $style = $self->style;
-    my $feature = $self->feature;
+    my $self       = shift;
+    my $style      = $self->style;
+    my $featur e   = $self->feature;
 
-    my $rowheight = $feature->{'row_height'};
-    my $start = $feature->{'start'};
-    my $end = $feature->{'end'};
+    my $rowheight  = $feature->{'row_height'};
+    my $start      = $feature->{'start'};
+    my $end        = $feature->{'end'};
     my $pix_per_bp = $feature->{'pix_per_bp'};
-    my $y_offset = $feature->{'y_offset'};
+    my $y_offset   = $feature->{'y_offset'};
    
     my $linecolour = $style->{'fgcolor'};
     my $fillcolour = $style->{'bgcolor'} || $style->{'colour'};
-    $linecolour ||= $fillcolour;
+    $linecolour  ||= $fillcolour;
 
-    my $height = $style->{'height'};
+    my $height     = $style->{'height'};
 
     return new Sanger::Graphics::Glyph::Rect({
-    	'x'          => $start-1,
-	'y'          => $y_offset,
-	'width'      => $end-$start+1,
-	'height'     => $height,
-	'colour'     => $fillcolour,
-	'bordercolour' => $linecolour,
-	'absolutey' => 1,
-	(exists($style->{pattern}) ? (pattern=>$style->{pattern}) : ()),
+    	'x'             => $start-1,
+	'y'             => $y_offset,
+	'width'         => $end-$start+1,
+	'height'        => $height,
+	'colour'        => $fillcolour,
+	'bordercolour'  => $linecolour,
+	'absolutey'     => 1,
+	'patterncolour' => $linecolour,
+ 	(exists($style->{pattern}) ? (pattern=>$style->{pattern}) : ()),
     });
 }
 
