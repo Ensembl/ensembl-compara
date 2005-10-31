f134349e06b358601aefe9c9d064259a469de4ea
Web Admin <w3adm@sanger.ac.uk>
2005-10-31 16:02:10 +0000

    ciona stuff

---
+++ modules/Bio/EnsEMBL/GlyphSet/mouse_protein_transcript.pm
@@ -0,0 +1,61 @@
+package Bio::EnsEMBL::GlyphSet::mouse_protein_transcript;
+use strict;
+use vars qw(@ISA);
+use Bio::EnsEMBL::GlyphSet_transcript;
+@ISA = qw(Bio::EnsEMBL::GlyphSet_transcript);
+
+sub my_label {
+    my $self = shift;
+    return "Mouse proteins";
+}
+
+sub colours {
+    my $self = shift;
+    my $Config = $self->{'config'};
+    return $Config->get('mouse_protein_transcript','colours');
+}
+
+sub transcript_type {
+  my $self = shift;
+  return 'mouse_protein';
+}
+
+sub colour {
+    my ($self, $gene, $transcript, $colours, %highlights) = @_;
+    my $colour = $colours->{'_col'}[0];
+    my $highlight;
+    if( exists $highlights{lc($gene->stable_id)} ){
+        $highlight = $colours->{'hi'};
+    }
+    return ( $colour, $highlight );
+}
+
+sub href {
+    my ($self, $gene, $transcript) = @_;
+    return $self->ID_URL( 'UNIPROT', $gene->stable_id );
+}
+
+sub features {
+  my ($self) = @_;
+  return $self->{'container'}->get_all_Genes_by_type('mouse_protein');
+}
+
+sub zmenu {
+    my ($self, $gene, $transcript) = @_;
+    my $pid  = $transcript->stable_id();
+    return {
+        'caption'  => "Mouse protein",
+        $pid       => $self->href( $gene, $transcript )
+    };
+}
+
+sub text_label {
+    my ($self, $gene, $transcript) = @_;
+    return $transcript->stable_id();
+}
+
+sub legend { return ; }
+
+sub error_track_name { return 'Mouse proteins'; }
+
+1;
