2f2f2520561f7959abe0752df3e5f16092c199fa
Anne Parker <ap5@sanger.ac.uk>
2005-11-14 16:16:45 +0000

    First pass at POD

--- modules/EnsEMBL/Web/DBSQL/NewsAdaptor.pm
+++ modules/EnsEMBL/Web/DBSQL/NewsAdaptor.pm
@@ -653,3 +653,159 @@ sub add_release_species {
 
 1;
 
+__END__
+                                                                                
+=head1 Ensembl::Web::DBSQL::NewsAdaptor
+                                                                                
+=head2 SYNOPSIS
+                                                                                
+This package is called by a data object which needs to connect to the news tables in the ensembl_website database. E.g.
+                                                                                
+    use EnsEMBL::Web::DBSQL::NewsAdaptor;
+
+    my $DB = $self->species_defs->databases->{'ENSEMBL_WEBSITE'};
+    $self->__data->{'news_db'} = EnsEMBL::Web::DBSQL::NewsAdaptor->new( $DB );
+                                                                                
+
+=head2 DESCRIPTION
+                                                                                
+This class consists of methods for querying the ensembl_website database. This includes SELECT, INSERT and UPDATE queries, since the adaptor is used with a db admin interface as well as to retrieve news items for display.
+                                                                                
+=head2 METHODS
+
+                                                                                
+=head3 B<new>
+                                                                                
+Description: Constructor method
+
+Arguments:    
+                                                                                
+Returns: EnsEMBL::Web::DBSQL::NewsAdaptor object
+
+=head3 B<db>
+                                                                                
+Description: Connects to the required database using the normal (generally read-only) user
+
+Arguments:     
+                                                                                
+Returns:  
+
+=head3 B<db_write>
+                                                                                
+Description: Connects to the required database using a user with write privileges (assuming this is correctly configured in the ini file!)
+
+Arguments:     
+                                                                                
+Returns:  
+
+=head3 B<fetch_items>
+                                                                                
+Description: SELECT query which can take a number of modifying arguments
+
+Arguments: a reference to a hash of query modifiers, with the following elements (all optional):
+    $options = {
+      'criteria'=>{hash of fields and their values},
+      'order_by'=>[fields by which to order the results],
+      'limit'=>integer
+    };     
+                                                                                
+Returns: a reference to an array of hashes containing individual news stories. Note that the database stores a newsitem-to-species cross-reference only if a story applies to a limited number of species; stories relevant to all species have no cross-reference. Thus, queries for 'stories relevant to a given species' must be done in two steps: first stories with cross-references that match the species ID, then stories that have no cross-references at all. The user may thus need to re-sort the results after querying, if this is not the desired order.
+
+=head3 B<fetch_releases>
+                                                                                
+Description: SELECT query
+
+Arguments: a reference to a hash of form {'release'=>ID1, 'species'=>ID2} - both values are optional    
+                                                                                
+Returns:  a reference to a hash containing one or more release records. Each record contains the release ID, the release number, the full date [2005-12-31 (31stDecember 2005)] and the short date [Dec 2005]. 
+
+=head3 B<fetch_species>
+                                                                                
+Description: SELECT query
+
+Arguments: release ID (optional)    
+                                                                                
+Returns: a reference to a hash of species IDs and species names
+
+=head3 B<fetch_cats>
+                                                                                
+Description: SELECT query
+
+Arguments: none
+                                                                                
+Returns: a reference to an array of hashes contatining category IDs, codes and names, ordered by the 'priority' field in the database
+
+=head3 B<fetch_assemblies>
+                                                                                
+Description: Used by the archive pages to get assembly names for species
+
+Arguments: release ID (integer)
+                                                                                
+Returns: a reference to an array of hashes, each of form {'species'=>string, 'assembly_name'=>string}
+
+=head3 B<add_news_item>
+                                                                                
+Description: INSERT query which adds a news record to the database
+
+Arguments: a reference to a hash containing the database fields and values to be added    
+                                                                                
+Returns: none
+
+=head3 B<update_news_item>
+                                                                                
+Description: UPDATE query for a news record
+
+Arguments:  a reference to a hash containing the database fields and values to be updated       
+                                                                                
+Returns:  none
+
+=head3 B<add_release>
+                                                                                
+Description: INSERT query for a release record
+
+Arguments: a reference to a hash containing the database fields and values to be added        
+                                                                                
+Returns:  none
+
+=head3 B<set_release_date>
+                                                                                
+Description: UPDATE query - used to reset release date when it slips
+
+Arguments: release ID, date (in yyyy-mm-dd format)    
+                                                                                
+Returns:  none
+
+=head3 B<add_species>
+                                                                                
+Description: INSERT query for a species record (e.g. from an ini file). The handler checks if the species is already listed in the database
+
+Arguments: a reference to a record hash ([scientific] name, common name, [2-letter] code)
+                                                                                
+Returns:  ID for new record (integer)
+
+=head3 B<add_release_species>
+                                                                                
+Description: INSERT query - adds a record to the release_species cross-reference table. Can be used as part of a db update script to add all current species to a new release
+
+Arguments:  a reference to a record hash holding the release ID, species ID, species code [Ensembl internal Golden Path ID] and assembly name
+                                                                                
+Returns:  a status message (string) 
+
+
+=head2 BUGS AND LIMITATIONS
+                                                                                
+None known at present.
+                                                                                                                                                              
+=head2 AUTHOR
+                                                                                
+Anne Parker, Ensembl Web Team
+Support enquiries: helpdesk\@ensembl.org
+                                                                                
+=head2 COPYRIGHT
+                                                                                
+See http://www.ensembl.org/info/about/code_licence.html
+                                                                                
+=cut
+
+
+
