43b3f09c79c2b1c1ff053caa466f53009d08e70a
Anne Parker <ap5@sanger.ac.uk>
2005-11-23 15:47:06 +0000

    New form element to add an H3 header

---
+++ modules/EnsEMBL/Web/Form/Element/Header.pm
@@ -0,0 +1,13 @@
+package EnsEMBL::Web::Form::Element::Header;
+
+use EnsEMBL::Web::Form::Element;
+our @ISA = qw( EnsEMBL::Web::Form::Element );
+
+sub new {
+  my $class = shift;
+  return $class->SUPER::new( @_, 'spanning' => 'yes' );
+}
+
+sub render { return '<h3>'.$_[0]->value.'</h3>'; }
+
+1;
