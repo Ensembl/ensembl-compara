9a813d153dae8d1d527bebe1a78239b4dc6e29c4
Eugene Kulesha <ek@ebi.ac.uk>
2005-11-25 16:28:45 +0000

    new align compara menu

--- modules/EnsEMBL/Web/Document/DropDown/Menu/AlignCompara.pm
+++ modules/EnsEMBL/Web/Document/DropDown/Menu/AlignCompara.pm
@@ -11,25 +11,38 @@ sub new {
     @_, ## This contains the menu containers as the first element
     'image_name'  => 'y-compara',
     'image_width' => 88,
-    'alt'         => 'Compara'
+    'alt'         => 'Multiple Alignments'
   );
-  my @menu_entries = @{$self->{'config'}->get('_settings','aligncompara')||[]};
-
-  my $LINK = sprintf qq(/%s/%s?%s), $self->{'species'}, $self->{'script'}, $self->{'LINK'};
-
   
-  # Find one that is active;
-  my $active_option = $self->{'config'}->get('aligncompara', 'align_species');
+  my $species = $ENV{ENSEMBL_SPECIES};
 
-  foreach my $me ( @menu_entries ) {
-      my ($option, $label) = @$me;
-      my $link = "$LINK&align=$option";
-      if ($option eq $active_option) {
-	  $label = "* $label";
-      }
-      $self->add_link( $label, $link, '' );
+### Get all MLAGAN alignment sets
+### It is understood that all set ids will be like MLAGAN-XXX
+  my @mlagan_alignments = grep { /MLAGAN-/ } keys %{$self->{'config'}->{'species_defs'}->{'_multi'}};
+
+  if (@mlagan_alignments) {
+      $self->add_text("Multiple Alignments");
   }
 
+  foreach my $align (@mlagan_alignments) {
+      my $h = $self->{'config'}->{'species_defs'}->{'_multi'}->{$align};
+      $self->add_radiobutton( "opt_alignm_$align", $align );
+
+      foreach my $sp (sort keys %$h ) { 
+	  $self->add_checkbox( "opt_${align}_$sp", $sp ) if ($sp ne $species);
+      }
+      $self->add_text(" ");
+  }
+
+  my $align = 'BLASTZ_NET';
+  my $h = $self->{'config'}->{'species_defs'}->{'_multi'}->{$align}->{$species};
+  $self->add_text("Pairwise Alignments");  
+
+  my @species = sort keys %$h ;
+  foreach my $label ( @species ) {
+      $self->add_radiobutton( "opt_alignp_${align}_$label", $label) 
+  }
+  
   return $self;
 }
 
--- modules/EnsEMBL/Web/ScriptConfig/alignsliceview.pm
+++ modules/EnsEMBL/Web/ScriptConfig/alignsliceview.pm
@@ -14,6 +14,27 @@ sub init {
     zoom_width     100
     image_width    700
     context    100000
+    opt_MLAGAN-167_Homo_sapiens on
+    opt_MLAGAN-167_Canis_familiaris on
+    opt_MLAGAN-167_Mus_musculus on
+    opt_MLAGAN-167_Rattus_norvegicus on
+    opt_MLAGAN-190_Homo_sapiens on
+    opt_MLAGAN-190_Canis_familiaris on
+    opt_MLAGAN-190_Mus_musculus on
+    opt_MLAGAN-190_Rattus_norvegicus on
+    opt_MLAGAN-190_Gallus_gallus on
+    opt_MLAGAN-190_Bos_taurus on
+    opt_MLAGAN-190_Monodelphis_domestica on
+    opt_alignm_MLAGAN-167 on
+    opt_alignm_MLAGAN-190 off
+    opt_alignp_BLASTZ_NET_Homo_sapiens off
+    opt_alignp_BLASTZ_NET_Canis_familiaris off
+    opt_alignp_BLASTZ_NET_Mus_musculus off
+    opt_alignp_BLASTZ_NET_Rattus_norvegicus off
+    opt_alignp_BLASTZ_NET_Gallus_gallus off
+    opt_alignp_BLASTZ_NET_Bos_taurus off
+    opt_alignp_BLASTZ_NET_Pan_troglodytes off
+
   ));
 }
 1;
