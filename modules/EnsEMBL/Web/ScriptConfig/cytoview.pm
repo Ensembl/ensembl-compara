9074991b73a8a3b43c90a75f4f4d9d226d9ffacf
Web Admin <w3adm@sanger.ac.uk>
2005-12-06 07:23:42 +0000

    added gene labels flag

--- modules/EnsEMBL/Web/ScriptConfig/cytoview.pm
+++ modules/EnsEMBL/Web/ScriptConfig/cytoview.pm
@@ -7,6 +7,7 @@ sub init {
   my ($script_config ) = @_;
 
   $script_config->_set_defaults(qw(
+    opt_gene_labels   on
     panel_ideogram    on
     panel_bottom      on
     image_width      700
