b2ab369f255a7b7477828452ca0024c1293738cd
Abel Ureta-Vidal <abel@sanger.ac.uk>
2005-12-13 16:38:06 +0000

    fix the schema_version in the meta table

--- sql/patch_35_36.sql
+++ sql/patch_35_36.sql
@@ -1,1 +1,3 @@
 alter table genomic_align_group change group_id  group_id bigint(20) unsigned NOT NULL auto_increment;
+delete from meta where meta_key="schema_version";
+insert into meta (meta_key,meta_value) values ("schema_version",36);
