6fa3da43daf7c2d8579183fd457cc9281f54bd52
Eugene Kulesha <ek@ebi.ac.uk>
2006-01-12 14:14:16 +0000

    added two new elements for the web forms

---
+++ modules/EnsEMBL/Web/Form/Element/RadioButton.pm
@@ -0,0 +1,35 @@
+package EnsEMBL::Web::Form::Element::RadioButton;
+
+use EnsEMBL::Web::Form::Element;
+use CGI qw(escapeHTML);
+our @ISA = qw( EnsEMBL::Web::Form::Element );
+
+sub new {
+  my $class = shift;
+  return $class->SUPER::new( @_ );
+}
+                                                                                
+sub new {
+  my $class = shift;
+  my %params = @_;
+  my $self = $class->SUPER::new(
+    %params,
+  );
+  $self->checked = $params{'checked'};
+  return $self;
+}
+sub checked  :lvalue { $_[0]->{'checked'};  }
+
+sub render {
+  my $self = shift;
+  return sprintf(
+    qq(<input type="radio" name="%s" id="%s" value="%s" %s/>),
+    CGI::escapeHTML( $self->name ), CGI::escapeHTML( $self->id ),
+    $self->value || 'yes', $self->checked ? 'checked="checked" ' : ''
+  );
+}
+                                                                                
+sub validate { return 1; }
+
+
+1;
---
+++ modules/EnsEMBL/Web/Form/Element/RadioGroup.pm
@@ -0,0 +1,47 @@
+package EnsEMBL::Web::Form::Element::RadioGroup;
+
+use EnsEMBL::Web::Form::Element;
+use CGI qw(escapeHTML);
+our @ISA = qw( EnsEMBL::Web::Form::Element );
+
+sub new {
+  my $class = shift;
+  my %params = @_;
+  my $self = $class->SUPER::new( %params, 'render_as' => $params{'select'} ? 'select' : 'radiobutton', 'values' => $params{'values'} );
+  $self->{'class'} = $params{'class'} || 'radiocheck';
+  return $self;
+}
+
+sub validate { return $_[0]->render_as eq 'select'; }
+
+sub render {
+  my $self =shift;
+
+  my $output = '';
+  my $K = 0;
+  my $checked;
+  foreach my $V ( @{$self->values} ) {
+      $checked = 'no';
+      # check if we want to tick this box
+      foreach my $M ( @{$self->value||[]} ) {
+	  if ($M eq $$V{'value'}) {
+	      $checked = 'yes';
+	      last;
+	  }
+      }
+      if ($V->{'checked'}) {
+	  $checked = 'yes';
+      }
+      $output .= sprintf( "    <div class=\"%s\"><input id=\"%s_%d\" class=\"radio\" type=\"radio\" name=\"%s\" value=\"%s\" %s/><label for=\"%s_%d\">%s</label></div>\n",
+			  $self->{'class'},
+			  CGI::escapeHTML($self->id), $K, CGI::escapeHTML($self->name), CGI::escapeHTML($V->{'value'}),
+			  $checked eq 'yes' ? ' checked="checked"' : '', CGI::escapeHTML($self->id), $K,
+			  CGI::escapeHTML($V->{'name'})
+			  );
+      $K++;
+  }
+  return $self->introduction.$output.$self->notes;
+
+}
+
+1;
