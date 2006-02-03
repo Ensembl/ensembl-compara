171cc6e3df8913583df0eab303b39447d169afe6
Steve Trevanion <st3@sanger.ac.uk>
2006-02-03 14:09:06 +0000

    lower case and black

--- modules/Bio/EnsEMBL/GlyphSet/gene_legend.pm
+++ modules/Bio/EnsEMBL/GlyphSet/gene_legend.pm
@@ -43,7 +43,7 @@ sub _init {
 
     my $vc            = $self->{'container'};
     my $Config        = $self->{'config'};
-    my $FONTNAME      = $Config->species_defs->ENSEMBL_STYLE->{'LABEL_FONT'};
+    my $FONTNAME      = 'Small';
     my ($w,$th)       = $Config->texthelper()->px2bp($FONTNAME);
 
     my $im_width      = $Config->image_width();
@@ -85,8 +85,8 @@ sub _init {
                 'y'         => $y * ( $th + 3 ) + 4,
                 'height'    => $Config->texthelper->height($FONTNAME),
                 'font'      => $FONTNAME,
-                'colour'    => $colour,
-                'text'      => uc(" $legend"),
+                'colour'    => 'black',
+                'text'      => " $legend",
                 'absolutey' => 1,
                 'absolutex' => 1,'absolutewidth'=>1,
             }));
