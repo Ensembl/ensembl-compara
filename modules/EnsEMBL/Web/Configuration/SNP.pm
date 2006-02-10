481a8f930221036d6a1b58a33313036931edfe30
Fiona Cunningham <fiona@ebi.ac.uk>
2006-02-10 12:22:49 +0000

    added filtering by source

--- modules/EnsEMBL/Web/Configuration/SNP.pm
+++ modules/EnsEMBL/Web/Configuration/SNP.pm
@@ -8,14 +8,14 @@ our @ISA = qw( EnsEMBL::Web::Configuration );
 
 sub snpview {
   my $self   = shift;
-
-  my $params = { 'snp' => $self->{object}->name };
-     $params->{'c'} =  $self->{object}->param('c') if  $self->{object}->param('c');
-     $params->{'w'} =  $self->{object}->param('w') if  $self->{object}->param('w');
-     $params->{'source'} =  $self->{object}->param('source') if  $self->{object}->param('source');
+  my $obj    = $self->{'object'}; 
+  my $params = { 'snp' => $obj->name };
+     $params->{'c'} =  $obj->param('c') if  $obj->param('c');
+     $params->{'w'} =  $obj->param('w') if  $obj->param('w');
+     $params->{'source'} =  $obj->param('source') if  $obj->param('source');
      
   my @params = (
-    'object' => $self->{object},
+    'object' => $obj,
     'params' => $params
   );
 
@@ -46,7 +46,7 @@ if (
     'caption' => "Genotype frequencies per population",
     @params,
     'status'  => 'panel_genotypes',
-    'null_data' => '<p>This SNP has not been genotyped</p>'
+    'null_data' => '<p>This SNP has not been genotyped in a population.</p>'
 				      )) {
 
   $genotype_panel->add_components( qw(genotype_freqs EnsEMBL::Web::Component::SNP::genotype_freqs) );
@@ -60,7 +60,7 @@ if (
     'caption' => "Allele frequencies per population",
     @params,
     'status'  => 'panel_alleles',
-    'null_data' => '<p>This SNP has not been genotyped</p>'
+    'null_data' => '<p>This SNP has no allele frequences per population.</p>'
 				    )) {
   $allele_panel->add_components( qw(allele_freqs EnsEMBL::Web::Component::SNP::allele_freqs)  );
   $self->{page}->content->add_panel( $allele_panel );
@@ -70,7 +70,7 @@ if (
 if ( 
 my $mapping_panel = $self->new_panel('SpreadSheet',
     'code'    => "mappings $self->{flag}",
-    'caption' => "SNP ". $self->{object}->name." is located in the following transcripts",
+    'caption' => "SNP ". $obj->name." is located in the following transcripts",
      @params,
     'status'  => 'panel_locations',
     'null_data' => '<p>There are no transcripts that contain this SNP.</p>'
@@ -81,15 +81,40 @@ my $mapping_panel = $self->new_panel('SpreadSheet',
 
 # Neighbourhood image -------------------------------------------------------
   ## Now create the image panel
-  my @context = $self->{object}->seq_region_data;
+  my @context = $obj->seq_region_data;
   if (my $image_panel = $self->new_panel('Image',
      'code'    => "image_$self->{flag}",
      'caption' => "SNP Context - $context[-1] $context[0] $context[1]",
      'status'  => 'panel_bottom',  @params,
 					)) {
 
+ # Set default sources--------------
+ my @sources = keys %{ $obj->species_defs->VARIATION_SOURCES || {} } ;
+ my $default_source = $obj->get_source("default");;
+ my $script_config = $obj->get_scriptconfig();
+ my $restore_default = 1;
+
+ $self->update_configs_from_parameter( 'snpview', 'snpview' );
+ foreach my $source ( @sources ) {
+   $restore_default = 0 if $script_config->get(lc("opt_$source") ) eq 'on';
+ }
+
+ if( $restore_default ) { # if none of species' sources are on
+   foreach my $source ( @sources ) {
+     my $switch;
+     if ($default_source) {
+       $switch = $source eq $default_source ? 'on' : 'off' ;
+     }
+     else {
+       $switch = 'on';
+     }
+     $script_config->set(lc("opt_$source"), $switch, 1);
+   }
+ }#--------- end source
+
+
   $self->update_configs_from_parameter( 'snpview', 'snpview' );
-  if( $self->{object}->seq_region_data ) {
+  if( $obj->seq_region_data ) {
     ## Initialize the javascript for the zmenus and dropdown menus
     $self->initialize_zmenu_javascript;
     $self->initialize_ddmenu_javascript;
@@ -109,7 +134,7 @@ my $mapping_panel = $self->new_panel('SpreadSheet',
 if (
   my $individual_panel = $self->new_panel('SpreadSheet',
     'code'    => "individual $self->{flag}",
-    'caption' => "Individual genotypes for SNP ". $self->{object}->name,
+    'caption' => "Individual genotypes for SNP ". $obj->name,
      @params,
     'status'  => 'panel_individual',
 					 )) {
@@ -120,7 +145,7 @@ if (
 
 sub context_menu {
   my $self = shift;
-  my $obj  = $self->{object};
+  my $obj  = $self->{'object'};
   my $species = $obj->species;
   my $name = $obj->name;
   $self->add_block( "snp$self->{flag}", 'bulleted',
@@ -138,8 +163,8 @@ sub context_menu {
   }
   
   my $snpview_href = "/$species/snpview?snp=$name";
-  if ($self->{object}->param('source')) {
-    $snpview_href .= ';source='.$self->{object}->param('source');
+  if ($obj->param('source')) {
+    $snpview_href .= ';source='.$obj->param('source');
   }
   $self->add_entry(
         "snp$self->{flag}",
