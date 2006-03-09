c4af385edc2f4da485cc70702c1449c964844d46
Javier Herrero <jh7@sanger.ac.uk>
2006-03-09 12:35:49 +0000

    Change: Use Bio::EnsEMBL::Analysis::Runnable::BlastDB instead of the deprecated Bio::EnsEMBL::Pipeline::Runnable::BlastDB

--- modules/Bio/EnsEMBL/Compara/RunnableDB/BlastComparaPep.pm
+++ modules/Bio/EnsEMBL/Compara/RunnableDB/BlastComparaPep.pm
@@ -27,10 +27,10 @@ $repmask->write_output(); #writes to DB
 
 =head1 DESCRIPTION
 
-This object wraps Bio::EnsEMBL::Pipeline::Runnable::Blast to add
+This object wraps Bio::EnsEMBL::Analysis::Runnable::Blast to add
 functionality to read and write to databases.
 The appropriate Bio::EnsEMBL::Analysis object must be passed for
-extraction of appropriate parameters. A Bio::EnsEMBL::Pipeline::DBSQL::Obj is
+extraction of appropriate parameters. A Bio::EnsEMBL::Analysis::DBSQL::Obj is
 required for databse access.
 
 =cut
@@ -52,17 +52,31 @@ Internal methods are usually preceded with a _
 
 package Bio::EnsEMBL::Compara::RunnableDB::BlastComparaPep;
 
+# use strict;
+# 
+# use Bio::EnsEMBL::Analysis::Runnable;
+# use Bio::EnsEMBL::Analysis::RunnableDB;
+# use Bio::EnsEMBL::Analysis::Runnable::Blast;
+# use Bio::EnsEMBL::Analysis::RunnableDB::Blast;
+# use Bio::EnsEMBL::Compara::DBSQL::PeptideAlignFeatureAdaptor;
+# use Bio::EnsEMBL::Compara::Member;
+# use Bio::EnsEMBL::Utils::Exception;
 use strict;
+use warnings;
 
-use Bio::EnsEMBL::DBSQL::DBAdaptor;
-use Bio::EnsEMBL::Pipeline::RunnableDB;
-use Bio::EnsEMBL::Pipeline::Runnable::Blast;
-use Bio::EnsEMBL::Pipeline::Runnable::BlastDB;
+use Bio::EnsEMBL::Utils::Exception qw(throw warning);
 use Bio::EnsEMBL::Compara::DBSQL::DBAdaptor;
-use Bio::EnsEMBL::Compara::DBSQL::PeptideAlignFeatureAdaptor;
-use Bio::EnsEMBL::Compara::Member;
+use Bio::EnsEMBL::Analysis::Runnable;
+use Bio::EnsEMBL::Analysis::Runnable::Blast;
+use Bio::EnsEMBL::Analysis::Tools::BPliteWrapper;
+use Bio::EnsEMBL::Analysis::Tools::FilterBPlite;
+use Bio::EnsEMBL::Hive::Process;
+use vars qw(@ISA);
 
-our @ISA = qw(Bio::EnsEMBL::Pipeline::RunnableDB);
+@ISA = qw(Bio::EnsEMBL::Hive::Process);
+
+# 
+# our @ISA = qw(Bio::EnsEMBL::Analysis::RunnableDB::Blast);
 
 my $g_BlastComparaPep_workdir;
 
@@ -77,90 +91,108 @@ my $g_BlastComparaPep_workdir;
 sub fetch_input {
   my( $self) = @_;
 
-  $self->throw("No input_id") unless defined($self->input_id);
+  throw("No input_id") unless defined($self->input_id);
 
-  #create a Compara::DBAdaptor which shares the same DBI handle
-  #with the Pipeline::DBAdaptor that is based into this runnable
+  ## Get the query (corresponds to the member with a member_id = input_id
   $self->{'comparaDBA'} = Bio::EnsEMBL::Compara::DBSQL::DBAdaptor->new(-DBCONN=>$self->db->dbc);
   $self->{'comparaDBA'}->dbc->disconnect_when_inactive(0);
+  my $member_id = $self->input_id;
+  my $member = $self->{'comparaDBA'}->get_MemberAdaptor->fetch_by_dbID($member_id);
+  throw("No member in compara for member_id = $member_id") unless defined($member);
+  my $query = $member->bioseq();
+  throw("Unable to make bioseq for member_id = $member_id") unless defined($query);
 
+  ## Get the db_file (defined in the analysis)
+  my $dbfile = $self->analysis->db_file;
 
-  my $member_id  = $self->input_id;
-  my $member     = $self->{'comparaDBA'}->get_MemberAdaptor->fetch_by_dbID($member_id);
-  $self->throw("No member in compara for member_id=$member_id") unless defined($member);
-  #$member->print_member;
-
-  
-  my $bioseq     = $member->bioseq();
-  $self->throw("Unable to make bioseq for member_id=$member_id") unless defined($bioseq);
-  $self->query($bioseq);
-
+  ## Define the filter from the parameters
   my ($thr, $thr_type, $options);
   my $p = eval($self->analysis->parameters);
 
   if (defined $p->{'-threshold'} && defined $p->{'-threshold_type'}) {
       $thr      = $p->{-threshold};
       $thr_type = $p->{-threshold_type};
-  }
-  else {
+  } else {
       $thr_type = 'PVALUE';
       $thr      = 1e-10;
   }
+
   if (defined $p->{'options'}) {
     $options = $p->{'options'};
-    # print("!!!found my options : $options\n");
-  }
-  else {
+  } else {
     $options = '';
   }
 
-  my $dbfile = $self->analysis->db_file;
-#  $dbfile = $self->dumpPeptidesToFasta();
-  
+  ## Create a parser object. This Bio::EnsEMBL::Analysis::Tools::FilterBPlite
+  ## object wraps the Bio::EnsEMBL::Analysis::Tools::BPliteWrapper which in
+  ## turn wraps the Bio::EnsEMBL::Analysis::Tools::BPlite (a port of Ian
+  ## Korf's BPlite from bioperl 0.7 into ensembl). This parser also filter
+  ## the results according to threshold_type and threshold.
+  my $parser = Bio::EnsEMBL::Analysis::Tools::FilterBPlite->new(
+          -regex => '^(\S+)\s*',
+          -query_type => "pep",
+          -input_type => "pep",
+          -threshold_type => $thr_type,
+          -threshold => $thr,
+      );
 
-=head3
-  my $stable_id  = $member->stable_id();
-  my $logic_name = $self->analysis->logic_name();
-  print("BlastComparaPep query='$stable_id'  anal='$logic_name'\n");
-  my $seq_string = $member->sequence();
-  print("  seq : $seq_string\n");
-  my $options = $self->analysis->parameters;
-  print("  option = '$options'\n");
-
-  my $runnable = new Bio::EnsEMBL::Pipeline::Runnable::Blast(
-      -query          => $bioseq,
-      -database       => $self->analysis->db_file,
-      -threshold      => 1e-10,
-      -options        => "-filter none -span1 -postsw -V=20 -B=20 -sort_by_highscore -warnings -cpus 1",
-      -threshold_type => "PVALUE",
-      -program        => '/scratch/local/ensembl/bin/wublastp');
+  ## Create the runnable with the previous parser. The filter is not required
+  my $runnable = Bio::EnsEMBL::Analysis::Runnable::Blast->new(
+        -query     => $query,
+        -database  => $dbfile,
+        -program   => $self->analysis->program_file,
+        -analysis  => $self->analysis,
+        -options   => $options,
+        -parser    => $parser,
+        -filter    => undef,
+      );
   $self->runnable($runnable);
-=cut
 
-  #print("running with analysis '".$self->analysis->logic_name."'\n");
-  my $runnable = Bio::EnsEMBL::Pipeline::Runnable::Blast->new(
-                     -query          => $self->query,
-                     -database       => $dbfile,
-                     -program        => $self->analysis->program_file,
-                     -options        => $options,
-                     -threshold      => $thr,
-                     -threshold_type => $thr_type
-                    );
-  $dbfile =~ s/\/tmp\///g if($dbfile =~/\/tmp\//);
-  $runnable->add_regex($dbfile, '^(\S+)\s*');
-  $self->runnable($runnable);
   return 1;
 }
 
 
-sub run
-{
+=head2 runnable
+
+  Arg[1]     : (optional) Bio::EnsEMBL::Analysis::Runnable $runnable
+  Example    : $self->runnable($runnable);
+  Function   : Getter/setter for the runnable
+  Returns    : Bio::EnsEMBL::Analysis::Runnable $runnable
+  Exceptions : none
+
+=cut
+
+sub runnable {
+  my $self = shift(@_);
+
+  if (@_) {
+    $self->{_runnable} = shift;
+  }
+
+  return $self->{_runnable};
+}
+
+
+=head2 run
+
+  Arg[1]     : -none-
+  Example    : $self->run;
+  Function   : Runs the runnable set in fetch_input
+  Returns    : 1 on succesfull completion
+  Exceptions : dies if runnable throws an unexpected error
+
+=cut
+
+sub run {
   my $self = shift;
-  #call superclasses run method
+
   $self->{'comparaDBA'}->dbc->disconnect_when_inactive(1);
-  eval { $self->SUPER::run(); };
-  if($@) {
-    printf(STDERR "Bio::EnsEMBL::Pipeline::Runnable::Blast threw exception:\n$@$_");
+
+  ## call runnable run method in eval block
+  eval { $self->runnable->run(); };
+  ## Catch errors if any
+  if ($@) {
+    printf(STDERR ref($self->runnable)." threw exception:\n$@$_");
     if($@ =~ /"VOID"/) {
       printf(STDERR "this is OK: member_id=%d doesn't have sufficient structure for a search\n", $self->input_id);
     } else {
@@ -168,6 +200,7 @@ sub run
     }
   }
   $self->{'comparaDBA'}->dbc->disconnect_when_inactive(0);
+
   return 1;
 }
 
@@ -181,15 +214,23 @@ sub write_output {
   #which are a shadow of the real analysis object ($self->analysis)
   #The returned FeaturePair objects thus need to be reset to the real analysis object
 
-  foreach my $feature ($self->output) {
+  foreach my $feature (@{$self->output}) {
     if($feature->isa('Bio::EnsEMBL::FeaturePair')) {
       $feature->analysis($self->analysis);
     }
   }
 
-  $self->{'comparaDBA'}->get_PeptideAlignFeatureAdaptor->store($self->output);
+  $self->{'comparaDBA'}->get_PeptideAlignFeatureAdaptor->store(@{$self->output});
 }
 
+
+sub output {
+  my ($self, @args) = @_;
+
+  throw ("Cannot call output without a runnable") if (!defined($self->runnable));
+
+  return $self->runnable->output(@args);
+}
 
 sub global_cleanup {
   my $self = shift;
@@ -236,7 +277,7 @@ sub dumpPeptidesToFasta
   $self->{'comparaDBA'}->get_SubsetAdaptor->dumpFastaForSubset($subset, $fastafile);
 
   # configure the fasta file for use as a blast database file
-  my $blastdb     = new Bio::EnsEMBL::Pipeline::Runnable::BlastDB (
+  my $blastdb     = new Bio::EnsEMBL::Analysis::Runnable::BlastDB (
       -dbfile     => $fastafile,
       -type       => 'PROTEIN');
   $blastdb->run;
