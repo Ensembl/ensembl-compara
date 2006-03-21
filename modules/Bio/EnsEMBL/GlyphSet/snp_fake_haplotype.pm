2fca69d0b09ede9ce9e7180c61ad7338a0562a68
Fiona Cunningham <fiona@ebi.ac.uk>
2006-03-21 17:22:16 +0000

    added subroutines to reduce repetition and make easier to read

--- modules/Bio/EnsEMBL/GlyphSet/snp_fake_haplotype.pm
+++ modules/Bio/EnsEMBL/GlyphSet/snp_fake_haplotype.pm
@@ -18,16 +18,16 @@ sub _init {
   my @snps = @{$Config->{'snps'}};
   return unless scalar @snps;
 
-  my ($w,$th) = $Config->texthelper()->px2bp($Config->species_defs->ENSEMBL_STYLE->{'LABEL_FONT'});
-
-  my $track_height = $th + 4;
-  $self->push(new Sanger::Graphics::Glyph::Space({ 'y' => 0, 'height' => $track_height, 'x' => 1, 'w' => 1, 'absolutey' => 1, }));
-
+  # Info text ---------------------------------------------------------
   # Get reference strain name for start of track:
   my $pop_adaptor = $self->{'container'}->adaptor->db->get_db_adaptor('variation')->get_PopulationAdaptor;
   my $reference_name = $Config->{'reference'} || $pop_adaptor->get_reference_strain_name();
-
   my $info_text = "Comparison to reference $reference_name strain alleles (green = same allele; purple = different allele)";
+
+  my ($w,$th) = $Config->texthelper()->px2bp($Config->species_defs->ENSEMBL_STYLE->{'LABEL_FONT'});
+  my $track_height = $th + 4;
+  $self->push(new Sanger::Graphics::Glyph::Space({ 'y' => 0, 'height' => $track_height, 'x' => 1, 'w' => 1, 'absolutey' => 1, }));
+
 
   my ($small_w, $small_th) = $Config->texthelper()->px2bp('Small');
   my $textglyph = new Sanger::Graphics::Glyph::Text({
@@ -43,20 +43,11 @@ sub _init {
   $self->push( $textglyph );
 
 
+  # Reference track ----------------------------------------------------
   $reference_name = "Reference $reference_name"; 
-  my $ref_bp_textwidth = $w * length($reference_name) * 1.2;
+
   my $offset = $small_th + 4;
-  my $textglyph = new Sanger::Graphics::Glyph::Text({
-      'x'          => -$w - $ref_bp_textwidth,
-      'y'          => $offset+1,
-      'width'      => $ref_bp_textwidth,
-      'height'     => $th,
-      'font'       => $Config->species_defs->ENSEMBL_STYLE->{'LABEL_FONT'},
-      'colour'     => 'black',
-      'text'       => $reference_name,
-      'absolutey'  => 1,
-    });
-  $self->push( $textglyph );
+  $self->strain_name_text($w, $th, $offset, $reference_name, $Config);
 
 
   ## First lets draw the reference SNPs....
@@ -75,44 +66,14 @@ sub _init {
     $snp_ref->[3] = { $reference_base => $colours[0] };
     $snp_ref->[4] = $reference_base;
 
-
     my $alleles_bp_textwidth = $w * length("$label");
     my $strain_bp_textwidth  = $w * length($alleles[0]) ;
 
-    my $tmp_width = $alleles_bp_textwidth + $w*4;
-    if ( ($end - $start + 1) > $tmp_width ) {
-      $start = ( $end + $start-$tmp_width )/2;
-      $end =  $start+$tmp_width ;
-    }
-
-    my $back_glyph = new Sanger::Graphics::Glyph::Rect({
-     'x'         => $start-1,
-     'y'         => $offset,
-     'colour'    => $snp_ref->[3]{$reference_base},
-     'bordercolour' => 'black',
-     'absolutey' => 1,
-     'height'    => $th+2,
-     'width'     => $end-$start+1
-    });
-    $self->push( $back_glyph );
-
-    if ( ($end-$start+1) > $strain_bp_textwidth ) {
-      my $textglyph = new Sanger::Graphics::Glyph::Text({
-        'x'          => ( $end + $start - 1 - $strain_bp_textwidth)/2,
-        'y'          => $offset+1,
-        'width'      => $alleles_bp_textwidth,
-        'height'     => $th,
-        'font'       => $Config->species_defs->ENSEMBL_STYLE->{'LABEL_FONT'},
-        'colour'     => 'white',
-        'text'       => $reference_base,
-        'absolutey'  => 1,
-      });
-      $self->push( $textglyph );
-    }
+       $self->do_glyphs($offset, $th, $w, $start, $end, $snp_ref->[3]{$reference_base}, $strain_bp_textwidth, $alleles_bp_textwidth, $Config, $reference_base);
   } #end foreach $snp_ref
 
 
-   # Now draw SNPs for each strain
+   # Now draw SNPs for each strain -----------------------------------------
    my %strain_alleles;   # $strain_alleles{strain}{id::start} = allele
    my %coverage;         # $coverage{strain} = [ [start, end, level], [start, end, level]   ];
 
@@ -125,28 +86,19 @@ sub _init {
       push @{ $coverage{$strain} }, [ $c_ref->[2]->start, $c_ref->[2]->end, $c_ref->[2]->level ];
     }
   }
+
+  # Start with last strain
    foreach my $data ( reverse @{$Config->{'snp_fake_haplotype'}} ) {
      $offset += $track_height;
      my( $strain ) = @$data;
-
-    # Write strain names
-    my $strain_bp_textwidth = $w * length($strain) * 1.2;
-    my $textglyph = new Sanger::Graphics::Glyph::Text({
-      'x'          => -$w - $strain_bp_textwidth,
-      'y'          => 1+$offset,
-      'width'      => $strain_bp_textwidth,
-      'height'     => $th,
-      'font'       => $Config->species_defs->ENSEMBL_STYLE->{'LABEL_FONT'},
-      'colour'     => 'black',
-      'text'       => $strain,
-      'absolutey'  => 1,
-    });
-    $self->push( $textglyph );
+     $self->strain_name_text($w, $th, $offset, $strain, $Config);
 
      foreach my $snp_ref ( @snps ) {
        my $snp = $snp_ref->[2];
        my $start  = $snp->start;
        my $allele_string =  $strain_alleles{$strain}{ join "::", $snp->{_variation_id}, $start };
+
+       # If no allele for SNP but there is coverage, allele = reference allele
        unless( $allele_string ) {
          foreach my $cov ( @{$coverage{$strain}} ) {
            if( $start >= $cov->[0] && $start <= $cov->[1] ) {
@@ -155,6 +107,7 @@ sub _init {
            }
          }
        }
+
        my $alleles_bp_textwidth = $w * length($snp->allele_string);
        my $strain_bp_textwidth = $w * length($allele_string||1) ;
        my $colour = undef;
@@ -167,27 +120,62 @@ sub _init {
       my( $S,$E ) = ($snp_ref->[0], $snp_ref->[1] );
       $S = 1 if $S < 1;
       $E = $length if $E > $length;
+       $self->do_glyphs($offset, $th, $w, $S, $E, $colour, $strain_bp_textwidth, $alleles_bp_textwidth, $Config, $allele_string);
 
-       my $tmp_width = $alleles_bp_textwidth + $w*4;
-       if ( ($E - $S + 1) > $tmp_width ) {
-	 $S = ( $E + $S-$tmp_width )/2;
-	 $E =  $S+$tmp_width ;
-       }
-       my $back_glyph = new Sanger::Graphics::Glyph::Rect({
-         'x'         => $S-1,
+     }
+   }
+   $self->push(new Sanger::Graphics::Glyph::Space({ 'y' => $offset + $track_height, 'height' => $th+2, 'x' => 1, 'width' => 1, 'absolutey' => 1, }));
+  return 1;
+}
+
+
+
+# Glyphs ###################################################################
+
+sub strain_name_text {
+  my ($self, $w, $th, $offset, $reference_name, $Config) = @_;
+  my $name_bp_textwidth = $w * length($reference_name) * 1.2;
+  my $textglyph = new Sanger::Graphics::Glyph::Text({
+      'x'          => -$w - $name_bp_textwidth,
+      'y'          => $offset+1,
+      'width'      => $name_bp_textwidth,
+      'height'     => $th,
+      'font'       => $Config->species_defs->ENSEMBL_STYLE->{'LABEL_FONT'},
+      'colour'     => 'black',
+      'text'       => $reference_name,
+      'absolutey'  => 1,
+    });
+  $self->push( $textglyph );
+  return 1;
+}
+
+
+
+
+sub do_glyphs {
+  my ($self, $offset, $th, $w, $start, $end, $colour, $strain_bp_textwidth, $alleles_bp_textwidth, $Config, $allele_string) = @_;
+
+  my $tmp_width = $alleles_bp_textwidth + $w*4;
+  if ( ($end - $start + 1) > $tmp_width ) {
+    $start = ( $end + $start-$tmp_width )/2;
+    $end =  $start+$tmp_width ;
+  }
+
+  my $back_glyph = new Sanger::Graphics::Glyph::Rect({
+         'x'         => $start-1,
          'y'         => $offset,
          'colour'    => $colour,
          'bordercolour' => 'black',
          'absolutey' => 1,
          'height'    => $th+2,
-         'width'     => $E-$S+1,
+         'width'     => $end-$start+1,
          'absolutey' => 1,
  	});
-       $self->push( $back_glyph );
+  $self->push( $back_glyph );
 
-       if ( ($E-$S+1)  >$strain_bp_textwidth) {
-	 my $textglyph = new Sanger::Graphics::Glyph::Text({
-            'x'          => ( $E + $S - 1 - $strain_bp_textwidth)/2,
+  if ( ($end-$start + 1)  >$strain_bp_textwidth) {
+    my $textglyph = new Sanger::Graphics::Glyph::Text({
+            'x'          => ( $end + $start - 1 - $strain_bp_textwidth)/2,
             'y'          => 2+$offset,
             'width'      => $alleles_bp_textwidth,
             'height'     => $th,
@@ -196,11 +184,10 @@ sub _init {
             'text'       => $allele_string,
             'absolutey'  => 1,
           }) if $allele_string;
-	 $self->push( $textglyph ) if defined $textglyph;
-       }
-     }
-   }
-   $self->push(new Sanger::Graphics::Glyph::Space({ 'y' => $offset + $track_height, 'height' => $th+2, 'x' => 1, 'width' => 1, 'absolutey' => 1, }));
+    $self->push( $textglyph ) if defined $textglyph;
+  }
+  return 1;
 }
+
 
 1;
