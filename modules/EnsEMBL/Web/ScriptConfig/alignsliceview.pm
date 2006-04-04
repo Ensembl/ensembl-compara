65e1f0e29c636eab06eb1ec96b225638dc839d34
Eugene Kulesha <ek@ebi.ac.uk>
2006-04-04 14:26:45 +0000

    removed alignment type specific stuff. not ideal, but ...

--- modules/EnsEMBL/Web/ScriptConfig/alignsliceview.pm
+++ modules/EnsEMBL/Web/ScriptConfig/alignsliceview.pm
@@ -1,5 +1,24 @@
 package EnsEMBL::Web::ScriptConfig::alignsliceview;
 
+=head1 NAME
+
+EnsEMBL::Web::ScriptConfig::alignsliceview;
+
+=head1 SYNOPSIS
+
+The object handles the config of alignsliceview script
+
+=head1 LICENCE
+
+This code is distributed under an Apache style licence:
+Please see http://www.ensembl.org/code_licence.html for details
+
+=head1 CONTACT
+
+Eugene Kulesha - ek3@sanger.ac.uk
+
+=cut
+
 use strict;
 no strict 'refs';
 
@@ -14,27 +33,31 @@ sub init {
     zoom_width     100
     image_width    700
     context    100000
-    opt_MLAGAN-167_Homo_sapiens on
-    opt_MLAGAN-167_Canis_familiaris on
-    opt_MLAGAN-167_Mus_musculus on
-    opt_MLAGAN-167_Rattus_norvegicus on
-    opt_MLAGAN-170_Homo_sapiens on
-    opt_MLAGAN-170_Canis_familiaris on
-    opt_MLAGAN-170_Mus_musculus on
-    opt_MLAGAN-170_Rattus_norvegicus on
-    opt_MLAGAN-170_Gallus_gallus on
-    opt_MLAGAN-170_Bos_taurus on
-    opt_MLAGAN-170_Monodelphis_domestica on
-    opt_alignm_MLAGAN-167 on
-    opt_alignm_MLAGAN-170 off
-    opt_alignp_BLASTZ_NET_Homo_sapiens off
-    opt_alignp_BLASTZ_NET_Canis_familiaris off
-    opt_alignp_BLASTZ_NET_Mus_musculus off
-    opt_alignp_BLASTZ_NET_Rattus_norvegicus off
-    opt_alignp_BLASTZ_NET_Gallus_gallus off
-    opt_alignp_BLASTZ_NET_Macaca_mulatta off
-    opt_alignp_BLASTZ_NET_Pan_troglodytes off
 
+    opt_1_Bos_taurus on
+    opt_1_Canis_familiaris on
+    opt_1_Homo_sapiens on
+    opt_1_Macaca_mulatta on
+    opt_1_Mus_musculus on
+    opt_1_Pan_troglodytes on
+    opt_1_Rattus_norvegicus on
+
+    opt_2_Bos_taurus on
+    opt_2_Canis_familiaris on
+    opt_2_Homo_sapiens on
+    opt_2_Macaca_mulatta on
+    opt_2_Mus_musculus on
+    opt_2_Pan_troglodytes on
+    opt_2_Rattus_norvegicus on
+    opt_2_Gallus_gallus on
+    opt_2_Monodelphis_domestica on
+
+    opt_align_1 on
+    opt_align_2 off
+    opt_align_171 off
+    opt_align_172 off
+    opt_align_173 off
+    opt_align_174 off
   ));
 }
 1;
