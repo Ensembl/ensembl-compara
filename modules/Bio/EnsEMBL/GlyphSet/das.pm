25b0ed674c56cc011a00d74f042bbd246c29faa2
Eugene Kulesha <ek@ebi.ac.uk>
2006-04-25 16:16:45 +0000

    fixed the Collapse display

--- modules/Bio/EnsEMBL/GlyphSet/das.pm
+++ modules/Bio/EnsEMBL/GlyphSet/das.pm
@@ -742,7 +742,8 @@ sub _init {
   my @das_features = ();
 
   $configuration->{colour} = $Config->get($das_config_key, 'col') || $Extra->{color} || $Extra->{col} || 'contigblue1';
-  $configuration->{depth} =  defined($Config->get($das_config_key, 'dep')) ? $Config->get($das_config_key, 'dep') : $Extra->{depth} || 4;
+  $configuration->{depth} =  defined($Extra->{depth}) ? $Extra->{depth} : defined($Config->get($das_config_key, 'dep')) ?  $Config->get($das_config_key, 'dep') : 4;
+
   $configuration->{use_style} = $Extra->{stylesheet} ? uc($Extra->{stylesheet}) eq 'Y' : uc($Config->get($das_config_key, 'stylesheet')) eq 'Y';
   $configuration->{'labelling'} =($Config->get($das_config_key, 'lflag') || $Extra->{'labelflag'}) =~ /^n$/i ? 0 : 1,
   $configuration->{length} = $container_length;
