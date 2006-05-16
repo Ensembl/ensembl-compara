72f384d689275670404a02899be6bb24364ed863
Anne Parker <ap5@sanger.ac.uk>
2006-05-16 14:12:27 +0000

    Extra sanity check at line 300

--- modules/EnsEMBL/Web/DBSQL/NewsAdaptor.pm
+++ modules/EnsEMBL/Web/DBSQL/NewsAdaptor.pm
@@ -296,7 +296,7 @@ sub fetch_random_ad {
   );
                                                                                 
   my $record = $self->db->selectall_arrayref($sql);
-  return unless ($record && ref($record) eq 'ARRAY');
+  return unless ($record && ref($record) eq 'ARRAY' && ref($record->[0]) eq 'ARRAY');
   my @array = @{$record->[0]};
   my $result = {
       'image' => $array[0],
