6566ab9499bb7396854fe4cd064073c74b788d7c
Abel Ureta-Vidal <abel@sanger.ac.uk>
2006-05-24 10:35:48 +0000

    Now takes the dNdS method_link_type setting from the conf file

--- scripts/pipeline/loadHomologySystem.pl
+++ scripts/pipeline/loadHomologySystem.pl
@@ -413,7 +413,7 @@ sub prepareGenomeAnalysis
   if (defined $dnds_params{'species_sets'}) {
     Bio::EnsEMBL::Hive::DBSQL::AnalysisJobAdaptor->CreateNewJob
         (
-         -input_id       => '{species_sets=>' . $dnds_params{'species_sets'} . '}',
+         -input_id       => '{species_sets=>' . $dnds_params{'species_sets'} . ',method_link_type=>\''.$dnds_params{'method_link_type'}.'\'}',
          -analysis       => $threshold_on_dS,
         );
   }
