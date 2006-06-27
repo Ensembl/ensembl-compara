902796bb38d81803c46fe038bbb8a952aa637e33
Fiona Cunningham <fiona@ebi.ac.uk>
2006-06-27 09:55:51 +0000

    changed v to release

--- modules/EnsEMBL/Web/Document/HTML/Release.pm
+++ modules/EnsEMBL/Web/Document/HTML/Release.pm
@@ -12,7 +12,7 @@ sub render {
   $_[0]->printf( qq(
 <div id="release-t">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
 <div id="release"><div>
-  %s v%s - <a href="/Multi/newsview?rel=%s" title="What's New">%s</a>
+  %s release %s - <a href="/Multi/newsview?rel=%s" title="What's New">%s</a>
 </div></div>), CGI::escapeHTML($_[0]->site_name), CGI::escapeHTML($_[0]->version), CGI::escapeHTML($_[0]->version), CGI::escapeHTML($_[0]->date)
   );;
 }
