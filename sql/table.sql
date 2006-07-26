fe9efa5c60a274def7ab2a37c3de8c6e3ef45eae
Kathryn Beal <kbeal@ebi.ac.uk>
2006-07-26 14:06:14 +0000

    Added ConservationScore table

--- sql/table.sql
+++ sql/table.sql
@@ -550,6 +550,20 @@ CREATE TABLE domain_member (
   UNIQUE (member_id,domain_id,member_start,member_end)
 ) COLLATE=latin1_swedish_ci;
 
+#
+# Table structure for table 'conservation_score'
+#
+
+CREATE TABLE conservation_score (
+  genomic_align_block_id bigint unsigned not null,
+  window_size            smallint unsigned not null,
+  position	         int unsigned not null,
+  observed_score         blob,
+  expected_score         blob,
+  diff_score             blob,
+
+  KEY (genomic_align_block_id, window_size)
+) MAX_ROWS = 15000000 AVG_ROW_LENGTH = 841 COLLATE=latin1_swedish_ci;
 
 ------------------------------------------------------------------------------------
 --
@@ -635,4 +649,3 @@ CREATE TABLE protein_tree_tag (
 
 # Auto add schema version to database
 INSERT INTO meta (meta_key, meta_value) VALUES ("schema_version", "40");
-
