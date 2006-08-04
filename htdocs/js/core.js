a7b02170b6262c46e1cfa6b2970c801721ff2171
Web Admin <w3adm@sanger.ac.uk>
2006-08-04 22:01:23 +0000

    added in extra javascript functionality

---
+++ htdocs/js/core.js
@@ -0,0 +1,8 @@
+/* A place to dump random javascript */
+
+// Return a link based on the current URL to the archive...
+function archive( release ) {
+  URL = document.location.href;
+  document.location = URL.replace(/^https?:\/\/[^\/]+/,'http://'+release+'.archive.ensembl.org');
+  return true; 
+}
