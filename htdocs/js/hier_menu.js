0670d12dadbf24aee80fec5e1de5ecd8010aaac9
Web Admin <w3adm@sanger.ac.uk>
2006-08-04 22:04:04 +0000

    minor fixes

--- htdocs/js/hier_menu.js
+++ htdocs/js/hier_menu.js
@@ -1,5 +1,65 @@
 // Javascript to do collapsible hierarchical menus
 
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if( typeof window.onload != 'function' ) {
+    window.onload = func;
+  } else {
+    window.onload = function() {
+      oldonload();
+      func();
+    }
+  }
+}
+
+function codelinks() {
+  pre_blocks = document.getElementsByTagName( 'pre' );
+  if(!document.all){
+  for( i = 0; i< pre_blocks.length; i++ ) {
+    c = pre_blocks[i];
+    T = c.innerHTML;
+    T = T.replace( /^(#[^!].*)$/gm, '<div style="background-color: #ffcc99; margin: 0.2em 0 -0.8em 0; padding: 0.3em">$1</div>' );
+    T = T.replace( /^sub (.*)$/gm, '<span style="font-weight: bold">sub <span style="color: #00c">$1</span></span>' );
+    T = T.replace( /(\$\w+)/gm, '<span style="color: #900; font-weight: bold">$1</span>' );
+    T = T.replace( /([\[\](){}])/gm, '<span style="font-weight: bold">$1</span>' );
+    T = T.replace( /([@$%])/gm, '<span style="font-weight: bold">$1</span>' );
+    T = T.replace( /-&gt;(\w+)/gm, '<span style="font-weight: bold">-&gt;<span style="color: #00c">$1</span></span>' );
+//    T = T.replace( /'([^']+)'/gm, "'<span style=\"background-color: #ccffcc\">$1</span>'" );
+    c.innerHTML = T;
+  }
+  }
+  code_entries = document.getElementsByTagName( 'code' );
+  for( i = 0; i< code_entries.length ; i++ ) {
+    c = code_entries[i];
+    cl = c.getAttribute( 'class' ) ;
+    switch( cl ) {
+      case 'module':
+        c.style.cursor = 'pointer';
+        c.onclick   = auto_cvs_link;
+        c.setAttribute( 'title', 'View source code of '+c.innerHTML+' from cvs.sanger.ac.uk' );
+    }
+  }
+}
+
+function auto_cvs_link() {
+  cvs_link( this.getAttribute('class'), this.innerHTML );
+}
+
+function cvs_link( type, module_name ) {
+  mod_path = module_name.replace( /::/g, '/' ); 
+  prefix = 'http://cvs.sanger.ac.uk/cgi-bin/viewcvs.cgi/';
+  if( type == 'module' ) {
+    area = 'ensembl-webcode/modules';
+    extn = '.pm';
+  } else if( type == 'script' ) {
+    area = 'ensembl-webcode/perl/default';
+    extn = '';
+  }
+  URL = prefix+area+'/'+mod_path+extn+'?rev=HEAD&view=markup'
+  window.open( URL, 'cvs', '' );
+}
+
+function open_code_window( anchor ) { window.open('sourcecode.html#'+anchor,'sourcecode',''); }
 function exp_coll(ind)
 
 {
@@ -46,3 +106,5 @@ function exp_all()
 
  exp(0);
 }
+addLoadEvent( codelinks );
+
--- htdocs/js/new_support.js
+++ htdocs/js/new_support.js
@@ -1,4 +1,5 @@
 // General support functionality....
+
 MAC = navigator.userAgent.toLowerCase().indexOf('mac')==-1?0:1;
 var LOADED = 0;    // Has the page been loaded
 var DIAG   = 0;    // Warn missing objects??
@@ -79,16 +80,21 @@ function egYr( i ) { return parseInt( i.offsetTop ); }
 NNNN = 5;
 function egeX(e) {
  switch(MODE) {
-  case 'IE6': return event.clientX + (document.body.scrollLeft?document.body.scrollLeft:(document.documentElement?(document.documentElement.scrollLeft?document.documentElement.scrollLeft:0):0));  
+  case 'IE6': return event.clientX + (
+    document.body.scrollLeft ? document.body.scrollLeft : (
+      document.documentElement ? (
+        document.documentElement.scrollLeft ? document.documentElement.scrollLeft : 0
+      ) : 0 ) - 2
+    );  
   case 'NS6': return e.pageX;
 }}
 
 function egeY(e) {
   if(NNNN-->0) {
-//    alert( e.clientY +'..'+e.pageY+'..'+e.screenY +'..'+ ( (document.body.scrollTop)?document.body.scrollTop:document.documentElement.scrollTop) +'..' + ( window.pageYOffset ) );
+//    alert( e.clientY +'..'+e.pageY+'..'+e.screenY +'..'+ ( (document.body.scrollTop)?document.body.scrollTop:document.documentElement.scrollTop) +'..' + ( window.pageYOffset ) ) - 2;
   }
 switch(MODE) {
-  case 'IE6': return event.clientY + (document.body.scrollTop?document.body.scrollTop:(document.documentElement?(document.documentElement.scrollTop?document.documentElement.scrollTop:0):0));   
+  case 'IE6': return event.clientY + (document.body.scrollTop?document.body.scrollTop:(document.documentElement?(document.documentElement.scrollTop?document.documentElement.scrollTop:0):0)) - 2;   
   case 'NS6': return e.pageY;
 }}
 function egeXr(e) {switch(MODE) {
