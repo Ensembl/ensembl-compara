816f6ece73731f6855975e8d76b4aac84fbdecc5
Web Admin <w3adm@sanger.ac.uk>
2006-08-08 21:44:57 +0000

    added cytoview link code

--- htdocs/js/core.js
+++ htdocs/js/core.js
@@ -1,6 +1,12 @@
 /* A place to dump random javascript */
 
 // Return a link based on the current URL to the archive...
+
+function cytoview_link() {
+  URL = document.location.href;
+  document.location = URL.replace(/(\w+view)/,'cytoview');
+  return true; 
+}
 function archive( release ) {
   URL = document.location.href;
   document.location = URL.replace(/^https?:\/\/[^\/]+/,'http://'+release+'.archive.ensembl.org');
