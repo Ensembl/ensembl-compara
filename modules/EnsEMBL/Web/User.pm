64bcd7ccf73133b722f0254a4175eb6bb6178a38
Anne Parker <ap5@sanger.ac.uk>
2006-08-15 12:07:40 +0000

    New user modules

--- modules/EnsEMBL/Web/User.pm
+++ modules/EnsEMBL/Web/User.pm
@@ -4,13 +4,12 @@ use strict;
 use warnings;
 no warnings "uninitialized";
 
-## STUB -  see sanger-plugins/ap5 for work in progress :)
 
 sub new {
   my $class = shift;
 
   ## is the user logged in?
-  my $user_id = $ENV{'ENSEMBL_USER'} || 0;
+  my $user_id = $ENV{'ENSEMBL_USER_ID'} || 0;
 
   my $self = {
     '_user_id'      => $user_id,
