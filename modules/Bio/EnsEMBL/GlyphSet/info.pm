3086bad41da4aa1a304f3b3ece108735de2c2475
Steve Trevanion <st3@sanger.ac.uk>
2006-08-18 09:20:38 +0000

    merge from branch-vega-39-dev

--- modules/Bio/EnsEMBL/GlyphSet/info.pm
+++ modules/Bio/EnsEMBL/GlyphSet/info.pm
@@ -18,14 +18,15 @@ sub _init {
   my $Assembly  = $Config->species_defs->ASSEMBLY_ID;
   my $SpVersion = $Config->species_defs->SPECIES_RELEASE_VERSION;
   my $species   = $Config->species_defs->SPECIES_BIO_NAME;
+  my $sitetype  = $Config->species_defs->ENSEMBL_SITETYPE;
 
   my $type = $self->{'container'}->coord_system->name();
   $type = ucfirst( $type );
   my $chr = $self->{'container'}->seq_region_name();
   $chr = "$type $chr" unless $chr =~ /^$type/i;
 
-  my $text_to_display = sprintf( "Ensembl %s version %s.%s (%s) %s %s - %s",
-    $species, $Version, $SpVersion, $Assembly,
+  my $text_to_display = sprintf( "%s %s version %s.%s (%s) %s %s - %s",
+    $sitetype, $species, $Version, $SpVersion, $Assembly,
     $chr,
     $self->thousandify( $self->{'container'}->start() ),
     $self->thousandify( $self->{'container'}->end )
