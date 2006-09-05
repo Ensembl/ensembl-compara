839bc70cfc420c210b8c876782af535c57519cd8
Abel Ureta-Vidal <abel@sanger.ac.uk>
2006-09-05 14:02:31 +0000

    add sql to add display_label column in member table

--- sql/patch_40_41.sql
+++ sql/patch_40_41.sql
@@ -4,7 +4,10 @@
 DELETE FROM meta WHERE meta_key="schema_version";
 INSERT INTO meta (meta_key,meta_value) VALUES ("schema_version",41);
 
-# Add displayable column to analysis_description
+# Add displayable column to analysis_description table
 
 ALTER TABLE analysis_description ADD COLUMN displayable BOOLEAN DEFAULT 1 NOT NULL;
 
+# Add display_label column to member table
+
+ALTER TABLE member ADD COLUMN display_label VARCHAR(128) DEFAULT NULL
