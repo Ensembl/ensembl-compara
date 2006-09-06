95d1047ef69e2b4e38621e933cbb64cfa8f0d25b
Matthew Wood <mw4@sanger.ac.uk>
2006-09-06 10:28:46 +0000

    Added script for automatic document generation.

---
+++ utils/edoc/document.pl
@@ -0,0 +1,24 @@
+#! /usr/bin/perl 
+
+use strict;
+use warnings;
+
+use EnsEMBL::Web::Tools::Document;
+
+my $export = shift @ARGV;
+my $base = shift @ARGV;
+my $support = shift @ARGV;
+my @locations = @ARGV;
+
+if (-e $export) {
+print "Exporting to $export\n";
+print "Searching for documentation\n";
+my $document = EnsEMBL::Web::Tools::Document->new( (
+                 directory => \@locations,
+                 identifier => "##"
+               ) );
+
+$document->find_modules;
+$document->generate_html($export, $base, $support);
+print "Done\n";
+}
