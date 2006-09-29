f391eeba45878881043b72e43104fec57949862f
Web Admin <w3adm@sanger.ac.uk>
2006-09-29 11:07:18 +0000

    *** empty log message ***

--- utils/Tool.pm
+++ utils/Tool.pm
@@ -1,5 +1,7 @@
 package utils::Tool;
 
+### Collection of methods created for use with the dumping scripts
+
 use strict;
 use warnings;
 use Pod::Usage;
@@ -22,36 +24,28 @@ BEGIN {
 require EnsEMBL::Web::SpeciesDefs; 
 
 #----------------------------------------------------------------------
-
-=head2 all_species
-
-  Arg[1]      : 
-  Example     : my @all_species = @{ utils::Tool::all_species };
-  Description : returns a list of all the species configured in SiteDefs
-                ENSEMBL_SPECIES
-  Return type : arrayref
-
-=cut
-
 sub all_species {
+
+  ### Example     : my @all_species = @{ utils::Tool::all_species };
+  ### Description : returns a list of all the species configured in SiteDefs
+  ###                ENSEMBL_SPECIES
+  ### Returns arrayref
+
   return $SiteDefs::ENSEMBL_SPECIES || [];
 }
 
 #------------------------------------------------------------------------
-
-=head2 start_with_species
-
-  Arg[1]      : arrayref
-  Example     : my @species = @{ utils::Tool::start_with_species };
-  Description : returns a list of all the species configured in SiteDefs
-                ENSEMBL_SPECIES
-  Return type : arrayref
-
-=cut
-
 sub start_with_species {
+
+  ### Arg1        : species name
+  ### Arg2        : arrayref
+  ### Example     : my @species = @{ utils::Tool::start_with_species };
+  ### Description : removes species from the name supplied whoose names 
+  ###              come before the species name supplied (when listed 
+  ###              alphabetically)
+  ### Returns arrayref
+
   my ($start_with, $species) = @_;
-  ### Start with a species further down in the alphabet
 
   foreach (sort @$species) {
     last if $start_with eq $_;
@@ -62,19 +56,37 @@ sub start_with_species {
 }
 
 #----------------------------------------------------------------------
+sub end_with_species {
 
-=head 2 check_dir
+  ### Arg1        : species name
+  ### Arg2        : arrayref
+  ### Example     : my @species = @{ utils::Tool::end_with_species };
+  ### Description : removes species from a list whoose names start
+  ###                after the argument supplied when listed alphabetically
+  ### Returns arrayref
 
-  Arg[1]     : directory name and path
-  Example    : utils::Tool::check_dir($dir);
-  Description: checks to see if directory exists. If yes, returns, if no, 
-               it creates all the necessary directories in the path for the 
-               directory to exist
-  Return type: 1
+  my ($end_with, $species) = @_;
 
-=cut
+  my @return;
+  foreach (sort @$species) {
+    push @return, shift @$species;
+    last if $end_with eq $_;
+  }
 
+  info(1, "Skipping these species: @$species");
+  return \@return || [] ;
+}
+
+#----------------------------------------------------------------------
 sub check_dir {
+
+  ### Arg[1]     : directory name and path
+  ### Example    : utils::Tool::check_dir($dir);
+  ### Description: checks to see if directory exists. If yes, returns, if no, 
+  ###            it creates all the necessary directories in the path for the 
+  ###         directory to exist
+  ###  Return type: 1
+
   my $dir = shift;
   if( ! -e $dir ){
     info(1, "Creating $dir" );
@@ -84,21 +96,15 @@ sub check_dir {
     }
   }
   return 1;
-
 }
-
   
 #------------------------------------------------------------------------
-
-=head2 mail_log
-
- Arg[1]: log to check
- Arg[2]: email address
- Description: checks log and emails you the results
-
-=cut
-
 sub mail_log {
+
+ ### Arg1 (optional): log to check
+ ### Arg2 (optional): email address
+ ### Description: checks log and emails you the results
+
   my ($file, $email_address, $additional_text) = @_;
   open IN, "< $file" or die "Can't open $file : $!";
   my $content;
@@ -120,21 +126,17 @@ sub mail_log {
 
 
 #-----------------------------------------------------------------------
-
-=head2 check_species
-
-  Arg1        : arrayref of species to check
-  Arg[2]      : (Optional) personalised error message
-  Example     : my @OK_SPECIES = @{ utils::Tool::check_species(\@SPECIES) };
-  Description : checks each species from Arg1 to see if they are valid
-                returns Arg1 arrayref if they are all valid.  
-                Otherwise throws an error
-  Return type : array_ref or error message
-
-=cut
-
 sub check_species {
-  my $species = shift;
+
+  ### Arg1        : arrayref of species to check
+  ### Arg2(Optional):  personalised error message
+  ### Example     : my @OK_SPECIES = @{ utils::Tool::check_species(\@SPECIES) };
+  ### Description : checks each species from arg1 to see if they are valid
+  ###                returns arrayref if they are all valid.  
+  ###             Otherwise throws an error
+  ### Returns array_ref or error message
+
+   my $species = shift;
   my $error   = shift || "[*DIE] Invalid species. Select from:\n";
   my %all_species = map{$_=>1} ( @{&all_species}, "Multi");
   
@@ -145,17 +147,14 @@ sub check_species {
   return $species;
 }
 #---------------------------------------------------------------------
-=head2 get_config
-
-  Arg1        : hashref of arguments 
-  Example     : my %tmp = %{utils::Tool::get_config(  { species=>$spp, 
-				 	      values => $source}   )};
-  Description : retrieves the value of $args->{values} from species defs
-  Return type : hashref
-
-=cut
-
 sub get_config {
+
+  ### Arg1        : hashref of arguments 
+  ### Example     : my %tmp = %{utils::Tool::get_config(  { species=>$spp, 
+  ###				 	      values => $source}   )};
+  ### Description : retrieves the value of $args->{values} from species defs
+  ### Returns hashref
+
   my $args = shift;
   my $species_defs = &species_defs;
   my $values = $species_defs->get_config($args->{species}, $args->{values});
@@ -181,21 +180,18 @@ sub sz {
     $rss *= 1048576;   # 1024*1024
   }
 
-  return($size, $rss); # return 0 for share, to avoid undef warnings
+  return($size, $rss); ### Returns 0 for share, to avoid undef warnings
 }
 
 #------------------------------------------------------------------------
-=head2 info
-
-  Arg[1]      : verbosity
-  Arg[1]      : message
-  Example     : utils::Tool::info(1, "Current release is $release");
-  Description : Prints message to STDERR
-  Return type : none
-
-=cut
-
 sub info{
+
+  ### Arg1      : verbosity
+  ### Arg2      : message
+  ### Example     : utils::Tool::info(1, "Current release is $release");
+  ### Description : Prints message to STDERR
+  ### Returns none
+
   my $v   = shift;
   my $msg = shift;
   if( ! defined($msg) ){ $msg = $v; $v = 0 }
@@ -215,16 +211,13 @@ sub info{
 
 
 #--------------------------------------------------------------------
-=head2 mysql_db
-
-  Arg[1]      : current ensembl release e.g. 30
-  Example     : our %mysql_db =  %{ utils::Tool::mysql_db($release) };
-  Description : produces a list of all the mysql database that match *$release*
-  Return type : hashref
-
-=cut
-
 sub mysql_db {
+
+  ### Arg[1]      : current ensembl release e.g. 30
+  ### Example     : our %mysql_db =  %{ utils::Tool::mysql_db($release) };
+  ### Description : produces a list of all the mysql database that match *$release*
+  ### Returns hashref
+
   my $release = shift || "";
   my $species_defs = &species_defs;
   my $dsn = "DBI:mysql:host=". $species_defs->ENSEMBL_HOST .";port=" . $species_defs->ENSEMBL_HOST_PORT;
@@ -240,20 +233,16 @@ sub mysql_db {
 }
 
 #--------------------------------------------------------------------------
-=head2 print_next
-
-  Arg1      : arrayref where each value is a line of the file
-  Arg2      : string to match
-  Arg3      : file handle
-  Example     : $contents =  utils::Tool::mysql_db(\@contents, "Update here", $fh);
-  Description : Takes an array of file contents.  Prints each line in the array to $fh
-                until a line matches Arg2. Returns the remainder of the file as 
-                arrayref
-  Return type : arrayref
-
-=cut
-
 sub print_next {
+
+  ### Arg1      : arrayref where each value is a line of the file
+  ### Arg2      : string to match
+  ### Arg3      : file handle
+  ### Example     : $contents =  utils::Tool::mysql_db(\@contents, "Update here", $fh);
+  ### Description : Takes an array of file contents.  Prints each line in the array to $fh
+  ### until a line matches arg2.
+  ### Returns arrayref
+
   my $contents = shift;
   my $last     = shift;
   my $fh       = shift;
@@ -270,43 +259,12 @@ sub print_next {
 }
 
 #--------------------------------------------------------------------
-=head2 release_month
-
-  Arg[1]      : none
-  Example     : my $mth =  utils::Tool::release_month();
-  Description : Returns the first 3 letters (lowercase) of 
-                either the current month if today's ' date is within the 
-                first 10 days of the month 
-                or the next month if today's ' date is after the 15th.
-                This is a rough hack to estimate the month of the next Ensembl release
-  Return type : three letter string
-
-=cut
-
-
-sub release_month {
-  my $archive_date = lc($SiteDefs::ARCHIVE_VERSION);
-  $archive_date =~ s/\d+//;
-  return $archive_date;
-
-  # Old way
-  my @months = qw (jan feb mar apr may jun jul aug sep oct nov dec);
-  my $day      = localtime->mday;
-  my $curr_mth = localtime->mon;
-  return $months[$curr_mth];
-  #return  $day <10 ? $months[$curr_mth] : $months[$curr_mth +1];
-}
-#--------------------------------------------------------------------
-=head2 site_logo
-
-  Arg[1]      : none  
-  Example     : my %logo = %{ utils::Tool::site_logo() };
-  Description : Retreives the SITE_LOGO as defined in DEFAULTS.ini by SITE_LOGO_KEY
-  Return type : hashref of info
-
-=cut
-
 sub site_logo {
+
+  ### Example     : my %logo = %{ utils::Tool::site_logo() };
+  ### Description : Retreives the SITE_LOGO as defined in DEFAULTS.ini by SITE_LOGO_KEY
+  ### Returns hashref of info
+
   my $species_defs = &species_defs;
   return
       { src    => $species_defs->SITE_LOGO,          
@@ -318,36 +276,27 @@ sub site_logo {
 }
 
 #----------------------------------------------------------------------
-=head2 species_defs
-
-  Arg[1]      : none  
-  Example     : utils::Tool::species_defs
-  Description : 
-  Return type : $species_defs
-
-=cut
-
 sub species_defs {
+
+  ### Example     : utils::Tool::species_defs
+  ### Description : 
+  ### Returns $species_defs
+
   my $SPECIES_DEFS = EnsEMBL::Web::SpeciesDefs->new(); 
   $SPECIES_DEFS || pod2usage("$0: SpeciesDefs config not found");
   return $SPECIES_DEFS;
 }
 
 #----------------------------------------------------------------------
-=head2 validate_types
-
-  Arg1      : hashref of valid types
-               my %valid_types = map{ $_ => 1 }  qw( all db blast ftp_dir  );
-  Arg2      : hashref of compound types
-              my %compound_types =   ( all   => [ qw( db blast ftp_dir  ) ]   );
-  Arg3      : arrayref of user types
-  Example   : my %types = %{ utils::Tool::validate_types(\%valid_types, \%compound_types, \@user_types) };
-  Description : Used make a list of types from a mixture of compound types and or valid types 
-  Return type : hashref of types to use
-
-=cut
-
 sub validate_types {
+
+  ### Arg1      : hashref of valid types e.g. %vt = map{$_=>1}  qw(all blast ftp);
+  ### Arg2      : hashref of compound types e.g. %ct = ( all => [ qw(db blast ftp) ] );
+  ### Arg3      : arrayref of user types
+  ### Example   : %types = %{ utils::Tool::validate_types(\%vt, \%ct, \@user_types) };
+  ### Description : Used make a list of types from a mixture of compound types and or valid types 
+  ### Returns hashref of types to use
+
    my $valid_types    = shift;
    my $compound_types = shift;
    my $user_types     = shift;
@@ -369,18 +318,14 @@ sub validate_types {
    return \@types;
  }
 #-------------------------------------------------------------------------
-
-=head2 warning
-
-  Arg[1]      : verbosity
-  Arg 2       : message
-  Example     : 
-  Description : Prints warning message
-  Return type : none
-
-=cut
-
 sub warning{
+
+  ### Arg[1]      : verbosity
+  ### Arg 2       : message
+  ### Example     : 
+  ### Description : Prints warning message
+  ### Returns 1
+
   my $v   = shift;
   my $msg = shift;
   if( ! defined($msg) ){ $msg = $v; $v = 0 }
@@ -394,86 +339,4 @@ sub warning{
 #----------------------------------------------------------------------
 
 
-__END__
-           
-=head1 NAME
-                                                                                
-Tool
-
-=head1 SYNOPSIS
-
-    use <path>::Tool;
-    # Brief but working code example(s) here showing the most common usage
-
-    # This section will be as far as many users bother reading,
-    # so make it as educational and exemplary as possible!
-
-=head1 DESCRIPTION
-
-A full description of the module and its features.
-May include numerous subsections (i.e. =head2, =head3, etc).
-
-=head1 METHODS
-
-An object of this class represents...
-
-Below is a list of all public methods:
-
-
-
-=head1 BUGS AND LIMITATIONS
-
-A list of known problems with the module, together with some indication of 
-whether they are likely to be fixed in an upcoming release.
-
-=head1 AUTHOR
-                                                                                
-[name], Ensembl Web Team
-Support enquiries: helpdesk@ensembl.org
-                                                                                
-=head1 COPYRIGHT
-                                                                                
-See http://www.ensembl.org/info/about/code_licence.html
-
-
-__END__
-           
-=head1 NAME
-                                                                                
-Tool
-
-=head1 SYNOPSIS
-
-    use <path>::Tool;
-    # Brief but working code example(s) here showing the most common usage
-
-    # This section will be as far as many users bother reading,
-    # so make it as educational and exemplary as possible!
-
-=head1 DESCRIPTION
-
-A full description of the module and its features.
-May include numerous subsections (i.e. =head2, =head3, etc).
-
-=head1 METHODS
-
-An object of this class represents...
-
-Below is a list of all public methods:
-
-
-
-=head1 BUGS AND LIMITATIONS
-
-A list of known problems with the module, together with some indication of 
-whether they are likely to be fixed in an upcoming release.
-
-=head1 AUTHOR
-                                                                                
-[name], Ensembl Web Team
-Support enquiries: helpdesk@ensembl.org
-                                                                                
-=head1 COPYRIGHT
-                                                                                
-See http://www.ensembl.org/info/about/code_licence.html
 1;
