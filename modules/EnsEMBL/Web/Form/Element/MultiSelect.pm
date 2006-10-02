6e474528af1b3ac61eeccc265816b42a150fc36f
Anne Parker <ap5@sanger.ac.uk>
2006-10-02 09:08:07 +0000

    Fixed minor conflict (broken syntax)

--- modules/EnsEMBL/Web/Form/Element/MultiSelect.pm
+++ modules/EnsEMBL/Web/Form/Element/MultiSelect.pm
@@ -33,7 +33,6 @@ sub render {
       $self->notes
     );
   } else {
-warn "Class ".$self->{'class'};
     my $output = '';
     my $K = 0;
     my $checked;
@@ -56,7 +55,7 @@ warn "Class ".$self->{'class'};
 	          CGI::escapeHTML($V->{'value'}),
             $checked eq 'yes' ? ' checked="checked"' : '', 
             CGI::escapeHTML($self->id), $K, 
-            $self->{'noescape'} ? $V->{'name'} : CGI::escapeHTML($V->{'name'}
+            $self->{'noescape'} ? $V->{'name'} : CGI::escapeHTML($V->{'name'})
         );
         $K++;
     }
