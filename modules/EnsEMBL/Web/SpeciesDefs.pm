dcff4636d67d96caec674973725db71bdb63e25f
Anne Parker <ap5@sanger.ac.uk>
2006-10-03 16:18:15 +0000

    Added sanity check to date parsing

--- modules/EnsEMBL/Web/SpeciesDefs.pm
+++ modules/EnsEMBL/Web/SpeciesDefs.pm
@@ -409,18 +409,21 @@ sub _parse_date {
   my $date = shift;
   my %parsed;
   my @a = ($date =~ /(\d{2})(\d{2})(.+)/);
-
+  my @now = localtime();
+  my $thisyear = '20'.$now[5];
   my %months = ('01'=>'Jan', '02'=>'Feb', '03'=>'Mar', '04'=>'Apr',
                   '05'=>'May', '06'=>'Jun', '07'=>'July','08'=>'Aug',
                   '09'=>'Sep', '10'=>'Oct', '11'=>'Nov', '12'=>'Dec');
-  my $month = $months{$a[1]};
 
-  my $year = $a[0];
-  if ($year < 70) {
-    $year = '20'.$year;
+  my $year = '20'.$a[0];
+  my $mon = $a[1];
+  my $month;
+  if ($mon && $mon < 13) {
+    $month = $months{$mon};
   }
-  else {
-    $year = '19'.$year;
+
+  if ($year > $thisyear || !$month) {
+    print STDERR "\t  [WARN] DATE FORMAT MAY BE REVERSED - parses as $mon $year\n";
   }
 
   $parsed{'date'} = $month.' '.$year;
