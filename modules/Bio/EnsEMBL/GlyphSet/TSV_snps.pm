3c5ac3762af3f9f3458b402aaaa33c588e8b62ea
Fiona Cunningham <fiona@ebi.ac.uk>
2006-10-03 15:17:24 +0000

    fixed black SNP box bug by using allele_string from ConsequenceType rather than AlleleFeature

--- modules/Bio/EnsEMBL/GlyphSet/TSV_snps.pm
+++ modules/Bio/EnsEMBL/GlyphSet/TSV_snps.pm
@@ -19,18 +19,16 @@ sub init_label {
 
 sub _init {
   my ($self) = @_;
-  my $type = $self->check();
-  return unless defined $type;
+  my $check = $self->check();
+  return unless defined $check;
   return unless $self->strand() == -1;
 
   my $Config        = $self->{'config'};
-  warn "dddddddddd", $Config;
   my $transcript = $Config->{'transcript'}->{'transcript'};
   my $consequences_ref = $Config->{'transcript'}->{'consequences'};
   my $alleles      = $Config->{'transcript'}->{'allele_info'};
   return unless $alleles && $consequences_ref;
-  warn "TSV_SNPS  ", $Config->{'transcript'}->{'sample'};
-  warn "ddd snps! ", $alleles;
+
   # Drawing params
   my( $fontname, $fontsize ) = $self->get_font_details( 'innertext' );
   my $pix_per_bp    = $Config->transform->{'scalex'};
@@ -48,8 +46,6 @@ sub _init {
   my $colour_map = $Config->get('TSV_snps','colours' );
   my $EXTENT        = $Config->get('_settings','context')|| 1e6;
      $EXTENT        = 1e6 if $EXTENT eq 'FULL';
-  my $seq_region_name = $self->{'container'}->seq_region_name();
-  my @consequences =  @$consequences_ref;
   warn "######## ERROR arrays should be same length" unless length @$alleles == length @$consequences_ref;
 
 
@@ -60,48 +56,29 @@ sub _init {
     @coverage_obj = sort {$a->[2]->start <=> $b->[2]->start} @$raw_coverage_obj;
   }
 
+  my $index = 0;
   foreach my $allele_ref (  @$alleles ) {
     my $allele = $allele_ref->[2];
-    next unless $allele;
-    warn "AF ", $allele->allele_string if $allele->variation_name eq 'rs31744050';
-    warn $allele if $allele->variation_name eq 'rs31744050';
-    my $conseq_type = shift @consequences;
-    next unless $conseq_type;
+    my $conseq_type = $consequences_ref->[$index];
+    $index++;
+    next unless $conseq_type && $allele;
     next if $allele->end < $transcript->start - $EXTENT;
     next if $allele->start > $transcript->end + $EXTENT;
-    if( $transcript->strand != $allele->strand ) {
-      my $tmp = join "", @{$conseq_type->alleles};
-      $tmp =~tr/ACGT/TGCA/;
-      warn "ERROR: Allele call on alleles ", $allele->allele_string, " Allele call on ConsequenceType is different: $tmp" if $allele->allele_string ne $tmp;
-    }
 
     # Alleles (if same as ref, draw empty box )---------------------
     my $aa_change =  $conseq_type->aa_alleles || [];
     my $label  = join "/", @$aa_change;
     my $S =  ( $allele_ref->[0]+$allele_ref->[1] )/2;
     my $width = $font_w_bp * length( $label );
+
+    # Note: due to some bizarre API caching, the allele->allele_string is incorrect here
+    # The alleles from conseq_type need to be used instead. 
     my $ref_allele = $allele->ref_allele_string();
-
-
-    if ($ref_allele eq $allele->allele_string) { # if 'negative snp'
-       my $bglyph = new Sanger::Graphics::Glyph::Rect({
-      'x'         => $S - $font_w_bp / 2,
-      'y'         => $height + 2,
-      'height'    => $height,
-      'width'     => $width + $font_w_bp +4,
-      'bordercolour' => 'grey70',
-      'absolutey' => 1,
-      'zindex'    => -4,
-     });
-      my $bump_start = int($bglyph->{'x'} * $pix_per_bp);
-      $bump_start = 0 if ($bump_start < 0);
-      my $bump_end = $bump_start + int($bglyph->width()*$pix_per_bp) +1;
-      $bump_end = $bitmap_length if ($bump_end > $bitmap_length);
-      my $row = & Sanger::Graphics::Bump::bump_row( $bump_start, $bump_end, $bitmap_length, \@bitmap );
-      $bglyph->y( $voffset + $bglyph->{'y'} + ( $row * (2+$height) ) + 1 );
-      $self->push( $bglyph );
-      next;
+    my @conseq_alleles = @{$conseq_type->alleles || [] };
+    if ($allele->strand != $transcript->strand) {
+      map {tr/ACGT/TGCA/} @conseq_alleles;
     }
+    warn "Consequence alleles has more than one alt allele" if $#conseq_alleles > 0;
 
 
     # Type and colour -------------------------------------------
@@ -109,16 +86,6 @@ sub _init {
     my $colour = $colour_map->{$type}->[0];
 
     my @tmp;
-    push @tmp, ("13:amino acid: $aa_change->[0] to $aa_change->[1]", '' ) if $aa_change->[1];
-
-    # Codon - make the letter for the SNP position in the codon bold
-    my $codon = $conseq_type->codon;
-    if ( $codon ) {
-      my $pos = ($conseq_type->cds_start % 3 || 3) - 1;
-      $codon =~ s/(\w{$pos})(\w)(.*)/$1<b>$2<\/b>$3/;
-      my $strand = $transcript->strand > 0 ? "+" : "-";
-      push @tmp, ("11:transcript codon ($strand strand) ".$codon => '');
-    }
 
     # Coverage -------------------------------------------------
     if ( grep { $_ eq "Sanger"}  @{$allele->get_all_sources() || []}  ) {
@@ -132,6 +99,41 @@ sub _init {
 	$coverage = ">".($coverage-1) if $coverage == $coverage_level->[-1];
 	push @tmp, ("17:resequencing coverage: $coverage" => '');
       }
+    }
+
+
+    # SARA snps ----------------------------------------------------
+    if ($ref_allele eq $conseq_alleles[0]) { # if 'negative snp'
+       my $bglyph = new Sanger::Graphics::Glyph::Rect({
+      'x'         => $S - $font_w_bp / 2,
+      'y'         => $height + 2,
+      'height'    => $height,
+      'width'     => $width + $font_w_bp +4,
+      'bordercolour' => 'grey70',
+      'absolutey' => 1,
+      'zmenu'     => $self->zmenu($allele, $type, $ref_allele, $conseq_alleles[0], \@tmp),
+      'zindex'    => -4,
+     });
+      my $bump_start = int($bglyph->{'x'} * $pix_per_bp);
+      $bump_start = 0 if ($bump_start < 0);
+      my $bump_end = $bump_start + int($bglyph->width()*$pix_per_bp) +1;
+      $bump_end = $bitmap_length if ($bump_end > $bitmap_length);
+      my $row = & Sanger::Graphics::Bump::bump_row( $bump_start, $bump_end, $bitmap_length, \@bitmap );
+      $bglyph->y( $voffset + $bglyph->{'y'} + ( $row * (2+$height) ) + 1 );
+      $self->push( $bglyph );
+      next;
+    }
+
+    # Normal SNPs
+    push @tmp, ("13:amino acid: $aa_change->[0] to $aa_change->[1]", '' ) if $aa_change->[1];
+
+    # Codon - make the letter for the SNP position in the codon bold
+    my $codon = $conseq_type->codon;
+    if ( $codon ) {
+      my $pos = ($conseq_type->cds_start % 3 || 3) - 1;
+      $codon =~ s/(\w{$pos})(\w)(.*)/$1<b>$2<\/b>$3/;
+      my $strand = $transcript->strand > 0 ? "+" : "-";
+      push @tmp, ("11:transcript codon ($strand strand) ".$codon => '');
     }
 
 
@@ -151,19 +153,7 @@ sub _init {
       'text'      => $label,
       'absolutey' => 1,
     });
- 
-    my $offset = $self->{'container'}->strand > 0 ? $self->{'container'}->start - 1 :  $self->{'container'}->end + 1;
-    my $chr_start = $allele->start() + $offset;
-    my $chr_end   = $allele->end() + $offset;
-    my $pos =  $chr_start;
-    if( $chr_end < $chr_start ) {
-      $pos = "between&nbsp;$chr_end&nbsp;&amp;&nbsp;$chr_start";
-    } elsif($chr_end > $chr_start ) {
-      $pos = "$chr_start&nbsp;-&nbsp;$chr_end";
-    }
 
-    my $href = "/@{[$self->{container}{_config_file_name_}]}/snpview?snp=@{[$allele->variation_name]};source=@{[$allele->source]};chr=$seq_region_name;vc_start=$chr_start";
-	
     my $bglyph = new Sanger::Graphics::Glyph::Rect({
       'x'         => $S - $W / 2,
       'y'         => $height + 2,
@@ -171,20 +161,9 @@ sub _init {
       'width'     => $W,
       'colour'    => $colour,
       'absolutey' => 1,
-      'zmenu' => {
-        'caption' => 'SNP '.$allele->variation_name,
-        "19:".$type => '',
-        @tmp,
-        "09:alleles: ".( length($ref_allele) <16 ? $ref_allele : substr($ref_allele,0,14).'..')."/".
-		  (length($allele->allele_string)<16 ? $allele->allele_string : substr($allele->allele_string,0,14).'..')
-		  => '',
-	"07:ambiguity code: ".&ambiguity_code(join "|", $allele->ref_allele_string(), $allele->allele_string) => '',
-       '01:SNP properties' => $href,
-       "03:bp $pos" => '',
-       "05:class: ".&variation_class(join "|", $allele->ref_allele_string(), $allele->allele_string) => '',
-       "15:source: ". (join ", ", @{$allele->get_all_sources ||[]}) => '',
-      }
+      'zmenu'     => $self->zmenu($allele, $type, $ref_allele, $conseq_alleles[0], \@tmp),
     });
+
     my $bump_start = int($bglyph->{'x'} * $pix_per_bp);
        $bump_start = 0 if ($bump_start < 0);
     my $bump_end = $bump_start + int($bglyph->width()*$pix_per_bp) +1;
@@ -198,5 +177,43 @@ sub _init {
 }
 
 sub error_track_name { return $_[0]->species_defs->AUTHORITY.' transcripts'; }
+
+sub zmenu {
+  my ($self, $allele, $type, $ref_allele, $alt_allele, $tmp)  = @_;
+
+  # Href
+  my $offset = $self->{'container'}->strand > 0 ? $self->{'container'}->start - 1 :  $self->{'container'}->end + 1;
+  my $chr_start = $allele->start() + $offset;
+  my $chr_end   = $allele->end() + $offset;
+  my $pos =  $chr_start;
+  if( $chr_end < $chr_start ) {
+    $pos = "between&nbsp;$chr_end&nbsp;&amp;&nbsp;$chr_start";
+    } elsif($chr_end > $chr_start ) {
+      $pos = "$chr_start&nbsp;-&nbsp;$chr_end";
+    }
+  my $seq_region_name = $self->{'container'}->seq_region_name();
+  my $href = "/@{[$self->{container}{_config_file_name_}]}/snpview?snp=@{[$allele->variation_name]};source=@{[$allele->source]};chr=$seq_region_name;vc_start=$chr_start";
+	
+
+  my $zmenu = {
+	       'caption'               => 'SNP '.$allele->variation_name,
+	       "19:".$type             => '',
+	       @$tmp,
+	       "09:alleles: ".( length($ref_allele) <16 ? $ref_allele : substr($ref_allele,0,14).'..')."/".
+		  (length($alt_allele)<16 ? $alt_allele : substr($alt_allele,0,14).'..')
+                                       => '',
+	       '01:SNP properties'     => $href,
+	       "03:bp $pos"            => '',
+	       "05:class: ".&variation_class(join "|", $ref_allele, $alt_allele) 
+                                       => '',
+	       "15:source: ". (join ", ", @{$allele->get_all_sources ||[]}) 
+	                               => '',
+	      };
+
+ $zmenu->{"07:ambiguity code: ".&ambiguity_code(join "|", $ref_allele, $alt_allele)} = "" unless $type eq 'SARA';
+
+  return ($zmenu || {});
+}
+
 
 1;
