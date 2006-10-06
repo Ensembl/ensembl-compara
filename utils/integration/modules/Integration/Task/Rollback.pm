6fb88935148e3cad0df5ebc10c8cdcbd3dcb50cd
Matthew Wood <mw4@sanger.ac.uk>
2006-10-06 16:26:43 +0000

    Added rollback task, and updated integration server.

---
+++ utils/integration/modules/Integration/Task/Rollback.pm
@@ -0,0 +1,75 @@
+package Integration::Task::Rollback;
+
+use strict;
+use warnings;
+
+use Integration::Task;
+our @ISA = qw(Integration::Task);
+
+{
+
+my %Prefix_of;
+
+sub new {
+  my ($class, %params) = @_;
+  my $self = $class->SUPER::new(%params);
+  $Prefix_of{$self} = defined $params{prefix} ? $params{prefix} : "old";
+  if ($self->prefix) {
+    my @elements = split(/\//, $self->source);
+    my $result = "";
+    my $count = 0;
+    foreach my $element (@elements) {
+      if ($count == $#elements) {
+        $result .= $self->prefix . $element;
+      } else {
+        $result .= $element . "/";
+      }
+      $count++;
+    }
+    $self->destination($result);
+  }
+  return $self;
+}
+
+sub rollback {
+  my $self = shift;
+  if (-e $self->source) {
+    my $command = "rm -r " . $self->source;
+    my $rm = `$command`;
+  }
+  my $command = "mv " . $self->destination . " " . $self->source;
+  my $mv = `$command`;
+  return 1;
+}
+
+sub purge {
+  my $self = shift;
+  my $command = "rm -r " . $self->destination;
+  my $rm = `$command`;
+  return 1;
+}
+
+sub process {
+  ### Sets up the {{source}} for future {{rollbacks}} of {{purging}}.
+  my $self = shift;
+  my $command = "mv " . $self->source . " " . $self->destination;
+  my $mv = `$command`;
+  return 1;
+}
+
+sub prefix {
+  ### a
+  my $self = shift;
+  $Prefix_of{$self} = shift if @_;
+  return $Prefix_of{$self};
+}
+
+
+sub DESTROY {
+  my $self = shift;
+  delete $Prefix_of{$self}; 
+}
+
+}
+
+1;
