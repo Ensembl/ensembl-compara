c0572e53eee2dc31ad3fe9e725598bfb722014d6
Web Admin <w3adm@sanger.ac.uk>
2006-10-06 15:35:34 +0000

    changed cache code

--- modules/EnsEMBL/Web/Wizard/Chromosome.pm
+++ modules/EnsEMBL/Web/Wizard/Chromosome.pm
@@ -337,7 +337,7 @@ sub kv_datacheck {
   my $error;
   if ($param) { 
     my $cache = new EnsEMBL::Web::File::Text($object->[1]->{'_species_defs'});
-    $cache->set_cache_filename($filename);
+    $cache->set_cache_filename('kv',$filename);
     $result = $cache->save($object, $param);
     $error = $$result{'error'};
   }
