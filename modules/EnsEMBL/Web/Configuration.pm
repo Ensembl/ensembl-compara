dd517bc7165cde5f88ac4508ebddb227efd327d1
Matthew Wood <mw4@sanger.ac.uk>
2006-10-16 16:19:34 +0000

    Updated Configuration to include javascript files. Updated transcript zmenu test.

--- modules/EnsEMBL/Web/Configuration.pm
+++ modules/EnsEMBL/Web/Configuration.pm
@@ -139,7 +139,7 @@ sub initialize_zmenu_javascript {
 sub initialize_zmenu_javascript_new {
   my $self = shift;
   $self->{page}->javascript->add_script( 'var LOADED = 0;' );
-  foreach( qw(dd_menus_32.js new_contigview_support.js new_drag_imagemap.js new_old_zmenu.js new_zmenu.js new_support.js) ) {
+  foreach( qw(dd_menus_32.js new_contigview_support.js new_drag_imagemap.js new_old_zmenu.js new_zmenu.js new_support.js prototype.js ajax_zmenu.js) ) {
     $self->{page}->javascript->add_source( "/js/$_" );
   }
   $self->{page}->javascript_div->add_div( 'jstooldiv', { 'style' => 'z-index: 200; position: absolute; visibility: hidden' } , '' );
--- modules/EnsEMBL/Web/Interface/ZMenu/ensembl_transcript.pm
+++ modules/EnsEMBL/Web/Interface/ZMenu/ensembl_transcript.pm
@@ -7,6 +7,8 @@ sub populate {
   my $self = shift;
   $self->SUPER::populate;
   $self->title($self->ident);
+  $self->add_text('text1', "This is a ZMenu");
+  $self->add_link(text => "Ensembl &hearts; AJAX", url => "http://www.ajaxian.com");
 }
 
 1;
