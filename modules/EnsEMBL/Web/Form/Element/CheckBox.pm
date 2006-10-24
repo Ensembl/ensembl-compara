9cba44ee49722a1546776821c3d8e6369d9406e9
Anne Parker <ap5@sanger.ac.uk>
2006-10-24 12:57:42 +0000

    Added notes field after checkbox

--- modules/EnsEMBL/Web/Form/Element/CheckBox.pm
+++ modules/EnsEMBL/Web/Form/Element/CheckBox.pm
@@ -23,9 +23,10 @@ sub checked  :lvalue { $_[0]->{'checked'};  }
 sub render {
   my $self = shift;
   return sprintf(
-    qq(<input type="checkbox" name="%s" id="%s" value="%s" %s/>),
+    qq(<input type="checkbox" name="%s" id="%s" value="%s" %s/>%s),
     CGI::escapeHTML( $self->name ), CGI::escapeHTML( $self->id ),
-    $self->value || 'yes', $self->checked ? 'checked="checked" ' : ''
+    $self->value || 'yes', $self->checked ? 'checked="checked" ' : '',
+    $self->notes
   );
 }
                                                                                 
