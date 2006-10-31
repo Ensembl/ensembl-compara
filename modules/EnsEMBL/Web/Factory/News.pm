df889d3f6bec4f359fa817843c2ce69c10fea0dd
Anne Parker <ap5@sanger.ac.uk>
2006-10-31 15:08:07 +0000

    Moved some queries from Factory to Object to make default object lighter

--- modules/EnsEMBL/Web/Factory/News.pm
+++ modules/EnsEMBL/Web/Factory/News.pm
@@ -25,85 +25,32 @@ sub news_adaptor {
 
 
 sub createObjects {
-### Creates a Proxy::Object of type News, containing a list of news stories
-### plus values for dropdown lists on forms (releases, species, etc) 
+### Creates a lightweight Proxy::Object of type News, containing some basic lists
+### for use with dropdown menus - actual news items are fetched on the fly
   my $self          = shift;
 
-## identify which item and/or release we're talking about
-  my $id              = $self->param( 'id' ) || $self->param('news_item_id');
+  ## allow a generic URL for current release
   my $current_release = $self->species_defs->ENSEMBL_VERSION;
-  my $release_id      = $self->param( 'rel' ) || $self->param('release_id') 
+  my $release_id      = $self->param( 'rel' ) 
+                          || $self->param('release_id')
                           || $current_release;
-
-  ## also allow a generic URL for current release
   if ($release_id eq 'current') {
     $release_id = $current_release;
   }
-  $release_id = '' if $release_id eq 'all';
-
-## create some handy lookups
-  my $current_spp = $self->news_adaptor->fetch_species($current_release);
-  my $all_spp = $self->news_adaptor->fetch_species;
-  my $all_cats = $self->news_adaptor->fetch_cats;
-  my $all_rels = $self->news_adaptor->fetch_releases;
-
-## prepare configuration for db query
-  my %where = ();
-  if ($self->param('news_cat_id')) {
-    $where{'category'} = $self->param('news_cat_id');    
-  }
-  if ($release_id) {
-    $where{'release'} = $release_id;    
+  elsif ($release_id eq 'all') {
+    $release_id = '';
   }
 
-## create generic news item objects from the database
-  warn "Fetching generic news";  
-  my $generic_items = $self->news_adaptor->fetch_news_items(\%where, 1);
+  my $species     = $self->news_adaptor->fetch_species;
+  my $categories  = $self->news_adaptor->fetch_cats;
+  my $releases    = $self->news_adaptor->fetch_releases;
 
-## sort out array of chosen species
-  my @sp_array = ();
-  if ($self->param('species')) {
-    @sp_array = ($self->param('species'));
-  }
-  else { ## create based on name of directory
-    my $sp_dir = $self->species;
-    if ($sp_dir eq 'Multi') { ## make array of all species ids
-      @sp_array = sort {$a <=> $b} keys %$all_spp;
-    }
-    else { ## look up species id from directory name
-      my %rev_hash = reverse %$all_spp;
-      @sp_array = ($rev_hash{$sp_dir});
-    }
-  }
-
-## get valid releases    
-  my $valid_rels = [];
-  my $species_items = [];
-  if (scalar(@sp_array) == 1 && $sp_array[0]) { ## single species
-    $where{'species'} = $sp_array[0];    
-    $valid_rels = $self->news_adaptor->fetch_releases({'species'=>$sp_array[0]});
-  }
-  else { ## in multi-species mode, all releases are valid
-    $valid_rels = $self->news_adaptor->fetch_releases;
-  }
-        
-## get valid species for the chosen release
-  my $valid_spp = $self->news_adaptor->fetch_species($release_id);
-
-## get species-specific news
-  warn "Fetching species news";  
-  my $species_items = $self->news_adaptor->fetch_news_items(\%where);
-  
   $self->DataObjects( new EnsEMBL::Web::Proxy::Object(
     'News', {
-      'releases'    => $all_rels,
-      'valid_rels'  => $valid_rels,
-      'all_spp'     => $all_spp,
-      'valid_spp'   => $valid_spp,
-      'current_spp' => $current_spp,
-      'all_cats'    => $all_cats,
-      'generic_items' => $generic_items,
-      'species_items' => $species_items,
+      'release_id'  => $release_id,
+      'releases'    => $releases,
+      'species'     => $species,
+      'categories'  => $categories,
     }, $self->__data
   ) ); 
 }
--- modules/EnsEMBL/Web/Object/News.pm
+++ modules/EnsEMBL/Web/Object/News.pm
@@ -6,21 +6,107 @@ no warnings "uninitialized";
 use CGI qw(escape);
 
 use EnsEMBL::Web::Object;
+use EnsEMBL::Web::Factory::News;
 
 our @ISA = qw(EnsEMBL::Web::Object);
 
-
 #------------------- ACCESSOR FUNCTIONS -----------------------------
 
-sub releases   { return $_[0]->Obj->{'releases'};   } ### a
-sub all_cats   { return $_[0]->Obj->{'all_cats'};   }
-sub all_spp   { return $_[0]->Obj->{'all_spp'};   }
-sub valid_spp   { return $_[0]->Obj->{'valid_spp'};   }
-sub current_spp   { return $_[0]->Obj->{'current_spp'};   }
-sub valid_rels   { return $_[0]->Obj->{'valid_rels'};   }
+sub release_id  { return $_[0]->Obj->{'release_id'};   }
+sub releases    { return $_[0]->Obj->{'releases'};   }
+sub all_cats    { return $_[0]->Obj->{'categories'};   }
+sub all_spp     { return $_[0]->Obj->{'species'};   }
 
-sub generic_items { return $_[0]->Obj->{'generic_items'}; }
-sub species_items { return $_[0]->Obj->{'species_items'}; }
+sub sp_array {
+  my $self = shift;
+  ## sort out array of chosen species
+  my $all_spp = $self->all_spp;
+  my @sp_array = ();
+  if ($self->param('species')) {
+    @sp_array = ($self->param('species'));
+  }
+  else { ## create based on name of directory
+    my $sp_dir = $self->species;
+    if ($sp_dir eq 'Multi') { ## make array of all species ids
+      @sp_array = sort {$a <=> $b} keys %$all_spp;
+    }
+    else { ## look up species id from directory name
+      my %rev_hash = reverse %$all_spp;
+      @sp_array = ($rev_hash{$sp_dir});
+    }
+  }
+  return \@sp_array; 
+}
+
+#------------------- DATABASE QUERIES -------------------------------
+
+sub valid_spp   { 
+  my $self = shift;
+  my $adaptor = $self->EnsEMBL::Web::Factory::News::news_adaptor;
+  my $species = $adaptor->fetch_species($self->release_id); 
+  return $species;  
+}
+
+sub current_spp   { 
+  my $self = shift;
+  my $adaptor = $self->EnsEMBL::Web::Factory::News::news_adaptor;
+  my $species = $adaptor->fetch_species($self->species_defs->ENSEMBL_VERSION); 
+  return $species;  
+}
+
+sub valid_rels   { 
+  my $self = shift;
+  my $adaptor = $self->EnsEMBL::Web::Factory::News::news_adaptor;
+  my $releases = [];
+  my @sp_array = @{$self->sp_array};
+  if (scalar(@sp_array) == 1 && $sp_array[0]) { ## single species
+    $releases = $self->news_adaptor->fetch_releases({'species'=>$sp_array[0]});
+  }
+  else { ## in multi-species mode, all releases are valid
+    $releases = $adaptor->fetch_releases;
+  }
+  return $releases;  
+}
+
+sub generic_items { 
+  my $self = shift;
+  my $adaptor = $self->EnsEMBL::Web::Factory::News::news_adaptor;
+  my $release_id = $self->release_id;
+  my %where = ();
+
+  ## set up query criteria
+  if ($self->param('news_cat_id')) {
+    $where{'category'} = $self->param('news_cat_id');
+  }
+  if ($release_id) {
+    $where{'release'} = $release_id;
+  }
+
+  my $items = $adaptor->fetch_news_items(\%where, 1);
+  return $items;
+}
+
+sub species_items { 
+  my $self = shift;
+  my $adaptor = $self->EnsEMBL::Web::Factory::News::news_adaptor;
+  my $release_id = $self->release_id;
+  my %where = ();
+
+  ## set up query criteria
+  if ($self->param('news_cat_id')) {
+    $where{'category'} = $self->param('news_cat_id');
+  }
+  if ($release_id) {
+    $where{'release'} = $release_id;
+  }
+  my @sp_array = @{$self->sp_array};
+  if (scalar(@sp_array) == 1 && $sp_array[0]) { ## single species
+    $where{'species'} = $sp_array[0];
+  }
+
+  my $items = $adaptor->fetch_news_items(\%where);
+  return $items;
+}
 
 sub sort_items {
 ### Complex custom sort for news stories. Will weed out duplicate news items if passed
