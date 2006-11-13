2746f72101482966df0f5201f675e6a89c6a6d3c
Matthew Wood <mw4@sanger.ac.uk>
2006-11-13 11:06:38 +0000

    Added user functionality to UserLinks.

--- modules/EnsEMBL/Web/Document/HTML/UserLinks.pm
+++ modules/EnsEMBL/Web/Document/HTML/UserLinks.pm
@@ -3,13 +3,18 @@ package EnsEMBL::Web::Document::HTML::UserLinks;
 use strict;
 use warnings;
 
+use EnsEMBL::Web::Object::User;
+use EnsEMBL::Web::DBSQL::UserDB;
+
 {
 
 sub render {
   my $html = "";
   my $user_id = $ENV{'ENSEMBL_USER_ID'};
   if ($user_id > 0) {
-    $html .= "Logged in";
+    my $user_adaptor = EnsEMBL::Web::DBSQL::UserDB->new();
+    my $user = $user_adaptor->find_user_by_user_id($user_id);
+    $html .= "Logged in as " . $user->name;
   } else {
     $html .= "Log in or register";
   }
