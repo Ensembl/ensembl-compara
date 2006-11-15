b6774f89ed4699860d7b28eacfdcaa2ea9fb525d
Matthew Wood <mw4@sanger.ac.uk>
2006-11-15 16:41:07 +0000

    Added form definition.

---
+++ modules/EnsEMBL/Web/Interface/FormDefinition.pm
@@ -0,0 +1,124 @@
+package EnsEMBL::Web::Interface::FormDefinition;
+
+use strict;
+use warnings;
+
+{
+
+my %Values_of;
+my %Labels_of;
+my %Descriptions_of;
+
+sub new {
+  ### c
+  my ($class, %params) = @_;
+  my $self = bless \my($scalar), $class;
+  $Values_of{$self}          = []; 
+  $Labels_of{$self}          = [];
+  $Descriptions_of{$self}    = [];
+
+  foreach my $label (@{ $params{'labels'} }) {
+    $self->add_label($label);
+  }
+
+  foreach my $value (@{ $params{'values'} }) {
+    $self->add_value($value);
+  }
+
+  foreach my $description (@{ $params{'descriptions'} }) {
+    $self->add_description($description);
+  }
+
+  return $self;
+}
+
+sub values {
+  ### a
+  my $self = shift;
+  $Values_of{$self} = shift if @_;
+  return $Values_of{$self};
+}
+
+sub add_value {
+  ### Adds a new value to the form definition, for a specified form field.
+  ### Values are returned to the database handler, if defined for a field.
+  my ($self, $value_def) = @_;
+  my ($key) = keys %{ $value_def };
+  my $value = $value_def->{$key};
+  push @{ $self->values }, { name => $key, value => $value };
+}
+
+sub value_for_field {
+  my ($self, $name) = @_;
+  foreach my $field (@{ $self->values }) {
+    if ($field->{name} eq $name) {
+      return $field->{value};
+    }
+  }
+  return undef;
+}
+
+sub labels {
+  ### a
+  my $self = shift;
+  $Labels_of{$self} = shift if @_;
+  return $Labels_of{$self};
+}
+
+sub add_label {
+  ### Defines a new label for a form field element.
+  ### Labels are used in place of form field names, if defined.
+  my ($self, $label_def)  = @_;
+  my ($key) = keys %{ $label_def };
+  my $value = $label_def->{$key};
+  push @{ $self->labels }, { name => $key, label => $value };
+}
+
+sub label_for_field {
+  ### Returns the label for a particular field.
+  my ($self, $name) = @_;
+  foreach my $field (@{ $self->labels }) {
+    if ($field->{name} eq $name) {
+      return $field->{label};
+    }
+  }
+  return undef;
+}
+
+sub descriptions {
+  ### a
+  my $self = shift;
+  $Descriptions_of{$self} = shift if @_;
+  return $Descriptions_of{$self};
+}
+
+sub add_description {
+  ### Adds a new description for a particular field.
+  my ($self, $description_def)  = @_;
+  my ($key) = keys %{ $description_def };
+  my $value = $description_def ->{$key};
+  push @{ $self->descriptions }, { name => $key, description => $value };
+}
+
+sub description_for_field {
+  ### Returns the description for a particular field.
+  my ($self, $name) = @_;
+  foreach my $field (@{ $self->descriptions }) {
+    if ($field->{name} eq $name) {
+      return $field->{description};
+    }
+  }
+  return undef;
+}
+
+sub DESTROY {
+  ### d
+  my $self = shift;
+  delete $Values_of{$self};
+  delete $Labels_of{$self};
+  delete $Descriptions_of{$self};
+}
+
+}
+
+1;
