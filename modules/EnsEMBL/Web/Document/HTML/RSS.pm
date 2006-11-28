e57803d1e330c30dac701a6f2d7fd697b25d3d74
James Smith <js5@sanger.ac.uk>
2006-11-28 15:15:37 +0000

    new

---
+++ modules/EnsEMBL/Web/Document/HTML/RSS.pm
@@ -0,0 +1,30 @@
+package EnsEMBL::Web::Document::HTML::RSS;
+use strict;
+use CGI qw(escapeHTML);
+use EnsEMBL::Web::Document::HTML;
+
+@EnsEMBL::Web::Document::HTML::RSS::ISA = qw(EnsEMBL::Web::Document::HTML);
+
+sub new      {
+  return shift->SUPER::new( 'feeds' => {} );
+}
+
+sub add      { 
+  my( $self, $URL, $title, $type ) = @_;
+  $self->{'feeds'}{$URL} = { 'title' => $title, 'type' => $type };
+}
+
+sub render   {
+  my $self = shift;
+  foreach (keys %{$self->{'feeds'}}) {
+    $self->printf(
+      qq(<link rel="alternate" type="application/%s+xml" title="%s" href="%s" />),
+      $self->{'feeds'}{$_}{'type'},
+      CGI::escapeHTML($self->{'feeds'}{$_}{'title'}),
+      $_ 
+    );
+  }
+}
+
+1;
+
