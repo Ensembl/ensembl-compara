a1ddae82880bc91615102313355f0d00ae200900
Albert Vilella <avilella@sanger.ac.uk>
2006-12-04 11:51:47 +0000

    updates for release 42

---
+++ sql/patch_41_42.sql
@@ -0,0 +1,14 @@
+
+# Updating the schema version
+
+DELETE FROM meta WHERE meta_key="schema_version";
+INSERT INTO meta (meta_key,meta_value) VALUES ("schema_version",42);
+
+# Add a UNIQUE constraint to the analysis_description.analysis_id
+
+ALTER TABLE analysis_description DROP INDEX analysis_idx;
+ALTER TABLE analysis_description ADD UNIQUE analysis_idx (analysis_id);
+
+# description: Add web_data column to analysis_description.
+
+ALTER TABLE analysis_description ADD COLUMN web_data TEXT;
