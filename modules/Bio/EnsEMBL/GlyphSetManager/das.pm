ae4256b3f0b36b6a8357288156561440e0a4106a
James Smith <js5@sanger.ac.uk>
2006-12-06 11:59:20 +0000

    using DASCOnfig objects from Session

--- modules/Bio/EnsEMBL/GlyphSetManager/das.pm
+++ modules/Bio/EnsEMBL/GlyphSetManager/das.pm
@@ -1,10 +1,8 @@
 package Bio::EnsEMBL::GlyphSetManager::das;
 
 use strict;
-use Sanger::Graphics::GlyphSetManager;
 use Bio::EnsEMBL::GlyphSet::das;
-use vars qw(@ISA);
-@ISA = qw(Sanger::Graphics::GlyphSetManager);
+use base qw(Sanger::Graphics::GlyphSetManager);
 
 ##
 ## 2001/07/03    js5        Added external DAS source code
@@ -27,16 +25,16 @@ sub init {
   }
 
   my $object = $Config->{_object};
-  my $ext_das = new EnsEMBL::Web::ExternalDAS( $object );
-  my $ds2 = $ext_das->{'data'};
-  my %das_list = map {(exists $ds2->{$_}->{'species'} && $ds2->{$_}->{'species'} ne $object->species) ? ():($_,$ds2->{$_}) } keys %$ds2;
-  foreach my $source ( sort { $das_list{$a}->{'label'} cmp $das_list{$b}->{'label'} } keys %das_list ) {
-    next unless $Config->get("managed_extdas_$source",'on') eq 'on';
-    my $das_species = $ds2->{$source}->{'species'};
-    next if( $das_species && $das_species ne '' && $das_species ne $ENV{'ENSEMBL_SPECIES'} );
-    my $extra_config = $ds2->{$source};
-    $extra_config->{'name'} = "managed_extdas_${source}";
-    $extra_config->{'url'}         ||= "http://$extra_config->{'URL'}/das";
+## Replace with session call to get DAS bits of the object...
+  
+  foreach my $source (@{ $Config->{_object}->get_session->get_das_filtered_and_sorted($ENV{'ENSEMBL_SPECIES'}) }) {
+    next unless $Config->get("managed_extdas_".$source->get_key,'on') eq 'on';
+    my $source_config = $source->get_data;
+    my $das_species   = $source_config->{'species'};
+    next if  $das_species && $das_species ne '' && $das_species ne $ENV{'ENSEMBL_SPECIES'};
+    my $extra_config = $source->get_data;
+    $extra_config->{'name'}  = "managed_extdas_${source}";
+    $extra_config->{'url'} ||= "http://$extra_config->{'URL'}/das";
 #        warn( "ADDING GLYPHSET $das_species $source" );
     $self->add_glyphset( $extra_config );        
   }
