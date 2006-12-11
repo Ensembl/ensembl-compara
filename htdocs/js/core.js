5382027ebc71c5d6aedbeb0fdb2e1a483704ff9b
Matthew Wood <mw4@sanger.ac.uk>
2006-12-11 12:19:27 +0000

    Update config link to work with new save_config script.

--- htdocs/js/core.js
+++ htdocs/js/core.js
@@ -1,5 +1,59 @@
 /* A place to dump random javascript */
 
+
+function toggle_settings_drawer() {
+  if ($('settings').style.display == 'none') {
+    new Effect.BlindDown('settings');
+  } else {
+    new Effect.BlindUp('settings');
+  }
+}
+
+function settings_drawer_change() {
+  var display_id = $('group_select').value;
+  set_style_for_class('all', 'none');
+  set_style_for_class(display_id, '');
+  save_drawer_change(display_id);
+}
+
+function getElementsByClass(searchClass,node,tag) {
+  var classElements = new Array();
+  if ( node == null )
+    node = document;
+  if ( tag == null )
+    tag = '*';
+  var els = node.getElementsByTagName(tag);
+  var elsLen = els.length;
+  var pattern = new RegExp('(^|\\s)'+searchClass+'(\\s|$)');
+  for (i = 0, j = 0; i < elsLen; i++) {
+    if ( pattern.test(els[i].className) ) {
+      classElements[j] = els[i];
+      j++;
+    }
+  }
+  return classElements;
+}
+
+function save_drawer_change(ident) {
+  var url = "/common/drawer";
+  var data = "group=" + ident;
+  var ajax_info = new Ajax.Request(url, {
+                           method: 'get',
+                           parameters: data,
+                           onComplete: drawer_change_saved
+                         });
+}
+
+function drawer_change_saved(response) {
+}
+
+function set_style_for_class(new_class,style) {
+  var elements = getElementsByClass(new_class);
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].style.display = style;
+  }
+}
+
 // Return a link based on the current URL to the archive...
 
 function addLoadEvent(func) {
