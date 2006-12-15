c42f0795b34377c4a6b71435e75e507cb5f32ff9
Anne Parker <ap5@sanger.ac.uk>
2006-12-15 14:20:37 +0000

    Fixed some code lost in merger

--- modules/EnsEMBL/Web/Object/Help.pm
+++ modules/EnsEMBL/Web/Object/Help.pm
@@ -33,8 +33,9 @@ sub send_email {
   $message .= join "\n", map {sprintf("%-16.16s %s","$_->[0]:",$_->[1])} @mail_attributes;
   $message .= "\n\nComments:\n\n@{[$self->param('comments')]}\n\n";
   my $mailer = new Mail::Mailer 'smtp', Server => "localhost";
-  my $sitetype = ucfirst(lc($self->species_defs->ENSEMBL_SITETYPE))||'Ensembl';
   my $recipient = $self->species_defs->ENSEMBL_HELPDESK_EMAIL;
+  my $sitetype = $self->species_defs->ENSEMBL_SITETYPE;
+  my $sitename = $sitetype eq 'EnsEMBL' ? 'Ensembl' : $sitetype;
   $mailer->open({ 'To' => $recipient, 'Subject' => "$sitetype website Helpdesk", 'From' => $self->param('email') });
   print $mailer $message;
   $mailer->close();
