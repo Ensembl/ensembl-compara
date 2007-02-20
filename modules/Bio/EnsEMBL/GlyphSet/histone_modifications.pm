7554dd57219a94ddaeb2eec30699f348e944aba8
Fiona Cunningham <fiona@ebi.ac.uk>
2007-02-20 17:12:26 +0000

    made it more generic

--- modules/Bio/EnsEMBL/GlyphSet/histone_modifications.pm
+++ modules/Bio/EnsEMBL/GlyphSet/histone_modifications.pm
@@ -16,6 +16,9 @@ my $track_name  = "Histone mod.";
 
 
 sub get_block_features {
+
+  ### block_features
+
   my ($self, $db, $slice) = @_;
   unless ( $self->{'block_features'} ) {
     my $feature_adaptor = $db->get_DataSetAdaptor();
@@ -31,11 +34,83 @@ sub get_block_features {
   return ( $self->{'block_features'}, $colour);
 }
 
+
+sub block_features {
+
+  ### block_features
+  ### Description: gets features for block features and passes to render_block_features
+  ### Returns 1 if draws blocks. Returns 0 if no blocks drawn
+
+  my ($self, $feature, $colour) = @_;
+  my $drawn_flag = 0;
+  foreach my $fset ( @{ $feature->get_displayable_FeatureSets() }){
+    my $display_label = $fset->display_label();
+    my $features = $fset->get_PredictedFeatures_by_Slice($self->{'container'} ) ;
+    next unless @$features;
+    $drawn_flag = 1;
+    $self->render_block_features( $features, $colour );
+    $self->render_track_name($display_label, $colour);
+  }
+
+  $self->render_space_glyph() if $drawn_flag;
+  return $drawn_flag;
+}
+
+
+sub block_features_zmenu {
+
+  ### Predicted features
+  ### Creates zmenu for predicted features track
+  ### Arg1: arrayref of Feature objects
+
+  my ($self, $f ) = @_;
+  my $offset = $self->{'container'}->strand > 0 ? $self->{'container'}->start - 1 :  $self->{'container'}->end + 1;
+  my $pos =  ($offset + $f->start )."-".($f->end+$offset);
+  my $score = sprintf("%.3f", $f->score());
+  my %zmenu = ( 
+    'caption'               => ($f->display_label || ''),
+    "03:bp:   $pos"       => '',
+    "05:description: ".($f->feature_type->description() || '-') => '',
+    "06:analysis:    ".($f->analysis->display_label() || '-')  => '',
+    "09:score: ".$score => '',
+  );
+  return \%zmenu || {};
+}
+
+
+
 sub get_wiggle_features {
+
+  ### Wiggle_plot
+
   my ($self, $db, $slice) = @_;
   my ($features) = $self->get_block_features($db, $slice);
   my $colour     = "contigblue1";
   return ($features, $colour);
+}
+
+
+sub wiggle_plot {
+
+  ### Wiggle_plot
+  ### Description: gets features for wiggle plot and passes to render_wiggle_plot
+  ### Returns 1 if draws wiggles. Returns 0 if no wiggles drawn
+
+  my( $self, $feature, $colour ) = @_;
+  my $slice = $self->{'container'};
+  my $drawn_wiggle_flag = 0;
+
+  foreach my $result_set  (  @{ $feature->get_displayable_ResultSets() } ){   
+    #get features for slice and experimtenal chip set
+    my @features = @{ $result_set->get_displayable_ResultFeatures_by_Slice($slice) };
+    next unless @features;
+
+    $drawn_wiggle_flag = 1;
+    @features   = sort { $a->score <=> $b->score  } @features;
+    my ($min_score, $max_score) = ($features[0]->score || 0, $features[-1]->score|| 0);
+    $self->render_wiggle_plot(\@features, $colour, $min_score, $max_score, $result_set->display_label);
+  }
+  return $drawn_wiggle_flag;
 }
 
 
@@ -110,179 +185,6 @@ sub _init {
 }
 
 
-
-sub wiggle_plot {
-
-  ### Wiggle plot
-  ### Description: draws wiggle plot using the score of the features
-  ### Returns 1 if draws wiggles. Returns 0 if no wiggles drawn
-
-  my( $self, $feature, $colour ) = @_;
-
-  my $drawn_wiggle_flag = 0;
-  my $slice = $self->{'container'};
-
-  foreach my $result_set  (  @{ $feature->get_displayable_ResultSets() } ){   
-
-    #get features for slice and experimtenal chip set
-    my @features = @{ $result_set->get_displayable_ResultFeatures_by_Slice($slice) };
-    next unless @features;
-
-    $drawn_wiggle_flag = 1;
-    @features   = sort { $a->score <=> $b->score  } @features;
-    my ($min_score, $max_score) = ($features[0]->score || 0, $features[-1]->score|| 0);
-
-    my $row_height = 60;
-    my $offset     = $self->_offset();
-    my $P_MAX = $max_score > 0 ? $max_score : 0;
-    my $N_MIN = $min_score < 0 ? $min_score : 0;
-    my $pix_per_score   = ($P_MAX-$N_MIN) ? $row_height / ( $P_MAX-$N_MIN ) : 0;
-    my $red_line_offset = $P_MAX * $pix_per_score;
-
-
-    # Draw the axis ------------------------------------------------
-    $self->push( new Sanger::Graphics::Glyph::Line({ # horzi line
-    'x'         => 0,
-    'y'         => $offset + $red_line_offset,
-    'width'     => $slice->length,
-    'height'    => 0,
-    'absolutey' => 1,
-    'colour'    => 'red',
-    'dotted'    => 1,
-						   }));
-
-    $self->push( new Sanger::Graphics::Glyph::Line({ # vertical line
-    'x'         => 0,
-        'y'         => $offset,
-    'width'     => 0,
-    'height'    => $row_height,
-    'absolutey' => 1,
-    'absolutex' => 1,
-    'colour'    => 'red',
-    'dotted'    => 1,
-						   }));
-
-
-    # Draw max and min score ---------------------------------------------
-    my $display_max_score = sprintf("%.2f", $P_MAX); 
-    my( $fontname_i, $fontsize_i ) = $self->get_font_details( 'innertext' );
-    my @res_i = $self->get_text_width( 0, $display_max_score, '', 
-				       'font'=>$fontname_i, 
-				       'ptsize' => $fontsize_i );
-    my $textheight_i = $res_i[3];
-    my $pix_per_bp = $self->{'config'}->transform->{'scalex'};
-
-    $self->push( new Sanger::Graphics::Glyph::Text({
-    'text'          => $display_max_score,
-    'width'         => $res_i[2],
-    'textwidth'     => $res_i[2],
-    'font'          => $fontname_i,
-    'ptsize'        => $fontsize_i,
-    'halign'        => 'right',
-    'valign'        => 'top',
-    'colour'        => 'red',
-    'height'        => $textheight_i,
-    'y'             => $offset,
-    'x'             => -4 - $res_i[2],
-    'absolutey'     => 1,
-    'absolutex'     => 1,
-    'absolutewidth' => 1,
-    }) );
-
-    if ($min_score < 0) {
-      my $display_min_score = sprintf("%.2f", $N_MIN); 
-      my @res_min = $self->get_text_width( 0, $display_min_score, '', 
-					   'font'=>$fontname_i, 
-					   'ptsize' => $fontsize_i );
-
-      $self->push(new Sanger::Graphics::Glyph::Text({
-      'text'          => $display_min_score,
-      'height'        => $textheight_i,
-      'width'         => $res_min[2],
-      'textwidth'     => $res_min[2],
-      'font'          => $fontname_i,
-      'ptsize'        => $fontsize_i,
-      'halign'        => 'right',
-      'valign'        => 'bottom',
-      'colour'        => 'red',
-      'y'             => $offset + $row_height - $textheight_i,
-      'x'             => -4 - $res_min[2],
-      'absolutey'     => 1,
-      'absolutex'     => 1,
-      'absolutewidth' => 1,
-						    }));
-    }
-
-
-    # Draw wiggly plot -------------------------------------------------
-    foreach my $f (@features) {
-      my $START = $f->start < 1 ? 1 : $f->start;
-      my $END   = $f->end   > $slice->length  ? $slice->length : $f->end;
-      my $score = $f->score || 0;
-     # warn(join('*', $f, $START, $END, $score));
-      my $y = $score < 0 ? 0 : -$score * $pix_per_score;
-
-      my $Glyph = new Sanger::Graphics::Glyph::Rect({
-      'y'         => $offset + $red_line_offset + $y,
-      'height'    => abs( $score * $pix_per_score ),
-      'x'         => $START-1,
-      'width'     => $END - $START,
-      'absolutey' => 1,
-      'title'     => sprintf("%.2f", $score),
-      'colour'    => $colour,
-						    });
-      $self->push( $Glyph );
-    }
-
-    $offset = $self->_offset($row_height);
-
-
-    # Add line of text -------------------------------------------
-    my @res_analysis = $self->get_text_width( 0,  $result_set->display_label(),
-					      '', 'font'=>$fontname_i, 
-					      'ptsize' => $fontsize_i );
-
-    $self->push( new Sanger::Graphics::Glyph::Text({
-    'text'      => $result_set->display_label(),
-    'width'     => $res_analysis[2],
-    'font'      => $fontname_i,
-    'ptsize'    => $fontsize_i,
-    'halign'    => 'left',
-    'valign'    => 'bottom',
-    'colour'    => $colour,
-    'y'         => $offset,
-    'height'    => $textheight_i,
-    'x'         => 1,
-    'absolutey' => 1,
-    'absolutex' => 1,
-						   }) );
-    
-    $self->_offset($textheight_i);  #update offset
-    $self->render_space_glyph(5);
-  } # foreach $result_set
-
-  return $drawn_wiggle_flag;
-}
-
-
-sub block_features {
-  my ($self, $feature, $colour) = @_;
-  my $drawn_flag = 0;
-  foreach my $fset ( @{ $feature->get_displayable_FeatureSets() }){
-    my $display_label = $fset->display_label();
-    my $features = $fset->get_PredictedFeatures_by_Slice($self->{'container'} ) ;
-    next unless @$features;
-    $drawn_flag = 1;
-    $self->render_block_features( $features, $colour );
-    $self->render_track_name($display_label, $colour);
-  }
-
-  $self->render_space_glyph() if $drawn_flag;
-  return $drawn_flag;
-}
-
-
-
 sub render_block_features {
 
   ### Predicted features
@@ -314,24 +216,143 @@ sub render_block_features {
 }
 
 
-sub block_features_zmenu {
+sub render_wiggle_plot {
 
-  ### Predicted features
-  ### Creates zmenu for predicted features track
-  ### Arg1: arrayref of Feature objects
+  ### Wiggle plot
+  ### Args: array_ref of features in score order, colour, min score for features, max_score for features, display label
+  ### Description: draws wiggle plot using the score of the features
+  ### Returns 1
 
-  my ($self, $f ) = @_;
-  my $offset = $self->{'container'}->strand > 0 ? $self->{'container'}->start - 1 :  $self->{'container'}->end + 1;
-  my $pos =  ($offset + $f->start )."-".($f->end+$offset);
-  my $score = sprintf("%.3f", $f->score());
-  my %zmenu = ( 
-    'caption'               => ($f->display_label || ''),
-    "03:bp:   $pos"       => '',
-    "05:description: ".($f->feature_type->description() || '-') => '',
-    "06:analysis:    ".($f->analysis->display_label() || '-')  => '',
-    "09:score: ".$score => '',
-  );
-  return \%zmenu || {};
+  my( $self, $features, $colour, $min_score, $max_score, $display_label ) = @_;
+  my $slice = $self->{'container'};
+
+  my $row_height = 60;
+  my $offset     = $self->_offset();
+  my $P_MAX = $max_score > 0 ? $max_score : 0;
+  my $N_MIN = $min_score < 0 ? $min_score : 0;
+  my $pix_per_score   = ($P_MAX-$N_MIN) ? $row_height / ( $P_MAX-$N_MIN ) : 0;
+  my $red_line_offset = $P_MAX * $pix_per_score;
+
+
+  # Draw the axis ------------------------------------------------
+  $self->push( new Sanger::Graphics::Glyph::Line({ # horzi line
+    'x'         => 0,
+    'y'         => $offset + $red_line_offset,
+    'width'     => $slice->length,
+    'height'    => 0,
+    'absolutey' => 1,
+    'colour'    => 'red',
+    'dotted'    => 1,
+						   }));
+
+  $self->push( new Sanger::Graphics::Glyph::Line({ # vertical line
+    'x'         => 0,
+    'y'         => $offset,
+    'width'     => 0,
+    'height'    => $row_height,
+    'absolutey' => 1,
+    'absolutex' => 1,
+    'colour'    => 'red',
+    'dotted'    => 1,
+						   }));
+
+
+  # Draw max and min score ---------------------------------------------
+  my $display_max_score = sprintf("%.2f", $P_MAX); 
+  my( $fontname_i, $fontsize_i ) = $self->get_font_details( 'innertext' );
+  my @res_i = $self->get_text_width( 0, $display_max_score, '', 
+				       'font'=>$fontname_i, 
+				       'ptsize' => $fontsize_i );
+  my $textheight_i = $res_i[3];
+  my $pix_per_bp = $self->{'config'}->transform->{'scalex'};
+
+  $self->push( new Sanger::Graphics::Glyph::Text({
+    'text'          => $display_max_score,
+    'width'         => $res_i[2],
+    'textwidth'     => $res_i[2],
+    'font'          => $fontname_i,
+    'ptsize'        => $fontsize_i,
+    'halign'        => 'right',
+    'valign'        => 'top',
+    'colour'        => 'red',
+    'height'        => $textheight_i,
+    'y'             => $offset,
+    'x'             => -4 - $res_i[2],
+    'absolutey'     => 1,
+    'absolutex'     => 1,
+    'absolutewidth' => 1,
+    }) );
+
+  if ($min_score < 0) {
+    my $display_min_score = sprintf("%.2f", $N_MIN); 
+    my @res_min = $self->get_text_width( 0, $display_min_score, '', 
+					   'font'=>$fontname_i, 
+					   'ptsize' => $fontsize_i );
+
+    $self->push(new Sanger::Graphics::Glyph::Text({
+      'text'          => $display_min_score,
+      'height'        => $textheight_i,
+      'width'         => $res_min[2],
+      'textwidth'     => $res_min[2],
+      'font'          => $fontname_i,
+      'ptsize'        => $fontsize_i,
+      'halign'        => 'right',
+      'valign'        => 'bottom',
+      'colour'        => 'red',
+      'y'             => $offset + $row_height - $textheight_i,
+      'x'             => -4 - $res_min[2],
+      'absolutey'     => 1,
+      'absolutex'     => 1,
+      'absolutewidth' => 1,
+						    }));
+  }
+
+
+  # Draw wiggly plot -------------------------------------------------
+  foreach my $f (@$features) {
+    my $START = $f->start < 1 ? 1 : $f->start;
+    my $END   = $f->end   > $slice->length  ? $slice->length : $f->end;
+    my $score = $f->score || 0;
+    # warn(join('*', $f, $START, $END, $score));
+    my $y = $score < 0 ? 0 : -$score * $pix_per_score;
+
+    my $Glyph = new Sanger::Graphics::Glyph::Rect({
+      'y'         => $offset + $red_line_offset + $y,
+      'height'    => abs( $score * $pix_per_score ),
+      'x'         => $START-1,
+      'width'     => $END - $START,
+      'absolutey' => 1,
+      'title'     => sprintf("%.2f", $score),
+      'colour'    => $colour,
+						    });
+    $self->push( $Glyph );
+  }
+
+  $offset = $self->_offset($row_height);
+
+
+  # Add line of text -------------------------------------------
+  my @res_analysis = $self->get_text_width( 0,  $display_label,
+					      '', 'font'=>$fontname_i, 
+					      'ptsize' => $fontsize_i );
+
+  $self->push( new Sanger::Graphics::Glyph::Text({
+    'text'      => $display_label,
+    'width'     => $res_analysis[2],
+    'font'      => $fontname_i,
+    'ptsize'    => $fontsize_i,
+    'halign'    => 'left',
+    'valign'    => 'bottom',
+    'colour'    => $colour,
+    'y'         => $offset,
+    'height'    => $textheight_i,
+    'x'         => 1,
+    'absolutey' => 1,
+    'absolutex' => 1,
+						 }) ); 
+  $self->_offset($textheight_i);  #update offset
+  $self->render_space_glyph(5);
+  return 1;
 }
 
 
@@ -399,5 +420,6 @@ sub _offset {
   $self->{'offset'} += $offset if $offset;
   return $self->{'offset'} || 0;
 }
+
 
 1;
