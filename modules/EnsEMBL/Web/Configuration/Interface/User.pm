c73070fc0f7c3707d8796bd166c1b0f1b9f7ccce
Anne Parker <ap5@sanger.ac.uk>
2007-02-21 14:00:13 +0000

    Lots more code to support user registration!

--- modules/EnsEMBL/Web/Configuration/Interface/User.pm
+++ modules/EnsEMBL/Web/Configuration/Interface/User.pm
@@ -5,6 +5,8 @@ package EnsEMBL::Web::Configuration::Interface::User;
 use strict;
 use EnsEMBL::Web::Configuration::Interface;
 use EnsEMBL::Web::Object::User;
+use EnsEMBL::Web::Tools::RandomString;
+use EnsEMBL::Web::Tools::Encryption;
 use EnsEMBL::Web::Mailer::User;
 use EnsEMBL::Web::RegObj;
 
@@ -29,20 +31,24 @@ sub save {
     }
     else {
       $interface->cgi_populate($object, $id);
-      my $success = 0; #= $interface->data->save;
+      if (!$id) {
+        $interface->data->salt(EnsEMBL::Web::Tools::RandomString::random_string(8));
+      }
+      my $success = $interface->data->save;
 
       if ($success) {
         my $user = EnsEMBL::Web::Object::User->new(
                       { adaptor => $ENSEMBL_WEB_REGISTRY->userAdaptor, id => $interface->data->id });
         if (!$user->password) { ## New user
           ## Send activation email here!
-          warn "New user saved - time to send the email!";
           my $mailer = EnsEMBL::Web::Mailer::User->new();
           $mailer->email($user->email);
-          $mailer->send_activation_email(
-                      ( 'code' => $user->salt, 'link' => $user->activation_link, 
-                        group_id => $object->param('group_id') ));
-          $url = "/common/$script?dataview=success";
+          $mailer->send_activation_email({
+                              'code' => $user->salt || '', 
+                              'link' => $user->activation_link || '', 
+                              group_id => $object->param('group_id') 
+                  });
+          $url = "/common/$script?dataview=success;user_id=".$interface->data->id;
         }
         else {
           $url = "/common/accountview";
@@ -60,8 +66,8 @@ sub confirm {
   ### Creates a panel containing a record form populated with data
   my ($self, $object, $interface) = @_;
   if (my $panel = $self->interface_panel($interface, 'confirm', 'Activate your account')) {
-    $panel->add_components(qw(activate    EnsEMBL::Web::Component::Interface::User::confirm));
-    $self->add_form($panel, qw(activate   EnsEMBL::Web::Component::Interface::User::confirm_form));
+    $panel->add_components(qw(confirm    EnsEMBL::Web::Component::Interface::User::confirm));
+    $self->add_form($panel, qw(confirm   EnsEMBL::Web::Component::Interface::User::confirm_form));
     $self->{page}->content->add_panel($panel);
     my $type = $object->__objecttype;
     $self->{page}->set_title("Activate your account");
@@ -70,15 +76,57 @@ sub confirm {
 }
 
 sub activate {
-   ### Saves activation details to the user record and redirects to a feedback page
+  ## Checks that passwords match, are more than 6 characters long and have at least 1 digit
   my ($self, $object, $interface) = @_;
   my $script = $object->script;
-  my $url;
+  my ($url, $error);
   
-  my $primary_key = $interface->data->get_primary_key;
-  my $id = $object->param($primary_key);
+  my $pass_1 = $object->param('password'); 
+  my $pass_2 = $object->param('confirm_password');
+  if ($pass_1 ne $pass_2) {
+    $url = "/common/$script?dataview=password_error;error=mismatch";
+  }
+  elsif (length($pass_1) < 7 || $pass_1 !~ /\d+/ || $pass_1 !~ /[:alpha:]+/) {
+    $url = "/common/$script?dataview=password_error;error=insecure";
+  }
+  else {
+    if ($object->param('user_id') && $object->param('salt') && $object->param('password')) {
 
+      $interface->data->populate($object->param('user_id'));
+      my $password = $object->param('password');
+      my $salt = $interface->data->salt;
+      my $encrypted = EnsEMBL::Web::Tools::Encryption::encryptPassword($password, $salt); 
+      $interface->data->password($encrypted);
+      my $success = $interface->data->save;
 
+      if ($success) {
+        $interface->data->status('active');
+        $success = $interface->data->save;
+      }
+
+      if ($success) {
+        $url = "/common/$script?dataview=success;user_id=".$interface->data->id.';key='.$encrypted;
+      }
+      else {
+        $url = "/common/$script?dataview=failure";
+      }
+    }
+    else {
+      $url = "/common/$script?dataview=failure";
+    }
+  }
+  return $url; 
+}
+
+sub password_error {
+  my ($self, $object, $interface) = @_;
+  if (my $panel = $self->interface_panel($interface, 'error', 'Password Error')) {
+    $panel->add_components(qw(error    EnsEMBL::Web::Component::Interface::User::password_error));
+    $self->{page}->content->add_panel($panel);
+    my $type = $object->__objecttype;
+    $self->{page}->set_title("Password Error");
+  }
+  return undef;
 }
 
 1;
