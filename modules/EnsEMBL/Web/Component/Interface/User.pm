52219c9e4531fa3a00b5b4e4ad00de1bcd41f3a0
Anne Parker <ap5@sanger.ac.uk>
2007-02-21 13:58:15 +0000

    Lots of new code to support user registration

--- modules/EnsEMBL/Web/Component/Interface/User.pm
+++ modules/EnsEMBL/Web/Component/Interface/User.pm
@@ -11,13 +11,52 @@ use warnings;
 no warnings "uninitialized";
 
 sub confirm_form {
-  ### Builds an HTML form populated with a database record
+  ### Builds an partially-populated HTML form
   my($panel, $object) = @_;
 
   my $primary_key = $panel->interface->data->get_primary_key;
   my $id = $object->param($primary_key);
-  
-  my $form = _data_form($panel, $object, 'confirm');
+  if ($id) {
+    $panel->interface->data->populate($id);
+  }
+
+  my $script = $object->script;
+  my $form = EnsEMBL::Web::Form->new('confirm', "/common/$script", 'post');
+
+  $form->add_element(
+          'type'  => 'NoEdit',
+          'name'  => 'name',
+          'label' => 'Name',
+          'value' => $panel->interface->data->name,
+        );
+  $form->add_element(
+          'type'  => 'NoEdit',
+          'name'  => 'email',
+          'label' => 'Email',
+          'value' => $panel->interface->data->email,
+        );
+  $form->add_element(
+          'type'  => 'NoEdit',
+          'name'  => 'organisation',
+          'label' => 'Organisation',
+          'value' => $panel->interface->data->organisation,
+        );
+  $form->add_element(
+          'type'  => 'String',
+          'name'  => 'salt',
+          'label' => 'Activation Code',
+          'value' => '',
+        );
+  $form->add_element(
+          'type'  => 'Password',
+          'name'  => 'password',
+          'label' => 'Password',
+        );
+  $form->add_element(
+          'type'  => 'Password',
+          'name'  => 'confirm_password',
+          'label' => 'Confirm Password',
+        );
   $form->add_element(
           'type'  => 'Hidden',
           'name'  => $primary_key,
@@ -25,7 +64,6 @@ sub confirm_form {
         );
 
   ## navigation elements
-  $form->add_element( 'type' => 'Hidden', 'name' => 'db_action', 'value' => 'save');
   $form->add_element( 'type' => 'Hidden', 'name' => 'dataview', 'value' => 'activate');
   $form->add_element( 'type' => 'Submit', 'value' => 'Activate');
   return $form ;
@@ -39,9 +77,12 @@ sub confirm {
     $html .= qq(<p><strong>Thanks for confirming your email address.</strong></p><p>To start using your new Ensembl user account, just choose a password below. You'll need to use this password each time you log in to Ensembl.</p>);
   }
   else {
-    $html .= qq(<p>Before you can start using your Ensembl user account, you need to validate your email address. We've just send you an email containing an activation code. Enter this code below, and choose a password. You'll need to use this password each time you log in to Ensembl.</p>);
+    $html .= qq(<p>Before you can start using your Ensembl user account, you need to validate your email address. We've just send you an email containing an activation code. Enter this code below, and choose a password. You'll need to use this password each time you log in to Ensembl.</p>
+<p><strong>Important note:</strong> If you do not receive your activation email within the next few 
+hours, please check your spam filter or <a href="/common/helpview?node=hv_contact">contact Helpdesk</a></p>
+);
   }
-  $html .= _render_form($panel, 'confirm');
+  $html .= EnsEMBL::Web::Component::Interface::_render_form($panel, 'confirm');
   $panel->print($html);
 }
 
@@ -71,10 +112,49 @@ later, as it may simply be that our servers are very busy right now.</p>
   $panel->print($html);
 }
 
-sub failed_activation {
+sub password_error {
   my( $panel, $user) = @_;
-  my $html = qq();
+  my $error = $user->param('error');
+  my $html;
+
+  if ($error eq 'mismatch') {
+    $html = qq(<h3 class="plain">Password Mismatch</h3>
+<p>Sorry - you entered two different passwords!</p>);
+  }
+  else {
+    $html = qq(<h3 class="plain">Insecure Password</h3>
+<p>The password you entered was too short or too simple. For security, please ensure that your
+password is more than 6 characters long and contains both letters and numbers.</p>);
+  }
+  $html .= qq(<p>Please click on the Back button and try again.</p>);
+
   $panel->print($html);
 }
+
+sub failed_activation {
+  my( $panel, $user) = @_;
+  my $error = $user->param('error');
+  my $id = $user->id;
+  my $html;
+  if ($error eq 'cookie_not_set') {
+    $html = qq(
+<h3 class="plain">Could not set cookie</h3>
+<p>Sorry, we could not set a cookie to log you in. Please check your browser settings, then
+click on the Back button to try again.</p>
+);
+  }
+  else {
+    $html = qq(
+<h3 class="plain">Database problem</h3>
+<p>Sorry, we were unable to activate your user account. Please try again
+later, as it may simply be that our servers are very busy right now.</p>
+
+<p>If you continue to have problems, please <a href="/common/helpview?node=hv_contact">contact Helpdesk</a>.</p>
+
+);
+  }
+  $panel->print($html);
+}
+
 
 1;
