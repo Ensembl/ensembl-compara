f7ebc045740aae88b883cb9161c54796ea435294
Matthew Wood <mw4@sanger.ac.uk>
2007-02-28 11:10:47 +0000

    New front controller commands and filters.

---
+++ modules/EnsEMBL/Web/Controller/Command/Filter/ActivationCode.pm
@@ -0,0 +1,39 @@
+package EnsEMBL::Web::Controller::Command::Filter::ActivationCode;
+
+use strict;
+use warnings;
+
+use EnsEMBL::Web::Object::Data::User;
+use EnsEMBL::Web::RegObj;
+
+our @ISA = qw(EnsEMBL::Web::Controller::Command::Filter);
+
+{
+
+sub allow {
+  my ($self) = @_;
+  warn "Getting named: " . $self->get_action->get_named_parameter('code');
+  my $user = $self->SUPER::user($self->get_action->get_named_parameter('id'));
+  warn "USER: " . $user->name;
+  warn "SALT: " . $user->salt;
+  if ($user->salt eq $self->get_action->get_named_parameter('code')) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+sub message {
+  my $self = shift;
+  return 'Authorisation code does not match' . $self->SUPER::message;
+}
+
+sub inherit {
+  my ($self, $parent) = @_;
+  unshift @ISA, ref $parent;
+  return 1;
+}
+
+}
+
+1;
---
+++ modules/EnsEMBL/Web/Controller/Command/User/ResetPassword.pm
@@ -0,0 +1,59 @@
+package EnsEMBL::Web::Controller::Command::User::ResetPassword;
+
+use strict;
+use warnings;
+
+use Class::Std;
+use CGI;
+
+use EnsEMBL::Web::Document::Interface;
+use EnsEMBL::Web::Interface::InterfaceDef;
+
+use base 'EnsEMBL::Web::Controller::Command::User';
+
+{
+
+sub BUILD {
+  my ($self, $ident, $args) = @_; 
+  $self->add_filter(EnsEMBL::Web::Controller::Command::Filter::DataUser->new);
+  $self->add_filter(EnsEMBL::Web::Controller::Command::Filter::ActivationCode->new);
+}
+
+sub render {
+  my ($self, $action) = @_;
+  $self->set_action($action);
+  $self->filters->set_action($action);
+  if ($self->filters->allow) {
+    $self->render_page;
+  } else {
+    print "Content-type: text/html\n\n";
+    print $self->filters->message; 
+  }
+}
+
+sub render_page {
+  my $self = shift;
+  my $user = $self->filters->user($self->get_action->get_named_parameter('id'));
+  warn "RENDERING PAGE";
+  my $webpage = EnsEMBL::Web::Document::Interface::simple('User');
+  
+  my $interface = EnsEMBL::Web::Interface::InterfaceDef->new();
+  my $data =EnsEMBL::Web::Object::Data::User->new();;
+  $interface->data($user);
+  $interface->discover;
+  
+  $interface->default_view('password');
+  $interface->panel_header({'password'=>qq(<p>You can reset your password below.</p>)});
+  $interface->on_success('EnsEMBL::Web::Component::Interface::User::saved_password');
+  $interface->on_failure('EnsEMBL::Web::Component::Interface::User::failed_activation');
+  $interface->caption({'on_failure'=>'Password change failed'});
+  $interface->element_order('email', 'name');
+  $interface->script_name('user/reset_password');
+  
+  $webpage->process($interface, 'EnsEMBL::Web::Configuration::Interface::User');
+  
+  }
+  
+}
+
+1;
