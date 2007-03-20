6349c1f7aace996fbbaba18e20d0783e08013838
Stephen Rice <sr7@sanger.ac.uk>
2007-03-20 13:02:50 +0000

    temp hack to turn this off

--- modules/Bio/EnsEMBL/GlyphSet/missing.pm
+++ modules/Bio/EnsEMBL/GlyphSet/missing.pm
@@ -2,6 +2,8 @@ package Bio::EnsEMBL::GlyphSet::missing;
 use strict;
 use vars qw(@ISA);
 use Bio::EnsEMBL::GlyphSet;
+
+use warnings;
 @ISA = qw(Bio::EnsEMBL::GlyphSet);
 
 sub init_label {
@@ -12,11 +14,11 @@ sub _init {
   my ($self) = @_;
   return unless ($self->strand() == -1);
   my $tracks   = $self->{'config'}->{'missing_tracks'};
-
   my $Config        = $self->{'config'};
   my( $FONT,$FONTSIZE)  = $self->get_font_details( 'text' );
 
-  my $text_to_display = "All tracks are currently switched on";
+  #my $text_to_display = "All tracks are currently switched on";
+  my $text_to_display= "";  # temporary measure to stop incorrect info on browser until fixed properly
 
   if( $tracks > 1 ) {
     $text_to_display =  "There are currently $tracks tracks switched off, use the menus above the image to turn them on." ;
