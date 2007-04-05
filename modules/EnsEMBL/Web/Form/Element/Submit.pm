81fee20c49e5dcfee4b061deba1d00727649eb4a
Anne Parker <ap5@sanger.ac.uk>
2007-04-05 13:56:36 +0000

    Added onclick attribute to render method

--- modules/EnsEMBL/Web/Form/Element/Submit.pm
+++ modules/EnsEMBL/Web/Form/Element/Submit.pm
@@ -5,6 +5,14 @@ use CGI qw(escapeHTML);
 our @ISA = qw( EnsEMBL::Web::Form::Element );
 
 sub new { my $class = shift; return $class->SUPER::new( @_ ); }
-sub render { return sprintf( '<input type="submit" name="%s" value="%s" class="red-button" />', CGI::escapeHTML($_[0]->name) || 'submit', CGI::escapeHTML($_[0]->value) ); }
+sub render { 
+  my $self = shift;
+  my $html =  sprintf( '<input type="submit" name="%s" value="%s"', CGI::escapeHTML($self->name) || 'submit', CGI::escapeHTML($self->value) ); 
+  if ($self->onclick) {
+    $html .= ' onclick="'.CGI::escapeHTML($self->onclick).'"';
+  }
+  $html .= ' class="red-button" />';
+  return $html;
+}
 
 1;
