b1d204d83185751c7884516260ab6b733862b8c6
Matthew Wood <mw4@sanger.ac.uk>
2007-04-11 13:52:29 +0000

    Added TableName parsing module, for multiple save locations.

---
+++ modules/EnsEMBL/Web/Tools/DBSQL/TableName.pm
@@ -0,0 +1,24 @@
+package EnsEMBL::Web::Tools::DBSQL::TableName;
+
+sub parse_table_name {
+  my ($string) = @_;
+  if ($string=~ /%%(.*)%%/) {
+    warn "TEMPLATING: " . $string;
+    warn "CHECKING:" . $1;
+    my $name;
+    my $species_defs = $EnsEMBL::Web::RegObj::ENSEMBL_WEB_REGISTRY->species_defs;
+    if ($1 eq 'user_record') {
+      $name = $species_defs->ENSEMBL_USER_DATA_TABLE;
+    } elsif ($1 eq 'group_record') {
+      $name = $species_defs->ENSEMBL_GROUP_DATA_TABLE;
+    } else {
+      $name = $1;
+    }
+    $string =~ s/%%(.*)%%/$name/;
+  }
+  warn "USING: " . $string;
+  return $string;
+}
+
+
+1;
