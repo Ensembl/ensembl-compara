ceca013c0866d0dc37ece3e7312c9e917c918766
Web Admin <w3adm@sanger.ac.uk>
2007-04-13 15:51:58 +0000

    fixed url issue for search

--- modules/EnsEMBL/Web/Document/HTML/RSS.pm
+++ modules/EnsEMBL/Web/Document/HTML/RSS.pm
@@ -2,6 +2,7 @@ package EnsEMBL::Web::Document::HTML::RSS;
 use strict;
 use CGI qw(escapeHTML);
 use EnsEMBL::Web::Document::HTML;
+use EnsEMBL::Web::RegObj;
 
 @EnsEMBL::Web::Document::HTML::RSS::ISA = qw(EnsEMBL::Web::Document::HTML);
 
@@ -16,7 +17,12 @@ sub add      {
 
 sub render   {
   my $self = shift;
-  $self->printf(qq(<link rel="search" href="http://dev.ensembl.org/opensearchdescription.xml" type="application/opensearchdescription+xml" title="Ensembl" />\n));
+  $self->printf(
+    '<link rel="search" href="'.
+    $ENSEMBL_WEB_REGISTRY->species_defs->ENSEMBL_BASE_URL.
+    'opensearchdescription.xml" type="application/opensearchdescription+xml" title="Ensembl" />'.
+    "\n"
+  );
   foreach (keys %{$self->{'feeds'}}) {
     $self->printf(
       qq(<link rel="alternate" type="application/%s+xml" title="%s" href="%s" />),
