34c3ddf5197ebe2c5c6eeb7fbf3382c206073795
Web Admin <w3adm@sanger.ac.uk>
2007-04-16 11:07:43 +0000

    updated ids for alignslice view

--- modules/EnsEMBL/Web/ScriptConfig/alignsliceview.pm
+++ modules/EnsEMBL/Web/ScriptConfig/alignsliceview.pm
@@ -43,19 +43,20 @@ opt_259_Canis_familiaris on
 opt_259_Mus_musculus on
 opt_259_Rattus_norvegicus on
 
-    opt_align_261 off
-opt_261_Homo_sapiens on
-opt_261_Pan_troglodytes on
-opt_261_Macaca_mulatta on
-opt_261_Bos_taurus on
-opt_261_Canis_familiaris on
-opt_261_Gallus_gallus on
-opt_261_Mus_musculus on
-opt_261_Monodelphis_domestica on
-opt_261_Ornithorhynchus_anatinus on
-opt_261_Rattus_norvegicus on
-opt_261_constrained_elem on
-opt_261_conservation_score on
+    opt_align_267 off
+opt_267_Homo_sapiens on
+opt_267_Pan_troglodytes on
+opt_267_Macaca_mulatta on
+opt_267_Bos_taurus on
+opt_267_Canis_familiaris on
+opt_267_Gallus_gallus on
+opt_267_Mus_musculus on
+opt_267_Monodelphis_domestica on
+opt_267_Ornithorhynchus_anatinus on
+opt_267_Rattus_norvegicus on
+
+opt_268_constrained_elem on
+opt_268_conservation_score on
  ),
   map {( "opt_align_$_" => "off")} (4,93,154..400)
   );
