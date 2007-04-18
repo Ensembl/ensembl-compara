53707c5817165cfb28ca22fb882e29b0ca99b358
Anne Parker <ap5@sanger.ac.uk>
2007-04-18 09:29:31 +0000

    Alterations to nodes

--- modules/EnsEMBL/Web/Commander/Node/UserData.pm
+++ modules/EnsEMBL/Web/Commander/Node/UserData.pm
@@ -16,17 +16,20 @@ sub start {
   $self->title('Where is the Data?');
   my $text = qq(
 <ul class="spaced">
+<li><a href="/common/user_data_wizard?node_name=file_upload;previous_node=start;source=local">In a file on my local machine</a></li>
 <li>On the internet:
-  <ul>
+<ul>
+  <li><a href="/common/user_data_wizard?node_name=file_info;previous_node=start;source=url">In a file on a web server</a> (URL-based data)</li>
   <li><a href="/common/user_data_wizard?node_name=das_servers;data_type=UserDAS;DASdomain=das.ensembl.org">On an existing DAS server</a></li>
-  <li><a href="/common/user_data_wizard?node_name=file">In a file on a web server</a> (URL-based data)</li>
-  </ul>
+  <li><a href="/info/data/external_data/das/das_server.html">I would like help in setting up my own DAS server</a></li>);
+  if ($ENV{'ENSEMBL_USER_ID'}) {
+    $text .= qq(
+<li><a href="/common/user_data_wizard?node_name=account">I have already uploaded the data to my Ensembl user account</li>);
+  }
+  $text .= qq(</ul>
 </li>
-<li><a href="/common/user_data_wizard?node_name=file">In a file on my local machine</a></li>
-
-<li><a href="/common/user_data_wizard?node_name=account">I have already uploaded it to Ensembl</a> [warning if not logged in]</li>
 </ul>
-);
+  );
   $self->text_above($text);
 }
 
@@ -46,8 +49,8 @@ sub das_servers {
 
 sub das_sources {
   my $self = shift;
-
   $self->title('Add Sources');
+
   ## To Do - get sources dynamically from object
   my @sources = (
   {'value' => 'source_1', 'name' => qq(<h4 class="alt">Source 1</h4><strong>http://db.systemsbiology.net:8080/das/HumanPlasma_ALL_Ens32_P09</strong><br />Peptides from the PeptideAtlas build HumanPlasma_ALL_Ens32_P09<br /><a href="javascript:X=window.open('http://db.systemsbiology.net:8080/das/HumanPlasma_ALL_Ens32_P09', 'DAS source details', 'left=50,top=50,resizable,scrollbars=yes');X.focus();void(0);">details about PeptideAtlas build HumanPlasma_ALL_Ens32_P09</a><br /><br />)},
@@ -57,6 +60,59 @@ sub das_sources {
   $self->add_element(( type => 'MultiSelect', name => 'source', 'noescape' => 1, class => 'radiocheck1col',
                             values => \@sources ));
   $self->add_element(( type => 'String', name => 'other_source', label => 'Or other DAS source' ));
+}
+
+sub file_info {
+  my $self = shift;
+  $self->title('How is your data distributed on the genome?');
+
+  $self->add_element(( type => 'RadioButton', name => 'distribution', id => 'dist_1', value => 'global', label => 'Across the genome', checked => 'checked'));
+  $self->add_element(( type => 'RadioButton', name => 'distribution', id => 'dist_2', value => 'local', label => 'Concentrated in one region'));
+
+  ## TO DO: Replace with built-in option code
+
+}
+
+sub file_upload {
+  my $self = shift;
+  $self->title('File Upload');
+
+  $self->text_above('<p>Tip: you can upload more than one data file, and configure them as separate tracks.</p>');
+
+  my @formats = (
+    {name => '-- Please Select --', value => ''},
+    {name => 'generic', value => 'Generic'},
+    {name => 'BED', value => 'BED'},
+    {name => 'GBrowse', value => 'GBrowse'},
+    {name => 'GFF', value => 'GFF'},
+    {name => 'GTF', value => 'GTF'},
+    {name => 'LDAS', value => 'LDAS'},
+    {name => 'PSL', value => 'PSL'},
+  );
+
+  my $checked;
+  if ($self->object->param('distribution') && $self->object->param('distribution') eq 'global') {
+    $self->add_element(( type => 'Information', value => 'We recommend saving your data in an Ensembl account for quicker page loads'));
+    $checked = 'yes';
+  }
+
+  if ($ENV{'ENSEMBL_USER_ID'}) {
+    $self->add_element(( type => 'CheckBox', name => 'save', label => 'Save to my Ensembl user account', checked => $checked, notes => ''));
+  }
+  else {
+    $self->add_element(( type => 'Information', value => 'Please <a href="javascript:login_link();">log in</a> or <a href="/common/user/register">register</a> if you wish to save your data in an Ensembl account'));
+  }
+  $self->add_element(( type => 'DropDown', name => 'format', label => 'File format', select => 'select', values => \@formats));
+  $self->add_element(( type => 'String', name => 'track_name', label => 'Track name (optional)'));
+
+  if ($self->object->param('source') eq 'url') {
+    $self->add_element(( type => 'String', name => 'url', label => 'File URL' ));
+  }
+  else {
+    $self->add_element(( type => 'Text', name => 'paste', label => 'Paste file content' ));
+    $self->add_element(( type => 'File', name => 'upload', label => 'or upload file' ));
+  }
+
 }
 
 sub finish {
