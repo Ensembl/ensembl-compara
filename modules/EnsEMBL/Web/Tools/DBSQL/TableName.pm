0f7ed4e7fbc129e8a1e3a625f0a8cefeeb132b29
Anne Parker <ap5@sanger.ac.uk>
2007-05-14 15:40:26 +0000

    Added method for parsing primary key

--- modules/EnsEMBL/Web/Tools/DBSQL/TableName.pm
+++ modules/EnsEMBL/Web/Tools/DBSQL/TableName.pm
@@ -20,5 +20,26 @@ sub parse_table_name {
   return $string;
 }
 
+sub parse_primary_key {
+  my ($string) = @_;
+  if ($string=~ /%%(.*)%%/) {
+    #warn "TEMPLATING: " . $string;
+    #warn "CHECKING:" . $1;
+    my $name;
+    my $species_defs = $EnsEMBL::Web::RegObj::ENSEMBL_WEB_REGISTRY->species_defs;
+    if ($1 eq 'user_record') {
+      $name = $species_defs->ENSEMBL_USER_DATA_TABLE . '_id';
+    } elsif ($1 eq 'group_record') {
+      $name = $species_defs->ENSEMBL_GROUP_DATA_TABLE . '_id';
+    } else {
+      $name = $1 . '_id';
+    }
+    $string = $name;
+  }
+  #warn "USING: " . $string;
+  return $string;
+}
+
+
 
 1;
