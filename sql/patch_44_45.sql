8932123d114a1678f266aa06d35f92c2027120c2
Javier Herrero <jh7@sanger.ac.uk>
2007-05-18 10:10:30 +0000

    *** empty log message ***

---
+++ sql/patch_44_45.sql
@@ -0,0 +1,17 @@
+
+# Updating the schema version
+
+UPDATE meta SET meta_value = 45 where meta_key = "schema_version";
+
+# Add new column in the genomic_align_block table
+
+ALTER TABLE genomic_align_block ADD group_id bigint unsigned DEFAULT NULL;
+
+# Move "default" and "split" groups to the new column
+
+UPDATE genomic_align_block gab, genomic_align ga, genomic_align_group gag SET gab.group_id = gag.group_id
+ WHERE gab.genomic_align_block_id = ga.genomic_align_block_id AND ga.genomic_align_id = gag.genomic_align_id
+ AND gag.type IN ("default", "split");
+
+DELETE FROM genomic_algin_group WHERE type IN ("default", "split");
+
