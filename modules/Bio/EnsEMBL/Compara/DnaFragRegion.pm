869e6b8506ea02ef1a18c3c7ab50a994159ae5b3
Javier Herrero <jh7@sanger.ac.uk>
2007-05-23 14:35:58 +0000

    Clean up code. Fix POD. Add genome_db and length methods

--- modules/Bio/EnsEMBL/Compara/DnaFragRegion.pm
+++ modules/Bio/EnsEMBL/Compara/DnaFragRegion.pm
@@ -15,15 +15,59 @@ Bio::EnsEMBL::Compara::DnaFragRegion - dnafrag region on one species
 
 =head1 SYNOPSIS
 
-
+  my $slice = $dnafrag_region->slice;
+  my $dnafrag = $dnafrag_region->dnafrag;
+  my $genome_db = $dnafrag_region->genome_db;
+  my $dnafrag_start = $dnafrag_region->dnafrag_start;
+  my $dnafrag_end = $dnafrag_region->dnafrag_end;
+  my $dnafrag_strand = $dnafrag_region->dnafrag_strand;
+  my $length = $dnafrag_region->length;
 
 =head1 DESCRIPTION
 
-Describe the object here
+DnaFragRegion are the objects underlying the SyntenyRegion objects. Each synteny is
+represented as a Bio::EnsEMBL::Compara::SyntenyRegion object. Each of these objects
+contain one Bio::EnsEMBL::Compara::DnaFragRegion object per region which defines the
+synteny. For instance, for a syntenic region between human and mouse, there will be
+one DnaFragRegion object for the human region and another one for the mouse one.
 
 =head1 CONTACT
 
 Ensembl - ensembl-dev@ebi.ac.uk
+
+=head1 OBJECT ATTRIBUTES
+
+=over
+
+=item adaptor
+
+Bio::EnsEMBL::Compara::DBSQL::DnaFragRegionAdaptor object to access DB
+
+=item synteny_region_id
+
+corresponds to dnafrag.synteny_region_id (external ref.)
+
+=item dnafrag_id
+
+corresponds to dnafrag.dnafrag_id (external ref.)
+
+=item dnafrag
+
+Bio::EnsEMBL::Compara::DnaFrag object corresponding to dnafrag_id
+
+=item dnafrag_start
+
+corresponds to dnafrag_region.dnafrag_start
+
+=item dnafrag_end
+
+corresponds to dnafrag_region.dnafrag_end
+
+=item dnafrag_strand
+
+corresponds to dnafrag_region.dnafrag_strand
+
+=back
 
 =head1 APPENDIX
 
@@ -40,6 +84,20 @@ use Bio::EnsEMBL::Utils::Exception;
 
 use Bio::EnsEMBL::Compara::NestedSet;
 our @ISA = qw(Bio::EnsEMBL::Compara::NestedSet);
+
+
+=head2 new_fast
+
+  Arg         : possible keys: ADAPTOR, SYNTENY_REGION_ID, DNAFRAG_ID,
+                DNAFRAG_START, DNAFRAG_END, DNAFRAG_STRAND
+                See also parent object: Bio::EnsEMBL::Compara::NestedSet
+  Example     : none
+  Description : Object constructor.
+  Returntype  : Bio::EnsEMBL::Compara::DnaFragRegion object
+  Exceptions  : none
+  Caller      : general
+
+=cut
 
 sub new {
   my ($class, @args) = @_;
@@ -61,15 +119,16 @@ sub new {
   return $self;
 }
 
+
 =head2 new_fast
 
-  Arg [1]    : hash reference $hashref
-  Example    : none
-  Description: This is an ultra fast constructor which requires knowledge of
-               the objects internals to be used.
-  Returntype :
-  Exceptions : none
-  Caller     :
+  Arg 1       : hash reference $hashref
+  Example     : none
+  Description : This is an ultra fast constructor which requires knowledge of
+                the objects internals to be used.
+  Returntype  : Bio::EnsEMBL::Compara::DnaFragRegion object
+  Exceptions  : none
+  Caller      : general
 
 =cut
 
@@ -82,104 +141,154 @@ sub new_fast {
 
 =head2 synteny_region_id
 
- Title   : synteny_region_id
- Usage   : $obj->synteny_region_id($newval)
- Function: 
- Returns : value of synteny_region_id
- Args    : newvalue (optional)
-
+  Arg 1       : (optional) integer $synteny_region_id
+  Example     : my $synteny_region_id = $dnafrag->synteny_region_id;
+  Description : Getter/setter for the synteny_region_id attribute
+  Returntype  : integer
+  Exceptions  : none
+  Caller      : general
 
 =cut
 
-sub synteny_region_id{
-   my $obj = shift;
-   if( @_ ) {
-      my $value = shift;
-      $obj->{'synteny_region_id'} = $value;
-    }
-    return $obj->{'synteny_region_id'};
+sub synteny_region_id {
+  my $obj = shift;
 
+  if (@_) {
+    my $value = shift;
+    $obj->{'synteny_region_id'} = $value;
+  }
+
+  return $obj->{'synteny_region_id'};
 }
+
 
 =head2 dnafrag_id
 
- Title   : dnafrag_id
- Usage   : $obj->dnafrag_id($newval)
- Function: 
- Returns : value of dnafrag_id
- Args    : newvalue (optional)
-
+  Arg 1       : (optional) integer $dnafrag_id
+  Example     : my $dnafrag_id = $dnafrag->dnafrag_id;
+  Description : Getter/setter for the dnafrag_id attribute
+  Returntype  : integer
+  Exceptions  : none
+  Caller      : general
 
 =cut
 
-sub dnafrag_id{
+sub dnafrag_id {
   my $obj = shift;
-  if( @_ ) {
+
+  if (@_) {
     my $value = shift;
     $obj->{'dnafrag_id'} = $value;
   }
+
   return $obj->{'dnafrag_id'};
 }
 
-sub dnafrag_start{
-  my $obj = shift;
-  if( @_ ) {
-    my $value = shift;
-    $obj->{'dnafrag_start'} = $value;
-  }
-  return $obj->{'dnafrag_start'};
-}
 
-sub dnafrag_end{
-  my $obj = shift;
-  if( @_ ) {
-    my $value = shift;
-    $obj->{'dnafrag_end'} = $value;
-  }
-  return $obj->{'dnafrag_end'};
-}
+=head2 dnafrag_start
 
-sub dnafrag_strand{
-  my $obj = shift;
-  if( @_ ) {
-    my $value = shift;
-    $obj->{'dnafrag_strand'} = $value;
-  }
-  return $obj->{'dnafrag_strand'};
-}
-
-=head2 adaptor
-
- Title   : adaptor
- Usage   : $obj->adaptor($newval)
- Function: 
- Returns : value of adaptor
- Args    : newvalue (optional)
-
+  Arg 1       : (optional) integer $dnafrag_start
+  Example     : my $dnafrag_start = $dnafrag->dnafrag_start;
+  Description : Getter/setter for the dnafrag_start attribute
+  Returntype  : integer
+  Exceptions  : none
+  Caller      : general
 
 =cut
 
-sub adaptor{
-   my $obj = shift;
-   if( @_ ) {
-      my $value = shift;
-      $obj->{'adaptor'} = $value;
-    }
-    return $obj->{'adaptor'};
+sub dnafrag_start {
+  my $obj = shift;
 
+  if (@_) {
+    my $value = shift;
+    $obj->{'dnafrag_start'} = $value;
+  }
+
+  return $obj->{'dnafrag_start'};
 }
+
+
+=head2 dnafrag_end
+
+  Arg 1       : (optional) integer $dnafrag_end
+  Example     : my $dnafrag_end = $dnafrag->dnafrag_end;
+  Description : Getter/setter for the dnafrag_end attribute
+  Returntype  : integer
+  Exceptions  : none
+  Caller      : general
+
+=cut
+
+sub dnafrag_end {
+  my $obj = shift;
+
+  if (@_) {
+    my $value = shift;
+    $obj->{'dnafrag_end'} = $value;
+  }
+
+  return $obj->{'dnafrag_end'};
+}
+
+
+=head2 dnafrag_strand
+
+  Arg 1       : (optional) integer $dnafrag_strand
+  Example     : my $dnafrag_strand = $dnafrag->dnafrag_strand;
+  Description : Getter/setter for the dnafrag_strand attribute
+  Returntype  : integer (1 or -1)
+  Exceptions  : none
+  Caller      : general
+
+=cut
+
+sub dnafrag_strand {
+  my $obj = shift;
+
+  if (@_) {
+    my $value = shift;
+    $obj->{'dnafrag_strand'} = $value;
+  }
+
+  return $obj->{'dnafrag_strand'};
+}
+
+
+=head2 adaptor
+
+  Arg 1       : (optional) Bio::EnsEMBL::Compara::DBSQL::DnaFragRegioAdaptor $adaptor
+  Example     : my $adaptor = $dnafrag->adaptor;
+  Description : Getter/setter for the corresponding
+                Bio::EnsEMBL::Compara::DBSQL::DnaFragRegioAdaptor object
+  Returntype  : Bio::EnsEMBL::Compara::DBSQL::DnaFragRegioAdaptor object
+  Exceptions  : none
+  Caller      : general
+
+=cut
+
+sub adaptor {
+  my $obj = shift;
+
+  if (@_) {
+    my $value = shift;
+    $obj->{'adaptor'} = $value;
+  }
+
+  return $obj->{'adaptor'};
+}
+
 
 =head2 dnafrag
 
- Arg 1      : -none-
- Example    : $dnafrag = $dnafragregion->dnafrag;
- Description: Returns the Bio::EnsEMBL::Compara::DnaFrag object corresponding to this
-              Bio::EnsEMBL::Compara::DnaFragRegion object.
- Returntype : Bio::EnsEMBL::Compara::Dnafrag object
- Exceptions : warns when the corresponding Bio::EnsEMBL::Compara::GenomeDB,
-              coord_system_name, name or Bio::EnsEMBL::DBSQL::DBAdaptor
-              cannot be retrieved and returns undef.
- Caller     : $object->methodname
+  Arg 1       : -none-
+  Example     : $dnafrag = $dnafragregion->dnafrag;
+  Description : Returns the Bio::EnsEMBL::Compara::DnaFrag object corresponding to this
+                Bio::EnsEMBL::Compara::DnaFragRegion object.
+  Returntype  : Bio::EnsEMBL::Compara::Dnafrag object
+  Exceptions  : warns when the corresponding Bio::EnsEMBL::Compara::GenomeDB,
+                coord_system_name, name or Bio::EnsEMBL::DBSQL::DBAdaptor
+                cannot be retrieved and returns undef.
+  Caller      : $object->methodname
 
 =cut
 
@@ -201,17 +310,18 @@ sub dnafrag {
   return $self->{'_dnafrag'};
 }
 
+
 =head2 slice
 
- Arg 1      : -none-
- Example    : $slice = $dnafragregion->slice;
- Description: Returns the Bio::EnsEMBL::Slice object corresponding to this
-              Bio::EnsEMBL::Compara::DnaFrag object.
- Returntype : Bio::EnsEMBL::Slice object
- Exceptions : warns when the corresponding Bio::EnsEMBL::Compara::GenomeDB,
-              coord_system_name, name or Bio::EnsEMBL::DBSQL::DBAdaptor
-              cannot be retrieved and returns undef.
- Caller     : $object->methodname
+  Arg 1       : -none-
+  Example     : $slice = $dnafragregion->slice;
+  Description : Returns the Bio::EnsEMBL::Slice object corresponding to this
+                Bio::EnsEMBL::Compara::DnaFrag object.
+  Returntype  : Bio::EnsEMBL::Slice object
+  Exceptions  : warns when the corresponding Bio::EnsEMBL::Compara::GenomeDB,
+                coord_system_name, name or Bio::EnsEMBL::DBSQL::DBAdaptor
+                cannot be retrieved and returns undef.
+  Caller      : $object->methodname
 
 =cut
 
@@ -240,6 +350,49 @@ sub slice {
   }
 
   return $self->{'_slice'};
+}
+
+
+=head2 genome_db
+
+  Arg 1       : -none-
+  Example     : $genome_db = $dnafragregion->genome_db;
+  Description : Returns the Bio::EnsEMBL::Compara::GenomeDB object corresponding to this
+                Bio::EnsEMBL::Compara::DnaFragRegion object. This method is a shortcut
+                for $dnafragregion->dnafrag->genome_db
+  Returntype  : Bio::EnsEMBL::Compara::GenomeDB object
+  Exceptions  : return undef if no dnafrag can be found for this DnaFragRegion object.
+                See dnafrag method elsewhere in this document.
+  Caller      : $object->methodname
+
+=cut
+
+sub genome_db {
+  my ($self) = @_;
+
+  if ($self->dnafrag) {
+    return $self->dnafrag->genome_db;
+  }
+
+  return undef;
+}
+
+
+=head2 length
+
+  Arg 1       : -none-
+  Example     : $length = $dnafragregion->length;
+  Description : Returns the lenght of this DnaFragRegion
+  Returntype  : integer
+  Exceptions  :
+  Caller      : $object->methodname
+
+=cut
+
+sub length {
+  my ($self) = @_;
+
+  return $self->dnafrag_end - $self->dnafrag_start + 1;
 }
 
 1;
