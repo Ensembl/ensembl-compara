fa0a1150a004328adb7ac1a10455db9273d9319d
Web Admin <w3adm@sanger.ac.uk>
2007-06-04 13:05:53 +0000

    Vega alignement methods

--- sql/patch_42_43.sql
+++ sql/patch_42_43.sql
@@ -11,7 +11,7 @@ ALTER TABLE method_link add `class` varchar(50) NOT NULL default '';
 UPDATE method_link SET class = "GenomicAlignBlock.pairwise_alignment"
   WHERE type in ("BLASTZ_NET", "BLASTZ_NET_TIGHT", "BLASTZ_RECIP_NET",
   "PHUSION_BLASTN", "PHUSION_BLASTN_TIGHT", "TRANSLATED_BLAT",
-  "BLASTZ_GROUP", "BLASTZ_GROUP_TIGHT");
+  "BLASTZ_GROUP", "BLASTZ_GROUP_TIGHT", "BLASTZ_RAW", "BLASTZ_CHAIN");
 
 UPDATE method_link SET class = "GenomicAlignBlock.multiple_alignment"
   WHERE type in ("MLAGAN", "PECAN");
