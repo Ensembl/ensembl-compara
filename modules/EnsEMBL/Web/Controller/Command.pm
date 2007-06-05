3d254818230a93b9f4b3f6c4b171824e0c0dac35
Anne Parker <ap5@sanger.ac.uk>
2007-06-05 15:31:13 +0000

    Added method to render filter messages within a normal Ensembl page

--- modules/EnsEMBL/Web/Controller/Command.pm
+++ modules/EnsEMBL/Web/Controller/Command.pm
@@ -67,6 +67,30 @@ sub initialize_accessor {
   };
 }
 
+sub render_message {
+  my $self = shift;
+
+    my $webpage= new EnsEMBL::Web::Document::WebPage(
+    'renderer'   => 'Apache',
+    'outputtype' => 'HTML',
+    'scriptname' => 'filter',
+    'objecttype' => 'User',
+    'command'    => $self,
+  );
+
+  if( $webpage->has_a_problem() ) {
+    $webpage->render_error_page( $webpage->problem->[0] );
+  } else {
+    foreach my $object( @{$webpage->dataObjects} ) {
+      $webpage->configure( $object, 'message' );
+    }
+    $webpage->render();
+  }
+
+
+}
+
+
 }
 
 1;
