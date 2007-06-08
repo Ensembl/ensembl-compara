cfb6eb14d13055d4f72270abe43c40774613a2a8
Anne Parker <ap5@sanger.ac.uk>
2007-06-08 13:56:36 +0000

    Altered to dynamically use filters, plus added message-rendering function

--- modules/EnsEMBL/Web/Controller/Command.pm
+++ modules/EnsEMBL/Web/Controller/Command.pm
@@ -3,21 +3,14 @@ package EnsEMBL::Web::Controller::Command;
 use strict;
 use warnings;
 
+use EnsEMBL::Web::Controller::Command::Filter;
 use EnsEMBL::Web::Document::Interface;
 use EnsEMBL::Web::Interface::InterfaceDef;
 use EnsEMBL::Web::Object::Data::User;
 use EnsEMBL::Web::SpeciesDefs;
-
-use EnsEMBL::Web::Controller::Command::Filter;
-use EnsEMBL::Web::Controller::Command::Filter::Logging;
-use EnsEMBL::Web::Controller::Command::Filter::LoggedIn;
-use EnsEMBL::Web::Controller::Command::Filter::Authentication;
-use EnsEMBL::Web::Controller::Command::Filter::Ajax;
-use EnsEMBL::Web::Controller::Command::Filter::DataUser;
-use EnsEMBL::Web::Controller::Command::Filter::Redirect;
-use EnsEMBL::Web::Controller::Command::Filter::ActivationCode;
-
+use EnsEMBL::Web::Root;
 use Class::Std;
+
 
 {
 
@@ -35,10 +28,13 @@ sub filters {
 }
 
 sub add_filter {
-  my ($self, $filter) = @_;
-  if ($filter->isa('EnsEMBL::Web::Controller::Command::Filter')) {
-    $filter->inherit($self->get_filter);
-    $self->set_filter($filter);
+  my ($self, $class) = @_;
+  if (EnsEMBL::Web::Root::dynamic_use(undef, $class)) {
+    my $filter = $class->new();
+    if ($filter->isa('EnsEMBL::Web::Controller::Command::Filter')) {
+      $filter->inherit($self->get_filter);
+      $self->set_filter($filter);
+    }
   } else {
     warn "Failed to add filter: must be of class Filter.";;
   }
@@ -66,30 +62,6 @@ sub initialize_accessor {
     return $self->get_value($attribute);
   };
 }
-
-sub render_message {
-  my $self = shift;
-
-    my $webpage= new EnsEMBL::Web::Document::WebPage(
-    'renderer'   => 'Apache',
-    'outputtype' => 'HTML',
-    'scriptname' => 'filter',
-    'objecttype' => 'User',
-    'command'    => $self,
-  );
-
-  if( $webpage->has_a_problem() ) {
-    $webpage->render_error_page( $webpage->problem->[0] );
-  } else {
-    foreach my $object( @{$webpage->dataObjects} ) {
-      $webpage->configure( $object, 'message' );
-    }
-    $webpage->render();
-  }
-
-
-}
-
 
 }
 
