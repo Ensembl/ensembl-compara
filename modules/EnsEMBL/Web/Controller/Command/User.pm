86050eb4f037319657e8ec185ca8284d93ee55d3
Anne Parker <ap5@sanger.ac.uk>
2007-06-08 13:57:16 +0000

    Moved message function from parent module

--- modules/EnsEMBL/Web/Controller/Command/User.pm
+++ modules/EnsEMBL/Web/Controller/Command/User.pm
@@ -5,4 +5,28 @@ use warnings;
 
 use base 'EnsEMBL::Web::Controller::Command';
 
+sub render_message {
+  my $self = shift;
+
+    my $webpage= new EnsEMBL::Web::Document::WebPage(
+    'renderer'   => 'Apache',
+    'outputtype' => 'HTML',
+    'scriptname' => 'filter',
+    'objecttype' => 'User',
+    'command'    => $self,
+  );
+
+  if( $webpage->has_a_problem() ) {
+    $webpage->render_error_page( $webpage->problem->[0] );
+  } else {
+    foreach my $object( @{$webpage->dataObjects} ) {
+      $webpage->configure( $object, 'message' );
+    }
+    $webpage->render();
+  }
+
+
+}
+
+
 1;
