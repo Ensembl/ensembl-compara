76fce168703f40825fc8a8d6ee3ead94baee3ca2
Javier Herrero <jh7@sanger.ac.uk>
2007-06-08 12:54:11 +0000

    Fix POD and clean up the code

--- modules/Bio/EnsEMBL/Compara/SyntenyRegion.pm
+++ modules/Bio/EnsEMBL/Compara/SyntenyRegion.pm
@@ -11,7 +11,7 @@
 
 =head1 NAME
 
-Bio::EnsEMBL::Compara::SyntenyRegion - Synteny region on one species
+Bio::EnsEMBL::Compara::SyntenyRegion - Synteny region
 
 =head1 SYNOPSIS
 
@@ -19,14 +19,19 @@ Bio::EnsEMBL::Compara::SyntenyRegion - Synteny region on one species
 
   print $this_synteny_region->method_link_species_set_id;
 
-  my $these_dnafrag_regions = $this_synteny_region->children();
+  my $these_dnafrag_regions = $this_synteny_region->get_all_DnaFragRegions();
   foreach my $this_dnafrag_region (@$these_dnafrag_regions) {
-    print $this_dnafrag_region->dnafrag->genome_db->name, ": ", $this_dnafrag_region->slice->name, "\n";
+    print $this_dnafrag_region->genome_db->name, ": ", $this_dnafrag_region->slice->name, "\n";
   }
 
 =head1 DESCRIPTION
 
-Describe the object here
+A Bio::EnsEMBL::Compara::SyntenyRegion object is a container of Bio::EnsEMBL::Compara::DnaFragRegion
+objects. Each Bio::EnsEMBL::Compara::DnaFragRegion represent a genomic region which is in synteny
+with the other regions represented in the Bio::EnsEMBL::Compara::SyntenyRegion object.
+
+Also, the Bio::EnsEMBL::Compara::SyntenyRegion object implicitly contains a
+Bio::EnsEMBL::Compara::MethodLinkSpeciesSet object which defines the type of synteny.
 
 =head1 OBJECT ATTRIBUTES
 
@@ -69,19 +74,35 @@ use Bio::EnsEMBL::Utils::Exception;
 use Bio::EnsEMBL::Compara::NestedSet;
 our @ISA = qw(Bio::EnsEMBL::Compara::NestedSet);
 
+=head2 new_fast
+
+  Arg [-DBID] : integer $dbID
+  Arg [-METHOD_LINK_SPECIES_SET_ID]
+              : integer $method_link_species_set_id
+  Arg [-ADAPTOR]
+              : Bio::EnsEMBL::Compara::DBSQL::SyntenyRegionAdaptor $adaptor
+  Example     : none
+  Description : This is the default constructor
+  Returntype  : Bio::EnsEMBL::Compara::SyntenyRegion object
+  Exceptions  : none
+  Caller      :
+  Status      : Stable
+
+=cut
+
 sub new {
   my ($class, @args) = @_;
 
   my $self = $class->SUPER::new(@args);
 
 #  my $self = bless {}, $class;
-  
+
   if (scalar @args) {
     #do this explicitly.
-    my ($dbid, $stable_id, $method_link_species_set_id, $adaptor) = rearrange([qw(DBID STABLE_ID METHOD_LINK_SPECIES_SET_ID ADAPTOR)], @args);
+    my ($dbid, $method_link_species_set_id, $adaptor) =
+        rearrange([qw(DBID METHOD_LINK_SPECIES_SET_ID ADAPTOR)], @args);
 
     $dbid && $self->dbID($dbid);
-    $stable_id && $self->stable_id($stable_id);
     $method_link_species_set_id && $self->method_link_species_set_id($method_link_species_set_id);
     $adaptor && $self->adaptor($adaptor);
   }
@@ -89,15 +110,17 @@ sub new {
   return $self;
 }
 
+
 =head2 new_fast
 
-  Arg [1]    : hash reference $hashref
-  Example    : none
-  Description: This is an ultra fast constructor which requires knowledge of
-               the objects internals to be used.
-  Returntype :
-  Exceptions : none
-  Caller     :
+  Arg [1]     : hash reference $hashref
+  Example     : none
+  Description : This is an ultra fast constructor which requires knowledge of
+                the objects internals to be used.
+  Returntype  : Bio::EnsEMBL::Compara::SyntenyRegion object
+  Exceptions  : none
+  Caller      :
+  Status      : Stable
 
 =cut
 
@@ -110,38 +133,37 @@ sub new_fast {
 
 =head2 stable_id
 
- Title   : stable_id
- Usage   : $obj->stable_id($newval)
- Function: 
- Returns : value of stable_id
- Args    : newvalue (optional)
-
+DEPRECATED: SyntenyRegions don't have any stable id.
 
 =cut
 
-sub stable_id{
-   my $obj = shift;
-   if( @_ ) {
-      my $value = shift;
-      $obj->{'stable_id'} = $value;
-    }
-    return $obj->{'stable_id'};
+sub stable_id {
+  my $obj = shift;
 
+  deprecate("SyntenyRegions don't have any stable id.");
+
+  if( @_ ) {
+    my $value = shift;
+    $obj->{'stable_id'} = $value;
+  }
+
+  return $obj->{'stable_id'};
 }
 
 
 =head2 method_link_species_set_id
 
- Title   : method_link_species_set_id
- Usage   : $obj->method_link_species_set_id($newval)
- Function: 
- Returns : value of method_link_species_set_id
- Args    : newvalue (optional)
-
+  Arg [1]     : (optional) integer $method_link_species_set_id
+  Example     : none
+  Description : Getter/setter for the method_link_species_set_id value.
+  Returntype  : integer
+  Exceptions  : none
+  Caller      : general
+  Status      : Stable
 
 =cut
 
-sub method_link_species_set_id{
+sub method_link_species_set_id {
   my $obj = shift;
   if( @_ ) {
     my $value = shift;
@@ -150,47 +172,53 @@ sub method_link_species_set_id{
   return $obj->{'method_link_species_set_id'};
 }
 
+
 =head2 dbID
 
- Title   : dbID
- Usage   : $obj->dbID($newval)
- Function: 
- Returns : value of dbID
- Args    : newvalue (optional)
-
+  Arg [1]     : (optional) integer $dbID
+  Example     : none
+  Description : Getter/setter for the dbID value. This corresponds to
+                synteny_region.synteny_region_id
+  Returntype  : integer
+  Exceptions  : none
+  Caller      : general
+  Status      : Stable
 
 =cut
 
-sub dbID{
-   my $obj = shift;
-   if( @_ ) {
-      my $value = shift;
-      $obj->{'dbID'} = $value;
-    }
-    return $obj->{'dbID'};
+sub dbID {
+  my $obj = shift;
 
+  if (@_) {
+    my $value = shift;
+    $obj->{'dbID'} = $value;
+  }
+
+  return $obj->{'dbID'};
 }
 
 
 =head2 adaptor
 
- Title   : adaptor
- Usage   : $obj->adaptor($newval)
- Function: 
- Returns : value of adaptor
- Args    : newvalue (optional)
-
+  Arg [1]     : (optional) Bio::EnsEMBL::Compara::DBSQL::SyntenyRegionAdaptor $adaptor
+  Example     : none
+  Description : Getter/setter for the adaptor
+  Returntype  : Bio::EnsEMBL::Compara::DBSQL::SyntenyRegionAdaptor object
+  Exceptions  : none
+  Caller      : general
+  Status      : Stable
 
 =cut
 
 sub adaptor {
-   my $obj = shift;
-   if( @_ ) {
-      my $value = shift;
-      $obj->{'adaptor'} = $value;
-    }
-    return $obj->{'adaptor'};
+  my $obj = shift;
 
+  if (@_) {
+    my $value = shift;
+    $obj->{'adaptor'} = $value;
+  }
+
+  return $obj->{'adaptor'};
 }
 
 
@@ -199,11 +227,12 @@ sub adaptor {
  Arg  1     : -none-
  Example    : my $all_dnafrag_regions = $obj->get_all_DnaFragRegions();
  Description: returns all the DnaFragRegion objects for this syntenic
-              region
+              region. This method is an alias for children(), see
+              Bio::EnsEMBL::Compara::NestedSet for more details.
  Returntype : a ref. to an array of Bio::EnsEMBL::Compara::DnaFragRegion
               objects
- Exception  : 
- Caller     :
+ Exception  :
+ Caller     : general
  Status     : Stable
 
 =cut
