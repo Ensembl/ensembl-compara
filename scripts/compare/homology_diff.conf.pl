b2fd34b9eee62a41f96946651c20465c303cadc1
Abel Ureta-Vidal <abel@sanger.ac.uk>
2007-06-19 10:07:02 +0000

    config file for homology_diff.pl perl script

---
+++ scripts/compare/homology_diff.conf.pl
@@ -0,0 +1,20 @@
+[
+# homology description for set1, referred by --url1
+{ 'ortholog_one2one' => 1,
+  'ortholog_one2many' => 2,
+  'ortholog_many2many' => 3,
+  'inparalog' => 4 }
+,
+# homology description for set2, referred by --url2
+{ 'ortholog_one2one' => 1,
+  'apparent_ortholog_one2one' => 2,
+  'ortholog_one2many' => 3,
+  'ortholog_many2many' => 4,
+  'within_species_paralog' => 5,
+  'between_species_paralog' => 6 }
+#{ 'UBRH' => 1,
+#  'MBRH' => 2,
+#  'RHS' => 3,
+#  'YoungParalogues' => 4 }
+]
+;
