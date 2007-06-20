fa2c63c3bdb1f54d3a13aed865edf8310e42ee17
Albert Vilella <avilella@sanger.ac.uk>
2007-06-20 13:14:40 +0000

    a cleaner leaner example on how to use the homologies given an human external gene name

--- scripts/examples/homology1.pl
+++ scripts/examples/homology1.pl
@@ -1,37 +1,41 @@
+#!/usr/local/bin/perl
 use strict;
 use Bio::EnsEMBL::Registry;
 
-Bio::EnsEMBL::Registry->load_registry_from_db(-host=>"ensembldb.ensembl.org", -user=>"anonymous", -verbose=>'0');
-
+Bio::EnsEMBL::Registry->load_registry_from_db
+    (-host=>"ensembldb.ensembl.org", 
+     -user=>"anonymous",
+     -db_version=>'45');
 my $human_gene_adaptor =
-    Bio::EnsEMBL::Registry->get_adaptor("Homo sapiens", "core", "Gene");
-
+    Bio::EnsEMBL::Registry->get_adaptor
+    ("Homo sapiens", "core", "Gene");
 my $member_adaptor =
-    Bio::EnsEMBL::Registry->get_adaptor("Compara", "compara", "Member");
-
+    Bio::EnsEMBL::Registry->get_adaptor
+    ("Compara", "compara", "Member");
 my $homology_adaptor =
-    Bio::EnsEMBL::Registry->get_adaptor("Compara", "compara", "Homology");
+    Bio::EnsEMBL::Registry->get_adaptor
+    ("Compara", "compara", "Homology");
 
-my $genes = $human_gene_adaptor->fetch_all_by_external_name('CTDP1');
+my $genes = $human_gene_adaptor->
+   fetch_all_by_external_name('CTDP1');
 
 foreach my $gene (@$genes) {
-  my $member = $member_adaptor->fetch_by_source_stable_id("ENSEMBLGENE",
-      $gene->stable_id);
+  my $member = $member_adaptor->
+  fetch_by_source_stable_id("ENSEMBLGENE",$gene->stable_id);
   my $all_homologies = $homology_adaptor->fetch_by_Member($member);
 
   foreach my $this_homology (@$all_homologies) {
     my $description = $this_homology->description;
-    print $description, " ";
-    next unless ($description =~ /one2one/);
-    my $all_member_attributes = $this_homology->get_all_Member_Attribute();
+    next unless ($description =~ /one2one/); # if only one2one wanted
+    my $all_member_attributes = 
+    $this_homology->get_all_Member_Attribute();
     my $first_found = 0;
-    foreach my $this_member_attribute (@$all_member_attributes) {
-      my ($this_member, $this_attribute) = @$this_member_attribute;
-      my $this_member_stable_id = $this_member->stable_id;
-      printf "  %-20s %-25s", $this_member->source_name, $this_member_stable_id;
-      print " ", $this_member->chr_name, " : ", $this_member->chr_start, "-",
-          $this_member->chr_end;
-      print "\n";
+    foreach my $ma (@$all_member_attributes) {
+      1;#??
+      my ($mb, $attr) = @$ma;
+      my $label = $mb->display_label || $mb->stable_id;
+      print $mb->genome_db->short_name, ",", $label, "\t";
     }
+    print "\n";
   }
 }
