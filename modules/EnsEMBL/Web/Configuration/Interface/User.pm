47aa84d96c3e09d2e8db4b90b92b60dbb1ce339d
Anne Parker <ap5@sanger.ac.uk>
2007-07-27 11:11:40 +0000

    Removed obsolete join_by_invite method

--- modules/EnsEMBL/Web/Configuration/Interface/User.pm
+++ modules/EnsEMBL/Web/Configuration/Interface/User.pm
@@ -6,11 +6,6 @@ use strict;
 use EnsEMBL::Web::Configuration::Interface;
 use EnsEMBL::Web::Tools::RandomString;
 
-use EnsEMBL::Web::Object::User;
-use EnsEMBL::Web::Record::Group;
-use EnsEMBL::Web::Object::Data::Invite;
-use EnsEMBL::Web::RegObj;
-
 our @ISA = qw( EnsEMBL::Web::Configuration::Interface );
 
 sub save {
@@ -36,51 +31,5 @@ sub save {
   }
   return $url;
 }
-
-
-sub join_by_invite {
-  my ($self, $object, $interface) = @_;
-  my $url;
-
-  my $record_id = $object->param('record_id');
-  #warn "RECORD ID: " . $record_id;
-  if ($record_id && $record_id =~ /^\d+$/) {
-    my @records = EnsEMBL::Web::Record::Group->find_invite_by_group_record_id($record_id, { adaptor => $EnsEMBL::Web::RegObj::ENSEMBL_WEB_REGISTRY->userAdaptor });
-    my $record = $records[0];
-    $record->adaptor($EnsEMBL::Web::RegObj::ENSEMBL_WEB_REGISTRY->userAdaptor);
-    my $email = $record->email;
-
-    my $user = EnsEMBL::Web::Object::User->new({ adaptor => $EnsEMBL::Web::RegObj::ENSEMBL_WEB_REGISTRY->userAdaptor,  email => $email });
-
-    if ($user->id) {
-      my $invite = EnsEMBL::Web::Object::Data::Invite->new({id => $object->param('record_id')});
-      my $group_id = $invite->group->id;
-
-      my $group = EnsEMBL::Web::Object::Group->new(( adaptor => $ENSEMBL_WEB_REGISTRY->userAdaptor, id => $group_id ));
-      # warn "WORKING WITH USER: " . $user->id . ": " . $user->email;
-      $user->add_group($group);
-      # warn "SAVING USER";
-      $user->save;
-      $invite->status('accepted');
-      # warn "SAVING RECORD";
-      $invite->save;
-    
-      if ($ENV{'ENSEMBL_USER_ID'}) {
-        $url = "/common/user/account";
-      }
-      else {
-        $url = '/common/user/login';
-      }
-    }
-    else {
-      $url = "/common/user/register?email=$email;status=active;record_id=$record_id";
-    }
-  }
-  else {
-      $url = "/common/register?dataview=failure;error=no_record";
-  }
-  return $url;
-}
-
 
 1;
