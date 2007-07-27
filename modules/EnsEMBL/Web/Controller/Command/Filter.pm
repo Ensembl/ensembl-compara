7ef0735af6ac104410ba947f47d6b5dbae2da35a
Anne Parker <ap5@sanger.ac.uk>
2007-07-27 09:49:25 +0000

    Added ability to attach parameters directly to filter (plugs security hole created by passing parameters in CGI)

--- modules/EnsEMBL/Web/Controller/Command/Filter.pm
+++ modules/EnsEMBL/Web/Controller/Command/Filter.pm
@@ -9,6 +9,18 @@ use Class::Std;
 
 my %Action :ATTR(:get<action> :set<action>);
 
+sub BUILD {
+### We need to attach validation parameters directly to the filter, as
+### passing them via CGI rather defeats the purpose!
+  my ($self, $ident, $args) = @_;
+  if ($args && ref($args) eq 'HASH') {
+    while (my ($k, $v) = each (%$args)) {
+      my $set_method = 'set_'.$k;
+      $self->$set_method($v) if $self->can($set_method);
+    }
+  }
+}
+
 sub redirect {
   return undef;
 }
