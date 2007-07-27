369c82f3e2e62665f4c66a2e27990b054f1a65b6
Abel Ureta-Vidal <abel@sanger.ac.uk>
2007-07-27 14:04:28 +0000

    Delete dumpNewickTree.pl from the repository. The newick dumping functionality is now in dumpTreeMSA.pl

--- scripts/tree/dumpTreeMSA.pl
+++ scripts/tree/dumpTreeMSA.pl
@@ -6,16 +6,23 @@ use Bio::AlignIO;
 use Bio::EnsEMBL::Registry;
 use Getopt::Long;
 
-
-
-
 my $root_id;
 my $url;
 my $help = 0;
+my $aln_out;
+my $nh_out;
+my $nhx_out;
+my $aa = 0;
+
+$| = 1;
 
 GetOptions('help'      => \$help,
            'root_id=i' => \$root_id,
-           'url=s'     => \$url);
+           'url=s'     => \$url,
+           'aln_out=s' => \$aln_out,
+           'nh_out=s'  => \$nh_out,
+           'nhx_out=s' => \$nhx_out,
+           'aa'        => \$aa);
 
 if ($help) {
   print "
@@ -24,6 +31,10 @@ $0 --root_id 1 --url mysql://ensro\@ens-livemirror:3306/42
 --root_id integer gives the root_id to which all the gene trees are connected
 --url string      database url location of the form,
                   mysql://username[:password]\@host[:port]/[release_version]
+--aln_out string  alignment output filename (extension .emf.gz will be added)
+--nh_out string   newick output filename (extension .emf.gz will be added)
+--nhx_out string  extended newick output filename (extension .emf.gz will be added)
+--aa              dump alignment in amino acid (default is in DNA)
 
 This scripts assumes that the compara db and all the core dbs related
 to it are on the same server
@@ -60,39 +71,101 @@ my $pta = $dba->get_ProteinTreeAdaptor;
 my $localtime = localtime;
 my $root = $pta->fetch_node_by_node_id($root_id);
 
-print "##FORMAT (gene_alignment)\n";
-print "##DATE $localtime\n";
-print "##RELEASE $release\n";
+my $aln_out_fh;
+my $nh_out_fh;
+my $nhx_out_fh;
 
+if (defined $aln_out) {
+  open F1, "|gzip -c > $aln_out.gz";
+  $aln_out_fh = \*F1;
+  print $aln_out_fh "##FORMAT (gene_alignment)\n";
+  print $aln_out_fh "##DATE $localtime\n";
+  print $aln_out_fh "##RELEASE $release\n";
+}
+
+if (defined $nh_out) {
+  open F2, "|gzip -c > $nh_out.gz";
+  $nh_out_fh = \*F2;
+  print $nh_out_fh "##FORMAT (gene_tree)\n";
+  print $nh_out_fh "##DATE $localtime\n";
+  print $nh_out_fh "##RELEASE $release\n";
+}
+
+if (defined $nhx_out) {
+  open F3, "|gzip -c > $nhx_out.gz";
+  $nhx_out_fh = \*F3;
+  print $nhx_out_fh "##FORMAT (gene_tree)\n";
+  print $nhx_out_fh "##DATE $localtime\n";
+  print $nhx_out_fh "##RELEASE $release\n";
+}
 
 foreach my $child (@{$root->children}) {
-  dumpTreeMultipleAlignment($child);
+  dumpTreeMultipleAlignment($child, $aln_out_fh) if ($aln_out);
+  dumpNewickTree($child,$nh_out_fh,0) if (defined $nh_out);
+  dumpNewickTree($child,$nhx_out_fh,1) if (defined $nhx_out);
   $child->release_tree;
 #  last;
 }
 
+close $aln_out_fh;
+close $nh_out_fh;
+close $nhx_out_fh;
+
 sub dumpTreeMultipleAlignment
 {
   my $tree = shift;
+  my $fh = shift;
   my @aligned_seqs;
   foreach my $leaf (@{$tree->get_all_leaves}) {
     #SEQ organism peptide_stable_id chr sequence_start sequence_stop strand gene_stable_id display_label
     my $species = $leaf->genome_db->name;
     $species =~ s/ /_/;
-    print "SEQ $species ".$leaf->stable_id." ".$leaf->chr_name." ".$leaf->chr_start." ".$leaf->chr_end." ".$leaf->chr_strand." ".$leaf->gene_member->stable_id." ".($leaf->gene_member->display_label || "NULL") ."\n";
-    
-    my $alignment_string = $leaf->cdna_alignment_string;
-    $alignment_string =~ s/\s+//g;
+    print $fh "SEQ $species ".$leaf->stable_id." ".$leaf->chr_name." ".$leaf->chr_start." ".$leaf->chr_end." ".$leaf->chr_strand." ".$leaf->gene_member->stable_id." ".($leaf->gene_member->display_label || "NULL") ."\n";
+
+    my $alignment_string;
+    if ($aa) {
+      $alignment_string = $leaf->alignment_string;
+    } else {
+      $alignment_string = $leaf->cdna_alignment_string;
+      $alignment_string =~ s/\s+//g;
+    }
     for (my $i = 0; $i<length($alignment_string); $i++) {
       $aligned_seqs[$i] .= substr($alignment_string, $i, 1);
     }
   }
-  $tree->release_tree;
-  undef $tree;
+#  $tree->release_tree;
+#  undef $tree;
 # will need to update the script when we will produce omega score for each column
 # of the alignment.
 # print "SCORE NULL\n";
-  print "DATA\n";
-  print join("\n", @aligned_seqs);
-  print "\n//\n";
+  print $fh "DATA\n";
+  print $fh join("\n", @aligned_seqs);
+  print $fh "\n//\n";
+}
+
+sub dumpNewickTree
+{
+  my $tree = shift;
+  my $fh = shift;
+  my $nhx = shift;
+#  print STDERR "node_id: ",$tree->node_id,"\n";
+  my @aligned_seqs;
+  foreach my $leaf (@{$tree->get_all_leaves}) {
+    #SEQ organism peptide_stable_id chr sequence_start sequence_stop strand gene_stable_id display_label
+    my $species = $leaf->genome_db->name;
+    $species =~ s/ /_/;
+    print $fh "SEQ $species ".$leaf->stable_id." ".$leaf->chr_name." ".$leaf->chr_start." ".$leaf->chr_end." ".$leaf->chr_strand." ".$leaf->gene_member->stable_id." ".($leaf->gene_member->display_label || "NULL") ."\n";
+  }
+# will need to update the script when we will produce omega score for each column
+# of the alignment.
+# print "SCORE NULL\n";
+  print $fh "DATA\n";
+  if ($nhx) {
+    print $fh $tree->nhx_format;
+  } else {
+    print $fh $tree->newick_simple_format;
+  }
+  print $fh "\n//\n";
+#  $tree->release_tree;
+#  undef $tree;
 }
