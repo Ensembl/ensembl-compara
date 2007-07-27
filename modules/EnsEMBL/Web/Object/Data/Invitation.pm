a1376e21abc9390d6f071e39401741eecd8e4f9c
Anne Parker <ap5@sanger.ac.uk>
2007-07-27 09:38:46 +0000

    Additional Object::Data types for new user code

---
+++ modules/EnsEMBL/Web/Object/Data/Invitation.pm
@@ -0,0 +1,30 @@
+package EnsEMBL::Web::Object::Data::Invitation;
+
+use strict;
+use warnings;
+
+use Class::Std;
+use EnsEMBL::Web::DBSQL::MySQLAdaptor;
+use EnsEMBL::Web::Object::Data::Trackable;
+use EnsEMBL::Web::Object::Data::Record;
+
+our @ISA = qw(EnsEMBL::Web::Object::Data::Trackable  EnsEMBL::Web::Object::Data::Record);
+
+
+{
+
+sub BUILD {
+  my ($self, $ident, $args) = @_;
+  $self->type('invite');
+  $self->attach_owner('group');
+  $self->set_primary_key($self->key);
+  $self->set_adaptor(EnsEMBL::Web::DBSQL::MySQLAdaptor->new({table => $self->table }));
+  $self->add_field({ name => 'email', type => 'text' });
+  $self->add_field({ name => 'status', type => 'text' });
+  $self->add_field({ name => 'code', type => 'text' });
+  $self->populate_with_arguments($args);
+}
+
+}
+
+1;
---
+++ modules/EnsEMBL/Web/Object/Data/NewsFilter.pm
@@ -0,0 +1,29 @@
+package EnsEMBL::Web::Object::Data::NewsFilter;
+
+use strict;
+use warnings;
+
+use Class::Std;
+use EnsEMBL::Web::DBSQL::MySQLAdaptor;
+use EnsEMBL::Web::Object::Data::Trackable;
+use EnsEMBL::Web::Object::Data::Record;
+
+our @ISA = qw(EnsEMBL::Web::Object::Data::Trackable  EnsEMBL::Web::Object::Data::Record);
+
+
+{
+
+sub BUILD {
+  my ($self, $ident, $args) = @_;
+  $self->type('news');
+  $self->attach_owner($args->{'record_type'});
+  $self->set_primary_key($self->key);
+  $self->set_adaptor(EnsEMBL::Web::DBSQL::MySQLAdaptor->new({table => $self->table }));
+  #$self->add_field({ name => 'topic', type => 'text' });
+  $self->add_field({ name => 'species', type => 'text' });
+  $self->populate_with_arguments($args);
+}
+
+}
+
+1;
