fd563cbf904e4a030769722ce82243fdef4a1d0b
Anne Parker <ap5@sanger.ac.uk>
2007-07-27 09:50:40 +0000

    Added wrapper method for determining whether the 'owner' or 'admin' filter is needed by a record-based Command module

--- modules/EnsEMBL/Web/Controller/Command/User.pm
+++ modules/EnsEMBL/Web/Controller/Command/User.pm
@@ -28,5 +28,19 @@ sub render_message {
 
 }
 
+sub user_or_admin {
+  ### Chooses correct filter for shareable records, based on whether user or group record
+  my ($self, $class, $id, $owner) = @_;
+  if (EnsEMBL::Web::Root::dynamic_use(undef, $class)) { ## inherited
+    if ($owner eq 'group') {
+      my $record = $class->new({'id'=>$id, 'record_type'=>'group'});
+      $self->add_filter('EnsEMBL::Web::Controller::Command::Filter::Admin', {'group_id' => $record->webgroup_id});
+    }
+    else {
+      my $record = $class->new({'id'=>$id, 'record_type'=>'user'});
+      $self->add_filter('EnsEMBL::Web::Controller::Command::Filter::Owner', {'user_id' => $record->user->id});
+    }
+  }
+}
 
 1;
