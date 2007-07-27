8fbce468098b174a3e2aec36abdbb3d9bf2bb452
Anne Parker <ap5@sanger.ac.uk>
2007-07-27 09:22:35 +0000

    New filters for user and group records

---
+++ modules/EnsEMBL/Web/Controller/Command/Filter/InvitationValid.pm
@@ -0,0 +1,39 @@
+package EnsEMBL::Web::Controller::Command::Filter::InvitationValid;
+
+use strict;
+use warnings;
+
+use EnsEMBL::Web::Object::User;
+use EnsEMBL::Web::RegObj;
+
+our @ISA = qw(EnsEMBL::Web::Controller::Command::Filter);
+
+### Checks if a given invitation code matches the value stored in the database
+
+{
+
+sub allow {
+  my ($self) = @_;
+  my $cgi = new CGI;
+  my $invitation = EnsEMBL::Web::Object::Data::Invitation->new({id => $cgi->param('id')});
+  if ($invitation->code eq $cgi->param('code')) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+sub message {
+  my $self = shift;
+  return 'Sorry, these details could not be validated.';
+}
+
+sub inherit {
+  my ($self, $parent) = @_;
+  unshift @ISA, ref $parent;
+  return 1;
+}
+
+}
+
+1;
---
+++ modules/EnsEMBL/Web/Controller/Command/Filter/Owner.pm
@@ -0,0 +1,37 @@
+package EnsEMBL::Web::Controller::Command::Filter::Owner;
+
+use strict;
+use warnings;
+
+use EnsEMBL::Web::RegObj;
+use EnsEMBL::Web::Registry;
+
+our @ISA = qw(EnsEMBL::Web::Controller::Command::Filter);
+
+{
+
+my %USER_ID :ATTR(:set<user_id> :get<user_id>);
+
+sub allow {
+  my $self = shift;
+  my $reg = $EnsEMBL::Web::RegObj::ENSEMBL_WEB_REGISTRY;
+  if ($reg->get_user->id == $self->get_user_id) {
+    return 1;
+  }
+  return 0;
+}
+
+sub message {
+  my $self = shift;
+  return "You are not the owner of this record.";
+}
+
+sub inherit {
+  my ($self, $parent) = @_;
+  unshift @ISA, ref $parent;
+  return 1;
+}
+
+}
+
+1;
