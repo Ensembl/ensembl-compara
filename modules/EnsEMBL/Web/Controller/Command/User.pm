199d7be6f1e1a3cd3c302f328c9f3a9dade45209
Anne Parker <ap5@sanger.ac.uk>
2007-08-23 10:31:48 +0000

    Removed messaging (moved to parent)

--- modules/EnsEMBL/Web/Controller/Command/User.pm
+++ modules/EnsEMBL/Web/Controller/Command/User.pm
@@ -5,29 +5,6 @@ use warnings;
 
 use base 'EnsEMBL::Web::Controller::Command';
 
-sub render_message {
-  my $self = shift;
-
-    my $webpage= new EnsEMBL::Web::Document::WebPage(
-    'renderer'   => 'Apache',
-    'outputtype' => 'HTML',
-    'scriptname' => 'filter',
-    'objecttype' => 'User',
-    'command'    => $self,
-  );
-
-  if( $webpage->has_a_problem() ) {
-    $webpage->render_error_page( $webpage->problem->[0] );
-  } else {
-    foreach my $object( @{$webpage->dataObjects} ) {
-      $webpage->configure( $object, 'message' );
-    }
-    $webpage->render();
-  }
-
-
-}
-
 sub user_or_admin {
   ### Chooses correct filter for shareable records, based on whether user or group record
   my ($self, $class, $id, $owner) = @_;
