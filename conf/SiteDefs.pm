0a5f62cb2b7852f71aa73cd4ce138a02a3d9be87
Ensembl Account <ensembl@sanger.ac.uk>
2007-09-20 09:04:39 +0000

    Updated $VERSION, $ARCHIVE_VERSION, and $ENSEMBL_RELEASE_DATE.

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -99,9 +99,9 @@ use Sys::Hostname;
 use Exporter();
 @ISA=qw(Exporter);
 
-$VERSION                   = 46;
-$ARCHIVE_VERSION           = "Aug2007";    # Change this to the archive site for this version
-$ENSEMBL_RELEASE_DATE      = 'Aug 2007';
+$VERSION                   = 47;
+$ARCHIVE_VERSION           = "Oct2007";    # Change this to the archive site for this version
+$ENSEMBL_RELEASE_DATE      = 'Oct 2007';
 
 $ENSEMBL_MIN_SPARE_SERVERS =  5;
 $ENSEMBL_MAX_SPARE_SERVERS = 20;
