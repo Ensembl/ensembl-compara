3d22ff2342fda3f029d40ffa757712eff69fc65d
Web Admin <w3adm@sanger.ac.uk>
2007-12-10 13:30:55 +0000

    fixed compara configs

--- modules/EnsEMBL/Web/ScriptConfig/alignsliceview.pm
+++ modules/EnsEMBL/Web/ScriptConfig/alignsliceview.pm
@@ -34,29 +34,41 @@ sub init {
     image_width    700
     context    100000
 
-    opt_align_259 off
-opt_259_Homo_sapiens on
-opt_259_Pan_troglodytes on
-opt_259_Macaca_mulatta on
-opt_259_Bos_taurus on
-opt_259_Canis_familiaris on
-opt_259_Mus_musculus on
-opt_259_Rattus_norvegicus on
+    opt_align_291 off
+opt_291_Homo_sapiens on
+opt_291_Pan_troglodytes on
+opt_291_Macaca_mulatta on
+opt_291_Bos_taurus on
+opt_291_Canis_familiaris on
+opt_291_Mus_musculus on
+opt_291_Rattus_norvegicus on
 
-    opt_align_267 off
-opt_267_Homo_sapiens on
-opt_267_Pan_troglodytes on
-opt_267_Macaca_mulatta on
-opt_267_Bos_taurus on
-opt_267_Canis_familiaris on
-opt_267_Gallus_gallus on
-opt_267_Mus_musculus on
-opt_267_Monodelphis_domestica on
-opt_267_Ornithorhynchus_anatinus on
-opt_267_Rattus_norvegicus on
+    opt_align_292 off
+opt_292_Homo_sapiens on
+opt_292_Pan_troglodytes on
+opt_292_Macaca_mulatta on
+opt_292_Bos_taurus on
+opt_292_Canis_familiaris on
+opt_292_Gallus_gallus on
+opt_292_Mus_musculus on
+opt_292_Monodelphis_domestica on
+opt_292_Ornithorhynchus_anatinus on
+opt_292_Rattus_norvegicus on
 
-opt_267_constrained_elem on
-opt_267_conservation_score on
+    opt_align_294 off
+opt_294_Homo_sapiens on
+opt_294_Pan_troglodytes on
+opt_294_Macaca_mulatta on
+opt_294_Bos_taurus on
+opt_294_Canis_familiaris on
+opt_294_Gallus_gallus on
+opt_294_Mus_musculus on
+opt_294_Monodelphis_domestica on
+opt_294_Ornithorhynchus_anatinus on
+opt_294_Rattus_norvegicus on
+
+opt_292_constrained_elem on
+opt_292_conservation_score on
  ),
   map {( "opt_align_$_" => "off")} (4,93,154..400)
   );
