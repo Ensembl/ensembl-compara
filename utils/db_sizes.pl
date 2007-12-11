6d143876aab996485d69d79d029bd2f5f8050590
Web Admin <w3adm@sanger.ac.uk>
2007-12-11 04:57:19 +0000

    reformatted

--- utils/db_sizes.pl
+++ utils/db_sizes.pl
@@ -11,44 +11,26 @@ while(<STDIN>) {
 
 print qq(
 <table class="ss autocenter" style="width: 50%; margin: 0 25%;" cellspacing="0"><tbody>
-  <tr class="ss_header">
-    <th class="bottom-border left-border">Species</th>
-    <th class="bottom-border right-border">Data size (Gb)</th>
-  </tr>);
+  <tr class="ss_header"> <th class="bottom-border left-border">Species</th> <th class="bottom-border right-border">Data size (Gb)</th> </tr>);
 my $F = 1;
 foreach ( sort keys %X ) {
   printf qq(
-  <tr style="vertical-align: top;" class="bg$F">
-    <td class="left-border">%-32.32s</td>
-    <td class="right-border" style="text-align: right;">%-11.11s</td>
-  </tr>),
+  <tr style="vertical-align: top;" class="bg$F"> <td class="left-border">%-32.32s</td> <td class="right-border" style="text-align: right;">%-11.11s</td> </tr>),
   $_, sprintf( "%0.1f",$X{$_}/1024/1024);
   $F = 3-$F;
 }
 printf qq(
-  <tr>
-    <th class="left-border" >Sub-total</th>
-    <th class="right-border" style="text-align: right;">%-11.11s</th>
-  </tr>
+  <tr> <th class="left-border" >Sub-total</th> <th class="right-border" style="text-align: right;">%-11.11s</th> </tr>
 ), sprintf( "%0.1f", $T2/1024/1024);
 print qq(
-  <tr>
-    <th class="left-border" >Multi-species</th>
-    <th class="right-border">&nbsp;</th>
-  </tr>);
+  <tr> <th class="left-border" >Multi-species</th> <th class="right-border">&nbsp;</th> </tr>);
 foreach ( sort keys %Y ) {
   printf qq(
-  <tr style="vertical-align: top;" class="bg$F">
-    <td class="left-border">%-32.32s</td>
-    <td class="right-border" style="text-align: right;">%-11.11s</td>
-  </tr>),
+  <tr style="vertical-align: top;" class="bg$F"> <td class="left-border">%-32.32s</td> <td class="right-border" style="text-align: right;">%-11.11s</td> </tr>),
   $_, sprintf( "%0.1f",$Y{$_}/1024/1024);
   $F = 3-$F;
 }
 printf qq(
-  <tr>
-    <th class="left-border">Total</th>
-    <th class="right-border" style="text-align: right;">%-11.11s</th>
-  </tr>
+  <tr> <th class="left-border">Total</th> <th class="right-border" style="text-align: right;">%-11.11s</th> </tr>
 </tbody></table>
 ), sprintf( "%0.1f", $T/1024/1024);
