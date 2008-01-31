de29f06afcf59a1b3e4ed31c01b251df309d4fb4
Anne Parker <ap5@sanger.ac.uk>
2008-01-31 10:04:03 +0000

    More tidying up of utils

---
+++ utils/server_maintenance/cvs_update_modules.sh
@@ -0,0 +1,4 @@
+cvs -q up -Pd biomart-plib/ biomart-web/  ensembl/ ensembl-compara/ ensembl-draw/ ensembl-variation/ htdocs/ modules/ perl/ public-plugins/ sanger-plugins/ utils/
+
+# Not used by website
+#cvs -q up -Pd  ensembl-external ensembl-personal ensembl-test 
