9c17c5dd4af0a5008c042b7efdf033d9bc10453b
Javier Herrero <jh7@sanger.ac.uk>
2008-02-11 13:27:56 +0000

    Add genomic_align_tree table

--- sql/table.sql
+++ sql/table.sql
@@ -223,6 +223,36 @@ CREATE TABLE genomic_align_group (
 
 
 #
+# Table structure for table 'genomic_align_tree'
+# 
+#   This table stores the phylogenetic relationship between different genomic_align entries.
+#   This is required to store the tree alignments, i.e. multiple sequence alignments with
+#   ancestral sequence reconstruction. This table stores the tree underlying each tree
+#   alignments
+#
+-- primary key is a foreign key to genomic_align.genomic_align_id
+
+CREATE TABLE genomic_align_tree (
+  node_id                     bigint(20) unsigned NOT NULL AUTO_INCREMENT, # internal id, FK genomic_align.genomic_align_id
+  parent_id                   bigint(20) unsigned NOT NULL default '0',
+  root_id                     bigint(20) unsigned NOT NULL default '0',
+  left_index                  int(10) NOT NULL default '0',
+  right_index                 int(10) NOT NULL default '0',
+  left_node_id                bigint(10) NOT NULL default '0',
+  right_node_id               bigint(10) NOT NULL default '0',
+  distance_to_parent          double NOT NULL default '1',
+
+  FOREIGN KEY (node_id) REFERENCES genomic_align(genomic_align_id),
+
+  PRIMARY KEY node_id (node_id),
+  KEY parent_id (parent_id),
+  KEY root_id (root_id),
+  KEY left_index (left_index),
+  KEY right_index (right_index)
+) COLLATE=latin1_swedish_ci;
+
+
+#
 # Table structure for table 'synteny_region'
 #
 # We have now decided that Synteny is inherently pairwise
