eba653a65c04d217b7d96d94ef57ea5922247dce
Evgeny Bragin <eb4@sanger.ac.uk>
2008-02-15 12:01:43 +0000

    *** empty log message ***

--- utils/integration/support/Plugins.test.pm
+++ utils/integration/support/Plugins.test.pm
@@ -1,9 +1,6 @@
-## If you wish to use the EnsEMBL web-code from the command line, you will
-## need to hardcode the server root here
-
-## $SiteDefs::ENSEMBL_SERVERROOT = '/path to root of ensembl tree';
 
 $SiteDefs::ENSEMBL_PLUGINS = [
+    'EnsEMBL::Sanger_integration'  => $SiteDefs::ENSEMBL_SERVERROOT.'/sanger-plugins/integration',
     'EnsEMBL::Sanger_head'  => $SiteDefs::ENSEMBL_SERVERROOT.'/sanger-plugins/head',
     'EnsEMBL::Sanger_dev'   => $SiteDefs::ENSEMBL_SERVERROOT.'/sanger-plugins/dev',
     'EnsEMBL::Sanger'       => $SiteDefs::ENSEMBL_SERVERROOT.'/sanger-plugins/sanger',
