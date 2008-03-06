a3d3bcacf6fdc15a3b27408b75f29d7099310c9f
Will Spooner <whs@sanger.ac.uk>
2008-03-06 11:42:00 +0000

    SQL that removes all of the pipeline and hive tables added by ensembl-compara/sql/pipeline-tables.sql and ensembl-hive/sql/tables.sql

---
+++ sql/drop-pipeline-tables.sql
@@ -0,0 +1,19 @@
+/* The following SQL removes all of the pipeline and hive tables added
+   to the compara database as part of the production process */
+
+/* from ensembl-compara/sql/pipeline-tables.sql */
+DROP TABLE subset;
+DROP TABLE subset_member;
+DROP TABLE genome_db_extn;
+DROP TABLE genome_db_stats;
+DROP TABLE dnafrag_chunk;
+DROP TABLE dnafrag_chunk_set;
+DROP TABLE dna_collection;
+DROP TABLE genomic_align_block_job_track;
+
+/* from  ensembl-hive/sql/tables.sql */
+DROP TABLE analysis_job_file;
+DROP TABLE analysis_data;
+DROP TABLE analysis_stats;
+DROP TABLE monitor;
+
