1e6f6acdefa9cf42b1bc1073466165e4171d7f9a
Beth Yates <bp1@sanger.ac.uk>
2008-03-07 10:59:05 +0000

    changed reg. feat link to look for tfeature table in FUNCGEN rather than core DB

--- modules/EnsEMBL/Web/UserConfig/geneview.pm
+++ modules/EnsEMBL/Web/UserConfig/geneview.pm
@@ -36,14 +36,14 @@ sub init {
       'on'  => "off",
       'pos' => '12',
       'str' => 'b',
-      'available'=> 'database_tables ENSEMBL_DB.regulatory_feature', 
+      'available'=> 'database_tables ENSEMBL_FUNCGEN.feature_set', 
     },
 
  'regulatory_search_regions' => {
       'on'  => "off",
       'pos' => '13',
        'str' => 'b',
-      'available'=> 'database_tables ENSEMBL_DB.regulatory_search_region',
+      'available'=> 'database_tables ENSEMBL_FUNCGEN.feature_set',
     },
 
 
