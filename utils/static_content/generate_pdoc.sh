c2743ef93973daadf2a10cb09bca8b8448d4ae3e
Evgeny Bragin <eb4@sanger.ac.uk>
2008-03-07 15:00:49 +0000

    *** empty log message ***

--- utils/static_content/generate_pdoc.sh
+++ utils/static_content/generate_pdoc.sh
@@ -33,7 +33,7 @@
 
 . /etc/profile
 
-PERLMOD_LOC="/ensemblweb/www/www_48"   # current server root
+PERLMOD_LOC="/ensemblweb/www/www_49"   # current server root
 #PERLMOD_LOC="/ensemblweb/www/server"   # current server root
 
 PDOC_LOC="$PERLMOD_LOC/htdocs/info/using/api/Pdoc"    # where you want Pdocs created
