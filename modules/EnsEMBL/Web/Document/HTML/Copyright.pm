ff8996393d51acbd71911a77ec3d768ef2f7bc9f
Anne Parker <ap5@sanger.ac.uk>
2008-04-01 08:35:13 +0000

    Removed logos and changed links to be hard-coded to live site - better for derived websites

--- modules/EnsEMBL/Web/Document/HTML/Copyright.pm
+++ modules/EnsEMBL/Web/Document/HTML/Copyright.pm
@@ -11,11 +11,9 @@ sub render {
   $_[0]->print( qq(
    <div id="copy"><div id="a1"><div id="a2">
     <p class="center">
-    <a href="http://www.sanger.ac.uk"><img src="/img/wtsi_rev.png" alt="WTSI" title="Wellcome Trust Sanger Institute" class="float-left" style="padding-left:20px" /></a>
-    <a href="http://www.ebi.ac.uk"><img src="/img/ebi_new.gif" alt="EMBL-EBI" title="European BioInformatics Institute" class="float-right" style="padding-right:20px" /></a>
     &copy; $year <a href="http://www.sanger.ac.uk/" class="nowrap">WTSI</a> /
     <a href="http://www.ebi.ac.uk/" style="white-space:nowrap">EBI</a>.
-    Ensembl is available to <a href="/info/downloads/index.html">download for public use</a> - please see the <a href="/info/code_licence.html">code licence</a> for details.
+    Ensembl is available to <a href="http://www.ensembl.org/info/downloads/index.html">download for public use</a> - please see the <a href="http://www.ensembl.org/info/code_licence.html">code licence</a> for details.
     </p>
     </div></div></div>
     ) 
