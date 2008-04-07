09fe46a617f296f4357dce30f526fed0a5bce978
Anne Parker <ap5@sanger.ac.uk>
2008-04-07 12:07:03 +0000

    Updated code licence link

--- modules/EnsEMBL/Web/Document/HTML/Copyright.pm
+++ modules/EnsEMBL/Web/Document/HTML/Copyright.pm
@@ -13,7 +13,7 @@ sub render {
     <p class="center">
     &copy; $year <a href="http://www.sanger.ac.uk/" class="nowrap">WTSI</a> /
     <a href="http://www.ebi.ac.uk/" style="white-space:nowrap">EBI</a>.
-    Ensembl is available to <a href="http://www.ensembl.org/info/downloads/index.html">download for public use</a> - please see the <a href="http://www.ensembl.org/info/code_licence.html">code licence</a> for details.
+    Ensembl is available to <a href="http://www.ensembl.org/info/downloads/index.html">download for public use</a> - please see the <a href="http://www.ensembl.org/info/about/legal/">code licence</a> for details.
     </p>
     </div></div></div>
     ) 
