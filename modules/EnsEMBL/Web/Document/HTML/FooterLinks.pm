d659b1f62dacf3243a4aa40a22fd1eba13203428
Anne Parker <ap5@sanger.ac.uk>
2008-04-29 12:57:49 +0000

    Revised and updated footer files

---
+++ modules/EnsEMBL/Web/Document/HTML/FooterLinks.pm
@@ -0,0 +1,20 @@
+package EnsEMBL::Web::Document::HTML::FooterLinks;
+use strict;
+use CGI qw(escapeHTML);
+use EnsEMBL::Web::Document::HTML;
+use EnsEMBL::Web::RegObj;
+
+our @ISA = qw(EnsEMBL::Web::Document::HTML);
+
+sub render {
+  my $sd = $ENSEMBL_WEB_REGISTRY->species_defs;
+
+  $_[0]->printf(
+    q(
+    <div class="twocol-right right">%s release %d - %s</div>),
+    $sd->ENSEMBL_SITE_NAME, $sd->ENSEMBL_VERSION, $sd->ENSEMBL_RELEASE_DATE
+  );
+}
+
+1;
+
