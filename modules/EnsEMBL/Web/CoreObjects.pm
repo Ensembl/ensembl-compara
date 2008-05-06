2df0600ac00b7fd5bd339480bdbaf90c0918eed0
James Smith <js5@sanger.ac.uk>
2008-05-06 10:00:43 +0000

    added

---
+++ modules/EnsEMBL/Web/CoreObjects.pm
@@ -0,0 +1,185 @@
+package EnsEMBL::Web::CoreObjects;
+
+use strict;
+
+use base qw(EnsEMBL::Web::Root);
+
+sub new {
+  my( $class, $input, $dbconnection ) = @_;
+  my $self = {
+    'input'      => $input,
+    'dbc'        => $dbconnection,
+    'objects' => {
+      'transcript' => undef,
+      'gene'       => undef,
+      'location'   => undef
+    },
+    'parameters' => {}
+  };
+  bless $self, $class;
+  $self->_generate_objects;
+  return $self;
+}
+
+sub database {
+  my $self = shift;
+  return $self->{'dbc'}->get_DBAdaptor(@_);
+}
+sub transcript {
+### a
+  my $self = shift;
+  $self->{objects}{transcript} = shift if @_;
+  return $self->{objects}{transcript};
+}
+
+sub transcript_short_caption {
+  my $self = shift;
+  return '-' unless $self->transcript;
+  my $dxr = $self->transcript->display_xref;
+  my $label = $dxr ? $dxr->display_id : $self->transcript->stable_id;
+  return length( $label ) < 15 ? "Transcript: $label" : "Trans: $label";
+}
+
+sub transcript_long_caption {
+  my $self = shift;
+  return '-' unless $self->transcript;
+  my $dxr = $self->transcript->display_xref;
+  my $label = $dxr ? " (".$dxr->display_id.")" : '';
+  return "Transcript: ".$self->transcript->stable_id.$label;
+}
+
+sub gene {
+### a
+  my $self = shift;
+  $self->{objects}{gene} = shift if @_;
+  return $self->{objects}{gene};
+}
+
+sub gene_short_caption {
+  my $self = shift;
+  return '-' unless $self->gene;
+  my $dxr = $self->gene->display_xref;
+  my $label = $dxr ? $dxr->display_id : $self->gene->stable_id;
+  return "Gene: $label";
+}
+
+sub gene_long_caption {
+  my $self = shift;
+  return '-' unless $self->gene;
+  my $dxr = $self->gene->display_xref;
+  my $label = $dxr ? " (".$dxr->display_id.")" : '';
+  return "Gene: ".$self->gene->stable_id.$label;
+}
+
+sub location {
+### a
+  my $self = shift;
+  $self->{objects}{location} = shift if @_;
+  return $self->{objects}{location};
+}
+
+sub location_short_caption {
+  my $self = shift;
+  return '-' unless $self->location;
+  my $label = $self->location->seq_region_name.':'.$self->thousandify($self->location->start).'-'.$self->thousandify($self->location->end);
+  return $label;
+  if( length($label)>30) {
+    return "Loc: $label";
+  } else {
+    return "Location: $label";
+  }
+}
+
+sub location_long_caption {
+  my $self = shift;
+  return '-' unless $self->location;
+  return "Location: ".$self->location->seq_region_name.':'.$self->thousandify($self->location->start).'-'.$self->thousandify($self->location->end);
+}
+
+sub param {
+  my $self = shift;
+  return $self->{input}->param(@_);
+}
+
+sub _generate_objects {
+  my $self = shift;
+  my $db = $self->{'parameters'}{'db'} = $self->param('db') || 'core';
+  my $db_adaptor = $self->database($db);
+  if( $self->param('t') ) {
+    $self->transcript( $db_adaptor->get_TranscriptAdaptor->fetch_by_stable_id( $self->param('t')) );
+    $self->_get_gene_location_from_transcript;
+  }
+  if( !$self->transcript && $self->param('g') ) {
+    $self->gene(       $db_adaptor->get_GeneAdaptor->fetch_by_stable_id(       $self->param('g')) );
+    $self->_get_location_transcript_from_gene;
+  }
+  if( $self->param('r') ) {
+    my($r,$s,$e) = $self->param('r') =~ /^([^:]+):(\w+)-(\w+)/;
+    $self->location(   $db_adaptor->get_SliceAdaptor->fetch_by_region( 'toplevel', $r, $s, $e ) );
+  }
+  $self->_get_gene_transcript_from_location unless $self->transcript;
+  $self->{'parameters'}{'r'} = $self->location->seq_region_name.':'.$self->location->start.'-'.$self->location->end
+    if $self->location;
+  $self->{'parameters'}{'t'} = $self->transcript->stable_id if $self->transcript;
+  $self->{'parameters'}{'g'} = $self->gene->stable_id       if $self->gene;
+}
+
+sub _get_gene_location_from_transcript {
+  my $self = shift;
+  return unless $self->transcript;
+  $self->gene(
+    $self->transcript->adaptor->db->get_GeneAdaptor->fetch_by_transcript_stable_id(
+      $self->transcript->stable_id
+    )
+  );
+  $self->location( $self->transcript->feature_Slice );
+}
+
+sub _get_location_transcript_from_gene {
+  my( $self ) = @_;
+  return unless $self->gene;
+## Replace this with canonical transcript calculation!!
+  $self->transcript( sort {$a->stable_id cmp $b->stable_id } @{$self->gene->get_all_Transcripts} );
+  $self->location(   $self->gene->feature_Slice );
+}
+
+sub _get_gene_transcript_from_location {
+  my( $self ) = @_;
+  return unless $self->location;
+  my $db = $self->{'parameters'}{'db'}||'core';
+  my $genes = $self->location->get_all_Genes( undef, $db, 1 );
+  my $nearest_transcript = [ undef, undef ];
+  my $nearest_distance   = 1e20;
+
+  my $s = $self->location->start;
+  my $e = $self->location->end;
+  my $c = ($s+$e)/2;
+  my @transcripts;
+  my $distance;
+  foreach my $g ( @$genes ) {
+    foreach my $t ( @{$g->get_all_Transcripts} ) {
+      my $ts = $t->seq_region_start;
+      my $te = $t->seq_region_end;
+      if( $ts <= $c && $te >= $c ) {
+        push @transcripts, [ $g,$t ];
+        $nearest_distance = 0;
+      } else {
+        $distance = $te<$c ? $c-$te : $ts-$c;
+        if( $distance < $nearest_distance ) {
+          $nearest_transcript = [ $g, $t ];
+          $nearest_distance   = $distance;
+        }
+      }
+    }
+  }
+  if( @transcripts ) {
+    my($T) = sort { $a->[0]->stable_id cmp $b->[0]->stable_id || $a->[1]->stable_id cmp $b->[1]->stable_id } @transcripts;
+    $self->gene(       $T->[0] );
+    $self->transcript( $T->[1] );
+  } elsif( $nearest_transcript->[1] ) {
+    $self->gene(       $nearest_transcript->[0] );
+    $self->transcript( $nearest_transcript->[1] );
+  }
+}
+
+1;
---
+++ modules/EnsEMBL/Web/Document/Panel/Summary.pm
@@ -0,0 +1,43 @@
+package EnsEMBL::Web::Document::Panel::Summary;
+
+use strict;
+use Data::Dumper qw(Dumper);
+
+use base qw(EnsEMBL::Web::Document::Panel);
+
+sub _start {
+  my $self = shift;
+}
+
+sub _end   { 
+  my $self = shift;
+}
+
+sub _error {
+  my( $self, $caption, $body ) = @_;
+  $self->add_row( $caption, $body );
+}
+
+sub add_description {
+  my( $self, $description ) = @_;
+  $self->printf( qq(
+  <p>%s</p>), $description );
+}
+
+
+sub add_row {
+  my( $self, $label, $content ) =@_;
+  $self->printf( qq(
+  <dl class="summary">
+    <dt>
+      %s
+    </dt>
+    <dd>
+      %s
+    </dd>
+  </dl>),
+    $label, $content
+  );
+}
+
+1;
