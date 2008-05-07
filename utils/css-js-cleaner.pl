981457b992e9074edf2d090b06c9c7d8e723c248
James Smith <js5@sanger.ac.uk>
2008-05-07 07:59:27 +0000

    added script to remove all but the active js/css file

---
+++ utils/css-js-cleaner.pl
@@ -0,0 +1,26 @@
+#!/usr/local/bin/perl -w
+
+use strict;
+
+use FindBin qw($Bin);
+use File::Basename qw( dirname );
+
+use vars qw( $SERVERROOT );
+warn $SERVERROOT = dirname( $Bin );
+
+open I,"$SERVERROOT/conf/packed/dhtml.ini";
+my %X;
+while(<I>) {
+  chomp;
+  my($k,$v) = /^(\w+)\s*=\s*(\w+)$/;
+  $X{$k} = $v;
+}
+
+foreach my $d (qw(minified merged)) {
+  opendir DH, "$SERVERROOT/htdocs/$d";
+  while(my $f = readdir(DH)) {
+    my( $code,$type ) = split /\./,$f;
+    next unless exists $X{$type};
+    unlink "$SERVERROOT/htdocs/$d/$f" if ($type eq 'css' || $type eq 'js') && $code ne $X{$type};
+  }
+}
