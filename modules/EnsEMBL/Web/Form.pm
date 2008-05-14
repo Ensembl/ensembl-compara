4357749ceb5177cb148f4bbed473c49f842ec70e
Anne Parker <ap5@sanger.ac.uk>
2008-05-14 15:50:20 +0000

    Updated code with new layout

--- modules/EnsEMBL/Web/Form.pm
+++ modules/EnsEMBL/Web/Form.pm
@@ -2,6 +2,7 @@ package EnsEMBL::Web::Form;
 
 use strict;
 use EnsEMBL::Web::Root;
+use EnsEMBL::Web::Form::FieldSet;
 use CGI qw(escapeHTML);
 our @ISA = qw( EnsEMBL::Web::Root );
 
@@ -13,12 +14,11 @@ sub new {
         'method'   => lc($method) || 'get' ,  
         'name'     => $name,
         'id'       => $name,
-        'onSubmit' => sprintf( 'return( on_submit( %s_vars ))', $name )
+        'onSubmit' => sprintf( 'return( on_submit( %s_vars ))', $name ),
+        'class'    => '',
     },
-    '_buttons'     => {},
-    '_button_order'=> [],
-    '_multibutton' => [],
-    '_elements'    => [],
+    '_buttons'     => [],
+    '_fieldsets'   => [],
     '_form_id'     => 1
   };
   bless $self, $class;
@@ -26,185 +26,108 @@ sub new {
 }
 
 sub add_button {
-  my( $self, $type, $value ) = @_;
-  push @{$self->{'_button_order'}}, $type unless exists $self->{'_button'}{$type};
-  $self->{'_buttons'}{$type} = $value;
+### Add a button element to the form
+  my( $self, %options ) = @_;
+  my $type = $options{'type'};
+  if ($type eq 'Submit' || $type eq 'Button') {
+    my $module = "EnsEMBL::Web::Form::Element::$type";
+    if( $self->dynamic_use( $module ) ) {
+      my $button = $module->new( 'form' => $self->{'_attributes'}{'name'}, %options );
+      push @{$self->{'_buttons'}}, $button;
+    } 
+    else {
+      warn "Button module $module appears to be missing!";
+    }
+  }
+  else {
+    warn "Not a button module!";
+  }
 } 
 
 sub add_attribute {
+### Add an attribute to the FORM tag
   my( $self, $type, $value ) = @_;
   $self->{'_attributes'}{$type} = $value;
 }
 
-sub add_element {
+sub add_fieldset {
+### Add a fieldset object to the form
   my( $self, %options ) = @_;
-  my $module = "EnsEMBL::Web::Form::Element::$options{'type'}";
-  
-  if( $self->dynamic_use( $module ) ) {
-    $self->_add_element( $module->new( 'form' => $self->{'_attributes'}{'name'}, %options ) );
-  } else {
-    warn "Unable to dynamically use module $module. Have you spelt the element type correctly?";
+  my $fieldset = EnsEMBL::Web::Form::FieldSet->new('form' => $self->{'_attributes'}{'name'}, %options );
+  if (!$fieldset->{'_name'}) {
+    $fieldset->{'_name'} =  $self->_next_id();
   }
-}
-
-sub _add_element {
-  my( $self, $element ) = @_;
-  if( $element->type eq 'File' ) { # File types must always be multipart Posts 
-    $self->add_attribute( 'method',  'post' );
-    $self->add_attribute( 'enctype', 'multipart/form-data' );
-  }
-  if (!$element->id) {
-    $element->id =  $self->_next_id();
-  }
-  $element->{form} = $self->{_form_id};
-  $element->{formname} = $self->{_attributes}{name};
-  if ($element->multibutton) {
-    push @{$self->{'_multibutton'}}, $element;
-  }
-  else {
-    push @{$self->{'_elements'}}, $element;
-  }
+  push @{$self->{'_fieldsets'}}, $fieldset;
+  return $fieldset;
 }
 
 sub _next_id {
+### Returns an autoincremented ID for fieldset (used if not defined manually in the component) 
   my $self = shift;
   return $self->{'_attributes'}{'name'}.'_'.($self->{'_form_id'}++);
 }
 
-sub _render_element {
-  my( $self, $element ) = @_;
-  my $output;
-  return $element->render() if $element->type eq 'Hidden';
-
-  my $style = 'formblock';
-  if( $element->layout eq 'spanning' ) {
-    $style = 'formwide';
-  } 
-  elsif( $element->layout eq 'center' ) { 
-    $style = 'formcenter';
-  } 
-  elsif( $element->layout eq 'inline' ) { 
-    $style = 'forminline';
+sub _render_buttons {
+  my $self = shift;
+  my $output = '<div class="submit">';
+  foreach my $button ( @{$self->{'_buttons'}}) {
+    $output .= $button->render;
   }
-  elsif ( $element->layout eq 'indent') {
-    $style = 'formblock formindent';
-  }
-  if( $element->hidden_label ) {
-    $output = sprintf qq(
-  <div class="formpadding">
-    <div class="hidden"><label for="%s">%s</label</div>
-  </div>), $element->id, ( $element->label eq '' ? "&nbsp;" : CGI::escapeHTML($element->label) )
-  } elsif( $element->label ne '' ) {
-    if( $element->comment ) { 
-      $output = sprintf qq(
-   <h6><label for="%s">%s<br><span style="color: #333; font-weight: normal;">%s<br /><br /></span></label></h6>),
-        $element->id, CGI::escapeHTML($element->label), CGI::escapeHTML($element->comment);
-    } else {
-      $output = sprintf qq(
-   <h6><label for="%s">%s</label></h6>),
-        $element->id, CGI::escapeHTML($element->label);
-    }
-  } elsif( $style eq 'formblock' ) {
-    $output = qq(
-  <div class=\"formpadding\"></div>);
-  }
-  return qq(
-  <div class="$style">$output
-    <div class="formcontent">
-    ).$element->render().qq(
-    </div>
-  </div>);
-}
+  $output .= '</div>';
 
-sub _render_multibuttons {
-  my( $self, $element ) = @_;
-
-  my @buttons = @{$self->{'_multibutton'}};
-  my $output = qq(<div class="formblock">
-  <div class=\"formpadding\"></div>
-  <div class="formcontent">); 
-
-  foreach my $button (@buttons) {
-    $output .= $button->render();
-  }
-  $output .= qq(</div>
-  </div>
-  );
   return $output;
 }
 
 sub render {
+### Render the FORM tag and its contents
   my $self = shift;
+
+  my $widgets = '';
+  my $has_file = 0; 
+  my $required = 0;
+  foreach my $fieldset ( @{$self->{'_fieldsets'}} ) {
+    $has_file = 1 if $fieldset->{'_file'};
+    $required = 1 if $fieldset->{'_required'};
+    $widgets .= $fieldset->render;
+  }
+
+  if( $has_file ) { # File types must always be multipart Posts 
+    $self->add_attribute( 'method',  'post' );
+    $self->add_attribute( 'enctype', 'multipart/form-data' );
+  }
+
   my $output = "<form";
-  foreach my $K ( keys %{$self->{'_attributes'}} ) {
-    $output .= sprintf ' %s="%s"', CGI::escapeHTML($K), CGI::escapeHTML($self->{'_attributes'}{$K});
+  while (my ($k, $v) = each ( %{$self->{'_attributes'}} )) {
+    $output .= sprintf ' %s="%s"', CGI::escapeHTML($k), CGI::escapeHTML($v);
   }
-  $output .= ">";
-  if( @{$self->{'_button_order'}} ) {
-    $self->add_element(
-      'type' => 'Information',
-      'value' => '<div style="text-align: center">'.
-      ( exists( $self->{'_buttons'}{'submit'} ) ? sprintf( '<input type="submit" value="%s" />', $self->{'_buttons'}{'submit'} ) : '' ).
-      ( exists( $self->{'_buttons'}{'reset'}  ) ? sprintf( '<input type="reset"  value="%s" />', $self->{'_buttons'}{'reset'}  ) : '' ).
-      '</div>'
-    );
-  }
-  my $F = 0;
-  foreach my $element ( @{$self->{'_elements'}} ) {
-    if( $element->required eq 'yes' ) {
-      $F=1;
-    }
-  }
-  $self->add_element( 'type' => 'Information', 
-    'value' => '<div style="text-align: right">Fields marked with <b>*</b> are required</div>'
-  ) if $F;
+  $output .= '>';
+
+  $output .= $widgets;
   
-  foreach my $element ( @{$self->{'_elements'}} ) {
-    $output .= $self->_render_element( $element );
+  if ($required) {
+    $self->add_element( 'type' => 'Information', 
+    'value' => '<div class="right">Fields marked with <strong>*</strong> are required</div>'
+    )
   }
-  $output .= $self->_render_multibuttons;
+
+  $output .= $self->_render_buttons;
   $output .= "\n</form>\n";
   return $output;
 }
 
-sub render_js {
-  my $self = shift;
-  my @entries;
-  foreach my $element ( @{$self->{'_elements'}} ) {
-    if($element->validate()) {
-      if( $element->type eq 'DropDownAndString' ) {
-        (my $T_name = $element->name)=~s/'/\\'/g;
-        (my $T_label = $element->label)=~s/'/\\'/g;
-        (my $TS_name = $element->string_name)=~s/'/\\'/g;
-        (my $TS_label = $element->string_label)=~s/'/\\'/g;
-        push @entries, sprintf(
-          " new form_obj( '%s', '%s', '%s', '%s', %d )", $self->{_attributes}{'name'},$T_name,
-          'DropDown', $T_label, $element->required eq 'yes'?1:0
-        );
-        push @entries, sprintf(
-          " new form_obj( '%s', '%s', '%s', '%s', %d )", $self->{_attributes}{'name'},$TS_name,
-          'String', $TS_label, $element->required eq 'yes'?1:0
-        );
-      } else { 
-        (my $T_name = $element->name)=~s/'/\\'/g;
-        (my $T_label = $element->label)=~s/'/\\'/g;
-        push @entries, sprintf(
-          " new form_obj( '%s', '%s', '%s', '%s', %d )", $self->{_attributes}{'name'},$T_name,
-          $element->type, $T_label, $element->required eq 'yes'?1:0
-        );
-      }
-    }
+sub add_element {
+### x
+### Replacement for old method, included for backwards compatibility
+### Tries to add the element to the last fieldset, or creates a new one if none exist
+
+  my( $self, %options ) = @_;
+
+  my $fieldset = $self->{'_fieldsets'}[-1];
+  if (!$fieldset) {
+    $fieldset = EnsEMBL::Web::Form::FieldSet->new('form' => $self->{'_attributes'}{'name'});
+    push @{$self->{'_fieldsets'}}, $fieldset;
   }
-  my $vars_array = $self->{'_attributes'}{'name'}."_vars";
-  if( @entries ) {
-    return {
-      'head_vars' => "$vars_array = new Array(\n  ".join(",\n  ",@entries)."\n);\n",
-      'body_code' => "on_load( $vars_array );",
-      'scripts'   => '/js/forms.js'
-    }; 
-  } else {
-    return {};
-  }
+  $fieldset->add_element(%options);
 }
 
 1;
