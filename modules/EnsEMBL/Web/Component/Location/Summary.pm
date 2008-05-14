431c2a701964445aba80b9e0cdf367a0e3b31029
James Smith <js5@sanger.ac.uk>
2008-05-14 08:56:18 +0000

    added in Location summary module which does nothingcvs add Location/Summary.pm

---
+++ modules/EnsEMBL/Web/Component/Location/Summary.pm
@@ -0,0 +1,19 @@
+package EnsEMBL::Web::Component::Location::Summary;
+
+use strict;
+use warnings;
+no warnings "uninitialized";
+use base qw(EnsEMBL::Web::Component::Location);
+use CGI qw(escapeHTML);
+
+sub _init {
+  my $self = shift;
+  $self->cacheable( 0 );
+  $self->ajaxable(  1 );
+}
+
+sub content {
+  return '';
+}
+
+1;
