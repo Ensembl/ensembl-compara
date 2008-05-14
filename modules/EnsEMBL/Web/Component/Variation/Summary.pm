9fb5b5f136aee2ebca4589427c34c0417247b02a
James Smith <js5@sanger.ac.uk>
2008-05-14 08:59:34 +0000

    added in blank summary modules

---
+++ modules/EnsEMBL/Web/Component/Variation/Summary.pm
@@ -0,0 +1,19 @@
+package EnsEMBL::Web::Component::Variation::Summary;
+
+use strict;
+use warnings;
+no warnings "uninitialized";
+use base qw(EnsEMBL::Web::Component::Variation);
+use CGI qw(escapeHTML);
+
+sub _init {
+  my $self = shift;
+  $self->cacheable( 0 );
+  $self->ajaxable(  1 );
+}
+
+sub content {
+  return '';
+}
+
+1;
