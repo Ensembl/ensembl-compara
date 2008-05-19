8f3e007c2c22853a3c28662372df9069c15c43e2
James Smith <js5@sanger.ac.uk>
2008-05-19 09:13:55 +0000

    removed global/local

--- modules/EnsEMBL/Web/Document/Dynamic.pm
+++ modules/EnsEMBL/Web/Document/Dynamic.pm
@@ -76,8 +76,8 @@ sub _initialize_HTML {
     breadcrumbs     EnsEMBL::Web::Document::HTML::BreadCrumbs
     tools           EnsEMBL::Web::Document::HTML::ToolLinks
     content         EnsEMBL::Web::Document::HTML::Content
-    global_context  EnsEMBL::Web::Document::HTML::GlobalContext
-    local_context   EnsEMBL::Web::Document::HTML::LocalContext
+#    global_context  EnsEMBL::Web::Document::HTML::GlobalContext
+#    local_context   EnsEMBL::Web::Document::HTML::LocalContext
     copyright       EnsEMBL::Web::Document::HTML::Copyright
     footerlinks     EnsEMBL::Web::Document::HTML::FooterLinks
     body_javascript EnsEMBL::Web::Document::HTML::BodyJavascript
