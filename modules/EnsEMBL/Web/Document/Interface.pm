0d9472e6aafe9a2a79543d5c92214424e90099a6
Anne Parker <ap5@sanger.ac.uk>
2008-06-03 09:15:08 +0000

    Added copy of old configure method, as updated Document::WebPage::configure possibly now not compatible

--- modules/EnsEMBL/Web/Document/Interface.pm
+++ modules/EnsEMBL/Web/Document/Interface.pm
@@ -27,7 +27,6 @@ sub simple {
   my $self = __PACKAGE__->new((
       'objecttype' => $type, 
       'interface' => undef,
-      'access' => $parameter->{'access'}
     ));
 
   if( $self->has_a_problem ) {
@@ -38,7 +37,7 @@ sub simple {
       if ($parameter->{'menu'}) {
         push @args, $parameter->{'menu'};
       }
-      $self->configure(@args);
+      $self->configure(@args); ## Now uses own version, not parent's
     }
     $self->factory->fix_session;
   }
@@ -103,6 +102,95 @@ sub process {
   else {
 warn "Can't use module $config_module_name!";
   } 
+}
+
+sub configure {
+### Copy of 'configure' method from old Document::WebPage, since it's been modified
+### and no longer works with Interface code
+  my( $self, $object, @functions ) = @_;
+  my $objecttype;
+  if (ref($object)) { ## Actual object
+    $objecttype = $object->__objecttype;
+  }
+  elsif ($object =~ /^\w+$/) { ## String (type of E::W object)
+    $objecttype = $object;
+  }
+  else {
+    $objecttype = 'Static';
+  }
+  $objecttype = 'DAS' if ($objecttype =~ /^DAS::.+/);
+
+  my $flag = 0;
+  my @T = ('EnsEMBL::Web', '', @{$ENSEMBL_WEB_REGISTRY->species_defs->ENSEMBL_PLUGINS});
+
+  my $FUNCTIONS_CALLED = {};
+  while( my ($module_root, $X) = splice( @T, 0, 2) ) {
+
+   # Starting with the standard EnsEMBL module configure
+    # the script....
+    # Then loop through the plugins in order after that...
+    # First work out what the module name is - to see if it
+    # can be "used"
+    $flag ++;
+    my $config_module_name = $module_root."::Configuration::$objecttype";
+
+    if( $self->dynamic_use( $config_module_name ) ) { ## Successfully used
+      # If it has been successfully used then look for
+      # the functions named in the script "configure" line
+      # of the script.
+      my $CONF = $config_module_name->new( $self->page, $object, $flag );
+      $CONF->{commander} = $self->{commander};
+      $CONF->{command} = $self->{command};
+      foreach my $FN ( @functions ) {
+        if( $CONF->can($FN) ) {
+    # If this configuration module can perform this function do so...
+          eval { $CONF->$FN(); };
+          $self->{wizard} = $CONF->{wizard};
+          if( $@ ) { # Catch any errors and display as a "configuration runtime error"
+            $self->page->content->add_panel(
+              new EnsEMBL::Web::Document::Panel(
+               'caption' => 'Configuration module runtime error',
+               'content' => sprintf( qq(
+    <p>
+      Unable to execute configuration $FN from configuration module <b>$config_module_name</b>
+      due to the following error:
+    </p>
+    <pre>%s</pre>), $self->_format_error($@) )
+                       )
+             );
+          } else {
+            $FUNCTIONS_CALLED->{$FN} = 1;
+          }
+        }
+      }
+    } elsif( $self->dynamic_use_failure( $config_module_name ) !~ /^Can't locate/ ) {
+                           # Handle "use" failures gracefully...
+                           # Firstly skip Can't locate errors
+                           # o/w display a "compile time" error message.
+      $self->page->content->add_panel(
+        new EnsEMBL::Web::Document::Panel(
+         'caption' => 'Configuration module compilation error',
+         'content' => sprintf( qq(
+    <p>
+      Unable to use Configuration module <b>$config_module_name</b> due to
+      the following error:
+    </p>
+    <pre>%s</pre>), $self->_format_error( $self->dynamic_use_failure( $config_module_name )) )
+        )
+      );
+    }
+  }
+  foreach my $FN ( @functions ) {
+    unless( $FUNCTIONS_CALLED->{$FN} ) {
+      if( $objecttype eq 'DAS' ) {
+        $self->problem('Fatal', 'Bad request', 'Unimplemented');
+      } else {
+        warn "Can't do configuration function $FN on $objecttype objects, or an error occurred when excuting that function.";
+      }
+    }
+  }
+  $self->add_error_panels(); # Add error panels to end of display!!
+  $self->_prof("Script configured ($objecttype)");
 }
 
 }
