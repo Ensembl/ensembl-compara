d8491f37d27389a3540c18aa59d568cc151523f0
Anne Parker <ap5@sanger.ac.uk>
2008-06-10 10:23:51 +0000

    Changed to be new-style object

--- modules/EnsEMBL/Web/Object/UserData.pm
+++ modules/EnsEMBL/Web/Object/UserData.pm
@@ -5,13 +5,32 @@ use warnings;
 no warnings "uninitialized";
                                                                                    
 use EnsEMBL::Web::Object;
+use EnsEMBL::Web::RegObj;
                                                                                    
-@EnsEMBL::Web::Object::UserData::ISA = qw(EnsEMBL::Web::Object);
+our @ISA = qw(EnsEMBL::Web::Object);
 
 
 ## Currently just a placeholder object for wizard steps where the data type is as yet unknown
 
 sub data        : lvalue { $_[0]->{'_data'}; }
 sub data_type   : lvalue {  my ($self, $p) = @_; if ($p) {$_[0]->{'_data_type'} = $p} return $_[0]->{'_data_type' }; }
+
+sub caption           {
+  my $self = shift;
+  return 'Custom Data';
+}
+
+sub short_caption {
+  my $self = shift;
+  return 'Data Management';
+}
+
+sub counts {
+  my $self = shift;
+  my $user = $ENSEMBL_WEB_REGISTRY->get_user;
+  my $counts = {};
+  return $counts;
+}
+
 
 1;
