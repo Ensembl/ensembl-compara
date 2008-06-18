3f57bf33b0ad141664c797985bbc218e8ec50c10
Albert Vilella <avilella@sanger.ac.uk>
2008-06-18 16:21:07 +0000

    adding the two sitewise dnds tables -- empty if you havent done the analysis

--- sql/patch_49_50.sql
+++ sql/patch_49_50.sql
@@ -12,4 +12,33 @@ UPDATE meta SET meta_value = 50 where meta_key = "schema_version";
 
 INSERT INTO genomic_align_group SELECT node_id, "epo", node_id FROM genomic_align_tree;
 
-# DN/DS
+# sitewise DN/DS
+
+CREATE TABLE sitewise_aln (
+  sitewise_id                 int(10) unsigned NOT NULL auto_increment, # unique internal id
+  aln_position                int(10) unsigned NOT NULL,
+  node_id                     int(10) unsigned NOT NULL,
+  tree_node_id                int(10) unsigned NOT NULL,
+  omega                       float(10,5),
+  omega_lower                 float(10,5),
+  omega_upper                 float(10,5),
+  threshold_on_branch_ds      float(10,5),
+  type                        varchar(10) NOT NULL,
+
+  FOREIGN KEY (node_id) REFERENCES protein_tree_node(node_id),
+
+  UNIQUE aln_position_node_id_ds (aln_position,node_id,threshold_on_branch_ds),
+  PRIMARY KEY (sitewise_id),
+  KEY (node_id)
+) COLLATE=latin1_swedish_ci;
+
+CREATE TABLE sitewise_member (
+  sitewise_id                 int(10) unsigned NOT NULL,
+  member_id                   int(10) unsigned NOT NULL,
+  member_position             int(10) unsigned NOT NULL,
+
+  FOREIGN KEY (sitewise_id) REFERENCES sitewise_aln(sitewise_id),
+
+  UNIQUE sitewise_member_position (sitewise_id,member_id,member_position),
+  KEY (member_id)
+) MAX_ROWS = 1000000000 COLLATE=latin1_swedish_ci;
