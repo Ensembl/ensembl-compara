90be4e902cdb0cd3274344dd6711748eba6319f2
Anne Parker <ap5@sanger.ac.uk>
2008-06-19 08:44:18 +0000

    Updated to correctly display user records

--- modules/EnsEMBL/Web/Component/Account/Annotations.pm
+++ modules/EnsEMBL/Web/Component/Account/Annotations.pm
@@ -26,9 +26,44 @@ sub content {
 
   my $user = $ENSEMBL_WEB_REGISTRY->get_user;
   my $sitename = $self->site_name;
-  my $has_configs = 0;
 
-  if (!$has_configs) {
+  my @notes = $user->annotations;
+  my $has_notes = 0;
+
+  if ($#notes > -1) {
+
+    $html .= qq(<h3>Your annotations</h3>);
+    ## Sort user notes by name if required
+
+    ## Display user notes
+    my $table = new EnsEMBL::Web::Document::SpreadSheet( [], [], {'margin' => '1em 0px'} );
+
+    $table->add_columns(
+        { 'key' => 'gene',      'title' => 'Gene ID',       'width' => '20%', 'align' => 'left' },
+        { 'key' => 'title',     'title' => 'Title',         'width' => '50%', 'align' => 'left' },
+        { 'key' => 'rename',    'title' => '',              'width' => '10%', 'align' => 'left' },
+        { 'key' => 'share',     'title' => '',              'width' => '10%', 'align' => 'left' },
+        { 'key' => 'delete',    'title' => '',              'width' => '10%', 'align' => 'left' },
+    );
+
+    foreach my $note (@notes) {
+      my $row = {};
+
+      $row->{'gene'} = sprintf(qq(<a href="/Gene/Summary?g=%s">%s</a>),
+                        $note->stable_id, $note->stable_id);
+
+      $row->{'title'} = $note->title;
+      $row->{'rename'} = $self->rename_link('Annotation', $note->id);
+      $row->{'share'} = $self->share_link('Annotation', $note->id);
+      $row->{'delete'} = $self->delete_link('Annotation', $note->id);
+      $table->add_row($row);
+      $has_notes = 1;
+    }
+    $html .= $table->render;
+  }
+
+
+  if (!$has_notes) {
     $html .= qq(<p class="center"><img src="/img/help/note_example.gif" alt="Sample screenshot" title="SAMPLE" /></p>);
     $html .= qq(<p class="center">You haven't saved any $sitename notes. <a href='/info/about/custom.html#notes'>Learn more about notes &rarr;</a>);
   }
--- modules/EnsEMBL/Web/Component/Account/Configurations.pm
+++ modules/EnsEMBL/Web/Component/Account/Configurations.pm
@@ -26,7 +26,42 @@ sub content {
 
   my $user = $ENSEMBL_WEB_REGISTRY->get_user;
   my $sitename = $self->site_name;
+  
+  my @configs = $user->configurations;
   my $has_configs = 0;
+
+  if ($#configs > -1) {
+
+    $html .= qq(<h3>Your configurations</h3>);
+    ## Sort user configs by name if required
+
+    ## Display user configs
+    my $table = new EnsEMBL::Web::Document::SpreadSheet( [], [], {'margin' => '1em 0px'} );
+
+    $table->add_columns(
+        { 'key' => 'name',      'title' => 'Name',          'width' => '20%', 'align' => 'left' },
+        { 'key' => 'desc',      'title' => 'Description',   'width' => '50%', 'align' => 'left' },
+        { 'key' => 'rename',    'title' => '',              'width' => '10%', 'align' => 'left' },
+        { 'key' => 'share',     'title' => '',              'width' => '10%', 'align' => 'left' },
+        { 'key' => 'delete',    'title' => '',              'width' => '10%', 'align' => 'left' },
+    );
+
+    foreach my $config (@configs) {
+      my $row = {};
+
+      my $description = $config->description || '&nbsp;';
+      $row->{'name'} = sprintf(qq(<a href="/Account/_use_config?id=%s" title="%s">%s</a>),
+                        $config->id, $description, $config->name);
+
+      $row->{'desc'} = $description;
+      $row->{'rename'} = $self->rename_link('Configuration', $config->id);
+      $row->{'share'} = $self->share_link('Configuration', $config->id);
+      $row->{'delete'} = $self->delete_link('Configuration', $config->id);
+      $table->add_row($row);
+      $has_configs = 1;
+    }
+    $html .= $table->render;
+  }
 
   if (!$has_configs) {
     $html .= qq(<p class="center"><img src="/img/help/config_example.gif" /></p>);
