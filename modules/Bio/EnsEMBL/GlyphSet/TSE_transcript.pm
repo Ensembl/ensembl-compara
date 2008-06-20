4db9a7b7c3648ec67c097eb3c00c9d3f4140046a
Steve Trevanion <st3@sanger.ac.uk>
2008-06-20 13:34:31 +0000

    tidy up of _init, added direction arrow

--- modules/Bio/EnsEMBL/GlyphSet/TSE_transcript.pm
+++ modules/Bio/EnsEMBL/GlyphSet/TSE_transcript.pm
@@ -5,9 +5,7 @@ use Bio::EnsEMBL::GlyphSet;
 @ISA = qw(Bio::EnsEMBL::GlyphSet);
 use Sanger::Graphics::Glyph::Rect;
 use Sanger::Graphics::Glyph::Text;
-use Sanger::Graphics::Glyph::Composite;
 use Sanger::Graphics::Glyph::Line;
-use Sanger::Graphics::Bump;
 use Bio::EnsEMBL::Utils::Eprof qw(eprof_start eprof_end);
 use Data::Dumper;
 
@@ -17,109 +15,115 @@ sub init_label {
   $self->init_label_text( $sample );
 }
 
-#code to draw arrow is in Glyphset_transcript - search for $check and $line. Doesn't work if I inherit from this, but TSV somehow shows it...?
-
-
 sub _init {
-  my ($self) = @_;
-  my $type = $self->check();
-  return unless defined $type;
-#  return unless $self->strand() == -1;
-  my $offset = $self->{'container'}->start - 1;
-  my $Config        = $self->{'config'};
-  my @transcripts   = $Config->{'transcripts'};
-  my $y             = 0;
-  my $h             = 8;   #Single transcript mode - set height to 30 - width to 8!
-    
-  my %highlights;
-  @highlights{$self->highlights} = ();    # build hashkeys of highlight list
+	my ($self) = @_;
+#	my $offset = $self->{'container'}->start - 1;
 
-  my $colours       = $self->colours();
-  my $fontname      = $Config->species_defs->ENSEMBL_STYLE->{'GRAPHIC_FONT'};
-  my $pix_per_bp    = $Config->transform->{'scalex'};
-  my $bitmap_length = $Config->image_width(); #int($Config->container_width() * $pix_per_bp);
+	my $Config  = $self->{'config'};
+	my $h       = 8;   #Increasing this increases glyph height
 
-  my $length  = $Config->container_width();
-  my $transcript_drawn = 0;
-    
-  my $voffset = 0;
-  my($font_w_bp, $font_h_bp) = $Config->texthelper->px2bp($fontname);
-  my $trans_ref = $Config->{'transcript'};
-  my $strand = $trans_ref->{'exons'}[0][2]->strand;
-  #my $gene = $trans_ref->{'gene'};
-  my $transcript = $trans_ref->{'transcript'};
-  my @exons = sort {$a->[0] <=> $b->[0]} @{$trans_ref->{'exons'}};
-  # If stranded diagram skip if on wrong strand
-  # For exon_structure diagram only given transcript
-  # my $Composite = new Sanger::Graphics::Glyph::Composite({'y'=>0,'height'=>$h});
-  my($colour, $label, $hilight) = $self->colour( $transcript, $colours, %highlights );
-  my $coding_start = $trans_ref->{'coding_start'};
-  my $coding_end   = $trans_ref->{'coding_end'  };
+	my $colours     = $self->colours();
+	my $pix_per_bp  = $Config->transform->{'scalex'};
+	my $length      = $Config->container_width();
 
-#warn "in the glyph: $coding_start--$coding_end";
-  ## First of all draw the lines behind the exons.....
+	my $trans_ref = $Config->{'transcript'};
+	my $coding_start = $trans_ref->{'coding_start'};
+	my $coding_end   = $trans_ref->{'coding_end'  };
+	my $strand = $trans_ref->{'exons'}[0][2]->strand;
 
-  my $l_ext = $Config->{'intron_context'};
-#  warn Dumper($Config->{'subslices'});
-  foreach my $subslice (@{$Config->{'subslices'}}) {
-    $self->push( new Sanger::Graphics::Glyph::Rect({
-      'x' => $subslice->[0]+$subslice->[2]-1,#-$l_ext,
-	  'y' => $h/2,
-	  'h'=>1,
-	  'width'=>$subslice->[1]-$subslice->[0],#+2*$l_ext,
-	  'colour'=>$colour,
-	  'absolutey'=>1,
-    }));
-  }
-  ## Now draw the exons themselves....
+	my $transcript = $trans_ref->{'transcript'};
+	my @exons = sort {$a->[0] <=> $b->[0]} @{$trans_ref->{'exons'}};
 
-  foreach my $exon (@exons) { 
-    next unless defined $exon; #Skip this exon if it is not defined (can happen w/ genscans) 
-      # We are finished if this exon starts outside the slice
-    my($box_start, $box_end);
-      # only draw this exon if is inside the slice
-    $box_start = $exon->[0];
-    $box_start = 1 if $box_start < 1 ;
-    $box_end   = $exon->[1];
-    $box_end = $length if $box_end > $length;
-#	warn "box start = $box_start, box end = $box_end";
+	my %highlights;
+	@highlights{$self->highlights} = ();    # build hashkeys of highlight list
+	my($colour, $label, $hilight) = $self->colour( $transcript, $colours, %highlights );
+	
+	## First of all draw the lines behind the exons.....
+	foreach my $subslice (@{$Config->{'subslices'}}) {
+		$self->push( new Sanger::Graphics::Glyph::Rect({
+			'x' => $subslice->[0]+$subslice->[2]-1,
+			'y' => $h/2,
+			'h'=>1,
+			'width'=>$subslice->[1]-$subslice->[0],
+			'colour'=>$colour,
+			'absolutey'=>1,
+		}));
+	}
 
-    # Calculate and draw the coding region of the exon
-    my $filled_start = $box_start < $coding_start ? $coding_start : $box_start;
-    my $filled_end   = $box_end > $coding_end  ? $coding_end   : $box_end;
-#	warn "filled start = $filled_start, filled end = $filled_end";
-             # only draw the coding region if there is such a region
-    if( $filled_start <= $filled_end ) {
-    #Draw a filled rectangle in the coding region of the exon
-      $self->push( new Sanger::Graphics::Glyph::Rect({
-        'x' => $filled_start -1,
-        'y'         => 0,
-        'width'     => $filled_end - $filled_start + 1,
-        'height'    => $h,
-        'colour'    => $colour,
-        'absolutey' => 1
-      }));
-    }
-     #if($box_start < $coding_start || $box_end > $coding_end ) {
-      # The start of the transcript is before the start of the coding
-      # region OR the end of the transcript is after the end of the
-      # coding regions.  Non coding portions of exons, are drawn as
-      # non-filled rectangles
-      #Draw a non-filled rectangle around the entire exon
-	my $G = new Sanger::Graphics::Glyph::Rect({
-        'x'         => $box_start -1 ,
-        'y'         => 0,
-        'width'     => $box_end-$box_start +1,
-        'height'    => $h,
-        'bordercolour' => $colour,
-        'absolutey' => 1,
-        'title'     => $exon->[2]->stable_id,
-        'href'     => $self->href(  $transcript, $exon->[2], %highlights ),
-      });
-      $G->{'zmenu'} = $self->zmenu( $transcript, $exon->[2] ) unless $Config->{'_href_only'};
-      $self->push( $G );
-    # } 
-  } #we are finished if there is no other exon defined
+	## Now draw the exons themselves....	
+	foreach my $exon (@exons) { 
+		next unless defined $exon; #Skip this exon if it is not defined (can happen w/ genscans) 
+
+		# only draw this exon if is inside the slice (of course it really should be but no harm in checking_
+		my $box_start = $exon->[0];
+		$box_start    = 1 if $box_start < 1 ;
+		my $box_end   = $exon->[1];
+		$box_end      = $length if $box_end > $length;
+		
+		# calculate and draw the coding part of the exon
+		my $filled_start = $box_start < $coding_start ? $coding_start : $box_start;
+		my $filled_end   = $box_end   > $coding_end   ? $coding_end   : $box_end;
+		if( $filled_start <= $filled_end ) {
+			$self->push( new Sanger::Graphics::Glyph::Rect({
+				'x'         => $filled_start -1,
+				'y'         => 0,
+				'width'     => $filled_end - $filled_start + 1,
+				'height'    => $h,
+				'colour'    => $colour,
+				'absolutey' => 1
+			}));
+		}
+
+		# draw a non-filled rectangle around the entire exon
+		my $G = new Sanger::Graphics::Glyph::Rect({
+			'x'         => $box_start -1 ,
+			'y'         => 0,
+			'width'     => $box_end-$box_start +1,
+			'height'    => $h,
+			'bordercolour' => $colour,
+			'absolutey' => 1,
+			'title'     => $exon->[2]->stable_id,
+			'href'      => $self->href(  $transcript, $exon->[2], %highlights ),
+		});
+		$G->{'zmenu'} = $self->zmenu( $transcript, $exon->[2] ) unless $Config->{'_href_only'};
+		$self->push( $G );
+	}
+
+	#draw a direction arrow
+	if($strand == 1) {
+		$self->push(new Sanger::Graphics::Glyph::Line({
+			'x'         => 0,
+			'y'         => -4,
+			'width'     => $length,
+			'height'    => 0,
+			'absolutey' => 1,
+			'colour'    => $colour
+		}));
+		$self->push( new Sanger::Graphics::Glyph::Poly({
+			'points' => [
+				$length - 4/$pix_per_bp,-2,
+				$length                ,-4,
+				$length - 4/$pix_per_bp,-6],
+			'colour'    => $colour,
+			'absolutey' => 1,
+		}));
+	} else {
+		$self->push(new Sanger::Graphics::Glyph::Line({
+			'x'         => 0,
+			'y'         => $h+4,
+			'width'     => $length,
+			'height'    => 0,
+			'absolutey' => 1,
+			'colour'    => $colour
+		}));
+		$self->push(new Sanger::Graphics::Glyph::Poly({
+			'points'    => [ 4/$pix_per_bp,$h+6,
+							 0,              $h+4,
+							 4/$pix_per_bp,$h+2],
+			'colour'    => $colour,
+			'absolutey' => 1,
+		}));
+	}
 }
 
 sub colours {
@@ -174,6 +178,11 @@ sub zmenu {
     	qq(/@{[$self->{container}{_config_file_name_}]}/exportview?options=peptide;action=select;format=fasta;type1=peptide;anchor1=$pid);	
     }
     return $zmenu;
+}
+
+sub text_label {
+	warn "drawing label";
+	return 'name';
 }
 
 sub error_track_name { return $_[0]->species_defs->AUTHORITY.' transcripts'; }
