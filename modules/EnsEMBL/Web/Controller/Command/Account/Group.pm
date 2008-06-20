494220fa9c2e7f263355232f2b4bb0e40709991a
Anne Parker <ap5@sanger.ac.uk>
2008-06-20 09:39:17 +0000

    Updated to work with new code

--- modules/EnsEMBL/Web/Controller/Command/Account/Group.pm
+++ modules/EnsEMBL/Web/Controller/Command/Account/Group.pm
@@ -6,6 +6,7 @@ use warnings;
 use Class::Std;
 use CGI;
 
+use EnsEMBL::Web::Data::User;
 use EnsEMBL::Web::Data::Group;
 
 use base 'EnsEMBL::Web::Controller::Command::Account';
@@ -15,10 +16,13 @@ use base 'EnsEMBL::Web::Controller::Command::Account';
 sub BUILD {
   my ($self, $ident, $args) = @_; 
   $self->add_filter('EnsEMBL::Web::Controller::Command::Filter::LoggedIn');
-  ## If editing, ensure that this user is an administrator of this group
+  ## If editing, ensure that this record is editable by the logged-in user!
   my $cgi = new CGI;
+  my $record;
   if ($cgi->param('id')) {
-    $self->add_filter('EnsEMBL::Web::Controller::Command::Filter::Admin', {'group_id' => $cgi->param('id')});
+    $self->add_filter('EnsEMBL::Web::Controller::Command::Filter::Admin',
+        {'group_id' => $cgi->param('id')}
+    );
   }
 }
 
@@ -34,37 +38,36 @@ sub render {
 
 sub render_page {
   my $self = shift;
-  my $cgi = new CGI;
+  my $cgi  = new CGI;
 
   ## Create basic page object, so we can access CGI parameters
   my $webpage = EnsEMBL::Web::Document::Interface::simple('Account');
 
   my $sd = EnsEMBL::Web::SpeciesDefs->new();
   my $help_email = $sd->ENSEMBL_HELPDESK_EMAIL;
-  my $sitename = $sd->ENSEMBL_SITETYPE;
 
   ## Create interface object, which controls the forms
   my $interface = EnsEMBL::Web::Interface::InterfaceDef->new;
   my $data = EnsEMBL::Web::Data::Group->new($cgi->param('id'));
+  
   $interface->data($data);
   $interface->discover;
 
   ## Customization
   ## Page components
   $interface->default_view('add');
-  $interface->panel_header({'add'=>qq(<p>You can create a new $sitename group from here. $sitename groups allow you to share customisations and settings between collections of users.</p><p>Setting up a new group takes about 2 minutes.</p>)});
   $interface->panel_footer({'add'=>qq(<p>Need help? <a href="mailto:$help_email">Contact the helpdesk</a> &middot; <a href="/info/about/privacy.html">Privacy policy</a><p>)});
-  $interface->on_success($self->url('/Account/Details'));
+  $interface->on_success($self->url('/Account/AdminGroups'));
   $interface->on_failure($self->url('/Account/UpdateFailed'));
   $interface->script_name($self->get_action->script_name);
 
 ## Form elements
-  $interface->caption({'add'=>'Create group'});
-  $interface->caption({'edit'=>'Edit group details'});
+  $interface->caption({add  => 'Save group'});
+  $interface->caption({edit => 'Edit group'});
   $interface->permit_delete('yes');
-  $interface->element('name', {'type'=>'String', 'label'=>'Name of Group'});
-  $interface->element('blurb', {'type'=>'Text', 'label'=>'Short description'});
-  $interface->element_order('name', 'blurb');
+  $interface->element('name',         { type => 'String', label => 'Group name'});
+  $interface->element('blurb',  { type => 'Text',   label => 'A brief description of your group'});
+  $interface->element_order(qw/name blurb/);
 
   ## Render page or munge data, as appropriate
   $webpage->process($interface, 'EnsEMBL::Web::Configuration::Interface::Group');
