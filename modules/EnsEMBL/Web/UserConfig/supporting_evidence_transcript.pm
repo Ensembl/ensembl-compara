3eabfcab801d49d493fbe2c99a7926cd7c721480
Steve Trevanion <st3@sanger.ac.uk>
2008-06-24 15:22:28 +0000

    attach vertical striping to transcript glyphset, so now no longer needed at the top of the page

--- modules/EnsEMBL/Web/UserConfig/supporting_evidence_transcript.pm
+++ modules/EnsEMBL/Web/UserConfig/supporting_evidence_transcript.pm
@@ -10,7 +10,7 @@ sub init {
 	$self->{'_transcript_names_'} = 'yes';
 	$self->{'_add_labels' }  = 1;
 	$self->{'general'}->{'supporting_evidence_transcript'} = {
-		'_artefacts' => [qw(TSE_background_exon_1 TSE_background_exon_2 TSE_transcript TSE_generic_match SE_generic_match non_can_intron spacer1 spacer2 spacer3)],
+		'_artefacts' => [qw(TSE_background_exon TSE_transcript TSE_generic_match SE_generic_match non_can_intron spacer1 spacer2 spacer3)],
 		'_options'  => [],
 		'_settings' => {
 			'opt_pdf' => 0, 'opt_svg' => 0, 'opt_postscript' => 0,
@@ -25,22 +25,15 @@ sub init {
 			'types' => [ ],
 			'features' => [],
 		},
-		'TSE_background_exon_1' => {
-			'on'          => "on",
-			'pos'         => '5000',
-			'str'         => 'f',
-			'src'         => 'all',
-			'col'         => 'bisque',
-			'tag'         => 1,
-			'flag'        => 0,
-			'glyphset'    => 'TSE_background_exon',
-		},
+
 		'TSE_transcript' => {
 			'on'          => "on",
 			'pos'         => '300',
 			'str'         => 'f',
 			'src'         => 'all',
 			'colours' => {$self->{'_colourmap'}->colourSet( 'all_genes' )} ,
+#			'col'         => 'd8ddff',
+			'col'         => 'bisque',
 		},
 
 		'spacer1' => { 'on'=>'on','pos'=>280, 'height' => 30, 'str' => 'b', 'glyphset' => 'spacer' },
@@ -72,11 +65,12 @@ sub init {
 			'src'         => 'all', # 'ens' or 'all'
 			'colours' => {$self->{'_colourmap'}->colourSet( 'all_genes' )} ,
 		},
-		'TSE_background_exon_2' => {
+		'TSE_background_exon' => {
 			'on'          => "on",
 			'pos'         => '0',
 			'str'         => 'f',
 			'src'         => 'all',
+#			'col'         => 'd8ddff',
 			'col'         => 'bisque',
 			'tag'         => 1,
 			'flag'        => 1,
