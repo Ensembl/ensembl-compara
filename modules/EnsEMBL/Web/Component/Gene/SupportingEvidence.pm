1542357b385d76e8fc04fba130a7aaf1b4ffe84e
Steve Trevanion <st3@sanger.ac.uk>
2008-06-27 13:27:31 +0000

    reformat table - less text, IDs aligned

--- modules/EnsEMBL/Web/Component/Gene/SupportingEvidence.pm
+++ modules/EnsEMBL/Web/Component/Gene/SupportingEvidence.pm
@@ -23,7 +23,7 @@ sub content {
 	my $html;# = qq(<div class="content">);
 	my $e = $object->get_gene_supporting_evidence;
 	if (! $e) {
-		$html .=  qq( <dt>No Evidence</dt><dd>);
+		$html .=  qq(<dt>No Evidence</dt><dd>);
 		if ($type =~ /otter/ ){
 			$html .= qq(<p>Although this Vega Havana gene has been manually annotated and it's structure is supported by experimental evidence, this evidence is currently missing from the database. We are adding the evidence back to the database as time permits</p>);
 		}
@@ -34,68 +34,70 @@ sub content {
     }
 
 	$html .= qq(<table class="ss tint">);
-	$html .= qq(<tr><th width="16%">Transcript</th><th width="28%"></th><th width="28%" >View record</th><th width="28%">View alignment</th></tr>);
+	$html .= qq(<tr>
+                <th width="20%">Transcript</th>
+                <th width="20%">CDS support</th>
+                <th width="20%" >UTR support</th>
+                <th width="20%">Other transcript support</th>
+                <th width="20%">Exon support</th></tr>);
 	foreach my $tsi (sort keys %{$e}) {
-		$html .= qq(<tr>);
-
-		#retrieve evidence and count no of types
-		my ($utr_ids,$cds_ids,$other_ids);
-		my $trans_evi = $e->{$tsi}{'evidence'};
-		my $extra_evi = $e->{$tsi}{'extra_evidence'};
-		my $row_c;
-		if ($trans_evi) {
-			$row_c++ if ($utr_ids = $trans_evi->{'UTR'});
-			$row_c++ if ($cds_ids = $trans_evi->{'CDS'});
-			$row_c++ if ($other_ids = $trans_evi->{'UNKNOWN'});
-		}
-		else { $row_c++; }
-		$row_c++ if $extra_evi;
-			
+		my $ln = $e->{$tsi}{'logic_name'};
 		my $url = $self->object->_url({
 			'type'   => 'Transcript',
 			'action' => 'Evidence',
 			't'      => $tsi,
 		});
-		
-		$html .= $row_c ? "<td class=\"bg2\"  rowspan=$row_c>" : "<td>";
-		$html .= "<a href=\"$url\">$tsi</a></td>";
-		if ($trans_evi) {
-			if ($cds_ids) {
-				my $links = $object->add_evidence_links($cds_ids);
-				$html .= qq(<td>Translation support:</td><td>$links</td>);
-				my $txt = join q{, }, sort(keys %$cds_ids);
-				$html .= qq(<td>$txt</td>);
-				$html .= qq(</tr><tr>);
+
+		$html .= qq(<tr>
+                    <td class="bg2"><a href=\"$url\">$tsi</a> <span class="small">[$ln]</span></td>);
+		if (my $trans_evi = $e->{$tsi}{'evidence'}) {
+			if (my $cds_ids = $trans_evi->{'CDS'}) {
+				$html .= qq(<td>);
+				foreach my $link (@{$object->add_evidence_links($cds_ids)}) {
+					$html .= qq(
+                                <p>$link [align]</p>);
+				}
+				$html .= qq(</td>);
+			} 
+			else {
+				$html .= qq(<td></td>);
 			}
-			if ($utr_ids) {
-				my $links = $object->add_evidence_links($utr_ids);
-				$html .= qq(<td>UTR support:</td><td>$links</td>);
-				my $txt = join q{, }, sort(keys %$utr_ids);
-				$html .= qq(<td>$txt</td>);
-				$html .= qq(</tr><tr>);
+
+			if (my $utr_ids = $trans_evi->{'UTR'}) {
+				$html .= qq(<td>);
+				foreach my $link (@{$object->add_evidence_links($utr_ids)}) {
+					$html .= qq(
+                                <p>$link [align]</p>);
+				}
+				$html .= qq(</td>);
 			}
-			if ($other_ids) { 
-				my $links = $object->add_evidence_links($other_ids);
-				$html .= qq(<td>Other support:</td><td>$links</td>);
-				my $txt = join q{, }, sort(keys %$other_ids);
-				$html .= qq(<td>$txt</td>);
-				$html .= qq(</tr><tr>);
+			else {
+				$html .= qq(<td></td>);
+			}
+
+			if (my $other_ids= $trans_evi->{'UNKNOWN'}) {
+				$html .= qq(<td>);
+				foreach my $link (@{$object->add_evidence_links($other_ids)}) {
+					$html .= qq(
+                                <p>$link [align]</p>);
+				}
+				$html .= qq(</td>);
+			}
+			else {
+				$html .= qq(<td></td>);
 			}
 		}
 		else {
-			$html .= qq(<td colspan=3>No evidence stored for the transcript</td></tr><tr>);
+			$html .= qq(<td colspan=3></td>);
 		}
-		if ($extra_evi) {
+		if (my $extra_evi = $e->{$tsi}{'extra_evidence'}) {
 			my $c = scalar(keys(%$extra_evi));
-			$html .= "<td colspan=3>Exons are further supported by another <a href=\"$url\">$c features</a></td></tr><tr>";
+			$html .= qq(
+                        <td><a href=\"$url\">$c features</a></td>);
 		}
-
-		unless ($trans_evi || $extra_evi) {
-			$html .= qq(<td>No supporting evidence available for this gene</td></tr>);
-		}
-		$html .= "</td></tr>";
+		$html .= qq(</tr>);
 	}		
-	$html .=  "</table>";
+	$html .=  qq(</table>);
 	return $html;
 }
 
