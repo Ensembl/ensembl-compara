da0d4aba93e0bc23789f5b90a4663167e729a754
Steve Trevanion <st3@sanger.ac.uk>
2008-07-02 14:33:43 +0000

    take tag locations and colours from Config, for both CDS and UTRs

--- modules/Bio/EnsEMBL/GlyphSet/TSE_background_exon.pm
+++ modules/Bio/EnsEMBL/GlyphSet/TSE_background_exon.pm
@@ -10,44 +10,27 @@ use Bio::EnsEMBL::GlyphSet;
 use Data::Dumper;
   
 @Bio::EnsEMBL::GlyphSet::TSE_background_exon::ISA = qw(Bio::EnsEMBL::GlyphSet);
+
+#needed for drawing vertical lines on supporting evidence view
 sub _init {
 	my ($self) = @_;
-	my $Config        = $self->{'config'};
-	my $container     = $self->{'container'};
-	my $length  = $container->length;
-	my $start = $container->start();
-	my $colour        = $Config->get('TSE_background_exon_1','col' );
-	my $pix_per_bp    = $Config->transform->{'scalex'};
-	
-	my $tag = $Config->get('TSE_background_exon_1','tag');	
-    my $flag=$self->my_config('flag');
+	my $Config = $self->{'config'};
+    my $flag   = $self->my_config('flag');
 
-	#retrieve each exon and sort by start / end
-	my $trans_ref = $Config->{'transcript'};
-	my %exons = ();
-	foreach my $exon (@{$trans_ref->{'exons'}}) {
-		my $tag = "@{[$exon->[2]->start]}:@{[$exon->[2]->end]}";
-		$exons{"$exon->[0]:$exon->[1]:$tag"}++; 
-	}
-	my @exons = sort { $a->[0] <=> $b->[0] || $a->[1] <=> $b->[1] } map { [ split /:/, $_ ] } keys %exons;
-
-	#draw spacer glyphs and add tags
-	foreach my $exon (@exons) {
-		my( $S,$E,$S2,$E2 ) = @$exon;
-		next if $E<1;
-		next if $S>$length;
-		my $tag = "@{[$E2]}:@{[$S2]}";
-		$S = 1 if $S < 1;
-		$E = $length if $E > $length;
+	#retrieve tag locations and colours calculated by TSE_transcript track
+	my $tags   = $Config->{'tags'};
+	foreach my $tag (@{$tags}) {
+		my ($extra,$e,$s) = split ':', $tag->[0];
+		my $col = $tag->[1];
 		my $tglyph = new Sanger::Graphics::Glyph::Space({
-			'x' => $S-1,
+			'x' => $s-1,
 			'y' => 0,
 			'height' => 0,
-			'width'  => $E-$S+1,
-			'colour' => '$colour',
+			'width'  => $e-$s+1,
+			'colour' => '$col',
 		});
-		$self->join_tag( $tglyph, "X:$tag", $flag,  0, $colour, 'fill', -99 );
-		$self->join_tag( $tglyph, "X:$tag", 1-$flag,0, $colour, 'fill', -99  );
+		$self->join_tag( $tglyph, $tag->[0], $flag,  0, $col, 'fill', -99 );
+		$self->join_tag( $tglyph, $tag->[0], 1-$flag,0, $col, 'fill', -99  );
 		$self->push( $tglyph );
 	}
 }
