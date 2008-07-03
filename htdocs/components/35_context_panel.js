b47874a1f7275de52f6a7ebc76802eea3766f45e
Anne Parker <ap5@sanger.ac.uk>
2008-07-03 16:06:10 +0000

    Fixed button labels, and extended functionality so it can be used with other collapsible tables

--- htdocs/components/35_context_panel.js
+++ htdocs/components/35_context_panel.js
@@ -6,31 +6,32 @@
 **                                                                    **
 ***********************************************************************/
 
-  function __init_transcript_dropdown() {
+  function __init_transcript_dropdown(table_id) {
 /** Initialize link to either collapse or expand transcript list.
 
     PRIVATE - should only be executed once on page load
 **/
 
 // If no transcripts OR already generated link so return....
-    if( !$('transcripts') || $('transcripts_link') ) return;
+    if( !$(table_id) || $(table_id + '_link') ) return;
 
     var initial_open = 1;
-    var txt_on       = 'show transcripts';
-    var txt_off      = 'hide transcripts';
+    var txt_on       = 'hide ' + table_id;
+    var txt_off      = 'show ' + table_id;
 
-    $('transcripts_text').appendChild(
+    $(table_id + '_text').appendChild(
       Builder.node( 'div',
-        { id: 'transcripts_link' }, 
+        { id: table_id + '_link' }, 
         [ initial_open ? txt_off : txt_on ]
       )
     );
-    if( initial_open != 1 ) $('transcripts').hide();
-    Event.observe($('transcripts_link'),'click',function(event){
-      $('transcripts').toggle();
-      var T = $('transcripts_link');
+    if( initial_open != 1 ) $(table_id).hide();
+    Event.observe($(table_id + '_link'),'click',function(event){
+      $(table_id).toggle();
+      var T = $(table_id + '_link');
       T.innerHTML = T.innerHTML == txt_off ? txt_on : txt_off;
     });
   }
 
-  Event.observe(window, 'load', __init_transcript_dropdown );
+  Event.observe(window, 'load', __init_table_dropdown('transcripts'));
+  Event.observe(window, 'load', __init_table_dropdown('locations') );
