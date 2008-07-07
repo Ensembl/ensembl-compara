029b744eb6e5603337f94898516bad3722ed14b2
Beth Yates <bp1@sanger.ac.uk>
2008-07-07 15:49:46 +0000

    new code for e50 (image from bottom of snpview)

---
+++ modules/EnsEMBL/Web/Component/Variation/Context.pm
@@ -0,0 +1,79 @@
+package EnsEMBL::Web::Component::Variation::Context;
+
+use strict;
+use warnings;
+no warnings "uninitialized";
+use base qw(EnsEMBL::Web::Component::Variation);
+use CGI qw(escapeHTML);
+
+sub _init {
+  my $self = shift;
+  $self->cacheable( 0 );
+  $self->ajaxable(  1 );
+}
+
+
+sub content {
+  my $self = shift;
+  my $object = $self->object;
+
+
+  my $width = $object->param('w') || "30000";
+
+  # first determine correct SNP location 
+  my %mappings = %{ $object->variation_feature_mapping };
+  my $location = $object->core_objects->{'parameters'}{'r'};
+  my ($seq_region, $start, $seq_type);
+
+  foreach my $varif_id (keys %mappings) {
+    ## Check vari feature matches the location we are intrested in
+    my $seq_reg = $mappings{$varif_id}{Chr};
+    my $st  = $mappings{$varif_id}{start};
+    my $end    = $mappings{$varif_id}{end};
+    my $v_loc  = $seq_reg.":".$st."-".$end;
+    my $type =  $mappings{$varif_id}{region_type};
+ 
+    if  ($v_loc eq $location){
+      $seq_region = $seq_reg;
+      $start = $st;
+      $seq_type = $type; 
+    } else { next;}
+  
+         
+  }
+
+  unless ($seq_region) { 
+  my $html = "<p>Unable to draw SNP neighbourhood as we cannot uniquely determine the SNP's location</p>";
+  return $html; 
+  }
+
+  my $end   = $start + ($width/2);
+  $start -= ($width/2);
+  my $slice =
+    $object->database('core')->get_SliceAdaptor()->fetch_by_region(
+    $seq_type, $seq_region, $start, $end, 1
+  );
+
+  my $sliceObj = EnsEMBL::Web::Proxy::Object->new(
+        'Slice', $slice, $object->__data
+       );
+
+  my ($count_snps, $filtered_snps) = $sliceObj->getVariationFeatures();
+  my ($genotyped_count, $genotyped_snps) = $sliceObj->get_genotyped_VariationFeatures();
+
+  my $wuc = $object->user_config_hash( 'snpview' );
+  $wuc->set( '_settings', 'width', $object->param('image_width') );
+  $wuc->{'snps'}           = $filtered_snps;
+  $wuc->{'genotyped_snps'} = $genotyped_snps;
+  $wuc->{'snp_counts'}     = [$count_snps+$genotyped_count, scalar @$filtered_snps+scalar @$genotyped_snps];
+
+  ## If you want to resize this image
+  my $image    = $object->new_image( $slice, $wuc, [$object->name] );
+  $image->imagemap = 'yes';
+
+ my $T = $image->render;
+ return $T;
+}
+
+
+1;
