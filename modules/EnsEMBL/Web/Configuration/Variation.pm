a8f36698611e28a248b0fe3c8f3b8809c2d86a6a
Beth Yates <bp1@sanger.ac.uk>
2008-07-07 15:45:09 +0000

    added in new e50 style code

--- modules/EnsEMBL/Web/Configuration/Variation.pm
+++ modules/EnsEMBL/Web/Configuration/Variation.pm
@@ -10,22 +10,43 @@ use base qw(EnsEMBL::Web::Configuration);
 
 sub set_default_action {
   my $self = shift;
-  $self->{_data}{default} = 'Structure';
+  $self->{_data}{default} = 'Summary';
 }
 
 sub global_context { return $_[0]->_global_context; }
 sub ajax_content   { return $_[0]->_ajax_content;   }
 sub local_context  { return $_[0]->_local_context;  }
+sub local_tools    { return $_[0]->_local_tools;  }
 sub content_panel  { return $_[0]->_content_panel;  }
 sub context_panel  { return $_[0]->_context_panel;  }
 
 sub populate_tree {
   my $self = shift;
 
-  $self->create_node( 'Structure', "Transcript Neighbourhood",
-    [qw(neighbourhood EnsEMBL::Web::Component::Gene::transcript_neighbourhood)],
-    { 'availability' => 1}
+  $self->create_node( 'Summary', "Variation Summary",
+    [qw(summary EnsEMBL::Web::Component::Variation::VariationSummary
+        flanking EnsEMBL::Web::Component::Variation::FlankingSequence )],
+    { 'availability' => 1, 'concise' => 'Summary' }
   );
+  $self->create_node( 'Mappings', "Location",
+    [qw(summary EnsEMBL::Web::Component::Variation::Mappings)],
+    { 'availability' => 1, 'concise' => 'Location' }
+  );
+  $self->create_node( 'Population', "Population genotypes and allele frequencies",
+    [qw(summary EnsEMBL::Web::Component::Variation::PopulationGenotypes)],
+    { 'availability' => 1, 'concise' => 'Population genotypes' }
+  );
+  $self->create_node( 'Individual', "Individual genotypes",
+    [qw(summary EnsEMBL::Web::Component::Variation::IndividualGenotypes)],
+    { 'availability' => 1, 'concise' => 'Individual genotypes' }
+  );
+  $self->create_node( 'context', "Feature Context",
+    [qw(summary EnsEMBL::Web::Component::Variation::Context)],
+    { 'availability' => 1, 'concise' => 'Feature context' }
+  );
+
+
+
 }
 
 1;
