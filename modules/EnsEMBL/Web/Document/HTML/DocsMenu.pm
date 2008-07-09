4c827f6028144ae4071bbae5f1178d04d4065ee5
Anne Parker <ap5@sanger.ac.uk>
2008-07-09 09:00:54 +0000

    Preliminary version of dynamic menu

--- modules/EnsEMBL/Web/Document/HTML/DocsMenu.pm
+++ modules/EnsEMBL/Web/Document/HTML/DocsMenu.pm
@@ -12,13 +12,68 @@ sub render {
   my $sd = $ENSEMBL_WEB_REGISTRY->species_defs;
 
 ## TO DO - make this dynamic, using web tree!
+  my $tree = $sd->STATIC_INFO;
+  warn "SECTIONS: ", join(' ', keys %$tree);
 
-  return( q(<dl id="local">
-<dd class="closed"><a href="/info/website/">Using this website</a></dd>    
-<dd class="closed"><a href="/info/data/">Fetching data</a></dd>    
-<dd class="closed"><a href="/info/docs/">Code documentation</a></dd>    
-<dd class="closed"><a href="/info/about/">About us</a></dd>    
-</dl>) );
+  my $html = qq(
+<dl id="local">
+<dt>Documentation</dt>
+);
+
+  my $title;
+
+  my @sortable_sections;
+  foreach my $section (keys %$tree) {
+    push (@sortable_sections, $section) if ref($tree->{$section}) eq 'HASH';  
+  }
+  my @section_order = sort {
+        $tree->{$a}{_order} <=> $tree->{$b}{_order}
+        || $tree->{$a}{_title} cmp $tree->{$b}{_title}
+        || $tree->{$a} cmp $tree->{$b}
+    }
+    @sortable_sections;
+  
+  foreach my $section (@section_order) {
+    next if $section =~ /^_/;
+    my $subsection = $tree->{$section};
+    next unless keys %$subsection;
+    $title = $subsection->{'_title'} || ucfirst($section);
+    $html .= sprintf(qq(<dd class="open"><a href="%s">%s</a>),
+        $subsection->{'_path'}, $title
+    );
+    my @sortable_subsections;
+    foreach my $sub (keys %$subsection) {
+      push (@sortable_subsections, $sub) if ref($subsection->{$sub}) eq 'HASH';  
+    }
+
+    if (scalar(@sortable_subsections)) { ## we have subpages/dirs, not just metadata
+      my @sub_order = sort { 
+        $subsection->{$a}{_order} <=> $subsection->{$b}{_order}
+        || $subsection->{$a}{_title} cmp $subsection->{$b}{_title}
+        || $subsection->{$a} cmp $subsection->{$b}
+        } 
+         @sortable_subsections;
+      $html .= sprintf(qq(
+        <dl>
+      ), );
+      foreach my $sub (@sub_order) {
+        next if $sub =~ /^_/;
+        my $pages = $subsection->{$sub};
+        next unless keys %$pages;
+        my $path = $pages->{'_path'}.$sub;
+        $title = $pages->{'_title'} || ucfirst($sub);
+        $html .= sprintf(qq(<dd><a href="%s">%s</a>),
+            $path, $title
+        );
+        
+      }
+      $html .= qq(</dl>\n);
+    }
+    $html .= '</dd>';
+  }
+  
+  $html .= '</dl>';
+  return $html;
 }
 
 1;
