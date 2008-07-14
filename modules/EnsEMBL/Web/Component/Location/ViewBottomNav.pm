906834b57f3f414f3f69f328c66ff3406ade513d
James Smith <js5@sanger.ac.uk>
2008-07-14 14:37:02 +0000

    added new nav

--- modules/EnsEMBL/Web/Component/Location/ViewBottomNav.pm
+++ modules/EnsEMBL/Web/Component/Location/ViewBottomNav.pm
@@ -12,7 +12,10 @@ sub _init {
   $self->ajaxable(  0 );
 }
 
-
+sub _nav_url {
+  my( $self, $s, $e ) = @_;
+  return $self->object->_url({'r'=>$self->object->seq_region_name.':'.$s.'-'.$e});
+}
 sub content {
   my $self   = shift;
   my $object = $self->object;
@@ -22,8 +25,11 @@ sub content {
 
   my $extra_html = '';
   my @ramp_entries = ( [4,1e3], [6,5e3], [8,1e4], [10,5e4], [12,1e5], [14,2e5], [16,5e5], [18,1e6] );
+  my $cp = int( ($object->seq_region_end+$object->seq_region_start) / 2);
+  my $wd = $object->seq_region_end-$object->seq_region_start+1;
   foreach(@ramp_entries) {
-    $extra_html .= sprintf( '<a href="#%s"><img alt="" src="/i/blank.gif" class="blank" style="height:%dpx" /></a>', $_->[1],$_->[0] );
+    $extra_html .= sprintf( '<a href="%s"><img title="%d bp" alt="%s bp" src="/i/blank.gif" class="blank" style="height:%dpx" /></a>', 
+      $self->_nav_url( $cp - $_->[1]/2+1, $cp + $_->[1]/2), $_->[1], $_->[1],$_->[0] );
   }
   return sprintf qq(
   <div class="autocenter navbar" style="width:%spx">
@@ -35,7 +41,7 @@ sub content {
     </div></form>
     <a href="%s"><img src="/i/nav-l2.gif" class="zoom" alt="1Mb left"/></a><a 
        href="%s"><img src="/i/nav-l1.gif" class="zoom" alt="window left"/></a><a
-       href="%s"><img src="/i/zoom-plus.gif" class="zoom" alt="zoom in"/></a>$extra_html<a
+       href="%s"><img src="/i/zoom-plus.gif" class="zoom" alt="zoom in"/></a>%s<a
        href="%s"><img src="/i/zoom-minus.gif" class="zoom" alt="zoom out"/></a><a
        href="%s"><img src="/i/nav-r1.gif" class="zoom" alt="window left"/></a><a
        href="%s"><img src="/i/nav-r2.gif" class="zoom" alt="1Mb left"/></a>
@@ -43,12 +49,13 @@ sub content {
    $object->seq_region_name,
    $object->seq_region_start,
    $object->seq_region_end,
-   '#',
-   '#',
-   '#',
-   '#',
-   '#',
-   '#';
+   $self->_nav_url( $object->seq_region_start - 1e6, $object->seq_region_end - 1e6 ),
+   $self->_nav_url( $object->seq_region_start - $wd, $object->seq_region_end - $wd ),
+   $self->_nav_url( $cp - int($wd/4)+1,              $cp + int($wd/4) ),
+   $extra_html,
+   $self->_nav_url( $cp - $wd+1,                     $cp + $wd ),
+   $self->_nav_url( $object->seq_region_start - $wd, $object->seq_region_end - $wd ),
+   $self->_nav_url( $object->seq_region_start + 1e6, $object->seq_region_end + 1e6 ),
 }
 
 
