4ecfb049235747826607bce0e749aaad8f163bd6
Steve Trevanion <st3@sanger.ac.uk>
2008-07-14 10:20:29 +0000

    new options for formatting wise2 output

--- modules/EnsEMBL/Web/Object/Transcript.pm
+++ modules/EnsEMBL/Web/Object/Transcript.pm
@@ -1535,8 +1535,10 @@ sub get_alignment{
   }
   my $int_seq_file = $self->save_seq($int_seq);
   my $ext_seq_file = $self->save_seq($ext_seq);
+  my $label_width  = '22'; #width of column for e! object label
+  my $output_width = 60; #width of alignment
   my $dnaAlignExe = "%s/bin/matcher -asequence %s -bsequence %s -outfile %s %s";
-  my $pepAlignExe = "%s/bin/psw -m %s/wisecfg/blosum62.bla %s %s > %s";
+  my $pepAlignExe = "%s/bin/psw -m %s/wisecfg/blosum62.bla %s %s -n %s -w %s > %s";
 
   my $out_file = time().int(rand()*100000000).$$;
   $out_file = $self->species_defs->ENSEMBL_TMP_DIR.'/'.$out_file.'.out';
@@ -1555,7 +1557,7 @@ sub get_alignment{
   }
 
   elsif( $seq_type eq 'PEP' ){
-	  $command = sprintf( $pepAlignExe, $self->species_defs->ENSEMBL_WISE2_PATH, $self->species_defs->ENSEMBL_WISE2_PATH, $int_seq_file, $ext_seq_file, $out_file );
+	  $command = sprintf( $pepAlignExe, $self->species_defs->ENSEMBL_WISE2_PATH, $self->species_defs->ENSEMBL_WISE2_PATH, $int_seq_file, $ext_seq_file, $label_width, $output_width, $out_file );
 	  `$command`;
 	  unless (open( OUT, "<$out_file" )) {
 		  $self->problem('fatal', "Cannot open alignment file.", $!);
