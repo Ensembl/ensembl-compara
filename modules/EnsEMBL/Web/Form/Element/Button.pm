22092ca565a9ed5cbdf26cc6f38ff8dc063dd996
Anne Parker <ap5@sanger.ac.uk>
2008-07-14 12:33:02 +0000

    Removed ability to set onclick attribute

--- modules/EnsEMBL/Web/Form/Element/Button.pm
+++ modules/EnsEMBL/Web/Form/Element/Button.pm
@@ -12,10 +12,10 @@ sub new {
  }
 
 sub render { 
-    return sprintf( '<input type="button" name="%s" id="%s" value="%s" class="red-button" %s />', 
+    return sprintf( '<input type="button" name="%s" id="%s" value="%s" class="red-button" />', 
 		    CGI::escapeHTML($_[0]->name) || 'submit', 
         CGI::escapeHTML($_[0]->id) || 'button_'.CGI::escapeHTML($_[0]->name),
 		    CGI::escapeHTML($_[0]->value), 
-		    $_[0]->onclick ? sprintf("onclick=\"%s\"", $_[0]->onclick) : '');
+		    );
 }  
 1;
