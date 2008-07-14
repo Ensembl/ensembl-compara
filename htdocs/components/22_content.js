74643feed15e8db9a6a73dea55a346f3c48a8f35
James Smith <js5@sanger.ac.uk>
2008-07-14 08:50:19 +0000

    tidied up saving of favourites - now with less functions and doesnt need any javascript links

--- htdocs/components/22_content.js
+++ htdocs/components/22_content.js
@@ -1,6 +1,8 @@
 /* Bits of JS used on static pages */
 
-/* "Jump to" on species list (home page) */
+/** 
+  "Jump to" on species list (home page)
+**/
 
 function dropdown_redirect( evt ) {
   var el = Event.findElement(evt,'select');
@@ -15,51 +17,45 @@ function __init_dropdown_redirect( ) {
   });
 }
 addLoadEvent( __init_dropdown_redirect );
-/* Drag'n'drop selection of favourite species */
+
+/**
+  Drag'n'drop selection of favourite species
+**/
 
 var performedSave = 0;
 
 function toggle_reorder() {
-  if ($('reorder_species').style.display == 'none') {
-    $('reorder_species').style.display = 'block';
-    $('full_species').style.display = 'none';
-  } else {
-    $('full_species').style.display = 'block';
-    $('reorder_species').style.display = 'none';
-  }
+  $('reorder_species').toggle();
+  $('full_species').toggle();
 }
 
 function update_species(element) {
   //alert(element.id);
-  if (!performedSave) {
+  if( !performedSave ) {
     performedSave = 1;
-    var url = "/Account/SaveFavourites";
-    var data = "favourites=" + serialize_fave('favourites_list');
-    var prepare = new Ajax.Request(url,
-                         { method: 'get', parameters: data, onComplete: saved });
+    var serialized_data = $A($('favourites_list').childNodes).map(function(n){return n.id.split('-')[1];}).join(',');
+    new Ajax.Request( '/Account/SaveFavourites', {
+      method    : 'get',
+      parameters: { favourites: serialize_fave('favourites_list') },
+      onSuccess: function(response){ $('full_species').innerHTML = response.responseText; performedSave = 0; },
+      onFailure: function(response){ performedSave = 0; }
+    });
   } 
-}
-
-function saved(response) {
-  $('full_species').innerHTML = response.responseText;
-  performedSave = 0;
-}
-
-function serialize_fave(element) {
-  // Based on Sortable.serialize from Scriptaculous
-  var items = $(element).childNodes;
-  var queryComponents = new Array();
-  for(var i=0; i<items.length; i++) {
-    queryComponents.push(items[i].id.split("-")[1]);
-  }
-  return queryComponents.join(",");
 }
 
 function __init_species_reorder() {
   if ($('species_list')) {
-    Sortable.create('species_list', {"onUpdate":update_species, containment:["species_list","favourites_list"], dropOnEmpty:false});
-    Sortable.create('favourites_list', {"onUpdate":update_species, containment:["species_list","favourites_list"], dropOnEmpty:false});
+    ['species_list','favourites_list'].each(function(v){
+      Sortable.create( v,{
+        'onUpdate'  : update_species,
+        containment : ["species_list","favourites_list"],
+        dropOnEmpty : false
+      });
+    });
   }
+  $$('.toggle_link').each(function(n){
+    Event.observe(n,'click',toggle_reorder);
+  });
 }
 
 addLoadEvent( __init_species_reorder );
