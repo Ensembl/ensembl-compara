f1f40ec0762337d8cfa05b30960a9ad129dda519
Steve Trevanion <st3@sanger.ac.uk>
2008-07-21 16:19:17 +0000

    whitespace

--- modules/Bio/EnsEMBL/GlyphSet/TSE_background_exon.pm
+++ modules/Bio/EnsEMBL/GlyphSet/TSE_background_exon.pm
@@ -13,26 +13,28 @@ use Data::Dumper;
 
 #needed for drawing vertical lines on supporting evidence view
 sub _init {
-	my ($self) = @_;
-	my $Config = $self->{'config'};
+    my ($self) = @_;
+    my $Config = $self->{'config'};
     my $flag   = $self->my_config('flag');
+    
+    #retrieve tag locations and colours calculated by TSE_transcript track
+    my $tags   = $Config->{'tags'};
 
-	#retrieve tag locations and colours calculated by TSE_transcript track
-	my $tags   = $Config->{'tags'};
+#    warn Dumper($tags);
 
-	foreach my $tag (@{$tags}) {
-		my ($extra,$e,$s) = split ':', $tag->[0];
-		my $col = $tag->[1];
-		my $tglyph = new Sanger::Graphics::Glyph::Space({
-			'x' => $s-1,
-			'y' => 0,
-			'height' => 0,
-			'width'  => $e-$s,
-			'colour' => '$col',
-		});
-		$self->join_tag( $tglyph, $tag->[0], $flag,  0, $col, 'fill', -99 );
-		$self->join_tag( $tglyph, $tag->[0], 1-$flag,0, $col, 'fill', -99  );
-		$self->push( $tglyph );
-	}
+    foreach my $tag (@{$tags}) {
+	my ($extra,$e,$s) = split ':', $tag->[0];
+	my $col = $tag->[1];
+	my $tglyph = new Sanger::Graphics::Glyph::Space({
+	    'x' => $s-1,
+	    'y' => 0,
+	    'height' => 0,
+	    'width'  => $e-$s,
+	    'colour' => '$col',
+	});
+	$self->join_tag( $tglyph, $tag->[0], $flag,  0, $col, 'fill', -99 );
+	$self->join_tag( $tglyph, $tag->[0], 1-$flag,0, $col, 'fill', -99  );
+	$self->push( $tglyph );
+    }
 }
 1;
