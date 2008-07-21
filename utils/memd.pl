78051043c3220daccd55a6dba0809c59089865fd
Evgeny Bragin <eb4@sanger.ac.uk>
2008-07-21 16:38:47 +0000

    *** empty log message ***

--- utils/memd.pl
+++ utils/memd.pl
@@ -18,35 +18,45 @@ BEGIN{
 
 my $memd = new EnsEMBL::Web::Cache;
 
-if ($ARGV[0] =~ /get/i) {
-  print $memd->get($ARGV[1])."\n";
-} elsif ($ARGV[0] =~ /(tags?)?_?delete/i) {
-  shift @ARGV;
-  print $memd->delete_by_tags(@ARGV)."\n";
-} elsif ($ARGV[0] =~ /flush_?all/i) {
-  print "Flushing cache:\n";
-  print $memd->delete_by_tags." cache items deleted\n";
-} else {
+if ($memd) {
 
-  my $debug_key_list = $memd->get('debug_key_list');
-  my $key_list = {};
-  
-  if ($debug_key_list) {
-    if (my $pattern = $ARGV[0]) {
-  
-      %$key_list = map { $_ => $debug_key_list->{$_} }
-                     grep { /$pattern/ }
-                        keys %$debug_key_list;
-  
-    } else {
-      $key_list = $debug_key_list;
-    }
-    
-    print Dumper($key_list);
+  if ($ARGV[0] =~ /get/i) {
+    print $memd->get($ARGV[1])."\n";
+  } elsif ($ARGV[0] =~ /(tags?)?_?delete/i) {
+    shift @ARGV;
+    print $memd->delete_by_tags(@ARGV)."\n";
+  } elsif ($ARGV[0] =~ /flush_?all/i) {
+    print "Flushing cache:\n";
+    print $memd->delete_by_tags." cache items deleted\n";
+  } elsif ($ARGV[0] =~ /stats/i) {
+    shift @ARGV;
+    print "Stats:\n";
+    print Dumper($memd->stats(@ARGV))."\n";
   } else {
-    print "No debug_key_list found \n";
+  
+    my $debug_key_list = $memd->get('debug_key_list');
+    my $key_list = {};
+    
+    if ($debug_key_list) {
+      if (my $pattern = $ARGV[0]) {
+    
+        %$key_list = map { $_ => $debug_key_list->{$_} }
+                       grep { /$pattern/ }
+                          keys %$debug_key_list;
+    
+      } else {
+        $key_list = $debug_key_list;
+      }
+      
+      print Dumper($key_list);
+    } else {
+      print "No debug_key_list found \n";
+    }
+  
   }
-
+} else {
+   print "No memd server configured or can not connect\n";
 }
+
 
 1;
