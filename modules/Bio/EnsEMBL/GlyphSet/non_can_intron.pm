e4e453ca655c5d98ec7a4c13e5edbb502cae4ab0
Steve Trevanion <st3@sanger.ac.uk>
2008-07-22 16:13:38 +0000

    tweaks to position of the I bar

--- modules/Bio/EnsEMBL/GlyphSet/non_can_intron.pm
+++ modules/Bio/EnsEMBL/GlyphSet/non_can_intron.pm
@@ -3,13 +3,7 @@ use strict;
 use vars qw(@ISA);
 use Bio::EnsEMBL::GlyphSet;
 @ISA = qw(Bio::EnsEMBL::GlyphSet);
-use Sanger::Graphics::Glyph::Rect;
-use Sanger::Graphics::Glyph::Text;
-use Sanger::Graphics::Glyph::Composite;
 use Sanger::Graphics::Glyph::Line;
-use Sanger::Graphics::Bump;
-use Bio::EnsEMBL::Utils::Eprof qw(eprof_start eprof_end);
-use Data::Dumper;
 
 sub init_label {
   my ($self) = @_;
@@ -18,63 +12,59 @@ sub init_label {
 }
 
 sub _init {
-	my ($self) = @_;
-	my $Config  = $self->{'config'};
-	my $length  = $Config->container_width();
-	my $colour  = $Config->get('non_can_intron','col');
+    my ($self) = @_;
+    my $Config  = $self->{'config'};
+    my $length  = $Config->container_width();
+    my $colour  = $Config->get('non_can_intron','col');
+    my $trans_ref = $Config->{'transcript'};
+    return unless ($trans_ref->{'non_con_introns'});
+    my @introns = @{$trans_ref->{'non_con_introns'}};
 
-#	my $strand = $trans_ref->{'exons'}[0][2]->strand;
-	my $trans_ref = $Config->{'transcript'};
-	return unless ($trans_ref->{'non_con_introns'});
-	my @introns = @{$trans_ref->{'non_con_introns'}};
-
-	##draw the introns themselves....	
-	foreach my $intron (@introns) { 
-		next unless defined $intron; #Skip this exon if it is not defined (can happen w/ genscans) 
-		my($box_start, $box_end);
-		my $exon_names = $intron->[4];
-		# only draw this exon if is inside the slice
-		$box_start = $intron->[0];
-		$box_start = 1 if $box_start < 1 ;
-		$box_end   = $intron->[1];
-		$box_end    = $length if $box_end > $length;
+    foreach my $intron (@introns) { 
+	next unless defined $intron;
+	my $exon_names = $intron->[4];
+	
+	# only draw this exon if is inside the slice
+	my $box_start = $intron->[0];
+	$box_start = 1 if $box_start < 1 ;
+	my $box_end   = $intron->[1];
+	$box_end    = $length if $box_end > $length;
 		
-		#Draw an I-bar covering the intron
-		my $G = new Sanger::Graphics::Glyph::Line({
-			'x'         => $box_start ,
-			'y'         => 0,
-			'width'     => $box_end-$box_start +1,
-			'height'    => 0,
-			'colour'    => $colour,
-			'absolutey' => 1,
-			'title'     => "$exon_names",
-			'href'      => '',
-		});
-		$self->push( $G );
-		$G = new Sanger::Graphics::Glyph::Line({
-			'x'         => $box_start,
-			'y'         => -3,
-			'width'     => 0,
-			'height'    => 6,
-			'colour'    => $colour,
-			'absolutey' => 1,
-			'title'     => "$exon_names",
-			'href'      => '',
-		});
-		$self->push( $G );
-		$G = new Sanger::Graphics::Glyph::Line({
-			'x'         => $box_end+1 ,
-			'y'         => -3,
-			'width'     => 0,
-			'height'    => 6,
-			'colour'    => $colour,
-			'absolutey' => 1,
-			'title'     => "$exon_names",
-			'href'      => '',
-		});	
-		$self->push( $G )
-	}
+	#Draw an I-bar covering the intron
+	my $G = new Sanger::Graphics::Glyph::Line({
+	    'x'         => $box_start ,
+	    'y'         => 0,
+	    'width'     => $box_end-$box_start,
+	    'height'    => 0,
+	    'colour'    => $colour,
+	    'absolutey' => 1,
+	    'title'     => "$exon_names",
+	    'href'      => '',
+	});
+	$self->push( $G );
+	$G = new Sanger::Graphics::Glyph::Line({
+	    'x'         => $box_start,
+	    'y'         => -3,
+	    'width'     => 0,
+	    'height'    => 6,
+	    'colour'    => $colour,
+	    'absolutey' => 1,
+	    'title'     => "$exon_names",
+	    'href'      => '',
+	});
+	$self->push( $G );
+	$G = new Sanger::Graphics::Glyph::Line({
+	    'x'         => $box_end ,
+	    'y'         => -3,
+	    'width'     => 0,
+	    'height'    => 6,
+	    'colour'    => $colour,
+	    'absolutey' => 1,
+	    'title'     => "$exon_names",
+	    'href'      => '',
+	});	
+	$self->push( $G )
+    }
 }
-
 
 1;
