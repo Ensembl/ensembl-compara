70bb938297a9cd0c6da11cbce91f14d4b819e8cb
Steve Trevanion <st3@sanger.ac.uk>
2008-07-28 15:09:46 +0000

    changes to work with uniprot (search on secondary accession)

--- modules/EnsEMBL/Web/ExtIndex/MFETCH.pm
+++ modules/EnsEMBL/Web/ExtIndex/MFETCH.pm
@@ -9,40 +9,48 @@ sub new { my $class = shift; my $self = {}; bless $self, $class; return $self; }
 sub get_seq_by_acc { my $self = shift; $self->get_seq_by_id( @_ ); }
 
 sub get_seq_by_id {
-	my ($self, $arghashref)=@_;
+    my ($self, $arghashref)=@_;
+    
+    # Get the ID to mfetch
+    my $id = $arghashref->{'ID'} || return [];
+    my $db = $arghashref->{'DB'};
+    my $str;
+    
+    #hack to get a CCDS record using mfetch - need a wildcard for version
+    if ($db eq 'CCDS') {
+	if ($id !~ /\.\d{1,3}$/) {
+	    $id .= '.*';
+	}
+	$str = qq(-d refseq -i ccds:).$id;
+	$str .= qq(&div:NM -v fasta);
+    }
+    elsif ($db =~ /uniprot/i) {
+	#strip off any version number for uniprot
+	$id =~ s/\.\d{1,3}$//;
+	$str = qq(-d uniprot -i acc:).$id.' -v fasta';
+    }
+    else {
+	$str == $id;
+    }
 
-	# Get the ID to mfetch
-	my $id = $arghashref->{'ID'} || return [];
-	my $db = $arghashref->{'DB'};
-	my $str;
-	
-	#hack to get a CCDS record using mfetch - need a wildcard for version
-	if ($db eq 'CCDS') {
-		if ($id !~ /\.d{1,3}$/) {
-			$id .= '.*';
-		}
-		$str = qq(-d refseq -i ccds:).$id;
-		$str .= qq(&div:NM -v fasta);
-	}
-
-	#get the mfetch server
-	my $server = $self->fetch_mfetch_server(
-		$arghashref->{'species_defs'}->ENSEMBL_MFETCH_SERVER_1,
-		$arghashref->{'species_defs'}->ENSEMBL_MFETCH_SERVER_2,
-		$arghashref->{'species_defs'}->ENSEMBL_MFETCH_PORT
-	);
-	#get the sequence from the server
-	if ($server){
-		my $hostname = &Sys::Hostname::hostname();
-		print $server "$str \n";		
-		my $output;
-		push @$output, $_ while(<$server>);
-		return $output;
-	}
-	else {
-		print qq(Problems retrieving the $db sequence, please refresh the page\n);
-	}
-#	$server->shutdown;
+    #get the mfetch server
+    my $server = $self->fetch_mfetch_server(
+	$arghashref->{'species_defs'}->ENSEMBL_MFETCH_SERVER_1,
+	$arghashref->{'species_defs'}->ENSEMBL_MFETCH_SERVER_2,
+	$arghashref->{'species_defs'}->ENSEMBL_MFETCH_PORT
+    );
+    #get the sequence from the server
+    if ($server){
+	my $hostname = &Sys::Hostname::hostname();
+	print $server "$str \n";		
+	my $output;
+	push @$output, $_ while(<$server>);
+	return $output;
+    }
+    else {
+	print qq(Problems retrieving the $db sequence, please refresh the page\n);
+    }
+    #	$server->shutdown;
 }
 
 sub fetch_mfetch_server {
