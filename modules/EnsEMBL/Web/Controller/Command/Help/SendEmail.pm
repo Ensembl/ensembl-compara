1250954ef01daf8dd799ebbf835fb5d9ad9dc3d7
Anne Parker <ap5@sanger.ac.uk>
2008-07-30 15:28:46 +0000

    Updated to use E::W::Mailer directly instead of doing it via Help object

--- modules/EnsEMBL/Web/Controller/Command/Help/SendEmail.pm
+++ modules/EnsEMBL/Web/Controller/Command/Help/SendEmail.pm
@@ -1,9 +1,13 @@
 package EnsEMBL::Web::Controller::Command::Help::SendEmail;
 
+## Sends the contents of the helpdesk contact form (after checking for spam posting)
+
 use strict;
 use warnings;
 
 use Class::Std;
+use EnsEMBL::Web::Mailer;
+use EnsEMBL::Web::RegObj;
 
 use base 'EnsEMBL::Web::Controller::Command';
 
@@ -13,42 +17,47 @@ sub BUILD {
   my ($self, $ident, $args) = @_;
 }
 
-sub render {
-  my ($self, $action) = @_;
-  $self->set_action($action);
-  if ($self->not_allowed) {
-    $self->render_message;
-  } else {
-    $self->process;
-  }
-}
-
 sub process {
   my $self = shift;
+  my $cgi = $self->action->cgi;
 
-  ## Do search
-  my $webpage= new EnsEMBL::Web::Document::WebPage(
-      'doctype'    => 'Popup',
-      'renderer'   => 'Apache',
-      'outputtype' => 'HTML',
-      'scriptname' => 'help/send_email',
-      'objecttype' => 'Help',
-  );
+  my $mailer = EnsEMBL::Web::Mailer->new();
 
-  my $object;
-  if( $webpage->has_a_problem() ) {
-    $webpage->render_error_page( $webpage->problem->[0] );
-  } else {
-    foreach my $obj( @{$webpage->dataObjects} ) {
-      $object = $obj;
-    }
+  my $comments = $cgi->param('comments');
+  my $spam = $mailer->spam_check($comments);
+
+  unless ($spam) {
+    my $species_defs = $ENSEMBL_WEB_REGISTRY->species_defs;
+
+    my $subject = $cgi->param('subject') || $species_defs->ENSEMBL_SITETYPE.' Helpdesk';
+    $subject .= ' - '.$species_defs->ENSEMBL_SERVERNAME;
+
+    my @mail_attributes = ();
+    my @T = localtime();
+    my $date = sprintf "%04d-%02d-%02d %02d:%02d:%02d", $T[5]+1900, $T[4]+1, $T[3], $T[2], $T[1], $T[0];
+    my $url = CGI::unescape($cgi->param('ref'));
+    push @mail_attributes, (
+      [ 'Date',         $date ],
+      [ 'Name',         $cgi->param('name') ],
+      [ 'Referrer',     $url ],
+      [ 'Last Keyword', $cgi->param('kw')||'-none-' ],
+      [ 'User agent',   $ENV{'HTTP_USER_AGENT'}],
+    );
+    my $message = 'Support question from '.$species_defs->ENSEMBL_SERVERNAME."\n\n";
+    $message .= join "\n", map {sprintf("%-16.16s %s","$_->[0]:",$_->[1])} @mail_attributes;
+    $message .= "\n\nComments:\n\n$comments\n\n";
+
+    $mailer->set_mail_server('localhost');
+    $mailer->set_from($cgi->param('email'));
+    $mailer->set_to($species_defs->ENSEMBL_HELPDESK_EMAIL);
+    $mailer->set_subject($subject);
+    $mailer->set_message($message);
+
+    $mailer->send({'spam_check' => 0});
   }
- 
-  $object->send_email;
-   
-  $webpage->configure( $object, 'thanks', 'context_menu' );
-  $webpage->action();
-  
+
+  my $new_param = {};
+  $cgi->redirect($self->url('/Help/EmailSent', $new_param));
 }
 
 }
