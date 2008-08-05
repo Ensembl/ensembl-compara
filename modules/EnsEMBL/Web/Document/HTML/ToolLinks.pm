d3cceea5664e0a5c4c19c34262903f790362d4dd
James Smith <js5@sanger.ac.uk>
2008-08-05 17:21:59 +0000

    changed link to modal_link

--- modules/EnsEMBL/Web/Document/HTML/ToolLinks.pm
+++ modules/EnsEMBL/Web/Document/HTML/ToolLinks.pm
@@ -40,7 +40,7 @@ sub render   {
   $html .= qq(
       <a href="$species/Blast">BLAST</a> &nbsp;|&nbsp; 
       <a href="$species/Biomart">BioMart</a> &nbsp;|&nbsp;
-      <a href="/info/website/help/" id="help"><img src="/i/e-quest_bg.gif" alt="e?" style="vertical-align:middle" />&nbsp;Help</a>);
+      <a href="/info/website/help/" class="modal_link" id="help"><img src="/i/e-quest_bg.gif" alt="e?" style="vertical-align:middle" />&nbsp;Help</a>);
 
   $self->print($html);
 }
