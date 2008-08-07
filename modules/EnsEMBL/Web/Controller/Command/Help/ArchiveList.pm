9cb37abf383183014f6a81f9d270d5062d1d09b9
Anne Parker <ap5@sanger.ac.uk>
2008-08-07 09:24:28 +0000

    New modules for footer links

---
+++ modules/EnsEMBL/Web/Controller/Command/Help/ArchiveList.pm
@@ -0,0 +1,24 @@
+package EnsEMBL::Web::Controller::Command::Help::ArchiveList;
+
+use strict;
+use warnings;
+
+use Class::Std;
+
+use base 'EnsEMBL::Web::Controller::Command';
+
+{
+
+sub BUILD {
+  my ($self, $ident, $args) = @_; 
+}
+
+sub process {
+  my $self = shift;
+  EnsEMBL::Web::Magic::stuff('Help', 'ArchiveList', $self, 'Dynamic');
+}
+
+
+}
+
+1;
