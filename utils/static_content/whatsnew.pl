6c98add67c897c6331793b808efb3297feb42e88
Anne Parker <ap5@sanger.ac.uk>
2008-08-07 10:56:04 +0000

    Updated to use new Class::DBI objects

--- utils/static_content/whatsnew.pl
+++ utils/static_content/whatsnew.pl
@@ -15,8 +15,6 @@ use File::Basename qw( dirname );
 use Pod::Usage;
 use Getopt::Long;
 
-use EnsEMBL::Web::Data::NewsItem;
-
 our $SERVERROOT;
 my ( $help, $info, @species);
 
@@ -44,12 +42,12 @@ my $release_id = $SiteDefs::VERSION;
 print "Outputting Release $release_id What's New to $output_root\n\n";
 
 # Connect to web database and get news adaptor
-use EnsEMBL::Web::DBSQL::NewsAdaptor;
 use EnsEMBL::Web::RegObj;
+use EnsEMBL::Web::Data::NewsItem;
+use EnsEMBL::Web::Data::Species;
 
 $ENSEMBL_WEB_REGISTRY = EnsEMBL::Web::Registry->new();
 my $SD = $ENSEMBL_WEB_REGISTRY->species_defs;
-my $wa = $ENSEMBL_WEB_REGISTRY->newsAdaptor;
 
 
 #---------------- DO APPROPRIATE QUERIES AND OUTPUT NEWS FILE ----------------#
@@ -57,9 +55,12 @@ my $wa = $ENSEMBL_WEB_REGISTRY->newsAdaptor;
 # general query - will probably need it at least once
 
 # get a list of valid species for this release
-our $rel_spp = $wa->fetch_species($release_id);
-# reverse it so we can look up species ID by name
-our %rev_hash = reverse %$rel_spp;
+our @rel_spp = EnsEMBL::Web::Data::Species->find_all;
+# create reverse hash so we can look up species ID by name
+our %rev_hash;
+foreach my $sp_obj (@rel_spp) {
+  $rev_hash{$sp_obj->name} = $sp_obj->id;
+}
 
 # check validity of user-provided species
 my @valid_spp;
@@ -74,8 +75,8 @@ if (@species && $species[0] ne 'none') {
   }
 }
 elsif (!@species) { ## defaults to all
-  foreach my $sp (values %$rel_spp) {
-    push (@valid_spp, $sp);
+  foreach my $sp_obj (@rel_spp) {
+    push (@valid_spp, $sp_obj->name);
   }
 }
 
@@ -84,79 +85,90 @@ my ($output_dir, $html, $title, $extra);
 #### NEWS FOR INDIVIDUAL SPECIES ####
 foreach my $sp (@valid_spp) {
 
-        (my $pretty_sp = $sp) =~ s/_/ /;
+  (my $pretty_sp = $sp) =~ s/_/ /;
                                                                                 
-        $html = qq(<h3 class="boxed">What's New in Ensembl $release_id</h3>\n);
+  $html = qq(<h3 class="boxed">What's New in Ensembl $release_id</h3>\n);
         
-        # get stories for this species
-        my $species_id = $rev_hash{$sp};
-        my @stories = EnsEMBL::Web::Data::NewsItem->search(
-          {
-            release => $release_id,
-            status  => 'news_ok',
-            species.species_id => {-or => [ {IS => 'NULL'}, $species_id ]},
-          },
-          {
-            order_by => 'species_id, priority',
-            rows     => 5,
-          }
-        );
+  # get stories for this species
+  my $species_id = $rev_hash{$sp};
+  my $criteria = {
+    'species'     => $species_id,
+    'release_id'  => $release_id,
+    'news_done'   => 'Y',
+  };
+  my $attr = {
+    'order_by'  => 'priority DESC',
+    'limit'     => 5,
+  };
 
-        $html .= qq(<h4><i>$pretty_sp</i> News</h4>);
+  my @stories = EnsEMBL::Web::Data::NewsItem->fetch_news_items($criteria, $attr);
 
-        if (@stories) {
-          ## output species stories
-          $html .= qq(<ul class="spaced">\n);
-          $html .= &output_stories(\@stories, 'species');
-          $html .= qq(</ul>);
-        } else {
-          $html .= qq(<p>There is no <i>$pretty_sp</i>-specific news this release.</p>\n\n);
-        }
-    
-        print STDERR "\nINFO: Adding $total species stories for $sp\n";
+  $html .= qq(<h4><i>$pretty_sp</i> News</h4>);
 
-        ## finish file and write it out
-        $html .= qq(<p><a href="/$sp/newsview?rel=$release_id">More news...</a></p>\n\n);
-        $output_dir =  $output_root.$sp.'/ssi/';
-        &check_dir($output_dir);
-        &output_news_file($output_dir, $html);
-    }
+  if (@stories) {
+    ## output species stories
+    $html .= qq(<ul class="spaced">\n);
+    $html .= &output_stories(\@stories, 'species');
+    $html .= qq(</ul>);
+  } 
+  else {
+    $html .= qq(<p>There is no <i>$pretty_sp</i>-specific news this release.</p>\n\n);
+  }
 
+  my $total = @stories; 
+  if ($total < 5) {
+    $html .= qq(<h4>General News</h4>);
+    delete($criteria->{'species'});
+    $attr->{'limit'} = 5 - $total;
+    my @generic =  EnsEMBL::Web::Data::NewsItem->fetch_news_items($criteria, $attr);
+    if (@generic) {
+      ## output generic stories
+      $html .= qq(<ul class="spaced">\n);
+      $html .= &output_stories(\@generic);
+      $html .= qq(</ul>);
+    } 
+  }
+   
+  print STDERR "\nINFO: Adding $total species stories for $sp\n";
+
+  ## finish file and write it out
+  $html .= qq(<p><a href="/$sp/newsview?rel=$release_id">More news...</a></p>\n\n);
+  $output_dir =  $output_root.$sp.'/ssi/';
+  &check_dir($output_dir);
+  &output_news_file($output_dir, $html);
 }
 
 #--------------------------------------------------------
 
 sub output_stories {
-    my ($stories, $style, $limit) = @_;
+  my $stories = shift;
 
-    my $html;
-    my $prev_cat = 0;
-    my $prev_sp = 0;
+  my $html;
 
-    foreach my $story (@$stories) {
+  foreach my $story (@$stories) {
 
-        my $item_id = $story->id;
-        my $title      = $story->title;
-        my $content    = $story->content;
-        my $release_id = $story->release_id;
-        my @species    = $story->species;
+    my $item_id = $story->id;
+    my $title      = $story->title;
+    my $content    = $story->content;
+    my $release_id = $story->release_id;
+    my @species    = $story->species;
 
-        my $sp_dir = @species ? $species[0]->name : 'Multi';
+    my $sp_dir = @species ? $species[0]->name : 'Multi';
 
 
-        # truncate content if story is over nnn chars
-        if ($style && $style eq 'species' && length($content) > 250) {
-            my @story_lines = split(/[.:][ <]/, $content);
-            $content = $story_lines[0];
-            ## strip out list tags as they break the XHTML
-            $content =~ s/(ul|ol|dl)>/div>/g;
-            $content =~ s/(li|dt|dd)>/p>/g;
-            $content .= qq(.<br /><a href="/$sp_dir/newsview?rel=$release_id#item$item_id">Read more</a>...\n);
-        }
-
-        $html .= qq(<li><strong>$title</strong><br />$content</li>\n);
+    # truncate content if story is over nnn chars
+    if (length($content) > 250) {
+      my @story_lines = split(/[.:][ <]/, $content);
+      $content = $story_lines[0];
+      ## strip out list tags as they break the XHTML
+      $content =~ s/(ul|ol|dl)>/div>/g;
+      $content =~ s/(li|dt|dd)>/p>/g;
+      $content .= qq(.<br /><a href="/$sp_dir/newsview?rel=$release_id#item$item_id">Read more</a>...\n);
     }
-    return $html;
+
+    $html .= qq(<li><strong>$title</strong><br />$content</li>\n);
+  }
+  return $html;
 }
 
 sub check_dir {
