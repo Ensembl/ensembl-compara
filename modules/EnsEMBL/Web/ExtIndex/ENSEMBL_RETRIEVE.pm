aa79b2d019bb852bcaf52f5dcc6300dfea8e6723
Steve Trevanion <st3@sanger.ac.uk>
2008-08-08 16:53:34 +0000

    return CDS of *current* genes (longest CDS) and transcripts

--- modules/EnsEMBL/Web/ExtIndex/ENSEMBL_RETRIEVE.pm
+++ modules/EnsEMBL/Web/ExtIndex/ENSEMBL_RETRIEVE.pm
@@ -12,19 +12,53 @@ sub get_seq_by_id {
     my ($db,$sp,$type) = split '_',$arghashref->{'DB'};
     $sp = lc($sp);
     my $species = $arghashref->{'species_defs'}->species_full_name($sp);
-
-    #retrieve details of stable ids - use stable ID mapping to account for retired IDs
     my $ID = $arghashref->{'ID'};
-    my $archStableIdAdap = $reg->get_adaptor($species, 'core', 'ArchiveStableId');
-    my $obj = $archStableIdAdap->fetch_by_stable_id($ID);
-    my $all_records = $obj->get_all_associated_archived;
-    my @sorted_records = sort { $a->[0]->release <=> $b->[0]->release ||
+
+    my $seq;
+    my $pep_seq = '';
+
+    #retrieve peptide sequence
+    #- for translations use stable ID mapping to account for retired IDs
+    if ($type eq 'TRANSLATION') {
+	my $archStableIdAdap = $reg->get_adaptor($species, 'core', 'ArchiveStableId');
+	my $obj = $archStableIdAdap->fetch_by_stable_id($ID);
+	my $all_records = $obj->get_all_associated_archived;
+	return [] unless @$all_records;
+	my @sorted_records = sort { $a->[0]->release <=> $b->[0]->release ||
 				$a->[0]->stable_id cmp $b->[0]->stable_id } @$all_records;
+	$pep_seq = $sorted_records[0]->[3];
+	$seq = [ ">$ID" ];
+    }
+    #- for genes and transcripts only search the current IDS since I don't know how to get these archived IDS for these
+    elsif ($type eq 'GENE') {
+	my $gene_adap = $reg->get_adaptor($species, 'core', 'Gene');
+	if (my $gene = $gene_adap->fetch_by_stable_id($ID)) {
+	    foreach my $trans (@{$gene->get_all_Transcripts()}) {
+		if (my $trl = $trans->translation) {
+		    if ( length($trl->seq) > length($pep_seq) ) {
+			$pep_seq =  $trl->seq;
+			my $id = $trl->stable_id;
+			$seq = [ ">$id" ];
+		    }
+		}
+	    }
+	}
+    }
+    elsif ($type eq 'TRANSCRIPT') {
+	my $trans_adap = $reg->get_adaptor($species, 'core', 'Transcript');
+	if (my $trans = $trans_adap->fetch_by_stable_id($ID)) {
+	    if (my $trl = $trans->translate) {
+		$pep_seq =  $trl->seq;
+		my $id = $trl->stable_id;
+		$seq = [ ">$id" ];
+	    }
+	}
+    }
+
+    return [] if (! $pep_seq);
 
     #generate arrayref of header and amino acid sequence ready for using by WISE2
-    my $seq = [ ">$ID" ];
     my $pos = 0;
-    my $pep_seq = $sorted_records[0]->[3];
     while ( $pos < length($pep_seq) ) {
 	my $substr = substr($pep_seq,$pos,60);
 	$substr .= "\n";
