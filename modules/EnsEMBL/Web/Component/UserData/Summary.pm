8ff1c445f80e35c41315d395a66f905a97a5161a
Anne Parker <ap5@sanger.ac.uk>
2008-08-20 10:13:37 +0000

    Removed unused content

--- modules/EnsEMBL/Web/Component/UserData/Summary.pm
+++ modules/EnsEMBL/Web/Component/UserData/Summary.pm
@@ -1,13 +1,11 @@
 package EnsEMBL::Web::Component::UserData::Summary;
 
-### Module to create user data home page
+### Placeholder only - currently unused 
 
 use strict;
 use warnings;
 no warnings "uninitialized";
 use base qw(EnsEMBL::Web::Component::UserData);
-use EnsEMBL::Web::Form;
-use EnsEMBL::Web::RegObj;
 
 sub _init {
   my $self = shift;
@@ -15,23 +13,8 @@ sub _init {
   $self->ajaxable(  0 );
 }
 
-sub caption {
-  my $self = shift;
-  return undef;
-}
-
 sub content {
   my $self = shift;
-  my $html;
-  my $user = $ENSEMBL_WEB_REGISTRY->get_user;
-  my $sitename = $self->site_name;
-  if ($user && $user->id) {
-    $html .= "<p>Logged in $sitename as: <strong>".$user->name.'</strong></p>';
-  }
-  else {
-    $html .= "<p>Log into your $sitename account to save data for later use</p>";
-  }
-  return $html;
 }
 
 1;
