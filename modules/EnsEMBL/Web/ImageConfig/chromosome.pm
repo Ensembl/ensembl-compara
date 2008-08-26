2cc2bbc3e3e81ebc6905b5b81d7871f1daffc10b
James Smith <js5@sanger.ac.uk>
2008-08-26 10:57:15 +0000

    added configurations etc

--- modules/EnsEMBL/Web/ImageConfig/chromosome.pm
+++ modules/EnsEMBL/Web/ImageConfig/chromosome.pm
@@ -5,7 +5,22 @@ use base qw(EnsEMBL::Web::ImageConfig);
 
 sub init {
   my ($self) = @_;
-  $self->set_title( 'Chromosome' );
+
+  $self->set_parameters({
+    'title'         => 'Chromosome panel',
+    'show_buttons'  => 'no',  # do not show +/- buttons
+    'button_width'  => 8,     # width of red "+/-" buttons
+    'show_labels'   => 'yes', # show track names on left-hand side
+    'label_width'   => 113,   # width of labels on left-hand side
+    'margin'        => 5,     # margin
+    'spacing'       => 2,     # spacing
+
+## Finally some colours... background image colors;
+## and alternating colours for tracks...
+    'bgcolor'       => 'background1',
+    'bgcolour1'     => 'background2',
+    'bgcolour2'     => 'background3',
+  }):
 
   $self->create_menus(
     'decorations' => 'Chromosome',
@@ -15,12 +30,11 @@ sub init {
 #  $self->load_tracks();
 
 ## Now we have a number of tracks which we have to manually add...
-  $self->add_tracks( 'decorations' => [qw(ideogram assembly_exception)] );
+  $self->add_tracks( 'decorations', 
+    [ 'ideogram',           'Ideogram',            'ideogram', { } ],
+    [ 'assembly_exception', 'Assembly exceptions', 'assembly_exception', {} ],
+  );
+
   $self->configurable( 0 );
-  
-  $self->set_options({
-    'show_buttons'        => { 'value' => 0 },
-    'show_labels'         => { 'value' => 1 }
-  });
 }
 1;
