854cb18e68e97953af3e177ecbc0260121ed74ce
Anne Parker <ap5@sanger.ac.uk>
2008-09-01 10:48:06 +0000

    Added sanity check for release object, in case this release not yet listed in ensembl_website

--- modules/EnsEMBL/Web/Document/HTML/WhatsNew.pm
+++ modules/EnsEMBL/Web/Document/HTML/WhatsNew.pm
@@ -29,82 +29,86 @@ sub render {
   my $filtered = 0;
 
   my $release_id = $species_defs->ENSEMBL_VERSION;
-  my $release = EnsEMBL::Web::Data::Release->new($release_id);
-  my $release_date = $self->pretty_date($release->date);
-
-  my $news_url = '/info/website/news/index.html';
-
   my $html = qq(<h2 class="first">What's New in Release $release_id ($release_date)</h2>);
+  my $release = EnsEMBL::Web::Data::Release->new($release_id);
+  if ($release) {
+    my $release_date = $self->pretty_date($release->date);
+    my $news_url = '/info/website/news/index.html';
 
-  ## get news headlines
-  my $criteria = {'release_id' => $release_id};
-  if ($user && $user->id) {
-    $criteria->{'species'} = [];
-    ## check for user filters
-    my @filters = $user->newsfilters;
-    ## Look up species names for use in query
-    foreach my $f (@filters) {
-      if ($f->species && $f->species ne 'none') {
-        $filtered = 1;
-        $criteria->{'species'} = $f->species;
+    ## get news headlines
+    my $criteria = {'release_id' => $release_id};
+    if ($user && $user->id) {
+      $criteria->{'species'} = [];
+      ## check for user filters
+      my @filters = $user->newsfilters;
+      ## Look up species names for use in query
+      foreach my $f (@filters) {
+        if ($f->species && $f->species ne 'none') {
+          $filtered = 1;
+          $criteria->{'species'} = $f->species;
+        }
       }
     }
-  }
-  my $attr = {'limit' => 5, 'order_by' => ' n.priority DESC '};
+    my $attr = {'limit' => 5, 'order_by' => ' n.priority DESC '};
 
-  my @headlines = EnsEMBL::Web::Data::NewsItem->fetch_news_items($criteria, $attr);
+    my @headlines = EnsEMBL::Web::Data::NewsItem->fetch_news_items($criteria, $attr);
 
-  my %species_lookup; 
-  my @all_species = EnsEMBL::Web::Data::Species->find_all;
-  foreach my $sp (@all_species) {
-    $species_lookup{$sp->species_id} = $sp->name;
-  }
+    my %species_lookup; 
+    my @all_species = EnsEMBL::Web::Data::Species->find_all;
+    foreach my $sp (@all_species) {
+      $species_lookup{$sp->species_id} = $sp->name;
+    }
 
-  if (scalar(@headlines) > 0) {
+    if (scalar(@headlines) > 0) {
 
-    $html .= "<ul>\n";
+      $html .= "<ul>\n";
 
 ## format news headlines
-    foreach my $item (@headlines) {
+      foreach my $item (@headlines) {
 
-      ## sort out species names
-      my @species = $item->species; 
-      my (@sp_ids, $sp_id, $sp_name, $sp_count);
-      if (!scalar(@species)) {
-        $sp_name = 'all species';
-      }
-      elsif (scalar(@species) > 5) {
-        $sp_name = 'multiple species';
-      }
-      else {
-        my @names;
-        foreach my $sp (@species) {
-          if ($sp->common_name =~ /\./) {
-            push @names, '<i>'.$sp->common_name.'</i>';
-          }
-          else {
-            push @names, $sp->common_name;
-          } 
+        ## sort out species names
+        my @species = $item->species; 
+        my (@sp_ids, $sp_id, $sp_name, $sp_count);
+        if (!scalar(@species)) {
+          $sp_name = 'all species';
         }
-        $sp_name = join(', ', @names);
-      }
+        elsif (scalar(@species) > 5) {
+          $sp_name = 'multiple species';
+        }
+        else {
+          my @names;
+          foreach my $sp (@species) {
+            if ($sp->common_name =~ /\./) {
+              push @names, '<i>'.$sp->common_name.'</i>';
+            }
+            else {
+              push @names, $sp->common_name;
+            } 
+          }
+          $sp_name = join(', ', @names);
+        }
 ## generate HTML
-      $html .= sprintf(qq(<li><strong><a href="%s#%s" style="text-decoration:none">%s</a></strong> (%s)</li>\n),
+        $html .= sprintf(qq(<li><strong><a href="%s#%s" style="text-decoration:none">%s</a></strong> (%s)</li>\n),
               $news_url, $item->news_item_id, $item->title, $sp_name);
 
-    }
+      }
 
-    $html .= qq(</ul>
+      $html .= qq(</ul>
 <p><a href="$news_url">More news</a>...</p>\n);
-  }
-  else {
-    if ($filtered) {
-      $html .= qq(<p>No news could be found for your selected species/topics.</p>
-<p><a href="$news_url">Other news</a>...</p>\n);
     }
     else {
-      $html .= qq(<p>No news is currently available for release $release_id.</p>\n);
+      if ($filtered) {
+        $html .= qq(<p>No news could be found for your selected species/topics.</p>
+<p><a href="$news_url">Other news</a>...</p>\n);
+      }
+      else {
+        $html .= qq(<p>No news is currently available for release $release_id.</p>\n);
+      }
     }
+  }
+  else {
+    $html .= qq(<p>No information on this release</p>);
+    warn "NO RELEASE INFORMATION found in database ensembl_website!";
   }
 
   if ($ENSEMBL_WEB_REGISTRY->check_ajax) {
