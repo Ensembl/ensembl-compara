b6ec3caf03c0304c38fd8c1ea900e29e6517a4e0
Steve Trevanion <st3@sanger.ac.uk>
2008-09-08 11:31:27 +0000

    colours out of config, tidy up

--- modules/Bio/EnsEMBL/GlyphSet/TSE_legend.pm
+++ modules/Bio/EnsEMBL/GlyphSet/TSE_legend.pm
@@ -8,39 +8,51 @@ $Data::Dumper::Maxdepth = 2;
 
 sub _init {
     my ($self) = @_;
-
     my $BOX_WIDTH     = 20;
-    my $NO_OF_COLUMNS = 2;
-
+    my $NO_OF_COLUMNS = 3;
     my $Config        = $self->{'config'};
     my $im_width      = $Config->image_width();
-
     my $o_type = $Config->{'object_type'};
-
     my @colours;
-    my ($x,$y) = (0,0);
     my( $fontname, $fontsize ) = $self->get_font_details( 'legend' );
     my @res = $self->get_text_width( 0, 'X', '', 'font'=>$fontname, 'ptsize' => $fontsize );
     my $th = $res[3];
-    my $pix_per_bp = $self->{'config'}->transform()->{'scalex'};
-	
+    my $pix_per_bp = $self->{'config'}->transform()->{'scalex'};	
     my $FLAG = 0;
     my $start_x;
+    my ($x,$y) = (0,0.5);
     my $h = 8;
     my $G;
+
+    my $rect = $self->Rect({
+	'x'         => 0,
+	'y'         => $y,
+	'width'     => $im_width,
+	'height'    => 0,
+	'colour'    => 'grey50',
+	'absolutey' => 1,
+	'absolutex' => 1,
+	'absolutewidth'=>1,
+    });
+    $self->push($rect);
+
     #retrieve the features that were counted as they were drawn and add (two colum) legends for them
     my %features = $Config->{'TSE_legend'} ? %{$Config->{'TSE_legend'}} : ();
-     foreach my $f (sort { $features{$a}->{'priority'} <=> $features{$b}->{'priority'} } keys %features) {
-	$y = $y+0.5 if $x==0;
+    foreach my $f (sort { $features{$a}->{'priority'} <=> $features{$b}->{'priority'} } keys %features) {
 	my $colour = $features{$f}->{'colour'};
 	my $db_type = $f;
+
+	if($x==($NO_OF_COLUMNS)) {
+	    $x = 0;
+	    $y++;
+	}
+
 	#draw two exon hits and an intron for each feature type
 	$start_x = $im_width * $x/$NO_OF_COLUMNS;
-
 	$h = $features{$f}->{'height'};
 	my $G = $self->Rect({
 	    'x'             => $start_x,
-	    'y'             => $y * ( $th + 3 ) - 1,
+	    'y'             => $y*$th - 1,
 	    'width'         => $BOX_WIDTH,
 	    'height'        => $h,
 	    'bordercolour'  => $colour,
@@ -51,7 +63,7 @@ sub _init {
 	$self->push($G);
 	$G = $self->Line({
 	    'x'             => $start_x+$BOX_WIDTH,
-	    'y'             => $y * ( $th + 3 ) + 2,
+	    'y'             => $y*$th + 2,
 	    'h'             => 1,
 	    'width'         => $BOX_WIDTH,
 	    'colour'        => $colour,
@@ -62,7 +74,7 @@ sub _init {
 	$self->push($G);
 	$G = $self->Rect({
 	    'x'             => $start_x + 2*$BOX_WIDTH,
-	    'y'             => $y * ( $th + 3 ) - 1,
+	    'y'             => $y*$th - 1,
 	    'width'         => $BOX_WIDTH,
 	    'height'        => $h,
 	    'bordercolour'  => $colour,
@@ -73,7 +85,7 @@ sub _init {
 	$self->push($G);
 	$G = $self->Text({
 	    'x'             => $start_x + 3*$BOX_WIDTH + 4,
-	    'y'             => $y * $th,
+	    'y'             => $y*$th - 5,
 	    'height'        => $th,
 	    'valign'        => 'center',
 	    'halign'        => 'left',
@@ -87,59 +99,41 @@ sub _init {
 	});
 	$self->push($G);
 	$x++;
-
-	if($x==($NO_OF_COLUMNS)) {
-	    $x=0;
-	    $y++;
-	}
     }
 	
-    # Draw a separating line to distinguish the above dynamic legend from the following static legend
-    $y++;
-    my $rect = $self->Rect({
-	'x'         => 0,
-	'y'         => $y * ( $th + 3 ),
-	'width'     => $im_width,
-	'height'    => 0,
-	'colour'    => 'grey50',
-	'absolutey' => 1,
-	'absolutex' => 1,
-	'absolutewidth'=>1,
-    });
-    $self->push($rect);
-
+    $NO_OF_COLUMNS = 2;
     #start new line
     $y++;
-    $x = 0;
+    $x=0;
 
     #Draw a red I-bar (non-canonical intron)
     $start_x = $im_width * $x/$NO_OF_COLUMNS;
     my $colour = $self->my_colour('non_can_intron');
     my $G = $self->Line({
-	'x'         => $start_x,
-	'y'         => $y * ( $th + 3 ) + 2,
-	'width'     => $BOX_WIDTH,
-	'height'    => 0,
-	'colour'    => $colour,
-	'absolutey' => 1,
-	'absolutex' => 1,
-	'absolutewidth'=>1,
+	'x'             => $start_x,
+	'y'             => $y*$th + 2,
+	'width'         => $BOX_WIDTH,
+	'height'        => 0,
+	'colour'        => $colour,
+	'absolutey'     => 1,
+	'absolutex'     => 1,
+	'absolutewidth' => 1,
     });
     $self->push( $G );
     $G = $self->Line({
-	'x'         => $start_x,
-	'y'         => $y * ( $th + 3 ) - 1,
-	'width'     => 0,
-	'height'    => 6,
-	'colour'    => $colour,
-	'absolutey' => 1,
-	'absolutex' => 1,
-	'absolutewidth'=>1,
+	'x'             => $start_x,
+	'y'             => $y*$th - 1,
+	'width'         => 0,
+	'height'        => 6,
+	'colour'        => $colour,
+	'absolutey'     => 1,
+	'absolutex'     => 1,
+	'absolutewidth' => 1,
     });
     $self->push( $G );
     $G = $self->Line({
 	'x'             => $start_x + $BOX_WIDTH,
-	'y'             => $y * ( $th + 3 ) - 1,
+	'y'             => $y*$th - 1,
 	'width'         => 0,
 	'height'        => 6,
 	'colour'        => $colour,
@@ -150,14 +144,14 @@ sub _init {
     $self->push( $G );
     $G = $self->Text({
 	'x'             => $start_x + $BOX_WIDTH + 4,
-	'y'             => $y * $th + 2,
+	'y'             => $y*$th - 7,
 	'height'        => $th,
 	'valign'        => 'center',
 	'halign'        => 'left',
 	'ptsize'        => $fontsize,
 	'font'          => $fontname,
 	'colour'        => 'black',
-	'text'          => 'Non-canonical splice site',
+	'text'          => 'non-canonical splice site',
 	'absolutey'     => 0,
 	'absolutex'     => 1,
 	'absolutewidth' => 1
@@ -166,35 +160,33 @@ sub _init {
 
     if( $o_type ne 'vega' ) {
 	$x++;
-
 	#lines extending beyond the end of the hit
+	$colour = $self->my_colour('evi_long');
 	$start_x = $im_width * $x/$NO_OF_COLUMNS;
 	$G = $self->Line({
-	    'x'         => $start_x,
-	    'y'         =>  $y * ( $th + 3 ) - 2,
-	    'height'    => $h,
-	    'width'     => 0,
-	    'colour'    => 'red',
-	    'absolutey' => 1,
-	    'absolutex'     => 1,
-	'absolutewidth' => 1,});				
-	$self->push($G);
-	
-	$G = $self->Line({
-	    'x'         => $start_x,
-	    'y'         => $y * ( $th + 3 ) + 2,
-	    'h'         => 1,
-	    'width'     => 1.5*$BOX_WIDTH,
-	    'colour'    => 'red',
-	    'dotted'    => 1,
-	    'absolutey' => 1,
+	    'x'             => $start_x,
+	    'y'             => $y*$th - 2,
+	    'height'        => $h,
+	    'width'         => 0,
+	    'colour'        => $colour,
+	    'absolutey'     => 1,
 	    'absolutex'     => 1,
 	    'absolutewidth' => 1,});				
-	$self->push($G);
-	
+	$self->push($G);	
+	$G = $self->Line({
+	    'x'             => $start_x,
+	    'y'             => $y*$th + 2,
+	    'h'             => 1,
+	    'width'         => 1.5*$BOX_WIDTH,
+	    'colour'        => $colour,
+	    'dotted'        => 1,
+	    'absolutey'     => 1,
+	    'absolutex'     => 1,
+	    'absolutewidth' => 1,});				
+	$self->push($G);	
 	$G = $self->Rect({
 	    'x'             => $start_x + 1.5*$BOX_WIDTH,
-	    'y'             => $y * ( $th + 3 ) - 2,
+	    'y'             => $y*$th - 2,
 	    'width'         => $BOX_WIDTH,
 	    'height'        => $h,
 	    'bordercolour'  => 'black',
@@ -208,7 +200,7 @@ sub _init {
 
 	$G = $self->Rect({
 	    'x'             => $start_x,
-	    'y'             => $y * ( $th + 3 ) - 2,
+	    'y'             => $y*$th - 2,
 	    'width'         => $BOX_WIDTH,
 	    'height'        => $h,
 	    'bordercolour'  => 'black',
@@ -217,33 +209,30 @@ sub _init {
 	    'absolutewidth' => 1,
 	});
 	$self->push($G);
-	
 	$G = $self->Line({
-	    'x'         => $start_x + $BOX_WIDTH,
-	    'y'         => $y * ( $th + 3 ) + 2,
-	    'h'         => 1,
-	    'width'     => 1.5*$BOX_WIDTH,
-	    'colour'    => 'red',
-	    'dotted'    => 1,
-	    'absolutey' => 1,
+	    'x'             => $start_x + $BOX_WIDTH,
+	    'y'             => $y*$th + 2,
+	    'h'             => 1,
+	    'width'         => 1.5*$BOX_WIDTH,
+	    'colour'        => $colour,
+	    'dotted'        => 1,
+	    'absolutey'     => 1,
 	    'absolutex'     => 1,
 	    'absolutewidth' => 1,});				
 	$self->push($G);
-    
 	$G = $self->Line({
 	    'x'         => $start_x + 2.5*$BOX_WIDTH,
-	    'y'         =>  $y * ( $th + 3 ) - 2,
+	    'y'         => $y*$th - 2,
 	    'height'    => $h,
 	    'width'     => 0,
-	    'colour'    => 'red',
+	    'colour'    => $colour,
 	    'absolutey' => 1,
 	    'absolutex'     => 1,
 	    'absolutewidth' => 1,});				
 	$self->push($G);
-	
 	$G = $self->Text({
 	    'x'             => $start_x + 2.5*$BOX_WIDTH + 4,
-	    'y'             => $y * $th -2,
+	    'y'             => ($y-0.8)*$th - 4,
 	    'height'        => $th,
 	    'valign'        => 'center',
 	    'halign'        => 'left',
@@ -258,16 +247,17 @@ sub _init {
 	$self->push($G);
 	$y += 1.5;
     }
-    else { $y++; }
+    else { $y += 1.5; }
 
     #new line
-    $y += 1.5;
     $x=0;
 
     #draw legends for exon/CDS & hit mismatch
-    my %dets = (	'red'  => 'part of evidence missing from transcript structure' );
+    my $miss_col  = $self->my_colour('evi_missing');
+    my $extra_col = $self->my_colour('evi_extra');
+    my %dets = (	$miss_col  => 'part of evidence missing from transcript structure' );
     unless ($o_type eq 'vega') {
-	$dets{'blue'} = 'part of evidence duplicated in transcript structure';
+	$dets{$extra_col} = 'part of evidence duplicated in transcript structure';
     }
 
     foreach my $c (sort { $b cmp $a} keys %dets) {
@@ -277,7 +267,7 @@ sub _init {
 	$start_x = $im_width * $x/$NO_OF_COLUMNS;
 	my $G = $self->Rect({
 	    'x'             => $start_x,
-	    'y'             => $y * ( $th + 3 ) - 2,
+	    'y'             => $y*$th - 2,
 	    'width'         => $BOX_WIDTH,
 	    'height'        => $h,
 	    'bordercolour'  => 'black',
@@ -288,7 +278,7 @@ sub _init {
 	$self->push($G);
 	$G = $self->Line({
 	    'x'             => $start_x + $BOX_WIDTH + 1,
-	    'y'             => $y * ( $th + 3 ) + 2,
+	    'y'             => $y*$th + 2,
 	    'h'             => 1,
 	    'width'         => $BOX_WIDTH - 1,
 	    'colour'        => $c,
@@ -300,7 +290,7 @@ sub _init {
 	$self->push($G);
 	$G = $self->Rect({
 	    'x'             => $start_x + 2*$BOX_WIDTH,
-	    'y'             => $y * ( $th + 3 ) - 2,
+	    'y'             => $y*$th - 2,
 	    'width'         => $BOX_WIDTH,
 	    'height'        => $h,
 	    'bordercolour'  => 'black',
@@ -311,7 +301,7 @@ sub _init {
 	$self->push($G);
 	$G = $self->Text({
 	    'x'             => $start_x + 3*$BOX_WIDTH + 4,
-	    'y'             => $y * $th + 8,
+	    'y'             => ($y-0.8)*$th + 2,
 	    'height'        => $th,
 	    'valign'        => 'center',
 	    'halign'        => 'left',
@@ -324,7 +314,6 @@ sub _init {
 	    'absolutewidth' => 1,
 	});
 	$self->push($G);
-	
 	$x++;
     }
 
@@ -338,8 +327,8 @@ sub _init {
     }
     else {
 	%dets = (
-	    'blue' => 'evidence start / ends within exon / CDS',
-	    'red'  => 'evidence extends beyond exon / CDS',
+	    $self->my_colour('evi_short') => 'evidence start / ends within exon / CDS',
+	    $self->my_colour('evi_long') => 'evidence extends beyond exon / CDS',
 	);
     }
 
@@ -351,7 +340,7 @@ sub _init {
 	$colour = 'black';
 	$G = $self->Rect({
 	    'x'             => $start_x,
-	    'y'             => $y * ( $th + 3 ) - 1,
+	    'y'             => $y*$th - 1,
 	    'width'         => $BOX_WIDTH,
 	    'height'        => $h,
 	    'bordercolour'  => $colour,
@@ -359,23 +348,21 @@ sub _init {
 	    'absolutex'     => 1,
 	    'absolutewidth' => 1,
 	});
-	$self->push($G);
-	
+	$self->push($G);	
 	$G = $self->Line({
 	    'x'             => $start_x,
-	    'y'             => $y * ( $th + 3 ) - 1,
+	    'y'             => $y*$th - 1,
 	    'width'         => 0,
 	    'height'        => $h,
-	    'colour'         => $c,
+	    'colour'        => $c,
 	    'absolutey'     => 1,
 	    'absolutex'     => 1,
 	    'absolutewidth' => 1,
 	});
-	$self->push($G);
-	
+	$self->push($G);	
 	$G = $self->Line({
 	    'x'             => $start_x + 1,
-	    'y'             => $y * ( $th + 3 ) - 1,
+	    'y'             => $y*$th - 1,
 	    'width'         => 0,
 	    'height'        => $h,
 	    'colour'        => 'black',
@@ -387,7 +374,7 @@ sub _init {
 
 	$G = $self->Line({
 	    'x'             => $start_x + 2,
-	    'y'             => $y * ( $th + 3 ) - 1,
+	    'y'             => $y*$th - 1,
 	    'width'         => 0,
 	    'height'        => $h,
 	    'colour'        => $c,
@@ -400,7 +387,7 @@ sub _init {
 	
 	$G = $self->Rect({
 	    'x'             => $start_x,
-	    'y'             => $y * ( $th + 3 ) - 1,
+	    'y'             => $y*$th - 1,
 	    'width'         => $BOX_WIDTH,
 	    'height'        => $h,
 	    'bordercolour'  => $colour,
@@ -412,7 +399,7 @@ sub _init {
 	
 	$G = $self->Line({
 	    'x'             => $start_x+$BOX_WIDTH-1,
-	    'y'             => $y * ( $th + 3 ) - 1,
+	    'y'             => $y*$th - 1,
 	    'width'         => 0,
 	    'height'        => $h,
 	    'colour'        => 'black',
@@ -424,7 +411,7 @@ sub _init {
 
 	$G = $self->Line({
 	    'x'             => $start_x+$BOX_WIDTH-2,
-	    'y'             => $y * ( $th + 3 ) - 1,
+	    'y'             => $y*$th - 1,
 	    'width'         => 0,
 	    'height'        => $h,
 	    'colour'        => $c,
@@ -436,7 +423,7 @@ sub _init {
 	
 	$G = $self->Line({
 	    'x'             => $start_x+$BOX_WIDTH,
-	    'y'             => $y * ( $th + 3 ) - 1,
+	    'y'             => $y*$th - 1,
 	    'width'         => 0,
 	    'height'        => $h,
 	    'colour'        => $c,
@@ -448,7 +435,7 @@ sub _init {
 	
 	$G = $self->Text({
 	    'x'             => $start_x + $BOX_WIDTH + 4,
-	    'y'             => $y * $th + 5,
+	    'y'             => ($y-0.8)*$th,
 	    'height'        => $th,
 	    'valign'        => 'center',
 	    'halign'        => 'left',
@@ -464,102 +451,6 @@ sub _init {
 	$x++;
 	$y -= 0.8;
     }
-
-    if ($o_type ne 'vega') {
-	$x=0;
-	$y += 2.3;
-
-	#lines extending beyond the end of the hit
-	$start_x = $im_width * $x/$NO_OF_COLUMNS;
-	$G = new Sanger::Graphics::Glyph::Line({
-	    'x'         => $start_x,
-	    'y'         =>  $y * ( $th + 3 ) - 2,
-	    'height'    => $h,
-	    'width'     => 0,
-	    'colour'    => 'red',
-	    'absolutey' => 1,
-	    'absolutex'     => 1,
-	'absolutewidth' => 1,});				
-	$self->push($G);
-	
-	$G = new Sanger::Graphics::Glyph::Line({
-	    'x'         => $start_x,
-	    'y'         => $y * ( $th + 3 ) + 2,
-	    'h'         => 1,
-	    'width'     => 1.5*$BOX_WIDTH,
-	    'colour'    => 'red',
-	    'dotted'    => 1,
-	    'absolutey' => 1,
-	    'absolutex'     => 1,
-	    'absolutewidth' => 1,});				
-	$self->push($G);
-	
-	$G = new Sanger::Graphics::Glyph::Rect({
-	    'x'             => $start_x + 1.5*$BOX_WIDTH,
-	    'y'             => $y * ( $th + 3 ) - 2,
-	    'width'         => $BOX_WIDTH,
-	    'height'        => $h,
-	    'bordercolour'  => 'black',
-	    'absolutey'     => 1,
-	    'absolutex'     => 1,
-	    'absolutewidth' => 1,
-	});
-	$self->push($G);
-
-	$y += 0.8;
-
-	$G = new Sanger::Graphics::Glyph::Rect({
-	    'x'             => $start_x,
-	    'y'             => $y * ( $th + 3 ) - 2,
-	    'width'         => $BOX_WIDTH,
-	    'height'        => $h,
-	    'bordercolour'  => 'black',
-	    'absolutey'     => 1,
-	    'absolutex'     => 1,
-	    'absolutewidth' => 1,
-	});
-	$self->push($G);
-	
-	$G = new Sanger::Graphics::Glyph::Line({
-	    'x'         => $start_x + $BOX_WIDTH,
-	    'y'         => $y * ( $th + 3 ) + 2,
-	    'h'         => 1,
-	    'width'     => 1.5*$BOX_WIDTH,
-	    'colour'    => 'red',
-	    'dotted'    => 1,
-	    'absolutey' => 1,
-	    'absolutex'     => 1,
-	    'absolutewidth' => 1,});				
-	$self->push($G);
-    
-	$G = new Sanger::Graphics::Glyph::Line({
-	    'x'         => $start_x + 2.5*$BOX_WIDTH,
-	    'y'         =>  $y * ( $th + 3 ) - 2,
-	    'height'    => $h,
-	    'width'     => 0,
-	    'colour'    => 'red',
-	    'absolutey' => 1,
-	    'absolutex'     => 1,
-	    'absolutewidth' => 1,});				
-	$self->push($G);
-	
-	$G = new Sanger::Graphics::Glyph::Text({
-	    'x'             => $start_x + 2.5*$BOX_WIDTH + 4,
-	    'y'             => $y * $th + 10,
-	    'height'        => $th,
-	    'valign'        => 'center',
-	    'halign'        => 'left',
-	    'ptsize'        => $fontsize,
-	    'font'          => $fontname,
-	    'colour'        => 'black',
-	    'text'          => 'evidence extends beyond the end of the transcript',
-	    'absolutey'     => 1,
-	    'absolutex'     => 1,
-	    'absolutewidth' => 1,
-	});
-	$self->push($G);
-    }
-
 }
 
 1;
