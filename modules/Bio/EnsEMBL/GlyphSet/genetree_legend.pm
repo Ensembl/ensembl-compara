ea5a68c9ffa1949f24dec8ae4d1317cf6edb5981
James Smith <js5@sanger.ac.uk>
2008-09-19 13:03:16 +0000

    fixes to move genetree view to new code

--- modules/Bio/EnsEMBL/GlyphSet/genetree_legend.pm
+++ modules/Bio/EnsEMBL/GlyphSet/genetree_legend.pm
@@ -1,11 +1,9 @@
 package Bio::EnsEMBL::GlyphSet::genetree_legend;
 
 use strict;
-use Bio::EnsEMBL::GlyphSet;
-use Bio::EnsEMBL::Utils::Eprof qw(eprof_start eprof_end);
-our @ISA = qw(Bio::EnsEMBL::GlyphSet);
+use base qw(Bio::EnsEMBL::GlyphSet);
 
-sub _init {
+sub render_normal {
   my ($self) = @_;
 
   return unless ($self->strand() == -1);
@@ -14,14 +12,13 @@ sub _init {
   my $NO_OF_COLUMNS = 4;
 
   my $vc            = $self->{'container'};
-  my $Config        = $self->{'config'};
-  my $im_width      = $Config->image_width();
-  my $type          = $Config->get('genetree_legend', 'src');
+  my $im_width      = $self->image_width();
+  my $type          = $self->my_config('src');
  
   my( $fontname, $fontsize ) = $self->get_font_details( 'legend' );
   my @res = $self->get_text_width( 0, 'X', '', 'font'=>$fontname, 'ptsize' => $fontsize );
   my $th = $res[3];
-  my $pix_per_bp = $self->{'config'}->transform()->{'scalex'};
+  my $pix_per_bp = $self->scalex;
 
   my @branches = (
     ['x1 branch length', 'blue', undef],
@@ -38,7 +35,7 @@ sub _init {
   );
   my @boxes = (
     ['AA alignment match/mismatch', 'yellowgreen', 'yellowgreen'],
-    ['AA alignment gap', 'white', 'yellowgreen'],
+    ['AA alignment gap',            'white',       'yellowgreen'],
   );
 
   my ($legend, $colour, $style, $border, $label, $text);
@@ -56,9 +53,7 @@ sub _init {
         'absolutey' => 1,
         'absolutex' => 1,
         'absolutewidth'=>1
-
-        })
-      );
+  }));
 
 
   my ($x,$y) = (0, 0);
@@ -73,7 +68,7 @@ sub _init {
       'dotted'    => $style,
       })
     );
-    $label = _create_label($im_width, $x, $y, $NO_OF_COLUMNS, $BOX_WIDTH, $th, $fontsize, $fontname, $legend);
+    $label = $self->_create_label($im_width, $x, $y, $NO_OF_COLUMNS, $BOX_WIDTH, $th, $fontsize, $fontname, $legend);
     $self->push($label);
     $y++;
   }
@@ -97,7 +92,7 @@ sub _init {
 
         })
       );
-    $label = _create_label($im_width, $x, $y, $NO_OF_COLUMNS, $BOX_WIDTH + 20, $th, $fontsize, $fontname, $legend);
+    $label = $self->_create_label($im_width, $x, $y, $NO_OF_COLUMNS, $BOX_WIDTH + 20, $th, $fontsize, $fontname, $legend);
     $self->push($label);
     $y++;
   }
@@ -113,7 +108,7 @@ sub _init {
         'colour'   => $colour,
         })
       );
-    $label = _create_label($im_width, $x, $y, $NO_OF_COLUMNS, $BOX_WIDTH - 20, $th, $fontsize, $fontname, $legend);
+    $label = $self->_create_label($im_width, $x, $y, $NO_OF_COLUMNS, $BOX_WIDTH - 20, $th, $fontsize, $fontname, $legend);
     $self->push($label);
     $y++;
   }
@@ -145,7 +140,7 @@ sub _init {
         'colour'    => $border,
         })
       );
-    $label = _create_label($im_width, $x, $y, $NO_OF_COLUMNS, $BOX_WIDTH - 10, $th, $fontsize, $fontname, $legend);
+    $label = $self->_create_label($im_width, $x, $y, $NO_OF_COLUMNS, $BOX_WIDTH - 10, $th, $fontsize, $fontname, $legend);
     $self->push($label);
     $y++;
   }
@@ -153,7 +148,7 @@ sub _init {
 }
 
 sub _create_label {
-  my ($im_width, $x, $y, $NO_OF_COLUMNS, $BOX_WIDTH, $th, $fontsize, $fontname, $legend) = @_;
+  my ($self,$im_width, $x, $y, $NO_OF_COLUMNS, $BOX_WIDTH, $th, $fontsize, $fontname, $legend) = @_;
   return $self->Text({
       'x'         => $im_width * $x/$NO_OF_COLUMNS + $BOX_WIDTH + 5,
       'y'         => $y * ( $th + 3 ) + $th,
