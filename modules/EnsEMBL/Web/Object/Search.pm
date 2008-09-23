e1c17ecd38e5a4f7d2ee7cefa15fcccc14dc4463
Steve Trevanion <st3@sanger.ac.uk>
2008-09-23 16:16:16 +0000

    starting to get search working

--- modules/EnsEMBL/Web/Object/Search.pm
+++ modules/EnsEMBL/Web/Object/Search.pm
@@ -4,7 +4,23 @@ use strict;
 use warnings;
 no warnings "uninitialized";
 
-use EnsEMBL::Web::Object;
-our @ISA = qw(EnsEMBL::Web::Object);
+use base qw(EnsEMBL::Web::Object);
+
+sub short_caption {
+  my $self = shift;
+  return 'Search Ensembl';
+}
+
+sub caption {
+    my $self = shift;
+    my $caption = 'Ensembl text search';
+    return $caption;
+}
+
+sub counts {
+    my $self = shift;
+    return;
+}
+
 
 1;
