fb963ba2e2aeb3dcf914727b6579769b61d0c852
Beth Yates <bp1@sanger.ac.uk>
2008-09-23 15:53:07 +0000

    changes to some glyphset names

--- modules/EnsEMBL/Web/ImageConfig/genesnpview_transcript.pm
+++ modules/EnsEMBL/Web/ImageConfig/genesnpview_transcript.pm
@@ -9,40 +9,41 @@ sub init {
   my ($self) = @_;
 
   $self->set_parameters({
-    'title'         => 'Transcript slice',
-    'show_buttons'  => 'yes',   # show +/- buttons
-    'button_width'  => 8,       # width of red "+/-" buttons
-    'show_labels'   => 'yes',   # show track names on left-hand side
-    'label_width'   => 100,     # width of labels on left-hand side
-    'margin'        => 5,       # margin
-    'spacing'       => 2,       # spacing
+    'title'            => 'Transcript slice',
+    'show_buttons'     => 'yes',   # show +/- buttons
+    'button_width'     => 8,       # width of red "+/-" buttons
+    'show_labels'      => 'yes',   # show track names on left-hand side
+    'label_width'      => 100,     # width of labels on left-hand side
+    'margin'           => 5,       # margin
+    'spacing'          => 2,       # spacing
     'opt_halfheight'   => 0,    # glyphs are half-height [ probably removed when this becomes a track config ]
     'opt_empty_tracks' => 0,    # include empty tracks..
   });
   $self->create_menus(
     'sequence'        => 'Sequence',
     'gsv_transcript'  => 'Transcripts',
-    'gsv_peptides'    => 'Protein domains',
+    'gsv_domain'      => 'Protein domains',
     'gsv_variation'   => 'Variations',
     'other'           => 'Other'
   );
 
 
-  $self->add_tracks( 'sequence',
-    [ 'contig',    'Contigs',              'stranded_contig', { 'on' => 'on',  'strand' => 'r'  } ],
-  );
+#  $self->add_tracks( 'sequence',
+#    [ 'contig',    'Contigs',              'stranded_contig', { 'on' => 'on',  'strand' => 'r'  } ],
+#  );
   $self->load_tracks();
 
   $self->add_tracks( 'gsv_variation',
-    [ 'snps',             '',     'gsv_snps',          { 'on' => 'on',  'strand' => 'r', 'menu' => 'no'         } ]
+    [ 'variation',             '',     'gsv_variations',          { 'on' => 'on',  'strand' => 'r', 'menu' => 'no'         } ],
+    [ 'domain',               '',     'gsv_domain',             { 'on' => 'on',  'strand' => 'r', 'menu' => 'no'         } ],
   );
   $self->add_tracks( 'other',
     [ 'geneexon_bgtrack', '',     'geneexon_bgtrack',  { 'on' => 'on',  'strand' => 'b', 'menu' => 'no'         } ],
     [ 'draggable',        '',     'draggable',         { 'on' => 'on',  'strand' => 'b', 'menu' => 'no'         } ],
     [ 'snp_join',         '',     'snp_join',          { 'on' => 'on',  'strand' => 'b', 'menu' => 'no'         } ],
     [ 'spacer',           '',     'spacer',            { 'on' => 'on',  'strand' => 'r', 'menu' => 'no'         } ],
-    [ 'ruler',            '',     'ruler',             { 'on' => 'on',  'strand' => 'f', 'name' => 'Ruler'      } ],
-    [ 'scalebar',         '',     'scalebar',          { 'on' => 'on',  'strand' => 'f', 'name' => 'Scale bar'  } ],
+ #   [ 'ruler',            '',     'ruler',             { 'on' => 'on',  'strand' => 'f', 'name' => 'Ruler'      } ],
+ #   [ 'scalebar',         '',     'scalebar',          { 'on' => 'on',  'strand' => 'f', 'name' => 'Scale bar'  } ],
   );
   $self->modify_configs(
     [qw(gsv_transcript variation)],
