0282d3740fbd4338c679d3551b16073a2e672531
Steve Trevanion <st3@sanger.ac.uk>
2008-09-26 13:32:26 +0000

    tidy up, identified what still needs doing

--- modules/EnsEMBL/Web/Configuration/Search.pm
+++ modules/EnsEMBL/Web/Configuration/Search.pm
@@ -3,21 +3,43 @@ package EnsEMBL::Web::Configuration::Search;
 use strict;
 use base qw( EnsEMBL::Web::Configuration );
 use ExaLead::Renderer::HTML;
-use Data::Dumper;
 
 sub global_context { return $_[0]->_global_context; }
 sub ajax_content   { return $_[0]->_ajax_content;   }
 sub local_context  { return $_[0]->_local_context;  }
 sub local_tools    { return $_[0]->_local_tools;  }
-#sub content_panel  { return $_[0]->_content_panel;  }
 sub context_panel  { return $_[0]->_context_panel;  }
 
+#TO DO
+# - check active flags work on child1
+# - filters on child1 know nothing about filters on child2 which makes navigation not intuitive
+#ie restrict on gene then on Felis cattus; now if gene filter is removed then the Felis catus filter is also lost
+# - labels are often wider than the panel -> shoren species name when shorter than eg 6 characters ?
+ 
 sub populate_tree {
     my $self = shift;
-    $Data::Dumper::Maxdepth = 2;
-    my $exa_obj = $self->object->Obj;
-    my %seen_results;
     my $menu;
+    $self->set_title( '- text search' );
+    my $exa_obj = $self->object->Obj;
+
+    #are there any filters in place ?
+    my $filtered_already = 0;
+    foreach my $group ( $exa_obj->groups ) {
+	foreach my $child ($group->children) {
+	    if ($child->link( 'reset' )) {
+		$filtered_already = 1;
+	    }
+	}
+    }
+
+    #prevent selection of a node when there is no filtering by setting a hidden node to be active
+    unless ($filtered_already) {	    
+	$menu = $self->create_node('top','Show all',
+				   [],
+				   {'no_menu_entry' => 1, 'active'=> 1} );
+    }
+
+
     foreach my $group ( $exa_obj->groups ) {
 	my $name = $group->name;
 	$name =~ s/answergroup\.//;
@@ -31,36 +53,42 @@ sub populate_tree {
 	}
 	else { $c = scalar($group->children); }
 
-	#prevent initial selection of a node by setting a hidden node to be active
-	$menu = $self->create_node("","",[],{'no_menu_entry' => 1, 'active'=> 1} );
 
-	my $url;
-	if ($group->link( 'refine' )) {
-	    $url = $group->link( 'refine' )->URL;
-	}
-	$menu = $self->create_node( "$name", "$name ($c)",
-				    [qw(summary EnsEMBL::Web::Component::Search::Summary)],
-				    {'availability' => 1, 'url'=> $url} );
-	foreach my $category1 ( sort {$a->name cmp $b->name} $group->children ) {
-	    my $name1 = $category1->name;
-	    my $c1    = $category1->count;
+	#create top level node
+	$menu = $self->create_node( "$name", "$name ($c)",[],{});
+	
+	foreach my $child1 ( sort {$a->name cmp $b->name} $group->children ) {
+	    my $name1 = $child1->name;
+	    my $c1    = $child1->count;
+	    my $disp_name = "$name1 ($c1)";
 	    my $url1;
-	    if ($category1->link( 'refine' )) {
-		$url1 = $category1->link( 'refine' )->URL;
+	    my $active = 0;
+	    if ($child1->link( 'refine' )) {
+		$url1 = $child1->link( 'refine' )->URL;
 	    }
-	    my $menu1 = $self->create_node ( "$name:$name1", "$name1 ($c1)",
+	    if ( $child1->link( 'reset' )) {
+		$url1 = $child1->link( 'reset' )->URL;
+		$disp_name = "$name1 [click to reset]";
+		$active = 1;
+	    }
+	    my $menu1 = $self->create_node ( "$name:$name1", "$disp_name",
 					     [qw(summary EnsEMBL::Web::Component::Search::Summary)],
-					     {'availability' => 1, 'url'=> $url1} );
+					     {'availability' => 1, 'url'=> $url1, 'active' => $active} );
 	    $menu->append($menu1);
-	    foreach my $category2 ( sort {$a->name cmp $b->name} $category1->children ) {
-		my $name2 = $category2->name;
-		my $c2    = $category2->count;
-		
+
+	    foreach my $child2 ( sort {$a->name cmp $b->name} $child1->children ) {
+		my $name2 = $child2->name;
+		my $c2    = $child2->count;
+		my $disp_name = "$name2 ($c2)";
 		my $url2;
-		if ($category2->link( 'refine' )) {
-		    $url2 = $category2->link( 'refine' )->URL;
+		if ($child2->link( 'refine' )) {
+		    $url2 = $child2->link( 'refine' )->URL;
 		}
-		my $menu2 = $self->create_node( "$name:$name1:$name2", "$name2 ($c2)",
+		if ( $child2->link( 'reset' )) {
+		    $url2 = $child2->link( 'reset' )->URL;
+		    $disp_name = "$name2 [click to reset]"
+		}
+		my $menu2 = $self->create_node( "$name:$name1:$name2", "$disp_name",
 						[qw(summary EnsEMBL::Web::Component::Search::Summary)],
 						{'availability' => 1, 'url'=> $url2 } );
 		$menu1->append($menu2);
