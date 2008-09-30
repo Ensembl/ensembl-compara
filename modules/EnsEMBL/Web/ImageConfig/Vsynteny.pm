ace57fc23ac046dcd80c8d7bcf0f84ec75ffa47a
Anne Parker <ap5@sanger.ac.uk>
2008-09-30 11:11:07 +0000

    Updated to new syntax

--- modules/EnsEMBL/Web/ImageConfig/Vsynteny.pm
+++ modules/EnsEMBL/Web/ImageConfig/Vsynteny.pm
@@ -1,36 +1,37 @@
 package EnsEMBL::Web::ImageConfig::Vsynteny;
 use strict;
 use EnsEMBL::Web::ImageConfig;
-use vars qw(@ISA);
-@ISA = qw(EnsEMBL::Web::ImageConfig);
+use base qw(EnsEMBL::Web::ImageConfig);
 
 sub init {
   my ($self) = @_;
-  $self->{'_label'}             = 'above',
-  $self->{'_band_labels'}       = 'off',
-  $self->{'_userdatatype_ID'}   = 22;
-  $self->{'_image_height'}      = 500,
-  $self->{'_top_margin'}        = 20,
-  $self->{'general'}->{'Vsynteny'} = {
-    '_artefacts'   => [qw(Vsynteny)],
-    '_options'     => [],
-    '_settings'    => {
-      'opt_zclick' => 1,
-      'bgcolor'    => 'background1',
-      'width'      => 550 # really height <g>
-    },
-    'Vsynteny' => {
-      '_image_height'    => 500,
-      '_top_margin'      => 20,
-      'on'               => 'on',
-      'pos'              => '1',
-      '_main_width'      => 30,
-      '_secondary_width' => 12,
-      '_padding'         => 4,
-      '_spacing'         => 20,
-      '_inner_padding'   => 140,
-      '_outer_padding'   => 20,
-    },
-  };
+
+$self->set_parameters({
+    'title'           => 'Synteny panel',
+    'label'           => 'above',     # margin
+    'band_labels'     => 'off',
+    'image_height'    => 500,
+    'image_width'     => 550,
+    'top_margin'      => 20,
+    'band_links'      => 'no',
+    'main_width'      => 30,
+    'secondary_width' => 12,
+    'padding'         => 4,
+    'spacing'         => 20,
+    'inner_padding'   => 140,
+    'outer_padding'   => 20,
+  });
+
+  $self->create_menus( 'features' => 'Features' );
+
+  $self->add_tracks( 'features',
+    [ 'Vsynteny', 'Videogram', 'Vsynteny', {
+      'display'   => 'normal',
+      'renderers' => [qw(normal normal)],
+      'colourset' => 'ideogram'
+    } ],
+  );
+
 }
+
 1;
