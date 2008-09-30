a33ab4eb27340ce5305d5c8fb6c8299f458d784c
Anne Parker <ap5@sanger.ac.uk>
2008-09-30 13:21:02 +0000

    Rearranged table columns now that we have more space to play with!

--- modules/EnsEMBL/Web/Component/Location/SyntenyMatches.pm
+++ modules/EnsEMBL/Web/Component/Location/SyntenyMatches.pm
@@ -34,38 +34,55 @@ sub content {
   if (scalar(@$data)) {
     my $table = EnsEMBL::Web::Document::SpreadSheet->new(); 
 
+    ## TODO : add sixth column 'alignment' once multicontigview is ported
+
+    $table->add_spanning_headers(
+      {'title' => "<i>$sp_tidy</i> Genes", 'colspan' => 2},
+      {'title' => ''}, ## empty header for arrows 
+      {'title' => "<i>$other_tidy</i> Homologues", 'colspan' => 2},
+    );
+
     $table->add_columns(
-      {'key' => 'genes', 'title' => "<i>$sp_tidy</i> Genes", 'width' => '40%', 'align' => 'left' },
-      {'key' => 'arrow', 'title' => "&nbsp;", 'width' => '20%', 'align' => 'center' },
-      {'key' => 'homologues', 'title' => "<i>$other_tidy</i> Homologues", 'width' => '40%', 'align' => 'left' },
+      {'key' => 'gene_ids', 'title' => 'ID', 'width' => '20%', 'align' => 'left' },
+      {'key' => 'gene_loc', 'title' => 'Location', 'width' => '20%', 'align' => 'left' },
+      {'key' => 'arrow', 'title' => "&nbsp;", 'width' => '10%', 'align' => 'center' },
+      {'key' => 'homo_ids', 'title' => 'ID', 'width' => '20%', 'align' => 'left' },
+      {'key' => 'homo_loc', 'title' => 'Location', 'width' => '20%', 'align' => 'left' },
       );
-    my ($sp_links, $arrow, $other_links, $data_row);
+    my ($sp_ids, $sp_loc, $arrow, $other_ids, $other_loc, $data_row);
     my $old_id = '';
     foreach my $row ( @$data ) {
       my $sp_stable_id        = $row->{'sp_stable_id'};
       my $sp_synonym          = $row->{'sp_synonym'};
+      my $sp_chr              = $row->{'sp_chr'};
+      my $sp_start            = $row->{'sp_start'};
+      my $sp_end              = $row->{'sp_end'};
       my $sp_length           = $row->{'sp_length'};
       my $other_stable_id     = $row->{'other_stable_id'};
       my $other_synonym       = $row->{'other_synonym'};
       my $other_length        = $row->{'other_length'};
       my $other_chr           = $row->{'other_chr'};
+      my $other_start         = $row->{'other_start'};
+      my $other_end           = $row->{'other_end'};
       my $homologue_no        = $row->{'homologue_no'};
 
       $arrow = $homologue_no ? '-&gt;' : '';
       my $sp_links = '';
       if( $old_id ne $sp_stable_id ) { 
-        $sp_links = qq(<a href="/$species/Gene/Summary?g=$sp_stable_id"><strong>$sp_synonym</strong></a> \($sp_length\)<br />[<a href="/$species/Location/View?g=$sp_stable_id">View Region</a>]);
+        $sp_ids = qq#<a href="/$species/Gene/Summary?g=$sp_stable_id"><strong>$sp_synonym</strong></a> ($sp_stable_id)#;
+        $sp_loc = qq#<a href="/$species/Location/View?g=$sp_stable_id">$sp_chr:$sp_start-$sp_end</a>#;
         $old_id = $sp_stable_id;
       }
       if( $other_stable_id ) {
-        $other_links = qq(<a href="/$other/Gene/Summary?g=$other_stable_id"><strong>$other_synonym</strong></a><br />);
-        $other_links .= "($other_chr: $other_length)<br />";
-        $other_links .= qq([<a href="/$other/Location/View?g=$other_stable_id" title="Chr $other_chr: $other_length">View region</a>] [<a href="/$species/multicontigview?gene=$sp_stable_id;s1=$other;g1=$other_stable_id">View alignment</a>]);
+        $other_ids = qq#<a href="/$other/Gene/Summary?g=$other_stable_id"><strong>$other_synonym</strong></a> ($other_stable_id)#;
+        $other_loc = qq#<a href="/$other/Location/View?g=$other_stable_id">$other_chr:$other_start-$other_end</a>#;
       } 
       else {
-        $other_links = 'No homologues';
+        $other_ids = 'No homologues';
+        $other_loc = '';
       }
-      $data_row = { 'genes'  => $sp_links, 'arrow' => $arrow, 'homologues' => $other_links };
+      $data_row = { 'gene_ids'  => $sp_ids, 'gene_loc' => $sp_loc, 'arrow' => $arrow, 
+                    'homo_ids' => $other_ids, 'homo_loc' => $other_loc };
       $table->add_row( $data_row );
     }
     $html .= $table->render;
