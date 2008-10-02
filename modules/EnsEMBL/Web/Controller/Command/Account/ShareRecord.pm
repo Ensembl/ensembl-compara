4f62dd21792cc466813235fc204216fe08169a0b
Anne Parker <ap5@sanger.ac.uk>
2008-10-02 10:23:59 +0000

    Temporary fix to record filtering - but needs consultation with eb4

--- modules/EnsEMBL/Web/Controller/Command/Account/ShareRecord.pm
+++ modules/EnsEMBL/Web/Controller/Command/Account/ShareRecord.pm
@@ -16,12 +16,18 @@ sub BUILD {
   my ($self, $ident, $args) = @_;
   $self->add_filter('EnsEMBL::Web::Controller::Command::Filter::LoggedIn');
   my $cgi = $self->action->cgi;
-
   my $user = $EnsEMBL::Web::RegObj::ENSEMBL_WEB_REGISTRY->get_user;
-  my ($records_accessor) = grep { $_ eq $cgi->param('type') } keys %{ $user->relations };
+
+  ## TODO: this replicates some of the filter Owner functionality - move into filter
+  my $type = lc($cgi->param('type')).'s';
+  my ($records_accessor) = grep { $_ eq $type } keys %{ $user->relations };
   ## TODO: this should use abstraction limiting facility rather then grep
   my ($user_record)      = grep { $_->id == $cgi->param('id') } $user->$records_accessor;
-  $self->add_filter('EnsEMBL::Web::Controller::Command::Filter::Owner', {'user_id' => $user_record->user_id});
+  my $owner_id = 0;
+  if ($user_record) {
+    $owner_id = $user_record->user_id;
+  }
+  $self->add_filter('EnsEMBL::Web::Controller::Command::Filter::Owner', {'user_id' => $owner_id});
 }
 
 sub process {
