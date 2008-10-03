d45a0e90d245155bd612e0b14376594cce26fad0
Beth Yates <bp1@sanger.ac.uk>
2008-10-03 16:06:18 +0000

    fixed for gene splice/snp view

--- modules/Bio/EnsEMBL/GlyphSet/spacer.pm
+++ modules/Bio/EnsEMBL/GlyphSet/spacer.pm
@@ -4,10 +4,10 @@ use base qw(Bio::EnsEMBL::GlyphSet);
 
 sub _init {
   my ($self) = @_;
-  my $Config      = $self->{'config'};
-  my $height  = $Config->my_config('height') || 20; 
-
-  $self->push( $self->Space({
+  my $Config      = $self->{'config'};  
+  my $height  = $self->my_config('height') || 20;
+  
+  $self->push( $self->Rect({
     'x'         => 1,
     'y'         => 0,
     'width'     => 1,
