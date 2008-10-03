7d106a0fc3c762cab4a5c7439183288b08bfd8fe
Beth Yates <bp1@sanger.ac.uk>
2008-10-03 16:10:42 +0000

    fixed some problems with the new config

--- modules/EnsEMBL/Web/ImageConfig/genesnpview_transcript.pm
+++ modules/EnsEMBL/Web/ImageConfig/genesnpview_transcript.pm
@@ -6,7 +6,7 @@ use vars qw(@ISA);
 
 
 sub init {
-  my ($self) = @_;
+  my ($self) = @_; 
 
   $self->set_parameters({
     'title'            => 'Transcript slice',
@@ -20,35 +20,39 @@ sub init {
     'opt_empty_tracks' => 0,    # include empty tracks..
   });
   $self->create_menus(
-    'sequence'        => 'Sequence',
-    'gsv_transcript'  => 'Transcripts',
-    'gsv_domain'      => 'Protein domains',
-    'gsv_variations'  => 'Variations',
-    'other'           => 'Other'
+#    'sequence'        => 'Sequence',
+#    'gsv_transcript'  => 'Transcripts',
+#    'gsv_domain'      => 'Protein domains',
+#    'gsv_variations'  => 'Variations',
+    'other'           => 'Decorations'
   );
 
 
+   $self->add_tracks( 'other',
+    [ 'gsv_transcript',   '',     'gsv_transcript',   { 'display' => 'on', 'colours' => $self->species_defs->colour('gene'), 'src' => 'all',  'strand' => 'b', 'menu' => 'no'  } ],
+    [ 'gsv_domain',   '',     'gsv_domain',   { 'display' => 'on', 'colours' => $self->species_defs->colour('gene'), 'src' => 'all',  'strand' => 'b', 'menu' => 'no'  } ],
+   );
 #  $self->add_tracks( 'sequence',
 #    [ 'contig',    'Contigs',              'stranded_contig', { 'display' => 'normal',  'strand' => 'r'  } ],
 #  );
   $self->load_tracks();
 
-  $self->add_tracks( 'gsv_variations',
-    [ 'variation',             '',     'gsv_variations',          { 'display' => 'normal',  'strand' => 'r', 'menu' => 'no'         } ],
-    [ 'domain',               '',     'gsv_domain',             { 'display' => 'normal',  'strand' => 'r', 'menu' => 'no'         } ],
-  );
+#  $self->add_tracks( 'gsv_variations',
+#    [ 'variation',             '',     'gsv_variations',          { 'display' => 'normal',  'strand' => 'r', 'menu' => 'no'         } ],
+#    [ 'domain',               '',     'gsv_domain',             { 'display' => 'normal',  'strand' => 'r', 'menu' => 'no'         } ],
+#  );
   $self->add_tracks( 'other',
-    [ 'geneexon_bgtrack', '',     'geneexon_bgtrack',  { 'display' => 'normal',  'strand' => 'b', 'menu' => 'no'         } ],
-    [ 'draggable',        '',     'draggable',         { 'display' => 'normal',  'strand' => 'b', 'menu' => 'no'         } ],
-    [ 'snp_join',         '',     'snp_join',          { 'display' => 'normal',  'strand' => 'b', 'menu' => 'no'         } ],
+ #   [ 'geneexon_bgtrack', '',     'geneexon_bgtrack',  { 'display' => 'normal',  'strand' => 'b', 'menu' => 'no'         } ],
+ #   [ 'draggable',        '',     'draggable',         { 'display' => 'normal',  'strand' => 'b', 'menu' => 'no'         } ],
+ #   [ 'snp_join',         '',     'snp_join',          { 'display' => 'normal',  'strand' => 'b', 'menu' => 'no'         } ],
     [ 'spacer',           '',     'spacer',            { 'display' => 'normal',  'strand' => 'r', 'menu' => 'no'         } ],
  #   [ 'ruler',            '',     'ruler',             { 'display' => 'normal',  'strand' => 'f', 'name' => 'Ruler'      } ],
  #   [ 'scalebar',         '',     'scalebar',          { 'display' => 'normal',  'strand' => 'f', 'name' => 'Scale bar'  } ],
   );
-  $self->modify_configs(
-    [qw(gsv_transcript variation)],
-    {'on'=>'off'}
-  );
+#  $self->modify_configs(
+#    [qw(gsv_transcript) ],
+#    {'display'=>'on'}
+#  );
 }
 1;
 
