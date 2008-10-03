4a4a767b4c76fc74142884ae48dc3cb11667a021
Beth Yates <bp1@sanger.ac.uk>
2008-10-03 16:10:05 +0000

    fixed some issues with the config for new e51 code

--- modules/EnsEMBL/Web/ImageConfig/genesnpview_gene.pm
+++ modules/EnsEMBL/Web/ImageConfig/genesnpview_gene.pm
@@ -20,25 +20,23 @@ sub init {
   });
   $self->create_menus(
     'sequence'        => 'Sequence',
-    'gsv_transcript'  => 'Transcripts',
-    'gsv_domain'    => 'Protein domains',
-    'gsv_variations'   => 'Variations',
+    'transcript'     => 'Genes',
     'other'           => 'Other'
   );
   $self->load_tracks();
 
-  $self->add_tracks( 'gsv_variation',
-    [ 'variation',             '',     'gsv_variations',          { 'display' => 'normal',  'strand' => 'r', 'menu' => 'no'         } ]
-  );
   $self->add_tracks( 'other',
-    [ 'geneexon_bgtrack', '',     'geneexon_bgtrack',  { 'display' => 'normal',  'strand' => 'b', 'menu' => 'no'         } ],
+    [ 'geneexon_bgtrack', '',     'geneexon_bgtrack',  { 'display' => 'normal', 'src' => 'all', 'colour' => 'bisque', 'tag' => 1, 'strand' => 'b', 'menu' => 'no'         } ],
     [ 'draggable',        '',     'draggable',         { 'display' => 'normal',  'strand' => 'b', 'menu' => 'no'         } ],
-    [ 'snp_join',         '',     'snp_join',          { 'display' => 'normal',  'strand' => 'b', 'menu' => 'no'         } ],
-    [ 'spacer',           '',     'spacer',            { 'display' => 'normal',  'strand' => 'r', 'menu' => 'no'         } ],
+ #   [ 'snp_join',         '',     'snp_join',          { 'display' => 'normal',  'strand' => 'b', 'menu' => 'no'         } ],
+    [ 'spacer',           '',     'spacer',            { 'display' => 'normal', 'height' => 50,   'strand' => 'r', 'menu' => 'no'         } ],
   );
+
+
+
   $self->modify_configs(
-    [qw(gsv_transcript variation)],
-    {'on'=>'off'}
+    [qw(transcript )],
+    {'display'=>'off'}
   );
 }
 
