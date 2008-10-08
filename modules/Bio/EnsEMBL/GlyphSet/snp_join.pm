1a288ac366e199a635d0f02094b11fae1bf8a04a
Beth Yates <bp1@sanger.ac.uk>
2008-10-08 21:10:22 +0000

    fixed bug with snp_join on genes

--- modules/Bio/EnsEMBL/GlyphSet/snp_join.pm
+++ modules/Bio/EnsEMBL/GlyphSet/snp_join.pm
@@ -17,14 +17,14 @@ sub _init {
   my $strand        = $self->strand();
   return if ( $strand_flag eq 'f' && $strand != 1 ) || ( $strand_flag eq 'r'  && $strand == 1 );
 
-  my @snps = @ { $self->get_snps($Config)  || []}; 
+  my @snps = @ { $self->get_snps($Config)  || []};  
   my $tag = $self->my_config('tag'); 
   my $tag2 = $tag + ($strand == -1 ? 1 : 0); 
   my $T = $strand == 1 ? 1 : 0;
-  my $C=0;
+  my $C=0; 
   my $container     = exists $self->{'container'}{'ref'} ? $self->{'container'}{'ref'} : $self->{'container'};
-  my $start = $container->start();
-  my $length  = $container->length;
+  my $start = $container->start();  
+  my $length  = $container->length;  
   my $colours = $self->my_config('colours'); 
 
   foreach my $snp_ref ( @snps ) {  
@@ -48,43 +48,44 @@ sub _init {
 }
 
 sub get_snps {
-  my ($self, $Config) = @_;
+  my ($self, $Config) = @_; 
   return $Config->{'snps'} if $Config->{'fakeslice'} ;
 
-  my @snps;
-  if ( $Config->{'filtered_fake_snps'} ){
+  my @snps; 
+  if ( $Config->{'filtered_fake_snps'} ){ 
     @snps  = @{ $Config->{'filtered_fake_snps'} };
   }
-  else {
+  else { 
     my %ct = %Bio::EnsEMBL::Variation::VariationFeature::CONSEQUENCE_TYPES;
     @snps = map  { [ $_->[1]->start, $_->[1]->end, $_->[1]  ] } 
       sort { $a->[0] <=> $b->[0] }
-	map { [ - $ct{$_->display_consequence} * 1e9 + $_->start, $_ ] }
+	  map { [ - $ct{$_->display_consequence} * 1e9 + $_->start, $_ ] }
 	  grep { $_->map_weight < 4 }
 	    @{$self->{'container'}->get_all_VariationFeatures()};
   }
 
   my %exons = ();
-  my $target_gene   = $Config->{'geneid'}; 
+  my $target_gene   = $Config->{'geneid'};  
   my $context = $self->my_config('context')||100; 
-  if( $context && @snps ) {
-    my $features = $self->{'container'}->get_all_Genes(lc($self->species_defs->AUTHORITY)); 
-    foreach my $gene ( @$features ) { 
+  if( $context && @snps ) {  
+    #my $features = $self->{'container'}->get_all_Genes(lc($self->species_defs->AUTHORITY)); 
+    my $features = $self->{'container'}->get_all_Genes();
+    foreach my $gene ( @$features ) {  
       next if $target_gene && ($gene->stable_id() ne $target_gene); 
-      foreach my $transcript (@{$gene->get_all_Transcripts()}) {
-	foreach my $exon (@{$transcript->get_all_Exons()}) {
-	  $exons{ "@{[$exon->start]}:@{[$exon->end]}" }++;
-	}
+      foreach my $transcript (@{$gene->get_all_Transcripts()}) { 
+	      foreach my $exon (@{$transcript->get_all_Exons()}) {
+	        $exons{ "@{[$exon->start]}:@{[$exon->end]}" }++;
+	      }
       }
     }
     my @snps2 = ();
     my @exons = map { [ split /:/, $_ ] } keys %exons;
-    foreach my $snp (@snps) {
-      foreach my $exon (@exons) { 
-	if( $snp->[0] <= $exon->[1]+$context && $snp->[1] >= $exon->[0]-$context ) {
-	  push @snps2, $snp;
-	  last;
-	}
+    foreach my $snp (@snps) { 
+      foreach my $exon (@exons) {  
+	      if( $snp->[0] <= $exon->[1]+$context && $snp->[1] >= $exon->[0]-$context ) {
+	        push @snps2, $snp;
+	        last;
+	      }
       }
     }
     @snps = @snps2;
