88161336448ae0541ec5d1f2ea44d2061cfdde87
Beth Yates <bp1@sanger.ac.uk>
2008-10-08 14:40:25 +0000

    updated to new configuration

--- modules/EnsEMBL/Web/ImageConfig/genesnpview_transcript.pm
+++ modules/EnsEMBL/Web/ImageConfig/genesnpview_transcript.pm
@@ -9,7 +9,7 @@ sub init {
   my ($self) = @_;
   
   $self->set_parameters({
-    'title'            => 'Transcript slice',
+    'title'            => 'Transcripts',
     'show_buttons'     => 'yes',   # show +/- buttons
     'button_width'     => 8,       # width of red "+/-" buttons
     'show_labels'      => 'yes',   # show track names on left-hand side
@@ -21,24 +21,20 @@ sub init {
   });
 
   $self->create_menus(
-#    'gsv_variations'  => 'Variations',
     'other'           => 'Decorations',
     'gsv_domain'      => 'Protein Domains'    
  );
 
 
    $self->add_tracks( 'other',
-    [ 'gsv_transcript',   '',     'gsv_transcript',   { 'display' => 'on', 'colours' => $self->species_defs->colour('gene'), 'src' => 'all',  'strand' => 'b', 'menu' => 'no'  } ],
+    [ 'gsv_transcript',   '',     'gsv_transcript',   { 'display' => 'on', 'add_labels' => 1, 'colours' => $self->species_defs->colour('gene'), 'src' => 'all',  'strand' => 'b', 'menu' => 'no'  } ],
+    [ 'gsv_variations',   '',   'gsv_variations',     { 'display' => 'on', 'colours' => $self->species_defs->colour('variation'),  'strand' => 'r', 'menu' => 'no' } ],
    );
 
   $self->load_tracks();
 
-#  $self->add_tracks( 'gsv_variations',
-#    [ 'variation',             '',     'gsv_variations',          { 'display' => 'normal',  'strand' => 'r', 'menu' => 'no'         } ],
-#  );
-  $self->add_tracks( 'other',
-    [ 'draggable',        '',     'draggable',         { 'display' => 'normal',  'strand' => 'b', 'menu' => 'no'         } ],
- #   [ 'snp_join',         '',     'snp_join',          { 'display' => 'normal',  'strand' => 'b', 'menu' => 'no'         } ],
+ 
+ $self->add_tracks( 'other',
     [ 'spacer',           '',     'spacer',            { 'display' => 'normal',  'strand' => 'r', 'menu' => 'no'         } ],
   );
 
@@ -54,59 +50,12 @@ sub init {
     {qw(display on) }
   );
 
+  $self->modify_configs(
+    [qw(gsv_variations)],
+    {qw(display box) }
+  );
+
+
 }
 1;
 
-__END__
-      'validation' => [
-        [ 'opt_freq'       => 'By frequency' ],
-        [ 'opt_cluster'    => 'By cluster' ],
-        [ 'opt_doublehit'  => 'By doublehit' ],
-        [ 'opt_submitter'  => 'By submitter' ],
-        [ 'opt_hapmap'     => 'Hapmap' ],
-        [ 'opt_noinfo'     => 'No information' ],
-      ],
-      'classes' => [
-        [ 'opt_in-del'   => 'In-dels' ],
-        [ 'opt_snp'      => 'SNPs' ],
-        [ 'opt_mixed'    => 'Mixed variations' ],
-        [ 'opt_microsat' => 'Micro-satellite repeats' ],
-        [ 'opt_named'    => 'Named variations' ],
-        [ 'opt_mnp'      => 'MNPs' ],
-        [ 'opt_het'      => 'Hetrozygous variations' ],
-        [ 'opt_'         => 'Unclassified' ],
-      ],
-      'types' => [
-       [ 'opt_non_synonymous_coding' => 'Non-synonymous' ],
-       [ 'opt_synonymous_coding'     => 'Synonymous' ],
-       [ 'opt_frameshift_coding'     => 'Frameshift' ],
-       [ 'opt_stop_lost',            => 'Stop lost' ],
-       [ 'opt_stop_gained',          => 'Stop gained' ],
-       [ 'opt_essential_splice_site' => 'Essential splice site' ],
-       [ 'opt_splice_site'           => 'Splice site' ],
-       [ 'opt_upstream'              => 'Upstream' ],
-       [ 'opt_regulatory_region',    => 'Regulatory region' ],
-       [ 'opt_5prime_utr'            => "5' UTR" ],
-       [ 'opt_intronic'              => 'Intronic' ],
-       [ 'opt_3prime_utr'            => "3' UTR" ],
-       [ 'opt_downstream'            => 'Downstream' ],
-       [ 'opt_intergenic'            => 'Intergenic' ], 
-      ],
-    'GSV_transcript' => {
-      'on'          => "on",
-      'pos'         => '100',
-      'str'         => 'b',
-      'src'         => 'all', # 'ens' or 'all'
-      'colours' => {$self->{'_colourmap'}->colourSet( 'all_genes' )} ,
-    },
-    'GSV_snps' => {
-      'on'          => "on",
-      'pos'         => '200',
-      'str'         => 'r',
-      'colours'=>{$self->{'_colourmap'}->colourSet('variation')},
-    },
-
-  };
-  $self->ADD_ALL_PROTEIN_FEATURE_TRACKS_GSV;
-} 
-1;
