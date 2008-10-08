18d09c9c3e1609e90aa439354458bed18be88d67
Beth Yates <bp1@sanger.ac.uk>
2008-10-08 14:35:40 +0000

    added snp configuration

--- modules/EnsEMBL/Web/ImageConfig/genesnpview_context.pm
+++ modules/EnsEMBL/Web/ImageConfig/genesnpview_context.pm
@@ -3,7 +3,7 @@ use strict;
 use base qw(EnsEMBL::Web::ImageConfig);
 
 sub init {
-  my ($self) = @_;
+  my ($self) = @_; 
 
   $self->set_parameters({
     'title'         => 'Context slice',
@@ -13,13 +13,14 @@ sub init {
     'label_width'   => 100,     # width of labels on left-hand side
     'margin'        => 5,       # margin
     'spacing'       => 2,       # spacing
+    'features'      => [],
     'opt_halfheight'   => 0,    # glyphs are half-height [ probably removed when this becomes a track config ]
     'opt_empty_tracks' => 0,    # include empty tracks..
   });
   $self->create_menus(
     'sequence'        => 'Sequence',
     'transcript'      => 'Genes',
-#    'variation'       => 'Variation',
+    'variation'       => 'Variations', 
     'other'           => 'Other'
   );
   
@@ -27,20 +28,25 @@ sub init {
   $self->add_tracks( 'sequence',
     [ 'contig',    'Contigs',              'stranded_contig', { 'display' => 'normal',  'strand' => 'r'  } ],
   );
+
+
   $self->load_tracks();
 
   $self->add_tracks( 'other',
-    [ 'geneexon_bgtrack', '',     'geneexon_bgtrack',  { 'display' => 'normal',  'src' => 'all', 'colour' => 'bisque', 'tag' => 0,'strand' => 'b', 'menu' => 'no'         } ],
+    [ 'geneexon_bgtrack', '',     'geneexon_bgtrack',  { 'display' => 'normal',  'src' => 'all', 'colours' => 'bisque', 'tag' => 0,'strand' => 'b', 'menu' => 'no'         } ],
     [ 'draggable',        '',     'draggable',         { 'display' => 'normal',  'strand' => 'b', 'menu' => 'no'         } ],
-#    [ 'snp_join',         '',     'snp_join',          { 'display' => 'normal',  'strand' => 'b', 'menu' => 'no'         } ],
+    [ 'snp_join',         '',     'snp_join',          { 'display' => 'on',  'strand' => 'r', 'colours' => $self->species_defs->colour('variation'), 'tag' => 0, 'menu' => 'no'         } ],
     [ 'spacer',           '',     'spacer',            { 'display' => 'normal',  'strand' => 'r', 'menu' => 'no'         } ],
     [ 'ruler',            '',     'ruler',             { 'display' => 'normal',  'strand' => 'f', 'name' => 'Ruler'      } ],
     [ 'scalebar',         '',     'scalebar',          { 'display' => 'normal',  'height' => 50, 'strand' => 'f', 'name' => 'Scale bar'  } ],
   );
 
-#  $self->modify_configs(
-#    [qw(transcript variation)],
-#    {'on'=>'off'}
-#  );
+
+  $self->modify_configs(
+    [qw(variation_feature_variation)],
+    {qw(display normal), 'caption' => 'Variations'}
+  );
+
+
 }
 1;
