016be4f8878eef14e54d165f4300b1e5b39fdcb0
James Smith <js5@sanger.ac.uk>
2008-10-08 04:24:49 +0000

    added a setter so the bookmark link can get the title

--- modules/EnsEMBL/Web/Document/HTML/Title.pm
+++ modules/EnsEMBL/Web/Document/HTML/Title.pm
@@ -6,6 +6,7 @@ use EnsEMBL::Web::Document::HTML;
 
 sub new    { return shift->SUPER::new( 'title' => 'Untitled document' ); }
 sub set    { $_[0]{'title'}  = $_[1]; }
+sub get    { return $_[0]{'title'}; }
 sub render { $_[0]->printf( qq(  <title>%s</title>\n), CGI::escapeHTML($_[0]->{'title'}) ); }
 
 1;
