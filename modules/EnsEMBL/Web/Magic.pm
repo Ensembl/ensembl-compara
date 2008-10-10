f8774d7f622496832e64fad2ebe958e3990dad5f
James Smith <js5@sanger.ac.uk>
2008-10-10 07:45:53 +0000

    check to stop error that object exists before trying to redirect

--- modules/EnsEMBL/Web/Magic.pm
+++ modules/EnsEMBL/Web/Magic.pm
@@ -384,7 +384,7 @@ warn "SETTING ....".$webpage->page->{'_modal_dialog_'};
         $webpage->configure( $object, @sections );
       }
 warn "==================== ",$webpage->dataObjects->[0]->get_problem_type('redirect');
-      if( $webpage->dataObjects->[0]->has_problem_type( 'redirect' ) ) {
+      if( $webpage->dataObjects->[0] && $webpage->dataObjects->[0]->has_problem_type( 'redirect' ) ) {
         my($p) = $webpage->dataObjects->[0]->get_problem_type('redirect');
   use Data::Dumper;      warn Data::Dumper::Dumper( $p );
         warn $p->name;
