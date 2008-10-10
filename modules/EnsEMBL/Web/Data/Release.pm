57f06e8fff870693f83f49354a3b3705023368e7
Simon Brent <sb23@sanger.ac.uk>
2008-10-10 13:43:41 +0000

    added online column, corrected date formatting

--- modules/EnsEMBL/Web/Data/Release.pm
+++ modules/EnsEMBL/Web/Data/Release.pm
@@ -16,9 +16,10 @@ __PACKAGE__->add_queriable_fields(
   number  => 'varchar(5)',
   date    => 'date',
   archive => 'varchar(7)',
+  online  => "enum('N','Y')"
 );
 
-__PACKAGE__->columns(TEMP => qw/full_date short_date long_date/);
+__PACKAGE__->columns(TEMP => qw/full_date short_date shorter_date long_date/);
 
 __PACKAGE__->has_many(news_items => 'EnsEMBL::Web::Data::NewsItem');
 __PACKAGE__->has_many(species    => 'EnsEMBL::Web::Data::ReleaseSpecies');
@@ -27,9 +28,10 @@ __PACKAGE__->add_trigger(select => \&format_time);
 
 sub format_time {
   my $self = shift;
-  $self->full_date(strftime('%D %M %Y', localtime( str2time($self->date) )));
+  $self->full_date(strftime('%d %m %Y', localtime( str2time($self->date) )));
   $self->short_date(strftime('%b %Y', localtime( str2time($self->date) )));
-  $self->long_date(strftime('%M %Y', localtime( str2time($self->date) )));
+  $self->shorter_date(strftime('%b%y', localtime( str2time($self->date) )));
+  $self->long_date(strftime('%m %Y', localtime( str2time($self->date) )));
 }
 
 1;
