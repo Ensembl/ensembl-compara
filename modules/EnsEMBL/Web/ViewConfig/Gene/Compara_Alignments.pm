fc459dbaaf2af7232add6606be88b1a4cc26824e
Steve Trevanion <st3@sanger.ac.uk>
2008-10-14 13:15:11 +0000

    use shared configurations

--- modules/EnsEMBL/Web/ViewConfig/Gene/Compara_Alignments.pm
+++ modules/EnsEMBL/Web/ViewConfig/Gene/Compara_Alignments.pm
@@ -3,6 +3,7 @@ package EnsEMBL::Web::ViewConfig::Gene::Compara_Alignments;
 use strict;
 use warnings;
 no warnings 'uninitialized';
+use EnsEMBL::Web::Constants;
 
 sub init {
   my ($view_config) = @_;
@@ -26,98 +27,31 @@ sub form {
   my( $view_config, $object ) = @_;
 
   #options shared with marked-up sequence
-  $view_config->add_form_element({
-    'type' => 'NonNegInt', 'required' => 'yes',
-    'label' => "5' Flanking sequence",  'name' => 'flank5_display',
-  });
-  $view_config->add_form_element({
-    'type' => 'NonNegInt', 'required' => 'yes',
-    'label' => "3' Flanking sequence",  'name' => 'flank3_display',
-  });
-  my $values = [
-    { 'value' => 'off',           'name' => 'No exon markup' },
-    { 'value' => 'Ab-initio',     'name' => 'Ab-initio exons' },
-    { 'value' => 'core',          'name' => "Core exons" }
-  ];
-  push @$values, { 'value' => 'vega', 'name' => 'Vega exons' }
-    if $object->species_defs->databases->{'DATABASE_VEGA'};
-  push @$values, { 'value' => 'otherfeatures', 'name' => 'EST gene exons' }
-    if $object->species_defs->databases->{'DATABASE_OTHERFEATURES'};
+  my %gene_markup_options    =  EnsEMBL::Web::Constants::GENE_MARKUP_OPTIONS;
+  #options shared with resequencing and marked-up sequence
+  my %general_markup_options = EnsEMBL::Web::Constants::GENERAL_MARKUP_OPTIONS;
+  #options shared with resequencing
+  my %other_markup_options = EnsEMBL::Web::Constants::OTHER_MARKUP_OPTIONS;
 
-  $view_config->add_form_element({
-    'type'     => 'DropDown', 'select'   => 'select',
-    'required' => 'yes',      'name'     => 'exon_display',
-    'label'    => 'Additional exons to display',
-    'values'   => $values
-  });
-  $view_config->add_form_element({
-    'type'     => 'DropDown', 'select'   => 'select',
-    'required' => 'yes',      'name'     => 'exon_ori',
-    'label'    => "Orientation of additional exons",
-    'values'   => [
-      { 'value' =>'fwd' , 'name' => 'Display same orientation exons only' },
-      { 'value' =>'rev' , 'name' => 'Display reverse orientation exons only' },
-      { 'value' =>'all' , 'name' => 'Display exons in both orientations' }
-    ]
-  });
-  if( $object->species_defs->databases->{'DATABASE_VARIATION'} ) {
-    $view_config->add_form_element({
-      'type'     => 'DropDown', 'select'   => 'select',
-      'required' => 'yes',      'name'     => 'snp_display',
-      'label'    => 'Show variations',
-      'values'   => [
-        { 'value' =>'off',       'name' => 'No' },
-        { 'value' =>'snp',       'name' => 'Yes' },
-        { 'value' =>'snp_link' , 'name' => 'Yes and show links' },
-      ]
-    });
+  $view_config->add_form_element($gene_markup_options{'flank5_display'});
+  $view_config->add_form_element($gene_markup_options{'flank3_display'});
+  $view_config->add_form_element($other_markup_options{'display_width'});
+
+  if ($object->species_defs->databases->{'DATABASE_VEGA'}) {
+      push @{$gene_markup_options{'exon_display'}{'values'}}, { 'value' => 'vega', 'name' => 'Vega exons' };
   }
-  $view_config->add_form_element({
-    'type'     => 'DropDown', 'select'   => 'select',
-    'required' => 'yes',      'name'     => 'line_numbering',
-    'label'    => 'Line numbering',
-    'values'   => [
-      { 'value' =>'sequence' , 'name' => 'Relative to this sequence' },
-      { 'value' =>'slice'    , 'name' => 'Relative to coordinate systems' },
-      { 'value' =>'off'      , 'name' => 'None' },
-    ]
-  });
+  if ($object->species_defs->databases->{'DATABASE_OTHERFEATURES'}) {
+      push @{$gene_markup_options{'exon_display'}{'values'}},  { 'value' => 'otherfeatures', 'name' => 'EST gene exons' };
+  }
+  $view_config->add_form_element($gene_markup_options{'exon_display'});
 
-  #specific options
-  $view_config->add_form_element({
-    'type' => 'NonNegInt', 'required' => 'yes',
-    'label' => "Alignment width",  'name' => 'display_width',
-  });
-  my $conservation = [
-    { 'value' =>'all' , 'name' => 'All conserved regions' },
-    { 'value' =>'off' , 'name' => 'None' },
-  ];
-  $view_config->add_form_element({
-    'type'     => 'DropDown', 'select'   => 'select',
-    'required' => 'yes',      'name'     => 'conservation',
-    'label'    => 'Conservation regions',
-    'values'   => $conservation,
-  });
-  my $codons_display = [
-    { 'value' =>'all' , 'name' => 'START/STOP codons' },
-    { 'value' =>'off' , 'name' => "Do not show codons" },
-  ];
-  $view_config->add_form_element({
-    'type'     => 'DropDown', 'select'   => 'select',
-    'required' => 'yes',      'name'     => 'codons_display',
-    'label'    => 'Codons',
-    'values'   => $codons_display,
-  });
-  my $title_display = [
-    { 'value' =>'all' , 'name' => 'Include `title` tags' },
-    { 'value' =>'off' , 'name' => 'None' },
-  ];
-  $view_config->add_form_element({
-    'type'     => 'DropDown', 'select'   => 'select',
-    'required' => 'yes',      'name'     => 'title_display',
-    'label'    => 'Title display',
-    'values'   => $title_display,
-  });
+  $view_config->add_form_element($general_markup_options{'exon_ori'});
+  if( $object->species_defs->databases->{'DATABASE_VARIATION'} ) {
+    $view_config->add_form_element( $general_markup_options{'snp_display'} );
+  }
+  $view_config->add_form_element($general_markup_options{'line_numbering'} );
+  $view_config->add_form_element($other_markup_options{'codons_display'});
+  $view_config->add_form_element($other_markup_options{'title_display'});
 
   my $species = $view_config->species;
   my $hash = $view_config->species_defs->multi_hash->{'DATABASE_COMPARA'}{'ALIGNMENTS'}||{};
@@ -165,7 +99,7 @@ sub form {
     next unless $row->{'species'}{$species};
     foreach( sort keys %{$row->{'species'}} ) {
       my $name = 'species_'.$row_key.'_'.lc($_);
-      warn $name;
+ #     warn $name;
       if( $_ ne $species ) {
         $view_config->add_form_element({
           'type'     => 'CheckBox', 'label' => $view_config->_species_label($_),
