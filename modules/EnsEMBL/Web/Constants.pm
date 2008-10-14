410b282fd1a49b860ea3132cd45db91f662f0e98
Steve Trevanion <st3@sanger.ac.uk>
2008-10-14 13:05:25 +0000

    shared configurations for compara_align, marked-up sequence and resequencing

--- modules/EnsEMBL/Web/Constants.pm
+++ modules/EnsEMBL/Web/Constants.pm
@@ -59,4 +59,93 @@ sub NEWICK_OPTIONS {
   );
 }
 
+#shared by 'Genomic Alignments', 'Marked-up Sequence' and 'Resequencing'
+sub GENERAL_MARKUP_OPTIONS {
+    return (
+	'snp_display' => {
+	    'type'     => 'DropDown', 'select'   => 'select',
+	    'required' => 'yes',      'name'     => 'snp_display',
+	    'label'    => 'Show variations',
+	    'values'   => [
+		{ 'value' =>'off',       'name' => 'No' },
+		{ 'value' =>'snp',       'name' => 'Yes' },
+		{ 'value' =>'snp_link' , 'name' => 'Yes and show links' },
+	    ]
+	},
+	'line_numbering' => {
+	    'type'     => 'DropDown', 'select'   => 'select',
+	    'required' => 'yes',      'name'     => 'line_numbering',
+	    'label'    => 'Line numbering',
+	    'values'   => [
+		{ 'value' =>'sequence' , 'name' => 'Relative to this sequence' },
+		{ 'value' =>'slice'    , 'name' => 'Relative to coordinate systems' },
+		{ 'value' =>'off'      , 'name' => 'None' },
+	    ]
+	},
+	'exon_ori' => {
+	    'type'     => 'DropDown', 'select'   => 'select',
+	    'required' => 'yes',      'name'     => 'exon_ori',
+	    'label'    => "Orientation of additional exons",
+	    'values'   => [
+		{ 'value' =>'fwd' , 'name' => 'Display same orientation exons only' },
+		{ 'value' =>'rev' , 'name' => 'Display reverse orientation exons only' },
+		{ 'value' =>'all' , 'name' => 'Display exons in both orientations' },
+	    ],
+	},
+    );
+}
+
+#shared by 'Genomic Alignments' and 'Marked-up Sequence'
+sub GENE_MARKUP_OPTIONS {
+    return (
+	'flank5_display' => {
+	    'type' => 'NonNegInt', 'required' => 'yes',
+	    'label' => "5' Flanking sequence",  'name' => 'flank5_display',
+	},
+	'flank3_display' => {
+	    'type' => 'NonNegInt', 'required' => 'yes',
+	    'label' => "3' Flanking sequence",  'name' => 'flank3_display',
+	},
+	'exon_display' => {
+	    'type'     => 'DropDown', 'select'   => 'select',
+	    'required' => 'yes',      'name'     => 'exon_display',
+	    'label'    => 'Additional exons to display',
+	    'values'   => [
+		{ 'value' => 'off',           'name' => 'No exon markup' },
+		{ 'value' => 'Ab-initio',     'name' => 'Ab-initio exons' },
+		{ 'value' => 'core',          'name' => "Core exons" },
+	    ],
+	},
+    );
+}
+
+#shared by 'Genomic Alignments' and 'Resequencing'
+sub OTHER_MARKUP_OPTIONS {
+    return (
+	'display_width' => {
+	    'type' => 'NonNegInt', 'required' => 'yes',
+	    'label' => "Alignment width",  'name' => 'display_width',
+	},
+	'codons_display' => {
+	    'type'     => 'DropDown', 'select'   => 'select',
+	    'required' => 'yes',      'name'     => 'codons_display',
+	    'label'    => 'Codons',
+	    'values'   => [
+		{ 'value' =>'all' , 'name' => 'START/STOP codons' },
+		{ 'value' =>'off' , 'name' => "Do not show codons" },
+	    ],
+	},
+	'title_display' => {
+	    'type'     => 'DropDown', 'select'   => 'select',
+	    'required' => 'yes',      'name'     => 'title_display',
+	    'label'    => 'Title display',
+	    'values'   => [
+		{ 'value' =>'all' , 'name' => 'Include `title` tags' },
+		{ 'value' =>'off' , 'name' => 'None' },
+	    ],
+	},
+    );
+}
+
+
 1;
