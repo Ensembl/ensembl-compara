fc9377ceeab1649c142d6e6077c948fadd6d0c51
Steve Trevanion <st3@sanger.ac.uk>
2008-10-14 13:12:19 +0000

    use shared configuration options

--- modules/EnsEMBL/Web/ViewConfig/Gene/Sequence.pm
+++ modules/EnsEMBL/Web/ViewConfig/Gene/Sequence.pm
@@ -3,6 +3,7 @@ package EnsEMBL::Web::ViewConfig::Gene::Sequence;
 use strict;
 use warnings;
 no warnings 'uninitialized';
+use EnsEMBL::Web::Constants;
 
 sub init {
   my ($view_config) = @_;
@@ -10,7 +11,7 @@ sub init {
   $view_config->title = 'Marked up gene sequence';
   $view_config->_set_defaults(qw(
     flank5_display          600
-    flank3_display          600 
+    flank3_display          600
     exon_display            core
     exon_ori                all
     snp_display             off
@@ -22,63 +23,27 @@ sub init {
 
 sub form {
   my( $view_config, $object ) = @_;
+  my %gene_markup_options    =  EnsEMBL::Web::Constants::GENE_MARKUP_OPTIONS;
+  my %general_markup_options = EnsEMBL::Web::Constants::GENERAL_MARKUP_OPTIONS;
 
-  $view_config->add_form_element({
-    'type' => 'NonNegInt', 'required' => 'yes',
-    'label' => "5' Flanking sequence",  'name' => 'flank5_display',
-  });
-  $view_config->add_form_element({
-    'type' => 'NonNegInt', 'required' => 'yes',
-    'label' => "3' Flanking sequence",  'name' => 'flank3_display',
-  });
-  my $values = [
-    { 'value' => 'off',           'name' => 'No exon markup' },
-    { 'value' => 'Ab-initio',     'name' => 'Ab-initio exons' },
-    { 'value' => 'core',          'name' => "Core exons" }
-  ];
-  push @$values, { 'value' => 'vega', 'name' => 'Vega exons' }
-    if $object->species_defs->databases->{'DATABASE_VEGA'};
-  push @$values, { 'value' => 'otherfeatures', 'name' => 'EST gene exons' }
-    if $object->species_defs->databases->{'DATABASE_OTHERFEATURES'};
+  #options shared with marked-up sequence
+  $view_config->add_form_element($gene_markup_options{'flank5_display'});
+  $view_config->add_form_element($gene_markup_options{'flank3_display'});
 
-  $view_config->add_form_element({
-    'type'     => 'DropDown', 'select'   => 'select',
-    'required' => 'yes',      'name'     => 'exon_display',
-    'label'    => 'additional exons to display',
-    'values'   => $values
-  });
-  $view_config->add_form_element({
-    'type'     => 'DropDown', 'select'   => 'select',
-    'required' => 'yes',      'name'     => 'exon_ori',
-    'label'    => "Orientation of additional exons",
-    'values'   => [
-      { 'value' =>'fwd' , 'name' => 'Display same orientation exons only' },
-      { 'value' =>'rev' , 'name' => 'Display reverse orientation exons only' },
-      { 'value' =>'all' , 'name' => 'Display exons in both orientations' }
-    ]
-  });
-  if( $object->species_defs->databases->{'DATABASE_VARIATION'} ) {
-    $view_config->add_form_element({
-      'type'     => 'DropDown', 'select'   => 'select',
-      'required' => 'yes',      'name'     => 'snp_display',
-      'label'    => 'Show variations',
-      'values'   => [
-        { 'value' =>'off',       'name' => 'No' },
-        { 'value' =>'snp',       'name' => 'Yes' },
-        { 'value' =>'snp_link' , 'name' => 'Yes and show links' },
-      ]
-    });
+  if ($object->species_defs->databases->{'DATABASE_VEGA'}) {
+      push @{$gene_markup_options{'exon_display'}{'values'}}, { 'value' => 'vega', 'name' => 'Vega exons' };
   }
-  $view_config->add_form_element({
-    'type'     => 'DropDown', 'select'   => 'select',
-    'required' => 'yes',      'name'     => 'line_numbering',
-    'label'    => 'Line numbering',
-    'values'   => [
-      { 'value' =>'sequence' , 'name' => 'Relative to this sequence' },
-      { 'value' =>'slice'    , 'name' => 'Relative to coordinate systems' },
-      { 'value' =>'off'      , 'name' => 'None' },
-    ]
-  });
+  if ($object->species_defs->databases->{'DATABASE_OTHERFEATURES'}) {
+      push @{$gene_markup_options{'exon_display'}{'values'}},  { 'value' => 'otherfeatures', 'name' => 'EST gene exons' };
+  }
+  $view_config->add_form_element($gene_markup_options{'exon_display'});
+
+  #options shared with resequencing and marked-up sequence
+  $view_config->add_form_element($general_markup_options{'exon_ori'});
+  if( $object->species_defs->databases->{'DATABASE_VARIATION'} ) {
+    $view_config->add_form_element( $general_markup_options{'snp_display'} );
+  }
+  $view_config->add_form_element( $general_markup_options{'line_numbering'} );
 
 }
 1;
