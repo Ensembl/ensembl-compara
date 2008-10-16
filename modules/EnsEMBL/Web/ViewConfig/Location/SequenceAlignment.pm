22e4cc7212e59c050b879c132aaae31c1441603d
Steve Trevanion <st3@sanger.ac.uk>
2008-10-16 10:26:56 +0000

    replacement for ViewConfig/Location/VariationAlignment.pm; nearly working but no variations nor passing of individuals to be compared

---
+++ modules/EnsEMBL/Web/ViewConfig/Location/SequenceAlignment.pm
@@ -0,0 +1,96 @@
+package EnsEMBL::Web::ViewConfig::Location::SequenceAlignment;
+
+use strict;
+use warnings;
+no warnings 'uninitialized';
+use EnsEMBL::Web::Constants;
+
+sub init {
+  my ($view_config) = @_;
+  $view_config->title = 'Resequencing Alignments';
+  $view_config->_set_defaults(qw(
+    display_width           120
+    exon_ori                all
+    match_display           off
+    snp_display             off
+    line_numbering          off
+    codons_display          off
+    title_display           off
+  ));
+  $view_config->storable = 1;
+}
+
+sub form {
+    use Data::Dumper;
+    my( $view_config, $object ) = @_;
+
+    #shared with compara_markup and marked-up sequence
+    my %general_markup_options = EnsEMBL::Web::Constants::GENERAL_MARKUP_OPTIONS;
+    #shared with compara_markup
+    my %other_markup_options = EnsEMBL::Web::Constants::OTHER_MARKUP_OPTIONS;
+
+    $view_config->add_form_element($other_markup_options{'display_width'});
+    push @{$general_markup_options{'exon_ori'}{'values'}}, { 'value' =>'off' , 'name' => 'None' };
+    $general_markup_options{'exon_ori'}{'label'} = 'Exons to highlight';
+    $view_config->add_form_element($general_markup_options{'exon_ori'});
+
+    $view_config->add_form_element({
+	'type'     => 'DropDown', 'select'   => 'select',
+	'required' => 'yes',      'name'     => 'match_display',
+	'label'    => 'Matching basepairs',
+	'values'   => [
+	    { 'value' =>'off' , 'name' => 'Show all' },
+	    { 'value' =>'dot' , 'name' => 'Replace matching bp with dots' },
+	],
+    });
+
+    if( $object->species_defs->databases->{'DATABASE_VARIATION'} ) {
+	$view_config->add_form_element($general_markup_options{'snp_display'} );
+    }
+    $view_config->add_form_element($general_markup_options{'line_numbering'} );
+    $view_config->add_form_element($other_markup_options{'codons_display'});
+    $view_config->add_form_element($other_markup_options{'title_display'});
+
+ #   $view_config->add_fieldset( "Options for Alignment" );
+    my $sp = $view_config->species;
+    my $vari_hash = $object->species_defs->vari_hash($sp);
+
+#    warn Dumper($vari_hash);
+
+    my $strains =  $object->species_defs->translate( 'strain' );
+    my $ref = $vari_hash->{'REFERENCE_STRAIN'};
+    $view_config->add_form_element({
+	'type'     => 'NoEdit',
+	'name'     => 'reference_individual',
+	'label'    => "Reference $strains",
+	'value'    => "$ref"
+    });
+
+    $strains .= 's';
+
+    my @comparisons;
+    foreach (qw(DEFAULT_STRAINS DISPLAY_STRAINS)) {
+	my $set = $vari_hash->{$_};
+	foreach (@{$set}) {
+	    push @comparisons, { value => $_, name => $_ };
+	}
+    }
+    $view_config->add_form_element({
+      'type'     => 'MultiSelect',
+      'name'     => 'individuals',
+      'label'    => "Resequenced $sp $strains",
+      'values'   => \@comparisons,
+      'value'    => $object->param('individuals'),
+      'raw'      => 1,
+    });
+#   $view_config->add_form_element({
+#      'type'     => 'CheckBox',
+#      'name'     => 'individuals',
+#      'label'    => "Resequenced $sp $strains",
+#      'values'   => 'Venter',
+#      'value'    => 'yes',
+#      'raw'      => 1,
+#    });
+}
+
+1;
