788418f16e3e8abdf5218ebaf7a2447b8fc24750
James Smith <js5@sanger.ac.uk>
2008-10-17 09:00:46 +0000

    tidied up title code to remove tags

--- modules/EnsEMBL/Web/Document/HTML/Title.pm
+++ modules/EnsEMBL/Web/Document/HTML/Title.pm
@@ -7,7 +7,12 @@ use EnsEMBL::Web::Document::HTML;
 sub new    { return shift->SUPER::new( 'title' => 'Untitled document' ); }
 sub set    { $_[0]{'title'}  = $_[1]; }
 sub get    { return $_[0]{'title'}; }
-sub render { $_[0]->printf( qq(  <title>%s</title>\n), CGI::escapeHTML($_[0]->{'title'}) ); }
-
+sub render {
+  my $self = shift;
+  my $t = $self->get;
+  $t =~ s/<[^>]+>//g;
+  $self->printf( q(
+  <title>%s</title>), $t );
+}
 1;
 
