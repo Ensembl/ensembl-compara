5cf77b7605fbdb8351334580ae4e3a9ae9ea84d6
James Smith <js5@sanger.ac.uk>
2008-10-17 09:01:06 +0000

    removed

--- modules/EnsEMBL/Web/URLfeature/GTF.pm
+++
@@ -1,8 +0,0 @@
-package EnsEMBL::Web::URLfeature::GTF;
-use strict;
-use EnsEMBL::Web::URLfeature::GFF;
-use vars qw(@ISA);
-@ISA = qw(EnsEMBL::Web::URLfeature::GFF);
-
-sub id      { my $self = shift; return $self->{'__raw__'}[16]; }
-1;
