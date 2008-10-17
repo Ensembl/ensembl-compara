a26416393c1b09c8e6d3937f330537a74ce89549
Beth Yates <bp1@sanger.ac.uk>
2008-10-17 12:49:32 +0000

    replaces TSV_transcript_bottom.pm, new style config code

---
+++ modules/EnsEMBL/Web/ImageConfig/tsv_transcripts_bottom.pm
@@ -0,0 +1,36 @@
+package EnsEMBL::Web::ImageConfig::tsv_transcripts_bottom;
+use strict;
+use base qw(EnsEMBL::Web::ImageConfig);
+
+sub init {
+  my ($self) = @_;
+
+  $self->set_parameters({
+    'title'             => 'Transcripts bottom',
+    'show_buttons'      => 'yes',   # show +/- buttons
+    'button_width'      => 8,       # width of red "+/-" buttons
+    'show_labels'       => 'yes',   # show track names on left-hand side
+    'label_width'       => 100,     # width of labels on left-hand side
+    'margin'            => 5,       # margin
+    'spacing'           => 2,       # spacing
+    'opt_halfheight'    => 0,    # glyphs are half-height [ probably removed when this becomes a track config ]
+    'opt_empty_tracks'  => 0,    # include empty tracks..
+  });
+
+  $self->create_menus(
+    'other'           => 'Other'
+  );
+
+  $self->add_tracks( 'other',
+    [ 'transcriptexon_bgtrack', '',     'geneexon_bgtrack',   { 'display' => 'normal', 'src' => 'all', 'colours' => 'bisque', 'tag' => 2, 'strand' => 'r', 'menu' => 'no'  } ],
+    [ 'snp_join',               '',     'snp_join',           { 'display' => 'on',  'strand' => 'r', 'context' =>50, 'tag' => 2, 'colours' => $self->species_defs->colour('variation'), 'menu' => 'no'         } ],
+    [ 'ruler',                  '',     'ruler',              { 'display' => 'normal',  'strand' => 'r', 'name' => 'Ruler' } ],
+    [ 'spacer',                 '',     'spacer',             { 'display' => 'normal', 'height' => 50,   'strand' => 'r', 'menu' => 'no'         } ],
+  );
+}
+
+
+
+
+
+1;
