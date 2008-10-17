416695f566cc889a52ce5277afeb90e744d31b2d
Beth Yates <bp1@sanger.ac.uk>
2008-10-17 12:46:36 +0000

    replaces TSV_transcript.pm, moved to new style config code

---
+++ modules/EnsEMBL/Web/ImageConfig/tsv_transcript.pm
@@ -0,0 +1,48 @@
+package EnsEMBL::Web::ImageConfig::tsv_transcript;
+use strict;
+use EnsEMBL::Web::ImageConfig;
+use vars qw(@ISA);
+@ISA = qw(EnsEMBL::Web::ImageConfig);
+
+sub init {
+  my ($self) = @_;
+
+  $self->set_parameters({
+    'title'             => 'Transcript slice',
+    '_options'          => [qw(pos col known unknown)],
+    'show_buttons'      => 'yes',   # show +/- buttons
+    'button_width'      => 8,       # width of red "+/-" buttons
+    'show_labels'       => 'yes',   # show track names on left-hand side
+    'label_width'       => 100,     # width of labels on left-hand side
+    'margin'            => 5,       # margin
+    'spacing'           => 2,       # spacing
+    'opt_halfheight'    => 0,    # glyphs are half-height [ probably removed when this becomes a track config ]
+    'opt_empty_tracks'  => 0,    # include empty tracks..
+    '_add_labels'       => 1, 
+ });
+
+
+  $self->create_menus(
+    'transcript'    => 'Genes',
+    'prediction'    => 'Prediction transcripts',
+    'other'         => 'Decorations',
+ );
+
+
+  $self->load_tracks();
+
+
+ $self->add_tracks( 'other',
+    [ 'transcriptexon_bgtrack', '',     'transcriptexon_bgtrack',   { 'display' => 'normal',  'src' => 'all', 'colours' => 'bisque', 'tag' => 1,'strand' => 'b', 'menu' => 'no'         } ],
+    [ 'spacer',                 '',     'spacer',                   { 'display' => 'normal', 'height' =>20,  'strand' => 'r', 'menu' => 'no'         } ],
+    [ 'snp_join',               '',     'snp_join',                 { 'display' => 'on',  'strand' => 'b','tag' => 1, 'colours' => $self->species_defs->colour('variation'), 'menu' => 'no'         } ],
+  );
+
+  $self->modify_configs(
+    [qw(transcript)],
+    {'display' => 'off'}
+  );
+
+}
+1;
+
