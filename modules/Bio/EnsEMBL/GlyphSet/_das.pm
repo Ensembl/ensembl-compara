562f12573170627c1e82a1d9cc3465ffc67f8da1
Andrew Jenkinson <aj5@sanger.ac.uk>
2008-10-20 20:39:47 +0000

    placeholder code

--- modules/Bio/EnsEMBL/GlyphSet/_das.pm
+++ modules/Bio/EnsEMBL/GlyphSet/_das.pm
@@ -2,26 +2,35 @@ package Bio::EnsEMBL::GlyphSet::_das;
 
 use strict;
 use base qw(Bio::EnsEMBL::GlyphSet_simple);
-use Data::Dumper;
+use Bio::EnsEMBL::ExternalData::DAS::Stylesheet;
 
 sub _das_type {  return 'das'; }
 
 sub features       { 
   my $self = shift;
-## Fetch all the das features...
+  
+  ## Fetch all the das features...
   unless( $self->cache('das_features') ) {
-    $self->cache('das_features', $self->cache('das_coord')->fetch_Features( $self->{'container'} )||{} ); 
-  } 
-  local $Data::Dumper::Indent = 1;
-  warn Dumper( $self->cache('das_features') );
-  my %T = %{$self->cache('das_features')};
-  warn "DAS: ",join "\t",@{$self->my_config('logicnames')};
-  my @features = @T{ @{$self->my_config('logicnames')} };
-
-## Filter for my source...
-  foreach( @features ) {
-    warn "\tDAS: ", join "\n\tDAS: ", keys %{$_};
+    # Query by slice:
+    $self->cache('das_features', $self->cache('das_coord')->fetch_Features( $self->{'container'} )||{} );
   }
+  
+  my $data = $self->cache('das_features');
+  
+  for my $logic_name ( @{ $self->my_config('logicnames') } ) {
+    
+    my $stylesheet = $data->{ $logic_name }{ 'stylesheet' } || Bio::EnsEMBL::ExternalData::DAS::Stylesheet->new();
+    my @features   = @{ $data->{ $logic_name }{ 'features' } };
+    my @errors     = @{ $data->{ $logic_name }{ 'errors'   } };
+    
+    warn sprintf "DAS / %s / %d features / %d errors", $logic_name, scalar @features, scalar @errors;
+    
+    for my $error ( @errors ) {
+      warn "DAS / $logic_name / $error";
+    }
+    
+  }
+  
   return [];
 }
 
