4bf2a64d59bca1b273ae3df7e1d677742e182e27
Beth Yates <bp1@sanger.ac.uk>
2008-10-21 13:16:20 +0000

    added info about how to configure gene variation views

---
+++ modules/EnsEMBL/Web/Component/Gene/GeneSNPInfo.pm
@@ -0,0 +1,31 @@
+package EnsEMBL::Web::Component::Gene::GeneSNPInfo;
+
+use strict;
+use warnings;
+no warnings "uninitialized";
+use base qw(EnsEMBL::Web::Component::Gene);
+
+sub _init {
+  my $self = shift;
+  $self->cacheable( 1 );
+  $self->ajaxable(  1 );
+}
+
+sub caption {
+  return;
+}
+
+
+sub content {
+  my $self = shift;
+
+  ### Adds text to Genetic variation nodes
+  my $html = "<p>The 'Configure this page' link in the menu on the left hand side of this page can be used to customise the exon context and types of SNPs displayed in both the tables below and the variation image. Please note the default 'Context' settings will probably filter out some intronic SNPs.</p><br />";
+ 
+
+  return $html;
+}
+
+1;
+
+
