92454bd42354821759a0139ad96e7f3527d794e2
Evgeny Bragin <eb4@sanger.ac.uk>
2008-10-22 14:56:33 +0000

    + Upload record type for user

---
+++ modules/EnsEMBL/Web/Data/Record/Upload.pm
@@ -0,0 +1,18 @@
+package EnsEMBL::Web::Data::Record::Upload;
+
+use strict;
+use warnings;
+use base qw(EnsEMBL::Web::Data::Record);
+
+__PACKAGE__->_type('upload');
+
+__PACKAGE__->add_fields(
+{
+  filename => 'text',
+  format   => 'text',
+  species  => 'text',
+  share_id => 'int',
+  assembly => 'text',
+};
+
+1;
