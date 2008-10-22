84ab0feef110bcc48dabae914ce163eb8361be45
Evgeny Bragin <eb4@sanger.ac.uk>
2008-10-22 14:51:34 +0000

    + Upload data class

--- modules/EnsEMBL/Web/Data/Record/Invite.pm
+++ modules/EnsEMBL/Web/Data/Record/Invite.pm
@@ -1,15 +1,18 @@
-package EnsEMBL::Web::Data::Record::Invite;
+package EnsEMBL::Web::Data::Record::Upload;
 
 use strict;
 use warnings;
 use base qw(EnsEMBL::Web::Data::Record);
 
-__PACKAGE__->_type('invite');
+__PACKAGE__->_type('upload');
 
 __PACKAGE__->add_fields(
-  email  => 'text',
-  status => 'text',
-  code   => 'text',
-);
+{
+  filename => 'text',
+  format   => 'text',
+  species  => 'text',
+  share_id => 'int',
+  assembly => 'text',
+};
 
 1;
