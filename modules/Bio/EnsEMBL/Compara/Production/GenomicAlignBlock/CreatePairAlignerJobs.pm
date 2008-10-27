d02f63212b5312401f1f2cbf811f4a03b7fd9e57
Javier Herrero <jh7@sanger.ac.uk>
2008-10-27 21:12:09 +0000

    Make the module inherit from Bio::EnsEMBL::Hive::Process

--- modules/Bio/EnsEMBL/Compara/Production/GenomicAlignBlock/CreatePairAlignerJobs.pm
+++ modules/Bio/EnsEMBL/Compara/Production/GenomicAlignBlock/CreatePairAlignerJobs.pm
@@ -54,8 +54,9 @@ use Bio::EnsEMBL::Compara::Production::DnaFragChunk;
 use Bio::EnsEMBL::Compara::Production::DnaFragChunkSet;
 use Bio::EnsEMBL::Compara::Production::DnaCollection;
 
-use Bio::EnsEMBL::Pipeline::RunnableDB;
-our @ISA = qw(Bio::EnsEMBL::Pipeline::RunnableDB);
+use Bio::EnsEMBL::Analysis::RunnableDB;
+use Bio::EnsEMBL::Hive::Process
+our @ISA = qw(Bio::EnsEMBL::Hive::Process);
 
 sub fetch_input {
   my $self = shift;
