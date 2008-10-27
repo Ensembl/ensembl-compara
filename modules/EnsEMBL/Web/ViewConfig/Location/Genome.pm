8a21abab31fc817fcc8fcf6143721d8dce0683cd
Anne Parker <ap5@sanger.ac.uk>
2008-10-27 16:35:22 +0000

    New module for integrating karyoview configuration with control panel

---
+++ modules/EnsEMBL/Web/ViewConfig/Location/Genome.pm
@@ -0,0 +1,29 @@
+package EnsEMBL::Web::ViewConfig::Location::Genome;
+
+use strict;
+use warnings;
+no warnings 'uninitialized';
+
+sub init {
+### Used by Constructor
+### init function called to set defaults for the passed
+### {{EnsEMBL::Web::ViewConfig}} object
+
+  my( $view_config ) = @_;
+
+  $view_config->_set_defaults(qw(
+    panel_top      yes
+    panel_zoom      no
+    image_width   1200
+    zoom_width     100
+    context       1000
+  ));
+  $view_config->add_image_configs({qw(
+    Vkaryotype    das
+  )});
+  $view_config->default_config = 'Vkaryotype';
+  $view_config->storable       = 1;
+}
+
+
+1;
