c9efefe8a9bf9c7429a59983b8dab48a4204863f
Andrew Jenkinson <aj5@sanger.ac.uk>
2008-10-28 12:54:37 +0000

    Updated fields

--- modules/EnsEMBL/Web/Data/Record/DAS.pm
+++ modules/EnsEMBL/Web/Data/Record/DAS.pm
@@ -8,17 +8,16 @@ use base qw(EnsEMBL::Web::Data::Record);
 __PACKAGE__->_type('das');
 
 __PACKAGE__->add_fields(
-  url         => 'text',
-  name        => 'text',
-  config      => 'text',
-  maintainer  => 'text',
   logic_name  => 'text',
+  url         => 'text',
+  dsn         => 'text',
+  maintainer  => 'text',
   description => 'text',
   on          => 'text',
   homepage    => 'text',
-  dsn         => 'text',
   label       => 'text',
   category    => 'text',
+  coords      => 'text',
 );
 
 1;
