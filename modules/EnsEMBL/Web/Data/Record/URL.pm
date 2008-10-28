a7915642409398ba90b3fd8bd51f91efe26d9754
Anne Parker <ap5@sanger.ac.uk>
2008-10-28 15:01:51 +0000

    New module for user url_data records

---
+++ modules/EnsEMBL/Web/Data/Record/URL.pm
@@ -0,0 +1,14 @@
+package EnsEMBL::Web::Data::Record::URL;
+
+use strict;
+use warnings;
+use base qw(EnsEMBL::Web::Data::Record);
+
+__PACKAGE__->_type('url');
+
+__PACKAGE__->add_fields(
+  address   => 'text',
+  name      => 'text',
+);
+
+1;
