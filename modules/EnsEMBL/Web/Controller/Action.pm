4448cda8923b3933e4277da8714ccec43beff6b3
Anne Parker <ap5@sanger.ac.uk>
2008-10-30 16:42:17 +0000

    Added species name to script name, to ensure control panel works correctly

--- modules/EnsEMBL/Web/Controller/Action.pm
+++ modules/EnsEMBL/Web/Controller/Action.pm
@@ -12,6 +12,7 @@ my %URL :ATTR(:set<url> :get<url> :init_arg<url>);
 my %CGI :ATTR(:set<cgi> :get<cgi> );
 my %Controller :ATTR(:set<controller> :get<controller>);
 my %Action :ATTR(:set<action> :get<action>);
+my %Species :ATTR(:set<species> :get<species>);
 my %Id :ATTR(:set<id> :get<id>);
 
 sub BUILD {
@@ -20,7 +21,11 @@ sub BUILD {
   $self->set_cgi(new CGI);
   my ($url, $params) = split(/\?/, $self->get_url);
   my @path_bits = split /\//, $url;
+  my $species = $ENV{'ENSEMBL_SPECIES'};
+  $species = '' if $species !~ /_/;
+
   $self->set_controller($ENV{'ENSEMBL_TYPE'});
+  $self->set_species($species);
   $self->set_action($path_bits[-1]);
 }
 
@@ -31,7 +36,7 @@ sub cgi {
 
 sub script_name {
   my $self = shift;
-  return $self->get_controller . "/" . $self->get_action;
+  return $self->get_species . '/' . $self->get_controller . '/' . $self->get_action;
 }
 
 }
