be0e8baf815de24ec2fac421b22a9652c05ee3fc
Beth Yates <bp1@sanger.ac.uk>
2008-10-30 17:09:11 +0000

    added availability check and tidied up zmenu code

--- modules/EnsEMBL/Web/Configuration/Variation.pm
+++ modules/EnsEMBL/Web/Configuration/Variation.pm
@@ -10,7 +10,15 @@ use base qw(EnsEMBL::Web::Configuration);
 
 sub set_default_action {
   my $self = shift;
-  $self->{_data}{default} = 'Summary';
+  unless( ref $self->object ){
+    $self->{_data}{default} = 'Summary';
+    return;
+  }
+  my $x = $self->object->availability || {};
+  if( $x->{'variation'} ) {
+    $self->{_data}{default} = 'Summary';
+  }
+
 }
 
 sub global_context { return $_[0]->_global_context; }
@@ -24,74 +32,11 @@ sub ajax_zmenu {
   my $self = shift;
   my $panel = $self->_ajax_zmenu;
   my $obj  = $self->object; 
-  my $dest = $obj->action().'/'.$obj->function();
-  my $variation_id = $obj->name;
-  my $location = $obj->var_location;
-  my $status = join ", ", @{$obj->status}; 
-  my ($seq_region, $start, $end) = split(/:|-/, $obj->var_location);
-  my $pos =  $start;
-
-  if($start > $end  ) {
-    $pos = "between&nbsp;$start&nbsp;&amp;&nbsp;$end";
-  }
-  elsif($start < $end ) {
-    $pos = "$start&nbsp;-&nbsp;$end";
+  my $action = $obj->action();
+  if( $action eq 'Variation'){
+    return $self->ajax_zmenu_variation($panel, $obj);
   }
 
-  
-  
-  
-  $panel->{'caption'} = $obj->short_caption;  
-
-  $panel->add_entry({
-    'label'    => 'Variation Properties',
-    'link'     => $obj->_url({'type'=>'Variation', 'action'=>'Summary', 'v'=>$variation_id}),
-    'priority' => 195
-  });
-
-  $panel->add_entry({
-    'type'     => 'bp:',
-    'label'    => $pos,
-    'priority' => 185
-  });
-
-  $panel->add_entry({
-    'type'     => 'status:',
-    'label'    => $status || '-',
-    'priority' => 175
-  });
-
-  $panel->add_entry({
-    'type'     => 'class:',
-    'label'    => $obj->Obj->var_class,
-    'priority' => 175
-  });
-
-  $panel->add_entry({
-    'type'     => 'ambiguity code:',
-    'label'    => $obj->Obj->ambig_code,
-    'priority' => 155
-  });
-
-  $panel->add_entry({
-    'type'     => 'alleles:',
-    'label'    => $obj->alleles,
-    'priority' => 145
-  });
-
-  $panel->add_entry({
-    'type'     => 'source:',
-    'label'    => $obj->Obj->source,
-    'priority' => 135
-  });
-
-  $panel->add_entry({
-    'type'     => 'type:',
-    'label'    => $obj->consequence_type,
-    'priority' => 125
-  });
-
- 
  return;
 }
 
@@ -101,23 +46,23 @@ sub populate_tree {
   $self->create_node( 'Summary', "Summary",
     [qw(summary EnsEMBL::Web::Component::Variation::VariationSummary
         flanking EnsEMBL::Web::Component::Variation::FlankingSequence )],
-    { 'availability' => 1, 'concise' => 'Variation summary' }
+    { 'availability' => 'variation', 'concise' => 'Variation summary' }
   );
   $self->create_node( 'Mappings', "Gene/Transcript",
     [qw(summary EnsEMBL::Web::Component::Variation::Mappings)],
-    { 'availability' => 1, 'concise' => 'Gene/Transcript' }
+    { 'availability' => 'variation', 'concise' => 'Gene/Transcript' }
   );
   $self->create_node( 'Population', "Population genotypes and allele frequencies",
     [qw(summary EnsEMBL::Web::Component::Variation::PopulationGenotypes)],
-    { 'availability' => 1, 'concise' => 'Population genotypes and allele frequencies' }
+    { 'availability' => 'variation', 'concise' => 'Population genotypes and allele frequencies' }
   );
   $self->create_node( 'Individual', "Individual genotypes",
     [qw(summary EnsEMBL::Web::Component::Variation::IndividualGenotypes)],
-    { 'availability' => 1, 'concise' => 'Individual genotypes' }
+    { 'availability' => 'variation', 'concise' => 'Individual genotypes' }
   );
   $self->create_node( 'Context', "Context",
     [qw(summary EnsEMBL::Web::Component::Variation::Context)],
-    { 'availability' => 1, 'concise' => 'Context' }
+    { 'availability' => 'variation', 'concise' => 'Context' }
   );
 
 
