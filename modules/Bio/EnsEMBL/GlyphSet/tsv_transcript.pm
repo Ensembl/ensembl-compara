0e3ba48170bb5d9ff73640d50fe61d6173c65b1b
Beth Yates <bp1@sanger.ac.uk>
2008-10-31 14:30:54 +0000

    fixed zmenu code

--- modules/Bio/EnsEMBL/GlyphSet/tsv_transcript.pm
+++ modules/Bio/EnsEMBL/GlyphSet/tsv_transcript.pm
@@ -139,7 +139,7 @@ sub href {
     my $tid = $transcript->stable_id();
     my $eid =  $exon->stable_id;
     my $href = $self->_url({
-      'type'   => 'Gene',
+      'type'   => 'Transcript',
       'action' => 'Variation_transcript',
       'vt'      => $tid,
       'e'      => $eid,
