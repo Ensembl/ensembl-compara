53523f544ff46ee08d325a9b769eec8b4b213202
Anne Parker <ap5@sanger.ac.uk>
2008-11-03 14:12:19 +0000

    Put some fixes in so component shouldn't fail horribly if no gene info passed

--- modules/EnsEMBL/Web/Component/Gene/FamilyGenes.pm
+++ modules/EnsEMBL/Web/Component/Gene/FamilyGenes.pm
@@ -25,7 +25,7 @@ sub content {
 
     $html .= "<h4>Ensembl genes containing proteins in family $family_id</h4>\n";
     my $families = $object->get_all_families;
-    my $genes = $families->{$family_id}{'info'}{'genes'};
+    my $genes = $families->{$family_id}{'info'}{'genes'} || [];
 
     ## Karyotype (optional)
     if (@{$object->species_defs->ENSEMBL_CHROMOSOMES}) {
@@ -33,6 +33,10 @@ sub content {
       $object->param('aggregate_colour', 'red'); ## Fake CGI param - easiest way to pass this parameter
       my $karyotype = undef;
       my $gene = $object->gene;
+      my $stable_id = '';
+      if ($gene) {
+        $current_gene = $gene->stable_id;
+      }
 
       my $image    = $object->new_karyotype_image();
       $image->cacheable  = 'no';
@@ -47,7 +51,7 @@ sub content {
           my $chr       = $g->slice->seq_region_name;
           my $start     = $g->start;
           my $end       = $g->end;
-          my $colour = $stable_id eq $gene->stable_id ? 'red' : 'blue';
+          my $colour = $stable_id eq $current_gene ? 'red' : 'blue';
           my $point = {
             'start' => $start,
             'end'   => $end,
@@ -70,33 +74,35 @@ sub content {
       $html .= $image->render if $image;
     }
 
-    ## Table of gene info
-    my $table = new EnsEMBL::Web::Document::SpreadSheet( [], [], {'margin' => '1em 0px'} );
-    $table->add_columns( 
-      {'key' => 'id',   'title' => 'Gene ID and Location', 'width' => '30%', 'align' => 'center'},
-      {'key' => 'name', 'title' => 'Gene Name',            'width' => '20%', 'align' => 'center'},
-      {'key' => 'desc', 'title' => 'Description(if known)','width' => '50%', 'align' => 'left'}
-    );
-    foreach my $gene ( sort { $object->seq_region_sort( $a->seq_region_name, $b->seq_region_name ) ||
+    if (@$genes) {
+      ## Table of gene info
+      my $table = new EnsEMBL::Web::Document::SpreadSheet( [], [], {'margin' => '1em 0px'} );
+      $table->add_columns( 
+        {'key' => 'id',   'title' => 'Gene ID and Location', 'width' => '30%', 'align' => 'center'},
+        {'key' => 'name', 'title' => 'Gene Name',            'width' => '20%', 'align' => 'center'},
+        {'key' => 'desc', 'title' => 'Description(if known)','width' => '50%', 'align' => 'left'}
+      );
+      foreach my $gene ( sort { $object->seq_region_sort( $a->seq_region_name, $b->seq_region_name ) ||
                             $a->seq_region_start <=> $b->seq_region_start } @$genes ) {
       
-      my $row = {};
-      $row->{'id'} = sprintf '<a href="/%s/Gene/Summary?g=%s" title="More about this gene">%s</a><br /><a href="/%s/Location/View?r=%s:%s-%s" title="View this location on the genome" class="small" style="text-decoration:none">%s: %s</a>',
+        my $row = {};
+        $row->{'id'} = sprintf '<a href="/%s/Gene/Summary?g=%s" title="More about this gene">%s</a><br /><a href="/%s/Location/View?r=%s:%s-%s" title="View this location on the genome" class="small" style="text-decoration:none">%s: %s</a>',
                 $object->species, $gene->stable_id, $gene->stable_id,
                 $object->species, $gene->slice->seq_region_name, $gene->start, $gene->end,
                 $self->neat_sr_name($object->coord_system, $gene->slice->seq_region_name), 
                 $object->round_bp( $gene->start );
-      my $xref = $gene->display_xref;
-      if( $xref ) {
-        $row->{'name'} = $object->get_ExtURL_link( $xref->display_id, $xref->dbname, $xref->primary_id);
-      } 
-      else {
-        $row->{'name'} = '-novel-';
+        my $xref = $gene->display_xref;
+        if( $xref ) {
+          $row->{'name'} = $object->get_ExtURL_link( $xref->display_id, $xref->dbname, $xref->primary_id);
+        } 
+        else {
+          $row->{'name'} = '-novel-';
+        }
+        $row->{'desc'} = $object->gene_description($gene);
+        $table->add_row($row);
       }
-      $row->{'desc'} = $object->gene_description($gene);
-      $table->add_row($row);
+      $html .= $table->render;
     }
-    $html .= $table->render;
   }
 
   return $html;
