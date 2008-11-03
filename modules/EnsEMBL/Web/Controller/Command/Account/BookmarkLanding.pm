8d5d1cc9e7ed7896342e6a24aceccd844e55d586
Anne Parker <ap5@sanger.ac.uk>
2008-11-03 11:20:55 +0000

    Changed to work better with AJAX control panel

--- modules/EnsEMBL/Web/Controller/Command/Account/BookmarkLanding.pm
+++ modules/EnsEMBL/Web/Controller/Command/Account/BookmarkLanding.pm
@@ -23,12 +23,9 @@ sub process {
   my $mode = $cgi->param('mode') || '';
   my $url = $cgi->param('url') || '';
 
-  ## Don't go to bookmark URL if adding external link or editing/deleting existing bookmark
-  if ($mode ne 'add' || $url !~ /$ENV{'SERVER_NAME'}/) {
-    $url = $self->url('/Account/Bookmarks');
-    if ($cgi->param('_referer')) {
-      $url .= '?_referer='.CGI::escape($cgi->param('_referer'));
-    }
+  $url = $self->url('/Account/Bookmarks');
+  if ($cgi->param('_referer')) {
+    $url .= '?_referer='.CGI::escape($cgi->param('_referer'));
   }
   $self->ajax_redirect($url);
 }
