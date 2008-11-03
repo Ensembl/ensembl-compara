f582130027539af5f07abe18efccf28deae6ffaf
Anne Parker <ap5@sanger.ac.uk>
2008-11-03 09:41:05 +0000

    Updated edit/share/delete links to include species name

--- modules/EnsEMBL/Web/Component/Account.pm
+++ modules/EnsEMBL/Web/Component/Account.pm
@@ -15,42 +15,25 @@ no warnings "uninitialized";
 our @ISA = qw( EnsEMBL::Web::Component);
 
 sub edit_link {
-  my ($self, $module, $id, $text) = @_;
+  my ($self, $dir, $module, $id, $text) = @_;
   $text = 'Edit' if !$text;
-  return sprintf(qq(<a class="modal_link" href="/Account/%s?dataview=edit;id=%s;_referer=%s">%s</a>), 
-          $module, $id, CGI::escape($self->object->param('_referer')), $text);
+  return sprintf(qq(<a class="modal_link" href="%s/Account/%s?dataview=edit;id=%s;_referer=%s">%s</a>), 
+          $dir, $module, $id, CGI::escape($self->object->param('_referer')), $text);
 } 
 
 sub delete_link {
-  my ($self, $module, $id, $text) = @_;
+  my ($self, $dir, $module, $id, $text) = @_;
   $text = 'Delete' if !$text;
-  return sprintf(qq(<a class="modal_link" href="/Account/%s?dataview=delete;id=%s;_referer=%s">%s</a>), 
-          $module, $id, CGI::escape($self->object->param('_referer')), $text);
+  return sprintf(qq(<a class="modal_link" href="%s/Account/%s?dataview=delete;id=%s;_referer=%s">%s</a>), 
+          $dir, $module, $id, CGI::escape($self->object->param('_referer')), $text);
 } 
 
 
 sub share_link {
-  my ($self, $call, $id) = @_;
+  my ($self, $dir, $call, $id) = @_;
   return sprintf(qq(<a class="modal_link" href="/Account/SelectGroup?id=%s;type=%s;_referer=%s">Share</a>), 
-          $id, $call, CGI::escape($self->object->param('_referer')) );
+          $dir, $id, $call, CGI::escape($self->object->param('_referer')) );
 } 
-
-=pod
-sub message {
-  ### Displays a message (e.g. error) from the Controller::Command::Account module
-  my ($panel, $object) = @_;
-  my $command = $panel->{command};
-
-  my $html;
-  if ($command->get_message) {
-    $html = $command->get_message;
-  }
-  else {
-    $html = '<p>'.$command->filters->message.'</p>';
-  }
-  $panel->print($html);
-}
-=cut
 
 sub dedupe {
 ### Removes objects from a list, using a hash of values to filter on and 
