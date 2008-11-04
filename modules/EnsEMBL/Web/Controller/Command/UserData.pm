7d14d3327c8cc8d7abd28caed6b31724449bbf79
Anne Parker <ap5@sanger.ac.uk>
2008-11-04 11:02:26 +0000

    Moved create_objects to parent module

--- modules/EnsEMBL/Web/Controller/Command/UserData.pm
+++ modules/EnsEMBL/Web/Controller/Command/UserData.pm
@@ -5,6 +5,23 @@ use warnings;
 
 use base 'EnsEMBL::Web::Controller::Command';
 
-## Stub - might be needed to contain generic userdata methods at some point
+sub create_object {
+### Helper method - creates a skeleton UserData object in order to talk to the userdata dbs
+  my $self = shift;
+
+  my $db_connection = EnsEMBL::Web::DBSQL::DBConnection->new(undef, $ENSEMBL_WEB_REGISTRY->species_defs);
+  my $core_objects = EnsEMBL::Web::CoreObjects->new( $self->action->cgi, $db_connection );
+  my $factory = EnsEMBL::Web::Proxy::Factory->new(
+    'UserData', {
+      '_input'         => $self->action->cgi,
+      '_core_objects'  => $core_objects,
+      '_databases'     => $db_connection,
+    }
+  );
+  $factory->createObjects;
+  my @objects = @{$factory->DataObjects};
+  return $objects[0];
+}
+
 
 1;
--- modules/EnsEMBL/Web/Controller/Command/UserData/DeleteUpload.pm
+++ modules/EnsEMBL/Web/Controller/Command/UserData/DeleteUpload.pm
@@ -19,31 +19,12 @@ sub process {
   my $cgi = $self->action->cgi;
 
   my $user = $EnsEMBL::Web::RegObj::ENSEMBL_WEB_REGISTRY->get_user;
-  my $object = $self->_create_object;
+  my $object = $self->create_object;
   if ($object) {
     $object->delete_userdata($cgi->param('id'));
   }
   $cgi->redirect($self->url('/UserData/ManageUpload'));
 }
-
-sub _create_object {
-### Helper method - creates a skeleton UserData object in order to talk to the userdata dbs
-  my $self = shift;
-
-  my $db_connection = EnsEMBL::Web::DBSQL::DBConnection->new(undef, $ENSEMBL_WEB_REGISTRY->species_defs);
-  my $core_objects = EnsEMBL::Web::CoreObjects->new( $self->action->cgi, $db_connection );
-  my $factory = EnsEMBL::Web::Proxy::Factory->new(
-    'UserData', {
-      '_input'         => $self->action->cgi,
-      '_core_objects'  => $core_objects,
-      '_databases'     => $db_connection,
-    }
-  );
-  $factory->createObjects;
-  my @objects = @{$factory->DataObjects};
-  return $objects[0];
-}
-
 
 }
 
