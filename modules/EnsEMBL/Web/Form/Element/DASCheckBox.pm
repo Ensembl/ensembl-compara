21fe20a9d187fcb58fe65b6fa9e4b7bf406546c9
Anne Parker <ap5@sanger.ac.uk>
2008-11-05 14:02:29 +0000

    Tweaked rendering to allow access to full text of description on mouseover

--- modules/EnsEMBL/Web/Form/Element/DASCheckBox.pm
+++ modules/EnsEMBL/Web/Form/Element/DASCheckBox.pm
@@ -13,21 +13,51 @@ sub new {
   $params{'name'}       ||= 'dsn';
   $params{'value'}      ||= $params{'das'}->logic_name;
   $params{'label'}      ||= $params{'das'}->label;
-  $params{'notes'}      ||= &_short_das_desc( $params{'das'} );
   my $self = $class->SUPER::new( %params );
   $self->checked = $params{'checked'};
   $self->{'class'} = $params{'long_label'} ? 'checkbox-long' : '';
+  $self->_short_das_desc( $params{'das'} );
   return $self;
 }
 
 sub _short_das_desc {
-  my ( $source ) = @_;
+  my ($self, $source ) = @_;
   my $desc = $source->description;
   if (length $desc > $DAS_DESC_WIDTH) {
+    $self->{'comment'} = $desc;
     $desc = substr $desc, 0, $DAS_DESC_WIDTH;
     $desc =~ s/\s[a-zA-Z0-9]+$/ \.\.\./; # replace final space with " ..."
   }
-  return $desc;
+  $self->{'notes'} = $desc;
 }
+
+sub render {
+  my $self = shift;
+  my $notes = '';
+  if ($self->notes) {
+    $notes = ' <div style="font-weight:normal">'.CGI::escapeHTML($self->notes);
+    $notes .= sprintf(' (<span title="%s">Mouseover&#160;for&#160;full&#160;text</span>)', CGI::escapeHTML($self->comment)) if $self->comment;
+    $notes .= '</div>';
+  }
+  return sprintf(
+    qq(
+  <dl>
+    <dt%s>
+      <label>%s%s</label>
+    </dt>
+    <dd%s>
+      <input type="checkbox" name="%s" id="%s" value="%s" class="input-checkbox" %s/>
+    </dd>
+  </dl>),
+    $self->{'class'} ? ' class="'.$self->{'class'}.'"' : '',
+    $self->{'raw'} ? $self->label : CGI::escapeHTML( $self->label ),
+    $notes,
+    $self->{'class'} ? ' class="'.$self->{'class'}.'"' : '',
+    CGI::escapeHTML( $self->name ),
+    CGI::escapeHTML( $self->id ),
+    $self->value || 'yes', $self->checked ? 'checked="checked" ' : '',
+  );
+}
+
 
 1;
