fc9b3f207d242a4d32631ad70302e4171b07648a
Beth Yates <bp1@sanger.ac.uk>
2008-11-05 16:19:58 +0000

    fixed configuration

--- modules/EnsEMBL/Web/ImageConfig/genesnpview_gene.pm
+++ modules/EnsEMBL/Web/ImageConfig/genesnpview_gene.pm
@@ -8,7 +8,7 @@ sub init {
   my ($self) = @_;
 
   $self->set_parameters({
-    'title'         => 'Transcript slice',
+    'title'         => 'Genes',
     'show_buttons'  => 'yes',   # show +/- buttons
     'button_width'  => 8,       # width of red "+/-" buttons
     'show_labels'   => 'yes',   # show track names on left-hand side
@@ -18,19 +18,21 @@ sub init {
     'opt_halfheight'   => 0,    # glyphs are half-height [ probably removed when this becomes a track config ]
     'opt_empty_tracks' => 0,    # include empty tracks..
   });
-  $self->create_menus(
-    'sequence'        => 'Sequence',
-    'transcript'      => 'Genes',
+  $self->create_menus(      
+    'transcript'      => 'Other Genes',
+    'variation'       => 'Variations',
     'other'           => 'Other'
   );
   $self->load_tracks();
 
+  $self->add_tracks( 'variation',
+    [ 'snp_join',         '',     'snp_join',           { 'display' => 'on',  'strand' => 'b','tag' => 0, 'colours' => $self->species_defs->colour('variation'), 'menu' => 'no'         } ],
+    [ 'geneexon_bgtrack', '',     'geneexon_bgtrack',   { 'display' => 'normal', 'src' => 'all', 'colours' => 'bisque', 'tag' => 0, 'strand' => 'b', 'menu' => 'no'         } ],
+  );
   $self->add_tracks( 'other',
-    [ 'geneexon_bgtrack', '',     'geneexon_bgtrack',  { 'display' => 'normal', 'src' => 'all', 'colours' => 'bisque', 'tag' => 1, 'strand' => 'b', 'menu' => 'no'         } ],
-    [ 'draggable',        '',     'draggable',         { 'display' => 'normal',  'strand' => 'b', 'menu' => 'no'         } ],
-    [ 'snp_join',         '',     'snp_join',          { 'display' => 'on',  'strand' => 'b', 'colours' => $self->species_defs->colour('variation'), 'tag' => 1, 'menu' => 'no'         } ], 
-    [ 'variation_legend',           '',     'variation_legend',            { 'display' => 'on', 'strand' => 'r',          } ],
-    [ 'spacer',           '',     'spacer',            { 'display' => 'normal', 'height' => 50,   'strand' => 'r', 'menu' => 'no'         } ],
+    [ 'scalebar',         '',     'scalebar',           { 'display' => 'normal', 'strand' => 'f', 'name' => 'Scale bar', 'menu' => 'no'  } ],
+    [ 'ruler',            '',     'ruler',              { 'display' => 'normal',  'strand' => 'f','notext' => 1, 'name' => 'Ruler', 'menu' => 'no'  } ],
+    [ 'spacer',           '',     'spacer',             { 'display' => 'normal', 'height' => 50,   'strand' => 'r', 'menu' => 'no'         } ],
   );
 
 
@@ -38,6 +40,15 @@ sub init {
     [qw(transcript )],
     {'display'=>'off'}
   );
+  $self->modify_configs(
+    [qw(variation )],
+    {'menu'=>'no'}
+  );
+  $self->modify_configs(
+    [qw(variation_feature_variation)],
+    {qw(display normal), 'caption' => 'Variations', 'strand' => 'f',}
+  );
+
 
 }
 
