62f30baef933002402fcc5f458f7d877c425c466
Anne Parker <ap5@sanger.ac.uk>
2008-11-10 15:06:49 +0000

    Added counts for user data records

--- modules/EnsEMBL/Web/Object/Account.pm
+++ modules/EnsEMBL/Web/Object/Account.pm
@@ -27,6 +27,9 @@ sub counts {
     $counts->{'bookmarks'}      = $user->bookmarks->count;
     $counts->{'configurations'} = $user->configurations->count;
     $counts->{'annotations'}    = $user->annotations->count;
+    $counts->{'uploads'}        = $user->uploads->count;
+    $counts->{'dases'}          = $user->dases->count;
+    $counts->{'urls'}           = $user->urls->count;
     my @groups = $user->find_nonadmin_groups;
     foreach my $group (@groups) {
       $counts->{'bookmarks'}      += $group->bookmarks->count;
