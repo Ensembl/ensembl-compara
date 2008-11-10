de015679f1bb722b0805976df9cefccc8e0daf06
Anne Parker <ap5@sanger.ac.uk>
2008-11-10 12:16:01 +0000

    Fixes to login form to work properly with AJAX

--- modules/EnsEMBL/Web/Component/Account/Login.pm
+++ modules/EnsEMBL/Web/Component/Account/Login.pm
@@ -22,15 +22,19 @@ sub caption {
 sub content {
   my $self = shift;
 
-  my $referer = CGI::escape($self->object->param('_referer'));
-  my $form = EnsEMBL::Web::Form->new( 'login', "/Account/SetCookie", 'post' );
-  my $reg_url = $self->url("/Account/Register?_referer=$referer");
-  my $pwd_url = $self->url("/Account/LostPassword?_referer=$referer");
+  ## Control panel fixes
+  my $dir = '/'.$ENV{'ENSEMBL_SPECIES'};
+  $dir = '' if $dir !~ /_/;
+  my $referer = '_referer='.$self->object->param('_referer').';x_requested_with=XMLHttpRequest';
+  my $form = EnsEMBL::Web::Form->new( 'login', "$dir/Account/SetCookie", 'post' );
+  my $reg_url = $self->url("$dir/Account/Register?$referer");
+  my $pwd_url = $self->url("$dir/Account/LostPassword?$referer");
 
   $form->add_element('type'  => 'Email',    'name'  => 'email', 'label' => 'Email', 'required' => 'yes');
   $form->add_element('type'  => 'Password', 'name'  => 'password', 'label' => 'Password', 'required' => 'yes');
   $form->add_element('type'  => 'Hidden',   'name'  => 'url', 'value' => $self->object->param('url'));
-  $form->add_element('type'  => 'Hidden',   'name'  => '_referer', 'value' => $referer);
+  $form->add_element('type'  => 'Hidden',   'name'  => '_referer', 'value' => $self->object->param('_referer'));
+  $form->add_element('type'  => 'Hidden',   'name'  => 'x_requested_with', 'value' => 'XMLHttpRequest');
   $form->add_element('type'  => 'Submit',   'name'  => 'submit', 'value' => 'Log in', 'class'=>'cp-refresh');
   $form->add_element('type'  => 'Information',
                      'value' => qq(<p><a href="$reg_url" class="modal_link">Register</a>
