2b85d46c6f79fb7f3cee120a57e057ecc5931f53
Steve Trevanion <st3@sanger.ac.uk>
2008-11-11 22:50:56 +0000

    proper zmenu

--- modules/EnsEMBL/Web/Configuration/Location.pm
+++ modules/EnsEMBL/Web/Configuration/Location.pm
@@ -174,7 +174,7 @@ sub _ajax_zmenu_view {
     return $self->_ajax_zmenu_misc_feature($panel,$obj);
   } elsif ($obj->param('region_n')) {
     return $self->_ajax_zmenu_region($panel,$obj);
-  } elsif ($obj->param('ori')) {
+  } elsif ($obj->param('r1') || $obj->param('ori')) {
     return $self->_ajax_zmenu_synteny($panel,$obj);
   } else {
     my $r = $obj->param('r');
@@ -190,25 +190,72 @@ sub _ajax_zmenu_synteny {
     my $panel= shift;
     my $obj  = shift; 
     my $action     = $obj->[1]{'_action'};
-    my $r   = $obj->param('r');
     my $sp  = $obj->[1]{'_species'};
     my $ori = $obj->param('ori');
+    my $r   = $obj->param('r');
     my ($chr, $loc) = split ':', $r;
-    $panel->{'caption'} = "$chr $loc";
-    my $url = $obj->_url({'type' => 'Location', 'action' => $action, 'r' => $r});
-    $panel->add_entry({
-	'label'    => "Jump to $sp",
-	'link'    => $url,
-	'priority'=> 100,
-    });
-    $panel->add_entry({
-	'label'    => "bp: $loc",
-	'priority'=> 90,
-    });
-    $panel->add_entry({
-	'label'    => "orientation: $ori",
-	'priority'=> 80,
-    });
+    my ($start,$stop) = split '-', $loc;
+    my $url = $obj->_url({'type' => 'Location', 'action' => $action, 'r' => $r });
+    $panel->{'caption'} = "$sp $chr:$loc";
+    if (my $r1  = $obj->param('r1')) {
+	my $sp1 = $obj->param('sp1');
+	$panel->add_entry({
+	    'label'    => sprintf("%s Chr %s:%0.1fM-%0.1fM",$sp,$chr,$start/1e6,$stop/1e6),
+	    'link'    => $url,
+	    'priority'=> 100,
+	});
+	my ($chr1, $loc1) = split ':', $r1;
+	my ($start1,$stop1) = split '-', $loc1;
+	my $url1 = $obj->_url({'type' => 'Location', 'action' => $action, 'r' => $r1, 'species' => $sp1 });
+	$panel->add_entry({
+	    'label'    => sprintf("%s Chr %s:%0.1fM-%0.1fM",$sp1,$chr1,$start1/1e6,$stop1/1e6),
+	    'link'    => $url1,
+	    'priority'=> 90,
+	});
+	if (my $ori = $obj->param('ori')) {
+	    my $synt_url = $obj->_url({'type' => 'Location',
+				   'action' => 'Synteny',
+				   'otherspecies' => $sp1,
+				   'r' => $r});
+	    $panel->add_entry({
+		'label'   => 'Center display on this chr',
+		'link'    => $synt_url,
+		'priority'=> 80,
+	    });
+	    $panel->add_entry({
+		'label'   => "Orientation: $ori",
+		'priority'=> 70,
+	    });
+	}
+	else {
+	    my $synt_url = $obj->_url({'type' => 'Location',
+				   'action' => 'Synteny',
+				   'otherspecies' => $sp1,
+				   'r' => $r});
+	     $panel->add_entry({
+		'label'   => 'Center gene list',
+		'link'    => $synt_url,
+		'priority'=> 80,
+	    });
+	 }
+    }
+    else {
+	my ($chr, $loc) = split ':', $r;
+	my $url = $obj->_url({'type' => 'Location', 'action' => $action, 'r' => $r});
+	$panel->add_entry({
+	    'label'    => "Jump to $sp",
+	    'link'    => $url,
+	    'priority'=> 100,
+	});
+	$panel->add_entry({
+	    'label'    => "bp: $loc",
+	    'priority'=> 90,
+	});
+	$panel->add_entry({
+	    'label'    => "orientation: $ori",
+	    'priority'=> 80,
+	});
+    }
     return;
 }
 
