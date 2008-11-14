ba2ec9792abd6d08a33d255314bcebab7c8c4f55
Andrew Jenkinson <aj5@sanger.ac.uk>
2008-11-14 14:11:52 +0000

    More useful text content

--- modules/EnsEMBL/Web/Component/Gene/ExternalData.pm
+++ modules/EnsEMBL/Web/Component/Gene/ExternalData.pm
@@ -17,7 +17,12 @@ sub caption {
 }
 
 sub content {
-  return "<p>This is the page that will be configured to turn DAS sources on/off</p>";
+  my $self = shift;
+  my $msg1 = 'In the near future this page will display personal annotations '.
+             'that you provide for a gene or transcript. This feature is currently in development.';
+  my $msg2 = "Click 'configure this page' to change the sources of external ".
+             "annotations that are available in the External Data menu.";
+  my $html = $self->_info('Coming soon', $msg1, '100%') . $self->_info('Info', $msg2, '100%');
 }
 
 1;
