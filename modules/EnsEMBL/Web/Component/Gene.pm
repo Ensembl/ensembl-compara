e39d54e1ded641b0b5215290058ec4a7f675dc45
Simon Brent <sb23@sanger.ac.uk>
2008-11-14 11:04:33 +0000

    moved some functions to Component.pm

--- modules/EnsEMBL/Web/Component/Gene.pm
+++ modules/EnsEMBL/Web/Component/Gene.pm
@@ -120,10 +120,21 @@ sub get_sequence_data {
   foreach my $sl (@$slices) {
     my $mk = {};
     my $slice = $sl->{'slice'};
+    my $seq = uc $slice->seq;
     
-    push (@sequence, [ map {{'letter' => $_ }} split (//, uc $slice->seq) ]);
+    push (@sequence, [ map {{ 'letter' => $_ }} split (//, $seq) ]);
     
     $config->{'length'} ||= $slice->length;
+    
+    # Markup inserts on comparisons
+    if ($config->{'align'}) {
+      while ($seq =~  m/(\-+)[\w\s]/gc) {
+        my $ins_length = length $1;
+        my $ins_end = pos ($seq) - 1;
+        
+        $mk->{$ins_end-$_}->{'insert'} = "$ins_length bp" for (1..$ins_length);
+      }
+    }
     
     # Get variations
     if ($config->{'snp_display'}) {
@@ -131,7 +142,7 @@ sub get_sequence_data {
       my $u_snps = {};
     
       eval {
-        $snps = $slice->get_all_VariationFeatures();
+        $snps = $slice->get_all_VariationFeatures;
       };
         
       if (scalar @$snps) {
@@ -140,7 +151,7 @@ sub get_sequence_data {
             next if ($u_slice->seq_region_name eq 'GAP');
             
             if (!$u_slice->adaptor) {
-              my $slice_adaptor = Bio::EnsEMBL::Registry->get_adaptor($sl->{'species'}, $config->{'db'}, 'slice');
+              my $slice_adaptor = Bio::EnsEMBL::Registry->get_adaptor($sl->{'name'}, $config->{'db'}, 'slice');
               $u_slice->adaptor($slice_adaptor);
             }
            
@@ -203,7 +214,7 @@ sub get_sequence_data {
         $mk->{$start}->{'link_text'} = "$snp_start:$alleles";
         $mk->{$start}->{'v'} = $snp->variation_name;
         $mk->{$start}->{'vf'} = $snp->dbID;
-        $mk->{$start}->{'species'} = $sl->{'species'};
+        $mk->{$start}->{'species'} = $sl->{'name'};
       }
     }
     
@@ -315,17 +326,61 @@ sub get_sequence_data {
   return (\@sequence, \@markup);
 }
 
+sub markup_variation {
+  my $self = shift;
+  my ($sequence, $markup, $config) = @_;
+
+  my ($snps, $deletes);
+  my $i = 0;
+
+  my $style = {
+    'snp'     => $config->{'colours'}->{'snp_default'},
+    'snpexon' => $config->{'colours'}->{'snpexon'},
+    'delete'  => $config->{'colours'}->{'snp_gene_delete'}
+  };
+
+  foreach my $data (@$markup) {
+    my $seq = $sequence->[$i];
+    
+    foreach (sort {$a <=> $b} keys %$data) {
+      my $mk = $data->{$_};
+      
+      if ($mk->{'variations'}) {
+        my $ambiguity = ambiguity_code($mk->{'alleles'});
+  
+        $seq->[$_]->{'letter'} = $ambiguity if $ambiguity;
+        $seq->[$_]->{'title'} .= ($seq->[$_]->{'title'} ? '; ' : '') . $mk->{'alleles'} if $config->{'title_display'};
+        
+        $seq->[$_]->{'background-color'} = $style->{$mk->{'variations'}};
+        
+        if ($config->{'snp_display'} eq 'snp_link' && $mk->{'link_text'}) {          
+          $seq->[$_]->{'post'} = qq{ <a href="/$mk->{'species'}/Variation/Summary?v=$mk->{'v'};vf=$mk->{'vf'};vdb=variation">$mk->{'link_text'}</a>;};
+        }
+  
+        $snps = 1 if $mk->{'variations'} eq 'snp';
+        $deletes = 1 if $mk->{'variations'} eq 'delete';
+      }
+    }
+    
+    $i++;
+  }
+
+  $config->{'key'} .= sprintf ($config->{'key_template'}, "background-color:$style->{'snp'};", "Location of SNPs") if ($snps);
+  $config->{'key'} .= sprintf ($config->{'key_template'}, "background-color:$style->{'delete'};", "Location of deletions") if ($deletes);
+}
+
 sub markup_comparisons {
   my $self = shift;
-  my ($seq, $data, $config) = @_;
+  my ($sequence, $markup, $config) = @_;
 
   my $length_species = length $config->{'species'};
   my $max_length = $length_species;
   my $padding = '';
+  my $i = 0;
 
   foreach (@{$config->{'slices'}}) {
     my $slice = $_->{'slice'};
-    my $species = $_->{'species'};
+    my $species = $_->{'name'};
 
     push (@{$config->{'seq_order'}}, $species);
     
@@ -343,6 +398,16 @@ sub markup_comparisons {
   foreach (@{$config->{'seq_order'}}) {
     my $pad = ' ' x ($max_length - length $_);
     $config->{'padded_species'}->{$_} = $_ . $pad;
+  }
+  
+  foreach my $data (@$markup) {
+    my $seq = $sequence->[$i];
+    
+    foreach (sort {$a <=> $b} keys %$data) {
+      $seq->[$_]->{'title'} .= ($seq->[$_]->{'title'} ? '; ' : '') . $data->{$_}->{'insert'} if $config->{'title_display'};
+    }
+    
+    $i++;
   }
   
   $config->{'v_space'} = "\n";
@@ -409,400 +474,6 @@ sub markup_conservation {
       "Location of conserved regions (where >50% of bases in alignments match)"
     );
   }
-}
-
-sub markup_codons {
-  my $self = shift;
-  my ($sequence, $markup, $config) = @_;
-
-  my ($codons, $i);
-
-  foreach my $data (@$markup) {
-    foreach (sort {$a <=> $b} keys %$data) {
-      if ($data->{$_}->{'codons'}) {
-        $sequence->[$i]->[$_]->{'background-color'} = $config->{'colours'}->{'codonutr'};
-        $sequence->[$i]->[$_]->{'title'} .= ($sequence->[$i]->[$_]->{'title'} ? '; ' : '') . $data->{$_}->{'codons'} if $config->{'title_display'};
-      }
-  
-      $codons = 1;
-    }
-    
-    $i++;
-  }
-
-  $config->{'key'} .= sprintf ($config->{'key_template'}, "background-color:$config->{'colours'}->{'codonutr'};", "Location of START/STOP codons") if ($codons);
-}
-
-sub markup_exons {
-  my $self = shift;
-  my ($sequence, $markup, $config) = @_;
-
-  my $exon_types = {};
-  
-  my $style = {
-    other => { 'background-color' => $config->{'colours'}->{'exon_other'} },
-    gene  => { 'color' => $config->{'colours'}->{'exon_gene'}, 'font-weight' => 'bold' },
-    compara_other => { 'color' => $config->{'colours'}->{'exon2'} }
-  };
-
-  my $i = 0;
-  
-  foreach my $data (@$markup) {
-    foreach (sort {$a <=> $b} keys %$data) {
-      if ($data->{$_}->{'exons'}) {
-        $sequence->[$i]->[$_]->{'title'} .= ($sequence->[$i]->[$_]->{'title'} ? '; ' : '') . $data->{$_}->{'exons'} if $config->{'title_display'};
-        
-        foreach my $type (@{$data->{$_}->{'exon_type'}}) {
-          foreach my $s (keys %{$style->{$type}}) {
-            $sequence->[$i]->[$_]->{$s} = $style->{$type}->{$s};
-          }
-  
-          $exon_types->{$type} = 1;
-        }
-      }
-    }
-    
-    $i++;
-  }
-
-  if ($exon_types->{'gene'}) {
-    $config->{'key'} .= sprintf (
-      $config->{'key_template'},
-      join( ';', map {"$_:$style->{'gene'}->{$_}"} keys %{$style->{'gene'}} ), "Location of $config->{'gene_name'} $config->{'gene_exon_type'}" );
-  }
-
-  for my $type ('other', 'compara_other') {
-    if ($exon_types->{$type}) {
-      my $selected = ucfirst $config->{'exon_display'};
-      $selected = $config->{'site_type'} if $selected eq 'Core';
-  
-      $config->{'key'} .= sprintf(
-        $config->{'key_template'},
-        join( ';', map {"$_:$style->{$type}->{$_}"} keys %{$style->{$type}} ), "Location of $selected exons" );
-    }
-  }
-}
-
-sub markup_variation {
-  my $self = shift;
-  my ($sequence, $markup, $config) = @_;
-
-  my ($snps, $deletes);
-  my $i = 0;
-
-  my $style = {
-    'snp'     => $config->{'colours'}->{'snp_default'},
-    'snpexon' => $config->{'colours'}->{'snpexon'},
-    'delete'  => $config->{'colours'}->{'snp_gene_delete'}
-  };
-
-  foreach my $data (@$markup) {
-    foreach (sort {$a <=> $b} keys %$data) {
-      my $mk = $data->{$_};
-      my $seq = $sequence->[$i];
-      
-      if ($mk->{'variations'}) {
-        my $ambiguity = ambiguity_code($mk->{'alleles'});
-  
-        $seq->[$_]->{'letter'} = $ambiguity if $ambiguity;
-        $seq->[$_]->{'title'} .= ($seq->[$_]->{'title'} ? '; ' : '') . $mk->{'alleles'} if $config->{'title_display'};
-        
-        $seq->[$_]->{'background-color'} = $style->{$mk->{'variations'}};
-        
-        if ($config->{'snp_display'} eq 'snp_link' && $mk->{'link_text'}) {          
-          $seq->[$_]->{'post'} = qq{ <a href="/$mk->{'species'}/Variation/Summary?v=$mk->{'v'};vf=$mk->{'vf'};vdb=variation">$mk->{'link_text'}</a>;};
-        }
-  
-        $snps = 1 if $mk->{'variations'} eq 'snp';
-        $deletes = 1 if $mk->{'variations'} eq 'delete';
-      }
-    }
-    
-    $i++;
-  }
-
-  $config->{'key'} .= sprintf ($config->{'key_template'}, "background-color:$style->{'snp'};", "Location of SNPs") if ($snps);
-  $config->{'key'} .= sprintf ($config->{'key_template'}, "background-color:$style->{'delete'};", "Location of deletions") if ($deletes);
-}
-
-sub markup_line_numbers {
-  my $self = shift;
-  my ($sequence, $config) = @_;
- 
-  # Incremented when species changes 
-  my $sp = 0;
-
-  # If we only have only one species, $config->{'seq_order'} won't exist yet (it's created in markup_comparisons)
-  $config->{'seq_order'} = [ $config->{'species'} ] unless $config->{'seq_order'};
- 
-  foreach my $sl (@{$config->{'slices'}}) {
-    my $slice = $sl->{'slice'};
-    my $species = $sl->{'species'};
-    
-    my @numbering;
-    my $align_slice = 0;
-
-    # DANGER, WILL ROBINSON! This assumes that the order of the slices is the same as the order
-    # of the species when displayed on the page. Which it is...for now.
-    $sp++ if $species ne $config->{'seq_order'}->[$sp];
-
-    my $species_seq = $sequence->[$sp];
-    
-    if ($config->{'line_numbering'} eq 'slice') {
-      my $start_pos = 0;
-      
-      if ($slice->isa('Bio::EnsEMBL::Compara::AlignSlice::Slice')) {
-       $align_slice = 1;
-      
-        # Get the data for all underlying slices
-        foreach (@{$sl->{'underlying_slices'}}) {
-          my $ostrand = $_->strand;
-          
-          if ($_->seq_region_name ne 'GAP') {
-            push (@numbering, {
-              dir => $ostrand,
-              start_pos => $start_pos,
-              start => $ostrand > 0 ? $_->start : $_->end,
-              end => $ostrand > 0 ? $_->end : $_->start,
-              chromosome => $_->seq_region_name . ':'
-            });
-            
-            $config->{'padding'}->{'pre_number'} = length $_->seq_region_name if length $_->seq_region_name > $config->{'padding'}->{'pre_number'};
-          }
-          
-          $start_pos += length $_->seq;
-        }
-      } else {
-        # Get the data for the slice
-        my $ostrand = $slice->strand;
-        
-        @numbering = ({ 
-          dir => $ostrand,  
-          start => $ostrand > 0 ? $slice->start : $slice->end,
-          end => $ostrand > 0 ? $slice->end : $slice->start,
-          chromosome => $slice->seq_region_name . ':'
-        });
-      }
-    } else {
-      # Line numbers are relative to the sequence (start at 1)
-      @numbering = ({ 
-        dir => 1,  
-        start => 1,
-        end => $config->{'length'},
-        chromosome => ''
-      });
-    }
-    
-    my $data = shift @numbering;
-    
-    my $s = 0;
-    my $e = $config->{'wrap'} - 1;
-    
-    my $row_start = $data->{'start'};
-    my ($start, $end);
-    
-    # One line longer than the sequence so we get the last line's numbers generated in the loop
-    my $loop_end = scalar @{$sequence->[0]} + $config->{'wrap'};
-    
-    while ($e < $loop_end) {
-      $start = '';
-      $end = '';
-      
-      # Comparison species
-      if ($align_slice) {
-        my $seq_length;
-        my $segment;
-        
-        # Build a segment containing the current line of sequence
-        for ($s..$e) {
-          # Check the array element exists - must be done so we don't create new elements and mess up the padding at the end of the last line
-          if ($species_seq->[$_]) {
-            $seq_length++ if $species_seq->[$_]->{'letter'} ne '.';
-            $segment .= $species_seq->[$_]->{'letter'};
-          }
-        }
-    
-        my $first_bp_pos = 0;
-        my $last_bp_pos = 0;
-
-        while ($segment =~ /\w/g) {
-          $last_bp_pos = pos $segment;
-          $first_bp_pos ||= $last_bp_pos; # Set the first position on the first match only
-        }
-        
-        # Get the data from the next slice if we have passed the end of the current one
-        if (scalar @numbering && $e >= $numbering[0]->{'start_pos'}) {
-          $data = shift @numbering;
-          
-          # Only set $row_start if the line begins with a .
-          # If it does not, the previous slice ends mid-line, so we just carry on with it's start number
-          $row_start = $data->{'start'} if $segment =~ /^\./;
-        }
-        
-        if ($seq_length && $last_bp_pos) {
-          # This is NOT necessarily the same as $end + $data->{'dir'}, as bits of sequence could be hidden
-          (undef, $row_start) = $slice->get_original_seq_region_position($s + $first_bp_pos);
-
-          $start = $row_start;
-
-          # For AlignSlice display the position of the last meaningful bp
-          (undef, $end) = $slice->get_original_seq_region_position($e + 1 + $last_bp_pos - $config->{'wrap'});
-        }
-
-        $s = $e + 1;
-      } else { # Single species
-        $end = $e < scalar @{$sequence->[0]} ? $row_start + ($data->{'dir'} * $config->{'wrap'}) - $data->{'dir'} : $data->{'end'};
-        
-        $start = $row_start;
-
-        # Next line starts at current end + 1 for forward strand, or - 1 for reverse strand
-        $row_start = $end + $data->{'dir'} if $end;
-      }
-      
-      my $ch = $start ? ($config->{'comparison'} && $data->{'chromosome'}) : '';   
-
-      push (@{$config->{'line_numbers'}->{$sp}}, { start => $start, end => $end, pre => $ch });
-
-      # Increase padding amount if required
-      $config->{'padding'}->{'number'} = length $start if length $start > $config->{'padding'}->{'number'};
-      
-      $e += $config->{'wrap'};
-    }
-  }
- 
-  $config->{'padding'}->{'pre_number'}++ if $config->{'padding'}->{'pre_number'}; # Compensate for the : for blank lines
- 
-  if ($config->{'line_numbering'} eq 'slice' && $config->{'align'}) {
-    $config->{'key'} .= qq{ NOTE: For secondary species we display the coordinates of the first and the last mapped (i.e A,T,G,C or N) basepairs of each line};
-  }
-}
-
-sub build_sequence {
-  my $self = shift;
-  my ($sequence, $config) = @_;
-  
-  my $line_numbers = $config->{'line_numbers'};
-  my $html; 
-  my @output;
-  my $s = 0;
-  
-  foreach my $lines (@$sequence) {
-    my ($row, $title, $previous_title, $new_line_title, $style, $previous_style, $new_line_style, $pre, $post);
-    my ($count, $i);
-    
-    foreach my $seq (@$lines) {
-      $previous_title = $title;
-      $title = $seq->{'title'} ? qq(title="$seq->{'title'}") : '';
-      
-      my $new_style = '';
-      $previous_style = $style;
-  
-      if ($seq->{'background-color'}) {
-        $new_style .= "background-color:$seq->{'background-color'};";
-      } elsif ($style =~ /background-color/) {
-        $new_style .= "background-color:auto;";
-      }
-  
-      if ($seq->{'color'}) {
-        $new_style .= "color:$seq->{'color'};";
-      } elsif ($style =~ /(?<!background-)color:/) {
-        $new_style .= "color:auto;";
-      }
-  
-      if ($seq->{'font-weight'}) {
-        $new_style .= "font-weight:$seq->{'font-weight'};";
-      }
-  
-      $style = qq(style="$new_style") if ($new_style);
-  
-      $post .= $seq->{'post'};
-  
-      if ($i == 0) {
-        $row .= "<span $style $title>";
-      } elsif ($style ne $previous_style || $title ne $previous_title) {
-        $row .= "</span><span $style $title>";
-      }
-  
-      $row .= $seq->{'letter'};
-  
-      $count++;
-      $i++;
-  
-      if ($count == $config->{'wrap'} || $i == scalar @$lines) {        
-        if ($i == $config->{'wrap'}) {
-          $row = "$row</span>";
-        } else {
-          $row = "<span $new_line_style $new_line_title>$row</span>";
-        }
-        
-        if ($config->{'comparison'}) {
-          if (scalar keys %{$config->{'padded_species'}}) {
-            $pre = $config->{'padded_species'}->{$config->{'seq_order'}->[$s]} || $config->{'species'};
-          } else {
-            $pre = $config->{'species'};
-          }
-
-          $pre .= '  ';
-        }
-         
-        push (@{$output[$s]}, { line => $row, length => $count, pre => $pre, post => $post });
-  
-        $new_line_style = $style || $previous_style;
-        $new_line_title = $title || $previous_title;
-        $count = 0;
-        $row = '';
-        $pre = '';
-        $post = '';
-      }
-    }
-    
-    $s++;
-  }
-
-  my $length = scalar @{$output[0]} - 1;
-
-  for my $x (0..$length) {
-    my $y = 0;
-    
-    foreach (@output) {
-      my $line = $_->[$x]->{'line'};
-      my $num = shift @{$line_numbers->{$y}};
-      
-      if ($config->{'number'}) {
-        my $pad1 = ' ' x ($config->{'padding'}->{'pre_number'} - length $num->{'pre'});
-        my $pad2 = ' ' x ($config->{'padding'}->{'number'} - length $num->{'start'});
-
-        $line = $config->{'h_space'} . sprintf("%6s ", "$pad1$num->{'pre'}$pad2$num->{'start'}") . $line;
-      }
-      
-      if ($x == $length && ($config->{'end_number'} || $_->[$x]->{'post'})) {
-        $line .= ' ' x ($config->{'wrap'} - $_->[$x]->{'length'});
-      }
-      
-      if ($config->{'end_number'}) {
-        my $pad1 = ' ' x ($config->{'padding'}->{'pre_number'} - length $num->{'pre'});
-        my $pad2 = ' ' x ($config->{'padding'}->{'number'} - length $num->{'end'});
-
-        $line .= $config->{'h_space'} . sprintf(" %6s", "$pad1$num->{'pre'}$pad2$num->{'end'}");
-      }
-      
-      $line = "$_->[$x]->{'pre'}$line" if $_->[$x]->{'pre'};
-      $line .= $_->[$x]->{'post'} if $_->[$x]->{'post'};      
-      
-      $html .= "$line\n";
-      $y++;
-    }
-    
-    $html .= $config->{'v_space'};
-  }
-  
-  $config->{'html_template'} ||= qq{<pre>%s</pre>};
-  
-  # Can't use sprintf because it throws the error 'Argument isn't numeric' for compara alignments when 
-  # conservation is turned on, even though IT'S NOT MEANT TO BE NUMERIC. Stupid sprintf.
-  $config->{'html_template'} =~ s/%s/$html/;
-
-  return $config->{'html_template'};
 }
 
 1;
