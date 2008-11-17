263c0612c67f69ed2dd457963d3836156290256a
Beth Yates <bp1@sanger.ac.uk>
2008-11-17 16:41:56 +0000

    changed to work with new 51 code

--- modules/EnsEMBL/Web/ImageConfig/ldview.pm
+++ modules/EnsEMBL/Web/ImageConfig/ldview.pm
@@ -5,106 +5,55 @@ use vars qw(@ISA);
 @ISA = qw(EnsEMBL::Web::ImageConfig);
 
 sub init {
-  my ($self) = @_;
-  $self->{'_userdatatype_ID'} = 30;
-  $self->{'_transcript_names_'} = 'yes';
-  #$self->{'_no_label'} = 'true';
-  $self->{'general'}->{'ldview'} = {
-    '_artefacts' => [qw( 
-			scalebar
-			ruler
-			variation
-			genotyped_variation
-			variation_legend
+  my $self = shift; 
 
-                    )],
-    '_options'  => [qw(pos col known unknown)],
-    '_settings' => { 
+  $self->set_parameters({
+    '_userdatatype_ID'    =>  30,
+    '_transcript_names_'  =>  'yes',
+    'title'         => 'LD slice',
+    'show_buttons'  => 'no',    # show +/- buttons
+    'button_width'  => 8,       # width of red "+/-" buttons
+    'show_labels'   => 'yes',   # show track names on left-hand side
+    'label_width'   => 100,     # width of labels on left-hand side
+    'margin'        => 5,       # margin
+    'spacing'       => 2,       # spacing
+    'image_width'   => 800,
+    'context'       => 20000,
+  });
 
-       'zoom_gifs'     => {
-       'zoom1'   =>  1000,   zoom2   =>  5000,  
-       'zoom3'   =>  10000,  zoom4   =>  20000,
-       'zoom5'   =>  50000,  zoom6   =>  100000 
-      },
-      'navigation_options' => [ '500k', '200k', '100k', 
-				'window', 'half', 'zoom' ],
+  $self->create_menus(
+    'transcript'      => 'Other Genes',
+    'prediction'      => 'Prediction transcripts',
+    'other'           => 'Other',
+    'variation'       => 'Variations',
+    'legends'         => 'Legends',
+  );
 
-     'features' => [
-                     [ 'variation'             => "SNPs"          ],
-                     [ 'variation_legend'      => "SNP legend"    ],
-                     [ 'genotyped_variation'   => "Genotyped SNPs"],
-                    ],
-      'options' => [
-                 [ 'opt_empty_tracks' => 'Show empty tracks' ],
-                 [ 'opt_zmenus'      => 'Show popup menus'  ],
-                 [ 'opt_zclick'      => '... popup on click'  ],
-                   ],
-       'snphelp' => [
-        [ 'ldview'  => 'LDView' ],
-      ],
-      'opt_empty_tracks' => 1,
-      'opt_zmenus'     => 1,
-      'opt_zclick'     => 1,
-      'show_labels'      => 'yes',
-      'width'     => 800,
-      'bgcolor'   => 'background1',
-      'bgcolour1' => 'background3',
-      'bgcolour2' => 'background1',
-     },
-    'ruler' => {
-      'on'          => "on",
-      'pos'         => '9999',
-      'col'         => 'black',
-     'str'         => 'f',
-    },
+  $self->load_tracks();
 
-    'scalebar' => {
-      'on'          => "on",
-      'nav'         => "off",
-      'pos'         => '4500',
-      'col'         => 'black',
-      'str'         => 'r',
-      'abbrev'      => 'on',
-      'navigation'  => 'off'
-    },
+  $self->modify_configs(
+    [qw(variation_feature_genotyped_variation)],
+    {qw(display normal), 'strand' => 'r', 'style' => 'box', 'depth' => 10000 }
+  );
 
-    'genotyped_variation' => {
-      'on'          => "on",
-      'pos'         => '4504',
-      'str'         => 'r',
-      'dep'         => '10000',
-      'col'         => 'blue',
-      'track_height'=> 7,
-      'compact'     => 0,
-      'track_label' => "Genotyped SNPs",
-      'hi'          => 'black',
-      'colours' => {$self->{'_colourmap'}->colourSet('variation')},
-      'available'=> 'databases DATABASE_VARIATION',
-    },
+  $self->add_tracks( 'other',
+    [ 'ruler',                  '',     'ruler',      { 'display' => 'normal',  'strand' => 'f', 'name' => 'Ruler'  } ],
+    [ 'scalebar',               '',     'scalebar',   { 'display' => 'normal', 'strand' => 'r', 'name' => 'Scale bar' } ],
+  );
 
-   'variation' => {
-      'on'          => "on",
-      'pos'         => '4509',
-      'str'         => 'r',
-      'dep'         => '0.1',
-      'col'         => 'blue',
-      'track_label' => "Variations",
-      'track_height'=> 7,
-      'hi'          => 'black',
-      'colours' => {$self->{'_colourmap'}->colourSet('variation')},
-      'available'=> 'databases DATABASE_VARIATION', 
-    },
+  $self->add_tracks( 'legends',
+    [ 'variation_legend',                  '',     'variation_legend',      { 'variation_legend' => 'on',  'strand' => 'r', 'caption' => 'Variation legend'  } ],
+  );
 
+  $self->modify_configs(
+    [qw(transcript_core_ensembl)],
+    {qw(display normal)}
+  );
+  $self->modify_configs(
+    [qw(variation_feature_variation)],
+    {qw(display normal), 'caption' => 'Variations', 'strand' => 'r',}
+  );
 
-    'variation_legend' => {
-      'on'          => "on",
-      'str'         => 'r',
-      'pos'         => '4566',
-    },
-  };
-  $self->ADD_ALL_TRANSCRIPTS(2000, "compact" =>'1');  #first is position
 }
-
-
-
 1;
+
