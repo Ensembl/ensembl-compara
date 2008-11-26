ab9e3eed5c25ff7927bf1c1800a957ab9411565e
Kathryn Beal <kbeal@ebi.ac.uk>
2008-11-26 12:39:41 +0000

    Updated for release 51

--- scripts/examples/getConservationScores.pl
+++ scripts/examples/getConservationScores.pl
@@ -6,7 +6,7 @@ use Bio::EnsEMBL::Utils::Exception qw(throw);
 
 #
 # Simple example to show how to get conservation scores for a slice.
-# Works for ensembl release 44.
+# Works for ensembl release 51
 #
 
 my $reg = "Bio::EnsEMBL::Registry";
@@ -14,16 +14,40 @@ my $species = "Homo sapiens";
 my $seq_region = "17";
 my $seq_region_start = 32020001;
 my $seq_region_end =   32020500;
+my $version = 51;
 
 $reg->load_registry_from_db(
       -host => "ensembldb.ensembl.org",
-      -user => "anonymous");
+      -user => "anonymous",
+      -db_version => $version);
 
 #get method_link_species_set adaptor
 my $mlss_adaptor = $reg->get_adaptor("Multi", "compara", "MethodLinkSpeciesSet");
 
-#get the method_link_species_set object for GERP_CONSERVATION_SCORE for 10 species
-my $mlss = $mlss_adaptor->fetch_by_method_link_type_registry_aliases("GERP_CONSERVATION_SCORE", ["human", "chimp", "rhesus", "cow", "dog", "mouse", "rat", "opossum", "platypus", "chicken"]);
+my $mlss; 
+
+#get the method_link_species_set object for GERP_CONSERVATION_SCORE for 21 species
+#$mlss = $mlss_adaptor->fetch_by_method_link_type_registry_aliases("GERP_CONSERVATION_SCORE", ["human", "chimp", "rhesus", "Pongo pygmaeus", "cow", "dog", "mouse", "rat", "horse", "Echinops telfairi", "Oryctolagus cuniculus", "Dasypus novemcinctus", "Tupaia belangeri", "Erinaceus europaeus", "Otolemur garnettii", "Spermophilus tridecemlineatus", "Myotis lucifugus", "Sorex araneus", "Microcebus murinus", "Felis catus", "Ochotona princeps"]);
+
+#A quicker way is to define the number of species in the alignment and
+#get all the method_link_species_sets with type "GERP_CONSERVATION_SCORE" and
+#select the one with the required number of species. This of course assumes 
+#that there are not two different method_link_species_sets with the same
+#number of species in which case you would have to use the method above and
+#declare each species individually.
+
+my $num_species = 21;
+
+my $all_mlss =  $mlss_adaptor->fetch_all_by_method_link_type("GERP_CONSERVATION_SCORE");
+foreach my $this_mlss (@$all_mlss) {
+    #print "number of species " . @{$this_mlss->species_set} . "\n";
+    if (@{$this_mlss->species_set} == $num_species) {
+	$mlss = $this_mlss;
+	last;
+    }
+}
+
+throw("Unable to find method_link_species_set") if (!defined($mlss));
 
 #get slice adaptor for $species
 my $slice_adaptor = $reg->get_adaptor($species, 'core', 'Slice');
