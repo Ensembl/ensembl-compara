d7f5db8422d13c9258a1aa67f04438bb40ab6bbb
Web Admin <w3adm@sanger.ac.uk>
2008-12-02 13:39:05 +0000

    fixed tool links

--- modules/EnsEMBL/Web/Document/HTML/ToolLinks.pm
+++ modules/EnsEMBL/Web/Document/HTML/ToolLinks.pm
@@ -11,6 +11,8 @@ our @ISA = qw(EnsEMBL::Web::Document::HTML);
 sub new { return shift->SUPER::new( 'logins' => '?' ); }
 
 sub logins    :lvalue { $_[0]{'logins'};   }
+sub blast     :lvalue { $_[0]{'blast'};   }
+sub biomart   :lvalue { $_[0]{'biomart'};   }
 sub referer   :lvalue { $_[0]{'referer'};   } ## Needed by CloseCP
 
 sub render   {
@@ -21,21 +23,21 @@ sub render   {
   my $html;
   
   if( $self->logins ) {
-    if ($ENV{'ENSEMBL_USER_ID'}) {
+    if( $ENV{'ENSEMBL_USER_ID'} ) {
       $html .= qq(
       <a style="display:none" href="$dir/Account/Links?_referer=$url" class="modal_link">Account</a> &nbsp;|&nbsp;
-      <a href="$dir/Account/Logout?_referer=$url">Logout</a> &nbsp;|&nbsp;);
-    }
-    else {
+      <a href="$dir/Account/Logout?_referer=$url">Logout</a> &nbsp;|&nbsp;
+      );
+    } else {
       $html .= qq(
       <a style="display:none" href="$dir/Account/Login?_referer=$url" class="modal_link">Login</a> / 
-      <a style="display:none" href="$dir/Account/Register?_referer=$url" class="modal_link">Register</a> &nbsp;|&nbsp;);
+      <a style="display:none" href="$dir/Account/Register?_referer=$url" class="modal_link">Register</a> &nbsp;|&nbsp;
+      );
     }
   }
-  $html .= qq(
-      <a href="/Multi/blastview">BLAST/BLAT</a> &nbsp;|&nbsp; 
-      <a href="/biomart/martview">BioMart</a> &nbsp;|&nbsp;
-      <a href="/info/website/help/" id="help">Docs &amp; FAQs</a>);
+  $html .= qq(<a href="/Multi/blastview">BLAST/BLAT</a> &nbsp;|&nbsp;) if $self->blast;
+  $html .= qq(<a href="/biomart/martview">BioMart</a> &nbsp;|&nbsp;)   if $self->biomart;
+  $html .= qq(<a href="/info/website/help/" id="help">Docs &amp; FAQs</a>);
 
   $self->print($html);
 }
