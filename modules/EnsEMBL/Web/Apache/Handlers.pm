a11049af458e102385c9f37daf317d9d23ddd661
Web Admin <w3adm@sanger.ac.uk>
2008-12-02 13:49:32 +0000

    fix to strip out extra stuff

--- modules/EnsEMBL/Web/Apache/Handlers.pm
+++ modules/EnsEMBL/Web/Apache/Handlers.pm
@@ -440,14 +440,14 @@ sub cleanURI {
   
   ## Clean out the uri
   my $uri = $ENV{'REQUEST_URI'};
-  if ($uri =~ s/time=\d+\.\d+;?//g) {
+  if ($uri =~ s/time=\d+\.\d+;?//g + $uri =~ s!([^:])/{2,}!$1/!g) {
     $r->parse_uri($uri);
     $r->subprocess_env('REQUEST_URI' => $uri);
   }
 
   ## Clean out the referrer
   my $referer = $ENV{'HTTP_REFERER'};
-  if ($referer =~ s/time=\d+\.\d+;?//g) {
+  if ($referer =~ s/time=\d+\.\d+;?//g + $referer =~ s!([^:])/{2,}!$1/!g) {
     $r->subprocess_env('HTTP_REFERER' => $referer);
   }
 
