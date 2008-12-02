1fe0c891d751d7777400a795e7c4b449b4fb3f39
Web Admin <w3adm@sanger.ac.uk>
2008-12-02 13:35:39 +0000

    use shared colour config for ideograms

--- modules/Bio/EnsEMBL/GlyphSet/chr_band.pm
+++ modules/Bio/EnsEMBL/GlyphSet/chr_band.pm
@@ -1,8 +1,10 @@
 package Bio::EnsEMBL::GlyphSet::chr_band;
+
 use strict;
-use vars qw(@ISA);
-use Bio::EnsEMBL::GlyphSet;
-@ISA = qw(Bio::EnsEMBL::GlyphSet);
+use warnings;
+no warnings 'uninitialized';
+
+use base qw(Bio::EnsEMBL::GlyphSet);
 
 my %SHORT = qw(
   'chromosome'  "Chr."
@@ -13,7 +15,6 @@ sub _init {
   my ($self) = @_;
 
   ########## only draw contigs once - on one strand
-  return unless ($self->strand() == 1);
 
   my $col    = undef;
   my $config = $self->{'config'};
@@ -22,21 +23,8 @@ sub _init {
  
   my $no_sequence = $self->{'config'}->species_defs->NO_SEQUENCE;
 
-  my %COL = (
-    'gpos100' => [ 'black',     'white' ],
-    'tip'     => [ 'slategrey'. 'white' ],
-    'gpos75'  => [ 'grey40',    'white' ],
-    'gpos66'  => [ 'grey50',    'white' ],
-    'gpos50'  => [ 'grey60',    'black' ],
-    'gpos33'  => [ 'grey75',    'black' ],
-    'gpos25'  => [ 'grey85',    'black' ],
-    'gpos'    => [ 'black'.     'white' ],
-    'gvar'    => [ 'grey88',    'black' ],
-    'gneg'    => [ 'white',     'black' ],
-    'acen'    => [ 'slategrey'. 'white' ],
-    'stalk'   => [ 'slategrey'. 'white' ]
-  );
-    
+  my @t_stains = qw(gpos25 gpos75);
+  
   my $im_width = $self->{'config'}->image_width();
    
   my $prev_end = 0;
@@ -49,8 +37,10 @@ sub _init {
   my $h = $res[3];
   my $pix_per_bp = $self->{'config'}->transform()->{'scalex'};
   
-  my $chr = @$bands ? $bands[0]->slice()->seq_region_name() : '';
-  foreach my $band (reverse @$bands){
+  my @t_colour = qw(gpos25 gpos75);
+  my $chr = @$bands ? $bands->[0]->slice()->seq_region_name() : '';
+  my @bands =  sort{$a->start <=> $b->start } @$bands;
+  foreach my $band (@bands) {
     my $bandname = $band->name();
        $bandname =~ /(\d+)\w?/;
     my $band_no = $1;
@@ -65,20 +55,26 @@ sub _init {
 
     my $min_start = $vc_band_start if(!defined $min_start || $min_start > $vc_band_start); 
     my $max_end   = $vc_band_end   if(!defined $max_end   || $max_end   < $vc_band_end); 
+
+    my $col = $self->my_colour( $stain );
+    unless( $col ) {
+      $col = shift @t_colour;
+      push @t_colour, ($col = shift @t_colour);
+    } 
     $self->push($self->Rect({
       'x'      => $vc_band_start -1 ,
       'y'      => 0,
       'width'  => $vc_band_end - $vc_band_start +1 ,
       'height' => $h + 4,
-      'colour' => $self->my_colour( $stain ) ||'white',
+      'colour' => $self->my_colour( $stain ) || 'white',
       'absolutey' => 1,
-    });
+    }));
     
-    my $fontcolour = $self->my_colour($stain,'label');
+    my $fontcolour = $self->my_colour( $stain,'label' ) || 'black';
     if( $fontcolour ne 'invisible' ) {
       my @res = $self->get_text_width( ($vc_band_end-$vc_band_start)*$pix_per_bp, $bandname, '', 'font'=>$fontname, 'ptsize' => $fontsize );
     # only add the lable if the box is big enough to hold it...
-      if( $res[0] &) {
+      if( $res[0] ) {
         $self->push($self->Text({
           'x'      => int(($vc_band_end + $vc_band_start-$res[2]/$pix_per_bp)/2),
           'y'      => 1,
@@ -90,7 +86,7 @@ sub _init {
           'colour' => $fontcolour,
           'text'   => $res[0],
           'absolutey'  => 1,
-        });
+        }));
       }
     }
     my $vc_adjust  = 1 - $self->{'container'}->start ;
@@ -103,8 +99,8 @@ sub _init {
       'height'        => $h + 4 ,
       'bordercolour'  => $black,
       'absolutey'     => 1,
-      'title'         => "Band: $bandname"
-      'href'          => $this->_url({'r'=>"$chr:$band_start-$band_end"})
+      'title'         => "Band: $bandname",
+      'href'          => $self->_url({'r'=>"$chr:$band_start-$band_end"})
     }));
   }
 }
