4df0e929a4e53b0e1f66ea45847b77d903018d0f
Anne Parker <ap5@sanger.ac.uk>
2008-12-08 09:51:21 +0000

    Fixed to be non-popup - and to set email address correctly!

--- modules/EnsEMBL/Web/Controller/Command/Account/SendActivation.pm
+++ modules/EnsEMBL/Web/Controller/Command/Account/SendActivation.pm
@@ -22,27 +22,21 @@ sub process {
   my $self = shift;
   my $cgi = $self->action->cgi;
 
+  my $mailer = EnsEMBL::Web::Mailer::User->new;
   my $user = EnsEMBL::Web::Data::User->find(email => $cgi->param('email'));
-  if ($cgi->param('record_id')) { ## Invited user
-      $self->ajax_redirect(
-		     $self->url('/Account/Activate', 
-				{'email' => $user->email, 'code' => $user->salt, 'url' => '/Account/Details;record_id='.CGI::escape($cgi->param('record_id'))}));
-  } 
-  else {
-    if ($user && $user->email) {
-      my $mailer = EnsEMBL::Web::Mailer::User->new;
-      $mailer->set_to($user->email);
-      $mailer->send_activation_email(
-          user      => $user,
-          lost      => $cgi->param('lost') || '',
-          group_id  => $cgi->param('group_id') || '',
-      );
-      $self->set_message(qq(<p>An email has been sent for each account associated with this address and should arrive shortly.</p><p>If you do not receive a message from us within a few hours, please check any spam filters on your email account, and <a href="mailto:helpdesk\@ensembl.org">contact Helpdesk</a> if you still cannot find the message.</p>));
-    }
-    ## Force this to use AJAX
-    $cgi->param('x_requested_with', 'XMLHttpRequest');
-    $self->render_message;
-  }
+
+  $mailer->set_to($cgi->param('email'));
+  $mailer->send_activation_email(
+      user      => $user,
+      lost      => $cgi->param('lost') || '',
+      group_id  => $cgi->param('group_id') || '',
+      record_id => $cgi->param('record_id') || '',
+  );
+  $self->set_message(qq(<p>An email has been sent for each account associated with this address and should arrive shortly.</p><p>If you do not receive a message from us within a few hours, please check any spam filters on your email account, and <a href="mailto:helpdesk\@ensembl.org">contact Helpdesk</a> if you still cannot find the message.</p>));
+  ## Force this to use AJAX
+  #$cgi->param('x_requested_with', 'XMLHttpRequest');
+  $cgi->param('no_popup', 1);
+  $self->render_message;
 }
 
 }
