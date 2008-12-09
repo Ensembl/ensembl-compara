5827c8800ec488b5d93aa51a93eaf4a571a6c24b
Web Admin <w3adm@sanger.ac.uk>
2008-12-09 12:04:13 +0000

    removed bug in bzip

--- modules/EnsEMBL/Web/CompressionSupport.pm
+++ modules/EnsEMBL/Web/CompressionSupport.pm
@@ -3,7 +3,7 @@ package EnsEMBL::Web::CompressionSupport;
 use strict;
 use Compress::Zlib;
 use Compress::Bzip2;
-use IO::Uncompress::Bunzip2 qw(bunzip2);
+use IO::Uncompress::Bunzip2;
 
 sub uncomp {
   my $content_ref = shift;
@@ -18,7 +18,7 @@ sub uncomp {
     my $t = Compress::Bzip2::decompress($content_ref); ## Try to uncompress a 1.02 stream!
 		unless($t) {
 		  my $T = $$content_ref;
-      my $status = bunzip2 \$T,\$t;            ## If this fails try a 1.03 stream!
+      my $status = IO::Uncompress::Bunzip2::bunzip2 \$T,\$t;            ## If this fails try a 1.03 stream!
 		}
 	  $$content_ref = $t;
   }
