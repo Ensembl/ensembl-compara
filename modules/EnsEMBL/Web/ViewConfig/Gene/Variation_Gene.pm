9c029f473d4b5ade3d16dc03510f07413a54a7bd
Beth Yates <bp1@sanger.ac.uk>
2008-12-09 14:09:03 +0000

    moved context selection to top of config form

--- modules/EnsEMBL/Web/ViewConfig/Gene/Variation_Gene.pm
+++ modules/EnsEMBL/Web/ViewConfig/Gene/Variation_Gene.pm
@@ -61,7 +61,25 @@ sub form {
   my  %class  = %{$options{'class'}};
   my  %type = %{$options{'type'}};
 
-   ### Add source selection
+  ### Add context selection
+  $view_config->add_fieldset('Context');
+  $view_config->add_form_element({
+    'type'     => 'DropDown', 'select'   => 'select',
+    'required' => 'yes',      'name'     => 'context',
+    'label'    => 'Context',
+    'values'   => [
+      { 'value' => '20',   'name' => '20bp' },
+      { 'value' => '50',   'name' => '50bp' },
+      { 'value' => '100',  'name' => '100bp' },
+      { 'value' => '200',  'name' => '200bp' },
+      { 'value' => '500',  'name' => '500bp' },
+      { 'value' => '1000', 'name' => '1000bp' },
+      { 'value' => '2000', 'name' => '2000bp' },
+      { 'value' => '5000', 'name' => '5000bp' },
+      { 'value' => 'FULL', 'name' => 'Full Introns' },
+    ]
+  });
+  ### Add source selection
   $view_config->add_fieldset('Select Variation Source');
   my $t = $object->table_info( 'variation', 'source' );
   my @sources = keys %{$t->{'counts'}};
@@ -103,24 +121,6 @@ sub form {
       'value'    => 'on', 'raw' => 1
     });
   }
-  ### Add context selection
-  $view_config->add_fieldset('Context');
-  $view_config->add_form_element({
-    'type'     => 'DropDown', 'select'   => 'select',
-    'required' => 'yes',      'name'     => 'context',
-    'label'    => 'Context',
-    'values'   => [
-      { 'value' => '20',   'name' => '20bp' },
-      { 'value' => '50',   'name' => '50bp' },
-      { 'value' => '100',  'name' => '100bp' },
-      { 'value' => '200',  'name' => '200bp' },
-      { 'value' => '500',  'name' => '500bp' },
-      { 'value' => '1000', 'name' => '1000bp' },
-      { 'value' => '2000', 'name' => '2000bp' },
-      { 'value' => '5000', 'name' => '5000bp' },
-      { 'value' => 'FULL', 'name' => 'Full Introns' },
-    ]
-  });
 }
 
 
