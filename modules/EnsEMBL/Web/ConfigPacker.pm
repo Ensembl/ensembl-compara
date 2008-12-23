e3555f09303d968eebc56afe0b420833e911e7f5
Simon Brent <sb23@sanger.ac.uk>
2008-12-23 16:18:42 +0000

    remove ancestral sequence from 29-way alignment. Smells like hack

--- modules/EnsEMBL/Web/ConfigPacker.pm
+++ modules/EnsEMBL/Web/ConfigPacker.pm
@@ -359,7 +359,7 @@ sub _summarise_compara_db {
       $constrained_elements->{$species_set_id} = $id;
       $name = "Constrained elements";
     } elsif( $class =~ /tree_alignment/ || $type  =~ /ORTHEUS/ ) {
-      unless( exists $self->db_tree->{$db_name}{$KEY}{$id} ) {
+      unless( $name eq '29 eutherian mammals EPO' || exists $self->db_tree->{$db_name}{$KEY}{$id} ) {
         $self->db_tree->{ $db_name }{$KEY}{$id}{'species'}{"Ancestral_sequences"}=1;
       }
     }
