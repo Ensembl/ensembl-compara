7bd6cc77878625159481829101cbabf7d1d63be1
Steve Trevanion <st3@sanger.ac.uk>
2009-01-13 17:28:04 +0000

    added transcript support (for oligos)

--- modules/EnsEMBL/Web/Component/Location/Genome.pm
+++ modules/EnsEMBL/Web/Component/Location/Genome.pm
@@ -115,18 +115,18 @@ sub content {
       my $i = 0;
       my $zmenu_config;
       foreach my $set  (@features) {
-	      my $pointer_ref = $image->add_pointers( $object, {
-	        'config_name'  => 'Vkaryotype',
-	        'features'      => $set->[0],
-	        'zmenu_config'  => $zmenu_config,
-	        'feature_type'  => $set->[2],
-	        'color'         => $object->param("col_$i")   || $pointer_defaults{$set->[2]}[0],
-	        'style'         => $object->param("style_$i") || $pointer_defaults{$set->[2]}[1]}
-			  );
-	      push(@$pointers, $pointer_ref);
-	      $i++;
+	my $pointer_ref = $image->add_pointers( $object, {
+	  'config_name'  => 'Vkaryotype',
+	  'features'      => $set->[0],
+	  'zmenu_config'  => $zmenu_config,
+	  'feature_type'  => $set->[2],
+	  'color'         => $object->param("col_$i")   || $pointer_defaults{$set->[2]}[0],
+	  'style'         => $object->param("style_$i") || $pointer_defaults{$set->[2]}[1]}
+					      );
+	push(@$pointers, $pointer_ref);
+	$i++;
       }
-    } 
+    }
     if (!@$pointers) { ## Ordinary "KaryoView"
       $image->image_name = "karyotype-$species";
       $image->imagemap = 'no';
@@ -158,100 +158,101 @@ sub content {
 }
 
 sub feature_tables {
-    my $self = shift;
-    my $feature_dets = shift;
-    my $object = $self->object;
-    my $data_type = $object->param('ftype');
-    my $html;
-    my @tables;
-    foreach my $feature_set (@{$feature_dets}) {
-	my $features = $feature_set->[0];
-	my $extra_columns = $feature_set->[1];
-	my $feat_type = $feature_set->[2];
+  my $self = shift;
+  my $feature_dets = shift;
+  my $object = $self->object;
+  my $data_type = $object->param('ftype');
+  my $html;
+  my @tables;
+  foreach my $feature_set (@{$feature_dets}) {
+    my $features = $feature_set->[0];
+    my $extra_columns = $feature_set->[1];
+    my $feat_type = $feature_set->[2];
 ##
-	#could show only gene links for xrefs, but probably not what is wanted:
-#	next SET if ($feat_type eq 'Gene' && $data_type =~ /Xref/);
+    #could show only gene links for xrefs, but probably not what is wanted:
+    #	next SET if ($feat_type eq 'Gene' && $data_type =~ /Xref/);
 ##
-	my $data_type = ($feat_type eq 'Gene') ? 'Gene Information:'
-	    : 'Feature Information:';
+    my $data_type = ($feat_type eq 'Gene') ? 'Gene Information:'
+      : ($feat_type eq 'Transcript') ? 'Transcript Information:'
+      : 'Feature Information:';
 
-	my $table = new EnsEMBL::Web::Document::SpreadSheet( [], [], {'margin' => '1em 0px'} );
-	if ($feat_type eq 'Gene') {
-	    $table->add_columns({'key'=>'names',  'title'=>'Ensembl ID',      'width'=>'25%','align'=>'left' });
-	    $table->add_columns({'key'=>'extname','title'=>'External names',  'width'=>'25%','align'=>'left' });
-
+    my $table = new EnsEMBL::Web::Document::SpreadSheet( [], [], {'margin' => '1em 0px'} );
+    if ($feat_type =~ /Gene|Transcript/) {
+      $table->add_columns({'key'=>'names',  'title'=>'Ensembl ID',      'width'=>'25%','align'=>'left' });
+      $table->add_columns({'key'=>'extname','title'=>'External names',  'width'=>'25%','align'=>'left' });
+    }
+    else {
+      $table->add_columns({'key'=>'loc',   'title'=>'Genomic location','width' =>'15%','align'=>'left' });
+      $table->add_columns({'key'=>'length','title'=>'Genomic length',  'width'=>'10%','align'=>'left' });
+      $table->add_columns({'key'=>'names', 'title'=>'Names(s)',        'width'=>'25%','align'=>'left' });
+    }
+	
+    my $c = 1;
+    for( @{$extra_columns||[]} ) {
+      $table->add_columns({'key'=>"extra_$c",'title'=>$_,'width'=>'10%','align'=>'left' });
+      $c++;
+    }
+	
+    my @data = map { $_->[0] }
+      sort { $a->[1] <=> $b->[1] || $a->[2] cmp $b->[2] || $a->[3] <=> $b->[3] }
+	map { [$_, $_->{'region'} =~ /^(\d+)/ ? $1 : 1e20 , $_->{'region'}, $_->{'start'}] }
+	  @{$features};
+    foreach my $row ( @data ) {
+      my $contig_link = 'Unmapped';
+      my $names = '';
+      my $data_row;
+      if ($row->{'region'}) {
+	$contig_link = sprintf('<a href="/%s/Location/View?r=%s:%d-%d;h=%s">%s:%d-%d(%d)</a>',
+			       $object->species,
+			       $row->{'region'}, $row->{'start'}, $row->{'end'}, $row->{'label'},
+			       $row->{'region'}, $row->{'start'}, $row->{'end'},
+			       $row->{'strand'});
+	if ($feat_type =~ /Gene|Transcript/ && $row->{'label'}) {
+	  my $t = $feat_type eq 'Gene' ? 'g' : 't';
+	  $names = sprintf('<a href="/%s/%s/Summary?%s=%s;r=%s:%d-%d">%s</a>',
+			   $object->species, $feat_type, $t, $row->{'label'},
+			   $row->{'region'}, $row->{'start'}, $row->{'end'},
+			   $row->{'label'});
+	  my $extname = $row->{'extname'};
+	  my $desc =  $row->{'extra'}[0];
+	  $data_row = { 'extname' => $extname, 'names' => $names};
 	}
 	else {
-	    $table->add_columns({'key'=>'loc',   'title'=>'Genomic location','width' =>'15%','align'=>'left' });
-	    $table->add_columns({'key'=>'length','title'=>'Genomic length',  'width'=>'10%','align'=>'left' });
-	    $table->add_columns({'key'=>'names', 'title'=>'Names(s)',        'width'=>'25%','align'=>'left' });
+	  if ($feat_type !~ /align|RegulatoryFactor|OligoProbe/i && $row->{'label'}) {
+	    $names = sprintf('<a href="/%s/Gene/Summary?g=%s;r=%s:%d-%d">%s</a>',
+			     $object->species, $row->{'label'},
+			     $row->{'region'}, $row->{'start'}, $row->{'end'},
+			     $row->{'label'});
+	  }
+	  else {
+	    $names  = $row->{'label'} if $row->{'label'};
+	  }
+	  my $length = $row->{'length'};
+	  $data_row = { 'loc'  => $contig_link, 'length' => $length, 'names' => $names, };
 	}
-	
-	my $c = 1;
-	for( @{$extra_columns||[]} ) {
-	    $table->add_columns({'key'=>"extra_$c",'title'=>$_,'width'=>'10%','align'=>'left' });
-	    $c++;
-	}
-	
-	my @data = map { $_->[0] }
-	    sort { $a->[1] <=> $b->[1] || $a->[2] cmp $b->[2] || $a->[3] <=> $b->[3] }
-		map { [$_, $_->{'region'} =~ /^(\d+)/ ? $1 : 1e20 , $_->{'region'}, $_->{'start'}] }
-		    @{$features};
-	foreach my $row ( @data ) {
-	    my $contig_link = 'Unmapped';
-	    my $names = '';
-	    my $data_row;
-	    if ($row->{'region'}) {
-		$contig_link = sprintf('<a href="/%s/Location/View?r=%s:%d-%d;h=%s">%s:%d-%d(%d)</a>',
-				       $object->species,
-				       $row->{'region'}, $row->{'start'}, $row->{'end'}, $row->{'label'},
-				       $row->{'region'}, $row->{'start'}, $row->{'end'},
-				       $row->{'strand'});
-		if ($feat_type eq 'Gene' && $row->{'label'}) {
-		    $names = sprintf('<a href="/%s/Gene/Summary?g=%s;r=%s:%d-%d">%s</a>',
-				     $object->species, $row->{'label'},
-				     $row->{'region'}, $row->{'start'}, $row->{'end'},
-				     $row->{'label'});
-		    my $extname = $row->{'extname'};
-		    my $desc =  $row->{'extra'}[0];
-		    $data_row = { 'extname' => $extname, 'names' => $names};
-		}
-		else {
-		    if ($feat_type !~ /align|RegulatoryFactor/i && $row->{'label'}) {
-			$names = sprintf('<a href="/%s/Gene/Summary?g=%s;r=%s:%d-%d">%s</a>',
-					 $object->species, $row->{'label'},
-					 $row->{'region'}, $row->{'start'}, $row->{'end'},
-					 $row->{'label'});
-		    }
-		    else {
-			$names  = $row->{'label'} if $row->{'label'};
-		    }
-		    my $length = $row->{'length'};
-		    $data_row = { 'loc'  => $contig_link, 'length' => $length, 'names' => $names, };
-		}
-	    }
-	    my $c = 1;
-	    for( @{$row->{'extra'}||[]} ) {
-		$data_row->{"extra_$c"} = $_;
-		$c++;
-	    }
-	    $c = 0;
-	    for( @{$row->{'initial'}||[]} ) {
-		$data_row->{"initial$c"} = $_;
-		$c++;
-	    }
-	    $table->add_row($data_row);
-	}
-	if (@data) {
-	    $html .= qq(<strong>$data_type</strong>);
-	    $html .= $table->render;
-	}
+      }
+      my $c = 1;
+      for( @{$row->{'extra'}||[]} ) {
+	$data_row->{"extra_$c"} = $_;
+	$c++;
+      }
+      $c = 0;
+      for( @{$row->{'initial'}||[]} ) {
+	$data_row->{"initial$c"} = $_;
+	$c++;
+      }
+      $table->add_row($data_row);
     }
-    if (! $html) {
-	my $id = $object->param('id');
-	$html .= qq(<br /><br />No mapping of $id found<br /><br />);
+    if (@data) {
+      $html .= qq(<strong>$data_type</strong>);
+      $html .= $table->render;
     }
-    return $html;
+  }
+  if (! $html) {
+    my $id = $object->param('id');
+    $html .= qq(<br /><br />No mapping of $id found<br /><br />);
+  }
+  return $html;
 }
 
 1;
