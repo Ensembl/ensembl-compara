fc5bb21511eddf0d8755528d9d2468e305b7cee4
Beth Yates <bp1@sanger.ac.uk>
2009-01-27 16:58:35 +0000

    updated to work with new external data in variation database

--- modules/EnsEMBL/Web/Component/Variation/ExternalData.pm
+++ modules/EnsEMBL/Web/Component/Variation/ExternalData.pm
@@ -30,23 +30,114 @@ sub content {
   my @data = @{$object->get_external_data};
   unless (scalar @data >= 1) { return "We do not have any external data for this variation";}
   
-  $html .=  qq(<dl class="summary">);
+  my $table_rows = table_data($object, \@data);
 
-  foreach my $va ( @data){
-    my $disorder = $va->phenotype_description(); 
-    my $code = $va->phenotype_name();
-    my $url = $object->species_defs->ENSEMBL_EXTERNAL_URLS->{'EGA'};
-    my $ext_id = $va->local_stable_id(); 
+  my $table = new EnsEMBL::Web::Document::SpreadSheet( [], [], {'margin' =>'1em 0px', });
+  $table->add_columns (
+    { 'key' => 'disease','title' => 'Disease/Trait', 'align' => 'left'},  
+    { 'key' => 'source', 'title' => 'Source', 'align' => 'left'},
+    { 'key' => 'study', 'title' => 'Study','align' => 'left'},
+    { 'key' => 'genes', 'title' => 'Associated Gene(s)', 'align' => 'left'},
+    { 'key' => 'allele', 'title' => 'Strongest risk allele', 'align' => 'left'},
+    { 'key' => 'variant', 'title' => 'Associated variant', 'align' => 'left'},
+    { 'key' => 'pvalue', 'title' => 'P value', 'align' => 'left'},
+  );
+ 
+  foreach my $row (values %$table_rows){
+    foreach ( @$row) {
+      $table->add_row($_);
+    }
+  }
+
+  $html .=  $table->render; 
+  return $html;
+  
+
+};
+
+sub table_data { 
+  my ($object, $external_data) = @_;
+  my (%rows, @data_row);
+  foreach my $va (@$external_data){ 
+    my $disorder = $va->phenotype_description;
+    if (exists $rows{lc ($disorder)}) { 
+      @data_row = @{ $rows{lc ($disorder)} };
+      $disorder = "";
+    }
+
+    my $code = $va->phenotype_name;
+    my $disease;
+    if ($disorder =~/^\w+/){  $disease = "<dt>$disorder ($code)</dt>"; } 
+    my $source = source_link($object, $va, $code);
+    my $study = study_link($va->study);
+    my $gene = gene_links($object, $va->associated_gene);
+    my $allele = $va->associated_variant_risk_allele;
+    my $variant_link = variation_link($object, $va->variation->name);
+    my $pval = $va->p_value;
+
+    my $row = {
+      'disease'     => $disease,
+      'source'      => $source,
+      'study'       => $study, 
+      'genes'       => $gene,
+      'allele'      => $allele,
+      'variant'     => $variant_link,
+      'pvalue'      => $pval,
+    };
+    push @data_row, $row;
+    $rows{lc ($va->phenotype_description)} = \@data_row;
+  } 
+
+  return \%rows;
+}
+
+sub gene_links {
+  my ($object, $data) = @_;
+  return unless $data;
+  my @genes = split (/,/, $data);
+  my @links;
+  foreach my $g (@genes){
+    if ($g =~/Intergenic/) { push (@links, $g); }
+    else { 
+      my $url = $object->_url({ 'type' => 'Gene', 'action' => 'Summary', 'g' => $g });
+      my $link = "<a href=" .$url.">$g</a>"; 
+      push (@links, $link);   
+    }
+  }
+  my $gene_links = join (',', @links); 
+  return $gene_links;
+}
+
+sub source_link {
+  my ($object, $va, $code) = @_;
+  my $source = $va->source_name;
+
+  my $url = $object->species_defs->ENSEMBL_EXTERNAL_URLS->{$source};
+  if ($url =~/ega/){
+    my $ext_id = $va->local_stable_id;
     $url =~s/###ID###/$ext_id/;
     $url =~s/###D###/$code/;
-    my $source = $va->source_name();
-    my $link = "<a href=" .$url .">[$source]</a>";
-    my $text = $object->name . " had a significant p-value in Genome Wide Association Study (" .$va->study_type() . ") " . $link; 
-    $html .= "<dt>$disorder ($code)</dt><dd>$text</dd>";
+  } elsif ($url =~/gwastudies/){
+    my $pubmed_id = $va->study;
+    $pubmed_id =~s/pubmed\///; 
+    $url =~s/###ID###/$pubmed_id/;       
   }
-    
-  $html .= "</dl>";
-  return $html;
+  my $link = "<a href=".$url.">[$source]</a>";
+  return $link;
+}
+
+sub study_link {
+  my ($study) = @_;
+  unless ($study =~/pubmed/){ return; }
+  my $link = "<a href=http://www.ncbi.nlm.nih.gov/".$study.">$study</a>";
+  return $link;
+}
+
+sub variation_link {
+  my ($object, $v) = @_;
+  my $url = $object->_url({ 'type' => 'Variation', 'action' => 'Summary', 'v' => $v });
+  my $link = "<a href=" .$url.">$v</a>";
+  return $link;
 }
 
 1;
