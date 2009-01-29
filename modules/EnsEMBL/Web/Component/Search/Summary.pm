785c814ebc4d36fe4d6d3c4c9fe7894403b9935c
Steve Trevanion <st3@sanger.ac.uk>
2009-01-29 17:54:06 +0000

    whitespace

--- modules/EnsEMBL/Web/Component/Search/Summary.pm
+++ modules/EnsEMBL/Web/Component/Search/Summary.pm
@@ -7,20 +7,20 @@ use base qw(EnsEMBL::Web::Component::Search);
 use CGI qw(escapeHTML);
 
 sub _init {
-    my $self = shift;
-    $self->cacheable( 0 );
-    $self->ajaxable(  0 );
+  my $self = shift;
+  $self->cacheable( 0 );
+  $self->ajaxable(  0 );
 }
 
 sub content {
-    my $self = shift;
-    my $exa_obj = $self->object->Obj;
-    my $renderer = new ExaLead::Renderer::HTML( $exa_obj );
-    my $html = $renderer->render_form.
-	       $renderer->render_summary.
-               $renderer->render_navigation.
-	       $renderer->render_hits;
-    return $html;
+  my $self = shift;
+  my $exa_obj = $self->object->Obj;
+  my $renderer = new ExaLead::Renderer::HTML( $exa_obj );
+  my $html = $renderer->render_form .
+    $renderer->render_summary .
+    $renderer->render_navigation .
+    $renderer->render_hits;
+  return $html;
 }
 
 1;
--- modules/EnsEMBL/Web/Factory/Search/Exalead.pm
+++ modules/EnsEMBL/Web/Factory/Search/Exalead.pm
@@ -10,58 +10,58 @@ use Data::Dumper;
 our @ISA = qw(EnsEMBL::Web::Factory);
 
 sub generic_search {
-    my $self = shift;
+  my $self = shift;
 }
 
 sub _search_all {
-    my $self = shift;
-    my $idx = shift;
-    my $species = shift;
-    my $exalead = new ExaLead;
-    $exalead->engineURL = $self->species_defs->ENSEMBL_SEARCH_URL;
-    $exalead->__timeout = 30;
-    $exalead->rootURL   = "/$ENV{'ENSEMBL_SPECIES'}/search";
-    ( my $SPECIES   = $ENV{'ENSEMBL_SPECIES'} ) =~ s/_/ /g;
-    my $source = "Top/Source/".$self->species_defs->ENSEMBL_EXALEAD_SITE_INDEX;
-    my $q = CGI->new();
-    my $not_fresh = $q->param;
-    if (@$species) {
-	$q->param('species',@$species);
-    }
-#    else { $q->param('species', ( $ENV{'ENSEMBL_SPECIES'} ) ); }
-    if( $not_fresh ) {
-	if( $q->param('q') ) {
-	    $q->param('_q',$q->param('q'));
-	    my $CAT = '';
-	    my $CAT2 = '';
-	    if( $q->param('species') && lc($q->param('species')) ne 'all' ) {
-		$CAT = "Top/Species/".$q->param('species');
-		if ( $q->param('idx') && lc($q->param('idx')) ne 'all' ) {
-		    $CAT .= '/'.$q->param('idx');
-		    $CAT2 = "Top/Feature type/".$q->param('idx').'/'.$q->param('species');
-		}
-	    }
-	    elsif( $q->param('idx') && lc($q->param('idx')) ne 'all' ) {
-		$CAT = "Top/Feature type/".$q->param('idx');
-	    }
-	    if( $CAT ) {
-		$CAT =~ s/_/ /g;
-		if( $CAT2 ) {
-		    $CAT2 =~ s/_/ /g;
-		    $q->param('_q', $q->param('_q').qq( corporate/tree:"$CAT" corporate/tree:"$CAT2" corporate/tree:"$source") );
-		} else {
-		    $q->param('_q', $q->param('_q').qq( corporate/tree:"$CAT" corporate/tree:"$source"));
-		}
-	    }
-	    else {
-		$q->param('_q', $q->param('_q').qq( corporate/tree:"$source"));
-	    }
-	} elsif( $q->param('_q') ){
-	    $q->param('_q', $q->param('_q').qq( corporate/tree:"$source")) unless $q->param('_q') =~/ corporate\/tree:"$source"/;
+  my $self = shift;
+  my $idx = shift;
+  my $species = shift;
+  my $exalead = new ExaLead;
+  $exalead->engineURL = $self->species_defs->ENSEMBL_SEARCH_URL;
+  $exalead->__timeout = 30;
+  $exalead->rootURL   = "/$ENV{'ENSEMBL_SPECIES'}/search";
+  ( my $SPECIES   = $ENV{'ENSEMBL_SPECIES'} ) =~ s/_/ /g;
+  my $source = "Top/Source/".$self->species_defs->ENSEMBL_EXALEAD_SITE_INDEX;
+  my $q = CGI->new();
+  my $not_fresh = $q->param;
+  if (@$species) {
+    $q->param('species',@$species);
+  }
+  #    else { $q->param('species', ( $ENV{'ENSEMBL_SPECIES'} ) ); }
+  if( $not_fresh ) {
+    if( $q->param('q') ) {
+      $q->param('_q',$q->param('q'));
+      my $CAT = '';
+      my $CAT2 = '';
+      if( $q->param('species') && lc($q->param('species')) ne 'all' ) {
+	$CAT = "Top/Species/".$q->param('species');
+	if ( $q->param('idx') && lc($q->param('idx')) ne 'all' ) {
+	  $CAT .= '/'.$q->param('idx');
+	  $CAT2 = "Top/Feature type/".$q->param('idx').'/'.$q->param('species');
 	}
-	$exalead->parse( $q );
+      }
+      elsif( $q->param('idx') && lc($q->param('idx')) ne 'all' ) {
+	$CAT = "Top/Feature type/".$q->param('idx');
+      }
+      if( $CAT ) {
+	$CAT =~ s/_/ /g;
+	if( $CAT2 ) {
+	  $CAT2 =~ s/_/ /g;
+	  $q->param('_q', $q->param('_q').qq( corporate/tree:"$CAT" corporate/tree:"$CAT2" corporate/tree:"$source") );
+	} else {
+	  $q->param('_q', $q->param('_q').qq( corporate/tree:"$CAT" corporate/tree:"$source"));
+	}
+      }
+      else {
+	$q->param('_q', $q->param('_q').qq( corporate/tree:"$source"));
+      }
+    } elsif( $q->param('_q') ){
+      $q->param('_q', $q->param('_q').qq( corporate/tree:"$source")) unless $q->param('_q') =~/ corporate\/tree:"$source"/;
     }
-    return EnsEMBL::Web::Proxy::Object->new( 'Search', $exalead, $self->__data );
+    $exalead->parse( $q );
+  }
+  return EnsEMBL::Web::Proxy::Object->new( 'Search', $exalead, $self->__data );
 }
 
 
