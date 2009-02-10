4f713bdbca53f377487ed225580c6b683d1aa18f
James Smith <js5@sanger.ac.uk>
2009-02-10 16:49:31 +0000

    search box and active

--- htdocs/components/90_configurator.js
+++ htdocs/components/90_configurator.js
@@ -50,17 +50,39 @@ function __load_into_new_page(event) {
   window.close();
   Event.stop(event);
 }
-function __init_config_menu() {
-/*
-  Section 1)
-  Loop through all the track configuration links on the left hand side of the
-  configuration box, remove the link (so the href doesn't get fired (yuk!)
-  add an onclick event to show the appropriate menu and hide the menu
-*/
-  if( ! initial_configuration ) {
-     initial_configuration = $('configuration') ? $('configuration').serialize(true) : false;
+
+function __initialize_export_configuration() {
+  $$('#export_configuration input.submit').each(function (el) {
+    el.observe('click', function () {
+      $$('#export_configuration input.input-checkbox').each(function (checkbox) {
+        if (checkbox.checked === false) {
+          // overwrite the checkbox with a hidden input with the value of "no"
+          // so that we know which boxes have been deselected
+          var input = document.createElement('input');
+          input.type = 'hidden';
+          input.name = checkbox.name;
+          input.value = 'no';
+          $('export_configuration').appendChild(input);
+          input = null;
+        }
+      });
+    });
+  });
+}
+
+function __initialize_modal_closes() {
+  var closeEls = $$('.modal_close');
+  
+  if( closeEls.length ) {
+    closeEls.each(function (el) {
+      Element.observe(el, 'click', function () {
+        modal_dialog_close();
+      });
+    });
   }
-  var has_configuration = $$('.track_configuration').length > 0;
+}
+
+function __change_close_button_action() {
   if(
     ( $$('.track_configuration').length + $$('.view_configuration').length ) > 0
   ) {
@@ -74,49 +96,21 @@ function __init_config_menu() {
   } else if($('cp_close')) {
     Event.observe( $('cp_close'), 'click', __load_into_new_page );
   }
+}
 
-  var closeEls = $$('.modal_close');
+function __configuration_search() {
   
-  if (closeEls.length) {
-    closeEls.each(function (el) {
-      Element.observe(el, 'click', function () {
-        modal_dialog_close();
-      });
-    });
-  }
-
-  closeEls = null;
-
-
-  if ($('export_configuration')) {
-    $$('#export_configuration input.submit').each(function (el) {
-      el.observe('click', function () {
-        $$('#export_configuration input.input-checkbox').each(function (checkbox) {
-          if (checkbox.checked === false) {
-            // overwrite the checkbox with a hidden input with the value of "no"
-            // so that we know which boxes have been deselected
-            var input = document.createElement('input');
-            input.type = 'hidden';
-            input.name = checkbox.name;
-            input.value = 'no';
-            
-            $('export_configuration').appendChild(input);
-            
-            input = null;
-          }
-        });
-      });
-    });
-  }
-
+}
+function __initialize_track_configuration() {
   $$('.track_configuration dd').each(function(n) {
     var link_id = n.id;
     var menu_id = 'menu_'+link_id.substr(5);
     if( n.hasClassName('active') ) {
       current_menu_id = menu_id;
       current_link_id = link_id;
+      if( !$(menu_id) ) show_active_tracks(); // If there is no menu - then it must be the active tracks link!
     } else {
-      $(menu_id).hide();
+      if( $(menu_id) ) $(menu_id).hide(); // If there is a menu hide it if it isn't active....
     }
     var an = n.select('a')[0]; 
     if( an ) {
@@ -130,19 +124,27 @@ function __init_config_menu() {
       var link_id = dd_n.id;
       var menu_id = 'menu_'+link_id.substr(5);
       if( current_menu_id ) {
-        $(current_menu_id).hide();
+        if( $(current_menu_id) ) {
+          $(current_menu_id).hide();
+        } else {
+          close_all_menus();
+        }
         $(current_link_id).removeClassName('active');
       }
-      $(menu_id).show();
+      if($(menu_id)) { // Real menu node!
+        $(menu_id).show();
+        $(menu_id).select('dl dt').each(function(en){ en.show(); });
+      } else {         // This is the active track node!
+        show_active_tracks();
+      }
       dd_n.addClassName('active');
       current_menu_id=menu_id;
       current_link_id=link_id;
     });
   });
-/*
-  Section 2)
-  Add a hide/show link to each of the menu items on the right handside...
-*/
+}
+
+function __hide_show_track_configuration() {
   $$('dl.config_menu').each(function(menu_n){
     var col          = 1;
     var current_node = 0;
@@ -152,14 +154,14 @@ function __init_config_menu() {
         if( current_node ) $('mn_'+current_node).remove();
         col = 3-col;
         n.addClassName('col'+col);
-	if( !n.hasClassName('munged') ) {
-	  n.addClassName( 'munged' ); // Stop a double rendering of the links!
+        if( !n.hasClassName('munged') ) {
+          n.addClassName( 'munged' ); // Stop a double rendering of the links!
           _menu_create_node( n, 'mn_'+next_menu_node_id, 0 );
-	}
+        }
         current_node = next_menu_node_id;
         next_menu_node_id++;
         n.select('select').each(function(sn){
-	  if(sn.visible()) {
+          if(sn.visible()) {
             var current_selected = 'off';
             sn.hide();
             sn.select('option').each(function(on){
@@ -168,9 +170,9 @@ function __init_config_menu() {
                 var i = Builder.node('img',{ src:'/i/render/'+x+'.gif', title:on.text, cursor: 'pointer', id:'gif_'+sn.id });
                 n.insertBefore(i,sn.nextSibling);
                 Event.observe(i,'click',change_img_value);
-             }
+              }
             });
-	  }
+          }
         });
       } else if(n.nodeName == 'DD' && current_node) {
         n.setAttribute('id','mn_'+current_node+'_dd');
@@ -181,15 +183,108 @@ function __init_config_menu() {
     });
     if( current_node ) $('mn_'+current_node).remove();
   });
-  $$('#configuration input.submit').each(function(n){ n.hide(); });
-/* deprecated!!  
-  if( $('config') ) {
-    Event.observe( $('config'), 'submit', function(f){
-      alert($('config').serialize());
-    });
-  } */
 }
 
+var configuration_search_value = '';
+function configuration_change_search(e) {
+  var search_box = Event.element(e);
+  var s = search_box.value;
+  if( configuration_search_value == s ) return;
+  configuration_search_value = s;
+  if( s.length < 3 ) return; // Don't do searches for less than three characters!
+  // Now lets show all the values.... 
+  $$('.track_configuration dd').each(function(n) { // Nicked from show active!
+    var current_dt = '';
+    var link_id = n.id;
+    var menu_id = 'menu_'+link_id.substr(5);
+    var flag = 0;
+    if($(menu_id)) { 
+      $(menu_id).hide();
+      // Hide all the "descriptions" of the tracks...
+      $(menu_id).select('dl dt').each(function(en){
+        var spans = en.select('span.menu_help');
+        var dd_node = 0;
+        if( spans.length) {
+          dd_node = $(spans[0].id+'_dd');
+        }
+        // Now check for match...
+        var st = en.innerHTML.stripTags();
+        if( dd_node ) {
+          st += ' '+dd_node.innerHTML.stripTags();
+        }
+        var patt=new RegExp(s,'i');
+        if( st.match(patt) ) {
+          en.show();
+          flag = 1;
+        } else {
+          en.hide();
+          if(dd_node) dd_node.hide();
+        }
+      });
+      if(flag==1) $(menu_id).show();
+    }
+  });
+}
+
+function __init_config_menu() {
+/*
+  Section 1)
+  Loop through all the track configuration links on the left hand side of the
+  configuration box, remove the link (so the href doesn't get fired (yuk!)
+  add an onclick event to show the appropriate menu and hide the menu
+*/
+  if( ! initial_configuration ) {
+     initial_configuration = $('configuration') ? $('configuration').serialize(true) : false;
+  }
+  var has_configuration = $$('.track_configuration').length > 0;
+
+  __change_close_button_action();
+  __initialize_modal_closes();
+  if($('export_configuration')) __initialize_export_configuration();
+  var T = $('configuration_search_text');
+  if(T) { 
+    configuration_search_value = '';
+    Event.observe(T,'keyup',configuration_change_search);
+    Event.observe(T,'change',configuration_change_search);
+  }
+  __initialize_track_configuration();
+/*
+  Section 2) Add a hide/show link to each of the menu items on the right handside...
+*/
+  __hide_show_track_configuration();   
+  $$('#configuration input.submit').each(function(n){ n.hide(); });
+}
+
+function close_all_menus() {
+  $$('.track_configuration dd').each(function(n) {
+    var link_id = n.id;
+    var menu_id = 'menu_'+link_id.substr(5);
+    if($(menu_id)) $(menu_id).hide();
+  });
+}
+
+function show_active_tracks() {
+  $$('.track_configuration dd').each(function(n) {
+    var link_id = n.id;
+    var menu_id = 'menu_'+link_id.substr(5);
+    var flag = 0;
+    if($(menu_id)) { 
+      $(menu_id).hide();
+      // Hide all the "descriptions" of the tracks...
+      $(menu_id).select('dl dd').each(function(en){ en.hide(); });
+      $(menu_id).select('dl dt').each(function(en){
+        // Hide the individual elements...
+        if( en.firstChild.nextSibling.title == 'Off' ) {
+          en.hide();
+        } else {
+          en.show();
+          flag = 1;
+        }
+      });
+      if(flag==1) $(menu_id).show();
+    }
+  });
+}
 var configurator_action_url = '';
 var configurator_action_title = '';
 
@@ -222,9 +317,7 @@ function configurator_submit_form( url, title ) {
       return;
     }
   }
-  if( config_name ) {
-    diff_configuration[ 'config' ] = config_name;
-  }
+  if( config_name ) diff_configuration[ 'config' ] = config_name;
   diff_configuration[ 'submit' ] = 1;
   var T = new Date();
   diff_configuration[ 'time' ] = T.getTime();
@@ -239,7 +332,7 @@ function configurator_submit_form( url, title ) {
       onSuccess: function( transport ) { configurator_success(transport) },
       onFailure: function( transport ) {
         $('modal_disable').hide();
-	$('modal_content').innerHTML = '<p class="ajax-error">Failure: unable to update configuration</p>';
+        $('modal_content').innerHTML = '<p class="ajax-error">Failure: unable to update configuration</p>';
       }
     });
   } else {
@@ -314,6 +407,7 @@ function change_img_value(e) {
         if(on.text == value) {
           on.selected = "selected";
           $('gif_'+current_selected_index).src = '/i/render/'+on.value+'.gif';
+          $('gif_'+current_selected_index).title = on.text;s
           current_selected_index = 0; return;
         }
       });
