12216782fe922166c940b36b1301772708bd1502
Beth Yates <bp1@sanger.ac.uk>
2009-02-10 14:57:55 +0000

    added headers to transcript selection table and changed to display table even if only one transcript

--- modules/EnsEMBL/Web/Component/Gene/Summary.pm
+++ modules/EnsEMBL/Web/Component/Gene/Summary.pm
@@ -101,89 +101,69 @@ sub content {
 
 ## Now create the transcript information...
   my $transcripts = $object->Obj->get_all_Transcripts; 
-  my $count = @$transcripts; 
-  if( $count > 1 ) {
-    my $transcript = $object->core_objects->{'parameters'}{'t'};
-    $html .= qq(
-    <dl class="summary">
-      <dt>Transcripts</dt>
-      <dd>
-        <p id="transcripts_text">There are $count transcripts in this gene:</p>
-        <table id="transcripts" summary="List of transcripts for this gene - along with translation information and type">);
-    foreach(
-      map { $_->[2] }
-      sort { $a->[0] cmp $b->[0] || $a->[1] cmp $b->[1] } 
-      map { [$_->external_name, $_->stable_id, $_] }
-      @$transcripts
-    ) {
-      my $url = $self->object->_url({
-        'type'   => 'Transcript',
-        'action' => 'Summary',
-        't'      => $_->stable_id
-      }); 
-      my $protein = 'No protein product';
-      if( $_->translation ) {
-        $protein = sprintf '<a href="%s">%s</a>',
-          $self->object->_url({
-            'type'   => 'Transcript',
-            'action' => 'ProteinSummary',
-            't'      => $_->stable_id
-          }),
-	  CGI::escapeHTML( $_->translation->stable_id );
-      }
-      $html .= sprintf( '
-          <tr%s>
-            <th>%s</th>
-            <td><a href="%s">%s</a></td>
-            <td>%s</td>
-            <td>%s</td>
-          </tr>',
-        $_->stable_id eq $transcript ? ' class="active"' : '',
-        CGI::escapeHTML( $_->display_xref ? $_->display_xref->display_id : 'Novel' ),
-        $url,
-        CGI::escapeHTML( $_->stable_id ),
-	$protein,
-	$_->biotype
-      );
-    }
-    $html .= '
-        </table>
-      </dd>
-    </dl>';
-  } else {
-    my $transcript = @$transcripts[0];
-    my $display;
-    eval {
-	$display = $transcript->display_xref->display_id || $transcript->stable_id;
-    };
-    my $id = $transcript->stable_id;
-    $html .= qq(
-    <dl class="summary">
-      <dt>Transcripts</dt>
-      <dd>
-        <p id="transcripts_text">There is one transcript in this gene: );
+  my $count = @$transcripts;
+  my $plural_1 = "are";
+  my $plural_2 = "transcripts"; 
+  if( $count == 1  ) {
+    $plural_1 = "is"; 
+    $plural_2 =~s/s$//; 
+  }
 
+  my $transcript = $object->core_objects->{'parameters'}{'t'};
+  $html .= qq(
+    <dl class="summary">
+      <dt>Transcripts</dt>
+      <dd>
+        <p id="transcripts_text">There $plural_1  $count $plural_2 in this gene:</p>
+        <table id="transcripts" summary="List of transcripts for this gene - along with translation information and type">
+        <tr>
+          <th>Name</th>
+          <th>Transcript ID</th>
+          <th>Protein ID</th>
+          <th>Description</th> 
+        </tr>
+  );
+ 
+  foreach(
+    map { $_->[2] }
+    sort { $a->[0] cmp $b->[0] || $a->[1] cmp $b->[1] } 
+    map { [$_->external_name, $_->stable_id, $_] }
+    @$transcripts
+  ){
     my $url = $self->object->_url({
       'type'   => 'Transcript',
       'action' => 'Summary',
-      't'      => $transcript->stable_id
-    });
- 
-    $html .= sprintf q(
-       <a href="%s">%s</a> (<a href="%s">%s</a>)), $url, CGI::escapeHTML( $display), $url, CGI::escapeHTML( $id );
-    if( $transcript->translation ) {
-      $html .= sprintf ', with protein product <a href="%s">%s</a>', $self->object->_url({
+      't'      => $_->stable_id
+    }); 
+    my $protein = 'No protein product';
+    if( $_->translation ) {
+      $protein = sprintf '<a href="%s">%s</a>',
+      $self->object->_url({
         'type'   => 'Transcript',
         'action' => 'ProteinSummary',
-        't'      => $transcript->stable_id
-      }), CGI::escapeHTML( $transcript->translation->stable_id );
+        't'      => $_->stable_id
+      }),
+	    CGI::escapeHTML( $_->translation->stable_id );
     }
-       
-    $html .= qq(.</p>
-     </dd>
-    </dl>
+    $html .= sprintf( '
+      <tr%s>      
+        <th>%s</th>
+        <td><a href="%s">%s</a></td>
+        <td>%s</td>
+        <td>%s</td>
+      </tr>',
+      $_->stable_id eq $transcript ? ' class="active"' : '',
+      CGI::escapeHTML( $_->display_xref ? $_->display_xref->display_id : 'Novel' ),
+      $url,
+      CGI::escapeHTML( $_->stable_id ),
+	    $protein,
+	    $_->biotype
     );
-  }
+   }
+  $html .= '
+      </table>
+    </dd>
+  </dl>';
   return $html;
 }
 
