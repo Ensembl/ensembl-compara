1b2892c323dd69f41189d823c5890f4e11b3d0ad
Andrew Yates <ayates@ebi.ac.uk>
2009-02-23 12:32:07 +0000

    Changed to using the registry to find a GenomeDB rather than just binomial; allows it to be used with Ensembl Genomes

--- scripts/synteny/LoadSyntenyData.pl
+++ scripts/synteny/LoadSyntenyData.pl
@@ -1,6 +1,7 @@
-#!/usr/local/ensembl/bin/perl -w
+#!/usr/local/ensembl/bin/perl
 
 use strict;
+use warnings;
 use Bio::EnsEMBL::Registry;
 use Getopt::Long;
 use Bio::EnsEMBL::Compara::DnaFrag;
@@ -34,36 +35,25 @@ if ($help) {
 Bio::EnsEMBL::Registry->no_version_check(1);
 Bio::EnsEMBL::Registry->load_all($reg_conf);
 
-my $dbc = Bio::EnsEMBL::Registry->get_DBAdaptor($dbname, 'compara')->dbc;
+my $dba = Bio::EnsEMBL::Registry->get_DBAdaptor($dbname, 'compara');
+my $dbc = $dba->dbc();
 
-my $gdba = Bio::EnsEMBL::Registry->get_adaptor($dbname, 'compara', 'GenomeDB');
-my $dfa = Bio::EnsEMBL::Registry->get_adaptor($dbname, 'compara', 'DnaFrag');
-my $mlssa = Bio::EnsEMBL::Registry->get_adaptor($dbname, 'compara', 'MethodLinkSpeciesSet');
+my $gdba = $dba->get_GenomeDBAdaptor();
+my $dfa = $dba->get_DnaFragAdaptor();
+my $mlssa = $dba->get_MethodLinkSpeciesSetAdaptor();
 
-my $qy_binomial = Bio::EnsEMBL::Registry->get_adaptor($qy_species,'core','MetaContainer')->get_Species->binomial;
-my $tg_binomial = Bio::EnsEMBL::Registry->get_adaptor($tg_species,'core','MetaContainer')->get_Species->binomial;
+my $qy_gdb = $gdba->fetch_by_registry_name($qy_species);
+my $tg_gdb = $gdba->fetch_by_registry_name($tg_species);
 
-my $qy_gdb = $gdba->fetch_by_name_assembly($qy_binomial);
-my $tg_gdb = $gdba->fetch_by_name_assembly($tg_binomial);
-my $mlss = new Bio::EnsEMBL::Compara::MethodLinkSpeciesSet;
-$mlss->method_link_type($method_link_type);
-$mlss->species_set([$qy_gdb, $tg_gdb]);
+my $mlss = Bio::EnsEMBL::Compara::MethodLinkSpeciesSet->new(
+	-METHOD_LINK_TYPE => $method_link_type, -SPECIES_SET => [$qy_gdb, $tg_gdb]);
 $mlssa->store($mlss);
 
-my $qy_sa = Bio::EnsEMBL::Registry->get_adaptor($qy_species, 'core', 'Slice');
-my %qy_slices;
-foreach my $qy_slice (@{$qy_sa->fetch_all('toplevel')}) {
-  $qy_slices{$qy_slice->seq_region_name} = $qy_slice;
-}
+my $qy_slices = get_slices($qy_gdb);
+my $tg_slices = get_slices($tg_gdb);
 
-my $tg_sa = Bio::EnsEMBL::Registry->get_adaptor($tg_species, 'core', 'Slice');
-my %tg_slices;
-foreach my $tg_slice (@{$tg_sa->fetch_all('toplevel')}) {
-  $tg_slices{$tg_slice->seq_region_name} = $tg_slice;
-}
-
-my $sth_synteny_region = $dbc->prepare("insert into synteny_region (method_link_species_set_id) values (?)");
-my $sth_dnafrag_region = $dbc->prepare("insert into dnafrag_region (synteny_region_id,dnafrag_id,dnafrag_start,dnafrag_end,dnafrag_strand) values (?,?,?,?,?)");
+my $sth_synteny_region = $dbc->prepare('insert into synteny_region (method_link_species_set_id) values (?)');
+my $sth_dnafrag_region = $dbc->prepare(q{insert into dnafrag_region (synteny_region_id,dnafrag_id,dnafrag_start,dnafrag_end,dnafrag_strand) values (?,?,?,?,?)});
 
 my $line_number = 1;
 
@@ -72,27 +62,15 @@ while (defined (my $line = <>) ) {
   if ($line =~ /^(\S+)\t.*\t.*\t(\d+)\t(\d+)\t.*\t(-1|1)\t.*\t(\S+)\t(\d+)\t(\d+)$/) {#####This will need to be changed
     my ($qy_chr,$qy_start,$qy_end,$rel,$tg_chr,$tg_start,$tg_end) = ($1,$2,$3,$4,$5,$6,$7);
 
-    my $qy_dnafrag = new Bio::EnsEMBL::Compara::DnaFrag;
-    $qy_dnafrag->name($qy_chr);
-    $qy_dnafrag->genome_db($qy_gdb);
-    $qy_dnafrag->coord_system_name($qy_slices{$qy_chr}->coord_system->name);
-    $qy_dnafrag->length($qy_slices{$qy_chr}->seq_region_length);
- 
-    $dfa->store_if_needed($qy_dnafrag);
-    
-    my $tg_dnafrag = new Bio::EnsEMBL::Compara::DnaFrag;
-    $tg_dnafrag->name($tg_chr);
-    $tg_dnafrag->genome_db($tg_gdb);
-    $tg_dnafrag->coord_system_name($tg_slices{$tg_chr}->coord_system->name);
-    $tg_dnafrag->length($tg_slices{$tg_chr}->seq_region_length);
-    $dfa->store_if_needed($tg_dnafrag);
+    my $qy_dnafrag = store_dnafrag($qy_chr, $qy_gdb, $qy_slices, $dfa);
+    my $tg_dnafrag = store_dnafrag($tg_chr, $tg_gdb, $tg_slices, $dfa);
 
 # print STDERR "1: $qy_chr, 2: $tg_chr, qy_end: " .$qy_dnafrag->end.", tg_end: ". $tg_dnafrag->end."\n";
   
     $sth_synteny_region->execute($mlss->dbID);
     my $synteny_region_id = $sth_synteny_region->{'mysql_insertid'};
-    $sth_dnafrag_region->execute($synteny_region_id,$qy_dnafrag->dbID,$qy_start,$qy_end,1);
-    $sth_dnafrag_region->execute($synteny_region_id,$tg_dnafrag->dbID,$tg_start,$tg_end,$rel);
+    $sth_dnafrag_region->execute($synteny_region_id, $qy_dnafrag->dbID, $qy_start, $qy_end, 1);
+    $sth_dnafrag_region->execute($synteny_region_id, $tg_dnafrag->dbID, $tg_start, $tg_end, $rel);
     print STDERR "synteny region line number $line_number loaded\n";
     $line_number++;
   } else {
@@ -101,4 +79,32 @@ EXIT 1\n";
     exit 1;
   }
 
+}
+
+$sth_synteny_region->finish();
+$sth_dnafrag_region->finish();
+
+#Returns a hash of slices keyed by name; slice names at toplevel must be 
+#unique otherwise this entire pipeline goes wrong
+sub get_slices {
+	my ($gdb) = @_;
+	my $sa = $gdb->db_adaptor()->get_SliceAdaptor();
+	my %slice_hash;
+	foreach my $slice (@{$sa->fetch_all('toplevel')}) {
+		$slice_hash{$slice->seq_region_name} = $slice;
+	}
+	return \%slice_hash;
+}
+
+sub store_dnafrag {
+	my ($chr, $gdb, $slices, $adaptor) = @_;
+	my $slice = $slices->{$chr};
+	my $dnafrag = Bio::EnsEMBL::Compara::DnaFrag->new(
+		-NAME => $chr,
+		-GENOME_DB => $gdb,
+		-COORD_SYSTEM_NAME => $slice->coord_system()->name(),
+		-LENGTH => $slice->seq_region_length()
+	);
+	$adaptor->store_if_needed($dnafrag);
+	return $dnafrag;
 }
