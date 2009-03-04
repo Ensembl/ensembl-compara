e20cad2e0baa61115b2626a45973e919b3e5f671
Javier Herrero <jh7@sanger.ac.uk>
2009-03-04 12:57:35 +0000

    Show link to species from constrained elements

--- modules/EnsEMBL/Web/Configuration/Location.pm
+++ modules/EnsEMBL/Web/Configuration/Location.pm
@@ -702,6 +702,19 @@ sub _ajax_zmenu_av {
       if ($caption =~ /^(\d+)/) {
         $caption = "Constrained el. $1 way";
       }
+      my @alignment_segments = sort {$a->[3] cmp $b->[3]} @{$feature->alignment_segments};
+      for (my $i = 0; $i < @alignment_segments; $i++) {
+        my $segment = $alignment_segments[$i];
+        my ($dnafrag_id, $start, $end, $genome_db_name, $dnafrag_name) = @$segment;
+        my $seg_url = "/$genome_db_name/Location/View?r=$dnafrag_name:$start-$end";
+        $seg_url =~ s/ /_/g;
+        $panel->add_entry({
+          'type' => $genome_db_name,
+          'label' => "View region (".($end-$start+1)." bp)",
+          'link' => $seg_url,
+          'priority' => 99 - $i,
+        });
+      }
     } elsif ($obj_type eq "GenomicAlignBlock" and $obj->param('ref_id')) {
       $feature->{reference_genomic_align_id} = $obj->param('ref_id');
       $start = $feature->reference_genomic_align->dnafrag_start;
