08b690481ccc1a8e064e9fbc04236685a8ab72b8
Albert Vilella <avilella@sanger.ac.uk>
2009-03-05 17:05:02 +0000

    adding back the diff bp functionality that st3 deleted (I hope unintentionally) a couple of versions ago

--- htdocs/components/71_zmenus.js
+++ htdocs/components/71_zmenus.js
@@ -132,7 +132,9 @@ function _show_zmenu_range( x ) {
 */
   __zmenu_remove();
   var Q = __zmenu_init('zmenu_nav','Region: '+x.bp_start+'-'+x.bp_end);
-  __zmenu_add( Q, '', 'Jump to region',   _new_url( x.bp_start, x.bp_end ) );
+  var D = x.bp_end-x.bp_start;
+  var R = 'Jump to region ('+D+' bp)';
+  __zmenu_add( Q, '', R,   _new_url( x.bp_start, x.bp_end ) );
   __zmenu_add( Q, '', 'Centre here',      _new_url_cp( (x.bp_start+x.bp_end)/2, __seq_region.width-1 ) );
   __zmenu_show(Q, x.x, x.y);
 }
