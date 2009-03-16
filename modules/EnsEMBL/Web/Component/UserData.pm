7b414e7aef77ee2e6ee2d3a1033b9e1b0f9233be
Anne Parker <ap5@sanger.ac.uk>
2009-03-16 11:37:17 +0000

    New stuff for Magic Wizards

--- modules/EnsEMBL/Web/Component/UserData.pm
+++ modules/EnsEMBL/Web/Component/UserData.pm
@@ -25,5 +25,28 @@ sub is_configurable {
   return $flag;
 }
 
+sub get_assemblies {
+### Tries to identify coordinate system from file contents
+### If on chromosomal coords and species has multiple assemblies,
+### return assembly info
+  my ($self, $species) = @_;
+
+  my @assemblies = split(',', $self->object->species_defs->get_config($species, 'CURRENT_ASSEMBLIES'));
+  return \@assemblies;
+}
+
+sub output_das_text {
+  my ( $self, @sources ) = @_;
+  map {
+    $self->add_element( 'type'    => 'Information',
+                        'classes'  => ['no-bold'],
+                        'value'   => sprintf '<strong>%s</strong><br />%s<br /><a href="%s">%3$s</a>',
+                                           $_->label,
+                                           $_->description,
+                                           $_->homepage );
+  } @sources;
+}
+
+
 1;
 
