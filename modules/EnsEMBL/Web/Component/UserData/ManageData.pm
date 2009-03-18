0dc3b7bf8d84aea3c03fb6823bb1dcdd0e6eafbc
Simon Brent <sb23@sanger.ac.uk>
2009-03-18 11:13:58 +0000

    Warning message if file could not be found. Change class on delete link so that it can display a js confirm box if the data is shared. Don't allow deleting data if it was uploaded by another user

--- modules/EnsEMBL/Web/Component/UserData/ManageData.pm
+++ modules/EnsEMBL/Web/Component/UserData/ManageData.pm
@@ -10,139 +10,167 @@ use Apache2::RequestUtil;
 
 sub _init {
   my $self = shift;
-  $self->cacheable( 0 );
-  $self->ajaxable(  0 );
+  $self->cacheable(0);
+  $self->ajaxable(0);
 }
 
 sub content {
   my $self = shift;
   my $object = $self->object;
   my $sd = $object->species_defs;
+    
+  my $r = Apache2::RequestUtil->request;
+  my $referer = '_referer=' . $object->param('_referer') . ';x_requested_with=' . ($object->param('x_requested_with') || $r->headers_in->{'X-Requested-With'});
+  my $user = $ENSEMBL_WEB_REGISTRY->get_user;
+  my @data = $object->get_session->get_data(type => 'upload');
   
-  ## Control panel fixes
+  # Control panel fixes
   my $dir = '/'.$ENV{'ENSEMBL_SPECIES'};
   $dir = '' if $dir !~ /_/;
-  my $r = Apache2::RequestUtil->request();
-
-  my $referer = '_referer='.$self->object->param('_referer').';x_requested_with='.
-     ( $self->object->param('x_requested_with')||$r->headers_in->{'X-Requested-With'} );
-
+  
   my $html;
-  if( $self->object->param('reload') ) {
-    $html .= '<div id="modal_reload">.</div>';
-  }
-  my $user = $ENSEMBL_WEB_REGISTRY->get_user;
-
-  ## Uploads
-  $html .= "<h3>Your data</h3>";
-
-  my @data = $self->object->get_session->get_data(type => 'upload');
-  ## Extra check if file exists, if not - delete the record
-  foreach my $file (@data) {
-    $self->object->get_session->purge_data($file)
-      unless EnsEMBL::Web::TmpFile::Text->new(filename => $file->{'filename'})->exists;
+  $html .= '<div id="modal_reload">.</div>' if $object->param('reload');
+  $html .= '<h3>Your data</h3>'; # Uploads
+  
+  my $not_found = 0;
+  
+  # Extra check if file exists, if not - delete the record
+  foreach my $file (@data) {    
+    if ($file->{'filename'} && !EnsEMBL::Web::TmpFile::Text->new(filename => $file->{'filename'})->exists) {
+      $file->{'name'} .= ' (File could not be found)';
+      $not_found++;
+    }
   }
   
   push @data, $user->uploads if $user;
-  push @data, values %{$self->object->get_session->get_all_das};
+  push @data, values %{$object->get_session->get_all_das};
   push @data, $user->dases if $user;
-  push @data, $self->object->get_session->get_data(type => 'url');
+  push @data, $object->get_session->get_data(type => 'url');
   push @data, $user->urls if $user;
 
   if (@data) {
-    my $table = EnsEMBL::Web::Document::SpreadSheet->new();
+    my $table = EnsEMBL::Web::Document::SpreadSheet->new;
+    
     $table->add_columns(
-      {'key' => "type",   'title' => 'Type',          'width' => '10%', 'align' => 'left' },
-      {'key' => "name",   'title' => 'File',          'width' => '45%', 'align' => 'left' },
+      { 'key' => 'type', 'title' => 'Type', 'width' => '10%', 'align' => 'left' },
+      { 'key' => 'name', 'title' => 'File', 'width' => '45%', 'align' => 'left' }
     );
+    
     if ($sd->ENSEMBL_LOGINS) {
       $table->add_columns(
-        {'key' => "date",   'title' => 'Last updated',  'width' => '15%', 'align' => 'left' },
-        {'key' => "save",   'title' => '',              'width' => '15%', 'align' => 'left' },
+        { 'key' => 'date', 'title' => 'Last updated', 'width' => '15%', 'align' => 'left' },
+        { 'key' => 'save', 'title' => '', 'width' => '15%', 'align' => 'left' }
       );
     }
+    
     $table->add_columns(
-      {'key' => "delete", 'title' => '',              'width' => '15%', 'align' => 'left' },
+      { 'key' => 'delete', 'title' => '', 'width' => '15%', 'align' => 'left' }
     );
+    
     foreach my $file (@data) {
       my $row;
-      if (ref($file) =~ /Record/) { ## from user account
+      my $sharers = $object->get_session->get_sharers($file);
+      my $delete_class = $sharers ? 'modal_confirm' : 'modal_link';
+      my $title = sprintf(' title="This data is shared with %d user%s"', $sharers, $sharers > 1 ? 's' : '') if $sharers;
+      
+      # from user account
+      if (ref $file =~ /Record/) {
         my ($type, $name, $date, $delete);
-        if (ref($file) =~ /Upload/) {
+        
+        if (ref $file =~ /Upload/) {
           $type = 'Upload';
           $name = $file->name;
           $date = $file->modified_at || $file->created_at;
           $date = $self->pretty_date($date);
-          $delete = sprintf('<a href="%s/UserData/DeleteUpload?type=user;id=%s;%s" class="modal_link">Delete</a>', $dir, $file->id, $referer);
-        }
-        elsif (ref($file) =~ /DAS/) {
+          $delete = sprintf('<a href="%s/UserData/DeleteUpload?type=user;id=%s;%s" class="$delete_class"$title>Delete</a>', $dir, $file->id, $referer);
+        } elsif (ref $file =~ /DAS/) {
           $type = 'DAS';
           $name = $file->label;
           $date = '-';
           $delete = sprintf('<a href="%s/UserData/DeleteRemote?type=das;id=%s;%s" class="modal_link">Delete</a>', $dir, $file->id, $referer);
-        }
-        elsif (ref($file) =~ /URL/) {
+        } elsif (ref $file =~ /URL/) {
           $type = 'URL';
           $name = $file->name;
           $date = '-';
           $delete = sprintf('<a href="%s/UserData/DeleteRemote?id=%s;%s" class="modal_link">Delete</a>', $dir, $file->id, $referer);
         }
+        
         if ($sd->ENSEMBL_LOGINS) {
-          $row = {'type' => $type, 'name' => $name, 'date' => $date, 'save' => 'Saved', 'delete' => $delete };
+          $row = { 'type' => $type, 'name' => $name, 'delete' => $delete, 'date' => $date, 'save' => 'Saved' };
+        } else {
+          $row = { 'type' => $type, 'name' => $name, 'delete' => $delete };
         }
-        else {
-          $row = {'type' => $type, 'name' => $name, 'delete' => $delete };
-        }
-      }
-      else {
+      } else {
         my $save = sprintf('<a href="%s/Account/Login?%s" class="modal_link">Log in to save</a>', $dir, $referer);
         my ($type, $name, $delete);
-        if (ref($file) =~ /DASConfig/i) {
+        
+        if (ref $file =~ /DASConfig/i) {
           $type = 'DAS';
           $name = $file->label;
+          
           if ($sd->ENSEMBL_LOGINS && $user) {
             $save = sprintf('<a href="%s/UserData/SaveRemote?dsn=%s;%s" class="modal_link">Save to account</a>', $dir, $file->logic_name, $referer);
           }
+          
           $delete = sprintf('<a href="%s/UserData/DeleteRemote?logic_name=%s;%s" class="modal_link">Remove</a>', $dir, $file->logic_name, $referer);
-  
-        }
-        elsif ($file->{'url'}) {
+        } elsif ($file->{'url'}) {
           $type = 'URL';
-          $name = '<strong>'.$file->{'name'}.'</strong><br />' if $file->{'name'};
-          $name .= $file->{'url'}.' ('.$file->{'species'}.')';
+          $name = "<strong>$file->{'name'}</strong><br />" if $file->{'name'};
+          $name .= "$file->{'url'} ($file->{'species'})";
+          
           if ($sd->ENSEMBL_LOGINS && $user) {
             $save = sprintf('<a href="%s/UserData/SaveRemote?code=%s;species=%s;%s" class="modal_link">Save to account</a>', $dir, $file->{'code'}, $file->{'species'}, $referer);
           }
+          
           $delete = sprintf('<a href="%s/UserData/DeleteRemote?type=url;code=%s;%s" class="modal_link">Remove</a>', $dir, $file->{'code'}, $referer);
-        }
-        else {
+        } else {
           $type = 'Upload';
           $name = '<p>';
-          $name .= '<strong>'.$file->{'name'}.'</strong><br />' if $file->{'name'};
-          $name .= $file->{'format'}.' file for '.$file->{'species'};
-          my $extra = 'type='.$file->{'type'}.';code='.$file->{'code'};
+          $name .= "<strong>$file->{'name'}</strong><br />" if $file->{'name'};
+          $name .= "$file->{'format'} file for $file->{'species'}";
+          my $extra = "type=$file->{'type'};code=$file->{'code'}"; 
+          
           if ($sd->ENSEMBL_LOGINS && $user) {
             $save = qq(<a href="$dir/UserData/SaveUpload?$extra;$referer" class="modal_link">Save to account</a>);
           }
-          $delete = qq(<a href="$dir/UserData/DeleteUpload?$extra;$referer" class="modal_link">Remove</a></p>);
+          
+          $delete = qq(<a href="$dir/UserData/DeleteUpload?$extra;$referer" class="$delete_class"$title>Remove</a></p>);
         }
+        
+        if ($file->{'analyses'}) {
+          $file->{'analyses'} =~ /^session_(\d+)_/;
+          $delete = '' if $1 != $object->get_session->get_session_id; # It's not your data, so you aren't allowed to delete it.
+        }
+        
         if ($sd->ENSEMBL_LOGINS) {
-          $row = {'type' => $type, 'name' => $name, 'date' => '-', 'save' => $save, 'delete' => $delete };
-        }
-        else {
-          $row = {'type' => $type, 'name' => $name, 'delete' => $delete };
+          $row = { 'type' => $type, 'name' => $name, 'delete' => $delete, 'date' => '-', 'save' => $save };
+        } else {
+          $row = { 'type' => $type, 'name' => $name, 'delete' => $delete };
         }
       }
+      
       $table->add_row($row);
     }
-    $html .= $table->render;  
-  } 
-  else {
+    
+    $html .= $table->render;
+    
+    if ($not_found) {
+      my ($s, $are);
+      
+      if ($not_found == 1) {
+        $are = 'is';
+      } else {
+        $s = 's';
+        $are = 'are';
+      }
+      
+      $html .= $self->_warning('File not found', "The file$s marked not found $are unavailable. Please try again later.", '100%');
+    }
+  } else {
     $html .= qq(<p class="space-below">You have no custom data.</p>);
   }
 
-## URL
-
+  # URL
   unless ($self->is_configurable) {
     $html .= $self->_info(
       'Adding tracks',
