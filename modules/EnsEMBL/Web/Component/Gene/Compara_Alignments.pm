7c2890fbfdc6844512e8e5052059f91e8b88b62d
Simon Brent <sb23@sanger.ac.uk>
2009-03-25 14:24:01 +0000

    stub for Component/Compara_Alignments.pm

---
+++ modules/EnsEMBL/Web/Component/Gene/Compara_Alignments.pm
@@ -0,0 +1,7 @@
+package EnsEMBL::Web::Component::Gene::Compara_Alignments;
+
+use strict;
+
+use base qw(EnsEMBL::Web::Component::Compara_Alignments);
+
+1;
---
+++ modules/EnsEMBL/Web/Component/Location/Compara_Alignments.pm
@@ -0,0 +1,7 @@
+package EnsEMBL::Web::Component::Location::Compara_Alignments;
+
+use strict;
+
+use base qw(EnsEMBL::Web::Component::Compara_Alignments);
+
+1;
