64410a149f29468d068e2c9f0741b36dbcc25486
Albert Vilella <avilella@sanger.ac.uk>
2009-04-03 09:10:05 +0000

    clean up

--- modules/Bio/EnsEMBL/Compara/DBSQL/SequenceAdaptor.pm
+++ modules/Bio/EnsEMBL/Compara/DBSQL/SequenceAdaptor.pm
@@ -27,14 +27,14 @@ sub fetch_by_dbID {
 sub store {
   my ($self, $sequence) = @_;
   my $seqID;
-  
+
   return 0 unless($sequence);
 
   my $dcs = $self->dbc->disconnect_when_inactive();
   $self->dbc->disconnect_when_inactive(0);
-  
+
   $self->dbc->do("LOCK TABLE sequence WRITE");
-  
+
   my $sth = $self->prepare("SELECT sequence_id FROM sequence WHERE sequence = ?");
   $sth->execute($sequence);
   ($seqID) = $sth->fetchrow_array();
@@ -48,7 +48,7 @@ sub store {
     $seqID = $sth2->{'mysql_insertid'};
     $sth2->finish;
   }
-  
+
   $self->dbc->do("UNLOCK TABLES");
   $self->dbc->disconnect_when_inactive($dcs);
   return $seqID;
