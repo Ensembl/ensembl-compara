01dfff5a7ab525150963b240cd70828baac7cb35
Evgeny Bragin <eb4@sanger.ac.uk>
2009-04-07 10:33:28 +0000

    *** empty log message ***

--- modules/EnsEMBL/Web/Data.pm
+++ modules/EnsEMBL/Web/Data.pm
@@ -276,7 +276,7 @@ sub has_many {
   
     my $real_accessor = '_'. $accessor;
     $class->SUPER::has_many($real_accessor => $relation_class);
-    $class->__meta_info->{has_many}->{$accessor} = $real_accessor;
+    $class->__meta_info->{has_many}->{$accessor} = $class->__meta_info->{has_many}->{$real_accessor};
     
     *{$class."::$accessor"} =
       sub {
