6df7013197ba6c4284a39ab3e2c803784dd5605c
Ensembl Account <ensembl@sanger.ac.uk>
2009-04-09 14:14:33 +0000

    Added 'id' to list of available fields in sub discover (ID is displayed in help interface, for creating help URLs).

--- modules/EnsEMBL/Web/Interface.pm
+++ modules/EnsEMBL/Web/Interface.pm
@@ -254,6 +254,7 @@ sub discover {
   my $self = shift;
   
   my %fields = %{ $self->data->get_all_fields };
+  $fields{'id'} = 'int';
   my %hasa_fields = %{ $self->data->hasa_relations };
 
   my (%elements, @element_order);
@@ -500,12 +501,14 @@ sub edit_fields {
   my $data = $self->data;
   my $dataview = $ENV{'ENSEMBL_FUNCTION'};
   my $element_order = $self->element_order;
+  warn "@@@ ELEMENTS @$element_order";
   my %has_many = %{ $self->data->hasmany_relations };
 
   ## populate widgets from Data_of{$self}
   foreach my $field (@$element_order) {
     my $element;
     $element = $self->element($field);
+    warn ">>> $field = $element";
     next unless $element;
 
     my %param = %{$element->widget};
