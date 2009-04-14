0f69c389a73a8f043b2437c780a82ede0c51ca8e
Leo Gordon <lg4@ebi.ac.uk>
2009-04-14 09:44:52 +0000

    a simplified version

--- scripts/family/LaunchMafftOnFamilies_batch.pl
+++ scripts/family/LaunchMafftOnFamilies_batch.pl
@@ -17,19 +17,13 @@ Options:
 -dbname family dbname
 -dbuser
 -dbpass
--family_stable_id|-family_id
--mafft_file
+-starting_family_id
+-family_stable_id
 -dir
 -store
 
 \n";
 
-my ($family_stable_id,$starting_family_id,$starting_family_id_file,$num_families);
-
-my $dir = ".";
-my $store = 0;
-
-#my $mafft_executable = "/nfs/acari/avilella/src/mafft/mafft-6.522/scripts/mafft";
 my $mafft_executable = "/software/ensembl/compara/mafft-6.522/bin/mafft";
 BEGIN {$ENV{MAFFT_BINARIES} = '/software/ensembl/compara/mafft-6.522'; }
 
@@ -44,9 +38,11 @@ my $port = "";
 my $dbname;
 my $dbuser;
 my $dbpass;
-my $mafft_file;
-my $fast = 0;
+my $dir = ".";
+my $store = 0;
 my $parttree = 0;
+my $fast = 0;
+my ($family_stable_id,$starting_family_id,$num_families);
 
 GetOptions('help' => \$help,
 	   'h|host=s' => \$host,
@@ -56,49 +52,31 @@ GetOptions('help' => \$help,
 	   'ps|dbpass=s' => \$dbpass,
 	   'fs|family_stable_id=s' => \$family_stable_id,
 	   'f|starting_family_id=s' => \$starting_family_id,
-	   'starting_family_id_file=s' => \$starting_family_id_file,
 	   'n|num_families=s' => \$num_families,
 	   'dir=s' => \$dir,
-           'mafft_file=s' => \$mafft_file,
 	   's|store' => \$store,
-           'parttree' => \$parttree,
-           'fast' => \$fast);
+       'parttree' => \$parttree,
+       'fast' => \$fast);
 
 if ($help) {
-  print $usage;
-  exit 0;
+    print $usage;
+    exit 0;
 }
-
-if (defined $mafft_file && $store == 0) {
-  die "If you provide a mafft_file, you should also set -store option\n"
-}
-
-my $rand;
 
 my $db = new Bio::EnsEMBL::Compara::DBSQL::DBAdaptor(-host   => $host,
                                                      -port   => $port,
                                                      -user   => $dbuser,
                                                      -pass   => $dbpass,
                                                      -dbname => $dbname);
-
-my $family;
-my $id;
-
-if (defined($starting_family_id_file)) {
-    open LIST, "$starting_family_id_file" or die "couldnt open family file $starting_family_id_file: $!\n";
-    while (<LIST>) {
-      chomp $_;
-      $starting_family_id = $_;
-    }
-    close LIST;
-}
+my $FamilyAdaptor = $db->get_FamilyAdaptor;
 
 die "Need to find starting_family_id + num_families: $!\n" unless (defined($starting_family_id) && defined($num_families));
+
 my $starting_id = $starting_family_id;
-my $end_id = $starting_family_id + $num_families;
-my $temp;
+my $end_id = $starting_family_id + $num_families - 1;
+
 if ($starting_id > $end_id) {
-  $temp = $starting_id;
+  my $temp = $starting_id;
   $starting_id = $end_id;
   $end_id = $temp;
 }
@@ -106,167 +84,116 @@ if ($starting_id > $end_id) {
 my $pep_dir = "$dir/pep";
 my $msc_dir = "$dir/msc";
 
-mkdir $pep_dir;
-mkdir $msc_dir;
-
-for my $family_id ($starting_id .. $end_id) {
-  $mafft_file = undef;
-  $id = $family_id;
-  my $FamilyAdaptor = $db->get_FamilyAdaptor;
-  $family = $FamilyAdaptor->fetch_by_dbID($family_id);
-  unless (defined($family)) {
-    print STDERR "Failed: $family_id\n";
-    next;
-  }
-
-  my $aln;
-  eval {$aln = $family->get_SimpleAlign};
-
-  unless ($@) {
-    my $flush = $aln->is_flush;
-    # print STDERR "Family $family_id already aligned\n";
-    next if (defined($flush));
-  }
-
-  $rand = time().rand(1000);
-  my $sb_file = "/tmp/sb.$rand";
-
-  unless (defined $mafft_file) {
-
-    my @members_attributes;
-
-    push @members_attributes,@{$family->get_Member_Attribute_by_source('ENSEMBLPEP')};
-    push @members_attributes,@{$family->get_Member_Attribute_by_source('Uniprot/SWISSPROT')};
-    push @members_attributes,@{$family->get_Member_Attribute_by_source('Uniprot/SPTREMBL')};
-
-    unless (0 < scalar(@members_attributes)) {
-      print STDERR "Failed: $family_id\n";
-      next;
-    }
-
-    open S, ">$sb_file";
-
-    foreach my $member_attribute (@members_attributes) {
-      my ($member,$attribute) = @{$member_attribute};
-      my $member_stable_id = $member->stable_id;
-      # print STDERR $member_stable_id,"\n";
-      print S ">$member_stable_id\n";
-      my $seq = $member->sequence;
-      $seq =~ s/(.{72})/$1\n/g;
-      chomp $seq;
-      unless (defined($seq)) {
-        print STDERR "Failed: $family_id\n";
-        next;
-      }
-      print S $seq,"\n";
-    }
-
-    close S;
-
-    # If only one member no need for a multiple alignment.
-    # Just load the sequence as it is in the family db
-
-    if (scalar @members_attributes == 1) {
-      my $FH = IO::File->new();
-      $FH->open($sb_file) || die "Could not open fasta file [$sb_file], $!\n;";
-
-      my $member_stable_id;
-      my $member_seq;
-
-      my $number_of_sequence = 0;
-
-      while (<$FH>) {
-        if (/^>(\S+)\s*.*$/) {
-          $member_stable_id = $1;
-          $number_of_sequence++;
-        } elsif (/^[a-zA-Z\*]+$/) { ####### add * for protein with stop in it!!!!
-          chomp;
-          $member_seq .= $_;
-        }
-      }
-
-      if ($number_of_sequence != 1) {
-        warn "For family " . $family->stable_id . " we get $number_of_sequence sequence instead of 1
-EXIT 1\n";
-        exit 1;
-      }
-
-      $FH->close;
-
-      if (defined $member_stable_id && defined $member_seq) {
-        my $MemberAdaptor = $db->get_MemberAdaptor;
-        my $member = $MemberAdaptor->fetch_by_source_stable_id(undef,$member_stable_id);
-        my $AttributeAdaptor = $db->get_AttributeAdaptor;
-        my $attribute = $AttributeAdaptor->fetch_by_Member_Relation($member,$family);
-        my $alignment_string = $member_seq;
-        $alignment_string =~ s/\-([A-Z])/\- $1/g;
-        $alignment_string =~ s/([A-Z])\-/$1 \-/g;
-        my @cigar_segments = split " ",$alignment_string;
-        my $cigar_line = "";
-        foreach my $segment (@cigar_segments) {
-          my $seglength = length($segment);
-          $seglength = "" if ($seglength == 1);
-          if ($segment =~ /^\-+$/) {
-            $cigar_line .= $seglength . "D";
-          } else {
-            $cigar_line .= $seglength . "M";
-          }
-        }
-        $member->sequence($member_seq);
-        eval { $attribute->cigar_line($cigar_line) };
-      
-        $FamilyAdaptor->update_relation([$member, $attribute]) unless ($@);
-      
-        undef $member_stable_id;
-        undef $member_seq;
-      } else {
-        warn "For family " . $family->stable_id . " member_stable_id or sequence are not defined
-EXIT 2\n";
-        exit 2;
-      }
-    
-      exit 0;
-    }
-
-  $mafft_file = "/tmp/mafft.$rand";
-
-  my $status;
-  my $extratags = '';
-  if($parttree) { $extratags .= " --parttree " }
-  if($fast) { $extratags .= " --retree 1 " }
-  print STDERR "### $mafft_executable $extratags $sb_file > $mafft_file.msc\n";
-  $status = system("$mafft_executable $extratags $sb_file > $mafft_file.msc");
-  
-    unless ($status == 0) {
-      unlink glob("/tmp/*$rand*");
-      print STDERR "Failed: $family_id\n";
-      next;
-      #      die "error in mafft, $!\n";
-    }
-  }
-
-
-  if ($store) {
-    $family->read_fasta("$mafft_file.msc");
-    my @members_attributes;
-
-    push @members_attributes,@{$family->get_Member_Attribute_by_source('ENSEMBLPEP')};
-    push @members_attributes,@{$family->get_Member_Attribute_by_source('Uniprot/SWISSPROT')};
-    push @members_attributes,@{$family->get_Member_Attribute_by_source('Uniprot/SPTREMBL')};
-
-    foreach my $member_attribute (@members_attributes) {
-      $FamilyAdaptor->update_relation($member_attribute);
-    }
-  } else {
-    unless (system("cp $mafft_file.msc $msc_dir/$id.msc") == 0) {
-      unlink glob("/tmp/*$rand*");
-      die "error in cp $id.out,$1\n";
-    }
-    system("cat $mafft_file.msc.log > $dir/$starting_id.msc.log");
-    system("cp $sb_file $pep_dir/$id.pep");
-    unlink glob("/tmp/*$rand*");
-    $mafft_file = undef;
-  }
+unless($store) {
+    mkdir $pep_dir;
+    mkdir $msc_dir;
 }
 
-1;
+my $failed = 0;
+
+for my $family_id ($starting_id .. $end_id) {
+
+    my $family = $FamilyAdaptor->fetch_by_dbID($family_id);
+    unless (defined($family)) {
+        print STDERR "Failed: family $family_id could not have been fetched by the adaptor\n";
+        $failed = 1;
+        next;
+    }
+
+    my $aln;
+    eval {$aln = $family->get_SimpleAlign};
+    unless ($@) {
+    my $flush = $aln->is_flush;
+        # print STDERR "Family $family_id already aligned\n";
+        next if (defined($flush));
+    }
+
+    my @members_attributes = ();
+
+    push @members_attributes,@{$family->get_Member_Attribute_by_source('ENSEMBLPEP')};
+    push @members_attributes,@{$family->get_Member_Attribute_by_source('Uniprot/SWISSPROT')};
+    push @members_attributes,@{$family->get_Member_Attribute_by_source('Uniprot/SPTREMBL')};
+
+    if(scalar @members_attributes == 0) {
+        print STDERR "Failed: family $family_id does not seem to contain any members\n";
+        $failed = 1;
+        next;
+
+    } elsif(scalar @members_attributes == 1) {    # the simple singleton case: just load the fake cigar_line
+
+        my ($member,$attribute) = @{$members_attributes[0]};
+
+        my $cigar_line = length($member->sequence).'M';
+        eval { $attribute->cigar_line($cigar_line) };
+        if($@) {
+            print STDERR "Failed: could not set the cigar_line for singleton family $family_id, because: $@\n";
+            $failed = 1;
+            next;
+        } elsif($store) {
+            $FamilyAdaptor->update_relation([$member, $attribute]);
+        }
+        
+        next;
+    }
+
+    # otherwise prepare the files and perform the actual mafft run:
+
+    my $rand = time().rand(1000);
+    my $pep_file = "/tmp/family_${family_id}.pep.$rand";
+
+    open PEP, ">$pep_file";
+
+    foreach my $member_attribute (@members_attributes) {
+        my ($member,$attribute) = @{$member_attribute};
+        my $member_stable_id = $member->stable_id;
+        my $seq = $member->sequence;
+
+        print PEP ">$member_stable_id\n";
+        $seq =~ s/(.{72})/$1\n/g;
+        chomp $seq;
+        unless (defined($seq)) {
+            print STDERR "Failed: member $member_stable_id in family $family_id doesn't have a sequence\n";
+            $failed = 1;
+            next;
+        }
+        print PEP $seq,"\n";
+    }
+
+    close PEP;
+
+    my $mafft_file = "/tmp/family_${family_id}.mafft.$rand";
+
+    my $mafft_args = '';
+    if($parttree) { $mafft_args .= " --parttree " }
+    if($fast)     { $mafft_args .= " --retree 1 " }
+
+    my $cmd_line = "$mafft_executable $mafft_args $pep_file > $mafft_file";
+    print STDERR "About to execute: $cmd_line\n";
+
+    if(system($cmd_line)) {
+        print STDERR "Failed: running mafft on family $family_id failed, because: $!\n";
+        $failed = 1;
+        next;
+    }
+
+    if ($store) {
+        $family->load_cigars_from_fasta("$mafft_file", 1);
+    } else {
+        if(system("cp $mafft_file $msc_dir/$family_id.msc")) {
+            print STDERR "Failed: could not copy '$mafft_file', because: $!\n";
+            $failed = 1;
+            next;
+        }
+        system("cat $mafft_file.log > $dir/family_${family_id}.log");
+        if(system("cp $pep_file $pep_dir/family_${family_id}.pep")) {
+            print STDERR "Failed: could not copy '$pep_file', because: $!\n";
+            $failed = 1;
+            next;
+        }
+    }
+
+        # the files will be removed on success and should stay undeleted on failure
+    unlink $pep_file, $mafft_file;
+}
+
+exit($failed);
