0d92b8f73dba23a02b365a4888361d3ead4f2ff6
Anne Parker <ap5@sanger.ac.uk>
2009-04-14 13:54:51 +0000

    Updated URL redirection

--- modules/EnsEMBL/Web/Command/Account/ShareRecord.pm
+++ modules/EnsEMBL/Web/Command/Account/ShareRecord.pm
@@ -17,9 +17,9 @@ sub process {
   my $object = $self->object;
 
   my $user = $EnsEMBL::Web::RegObj::ENSEMBL_WEB_REGISTRY->get_user;
-  my $type = lc($object->param('type')).'s';
+  my $accessor = lc($object->param('type')).'s';
 
-  my ($records_accessor) = grep { $_ eq $type } keys %{ $user->relations };
+  my ($records_accessor) = grep { $_ eq $accessor } keys %{ $user->relations };
   ## TODO: this should use abstraction limiting facility rather then grep
 
   my ($user_record)      = grep { $_->id == $object->param('id') } $user->$records_accessor;
@@ -30,11 +30,29 @@ sub process {
     my $add_to_accessor = 'add_to_'. $records_accessor;
     my $clone = $user_record->clone;
     $group->$add_to_accessor($user_record->clone);
-  } else {
-    ## TODO: error exception
+  } 
+  #else {
+  #  ## TODO: error exception
+  #}
+  my $url;
+  my $param = {
+    'id' => $group->id,
+    '_referer' => $object->param('_referer'),
+    'x_requested_with' => $object->param('x_requested_with'),
+  };
+  if ($user->is_administrator_of($group)) {
+    $url = '/Account/Group/List';
   }
-  
-  $self->ajax_redirect('/Account/Group/Display' {'id'=>$object->param('webgroup_id')});
+  else {
+    $url = '/Account/MemberGroups';
+  } 
+ 
+  if ($object->param('x_requested_with')) {
+    $self->ajax_redirect($url, $param);
+  }
+  else {
+    $object->redirect($url, $param);
+  }
 }
 
 }
