4310893b1c60df12ecb28e99b699d50259cd34e2
Anne Parker <ap5@sanger.ac.uk>
2009-04-14 10:57:02 +0000

    Updated to match revised data model

--- modules/EnsEMBL/Web/Command/Account/Interface/Annotation.pm
+++ modules/EnsEMBL/Web/Command/Account/Interface/Annotation.pm
@@ -30,14 +30,15 @@ sub process {
   ## Customization
   $interface->caption({add  => 'Create annotation'});
   $interface->caption({edit => 'Edit annotation'});
-  $interface->permit_delete('yes');
+  #$interface->permit_delete('yes');
   $interface->option_columns([qw/stable_id title/]);
-  $interface->modify_element('title',      {type => 'String', label =>'Title'});
-  $interface->modify_element('annotation', {type =>'Text'   , label =>'Annotation notes'});
-  $interface->modify_element('stable_id',  {type =>'NoEdit' , label =>'Stable ID'});
-  $interface->modify_element('url',        {type =>'Hidden'});
-  $interface->modify_element('owner_type', {type => 'Hidden'});
-  $interface->element_order([qw/stable_id title annotation url owner_type/]);
+  $interface->modify_element('title',       {type => 'String', label =>'Title'});
+  $interface->modify_element('annotation',  {type =>'Text'   , label =>'Annotation notes'});
+  $interface->modify_element('stable_id',   {type =>'NoEdit' , label =>'Stable ID'});
+  $interface->modify_element('ftype',       {type =>'Hidden'});
+  $interface->modify_element('species',     {type =>'Hidden'});
+  $interface->modify_element('owner_type',  {type => 'Hidden'});
+  $interface->element_order([qw/stable_id title annotation ftype species owner_type/]);
 
   ## Render page or munge data, as appropriate
   $interface->configure($self->webpage, $object);
