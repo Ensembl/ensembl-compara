b94aacd686045761c1c67b10e31ba002799c8479
Simon Brent <sb23@sanger.ac.uk>
2009-04-20 08:50:18 +0000

    restrict dropdown for new variation compara view

--- modules/EnsEMBL/Web/Component/Compara_AlignSliceSelector.pm
+++ modules/EnsEMBL/Web/Component/Compara_AlignSliceSelector.pm
@@ -15,7 +15,7 @@ sub _init {
 sub content {
   my $self = shift;
   my $object = $self->object;
-  my $url = $object->_url({}, 1 );
+  my $url = $object->_url({ align => undef }, 1 );
   my $extra_inputs;
   
   foreach (sort keys %{$url->[1]||{}}) {
@@ -44,36 +44,39 @@ sub content {
       escapeHTML($row->{'name'});
   }
   
- $options .= qq{
-        <optgroup label="Pairwise alignments">};
+  # For the variation compara view, only allow multi-way alignments
+  if ($object->type ne 'Variation') {
+    $options .= qq{
+          <optgroup label="Pairwise alignments">};
 
-  my $species_hash = {};
-  
-  foreach my $i (keys %$hash) {
-    foreach (keys %{$hash->{$i}->{'species'}}) {
-      if ($hash->{$i}->{'class'} =~ /pairwise/ && $hash->{$i}->{'species'}->{$species} && $_ ne $species) {
-        my $type = lc $hash->{$i}->{'type'};
-        
-        $type =~ s/_net//i;
-        $type =~ s/_/ /g;
-        
-        $species_hash->{$object->species_defs->species_label($_, 1) . "###$type"} = $i;
-      }
-    } 
-  }
-  
-  foreach (sort { $a cmp $b } keys %$species_hash) {
-    my ($name, $type) = split (/###/, $_);
+    my $species_hash = {};
     
-    $options .= sprintf '
-          <option value="%d"%s>%s</option>',
-      $species_hash->{$_},
-      $species_hash->{$_} eq $align ? ' selected="selected"' : '',
-      "$name - $type";
+    foreach my $i (keys %$hash) {
+      foreach (keys %{$hash->{$i}->{'species'}}) {
+        if ($hash->{$i}->{'class'} =~ /pairwise/ && $hash->{$i}->{'species'}->{$species} && $_ ne $species) {
+          my $type = lc $hash->{$i}->{'type'};
+          
+          $type =~ s/_net//i;
+          $type =~ s/_/ /g;
+          
+          $species_hash->{$object->species_defs->species_label($_, 1) . "###$type"} = $i;
+        }
+      } 
+    }
+    
+    foreach (sort { $a cmp $b } keys %$species_hash) {
+      my ($name, $type) = split (/###/, $_);
+      
+      $options .= sprintf '
+            <option value="%d"%s>%s</option>',
+        $species_hash->{$_},
+        $species_hash->{$_} eq $align ? ' selected="selected"' : '',
+        "$name - $type";
+    }
+    
+    $options .= qq{
+          </optgroup>};
   }
-  
- $options .= qq{
-        </optgroup>};
   
   
   ## Get the species in the alignment and turn on the approriate Synteny tracks!
