2324b01da85369fa48a9a2b3d635509713969e2e
Simon Brent <sb23@sanger.ac.uk>
2009-04-22 11:13:59 +0000

    turn off flanking sequence

--- modules/EnsEMBL/Web/ViewConfig/Location/Compara_Alignments.pm
+++ modules/EnsEMBL/Web/ViewConfig/Location/Compara_Alignments.pm
@@ -4,7 +4,17 @@ use strict;
 
 use EnsEMBL::Web::ViewConfig::Gene::Compara_Alignments;
 
-sub init { EnsEMBL::Web::ViewConfig::Gene::Compara_Alignments::init(@_); }
+sub init { 
+  my $view_config = shift;
+  
+  EnsEMBL::Web::ViewConfig::Gene::Compara_Alignments::init($view_config);
+  
+  $view_config->{'no_flanking'} = 1;
+  $view_config->_set_defaults(qw(
+    flank5_display  0 
+    flank3_display  0
+  )); 
+}
 sub form { EnsEMBL::Web::ViewConfig::Gene::Compara_Alignments::form(@_); }
 
 1;
