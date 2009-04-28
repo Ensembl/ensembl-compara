be6f0db207342807a8fe776e29f2c8260cf56ae5
Anne Parker <ap5@sanger.ac.uk>
2009-04-28 12:24:52 +0000

    Additional messages needed by userdata pages

--- modules/EnsEMBL/Web/Filter/Data.pm
+++ modules/EnsEMBL/Web/Filter/Data.pm
@@ -14,9 +14,10 @@ sub BUILD {
   my ($self, $ident, $args) = @_;
   ## Set the messages hash here
   $self->set_messages({
-    'no_file' => 'No data was uploaded. Please try again.',
     'no_url'  => 'No URL was entered. Please try again.',
+    'no_response' => 'We were unable to access your data file. Please check the file path and try again. If you continue to get this message, your file may be too large for us to upload.',
     'empty'   => 'Your file appears to be empty. Please check that it contains correctly-formatted data.',
+    'too_big' => 'Your file is too big to parse. Please select a smaller file.',
     'no_save' => 'Your data could not be saved. Please check the file contents and try again.',
   });
 }
