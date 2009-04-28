02c5d8700960ebf1362eb7cc060e79cfa5fe9b3e
Anne Parker <ap5@sanger.ac.uk>
2009-04-28 12:18:43 +0000

    Added in ability to check for existing shares, but needs more work on Data::Record

--- modules/EnsEMBL/Web/Command/UserData/CheckShare.pm
+++ modules/EnsEMBL/Web/Command/UserData/CheckShare.pm
@@ -15,33 +15,68 @@ sub BUILD {
 
 sub process {
   my $self = shift;
-  my $url = '/'.$self->object->data_species.'/UserData/';
-  my $param;
+  my $object = $self->object;
+  my $url = '/'.$object->data_species.'/UserData/';
+  my $param = {
+    '_referer' => $object->param('_referer'),
+    'x_requested_with' => $object->param('x_requested_with'),
+  };
 
-  my @shares = ($self->object->param('share_id'));
-  foreach my $code (@shares) {
-    if ($code !~ /^d+$/) {
-      my $data = $self->object->get_session->get_data(type => 'upload', code => $code);
-      if ($data->{filename}) {
-        if (my $ref = $self->object->store_data(type => 'upload', code => $code)) {
-          @shares = grep {$_ ne $code} @shares;
-          push @shares, $ref;
-        } else {
-          $param->{'filter_module'} = 'Data';
-          $param->{'filter_code'} = 'no_save';
+  if (my $group_id = $object->param('webgroup_id')) { ## Share with group
+    ## Check if it is already shared
+    my @ids = ($object->param('share_id'));
+    my @shareables;
+    my $group = EnsEMBL::Web::Data::Group->new($group_id);
+    my $accessor = $object->param('type').'s';
+    foreach my $id (@ids) {
+      warn "CHECKING ID $id";
+      my $shared = $group->$accessor('cloned_from' => $id);
+      push @shareables, $id unless $shared;
+    }
+    if (@shareables) {
+      $url .= 'ShareRecord';
+      $param->{'webgroup_id'} = $group_id;
+      $param->{'id'} = \@shareables;
+      $param->{'type'} = $object->param('type');
+    }
+    else {
+      $url .= 'SelectShare';
+      $param->{'filter_module'} = 'Shareable';
+      $param->{'filter_code'} = 'shared';
+    }
+  }
+  else { ## Share via URL
+    my @shares = ($object->param('share_id'));
+    foreach my $code (@shares) {
+      if ($code !~ /^d+$/) {
+        my $data = $object->get_session->get_data(type => 'upload', code => $code);
+        if ($data->{filename}) {
+          if (my $ref = $object->store_data(type => 'upload', code => $code)) {
+            @shares = grep {$_ ne $code} @shares;
+            push @shares, $ref;
+          } 
+          else {
+            $param->{'filter_module'} = 'Data';
+            $param->{'filter_code'} = 'no_save';
+          }
         }
       }
     }
-  }
-  if (@shares) {
-    $url .= 'ShareURL';
-    $param->{'share_id'} = \@shares;
-  }
-  else {
-    $url .= 'SelectShare';
+    if (@shares) {
+      $url .= 'ShareURL';
+      $param->{'share_id'} = \@shares;
+    }
+    else {
+      $url .= 'SelectShare';
+    }
   }
 
-  $self->ajax_redirect($url, $param); 
+  if ($object->param('x_requested_with')) {
+    $self->ajax_redirect($url, $param);
+  }
+  else {
+    $object->redirect($url, $param);
+  }
 
 }
 
