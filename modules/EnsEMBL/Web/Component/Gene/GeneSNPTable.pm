d5595fc2e58e81cd2030287d6b27445edf5c388c
Beth Yates <bp1@sanger.ac.uk>
2009-05-01 15:20:37 +0000

    changed column heading as requested by a user

--- modules/EnsEMBL/Web/Component/Gene/GeneSNPTable.pm
+++ modules/EnsEMBL/Web/Component/Gene/GeneSNPTable.pm
@@ -35,7 +35,7 @@ sub content {
       { 'key' => 'chr' , 'title' => 'Chr: bp',  },
       { 'key' => 'Alleles', 'align' => 'center' },
       { 'key' => 'Ambiguity', 'align' => 'center', },
-      { 'key' => 'aachange', 'title' => 'AA', 'align' => 'center' },
+      { 'key' => 'aachange', 'title' => 'Amino Acid', 'align' => 'center' },
       { 'key' => 'aacoord',  'title' => 'AA co-ordinate', 'align' => 'center' },
       { 'key' => 'class', 'title' => 'Class', 'align' => 'center' },
       { 'key' => 'Source', },
