448b52e8af84ac11bad7ae3331cf45386052f006
Steve Trevanion <st3@sanger.ac.uk>
2009-05-08 17:27:02 +0000

    for new webcode

--- modules/Bio/EnsEMBL/GlyphSet/annotation_status.pm
+++ modules/Bio/EnsEMBL/GlyphSet/annotation_status.pm
@@ -2,32 +2,27 @@ package Bio::EnsEMBL::GlyphSet::annotation_status;
 use strict;
 use base qw(Bio::EnsEMBL::GlyphSet_simple);
 
+#annotation status display for vega
+
 sub features {
   my $self = shift;
-  
-  my @features;
-  push @features,
-      @{ $self->{'container'}->get_all_MiscFeatures('NoAnnotation') },
-      @{ $self->{'container'}->get_all_MiscFeatures('CORFAnnotation') };
-
-
+  my @features = @{ $self->{'container'}->get_all_MiscFeatures('NoAnnotation') };
   foreach my $f (@features) {
     my ($ms) = @{ $f->get_all_MiscSets('NoAnnotation') };
-       ($ms) = @{ $f->get_all_MiscSets('CORFAnnotation') } unless $ms;
     $f->{'_miscset_code'} = $ms->code;
   }
-  
   return \@features;
 }
 
-sub colour
-  my( $self, $f ) = @_;
-  return $self->my_colour( $f->{'_miscset_code'} );
+sub colour_key {
+  my $self = shift;
+  return 'NoAnnotation';
 }
 
 sub feature_label {
   return undef;
 }
+
 sub title {
   my( $self, $f ) = @_;
   return $self->my_colour( $f->{'_miscset_code'}, 'text' );
@@ -40,13 +35,14 @@ sub href {
 
 sub tag {
   my ($self, $f) = @_;
-
+  my $colour = $self->my_colour( $f->{'_miscset_code'},'join' );
   return {
-    'style'   => 'join',
-    'tag'     => $f->{'start'}.'-'.$f->{'end'},
-    'colour'  => $self->my_colour( $f->{'_miscset_code'} )
-    'zindex'  => -20,
+    'style'  => 'join',
+    'tag'    => $f->{'start'}.'-'.$f->{'end'},
+    'colour' => $colour,
+    'zindex' => -20,
   };
 }
+
 
 1;
