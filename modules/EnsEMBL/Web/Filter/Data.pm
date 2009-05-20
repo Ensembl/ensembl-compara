2370174f167c7f73159c7467cbb61f33adfdf6f3
Anne Parker <ap5@sanger.ac.uk>
2009-05-20 13:12:41 +0000

    Added a message for invalid mime type

--- modules/EnsEMBL/Web/Filter/Data.pm
+++ modules/EnsEMBL/Web/Filter/Data.pm
@@ -15,7 +15,8 @@ sub BUILD {
   ## Set the messages hash here
   $self->set_messages({
     'no_url'  => 'No URL was entered. Please try again.',
-    'no_response' => 'We were unable to access your data file. Please check the file path and try again. If you continue to get this message, there may be an network issue, or your file may be too large for us to upload.',
+    'no_response' => 'We were unable to access your data file. If you continue to get this message, there may be an network issue, or your file may be too large for us to upload.',
+    'invalid_mime_type' => 'Your file does not appear to be in a valid format. Please try again.',
     'empty'   => 'Your file appears to be empty. Please check that it contains correctly-formatted data.',
     'too_big' => 'Your file is too big to parse. Please select a smaller file.',
     'no_save' => 'Your data could not be saved. Please check the file contents and try again.',
