9b3f7614b7a64d3218be5623e472e300fd9de874
Anne Parker <ap5@sanger.ac.uk>
2009-05-21 13:53:12 +0000

    Fixed problems caused by addition of missing 'strict'

--- modules/EnsEMBL/Web/Document/HTML/HomeSearch.pm
+++ modules/EnsEMBL/Web/Document/HTML/HomeSearch.pm
@@ -8,24 +8,7 @@ use base qw(EnsEMBL::Web::Document::HTML);
 
 use EnsEMBL::Web::RegObj;
 
-sub new {
-  return shift->SUPER::new(
-    '_home_url' => $ENSEMBL_WEB_REGISTRY->species_defs->ENSEMBL_WEB_ROOT,
-    '_default'  => $ENSEMBL_WEB_REGISTRY->species_defs->ENSEMBL_DEFAULT_SEARCHCODE,
-  );
-}
-
-sub home_url { return $_[0]{'_home_url'};  }
-
-sub default_search_code { return $_[0]{'_default'}; }
-
-sub search_url {
-    my $species = $_[0]->home_url.$ENV{'ENSEMBL_SPECIES'};
-    return $species ? "$species/psychic" : '/common/psychic';
-}
-
 sub render {
-  my $self = shift;
   my $species_defs = $ENSEMBL_WEB_REGISTRY->species_defs;
   my $page_species = $ENV{'ENSEMBL_SPECIES'};
   $page_species = '' if $page_species eq 'common';
@@ -37,12 +20,15 @@ sub render {
   my $html = q(
 <div class="center">);
 
+  my $search_url = $species_defs->ENSEMBL_WEB_ROOT.'/'.$page_species.'/psychic';
+  my $default_search_code = $species_defs->ENSEMBL_DEFAULT_SEARCHCODE;
+
   $html .= sprintf q(
   <h2 class="first">Search %s %s</h2>), $species_defs->ENSEMBL_SITETYPE, $species_name unless $species_name;
   $html .= sprintf q(
   <form action="%s" method="get"><div>
     <input type="hidden" name="site" value="%s" />),
-  $self->search_url, $self->default_search_code;
+  $search_url, $default_search_code;
 
   my $input_size = 50;
 
