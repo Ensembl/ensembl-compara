76d5a06eb28e17d1f5797e478ad58047128bcefe
Anne Parker <ap5@sanger.ac.uk>
2009-05-26 10:49:40 +0000

    New module allowing configuration of 'mapview', so we can show userdata on a single chromosome

---
+++ modules/EnsEMBL/Web/ViewConfig/Location/Chromosome.pm
@@ -0,0 +1,32 @@
+package EnsEMBL::Web::ViewConfig::Location::Chromosome;
+
+use strict;
+use warnings;
+no warnings 'uninitialized';
+
+sub init {
+### Used by Constructor
+### init function called to set defaults for the passed
+### {{EnsEMBL::Web::ViewConfig}} object
+
+  my( $view_config ) = @_;
+
+  $view_config->_set_defaults(qw(
+    panel_top      yes
+    panel_zoom      no
+    zoom_width    100
+    context       1000
+  ));
+  $view_config->add_image_configs({qw(
+    Vmapview      nodas
+  )});
+  $view_config->default_config = 'Vmapview';
+  $view_config->storable       = 1;
+  $view_config->can_upload = 1;
+}
+
+sub form {
+
+}
+
+1;
