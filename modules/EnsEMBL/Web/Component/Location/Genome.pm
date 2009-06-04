ebe01ae4c1952949e602ada0a9297b5cd828acc3
Anne Parker <ap5@sanger.ac.uk>
2009-06-04 12:31:54 +0000

    Added missing line back in

--- modules/EnsEMBL/Web/Component/Location/Genome.pm
+++ modules/EnsEMBL/Web/Component/Location/Genome.pm
@@ -75,6 +75,7 @@ sub content {
         my $pointer_ref = $image->add_pointers( $object, {
 	        'config_name'   => 'Vkaryotype',
 	        'zmenu_config'  => $zmenu_config,
+          'features'      => $set->[0],
 	        'feature_type'  => $set->[2],
           'color'         => $defaults->[0],
           'style'         => $defaults->[1],
