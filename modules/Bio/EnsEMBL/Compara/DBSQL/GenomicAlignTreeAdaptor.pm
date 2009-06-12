4e7bf3d58def8db9c8eb17cc7462ce0c5c6ba2e8
Javier Herrero <jh7@sanger.ac.uk>
2009-06-12 14:20:50 +0000

    Implement restrict option for fetch_all_by_MLSS_Slice and fetch_all_by_MLSS_DnaFrag

--- modules/Bio/EnsEMBL/Compara/DBSQL/GenomicAlignTreeAdaptor.pm
+++ modules/Bio/EnsEMBL/Compara/DBSQL/GenomicAlignTreeAdaptor.pm
@@ -171,6 +171,18 @@ sub fetch_all_by_MethodLinkSpeciesSet_DnaFrag {
     push(@$genomic_align_trees, $root);
   }
 
+  if (defined($start) and defined($end) and $restrict) {
+    my $restricted_genomic_align_trees = [];
+    foreach my $this_genomic_align_tree (@$genomic_align_trees) {
+      $this_genomic_align_tree = $this_genomic_align_tree->restrict_between_reference_positions(
+          $start, $end, undef, "skip_empty_genomic_aligns");
+      if (@{$this_genomic_align_tree->get_all_leaves()} > 1) {
+        push(@$restricted_genomic_align_trees, $this_genomic_align_tree);
+      }
+    }
+    $genomic_align_trees = $restricted_genomic_align_trees;
+  }
+
   return $genomic_align_trees;
 }
 
