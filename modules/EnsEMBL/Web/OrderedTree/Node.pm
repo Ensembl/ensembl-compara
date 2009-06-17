01acc3134d3adaa982883fd634a35cda29087489
James Smith <js5@sanger.ac.uk>
2009-06-17 14:14:30 +0000

    doc fix

--- modules/EnsEMBL/Web/OrderedTree/Node.pm
+++ modules/EnsEMBL/Web/OrderedTree/Node.pm
@@ -307,7 +307,7 @@ sub add_before {
 }
 
 sub add_after {
-### Splice in subtree $node as sibling before current node!
+### Splice in subtree $node as sibling after current node!
   my( $self, $node ) = @_;
   return undef if $self->is_descendant_of( $node ); # Cannot splice tree as part of parent
   $node->_node()->{parent_key} = $self->parent_key;
