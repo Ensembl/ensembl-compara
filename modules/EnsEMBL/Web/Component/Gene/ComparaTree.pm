ad058da322d35f5163fcf2a524b307a965624997
Javier Herrero <jh7@sanger.ac.uk>
2009-06-25 10:26:37 +0000

    Implement FG and BG colouring of gene trees

--- modules/EnsEMBL/Web/Component/Gene/ComparaTree.pm
+++ modules/EnsEMBL/Web/Component/Gene/ComparaTree.pm
@@ -56,6 +56,32 @@ sub content {
   my $wuc          = $object->image_config_hash( 'genetreeview' );
   my $image_width  = $self->image_width || 800;
   my $collapsability = $object->param('collapsability') || 'gene';
+  my $colouring    = $object->param('colouring') || 'background';
+  my @hidden_clades  = grep {$_ =~ /^group_/ and $object->param($_) eq "hide"} $object->param();
+  my @collapsed_clades  = grep {$_ =~ /^group_/ and $object->param($_) eq "collapse"} $object->param();
+
+  my $hidden_taxa;
+  my $hidden_genes_counter = 0;
+  if (@hidden_clades) {
+    $hidden_taxa = "_";
+    foreach my $clade (@hidden_clades) {
+      my ($clade_name) = $clade =~ /group_([\w\-]+)/;
+      $hidden_taxa .= $object->param("group_${clade_name}_taxa") . "_";
+    }
+    my $leaves = $tree->get_all_leaves;
+    foreach my $this_leaf (@$leaves) {
+      my $taxon_id = $this_leaf->genome_db->taxon_id;
+      next if ($taxon_id == $member->genome_db->taxon_id);
+      if ($hidden_taxa =~ /_${taxon_id}_/) {
+        $hidden_genes_counter++;
+        $this_leaf->disavow_parent;
+        $tree = $tree->minimize_tree;
+      }
+    }
+    if ($hidden_genes_counter) {
+      print $self->_info("Hidden genes", "There are $hidden_genes_counter hidden genes in the tree. Use the 'configure page' link in the left panel to change the options.");
+    }
+  }
 
   $wuc->set_parameters({
     'container_width'   => $image_width,
@@ -80,7 +106,54 @@ sub content {
     $collapsed_nodes ||= '';
   }
 
+  # print $self->_info("Collapsed nodes",  join(" -- ", split(",", $collapsed_nodes)));
+  if (@collapsed_clades) {
+    foreach my $clade (@collapsed_clades) {
+      my ($clade_name) = $clade =~ /group_([\w\-]+)/;
+      my $extra_collapsed_nodes = _find_nodes_by_taxa($tree,
+          [split("_", $object->param("group_${clade_name}_taxa"))], "internal");
+      if (%$extra_collapsed_nodes) {
+        $collapsed_nodes .= "," if ($collapsed_nodes);
+        $collapsed_nodes .= join(",", keys %$extra_collapsed_nodes);
+      }
+    }
+    # print $self->_info("Collapsed nodes",  join(" -- ", split(",", $collapsed_nodes)));
+  }
+
+  my $coloured_nodes;
+  if ($colouring eq "background") {
+    my @clades = grep {$_ =~ /^group_.+_bgcolour/ } $object->param();
+    # print $self->_info("BG colours ",  join(" -- ", split(@clades)));
+    foreach my $clade (@clades) {
+      my ($clade_name) = $clade =~ /group_(.+)_bgcolour/;
+      my $colour = $object->param("group_${clade_name}_bgcolour") || "magenta";
+      my $key = "$clade_name-bg-$colour";
+      my $taxa = [split("_", $object->param("group_${clade_name}_taxa"))];
+      my $these_coloured_nodes = _find_nodes_by_taxa($tree, $taxa);
+      if (%$these_coloured_nodes) {
+        $coloured_nodes->{$key} = [keys %$these_coloured_nodes];
+        # print $self->_info("Coloured nodes ($key)",  join(" -- ", @{$coloured_nodes->{$key}}));
+      }
+    }
+  } elsif ($colouring eq "foreground") {
+    my @clades = grep {$_ =~ /^group_.+_fgcolour/ } $object->param();
+    # print $self->_info("FG colours ",  join(" -- ", split(@clades)));
+    foreach my $clade (@clades) {
+      my ($clade_name) = $clade =~ /group_(.+)_fgcolour/;
+      my $colour = $object->param("group_${clade_name}_fgcolour") || "magenta";
+      my $key = "$clade_name-fg-$colour";
+      my $taxa = [split("_", $object->param("group_${clade_name}_taxa"))];
+      my $these_coloured_nodes = _find_nodes_by_taxa($tree, $taxa, "all");
+      if (%$these_coloured_nodes) {
+        $coloured_nodes->{$key} = [keys %$these_coloured_nodes];
+        # print $self->_info("Coloured nodes ($key)",  join(" -- ", @{$coloured_nodes->{$key}}));
+      }
+    }
+  }
+
   push @highlights, $collapsed_nodes || undef;
+
+  push @highlights, $coloured_nodes || undef;
 
   my $image  = $self->new_image
       ( $tree, $wuc, [@highlights] );
@@ -95,9 +168,11 @@ sub content {
   $image->{'panel_number'} = 'tree';
   $image->set_button( 'drag', 'title' => 'Drag to select region' );
 
+  my $info;
   my $li_tmpl = qq(
 <li><a href="%s">%s</a></li>);
   my @view_links;
+
   push @view_links, sprintf( $li_tmpl,
                              $object->_url({'collapse'=>$collapsed_to_gene}),
                              'View current gene only');
@@ -119,7 +194,7 @@ sub content {
 <small><ul>%s</ul></small>
 Use the 'configure page' link in the left panel to set the default. Further options are available from menus on individual tree nodes.</div>), join( '', @view_links) );
 
-  return $image->render . $view_options_html ;
+  return $image->render . $info . $view_options_html ;
 }
 
 sub _collapsed_nodes{
@@ -174,6 +249,43 @@ sub _collapsed_nodes{
   my $collapsed_node_ids
       = join( ',', grep{! $expanded_nodes{$_}} keys %collapsed_nodes );
   return $collapsed_node_ids;
+}
+
+sub _find_nodes_by_taxa {
+  my ($tree, $taxa, $mode) = @_;
+  my $node_ids = {};
+
+  if ($tree->is_leaf()) {
+    my $taxon_id = $tree->genome_db->taxon_id;
+    if (grep {$_ eq $taxon_id} @$taxa) {
+      $node_ids->{$tree->node_id} = 1;
+    }
+  } else {
+    my $tag = 1;
+    foreach my $this_child (@{$tree->children}) {
+      my $these_node_ids = _find_nodes_by_taxa($this_child, $taxa, $mode);
+      foreach my $node_id (keys %$these_node_ids) {
+        $node_ids->{$node_id} = 1;
+      }
+      $tag = 0 if (!$node_ids->{$this_child->node_id});
+    }
+    if ($mode eq "internal") {
+      foreach my $this_child (@{$tree->children}) {
+        delete($node_ids->{$this_child->node_id}) if $this_child->is_leaf();
+      }
+    }
+    if ($tag) {
+      unless ($mode eq "all") {
+        foreach my $this_child (@{$tree->children}) {
+          delete($node_ids->{$this_child->node_id});
+        }
+      }
+      $node_ids->{$tree->node_id} = 1;
+    }
+  }
+
+delete($node_ids->{403985}) if ($mode eq "internal");
+  return $node_ids;
 }
 
 sub content_align {
