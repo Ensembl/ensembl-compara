8761aa8be7853f9144bdebef687c74ec355fc387
Steve Trevanion <st3@sanger.ac.uk>
2009-06-30 13:39:31 +0000

    one panel, multiple images

--- modules/EnsEMBL/Web/Component/Location/MultiIdeogram.pm
+++ modules/EnsEMBL/Web/Component/Location/MultiIdeogram.pm
@@ -21,29 +21,19 @@ sub content {
   my $self = shift;
   my $object = $self->object;
   my $html;
-  $Data::Dumper::Maxdepth = 2;
   my $ploc = $object->[2][0];
   my $pslice = $ploc->slice;
-#  warn Dumper($pslice);
   my $counter = 1;
   my $max_count = @{$object->[2][1]} + 1;
   my $wuc = $object->image_config_hash( "chromosome_$counter","MultiIdeogram");
-  warn ref($wuc);
   $wuc->set_parameters({
     'container_width' => $object->seq_region_length,
     'image_width'     => $self->image_width,
     'slice_number'    => "1|$max_count",
     'caption'         => $object->seq_region_type.' '.$object->seq_region_name,
      });
-
-  my $image    = $self->new_image( $pslice, $wuc );
-#  warn Dumper($image);
-  return if $self->_export_image( $image );
-
-#  $image->{'panel_number'} = 'ideogram1';
-  $image->imagemap = 'yes';
-  $image->set_button( 'drag', 'title' => 'Click or drag to centre display' );
-  $html = '<p>.</p>'.$image->render;
+  my $images;
+  push @$images, ( $pslice, $wuc);
 
   foreach my $loc ( @{$object->[2][1]}) {
     $counter++;
@@ -55,13 +45,14 @@ sub content {
       'slice_number'    => "$counter|$max_count",
       'caption'         => $object->seq_region_type.' '.$object->seq_region_name,
     });
-    my $image = $self->new_image( $slice, $wuc );
-    $image->imagemap = 'yes';
-    $image->set_button( 'drag', 'title' => 'Click or drag to centre display' );
-
-    $html .= $image->render;
+    push @$images, ( $slice, $wuc);
     $counter++;
   }
+  my $image = $self->new_image( $images );
+  $image->imagemap = 'yes';
+  $image->set_button( 'drag', 'title' => 'Click or drag to centre display' );
+  return if $self->_export_image( $image );
+  $html .= $image->render;
   return $html;
 }
 
