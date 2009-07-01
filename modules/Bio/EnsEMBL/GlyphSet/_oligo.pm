3d35a586edf375c2b14aed85a13ad54c7c9db29e
Beth Yates <bp1@sanger.ac.uk>
2009-07-01 15:29:10 +0000

    changed to worg with funcgen API

--- modules/Bio/EnsEMBL/GlyphSet/_oligo.pm
+++ modules/Bio/EnsEMBL/GlyphSet/_oligo.pm
@@ -4,50 +4,75 @@ use strict;
 
 use base qw(Bio::EnsEMBL::GlyphSet::_alignment);
 
-sub features { ## Hack in db in the future!!
+sub features { 
   my ($self) = @_;
+  my $slice = $self->{'container'};
+  
+  my $fg_db = undef;
+  my $db_type  = $self->my_config('db_type')||'funcgen';
+  unless($self->{'container'}->isa("Bio::EnsEMBL::Compara::AlignSlice::Slice")) {
+    $fg_db = $self->{'container'}->adaptor->db->get_db_adaptor($db_type);
+    if(!$fg_db) {
+      warn("Cannot connect to $db_type db");
+      return [];
+    }
+  }; 
+  my $probe_feature_adaptor = $fg_db->get_ProbeFeatureAdaptor();
+
   $self->timer_push( 'Preped');
-  my $T = $self->{'container'}->get_all_OligoFeatures( $self->my_config('array') );
+  my ($vendor_name, $array_name ) = split (/:/, $self->my_config('array'));
+  #warn $self->{'container'};
+  my $T = $probe_feature_adaptor->fetch_all_by_Slice_array_vendor($slice, $array_name, $vendor_name);
   $self->timer_push( 'Retrieved oligos', undef, 'fetch' );
   return ( $self->my_config('array') => [$T] );
 }
 
 sub feature_group {
-  my( $self, $f ) = @_;
-  return $f->probeset;    ## For core features this is what the sequence name is...
+  my( $self, $f ) = @_; 
+  next unless ( $f && $f->isa('Bio::EnsEMBL::Funcgen::ProbeFeature'));
+  my ($vendor_name, $array_name ) = split (/:/, $self->my_config('array')); 
+  if ( $f->probeset_id) { 
+    return $f->probe->probeset->name;
+  } else { 
+    return $f->probe->get_probename($array_name);
+  }  
 }
 
 sub feature_label {
   my( $self, $f ) = @_;
-  return $f->probeset;
+  return $self->feature_group($f);
 }
 
 sub feature_title {
-  my( $self, $f ) = @_;
-  return "Probe set: ".$f->probeset;
+  my( $self, $f ) = @_; 
+  return $self->feature_group($f);
+#  return "Probe set: ".$f->probe->probeset->name;
 }
 
 sub href {
 ### Links to /Location/Feature with type of 'OligoProbe'
   my ($self, $f ) = @_;
-  return $self->_url({
-    'object' => 'Location',
-    'action' => 'Genome',
-    'db'     => $self->my_config('db'),
-    'ftype'  => 'OligoFeature',
-    'id'     => $f->probeset,
-    'array'  => $self->my_config( 'caption' ),
-  });
+  my ($vendor, $name ) = split (/:/, $self->my_config('array'));
+  return;
+
+#  return $self->_url({
+#    'object' => 'Location',
+#    'action' => 'Genome',
+#    'db'     => 'funcgen',
+#    'ftype'  => 'ProbeFeature',
+#    'id'     => $f->probe->probeset->name,
+#    'array'  => $name,
+#  });
 }
 
 sub export_feature {
   my $self = shift;
   my ($feature, $source) = @_;
-  
-  return $self->_render_text($feature, 'Oligo', {
-    'headers' => [ 'probeset' ],
-    'values' => [ $feature->can('probeset') ? $feature->probeset : '' ]
-  }, { 'source' => $source });
+  return; 
+#  return $self->_render_text($feature, 'Oligo', {
+#    'headers' => [ 'probeset' ],
+#    'values' => [ $feature->can('probeset') ? $feature->probeset : '' ]
+#  }, { 'source' => $source });
 }
 
-1;
+1
