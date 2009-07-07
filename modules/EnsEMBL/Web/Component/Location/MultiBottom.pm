f0c87be595a8915ed1fd90974bf1465b4a487fe2
Steve Trevanion <st3@sanger.ac.uk>
2009-07-07 09:43:09 +0000

    draw sp / chr labels consistently with ideogram

--- modules/EnsEMBL/Web/Component/Location/MultiBottom.pm
+++ modules/EnsEMBL/Web/Component/Location/MultiBottom.pm
@@ -5,7 +5,12 @@ use warnings;
 no warnings "uninitialized";
 use base qw(EnsEMBL::Web::Component::Location);
 
-use Data::Dumper;
+#use Data::Dumper;
+
+my %SHORT = qw(
+  chromosome Chr.
+  supercontig S'ctg
+);
 
 sub _init {
   my $self = shift;
@@ -13,7 +18,7 @@ sub _init {
   $self->ajaxable(  1 );
 }
 
-sub caption { 
+sub caption {
   my $self = shift;
   return 'Detailed View';
 }
@@ -26,14 +31,37 @@ sub content {
   my $pslice = $ploc->slice;
   my $counter = 1;
   my $other_locs = $object->other_locations;
+  my $base_url = $object->_url();
+#  warn $base_url;
   my $max_count = @{$other_locs} + 1;
   my $pwuc = $object->image_config_hash( "contigviewbottom_$counter", 'MultiBottom', $ploc->species);
   $pwuc->set_parameters({
     'container_width' => $object->length,
     'image_width'     => $self->image_width,
-    'slice_number'    => "1|$max_count",
+    'slice_number'    => 0,
     'caption'         => $object->seq_region_type.' '.$object->seq_region_name,
+    'multi'           => 1,
+    'compara'         => 'primary',
+    'base_url'        => $base_url,
   });
+  #add panel caption (displayed by scalebar glyphset)
+  my $type = $pslice->coord_system_name();
+  my $chr = $pslice->seq_region_name();
+  my $chr_raw = $chr;
+  unless( $chr =~ /^$type/i ) {
+    $type = $SHORT{lc($type)} || ucfirst( $type );
+    $chr = "$type $chr";
+  }
+  if( length($chr) > 9 ) {
+    $chr = $chr_raw;
+  }
+  (my $abbrev = $ploc->species ) =~ s/^(\w)\w+_(\w{3})\w+$/$1$2/g;
+  $chr = "$abbrev $chr"; 
+  $self->{'caption'} = $chr;
+  $pwuc->get_node('scalebar')->set('caption', $chr );
+
+  $pwuc->mult;
+#  $ploc->slice->{_config_file_name_} = $loc->real_species;
   my $images = [];
   push @$images, ($pslice, $pwuc) unless ($max_count > 2);
 
@@ -46,9 +74,30 @@ sub content {
       $wuc->set_parameters({
 	'container_width' => $slice->length,
 	'image_width'     => $self->image_width,
-	'slice_number'    => "$counter|$max_count",
+	'slice_number'    => $counter,
 	'caption'         => $object->seq_region_type.' '.$object->seq_region_name,
+	'multi'           => 1,
+	'compara'         => 'secondary',
       });
+
+      #add panel caption (displayed by scalebar glyphset)
+      my $type = $slice->coord_system_name();
+      my $chr = $slice->seq_region_name();
+      my $chr_raw = $chr;
+      unless( $chr =~ /^$type/i ) {
+	$type = $SHORT{lc($type)} || ucfirst( $type );
+	$chr = "$type $chr";
+      }
+      if( length($chr) > 9 ) {
+	$chr = $chr_raw;
+      }
+      (my $abbrev = $loc->{'real_species'} ) =~ s/^(\w)\w+_(\w{3})\w+$/$1$2/g;
+      $chr = "$abbrev $chr"; 
+      $self->{'caption'} = $chr;
+      $wuc->get_node('scalebar')->set('caption', $chr );
+
+      $wuc->mult;
+#      $loc->slice->{_config_file_name_} = $loc->{'real_species'};
       push @$images, ($slice, $wuc);
       push @$images, ($pslice, $pwuc) if ( ($max_count > 2) && ($counter < $max_count));
     }
--- modules/EnsEMBL/Web/Component/Location/MultiIdeogram.pm
+++ modules/EnsEMBL/Web/Component/Location/MultiIdeogram.pm
@@ -5,7 +5,12 @@ use warnings;
 no warnings "uninitialized";
 use base qw(EnsEMBL::Web::Component::Location);
 
-use Data::Dumper;
+#use Data::Dumper;
+
+my %SHORT = qw(
+  chromosome Chr.
+  supercontig S'ctg
+);
 
 sub _init {
   my $self = shift;
@@ -27,28 +32,73 @@ sub content {
   my $counter = 1;
   my $other_locs = $object->other_locations;
   my $max_count = @{$other_locs} + 1;
-  my $wuc = $object->image_config_hash( "chromosome_$counter", 'MultiIdeogram', $ploc->species);
-  $wuc->set_parameters({
+  my $species = $ploc->species;
+
+  #add panel caption (displayed by ideogram glyphset) - go into factory ?
+  my $type = $pslice->coord_system_name();
+  my $chr = $pslice->seq_region_name();
+  my $chr_raw = $chr;
+  unless( $chr =~ /^$type/i ) {
+    $type = $SHORT{lc($type)} || ucfirst( $type );
+    $chr = "$type $chr";
+  }
+  if( length($chr) > 9 ) {
+    $chr = $chr_raw;
+  }
+  (my $abbrev = $ploc->species ) =~ s/^(\w)\w+_(\w{3})\w+$/$1$2/g;
+  $chr = "$abbrev $chr";
+
+  my $pwuc = $object->image_config_hash( "chromosome_$counter", 'MultiIdeogram', $species );
+
+  $pwuc->container_width( $pslice->seq_region_length );
+  $pwuc->set_parameters({
     'container_width' => $object->seq_region_length,
     'image_width'     => $self->image_width,
     'slice_number'    => "1|$max_count",
-    'caption'         => $object->seq_region_type.' '.$object->seq_region_name,
   });
+
+#  warn "1. ",$self->image_width;
+#  warn "2. ",$object->param('image_width');
+
+  #send panel name to the ideogram glyphset
+  $pwuc->get_node('ideogram')->set('caption', $chr );
+
   my $images;
-  push @$images, ( $pslice, $wuc);
+  push @$images, ( $pslice, $pwuc);
 
   #add secondary slices
   if (@{$other_locs}) {
     foreach my $loc ( @{$other_locs}) {
       $counter++;
-      my $slice = $loc->{'slice'};
-      my $wuc = $object->image_config_hash( "chromosome_$counter", 'MultiIdeogram',$loc->{'real_species'} );
+      my $slice = $loc->{'slice'}; 
+      my $species = $loc->{'real_species'};
+
+      #add panel caption (displayed by ideogram glyphset) - go into factory ?
+      my $type = $slice->coord_system_name();
+      my $chr = $slice->seq_region_name();
+      my $chr_raw = $chr;
+      unless( $chr =~ /^$type/i ) {
+	$type = $SHORT{lc($type)} || ucfirst( $type );
+	$chr = "$type $chr";
+      }
+      if( length($chr) > 9 ) {
+	$chr = $chr_raw;
+      }
+      (my $abbrev = $species ) =~ s/^(\w)\w+_(\w{3})\w+$/$1$2/g;
+      $chr = "$abbrev $chr";
+      $self->{'caption'} = $chr;
+
+      my $wuc = $object->image_config_hash( "chromosome_$counter", 'MultiIdeogram', $species );
+      $wuc->container_width( $slice->seq_region_length );
+#      warn "chr length is ", $slice->seq_region_length;
+#      warn "slice_length = ",$slice->length;
+
       $wuc->set_parameters({
-	'container_width' => $slice->seq_region_length,
+#	'container_width' => $slice->seq_region_length,
 	'image_width'     => $self->image_width,
-	'slice_number'    => "$counter|$max_count",
-	'caption'         => $object->seq_region_type.' '.$object->seq_region_name,
+#	'slice_number'    => "$counter|$max_count",
       });
+      $wuc->get_node('ideogram')->set('caption', $chr );
       push @$images, ( $slice, $wuc);
       $counter++;
     }
