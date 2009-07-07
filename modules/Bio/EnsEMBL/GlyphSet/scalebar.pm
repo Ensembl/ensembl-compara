edbd0f719257f5867a28f26ca66136447c5ead94
Steve Trevanion <st3@sanger.ac.uk>
2009-07-07 09:51:45 +0000

    mcv stuff

--- modules/Bio/EnsEMBL/GlyphSet/scalebar.pm
+++ modules/Bio/EnsEMBL/GlyphSet/scalebar.pm
@@ -13,147 +13,168 @@ my %SHORT = qw(
 );
 
 sub _init {
-    my ($self) = @_;
-    #return unless ($self->strand() == -1);
+  my ($self) = @_;
+  #return unless ($self->strand() == -1);
 
-    my $Config         = $self->{'config'};
-    my $Container      = $self->{'container'};
-    
+  my $Config         = $self->{'config'};
+  my $Container      = $self->{'container'};
   my( $fontname, $fontsize ) = $self->get_font_details( 'innertext' );
 
-    my $contig_strand  = $Container->can('strand') ? $Container->strand : 1;
-    my $pix_per_bp     = $Config->transform()->{'scalex'};
-    my $h              = 0;
-    my $highlights     = $self->highlights();
+  my $contig_strand  = $Container->can('strand') ? $Container->strand : 1;
+  my $pix_per_bp     = $Config->transform()->{'scalex'};
+  my $h              = 0;
+  my $highlights     = $self->highlights();
 
-    my $black          = 'black';
-    my $highlights     = join('|',$self->highlights());
-    $highlights        = $highlights ? ";h=$highlights" : '';
- if( $self->{'config'}->{'compara'} ) { ## this is where we have to add in the other species....
-    my $C = 0;
+  my $black          = 'black';
+  my $highlights     = join('|',$self->highlights());
+  $highlights        = $highlights ? ";h=$highlights" : '';
+
+  #do stuff here for a multicontigview display (add lhs line and tags for primary compara species). Only display on forward strand
+  if( $self->{'config'}->{'_parameters'}{'compara'} && ($self->strand > 0) ) {
+
+    my $line = new Sanger::Graphics::Glyph::Rect({
+      'z' => 11,
+      'x' => -120,
+      'y' => 0,
+      'colour' => 'black',
+      'width' => 120,
+      'height' => 0,
+      'absolutex'     => 1,
+      'absolutewidth' => 1,
+      'absolutey'     => 1,
+    });
+    $self->join_tag( $line, "bracket", 0,0, 'black' );
+
+    if( $self->{'config'}->{'_parameters'}{'compara'} eq 'primary' ) {
+      $self->join_tag( $line, "bracket2", 0  ,0, 'rosybrown1', 'fill', -40 );
+      $self->join_tag( $line, "bracket2", 0.9,0, 'rosybrown1', 'fill', -40 );
+    }
+    $self->push($line);
+
+    my $C = 0; ###doubt this works
     foreach( @{ $self->{'config'}{'other_slices'}} ) {
       if( $C!= $self->{'config'}->{'slice_number'} ) {
         if( $C ) {
           if( $_->{'location'} ) {
             $highlights .= sprintf( ";s$C=%s;c$C=%s:%s:%s;w$C=%s", $_->{'species'}, 
-                         $_->{'location'}->seq_region_name, $_->{'location'}->centrepoint, $_->{'ori'}, $_->{'location'}->length );
+				    $_->{'location'}->seq_region_name, $_->{'location'}->centrepoint, $_->{'ori'}, $_->{'location'}->length );
           } else {
             $highlights .= sprintf( ";s$C=%s", $_->{'species'} ); 
           }
         } else {
           $highlights .= sprintf( ";c=%s:%s:1;w=%s", 
-                         $_->{'location'}->seq_region_name, $_->{'location'}->centrepoint, $_->{'location'}->length );
+				  $_->{'location'}->seq_region_name, $_->{'location'}->centrepoint, $_->{'location'}->length );
         }
       }
       $C++;
     }
- } ##
-    my $REGISTER_LINE  = $Config->get_parameter( 'opt_lines');
-    my $feature_colour = $self->my_config('col');
-    my $subdivs        = $self->my_config('subdivs');
-    my $max_num_divs   = $self->my_config('max_divisions') || 12;
-    my $navigation     = $self->my_config('navigation');
-    my $abbrev         = $self->my_config('abbrev');
-    my $param_string   = $Container->seq_region_name();
+  }
+  my $REGISTER_LINE  = $Config->get_parameter( 'opt_lines');
+  my $feature_colour = $self->my_config('col');
+  my $subdivs        = $self->my_config('subdivs');
+  my $max_num_divs   = $self->my_config('max_divisions') || 12;
+  my $navigation     = $self->my_config('navigation');
+  my $abbrev         = $self->my_config('abbrev');
+  my $param_string   = $Container->seq_region_name();
+  
+  my $main_width     = $Config->get_parameter(  'main_vc_width');
+  my $len            = $Container->length();
+  my $global_start   = $contig_strand < 0 ? -$Container->end() : $Container->start();
+  my $global_end     = $contig_strand < 0 ? -$Container->start() : $Container->end();
+  
+  my( $major_unit, $minor_unit );
 
-    my $main_width     = $Config->get_parameter(  'main_vc_width');
-    my $len            = $Container->length();
-    my $global_start   = $contig_strand < 0 ? -$Container->end() : $Container->start();
-    my $global_end     = $contig_strand < 0 ? -$Container->start() : $Container->end();
-
-    my( $major_unit, $minor_unit );
-
-    if( $len <= 51 ) {
-       $major_unit = 10;
-       $minor_unit = 1; 
+  if( $len <= 51 ) {
+    $major_unit = 10;
+    $minor_unit = 1; 
+  } else {
+    my $exponent = 10 ** int( log($len)/log(10) );
+    my $mantissa  = $len / $exponent;
+    if( $mantissa < 1.2 ) {
+      $major_unit = $exponent / 10 ;
+      $minor_unit = $major_unit / 5 ;
+    } elsif( $mantissa < 2.5 ) {
+      $major_unit = $exponent / 5 ;
+      $minor_unit = $major_unit / 4 ;
+    } elsif( $mantissa < 5 ) {
+      $major_unit = $exponent / 2 ;
+      $minor_unit = $major_unit / 5 ;
     } else {
-       my $exponent = 10 ** int( log($len)/log(10) );
-       my $mantissa  = $len / $exponent;
-       if( $mantissa < 1.2 ) {
-          $major_unit = $exponent / 10 ;
-          $minor_unit = $major_unit / 5 ;
-       } elsif( $mantissa < 2.5 ) {
-          $major_unit = $exponent / 5 ;
-          $minor_unit = $major_unit / 4 ;
-       } elsif( $mantissa < 5 ) {
-          $major_unit = $exponent / 2 ;
-          $minor_unit = $major_unit / 5 ;
-       } else {
-          $major_unit = $exponent;
-          $minor_unit = $major_unit / 5 ;
-       }
+      $major_unit = $exponent;
+      $minor_unit = $major_unit / 5 ;
     }
+  }
 
-    ## Now lets draw these....
+  ## Now lets draw these....
 
-    my $start = floor( $global_start / $minor_unit ) * $minor_unit;
-    my $filled = 1;
-    my $last_text_X = -1e20;
-    while( $start <= $global_end ) { 
-      my $end       = $start + $minor_unit - 1;
-      $filled = 1 - $filled;
-      my $box_start = $start < $global_start ? $global_start : $start;
-      my $box_end   = $end   > $global_end   ? $global_end   : $end;
-
-      ## Draw the glyph for this box!
-      my $t = $self->Rect({
-         'x'         => $box_start - $global_start, 
-         'y'         => 0,
-         'width'     => abs( $box_end - $box_start + 1 ),
-         'height'    => 3,
-         ( $filled == 1 ? 'colour' : 'bordercolour' )  => 'black',
-         'absolutey' => 1,
-      });
-      if ($navigation eq 'on' && $Config->{'compara'} ne 'secondary' ){
+  my $start = floor( $global_start / $minor_unit ) * $minor_unit;
+  my $filled = 1;
+  my $last_text_X = -1e20;
+  while( $start <= $global_end ) { 
+    my $end       = $start + $minor_unit - 1;
+    $filled = 1 - $filled;
+    my $box_start = $start < $global_start ? $global_start : $start;
+    my $box_end   = $end   > $global_end   ? $global_end   : $end;
+    
+    ## Draw the glyph for this box!
+    my $t = $self->Rect({
+      'x'         => $box_start - $global_start, 
+      'y'         => 0,
+      'width'     => abs( $box_end - $box_start + 1 ),
+      'height'    => 3,
+      ( $filled == 1 ? 'colour' : 'bordercolour' )  => 'black',
+      'absolutey' => 1,
+    });
+    if ($navigation eq 'on' && $Config->{'compara'} ne 'secondary' ){
 #        ($t->{'href'},$t->{'zmenu'}) = $self->interval( $param_string, $start, $end, $contig_strand, $global_start, $global_end-$global_start+1, $highlights);
-      } elsif( $navigation eq 'zoom' ) {
-#        ($t->{'href'},$t->{'zmenu'}) = $self->zoom_interval( $param_string, $start, $end, $contig_strand, $global_start, $main_width, $highlights, $global_end-$global_start);
-      }
-      $self->push($t);
-      if($start == $box_start ) { # This is the end of the box!
-        $self->join_tag( $t, "ruler_$start", 0, 0 , $start%$major_unit ? 'grey90' : 'grey80'  ) if($REGISTER_LINE);
-      }
-      if( ( $box_end==$global_end ) && !( ( $box_end+1) % $minor_unit ) ) {
-        $self->join_tag( $t, "ruler_end", 1, 0 , ($global_end+1)%$major_unit ? 'grey90' : 'grey80'  ) if($REGISTER_LINE);
-      }
-      unless( $box_start % $major_unit ) { ## Draw the major unit tick 
-        $self->push($self->Rect({
-            'x'         => $box_start - $global_start,
-            'y'         => 0,
-            'width'     => 0,
-            'height'    => 5,
-            'colour'    => 'black',
-            'absolutey' => 1,
-        }));
-        my $LABEL = $minor_unit < 1000 ? $self->commify($box_start * $contig_strand ): $self->bp_to_nearest_unit( $box_start * $contig_strand, 2 );
-        my($TXT,$PART,$W,$H) = $self->get_text_width(0,$LABEL,'','font'=>$fontname,'ptsize'=>$fontsize);
-        if( $last_text_X + $W/$pix_per_bp * 1.5 < $box_start ) {
-          $self->push($self->Text({
-            'x'         => $box_start - $global_start,
-            'y'         => 5,
-            'height'    => $H,
-            'font'      => $fontname,
-            'ptsize'    => $fontsize,
-            'halign'    => 'left',
-            'colour'    => $feature_colour,
-            'text'      => $LABEL,
-            'absolutey' => 1,
-          }));
-          $last_text_X = $box_start;
-        }
-      } 
-      $start += $minor_unit;
+    } elsif( $navigation eq 'zoom' ) {
+ #       ($t->{'href'},$t->{'zmenu'}) = $self->zoom_interval( $param_string, $start, $end, $contig_strand, $global_start, $main_width, $highlights, $global_end-$global_start);
     }
-    unless( ($global_end+1) % $major_unit ) { ## Draw the major unit tick 
+    $self->push($t);
+    if($start == $box_start ) { # This is the end of the box!
+      $self->join_tag( $t, "ruler_$start", 0, 0 , $start%$major_unit ? 'grey90' : 'grey80'  ) if($REGISTER_LINE);
+    }
+    if( ( $box_end==$global_end ) && !( ( $box_end+1) % $minor_unit ) ) {
+      $self->join_tag( $t, "ruler_end", 1, 0 , ($global_end+1)%$major_unit ? 'grey90' : 'grey80'  ) if($REGISTER_LINE);
+    }
+    unless( $box_start % $major_unit ) { ## Draw the major unit tick 
       $self->push($self->Rect({
-        'x'         => $global_end - $global_start + 1,
-        'y'         => 0,
-        'width'     => 0,
-        'height'    => 5,
-        'colour'    => 'black',
-        'absolutey' => 1,
+	'x'         => $box_start - $global_start,
+	'y'         => 0,
+	'width'     => 0,
+	'height'    => 5,
+	'colour'    => 'black',
+	'absolutey' => 1,
       }));
-    }
+      my $LABEL = $minor_unit < 1000 ? $self->commify($box_start * $contig_strand ): $self->bp_to_nearest_unit( $box_start * $contig_strand, 2 );
+      my($TXT,$PART,$W,$H) = $self->get_text_width(0,$LABEL,'','font'=>$fontname,'ptsize'=>$fontsize);
+      if( $last_text_X + $W/$pix_per_bp * 1.5 < $box_start ) {
+	$self->push($self->Text({
+	  'x'         => $box_start - $global_start,
+	  'y'         => 5,
+	  'height'    => $H,
+	  'font'      => $fontname,
+	  'ptsize'    => $fontsize,
+	  'halign'    => 'left',
+	  'colour'    => $feature_colour,
+	  'text'      => $LABEL,
+	  'absolutey' => 1,
+	}));
+	$last_text_X = $box_start;
+      }
+    } 
+    $start += $minor_unit;
+  }
+  unless( ($global_end+1) % $major_unit ) { ## Draw the major unit tick 
+    $self->push($self->Rect({
+      'x'         => $global_end - $global_start + 1,
+      'y'         => 0,
+      'width'     => 0,
+      'height'    => 5,
+      'colour'    => 'black',
+      'absolutey' => 1,
+    }));
+  }
 }
 
 sub interval {
