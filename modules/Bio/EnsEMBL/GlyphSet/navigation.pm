ab96b7bd01bfeaafe2a40c152a3b521675e766e1
Steve Trevanion <st3@sanger.ac.uk>
2009-07-07 09:54:41 +0000

    enabled for new code, but includes a dodgy image width hack

--- modules/Bio/EnsEMBL/GlyphSet/navigation.pm
+++ modules/Bio/EnsEMBL/GlyphSet/navigation.pm
@@ -6,7 +6,6 @@ use base qw(Bio::EnsEMBL::GlyphSet);
 
 sub _init {
   my ($self) = @_;
-
   if( $self->{'container'}->isa("Bio::EnsEMBL::Compara::AlignSlice::Slice")) {
       my $line = $self->Rect({
 	  'z' => 11,
@@ -19,27 +18,26 @@ sub _init {
 	  'absolutewidth' => 1,
 	  'absolutey'     => 1,
       });
-      
       $self->push($line);
-
       return;
   }
 
-  # return unless ($self->strand() == 1);
-  my $base_href = $self->{'config'}->{'base_url'};
-  my $im_width       = $self->{'config'}->image_width();
+  my $base_href = $self->{'config'}->{'_parameters'}{'base_url'};
+  my $im_width  = $self->{'config'}->{'_parameters'}{'image_width'};
+
+  $im_width -= 128; #really don't like doing this!
 
   my $SPRITE_SIZE = 20;
   my $SPRITE_PAD = 3;
   my $SPRITE_STEP = $SPRITE_SIZE + $SPRITE_PAD;
 
-  my $SPRITES = { 
-    'nav'   => [ -10-$SPRITE_SIZE,             -$SPRITE_STEP, 
+  my $SPRITES = {
+    'nav'   => [ -10-$SPRITE_SIZE,             -$SPRITE_STEP,
        ['zoom_out' => 'out'],
        ['realign'  => 'realign'],
        ['zoom_in'  => 'in']
     ],
-    'left'  => [ 0,                             $SPRITE_STEP, 
+    'left'  => [ 0,                             $SPRITE_STEP,
       ['left'       => 'left2'],
       ['nudge_left' => 'left']
     ],
@@ -48,35 +46,35 @@ sub _init {
       ['nudge_right'=> 'right']
     ]
   };
- 
-  if(! ($self->{'config'}->{'align_slice'})) { 
-# in case of AlignSlice - don't display navigation buttons
-  push @{$SPRITES->{'nav'}}, 
-    ['flip_strand'    => 'flip'],
-    ['set_as_primary' => 'primary'] if $self->{'config'}->{'slice_number'};
 
-  foreach my $key ( keys %$SPRITES ) {
-    my( $pos, $step,  @sprite_array ) =  @{$SPRITES->{$key}};
+  if(! ($self->{'config'}->{'align_slice'})) {
+    # in case of AlignSlice - don't display navigation buttons
+    push @{$SPRITES->{'nav'}}, 
+      ['flip_strand'    => 'flip'],
+	['set_as_primary' => 'primary'] if $self->{'config'}->{'_parameters'}{'slice_number'};
 
-    foreach my $sprite ( @sprite_array ) {
-      (my $N = ucfirst($sprite->[0])) =~ s/_/ /g;
-      $self->push($self->Sprite({
-        'z'             => 1000,
-        'x'             => $pos,
-        'y'             => 0,
-        'sprite'        => $sprite->[0],
-        'width'         => $SPRITE_SIZE,
-        'height'        => $SPRITE_SIZE,
-        'id'            => $N,
-        'absolutex'     => 1,
-        'absolutewidth' => 1,
-        'absolutey'     => 1,
-        'href'          => "$base_href;action=$sprite->[1];id=$self->{'config'}->{'slice_number'}",
-      }));
-      $pos += $step;
+    foreach my $key ( keys %$SPRITES ) {
+      my( $pos, $step,  @sprite_array ) =  @{$SPRITES->{$key}};
+      foreach my $sprite ( @sprite_array ) {
+	(my $N = ucfirst($sprite->[0])) =~ s/_/ /g;
+	$self->push($self->Sprite({
+	  'z'             => 1000,
+	  'x'             => $pos,
+	  'y'             => 0,
+	  'sprite'        => $sprite->[0],
+	  'width'         => $SPRITE_SIZE,
+	  'height'        => $SPRITE_SIZE,
+	  'id'            => $N,
+	  'absolutex'     => 1,
+	  'absolutewidth' => 1,
+	  'absolutey'     => 1,
+	  'href'          => "$base_href;action=$sprite->[1];id=$self->{'config'}->{'slice_number'}",
+	}));
+	$pos += $step;
+      }
     }
   }
-}
+
   my $line = $self->Rect({
     'z' => 11,
     'x' => -120,
@@ -89,7 +87,7 @@ sub _init {
     'absolutey'     => 1,
   });
   $self->join_tag( $line, "bracket", 0,0, 'black' );
-  if( $self->{'config'}->{'compara'} eq 'primary' ) {
+  if( $self->{'config'}->{'_parameters'}{'compara'} eq 'primary' ) {
     $self->join_tag( $line, "bracket2", 0.9,0, 'rosybrown1', 'fill', -40 );
     $self->join_tag( $line, "bracket2", 0,0, 'rosybrown1', 'fill', -40 );
   }
@@ -109,5 +107,5 @@ sub _init {
   $self->push($line);
 
 }
+
 1;
-        
