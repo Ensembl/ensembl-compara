66d7b93c98ff4521efb8966f55acbd2043f74741
Evgeny Bragin <eb4@sanger.ac.uk>
2009-07-14 13:28:11 +0000

    * Blog entries: cache ready html string  instead of atom::feeds->entries objects

--- modules/EnsEMBL/Web/Document/HTML/Blog.pm
+++ modules/EnsEMBL/Web/Document/HTML/Blog.pm
@@ -25,44 +25,40 @@ our $MEMD = EnsEMBL::Web::Cache->new(
 
 sub render {
   my $self  = shift;
-  
   ## Ensembl blog (ensembl.blogspot.com)
-  my $html = qq(<h2>Latest Blog Entries</h2>);
 
-  my @entries = $MEMD ? (@{ $MEMD->get('::BLOG') || [] }) : ();
-
-  unless (@entries) {
+  my $html = $MEMD && $MEMD->get('::BLOG') || '';
+  
+  unless ($html) {
     my $ua = new LWP::UserAgent;
-
     my $proxy = $EnsEMBL::Web::RegObj::ENSEMBL_WEB_REGISTRY->species_defs->ENSEMBL_WWW_PROXY;
     $ua->proxy( 'http', $proxy ) if $proxy;
   
     my $response = $ua->get('http://ensembl.blogspot.com/rss.xml');
     my $feed = XML::Atom::Feed->new(\$response->decoded_content);
     
-    @entries = $feed->entries;
-    
-    $ENV{CACHE_TIMEOUT} = 3600;
-    $MEMD->set('::BLOG', \@entries, $ENV{CACHE_TIMEOUT}, qw(STATIC BLOG))
+    my @entries = $feed->entries;
+
+    my $count = 3;
+    if (@entries) {
+      $html .= "<ul>\n";
+      for (my $i = 0; $i < $count && $i < scalar(@entries);$i++) {
+        my $title = $entries[$i]->title;
+        my $url   = $entries[$i]->link->href;
+        my $date  = substr($entries[$i]->updated, 0, 10);
+  
+        $html .= "<li>$date: <a href=\"$url\">$title</a></li>\n"; 
+      }
+      $html .= "</ul>\n";
+    } else {
+      $html .= qq(<p>Sorry, no feed is available from our blog at the moment</p>);
+    }
+  
+    $html .= qq(<a href="http://ensembl.blogspot.com/">Go to Ensembl blog &rarr;</a>);
+
+    $MEMD->set('::BLOG', $html, 3600, qw(STATIC BLOG))
       if $MEMD;
   }
-
-  my $count = 3;
-  if (@entries) {
-    $html .= "<ul>\n";
-    for (my $i = 0; $i < $count && $i < scalar(@entries);$i++) {
-      my $title = $entries[$i]->title;
-      my $url   = $entries[$i]->link->href;
-      my $date  = substr($entries[$i]->updated, 0, 10);
-
-      $html .= "<li>$date: <a href=\"$url\">$title</a></li>\n"; 
-    }
-    $html .= "</ul>\n";
-  } else {
-    $html .= qq(<p>Sorry, no feed is available from our blog at the moment</p>);
-  }
-
-  $html .= qq(<a href="http://ensembl.blogspot.com/">Go to Ensembl blog &rarr;</a>);
 
   return $html;
 }
