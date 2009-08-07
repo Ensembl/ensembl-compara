23b0bc1ee1fa337327c3a72ca2b7fa5eda88a5ec
Simon Brent <sb23@sanger.ac.uk>
2009-08-07 14:58:35 +0000

    use existing Compara Alignments config

--- modules/EnsEMBL/Web/ViewConfig/Location/Align.pm
+++ modules/EnsEMBL/Web/ViewConfig/Location/Align.pm
@@ -1,66 +1,29 @@
 package EnsEMBL::Web::ViewConfig::Location::Align;
 
 use strict;
-use warnings;
-no warnings 'uninitialized';
-no strict 'refs';
 
-sub init {
-  my( $view_config ) = @_;
+use EnsEMBL::Web::ViewConfig::Gene::Compara_Alignments;
 
-  $view_config->_set_defaults(qw(
-    panel_top      yes 
-    panel_zoom      no
-    zoom_width     100
-    context     100000
-  ));
-  my @multiple = ();
-  my $hash = $view_config->species_defs->multi_hash->{'DATABASE_COMPARA'}{'ALIGNMENTS'}||{};
-
-  foreach my $row_key (
-    grep { $hash->{$_}{'class'} !~ /pairwise/ }
-    keys %$hash
-  ) {
-    $view_config->_set_defaults( map { ( lc("species_$row_key"."_$_"), 'yes') } keys %{ $hash->{$row_key}{'species'} } );
-  }
-  $view_config->storable = 1;
+sub init { 
+  my $view_config = shift;
+  
+  EnsEMBL::Web::ViewConfig::Gene::Compara_Alignments::init($view_config);
+  
+  $view_config->_set_defaults(qw(panel_top yes));
+  
   $view_config->add_image_configs({qw(
     contigviewtop        nodas
     alignsliceviewbottom nodas
   )});
+  
+  $view_config->default_config = 'alignsliceviewbottom';
 }
 
 sub form {
-  my( $view_config, $object ) = @_;
-
+  my ($view_config, $object) = @_;
+  
   $view_config->add_form_element({ 'type' => 'YesNo', 'name' => 'panel_top',  'select' => 'select', 'label'  => 'Show overview panel' });
- 
-  my $hash = $view_config->species_defs->multi_hash->{'DATABASE_COMPARA'}{'ALIGNMENTS'}||{};
-  my $species = $view_config->species;
-
-  foreach my $row_key (
-#    sort { scalar(@{$hash->{$a}{'species'}})<=> scalar(@{$hash->{$b}{'species'}}) }
-    grep { $hash->{$_}{'class'} !~ /pairwise/ }
-    keys %$hash
-  ) {
-    my $row = $hash->{$row_key};
-    next unless $row->{'species'}{$species};
-    $view_config->add_fieldset( "Options for ".$row->{'name'} );
-    foreach( sort keys %{$row->{'species'}} ) {
-      my $name = 'species_'.$row_key.'_'.lc($_);
-      if( $_ eq $species ) {
-        $view_config->add_form_element({
-          'type'     => 'Hidden',   'name' => $name
-        });
-      } else {
-        $view_config->add_form_element({
-          'type'     => 'CheckBox', 'label' => $view_config->_species_label($_),
-          'name'     => $name,
-          'value'    => 'yes', 'raw' => 1
-        });
-     }
-    } 
-  }
-    
+  
+  EnsEMBL::Web::ViewConfig::Gene::Compara_Alignments::form($view_config, $object, 1);
 }
 1;
