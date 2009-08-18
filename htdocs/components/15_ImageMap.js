7b5ffea07a1259d157d04650589edea821d757a7
Simon Brent <sb23@sanger.ac.uk>
2009-08-18 12:44:24 +0000

    added multi species code for multi contig view. moved highlighting code in from PanelManager

--- htdocs/components/15_ImageMap.js
+++ htdocs/components/15_ImageMap.js
@@ -4,14 +4,15 @@ Ensembl.Panel.ImageMap = Ensembl.Panel.Content.extend({
   constructor: function (id, params) {
     this.base(id, params);
     
-    this.linked = false;
     this.dragging = false;
     this.clicking = true;
     this.dragCoords = {};
     
-    this.region = {};
+    this.dragRegion = {};
+    this.highlightRegions = {};
     this.areas = []; // TODO: do we need both areas and draggables?
     this.draggables = [];
+    this.speciesCount = 0;
     
     Ensembl.EventManager.register('highlightImage', this, this.highlightImage);
     Ensembl.EventManager.register('dragStop', this, this.dragStop);
@@ -28,6 +29,7 @@ Ensembl.Panel.ImageMap = Ensembl.Panel.Content.extend({
     this.elLk.exportMenu = $('.iexport_menu', this.el);
     
     this.vdrag = this.elLk.areas.hasClass('vdrag');
+    this.multi = this.elLk.areas.hasClass('multi');
     
     this.makeImageMap(); 
     
@@ -41,8 +43,9 @@ Ensembl.Panel.ImageMap = Ensembl.Panel.Content.extend({
   makeImageMap: function () {
     var myself = this;
     
+    var highlight = !!(window.location.pathname.match(/\/Location\//) && !this.vdrag);
     var rect = [ 'l', 't', 'r', 'b' ];
-    var c;
+    var species, r, c;
     
     this.elLk.areas.each(function () {
       c = { a: this };
@@ -59,15 +62,24 @@ Ensembl.Panel.ImageMap = Ensembl.Panel.Content.extend({
       if (this.className.match(/drag/)) {
         myself.draggables.push(c);
         
-        if (!myself.vrdag && this.href.split('|')[3] == Ensembl.species) {
-          myself.region = c;
+        if (highlight === true) {
+          r = this.href.split('|');
+          species = r[3];
+          
+          if (myself.multi || species == Ensembl.species) {
+            if (!myself.highlightRegions[species]) {
+              myself.highlightRegions[species] = [];
+              myself.speciesCount++;
+            }
+            
+            myself.highlightRegions[species].push({ region: c, linked: false });
+            myself.imageNumber = parseInt(r[2]);
+            
+            Ensembl.EventManager.trigger('highlightImage', myself.imageNumber, species, parseInt(r[5]), parseInt(r[6]));
+          }
         }
       }
     });
-    
-    if (this.draggables.length && !this.vdrag) {
-      Ensembl.EventManager.trigger('highlightImageMaps');
-    }
     
     this.elLk.img.mousedown(function (e) {
       // Only draw the drag box for left clicks.
@@ -96,18 +108,14 @@ Ensembl.Panel.ImageMap = Ensembl.Panel.Content.extend({
     // Have to use this instead of the map coords because IE can't cope with offsetX/Y and relative positioned elements
     this.dragCoords.offset = { x: e.pageX - this.dragCoords.map.x, y: e.pageY - this.dragCoords.map.y }; 
     
-    while (i--) {
-      if (this.inRect(this.draggables[i], this.dragCoords.map.x, this.dragCoords.map.y)) {
-        this.region = this.draggables[i];
-        
-        this.elLk.img.mousemove(function (e2) {
-          myself.dragging = e; // store mousedown event
-          myself.drag(e2);
-          return false;
-        });
-        
-        break;
-      }
+    this.dragRegion = this.getArea(this.dragCoords.map.x, this.dragCoords.map.y, true);
+    
+    if (this.dragRegion) {
+      this.elLk.img.mousemove(function (e2) {
+        myself.dragging = e; // store mousedown event
+        myself.drag(e2);
+        return false;
+      });
     }
   },
   
@@ -150,94 +158,146 @@ Ensembl.Panel.ImageMap = Ensembl.Panel.Content.extend({
       case false: coords.b = y; coords.t = this.dragCoords.map.y; break;
     }
     
-    if (x < this.region.l) {
-      coords.l = this.region.l;
-    } else if (x > this.region.r) {
-      coords.r = this.region.r;
+    if (x < this.dragRegion.l) {
+      coords.l = this.dragRegion.l;
+    } else if (x > this.dragRegion.r) {
+      coords.r = this.dragRegion.r;
     }
     
-    if (y < this.region.t) {
-      coords.t = this.region.t;
-    } else if (y > this.region.b) {
-      coords.b = this.region.b;
+    if (y < this.dragRegion.t) {
+      coords.t = this.dragRegion.t;
+    } else if (y > this.dragRegion.b) {
+      coords.b = this.dragRegion.b;
     }
     
     this.highlight(coords, 'rubberband');
   },
   
   makeZMenu: function (e, coords) {
-    var area = coords.r ? this.region : this.getArea(coords.x, coords.y);
+    var area = coords.r ? this.dragRegion : this.getArea(coords.x, coords.y);
     
     if (!area) {
       return;
     }
     
-    var id = 'zmenu_' + area.a.coords.replace(/[ ,]/g, '_');
-    var location, fuzziness;
+    if ($(area.a).hasClass('nav')) {
+      window.location = area.a.href;
+      return;
+    }
     
-    if (this.range) {
-      location = this.range.start + (this.range.scale * (coords.x - this.region.l));
-      fuzziness = this.range.scale * 2; // Increase the size of the click so we can have some measure of certainty for returning the right menu
+    var id = 'zmenu_' + area.a.coords.replace(/[ ,]/g, '_');
+    
+    if ($(area.a).hasClass('das') && this.highlightRegions) {
+      var species = Ensembl.species;
       
-      coords.clickStart = Math.floor(location - fuzziness);
-      coords.clickEnd = Math.ceil(location + fuzziness);
-      
-      if (coords.clickStart < this.range.start) {
-        coords.clickStart = this.range.start;
+      if (this.speciesCount > 1) {
+        var dragArea = this.getArea(coords.x, coords.y, true);
+        
+        if (dragArea) {
+          species = dragArea.a.href.split('|')[3]
+        }
+        
+        dragArea = null;
       }
       
-      if (coords.clickEnd > this.range.end) {
-        coords.clickEnd = this.range.end;
+      var range = this.highlightRegions[species][0].range;
+      var location, fuzziness;
+      
+      if (range) {
+        location = range.start + (range.scale * (coords.x - this.dragRegion.l));
+        fuzziness = range.scale * 2; // Increase the size of the click so we can have some measure of certainty for returning the right menu
+        
+        coords.clickStart = Math.floor(location - fuzziness);
+        coords.clickEnd = Math.ceil(location + fuzziness);
+        
+        if (coords.clickStart < range.start) {
+          coords.clickStart = range.start;
+        }
+        
+        if (coords.clickEnd > range.end) {
+          coords.clickEnd = range.end;
+        }
       }
     }
     
     Ensembl.EventManager.trigger('makeZMenu', id, { position: { left: e.pageX, top: e.pageY }, coords: coords, area: area });
   },
   
-  highlightImage: function (start, end, link) {    
-    if (this.linked === true || !this.region.a) {
+  /**
+   * Highlights regions of the image.
+   * In MultiContigView, each image can have numerous regions to highlight - one per species
+   *
+   * redbox:  Dotted red line outlining the draggable region of an image. 
+   *          Only shown where an image displays a region contained in another region.
+   *          In practice this means redbox never appears on the first image on the page.
+   *
+   * redbox2: Solid red line outlining the region of an image displayed on the next image.
+   *          If there is only one image, or the next image has an invalid coordinate system 
+   *          (eg AlignSlice or whole chromosome), highlighting is taken from the r parameter in the url.
+   */
+  highlightImage: function (imageNumber, species, start, end) {
+    // Make sure each image is highlighted based only on the next image on the page
+    if (imageNumber - this.imageNumber > 1 || imageNumber - this.imageNumber < 0) {
       return;
     }
     
-    var r = this.region.a.href.split('|'); // Find bp range for the map
+    var highlight;
+    var i = this.highlightRegions[species].length;
+    var link = true; // Defines if the highlighted region has come from another image or the url
     
-    if (r[3] != Ensembl.species) {
-      return;
-    }
-    
-    var min = parseInt(r[5]);
-    var max = parseInt(r[6]);
-    var scale = (max - min + 1) / (this.region.r - this.region.l); // bps per pixel on image
-    
-    var coords = {
-      t: this.region.t + 2,
-      b: this.region.b - 2,
-      l: ((start - min) / scale) + this.region.l,
-      r: ((end - min) / scale) + this.region.l
-    };
-    
-    // Don't draw the redbox on the first imagemap on the page
-    if (parseInt(r[2]) != 1) {
-      this.highlight(this.region, 'redbox');
-    }
-    
-    if (link === true) {
-      if (start >= min && end <= max) {
-        this.highlight(coords, 'redbox2');
+    while (i--) {
+      highlight = this.highlightRegions[species][i];
+      
+      if (!highlight.region.a) {
+        break;
       }
-    
-      this.linked = true;
-    } else if (start > min && end < max) {
-      // In this case start and end have come from Ensembl.location, so don't draw the red box if start = min and end = max
-      this.highlight(coords, 'redbox2');
+      
+      // r = [ '#drag', image number, species number, species name, region, start, end, strand ]
+      var r = highlight.region.a.href.split('|');
+      
+      var min = parseInt(r[5]);
+      var max = parseInt(r[6]);
+      var scale = (max - min + 1) / (highlight.region.r - highlight.region.l); // bps per pixel on image
+      
+      // Don't draw the redbox on the first imagemap on the page
+      if (parseInt(r[2]) != 1) {
+        this.highlight(highlight.region, 'redbox', species);
+      }
+      
+      // Highlighting base on self. Take start and end from Ensembl core parameters
+      if (this.imageNumber == imageNumber) {
+        if (Ensembl.multiSpecies[species]) {
+          start = Ensembl.multiSpecies[species].location.start;
+          end = Ensembl.multiSpecies[species].location.end;
+        } else {
+          start = Ensembl.location.start;
+          end = Ensembl.location.end;
+        }
+          
+        link = false;
+      }
+      
+      var coords = {
+        t: highlight.region.t + 2,
+        b: highlight.region.b - 2,
+        l: ((start - min) / scale) + highlight.region.l,
+        r: ((end - min) / scale) + highlight.region.l
+      };
+      
+      // Highlight unless it's the bottom image on the page
+      if (start >= min && end <= max && (link === true || !(start == min && end == max))) {
+        this.highlight(coords, 'redbox2', species);
+      }
+      
+      // Ok to overwrite because the only time we have more than one highlightRegions is MultiContigView, where each species image is identical
+      highlight.range = { start: min, end: max, scale: scale };
     }
-    
-    this.range = { start: min, end: max, scale: scale };
   },
   
-  highlight: function (coords, cl) {  
+  highlight: function (coords, cl, species) {  
     var w = coords.r - coords.l + 1;
     var h = coords.b - coords.t + 1;
+    var originalClass;
     
     var style = {
       l: { left: coords.l, width: 1, top: coords.t, height: h },
@@ -245,6 +305,11 @@ Ensembl.Panel.ImageMap = Ensembl.Panel.Content.extend({
       t: { left: coords.l, width: w, top: coords.t, height: 1 },
       b: { left: coords.l, width: w, top: coords.b, height: 1 }
     };
+    
+    if (species) {
+      originalClass = cl;
+      cl = cl + '_' + species;
+    }
     
     if (!$('.' + cl, this.el).length) {
       this.elLk.img.after(
@@ -255,9 +320,15 @@ Ensembl.Panel.ImageMap = Ensembl.Panel.Content.extend({
       );
     }
     
-    $('.' + cl, this.el).each(function () {
+    var els = $('.' + cl, this.el).each(function () {
       $(this).css(style[this.className.split(' ')[1]]);
     });
+    
+    if (species) {
+      els.addClass(originalClass);
+    }
+    
+    els = null;
   },
   
   getMapCoords: function (e) {
@@ -267,12 +338,13 @@ Ensembl.Panel.ImageMap = Ensembl.Panel.Content.extend({
     };
   },
   
-  getArea: function (x, y) {
+  getArea: function (x, y, draggables) {
     var test = false;
+    var areas = draggables ? this.draggables : this.areas;
     var c;
     
-    for (var i = 0; i < this.areas.length; i++) {
-      c = this.areas[i];
+    for (var i = 0; i < areas.length; i++) {
+      c = areas[i];
       
       switch (c.a.shape.toLowerCase()) {
         case 'circle': test = this.inCircle(c.c, x, y); break;
