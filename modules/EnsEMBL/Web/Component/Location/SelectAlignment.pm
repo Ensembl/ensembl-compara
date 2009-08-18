4cb7c20b85b4ec122bca6bd37d1d5a374783ac53
Simon Brent <sb23@sanger.ac.uk>
2009-08-18 12:42:20 +0000

    split into two dropdowns

--- modules/EnsEMBL/Web/Component/Location/SelectAlignment.pm
+++ modules/EnsEMBL/Web/Component/Location/SelectAlignment.pm
@@ -3,8 +3,10 @@ package EnsEMBL::Web::Component::Location::SelectAlignment;
 use strict;
 use warnings;
 no warnings "uninitialized";
-use base qw(EnsEMBL::Web::Component);
+
 use CGI qw(escapeHTML);
+
+use base qw(EnsEMBL::Web::Component);
 
 sub _init {
   my $self = shift;
@@ -15,99 +17,78 @@ sub _init {
 sub content {
   my $self = shift;
   my $object = $self->object;
-  my $url = $object->_url({ align => undef }, 1 );
-#  $url = $object->_url();
-#  warn Data::Dumper::Dumper($url);
-
-  ## Get the compara database hash!
-  my $hash = $object->species_defs->multi_hash->{'DATABASE_COMPARA'}{'ALIGNMENTS'}||{};
-#  warn Data::Dumper::Dumper($hash);
-  my $species = $object->species;
-  my $options;
-  my $species_hash;
-  my $c = 0;
-  foreach my $i (keys %$hash) {
-    foreach (keys %{$hash->{$i}->{'species'}}) {
-      #this will fail for vega intra species compara
-      if ($hash->{$i}->{'class'} =~ /pairwise/ && $hash->{$i}->{'species'}->{$species} && $_ ne $species) {
-	my $type = lc $hash->{$i}->{'type'};
-	$type =~ s/_/ /g;
-	next if ($object->species_defs->species_label($_, 1) eq 'Ancestral sequence');
-	$c++;
-	$species_hash->{$_} = $object->species_defs->species_label($_, 1) . "###$type";
+  
+  my $url = $object->_url($object->multi_params, 1);
+  my $alignments = $object->species_defs->multi_hash->{'DATABASE_COMPARA'}->{'ALIGNMENTS'} || {};
+  my $primary_species = $object->species;
+  
+  my %species;
+  my (@add, @remove);
+  my $extra_inputs;
+  
+  my $add_options = '<option value="">-- Select a species --</option>';
+  my $remove_options = $add_options;
+  
+  # get species (and parameters) already shown on the page
+  my %shown = map { $object->param("s$_") => $_ } grep s/^s(\d+)$/$1/, $object->param;
+  my $next_id = 1 + scalar keys %shown;
+  
+  foreach (sort keys %{$url->[1]}) {
+    $extra_inputs .= sprintf '
+      <input type="hidden" name="%s" value="%s" />', escapeHTML($_), escapeHTML($url->[1]{$_});
+  }
+  
+  foreach my $i (grep { $alignments->{$_}{'class'} =~ /pairwise/ } keys %$alignments) {
+    foreach (keys %{$alignments->{$i}->{'species'}}) {
+      # this will fail for vega intra species compara
+      if ($alignments->{$i}->{'species'}->{$primary_species} && $_ ne $primary_species && $_ ne 'merged') {
+        my $type = lc $alignments->{$i}->{'type'};
+        
+        $type =~ s/_net//;
+        $type =~ s/_/ /g;
+        
+        $species{$_} = $object->species_defs->species_label($_, 1) . "###$type";
       }
     }
   }
-
-  #get core input param
-#  my $extra_inputs;
-#  foreach (sort keys %{$url->[1]||{}}) {
-#    warn "extra input = $_";
-#    $extra_inputs .= sprintf '
-#      <input type="hidden" name="%s" value="%s" />', escapeHTML($_), escapeHTML($url->[1]{$_});
-#  }
-
-
-  #get species (and parameters) already shown on the page
-  my $max_s;
-  my @shown_species;
-  my $extra_inputs;
-  foreach my $param($object->param) {
-    if ($param =~ /^s/) {
-      (my $c) = $param =~ /(\d+)/;
-      $max_s = $c > $max_s ? $c : $max_s;
-      my $species = $object->param($param);
-      push @shown_species, [ $species, $param ];
-    }
-    if ($param =~ /^[srg]/) {
-#      next if $param eq 'r';
-      $extra_inputs .= sprintf '
-      <input type="hidden" name="%s" value="%s" />', escapeHTML($param), escapeHTML($object->param($param));
+  
+  foreach (sort { $species{$a} cmp $species{$b} } keys %species) {
+    if ($shown{$_}) {
+      push @remove, [ $shown{$_}, join ' - ', split /###/, $species{$_} ];
+    } else {
+      push @add, [ $_, join ' - ', split /###/, $species{$_} ];
     }
   }
-
-  #new species
-#  warn Data::Dumper::Dumper($species_hash);
-  $options .= qq{<optgroup label="Add alignment">};
-  foreach my $sp (sort { $species_hash->{$a} cmp $species_hash->{$b} } keys %$species_hash) {
-    if (! grep {$_->[0] eq $sp} @shown_species) {
-      my ($name, $type) = split (/###/, $species_hash->{$sp});
-      $options .= sprintf '
-              <option value="%s"%s>%s</option>',
-	      $sp,
-	      '',
-	      "$name - $type";
-    }
-  }
-  $options .= qq{</optgroup>};
-
- #species to remove
-  $options .= qq{<optgroup label="Remove alignment">};
-  foreach my $sp (sort { $a cmp $b } keys %$species_hash) {
-    if (grep {$_->[0] eq $sp} @shown_species) {
-      my ($name, $type) = split (/###/, $species_hash->{$sp});
-      $options .= sprintf '
-              <option value="%s"%s>%s</option>',
-	      $sp,
-	      '',
-	      "$name - $type";
-    }
-  }
-  $options .= qq{</optgroup>};
-
-  $max_s++;
-  #render the navigation bar
-  return sprintf qq(
-  <div class="autocenter navbar" style="width:%spx; text-align:left" >
-    <form action="%s" method="get"><div style="padding:2px;">
-      <label for="align">Add/Remove an alignment:</label>
-      <select name="%s" id="species">%s</select>%s
-      <input value="Go&gt;" type="submit" class="go-button" />
-    </div></form>
-  </div>),
+  
+  $add_options    .= qq{<option value="$_->[0]">$_->[1]</option>} for @add;
+  $remove_options .= qq{<option value="$_->[0]">$_->[1]</option>} for @remove;
+  
+  return sprintf ('
+    <div class="autocenter navbar" style="width:%spx; text-align:left">
+      <form action="%s" method="get">
+        <div class="alignment_selector">
+          <label for="align">Add an alignment:</label>
+          <select name="s%s">
+            %s
+          </select>
+          <input value="Go&gt;" type="submit" class="go-button" />
+        </div>
+        <div class="alignment_selector">
+          <label for="align">Remove an alignment:</label>
+          <select name="remove_alignment">
+            %s
+          </select>
+        </div>
+        %s
+      </form>
+    </div>',
     $self->image_width,
     $url->[0],
-    's'.$max_s, $options, $extra_inputs;
+    $next_id, 
+    $add_options,
+    $remove_options,
+    $extra_inputs
+  );
 }
 
 1;
