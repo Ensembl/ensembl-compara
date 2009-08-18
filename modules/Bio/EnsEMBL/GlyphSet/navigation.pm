9a51b31568e1125727340f21f0881761417d281a
Simon Brent <sb23@sanger.ac.uk>
2009-08-18 12:36:25 +0000

    add nav class to imagemap, cleanup

--- modules/Bio/EnsEMBL/GlyphSet/navigation.pm
+++ modules/Bio/EnsEMBL/GlyphSet/navigation.pm
@@ -5,106 +5,115 @@ use strict;
 use base qw(Bio::EnsEMBL::GlyphSet);
 
 sub _init {
-  my ($self) = @_;
-  if( $self->{'container'}->isa("Bio::EnsEMBL::Compara::AlignSlice::Slice")) {
-      my $line = $self->Rect({
-	  'z' => 11,
-	  'x' => -120,
-	  'y' => 4, # 22,
-	  'colour' => 'black',
-	  'width' => 20000,
-	  'height' => 0,
-	  'absolutex'     => 1,
-	  'absolutewidth' => 1,
-	  'absolutey'     => 1,
-      });
-      $self->push($line);
-      return;
+  my $self = shift;
+  
+  if ($self->{'container'}->isa('Bio::EnsEMBL::Compara::AlignSlice::Slice')) {
+    my $line = $self->Rect({
+      'z'             => 11,
+      'x'             => -120,
+      'y'             => 4,
+      'colour'        => 'black',
+      'width'         => 20000,
+      'height'        => 0,
+      'absolutex'     => 1,
+      'absolutewidth' => 1,
+      'absolutey'     => 1
+    });
+    
+    $self->push($line);
+    
+    return;
   }
 
-  my $base_href = $self->{'config'}->{'_parameters'}{'base_url'};
-  my $im_width  = $self->image_width;
+  my $href = $self->get_parameter('base_url') . ';action=%s;id=' . --[split '|', $self->get_parameter('slice_number')]->[0];
+  
+  my $sprite_size = 20;
+  my $sprite_pad = 3;
+  my $sprite_step = $sprite_size + $sprite_pad;
 
-  my $SPRITE_SIZE = 20;
-  my $SPRITE_PAD = 3;
-  my $SPRITE_STEP = $SPRITE_SIZE + $SPRITE_PAD;
-
-  my $SPRITES = {
-    'nav'   => [ -10-$SPRITE_SIZE,             -$SPRITE_STEP,
-       ['zoom_out' => 'out'],
-       ['realign'  => 'realign'],
-       ['zoom_in'  => 'in']
+  my $sprites = {
+    'nav'   => [ 
+      -10 - $sprite_size, 
+      -$sprite_step,
+      [ 'zoom_out' => 'out' ],
+      [ 'realign'  => 'realign' ],
+      [ 'zoom_in'  => 'in' ]
     ],
-    'left'  => [ 0,                             $SPRITE_STEP,
-      ['left'       => 'left2'],
-      ['nudge_left' => 'left']
+    'left'  => [ 
+      0, 
+      $sprite_step,
+      [ 'left'       => 'left2' ],
+      [ 'nudge_left' => 'left' ]
     ],
-    'right' => [ $im_width - $SPRITE_SIZE + 1, -$SPRITE_STEP,
-      ['right'      => 'right2'],
-      ['nudge_right'=> 'right']
+    'right' => [ 
+      $self->image_width - $sprite_size + 1, 
+      -$sprite_step,
+      [ 'right'       => 'right2' ],
+      [ 'nudge_right' => 'right' ]
     ]
   };
 
   if (!$self->{'config'}->{'align_slice'}) {
     # in case of AlignSlice - don't display navigation buttons
-    push @{$SPRITES->{'nav'}},
-      ['flip_strand'    => 'flip'],
-      ['set_as_primary' => 'primary'] if $self->get_parameter('compara') eq 'secondary';
+    push @{$sprites->{'nav'}}, [ 'flip_strand' => 'flip' ], [ 'set_as_primary' => 'primary' ] if $self->get_parameter('compara') eq 'secondary';
 
-    foreach my $key ( keys %$SPRITES ) {
-      my( $pos, $step,  @sprite_array ) =  @{$SPRITES->{$key}};
-      foreach my $sprite ( @sprite_array ) {
-	(my $N = ucfirst($sprite->[0])) =~ s/_/ /g;
-	$self->push($self->Sprite({
-	  'z'             => 1000,
-	  'x'             => $pos,
-	  'y'             => 0,
-	  'sprite'        => $sprite->[0],
-	  'width'         => $SPRITE_SIZE,
-	  'height'        => $SPRITE_SIZE,
-	  'id'            => $N,
-	  'absolutex'     => 1,
-	  'absolutewidth' => 1,
-	  'absolutey'     => 1,
-	  'href'          => "$base_href;action=$sprite->[1];id=$self->{'config'}->{'slice_number'}",
-	}));
-	$pos += $step;
+    foreach my $key (keys %$sprites) {
+      my ($pos, $step,  @sprite_array) = @{$sprites->{$key}};
+      
+      foreach my $sprite (@sprite_array) {        
+        $self->push($self->Sprite({
+          'z'             => 1000,
+          'x'             => $pos,
+          'y'             => 0,
+          'sprite'        => $sprite->[0],
+          'width'         => $sprite_size,
+          'height'        => $sprite_size,
+          'absolutex'     => 1,
+          'absolutewidth' => 1,
+          'absolutey'     => 1,
+          'href'          => sprintf($href, $sprite->[1]),
+          'class'         => 'nav'
+        }));
+        
+        $pos += $step;
       }
     }
   }
 
   my $line = $self->Rect({
-    'z' => 11,
-    'x' => -120,
-    'y' => 12, # 22,
-    'colour' => 'black',
-    'width' => 120,
-    'height' => 0,
+    'z'             => 11,
+    'x'             => -120,
+    'y'             => 12,
+    'colour'        => 'black',
+    'width'         => 120,
+    'height'        => 0,
     'absolutex'     => 1,
     'absolutewidth' => 1,
-    'absolutey'     => 1,
+    'absolutey'     => 1
   });
-  $self->join_tag( $line, "bracket", 0,0, 'black' );
+  
+  $self->join_tag($line, 'bracket', 0, 0, 'black');
   
   if ($self->get_parameter('compara') eq 'primary') {
-    $self->join_tag( $line, "bracket2", 0.9,0, 'rosybrown1', 'fill', -40 );
-    $self->join_tag( $line, "bracket2", 0,0, 'rosybrown1', 'fill', -40 );
+    $self->join_tag($line, 'bracket2', 0.9, 0, 'rosybrown1', 'fill', -40);
+    $self->join_tag($line, 'bracket2', 0,   0, 'rosybrown1', 'fill', -40);
   }
+  
   $self->push($line);
+  
   my $line = $self->Rect({
-    'z' => 11,
-    'x' => 0,
-    'y' => 12, # 22,
-    'colour' => 'black',
-    'width' => 20000,
-    'height' => 0,
+    'z'             => 11,
+    'x'             => 0,
+    'y'             => 12,
+    'colour'        => 'black',
+    'width'         => 20000,
+    'height'        => 0,
     'absolutex'     => 1,
     'absolutewidth' => 1,
-    'absolutey'     => 1,
+    'absolutey'     => 1
   });
 
   $self->push($line);
-
 }
 
 1;
