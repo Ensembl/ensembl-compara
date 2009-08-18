51c9cd6de6b594cd4ccdc7bdb8cbc736fb836052
Simon Brent <sb23@sanger.ac.uk>
2009-08-18 12:50:57 +0000

    changed to do everything in one for loop

--- modules/EnsEMBL/Web/Component/Location/MultiBottom.pm
+++ modules/EnsEMBL/Web/Component/Location/MultiBottom.pm
@@ -3,101 +3,71 @@ package EnsEMBL::Web::Component::Location::MultiBottom;
 use strict;
 use warnings;
 no warnings "uninitialized";
-use base qw(EnsEMBL::Web::Component::Location);
 
-#use Data::Dumper;
-my %SHORT = qw(
-  chromosome Chr.
-  supercontig S'ctg
-);
+use base qw(EnsEMBL::Web::Component::Location);
 
 sub _init {
   my $self = shift;
-  $self->cacheable( 1 );
-  $self->ajaxable(  1 );
-}
-
-sub caption {
-  my $self = shift;
-  return 'Detailed View';
+  $self->cacheable(1);
+  $self->ajaxable(1);
 }
 
 sub content {
   my $self = shift;
+  
   my $object = $self->object;
-  my $html;
-  my $ploc = $object->[2][0];
-  my $pslice = $ploc->slice;
-  my $counter = 1;
-  my $other_locs = $object->other_locations;
-  my $base_url = $object->_url();
-#  warn $base_url;
-  my $max_count = @{$other_locs} + 1;
-  my $pwuc = $object->image_config_hash( "contigviewbottom_$counter", 'MultiBottom', $ploc->species);
-  $pwuc->set_parameters({
-    'container_width' => $object->length,
-    'image_width'     => $self->image_width,
-    'slice_number'    => "1|3",
-    'caption'         => $object->seq_region_type.' '.$object->seq_region_name,
-    'multi'           => 1,
-    'compara'         => 'primary',
-    'base_url'        => $base_url,
-  });
-  #add panel caption (displayed by scalebar glyphset)
-  my $type = $pslice->coord_system_name();
-  my $chr = $pslice->seq_region_name();
-  my $chr_raw = $chr;
-  unless( $chr =~ /^$type/i ) {
-    $type = $SHORT{lc($type)} || ucfirst( $type );
-    $chr = "$type $chr";
-  }
-  if( length($chr) > 9 ) {
-    $chr = $chr_raw;
-  }
-  (my $abbrev = $ploc->species ) =~ s/^(\w)\w+_(\w{3})\w+$/$1$2/g;
-  $chr = "$abbrev $chr"; 
-  $pwuc->get_node('scalebar')->set('caption', $chr );
-
-  $pwuc->mult;
-#  $ploc->slice->{_config_file_name_} = $loc->real_species;
-  my $images = [];
-  push @$images, ($pslice, $pwuc) unless ($max_count > 2);
-
-  #add secondary slices
-  if (@$other_locs) {
-    foreach my $loc ( @{$other_locs}) {
-      $counter++;
-      my $slice = $loc->{'slice'};
-      my $wuc = $object->image_config_hash( "contigview_bottom_$counter", 'MultiBottom', $loc->{'real_species'} );
-      $wuc->set_parameters({
-	'container_width' => $slice->length,
-	'image_width'     => $self->image_width,
-	'slice_number'    => "$counter|3",
-	'caption'         => $object->seq_region_type.' '.$object->seq_region_name,
-	'multi'           => 1,
-	'compara'         => 'secondary',
-      });
-      $wuc->get_node('scalebar')->set('caption', $loc->{'short_name'} );
-
-      $wuc->mult;
-#      $loc->slice->{_config_file_name_} = $loc->{'real_species'};
-      push @$images, ($slice, $wuc);
-      push @$images, ($pslice, $pwuc) if ( ($max_count > 2) && ($counter < $max_count));
+  my $slice = $object->slice;
+  my $image_width = $self->image_width;
+  my $slices = $object->multi_locations;
+  my $max = scalar @$slices;
+  my $i = 1;
+  my @images;
+  my $primary_image_config;
+  
+  my $base_url = $object->_url($object->multi_params);
+  
+  foreach (@$slices) {
+    my $image_config = $object->image_config_hash('contigview_bottom_' . $i, 'MultiBottom', $_->{'real_species'});
+    
+    $image_config->set_parameters({
+      container_width => $_->{'slice'}->length,
+      image_width     => $image_width,
+      slice_number    => "$i|3",
+      caption         => $object->seq_region_type . ' ' . $object->seq_region_name,
+      multi           => 1,
+      compara         => $i == 1 ? 'primary' : 'secondary',
+      base_url        => $base_url
+    });
+    
+    $image_config->get_node('scalebar')->set('caption', $_->{'short_name'});
+    $image_config->mult;
+    
+    if ($i == 1) {
+      $primary_image_config = $image_config;
+      push @images, $slice, $primary_image_config unless $max > 2;
+    } else {
+      push @images, $_->{'slice'}, $image_config;
+      push @images, $slice, $primary_image_config if $max > 2 && $i < $max;
     }
+    
+    $i++;
   }
-  my $image = $self->new_image( $images );
-  return if $self->_export_image( $image );
+  
+  my $image = $self->new_image(\@images);
+  
+  return if $self->_export_image($image);
+  
   $image->imagemap = 'yes';
-  $image->set_button( 'drag', 'title' => 'Click or drag to centre display' );
+  $image->set_button('drag', 'title' => 'Click or drag to centre display');
   $image->{'panel_number'} = 'bottom';
+  
   my $html = $image->render;
+  
   $html .= $self->_info(
     'Configuring the display',
-    sprintf '
-  <p>
-    %s. To change the tracks you are displaying, use the "<strong>Configure this page</strong>" link on the left.
-  </p>'
+    '<p>To change the tracks you are displaying, use the "<strong>Configure this page</strong>" link on the left.</p>'
   );
+  
   return $html;
 }
 
--- modules/EnsEMBL/Web/Component/Location/MultiIdeogram.pm
+++ modules/EnsEMBL/Web/Component/Location/MultiIdeogram.pm
@@ -3,93 +3,52 @@ package EnsEMBL::Web::Component::Location::MultiIdeogram;
 use strict;
 use warnings;
 no warnings "uninitialized";
+
 use base qw(EnsEMBL::Web::Component::Location);
-
-#use Data::Dumper;
-
-my %SHORT = qw(
-  chromosome Chr.
-  supercontig S'ctg
-);
 
 sub _init {
   my $self = shift;
-  $self->cacheable( 1 );
-  $self->ajaxable(  1 );
-}
-
-sub caption {
-  my $self = shift;
-  return 'Top Level';
+  $self->cacheable(1);
+  $self->ajaxable(1);
 }
 
 sub content {
   my $self = shift;
+  
   my $object = $self->object;
-  my $ploc = $object->[2][0];
-  my $pslice = $ploc->slice;
-  my $counter = 1;
-  my $other_locs = $object->other_locations;
-  my $max_count = @{$other_locs} + 1;
-  my $species = $ploc->species;
-
-  #add panel caption (displayed by ideogram glyphset) - go into factory ?
-  my $type = $pslice->coord_system_name();
-  my $chr = $pslice->seq_region_name();
-  my $chr_raw = $chr;
-  unless( $chr =~ /^$type/i ) {
-    $type = $SHORT{lc($type)} || ucfirst( $type );
-    $chr = "$type $chr";
+  my $image_width = $self->image_width;
+  my $i = 1;
+  my @images;
+  
+  my $slices = $object->multi_locations;
+  
+  foreach (@$slices) {
+    my $image_config = $object->image_config_hash('chromosome_' . $i, 'MultiIdeogram', $_->{'real_species'});
+    my $chromosome = $_->{'slice'}->adaptor->fetch_by_region(undef, $_->{'slice'}->seq_region_name);
+    
+    $image_config->set_parameters({
+      container_width => $chromosome->seq_region_length,
+      image_width     => $image_width,
+      slice_number    => "$i|1",
+      multi           => 1
+    });
+    
+    $image_config->get_node('ideogram')->set('caption', $_->{'short_name'});
+    
+    push @images, $chromosome, $image_config;
+    $i++;
   }
-  if( length($chr) > 9 ) {
-    $chr = $chr_raw;
-  }
-  (my $abbrev = $ploc->species ) =~ s/^(\w)\w+_(\w{3})\w+$/$1$2/g;
-  $chr = "$abbrev $chr";
-
-  my $pwuc = $object->image_config_hash( "chromosome_$counter", 'MultiIdeogram', $species );
-
-  $pwuc->container_width( $pslice->seq_region_length );
-  $pwuc->set_parameters({
-    'container_width' => $object->seq_region_length,
-    'image_width'     => $self->image_width,
-    'slice_number'    => "1|1",
-  });
-
-#  warn "1. ",$self->image_width;
-#  warn "2. ",$object->param('image_width');
-
-  #send panel name to the ideogram glyphset
-  $pwuc->get_node('ideogram')->set('caption', $chr );
-
-  my $images;
-  push @$images, ( $pslice, $pwuc);
-
-  #add secondary slices
-  if (@{$other_locs}) {
-    foreach my $loc ( @{$other_locs}) {
-      $counter++;
-      my $slice = $loc->{'slice'}; 
-      my $species = $loc->{'real_species'};
-
-      my $wuc = $object->image_config_hash( "chromosome_$counter", 'MultiIdeogram', $species );
-      $wuc->container_width( $slice->seq_region_length );
-
-      $wuc->set_parameters({
-	'container_width' => $slice->seq_region_length,
-	'image_width'     => $self->image_width,
-	'slice_number'    => "$counter|1",
-      });
-      $wuc->get_node('ideogram')->set('caption', $loc->{'short_name'} );
-      push @$images, ( $slice, $wuc);
-    }
-  }
-  my $image = $self->new_image( $images );
-  return if $self->_export_image( $image );
+  
+  my $image = $self->new_image(\@images);
+  
+  return if $self->_export_image($image);
+  
   $image->imagemap = 'yes';
-  $image->set_button( 'drag', 'title' => 'Click or drag to centre display' );
+  $image->set_button('drag', 'title' => 'Click or drag to centre display');
   $image->{'panel_number'} = 'ideogram';
+  
   my $html = $image->render;
+  
   return $html;
 }
 
