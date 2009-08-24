d08a933fe68dbc5ea04028a5d3ba9e6c28309edd
Anne Parker <ap5@sanger.ac.uk>
2009-08-24 10:01:43 +0000

    Fixed validation to be ignored if no chromosomes, since it really only applies to the vertical drawing code

--- modules/EnsEMBL/Web/Text/FeatureParser.pm
+++ modules/EnsEMBL/Web/Text/FeatureParser.pm
@@ -115,12 +115,17 @@ sub parse {
           $columns = $self->parse_row($row);
         }
         if ($columns && scalar(@$columns)) {
-          my ($chr, $start, $end) = $empty->coords($columns);
 
-          ## Check the coordinates are valid for this assembly
-          next unless $valid_coords->{$chr}; ## Chromosome is valid and has length
-          next unless $start > 0 && $end <= $valid_coords->{$chr};
+          if (keys %$valid_coords) {
+            ## We only validate on chromosomal coordinates, to prevent errors on vertical code
+            my ($chr, $start, $end) = $empty->coords($columns);
+
+            ## Check the coordinates are valid for this assembly
+            next unless $valid_coords->{$chr}; ## Chromosome is valid and has length
+            next unless $start > 0 && $end <= $valid_coords->{$chr};
           
+          }
+
           ## Optional - filter content by location
           my $filter = $self->filter;
           if ($filter->{'chr'}) {
