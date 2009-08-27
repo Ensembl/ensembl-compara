af975b64c1d63852a07595db1f5874597230a0b7
Simon Brent <sb23@sanger.ac.uk>
2009-08-27 10:40:27 +0000

    add joining code

--- modules/EnsEMBL/Web/Component/Location/MultiBottom.pm
+++ modules/EnsEMBL/Web/Component/Location/MultiBottom.pm
@@ -17,37 +17,71 @@ sub content {
   
   my $object = $self->object;
   my $slice = $object->slice;
+  my $primary_species = $object->species;
   my $image_width = $self->image_width;
   my $slices = $object->multi_locations;
+  my $short_name = $slices->[0]->{'short_name'};
   my $max = scalar @$slices;
-  my $i = 1;
-  my @images;
-  my $primary_image_config;
-  
   my $base_url = $object->_url($object->multi_params);
+  my $i = 1;
+  my $primary_image_config;
+  my @images;
   
-  foreach (@$slices) {
-    my $image_config = $object->image_config_hash('contigview_bottom_' . $i, 'MultiBottom', $_->{'species'});
+  my $methods = {
+    BLASTZ_NET      => $object->param('pairwise_blastz'),
+    TRANSLATED_BLAT => $object->param('pairwise_tblat'),
+    OTHER           => $object->param('pairwise_align')
+  };
+  
+  my $multi = grep /yes/, values %$methods;
+  
+  foreach (@$slices) {  
+    my $image_config = $object->image_config_hash("contigview_bottom_$i", 'MultiBottom', $_->{'species'});
     
     $image_config->set_parameters({
       container_width => $_->{'slice'}->length,
       image_width     => $image_width,
       slice_number    => "$i|3",
-      caption         => $object->seq_region_type . ' ' . $object->seq_region_name,
       multi           => 1,
       compara         => $i == 1 ? 'primary' : 'secondary',
       base_url        => $base_url
     });
     
     $image_config->get_node('scalebar')->set('caption', $_->{'short_name'});
-    $image_config->mult;
     
     if ($i == 1) {
+      if ($max == 2) {
+        $image_config->multi($methods, $i, $max, $slices->[$i]->{'species'}) if $multi;
+        push @images, $slice, $image_config;
+      }
+      
       $primary_image_config = $image_config;
-      push @images, $slice, $primary_image_config unless $max > 2;
     } else {
+      $image_config->multi($methods, $i, $max, $primary_species) if $multi;
       push @images, $_->{'slice'}, $image_config;
-      push @images, $slice, $primary_image_config if $max > 2 && $i < $max;
+      
+      if ($max > 2 && $i < $max) {
+        # Make new versions of the primary image config because the alignments required will be different each time
+        if ($multi) {
+          my @species = map $slices->[$_]->{'species'}, $i-1, $i;
+          
+          $primary_image_config = $object->image_config_hash("contigview_bottom_1_$i", 'MultiBottom', $primary_species);
+          
+          $primary_image_config->set_parameters({
+            container_width => $slice->length,
+            image_width     => $image_width,
+            slice_number    => '1|3',
+            multi           => 1,
+            compara         => 'primary',
+            base_url        => $base_url
+          });
+          
+          $primary_image_config->get_node('scalebar')->set('caption', $short_name);
+          $primary_image_config->multi($methods, 1, $max, @species);
+        }
+        
+        push @images, $slice, $primary_image_config;
+      }
     }
     
     $i++;
