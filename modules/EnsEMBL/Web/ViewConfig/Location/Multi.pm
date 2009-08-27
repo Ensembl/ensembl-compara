4b9adc664a83d7ae5afb2a8755099a733a916d20
Simon Brent <sb23@sanger.ac.uk>
2009-08-27 10:42:23 +0000

    add pairwise options

--- modules/EnsEMBL/Web/ViewConfig/Location/Multi.pm
+++ modules/EnsEMBL/Web/ViewConfig/Location/Multi.pm
@@ -8,10 +8,13 @@ sub init {
   my $view_config = shift;
 
   $view_config->_set_defaults(qw(
-    panel_top      yes
+    panel_top       yes
     panel_zoom      no
-    zoom_width    100
-    context       1000
+    zoom_width      100
+    context         1000
+    pairwise_blastz no
+    pairwise_tblat  no
+    pairwise_align  no
   ));
   
   $view_config->add_image_configs({qw(
@@ -20,10 +23,32 @@ sub init {
     MultiBottom      nodas
   )});
   
-  $view_config->default_config = 'MultiBottom';
-  $view_config->storable       = 1;
+  $view_config->storable = 1;
 }
 
-sub form {}
+sub form {
+  my $view_config = shift;
+  
+  $view_config->add_form_element({
+    name   => 'pairwise_blastz',
+    label  => 'BLASTz net pairwise alignments',
+    type   => 'YesNo',
+    select => 'select'
+  });
+
+  $view_config->add_form_element({
+    name   => 'pairwise_tblat',
+    label  => 'Trans. BLAT net pairwise alignments',
+    type   => 'YesNo',
+    select => 'select'
+  });
+  
+  $view_config->add_form_element({
+    name   => 'pairwise_align',
+    label  => 'Pairwise alignments',
+    type   => 'YesNo',
+    select => 'select'
+  });
+}
 
 1;
