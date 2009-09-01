c9a675f8d532d6ae95262539bbe79a84cca1c4e1
Beth Yates <bp1@sanger.ac.uk>
2009-09-01 08:52:04 +0000

    added sample links to variation pages

--- modules/EnsEMBL/Web/Configuration/Info.pm
+++ modules/EnsEMBL/Web/Configuration/Info.pm
@@ -79,6 +79,9 @@ sub populate_tree {
 
     my $transcript_url  = "/$species_path/Transcript/Summary?t=".$sample_data->{'TRANSCRIPT_PARAM'};
     my $transcript_text = $sample_data->{'TRANSCRIPT_TEXT'} || 'not available';
+    
+    my $variation_url  = "/$species_path/Variation/Summary?v=".$sample_data->{'VARIATION_PARAM'};
+    my $variation_text = $sample_data->{'VARIATION_TEXT'} || 'not available';
 
     $data_menu->append( $self->create_node( 'Karyotype', $karyotype,
       [qw(location      EnsEMBL::Web::Component::Location::Genome)],
@@ -96,6 +99,10 @@ sub populate_tree {
     $data_menu->append( $self->create_node( 'Transcript', "Transcript ($transcript_text)",
       [qw(location      EnsEMBL::Web::Component::Transcript::Summary)],
       { 'availability' => 1, 'url' => $transcript_url, 'raw' => 1 }
+    ));
+    $data_menu->append( $self->create_node( 'Variation', "Variation ($variation_text)",
+      [qw(location      EnsEMBL::Web::Component::Variation::Summary)],
+      { 'availability' => 'database.variation', 'url' => $variation_url, 'raw' => 1 }
     ));
 
   }
