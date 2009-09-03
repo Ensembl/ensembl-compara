a220d97746d2bbafe6640a4dcaad44447ca60f6e
Eugene Kulesha <ek@ebi.ac.uk>
2009-09-03 15:32:15 +0000

    another attempt to back out gene xref changes

--- modules/EnsEMBL/Web/Component.pm
+++ modules/EnsEMBL/Web/Component.pm
@@ -270,6 +270,161 @@ sub new_karyotype_image {
 }
 
 sub _matches {
+  my(  $self, $key, $caption, @keys ) = @_;
+  my $object = $self->object;
+  my $label    = $object->species_defs->translate( $caption );
+  my $obj     = $object->Obj;
+
+  # Check cache
+  unless ($object->__data->{'links'}){
+    my @similarity_links = @{$object->get_similarity_hash($obj)};
+    return unless (@similarity_links);
+    $self->_sort_similarity_links( @similarity_links);
+  }
+
+  my @links = map { @{$object->__data->{'links'}{$_}||[]} } @keys;
+  return unless @links;
+
+  my $db = $object->get_db();
+  my $entry = $object->gene_type || 'Ensembl';
+
+  # add table call here
+  my $html;
+  if ($object->species_defs->ENSEMBL_SITETYPE eq 'Vega') {
+    $html = qq(<p></p>);
+  }
+  else {
+    $html = qq(<p><strong>This $entry entry corresponds to the following database identifiers:</strong></p>);
+  }
+  $html .= qq(<table cellpadding="4">);
+  if( $keys[0] eq 'ALT_TRANS' ) {
+      @links = $self->remove_redundant_xrefs(@links);
+  }
+  my $old_key = '';
+  foreach my $link (@links) {
+    my ( $key, $text ) = @$link;
+    if( $key ne $old_key ) {
+      if($old_key eq "GO") {
+        $html .= qq(<div class="small">GO mapping is inherited from swissprot/sptrembl</div>);
+      }
+      if( $old_key ne '' ) {
+        $html .= qq(</td></tr>);
+      }
+      $html .= qq(<tr><th style="white-space: nowrap; padding-right: 1em">$key:</th><td>);
+      $old_key = $key;
+    }
+    $html .= $text;
+  }
+  $html .= qq(</td></tr></table>);
+
+  return $html;
+}
+
+sub _sort_similarity_links {
+  my $self             = shift;
+  my $object           = $self->object;
+  my @similarity_links = @_;
+  my $database = $object->database;
+  my $db       = $object->get_db() ;
+  my $urls     = $object->ExtURL;
+  my @links ;
+
+  #default link to featureview is to retrieve an Xref
+  my $fv_type = $object->action eq 'Oligos' ? 'OligoFeature' : 'Xref';
+
+  my (%affy, %exdb);
+  # @ice names
+  foreach my $type (sort {
+    $b->priority        <=> $a->priority ||
+    $a->db_display_name cmp $b->db_display_name ||
+    $a->display_id      cmp $b->display_id
+  } @similarity_links ) {
+    my $link = "";
+    my $join_links = 0;
+    my $externalDB = $type->database();
+    my $display_id = $type->display_id();
+    my $primary_id = $type->primary_id();
+    next if ($type->status() eq 'ORTH');               # remove all orthologs
+    next if lc($externalDB) eq "medline";              # ditch medline entries - redundant as we also have pubmed
+    next if ($externalDB =~ /^flybase/i && $display_id =~ /^CG/ ); # Ditch celera genes from FlyBase
+    next if $externalDB eq "Vega_gene";                # remove internal links to self and transcripts
+    next if $externalDB eq "Vega_transcript";
+    next if $externalDB eq "Vega_translation";
+    next if ($externalDB eq 'OTTP') && $display_id =~ /^\d+$/; #don't show vega translation internal IDs
+    if( $externalDB eq "GO" ){
+      push @{$object->__data->{'links'}{'go'}} , $display_id;
+      next;
+    } elsif ($externalDB eq "GKB") {
+      my ($key, $primary_id) = split ':', $display_id;
+      push @{$object->__data->{'links'}{'gkb'}->{$key}} , $type ;
+      next;
+    }
+    my $text = $display_id;
+    (my $A = $externalDB ) =~ s/_predicted//;
+    if( $urls and $urls->is_linked( $A ) ) {
+      my $link;
+      $link = $urls->get_url( $A, $primary_id );
+
+      my $word = $display_id;
+      if( $A eq 'MARKERSYMBOL' ) {
+        $word = "$display_id ($primary_id)";
+      }
+      if( $link ) {
+        $text = qq(<a href="$link">$word</a>);
+      } else {
+        $text = qq($word);
+      }
+    }
+    if( $type->isa('Bio::EnsEMBL::IdentityXref') ) {
+      $text .=' <span class="small"> [Target %id: '.$type->target_identity().'; Query %id: '.$type->query_identity().']</span>';
+      $join_links = 1;
+    }
+    if( ( $object->species_defs->ENSEMBL_PFETCH_SERVER ) &&
+      ( $externalDB =~/^(SWISS|SPTREMBL|LocusLink|protein_id|RefSeq|EMBL|Gene-name|Uniprot)/i ) ) {
+      my $seq_arg = $display_id;
+      $seq_arg = "LL_$seq_arg" if $externalDB eq "LocusLink";
+      $text .= sprintf( ' [<a href="/%s/Transcript/Similarity/Align?t=%s;sequence=%s;db=%s">align</a>] ',
+                  $object->species, $object->stable_id, $seq_arg, $db );
+    }
+    if($externalDB =~/^(SWISS|SPTREMBL)/i) { # add Search GO link
+      $text .= ' [<a href="'.$urls->get_url('GOSEARCH',$primary_id).'">Search GO</a>]';
+    }
+    if( $type->description ) {
+      ( my $D = $type->description ) =~ s/^"(.*)"$/$1/;
+      $text .= "<br />".CGI::escapeHTML($D);
+      $join_links = 1;
+    }
+    if( $join_links  ) {
+      $text = qq(\n <div>$text);
+    } else {
+      $text = qq(\n <div class="multicol">$text);
+    }
+    # override for Affys - we don't want to have to configure each type, and
+    # this is an internal link anyway.
+    if( $externalDB =~ /^AFFY_/i) {
+      next if ($affy{$display_id} && $exdb{$type->db_display_name}); ## remove duplicates
+      $text = "\n".'  <div class="multicol"> '.$display_id;
+      $affy{$display_id}++;
+      $exdb{$type->db_display_name}++;
+    }
+
+    #add link to featureview
+    my $link_name = ($fv_type eq 'OligoFeature') ? $display_id : $primary_id;
+    my $link_type = ($fv_type eq 'OligoFeature') ? $fv_type : $fv_type . "_$externalDB";
+    my $k_url = $object->_url({
+	'type'   => 'Location',
+	'action' => 'Genome',
+	'id'     => $link_name,
+	'ftype'  => $link_type,
+    });
+    $text .= qq(  [<a href="$k_url">view all locations</a>]);
+
+    $text .= '</div>';
+    push @{$object->__data->{'links'}{$type->type}}, [ $type->db_display_name || $externalDB, $text ] ;
+  }
+}
+
+sub _matches_ek {
   my ( $self, $key, $caption, @keys ) = @_;
   my $object = $self->object;
   my $label  = $object->species_defs->translate( $caption );
@@ -315,7 +470,7 @@ sub _matches {
   return $html;
 }
 
-sub _sort_similarity_links {
+sub _sort_similarity_links_ek {
   my $self = shift;
   my @similarity_links = @_;
   
