de752e0bad83cd8958b1802a28ee1dfb57a809a9
Beth Yates <bp1@sanger.ac.uk>
2009-09-10 09:41:01 +0000

    added zmenu code

--- modules/Bio/EnsEMBL/GlyphSet/fg_wiggle.pm
+++ modules/Bio/EnsEMBL/GlyphSet/fg_wiggle.pm
@@ -84,7 +84,23 @@ sub process_wiggle_data {
 }
 
 sub block_features_zmenu {
-  return;
+  my ($self, $f) = @_;
+  my $offset = $self->{'container'}->strand > 0 ? $self->{'container'}->start - 1 :  $self->{'container'}->end + 1;
+  my $pos = $f->slice->seq_region_name .":". ($offset + $f->start )."-".($f->end+$offset);
+  my $feature_set = $f->feature_set->name;
+
+  my $id = $self->{'config'}->core_objects->regulation->stable_id;
+  my $href = $self->_url
+  ({
+    'action'  => 'FeatureEvidence',
+    'rf'      => $id,
+    'fdb'     => 'funcgen',
+    'pos'     => $pos,
+    'fs'      => $feature_set,
+  });
+
+  return $href;
 }
+
 
 1;
