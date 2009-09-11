4990096f9e7a4b055ee4f807baee1d96f8e9ed95
Anne Parker <ap5@sanger.ac.uk>
2009-09-11 10:03:30 +0000

    Added getter method for 'score'

--- modules/EnsEMBL/Web/Text/Feature/BED.pm
+++ modules/EnsEMBL/Web/Text/Feature/BED.pm
@@ -31,6 +31,7 @@ sub strand   { my $self = shift;
 sub rawstart { my $self = shift; return $self->{'__raw__'}[1]; }
 sub rawend   { my $self = shift; return $self->{'__raw__'}[2]; }
 sub id       { my $self = shift; return $self->{'__raw__'}[3]; }
+sub score    { my $self = shift; return $self->{'__raw__'}[4]; }
 sub external_data { my $self = shift; return $self->{'__extra__'} ? $self->{'__extra__'} : undef ; }
 
 sub cigar_string {
