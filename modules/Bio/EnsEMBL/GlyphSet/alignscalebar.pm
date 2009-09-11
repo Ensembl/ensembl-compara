e8a552ab7fa15ae21eb97b00b47b5d1484ab5a71
Simon Brent <sb23@sanger.ac.uk>
2009-09-11 16:06:41 +0000

    fix zmenu issue, positioning of align break glyphs. Add legend

--- modules/Bio/EnsEMBL/GlyphSet/alignscalebar.pm
+++ modules/Bio/EnsEMBL/GlyphSet/alignscalebar.pm
@@ -23,18 +23,16 @@ sub _init {
   
   # Draw the species separator line
   if ($strand > 0 && $compara ne 'primary') {
-    my $line = $self->Rect({
-      'x'             => -120,
-      'y'             => 0,
-      'colour'        => 'black',
-      'width'         => 20000,
-      'height'        => 0,
-      'absolutex'     => 1,
-      'absolutewidth' => 1,
-      'absolutey'     => 1
-    });
-      
-    $self->push($line);
+    $self->push($self->Rect({
+      x             => -120,
+      y             => -3,
+      colour        => 'black',
+      width         => 20000,
+      height        => 0,
+      absolutex     => 1,
+      absolutewidth => 1,
+      absolutey     => 1
+    }));
   }
 }
 
@@ -59,11 +57,11 @@ sub render_align_gap {
 
   my @inters = split /([MDG])/, $cigar_line;
   
-  my $ms = 0;
+  my $ms        = 0;
   my $box_start = 0;
-  my $box_end = 0;
-  my $colour = 'white';
-  my $z = -10;
+  my $box_end   = 0;
+  my $colour    = 'white';
+  my $join_z    = -10;
 
   while (@inters) {
     $ms = shift @inters || 1;
@@ -72,35 +70,37 @@ sub render_align_gap {
     
     $box_end = $box_start + $ms - 1;
     
-    # Skip normal alignment and gaps in alignments
-    $box_start = $box_end + 1 and next if $mtype =~ /G|M/;
-    
-    if ($box_start > $ge) {
-      $si++;
-      $hs = $mp->[$si] or return;
-      $gs = $hs->{'start'} - 1;
-      $ge = $hs->{'end'};
-    }
-    
-    if ($ms > $min_length && $box_start >= $gs && $box_end < $ge) { 
-      my $glyph = $self->Rect({
-        'x'         => $box_start,
-        'y'         => $y,
-        'z'         => $z,
-        'width'     => abs($box_end - $box_start + 1),
-        'height'    => 3,
-        'colour'    => $colour, 
-        'absolutey' => 1
-      });
+     # Skip normal alignment and gaps in alignments
+    if ($mtype !~ /G|M/) {
+      if ($box_start > $ge) {
+        $hs = $mp->[++$si];
+        
+        return unless $hs;
+        
+        $gs = $hs->{'start'} - 1;
+        $ge = $hs->{'end'};
+      }
       
-      $self->push($glyph);
-      
-      if ($self->{'strand'} < 0) {
-        $self->join_tag($glyph, "alignsliceG_$box_start", 0, 0, $colour, 'fill', $z);
-        $self->join_tag($glyph, "alignsliceG_$box_start", 1, 0, $colour, 'fill', $z);
-      } else {
-        $self->join_tag($glyph, "alignsliceG_$box_start", 1, 1, $colour, 'fill', $z);
-        $self->join_tag($glyph, "alignsliceG_$box_start", 0, 1, $colour, 'fill', $z);
+      if ($ms > $min_length && $box_start >= $gs && $box_end < $ge) { 
+        my $glyph = $self->Rect({
+          x         => $box_start,
+          y         => $y,
+          z         => $join_z,
+          width     => abs($box_end - $box_start + 1),
+          height    => 3,
+          colour    => $colour, 
+          absolutey => 1
+        });
+        
+        if ($self->{'strand'} < 0) {
+          $self->join_tag($glyph, "alignsliceG_$box_start", 0, 0, $colour, 'fill', $join_z);
+          $self->join_tag($glyph, "alignsliceG_$box_start", 1, 0, $colour, 'fill', $join_z);
+        } else {
+          $self->join_tag($glyph, "alignsliceG_$box_start", 1, 1, $colour, 'fill', $join_z);
+          $self->join_tag($glyph, "alignsliceG_$box_start", 0, 1, $colour, 'fill', $join_z);
+        }
+        
+        $self->push($glyph);
       }
     }
     
@@ -114,40 +114,34 @@ sub render_align_bar {
   
   my ($global_start, $global_end, $yc) = @_;
   
-  my $species = $self->species;
-  my $mp = $self->{'container'}->{'slice_mapper_pairs'};
-  
+  my $config      = $self->{'config'};
+  my $species     = $self->species;
   my $pix_per_bp  = $self->scalex;
-  my $last_end = -1;
-  my $last_chr = -1;
-  my $zc = -20;
-  my $last_s2s = -1;
-  my $last_s2e = -1;
-  my $last_s2st = 0;
+  my $last_end    = -1;
+  my $last_chr    = -1;
+  my $join_z      = -20;
+  my $last_s2s    = -1;
+  my $last_s2e    = -1;
+  my $last_s2st   = 0;
 
   my %colour_map;
   my %colour_map2;
-  my @colours2 = qw(antiquewhite3 brown gray rosybrown1 blue green red gray yellow);
   my @colours = qw(antiquewhite1 mistyrose1 burlywood1 khaki1 cornsilk1 lavenderblush1 lemonchiffon2 darkseagreen2 lightcyan1 papayawhip seashell1);
 
-  foreach my $s (sort {$a->{'start'} <=> $b->{'start'}} @$mp) {
-    my $s2 = $s->{'slice'};
-    
-    my $ss = $s->{'start'};
-    my $sst = $s->{'strand'};
-    my $se = $s->{'end'};
-    
-    my $s2s = $s2->{'start'};
-    my $s2e = $s2->{'end'};
-    my $s2st = $s2->{'strand'};
-    my $s2t = $s2->{'seq_region_name'};
-    
+  foreach my $s (sort {$a->{'start'} <=> $b->{'start'}} @{$self->{'container'}->{'slice_mapper_pairs'}}) {
+    my $s2        = $s->{'slice'};
+    my $ss        = $s->{'start'};
+    my $sst       = $s->{'strand'};
+    my $se        = $s->{'end'};
+    my $s2s       = $s2->{'start'};
+    my $s2e       = $s2->{'end'};
+    my $s2st      = $s2->{'strand'};
+    my $s2t       = $s2->{'seq_region_name'};
     my $box_start = $ss;
     my $box_end   = $se;
-    my $filled = $sst;
-    my $s2l = abs($s2e - $s2s) + 1;
-    my $sl = abs($se - $ss) + 1;
-    
+    my $filled    = $sst;
+    my $s2l       = abs($s2e - $s2s) + 1;
+    my $sl        = abs($se - $ss) + 1;
     my ($title, $href);
     
     if ($s2t eq 'GAP') {
@@ -166,9 +160,10 @@ sub render_align_bar {
     $colour_map{$s2t}  ||= shift @colours || 'grey';
     $colour_map2{$s2t} ||= 'darksalmon';
     
+    my $col  = $colour_map{$s2t};
     my $col2 = $colour_map2{$s2t};
     
-    my $t = $self->Rect({
+    my $glyph = $self->Rect({
       x         => $box_start - $global_start, 
       y         => $yc,
       width     => abs($box_end - $box_start + 1),
@@ -179,30 +174,23 @@ sub render_align_bar {
       ($filled == 1 ? 'colour' : 'bordercolour') => $col2
     });
     
-    $self->push($t);
-    
-    my $col = $colour_map{$s2t};
-    
     if ($self->{'strand'} < 0) {
-      $self->join_tag($t, "alignslice_$box_start", 0, 0, $col, 'fill', $zc);
-      $self->join_tag($t, "alignslice_$box_start", 1, 0, $col, 'fill', $zc);
+      $self->join_tag($glyph, "alignslice_$box_start", 0, 0, $col, 'fill', $join_z);
+      $self->join_tag($glyph, "alignslice_$box_start", 1, 0, $col, 'fill', $join_z);
     } else {
-      $self->join_tag($t, "alignslice_$box_start", 1, 1, $col, 'fill', $zc);
-      $self->join_tag($t, "alignslice_$box_start", 0, 1, $col, 'fill', $zc);
+      $self->join_tag($glyph, "alignslice_$box_start", 1, 1, $col, 'fill', $join_z);
+      $self->join_tag($glyph, "alignslice_$box_start", 0, 1, $col, 'fill', $join_z);
     }
+    
+    $self->push($glyph);
     
     # This happens when we have two contiguous underlying slices
     if ($last_end == $ss - 1) {
-      my $s3l = $s2s - $last_s2e - 1;
-      
-      if ($s2st == -1 && $last_s2st == -1) {
-        $s3l = $s2e - $last_s2s + 1;
-      }
-      
-      my $xc = $box_start - $global_start;
-      my $h = $yc - 2;
-      
+      my $s3l = $s2st == -1 && $last_s2st == -1 ? $s2e - $last_s2s + 1 : $s2s - $last_s2e - 1;
+      my $xc  = $box_start - $global_start;
+      my $h   = $yc - 2;
       my $colour;
+      my $legend;
       
       $href = '';
       
@@ -210,14 +198,17 @@ sub render_align_bar {
         # Different chromosomes
         $colour = 'black';
         $title = "AlignSlice Break; There is a breakpoint in the alignment between chromosome $last_chr and $s2t";
+        $legend = 'Breakpoint between different chromosomes';
       } elsif ($last_s2st ne $s2st) {
         # Same chromosome, different strand (inversion)
         $colour = '3333ff';
         $title = "AlignSlice Break; Info: There is an inversion in chromosome $s2t";
+        $legend = 'Inversion in chromosome';
       } elsif ($s3l > 0) {
         # Same chromosome, same strand, gap between the two underlying slices
         my ($from, $to);
         $colour = 'red';
+        $legend = 'Gap between two underlying slices';
         
         if ($s2st == 1) {
           $from = $last_s2e;
@@ -227,28 +218,41 @@ sub render_align_bar {
           $to = $last_s2s;
         }
         
-        $href = $self->_url({
-          species => $species,
-          r       => "$s2t:$from-$to",
-          break   => 1
-        });
+        $href = $self->_url({ species => $species, r => "$s2t:$from-$to", break => 1 });
       } else {
         # Same chromosome, same strand, no gap between the two underlying slices (BreakPoint in another species)
         $colour = 'indianred3';
         $title = "AlignSlice Break; There is a breakpoint in the alignment on chromosome $s2t";
+        $legend = 'Breakpoint on chromosome';
       }
       
+      my $base = $self->strand == 1 ? $h - 3 : $h + 9;
+      
       $self->push($self->Poly({
-        points    => [ 
-          $xc - 2/$pix_per_bp, $h,
-          $xc, $h + 6,
-          $xc + 2/$pix_per_bp, $h
-        ],
         colour    => $colour,
+        absolutey => 1,
+        points    => [ 
+          $xc - 2/$pix_per_bp, $base,
+          $xc, $h + 3,
+          $xc + 2/$pix_per_bp, $base
+        ]
+      }));
+      
+      # use a rectangle for the zmenu hit box because the triangles are hard to click on
+      $self->push($self->Rect({
+        x         => $xc - 2/$pix_per_bp,
+        y         => $self->strand == 1 ? $h-3 : $h+3,
+        width     => 4/$pix_per_bp,
+        height    => 6,
         absolutey => 1,
         title     => $title,
         href      => $href
       }));
+      
+      $config->{'alignslice_legend'}{$colour} = {
+        priority => $self->_pos,
+        legend   => $legend
+      };
     }
     
     $last_end = $se;
