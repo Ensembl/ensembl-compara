816d2b26eb5f559a6dcf7a321a0f1cd71a854c59
Simon Brent <sb23@sanger.ac.uk>
2009-09-11 16:11:00 +0000

    add legend

--- modules/EnsEMBL/Web/ImageConfig/alignsliceviewbottom.pm
+++ modules/EnsEMBL/Web/ImageConfig/alignsliceviewbottom.pm
@@ -14,36 +14,33 @@ sub init {
   my $self = shift;
   
   $self->set_parameters({
-    'title'         => 'Detailed panel',
-    'show_buttons'  => 'no',  # do not show +/- buttons
-    'button_width'  => 8,     # width of red "+/-" buttons
-    'show_labels'   => 'yes', # show track names on left-hand side
-    'label_width'   => 113,   # width of labels on left-hand side
-    'margin'        => 5,     # margin
-    'spacing'       => 2      # spacing
+    title         => 'Detailed panel',
+    show_buttons  => 'no',  # do not show +/- buttons
+    button_width  => 8,     # width of red "+/-" buttons
+    show_labels   => 'yes', # show track names on left-hand side
+    label_width   => 113,   # width of labels on left-hand side
+    margin        => 5,     # margin
+    spacing       => 2      # spacing
   });
 
   $self->create_menus(
-    'sequence'    => 'Sequence',
-    'transcript'  => 'Genes',
-    'repeat'      => 'Repeats',
-    'variation'   => 'Variation features',
-    'information' => 'Information'
+    sequence    => 'Sequence',
+    transcript  => 'Genes',
+    repeat      => 'Repeats',
+    variation   => 'Variation features',
+    information => 'Information'
   );
   
-  $self->add_tracks('sequence', 
-    [ 'contig', 'Contigs', 'stranded_contig', { 'display' => 'normal', 'strand' => 'r', 'description' => 'Track showing underlying assembly contigs' }],
+  $self->add_track('sequence', 'contig', 'Contigs', 'stranded_contig', { display => 'normal', strand => 'r', description => 'Track showing underlying assembly contigs' });
+  
+  $self->add_tracks('information', 
+    [ 'alignscalebar',     '',                  'alignscalebar',     { display => 'normal', strand => 'b', menu => 'no' }],
+    [ 'ruler',             '',                  'ruler',             { display => 'normal', strand => 'f', menu => 'no' }],
+    [ 'draggable',         '',                  'draggable',         { display => 'normal', strand => 'b', menu => 'no' }],
+    [ 'alignslice_legend', 'AlignSlice Legend', 'alignslice_legend', { display => 'normal', strand => 'r' }]
   );
   
   $self->load_tracks;
-  
-  $self->add_tracks('information',
-    [ 'missing',       '', 'text',          { 'display' => 'normal', 'strand' => 'r', 'name' => 'Disabled track summary', 'description' => 'Show counts of number of tracks turned off by the user' }],
-    [ 'info',          '', 'text',          { 'display' => 'normal', 'strand' => 'r', 'name' => 'Information',            'description' => 'Details of the region shown in the image' }],
-    [ 'alignscalebar', '', 'alignscalebar', { 'display' => 'normal', 'strand' => 'b', 'menu' => 'no' }],
-    [ 'ruler',         '', 'ruler',         { 'display' => 'normal', 'strand' => 'f', 'menu' => 'no' }],
-    [ 'draggable',     '', 'draggable',     { 'display' => 'normal', 'strand' => 'b', 'menu' => 'no' }]
-  );
   
   $self->modify_configs(
     [ 'transcript' ],
