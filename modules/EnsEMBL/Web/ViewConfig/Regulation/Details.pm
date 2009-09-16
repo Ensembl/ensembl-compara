04c50c30c1558ca9f8b5b31db64c613bc7e120f6
Beth Yates <bp1@sanger.ac.uk>
2009-09-16 14:33:23 +0000

    added context selection

--- modules/EnsEMBL/Web/ViewConfig/Regulation/Details.pm
+++ modules/EnsEMBL/Web/ViewConfig/Regulation/Details.pm
@@ -7,12 +7,14 @@ sub init {
 
   $view_config->_set_defaults(qw(
     image_width         800
+    context             200
     das_sources),       []
   );
 
   $view_config->add_image_configs({qw(
     reg_detail das
   )});
+
 
   ## Add config for focus feature track 
   $view_config->_set_defaults('opt_focus' => 'yes');
@@ -23,14 +25,34 @@ sub init {
     $view_config->_set_defaults('opt_ft_'.$_ => 'on');
   }
 
+  $view_config->has_images(1);
   $view_config->storable = 1;
 }
 
 sub form {
   my( $view_config, $object ) = @_;  
+
+  ### Add context selection
+  $view_config->add_fieldset('Context');
+  $view_config->add_form_element({
+    'type'     => 'DropDown', 'select'   => 'select',
+    'required' => 'yes',      'name'     => 'context',
+    'label'    => 'Context',
+    'values'   => [
+      { 'value' => '20',   'name' => '20bp' },
+      { 'value' => '50',   'name' => '50bp' },
+      { 'value' => '100',  'name' => '100bp' },
+      { 'value' => '200',  'name' => '200bp' },
+      { 'value' => '500',  'name' => '500bp' },
+      { 'value' => '1000', 'name' => '1000bp' },
+      { 'value' => '2000', 'name' => '2000bp' },
+      { 'value' => '5000', 'name' => '5000bp' },
+    ]
+  });
+
   my $reg_object = $object->core_objects->regulation;
   if ($reg_object->get_focus_attributes){
-    $view_config->add_form_element({ 'type' => 'YesNo', 'name' => 'opt_focus',  'select' => 'select', 'label'  => 'Show Focus track:' });
+    $view_config->add_form_element({ 'type' => 'YesNo', 'name' => 'opt_focus',  'select' => 'select', 'label'  => 'Show Core Evidence track' });
   }
   
   my $non_focus = $reg_object->get_nonfocus_attributes;
