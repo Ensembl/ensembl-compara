22a8397ff980fc5db4cb525fb4918a865ea14720
Simon Brent <sb23@sanger.ac.uk>
2009-10-21 09:40:46 +0000

    cleanup

--- modules/EnsEMBL/Web/Form.pm
+++ modules/EnsEMBL/Web/Form.pm
@@ -3,13 +3,16 @@
 package EnsEMBL::Web::Form;
 
 use strict;
-use EnsEMBL::Web::Root;
-use EnsEMBL::Web::Form::FieldSet;
+
 use CGI qw(escapeHTML);
-our @ISA = qw( EnsEMBL::Web::Root );
+
+use EnsEMBL::Web::Form::FieldSet;
+
+use base qw(EnsEMBL::Web::Root);
 
 sub new {
-  my( $class, $name, $action, $method, $style ) = @_;
+  my ($class, $name, $action, $method, $style) = @_;
+  
   my $self = {
     '_attributes' => {
         'action'   => $action,
@@ -22,114 +25,112 @@ sub new {
     '_fieldsets'      => [],
     '_form_id'        => 1
   };
+  
   bless $self, $class;
+  
   return $self;
 }
 
+# Add a button element to the form - used particularly for adding multiple buttons, e.g. on wizards
 sub add_button {
-### Add a button element to the form - used particularly for adding multiple buttons, e.g. on wizards
-  my( $self, %options ) = @_;
+  my ($self, %options) = @_;
+  
   my $type = $options{'type'};
+  
   if ($type eq 'Submit' || $type eq 'Button') {
     my $module = "EnsEMBL::Web::Form::Element::$type";
-    if( $self->dynamic_use( $module ) ) {
-      my $button = $module->new( 'form' => $self->{'_attributes'}{'id'}, %options );
+    
+    if ($self->dynamic_use($module)) {
+      my $button = $module->new('form' => $self->{'_attributes'}{'id'}, %options);
       push @{$self->{'_buttons'}}, $button;
-    } 
-    else {
+    } else {
       warn "Button module $module appears to be missing!";
     }
-  }
-  else {
-    warn "Not a button module!";
+  } else {
+    warn 'Not a button module!';
   }
 } 
 
 sub extra_buttons {
   my ($self, $buttons) = @_;
-  if ($buttons) {
-    $self->{'_extra_buttons'} = $buttons;
-  }
+  $self->{'_extra_buttons'} = $buttons if $buttons;
   return $self->{'_extra_buttons'};
 }
 
+# Add an attribute to the FORM tag
 sub add_attribute {
-### Add an attribute to the FORM tag
-  my( $self, $type, $value ) = @_;
-  if( $type eq 'class' && $self->{'_attributes'}{'class'} ) {
-    $self->{'_attributes'}{$type}.= " $value";
+  my ($self, $type, $value) = @_;
+  
+  if ($type eq 'class' && $self->{'_attributes'}{'class'}) {
+    $self->{'_attributes'}{$type} .= " $value";
   } else {
     $self->{'_attributes'}{$type} = $value;
   }
 }
 
+# Add a fieldset object to the form
 sub add_fieldset {
-### Add a fieldset object to the form
-  my( $self, %options ) = @_;
-  my $fieldset = EnsEMBL::Web::Form::FieldSet->new('form' => $self->{'_attributes'}{'id'}, %options );
-  if (!$fieldset->{'_name'}) {
-    $fieldset->{'_name'} =  $self->_next_id();
-  }
+  my ($self, %options) = @_;
+  
+  my $fieldset = EnsEMBL::Web::Form::FieldSet->new('form' => $self->{'_attributes'}{'id'}, %options);
+  $fieldset->{'_name'} =  $self->_next_id unless $fieldset->{'_name'};
   push @{$self->{'_fieldsets'}}, $fieldset;
+  
   return $fieldset;
 }
 
+# Returns an autoincremented ID for fieldset (used if not defined manually in the component) 
 sub _next_id {
-### Returns an autoincremented ID for fieldset (used if not defined manually in the component) 
   my $self = shift;
-  return $self->{'_attributes'}{'id'}.'_'.($self->{'_form_id'}++);
+  return $self->{'_attributes'}{'id'} . '_' . ($self->{'_form_id'}++);
 }
 
 sub _render_buttons {
   my $self = shift;
+  
   return unless  @{$self->{'_buttons'}};
+  
   my $class = $self->{'_attributes'}{'class'};
   $class =~ s/check//;
-  my $output = qq(<table style="width:100%" class="$class"><tbody>
-<tr>
-<th>&nbsp;</th><td>);
-  foreach my $button ( @{$self->{'_buttons'}}) {
-    $output .= $button->render;
-  }
-  $output .= qq(</td>
-</tr>
-</tbody></table>);
+  
+  my $output = qq{
+  <table style="width:100%" class="$class">
+  <tbody>
+    <tr>
+    <th>&nbsp;</th><td>
+  };
+  
+  $output .= $_->render for @{$self->{'_buttons'}};
+  
+  $output .= qq{
+    </td>
+  </tr>
+  </tbody>
+  </table>};
 
   return $output;
 }
 
 sub add_hidden {
-  my $self = shift;
-  my $hash_ref = shift;
-  foreach (keys %{ $hash_ref || {} } ) {
-    $self->add_element('type'=>'Hidden','name'=>$_,'value'=>$hash_ref->{$_});
-  }
+  my ($self, $hidden) = @_;
+  
+  $self->add_element('type' => 'Hidden', 'name' => $_, 'value' => $hidden->{$_}) for keys %{$hidden||{}};
 }
 
 sub add_notes {
   my ($self, $notes) = @_;
-  my @fieldsets = @{$self->{'_fieldsets'}};
-  my $fieldset = $fieldsets[0];
-  if ($fieldset && $notes) {
-    $fieldset->notes($notes);
-  }
+  
+  my $fieldset = $self->{'_fieldsets'}->[0];
+  
+  $fieldset->notes($notes) if $fieldset && $notes;
 } 
 
 # Render the FORM tag and its contents
 sub render {
   my $self = shift;
   
-  my $has_file = 0; 
-  my $required = 0;
-  
-  foreach my $fieldset (@{$self->{'_fieldsets'}}) {
-    $has_file = 1 if $fieldset->{'_file'};
-    $required = 1 if $fieldset->{'_required'};
-    
-    last if $has_file && $required;
-  }
-  
-  if ($has_file) { # File types must always be multipart Posts 
+  if (grep $_->{'_file'}, @{$self->{'_fieldsets'}}) {
+    #  File types must always be multipart Posts
     $self->add_attribute('method',  'post');
     $self->add_attribute('class',   'upload');
     $self->add_attribute('enctype', 'multipart/form-data');
@@ -142,38 +143,40 @@ sub render {
     );
   }
   
-  $self->add_element(type => 'Information', value => 'Fields marked with <strong>*</strong> are required') if $required;
-  
   my $output = '<form';
   
   while (my ($k, $v) = each (%{$self->{'_attributes'}})) {
-    $output .= sprintf ' %s="%s"', CGI::escapeHTML($k), CGI::escapeHTML($v);
+    $output .= sprintf ' %s="%s"', escapeHTML($k), escapeHTML($v);
   }
   
   $output .= '>';
   $output .= $self->_render_buttons if $self->{'_extra_buttons'} eq 'top';
   $output .= $_->render for @{$self->{'_fieldsets'}};
-  
   $output .= $self->_render_buttons;
   $output .= "\n</form>\n";
-  $output .= '<div style="height:1px;overflow: hidden;clear:both;font-size:1pt">&nbsp;</div>';
+  $output .= '<div style="height:1px; overflow:hidden; clear:both; font-size:1pt">&nbsp;</div>';
   
   return $output;
 }
 
+# Replacement for old method, included for backwards compatibility
+# Tries to add the element to the last fieldset, or creates a new one if none exist
 sub add_element {
-### x
-### Replacement for old method, included for backwards compatibility
-### Tries to add the element to the last fieldset, or creates a new one if none exist
+  my ($self, %options) = @_;
 
-  my( $self, %options ) = @_;
-
-  my $fieldset = $self->{'_fieldsets'}[-1];
+  my $fieldset = $self->{'_fieldsets'}->[-1];
+  my $new_fieldset;
+  
   if (!$fieldset) {
+    $new_fieldset = 1;
     $fieldset = EnsEMBL::Web::Form::FieldSet->new('form' => $self->{'_attributes'}{'id'});
+    $fieldset->class('generic');
     push @{$self->{'_fieldsets'}}, $fieldset;
   }
+  
   $fieldset->add_element(%options);
+  
+  return $fieldset if $new_fieldset;
 }
 
 1;
--- modules/EnsEMBL/Web/Proxiable.pm
+++ modules/EnsEMBL/Web/Proxiable.pm
@@ -11,7 +11,31 @@ use EnsEMBL::Web::Root;
 use EnsEMBL::Web::DBSQL::DBConnection;
 use CGI qw(escape escapeHTML);
 
-use base qw( EnsEMBL::Web::Root );
+use base qw(EnsEMBL::Web::Root);
+
+sub new {
+  my ($class, $data) = @_;
+  my $self = { data => $data };
+  bless $self, $class;
+  return $self; 
+}
+
+sub apache_handle     { return $_[0]->{'data'}{'_apache_handle'}; }
+sub __data            { return $_[0]->{'data'}; }
+sub core_objects      { return $_[0]->{'data'}{'_core_objects'}; }
+sub type              { return $_[0]->{'data'}{'_type'}; }
+sub action            { return $_[0]->{'data'}{'_action'};  }
+sub function          { return $_[0]->{'data'}{'_function'};  }
+sub script            { return $_[0]->{'data'}{'_script'};  }
+sub species           { return $_[0]->{'data'}{'_species'}; }
+sub species_defs      { return $_[0]->{'data'}{'_species_defs'} ||= $ENSEMBL_WEB_REGISTRY->species_defs; }
+sub DBConnection      { return $_[0]->{'data'}{'_databases'}    ||= new EnsEMBL::Web::DBSQL::DBConnection($_[0]->species, $_[0]->species_defs); }
+sub ExtURL            { return $_[0]->{'data'}{'_ext_url_'}     ||= new EnsEMBL::Web::ExtURL($_[0]->species, $_[0]->species_defs); } # Handling ExtURLs
+sub session           { return $_[0]->{'session'} ||= $ENSEMBL_WEB_REGISTRY->get_session; }
+sub get_session       { return $_[0]->{'session'} ||  $ENSEMBL_WEB_REGISTRY->get_session; }
+sub delete_param      { my $self = shift; $self->{'data'}{'_input'}->delete(@_); }
+sub get_databases     { my $self = shift; $self->DBConnection->get_databases(@_); }
+sub databases_species { my $self = shift; $self->DBConnection->get_databases_species(@_); }
 
 sub table_info {
   my $self = shift;
@@ -20,59 +44,58 @@ sub table_info {
 
 sub _url {
   my $self = shift;
-  my $params  = shift || {};
+  my $params = shift || {};
+  
   die "Not a hashref while calling _url ($params @_)" unless ref($params) eq 'HASH';
-  my $species = exists( $params->{'species'} ) ? $params->{'species'} : $self->species;
-  my $type    = exists( $params->{'type'}    ) ? $params->{'type'}    : $self->type;
-  my $action  = exists( $params->{'action'}  ) ? $params->{'action'}  : $self->action;
-  my $fn      = exists( $params->{'function'}) ? $params->{'function'} :
-                    ( $action eq $self->action ? $self->function      : undef );
-
-  my %pars = %{$self->core_objects->{'parameters'}};
-  if( $params->{'__clear'} ) {
+  
+  my $species = exists($params->{'species'})  ? $params->{'species'}  : $self->species;
+  my $type    = exists($params->{'type'})     ? $params->{'type'}     : $self->type;
+  my $action  = exists($params->{'action'})   ? $params->{'action'}   : $self->action;
+  my $fn      = exists($params->{'function'}) ? $params->{'function'} : ($action eq $self->action ? $self->function : undef);
+  my %pars    = %{$self->core_objects->{'parameters'}};
+  
+  if ($params->{'__clear'}) {
     %pars = ();
     delete $params->{'__clear'};
   }
+  
   delete $pars{'t'}  if $params->{'pt'};
   delete $pars{'pt'} if $params->{'t'};
   delete $pars{'t'}  if $params->{'g'} && $params->{'g'} ne $pars{'g'};
   delete $pars{'time'};
-
-  foreach( keys %$params ) {
+  
+  foreach (keys %$params) {
     next if $_ =~ /^(species|type|action|function)$/;
-    if( defined( $params->{$_} ) ) {
+    
+    if (defined $params->{$_}) {
       $pars{$_} = $params->{$_};
     } else {
       delete $pars{$_};
     }
   }
-  my $URL = sprintf '/%s/%s/%s', $species, $type, $action.( $fn ? "/$fn" : '' );
+  
+  my $url = sprintf '/%s/%s/%s', $species, $type, $action.( $fn ? "/$fn" : '' );
   my $join = '?';
-## Sort the keys so that the URL is the same for a given set of parameters...
+  
   my $flag = shift;
-  if( $flag ) {
-    return [$URL, \%pars];
-  } 
-  foreach ( sort keys %pars ) {
+  
+  return [$url, \%pars] if $flag;
+  
+  # Sort the keys so that the url is the same for a given set of parameters
+  foreach (sort keys %pars) {
     next unless defined $pars{$_};
-    $URL .= sprintf '%s%s=%s', $join, escape($_), $self->hack_escape($pars{$_}) ;
+    
+    $url .= sprintf '%s%s=%s', $join, escape($_), $self->hack_escape($pars{$_});
     $join = ';';
   }
 
-  return $URL;
+  return $url;
 }
 
 sub hack_escape {
-  my( $self, $s ) = @_;
+  my ($self, $s) = @_;
   (my $t = escape($s)) =~ s/%3A/:/g;
   return $t;
-}
-
-sub new {
-  my( $class, $data ) = @_;
-  my $self = { 'data' => $data };
-  bless $self, $class;
-  return $self; 
 }
 
 sub timer_push {
@@ -80,16 +103,10 @@ sub timer_push {
   return $self->{'data'}{'timer'}->push(@_);
 }
 
-sub __data { return $_[0]{'data'}; }
 sub input { 
   my $self = shift;
   $self->{'data'}{'_input'} = shift if @_;
   return $self->{'data'}{'_input'};
-}
-
-sub core_objects {
-  my $self = shift;
-  return $self->{'data'}{'_core_objects'};
 }
 
 sub _sanitize {
@@ -99,8 +116,8 @@ sub _sanitize {
   return $T;
 }
 
+# Does an ordinary CGI redirect
 sub redirect {
-## Does an ordinary CGI redirect
   my ($self, $url) = @_;
   $self->{'data'}{'_input'}->redirect($url);
 }
@@ -132,7 +149,7 @@ sub param {
 
 sub input_param  {
   my $self = shift;
-  return _sanitize( $self->{'data'}{'_input'}->param(@_) );
+  return _sanitize($self->{'data'}{'_input'}->param(@_));
 }
 
 sub multi_params {
@@ -148,106 +165,65 @@ sub multi_params {
   return \%params;
 }
 
-sub delete_param { my $self = shift; $self->{'data'}{'_input'}->delete(@_); }
-sub type         { return $_[0]{'data'}{'_type'};    }
-sub action       { return $_[0]{'data'}{'_action'};  }
-sub function     { return $_[0]{'data'}{'_function'};  }
-sub script       { return $_[0]{'data'}{'_script'};  }
-sub species      { return $_[0]{'data'}{'_species'}; }
-
+# Determines the species for userdata pages (mandatory, since userdata databases are species-specific)
 sub data_species {
-  ## Determines the species for userdata pages (mandatory, since userdata databases are species-specific)
   my $self = shift;
   my $species = $self->species;
-  if (!$species || $species eq 'common') {
-    $species = $self->species_defs->ENSEMBL_PRIMARY_SPECIES;
-  }
+  $species = $self->species_defs->ENSEMBL_PRIMARY_SPECIES if !$species || $species eq 'common';
   return $species;
-}
-
-sub fix_session {
-### Fix the session back to the database - if a session object has been created
-### calls store on... this will check whether (a) there are any saveable
-### viewconfigs AND (b) if any of the saveable viewconfigs have been altered
-  my( $self, $r ) = @_;
-return;
-  my $session = $self->get_session;
-  $session->store($self->apache_handle) if $session;
-}
-
-sub DBConnection {
-  $_[0]->{'data'}{'_databases'} ||= EnsEMBL::Web::DBSQL::DBConnection->new( $_[0]->species, $_[0]->species_defs );
-}
-sub session {
-  my $self = shift;
-  return $self->{'session'} ||= $ENSEMBL_WEB_REGISTRY->get_session;
-}
-
-sub get_session {
-  my $self = shift;
-  return $self->{'session'} || $ENSEMBL_WEB_REGISTRY->get_session;
 }
 
 sub database {
   my $self = shift;
-  if( $_[0]=~/compara/) {
-    return Bio::EnsEMBL::Registry->get_DBAdaptor( 'multi', $_[0] );
+  
+  if ($_[0] =~ /compara/) {
+    return Bio::EnsEMBL::Registry->get_DBAdaptor('multi', $_[0]);
   } else {
-    return $self->DBConnection->get_DBAdaptor( @_ );
+    return $self->DBConnection->get_DBAdaptor(@_);
   }
 }
 
-sub get_databases { my $self = shift; $self->DBConnection->get_databases( @_ ); }
-sub databases_species { my $self = shift; $self->DBConnection->get_databases_species( @_ ); }
-sub has_a_problem     { my $self = shift; return scalar( @{$self->{'data'}{'_problem'}} ); }
-sub has_fatal_problem { my $self = shift; return scalar( grep { $_->isFatal } @{$self->{'data'}{'_problem'}} ); }
-sub has_problem_type  { my( $self,$type ) = @_; return scalar( grep { $_->get_by_type($type) } @{$self->{'data'}{'_problem'}} ); }
-sub get_problem_type  { my( $self,$type ) = @_; return grep { $_->get_by_type($type) } @{$self->{'data'}{'_problem'}}; }
+sub has_a_problem     { return scalar @{$_[0]->{'data'}{'_problem'}}; }
+sub has_fatal_problem { return scalar grep $_->isFatal, @{$_[0]->{'data'}{'_problem'}}; }
+sub has_problem_type  { my ($self, $type) = @_; return scalar grep $_->get_by_type($type), @{$self->{'data'}{'_problem'}}; }
+sub get_problem_type  { my ($self, $type) = @_; return grep $_->get_by_type($type), @{$self->{'data'}{'_problem'}}; }
+sub clear_problems    { $_[0]{'data'}{'_problem'} = []; }
+
 sub problem {
   my $self = shift;
-  push @{$self->{'data'}{'_problem'}}, EnsEMBL::Web::Problem->new(@_) if @_;
+  push @{$self->{'data'}{'_problem'}}, new EnsEMBL::Web::Problem(@_) if @_;
   return $self->{'data'}{'_problem'};
 }
-sub clear_problems { $_[0]{'data'}{'_problem'} = []; }
 
-sub user { 
- ### x
- #warn "xxxxxxxxxxxxxxx DEPRECATED xxxxxxxxxxxxxxxx";
- return undef;
-}
-
-sub species_defs    { $_[0]{'data'}{'_species_defs'} ||= $ENSEMBL_WEB_REGISTRY->species_defs; }
-
-sub web_user_db { 
-  ### x
-  ### Deprecated. Use UserAdaptor from the Registry instead.
-  return undef;
-}
-
-sub apache_handle { $_[0]{'data'}{'_apache_handle'}; }
-
+# Returns the named (or one based on script) {{EnsEMBL::Web::ImageConfig}} object
 sub get_imageconfig  {
-### Returns the named (or one based on script) {{EnsEMBL::Web::ImageConfig}} object
-  my( $self, $key ) = @_;
+  my ($self, $key) = @_;
   my $session = $self->get_session || return;
-#warn "JS5 GUC $key";
-  my $T = $session->getImageConfig( $key ); ## No second parameter - this isn't cached!!
-  $T->_set_core( $self->core_objects );
+  my $T = $session->getImageConfig($key); # No second parameter - this isn't cached
+  $T->_set_core($self->core_objects);
   return $T;
 }
 
+# Retuns a copy of the script config stored in the database with the given key
 sub image_config_hash {
-### Retuns a copy of the script config stored in the database with the given key
-  my( $self, $key, $type, @species ) = @_;
+  my ($self, $key, $type, @species) = @_;
 
   $type ||= $key;
-### If anything left this is the species .....
+  
   my $session = $self->get_session;
-# warn "JS5 UCH $key $type @species";
   return undef unless $session;
-  my $T = $session->getImageConfig( $type, $key, @species );
+  my $T = $session->getImageConfig($type, $key, @species);
   return unless $T;
-  $T->_set_core( $self->core_objects );
+  $T->_set_core($self->core_objects);
+  return $T;
+}
+
+# Returns the named (or one based on script) {{EnsEMBL::Web::ViewConfig}} object
+sub get_viewconfig {
+  my ($self, $type, $action) = @_;
+  my $session = $self->get_session;
+  return undef unless $session;
+  my $T = $session->getViewConfig( $type || $self->type, $action || $self->action );
   return $T;
 }
 
@@ -258,63 +234,50 @@ sub viewconfig {
   return $self->{'data'}->{'_viewconfig'};
 }
 
-sub get_viewconfig {
-### Returns the named (or one based on script) {{EnsEMBL::Web::ViewConfig}} object
-  my( $self, $type, $action ) = @_;
-  my $session = $self->get_session;
-  return undef unless $session;
-  my $T = $session->getViewConfig( $type || $self->type, $action || $self->action );
-  return $T;
-}
-
 sub attach_image_config {
-  my( $self, $key, $image_key ) = @_;
+  my ($self, $key, $image_key) = @_;
   my $session = $self->get_session;
   return undef unless $session;
-  my $T = $session->attachImageConfig( $key, $image_key );
-  $T->_set_core( $self->core_objects );
+  my $T = $session->attachImageConfig($key, $image_key);
+  $T->_set_core($self->core_objects);
   return $T;
 }
 
-# Handling ExtURLs
-sub ExtURL { return $_[0]{'data'}{'_ext_url_'} ||= EnsEMBL::Web::ExtURL->new( $_[0]->species, $_[0]->species_defs ); }
-
-sub get_ExtURL      {
+sub get_ExtURL {
   my $self = shift;
-  my $new_url = CGI::escapeHTML($self->ExtURL) || return;
-  return $new_url->get_url( @_ );
+  my $new_url = escapeHTML($self->ExtURL) || return;
+  return $new_url->get_url(@_);
 }
 
 sub get_ExtURL_link {
   my $self = shift;
   my $text = shift;
-  my $URL = CGI::escapeHTML( $self->get_ExtURL(@_) );
-  return $URL ? qq(<a href="$URL">$text</a>) : $text;
+  my $url = escapeHTML($self->get_ExtURL(@_));
+  return $url ? qq(<a href="$url">$text</a>) : $text;
 }
 
-#use PFETCH etc to get description and sequence of an external record
-sub get_ext_seq{
-    my ($self, $id, $ext_db) = @_;
-    my $indexer = EnsEMBL::Web::ExtIndex->new( $self->species_defs );
-    return unless $indexer;
-    my $seq_ary;
-    my %args;
-    $args{'ID'} = $id;
-    $args{'DB'} = $ext_db ? $ext_db : 'DEFAULT';
+# use PFETCH etc to get description and sequence of an external record
+sub get_ext_seq {
+  my ($self, $id, $ext_db) = @_;
+  my $indexer = new EnsEMBL::Web::ExtIndex($self->species_defs);
+  
+  return unless $indexer;
+  
+  my $seq_ary;
+  my %args;
+  $args{'ID'} = $id;
+  $args{'DB'} = $ext_db ? $ext_db : 'DEFAULT';
 
-    eval{
-	$seq_ary = $indexer->get_seq_by_id(\%args);
-    };
-    if ( ! $seq_ary) {
-	warn "The $ext_db server is unavailable: $@";
-	return '';
-    }
-    else {
-	my $list = join " ", @$seq_ary;
-	return $list =~ /no match/i ? '' : $list ;
-    }
+  eval { $seq_ary = $indexer->get_seq_by_id(\%args); };
+  
+  if (!$seq_ary) {
+    warn "The $ext_db server is unavailable: $@";
+    return '';
+  } else {
+    my $list = join ' ', @$seq_ary;
+    return $list =~ /no match/i ? '' : $list;
+  }
 }
-
 
 1;
 
