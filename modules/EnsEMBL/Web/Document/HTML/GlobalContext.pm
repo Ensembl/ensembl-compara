6f06147bfb9f92264dafdc7a95e7fcacf644c80f
Simon Brent <sb23@sanger.ac.uk>
2009-10-21 08:50:32 +0000

    combine functionality from render and get_json

--- modules/EnsEMBL/Web/Document/HTML/GlobalContext.pm
+++ modules/EnsEMBL/Web/Document/HTML/GlobalContext.pm
@@ -2,17 +2,15 @@
 
 package EnsEMBL::Web::Document::HTML::GlobalContext;
 
-### Generates the global context navigation menu, used in dynamic pages
+# Generates the global context navigation menu, used in dynamic pages
 
 use strict;
+
 use CGI qw(escapeHTML);
-use EnsEMBL::Web::Document::HTML;
 
-our @ISA = qw(EnsEMBL::Web::Document::HTML);
-
+use base qw(EnsEMBL::Web::Document::HTML);
 
 sub add_entry {
-### a
   my $self = shift;
   push @{$self->{'_entries'}}, {@_};
 }
@@ -22,29 +20,46 @@ sub active {
   $self->{'_active'} = shift if @_;
   return $self->{'_active'};
 }
-sub entries {
-### a
-  my $self = shift;
-  return $self->{'_entries'}||[];
-}
 
-sub render_modal {
-  return;
+sub entries {
   my $self = shift;
-  my $T = $self->_content;
-     $T =~ s/id="tabs"/id="modal_tabs"/;
-     $T =~ s/class="link /class="/g;
-     $T =~ s/ class=""//g;
-  $self->print( $T );
+  return $self->{'_entries'} || [];
 }
 
 sub get_json {
   my $self = shift;
   
-  return unless scalar @{$self->entries};
+  my ($content, $active) = $self->_content('li');
   
-  my $content = '<ul class="tabs">';
-  my $i = 0;
+  if ($content) {
+    $content = qq{<ul class="tabs">$content</ul>};
+    return qq{'tabs':'$content','activeTab':'$active'};
+  }
+}
+
+sub render {
+  my $self = shift;
+  
+  my ($content) = $self->_content('dd');
+  
+  if ($content) {
+    $self->print(qq{
+      <div id="tabs">
+        <dl class="tabs">
+          $content
+          <dt class="hidden">.</dt>
+        </dl>
+      </div>
+    });
+  }
+}
+
+sub _content {
+  my ($self, $node) = @_;
+  
+  return '' unless scalar @{$self->entries};
+  
+  my $content;
   my $active;
   
   foreach my $entry (@{$self->entries}) {
@@ -57,54 +72,16 @@ sub get_json {
       $name =~ s/<\\\w+>//g;
       $name =~ s/<[^>]+>/ /g;
       $name =~ s/\s+/ /g;
-      $name = CGI::escapeHTML($name);
+      $name = escapeHTML($name);
       $name = qq{<a href="$entry->{'url'}">$name</a>} if $entry->{'url'};
     }
     
-    $content .= qq{<li id="$id" class="$entry->{'class'}">$name</li>};
+    $content .= qq{<$node id="$id" class="$entry->{'class'}">$name</$node>};
+
+    $active = $name if $entry->{'class'} =~ /\bactive\b/;
+  }
     
-    $active = $i if $entry->{'class'} =~ /\bactive\b/;
-    $i++;
-  }
-  
-  $content .= '</ul>';
-  
-  return qq{'tabs':'$content','activeTab':'$active'};
+  return ($content, $active);
 }
 
-sub render {
-  my $self = shift;
-  $self->print( $self->_content );
-}
-
-sub _content {
-  my $self = shift;
-  
-  return '' unless scalar(@{$self->entries});
-  my $content = '
-    <div id="tabs">
-      <dl class="tabs">';
-  foreach my $entry ( @{$self->entries} ) {
-    my $name = $entry->{caption};
-    if ($name eq '-') {
-      $name =  sprintf( '<span title="%s">%s</span>', $entry->{'disabled'}, $entry->{'type'});
-    } else { 
-      $name =~ s/<\\\w+>//g;
-      $name =~ s/<[^>]+>/ /g;
-      $name =~ s/\s+/ /g;
-      $name = CGI::escapeHTML( $name );
-      if( $entry->{'url'} ) {
-        $name = sprintf( '<a href="%s">%s</a>', $entry->{'url'}, $name );
-      }
-    }
-    $content .= sprintf( '
-        <dd id="%s" class="link %s">%s</dd>', lc('tab_'.($entry->{'id'}||$entry->{'type'})), $entry->{class}, $name );
-  }
-  $content .= '
-        <dt class="hidden">.</dt>
-      </dl>
-    </div>';
-  return $content;
-}
-
-return 1;
+1;
