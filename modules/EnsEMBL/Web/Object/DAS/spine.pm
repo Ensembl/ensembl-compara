e4635ba6416e939035871e3a18a954a9e932924c
Eugene Kulesha <ek@ebi.ac.uk>
2009-10-30 17:06:18 +0000

    a das source for integration with EBI SPINE service. it exports the gene summary info and gene image links

---
+++ modules/EnsEMBL/Web/Object/DAS/spine.pm
@@ -0,0 +1,204 @@
+package EnsEMBL::Web::Object::DAS::spine;
+
+use strict;
+use warnings;
+
+use EnsEMBL::Web::Object::DAS;
+our @ISA = qw(EnsEMBL::Web::Object::DAS);
+
+use CGI qw(escapeHTML);
+
+use Data::Dumper;
+
+sub Types {
+  my $self = shift;
+
+  return [
+	  {
+	      'REGION' => '*',
+	      'FEATURES' => [
+			     { 'id' => 'spine'  }
+			     ]
+			     }
+	  ];
+}
+
+
+sub Features {
+  my $self = shift;
+
+  $self->{_feature_label} = 'gene';
+  my @segments      = $self->Locations;
+  my %feature_types = map { $_ ? ($_=>1) : () } @{$self->FeatureTypes  || []};
+  my @group_ids     = grep { $_ }               @{$self->GroupIDs      || []};
+  my @feature_ids   = grep { $_ }               @{$self->FeatureIDs    || []};
+
+  my @features;
+
+
+  #View templates
+  $self->{'templates'} ||= {};
+  $self->{'templates'}{'geneview_URL'}  = sprintf( '%s/Gene/Summary?g=%%s;db=%%s',        $self->species_defs->species_path($self->real_species ));
+  $self->{'templates'}{'image_URL'}  = sprintf( '%s/Component/Gene/Web/TranscriptsImage?export=png;g=%%s;db=%%s;image_width=400',        $self->species_defs->species_path($self->real_species ));
+  $self->{'templates'}{'varview_URL'}  = sprintf( '%s/Gene/Variation_Gene/Table?g=%%s;db=%%s',        $self->species_defs->species_path($self->real_species ));
+  $self->{'templates'}{'compara_URL'}  = sprintf( '%s/Gene/Compara_%%s?g=%%s;db=%%s',        $self->species_defs->species_path($self->real_species ));
+
+
+  my $h =  $self->{data}->{_databases}->get_databases('core', 'variation', 'compara');
+
+
+
+  if (my $cdb = $h->{'core'}) {
+      my $ga = $cdb->get_adaptor('Gene');
+
+      foreach my $segment (@segments) {
+	  if( ref($segment) eq 'HASH' && $segment->{'TYPE'} eq 'ERROR' ) {
+	      push @features, $segment;
+	      next;
+	  }
+
+	  my $gene_id = $segment->{REGION} or next;
+	  my $gene = $ga->fetch_by_stable_id($gene_id);
+
+	  my $description =  escapeHTML( $gene->description() );
+	  
+	  my $f = {
+	      'ID'          => "description:".$gene->stable_id,
+	      'LABEL'       => $gene->display_xref->display_id,
+	      'TYPE'        => 'description',
+	      'ORIENTATION' => $self->ori($gene->strand),
+	      'NOTE' => [ $description ],
+	      'LINK' => [
+			 { 'text' => 'GeneView '.$gene->stable_id ,
+			   'href' => sprintf( $self->{'templates'}{'geneview_URL'}, $gene->stable_id, 'core' ),
+		       }
+			 ],
+	  };
+
+	  push @{$self->{_features}{$gene_id}{'FEATURES'}}, $f;
+
+	  my $fi = {
+	      'ID'          => "image:".$gene->stable_id,
+	      'LABEL'       => "Image ".$gene->display_xref->display_id,
+	      'TYPE'        => 'image',
+	      'LINK' => [
+			 { 'text' => 'Gene Image '.$gene->stable_id ,
+			   'href' => sprintf( $self->{'templates'}{'image_URL'}, $gene->stable_id, 'core' ),
+		       }
+			 ],
+	      
+	  };
+
+	  push @{$self->{_features}{$gene_id}{'FEATURES'}}, $fi;
+
+
+
+	  my $notes;
+	  push @$notes, sprintf ("%s spans %d bp", $gene->stable_id, ($gene->seq_region_end - $gene->seq_region_start));
+	  push @$notes, sprintf ("%s has %d exons", $gene->stable_id, scalar(@{ $gene->get_all_Exons }));
+	  push @$notes, sprintf ("%s has %d transcripts", $gene->stable_id, scalar(@{ $gene->get_all_Transcripts }));
+
+
+	  my $s1 = {
+	      'ID'          => "core_summary:".$gene->stable_id,
+	      'LABEL'       => "Gene Summary ".$gene->display_xref->display_id,
+	      'TYPE'        => 'summary',
+	      'NOTE' => $notes,
+	      'LINK' => [
+			 { 'text' => 'GeneView '.$gene->stable_id ,
+			   'href' => sprintf( $self->{'templates'}{'geneview_URL'}, $gene->stable_id, 'core' ),
+		       }
+			 ],
+	      
+	  };
+
+	  push @{$self->{_features}{$gene_id}{'FEATURES'}}, $s1;
+
+	  if (my $vdb = $h->{'variation'}) {
+	      my $fs = $gene->feature_Slice();
+	      my $snps = $fs->get_all_VariationFeatures;
+	      my $notes1;
+	      push @$notes1, sprintf ("%s has %d SNPs", $gene->stable_id, scalar(@{ $snps }));
+	  
+	      my $s2 = {
+		  'ID'          => "var_summary:".$gene->stable_id,
+		  'LABEL'       => "Gene Summary ".$gene->display_xref->display_id,
+		  'TYPE'        => 'summary',
+		  'NOTE' => $notes1,
+		  'LINK' => [
+			     { 'text' => 'Variation Image '.$gene->stable_id ,
+			       'href' => sprintf( $self->{'templates'}{'varview_URL'}, $gene->stable_id, 'core' ),
+			   }
+			     ],
+	      
+	      };
+	      push @{$self->{_features}{$gene_id}{'FEATURES'}}, $s2;
+	  }
+
+	  if (my $cmpdb = $h->{'compara'}) {
+	      my %homologues;
+
+
+	      my $member_adaptor = $cmpdb->get_adaptor('Member');
+	      my $query_member = $member_adaptor->fetch_by_source_stable_id("ENSEMBLGENE",$gene_id);
+
+	      next unless defined $query_member ;
+	      my $homology_adaptor = $cmpdb->get_adaptor('Homology');
+#  It is faster to get all the Homologues and discard undesired entries
+#  my $homologies_array = $homology_adaptor->fetch_all_by_Member_method_link_type($query_member,$homology_source);
+	      my $hmgs = $homology_adaptor->fetch_all_by_Member($query_member);
+
+	      my $hHash ;
+	      foreach my $homology (@{$hmgs}){
+		  $hHash->{ortholog} += 1 if ($homology->description =~ /ortholog/);
+		  next if ($homology->description =~ /between_species_paralog/);
+		  $hHash->{paralog} += 1 if ($homology->description =~ /paralog|gene_split/);
+	      }
+
+	      my $notes2;
+	      push @$notes2, sprintf ("%s has %d orthologues", $gene->stable_id, $hHash->{ortholog});
+
+	  
+	      my $s3 = {
+		  'ID'          => "orthologue_summary:".$gene->stable_id,
+		  'LABEL'       => "Orthologue Summary ".$gene->display_xref->display_id,
+		  'TYPE'        => 'summary',
+		  'NOTE' => $notes2,
+		  'LINK' => [
+			     { 'text' => 'Orthologues '.$gene->stable_id ,
+			       'href' => sprintf( $self->{'templates'}{'compara_URL'}, 'Ortholog', $gene->stable_id, 'core' ),
+			   }
+			     ],
+	      };
+	      push @{$self->{_features}{$gene_id}{'FEATURES'}}, $s3;
+
+	      my $notes3;
+	      push @$notes3, sprintf ("%s has %d paralogues", $gene->stable_id, $hHash->{paralog});
+
+	  
+	      my $s3 = {
+		  'ID'          => "paralogue_summary:".$gene->stable_id,
+		  'LABEL'       => "Paralogue Summary ".$gene->display_xref->display_id,
+		  'TYPE'        => 'summary',
+		  'NOTE' => $notes3,
+		  'LINK' => [
+			     { 'text' => 'Paralogues '.$gene->stable_id ,
+			       'href' => sprintf( $self->{'templates'}{'compara_URL'}, 'Paralog', $gene->stable_id, 'core' ),
+			   }
+			     ],
+	      };
+	      push @{$self->{_features}{$gene_id}{'FEATURES'}}, $s3;
+
+
+	  }
+
+      }
+  }
+
+  push @features, values %{ $self->{'_features'} };
+
+  return \@features;
+}
+
+1;
+
