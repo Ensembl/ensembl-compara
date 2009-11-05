612d66eb06b64ea9b425ea2345c3c10d40eef32e
Beth Yates <bp1@sanger.ac.uk>
2009-11-05 14:57:19 +0000

    new class of variation

---
+++ modules/EnsEMBL/Web/ZMenu/StructuralVariation.pm
@@ -0,0 +1,68 @@
+package EnsEMBL::Web::ZMenu::StructuralVariation;
+
+use strict;
+
+use base qw(EnsEMBL::Web::ZMenu);
+
+sub content {
+  my $self = shift; warn $self;
+  my $object = $self->object;
+  return unless $object->param('vid');
+
+  my $v_id            = $object->param('vid');
+  my $vf              = $object->param('vf');
+  my $db_adaptor      = $object->database('variation');
+  my $var_adaptor     = $db_adaptor->get_StructuralVariation;
+  my $variation       = $var_adaptor->fetch_by_dbID($v_id); 
+  my $seq_region      = $variation->slice->seq_region_name(); 
+  my $seq_region_type = $variation->slice->coord_system->name;
+  my $neat_sr_name    = $object->neat_sr_name($seq_region_type, $seq_region);
+  my $start           = $object->thousandify(($variation->slice->start  + $variation->start) -1);
+  my $end             = $object->thousandify(($variation->slice->start + $variation->end) -1);
+  my $position        = $neat_sr_name .':'.$start.'-'.$end;
+  my $length          = $end -$start;
+  my $action          = 'View';
+  my $scale           = $object->species_defs->ENSEMBL_GENOME_SIZE || 1;    
+  my $max_length      = $scale *= 1e6;
+  my $description     = '';  
+  my $pubmed_link     = '';
+
+  if ($length >> $max_length) { $action = 'Overview'; } 
+  if ($variation->can('description')) { $description = $variation->description; }
+  if ($description =~/PMID/) {
+    my @description_string = split (':', $description);
+    my $pubmed_id = pop @description_string;
+    $pubmed_link = $object->get_ExtURL('PUBMED', $pubmed_id);  
+  }    
+
+  $self->caption('Structural variation: ' . $variation->variation_name);
+  
+  $self->add_entry({
+    type  => 'Source',
+    label => $variation->source,
+  });
+
+  $self->add_entry({
+    type  => 'Description',
+    label => $description,
+    link  => $pubmed_link, 
+  });
+
+  $self->add_entry({
+    type  => 'Class',
+    label => $variation->class,
+  });
+
+  $self->add_entry({
+    type  => 'Location',
+    label => $position,
+    link  => $object->_url({
+      type   => 'Location',
+      action => $action,
+      r      => $seq_region .':'.$start.'-'.$end,
+    }) 
+  });
+
+}
+
+1;
