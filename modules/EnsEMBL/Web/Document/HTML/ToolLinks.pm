ff854b8e505ff83dd5f366daf617e34ef1b18643
Simon Brent <sb23@sanger.ac.uk>
2009-11-06 16:28:01 +0000

    use species_path

--- modules/EnsEMBL/Web/Document/HTML/ToolLinks.pm
+++ modules/EnsEMBL/Web/Document/HTML/ToolLinks.pm
@@ -3,54 +3,58 @@ package EnsEMBL::Web::Document::HTML::ToolLinks;
 ### Generates links to site tools - BLAST, help, login, etc (currently in masthead)
 
 use strict;
-use EnsEMBL::Web::Document::HTML;
-use CGI qw(escape);
 
-our @ISA = qw(EnsEMBL::Web::Document::HTML);
+use CGI qw(escape);
+use EnsEMBL::Web::RegObj;
+
+use base qw(EnsEMBL::Web::Document::HTML);
 
 sub new { return shift->SUPER::new( 'logins' => '?' ); }
 
-sub logins    :lvalue { $_[0]{'logins'};   }
-sub blast     :lvalue { $_[0]{'blast'};   }
-sub biomart   :lvalue { $_[0]{'biomart'};   }
-sub referer   :lvalue { $_[0]{'referer'};   } ## Needed by CloseCP
-sub mirror_icon :lvalue { $_[0]{'mirror_icon'};   }
+sub logins      :lvalue { $_[0]{'logins'}; }
+sub blast       :lvalue { $_[0]{'blast'}; }
+sub biomart     :lvalue { $_[0]{'biomart'}; }
+sub referer     :lvalue { $_[0]{'referer'}; } # Needed by CloseCP
+sub mirror_icon :lvalue { $_[0]{'mirror_icon'}; }
 
-sub render   {
-  my $self    = shift;
-  my $dir = '/'.$ENV{'ENSEMBL_SPECIES'};
+sub render {
+  my $self = shift;
+  
+  my $species_defs = $EnsEMBL::Web::RegObj::ENSEMBL_WEB_REGISTRY->species_defs;
+  my $dir = $species_defs->species_path;
   $dir = '' if $dir !~ /_/;
-  my $url     = CGI::escape($ENV{'REQUEST_URI'});
+  
+  my $url  = escape($ENV{'REQUEST_URI'});
   my $html = '<div class="print_hide">';
 
   my $blast_dir;
   my $sp_dir = $ENV{'ENSEMBL_SPECIES'};
+  
   if (!$sp_dir || $sp_dir eq 'Multi' || $sp_dir eq 'common') {
     $blast_dir = 'Multi';
-  }
-  else {
+  } else {
     $blast_dir = $sp_dir;
   }
-
   
-  if( $self->logins ) {
-    if( $ENV{'ENSEMBL_USER_ID'} ) {
-      $html .= qq(
-      <a style="display:none" href="$dir/Account/Links?_referer=$url" class="modal_link">Account</a> &nbsp;|&nbsp;
-      <a href="$dir/Account/Logout?_referer=$url">Logout</a> &nbsp;|&nbsp;
-      );
+  if ($self->logins) {
+    if ($ENV{'ENSEMBL_USER_ID'}) {
+      $html .= qq{
+        <a style="display:none" href="$dir/Account/Links?_referer=$url" class="modal_link">Account</a> &nbsp;|&nbsp;
+        <a href="$dir/Account/Logout?_referer=$url">Logout</a> &nbsp;|&nbsp;
+      };
     } else {
-      $html .= qq(
-      <a style="display:none" href="$dir/Account/Login?_referer=$url" class="modal_link">Login</a> / 
-      <a style="display:none" href="$dir/Account/User/Add?_referer=$url" class="modal_link">Register</a> &nbsp;|&nbsp;
-      );
+      $html .= qq{
+        <a style="display:none" href="$dir/Account/Login?_referer=$url" class="modal_link">Login</a> / 
+        <a style="display:none" href="$dir/Account/User/Add?_referer=$url" class="modal_link">Register</a> &nbsp;|&nbsp;
+      };
     }
   }
-  $html .= qq(<a href="/$blast_dir/blastview">BLAST/BLAT</a> &nbsp;|&nbsp;) if $self->blast;
-  $html .= qq(<a href="/biomart/martview">BioMart</a> &nbsp;|&nbsp;)   if $self->biomart;
-  $html .= qq(<a href="/info/website/help/" id="help">Docs &amp; FAQs</a>);
+  
+  $html .= qq{<a href="/$blast_dir/blastview">BLAST/BLAT</a> &nbsp;|&nbsp;} if $self->blast;
+  $html .= qq{<a href="/biomart/martview">BioMart</a> &nbsp;|&nbsp;}        if $self->biomart;
+  $html .= qq{<a href="/info/website/help/" id="help">Docs &amp; FAQs</a>};
   $html .= '</div>';
-
+  
   $self->print($html);
 }
 
