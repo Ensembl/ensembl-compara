b165b4231b865340d06e290a855dc44d709425a7
Simon Brent <sb23@sanger.ac.uk>
2009-11-23 11:09:07 +0000

    tidied. Don't use core objects here.

--- modules/EnsEMBL/Web/Component/Gene/Summary.pm
+++ modules/EnsEMBL/Web/Component/Gene/Summary.pm
@@ -4,138 +4,149 @@ package EnsEMBL::Web::Component::Gene::Summary;
 
 use strict;
 use warnings;
-no warnings "uninitialized";
+no warnings 'uninitialized';
+
 use base qw(EnsEMBL::Web::Component::Gene);
 use CGI qw(escapeHTML);
+
 sub _init {
   my $self = shift;
-  $self->cacheable( 0 );
-  $self->ajaxable(  0 );
+  $self->cacheable(0);
+  $self->ajaxable(0);
 }
 
 sub content {
   my $self = shift;
   my $object = $self->object;
 
-## Grab the description of the object...
-
-  if( $object->Obj->isa('Bio::EnsEMBL::Compara::Family' ) ){
-    return sprintf '<p>%s</p>', CGI::escapeHTML( $object->Obj->description );
-  } # elsif( $object->Obj->isa('Bio::EnsEMBL::ID
-  if( $object->Obj->isa('Bio::EnsEMBL::ArchiveStableId' ) ){
+  # Grab the description of the object
+  if ($object->Obj->isa('Bio::EnsEMBL::Compara::Family')) {
+    return sprintf '<p>%s</p>', escapeHTML($object->Obj->description);
+  }
+  
+  if ($object->Obj->isa('Bio::EnsEMBL::ArchiveStableId')) {
     return sprintf '<p>%s</p>', 'This identifier is not in the current EnsEMBL database';
   }
+  
   my $html = '';
-  my($edb, $acc);
-  my $description = escapeHTML( $object->gene_description() );
-  if( $description ) {
+  my $description = escapeHTML($object->gene_description);
+  my ($edb, $acc);
+  
+  if ($description) {
     if ($description ne 'No description') {
       if ($object->get_db eq 'vega') {
         $edb = 'Vega';
         $acc = $object->Obj->stable_id;
-        $description .= qq( <span class="small">@{[ $object->get_ExtURL_link("Source: $edb", $edb.'_gene', $acc) ]}</span>);
-      }
-      else {
+        $description .= sprintf ' <span class="small">%s</span>', $object->get_ExtURL_link("Source: $edb", $edb . '_gene', $acc);
+      } else {
         $description =~ s/EC\s+([-*\d]+\.[-*\d]+\.[-*\d]+\.[-*\d]+)/$self->EC_URL($1)/e;
         $description =~ s/\[\w+:([-\w\/\_]+)\;\w+:(\w+)\]//g;
         ($edb, $acc) = ($1, $2);
-	my $link = $object->get_ExtURL_link("Source: $edb $acc",$edb, $acc);
-        $description .= qq( <span class="small">@{[ $object->get_ExtURL_link("Source: $edb $acc",$edb, $acc) ]}</span>) if ($acc ne 'content');
+        $description .= sprintf ' <span class="small">%s</span>', $object->get_ExtURL_link("Source: $edb $acc", $edb, $acc) if $acc ne 'content';
       }
-      $html .= qq(
-      <p>
-        $description
-      </p>);
+      
+      $html .= "<p>$description</p>";
     }
   }
 
-## Now a link to location;
-
+  # Now a link to location;
   my $url = $self->object->_url({
-    'type'   => 'Location',
-    'action' => 'View',
-    'r'      => $object->seq_region_name.':'.$object->seq_region_start.'-'.$object->seq_region_end
+    type   => 'Location',
+    action => 'View',
+    r      => $object->seq_region_name . ':' . $object->seq_region_start . '-' . $object->seq_region_end
   });
-
-  my $location_html = sprintf( '<a href="%s">%s: %s-%s</a> %s.',
+  
+  my $location_html = sprintf(
+    '<a href="%s">%s: %s-%s</a> %s.',
     $url,
-    $object->neat_sr_name( $object->seq_region_type, $object->seq_region_name ),
-    $object->thousandify( $object->seq_region_start ),
-    $object->thousandify( $object->seq_region_end ),
+    $object->neat_sr_name($object->seq_region_type, $object->seq_region_name),
+    $object->thousandify($object->seq_region_start),
+    $object->thousandify($object->seq_region_end),
     $object->seq_region_strand < 0 ? ' reverse strand' : 'forward strand'
   );
-
+  
   # alternative (Vega) coordinates
-  my $lc_type  = lc( $object->type_name );
+  my $lc_type      = lc $object->type_name;
   my $alt_assembly = $object->species_defs->ALTERNATIVE_ASSEMBLY;
-  if ( $alt_assembly and $object->get_db eq 'vega' ) {
+  
+  if ($alt_assembly && $object->get_db eq 'vega') {
     # set dnadb to 'vega' so that the assembly mapping is retrieved from there
-    my $reg = "Bio::EnsEMBL::Registry";
-    my $orig_group = $reg->get_DNAAdaptor($object->species, "vega")->group;
-    $reg->add_DNAAdaptor($object->species, "vega", $object->species, "vega");
-    # project feature slice onto Vega assembly
-    my $alt_slices = $object->vega_projection($alt_assembly);
+    my $reg        = 'Bio::EnsEMBL::Registry';
+    my $orig_group = $reg->get_DNAAdaptor($object->species, 'vega')->group;
+    my $alt_slices = $object->vega_projection($alt_assembly); # project feature slice onto Vega assembly
+    
+    $reg->add_DNAAdaptor($object->species, 'vega', $object->species, 'vega');
+    
     # link to Vega if there is an ungapped mapping of whole gene
-    if ((scalar(@$alt_slices) == 1) && ($alt_slices->[0]->length == $object->feature_length) ) {
-      my $l = $alt_slices->[0]->seq_region_name.":".$alt_slices->[0]->start."-".$alt_slices->[0]->end;
+    if (scalar @$alt_slices == 1 && $alt_slices->[0]->length == $object->feature_length) {
+      my $l   = $alt_slices->[0]->seq_region_name . ':' . $alt_slices->[0]->start . '-' . $alt_slices->[0]->end;
       my $url = $object->ExtURL->get_url('VEGA_CONTIGVIEW', $l);
-      $location_html .= qq( [<span class="small">This corresponds to );
-      $location_html .= sprintf(qq(<a href="%s" target="external">%s-%s</a>),
-				$url,
-				$object->thousandify($alt_slices->[0]->start),
-				$object->thousandify($alt_slices->[0]->end)
-			    );
-      $location_html .= " in $alt_assembly coordinates</span>]";
-  } else {
-    $location_html .= qq( [<span class="small">There is no ungapped mapping of this $lc_type onto the $alt_assembly assembly</span>]);
-  }
-    # set dnadb back to the original group
-    $reg->add_DNAAdaptor($object->species, "vega", $object->species, $orig_group);
+      
+      $location_html .= ' [<span class="small">This corresponds to ';
+      $location_html .= sprintf(
+        '<a href="%s" target="external">%s-%s</a>',
+        $url,
+        $object->thousandify($alt_slices->[0]->start),
+        $object->thousandify($alt_slices->[0]->end)
+      );
+      
+      $location_html .= ' in $alt_assembly coordinates</span>]';
+    } else {
+      $location_html .= qq{ [<span class="small">There is no ungapped mapping of this $lc_type onto the $alt_assembly assembly</span>]};
+    }
+    
+    $reg->add_DNAAdaptor($object->species, 'vega', $object->species, $orig_group); # set dnadb back to the original group
   }
 
   # Haplotype/PAR locations
   my $alt_locs = $object->get_alternative_locations;
-  if( @$alt_locs ) {
-    $location_html .= qq(
-          <p> This gene is mapped to the following HAP/PARs:</p>
-          <ul>);
+  
+  if (@$alt_locs) {
+    $location_html .= '
+      <p> This gene is mapped to the following HAP/PARs:</p>
+      <ul>';
+    
     foreach my $loc (@$alt_locs){
       my ($altchr, $altstart, $altend, $altseqregion) = @$loc;
-      $location_html .= sprintf( qq(
-                                 <li>
-                                 <a href="/%s/Location/View?l=%s:%s-%s">%s : %s-%s</a>
-                                 </li>), $object->species, $altchr, $altstart, $altend, $altchr,
-		       $object->thousandify( $altstart ),
-		       $object->thousandify( $altend ));
+      $location_html .= sprintf('
+        <li><a href="/%s/Location/View?l=%s:%s-%s">%s : %s-%s</a></li>', 
+        $object->species, $altchr, $altstart, $altend, $altchr,
+        $object->thousandify($altstart),
+        $object->thousandify($altend)
+      );
     }
-    $location_html .= "\n    </ul>";
+    
+    $location_html .= '
+      </ul>';
   }
 
-  $html .= qq(
+  $html .= qq{
     <dl class="summary">
       <dt>Location</dt>
       <dd>
         $location_html
       </dd>
-    </dl>);
+    </dl>
+  };
 
 
-## Now create the transcript information...
+  # Now create the transcript information
   my $transcripts = $object->Obj->get_all_Transcripts; 
-  my $count = @$transcripts;
-  my $plural_1 = "are";
-  my $plural_2 = "transcripts"; 
-  if( $count == 1  ) {
-    $plural_1 = "is"; 
-    $plural_2 =~s/s$//; 
+  my $transcript  = $object->param('t');
+  my $count       = @$transcripts;
+  my $plural_1    = 'are';
+  my $plural_2    = 'transcripts'; 
+  
+  if ($count == 1) {
+    $plural_1 = 'is'; 
+    $plural_2 =~ s/s$//; 
   }
-
-  my $transcript = $object->core_objects->{'parameters'}{'t'};
-  $html .= qq(
+  
+  $html .= qq{
     <dl class="summary">
       <dt>Transcripts</dt>
       <dd>
-        <p class="toggle_text" id="transcripts_text">There $plural_1  $count $plural_2 in this gene:</p>
+        <p class="toggle_text" id="transcripts_text">There $plural_1 $count $plural_2 in this gene:</p>
         <table class="toggle_table" id="transcripts" summary="List of transcripts for this gene - along with translation information and type">
         <tr>
           <th>Name</th>
@@ -143,62 +154,64 @@ sub content {
           <th>Protein ID</th>
           <th>Description</th> 
         </tr>
-  );
+  };
  
-  foreach(
-    map { $_->[2] }
-    sort { $a->[0] cmp $b->[0] || $a->[1] cmp $b->[1] } 
-    map { [$_->external_name, $_->stable_id, $_] }
-    @$transcripts
-  ){
-    my $url = $self->object->_url({
-      'type'   => 'Transcript',
-      'action' => 'Summary',
-      't'      => $_->stable_id
-    }); 
+  foreach (map { $_->[2] } sort { $a->[0] cmp $b->[0] || $a->[1] cmp $b->[1] } map { [ $_->external_name, $_->stable_id, $_ ] } @$transcripts) {
     my $protein = 'No protein product';
-    if( $_->translation ) {
-      $protein = sprintf '<a href="%s">%s</a>',
-      $self->object->_url({
-        'type'   => 'Transcript',
-        'action' => 'ProteinSummary',
-        't'      => $_->stable_id
-      }),
-	    CGI::escapeHTML( $_->translation->stable_id );
+    
+    my $url = $self->object->_url({
+      type   => 'Transcript',
+      action => 'Summary',
+      t      => $_->stable_id
+    });
+    
+    if ($_->translation) {
+      $protein = sprintf(
+        '<a href="%s">%s</a>',
+        $self->object->_url({
+          type   => 'Transcript',
+          action => 'ProteinSummary',
+          t      => $_->stable_id
+        }),
+        escapeHTML($_->translation->stable_id)
+      );
     }
-    $html .= sprintf( '
+    
+    $html .= sprintf('
       <tr%s>      
         <th>%s</th>
         <td><a href="%s">%s</a></td>
         <td>%s</td>
         <td>%s</td>
       </tr>',
-      $_->stable_id eq $transcript ? ' class="active"' : '',
-      CGI::escapeHTML( $_->display_xref ? $_->display_xref->display_id : 'Novel' ),
+      $count == 1 || $_->stable_id eq $transcript ? ' class="active"' : '',
+      escapeHTML($_->display_xref ? $_->display_xref->display_id : 'Novel'),
       $url,
-      CGI::escapeHTML( $_->stable_id ),
-	    $protein,
-	    $_->biotype
+      escapeHTML($_->stable_id),
+      $protein,
+      $_->biotype
     );
-   }
+  }
+  
   $html .= '
-      </table>';
-  $html .= '
+      </table>
     </dd>
   </dl>';
+  
   my $site_type = $object->species_defs->ENSEMBL_SITETYPE;
-  $html .= $self->_hint( 'gene', 'Transcript and Gene level displays',
-   qq(
-  <p>
-In $site_type a gene is made up of one or more transcripts. We provide displays at two levels:
-</p>
-<ul>
-  <li>Transcript views which provide information specific to an individual transcript such as the cDNA and CDS sequences and protein domain annotation.</li>
-  <li>Gene views which provide displays for data associated at the gene level such as orthologues and paralogues, regulatory regions and splice variants.</li>
-</ul>
-<p>
-This view is a gene level view. To access the transcript level displays select a Transcript ID in the table above and then navigate to the information you want using the menu at the left hand side of the page.  To return to viewing gene level information click on the Gene tab in the menu bar at the top of the page.
-</p>) );
+  
+  $html .= $self->_hint('gene', 'Transcript and Gene level displays', "
+    <p>In $site_type a gene is made up of one or more transcripts. We provide displays at two levels:</p>
+    <ul>
+      <li>Transcript views which provide information specific to an individual transcript such as the cDNA and CDS sequences and protein domain annotation.</li>
+      <li>Gene views which provide displays for data associated at the gene level such as orthologues and paralogues, regulatory regions and splice variants.</li>
+    </ul>
+    <p>
+      This view is a gene level view. To access the transcript level displays select a Transcript ID in the table above and then navigate to the information you want using the menu at the left hand side of the page.  
+      To return to viewing gene level information click on the Gene tab in the menu bar at the top of the page.
+    </p>"
+  );
+
   return $html;
 }
 
