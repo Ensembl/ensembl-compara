58e5cf0faeda64a9aaa64ca09f0413d515a4b47d
Simon Brent <sb23@sanger.ac.uk>
2009-11-26 09:12:17 +0000

    load contents by ajax

--- modules/EnsEMBL/Web/Component/UserData/UploadParsed.pm
+++ modules/EnsEMBL/Web/Component/UserData/UploadParsed.pm
@@ -10,8 +10,8 @@ use base qw(EnsEMBL::Web::Component::UserData);
 
 sub _init {
   my $self = shift;
-  $self->cacheable( 0 );
-  $self->ajaxable(  0 );
+  $self->cacheable(0);
+  $self->ajaxable(1);
 }
 
 sub caption {
@@ -19,32 +19,38 @@ sub caption {
 }
 
 sub content {
-  my $self = shift;
+  my $self   = shift;
   my $object = $self->object;
+  my $url    = $self->ajax_url('ajax', 1) . ';r=' . $object->parent->{'params'}->{'r'} . ';code=' .  $object->param('code');
+  
+  return qq{<div class="ajax" title="['$url']"></div>};
+}
+
+sub content_ajax {
+  my $self = shift;
+  
+  my $object = $self->object;
+  my $upload = $object->get_session->get_data('code' => $object->param('code'));
   my $html;
 
-  my $upload = $object->get_session->get_data('code' => $object->param('code'));
-  #$html = '<p style="margin-top:1em">Checking file contents: <img src="/i/ajax_progress.gif" alt="" style="margin-left:10px" /></p>';
-
-  ## Here's what we actually want to do!
+  # Here's what we actually want to do!
   my $total_features;
-  my $referer = $object->param('_referer');
-  $referer =~ /r=(\w+(:\d+-\d+)?)/; ## Get current coordinates from parent URL
-  my $location = $1;
-  my $parser = EnsEMBL::Web::Text::FeatureParser->new($object->species_defs, $location);
+  my $parser = EnsEMBL::Web::Text::FeatureParser->new($object->species_defs, $object->param('r'));
+  
   if ($upload->{'type'} eq 'upload') {
-    my $file = new EnsEMBL::Web::TmpFile::Text( filename => $upload->{'filename'});
+    my $file = new EnsEMBL::Web::TmpFile::Text(filename => $upload->{'filename'});
     my $data = $file->retrieve;
+
     $parser->parse($data, $upload->{'format'});
-    unless ($upload->{'format'}) {
-      $upload->{'format'} = $parser->format;
-    }
-    $upload->{'style'} = $parser->style;
+    $upload->{'format'} = $parser->format unless $upload->{'format'};
+    $upload->{'style'}  = $parser->style;
     $object->get_session->set_data($upload);
-    $html .= '<p class="space-below"><strong>Total features found</strong>: '.$parser->feature_count.'</p>';
+
+    $html .= '<p class="space-below"><strong>Total features found</strong>: ' . $parser->feature_count . '</p>';
+
     if ($parser->nearest) {
       $html .= '<p class="space-below"><strong>Nearest region with data</strong>: ';
-      $html .= '<a href="/'.$upload->{'species'}.'/Location/View?r='.$parser->nearest.'">'.$parser->nearest.'</a>';
+      $html .= qq{<a href="/$upload->{'species'}/Location/View?r=} . $parser->nearest . '">' . $parser->nearest . '</a>';
     }
   }
 
