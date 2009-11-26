8cea869932bba169a3fa851b28191fb6de1ea6b8
Beth Yates <bp1@sanger.ac.uk>
2009-11-26 16:52:33 +0000

    new code to handle date uploaded using the variation consequence calculation tool

---
+++ modules/EnsEMBL/Web/Text/Feature/CONSEQUENCE.pm
@@ -0,0 +1,24 @@
+package EnsEMBL::Web::Text::Feature::CONSEQUENCE;
+
+use strict;
+use warnings;
+no warnings 'uninitialized';
+
+use base qw(EnsEMBL::Web::Text::Feature);
+
+sub seqname   	    { return $_[0]->{'__raw__'}[0]; }
+sub start   	      { return $_[0]->{'__raw__'}[1]; }
+sub end 	  	      { return $_[0]->{'__raw__'}[2]; }
+sub allele_string   { return $_[0]->{'__raw__'}[3]; }
+sub strand          { return $_[0]->{'__raw__'}[4]; }
+sub consequence     { return $_[0]->{'__raw__'}[5]; }
+sub extra           { return $_[0]->{'__raw__'}[6]; }
+sub external_data   { return undef; }
+
+sub coords {
+  my ($self, $data) = @_;
+  return ($data->[0], $data->[1], $data->[2]);
+}
+ 
+
+1;
