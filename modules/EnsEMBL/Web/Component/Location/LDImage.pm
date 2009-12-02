589b6b7df79e0b9b7ce90333fe6462e3f3af23ac
Beth Yates <bp1@sanger.ac.uk>
2009-12-02 15:36:57 +0000

    changed to handle multiple populations

--- modules/EnsEMBL/Web/Component/Location/LDImage.pm
+++ modules/EnsEMBL/Web/Component/Location/LDImage.pm
@@ -16,10 +16,6 @@ sub _init {
 
 sub caption {
   my $self = shift;
-  my $object = $self->object;
-  my ($seq_region, $start, $end, $seq_type ) = ($object->seq_region_name, $object->seq_region_start, $object->seq_region_end, $object->seq_region_type);
-
-  my $caption = "Context " . $seq_type ." ". $seq_region ." ". $start;
   return;
 }
 
@@ -27,28 +23,22 @@ sub caption {
 sub content {
   my $self = shift;
   my $object = $self->object;
+  return unless $object->param('pop1');
   my ($seq_region, $start, $end, $seq_type ) = ($object->seq_region_name, $object->seq_region_start, $object->seq_region_end, $object->seq_region_type);
+
 
   ## set path information for LD calculations
   $Bio::EnsEMBL::Variation::DBSQL::LDFeatureContainerAdaptor::BINARY_FILE = $object->species_defs->ENSEMBL_CALC_GENOTYPES_FILE;
   $Bio::EnsEMBL::Variation::DBSQL::LDFeatureContainerAdaptor::TMP_PATH = $object->species_defs->ENSEMBL_TMP_TMP;
 
   my $wuc_ldview = $object->get_imageconfig( 'ldview' );
-  my $context = $wuc_ldview->get_parameter('context');
-  my $var_length = ($end -$start) +1;           
-  my $offset = ($context - $var_length) /2; 
-  my $r_start = $start - $offset;
-  my $r_end = $end + $offset;
   my $slice =
     $object->database('core')->get_SliceAdaptor()->fetch_by_region(
-    $seq_type, $seq_region, $r_start, $r_end, 1
+    $seq_type, $seq_region, $start, $end, 1
   );
    
- 
   my $ld_object = EnsEMBL::Web::Proxy::Object->new( 'Slice', $slice, $object->__data );
-   
-#  my ($count_snps, $snps) = $object->getVariationsOnSlice();
-#  my ($genotyped_count, $genotyped_snps) = $object->get_genotyped_VariationsOnSlice();
+
   my ($count_snps, $snps) = $ld_object->getVariationFeatures();
   my ($genotyped_count, $genotyped_snps) = $ld_object->get_genotyped_VariationFeatures();
 
@@ -68,7 +58,6 @@ sub content {
     my $pop_obj = $object->pop_obj_from_name($pop_name);
     next unless $pop_obj->{$pop_name}; # i.e. skip name if not a valid pop name
    
-    #my $wuc_pop = $object->get_imageconfig( "LD_population_$pop_name", 'LD_population' );
     my $wuc_pop = $object->get_imageconfig( 'ld_population' );
     $wuc_pop->set_parameters({ 
       'image_width'     => $self->image_width ||800,
