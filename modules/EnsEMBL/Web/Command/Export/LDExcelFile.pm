bda7ab0d3a3ac08add9d21e9ff6c71f295f67e5f
Beth Yates <bp1@sanger.ac.uk>
2009-12-02 15:42:45 +0000

    handle multipe populations

--- modules/EnsEMBL/Web/Command/Export/LDExcelFile.pm
+++ modules/EnsEMBL/Web/Command/Export/LDExcelFile.pm
@@ -29,80 +29,81 @@ sub make_file {
   my $object = $self->object;
   
   my $params = $object->parent->{'params'};
-  my $pop_param = $params->{'opt_pop'}->[0];
-  my $zoom = 20000; # Currently non-configurable
+  my %pop_params = map { $object->param("pop$_") => $_ } grep s/^pop(\d+)$/$1/, $object->param;
+  warn 'ERROR: No population defined' and return unless %pop_params;
   
-  $pop_param =~ s/(%3A|:)on//; # FIXME: Hack to remove :on from opt_pop
-  
-  warn 'ERROR: No population defined' and return unless $pop_param;
-  
-  my @colour_gradient = ('ffffff', $object->image_config_hash('ldview')->colourmap->build_linear_gradient(41, 'mistyrose', 'pink', 'indianred2', 'red'));
-  
-  my $ld_values = $object->get_ld_values($pop_param, $params->{'v'}->[0], $zoom);
-  
-  my $populations = {};
-  map { $populations->{$_} = 1 } map { keys %{$ld_values->{$_}} } keys %$ld_values;
-  
-  my $file = new EnsEMBL::Web::TmpFile::Text(extension => 'xls', prefix => '');
-  my $renderer = new EnsEMBL::Web::Document::Renderer::Excel({ fh => $file });
+  my $file = new EnsEMBL::Web::TmpFile::Text(extension => 'xls', prefix => ''); 
+  my $renderer = new EnsEMBL::Web::Document::Renderer::Excel({ fh => $file }); 
   my $table = $renderer->new_table_renderer;
+ 
+  my @colour_gradient = ('ffffff', $object->image_config_hash('ldview')->colourmap->build_linear_gradient(41, 'mistyrose', 'pink', 'indianred2', 'red'));
+
+  foreach (values %pop_params){ 
+    my $pop_param = $object->param('pop'.$_); 
+    my $zoom = 20000; # Currently non-configurable
   
-  foreach my $pop_name (sort { $a cmp $b } keys %$populations) {
-    my $flag = 1;
+    my $ld_values = $object->get_ld_values($pop_param, $params->{'v'}->[0], $zoom);
+  
+    my $populations = {};
+    map { $populations->{$_} = 1 } map { keys %{$ld_values->{$_}} } keys %$ld_values;
+  
+  
+    foreach my $pop_name (sort { $a cmp $b } keys %$populations) {
+      my $flag = 1;
     
-    foreach my $ld_type (keys %$ld_values) {
-      next unless $ld_values->{$ld_type}{$pop_name}{'data'};
+      foreach my $ld_type (keys %$ld_values) {
+        next unless $ld_values->{$ld_type}{$pop_name}{'data'};
       
-      my ($starts, $snps, $data) = (@{$ld_values->{$ld_type}{$pop_name}{'data'}});
+        my ($starts, $snps, $data) = (@{$ld_values->{$ld_type}{$pop_name}{'data'}});
       
-      unshift @$data, [];
+        unshift @$data, [];
       
-      (my $sheet_name = $pop_name) =~ s/[^\w\s]/_/g;
+        (my $sheet_name = $pop_name) =~ s/[^\w\s]/_/g;
       
-      if ($flag) {
-        $table->new_sheet($sheet_name); # Start a new sheet(and new table)
-        $flag = 0;
-      } else {
-        $table->new_table; # Start a new table
-      }
-      
-      $table->set_width(2 + @$snps);
-      $table->heading($ld_values->{$ld_type}{$pop_name}{'text'});
-      $table->new_row;
-      
-      $table->write_header_cell('bp position');
-      $table->write_header_cell('SNP');
-      
-      $table->write_header_cell($_) for @$snps;
-      $table->new_row;
-      
-      foreach my $row (@$data) {
-        next unless ref $row eq 'ARRAY';
-        
-        my $snp = shift @$snps;
-        my $pos = shift @$starts;
-        
-        my @values = map { $_ ? sprintf("%.3f", $_) : '-' } @$row;
-        my @row_style = map { 'background-color:#' . ($_ eq '-' ? 'ffffff' : $colour_gradient[floor($_*40)]) . ';' } @values;
-        
-        $table->write_header_cell($pos);
-        $table->write_header_cell($snp);
-        
-        foreach my $value (@values) {
-          my $format = $table->new_format({
-            align   => 'center',
-            bgcolor => $value eq '-' ? 'ffffff' : $colour_gradient[floor($value*40)]
-          });
-          
-          $table->write_cell($value, $format);
+        if ($flag) {
+          $table->new_sheet($sheet_name); # Start a new sheet(and new table)
+          $flag = 0;
+        } else {
+          $table->new_table; # Start a new table
         }
-        
-        $table->write_header_cell($snp);
+      
+        $table->set_width(2 + @$snps);
+        $table->heading($ld_values->{$ld_type}{$pop_name}{'text'});
         $table->new_row;
+      
+        $table->write_header_cell('bp position');
+        $table->write_header_cell('SNP');
+      
+        $table->write_header_cell($_) for @$snps;
+        $table->new_row;
+      
+        foreach my $row (@$data) {
+          next unless ref $row eq 'ARRAY';
+        
+          my $snp = shift @$snps;
+          my $pos = shift @$starts;
+        
+          my @values = map { $_ ? sprintf("%.3f", $_) : '-' } @$row;
+          my @row_style = map { 'background-color:#' . ($_ eq '-' ? 'ffffff' : $colour_gradient[floor($_*40)]) . ';' } @values;
+        
+          $table->write_header_cell($pos);
+          $table->write_header_cell($snp);
+        
+          foreach my $value (@values) {
+            my $format = $table->new_format({
+              align   => 'center',
+              bgcolor => $value eq '-' ? 'ffffff' : $colour_gradient[floor($value*40)]
+            });
+          
+            $table->write_cell($value, $format);
+          }
+        
+          $table->write_header_cell($snp);
+          $table->new_row;
+        }
       }
     }
   }
-  
   $renderer->close;
   $file->save;
   
