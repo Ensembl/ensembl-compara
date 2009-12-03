31ddba35668048c71dce08c98c9b70daa736d9a6
Beth Yates <bp1@sanger.ac.uk>
2009-12-03 16:37:25 +0000

    typo

--- modules/EnsEMBL/Web/Component/Location/LDImage.pm
+++ modules/EnsEMBL/Web/Component/Location/LDImage.pm
@@ -28,7 +28,7 @@ sub content {
 
   my $slice_length = ($end - $start) +1;  
   if ($slice_length >= 200001) {
-    my $html = "<p>The region you have selected is too large to display linkage data, a maximum region of 200kb is allowed. Please change the region using the naviagation controls above.<p>";
+    my $html = "<p>The region you have selected is too large to display linkage data, a maximum region of 200kb is allowed. Please change the region using the navigation controls above.<p>";
     return $self->_error( 'Region too large', $html );
   }
 
