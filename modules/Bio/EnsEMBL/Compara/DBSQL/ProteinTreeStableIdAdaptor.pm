930b2d5e5197d3cc3485d23104be8554a3af8a9c
Albert Vilella <avilella@sanger.ac.uk>
2009-12-03 10:44:11 +0000

    added fetch_node_by_stable_id method

--- modules/Bio/EnsEMBL/Compara/DBSQL/ProteinTreeStableIdAdaptor.pm
+++ modules/Bio/EnsEMBL/Compara/DBSQL/ProteinTreeStableIdAdaptor.pm
@@ -9,6 +9,11 @@ our @ISA = qw(Bio::EnsEMBL::DBSQL::BaseAdaptor);
 
 sub fetch_by_node_id {
   my ($self, $node_id) = @_;
+  return $self->fetch_stable_id_by_node_id($node_id);
+}
+
+sub fetch_stable_id_by_node_id {
+  my ($self, $node_id) = @_;
 
   my $sql = "SELECT stable_id FROM protein_tree_stable_id WHERE node_id = ?";
   my $sth = $self->prepare($sql);
@@ -17,6 +22,19 @@ sub fetch_by_node_id {
   my ($stable_id) = $sth->fetchrow_array();
   $sth->finish();
   return $stable_id;
+}
+
+sub fetch_node_id_by_stable_id {
+  my ($self, $stable_id) = @_;
+
+  my $sql = "SELECT node_id FROM protein_tree_stable_id WHERE stable_id=\"$stable_id\"";
+  my $sth = $self->prepare($sql);
+  $sth->execute();
+
+  my ($node_id) = $sth->fetchrow_array();
+  $sth->finish();
+
+  return $node_id;
 }
 
 1;
