862c27ade3fddf323d844f4f347bee168d9be881
Simon Brent <sb23@sanger.ac.uk>
2009-12-08 15:59:30 +0000

    don't use CGI, x_requested_with param

--- modules/EnsEMBL/Web/Component/Account.pm
+++ modules/EnsEMBL/Web/Component/Account.pm
@@ -1,40 +1,37 @@
 package EnsEMBL::Web::Component::Account;
 
-use EnsEMBL::Web::Component;
-use EnsEMBL::Web::Proxy::Object;
-use EnsEMBL::Web::Data::Group;
-use EnsEMBL::Web::RegObj;
-use EnsEMBL::Web::Form;
-
-use CGI;
-
 use strict;
 use warnings;
 no warnings "uninitialized";
 
-our @ISA = qw( EnsEMBL::Web::Component);
+use URI::Escape qw(uri_escape);
+
+use EnsEMBL::Web::Data::Group;
+use EnsEMBL::Web::Form;
+use EnsEMBL::Web::Proxy::Object;
+use EnsEMBL::Web::RegObj;
+
+use base qw(EnsEMBL::Web::Component);
 
 sub edit_link {
   my ($self, $module, $id, $text) = @_;
   $text = 'Edit' if !$text;
-  return sprintf(qq(<a class="modal_link" href="/Account/%s/Edit?id=%s;_referer=%s;x_requested_with=%s">%s</a>), 
-          $module, $id, CGI::escape($self->object->param('_referer')), 
-          CGI::escape($self->object->param('x_requested_with')), $text);
+  return sprintf(qq(<a class="modal_link" href="/Account/%s/Edit?id=%s;_referer=%s">%s</a>), 
+          $module, $id, uri_escape($self->object->param('_referer')), $text);
 } 
 
 sub delete_link {
   my ($self, $module, $id, $text) = @_;
   $text = 'Delete' if !$text;
-  return sprintf(qq(<a class="modal_link" href="/Account/%s/Delete?id=%s;_referer=%s;x_requested_with=%s">%s</a>), 
-          $module, $id, CGI::escape($self->object->param('_referer')), 
-          CGI::escape($self->object->param('x_requested_with')), $text);
+  return sprintf(qq(<a class="modal_link" href="/Account/%s/Delete?id=%s;_referer=%s">%s</a>), 
+          $module, $id, uri_escape($self->object->param('_referer')), $text);
 } 
 
 
 sub share_link {
   my ($self, $call, $id) = @_;
-  return sprintf(qq(<a class="modal_link" href="/Account/SelectGroup?id=%s;type=%s;_referer=%s;x_requested_with=%s">Share</a>), 
-          $id, $call, CGI::escape($self->object->param('_referer')), CGI::escape($self->object->param('x_requested_with')) );
+  return sprintf(qq(<a class="modal_link" href="/Account/SelectGroup?id=%s;type=%s;_referer=%s">Share</a>), 
+          $id, $call, uri_escape($self->object->param('_referer')));
 } 
 
 sub dedupe {
