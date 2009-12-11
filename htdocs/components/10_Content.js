25f77515e1c3478dae0c0fca8f119dfcd1622c47
Simon Brent <sb23@sanger.ac.uk>
2009-12-11 10:56:39 +0000

    blastLink function

--- htdocs/components/10_Content.js
+++ htdocs/components/10_Content.js
@@ -10,6 +10,7 @@ Ensembl.Panel.Content = Ensembl.Panel.extend({
     this.hideHints();
     this.toggleTable();
     this.toggleList();
+    this.blastLink();
     
     Ensembl.EventManager.register('updatePanel', this, this.getContent);
     Ensembl.EventManager.trigger('validateForms', this.el);
@@ -182,5 +183,12 @@ Ensembl.Panel.Content = Ensembl.Panel.extend({
       
       return false;
     });
+  },
+  
+  blastLink: function () {
+    $('a.seq_blast', this.el).click(function () {
+      $('form.seq_blast', this.el).submit();
+      return false;
+    })
   }
 });
