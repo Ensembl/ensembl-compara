5d652947e4b8ef81beace6620afb9497f695f6ee
Simon Brent <sb23@sanger.ac.uk>
2009-12-14 16:30:11 +0000

    use species_path

--- modules/EnsEMBL/Web/Component/UserData/UploadParsed.pm
+++ modules/EnsEMBL/Web/Component/UserData/UploadParsed.pm
@@ -5,6 +5,7 @@ use warnings;
 no warnings "uninitialized";
 
 use EnsEMBL::Web::Text::FeatureParser;
+use EnsEMBL::Web::TmpFile::Text;
 
 use base qw(EnsEMBL::Web::Component::UserData);
 
@@ -22,7 +23,7 @@ sub content {
   my $self   = shift;
   my $object = $self->object;
   my $url    = $self->ajax_url('ajax', 1) . ';r=' . $object->parent->{'params'}->{'r'}[0] . ';code=' .  $object->param('code');
-  
+
   return qq{<div class="ajax" title="['$url']"></div><div class="modal_reload"></div>};
 }
 
@@ -35,35 +36,31 @@ sub content_ajax {
 
   # Here's what we actually want to do!
   my $total_features;
-  my $parser = EnsEMBL::Web::Text::FeatureParser->new($object->species_defs, $object->param('r'));
+  my $parser = new EnsEMBL::Web::Text::FeatureParser($object->species_defs, $object->param('r'));
   
   if ($upload->{'type'} eq 'upload') {
     my $size = int($upload->{'filesize'} / (1024 ** 2));
+    
     if ($size > 10) {
       $html .= "<p>Your uncompressed file is over $size MB, which may be very slow to parse and load. Please consider uploading a smaller dataset.</p>";
-    }
-    else {
+    } else {
       my $file = new EnsEMBL::Web::TmpFile::Text(filename => $upload->{'filename'}, extension => $upload->{'extension'});
       my $data = $file->retrieve;
-
+      
       $parser->parse($data, $upload->{'format'});
       $upload->{'format'} = $parser->format unless $upload->{'format'};
       $upload->{'style'}  = $parser->style;
       $upload->{'nearest'} = $parser->nearest;
       $object->get_session->set_data(%$upload);
-
-      $html .= '<p class="space-below"><strong>Total features found</strong>: ' . $parser->feature_count . '</p>';
-
+      
+      $html .= sprintf '<p class="space-below"><strong>Total features found</strong>: %s</p>', $parser->feature_count;
+      
       if ($parser->nearest) {
-        if ($object->parent->{'params'}{'r'}) {
-          $html .= '<p class="space-below"><strong>Go to nearest region with data</strong>: ';
-        }
-        else {
-          $html .= '<p class="space-below"><strong>Go to first region with data</strong>: ';
-        }
-        $html .= qq{<a href="/$upload->{'species'}/Location/View?r=} . $parser->nearest . '">' . $parser->nearest . '</a></p>';
+        $html .= sprintf '<p class="space-below"><strong>Go to %s region with data</strong>: ', $object->parent->{'params'}{'r'} ? 'nearest' : 'first';
+        $html .= sprintf '<a href="%s/Location/View?r=%s">%s</a></p>', $object->species_path($upload->{'species'}), $parser->nearest, $parser->nearest;
         $html .= '<p class="space-below">or</p>';
       }
+      
       $html .= '<p>Close this window to return to current page</p>';
     }
   }
