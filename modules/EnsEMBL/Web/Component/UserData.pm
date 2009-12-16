06e9b17dfad879b88e1a39ba6a7a8ba337fc3a0a
Simon Brent <sb23@sanger.ac.uk>
2009-12-16 09:41:54 +0000

    remove unused function

--- modules/EnsEMBL/Web/Component/UserData.pm
+++ modules/EnsEMBL/Web/Component/UserData.pm
@@ -10,21 +10,6 @@ use strict;
 use warnings;
 no warnings "uninitialized";
 
-sub is_configurable {
-  my $self = shift;
-  ## Can we do upload/DAS on this page?
-  my $flag = 0;
-  my $referer = $self->object->param('_referer');
-  my @path = split(/\//, $referer);
-  my $type = $path[2];
-  if ($type eq 'Location' || $type eq 'Gene' || $type eq 'Transcript') {
-    (my $action = $path[3]) =~ s/\?.*//;
-    my $vc = $self->object->session->getViewConfig( $type, $action);
-    $flag = 1 if $vc && $vc->can_upload;
-  }
-  return $flag;
-}
-
 sub get_assemblies {
 ### Tries to identify coordinate system from file contents
 ### If on chromosomal coords and species has multiple assemblies,
