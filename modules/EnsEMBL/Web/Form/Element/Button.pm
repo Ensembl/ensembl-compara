5ddf224068d0121e88d6dd4f85f681ee915f9ab5
Simon Brent <sb23@sanger.ac.uk>
2009-12-16 09:35:32 +0000

    make Button work

--- modules/EnsEMBL/Web/Form/Element/Button.pm
+++ modules/EnsEMBL/Web/Form/Element/Button.pm
@@ -5,19 +5,11 @@ use HTML::Entities qw(encode_entities);
 
 use base qw(EnsEMBL::Web::Form::Element);
 
-sub new { 
-  my $class = shift; 
-  my $self = $class->SUPER::new( @_ );
-  return $self;
-}
+sub new { my $class = shift; return $class->SUPER::new(@_); }
 
 sub render { 
-  ## NB: this function is normally called from Form::render_buttons, which wraps the buttons in a TR tag
-  my ($self = shift; 
-  return sprintf(
-    '<input type="button" name="%s" value="%s" class="submit" style="margin-left:0.5em;margin-right:0.5em" />', 
-    encode_entities($_[0]->name) || 'submit', 
-    encode_entities($_[0]->value), 
-  );
-}  
+  my $self = shift; 
+  return sprintf '<input type="button" name="%s" value="%s" %s />',  encode_entities($self->name) || 'submit', encode_entities($self->value), $self->class_attrib || 'class="submit"';
+}
+
 1;
