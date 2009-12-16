6eb156ff40f5fcc68ecc20882dc9c3f57c3a5de7
Simon Brent <sb23@sanger.ac.uk>
2009-12-16 09:44:36 +0000

    fix back button functionality

--- modules/EnsEMBL/Web/Command/Wizard.pm
+++ modules/EnsEMBL/Web/Command/Wizard.pm
@@ -10,46 +10,48 @@ use EnsEMBL::Web::RegObj;
 use base qw(EnsEMBL::Web::Command);
 
 sub process {
-  my $self = shift;
+  my $self   = shift;
   my $object = $self->object;
+  my $back   = $object->param('wizard_back'); # Check if we're going back
+  my @steps  = $object->param('_backtrack');
+  my $params = {};
   my $url;
-
-  ## Work out where we want to go
-  my $submit = $object->param('wizard_ajax_submit') || $object->param('wizard_submit');
-  if( $submit && $submit =~ /Back/i ) {
+  
+  if ($back) {
     my $current_node = 'Summary'; ## Default value to stop Magic from barfing
     my $species = $object->type eq 'UserData' ? $object->data_species : $object->species;
-    if ($species) {
-      $url .= $object->species_path($species);
-    }
-    $url .= '/'.$object->type;
-
-    my @steps = $object->param('_backtrack');
-    pop(@steps);
-    $current_node = pop(@steps);
-    $url .= '/'.$current_node;
-  }
-  else {
+    
+    $url .= $object->species_path($species) if $species;
+    $url .= '/' . $object->type;
+    
+    pop @steps;
+    $current_node = pop @steps;
+    $url .= "/$current_node";
+  } else {
     $url = $object->param('wizard_next');
   }
-
-  ## Pass the "normal" parameters but munge the wizard ones
-  my $param = {};
+  
+  # Pass the "normal" parameters but munge the wizard ones
   foreach my $name ($object->param) {
     next if $name =~ /^wizard_/;
+    
     my @value = $object->param($name);
     my $value = (@value) ? \@value : $value[0];
-
-    if ($name eq '_backtrack') {
-      my $submit = $object->param('wizard_submit');
-      if ($submit && $submit =~ /Back/) {
-        pop(@$value) if ref($value) eq 'ARRAY';
+    
+    if ($name eq '_backtrack' && $back) {
+      $value = \@steps;
+      
+      if (scalar @steps) {
+        $object->param('_backtrack', @steps);
+      } else {
+        $object->delete_param('_backtrack');
       }
-    }
-    $param->{$name} = $value;
+    };
+    
+    $params->{$name} = $value;
   }
-
-  $self->ajax_redirect($url, $param); 
+  
+  $self->ajax_redirect($url, $params); 
 }
 
 1;
