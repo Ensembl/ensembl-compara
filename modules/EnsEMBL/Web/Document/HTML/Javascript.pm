6ed81585544e77db0f95b7c90fbf9ac1db067b22
Simon Brent <sb23@sanger.ac.uk>
2009-12-21 09:59:00 +0000

    still needed by blast/mart. boo

---
+++ modules/EnsEMBL/Web/Document/HTML/Javascript.pm
@@ -0,0 +1,29 @@
+package EnsEMBL::Web::Document::HTML::Javascript;
+
+use strict;
+
+use base qw(EnsEMBL::Web::Document::HTML);
+
+sub new {
+  my $class = shift;
+  my $self = $class->SUPER::new('scripts' => '', 'sources' => {});
+  return $self;
+}
+
+sub add_source { 
+  my ($self, $src) = @_;
+  return unless $src;
+  
+  return if $self->{'sources'}{$src};
+  $self->{'sources'}{$src} = 1;
+  $self->{'scripts'} .= qq{ <script type="text/javascript" src="$src"></script>\n};
+}
+
+sub add_script {
+  return unless $_[1];
+  $_[0]->{'scripts'} .= qq{  <script type="text/javascript">\n$_[1]</script>\n};
+}
+
+sub render { $_[0]->print($_[0]->{'scripts'}); }
+
+1;
