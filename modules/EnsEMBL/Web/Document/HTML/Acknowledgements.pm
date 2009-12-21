d033eb2b786113747d521ec0bd9e47e9893eea47
Simon Brent <sb23@sanger.ac.uk>
2009-12-21 13:04:12 +0000

    renamed from Ack.pm

---
+++ modules/EnsEMBL/Web/Document/HTML/Acknowledgements.pm
@@ -0,0 +1,45 @@
+package EnsEMBL::Web::Document::HTML::Acknowledgements;
+
+### Generates acknowledgements
+### If you add ACKNOWLEDGEMENT entry to an ini file then you get
+### a box with the ACKNOWLEDGEMENT text at the bottom of LH menu. It will link to /info/acknowledgement.html which 
+### you will have to create
+### If you add DB_BUILDER entry to an ini file then you get
+### a box with the text DB built by XXX at the bottom of LH menu. It will link to the current species' homepage
+
+use strict;
+
+use EnsEMBL::Web::RegObj;
+
+use base qw(EnsEMBL::Web::Document::HTML);
+
+sub render {
+  my $self = shift;
+  
+  my $species_defs = $ENSEMBL_WEB_REGISTRY->species_defs;
+  my $species_path = $species_defs->species_path;
+  my $ack_text     = $species_defs->ACKNOWLEDGEMENT;
+  my $db_provider  = $species_defs->DB_BUILDER;
+  
+  if ($ack_text) {
+    $self->print(qq{
+      <div>
+        <ul>
+          <li style="list-style:none"><a href="/info/acknowledgement.html">$ack_text</a></li>
+        </ul>
+      </div>
+    });
+  }
+
+  if ($db_provider) {
+    $self->print(qq{
+      <div>
+        <ul>
+          <li style="list-style:none"><a href="$species_path/Info/Index">DB built by $db_provider</a></li>
+        </ul>
+      </div>
+    });
+  }
+}
+
+1;
