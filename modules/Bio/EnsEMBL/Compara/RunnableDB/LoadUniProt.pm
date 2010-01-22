22941b0a7a7d99955f8c0b2a6e121fac496e4f5f
Leo Gordon <lg4@ebi.ac.uk>
2010-01-22 21:16:32 +0000

    now descriptions are fixed in the module - no need to run external tools

--- modules/Bio/EnsEMBL/Compara/RunnableDB/LoadUniProt.pm
+++ modules/Bio/EnsEMBL/Compara/RunnableDB/LoadUniProt.pm
@@ -287,7 +287,7 @@ sub store_bioseq {
     $member->stable_id( $self->param('accession_number') ? $bioseq->accession_number : $bioseq->display_id );
     $member->display_label($bioseq->display_id);
     $member->taxon_id($ncbi_taxon_id);
-    $member->description($bioseq->desc);
+    $member->description( parse_description($bioseq->desc) );
     $member->source_name($source_name);
     $member->sequence($bioseq->seq);
     $member->genome_db_id($self->param('genome_db_id')) if($self->param('genome_db_id'));
@@ -321,6 +321,82 @@ sub set_id_auto_increment { # increment the IDs of both member and sequence tabl
     }
 
     $self->param('auto_increment_been_here', 1);    # so that we never come here again (for any reason)
+}
+
+sub parse_description {
+    my $old_desc = shift @_;
+
+    my @top_parts = split(/(?!\[\s*)(Includes|Contains):/,$old_desc);
+    unshift @top_parts, '';
+
+    my ($name, $desc, $flags, $top_prefix, $prev_top_prefix) = (('') x 3);
+    while(@top_parts) {
+        $prev_top_prefix = $top_prefix;
+        $top_prefix      = shift @top_parts;
+
+        if($top_prefix) {
+            if($top_prefix eq $prev_top_prefix) {
+                $desc .='; ';
+            } else {
+                if($prev_top_prefix) {
+                    $desc .=']';
+                }
+                $desc .= "[$top_prefix ";
+            }
+        }
+        my $top_data         = shift @top_parts;
+        
+        if($top_data=~/^\s*\w+:/) {
+            my @parts = split(/(RecName|SubName|AltName|Flags):/, $top_data);
+            shift @parts;
+            while(@parts) {
+                my $prefix = shift @parts;
+                my $data   = shift @parts;
+
+                if($prefix eq 'Flags') {
+                    $data=~/^(.*?);/;
+                    $flags .= $1;
+                } else {
+                    while($data=~/(\w+)\=([^\[;]*?(?:\[[^\]]*?\])?[^\[;]*?);/g) {
+                        my($subprefix,$subdata) = ($1,$2);
+                        if($subprefix eq 'Full') {
+                            if($prefix eq 'RecName') {
+                                if($top_prefix) {
+                                    $desc .= $subdata;
+                                } else {
+                                    $name .= $subdata;
+                                }
+                            } elsif($prefix eq 'SubName') {
+                                $name .= $subdata;
+                            } elsif($prefix eq 'AltName') {
+                                $desc .= "($subdata)";
+                            }
+                        } elsif($subprefix eq 'Short') {
+                            $desc .= "($subdata)";
+                        } elsif($subprefix eq 'EC') {
+                            $desc .= "(EC $subdata)";
+                        } elsif($subprefix eq 'Allergen') {
+                            $desc .= "(Allergen $subdata)";
+                        } elsif($subprefix eq 'INN') {
+                            $desc .= "($subdata)";
+                        } elsif($subprefix eq 'Biotech') {
+                            $desc .= "($subdata)";
+                        } elsif($subprefix eq 'CD_antigen') {
+                            $desc .= "($subdata antigen)";
+                        }
+                    }
+                }
+            }
+        } else {
+            $desc .= $top_data; # This is to save the names that do not follow the pattern.
+                                # Uniprot curators [should want to] thank us very much for this!
+        }
+    }
+    if($top_prefix) {
+        $desc .= ']';
+    }
+
+    return $name . $flags . $desc;
 }
 
 1;
