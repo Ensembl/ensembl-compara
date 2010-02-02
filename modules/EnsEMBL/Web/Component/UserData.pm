7752353bf913532f5ce1c787e76c65528884a386
Simon Brent <sb23@sanger.ac.uk>
2010-02-02 15:39:06 +0000

    remove unneeded uses

--- modules/EnsEMBL/Web/Component/UserData.pm
+++ modules/EnsEMBL/Web/Component/UserData.pm
@@ -2,9 +2,7 @@ package EnsEMBL::Web::Component::UserData;
 
 ## Placeholder - no generic methods needed as yet
 
-use EnsEMBL::Web::Proxy::Object;
-use EnsEMBL::Web::Form;
-use base qw( EnsEMBL::Web::Component);
+use base qw(EnsEMBL::Web::Component);
 
 use strict;
 use warnings;
