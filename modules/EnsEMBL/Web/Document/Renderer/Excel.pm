c95c829d76361d159d929193cc073e7f552f0e48
Simon Brent <sb23@sanger.ac.uk>
2010-02-04 11:41:26 +0000

    don't use Class::Std

--- modules/EnsEMBL/Web/Document/Renderer/Excel.pm
+++ modules/EnsEMBL/Web/Document/Renderer/Excel.pm
@@ -1,153 +1,134 @@
 package EnsEMBL::Web::Document::Renderer::Excel;
 
 use strict;
+
 use Spreadsheet::WriteExcel;
-use EnsEMBL::Web::Document::Renderer::Table::Excel;
-use EnsEMBL::Web::Document::Renderer::CellFormat::Excel;
 
-use Class::Std;
+use EnsEMBL::Web::Document::Renderer::Excel::Table;
+use EnsEMBL::Web::Document::Renderer::Excel::CellFormat;
 
-{
-  my %Sheet_of            :ATTR( :get<sheet> );
-  my %Row_of              :ATTR( :get<row> );
-  my %Col_of              :ATTR( :get<col> );
-  my %Width_of            :ATTR( :get<width> :set<width> );
-  my %Workbook_of         :ATTR( :get<workbook> );
-  my %Species_defs_of     :ATTR();
-  my %Format_hashref_of   :ATTR( :get<format_hashref> );
-  my %Colour_hashref_of   :ATTR( :get<colour_hashref> );
+sub new {
+  ### Builder function
+  ### Creates a new build function which sets sheets to -1 (i.e. no sheet created)
+  ### rows and columns to 0 and creates the workbook
+  
+  my ($class, $fh) = @_;
+  
+  my $workbook = new Spreadsheet::WriteExcel($fh);
+  
+  my $self = {
+    sheet    => -1,
+    row      => 0,
+    col      => 0,
+    workbook => $workbook,
+    format   => {},
+    colour   => {
+      _max_value => 9,
+      000000     => $workbook->set_custom_color(8, 0, 0, 0),
+      ffffff     => $workbook->set_custom_color(9, 255, 255, 255),
+    }
+  };
+  
+  bless $self, $class;
+  
+  return $self;
+}
 
-  sub BUILD {
-### Builder function
-### Creates a new build function which sets sheets to -1 (i.e. no sheet created)
-### rows and columns to 0 and creates the workbook!
-    my( $self, $ident, $arg_ref ) = @_;
-    $Sheet_of{    $ident } = -1; 
-    $Row_of{      $ident } = 0; 
-    $Col_of{      $ident } = 0; 
-    $Workbook_of{ $ident } = Spreadsheet::WriteExcel->new( $arg_ref->{'fh'} );
-    $Format_hashref_of{ $ident } = {};
-    $Colour_hashref_of{ $ident } = {
-      '_max_value' => 9,
-      '000000'     => $self->get_workbook->set_custom_color(8, 0, 0, 0),
-      'ffffff'     => $self->get_workbook->set_custom_color(9,255,255,255),
-    };
-  }
+sub sheet     :lvalue { $_[0]->{'sheet'};     }
+sub row       :lvalue { $_[0]->{'row'};       }
+sub col       :lvalue { $_[0]->{'col'};       }
+sub width     :lvalue { $_[0]->{'width'};     }
+sub workbook  :lvalue { $_[0]->{'workbook'};  }
+sub format    :lvalue { $_[0]->{'format'};    }
+sub colour    :lvalue { $_[0]->{'colour'};    }
 
-  sub new_table_renderer {
-### Create a new table renderer.
-    my $self = shift;
-    return EnsEMBL::Web::Document::Renderer::Table::Excel->new( { 'renderer' => $self } );
-  }
+sub set_width { $_[0]->width = $_[1];         }
+sub new_cell  { $_[0]->col++;                 }
+sub new_row   { $_[0]->row++; $_[0]->col = 0; }
 
-  sub valid  {
-### Returns a true value if their is a workbook attached (so can write to this)
-    my $self = shift;
-    return $self->get_workbook;
-  }
+sub new_sheet {
+  my $self = shift;
+  my $name = shift;
+  
+  $self->sheet++;
+  $self->row = 0;
+  $self->col = 0;
+  $self->workbook->add_worksheet($name);
+}
 
-  sub new_cell_format {
-    my( $self, $arg_ref ) = @_;
-    $arg_ref ||= {};
-    $arg_ref->{ 'format_hashref' } = $self->get_format_hashref ;
-    $arg_ref->{ 'colour_hashref' } = $self->get_colour_hashref ;
-    $arg_ref->{ 'workbook'       } = $self->get_workbook ;
-    my $format = EnsEMBL::Web::Document::Renderer::CellFormat::Excel->new($arg_ref);
-    return $format;
+sub new_table_renderer {
+  my $self = shift;
+  return new EnsEMBL::Web::Document::Renderer::Excel::Table($self);
+}
+
+sub new_cell_renderer {
+  my ($self, $args) = @_;
+  
+  $args ||= {};
+  $args->{'format'}   = $self->format;
+  $args->{'colour'}   = $self->colour;
+  $args->{'workbook'} = $self->workbook;
+  
+  return new EnsEMBL::Web::Document::Renderer::Excel::Cell($args);
+}
+
+sub printf { shift->print(@_, 'printf'); }
+
+sub print {
+  my $self   = shift;
+  my $method = $_[-1] eq 'printf' ? pop @_ : 'print';
+  
+  $self->new_row if $self->col;
+  
+  my $worksheet = $self->workbook->sheets($self->sheet);
+  my $format;
+  
+  if (@_ && ref($_[-1]) eq 'EnsEMBL::Web::Document::Renderer::Excel::CellFormat') {
+    $format = pop @_;
+  } else {
+    $format = $self->new_cell_renderer;
   }
   
-  sub printf {
-    my $self = shift;
-    $self->new_row if( $self->get_col );
-    my $worksheet = $self->get_workbook->sheets($self->{'sheet'});
-    my $format;
-    if( @_ && ref( $_[-1] ) eq 'EnsEMBL::Web::Document::Renderer::CellFormat::Excel' ) {
-      $format = pop @_;
-    } else {
-      $format = $self->new_cell_format();
-    }
-    $worksheet->merge_range(
-      $self->get_row,0,$self->get_row,$self->get_width-1,sprintf(@_),$format->evaluate
-    );
-    $self->new_row;
-  }
- 
-  sub print {
-    my $self = shift;
-    $self->new_row if( $self->get_col );
-    my $worksheet = $self->get_workbook->sheets($self->get_sheet);
-    my $format;
-    if( @_ && ref( $_[-1] ) eq 'EnsEMBL::Web::Document::Renderer::CellFormat::Excel' ) {
-      $format = pop @_;
-    } else {
-      $format = $self->new_cell_format();
-    }
+  my $content = $method eq 'printf' ? sprintf(@_) : join('', @_);
+  
+  $worksheet->merge_range($self->row, 0, $self->row, $self->width - 1, $content, $format->evaluate);
+  $self->new_row;
+}
 
-    $worksheet->merge_range(
-      $self->get_row,0,$self->get_row,$self->get_width-1,join('',@_),$format->evaluate
-    );
-    $self->new_row
-  }
- 
-  sub close  { 
-    my $self = shift;
-    return unless $self->get_workbook;
-    $self->get_workbook->close;
-    $Workbook_of{ ident $self } = undef;
-  }
+sub write_cell {
+  my $self = shift;
+  $self->write_sheet($self->row, $self->col, @_);
+  $self->new_cell;
+}
 
-  sub DESTROY {
-    my $self = shift;
-    $self->close;
-  }
-
-  sub write_cell {
-    my $self = shift;;
-    $self->write_sheet( $self->get_row, $self->get_col, @_ );
-    $self->new_cell;
-  }
-
-  sub new_cell {
-    my $self = shift;
-    my $ident = ident $self;
-    $Col_of{ $ident } ++;
-  }
-
-  sub new_row {
-    my $self = shift;
-    my $ident = ident $self;
-    $Row_of{ $ident } ++;
-    $Col_of{ $ident } =0;
-  }
-
-  sub new_sheet {
-    my $self = shift;
-    my $ident = ident $self;
-    my $name = shift;
-    $Sheet_of{ $ident }++;
-    $self->get_workbook->add_worksheet( $name );
-    $Row_of{ $ident } =0;
-    $Col_of{ $ident } =0;
-  }
-
-  sub write_sheet {
+sub write_sheet {
   ### Arg[1]      : row to print to
   ### Arg[2]      : column (or start column if you pass an array of data)
-  ###  Arg[3]      : string or array ref of data (array ref, one value per cell)
+  ### Arg[3]      : string or array ref of data (array ref, one value per cell)
   ### Arg[4]      : format (optional)
   ### Example     : $output->write_sheet(2, 0, \@my_friends, $bold_format)
   ### Description : Addes worksheet to excel workbook (filehandle)
   ### Returns none
 
-    my $self   = shift;
-    my $row    = shift || 0;
-    my $col    = shift || 0;
-    my $text   = shift;
-    my $format = shift;
-    my $worksheet = $self->get_workbook->sheets($self->get_sheet);
-    return unless $worksheet;
-    $worksheet->write($row, $col, $text, $format->evaluate);
-  }
+  my $self      = shift;
+  my $row       = shift || 0;
+  my $col       = shift || 0;
+  my $text      = shift;
+  my $format    = shift;
+  my $worksheet = $self->workbook->sheets($self->sheet);
+  
+  return unless $worksheet;
+  
+  $worksheet->write($row, $col, $text, $format->evaluate);
+}
+
+sub close { 
+  my $self = shift;
+  
+  return unless $self->workbook;
+  
+  $self->workbook->close;
+  $self->workbook = undef;
 }
 
 1;
