9d9bc958faa3f6f90ed67557a85f7904f0e819b5
Simon Brent <sb23@sanger.ac.uk>
2010-02-04 11:41:10 +0000

    removed new_table_renderer - unused

--- modules/EnsEMBL/Web/Document/Renderer/String.pm
+++ modules/EnsEMBL/Web/Document/Renderer/String.pm
@@ -4,31 +4,22 @@ use strict;
 
 use IO::String;
 
-use EnsEMBL::Web::Document::Renderer::Table::Text;
-
-use base 'EnsEMBL::Web::Document::Renderer';
+use base qw(EnsEMBL::Web::Document::Renderer);
 
 sub new {
   my $class = shift;
-
   my $self = $class->SUPER::new(string => '', @_);
   return $self;
 }
 
-sub new_table_renderer {
-### Create a new table renderer.
-  my $self = shift;
-  return EnsEMBL::Web::Document::Renderer::Table::Text->new( { 'renderer' => $self } );
-}
-
-sub printf  { shift->{'string'} .= sprintf(shift, @_);  }
-sub print   { shift->{'string'} .= join('', @_); }
-sub content { return $_[0]{'string'} }
+sub printf  { shift->{'string'} .= sprintf shift, @_; }
+sub print   { shift->{'string'} .= join '', @_; }
+sub content { return $_[0]{'string'}; }
 
 sub fh {
-  $_[0]{'fh'} = IO::String->new($_[0]{'string'})
-    unless $_[0]{'fh'};
-  return $_[0]{'fh'};
+  my $self = shift;
+  $self->{'fh'} = new IO::String($self->{'string'}) unless $self->{'fh'};
+  return $self->{'fh'};
 }
 
 1;
