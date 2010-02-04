6c27b9ec4096c9f7d3dd9564d25049864341bce5
Anne Parker <ap5@sanger.ac.uk>
2010-02-04 10:18:25 +0000

    Removed _referer from core parameters

--- modules/EnsEMBL/Web/SpeciesDefs.pm
+++ modules/EnsEMBL/Web/SpeciesDefs.pm
@@ -86,7 +86,7 @@ sub new {
   $self->{'_filename'} = $conffile;
 
   # TODO - these need to be pulled in dynamically from appropriate modules
-  my @params = qw/g h r t v db pt rf vf fdb vdb domain family protein _referer/;
+  my @params = qw/g h r t v db pt rf vf fdb vdb domain family protein/;
   $self->{'_core_params'} = \@params; 
   
   $self->parse unless $CONF;
