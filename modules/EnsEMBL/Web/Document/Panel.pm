9fd1da763dd8d512384f800d44484f10644ff9c8
Steve Trevanion <st3@sanger.ac.uk>
2010-02-08 16:12:18 +0000

    merge from branch-ensembl-dec09

--- modules/EnsEMBL/Web/Document/Panel.pm
+++ modules/EnsEMBL/Web/Document/Panel.pm
@@ -1,5 +1,3 @@
-# $Id$
-
 package EnsEMBL::Web::Document::Panel;
 
 use strict;
@@ -325,9 +323,9 @@ sub render {
     
     my $html = qq{<div class="panel js_panel"><input type="hidden" class="panel_type" value="$panel_type" />};
     my $counts = {};
-    
+
     if (!$self->{'omit_header'}) {
-      if (exists $self->{'previous'} || exists $self->{'next'}) {
+      if ( (exists $self->{'previous'} || exists $self->{'next'}) && $object->type ne 'Search') {
         my @buttons = (
           [ 'previous', 'left',  '&laquo;&nbsp;%s' ],
           [ 'next',     'right', '%s&nbsp;&raquo;' ]
