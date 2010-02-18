fff7c0f577ab49a6cd17ec4a122f7634544bb332
Ensembl Account <ensembl@sanger.ac.uk>
2010-02-18 11:17:33 +0000

    modified to exclude some modules from Pdoc in ensembl and ensembl_variation

--- utils/static_content/generate_pdoc.sh
+++ utils/static_content/generate_pdoc.sh
@@ -81,11 +81,13 @@ do
 #  else
     SOURCE="$PERLMOD_LOC/$i"
 #  fi
-#  if test $i = "ensembl"
-#   then	echo "$P2WDOCER -skip Collection,Utils,chimp,Lite -source $SOURCE -target $PDOC_LOC/$i -raw -webcvs http://cvs.sanger.ac.uk/cgi-bin/viewvc.cgi/$i/?root=ensembl -xltable $P2WDOC_LOC/$i.xlinks " >> $P2WDOC_LOC/make_html_docs.sh
-# else
+  if test $i = "ensembl"
+   then	echo "$P2WDOCER -skip Collection,Utils,chimp,Lite,misc-scripts,docs,t -source $SOURCE -target $PDOC_LOC/$i -raw -webcvs http://cvs.sanger.ac.uk/cgi-bin/viewvc.cgi/$i/?root=ensembl -xltable $P2WDOC_LOC/$i.xlinks " >> $P2WDOC_LOC/make_html_docs.sh
+ elif test $i = "ensembl-variation"
+   then	echo "$P2WDOCER -skip scripts -source $SOURCE -target $PDOC_LOC/$i -raw -webcvs http://cvs.sanger.ac.uk/cgi-bin/viewvc.cgi/$i/?root=ensembl -xltable $P2WDOC_LOC/$i.xlinks " >> $P2WDOC_LOC/make_html_docs.sh
+ else
  	echo "$P2WDOCER -source $SOURCE -target $PDOC_LOC/$i -raw -webcvs http://cvs.sanger.ac.uk/cgi-bin/viewvc.cgi/$i/?root=ensembl -xltable $P2WDOC_LOC/$i.xlinks " >> $P2WDOC_LOC/make_html_docs.sh
-# fi
+ fi
 
   echo "$PERLMOD_LOC/$F1 $HTTP/$F1
 $PERLMOD_LOC/$F2 $HTTP/$F2
