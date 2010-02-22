1768a4acde209a51f50a2cea7eb3e76824f1aa49
Beth Yates <bp1@sanger.ac.uk>
2010-02-22 16:01:31 +0000

    allow species selection

--- modules/EnsEMBL/Web/Component/UserData/UploadVariations.pm
+++ modules/EnsEMBL/Web/Component/UserData/UploadVariations.pm
@@ -2,6 +2,7 @@ package EnsEMBL::Web::Component::UserData::UploadVariations;
 
 use strict;
 use warnings;
+
 no warnings 'uninitialized';
 
 use base qw(EnsEMBL::Web::Component::UserData);
@@ -23,23 +24,42 @@ sub content {
 
   my $sitename = $object->species_defs->ENSEMBL_SITETYPE;
   my $current_species = $object->data_species;
+  my $action_url = $object->species_path($current_species)."/UserData/CheckConvert";
 
   ## Get assembly info
   my $html;
 
-  my $form = $self->modal_form('select', $object->species_path($current_species) . "/UserData/CheckConvert");
-  $form->add_notes({'heading'=>'IMPORTANT NOTE:', 'text' => qq(<p>Data should be uploaded as a list of tab separated values for more information on the expected format see <a href="/info/website/upload/index.html#Consequence">here.</a> There is also a 5.0MB limit on data uploads.</p>) });
+  ## Should default to 5.0MB :)
+  my $max_upload_size = sprintf("%.1f", $object->species_defs->CGI_POST_MAX / 1048576).'MB';
+
+  my $form = $self->modal_form('select', $action_url,);
+  $form->add_notes({ 
+    'heading'=>'IMPORTANT NOTE:',
+    'text'=>qq(<p>Data should be uploaded as a list of tab separated values for more information 
+              on the expected format see <a href="/info/website/upload/index.html#Consequence">here.</a> 
+              There is also a $max_upload_size limit on data uploads.</p>
+  )});
   my $subheader = 'Upload file';
 
    ## Species now set automatically for the page you are on
-  $form->add_element( type => 'NoEdit', name => 'show_species', label => 'Species', 'value' => $self->object->species_defs->species_label($current_species));
-  $form->add_element( type => 'Hidden', name => 'species', 'value' => $current_species);
+  my @species;
+  foreach my $sp ($object->species_defs->valid_species) {
+    push @species, {'value' => $sp, 'name' => $object->species_defs->species_label($sp, 1)};
+  }
+  @species = sort {$a->{'name'} cmp $b->{'name'}} @species;
+
   $form->add_element( type => 'Hidden', name => 'consequence_mapper', 'value' => 1);
   $form->add_element( type => 'Hidden', name => 'upload_format', 'value' => 'consequence');
   $form->add_element('type' => 'SubHeader', 'value' => $subheader);
-
+  $form->add_element(
+      'type'    => 'DropDown',
+      'name'    => 'species',
+      'label'   => "Species",
+      'values'  => \@species,
+      'value'   => $current_species,
+      'select'  => 'select',
+  );
   $form->add_element( type => 'String', name => 'name', label => 'Name for this upload (optional)' );
-
   $form->add_element( type => 'Text', name => 'text', label => 'Paste file' );
   $form->add_element( type => 'File', name => 'file', label => 'Upload file' );
   $form->add_element( type => 'URL',  name => 'url',  label => 'or provide file URL', size => 30 );
