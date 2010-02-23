c094c0b406e4dbdd682be5725b8d6b75ce4cbdf5
Albert Vilella <avilella@sanger.ac.uk>
2010-02-23 14:25:35 +0000

    using hmmer3 rc1

--- modules/Bio/EnsEMBL/Compara/RunnableDB/BuildHMM.pm
+++ modules/Bio/EnsEMBL/Compara/RunnableDB/BuildHMM.pm
@@ -297,7 +297,7 @@ sub run_buildhmm
 
   $self->{'hmm_file'} = $self->{'input_aln'} . "_hmmbuild.hmm ";
 
-  my $hmmer_dir = "/software/pfam/src/hmmer-3.0.a1/bin/";
+  my $hmmer_dir = "/software/ensembl/compara/hmmer3/hmmer-3.0rc1";
   my $buildhmm_executable;
   unless (-e $buildhmm_executable) {
     if (-e "/proc/version") {
