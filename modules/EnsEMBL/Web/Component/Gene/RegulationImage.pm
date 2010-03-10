a5cf6d9d62954e9620dc6923faedcea75f8afbd8
Beth Yates <bp1@sanger.ac.uk>
2010-03-10 16:24:43 +0000

    cleaned up code

--- modules/EnsEMBL/Web/Component/Gene/RegulationImage.pm
+++ modules/EnsEMBL/Web/Component/Gene/RegulationImage.pm
@@ -14,53 +14,9 @@ sub _init {
 sub content { 
   my $self = shift;
   my $object = $self->object; 
-
-  ## retrieve default slice
-  my $object_slice = $object->Obj->feature_Slice; 
-     $object_slice = $object_slice->invert if $object_slice->strand < 1; ## Put back onto correct strand!
-
-
-  ## retrieve gene_regulation_features_extended_slice
-  my $fg_db = undef;
-  my $db_type  = 'funcgen';
-  unless($object_slice->isa("Bio::EnsEMBL::Compara::AlignSlice::Slice")) {
-    $fg_db = $object_slice->adaptor->db->get_db_adaptor($db_type);
-    if(!$fg_db) {
-      warn("Cannot connect to $db_type db");
-      return [];
-    }
- }
-
-  my $fg_slice_adaptor = $fg_db->get_SliceAdaptor;
-  my $fsets = $object->feature_sets;  
-  my $gr_slice = $fg_slice_adaptor->fetch_by_Gene_FeatureSets($object->Obj, $fsets);
-  $gr_slice = $gr_slice->invert if $gr_slice->strand < 1; ## Put back onto correct strand!
-
-
-## Now we need to extend the slice!! Default is to add 2kb to either end of slice, if gene_reg slice is 
-## extends more than this use the values returned from this 
-  my $start = $object->Obj->start; 
-  my $end   = $object->Obj->end;  
-
-  my $gr_start = $gr_slice->start;
-  my $gr_end = $gr_slice->end;  
-  my ($new_start, $new_end);
-
-  if ( ($start  - 2000) < $gr_start) {
-     $new_start = 2000; 
-  } else {
-     $new_start = $start - $gr_start;
-  }
-
-  if ( ($end +2000) > $gr_end) {
-    $new_end = 2000;
-  }else {
-    $new_end = $gr_end - $end;
-  }
-
-  my $extended_slice =  $object_slice->expand($new_start, $new_end);
+  my $extended_slice = $object->get_extended_reg_region_slice;
   my $offset = $extended_slice->start -1;
-
+  my $fsets = $object->feature_sets;
   my $trans = $object->get_all_transcripts;
   my $gene_track_name =$trans->[0]->default_track_by_gene;
 
@@ -69,7 +25,6 @@ sub content {
  $wuc->set_parameters({
        'container_width'   => $extended_slice->length,
        'image_width',      => $self->image_width || 800,
-       'slice_number',     => '1|1',
      });
 
   ## We now need to select the correct track to turn on....
