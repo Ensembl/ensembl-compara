922f0cff08d89bb5f21357a6c5548472e5d5f055
Simon Brent <sb23@sanger.ac.uk>
2010-03-30 08:35:16 +0000

    add max, required_string

--- modules/EnsEMBL/Web/Form/Element/NonNegFloat.pm
+++ modules/EnsEMBL/Web/Form/Element/NonNegFloat.pm
@@ -7,5 +7,7 @@ sub new { my $class = shift; return $class->SUPER::new( @_, 'style' => 'short' )
 
 sub _is_valid { return $_[0]->value =~ /^([+-]?)(?=\d|\.\d)\d*(\.\d*)?([Ee]([+-]?\d+))?$/ && $_[0]->value > 0; }
 
-sub _class { return '_nonnegfloat'; }
+sub _class { return '_nonnegfloat' . ($_[0]->max ? ' max_' . $_[0]->max : ''); }
+
+sub required_string { return $_[0]->SUPER::required_string . ($_[0]->max ? sprintf ' (Maximum of %s)', $_[0]->max : ''); }
 1;
