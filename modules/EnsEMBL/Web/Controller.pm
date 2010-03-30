eeae00b436509376190961a6d3e03b59c830e974
Simon Brent <sb23@sanger.ac.uk>
2010-03-30 14:49:31 +0000

    add plugin namespace support back to zmenus

--- modules/EnsEMBL/Web/Controller.pm
+++ modules/EnsEMBL/Web/Controller.pm
@@ -219,26 +219,31 @@ sub build_menu {
   # Force values of action and type because apparently require "EnsEMBL::Web::ZMenu::::Gene" (for eg) doesn't fail. Stupid perl.
   my $type   = $model->hub->type   || 'NO_TYPE';
   my $action = $model->hub->action || 'NO_ACTION';
-  
   my $menu;
+  
+  my $i;
+  my @packages = (map({ ++$i % 2 ? $_ : () } @$ENSEMBL_PLUGINS), 'EnsEMBL::Web');
   
   ### Check for all possible module permutations.
   ### This way we can have, for example, ZMenu::Contig and ZMenu::Contig::Gene (contig menu with Gene page specific functionality),
   ### and also ZMenu::Gene and ZMenu::Gene::ComparaTree (has a similar menu to that of a gene, but has a different glyph in the drawing code)
   my @modules = (
-    "EnsEMBL::Web::ZMenu::$type",
-    "EnsEMBL::Web::ZMenu::$action",
-    "EnsEMBL::Web::ZMenu::${type}::$action",
-    "EnsEMBL::Web::ZMenu::${action}::$type"
+    "::ZMenu::$type",
+    "::ZMenu::$action",
+    "::ZMenu::${type}::$action",
+    "::ZMenu::${action}::$type"
   );
   
-  my $module_name = [ map { $self->dynamic_use($_) ? $_ : () } @modules ]->[-1];
-  
-  if ($module_name) {
-    $menu = $module_name->new($object, $menu);
-  } else {
-    my $error = $self->dynamic_use_failure($modules[-1]);
-    warn $error unless $error =~ /^Can't locate/;
+  foreach my $module_root (@packages) {
+    my $module_name = [ map { $self->dynamic_use("$module_root$_") ? "$module_root$_" : () } @modules ]->[-1];
+    
+    if ($module_name) {
+      $menu = $module_name->new($object, $menu);
+      last;
+    } else {
+      my $error = $self->dynamic_use_failure("$module_root$modules[-1]");
+      warn $error unless $error =~ /^Can't locate/;
+    }
   }
   
   $self->{'r'}->content_type('text/plain');
@@ -457,7 +462,7 @@ sub access_ok {
 }
 
 sub add_error_panel {
-  ### Wrapper for 
+  ### Wrapper for add_panel
   
   my ($self, $page, $caption, $template, @content) = @_;
   my $error = $self->_format_error(pop @content);
