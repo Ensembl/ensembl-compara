102ff9e3b7a9b3fdad9d7de4cc41fc1e148d4964
Simon Brent <sb23@sanger.ac.uk>
2010-03-31 14:12:16 +0000

    add class attribute

--- modules/EnsEMBL/Web/Form/Element/Hidden.pm
+++ modules/EnsEMBL/Web/Form/Element/Hidden.pm
@@ -8,8 +8,8 @@ use base qw(EnsEMBL::Web::Form::Element);
 
 sub render {
   return sprintf
-    '<input type="hidden" name="%s" value="%s" id="%s" />',
-    encode_entities( $_[0]->name ), encode_entities( $_[0]->value ), encode_entities( $_[0]->id );
+    '<input type="hidden" name="%s" value="%s" id="%s" %s />',
+    encode_entities($_[0]->name), encode_entities($_[0]->value), encode_entities($_[0]->id), $_[0]->class_attrib;
 }
 
 1;
