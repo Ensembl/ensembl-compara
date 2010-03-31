5291c8494889af021e213e4549997c866a69174d
Simon Brent <sb23@sanger.ac.uk>
2010-03-31 14:08:49 +0000

    panel for export stuff

---
+++ htdocs/components/20_Exporter.js
@@ -0,0 +1,41 @@
+// $Revision$
+
+Ensembl.Panel.Exporter = Ensembl.Panel.ModalContent.extend({
+  init: function () {
+    var myself = this;
+    
+    this.base();
+    this.filterInit();
+    
+    $('fieldset.general_options', this.elLk.content).find('select.output_type').live('change', function () {
+      myself.filter(this.value);
+    });
+  },
+  
+  filter: function (val) {
+    this.elLk.fieldsets.hide().filter('.' + val).show();
+  },
+  
+  filterInit: function () {
+    this.elLk.fieldsets = $('fieldset', this.el).filter('fieldset[class]').not('.general_options');
+    this.filter($('fieldset.general_options', this.el).find('select.output_type').val());
+  },
+  
+  formSubmit: function (form) {
+    var data = form.serialize();
+    
+    $('input.input-checkbox', form).each(function () {
+      // Give the value of "no" for deselected checkboxes
+      if (this.checked === false) {
+        data += "&" + this.name + "=no";
+      }
+    });
+    
+    this.base(form, data);
+  },
+  
+  updateContent: function (json) {
+    this.base(json);
+    this.filterInit();
+  }
+});
