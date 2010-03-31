95db9167013e9e31ed09e9286dcff2ac477995e2
Steve Trevanion <st3@sanger.ac.uk>
2010-03-31 09:58:54 +0000

    added stable ID for when we implement gene-based navigation code

--- modules/EnsEMBL/Web/Object/Gene.pm
+++ modules/EnsEMBL/Web/Object/Gene.pm
@@ -596,7 +596,7 @@ sub get_homology_matches {
         $homology_list{$display_spp}{$homologue_id}{'homology_dnds_ratio'} = $dnds_ratio; 
         $homology_list{$display_spp}{$homologue_id}{'display_id'}          = $homologue->display_label || 'Novel Ensembl prediction';
         $homology_list{$display_spp}{$homologue_id}{'ancestor_node_id'}    = $ancestor_node_id;
-        
+	$homology_list{$display_spp}{$homologue_id}{'stable_id'}           = $homologue_id;
         $order++;
       }
     }
