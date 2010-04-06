802604a9a30d20be83cce758e1c1d6350891455c
Albert Vilella <avilella@sanger.ac.uk>
2010-04-06 10:12:38 +0000

    adding the giant panda aliases for v59

--- scripts/taxonomy/ensembl_aliases.sql
+++ scripts/taxonomy/ensembl_aliases.sql
@@ -554,6 +554,16 @@ SET @this_value='common turkey';
 SET @this_name_class='ensembl common name';
 insert into ncbi_taxa_name select @this_taxon_id,@this_value,@this_name_class from ncbi_taxa_name WHERE taxon_id=@this_taxon_id and name_class=@this_name_class having count(*)=0;
 
+SET @this_taxon_id=9646;
+SET @this_value='Giant Panda';
+SET @this_name_class='ensembl alias name';
+insert into ncbi_taxa_name select @this_taxon_id,@this_value,@this_name_class from ncbi_taxa_name WHERE taxon_id=@this_taxon_id and name_class=@this_name_class having count(*)=0;
+
+SET @this_taxon_id=9646;
+SET @this_value='giant panda';
+SET @this_name_class='ensembl common name';
+insert into ncbi_taxa_name select @this_taxon_id,@this_value,@this_name_class from ncbi_taxa_name WHERE taxon_id=@this_taxon_id and name_class=@this_name_class having count(*)=0;
+
 -- -- Use these to ADD new species
 
 -- SET @this_taxon_id=;
