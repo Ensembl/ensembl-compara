b30e2561cff61d40c6e3dfcd8d6f4030c7cf1db8
Simon Brent <sb23@sanger.ac.uk>
2010-04-12 14:20:35 +0000

    added transcript_table function - used by GeneSummary and TranscriptSummary

--- modules/EnsEMBL/Web/Component.pm
+++ modules/EnsEMBL/Web/Component.pm
@@ -491,6 +491,238 @@ sub remove_redundant_xrefs {
   return @links;
 }
 
+sub transcript_table {
+  my $self = shift;
+  
+  my $object    = $self->object;
+  my $page_type = ref($self) =~ /::Gene\b/ ? 'gene' : 'transcript';
+  
+  my $description = encode_entities($object->gene_description);
+  $description    = '' if $description eq 'No description';
+  
+  if ($description) {
+    my ($edb, $acc);
+    
+    if ($object->get_db eq 'vega') {
+      $edb = 'Vega';
+      $acc = $object->Obj->stable_id;
+      $description .= sprintf ' <span class="small">%s</span>', $object->get_ExtURL_link("Source: $edb", $edb . '_' . lc $page_type, $acc);
+    } else {
+      $description =~ s/EC\s+([-*\d]+\.[-*\d]+\.[-*\d]+\.[-*\d]+)/$self->EC_URL($1)/e;
+      $description =~ s/\[\w+:([-\w\/\_]+)\;\w+:([\w\.]+)\]//g;
+      ($edb, $acc) = ($1, $2);
+      $description .= sprintf ' <span class="small">%s</span>', $object->get_ExtURL_link("Source: $edb $acc", $edb, $acc) if $acc ne 'content';
+    }
+    
+    $description = "<p>$description</p>";
+  }
+  
+  my $url = $self->object->_url({
+    type   => 'Location',
+    action => 'View',
+    r      => $object->seq_region_name . ':' . $object->seq_region_start . '-' . $object->seq_region_end
+  });
+  
+  my $location_html = sprintf(
+    '<a href="%s">%s: %s-%s</a> %s.',
+    $url,
+    $object->neat_sr_name($object->seq_region_type, $object->seq_region_name),
+    $object->thousandify($object->seq_region_start),
+    $object->thousandify($object->seq_region_end),
+    $object->seq_region_strand < 0 ? ' reverse strand' : 'forward strand'
+  );
+  
+  # alternative (Vega) coordinates
+  if ($object->get_db eq 'vega') {
+    my $alt_assemblies = $object->species_defs->ALTERNATIVE_ASSEMBLIES || [];
+    my ($vega_assembly) = map { $_ =~ /VEGA/; $_ } @$alt_assemblies;
+    
+    # set dnadb to 'vega' so that the assembly mapping is retrieved from there
+    my $reg = 'Bio::EnsEMBL::Registry';
+    my $orig_group = $reg->get_DNAAdaptor($object->species, 'vega')->group;
+    
+    $reg->add_DNAAdaptor($object->species, 'vega', $object->species, 'vega');
+
+    my $alt_slices = $object->vega_projection($vega_assembly); # project feature slice onto Vega assembly
+    
+    # link to Vega if there is an ungapped mapping of whole gene
+    if (scalar @$alt_slices == 1 && $alt_slices->[0]->length == $object->feature_length) {
+      my $l   = $alt_slices->[0]->seq_region_name . ':' . $alt_slices->[0]->start . '-' . $alt_slices->[0]->end;
+      my $url = $object->ExtURL->get_url('VEGA_CONTIGVIEW', $l);
+      
+      $location_html .= ' [<span class="small">This corresponds to ';
+      $location_html .= sprintf(
+        '<a href="%s" target="external">%s-%s</a>',
+        $url,
+        $object->thousandify($alt_slices->[0]->start),
+        $object->thousandify($alt_slices->[0]->end)
+      );
+      
+      $location_html .= " in $vega_assembly coordinates</span>]";
+    } else {
+      $location_html .= sprintf qq{ [<span class="small">There is no ungapped mapping of this %s onto the $vega_assembly assembly</span>]}, lc $object->type_name;
+    }
+    
+    $reg->add_DNAAdaptor($object->species, 'vega', $object->species, $orig_group); # set dnadb back to the original group
+  }
+  
+  if ($page_type eq 'gene') {
+    # Haplotype/PAR locations
+    my $alt_locs = $object->get_alternative_locations;
+    
+    if (@$alt_locs) {
+      $location_html .= '
+        <p> This gene is mapped to the following HAP/PARs:</p>
+        <ul>';
+      
+      foreach my $loc (@$alt_locs) {
+        my ($altchr, $altstart, $altend, $altseqregion) = @$loc;
+        
+        $location_html .= sprintf('
+          <li><a href="/%s/Location/View?l=%s:%s-%s">%s : %s-%s</a></li>', 
+          $object->species, $altchr, $altstart, $altend, $altchr,
+          $object->thousandify($altstart),
+          $object->thousandify($altend)
+        );
+      }
+      
+      $location_html .= '
+        </ul>';
+    }
+  }
+  
+  my $html = qq{
+    $description
+    <dl class="summary">
+      <dt>Location</dt>
+      <dd>
+        $location_html
+      </dd>
+    </dl>
+  };
+  
+  my $gene = $object->gene;
+  
+  if ($gene) {
+    my $transcript  = $page_type eq 'transcript' ? $object->stable_id : $object->param('t');
+    my $transcripts = $gene->get_all_Transcripts;
+    my $count       = @$transcripts;
+    my $plural_1    = 'are';
+    my $plural_2    = 'transcripts';
+    my $action      = $object->action;
+    my %biotype_rows;
+    
+    my %url_params = (
+      type     => 'Transcript',
+      action   => $page_type eq 'gene' || $action eq 'ProteinSummary' ? 'Summary' : $action,
+      function => undef
+    );
+    
+    if ($count == 1) { 
+      $plural_1 = 'is';
+      $plural_2 =~ s/s$//;
+    }
+    
+    my $label = "There $plural_1 $count $plural_2 in this gene:";
+    
+    if ($page_type eq 'transcript') {
+      my $gene_id  = $gene->stable_id;
+      my $gene_url = $object->_url({
+        type   => 'Gene',
+        action => 'Summary',
+        g      => $gene_id
+      });
+    
+      $label = qq{This transcript is a product of gene <a href="$gene_url">$gene_id</a> - $label};
+    }
+    
+    $html .= sprintf(qq{
+    <dl class="summary">
+      <dt>%s</dt>
+      <dd>
+        <p class="toggle_text" id="transcripts_text">%s </p>
+      </dd>
+    </dl>
+    <table class="toggle_table" id="transcripts" summary="List of transcripts for this gene - along with translation information and type">
+      <tr>
+        <th>Name</th>
+        <th>Transcript ID</th>
+        <th>Length (bp)</th>
+        <th>Protein ID</th>
+        <th>Length (aa)</th>
+        <th>Biotype</th> 
+    }, $page_type eq 'gene' ? 'Transcripts' : 'Gene', $label);
+
+    $html .= '<th>CCDS</th>' if $object->species =~ /^Homo|Mus/;
+    $html .= '</tr>';
+    
+    foreach (map { $_->[2] } sort { $a->[0] cmp $b->[0] || $a->[1] cmp $b->[1] } map { [ $_->external_name, $_->stable_id, $_ ] } @$transcripts) {
+      my $transcript_length = $_->length;
+      my $protein           = 'No protein product';
+      my $protein_length    = '-';
+      my $ccds              = '-';
+      my $url               = $self->object->_url({ %url_params, t => $_->stable_id });
+      
+      if ($_->translation) {
+        $protein = sprintf(
+          '<a href="%s">%s</a>',
+          $self->object->_url({
+            type   => 'Transcript',
+            action => 'ProteinSummary',
+            t      => $_->stable_id
+          }),
+          $_->translation->stable_id
+        );
+        
+        $protein_length = $_->translation->length;
+      }
+      
+      if (my @CCDS = grep { $_->dbname eq 'CCDS' } @{$_->get_all_DBLinks}) {
+        my %T = map { $_->primary_id => 1 } @CCDS;
+        @CCDS = sort keys %T;
+        $ccds = join ', ', map $object->get_ExtURL_link($_, 'CCDS', $_), @CCDS;
+      }
+
+      (my $biotype = $_->biotype) =~ s/_/ /g;
+      
+      my $html_row .= sprintf('
+        <tr%s>
+          <th>%s</th>
+          <td><a href="%s">%s</a></td>
+          <td>%s</td>
+          <td>%s</td>
+          <td>%s</td>
+          <td>%s</td>',
+        $count == 1 || $_->stable_id eq $transcript ? ' class="active"' : '',
+        $_->display_xref ? $_->display_xref->display_id : 'Novel',
+        $url,
+        $_->stable_id,
+        $transcript_length,
+        $protein,
+        $protein_length,
+        $self->glossary_mouseover(ucfirst $biotype)
+      );
+
+      $html_row .= "<td>$ccds</td>" if $object->species =~ /^Homo|Mus/;
+      $html_row .= '</tr>';
+      
+      $biotype = '.' if $biotype eq 'protein coding';
+      $biotype_rows{$biotype} = [] unless exists $biotype_rows{$biotype};
+      push @{$biotype_rows{$biotype}}, $html_row;
+    }
+
+    # Add rows to transcript table sorted by biotype
+    foreach my $type (sort { $a cmp $b } keys %biotype_rows) {
+      $html .= $_ for @{$biotype_rows{$type}};
+    }   
+
+    $html .= '
+    </table>';
+  }
+  
+  return $html;
+}
+
 # Simple subroutine to dump a formatted "warn" block to the error logs - useful when debugging complex
 # data structures etc... 
 # output looks like:
