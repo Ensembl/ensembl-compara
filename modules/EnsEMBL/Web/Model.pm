e87ba7a718d59f88795bd3e7f38347934ecff100
Anne Parker <ap5@sanger.ac.uk>
2010-04-13 12:47:07 +0000

    Further work to allow this module to take over functionality from Proxiable

--- modules/EnsEMBL/Web/Model.pm
+++ modules/EnsEMBL/Web/Model.pm
@@ -37,7 +37,8 @@ use base qw(EnsEMBL::Web::Root);
 sub new {
   my ($class, $args) = @_;
   my $self = { 
-    '_objects'      => {}, 
+    '_objects'  => {}, 
+    '_tabs'     => [],
   };
 
   ## Create the hub - a mass of connections to databases, Apache, etc
@@ -55,11 +56,25 @@ sub new {
 
 sub hub { return $_[0]->{'_hub'}; }
 sub all_objects { return $_[0]->{'_objects'}};
+sub tabs    :lvalue { $_[0]->{'_hub'}{'_tabs'}};
+
+
+sub add_tab {
+  my ($self, $tab, $direction) = @_;
+  return unless $tab && ref($tab) eq 'HASH';
+  if ($direction eq 'previous') {
+    unshift @{$self->hub->tab_order}, $tab->{'type'};
+  }
+  else {
+    push @{$self->hub->tab_order}, $tab->{'type'};
+  }
+  $self->hub->add_tab($tab);
+}
 
 sub objects {
 ### Getter/setter for domain objects - acts on the default data type 
 ### for this page if none is specified
-### Returns an array of objects of the appropriate type
+### Returns an arrayref of objects of the appropriate type
   my ($self, $type, $objects) = @_;
   $type ||= $self->type;
   if ($objects) {
@@ -67,14 +82,14 @@ sub objects {
     my @a = ref($objects) eq 'ARRAY' ? @$objects : ($objects); 
     push @$m, @a;
     $self->{'_objects'}{$type} = $m;
-  }
-  return @{$self->{'_objects'}{$type}};
+  } 
+  return $self->{'_objects'}{$type};
 }
 
 sub object {
 ### Getter/setter for data objects - acts on the default data type 
 ### for this page if none is specified
-### Returns the first object in the array of the appropriate type
+### Returns a single object, or undef if there is more than one 
   my ($self, $type, $object) = @_;
   $type ||= $self->hub->type;
   if ($object) {
@@ -82,24 +97,60 @@ sub object {
     push @$m, $object; 
     $self->{'_objects'}{$type} = $m;
   }
-  return $self->{'_objects'}{$type}[0];
+  if ($self->{'_objects'}{$type} && scalar @{$self->{'_objects'}{$type}} == 1) {
+    return $self->{'_objects'}{$type}[0];
+  }
+}
+
+sub raw_object {
+### returns the underlying object (mainly for API objects)
+  my ($self, $type) = @_;
+  my $object = $self->{'_objects'}{$type}[0];
+  return unless $object;
+  if ($type eq 'Location') {
+    return $object->slice;
+  }
+  else {
+    return $object->Obj;
+  }
 }
 
 sub add_objects {
 ### Adds domain objects created by the factory to this Model
-  my ($self, $data) = @_;
+  my ($self, $type, $data) = @_;
   return unless $data;
+  #warn ">>> DATA $data";
 
   ### Proxy Object(s)
   if (ref($data) eq 'ARRAY') {
-    foreach my $proxy_object (@$data) {
-      $self->object($self->hub->type, $proxy_object);
+    foreach my $element (@$data) {
+      #warn ">>> ELEMENT $element";
+      if (ref($element) eq 'HASH') {
+      ## "FEATUREVIEW"
+        while (my ($key, $array) = each (%$element)) {
+          #warn ">>> KEY $key = $array";
+          foreach (@$array) {
+            #warn "... ".$_->Obj;
+            $self->object($key, $_);
+          }
+        }
+      }
+      else {
+        $self->object($type, $element);
+      }
     }
   }
   ### Other object type
   elsif (ref($data) eq 'HASH') {
     while (my ($key, $object) = each (%$data)) {
-      $self->object($key, $object);
+      if (ref($object) eq 'ARRAY') {
+        foreach (@$object) {
+          $self->object($key, $_);
+        }
+      }
+      else {
+        $self->object($key, $object);
+      }
     }
   }
 }
@@ -115,7 +166,7 @@ sub create_data_object_of_type {
 }
 
 sub create_objects {  
-  my ($self, $type) = @_;
+  my ($self, $type, $params) = @_;
   
   my $hub     = $self->hub;
   my $factory = $self->create_factory($type);
@@ -124,18 +175,21 @@ sub create_objects {
   if ($factory) {
     if ($hub->has_fatal_problem) {
       $problem = $hub->problem('fatal', 'Fatal problem in the factory')->{'fatal'};
-    } else {
+    } 
+    else {
       eval {
-        $factory->createObjects;
+        $factory->createObjects($params);
       };
       
       $hub->problem('fatal', "Unable to execute createObject on Factory of type " . $hub->type, $@) if $@;
       
       # $hub->handle_problem returns string 'redirect', or array ref of EnsEMBL::Web::Problem object
       if ($hub->has_a_problem) {
-        $problem = $factory->handle_problem; 
-      } else {
-        $self->add_objects($factory->DataObjects);
+        $problem = $hub->handle_problem; 
+        warn "!!! OBJECT $type ".$problem->[0]->description;
+      } 
+      else {
+        $self->add_objects($type, $factory->DataObjects);
       }
     }
   }
@@ -152,13 +206,57 @@ sub create_factory {
   return unless $type;
   
   return $self->new_factory($type, {
+    _model         => $self,
     _hub           => $self->hub,
     _input         => $self->hub->input,
     _apache_handle => $self->hub->apache_handle,
     _databases     => $self->hub->databases,
-    _core_objects  => $self->hub->core_objects,
+    _core_info     => $self->hub->tabs,
     _parent        => $self->hub->parent,
   });
+}
+
+sub core_param_strings {
+  my $self = shift;
+
+  my $location     = $self->raw_object('Location');
+  my $gene         = $self->raw_object('Gene');
+  my $transcript   = $self->raw_object('Transcript');
+  my $params       = [];
+ 
+  push @$params, sprintf 'r=%s:%s-%s', $location->seq_region_name, $location->start, $location->end if $location;
+  push @$params, 'g=' . $gene->stable_id if $gene;
+  push @$params, 't=' . $transcript->stable_id if $transcript;
+
+  return $params;
+}
+
+
+sub munge_features_for_drawing {
+### Converts full objects into simple data structures that can be used by the drawing code
+  my ($self, $types) = @_;
+  my $drawable_features = {};
+
+  unless ($types) {
+    my @keys = keys %{$self->{'_objects'}};
+    foreach (@keys) {
+      push @$types, $_ unless $_ eq 'Location';
+    }
+  }
+
+  foreach my $type (@$types) {
+    my $objects = $self->objects($type);
+    my $features = [];
+    next unless $objects && @$objects;
+    foreach my $object (@$objects) {
+      next unless $object;
+      my ($f, $columns) = $object->convert_to_drawing_parameters;
+      push @{$features->[0]}, $f;
+      $features->[1] = $columns;
+    }
+    $drawable_features->{$type} = $features;
+  }
+  return $drawable_features;
 }
 
 1;
