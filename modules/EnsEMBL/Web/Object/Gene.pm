8c1170b36a038e960b05426d0a5867fd1ca40f60
Beth Yates <bp1@sanger.ac.uk>
2010-04-21 14:27:34 +0000

    new methods for gene regulation view

--- modules/EnsEMBL/Web/Object/Gene.pm
+++ modules/EnsEMBL/Web/Object/Gene.pm
@@ -906,6 +906,62 @@ sub get_fg_db {
   return $fg_db;
 }
 
+sub get_feature_view_link {
+  my ($self, $feature) = @_;
+  my $feature_id = $feature->display_label;
+  my $feature_set = $feature->feature_set->name;
+  return if $feature_set =~/cisRED|CRM/i;
+  my $link = $self->_url({
+    'type'    => 'Location',
+    'action'  => 'Genome',
+    'ftype'   => 'RegulatoryFactor',
+    'fset'    =>  $feature_set,
+    'id'      =>  $feature_id
+  });
+
+  my $url = "<span class='small'><a href=$link>[view all]</a></span>";
+  return $url;
+}
+
+sub get_extended_reg_region_slice {
+  my $self = shift;
+  ## retrieve default slice
+  my $object_slice = $self->Obj->feature_Slice;
+     $object_slice = $object_slice->invert if $object_slice->strand < 1; ## Put back onto correct strand!
+
+
+  my $fg_db = $self->get_fg_db;
+  my $fg_slice_adaptor = $fg_db->get_SliceAdaptor;
+  my $fsets = $self->feature_sets;
+  my $gr_slice = $fg_slice_adaptor->fetch_by_Gene_FeatureSets($self->Obj, $fsets);
+  $gr_slice = $gr_slice->invert if $gr_slice->strand < 1; ## Put back onto correct strand!
+
+
+  ## Now we need to extend the slice!! Default is to add 2kb to either end of slice, if gene_reg slice is
+  ## extends more than this use the values returned from this
+  my $start = $self->Obj->start;
+  my $end   = $self->Obj->end;
+
+  my $gr_start = $gr_slice->start;
+  my $gr_end = $gr_slice->end;
+  my ($new_start, $new_end);
+
+  if ( ($start  - 2000) < $gr_start) {
+     $new_start = 2000;
+  } else {
+     $new_start = $start - $gr_start;
+  }
+
+  if ( ($end +2000) > $gr_end) {
+    $new_end = 2000;
+  }else {
+    $new_end = $gr_end - $end;
+  }
+
+  my $extended_slice =  $object_slice->expand($new_start, $new_end);
+  return $extended_slice;
+}
+
 sub feature_sets {
   my $self = shift;
 
@@ -922,21 +978,41 @@ sub feature_sets {
 
 sub reg_factors {
   my $self = shift;
-  my $gene = $self->gene;  
+  my $gene = $self->gene;
   my $fsets = $self->feature_sets;
-  my $fg_db= $self->get_fg_db; 
-  my $ext_feat_adaptor = $fg_db->get_ExternalFeatureAdaptor; 
-  my $factors = $ext_feat_adaptor->fetch_all_by_Gene_FeatureSets($gene, $fsets, 1);
- 
- return $factors;   
+  my $fg_db= $self->get_fg_db;
+  my $ext_feat_adaptor = $fg_db->get_ExternalFeatureAdaptor;
+  my $fg_slice_adaptor = $fg_db->get_SliceAdaptor;
+  my $slice = $self->get_extended_reg_region_slice;
+  my $factors_by_gene = $ext_feat_adaptor->fetch_all_by_Gene_FeatureSets($gene, $fsets, 1);
+  my $factors_by_slice = $ext_feat_adaptor->fetch_all_by_Slice_FeatureSets($slice, $fsets);
+
+  my (%seen, @factors_to_return);
+
+  foreach (@$factors_by_gene){
+   my $label = $_->display_label .':'.  $_->start .''.$_->end;
+   unless (exists $seen{$label}){
+      push @factors_to_return, $_;
+      $seen{$label} = 1;
+   }
+  }
+
+  foreach (@$factors_by_slice){
+   my $label = $_->display_label .':'. $_->start .''.$_->end;
+   unless (exists $seen{$_->display_label}){
+      push @factors_to_return, $_;
+      $seen{$label} = 1;
+   }
+  }
+
+ return \@factors_to_return;
 }
 
 sub reg_features {
   my $self = shift; 
   my $gene = $self->gene;
   my $fg_db= $self->get_fg_db; 
-  my $slice = $self->get_Slice( @_ );
-
+  my $slice =  $self->get_extended_reg_region_slice;
   my $reg_feat_adaptor = $fg_db->get_RegulatoryFeatureAdaptor; 
   my $feats = $reg_feat_adaptor->fetch_all_by_Slice($slice);
   return $feats;
