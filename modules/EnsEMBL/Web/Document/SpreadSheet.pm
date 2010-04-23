218ef9c2edd9ac3a8caf1fa9b6796a218b62dff3
Simon Brent <sb23@sanger.ac.uk>
2010-04-23 12:27:06 +0000

    add support from javascript data tables

--- modules/EnsEMBL/Web/Document/SpreadSheet.pm
+++ modules/EnsEMBL/Web/Document/SpreadSheet.pm
@@ -1,29 +1,12 @@
 package EnsEMBL::Web::Document::SpreadSheet;
 
 use strict;
-use Exporter;
-use EnsEMBL::Web::Root;
 
-our @ISA = qw(Exporter EnsEMBL::Web::Root);
-our @EXPORT = qw(FORMAT_BOLD FORMAT_ITALIC FORMAT_BOLDITALIC FORMAT_NDP ALIGN_LEFT ALIGN_CENTER ALIGN_RIGHT ALTERNATING_BACKGROUND FORMAT_THOUSANDIFY);
-our @EXPORT_OK = qw(FORMAT_BOLD FORMAT_ITALIC FORMAT_BOLDITALIC FORMAT_NDP ALIGN_LEFT ALIGN_CENTER ALIGN_RIGHT ALTERNATING_BACKGROUND FORMAT_THOUSANDIFY);
-our %EXPORT_TAGS = (
-  'FORMATS' => [qw(FORMAT_BOLD FORMAT_ITALIC FORMAT_BOLDITALIC FORMAT_NDP ALIGN_LEFT ALIGN_CENTER ALIGN_RIGHT ALTERNATING_BACKGROUND FORMAT_THOUSANDIFY)]
-);
+use base qw(EnsEMBL::Web::Root);
 
-sub FORMAT_BOLD       { format => sub { "<strong>$_[0]</strong>" } }
-sub FORMAT_ITALIC     { format => sub { "<em>$_[0]</em>" } }
-sub FORMAT_BOLDITALIC { format => sub { "<strong><em>$_[0]</em></strong>" } }
-sub FORMAT_NDP        { my $T = shift; sub { sprintf "%0.${T}f", $_[0] } }
-sub FORMAT_THOUSANDIFY  { sprintf $_[0]->thousandify( $_[1] ); }
-sub ALIGN_LEFT        { align => 'left' }
-sub ALIGN_CENTER      { align => 'center' }
-sub ALIGN_RIGHT       { align => 'right' }
-sub ALTERNATING_BACKGROUND { my $self = shift ; rows => [qw(bg1 bg2)] };
-
-sub new { # All now configured by the component!!
-  my $class = shift;
-  my ($c, $d, $o, $s) = @_;
+# All now configured by the component
+sub new {
+  my ($class, $c, $d, $o, $s) = @_;
   
   $c ||= [];
   $d ||= [];
@@ -31,10 +14,10 @@ sub new { # All now configured by the component!!
   $s ||= [];
   
   my $self = {
-    '_columns'  => $c,
-    '_spanning' => $s,
-    '_data'     => $d,
-    '_options'  => $o
+    _columns  => $c,
+    _data     => $d,
+    _options  => $o,
+    _spanning => $s
   };
   
   bless $self, $class;
@@ -51,60 +34,80 @@ sub render {
   
   return unless @{$self->{'_columns'} || []};
   
-  my $options = $self->{'_options'} || {};
-  my $align = $options->{'align'} || 'autocenter';
-  my $width = $options->{'width'} || '100%';
-  my $margin = $options->{'margin'} || '0px';
+  my $options = $self->{'_options'}       || {};
+  my $width   = $options->{'width'}       || '100%';
+  my $margin  = $options->{'margin'}      || '0px';
   my $padding = $options->{'cellpadding'} || 0;
   my $spacing = $options->{'cellspacing'} || 0;
-
-  $align .= ' top-border' if $options->{'header'} eq 'no';
   
-  my $output = qq{\n<table class="ss $align" style="width:$width;margin:$margin" cellpadding="$padding" cellspacing="$spacing">};
-
-  if (scalar(@{$self->{'_spanning'}})) {
-    $output .= qq{\n  <tr class="ss_header">};
+  my $table_class = 'ss ' . ($options->{'align'} || 'autocenter');
+  my $config;
+  
+  if ($options->{'data_table'}) {
+    $table_class .= ' data_table';
+    $table_class .= " $options->{'data_table'}" if $options->{'data_table'} =~ /[a-z]/i;
+    $config      .= $self->sort_config;
+  }
+  
+  my %elements = ( thead => '', tbody => '', tfoot => '');
+  
+  if (scalar @{$self->{'_spanning'}}) {
+    $elements{'thead'} .= qq{\n  <tr class="ss_header">};
     
     foreach my $header (@{$self->{'_spanning'}}) {
       my $span = $header->{'colspan'} || 1;
-      $output .= qq{<th colspan="$span"><em>$header->{'title'}</em></th>};
+      $elements{'thead'} .= qq{<th colspan="$span"><em>$header->{'title'}</em></th>};
     }
     
-    $output .= "</tr>\n";
+    $elements{'thead'} .= "</tr>\n";
   }
 
   foreach my $row (@{$self->_process}) {
-    my $tag = 'td';
+    my $tag    = 'td';
+    my $output = 'tbody';
     
     if ($row->{'style'} eq 'header') {
-      $output .= qq{\n  <tr class="ss_header">};
-      $tag = 'th';
+      $elements{'thead'} .= qq{\n  <tr class="ss_header">};
+      $tag    = 'th';
+      $output = 'thead';
     } elsif ($row->{'style'} eq 'total') {
-      $output .= "\n  <tr>";
-      $tag = 'th';
+      $elements{'tfoot'} .= "\n  <tr>";
+      $tag    = 'th';
+      $output = 'tfoot';
     } else {
       my $valign = $row->{'valign'} || 'top';
-      my $class = $row->{'class'} ? qq{ class="$row->{'class'}"} : '';
+      my $class  = $row->{'class'} ? qq{ class="$row->{'class'}"} : '';
       
-      $output .= qq{\n  <tr style="vertical-align:$valign"$class>};
+      $elements{'tbody'} .= qq{\n  <tr style="vertical-align:$valign"$class>};
     }
     
     foreach my $cell (@{$row->{'cols'}}) {
-      my $extra = $cell->{'class'} ? qq{ class="$cell->{'class'}"} : '';
-      $extra .= $cell->{'style'} ? qq{ style="$cell->{'style'}"} : '';
-      $extra .= $cell->{'colspan'} ? qq{ colspan="$cell->{'colspan'}"} : '';
+      my $extra = $cell->{'class'}   ? qq{ class="$cell->{'class'}"}     : '';
+      $extra   .= $cell->{'style'}   ? qq{ style="$cell->{'style'}"}     : '';
+      $extra   .= $cell->{'colspan'} ? qq{ colspan="$cell->{'colspan'}"} : '';
       
-      my $val = defined ($cell->{'value'}) && $cell->{'value'} ne '' ? $cell->{'value'} : '<span style="display:none">-</span>';
+      my $val = defined $cell->{'value'} && $cell->{'value'} ne '' ? $cell->{'value'} : ' ';
          
-      $output .= "\n    <$tag$extra>$val</$tag>";
+      $elements{$output} .= "\n    <$tag$extra>$val</$tag>";
     }
 
-    $output .= "\n  </tr>";
+    $elements{$output} .= "\n  </tr>";
   }
   
-  $output .= "\n</table>";
+  $elements{$_} = "<$_>$elements{$_}</$_>" for grep $elements{$_}, keys %elements;
+  $config       = qq{<form class="data_table_config">$config</form>} if $config;
   
-  return $output;
+  # Yes, tfoot does come before tbody. Confusing I know, but that's the spec.
+  my $table = qq{
+  <table class="$table_class" style="width:$width;margin:$margin" cellpadding="$padding" cellspacing="$spacing">
+    $elements{'thead'}
+    $elements{'tfoot'}
+    $elements{'tbody'}
+  </table>
+  $config
+  };
+  
+  return $table;
 }
 
 sub render_Text {
@@ -113,17 +116,42 @@ sub render_Text {
   return unless @{$self->{'_columns'} || []};
   
   my $options = $self->{'_options'} || {};
-  my $align = $options->{'align'} ? $options->{'align'} : 'autocenter';
-  my $width = $options->{'width'} ? $options->{'width'} : '100%';
+  my $align   = $options->{'align'} ? $options->{'align'} : 'autocenter';
+  my $width   = $options->{'width'} ? $options->{'width'} : '100%';
 
   my $output = '';
   
+  $self->sort_rows($options->{'sorting'}) if $options->{'sorting'};
+  
   foreach my $row (@{$self->_process}) {
-    $output .= join "\t", map { $self->strip_HTML($_->{'value'}) } @{$row->{'cols'}};
+    $output .= join "\t", map $self->strip_HTML($_->{'value'}), @{$row->{'cols'}};
     $output .= "\n";
   }
   
   return $output;
+}
+
+# Returns a hidden input used to configure the sorting options for a javascript data table
+sub sort_config {
+  my $self = shift;
+  
+  return unless $self->{'_options'}->{'sorting'} && scalar @{$self->{'_data'}} && scalar @{$self->{'_columns'}};
+  
+  my $i = 0;
+  my %columns = map { $_->{'key'} => [ $_->{'sort'}, $i++ ] } @{$self->{'_columns'}};
+  my @config;
+  
+  foreach (@{$self->{'_options'}->{'sorting'}}) {
+    my ($col, $dir) = split / /;
+    
+    if (ref $columns{$col}) {
+      push @config, [ $columns{$col}->[1], $dir ];
+      $columns{$col} = $columns{$col}->[0];
+    }
+  }
+  
+  (my $value = $self->jsonify(\@config)) =~ s/"/'/g;
+  return qq{<input type="hidden" name="aaSorting" value="$value" />};
 }
 
 sub _process {
@@ -141,79 +169,55 @@ sub _process {
   
   foreach (0..($no_cols-1)) {
     my $col = $columns->[$_];
-    $col = $columns->[$_] = { 'key' => $col } unless ref $col eq 'HASH';
+    $col = $columns->[$_] = { key => $col } unless ref $col eq 'HASH';
     $counter++;
   }
 
   # Draw the header row unless the "header" options is set to "no"
-  unless ($options->{'header'} eq 'no') {
+  if ($options->{'header'} ne 'no') {
     $counter = 0;
     my $border;
-    my $row = { 'style' => 'header', 'cols' => [] };
-    my $average = int(100/scalar $columns);
+    my $row = { style => 'header', cols => [] };
+    my $average = int(100 / scalar $columns);
     
     foreach (@$columns) {
-      push (@{$row->{'cols'}}, {
-        'style' => 'text-align:' . ($options->{'alignheader'} || $_->{'align'} || 'center') . ';width:' . ($_->{'width'} || $average.'%'), 
-        'value' => defined $_->{'title'} ? $_->{'title'} : $_->{'key'}, 
-        'class' => 'bottom-border' 
-      });
+      push @{$row->{'cols'}}, {
+        style => 'text-align:' . ($options->{'alignheader'} || $_->{'align'} || 'center') . ';width:' . ($_->{'width'} || $average . '%'), 
+        value => defined $_->{'title'} ? $_->{'title'} : $_->{'key'}, 
+        class => $_->{'class'} . ($_->{'sort'} ? " sort_$_->{'sort'}" : '')
+      };
     }
     
-    push (@$return, $row);
+    push @$return, $row;
   }
 
   # Display each row in the table
-  my $row_count = 0;
-  my @sorted_data;
-  
-  if ($options->{'sort'}) {
-    # This doesn't actually work, and _sort_array doesn't exist
-    @sorted_data = (sort {
-      ref $options->{'sort'} eq 'CODE' ? &{$options->{'sort'}}($a, $b) : $self->_sort_array($self->_sort_pars($options->{'sort'}, $columns), $a, $b)
-    } @$data);
-  } else {
-    @sorted_data = @$data;
-  }
-  
+  my $row_count    = 0;  
   my @previous_row = ();
-  my @totals = ();
-  my $row_colours = exists $options->{'rows'} ? $options->{'rows'} : [ 'bg1', 'bg2' ];
+  my @totals       = ();
+  my $row_colours  = $options->{'data_table'} ? [] : exists $options->{'rows'} ? $options->{'rows'} : [ 'bg1', 'bg2' ];
   
-  foreach my $row (@sorted_data) {
+  foreach my $row (@$data) {
     my $flag = 0;
-    my $out_row = { 'style' => 'row', 'class' => $row_colours->[0], 'col' => [] };
+    my $out_row = { style => 'row', class => $row_colours->[0], col => [] };
     $counter = 0;
     
     foreach my $col (@$columns) {
-      my $value = get_value($row, $counter, $col->{'key'});
-      my $hidden_value = lc (exists $col->{'hidden_key'} ? get_value($row, $counter, $col->{'hidden_key'}) : $value);
-      my $class = '';
+      my $value        = $self->get_value($row, $counter, $col->{'key'});
+      my $hidden_value = lc (exists $col->{'hidden_key'} ? $self->get_value($row, $counter, $col->{'hidden_key'}) : $value);
+      my $style        = exists $col->{'align'} ? "text-align:$col->{'align'};" : ($col->{'type'} eq 'numeric' ? 'text-align:right;' : '');
+      $style          .= exists $col->{'width'} ? "width:$col->{'width'};" : '';
+      $style          .= $options->{'row_style'}->[$row_count]->[$counter] if exists $options->{'row_style'} && $options->{'row_style'}->[$row_count];
       
-      # $#sorted_data is the last index of @sorted_data
-      if ($row_count == $#sorted_data && !$options->{'total'}) {
-        $class .= ' bottom-border';
-      }
-            
-      my $style = exists $col->{'align'} ? "text-align:$col->{'align'};" : ($col->{'type'} eq 'numeric' ? 'text-align:right;' : '');
-      $style .= exists $col->{'width'} ? "width:$col->{'width'};" : '';
-      
-      if (exists $options->{'row_style'} && $options->{'row_style'}->[$row_count]) {
-        $style .= $options->{'row_style'}->[$row_count]->[$counter];
-      }
-      
-      if ($flag == $counter) {
-        if ($hidden_value eq $previous_row[$counter]) {
-          $flag = $counter+1;
-          
-          $value = '' if $options->{'triangular'};
-        }
+      if ($flag == $counter && $hidden_value eq $previous_row[$counter]) {
+        $flag  = $counter + 1;
+        $value = '' if $options->{'triangular'};
       }
       
       $previous_row[$counter] = $hidden_value;
       
       my $val = $value;
-      my $f = $col->{'format'};
+      my $f   = $col->{'format'};
       
       if ($value ne '' && $f) {
         if (ref $f eq 'CODE') {
@@ -223,32 +227,30 @@ sub _process {
         }
       }
       
-      push (@{$out_row->{'cols'}}, { 
-        'value' => $val,
-        'class' => $class,
-        'style' => $style
-      });
+      push @{$out_row->{'cols'}}, { 
+        value => $val,
+        style => $style
+      };
       
       $counter++;
     }
     
     next if $flag == $counter; # SKIP WHOLLY BLANK LINES
     
-    push (@$row_colours, shift @$row_colours);
+    push @$row_colours, shift @$row_colours;
 
     $row_count++;
     
     if ($options->{'total'} > 0) { # SUMMARY TOTALS
       if ($flag < $options->{'total'}) {
-        for (my $i = $options->{'total'}-1; $i > $flag; $i--) {
+        for (my $i = $options->{'total'} - 1; $i > $flag; $i--) {
           next unless @totals;
           
           my $TOTAL_ROW = pop @totals;
-          my $total_row = { 'style' => 'total', 'cols' => [] };
-          my $counter = 0;
+          my $total_row = { style => 'total', cols => [] };
+          my $counter   = 0;
           
           foreach my $col (@$columns) {
-            my $class = ($i == $flag+1) ? 'bottom-border' : '';
             my $value = '';
             my $style = '';
             
@@ -259,12 +261,12 @@ sub _process {
               $value = $self->thousandify($TOTAL_ROW->[$counter]);
             }
             
-            push (@{$total_row->{'cols'}}, { 'value' => $value, 'style' => $style, 'class' => $class });
+            push @{$total_row->{'cols'}}, { value => $value, style => $style };
             
             $counter++;
           }
           
-          push (@$return, $total_row);
+          push @$return, $total_row;
         }
       }
       
@@ -272,7 +274,7 @@ sub _process {
       
       foreach my $col (@$columns) {
         if ($col->{'type'} eq 'numeric') {
-          my $value = get_value($row, $counter, $col->{'key'});
+          my $value = $self->get_value($row, $counter, $col->{'key'});
             
           for (my $i = 0; $i < $options->{'total'}; $i++) {
             $totals[$i][$counter] += $value;
@@ -283,17 +285,16 @@ sub _process {
       }
     }
     
-    push (@$return, $out_row);
+    push @$return, $out_row;
   }
   
   if ($options->{'total'} > 0) { # SUMMARY TOTALS
     while (@totals) {
       my $TOTAL_ROW = pop @totals;
-      my $total_row = { 'style' => 'total', 'cols' => [] };
-      my $counter = 0;
+      my $total_row = { style => 'total', cols => [] };
+      my $counter   = 0;
       
       foreach my $col (@$columns) {
-        my $class = @totals ? 'bottom-border' : '';
         my $value = '';
         my $style = '';
         
@@ -304,18 +305,13 @@ sub _process {
           $style = 'text-align:right';
         }
         
-        push (@{$total_row->{'cols'}}, { 'value' => $value, 'style' => $style, 'class' => $class });
+        push @{$total_row->{'cols'}}, { value => $value, style => $style };
         
         $counter++;
       }
       
-      push (@$return, $total_row);
+      push @$return, $total_row;
     }
-  }
-  
-  foreach (@$return) {
-    $_->{'cols'}[0]{'class'} .= ' left-border';
-    $_->{'cols'}[-1]{'class'} .= ' right-border';
   }
   
   return $return;
@@ -326,9 +322,9 @@ sub add_option {
   my $key = shift;
   
   if (ref $self->{'_options'}->{$key} eq 'HASH') {
-    $self->{'_options'}->{$key} = {%{$self->{'_options'}->{$key}}, %{$_[0]}};
+    $self->{'_options'}->{$key} = { %{$self->{'_options'}->{$key}}, %{$_[0]} };
   } elsif (ref $self->{'_options'}->{$key} eq 'ARRAY') {
-    push (@{$self->{'_options'}->{$key}}, @_);
+    push @{$self->{'_options'}->{$key}}, @_;
   } elsif (scalar @_ == 1) {
     $self->{'_options'}->{$key} = ref $_[0] eq 'ARRAY' ? [ $_[0] ] : $_[0];
   } else {
@@ -338,21 +334,26 @@ sub add_option {
 
 sub add_columns {
   my $self = shift;
-  push (@{$self->{'_columns'}}, @_);
+  push @{$self->{'_columns'}}, @_;
 }
 
 sub add_spanning_headers {
   my $self = shift;
-  push (@{$self->{'_spanning'}}, @_);
+  push @{$self->{'_spanning'}}, @_;
 }
 
 sub add_row {
   my ($self, $data) = @_;
-  push (@{$self->{'_data'}}, $data);
+  push @{$self->{'_data'}}, $data;
+}
+
+sub add_rows {
+  my $self = shift;
+  push @{$self->{'_data'}}, @_;
 }
 
 sub get_value {
-  my ($row, $counter, $key) = @_;  
+  my ($self, $row, $counter, $key) = @_;  
   return unless $row;
   
   my $rtn = '--';
