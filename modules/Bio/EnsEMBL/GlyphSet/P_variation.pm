a53df18c13c7d1afe70daf06977ff545a30e3834
Simon Brent <sb23@sanger.ac.uk>
2010-04-29 10:38:42 +0000

    tidied and added P_variation_legend

--- modules/Bio/EnsEMBL/GlyphSet/P_variation.pm
+++ modules/Bio/EnsEMBL/GlyphSet/P_variation.pm
@@ -2,87 +2,130 @@ package Bio::EnsEMBL::GlyphSet::P_variation;
 
 use strict;
 
-no warnings "uninitialized";
-
 use base qw(Bio::EnsEMBL::GlyphSet);
 
 sub _init {
-  my ($self) = @_;
+  my $self = shift;
   
   return $self->render_text if $self->{'text_export'};
   
-  my $protein = $self->{'container'};
-  my $snps    = $self->cache('image_snps');
-  my $x       = 0;
-  my $y       = 0;
-  my $h       = $self->my_config('height')||4;
-  my $key        = "Prot SNP";    
+  my $config     = $self->{'config'};
+  my $snps       = $self->cache('image_snps');
+  my $x          = 0;
+  my $h          = $self->my_config('height') || 4; 
   my $last_indel = '##############';
   my $pix_per_bp = $self->scalex;
-  my $t_width    = $h*3/4/$pix_per_bp;
-  $self->_init_bump( undef,  $self->my_config('depth')||1e6 );
+  my $t_width    = $h * 0.75 / $pix_per_bp;
+  
+  $self->_init_bump(undef,  $self->my_config('depth') || 1e6);
 
   return unless $snps;
+  
   foreach my $int (@$snps) {
     $x++;
+    
     next if $last_indel eq $int->{'indel'};
-    my $id     = $int->{'type'}; 
-    if( $int->{'type'} eq 'insert' || $int->{'type'} eq 'delete' ) {
-      my( $in,$out,$end ) = $int->{'type'} eq 'insert' ? ($h,0,1) : (0,$h, length $int->{'allele'});
-      my $pos = $x ."-". ($x + $end); 
+    
+    if ($int->{'type'} eq 'insert' || $int->{'type'} eq 'delete') {
+      my ($in, $out, $end) = $int->{'type'} eq 'insert' ? ($h, 0, 1) : (0, $h, length $int->{'allele'});
+      my $pos    = "$x-" . ($x + $end);
+      my $colour = $self->my_colour('insert');
+      
       my $glyph = $self->Poly({
-        'x'             => $x-$t_width,
-        'y'        => 0,
-        'width'         => $t_width * 2,
-        'points'        => [ $x-$t_width, $out, $x, $in, $x+$t_width, $out  ],
-        'colour'        => $self->my_colour( 'insert' ),
-        'absolutey'     => 1,
-        'href'          => $self->_url({ 'type' => 'Variation', 'action' => 'VariationProtein', 'v' => $int->{'snp_id'}, 'vf' => $int->{'vdbid'}, 'vtype' => uc($int->{'type'}), 'pos' => $pos, 'len' => length( $int->{'allele'}), 'indel' => $int->{'allele'}  }),
-        'title'         => sprintf( '%sion %s; %s: %s; Position: %d-%d; Length: %d',
-          uc($int->{'type'}), $int->{'snp_id'},
-          uc($int->{'type'}), $int->{'allele'}, $x, $x+$end, length( $int->{'allele'} )
-        ),
+        x         => $x - $t_width,
+        y         => 0,
+        width     => $t_width * 2,
+        points    => [ $x - $t_width, $out, $x, $in, $x + $t_width, $out ],
+        colour    => $colour,
+        absolutey => 1
       });
-      my $bump_start = int( $glyph->x() * $pix_per_bp );
-      my $bump_end   = $bump_start + $glyph->width() + 3;
-      my $row        = $self->bump_row( $bump_start, $bump_end );
-      $glyph->y_transform(1.5 * $row * ($h+2));
-      $self->push( $glyph );
-      $last_indel =  $int->{'indel'};
-    } elsif( $int->{'type'} eq 'snp' || $int->{'type'} eq 'syn' ){  
-      my $type = $int->{'type'} eq 'snp' ? 'Non-synonymous' : 'Synonymous' ;
-      my $snp  = '';
-      my $type = 'Synonymous';
-      if( $int->{'type'} eq 'snp' ) {
+      
+      my $bump_start = int($glyph->x * $pix_per_bp);
+      my $bump_end   = $bump_start + $glyph->width + 3;
+      my $row        = $self->bump_row($bump_start, $bump_end);
+      
+      $glyph->y_transform(1.5 * $row * ($h + 2));
+      
+      $self->push($glyph);
+      
+      # use a rectangle for the zmenu hit box because the triangles are hard to click on
+      $self->push($self->Rect({
+        x         => $x - $t_width,
+        y         => $in + (1.5 * $row * ($h + 2)),
+        width     => 2 * $t_width,
+        height    => $h,
+        absolutey => 1,
+        title     => sprintf('%sion %s; %s: %s; Position: %d-%d; Length: %d', uc $int->{'type'}, $int->{'snp_id'}, uc $int->{'type'}, $int->{'allele'}, $x, $x + $end, length $int->{'allele'}),
+        href      => $self->_url({
+          type   => 'Variation',
+          action => 'VariationProtein',
+          v      => $int->{'snp_id'},
+          vf     => $int->{'vdbid'},
+          vtype  => uc $int->{'type'},
+          pos    => $pos,
+          len    => length $int->{'allele'},
+          indel  => $int->{'allele'}
+        })
+      }));
+      
+      $last_indel = $int->{'indel'};
+      
+      $config->{'P_variation_legend'}{ucfirst $int->{'type'}} ||= {
+        colour => $colour,
+        shape  => 'Poly'
+      };
+    } elsif ($int->{'type'} eq 'snp' || $int->{'type'} eq 'syn') {
+      my $snp    = '';
+      my $type   = 'Synonymous';
+      my $colour = $self->my_colour($int->{'type'});
+      
+      if ($int->{'type'} eq 'snp') {
         $type = 'Non-synonymous';
-        $snp  = "Alternative Residues: ". $int->{'pep_snp'}."; ";
+        $snp  = "Alternative Residues: $int->{'pep_snp'}; ";
       }
-      $snp .= "Codon: ";
-      my $codon;  
-      for my $letter ( 0..2 ){
-        $snp .= $int->{'ambigcode'}[$letter]  ? '['.$int->{'ambigcode'}[$letter].']' : $int->{'nt'}[$letter];   
-        $codon .= $int->{'ambigcode'}[$letter]  ? '['.$int->{'ambigcode'}[$letter].']' : $int->{'nt'}[$letter]; 
+      
+      $snp .= 'Codon: ';
+      
+      my $codon;
+      
+      for my $letter (0..2) {
+        my $string = $int->{'ambigcode'}[$letter] ? "[$int->{'ambigcode'}[$letter]]" : $int->{'nt'}[$letter];
+        $snp   .= $string;
+        $codon .= $string;
       }
+      
       my $glyph = $self->Rect({
-        'x'        => $x-$h/2,
-        'y'        => 0,
-        'width'    => $h,
-        'height'   => $h,
-        'colour'   => $self->my_colour( $int->{'type'} ),
-        'absolutey' => 1,
-        'absolutewidth' => 1,
-        'href'          => $self->_url({ 'type' => 'Variation', 'action' => 'VariationProtein', 'v' => $int->{'snp_id'}, 'vf' => $int->{'vdbid'}, 'res' => $x ,'cod'=> $codon, 'ar' => $int->{'pep_snp'},  'al' => $int->{'allele'}}),
-        'title'         => sprintf( '%s SNP %s; Type: %s; Residue: %d; %s; Alleles: %s',
-          $type, $int->{'snp_id'}, $int->{'allele'}, $x, $snp, $int->{'allele'}
-        )
+        x             => $x - $h /2,
+        y             => 0,
+        width         => $h,
+        height        => $h,
+        colour        => $colour,
+        absolutey     => 1,
+        absolutewidth => 1,
+        title         => sprintf('%s SNP %s; Type: %s; Residue: %d; %s; Alleles: %s', $type, $int->{'snp_id'}, $int->{'allele'}, $x, $snp, $int->{'allele'}),
+        href          => $self->_url({
+          type   => 'Variation',
+          action => 'VariationProtein',
+          v      => $int->{'snp_id'},
+          vf     => $int->{'vdbid'},
+          res    => $x,
+          cod    => $codon,
+          ar     => $int->{'pep_snp'},
+          al     => $int->{'allele'}
+        })
       });
-      my $bump_start = int( $glyph->x() * $pix_per_bp );
-      my $bump_end   = $bump_start + $glyph->width() + 3;
-      my $row        = $self->bump_row( $bump_start, $bump_end );
-      $glyph->y($glyph->y + 1.5 * $row * ($h+2) );
-      $self->push( $glyph );
-    } else {
-      next;
+      
+      my $bump_start = int($glyph->x * $pix_per_bp);
+      my $bump_end   = $bump_start + $glyph->width + 3;
+      my $row        = $self->bump_row($bump_start, $bump_end);
+      
+      $glyph->y($glyph->y + 1.5 * $row * ($h + 2));
+      $self->push($glyph);
+      
+      $config->{'P_variation_legend'}{$type} ||= {
+        colour => $colour,
+        shape  => 'Rect'
+      };
     }
   }
 }
@@ -120,12 +163,12 @@ sub render_text {
     }
     
     $export .= $self->_render_text($container, 'Variation', { 
-      'headers' => [ 'variation_name', 'alleles', 'class', 'type', 'alternative_residues', 'codon' ],
-      'values' => [ $id, $_->{'allele'}, $_->{'type'}, $type, $_->{'pep_snp'}, $codon ]
+      headers => [ 'variation_name', 'alleles', 'class', 'type', 'alternative_residues', 'codon' ],
+      values  => [ $id, $_->{'allele'}, $_->{'type'}, $type, $_->{'pep_snp'}, $codon ]
     }, { 
-      'start'  => $start,
-      'end'    => $start + $end,
-      'source' => $_->{'snp_source'}
+      start  => $start,
+      end    => $start + $end,
+      source => $_->{'snp_source'}
     });
   }
   
