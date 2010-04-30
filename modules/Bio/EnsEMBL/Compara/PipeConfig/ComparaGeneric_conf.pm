3c33296b76abfb085e91b036710e5e945a5c13ad
Leo Gordon <lg4@ebi.ac.uk>
2010-04-30 14:52:11 +0000

    cosmetic

--- modules/Bio/EnsEMBL/Compara/PipeConfig/ComparaGeneric_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/ComparaGeneric_conf.pm
@@ -7,10 +7,20 @@ use warnings;
 use base ('Bio::EnsEMBL::Hive::PipeConfig::HiveGeneric_conf');
 
 
+sub default_options {
+    my ($self) = @_;
+    return {
+        %{$self->SUPER::default_options},
+
+        'pipeline_name' => 'compara_generic',
+    };
+}
+
+
 sub pipeline_create_commands {
     my ($self) = @_;
     return [
-        @{$self->SUPER::pipeline_create_commands},
+        @{$self->SUPER::pipeline_create_commands},    # inheriting database and hive table creation
 
             # standard and pipeline Compara tables:
         'mysql '.$self->dbconn_2_mysql('pipeline_db', 1).' <'.$self->o('ensembl_cvs_root_dir').'/ensembl-compara/sql/table.sql',
