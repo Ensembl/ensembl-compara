f6a44e79fac9feeeeba34e21ba782b363556b86b
Kathryn Beal <kbeal@ebi.ac.uk>
2010-04-30 08:30:54 +0000

    Changed ensembl release to 58. Use fetch_by_gene_Member_root_id.

--- scripts/examples/homology8.pl
+++ scripts/examples/homology8.pl
@@ -5,7 +5,8 @@ use Bio::EnsEMBL::Registry;
 Bio::EnsEMBL::Registry->load_registry_from_db
   (-host=>"ensembldb.ensembl.org", 
    -user=>"anonymous", 
-   -db_version=>'45');
+   -db_version=>'58');
+
 my $human_gene_adaptor =
     Bio::EnsEMBL::Registry->get_adaptor("Homo sapiens", "core", "Gene");
 my $member_adaptor =
@@ -40,7 +41,7 @@ foreach my $gene (@$genes) {
 
   # Fetch the proteintree
   my $proteintree =  $proteintree_adaptor->
-    fetch_by_Member_root_id($member);
+    fetch_by_gene_Member_root_id($member);
 
   # Delete the part that is not one2one wrt the human gene
   foreach my $leaf (@{$proteintree->get_all_leaves}) {
