2d6fbb7ec2a9b7acc0dea78113e7135b00816fad
Beth Yates <bp1@sanger.ac.uk>
2010-05-06 14:23:04 +0000

    cleaned up code and fixed broken links

--- modules/EnsEMBL/Web/ZMenu/RegFeature.pm
+++ modules/EnsEMBL/Web/ZMenu/RegFeature.pm
@@ -8,93 +8,35 @@ use base qw(EnsEMBL::Web::ZMenu);
 
 sub content {
   my $self = shift;
+  my $object = $self->object;
+  my $feature = $object->database('funcgen')->get_ExternalFeatureAdaptor->fetch_by_dbID($object->param('dbid'));
+  my $location = $feature->slice->seq_region_name . ':' . $feature->start . '-' . $feature->end;
+
+  my $display_label  = $feature->display_label;
+  my $feature_name   = $feature->feature_type->name;
+  my $external_link  = $self->get_external_link($feature); 
+  my $feature_view_link = $self->get_feature_view_link($feature);
   
-  my $object       = $self->object; 
-  my $species_path = $object->species_path;
-  my $fid          = $object->param('fid')   || die 'No feature ID value in params';
-  my $ftype        = $object->param('ftype') || die 'No feature type value in params';
-  my $dbid         = $object->param('dbid');
-  my $feature      = $object->database('funcgen')->get_ExternalFeatureAdaptor->fetch_by_dbID($dbid);
-  my $location     = $feature->slice->seq_region_name . ':' . $feature->start . '-' . $feature->end;
-  my $caption      = 'Regulatory Region';
-  my $factor       = $fid;
-  my ($feature_link, $factor_link);
+  $self->caption('Regulatory Region');
+
+  $self->add_entry ({
+    type          => 'Name',
+    label         =>  $display_label,
+  });
   
-  if ($ftype eq 'cisRED') {
-    $factor =~ s/\D*//g;
-    
-    $feature_link = $self->object->species_defs->ENSEMBL_EXTERNAL_URLS->{'CISRED'}; 
-    $feature_link =~ s/###ID###/$factor/;
-    
-    $factor_link = "$species_path/Location/Genome?ftype=RegulatoryFactor;dbid=$dbid;id=$fid";
-  } elsif ($ftype eq 'miRanda') {
-   (my $name = $fid) =~ /\D+(\d+)/;
-    my @temp = split /:/, $name;
-    
-    $factor  = $temp[1];
-    $factor_link = "$species_path/Location/Genome?ftype=RegulatoryFactor;id=$factor;name=$fid";
-  } elsif ($ftype eq 'vista_enhancer') {
-    $factor_link = "$species_path/Location/Genome?ftype=RegulatoryFactor;id=$factor;name=$fid";
-  } elsif ($ftype eq 'NestedMICA') {
-    $factor_link = "$species_path/Location/Genome?ftype=RegulatoryFactor;id=$factor;name=$fid";
-    $feature_link = "http://servlet.sanger.ac.uk/tiffin/motif.jsp?acc=$fid";
-  } elsif ($ftype eq 'cisred_search') {
-    my ($id, $analysis_link, $associated_link, $gene_reg_link);
-    
-    foreach my $dbe (@{$feature->get_all_DBEntries}) {
-      my $dbname = $dbe->dbname;
-      $id = $dbe->primary_id;
-      
-      if ($dbname =~ /gene/i) {
-        $associated_link = $object->_url({ type => 'Gene', action => 'Summary',    g => $id });
-        $gene_reg_link   = $object->_url({ type => 'Gene', action => 'Regulation', g => $id });
-        
-        $analysis_link = $self->object->species_defs->ENSEMBL_EXTERNAL_URLS->{'CISRED'};
-        $analysis_link =~ s/siteseq\?fid=###ID###/gene_view?ensembl_id=$id/;
-      } elsif ($dbname =~ /transcript/i) {
-        $associated_link = $object->_url({ type => 'Transcript', action => 'Summary', t => $id });
-      } elsif ($dbname =~ /transcript/i) {
-        $associated_link = $object->_url({ type => 'Transcript', action => 'Summary', p => $id });
-      }
-    }
-    
-    $caption = 'Regulatory Search Region';
-    
-    $self->add_entry({
-      type       => 'Analysis',
-      label_html => $ftype,
-      link       => $analysis_link
+  if ($external_link =~/http/){
+    $self->add_entry ({
+      type          => 'FeatureType',
+      label_html    =>  $feature_name,
+      link          =>  $external_link,
     });
-    
-    $self->add_entry({
-      type       => 'Target Gene',
-      label_html => $id,
-      link       => $associated_link
+  } else {
+    $self->add_entry ({
+      type          => 'FeatureType',
+      label         =>  $feature_name,
     });
-    
-    if ($object->parent->{'ENSEMBL_TYPE'} ne 'Regulation') {
-      $self->add_entry({
-        label_html => 'View Gene Regulation',
-        link       => $gene_reg_link
-      });
-    }
   }
 
-  # add zmenu items that apply to all external regulatory features
-  if ($ftype ne 'cisred_search') { 
-    $self->add_entry({
-      type       => 'Feature',
-      label_html => $fid,
-      link       => $feature_link
-    });
-    
-    $self->add_entry({
-      type       => 'Factor',
-      label_html => $factor,
-      link       => $factor_link
-    });
-  }
-  
   $self->add_entry({
     type       => 'bp',
     label_html => $location,
@@ -105,7 +47,58 @@ sub content {
     })
   });
   
-  $self->caption($caption);
+  if ($feature_view_link){
+    $self->add_entry({
+      label_html => 'View all locations',
+      link       => $feature_view_link, 
+    });
+  }
+}
+
+sub get_external_link {
+  my ($self, $f) = @_;
+  my $type = $f->feature_type->name;
+  my $logic_name = $f->analysis->logic_name;
+  my $ext_id = $f->display_label;
+  my $external_link;
+  return if $ext_id =~/Search/;
+
+  if ( $logic_name =~/cisred/i ){
+    $ext_id =~s/\D*//g;
+    $external_link = $self->object->get_ExtURL_link($f->display_label, uc($logic_name), $ext_id );
+  } elsif( $logic_name =~/miranda/i )  {
+    my @display_names = split(/:/, $f->display_label);
+    $external_link = $self->object->get_ExtURL_link($display_names[1], uc($logic_name),{ 'ID' => $display_names[1]});
+  } elsif($logic_name =~/MICA/){
+     $external_link = $self->object->get_ExtURL_link($type, uc($logic_name), $ext_id );
+  } elsif($logic_name=~/REDFLY/i){
+    $external_link = $f->display_label;
+  } elsif ($logic_name=~/VISTA/i){
+    $ext_id =~s/LBNL-//;
+    $external_link = $self->object->get_ExtURL_link($f->display_label, uc($logic_name).'EXT', $ext_id );
+  }
+  
+  if ($external_link =~/href/){ 
+    my @link_info = split (/href\=|\>|\"/, $external_link);
+    $external_link = $link_info[2]; 
+  }
+  return $external_link;
+}
+
+sub get_feature_view_link {
+  my ($self, $feature) = @_;
+  my $feature_id = $feature->display_label;
+  my $feature_set = $feature->feature_set->name;
+  return if $feature_set =~/cisRED|CRM/i;
+  my $link = $self->object->_url({
+    'type'    => 'Location',
+    'action'  => 'Genome',
+    'ftype'   => 'RegulatoryFactor',
+    'fset'    =>  $feature_set,
+    'id'      =>  $feature_id,
+  });
+  
+  return $link;
 }
 
 1;
