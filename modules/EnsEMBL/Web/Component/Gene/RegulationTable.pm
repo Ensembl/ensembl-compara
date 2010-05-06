133d8b03695ba83a4498fe422e46eb73df6da09f
Beth Yates <bp1@sanger.ac.uk>
2010-05-06 14:29:01 +0000

    re-write to include correct data

--- modules/EnsEMBL/Web/Component/Gene/RegulationTable.pm
+++ modules/EnsEMBL/Web/Component/Gene/RegulationTable.pm
@@ -23,131 +23,173 @@ sub caption {
 
 sub content {
   my $self = shift;
-  my $object = $self->object;
-  my @factors =  @{$object->reg_factors};
-  my $species = $object->species;
-  my @reg_feats;
-  if ($species eq 'Homo_sapiens' || $species eq 'Mus_musculus'){ @reg_feats = @{$object->reg_features};}
-  my $object_slice = $object->Obj->feature_Slice;
-  my $offset = $object_slice->start -1;
-  my $object_strand = $object_slice->strand;
-  my $str = "positive";
-  if ($object_strand  <1 ){$str = "negative"; }
- 
-  ## return if no regulatory factors ##
-  my $size = @factors;
-  my $size2 = @reg_feats; # warn $size2;
-  
-  if ($size < 1 && $size2 <1) {
+  my $gene_object = $self->object;
+  my @reg_factors =  @{$gene_object->reg_factors};
+  my @reg_features;
+  unless ($self->object->species =~/Drosophila_melanogaster/) { @reg_features = @{$gene_object->reg_features}; }
+
+  ## return if no regulatory elements ##
+  if ( scalar @reg_factors < 1 && scalar @reg_features <1) {
     my $html = "<p><strong>There are no regulatory factors linked to this gene</strong></p>";
     return $html;
   }
- 
+
   ## If there are factors to display ##
+  my $gene_slice = $gene_object->Obj->feature_Slice;
+  my $offset = $gene_slice->start -1;
+  my $str = "positive";
+
   my $table = new EnsEMBL::Web::Document::SpreadSheet( [], [], {'margin' =>'1em 0px'});
   $table->add_columns(
-   {'key' => 'feature', 'title' => 'Reg. region', 'width' => '22%', 'align' => 'left'},
-   {'key' => 'analysis', 'title' => 'Analysis', 'width' => '16%', 'align' => 'left'},
-   {'key' => 'type', 'title' => 'Type', 'width' => '18%', 'align' => 'left'},
-   {'key' => 'location', 'title' => 'Location', 'width' => '20%', 'align' => 'left'},
-   {'key' => 'length', 'title' => 'Length', 'width' => '4%', 'align' => 'left'},
-   {'key' => 'seq', 'title' => 'Sequence ('. $str .' strand)', 'width' => '20%', 'align' => 'left'},
-  ); 
-
-  my $data = 0;
-  ## First process Ensembl Funcgen Reg. Factors ##
-  foreach my $obj (@reg_feats){
-    my $feature_obj = $self->new_object('Regulation', $obj, $object->__data);
-    my $row = {};
-    my ($position, $feature_link, $length, $type);
-
-    $position = $feature_obj->location_string($offset);
-    my $position_link = $feature_obj->get_location_url;
-
-    $position = qq(<a href=$position_link>$position</a>);
-
-    $length = $feature_obj->length;
-    $length = $object->thousandify( $length ). "bp";
- 
-    my $feature_name = $feature_obj->stable_id;
-    $type = $feature_obj->feature_type->name;
-    my $details_url = $feature_obj->get_details_page_url;
-    $feature_link = qq(<a href=$details_url>$feature_name</a>);
- 
-    $type = $feature_obj->feature_type->name;
-
-    my $seq = $feature_obj->get_seq($object_slice->strand);
-    $seq =~ s/([\.\w]{60})/$1<br \/>/g;
-  
+    { key => 'feature',  title => 'Reg. region',            width => '21%', align => 'left', sort => 'html'           },
+    { key => 'analysis', title => 'Analysis',               width => '16%', align => 'left', sort => 'html'           },
+    { key => 'type',     title => 'Type',                   width => '18%', align => 'left', sort => 'html'         },
+    { key => 'location', title => 'Location',               width => '20%', align => 'left', sort => 'position_html'  },
+    { key => 'length',   title => 'Length (bp)',            width => '5%',  align => 'left', sort => 'numeric'        },
+    { key => 'seq',      title => "Sequence ($str strand)", width => '20%', align => 'left', sort => 'none'           },
+  );
+  # First process Ensembl regulatory features
+  foreach my $feature (@reg_features){
+    my $regulation_obj = $self->new_object('Regulation', $feature, $gene_object->__data);
+    my $details_url = $regulation_obj->get_details_page_url;
+    my $feature_id = $feature->stable_id;
+    my $feature_link = qq(<a href=$details_url>$feature_id</a>);
     my $analysis  = qq(<a rel="external" href="/info/docs/funcgen/index.html">Ensembl Regulatory Build</a>);
-    $row = {
-      'location'  => $position,
+    my $type = $feature->feature_type->name;
+    my ($sequence, $length) = $self->get_sequence($regulation_obj);
+
+    my $row = {
+      'feature'   => $feature_link,
+      'analysis'  => $analysis,
+      'type'      => $type,
+      'location'  => $self->get_location_link($regulation_obj),
+      'seq'       => $sequence,
       'length'    => $length,
-      'seq'       => qq(<span class="sequence">$seq</span>),
-      'feature'    => $feature_link,
-      'type'   => $type,
-      'analysis'  => $analysis
-    };
-    $data = 1;
+  };
     $table->add_row($row);
   }
 
-  ## Now process external factors ##
-  foreach my $feature_obj (@factors){
-   my $row = {};
-   my ($position, $seq, $feature_link, $feature, $desc, $length);
-   $feature = $feature_obj->feature_type->name;
-   my $seq_name = $feature_obj->slice->seq_region_name;
-    
-   $position = $object->thousandify($feature_obj->start) ."-" . $object->thousandify($feature_obj->end) ;
-   $position = qq(<a href="@{[$object->species_path]}/Location/Summary?db=core;r=$seq_name:).$feature_obj->start . qq(-).$feature_obj->end .qq(">$seq_name:$position</a>);   
+  # Then add info from external sources
+  foreach my $factor (@reg_factors){
+    next if $factor->display_label =~/Search/;
+    my ($sequence, $length) = $self->get_sequence($factor);
+    my $row = {
+      'feature'   => $self->get_id_link($factor),
+      'analysis'  => $self->get_analysis($factor),
+      'type'      => $self->get_type($factor),
+      'location'  => $self->get_location_link($factor),
+      'seq'       => $sequence,
+      'length'    => $length,
+    };
 
-   my $region = $seq_name .":" .$feature_obj->start ."-".$feature_obj->end;
-   my $feature_name = $feature_obj->display_label;
-   my $logic_name= $feature_obj->analysis->logic_name;
-   my $dbid = $feature_obj->dbID;
-   if ($logic_name =~/cisRED/){
-    $feature_link = $feature_name ? qq(<a href="@{[$object->species_path]}/Location/Genome?r=$region;id=$feature_name;dbid=$dbid;ftype=RegulatoryFactor">$feature_name</a>) : "unknown";
-   } else {
-    $feature_link = $feature_name ? qq(<a href="@{[$object->species_path]}/Location/Genome?r=$region;id=$feature;ftype=RegulatoryFactor;name=$feature_name">$feature_name</a>) : "unknown";
-   }
-   $desc = $feature_obj->analysis->description;
-  next if $feature =~/cisRED\sSearch\sRegion/; 
-    # hack to get around problem with source data file for release 50
-   if ($feature_name  =~/cra.*/){
-         $desc =~s/cisRED\smotif\ssearch/cisRED atomic motifs/;
-         $feature  = "cisRED atomic motifs";
-   }
-     
-   if  ($desc =~/\(http/){ $desc =~ s/\(http/#http/;} 
-   elsif($desc =~/\(www/){ $desc =~ s/www/#http:\/\/www/;} 
-   $desc =~ s/\(#/#/;
-   $desc =~s/\)//;
-   my @temp = split(/#/, $desc);
-   my $description = qq(<a rel="external" href="$temp[1]">).$temp[0]."</a>"; 
- 
-   $seq = $feature_obj->seq();
-   $seq =~ s/([\.\w]{60})/$1<br \/>/g;
-   $length = $object->thousandify( length ($seq) ). "bp";
+    $table->add_row($row);
+  }
 
-   $row = {
-     'location'  => $position,
-     'length'    => $length,
-     'seq'       => qq(<span class="sequence">$seq</span>),
-     'feature'   => $feature_link,
-     'type'      => $feature,
-     'analysis'  => $description
-   };
-   $data = 1;
-   $table->add_row($row);
- }
-
-  if ($data ==1) {
-     return $table->render;
-   } else {  
-    my $html = "<p><strong>There are no regulatory factors linked to this gene</strong></p>";
-    return $html;
-  } 
+  return $table->render;
 }
 
+sub get_analysis {
+  my ($self, $f) = @_;
+  my $desc = $f->analysis->description;
+  my $analysis;
+
+  unless ($desc=~/www|http/){
+     my @names = split(/\s+/, $desc);
+     $analysis =  $self->object->get_ExtURL_link($desc,  uc($names[0]), $desc);
+     $analysis = $desc unless $analysis =~/\w+/;
+  } else {
+    # hack to get around problem with source data file for release 50
+    if ($f->display_label  =~/cra.*/){
+      $desc =~s/cisRED\smotif\ssearch/cisRED atomic motifs/;
+      $desc =~s/www/http:\/\/www/;
+    }
+    my @temp = split(/\(|\)/, $desc);
+    $analysis = qq(<a rel="external" href="$temp[1]">).$temp[0]."</a>";
+  }
+  return $analysis;
+}
+
+sub get_id_link {
+  my ($self, $f )= @_;
+  my $f_link = $f->display_label;
+  return $f_link;
+}
+
+sub get_location_link {
+  my ($self, $f) = @_;
+  my ($position, $type);
+  if ($f->isa('Bio::EnsEMBL::Funcgen::ExternalFeature')){
+    $type = 'regulatory_regions_funcgen_feature_set=normal';
+    my $seq_name = $f->slice->seq_region_name;
+    my $f_slice = $self->object->get_extended_reg_region_slice;
+    my $f_offset = $f_slice->start -1;
+    my $f_start = $f->start + $f_offset;
+    my $f_end = $f->end + $f_offset;
+    $position = $seq_name .':' .$f_start .'-' . $f_end;
+  } else {
+    $type = 'fg_regulatory_features_funcgen_reg_feats=normal';
+    my $offset = $self->object->get_extended_reg_region_slice->start -1;
+    $position = $f->location_string($offset);
+  }
+
+  my $position_url = $self->object->_url({
+    'type'              => 'Location',
+    'action'            => 'View',
+    'r'                 => $position,
+    'contigviewbottom'  => $type,
+  });
+  my $location_link = "<a href=$position_url>$position</a>";
+
+  return $location_link;
+}
+sub get_sequence {
+  my ($self, $f) = @_;
+  my $sequence;
+  if ($f->isa('EnsEMBL::Web::Proxy::Object')){
+    $sequence = $f->get_seq($self->object->Obj->feature_Slice->strand);
+  } else {
+    my $gene_slice = $self->object->get_extended_reg_region_slice;
+    if ($f->isa('EnsEMBL::Web::Object::Regulation')){
+      $sequence = $gene_slice->subseq($f->seq_region_start, $f->seq_region_end, 1);
+    } else {
+      $sequence = $gene_slice->subseq($f->start, $f->end, 1);
+    }
+  }
+
+  $sequence =~ s/([\.\w]{60})/$1<br \/>/g;
+  my $length = $self->object->thousandify( length ($sequence) );
+  my $formatted_sequence = "<span class='sequence'>$sequence</span>";
+  return ($formatted_sequence, $length);
+}
+sub get_type {
+  my ($self, $f) = @_;
+  my $type = $f->feature_type->name;
+  my $logic_name = $f->analysis->logic_name;
+  my $ext_id = $f->display_label;
+  my $external_link;
+
+  if ( $logic_name =~/cisred/i ){
+    $ext_id =~s/\D*//g;
+    $external_link = $self->object->get_ExtURL_link($f->display_label, uc($logic_name), $ext_id );
+  } elsif( $logic_name =~/miranda/i )  {
+    my @display_names = split(/:/, $f->display_label);
+    $external_link = $self->object->get_ExtURL_link($display_names[1], uc($logic_name),{ 'ID' => $display_names[1]});
+  } elsif($logic_name =~/MICA/){
+     $external_link = $self->object->get_ExtURL_link($type, uc($logic_name), $ext_id );
+  }elsif($logic_name=~/REDFLY/i){
+    $external_link = $f->display_label;
+  }elsif ($logic_name=~/VISTA/i){
+    $ext_id =~s/LBNL-//;
+    $external_link = $self->object->get_ExtURL_link($f->display_label, uc($logic_name).'EXT', $ext_id );
+  }
+
+  # add feature view link
+  my $all_link = $self->object->get_feature_view_link($f);
+  if ($all_link =~/\w+/){
+    $external_link .= " ". $all_link;
+  }
+
+  return $external_link;
+}
+1;
 1;
