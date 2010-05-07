92b4e23300fae564f36e53f113436a2ac03e75e3
Simon Brent <sb23@sanger.ac.uk>
2010-05-07 14:38:13 +0000

    using functions from ViewConfig::Gene::ExternalData

--- modules/EnsEMBL/Web/ViewConfig/Variation/ExternalData.pm
+++ modules/EnsEMBL/Web/ViewConfig/Variation/ExternalData.pm
@@ -2,41 +2,14 @@ package EnsEMBL::Web::ViewConfig::Variation::ExternalData;
 
 use strict;
 use warnings;
-use EnsEMBL::Web::RegObj;
 
-use base qw(Exporter);
+use EnsEMBL::Web::ViewConfig::Gene::ExternalData qw(form init);
 
-our @EXPORT_OK = qw(init form);
-our @EXPORT    = @EXPORT_OK;
 
-sub init {
-  my $view_config = shift;
-
-  $view_config->storable   = 1;
-  $view_config->can_upload = 1; # allows configuration of DAS etc
-  $view_config->_set_defaults(map { $_->logic_name => undef } values %{$ENSEMBL_WEB_REGISTRY->get_all_das});
-}
+## SEE SUPERCLASS FOR PRIMARY FUNCTIONALITY ##
 
 sub _view {
-#  return 'Gene/ExternalData';
   return 'Variation/ExternalData';
-}
-
-sub form {
-  my ($view_config, $object) = @_;
-  
-  $view_config->add_fieldset('DAS sources');
-  
-  my @all_das = sort { lc $a->label cmp lc $b->label } grep { $_->is_on(_view) } values %{$ENSEMBL_WEB_REGISTRY->get_all_das};
-
-  for my $das (@all_das) {
-    $view_config->add_form_element({
-      'type'  => 'DASCheckBox',
-      'das'   => $das,
-      'name'  => $das->logic_name,
-      'value' => 'yes'
-    });
-  }
 }
 
 
