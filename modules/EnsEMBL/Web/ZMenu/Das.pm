bf4cc15a475069c989546613352b47920fb49b8d
Simon Brent <sb23@sanger.ac.uk>
2010-05-07 12:00:09 +0000

    add ability to display notes nicely by using the format 'Title: Note'

--- modules/EnsEMBL/Web/ZMenu/Das.pm
+++ modules/EnsEMBL/Web/ZMenu/Das.pm
@@ -91,7 +91,17 @@ sub content {
       $self->add_entry({ type => 'Score:',  label_html => $score }) if $score;
       
       $self->add_entry({ label_html => $_->{'txt'}, link => decode_entities($_->{'href'}), extra => { external => ($_->{'href'} !~ /^http:\/\/www.ensembl.org/) } }) for @{$_->links};
-      $self->add_entry({ label_html => $validator->validate($_) ? encode_entities($_) : $_ }) for map decode_entities($_), @{$_->notes};
+      
+      foreach (map decode_entities($_), @{$_->notes}) {
+        my $note = $validator->validate($_) ? encode_entities($_) : $_;
+        
+        if ($note =~ /: /) {
+          my ($type, $label_html) = split /: /, $note;
+          $self->add_entry({ type => $type, label_html => $label_html });
+        } else {
+          $self->add_entry({ label_html => $note });
+        }
+      }
     }
   }
 }
