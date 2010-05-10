2380856568e03014be7aab88dab5b89bb6a20aa2
Anne Parker <ap5@sanger.ac.uk>
2010-05-10 08:27:09 +0000

    Configuration for new LRG views

---
+++ modules/EnsEMBL/Web/Configuration/LRG.pm
@@ -0,0 +1,159 @@
+package EnsEMBL::Web::Configuration::LRG;
+
+use strict;
+
+use base qw( EnsEMBL::Web::Configuration );
+
+sub global_context { return $_[0]->_global_context; }
+sub ajax_content   { return $_[0]->_ajax_content;   }
+sub configurator   { return $_[0]->_configurator;   }
+sub local_context  { return $_[0]->_local_context;  }
+sub local_tools    { return $_[0]->_local_tools;    }
+sub content_panel  { return $_[0]->_content_panel;  }
+sub context_panel  { return $_[0]->_context_panel;  }
+
+sub set_default_action {
+  my $self = shift;
+  
+  if (!ref $self->object) {
+    $self->{'_data'}->{'default'} = 'Summary';
+    return;
+  }
+  
+  my $x = $self->availability || {};
+  
+  if ($x->{'lrg'}) {
+    $self->{'_data'}->{'default'} = 'Summary';
+  }
+}
+
+sub short_caption {
+  my $self = shift;
+  return 'LRG-based displays';
+}
+
+sub caption           {
+  my $self = shift;
+  my $caption;
+=pod
+  my( $disp_id ) = $self->object->display_xref;
+  my $caption = $self->object->type_name.': ';
+  if( $disp_id ) {
+    $caption .= "$disp_id (".$self->object->stable_id.")";
+  } else {
+    $caption .= $self->object->stable_id;
+  }
+=cut
+  if ($self->model->hub->param('lrg')) {
+    $caption = 'LRG: '.$self->model->hub->param('lrg'); 
+  }
+  else {
+    $caption = 'LRGs';
+  }
+  return $caption;
+}
+
+sub availability {
+  my $self = shift;
+  my $hash = $self->get_availability;
+  $hash->{'lrg'} = $self->model->api_object('LRG') ? 1 : 0;
+
+  my $lrg = $self->model->object('LRG');
+  if ($lrg) {
+    my $rows = $lrg->table_info( $lrg->get_db, 'stable_id_event' )->{'rows'};
+    $hash->{'either'}   = 1;
+    $hash->{'core'}     = $lrg->get_db eq 'core' ? 1 : 0;
+    my $funcgen_db = $lrg->get_db('funcgen');
+    my $funcgen_res = 0;
+    if ($funcgen_db){
+     $funcgen_res = $lrg->table_info('funcgen', 'feature_set' )->{'rows'} ? 1 : 0;
+    }
+    $hash->{'regulation'} = $funcgen_res ? 1 : 0;
+  }
+  return $hash;
+}
+
+sub counts {
+  my $self = shift;
+  my $hub = $self->model->hub;
+  my $obj = $self->model->api_object('Gene');
+
+  return {} unless $obj;
+
+  my $key = '::COUNTS::GENE::'.$hub->species.'::'.$hub->param('db').'::'.$hub->param('lrg').'::';
+  my $counts = $hub->cache ? $hub->cache->get($key) : undef;
+
+  if (!$counts) {
+    $counts = {
+      transcripts   => scalar @{$self->model->api_object('Transcript')},
+      genes         => 1,
+    };
+
+    $hub->cache->set($key, $counts, undef, 'COUNTS') if $hub->cache;
+  }
+ 
+  return $counts;
+}
+
+
+sub populate_tree {
+  my $self = shift;
+
+  ## HACK THE AVAILABILITY, BECAUSE WE ARE MIXING OBJECT TYPES
+  my $has_lrg = $self->model->api_object('LRG') ? 1 : 0;
+
+  $self->create_node('Genome', 'All LRGs',
+    [qw(
+      karyotype EnsEMBL::Web::Component::Location::Genome 
+    )],
+    { 'availability' => 1 }
+  );
+
+  $self->create_node('Summary', 'LRG summary',
+    [qw(
+      summary     EnsEMBL::Web::Component::LRG::LRGSummary
+      transcripts EnsEMBL::Web::Component::LRG::TranscriptsImage  
+    )],
+    { 'availability' => $has_lrg }
+  );
+
+  $self->create_node('Sequence', 'Sequence',
+    [qw( exons EnsEMBL::Web::Component::LRG::LRGSeq )],
+    { 'availability' => $has_lrg}
+  );
+ 
+  my $var_menu = $self->create_submenu('Variation', 'Genetic Variation');
+
+  $var_menu->append($self->create_node('Variation_LRG/Table', 'Variation Table',
+    [qw(
+      snptable EnsEMBL::Web::Component::LRG::LRGSNPTable
+      snpinfo  EnsEMBL::Web::Component::LRG::GeneSNPInfo
+    )],
+    { 'availability' => "lrg database:variation" }
+  ));
+
+  $var_menu->append($self->create_node('Variation_LRG/Image',  'Variation Image',
+    [qw( image EnsEMBL::Web::Component::LRG::LRGSNPImage )],
+    #{ 'availability' => "lrg database:variation" }
+    { 'availability' => 0 }
+  ));
+
+  # External Data tree, including non-positional DAS sources
+  my $external = $self->create_node('ExternalData', 'External Data',
+    [qw( external EnsEMBL::Web::Component::Gene::ExternalData )],
+    { 'availability' => 'gene' }
+  );
+  
+  $external->append($self->create_node('UserAnnotation', 'Personal annotation',
+    [qw( manual_annotation EnsEMBL::Web::Component::Gene::UserAnnotation )],
+    { 'availability' => 'gene' }
+  ));
+  
+  
+  $self->create_subnode('Export', 'Export Gene Data',
+    [qw( export EnsEMBL::Web::Component::Export::Gene )],
+    { 'availability' => 'gene', 'no_menu_entry' => 1 }
+  );
+}
+
+1;
