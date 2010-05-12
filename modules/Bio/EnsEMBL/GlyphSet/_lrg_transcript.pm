d724634326468eda81df21d5be821ea64e459ab5
Anne Parker <ap5@sanger.ac.uk>
2010-05-12 08:27:52 +0000

    More RLG glyphsets

---
+++ modules/Bio/EnsEMBL/GlyphSet/_lrg_transcript.pm
@@ -0,0 +1,103 @@
+package Bio::EnsEMBL::GlyphSet::_lrg_transcript;
+
+use strict;
+use Data::Dumper;
+use base qw( Bio::EnsEMBL::GlyphSet_transcript );
+
+sub analysis_logic_name{
+  my $self = shift;
+  return $self->my_config('LOGIC_NAME');
+}
+
+sub features {
+  my ($self) = @_;
+  my $slice = $self->{'container'};
+  
+  my $db_alias = $self->my_config('db');
+  my $analyses = $self->my_config('logicnames');
+
+  my @T = map { @{$slice->get_all_Genes( $_, $db_alias )||[]} } grep { /LRG/i } @$analyses;
+
+  $self->timer_push( 'Fetched transcripts', undef, 'fetch' );
+  return \@T;
+}
+
+sub text_label {
+  my ($self, $gene, $transcript) = @_;
+  my $id  = $transcript->external_name || $transcript->stable_id;
+  return $id if $self->get_parameter( 'opt_shortlabels');
+  my $label = $self->transcript_label($transcript, $gene);
+  $id .= "\n$label" unless $label eq '-';
+  return $id;
+}
+
+sub gene_text_label {
+  my ($self, $gene ) = @_;
+  my $id  = $gene->external_name || $gene->stable_id;
+  return $id if $self->get_parameter( 'opt_shortlabels');
+  my $label = $self->gene_label( $gene);
+  $id .= "\n$label" unless $label eq '-';
+  return $id;
+}
+
+sub _add_legend_entry {
+  my $self = shift;
+  $self->{'legend_data'}{ $self->{'my_config'}->left }=1;
+}
+
+sub legend {
+  my ($self, $colours) = @_;
+  # TODO; make generic
+  return undef;
+}
+
+## sub error_track_name { return $_[0]->my_label }
+
+sub href { 	 
+  my ($self, $gene, $transcript) = @_;
+  
+#  warn "HREF !!!";
+
+  my $params = {
+    species => $self->species,
+    type    => 'Transcript',
+    action  => 'Summary',
+    t       => $transcript->stable_id,
+    g       => $gene->stable_id, 
+    db      => $self->my_config('db')
+  };
+  
+  $params->{'r'} = undef if $self->{'container'}->{'web_species'} ne $self->species;
+  
+  my $url = $self->_url($params); 	 
+#  warn "UR: $url \n";
+  return $self->_url($params); 	 
+} 	 
+
+sub gene_href { 	 
+  my ($self, $gene) = @_;
+#    warn "GENE HREF !!!";
+  my $params = {
+    species => $self->species,
+    type    => 'Gene',
+    action  => 'Summary',
+    g       => $gene->stable_id, 
+    db      => $self->my_config('db')
+  };
+  
+  $params->{'r'} = undef if $self->{'container'}->{'web_species'} ne $self->species;
+  
+  return $self->_url($params);
+}
+
+sub export_feature {
+  my $self = shift;
+  my ($feature, $transcript_id, $transcript_name, $gene_id, $gene_name, $gene_type) = @_;
+  
+  return $self->_render_text($feature, 'Exon', {
+    'headers' => [ 'gene_id', 'gene_name', 'transcript_id', 'transcript_name', 'exon_id', 'gene_type' ],
+    'values' => [ $gene_id, $gene_name, $transcript_id, $transcript_name, $feature->stable_id, $gene_type ]
+  });
+}
+
+1;
---
+++ modules/Bio/EnsEMBL/GlyphSet/lsv_variations.pm
@@ -0,0 +1,113 @@
+package Bio::EnsEMBL::GlyphSet::lsv_variations;
+
+use strict;
+
+use Sanger::Graphics::Bump;
+
+use base qw(Bio::EnsEMBL::GlyphSet);
+
+
+sub _init {
+  my ($self) = @_; 
+  my $type = $self->check(); ;
+  return unless defined $type;
+  return unless $self->strand() == -1;
+
+  my $Config = $self->{'config'}; 
+  my $transcript =  $Config->{'transcript'}->{'transcript'};
+ 
+  # Drawing params
+  
+  my( $fontname, $fontsize ) = $self->get_font_details( 'innertext' );
+  my $pix_per_bp    = $Config->transform->{'scalex'};
+  my @res = $self->get_text_width( 0, 'M', '', 'font'=>$fontname, 'ptsize' => $fontsize );
+  my( $font_w_bp, $font_h_bp) = ($res[2]/$pix_per_bp,$res[3]);
+  my $h = $res[3] + 4; 
+
+  # Data stuff
+  my $colour_map  = $self->my_config('colours');  
+  my $offset = $self->{'container'}->strand > 0 ? $self->{'container'}->start - 1 :  $self->{'container'}->end + 1; 
+  my $EXTENT        = $Config->get_parameter( 'context'); 
+   
+     $EXTENT        = 1e6 if $EXTENT eq 'FULL'; 
+  my $seq_region_name = $self->{'container'}->seq_region_name(); 
+
+  # Bumping params
+  my $bitmap_length = int($Config->container_width() * $pix_per_bp);
+  my $voffset = 0;
+  my @bitmap;
+  my $max_row = -1;
+
+  warn "SNPS : ", scalar(@{$Config->{'snps'}});
+
+  my $START = $transcript->start - $EXTENT; # - $offset;
+  my $END = $transcript->end + $EXTENT;# - $offset;
+
+  warn " SE : $START : $END \n";
+
+  foreach my $snpref ( @{$Config->{'snps'}} ) { 
+    my $snp = $snpref->[2];
+    my $cod_snp =  $Config->{'transcript'}->{'snps'}->{$snp->dbID()};
+    next unless $cod_snp;
+    next if $snp->end < $START;
+    next if $snp->start > $END;
+    my $snp_type = lc($cod_snp->display_consequence); 
+    my $colour = $colour_map->{$snp_type}->{'default'};
+    my $aa_change = $cod_snp->pep_allele_string || '';
+#    warn "D: $snp_type : ", $snp->dbID;
+
+    my $S =  ( $snpref->[0]+$snpref->[1] )/2;
+    my @res = $self->get_text_width( 0, $aa_change, '', 'font'=>$fontname, 'ptsize' => $fontsize );
+    my $W = $res[2]/$pix_per_bp;
+    my $tglyph = $self->Text({
+      'x'         => $S-$W/2,
+      'y'         => $h+4,
+      'height'    => $font_h_bp,
+      'width'     => $res[2]/$pix_per_bp,
+      'textwidth' => $res[2],
+      'font'      => $fontname,
+      'ptsize'    => $fontsize,
+      'colour'    => 'black',
+      'text'      => $aa_change,
+      'absolutey' => 1,
+    });
+    $W += 4/$pix_per_bp;
+
+    my $dbid = $cod_snp->dbID;  
+    my $variation_id = $snp->variation_name;
+    unless ($aa_change =~/^\w+/) {$aa_change = '-';} 
+
+    my $href = $self->_url
+    ({'action'  => 'Variation',
+      'v'     => $variation_id,
+      'vf'    => $dbid,
+      'var_box' => $aa_change,
+    });
+
+    my $type      = join ", ", @{$cod_snp->consequence_type || [] }; 
+    $type = lc($type);
+    my $bglyph = $self->Rect({
+      'x'         => $S - $W / 2,
+      'y'         => $h + 2,
+      'height'    => $h,
+      'width'     => $W,
+      'colour'    => $colour,
+      'absolutey' => 1,
+      'href'      => $href,
+    });
+    my $bump_start = int($bglyph->{'x'} * $pix_per_bp);
+       $bump_start = 0 if ($bump_start < 0);
+    my $bump_end = $bump_start + int($bglyph->width()*$pix_per_bp) +1;
+       $bump_end = $bitmap_length if ($bump_end > $bitmap_length);
+    my $row = & Sanger::Graphics::Bump::bump_row( $bump_start, $bump_end, $bitmap_length, \@bitmap );
+    $max_row = $row if $row > $max_row;
+    $tglyph->y( $voffset + $tglyph->{'y'} + ( $row * (2+$h) ) + 1 );
+    $bglyph->y( $voffset + $bglyph->{'y'} + ( $row * (2+$h) ) + 1 );
+    $self->push( $bglyph, $tglyph );
+  }
+}
+
+
+sub error_track_name { return $_[0]->species_defs->AUTHORITY.' transcripts'; }
+
+1;
