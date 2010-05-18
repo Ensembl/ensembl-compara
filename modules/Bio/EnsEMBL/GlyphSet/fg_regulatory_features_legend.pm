8aa58223e19a887b2604dc8dd885bd0ed6f2431f
Beth Yates <bp1@sanger.ac.uk>
2010-05-18 10:51:02 +0000

    changed to display all possible types to get around issue of data in multiple tracks

--- modules/Bio/EnsEMBL/GlyphSet/fg_regulatory_features_legend.pm
+++ modules/Bio/EnsEMBL/GlyphSet/fg_regulatory_features_legend.pm
@@ -5,7 +5,7 @@ use strict;
 use base qw( Bio::EnsEMBL::GlyphSet);
 
 sub _init {
-  my ($self) = @_;
+  my ($self) = @_ ; 
   return unless ($self->strand() == -1);
 
   my $BOX_WIDTH     = 20;
@@ -14,13 +14,11 @@ sub _init {
   my $vc            = $self->{'container'};
   my $Config        = $self->{'config'};
   my $im_width      = $Config->image_width();
- # my $type          = $Config->get_parameter('fg_regulatory_features_legend', 'src');
    
   my @colours;
   return unless $Config->{'fg_regulatory_features_legend_features'};
-  my %features = %{$Config->{'fg_regulatory_features_legend_features'}}; 
+  my %features = %{$self->my_config('colours')}; 
   return unless %features;
-
  
   my ($x,$y) = (0,0);
   my( $fontname, $fontsize ) = $self->get_font_details( 'legend' );
@@ -29,16 +27,15 @@ sub _init {
   my $pix_per_bp = $self->{'config'}->transform()->{'scalex'};
   
   my $FLAG = 0;
-  foreach (sort { $features{$b}->{'priority'} <=> $features{$a}->{'priority'} } keys %features) {                                              
-    @colours = @{$features{$_}->{'legend'}}; 
-
-    $y++ unless $x==0;
-    $x=0;
-    while( my ($legend, $colour) = splice @colours, 0, 2 ) {
-      $FLAG = 1;
-      my $tocolour='';
-      ($tocolour,$colour) = ($1,$2) if $colour =~ /(.*):(.*)/;
-      $self->push($self->Rect({
+  foreach (sort {$a <=> $b } keys %features){
+    my $colour = $self->my_colour($_); 
+    my $legend = $self->my_colour($_, 'text');  
+    next if $legend =~/unknown/i; 
+    
+    $FLAG = 1;
+    my $tocolour='';
+    ($tocolour,$colour) = ($1,$2) if $colour =~ /(.*):(.*)/;
+    $self->push($self->Rect({
         'x'         => $im_width * $x/$NO_OF_COLUMNS,
         'y'         => $y * ( $th + 3 ) + 2,
         'width'     => $BOX_WIDTH,
@@ -46,8 +43,8 @@ sub _init {
         $tocolour.'colour'    => $colour,
         'absolutey' => 1,
         'absolutex' => 1,'absolutewidth'=>1,
-      }));
-      $self->push($self->Text({
+    }));
+    $self->push($self->Text({
         'x'         => $im_width * $x/$NO_OF_COLUMNS + $BOX_WIDTH,
         'y'         => $y * ( $th + 3 ),
         'height'    => $th,
@@ -59,12 +56,11 @@ sub _init {
         'text'      => " $legend",
         'absolutey' => 1,
         'absolutex' => 1,'absolutewidth'=>1
-      }));
-      $x++;
-      if($x==$NO_OF_COLUMNS) {
-        $x=0;
-        $y++;
-      }
+    }));
+    $x++;
+    if($x==$NO_OF_COLUMNS) {
+      $x=0;
+      $y++;
     }
   }
 # Set up a separating line...
