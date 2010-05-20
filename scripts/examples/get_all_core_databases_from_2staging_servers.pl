4732358430b2181bd41fdc1bde03d1250a550273
Leo Gordon <lg4@ebi.ac.uk>
2010-05-20 15:35:54 +0000

    an example of how to load_registry_from_multiple_dbs

---
+++ scripts/examples/get_all_core_databases_from_2staging_servers.pl
@@ -0,0 +1,28 @@
+#!/usr/local/bin/perl -w
+
+# Generates a list of Bio::EnsEMBL::DBSQL::DBAdaptor objects for all core databases found on two staging servers
+# minus databases that contain ancestral sequences.
+
+use strict;
+use Bio::EnsEMBL::Registry;
+
+Bio::EnsEMBL::Registry->load_registry_from_multiple_dbs(
+    {   '-host' => 'ens-staging.internal.sanger.ac.uk',
+        '-port' => 3306,
+        '-user' => 'ensro',
+        '-pass' => '',
+    },
+    {   '-host' => 'ens-staging2.internal.sanger.ac.uk',
+        '-port' => 3306,
+        '-user' => 'ensro',
+        '-pass' => '',
+    },
+);
+
+my @core_dbas = grep { $_->species !~ /ancestral/i } @{ Bio::EnsEMBL::Registry->get_all_DBAdaptors( -group => 'core') };
+
+print "\n------------[Found a total of ".scalar(@core_dbas)."core databases on staging servers]------------\n";
+foreach my $dba (@core_dbas) {
+    print ''.$dba->species()."\n";
+}
+
