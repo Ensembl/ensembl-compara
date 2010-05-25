d2b12d88e668b7935ae2f59f32fbd0faa9550445
William McLaren <wm2@ebi.ac.uk>
2010-05-25 14:06:03 +0000

    Added HGNC name for LRG to data structure

--- modules/EnsEMBL/Web/Data/Bio/LRG.pm
+++ modules/EnsEMBL/Web/Data/Bio/LRG.pm
@@ -24,23 +24,31 @@ sub convert_to_drawing_parameters {
   foreach my $slice_pair (@$data) {
     my $lrg = $slice_pair->{'lrg'};
     my $chr = $slice_pair->{'chr'};
+    
+    # get the LRG's HGNC name
+    my $lrg_sr_name = $lrg->seq_region_name;
+    my $gene = (grep {$_->stable_id =~ /$lrg_sr_name\_/} @{$lrg->get_all_Genes_by_type('LRG_gene')})[0];
+    my $hgnc_name = (grep {$_->dbname =~ /hgnc/i} @{$gene->get_all_DBEntries})[0]->display_id;
+    
     if (ref($lrg) =~ /UnmappedObject/) {
       my $unmapped = $self->unmapped_object($lrg);
       push(@$results, $unmapped);
     }
+    
     else {
-      (my $lrg_number = $lrg->seq_region_name) =~ s/^LRG_//i; 
+      (my $lrg_number = $lrg_sr_name) =~ s/^LRG_//i; 
       push @$results, {
-        'lrg_name'    => $lrg->seq_region_name,
+        'lrg_name'    => $lrg_sr_name,
         'lrg_number'  => $lrg_number,
         'lrg_start'   => $lrg->start,
         'lrg_end'     => $lrg->end,
         'region'      => $chr->seq_region_name,
         'start'       => $chr->start,
         'end'         => $chr->end,
-        'strand'      => $chr->strand,
-        'length'      => $chr->seq_region_length,
+        'strand'      => $lrg->strand,
+        'length'      => $lrg->seq_region_length,
         'label'       => $chr->name,
+        'hgnc_name'   => $hgnc_name,
       };
     }
   }
