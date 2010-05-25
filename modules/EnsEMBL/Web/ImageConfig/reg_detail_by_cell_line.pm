71db8d19c85a176b9556a20b09e470adcd1caa1e
Beth Yates <bp1@sanger.ac.uk>
2010-05-25 15:33:11 +0000

    new code for multi_wiggle cell line display

---
+++ modules/EnsEMBL/Web/ImageConfig/reg_detail_by_cell_line.pm
@@ -0,0 +1,34 @@
+package EnsEMBL::Web::ImageConfig::reg_detail_by_cell_line;
+
+use strict;
+use warnings;
+no warnings 'uninitialized';
+
+use base qw(EnsEMBL::Web::ImageConfig);
+
+sub init {
+  my $self = shift;
+  
+  $self->set_parameters({
+    title             => 'Feature evidence',
+    show_buttons      => 'no',
+    show_labels       => 'yes',
+    label_width       => 113,
+    opt_lines         => 1,
+    margin            => 5,
+    spacing           => 2,
+  });  
+
+  $self->create_menus(
+    functional     => 'Functional Genomics',
+    other          => 'Decorations',
+  );
+
+  $self->add_tracks('other',
+    [ 'fg_multi_wiggle',          '', 'fg_multi_wiggle',          { display => 'tiling', strand => 'r', menu => 'no', colourset => 'feature_set', height => 120 }],
+);
+
+  $self->load_tracks;
+
+}
+1;
