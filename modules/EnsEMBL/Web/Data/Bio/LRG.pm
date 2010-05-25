14c523d02a541c1d18d078d4353f99a02466dfae
William McLaren <wm2@ebi.ac.uk>
2010-05-25 15:17:21 +0000

    Fixed strand

--- modules/EnsEMBL/Web/Data/Bio/LRG.pm
+++ modules/EnsEMBL/Web/Data/Bio/LRG.pm
@@ -25,6 +25,12 @@ sub convert_to_drawing_parameters {
     my $lrg = $slice_pair->{'lrg'};
     my $chr = $slice_pair->{'chr'};
     
+    # get the strand by projecting to lrg coord system
+    my %strands;    
+    foreach my $segment(@{$chr->project('lrg')}) {
+      $strands{$segment->to_Slice->strand} = 1;
+    }
+    
     # get the LRG's HGNC name
     my $lrg_sr_name = $lrg->seq_region_name;
     my $gene = (grep {$_->stable_id =~ /$lrg_sr_name\_/} @{$lrg->get_all_Genes_by_type('LRG_gene')})[0];
@@ -45,7 +51,7 @@ sub convert_to_drawing_parameters {
         'region'      => $chr->seq_region_name,
         'start'       => $chr->start,
         'end'         => $chr->end,
-        'strand'      => $lrg->strand,
+        'strand'      => (scalar keys %strands > 1 ? "mixed" : (keys %strands)[0]),
         'length'      => $lrg->seq_region_length,
         'label'       => $chr->name,
         'hgnc_name'   => $hgnc_name,
