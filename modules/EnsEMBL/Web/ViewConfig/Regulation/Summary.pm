662610bc29310aaa836cf15825ad476ded8d3f60
Beth Yates <bp1@sanger.ac.uk>
2010-05-25 16:06:01 +0000

    removed config that is no longer required

--- modules/EnsEMBL/Web/ViewConfig/Regulation/Summary.pm
+++ modules/EnsEMBL/Web/ViewConfig/Regulation/Summary.pm
@@ -49,35 +49,6 @@ sub form {
   $view_config->add_form_element({ type => 'YesNo', name => 'opt_focus', select => 'select', label => 'Show Core Evidence track' }) if $reg_object->get_focus_attributes;
 
   $view_config->add_form_element({ type => 'YesNo', name => 'opt_highlight', select => 'select', label => 'Highlight core region' }) if $reg_object->get_focus_attributes;
-
-  # First group displayable feature sets by histone modification 
-  my %sets_by_type;
-  
-  foreach my $set (@{$reg_object->get_nonfocus_attributes}) {
-    my $feature_type = $set->feature_set->feature_type->name . ':' . $set->feature_set->cell_type->name;
-    my $histone_mod = substr $feature_type, 0, 2;
-
-    $histone_mod = 'H1'    if $histone_mod =~ /H\D/;
-    $histone_mod = 'Other' if $histone_mod !~ /H\d/;
-
-    $sets_by_type{$histone_mod} = {} unless exists $sets_by_type{$histone_mod};
-    $sets_by_type{$histone_mod}{$feature_type} = $set; 
-  }
-
-  # Add each feature set to page config according to histone modification
-  foreach (sort keys %sets_by_type) {
-    $view_config->add_fieldset(ucfirst "$_ feature sets");
-    
-    foreach (sort {$a->feature_set->feature_type->name cmp $b->feature_set->feature_type->name} values %{$sets_by_type{$_}}) {
-      $view_config->add_form_element({
-        type  => 'CheckBox', 
-        label => $_->feature_set->feature_type->name . ' (' . $_->feature_set->cell_type->name . ')',
-        name  => 'opt_ft_' . $_->feature_set->feature_type->name . ':' . $_->feature_set->cell_type->name,
-        value => 'on', 
-        raw   => 1
-      });
-    } 
-  }
 }
 1;
 
