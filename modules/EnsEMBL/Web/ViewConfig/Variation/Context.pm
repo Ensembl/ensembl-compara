6b777e4bd5f58dff6bbefd93e832c9754d659c88
Simon Brent <sb23@sanger.ac.uk>
2010-06-07 10:07:04 +0000

    use EnsEMBL::Web::Constants::VARIATION_OPTIONS

--- modules/EnsEMBL/Web/ViewConfig/Variation/Context.pm
+++ modules/EnsEMBL/Web/ViewConfig/Variation/Context.pm
@@ -2,6 +2,8 @@ package EnsEMBL::Web::ViewConfig::Variation::Context;
 
 use strict;
 
+use EnsEMBL::Web::Constants;
+
 sub init {
   my ($view_config) = @_;
 
@@ -11,39 +13,18 @@ sub init {
     panel_locations  on
     panel_individual off
     image_width      900
-    context          30000   
-
-    opt_non_synonymous_coding  on
-    opt_frameshift_coding      on
-    opt_synonymous_coding      on
-    opt_5prime_utr             on
-    opt_3prime_utr             on
-    opt_intronic               on
-    opt_downstream             on
-    opt_upstream               on
-    opt_intergenic             on
-    opt_essential_splice_site  on
-    opt_splice_site            on
-    opt_regulatory_region      on
-    opt_stop_gained            on
-    opt_stop_lost              on
-
-    opt_freq        on
-    opt_cluster     on
-    opt_doublehit   on
-    opt_submitter   on
-    opt_hapmap      on 
-    opt_noinfo      on
-      
-    opt_in-del    on
-    opt_snp       on
-    opt_mixed     on
-    opt_microsat  on
-    opt_named     on
-    opt_mnp       on
-    opt_het       on
-    opt_          on
+    context          30000
   ));
+  
+  my %options = EnsEMBL::Web::Constants::VARIATION_OPTIONS; # Add other options
+
+  foreach (keys %options) {
+    my %hash = %{$options{$_}};
+    
+    foreach my $key (keys %hash){
+      $view_config->_set_defaults(lc($key) => $hash{$key}[0]);
+    }
+  }
   
   $view_config->add_image_configs({ snpview => 'nodas' });
 
--- modules/EnsEMBL/Web/ViewConfig/Variation/LD.pm
+++ modules/EnsEMBL/Web/ViewConfig/Variation/LD.pm
@@ -1,59 +1,38 @@
 package EnsEMBL::Web::ViewConfig::ldview;
 
 use strict;
-no strict 'refs';
 
-use EnsEMBL::Web::ViewConfig;
-our @ISA = qw(EnsEMBL::Web::ViewConfig);
+use EnsEMBL::Web::Constants;
 
 sub init {
-  my ($view_config ) = @_;
+  my ($view_config) = @_;
 
   $view_config->_set_defaults(qw(
     panel_options    on
     panel_image      on
     image_width      800
     context          10000
-
-    opt_freq        on
-    opt_cluster     on
-    opt_doublehit   on
-    opt_submitter   on
-    opt_hapmap      on 
-    opt_noinfo      on
-      
-    opt_in-del    on
-    opt_snp       on
-    opt_mixed     on
-    opt_microsat  on
-    opt_named     on
-    opt_mnp       on
-    opt_het       on
-    opt_          on
-
-    opt_non_synonymous_coding  on
-    opt_frameshift_coding      on
-    opt_synonymous_coding      on
-    opt_5prime_utr             on
-    opt_3prime_utr             on
-    opt_intronic               on
-    opt_downstream             on
-    opt_upstream               on
-    opt_intergenic             on
-    opt_essential_splice_site  on
-    opt_splice_site            on
-    opt_regulatory_region      on
-    opt_stop_gained            on
-    opt_stop_lost              on
-
   ));
+  
+  my %options = EnsEMBL::Web::Constants::VARIATION_OPTIONS; # Add other options
+
+  foreach (keys %options) {
+    my %hash = %{$options{$_}};
+    
+    foreach my $key (keys %hash){
+      $view_config->_set_defaults(lc($key) => $hash{$key}[0]);
+    }
+  }
+  
   $view_config->add_image_configs({qw(
     ldview        nodas
     LD_population nodas
   )});
+  
   $view_config->storable = 1;
 
 }
 
 sub form {}
+
 1;
