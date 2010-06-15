df92c6881c89daa72f6661bf0b305626f871318a
ridwan <ma7@sanger.ac.uk>
2010-06-15 09:58:28 +0000

    replacing core_objects with object from model

--- modules/EnsEMBL/Web/Component/Location/ViewBottomNav.pm
+++ modules/EnsEMBL/Web/Component/Location/ViewBottomNav.pm
@@ -20,10 +20,10 @@ sub content {
   my $self = shift;
   my $ramp_entries = shift || [ [4,1e3], [6,5e3], [8,1e4], [10,5e4], [12,1e5], [14,2e5], [16,5e5], [18,1e6] ];
   
-  my $object = $self->object;
+  my $object = $self->model->object('Location');
   
-  return if $object->core_objects->location->isa('EnsEMBL::Web::Fake');
-  
+  return if $object->isa('EnsEMBL::Web::Fake');
+
   my $image_width      = $self->image_width . 'px';
   my $seq_region_start = $object->seq_region_start;
   my $seq_region_end   = $object->seq_region_end;
