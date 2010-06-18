927f73b66aaaea3b2105030518314342e6d9c0a5
Beth Yates <bp1@sanger.ac.uk>
2010-06-18 15:12:55 +0000

    moved from ViewConfig/ExternalData/Gene.pm - shared across several objects

---
+++ modules/EnsEMBL/Web/ViewConfig/ExternalData.pm
@@ -0,0 +1,41 @@
+package EnsEMBL::Web::ViewConfig::ExternalData;
+
+use strict;
+use warnings;
+use EnsEMBL::Web::RegObj;
+
+use base qw(Exporter);
+
+our @EXPORT_OK = qw(init form);
+our @EXPORT    = @EXPORT_OK;
+
+sub init {
+  my $view_config = shift;
+
+  $view_config->storable   = 1;
+  $view_config->can_upload = 1; # allows configuration of DAS etc
+  $view_config->_set_defaults(map { $_->logic_name => undef } values %{$ENSEMBL_WEB_REGISTRY->get_all_das});
+}
+
+
+sub form {
+  my ($view_config, $object) = @_;
+  
+  $view_config->add_fieldset('DAS sources');
+  my $view =  $object->__objecttype .'/ExternalData';
+ 
+  my @all_das = sort { lc $a->label cmp lc $b->label } grep { $_->is_on($view) } values %{$ENSEMBL_WEB_REGISTRY->get_all_das};
+  
+  for my $das (@all_das) {
+
+    $view_config->add_form_element({
+      'type'  => 'DASCheckBox',
+      'das'   => $das,
+      'name'  => $das->logic_name,
+      'value' => 'yes'
+    });
+  }
+}
+
+
+1;
