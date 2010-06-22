bb5d87d2fc412d79bbaadf81fe83e60d146e0905
Simon Brent <sb23@sanger.ac.uk>
2010-06-22 11:46:09 +0000

    email_URL is unused

--- modules/EnsEMBL/Web/Component/Regulation.pm
+++ modules/EnsEMBL/Web/Component/Regulation.pm
@@ -1,15 +1,7 @@
 package EnsEMBL::Web::Component::Regulation;
 
 use strict;
-use warnings;
-no warnings 'uninitialized';
-
 
 use base qw(EnsEMBL::Web::Component);
-
-sub email_URL {
-  my $email = shift;
-  return qq(&lt;<a href='mailto:$email'>$email</a>&gt;) if $email;
-}
 
 1;
