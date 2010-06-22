3f8b4294b3501df0df572cd505d68cced0b7198e
Simon Brent <sb23@sanger.ac.uk>
2010-06-22 11:42:21 +0000

    moved EC_URL to Component.pm. email_URL is unused

--- modules/EnsEMBL/Web/Component/Gene.pm
+++ modules/EnsEMBL/Web/Component/Gene.pm
@@ -1,21 +1,8 @@
 package EnsEMBL::Web::Component::Gene;
 
 use strict;
-use warnings;
-no warnings 'uninitialized';
 
 use base qw(EnsEMBL::Web::Component);
 
-sub email_URL {
-    my $email = shift;
-    return qq(&lt;<a href='mailto:$email'>$email</a>&gt;) if $email;
-}
-
-sub EC_URL {
-  my( $self,$string ) = @_;
-  my $URL_string= $string;
-  $URL_string=~s/-/\?/g;
-  return $self->object->get_ExtURL_link( "EC $string", 'EC_PATHWAY', $URL_string );
-}
-
 1;
+
