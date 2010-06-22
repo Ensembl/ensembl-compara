b94f76cff65f39b02605a22b5b0dcc0e6e4753d7
Simon Brent <sb23@sanger.ac.uk>
2010-06-22 11:45:56 +0000

    moved EC_URL to Component.pm. tidied

--- modules/EnsEMBL/Web/Component/Transcript.pm
+++ modules/EnsEMBL/Web/Component/Transcript.pm
@@ -1,19 +1,8 @@
 package EnsEMBL::Web::Component::Transcript;
 
-use EnsEMBL::Web::Component;
-use EnsEMBL::Web::Form;
+use strict;
 
 use base qw(EnsEMBL::Web::Component);
-
-
-use strict;
-use warnings;
-no warnings "uninitialized";
-
-use Bio::Perl qw(translate_as_string);
-
-use EnsEMBL::Web::Form;
-use EnsEMBL::Web::Document::SpreadSheet;
 
 ## No sub stable_id   <- uses Gene's stable_id
 ## No sub name        <- uses Gene's name
@@ -22,20 +11,13 @@ use EnsEMBL::Web::Document::SpreadSheet;
 
 sub non_coding_error {
   my $self = shift;
-  return $self->_error( 'No protein product', '<p>This transcript does not have a protein product</p>' );
+  return $self->_error('No protein product', '<p>This transcript does not have a protein product</p>');
 }
 
 sub _flip_URL {
-  my( $transcript, $code ) = @_;
+  my ($transcript, $code) = @_;
   return sprintf '/%s/%s?transcript=%s;db=%s;%s', $transcript->species, $transcript->script, $transcript->stable_id, $transcript->get_db, $code;
 }
 
-
-sub EC_URL {
-  my( $self,$string ) = @_;
-  my $URL_string= $string;
-  $URL_string=~s/-/\?/g;
-  return $self->object->get_ExtURL_link( "EC $string", 'EC_PATHWAY', $URL_string );
-}
-
 1;
+
