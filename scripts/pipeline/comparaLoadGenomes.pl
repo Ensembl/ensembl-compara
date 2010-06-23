92cece5f02de7aac8a11c0c30ebb3acb6e10a669
Kathryn Beal <kbeal@ebi.ac.uk>
2010-06-23 13:14:36 +0000

    Changed _get_name to use the production_name in the meta table only. This MUST be set. If it causes issues, we may need to revert back to using the other methods of setting the genome_db name.

--- scripts/pipeline/comparaLoadGenomes.pl
+++ scripts/pipeline/comparaLoadGenomes.pl
@@ -20,7 +20,7 @@ my %compara_conf = ();
 #$compara_conf{'-user'} = 'ensadmin';
 $compara_conf{'-port'} = 3306;
 
-my ($help, $host, $user, $pass, $dbname, $port, $compara_conf, $adaptor, $ensembl_genomes);
+my ($help, $host, $user, $pass, $dbname, $port, $compara_conf, $adaptor);
 my ($subset_id, $genome_db_id, $prefix, $fastadir, $verbose);
 
 GetOptions('help'            => \$help,
@@ -30,7 +30,6 @@ GetOptions('help'            => \$help,
            'dbuser=s'        => \$user,
            'dbpass=s'        => \$pass,
            'dbname=s'        => \$dbname,
-           'ensembl_genomes' => \$ensembl_genomes,
            'v' => \$verbose,
           );
 
@@ -105,8 +104,7 @@ sub usage {
   print "  -dbname <name>         : compara mysql database <name>\n";
   print "  -dbuser <name>         : compara mysql connection user <name>\n";
   print "  -dbpass <pass>         : compara mysql connection password\n";
-	print "  -ensembl_genomes       : use ensembl genomes specific code\n";
-  print "comparaLoadGenomes.pl v1.2\n";
+  print "comparaLoadGenomes.pl v1.3\n";
 
   exit(1);
 }
@@ -168,7 +166,6 @@ sub submitGenome
   }
   $locator .= ";disconnect_when_inactive=1";
   print("    locator = $locator\n")  if($verbose);
-
   eval {
     $genomeDBA = Bio::EnsEMBL::DBLoader->new($locator);
   };
@@ -180,22 +177,6 @@ sub submitGenome
 
   my $meta = $genomeDBA->get_MetaContainer;
   my $taxon_id = $meta->get_taxonomy_id;
-
-#   # If we are in E_G then we need to look for a taxon in meta by 'NAME.species.taxonomy_id'
-#   if($ensembl_genomes) {
-#     if(!defined $taxon_id or $taxon_id == 1) {
-#       # We make the same call as in the MetaContainer code, but with the NAME appendage
-#       my $key = $species->{eg_name}.'.'.'species.taxonomy_id';
-#       my $arrRef = $meta->list_value_by_key($key);
-#       if( @$arrRef ) {
-#         $taxon_id = $arrRef->[0];
-#         print "Found taxonid ${taxon_id}\n" if $verbose;
-#       }
-#       else {
-#         warning("Please insert meta_key '${key}' in meta table at core db.\n");
-#       }
-#     }
-#   }
 
   my $ncbi_taxon = $self->{'comparaDBA'}->get_NCBITaxonAdaptor->fetch_node_by_taxon_id($taxon_id);
   my $genome_name = $self->_get_name($species, $meta, $ncbi_taxon);
@@ -327,43 +308,15 @@ sub submitUniprot
 }
 
 sub _get_name {
-	my ($self, $species, $meta, $ncbi_taxon) = @_;
-
-	my $genome_name;
-
-	#If we have both taxon & species then check rules & assign
-	if(defined $ncbi_taxon && defined $species->{species}) {
-		print "Taxon & Species given; checking for equality\n" if $verbose;
-		if($ncbi_taxon->binomial() ne $species->{species}) {
-			if($ensembl_genomes) {
-				print "Taxon bionmial & species name differ; using species name as we are in ensembl_genomes mode\n" if $verbose;
-				$genome_name = $species->{species};
-			}
-			else {
-				print "Taxon bionmial & species name differ; using taxon binomial as we are in compara mode\n" if $verbose;
-				$genome_name = $ncbi_taxon->binomial();
-			}
-		}
-		else {
-			print "Taxon binomial & species agree with name\n";
-			$genome_name = $ncbi_taxon->binomial();
-		}
-  }
-  #If just had taxon then taxon wins
-  elsif(defined $ncbi_taxon) {
-  	print "Taxon found but no species name given; using taxon binomal\n" if $verbose;
-		$genome_name = $ncbi_taxon->binomial();
-  }
-  #Otherwise best guess from meta
-  else {
-  	$verbose && print"  Cannot get binomial from NCBITaxon, try Meta...\n";
-  	if (defined($species->{species})) {
-      $genome_name = $species->{species};
+    my ($self, $species, $meta, $ncbi_taxon) = @_;
+    
+    #Use meta->get_production_name if it exists and fail if it doesn't.
+    #If this causes issues, we may need to revert back to using the other 
+    #methods of setting the genome_db name.
+    if (defined $meta->get_production_name) {
+	print "Using meta production_name \n" if $verbose;
+	return $meta->get_production_name;
+    } else {
+	throw("The production_name entry in the " . $species->{dbname} . " meta table has not been set. This needs to be added.");
     }
-    else {
-    	$genome_name = (defined $meta->get_Species()) ? $meta->get_Species()->binomial() : $species->{species};
-    }
-  }
-
-  return $genome_name;
 }
