b5692f3e0efcd038db93204ba6b9c9a6a31e2fc3
Kathryn Beal <kbeal@ebi.ac.uk>
2010-07-01 15:41:53 +0000

    Added dbname to arguments, changed ->fetch_by_tag_value to species_set_adaptor->fetch_all_by_tag_value and sorted to find the largest value. Converted species names to new schema.

--- scripts/pipeline/update_species_sets.pl
+++ scripts/pipeline/update_species_sets.pl
@@ -12,11 +12,13 @@ my $help;
 my $registry_file;
 my @url;
 my $compara_url;
+my $dbname = "Multi";
 
 GetOptions(
   "help" => \$help,
   "url=s" => \@url,
   "master_url|compara_url=s" => \$compara_url,
+  "dbname=s" => \$dbname,
   "conf|registry=s" => \$registry_file,
 );
 
@@ -36,7 +38,7 @@ if ($compara_url) {
   use Bio::EnsEMBL::Compara::DBSQL::DBAdaptor;
   $compara_dba = Bio::EnsEMBL::Compara::DBSQL::DBAdaptor->new(-url=>$compara_url);
 } else {
-  $compara_dba = $reg->get_DBAdaptor("Multi", "compara");
+  $compara_dba = $reg->get_DBAdaptor($dbname, "compara");
 }
 
 my $species_set_adaptor = $compara_dba->get_adaptor("SpeciesSet");
@@ -75,15 +77,16 @@ foreach my $this_species_set (@$species_sets_with_taxon_id) {
   }
 }
 
-my $low_coverage_species_set = $species_set_adaptor->fetch_by_tag_value("name", "low-coverage");
+my $low_coverage_species_set = $species_set_adaptor->fetch_all_by_tag_value("name", "low-coverage");
 if ($low_coverage_species_set) {
-  my $tag_value_hash = $low_coverage_species_set->get_tagvalue_hash();
+  @$low_coverage_species_set = sort {$b->dbID <=> $a->dbID } @$low_coverage_species_set;
+  my $tag_value_hash = $low_coverage_species_set->[0]->get_tagvalue_hash();
   my $all_genome_dbs = $genome_db_adaptor->fetch_all();
   my $low_coverage_genome_dbs = [];
   foreach my $this_genome_db (@$all_genome_dbs) {
     next if (!$this_genome_db->assembly_default);
-    next if ($this_genome_db->name eq "Ancestral sequences");
-    next if ($this_genome_db->name eq "Caenorhabditis elegans");
+    next if ($this_genome_db->name eq "ancestral_sequences");
+    next if ($this_genome_db->name eq "caenorhabditis_elegans");
     my $db_adaptor = $this_genome_db->db_adaptor;
     if (!$db_adaptor) {
       throw("Cannot connect to ".$this_genome_db->name." core DB\n");
