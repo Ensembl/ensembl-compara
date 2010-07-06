c9e870346ea2c4163cb4cfd06d2471dd95645ec2
Simon Brent <sb23@sanger.ac.uk>
2010-07-06 13:07:30 +0000

    added a zmenu url if there's an external url attatched to the feature. tidied.

--- modules/Bio/EnsEMBL/GlyphSet/_simple.pm
+++ modules/Bio/EnsEMBL/GlyphSet/_simple.pm
@@ -4,46 +4,47 @@ use strict;
 
 use base qw(Bio::EnsEMBL::GlyphSet_simple);
 
-sub _das_type {  return 'simple'; }
-
-sub features       { 
-  my $self = shift;
-  my $call = 'get_all_'.( $self->my_config( 'type' ) || 'SimpleFeatures' ); 
-  my @F = map { @{$self->{'container'}->$call( $_ )||[]} }
-          @{$self->my_config( 'logicnames' )||[]};
-  return \@F;
+sub features  { 
+  my $self     = shift;
+  my $call     = 'get_all_' . ($self->my_config('type') || 'SimpleFeatures'); 
+  my @features = map @{$self->{'container'}->$call($_)||[]}, @{$self->my_config('logicnames')||[]};
+  
+  return \@features;
 }
 
-sub colour_key {
-  my( $self, $f ) = @_;
-  return lc($f->analysis->logic_name);
-}
-
-sub feature_label {
-  return undef;
-}
+sub colour_key    { return lc $_[1]->analysis->logic_name; }
+sub feature_label { return undef; }
+sub tag           { return undef; }
+sub _das_type     { return 'simple'; }
 
 sub title {
-  my( $self, $f ) = @_;
-  my($start,$end) = $self->slice2sr( $f->start(), $f->end() );
-  return $f->analysis->logic_name.': '.$f->display_label.'; score: '.$f->score. "; bp: $start-$end";
+  my ($self, $f)    = @_;
+  my ($start, $end) = $self->slice2sr($f->start, $f->end);
+  return sprintf '%s: %s; score: %s; bp: %s', $f->analysis->logic_name, $f->display_label, $f->score, "$start-$end";
 }
 
 sub href {
-  my ($self, $f ) = @_;
-  return undef;
-}
-
-sub tag {
-  my ($self, $f ) = @_;
-  return; 
+  my ($self, $f) = @_;
+  my $ext_url = $self->my_config('ext_url');
+  
+  return undef unless $ext_url;
+  
+  my ($start, $end) = $self->slice2sr($f->start, $f->end);
+  
+  return $self->_url({
+    action        => 'SimpleFeature',
+    logic_name    => $f->analysis->logic_name,
+    display_label => $f->display_label,
+    score         => $f->score,
+    bp            => "$start-$end",
+    ext_url       => $ext_url
+  }); 
 }
 
 sub export_feature {
-  my $self = shift;
-  my ($feature, $feature_type) = @_;
+  my ($self, $feature, $feature_type) = @_;
   
-  my @label = $feature->can('display_label') ? split (/\s*=\s*/, $feature->display_label) : ();
+  my @label = $feature->can('display_label') ? split /\s*=\s*/, $feature->display_label : ();
   
   return $self->_render_text($feature, $feature_type, { 'headers' => [ $label[0] ], 'values' => [ $label[1] ] });
 }
