74813b282c754108bce643d1b3211f025c9ff322
Simon Brent <sb23@sanger.ac.uk>
2010-07-13 09:18:07 +0000

    remove short_caption - unnecessary now that it's on Configuration.pm

--- modules/EnsEMBL/Web/Configuration/Marker.pm
+++ modules/EnsEMBL/Web/Configuration/Marker.pm
@@ -5,23 +5,21 @@ use base qw( EnsEMBL::Web::Configuration );
 
 sub set_default_action {
   my $self = shift;
-  $self->{_data}{default} = 'Details';
+  $self->{'_data'}{'default'} = 'Details';
 }
 
 sub global_context { return $_[0]->_global_context }
-sub ajax_content   { return $_[0]->_ajax_content;   }
-sub local_context  { return $_[0]->_local_context;  }
-sub local_tools    { return $_[0]->_local_tools;  }
-sub content_panel  { return $_[0]->_content_panel;  }
-sub context_panel  { return undef;  } 
+sub ajax_content   { return $_[0]->_ajax_content;  }
+sub local_context  { return $_[0]->_local_context; }
+sub local_tools    { return $_[0]->_local_tools;   }
+sub content_panel  { return $_[0]->_content_panel; }
+sub context_panel  { return undef;                 }
 
 sub caption { 
   my $self = shift;
   my $marker = $self->model->hub->param('m');
   return "Marker $marker"; 
 }
-
-sub short_caption { return 'Marker-based displays'; }
 
 sub availability {
   my $self = shift;
@@ -31,9 +29,9 @@ sub availability {
 sub populate_tree {
   my $self  = shift;
 
-  $self->create_node( 'Details', "Details",
-    [qw(details    EnsEMBL::Web::Component::Marker::Details)],
-    { 'availability' => 1}
+  $self->create_node('Details', 'Details',
+    [qw(details EnsEMBL::Web::Component::Marker::Details)],
+    { 'availability' => 1 }
   );
 }
 
