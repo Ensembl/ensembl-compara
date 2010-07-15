825fb9aa9b9cac85a4dd491abe4f81728cc3d36c
Anne Parker <ap5@sanger.ac.uk>
2010-07-15 14:48:05 +0000

    New modules for displaying the changelog from the production database

---
+++ modules/EnsEMBL/Web/DBSQL/ProductionAdaptor.pm
@@ -0,0 +1,135 @@
+package EnsEMBL::Web::DBSQL::ProductionAdaptor;
+
+### A simple adaptor to fetch the changelog from the ensembl_production database
+### For full CRUD functionality, see public-plugins/orm, which uses the Rose::DB::Object
+### ORM framework
+
+use strict;
+use warnings;
+no warnings 'uninitialized';
+
+use DBI;
+
+sub new {
+  my ($class, $hub) = @_;
+
+    my $self = {
+    'NAME' => $hub->species_defs->multidb->{'DATABASE_PRODUCTION'}{'NAME'},
+    'HOST' => $hub->species_defs->multidb->{'DATABASE_PRODUCTION'}{'HOST'},
+    'PORT' => $hub->species_defs->multidb->{'DATABASE_PRODUCTION'}{'PORT'},
+    'USER' => $hub->species_defs->multidb->{'DATABASE_PRODUCTION'}{'USER'},
+    'PASS' => $hub->species_defs->multidb->{'DATABASE_PRODUCTION'}{'PASS'},
+  };
+  bless $self, $class;
+  return $self;
+}
+
+sub db {
+  my $self = shift;
+  return unless $self->{'NAME'};
+  $self->{'dbh'} ||= DBI->connect(
+      "DBI:mysql:database=$self->{'NAME'};host=$self->{'HOST'};port=$self->{'PORT'}",
+      $self->{'USER'}, "$self->{'PASS'}"
+  );
+  return $self->{'dbh'};
+}
+
+sub fetch_changelog {
+### Selects all changes for a given release and returns them as an arrayref of hashes
+  my ($self, $criteria) = @_;
+  my $changes = [];
+  return [] unless $self->db;
+
+  my ($sql, $sql2);
+  my @args = ($criteria->{'release'});
+
+  if ($criteria->{'species'}) {
+    $sql = qq(
+      SELECT
+        c.changelog_id, c.title, c.content, c.team, c.status 
+      FROM
+        changelog as c, species as s, changelog_species as cs 
+      WHERE 
+        c.changelog_id = cs.changelog_id
+        AND s.species_id = cs.species_id
+        AND c.title != ''
+        AND c.content != ''
+        AND c.release_id = ?
+        AND s.name = ?
+      ORDER BY
+        c.team, c.status
+    );
+    push @args, $criteria->{'species'};
+  }
+  else {
+    $sql = qq(
+      SELECT
+        c.changelog_id, c.title, c.content, c.team, c.status 
+      FROM
+        changelog as c
+      WHERE 
+        c.release_id = ?
+        AND c.title != ''
+        AND c.content != ''
+      ORDER BY
+        c.team, c.status
+    );
+  }
+
+  my $sth = $self->db->prepare($sql);
+  $sth->execute(@args);
+
+  ## Prepare species SQL
+  if ($criteria->{'species'}) {
+    $sql2 = qq(
+      SELECT
+        species_id, db_name, web_name
+      FROM
+        species
+      WHERE
+        s.db_name = ?
+    );
+  }
+  else {
+    $sql2 = qq(
+      SELECT
+        s.species_id, s.db_name, s.web_name
+      FROM
+        species as s, changelog_species as cs
+      WHERE
+        s.species_id = cs.species_id
+        AND cs.changelog_id = ?
+    );
+  }
+  my $sth2 = $self->db->prepare($sql2);
+
+  while (my @data = $sth->fetchrow_array()) {
+    
+    ## get the species info for this record
+    my $species = [];
+    my $arg2 = $criteria->{'species'} || $data[0];
+    $sth2->execute($arg2);
+    while (my @sp = $sth2->fetchrow_array()) {
+      push @$species, {
+        'id'          => $sp[0],
+        'name'        => $sp[1],
+        'common_name' => $sp[2],
+      };
+    }
+
+    my $record = {
+      'id'            => $data[0],
+      'title'         => $data[1],
+      'content'       => $data[2],
+      'team'          => $data[3],
+      'status'        => $data[4],
+      'species'       => $species,
+    };
+    push @$changes, $record;
+  }
+
+  return $changes;
+}
+
+
+1;
---
+++ modules/EnsEMBL/Web/Document/HTML/Changelog.pm
@@ -0,0 +1,85 @@
+package EnsEMBL::Web::Document::HTML::Changelog;
+
+### This module outputs news for previous Ensembl releases 
+
+use strict;
+use warnings;
+no warnings 'uninitialized';
+
+use EnsEMBL::Web::Hub;
+use EnsEMBL::Web::DBSQL::ProductionAdaptor;
+
+use base qw(EnsEMBL::Web::Document::HTML);
+
+sub render {
+  my $self = shift;
+  my $html;
+
+  my $hub = new EnsEMBL::Web::Hub;
+  my $release_id = $hub->param('id') || $hub->species_defs->ENSEMBL_VERSION;
+
+  if ($release_id < 59) {
+    $html .= '<p>Prior to release 59, changelogs were merged with news. Please see the <a href="/info/website/news/archive.html">news archive</a> for equivalent content.</p>';
+  }
+  else { 
+    $html .= qq(<h1>Release $release_id Changelog</h1>);
+  
+    ## get news changes
+    my $adaptor = EnsEMBL::Web::DBSQL::ProductionAdaptor->new($hub);
+    my @changes;
+
+    if ($adaptor) {
+      @changes = @{$adaptor->fetch_changelog({'release' => $release_id})};
+    }
+ 
+    if (scalar(@changes) > 0) {
+
+      my $prev_team = undef;
+      ## format news changes
+      foreach my $record (@changes) {
+
+        ## is it a new category?
+        if ($prev_team ne $record->{'team'}) {
+          $html .= "<h2>".$record->{'team'}."</h2>\n";
+        }
+
+        $prev_team = $record->{'team'};
+        $html .= '<h3 id="change_'.$record->{'id'}.'">'.$record->{'title'};
+        my @species = @{$record->{'species'}}; 
+        my $sp_text;
+  
+        if (!@species || !$species[0]) {
+          $sp_text = 'all species';
+        }
+        elsif (@species > 5) {
+          $sp_text = 'multiple species';
+        }
+        else {
+          my @names;
+          foreach my $sp (@species) {
+            if ($sp->{'common_name'} =~ /\./) {
+              push @names, '<i>'.$sp->{'common_name'}.'</i>';
+            }
+            else {
+              push @names, $sp->{'common_name'};
+            } 
+          }
+          $sp_text = join(', ', @names);
+        }
+        $html .= " ($sp_text)</h3>\n";
+        my $content = $record->{'content'};
+        #if ($content !~ /^</) { ## wrap bare content in a <p> tag
+        #  $content = "<p>$content</p>";
+        #}
+        $html .= $content."\n\n";
+      }
+    }
+    else {
+      $html .= qq(<p>No changelog is currently available for release $release_id.</p>\n);
+    }
+  }
+
+  return $html;
+}
+
+1;
