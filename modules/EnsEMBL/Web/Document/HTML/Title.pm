53b07b98e4b573c6fec58afbafddb35d5f1cf947
Simon Brent <sb23@sanger.ac.uk>
2010-07-20 15:35:24 +0000

    encode

--- modules/EnsEMBL/Web/Document/HTML/Title.pm
+++ modules/EnsEMBL/Web/Document/HTML/Title.pm
@@ -2,16 +2,19 @@ package EnsEMBL::Web::Document::HTML::Title;
 
 use strict;
 
+use HTML::Entities qw(encode_entities);
+
 use base qw(EnsEMBL::Web::Document::HTML);
 
-sub new    { return shift->SUPER::new( 'title' => 'Untitled document' ); }
-sub set    { $_[0]{'title'} = $_[1]; }
-sub get    { return $_[0]{'title'}; }
+sub new { return shift->SUPER::new( 'title' => 'Untitled document' ); }
+sub set { $_[0]{'title'} = $_[1]; }
+sub get { return $_[0]{'title'}; }
 
 sub render {
   my $self  = shift;
   my $title = $self->get;
   $title =~ s/<[^>]+>//g;
+  $title = encode_entities($title);
   $self->print("\n<title>$title</title>");
 }
 1;
