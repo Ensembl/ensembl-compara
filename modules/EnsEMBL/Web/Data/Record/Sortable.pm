98e30504a1866904a15c1f4b355add27fb8874fa
Anne Parker <ap5@sanger.ac.uk>
2010-07-21 14:05:29 +0000

    Added comment to effect that this module is not longer used (though it is still referred to in some user code)

--- modules/EnsEMBL/Web/Data/Record/Sortable.pm
+++ modules/EnsEMBL/Web/Data/Record/Sortable.pm
@@ -1,5 +1,7 @@
 package EnsEMBL::Web::Data::Record::Sortable;
 
+## no longer in use - part of old slide-down user account panel
+
 use strict;
 use warnings;
 use base qw(EnsEMBL::Web::Data::Record);
