59b1adc5887ee81ab9d26f121126b7fd3e6b3914
Simon Brent <sb23@sanger.ac.uk>
2010-07-26 15:41:58 +0000

    tidied, fixed for compara database changes

--- modules/EnsEMBL/Web/Component/Location/ChangeSpecies.pm
+++ modules/EnsEMBL/Web/Component/Location/ChangeSpecies.pm
@@ -4,48 +4,47 @@ package EnsEMBL::Web::Component::Location::ChangeSpecies;
 ### the lefthand menu dropdown of syntenous species
 
 use strict;
-use warnings;
-no warnings "uninitialized";
+
+use EnsEMBL::Web::Form;
+
 use base qw(EnsEMBL::Web::Component::Location);
+
 sub _init {
   my $self = shift;
-  $self->cacheable( 0 );
-  $self->ajaxable(  1 );
+  $self->cacheable(0);
+  $self->ajaxable(1);
 }
 
 sub content {
-  my $self = shift;
+  my $self   = shift;
   my $object = $self->object;
+  my $url    = $object->_url({ otherspecies => undef }, 1);
+  my $form   = new EnsEMBL::Web::Form('change_sp', $url->[0], 'get', 'nonstd check');
+  
+  $form->add_hidden($url->[1]);
 
-  my $url = $object->_url({'otherspecies'=>undef},1);
-#use Data::Dumper;warn Dumper($url);
-  my $form = EnsEMBL::Web::Form->new( 'change_sp', $url->[0], 'get', 'nonstd check' );
-  $form->add_hidden( $url->[1] );
-
-  my %synteny_hash = $object->species_defs->multi('DATABASE_COMPARA', 'SYNTENY');
-  my %synteny      = %{$synteny_hash{ $object->species || {} }};
-  my @species = keys %synteny;
-  my @sorted_by_common = sort { $a->{'common'} cmp $b->{'common'} }
-        map  { { 'name'=> $_, 'common' => $object->species_defs->get_config($_, "SPECIES_COMMON_NAME")} }
-                          @species;
+  my %synteny_hash     = $object->species_defs->multi('DATABASE_COMPARA', 'SYNTENY');
+  my %synteny          = %{$synteny_hash{$object->species} || {}};
+  my @sorted_by_common = sort { $a->{'common'} cmp $b->{'common'} } map {{ name => $_, common => $object->species_defs->get_config($_, 'SPECIES_COMMON_NAME') }} keys %synteny;
   my @values;
+  
   foreach my $next (@sorted_by_common) {
     next if $next->{'name'} eq $object->species;
-    push @values, {'name'=>$next->{'common'}, 'value'=>$next->{'name'}} ;
+    push @values, { name => $next->{'common'}, value => $next->{'name'} };
   }
 
   $form->add_element(
-    'type'     => 'DropDownAndSubmit',
-    'select'   => 'select',
-    'style'    => 'narrow',
-    'on_change' => 'submit',
-    'name'     => 'otherspecies',
-    'label'    => 'Change Species',
-    'values'   => \@values,
-    'value'   => $object->param('otherspecies') || $self->default_otherspecies,
-    'button_value' => 'Go'
+    type         => 'DropDownAndSubmit',
+    select       => 'select',
+    style        => 'narrow',
+    name         => 'otherspecies',
+    label        => 'Change Species',
+    values       => \@values,
+    value        => $object->param('otherspecies') || $object->param('species') || $self->default_otherspecies,
+    button_value => 'Go'
   );
-  return '<div class="center">'.$form->render.'</div>';
+  
+  return '<div class="center">' . $form->render . '</div>';
 }
 
 1;
--- modules/EnsEMBL/Web/Component/Location/SyntenyImage.pm
+++ modules/EnsEMBL/Web/Component/Location/SyntenyImage.pm
@@ -4,72 +4,69 @@ package EnsEMBL::Web::Component::Location::SyntenyImage;
 ### an image of the syntenous chromosome regions 
 
 use strict;
-use warnings;
-no warnings "uninitialized";
+
 use base qw(EnsEMBL::Web::Component::Location);
-use Data::Dumper;
 
 sub _init {
   my $self = shift;
-  $self->cacheable( 1 );
-  $self->ajaxable(  1 );
+  $self->cacheable(1);
+  $self->ajaxable(1);
 }
 
 sub content {
-  my $self = shift;
-  my $object = $self->object;
+  my $self    = shift;
+  my $object  = $self->object;
   my $species = $object->species;
-    
   my %synteny = $object->species_defs->multi('DATABASE_COMPARA', 'SYNTENY');
-  my $other = $object->param('otherspecies') || $object->param('species') || $self->default_otherspecies;
- 
-  my $chr = $object->seq_region_name;
-  my %chr_1  =  map { ($_,1) } @{$object->species_defs->ENSEMBL_CHROMOSOMES||[]};
-  my $chr_2 = scalar  @{$object->species_defs->get_config( $other , 'ENSEMBL_CHROMOSOMES' ) };
-        
-  unless ($synteny{ $other }){
-    $object->problem('fatal', "Can't display synteny",  "There is no synteny data for these two species ($species and $other)") ;
+  my $other   = $object->param('otherspecies') || $object->param('species') || $self->default_otherspecies;
+  my $chr     = $object->seq_region_name;
+  my %chr_1   = map { $_, 1 } @{$object->species_defs->ENSEMBL_CHROMOSOMES || []};
+  my $chr_2   = scalar @{$object->species_defs->get_config($other, 'ENSEMBL_CHROMOSOMES')};
+  
+  unless ($synteny{$other}) {
+    $object->problem('fatal', "Can't display synteny",  "There is no synteny data for these two species ($species and $other)");
     return undef;
   }
-  unless ( $chr_1{$chr} && $chr_2>0){
-    $object->problem( 'fatal', "Unable to display", "Synteny view only displays synteny between real chromosomes - not fragments") ;
+  
+  unless ($chr_1{$chr} && $chr_2 > 0) {
+    $object->problem('fatal', "Unable to display", "Synteny view only displays synteny between real chromosomes - not fragments");
     return undef;
   }
 
-  my $ka  = $object->get_adaptor('get_KaryotypeBandAdaptor', 'core', $species);
-  my $ka2 = $object->get_adaptor('get_KaryotypeBandAdaptor', 'core', $other);
+  my $ka         = $object->get_adaptor('get_KaryotypeBandAdaptor', 'core', $species);
+  my $ka2        = $object->get_adaptor('get_KaryotypeBandAdaptor', 'core', $other);
   my $compara_db = $object->database('compara');
-  my $raw_data = $object->chromosome->get_all_compara_Syntenies($other, undef, $compara_db);   
-
-  ## checks done ## 
+  my $raw_data   = $object->chromosome->get_all_compara_Syntenies($other, undef, $compara_db);   
   my $chr_length = $object->chromosome->length;
-  my ($localgenes,$offset) = $object->get_synteny_local_genes;
-  my $loc = ( @$localgenes ? $localgenes->[0]->start + $object->seq_region_start : 1 ); # Jump loc to the location of the genes
-        
-  my $Config = $object->get_imageconfig( 'Vsynteny' );
-  $Config->{'other_species_installed'} = $synteny{ $other };
-  $Config->container_width( $chr_length );
+  
+  my ($localgenes, $offset) = $object->get_synteny_local_genes;
+  my $loc = (@$localgenes ? $localgenes->[0]->start + $object->seq_region_start : 1); # Jump loc to the location of the genes
+  
+  my $image_config = $object->get_imageconfig('Vsynteny');
+  
+  $image_config->{'other_species_installed'} = $synteny{$other};
+  $image_config->container_width($chr_length);
 
-  my $image = $self->new_vimage(
-    {   'chr'           => $chr,
-        'ka_main'       => $ka,
-        'sa_main'       => $object->get_adaptor('get_SliceAdaptor'),
-        'ka_secondary'  => $ka2,
-        'sa_secondary'  => $object->get_adaptor('get_SliceAdaptor', 'core', $other),
-        'synteny'       => $raw_data,
-        'other_species' => $other,
-        'line'          => $loc
-    }, 
-    $Config
-  );
-  $image->imagemap           = 'yes';
-  $image->image_type         = 'syntenyview';
-  $image->image_name         = $species.'-'.$chr.'-'.$other;
-  $image->set_button( 'drag', 'title' => 'Click or drag to change region' );
+  my $image = $self->new_vimage({
+    chr           => $chr,
+    ka_main       => $ka,
+    sa_main       => $object->get_adaptor('get_SliceAdaptor'),
+    ka_secondary  => $ka2,
+    sa_secondary  => $object->get_adaptor('get_SliceAdaptor', 'core', $other),
+    synteny       => $raw_data,
+    other_species => $other,
+    line          => $loc
+  }, $image_config);
+  
+  $image->imagemap   = 'yes';
+  $image->image_type = 'syntenyview';
+  $image->image_name = "$species-$chr-$other";
+  $image->set_button('drag', 'title' => 'Click or drag to change region');
 
   foreach my $o (@$raw_data) { ## prevents memory leak!
     $o->release_tree;
   }
+  
   return $image->render;
 }
 
