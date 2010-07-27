8a3c4ab4561e12be12c53817f573237cfa28470d
Simon Brent <sb23@sanger.ac.uk>
2010-07-27 10:01:20 +0000

    not using breadcrumbs on dynamic pages, so removed that section

--- modules/EnsEMBL/Web/Document/HTML/BreadCrumbs.pm
+++ modules/EnsEMBL/Web/Document/HTML/BreadCrumbs.pm
@@ -9,7 +9,7 @@ use base qw(EnsEMBL::Web::Document::HTML);
 
 sub new {
   my $class = shift;
-  my $self = $class->SUPER::new('title' => undef );
+  my $self  = $class->SUPER::new('title' => undef );
   return $self;
 }
 
@@ -19,55 +19,25 @@ sub title {
   return $self->{'title'};
 }
 
-sub render   {
+sub render {
   my $self = shift;
-  my $species_defs = $self->species_defs;
-  my $you_are_here = $ENV{'SCRIPT_NAME'};
-  my $html = '<span class="print_hide">';
-  my $species = $ENV{'ENSEMBL_SPECIES'};
-  my $species_path = $species_defs->species_path;
+  my $path = $ENV{'SCRIPT_NAME'};
+  my $html = $path eq '/index.html' ? 'Home' : '<a href="/">Home</a>';
 
-  # Link to home page
-  if ($you_are_here eq '/index.html') {
-    $html .= '<strong>Home</strong>';
-  } else {
-    $html .= '<a href="/">Home</a>';
-  }
-  
-  $html .= '</span>';
-
-  if ($species && $species !~ /multi/i) {
-    $html .= '<span class="print_hide"> &gt; </span>';
-    if ($species eq 'common') {
-      $html .= '<strong>Control Panel</strong>';
-    } else {
-      $html .= '<a href="/' . $species_defs->GROUP_URL . '/">' . $species_defs->DISPLAY_NAME . '</a> &gt; ' if $species_defs->SPP_ARE_GROUPED;
-      
-      my $species_name = $species_defs->get_config($species, 'SPECIES_COMMON_NAME');
-      $species_name = '<i>' . $species_name . '</i>' if $species_name =~ /\./;
-      
-      if ($ENV{'ENSEMBL_TYPE'} eq 'Info') {
-        $html .= "<strong>$species_name</strong>";
-      } else {
-        $html .= qq{<a href="$species_path/Info/Index">$species_name</a>};
-      }
-      
-      $html .= ' <span style="font-size:75%">[' . $species_defs->ASSEMBLY_DISPLAY_NAME . ']</span>' if $species_defs->ASSEMBLY_DISPLAY_NAME;
-    }
-  } elsif ($you_are_here =~ m#^/info/#) {
-    $html .= '<span class="print_hide"> &gt; </span>';
+  if ($path =~ /^\/info\//) {
+    $html .= ' &gt; ';
     
     # Level 2 link
-    if ($you_are_here eq '/info/' || $you_are_here eq '/info/index.html') {
-      $html .= '<strong>Docs &amp; FAQs</strong>';
+    if ($path eq '/info/' || $path eq '/info/index.html') {
+      $html .= 'Docs &amp; FAQs';
     } else {
-      $html .= '<strong><a href="/info/">Docs &amp; FAQs</a></strong>';
+      $html .= '<a href="/info/">Docs &amp; FAQs</a>';
     }
     
-    $html .= '<span class="print_hide"> &gt; </span>' . $self->title if $self->title;
+    $html .= ' &gt; ' . $self->title if $self->title;
   }
   
-  $self->printf($html);
+  $self->print(qq{<div class="breadcrumbs print_hide">$html</div>});
 }
 
 1;
