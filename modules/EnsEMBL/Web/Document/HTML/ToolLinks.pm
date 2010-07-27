1c0643133a78e2a1875bd875c1bb6a805dcc4810
Anne Parker <ap5@sanger.ac.uk>
2010-07-27 13:15:22 +0000

    Fixed order of links

--- modules/EnsEMBL/Web/Document/HTML/ToolLinks.pm
+++ modules/EnsEMBL/Web/Document/HTML/ToolLinks.pm
@@ -36,8 +36,8 @@ sub render {
   
   push @links, qq{<a href="/$blast_dir/blastview">BLAST/BLAT</a>} if $self->blast;
   push @links, qq{<a href="/biomart/martview">BioMart</a>}        if $self->biomart;
-  push @links, qq{<a href="/downloads.html">Downloads</a>};
   push @links, qq{<a href="/tools.html">Tools</a>};
+  push @links, qq{<a href="/downloads.html">Downloads</a>};
   push @links, qq{<a href="/help.html">Help</a>};
   push @links, qq{<a href="/info/docs/">Documentation</a>};
 
