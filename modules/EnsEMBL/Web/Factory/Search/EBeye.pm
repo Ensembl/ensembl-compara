0badc3eae2dda4c678fdc47858a3db8962cca377
Eugene Kulesha <ek@ebi.ac.uk>
2010-08-05 15:54:49 +0000

    module to do EBEy search

---
+++ modules/EnsEMBL/Web/Factory/Search/EBeye.pm
@@ -0,0 +1,37 @@
+package EnsEMBL::Web::Factory::Search::EBeye;
+
+use strict;
+
+use EnsEMBL::Web::Factory;
+use EBeyeSearch;
+use CGI;
+use Data::Dumper;
+use Carp qw(cluck);
+
+our @ISA = qw(EnsEMBL::Web::Factory);
+
+sub generic_search {
+    my $self = shift;
+}
+
+sub _search_all {
+    my $self = shift;
+    my $idx = shift;
+    my $species = shift;
+     my $ebeye = new EBeyeSearch;
+
+     ( my $SPECIES   = $ENV{'ENSEMBL_SPECIES'} ) =~ s/_/ /g;
+     my $q = CGI->new();
+     if (@$species) {
+ 	$q->param('species',@$species);
+     }
+
+    eval {$ebeye->parse( $q );}; if ($@){ $self->problem( 'Fatal', 'Search Engine Error', "There is a problem with the Search engine." )  ; return undef;};
+
+
+    return $self->new_object( 'Search', $ebeye, $self->__data );
+
+}
+
+
+1;
