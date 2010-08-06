ea3db7b26a716b30b742535cc1a37ee70912110f
Simon Brent <sb23@sanger.ac.uk>
2010-08-06 15:27:28 +0000

    fix tree when memcache is in use

--- modules/EnsEMBL/Web/Configuration/Variation.pm
+++ modules/EnsEMBL/Web/Configuration/Variation.pm
@@ -20,10 +20,16 @@ sub set_default_action {
   $self->{'_data'}->{'default'} = 'Summary';
 }
 
+sub tree_cache_key {
+  my $self = shift;
+  my $key  = $self->SUPER::tree_cache_key(@_);
+  $key .= '::SOMATIC' if $self->object->Obj->is_somatic;
+  return $key;
+}
+
 sub populate_tree {
   my $self = shift;
-  my $pop_short_name = $self->object->Obj->is_somatic ? 'Sample information' : 'Population genetics';
-  my $pop_long_name = $self->object->Obj->is_somatic ? 'Sample information' : 'Population genotypes and allele frequencies';
+  my $somatic = $self->object->Obj->is_somatic;
 
   $self->create_node('Summary', 'Summary',
     [qw(
@@ -37,28 +43,32 @@ sub populate_tree {
     [qw( summary EnsEMBL::Web::Component::Variation::Mappings )],
     { 'availability' => 'variation has_transcripts', 'concise' => 'Gene/Transcript' }
   );
-  
-  $self->create_node('Population', $pop_short_name .' ([[counts::populations]])',
+    
+  $self->create_node('Population', 'Population genetics ([[counts::populations]])',
     [qw( summary EnsEMBL::Web::Component::Variation::PopulationGenotypes )],
-    { 'availability' => 'variation has_populations', 'concise' => $pop_long_name }
+    { 'availability' => 'variation has_populations', 'concise' => 'Population genotypes and allele frequencies', 'no_menu_entry' => $somatic }
   );
-  unless ($self->object->Obj->is_somatic){
-    $self->create_node('Individual', 'Individual genotypes ([[counts::individuals]])',
-      [qw( summary EnsEMBL::Web::Component::Variation::IndividualGenotypes )],
-      { 'availability' => 'variation has_individuals', 'concise' => 'Individual genotypes'}
-    ); 
-  }
+  
+  $self->create_node('Populations', 'Sample information ([[counts::populations]])',
+    [qw( summary EnsEMBL::Web::Component::Variation::PopulationGenotypes )],
+    { 'availability' => 'variation has_populations', 'concise' => 'Sample information', 'no_menu_entry' => !$somatic }
+  );
+  
+  $self->create_node('Individual', 'Individual genotypes ([[counts::individuals]])',
+    [qw( summary EnsEMBL::Web::Component::Variation::IndividualGenotypes )],
+    { 'availability' => 'variation has_individuals', 'concise' => 'Individual genotypes', 'no_menu_entry' => $somatic }
+  ); 
+
   $self->create_node('Context', 'Context',
     [qw( summary EnsEMBL::Web::Component::Variation::Context )],
     { 'availability' => 'variation', 'concise' => 'Context' }
   );
   
-  unless ($self->object->Obj->is_somatic){
-   $self->create_node('HighLD', 'Linked variations',
-      [qw( summary EnsEMBL::Web::Component::Variation::HighLD )],
-      { 'availability' => 'variation has_ldpops variation has_individuals', 'concise' => 'Linked variations'}
-    );
-  }
+  $self->create_node('HighLD', 'Linked variations',
+    [qw( summary EnsEMBL::Web::Component::Variation::HighLD )],
+    { 'availability' => 'variation has_ldpops variation has_individuals', 'concise' => 'Linked variations', 'no_menu_entry' => $somatic }
+  );
+    
   $self->create_node('Phenotype', 'Phenotype Data ([[counts::ega]])',
     [qw( summary EnsEMBL::Web::Component::Variation::Phenotype )],
     { 'availability' => 'variation has_ega', 'concise' => 'Phenotype Data' }
