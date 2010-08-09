c668a66ed54f53792f1d17b71b4e234035cc16f9
Simon Brent <sb23@sanger.ac.uk>
2010-08-09 11:52:33 +0000

    remove model, add hub

--- modules/EnsEMBL/Web/Command.pm
+++ modules/EnsEMBL/Web/Command.pm
@@ -18,7 +18,7 @@ sub new {
 }
 
 sub object :lvalue { $_[0]->{'object'}; }
-sub model  :lvalue { $_[0]->{'model'}; }
+sub hub    :lvalue { $_[0]->{'hub'};    }
 sub page   :lvalue { $_[0]->{'page'};   }
 sub r              { return $_[0]->page->renderer->r; }
 
