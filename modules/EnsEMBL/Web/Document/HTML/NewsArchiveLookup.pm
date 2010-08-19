753e582b4378fb819414a9926241d4670b464d79
Maurice Hendrix <mh18@sanger.ac.uk>
2010-08-19 16:12:33 +0000

    Bugs fixed (now shows the releases) and formatting and processing split out in different subs, which is useful for the vega plugin

--- modules/EnsEMBL/Web/Document/HTML/NewsArchiveLookup.pm
+++ modules/EnsEMBL/Web/Document/HTML/NewsArchiveLookup.pm
@@ -1,6 +1,7 @@
 package EnsEMBL::Web::Document::HTML::NewsArchiveLookup;
 
 ### This module outputs a form to select a news archive (Document::HTML::NewsArchive), 
+### The functionality was split out in several subs, to make plugin development easier
 
 use strict;
 use warnings;
@@ -11,38 +12,67 @@ use EnsEMBL::Web::DBSQL::WebsiteAdaptor;
 
 use base qw(EnsEMBL::Web::Document::HTML);
 
+  my EnsEMBL::Web::Hub $hub = undef;
+  my $ensembl_version = new EnsEMBL::Web::Hub->species_defs->ENSEMBL_VERSION;
+ 
+ 
 sub render {
   my $self = shift;
-  my $html;
-  
-  my $hub = new EnsEMBL::Web::Hub;
-  my $species_defs = $hub->species_defs;
-  my $sitename = $species_defs->ENSEMBL_SITETYPE;
+  $hub = new EnsEMBL::Web::Hub;
+
   
   ## Form for selecting other releases
-  my $adaptor = EnsEMBL::Web::DBSQL::WebsiteAdaptor->new($hub);
-  my @releases = $adaptor->fetch_releases;
-  if (@releases) {
-    $html .= qq(
-<form action="/info/website/news/archive.html" method="get">
-<select name="id">);
-    my @release_options;
-    foreach my $r (@releases) {
-      next if $r->{'id'} > $species_defs->ENSEMBL_VERSION; ## Sanity check - mainly for dev sites!
-      my $date = $self->pretty_date($r->date, 'short');
-      next if $r->id == $species_defs->ENSEMBL_VERSION;
-      $html .= '<option value="'.$r->{'id'}.'"';
-      $html .= ' selected="selected"' if $r->{'id'} == $species_defs->ENSEMBL_VERSION;
-      $html .= sprintf '>Release %s (%s)</option>', $r->{'id'}, $date;
-    }
-    $html .= qq(
-</select> <input type="submit" name="submit" value="Go">
-</form>
-    );
-  }
-
-  return $html;
+  return $self->format_releases(
+            $self->sort_releases(
+              $self->filter(
+                $self->get_releases              
+              )  
+            )
+         );
 }
 
+sub get_releases{
+  my $self = shift;
+  my $html;
+
+  ## Form for selecting other releases
+  my $adaptor = EnsEMBL::Web::DBSQL::WebsiteAdaptor->new($hub);
+  return @{$adaptor->fetch_releases};
+}
+
+sub sort_releases{
+  my ($self, @releases_to_sort) = @_;
+  return sort { $b->{'id'} <=> $a->{'id'} } @releases_to_sort;
+}
+
+sub filter{
+  my ($self, @releases_to_filter) = @_;
+  return grep { ($_->{'id'} != $ensembl_version) && ($_->{'id'} <= $ensembl_version) } @releases_to_filter;
+}
+
+sub format_release{
+  my ($self, $release)=@_;
+
+  my $formated_html= '<option value="'.$release->{'id'}.'"';
+  $formated_html .= ' selected="selected"' if $release->{'id'} == $hub->param('id');
+  $formated_html .= sprintf '>Release %s (%s)</option>', $release->{'id'}, $self->pretty_date($release->{'date'}, 'short');
+  return $formated_html;
+}
+
+sub format_releases{
+  my ($self, @releases)=@_;
+  my $formated_html;
+  if (@releases) {
+    $formated_html .= qq(
+    <form action="/info/website/news/archive.html" method="get">
+      <select name="id">);
+    map($formated_html.=$self->format_release($_),@releases);
+    $formated_html .= qq(
+      </select> <input type="submit" name="submit" value="Go">
+    </form>
+    );
+  }
+  return $formated_html  ;
+}
 
 1;
