c9acec8f3dee00bc99fdde7ec7e6295181dd7a9a
Albert Vilella <avilella@sanger.ac.uk>
2010-08-25 15:42:37 +0000

    updated list and second part of the sorting

--- scripts/examples/homology_genome_sets.pl
+++ scripts/examples/homology_genome_sets.pl
@@ -4,7 +4,8 @@ use Bio::EnsEMBL::Registry;
 
 Bio::EnsEMBL::Registry->load_registry_from_db
   (-host=>"ensembldb.ensembl.org", 
-   -user=>"anonymous");
+   -user=>"anonymous",
+  -db_version=>'59');
 
 my $homology_adaptor =
     Bio::EnsEMBL::Registry->get_adaptor("Compara", "compara", "Homology");
@@ -15,7 +16,8 @@ my $mlss_adaptor =
 my $genomedb_adaptor =
     Bio::EnsEMBL::Registry->get_adaptor("Compara", "compara", "GenomeDB");
 
-my @list_of_species = ("Homo sapiens","Drosophila melanogaster","Saccharomyces cerevisiae");
+# my @list_of_species = ("homo_sapiens","pan_troglodytes","macaca_mulatta");
+my @list_of_species = ("homo_sapiens","pan_troglodytes","macaca_mulatta","mus_musculus","rattus_norvegicus","canis_familiaris","bos_taurus","sus_scrofa","monodelphis_domestica","ornithorhynchus_anatinus","gallus_gallus","danio_rerio");
 my $hash_of_species;
 my @gdbs;
 foreach my $species_binomial (@list_of_species) {
@@ -32,15 +34,15 @@ while (my $sp1_gdb = shift @gdbs) {
     my @orthologies = @{$homology_adaptor->fetch_all_by_MethodLinkSpeciesSet($mlss_orth)};
     my $count = 0; my $total_count = scalar @orthologies;
     foreach my $ortholog (@orthologies) {
-      1;
+      next unless ($ortholog->description =~ /one2one/);
       # Create a hash of stable_id pairs with genome name as subkey
       my ($gene1,$gene2) = @{$ortholog->gene_list};
+      $count++;
+      print STDERR "[$count/$total_count]\n" if (0 == $count % 100);
       $present_in_all->{$gene1->stable_id}{$sp1_gdb->name}{$gene2->stable_id} = 1;
       $present_in_all->{$gene1->stable_id}{$sp2_gdb->name}{$gene1->stable_id} = 1;
       $present_in_all->{$gene2->stable_id}{$sp1_gdb->name}{$gene2->stable_id} = 1;
       $present_in_all->{$gene2->stable_id}{$sp2_gdb->name}{$gene1->stable_id} = 1;
-      $count++;
-      print STDERR "[$count/$total_count]\n" if (0 == $count % 100);
     }
   }
 }
@@ -62,12 +64,14 @@ foreach my $stable_id (keys %$present_in_all) {
     next if (defined($tagged_stable_id->{$stable_id})); #Print only once
     $tagged_stable_id->{$stable_id};
     $output_string = "$stable_id";
-    my @stable_ids;
-    push @stable_ids, $stable_id;
+    my $stable_ids;
+    $stable_ids->{$stable_id} = 1;
     foreach my $name (@list) {
-      push @stable_ids, keys %{$present_in_all->{$stable_id}{$name}};
+      foreach my $id (keys %{$present_in_all->{$stable_id}{$name}}) {
+        $stable_ids->{$id} = 1;
+      }
     }
-    print join(",",sort @stable_ids), "\n";
+    print join(",", keys %$stable_ids), "\n";
   }
 }
 
