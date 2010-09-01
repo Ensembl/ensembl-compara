93a4ed779e72d72c4803ecc40dfa9a48072a1ef2
Maurice Hendrix <mh18@sanger.ac.uk>
2010-09-01 15:21:49 +0000

    configure the xref types shown, now uses data created by configpacker.pm at startup

--- modules/EnsEMBL/Web/ViewConfig/Gene/Matches.pm
+++ modules/EnsEMBL/Web/ViewConfig/Gene/Matches.pm
@@ -5,47 +5,52 @@ use strict;
 use EnsEMBL::Web::Constants;
 
 sub init {
-#  my ($view_config) = @_;
 my $view_config = shift;
 my $help  = shift;
+my $nr_on_by_default  = 5;
   
   $view_config->storable = 1;
   $view_config->nav_tree = 1;
   my %defaults;
-  my $species= $ENV{'ENSEMBL_SPECIES'};
-  my $DBConnection = new EnsEMBL::Web::DBSQL::DBConnection("Homo_sapiens", $ENV);
-  my $dba = $DBConnection->get_DBAdaptor("core", $species);
-  my $sth = $dba->dbc->prepare("SELECT distinct(edb.db_display_name)
-  FROM object_xref ox join xref x on ox.xref_id=x.xref_id  join external_db edb on x.external_db_id = edb.external_db_id
-    where edb.type IN ('MISC', 'LIT')
-    and (ox.ensembl_object_type ='Transcript' or ox.ensembl_object_type ='Translation' )");  
-  $sth->execute();    
-  my @row;
-  while (@row=$sth->fetchrow_array()){
-    $defaults{$row[0]}='yes';
+  my @xref_types = get_xref_types();  
+
+  foreach (@xref_types){
+    $defaults{$_->{'name'}}=($nr_on_by_default > 0)?'yes':undef;
+	$nr_on_by_default--;
   }
   $view_config->_set_defaults(%defaults);
 }
 
 sub form {
   my ($view_config, $object) = @_;
+  my @xref_types = get_xref_types();
 
-  my $sth = $object->database('core',$object->species )->dbc->prepare("SELECT distinct(edb.db_display_name)
-  FROM object_xref ox join xref x on ox.xref_id=x.xref_id  join external_db edb on x.external_db_id = edb.external_db_id
-    where edb.type IN ('MISC', 'LIT')
-    and (ox.ensembl_object_type ='Transcript' or ox.ensembl_object_type ='Translation' )");
-  $sth->execute();
-  my @row;
-  while (@row=$sth->fetchrow_array()){
+  foreach (@xref_types){
      my $external_ref_type_chec_box = {
       'type'  => 'CheckBox',
       'select' => 'select',
-      'name'   => $row[0],
-      'label'  => $row[0],
+      'name'   => $_->{'name'},
+      'label'  => $_->{'name'},
       'value' => 'yes'
     };     
     $view_config->add_form_element($external_ref_type_chec_box);
-  }    
+  }
+}
+
+sub get_xref_types {
+  my $species= $ENV{'ENSEMBL_SPECIES'};
+  my $SPECIES_DEFS = EnsEMBL::Web::SpeciesDefs->new();
+  
+  my $xref_types_string = $SPECIES_DEFS->get_config($species, 'XREF_TYPES');
+  my @xref_types;
+  foreach(split(/,/, $xref_types_string)){
+    my @type_priorities = split(/=/,$_);
+    my $xref_type;
+	$xref_type->{'name'}=@type_priorities[0];
+	$xref_type->{'priority'}=@type_priorities[1];
+	push(@xref_types,$xref_type)
+  }
+  return @xref_types;
 }
 
 1;
