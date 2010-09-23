091dde0437ba53a50b01dcdd3a41f583b63af3f7
Maurice Hendrix <mh18@sanger.ac.uk>
2010-09-23 16:41:02 +0000

    bug fix

--- modules/EnsEMBL/Web/ViewConfig/Gene/Matches.pm
+++ modules/EnsEMBL/Web/ViewConfig/Gene/Matches.pm
@@ -11,12 +11,19 @@ my $help  = shift;
   $view_config->storable = 1;
   $view_config->nav_tree = 1;
   my %defaults;
+  my %defaults_off;
   my @xref_types = get_xref_types();  
-
   my @default_on = ('Ensembl Human Transcript' , 'HGNC (curated)', 'HGNC (automatic)', 'EntrezGene', 'CCDS', 'RefSeq RNA', 'UniProtKB/ Swiss-Prot', 'RefSeq peptide', 'RefSeq DNA', 'RFAM', 'miRBase', 'Vega transcript', 'MIM disease');
+  
   foreach (@xref_types){
-    if($_->{'type'} eq 'MISC' || $_->{'type'} eq 'LIT'){
-      $defaults{$_->{'name'}}=(grep {m|^$_->{'name'}?$|} @default_on)?'yes':undef;
+    my $default_enabled=0;
+    for( my $i=0; $i< scalar(@default_on) && !$default_enabled; $i++){
+      $default_enabled = $_->{'name'} eq $default_on[$i];
+    }
+    if($default_enabled){
+      $defaults{$_->{'name'}}='yes';
+    }else{
+      $defaults_off{$_->{'name'}}=undef;    
     }
   }
   $view_config->_set_defaults(%defaults);
@@ -25,6 +32,8 @@ my $help  = shift;
 sub form {
   my ($view_config, $object) = @_;
   my @xref_types = get_xref_types();
+
+  @xref_types = sort {sort_by_default_on($b) <=> sort_by_default_on($a)} @xref_types;
 
   foreach (@xref_types){
      my $external_ref_type_chec_box = {
@@ -47,11 +56,21 @@ sub get_xref_types {
   foreach(split(/,/, $xref_types_string)){
     my @type_priorities = split(/=/,$_);
     my $xref_type;
-	$xref_type->{'name'}=@type_priorities[0];
-	$xref_type->{'priority'}=@type_priorities[1];
-	push(@xref_types,$xref_type)
+	  $xref_type->{'name'}=@type_priorities[0];
+	  $xref_type->{'priority'}=@type_priorities[1];
+	  push(@xref_types,$xref_type)
   }
   return @xref_types;
 }
 
+sub sort_by_default_on{
+  my $value=shift;
+  my @default_on = ('Ensembl Human Transcript' , 'HGNC (curated)', 'HGNC (automatic)', 'EntrezGene', 'CCDS', 'RefSeq RNA', 'UniProtKB/ Swiss-Prot', 'RefSeq peptide', 'RefSeq DNA', 'RFAM', 'miRBase', 'Vega transcript', 'MIM disease');  
+  my $return=0;
+  
+  for (my $i=0; $i< scalar(@default_on) && !$return; $i++){
+    $return = ($default_on[$i] eq $value->{'name'});
+  }
+  return $return;
+}
 1;
