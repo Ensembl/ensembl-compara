7409f1471024e93b393afd635e3b3dafcc0c7533
Maurice Hendrix <mh18@sanger.ac.uk>
2010-09-24 12:31:25 +0000

    nodes didn't look right size on staging

--- modules/EnsEMBL/Web/Tools/OntologyVisualisation.pm
+++ modules/EnsEMBL/Web/Tools/OntologyVisualisation.pm
@@ -236,7 +236,7 @@ sub _get_width{
   foreach(split(/\n/, $node_name)){
     $length=($length< length($_))?length($_):$length;
   }
-  return $length*.08;
+  return $length*.085;
 }
 
 sub _add_parents{
