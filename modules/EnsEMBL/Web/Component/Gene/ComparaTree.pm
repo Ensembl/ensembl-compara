76a30e91640b87c2506c333f00fa097215690699
Anne Parker <ap5@sanger.ac.uk>
2010-09-27 14:25:03 +0000

    Put view options links back in for genetree version

--- modules/EnsEMBL/Web/Component/Gene/ComparaTree.pm
+++ modules/EnsEMBL/Web/Component/Gene/ComparaTree.pm
@@ -219,10 +219,10 @@ sub content {
   if ($gene) {
     push @view_links, sprintf $li_tmpl, $hub->url({ collapse => $collapsed_to_gene, g1 => $highlight_gene }), $highlight_gene ? 'View current genes only'        : 'View current gene only';
     push @view_links, sprintf $li_tmpl, $hub->url({ collapse => $collapsed_to_para, g1 => $highlight_gene }), $highlight_gene ? 'View paralogs of current genes' : 'View paralogs of current gene';
-    push @view_links, sprintf $li_tmpl, $hub->url({ collapse => $collapsed_to_dups, g1 => $highlight_gene }), 'View all duplication nodes';
-    push @view_links, sprintf $li_tmpl, $hub->url({ collapse => '', g1 => $highlight_gene }), 'View fully expanded tree';
-    push @view_links, sprintf $li_tmpl, $hub->url, 'Switch off highlighting' if $highlight_gene;
   }
+  push @view_links, sprintf $li_tmpl, $hub->url({ collapse => $collapsed_to_dups, g1 => $highlight_gene }), 'View all duplication nodes';
+  push @view_links, sprintf $li_tmpl, $hub->url({ collapse => '', g1 => $highlight_gene }), 'View fully expanded tree';
+  push @view_links, sprintf $li_tmpl, $hub->url, 'Switch off highlighting' if $highlight_gene;
 
   $html .= $image->render;
   $html .= sprintf(qq{
