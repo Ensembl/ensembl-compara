939acc4c7c8465876ae5e2cae1b9f6cdfa09dfa8
Simon Brent <sb23@sanger.ac.uk>
2010-09-27 11:54:52 +0000

    added class to delete links

--- modules/EnsEMBL/Web/Component/Account.pm
+++ modules/EnsEMBL/Web/Component/Account.pm
@@ -8,20 +8,20 @@ use base qw(EnsEMBL::Web::Component);
 
 sub edit_link {
   my ($self, $module, $id, $text) = @_;
-  $text = 'Edit' if !$text;
-  return sprintf qq(<a class="modal_link" href="/Account/%s/Edit?id=%s">%s</a>), $module, $id, $text;
+  $text ||= 'Edit';
+  return qq{<a class="modal_link" href="/Account/$module/Edit?id=$id">$text</a>};
 } 
 
 sub delete_link {
-  my ($self, $module, $id, $text) = @_;
-  $text = 'Delete' if !$text;
-  return sprintf qq(<a class="modal_link" href="/Account/%s/Delete?id=%s">%s</a>), $module, $id, $text;
+  my ($self, $module, $id, $text, $class) = @_;
+  $text ||= 'Delete';
+  return qq{<a class="modal_link $class" href="/Account/$module/Delete?id=$id">$text</a>};
 } 
 
 
 sub share_link {
   my ($self, $call, $id) = @_;
-  return sprintf qq(<a class="modal_link" href="/Account/SelectGroup?id=%s;type=%s">Share</a>), $id, $call;
+  return qq{<a class="modal_link" href="/Account/SelectGroup?id=$id;type=$call">Share</a>};
 } 
 
 sub dedupe {
