5cd732468b6742754c205fad25a458edecae18fa
Simon Brent <sb23@sanger.ac.uk>
2010-09-28 11:04:02 +0000

    Controller/configuration rewrite

--- modules/EnsEMBL/Web/Configuration/DAS.pm
+++ modules/EnsEMBL/Web/Configuration/DAS.pm
@@ -1,87 +1,41 @@
+# $Id$
+
 package EnsEMBL::Web::Configuration::DAS;
-# This configures the DAS server component of the website
 
 use strict;
-use EnsEMBL::Web::Configuration;
-our @ISA = qw( EnsEMBL::Web::Configuration);
+
+use EnsEMBL::Web::Document::Panel;
+
+use base qw(EnsEMBL::Web::Configuration);
 
 sub get_valid_action {
-  my $self = shift;
+  my $self   = shift;
   my $action = shift;
   my $func   = shift;
   return $func ? "$action/$func" : "action";
 }
 
-sub stylesheet {
-  my $self = shift;
-  my $page = $self->{'page'};
-  $page->set_doc_type('XML', 'DASSTYLE');
-  my $component = "EnsEMBL::Web::Component::DAS::Annotation";
-  if( my $das_panel = $self->new_panel( '', 'code' => 'das', ) ) {
-    $das_panel->add_components("das_features", $component.'::stylesheet');
-    $self->add_panel( $das_panel );
-  }
-}
-
-sub features {
-  my $self = shift;
-  my $page = $self->{'page'};
-  $page->set_doc_type('XML', 'DASGFF');
-#  my $component = $ENV{ENSEMBL_DAS_TYPE} eq 'reference' ?
-#    'EnsEMBL::Web::Component::DAS::Reference' : "EnsEMBL::Web::Component::DAS::Annotation";
-  if( my $das_panel = $self->new_panel( '', 'code' => 'das',  ) ) {
-    $das_panel->add_components("das_features", 'EnsEMBL::Web::Component::DAS::features');
-    $self->add_panel( $das_panel );
-  }
-}
-
-sub types{
-  my $self = shift;
-  my $page = $self->{'page'};
-  $page->set_doc_type('XML', 'DASTYPES');
-  my $component = "EnsEMBL::Web::Component::DAS";
-  if( my $das_panel = $self->new_panel( '', 'code' => 'das', ) ) {
-    $das_panel->add_components("das_features", $component.'::types');
-    $self->add_panel( $das_panel );
-  }
-}
-
-# Only applicable to a reference server
-
-sub entry_points {
-  my $self = shift;
-  my $page = $self->{'page'};
-  $page->set_doc_type('XML', 'DASEP');
-  my $component = 'EnsEMBL::Web::Component::DAS::Reference';
-  if( my $das_panel = $self->new_panel( '', 'code' => 'das', ) ) {
-    $das_panel->add_components("das_features", $component.'::entry_points');
-    $self->add_panel( $das_panel );
-  }
-}
+sub stylesheet   { $_[0]->new_panel('DASSTYLE',    'EnsEMBL::Web::Component::DAS::Annotation::stylesheet');  }
+sub features     { $_[0]->new_panel('DASGFF',      'EnsEMBL::Web::Component::DAS::features');                }
+sub types        { $_[0]->new_panel('DASTYPES',    'EnsEMBL::Web::Component::DAS::types');                   }
+sub sequence     { $_[0]->new_panel('DASSEQUENCE', 'EnsEMBL::Web::Component::DAS::Reference::sequence');     }
+sub entry_points { $_[0]->new_panel('DASEP',       'EnsEMBL::Web::Component::DAS::Reference::entry_points'); } # Only applicable to a reference server
+sub dna          { $_[0]->new_panel('DASDNA',      'EnsEMBL::Web::Component::DAS::Reference::dna');          } # Only applicable to a reference server
 
 
-# Only applicable to a reference server
-
-sub dna {
-  my $self = shift;
-  my $page = $self->{'page'};
-  $page->set_doc_type('XML', 'DASDNA');
-  my $component = 'EnsEMBL::Web::Component::DAS::Reference';
-  if( my $das_panel = $self->new_panel( '','code' => 'das', ) ) {
-    $das_panel->add_components("das_features", $component.'::dna');
-    $self->add_panel( $das_panel );
-  }
-}
-
-sub sequence {
-  my $self = shift;
-  my $page = $self->{'page'};
-  $page->set_doc_type('XML', 'DASSEQUENCE');
-  my $component = 'EnsEMBL::Web::Component::DAS::Reference';
-  if( my $das_panel = $self->new_panel( '', 'code' => 'das', ) ) {
-    $das_panel->add_components("das_features", $component.'::sequence');
-    $self->add_panel( $das_panel );
-  }
+sub new_panel {
+  my $self      = shift;
+  my $page      = $self->page;
+  my $das_panel = new EnsEMBL::Web::Document::Panel(
+    hub     => $self->hub,
+    builder => $self->builder,
+    object  => $self->object,
+    code    => 'das'
+  );
+  
+  $page->set_doc_type('XML', shift);
+  $das_panel->add_components('das_features', shift);
+  $page->content->add_panel($das_panel);
 }
 
 1;
