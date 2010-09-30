47ebfac4bf7231b8c40a3f8ef3344bb211c7c284
Simon Brent <sb23@sanger.ac.uk>
2010-09-30 12:42:54 +0000

    better variable names

--- modules/EnsEMBL/Web/Component/Gene/TranscriptsImage.pm
+++ modules/EnsEMBL/Web/Component/Gene/TranscriptsImage.pm
@@ -1,8 +1,7 @@
 package EnsEMBL::Web::Component::Gene::TranscriptsImage;
 
 use strict;
-use warnings;
-no warnings "uninitialized";
+
 use base qw(EnsEMBL::Web::Component::Gene);
 
 sub _init { 
@@ -12,50 +11,45 @@ sub _init {
 }
 
 sub caption {
-  my $html = 'Transcripts';
-  return $html;
+  return 'Transcripts';
 }
 
 sub content {
-  my $self = shift;
-  my $gene = $self->object;
-
-  my @trans = sort { $a->stable_id cmp $b->stable_id } @{$gene->get_all_transcripts};
-  my $gene_slice = $gene->Obj->feature_Slice->expand(10e3, 10e3);
-     $gene_slice = $gene_slice->invert if $gene->seq_region_strand < 0;
+  my $self   = shift;
+  my $object = $self->object;
+  my $gene   = $object->Obj;
+  
+  my $gene_slice = $gene->feature_Slice->expand(10e3, 10e3);
+     $gene_slice = $gene_slice->invert if $object->seq_region_strand < 0;
      
   # Get the web_image_config
-  my $image_config = $gene->get_imageconfig('gene_summary');
+  my $image_config = $object->get_imageconfig('gene_summary');
   
   $image_config->set_parameters({
-    'container_width' => $gene_slice->length,
-    'image_width',    => $gene->param('i_width') || $self->image_width || 800,
-    'slice_number'    => '1|1',
+    container_width => $gene_slice->length,
+    image_width     => $object->param('i_width') || $self->image_width || 800,
+    slice_number    => '1|1',
   });
   
   $self->_attach_das($image_config);
 
-  my $key = $image_config->get_track_key('transcript', $gene);
-  my $n   = $image_config->get_node(lc($key));
+  my $key  = $image_config->get_track_key('transcript', $object);
+  my $node = $image_config->get_node(lc $key);
   
-  $n->set('display', 'transcript_label') if $n && $n->get('display') eq 'off';
+  $node->set('display', 'transcript_label') if $node && $node->get('display') eq 'off';
 
-  my $image = $self->new_image($gene_slice, $image_config, [ $gene->Obj->stable_id ]);
+  my $image = $self->new_image($gene_slice, $image_config, [ $gene->stable_id ]);
   
   return if $self->_export_image($image);
   
   $image->imagemap         = 'yes';
   $image->{'panel_number'} = 'top';
   $image->set_button('drag', 'title' => 'Drag to select region');
-
-
-  my $html = $image->render;
-  $html .= $self->_info(
+  
+  return $image->render . $self->_info(
     'Configuring the display',
     '<p>Tip: use the "<strong>Configure this page</strong>" link on the left to show additional data in this region.</p>'
   );
-  
-  return $html;
 }
 
 1;
