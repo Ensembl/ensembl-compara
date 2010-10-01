d9c6d324fc301f27261852d4382d2816bba6d403
Simon Brent <sb23@sanger.ac.uk>
2010-10-01 14:27:28 +0000

    moving functionality from object to hub

--- modules/EnsEMBL/Web/Component/UserData/FeatureView.pm
+++ modules/EnsEMBL/Web/Component/UserData/FeatureView.pm
@@ -1,31 +1,30 @@
+# $Id$
+
 package EnsEMBL::Web::Component::UserData::FeatureView;
 
 use strict;
-use warnings;
-no warnings "uninitialized";
 
 use base qw(EnsEMBL::Web::Component::UserData);
 
 sub _init {
   my $self = shift;
-  $self->cacheable( 0 );
-  $self->ajaxable(  0 );
+  $self->cacheable(0);
+  $self->ajaxable(0);
 }
 
 sub caption {
-  my $self = shift;
   return 'Select Features to Display';
 }
 
 sub content {
-  my $self = shift;
-  my $object = $self->object;
+  my $self            = shift;
+  my $hub             = $self->hub;
+  my $species_defs    = $hub->species_defs;
+  my $sitename        = $species_defs->ENSEMBL_SITETYPE;
+  my $current_species = $hub->data_species;
   my $html;
 
-  my $sitename = $object->species_defs->ENSEMBL_SITETYPE;
-  my $current_species = $object->data_species;
-
-  my $form = new EnsEMBL::Web::Form('select', $object->species_path($current_species).'/UserData/FviewRedirect', 'post', 'std check');
+  my $form = new EnsEMBL::Web::Form('select', $hub->species_path($current_species).'/UserData/FviewRedirect', 'post', 'std check');
 
   $html .= $self->_hint('notes', 'Hint', qq{
 Using this form, you can select Ensembl features to display on a karyotype (formerly known as FeatureView).
@@ -33,8 +32,8 @@ Using this form, you can select Ensembl features to display on a karyotype (form
 
   ## Species is set automatically for the page you are on
   my @species;
-  foreach my $sp ($object->species_defs->valid_species) {
-    push @species, {'value' => $sp, 'name' => $object->species_defs->species_label($sp, 1)};
+  foreach my $sp ($species_defs->valid_species) {
+    push @species, {'value' => $sp, 'name' => $species_defs->species_label($sp, 1)};
   }
   @species = sort {$a->{'name'} cmp $b->{'name'}} @species;
   $form->add_element(
@@ -64,22 +63,6 @@ Using this form, you can select Ensembl features to display on a karyotype (form
 
   $form->add_element( type => 'Text', name => 'id', label => 'ID(s)', 'notes' => 'Hint: to display multiple features, enter them as a comma-delimited list' );
 
-=pod
-  my @chrs = ({'value' => 'all', 'name' => 'All' });
-  my @sp_chrs = @{$object->species_defs->ENSEMBL_CHROMOSOMES};
-  foreach my $chr (@sp_chrs) {
-    push @chrs, {'value' => $chr, 'name' => $chr };
-  }
-  $form->add_element(
-      'type'    => 'DropDown',
-      'name'    => 'chr',
-      'label'   => 'Chromosome(s)',
-      'values'  => \@chrs,
-      'value'   => 'all',
-      'select'  => 'select',
-  );
-=cut
-
   my @colours;
   foreach my $colour (@{$species_defs->TRACK_COLOUR_ARRAY}) {
     my $colourname = ucfirst($colour);
@@ -108,7 +91,6 @@ Using this form, you can select Ensembl features to display on a karyotype (form
       'values'  => \@styles,
       'select'  => 'select',
   );
-
 
   $form->add_button('type' => 'Submit', 'name' => 'submit', 'value' => 'Show features', 'classes' => ['submit', 'modal_close']);
   $form->add_element('type' => 'ForceReload');
