bb3632a01fb8817ceafa7ef2507ce8f467cc3b72
Steve Trevanion <st3@sanger.ac.uk>
2010-10-01 16:39:01 +0000

    added line for removed evidence

--- modules/EnsEMBL/Web/ZMenu/SupportingEvidenceAlignment.pm
+++ modules/EnsEMBL/Web/ZMenu/SupportingEvidenceAlignment.pm
@@ -16,43 +16,49 @@ sub content {
   my $hit_url    = $hub->get_ExtURL_link($link_name, $hit_db, $link_name);
   my $tsid       = $hub->param('t');
   my $esid       = $hub->param('exon');
-  
+
   $self->caption("$hit_name ($hit_db)");
-  
+
   if ($esid) {
     my $exon_length = $hub->param('exon_length');
-    
+
+    if ($hub->param('er')) {
+      $self->add_entry({
+	label_html => "Entry removed from $hit_db",
+      });
+    }
+
     $self->add_entry({
       type  => 'View alignments',
       label => "$esid ($tsid)",
       link  => $hub->url({
-        type     => 'Transcript',
-        action   => 'SupportingEvidence',
-        function => 'Alignment',
-        sequence => $hit_name,
-        exon     => $esid
+	type     => 'Transcript',
+	action   => 'SupportingEvidence',
+	function => 'Alignment',
+	sequence => $hit_name,
+	exon     => $esid
       })
     });
-    
+
     $self->add_entry({
       type  => 'View record',
       label => $hit_name,
       link  => $hit_url,
       extra => { abs_url => 1 }
     });
-    
+
     $self->add_entry({
       type  => 'Exon length',
       label => "$exon_length bp"
     });
-    
+
     if ($hub->param('five_end_mismatch')) {
       $self->add_entry({
         type  => "5' mismatch",
         label => $hub->param('five_end_mismatch') . ' bp'
       });
     }
-    
+
     if ($hub->param('three_end_mismatch')) {
       $self->add_entry({
         type  => "3' mismatch",
