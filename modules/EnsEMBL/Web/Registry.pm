ea6998a664ae04f746e69222869fb614dc01a6e4
Simon Brent <sb23@sanger.ac.uk>
2010-10-07 10:28:32 +0000

    function removal part 1

--- modules/EnsEMBL/Web/Registry.pm
+++ modules/EnsEMBL/Web/Registry.pm
@@ -13,7 +13,6 @@ use Data::Dumper;
 use EnsEMBL::Web::Timer;
 use EnsEMBL::Web::SpeciesDefs;
 use EnsEMBL::Web::Data::User;
-use EnsEMBL::Web::Session;
 use EnsEMBL::Web::DASConfig;
 
 sub new {
@@ -61,37 +60,6 @@ sub set_type { $_[0]->{'type'} = $_[1]; }
 sub get_action { return $_[0]->{'action'}; }
 sub set_action { $_[0]->{'action'} = $_[1]; }
 
-# This method gets all configured DAS sources for the current species.
-# Source configurations are retrieved first from SpeciesDefs, then additions and
-# modifications are added from the User and Session.
-# Returns a hashref, indexed by logic_name.
-sub get_all_das {
-  my $self    = shift;
-  my $species = shift || $ENV{'ENSEMBL_SPECIES'};
-  
-  if ( $species eq 'common' ) {
-    $species = '';
-  }
-  
-  my @spec_das = $self->species_defs->get_all_das( $species );
-  my @sess_das = $self->get_session ->get_all_das( $species );
-  my @user_das = $self->get_user ? $self->get_user->get_all_das( $species ) : ({},{});
-  
-  # TODO: group data??
-  
-  # First hash is keyed by logic_name, second is keyed by full_url
-  my %by_name = ( %{ $spec_das[0] }, %{ $user_das[0] }, %{ $sess_das[0] } );
-  my %by_url  = ( %{ $spec_das[1] || {}}, %{ $user_das[1] || {} }, %{ $sess_das[1] || {}} );
-  return wantarray ? ( \%by_name, \%by_url ) : \%by_name;
-}
-
-# This method gets a single named DAS source for the current species.
-# The source's configuration is an amalgam of species, user and session data.
-sub get_das_by_logic_name {
-  my ( $self, $name ) = @_;
-  return $self->get_all_das->{ $name };
-}
-
 sub timer_push {
   my $self = shift;
   $self->timer->push( @_ );
@@ -102,15 +70,13 @@ sub timer {
 }
 
 sub species_defs {
-### a
-### Lazy loaded SpeciesDefs object
+  ### a
+  ### Lazy loaded SpeciesDefs object
   my $self = shift;
-  return $self->{'species_defs'} ||=
-    EnsEMBL::Web::SpeciesDefs->new();
+  return $self->{'species_defs'} ||= new EnsEMBL::Web::SpeciesDefs;
 }
 
 sub initialize_user {
-###
   my ($self, $arg_ref) = @_;
   $arg_ref->{'cookie'}->retrieve($arg_ref->{'r'});
 
@@ -133,26 +99,14 @@ sub initialize_user {
 ##  defer => 'yes',
 }
 
+# TODO: delete if possible
 sub initialize_session {
-###
-  my($self,$arg_ref)=@_;
-  $self->set_species( $arg_ref->{'species'} );# if exists $arg_ref->{'species'};
-  $self->set_script(  $arg_ref->{'script'}  );# if exists $arg_ref->{'script'};
-  $self->set_action(  $arg_ref->{'action'}  );# if exists $arg_ref->{'action'};
-  $self->set_type(    $arg_ref->{'type'}    );# if exists $arg_ref->{'type'};
-  $arg_ref->{'cookie'}->retrieve($arg_ref->{'r'});
-
-  $self->set_session(
-    EnsEMBL::Web::Session->new({
-      'adaptor'      => $self,
-      'cookie'       => $arg_ref->{'cookie'},
-      'session_id'   => $arg_ref->{'cookie'}->get_value,
-      'species_defs' => $self->species_defs,
-      'species'      => $arg_ref->{'species'}
-    })
-  );
+  my ($self, $hub) = @_;
+  $self->set_species($hub->species);
+  $self->set_script($hub->script);
+  $self->set_action($hub->action);
+  $self->set_type($hub->type);
+  $self->set_session($hub->session);
 }
 
 1;
-
-
