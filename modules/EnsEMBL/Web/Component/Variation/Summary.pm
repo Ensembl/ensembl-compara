bbe51e03c43e7000ff2447ae2bf881ab52a47d03
Simon Brent <sb23@sanger.ac.uk>
2010-10-12 10:56:33 +0000

    use hub->url

--- modules/EnsEMBL/Web/Component/Variation/Summary.pm
+++ modules/EnsEMBL/Web/Component/Variation/Summary.pm
@@ -290,9 +290,9 @@ sub content {
       $location_string_long = $location_string.' ('.($str > 0 ? "forward" : "reverse").' strand)';
       
       # turn on somatic or normal variation track
-      my $track_name = $self->object->Obj->is_somatic ? 'somatic_mutation_COSMIC' : 'variation_feature_variation';
+      my $track_name = $is_somatic ? 'somatic_mutation_COSMIC' : 'variation_feature_variation';
       
-      my $location_link = $object->_url({ type =>'Location', action => 'View', r => $display_region, v => $id, source => $source, vf => $selected_vf, contigviewbottom => $track_name.'=normal' });
+      my $location_link = $hub->url({ type =>'Location', action => 'View', r => $display_region, v => $id, source => $source, vf => $selected_vf, contigviewbottom => $track_name.'=normal' });
       $location_link_html = qq( | <a href="$location_link">View in location tab</a>);
     }
     
