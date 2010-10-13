7aa59f1f0ef8f92aa3667e9d9c023b160f1cba2b
Maurice Hendrix <mh18@sanger.ac.uk>
2010-10-13 14:32:46 +0000

    removed eq fragment as this was a mistake

--- modules/EnsEMBL/Web/Document/HTML/SpeciesList.pm
+++ modules/EnsEMBL/Web/Document/HTML/SpeciesList.pm
@@ -3,13 +3,11 @@
 package EnsEMBL::Web::Document::HTML::SpeciesList;
 
 use strict;
-
-our $IMAGE_TYPE = '.gif';
 use EnsEMBL::Web::Document::HTML::SpeciesLister;
 
 sub render {
   my $class        = shift;
-  my $fragment     = shift eq 'fragment';
+  my $fragment     = shift;
   my $speciesLister = new EnsEMBL::Web::Document::HTML::SpeciesLister();
   return $speciesLister->render($fragment);
 }
