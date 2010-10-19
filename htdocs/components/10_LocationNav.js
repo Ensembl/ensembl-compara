a771383c0ead58b929816dd858f99f616f2f0f6a
Simon Brent <sb23@sanger.ac.uk>
2010-10-19 10:36:21 +0000

    autocomplete on gene input

--- htdocs/components/10_LocationNav.js
+++ htdocs/components/10_LocationNav.js
@@ -6,8 +6,10 @@ Ensembl.Panel.LocationNav = Ensembl.Panel.extend({
     
     this.matchRegex   = new RegExp(/[\?;&]r=([^;&]+)/);
     this.replaceRegex = new RegExp(/([\?;&]r=)[^;&]+(;&?)*/);
+    this.autoComplete = { query: false, all: 0 };
     
-    Ensembl.EventManager.register('hashChange', this, this.getContent);
+    Ensembl.EventManager.register('hashChange',   this, this.getContent);
+    Ensembl.EventManager.register('windowResize', this, this.resize);
     
     if (!window.location.pathname.match(/\/Multi/)) {
       Ensembl.EventManager.register('ajaxComplete', this, function () { this.enabled = true; });
@@ -16,6 +18,7 @@ Ensembl.Panel.LocationNav = Ensembl.Panel.extend({
   
   init: function () {
     var panel = this;
+    var hash, boundaries, r, l, i;
     
     this.base();
     
@@ -26,13 +29,17 @@ Ensembl.Panel.LocationNav = Ensembl.Panel.extend({
     this.enabled = this.params.enabled || false;
     this.reload  = false;
     
-    var sliderConfig = $('span.ramp', this.el).hide().children();
-    var sliderLabel  = $('.slider_label', this.el);
-    var hash, boundaries, r, l, i;
-    
-    this.elLk.updateURL     = $('.update_url', this.el);
+    this.elLk.sliderConfig  = $('span.ramp',          this.el).hide().children();
+    this.elLk.sliderLabel   = $('.slider_label',      this.el);
+    this.elLk.updateURL     = $('.update_url',        this.el);
+    this.elLk.slider        = $('.slider',            this.el);
     this.elLk.locationInput = $('.location_selector', this.el);
-    this.elLk.navLinks      = $('a', this.el).addClass('constant').bind('click', function (e) {
+    this.elLk.geneLocForm   = $('.gene_location',     this.el);
+    this.elLk.geneInput     = $('.gene_selector',     this.elLk.geneLocForm).attr('autocomplete', 'off');
+    this.elLk.g             = $('input[name=g]',      this.elLk.geneLocForm);
+    this.elLk.db            = $('input[name=db]',     this.elLk.geneLocForm);
+    this.elLk.autoComplete  = $('<ul>', { className: 'auto_complete', css: this.autoCompletePosition() }).insertAfter(this.elLk.geneInput);
+    this.elLk.navLinks      = $('a',                  this.el).addClass('constant').bind('click', function (e) {
       var newR;
       
       if (panel.enabled === true) {
@@ -55,46 +62,55 @@ Ensembl.Panel.LocationNav = Ensembl.Panel.extend({
       r    = hash.match(this.matchRegex)[1].split(/\W/);
       l    = r[2] - r[1] + 1;
       
-      sliderLabel.html(l);
-      sliderConfig.removeClass('selected');
+      this.elLk.sliderLabel.html(l);
+      this.elLk.sliderConfig.removeClass('selected');
       
-      i = sliderConfig.length;
+      i = this.elLk.sliderConfig.length;
       
-      if (l >= parseInt(sliderConfig[i-1].name, 10)) {
-        sliderConfig.last().addClass('selected');
+      if (l >= parseInt(this.elLk.sliderConfig[i-1].name, 10)) {
+        this.elLk.sliderConfig.last().addClass('selected');
       } else {
-        boundaries = $.map(sliderConfig, function (el, i) {
-          return Math.sqrt((i ? parseInt(sliderConfig[i-1].name, 10) : 0) * parseInt(el.name, 10));
+        boundaries = $.map(this.elLk.sliderConfig, function (el, i) {
+          return Math.sqrt((i ? parseInt(panel.elLk.sliderConfig[i-1].name, 10) : 0) * parseInt(el.name, 10));
         });
         
         boundaries.push(1e30);
         
         while (i--) {
           if (l > boundaries[i] && l <= boundaries[i+1]) {
-            sliderConfig.eq(i).addClass('selected');
+            this.elLk.sliderConfig.eq(i).addClass('selected');
             break;
           }
         }
       }
     }
     
+    this.sliderInit();
+    this.autoCompleteInit();
+    
+    Ensembl.EventManager.register('mouseUp', this, function () { panel.elLk.autoComplete.hide(); });
+  },
+    
+  sliderInit: function () {
+    var panel = this;
+    
     $('.slider_wrapper', this.el).children().css('display', 'inline-block');
     
-    this.elLk.slider = $('.slider', this.el).slider({
-      value: sliderConfig.filter('.selected').index(),
+    this.elLk.slider.slider({
+      value: panel.elLk.sliderConfig.filter('.selected').index(),
       step:  1,
       min:   0,
-      max:   sliderConfig.length - 1,
+      max:   panel.elLk.sliderConfig.length - 1,
       force: false,
       slide: function (e, ui) {
-        sliderLabel.html(sliderConfig[ui.value].name + ' bp').show();
+        panel.elLk.sliderLabel.html(panel.elLk.sliderConfig[ui.value].name + ' bp').show();
       },
       change: function (e, ui) {      
-        var input = sliderConfig[ui.value];
+        var input = panel.elLk.sliderConfig[ui.value];
         var url   = input.href;
         var r     = url.match(panel.matchRegex)[1];
         
-        sliderLabel.html(input.name + ' bp');
+        panel.elLk.sliderLabel.html(input.name + ' bp');
         
         input = null;
         
@@ -112,12 +128,12 @@ Ensembl.Panel.LocationNav = Ensembl.Panel.extend({
         window.location.hash = 'r=' + r;
       },
       stop: function () {
-        sliderLabel.hide();
+        panel.elLk.sliderLabel.hide();
         $('.ui-slider-handle', panel.elLk.slider).trigger('blur'); // Force the blur event to remove the highlighting for the handle
       }
     });
   },
-  
+   
   getContent: function () {
     var panel = this;
     
@@ -151,5 +167,166 @@ Ensembl.Panel.LocationNav = Ensembl.Panel.extend({
         }
       });
     }
+  },
+  
+  autoCompleteInit: function () {
+    var panel = this;
+    
+    // On gene form submit, stop the request going to psychic search if the user has selected a gene from the dropdown,
+    // or has typed in something which matches (case insensitive) a name from the dropdown.
+    this.elLk.geneLocForm.bind('submit', function () {
+      var form  = this;
+      var g     = panel.elLk.g.val();
+      var query = panel.elLk.geneInput.val().toUpperCase();
+      
+      if (g) {
+        this.action = window.location.pathname;
+      } else {
+        panel.elLk.autoComplete.find('span.name').each(function () {
+          if ($(this).text().toUpperCase() == query) {
+            form.action = window.location.pathname;
+            panel.elLk.g.val($(this).siblings('.stable_id').text());
+            return true;
+          }
+        });
+      }
+    });
+    
+    this.elLk.geneInput.bind('keyup', function (e) {
+      var value = this.value;
+      
+      // Filter down existing results as the user types more
+      // Returns false if a new search term has been entered (the user deleted back past the limit of the current query)
+      // or if the current results do not contain the complete set for that query (panel.autoComplete.all == 0)
+      function filter() {
+        var show = false;
+        
+        if (panel.autoComplete.all && panel.autoComplete.query && value.match('^' + panel.autoComplete.query)) {
+          panel.elLk.autoComplete.children().each(function () {
+            if ($(this).text().match('^' + value, 'i')) {
+              $(this).show();
+              show = true;
+            } else {
+              $(this).hide();
+            }
+          }).end()[show ? 'show' : 'hide']();
+          
+          return true;
+        } else {
+          return false;
+        }
+      }
+      
+      // e.keyCode = 38: escape
+      if (e.keyCode == 27) {
+        panel.elLk.autoComplete.hide();
+        return;
+      }
+      
+      // e.keyCode = 38: up
+      // e.keyCode = 40: down
+      if (panel.elLk.autoComplete && (e.keyCode == 38 || e.keyCode == 40)) {
+        panel.elLk.autoComplete.children().removeClass('focused');
+        
+        if (panel.autoComplete.focused) {
+          if (!panel.autoComplete.focused[e.keyCode == 38 ? 'prev' : 'next']().trigger('mouseover', true).length) {
+            panel.elLk.geneInput.val(value);
+          }
+        } else {
+          panel.elLk.autoComplete.children(e.keyCode == 38 ? ':last' : ':first').trigger('mouseover', true);
+        }
+        
+        return;
+      }
+      
+      // e.keyCode = 8:       backspace
+      // e.keyCode = 32:      space
+      // e.keyCode = 46:      delete
+      // e.keyCode > 47:      alphanumeric/symbols
+      // e.keyCode = 111-123: F keys
+      if (
+        value.length < 3 || 
+        e.ctrlKey || e.altKey || 
+        (e.keyCode < 46 && e.keyCode != 8 && e.keyCode != 32) || 
+        (e.keyCode > 111 && e.keyCode < 124) || 
+        value.match(/^\w+:\d+/)
+      ) {
+        return;
+      }
+      
+      if (panel.autoComplete.reposition === true) {
+        panel.elLk.autoComplete.css(panel.autoCompletePosition());
+      }
+      
+      if (panel.timer) {
+        clearTimeout(panel.timer);
+      }
+      
+      if (panel.xhr) {
+        panel.xhr.abort();
+        panel.xhr = false;
+      }
+      
+      if (!filter()) {
+        panel.timer = setTimeout(function () {
+          panel.xhr = $.ajax({
+            url: Ensembl.speciesPath + '/autocomplete',
+            data: { q: value },
+            dataType: 'json',
+            success: function (json) {
+              panel.elLk.autoComplete.empty();
+              panel.autoComplete = { query: value, all: json.all };
+              
+              for (var i in json.results) {
+                $('<li>', {
+                  html: [ '<span class="name">', json.results[i][0], '</span><span class="stable_id">', json.results[i][1], '</span><input type="hidden" class="db" value="', json.results[i][2], '" />' ].join(''),
+                  click: function () {
+                    panel.elLk.geneInput.val($('.name', this).text());
+                    panel.elLk.g.val($('.stable_id', this).text());
+                    panel.elLk.db.val($('.db', this).val());
+                    panel.elLk.geneLocForm.trigger('submit');
+                  },
+                  mouseover: function (e, keyPress) {
+                    $(this).siblings().removeClass('focused');
+                    
+                    panel.autoComplete.focused = $(this).addClass('focused');
+                    
+                    if (keyPress === true) {
+                      panel.elLk.geneInput.val($('.name', this).text());
+                    }
+                  },
+                  mouseout: function () {
+                    $(this).removeClass('focused');
+                  }
+                }).appendTo(panel.elLk.autoComplete);
+              }
+              
+              if (!json.results.length) {
+                panel.elLk.autoComplete.hide();
+              } else {
+                panel.elLk.autoComplete.show();
+              }
+              
+              filter();
+            }
+          });
+        }, 100);
+      }
+    });
+  },
+  
+  autoCompletePosition: function () {
+    var pos = this.elLk.geneInput.position();
+    
+    return {
+      top:   pos.top + this.elLk.geneInput.innerHeight(),
+      left:  pos.left,
+      width: this.elLk.geneInput.innerWidth()
+    };
+  },
+  
+  resize: function () {
+    this.autoComplete.reposition = true;
+    this.elLk.autoComplete.hide();
   }
 });
