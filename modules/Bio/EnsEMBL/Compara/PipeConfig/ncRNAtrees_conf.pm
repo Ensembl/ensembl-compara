98b3ca793140ade7099fb58465808cd78cc7f065
Leo Gordon <lg4@ebi.ac.uk>
2010-10-19 20:36:01 +0000

    added species tree generation

--- modules/Bio/EnsEMBL/Compara/PipeConfig/ncRNAtrees_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/ncRNAtrees_conf.pm
@@ -11,7 +11,7 @@
 
 =head1 DESCRIPTION  
 
-    Load GenomeDB entries starting from a mlss_id or species_set_id.
+    Load GenomeDB entries, subsets and ncRNA members starting from a mlss_id or species_set_id (work in progress)
 
 =head1 CONTACT
 
@@ -30,13 +30,16 @@ sub default_options {
     return {
         %{$self->SUPER::default_options},
 
+        'mlss_id'           => 40066,
+
         'release'           => '60',
-        'rel_suffix'        => 'a',    # an empty string by default, a letter otherwise
+        'rel_suffix'        => 'b',    # an empty string by default, a letter otherwise
         'rel_with_suffix'   => $self->o('release').$self->o('rel_suffix'),
 
-        'email'             => $ENV{'USER'}.'@ebi.ac.uk',    # NB: your EBI address may differ from the Sanger one!
+        'ensembl_cvs_root_dir' => $ENV{'HOME'}.'/work',     # some Compara developers might prefer $ENV{'HOME'}.'/ensembl_main'
+        'work_dir'             => $ENV{'HOME'}.'/ncrna_trees_'.$self->o('rel_with_suffix'),
 
-        'work_dir'          => $ENV{'HOME'}.'/ncrna_trees_'.$self->o('rel_with_suffix'),
+        'email'             => $ENV{'USER'}.'@ebi.ac.uk',    # NB: your EBI address may differ from the Sanger one!
 
         'pipeline_db' => {                                  # connection parameters
             -host   => 'compara2',
@@ -60,13 +63,21 @@ sub default_options {
             -pass   => '',
         },
 
-        master_db => {
+        'master_db' => {
             -host   => 'compara1',
             -port   => 3306,
             -user   => 'ensro',
             -pass   => '',
             -dbname => 'sf5_ensembl_compara_master',
-        }
+        },
+
+        'rel_db' => {
+            -host   => 'compara1',
+            -port   => 3306,
+            -user   => 'ensro',
+            -pass   => '',
+            -dbname => 'kb3_ensembl_compara_60',
+        },
     };
 }
 
@@ -80,22 +91,98 @@ sub pipeline_wide_parameters {  # these parameter values are visible to all anal
     };
 }
 
+sub pipeline_create_commands {
+    my ($self) = @_;
+    return [
+        @{$self->SUPER::pipeline_create_commands},  # here we inherit creation of database, hive tables and compara tables
+        
+        'mkdir -p '.$self->o('work_dir'),
+    ];
+}
+
 
 sub pipeline_analyses {
     my ($self) = @_;
     return [
+
+# ---------------------------------------------[copy tables from master and fix the offsets]---------------------------------------------
+
+        {   -logic_name => 'copy_table_factory',
+            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::JobFactory',
+            -parameters => {
+                'db_conn'   => $self->o('master_db'),
+                'inputlist' => [ 'method_link', 'species_set', 'method_link_species_set', 'ncbi_taxa_name', 'ncbi_taxa_node' ],
+                'input_id'  => { 'src_db_conn' => '#db_conn#', 'table' => '#_range_start#' },
+                'fan_branch_code' => 2,
+            },
+            -input_ids => [
+                {},
+            ],
+            -flow_into => {
+                2 => [ 'copy_table'  ],
+                1 => [ 'offset_tables' ],  # backbone
+            },
+        },
+
+        {   -logic_name    => 'copy_table',
+            -module        => 'Bio::EnsEMBL::Hive::RunnableDB::MySQLTransfer',
+            -parameters    => {
+                'mode'          => 'overwrite',
+            },
+            -hive_capacity => 10,
+        },
+
+        {   -logic_name => 'offset_tables',
+            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SqlCmd',
+            -parameters => {
+                'sql'   => [
+                    'ALTER TABLE member   AUTO_INCREMENT=200000001',
+                    'ALTER TABLE sequence AUTO_INCREMENT=200000001',
+                    'ALTER TABLE homology AUTO_INCREMENT=100000001',
+                ],
+            },
+            -wait_for => [ 'copy_table_factory', 'copy_table' ],    # have to wait until the tables have been copied
+            -flow_into => {
+                1 => [ 'innodbise_table_factory' ],
+            },
+        },
+
+# ---------------------------------------------[turn all tables except 'genome_db' to InnoDB]---------------------------------------------
+
+        {   -logic_name => 'innodbise_table_factory',
+            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::JobFactory',
+            -parameters => {
+                'inputquery'      => "SELECT table_name FROM information_schema.tables WHERE table_schema ='".$self->o('pipeline_db','-dbname')."' AND table_name!='genome_db' AND engine='MyISAM' ",
+                'input_id'        => { 'table_name' => '#_range_start#' },
+                'fan_branch_code' => 2,
+            },
+            -flow_into => {
+                2 => [ 'innodbise_table'  ],
+                1 => [ 'load_genomedb_factory' ],
+            },
+        },
+
+        {   -logic_name    => 'innodbise_table',
+            -module        => 'Bio::EnsEMBL::Hive::RunnableDB::SqlCmd',
+            -parameters    => {
+                'sql'         => "ALTER TABLE #table_name# ENGINE=InnoDB",
+            },
+            -hive_capacity => 10,
+        },
+
+# ---------------------------------------------[load GenomeDB entries from master+cores]---------------------------------------------
+
         {   -logic_name => 'load_genomedb_factory',
             -module     => 'Bio::EnsEMBL::Compara::RunnableDB::LoadGenomedbFactory',
             -parameters => {
-                'compara_db'           => $self->o('master_db'),
+                'compara_db'    => $self->o('master_db'),   # that's where genome_db_ids come from
+                'mlss_id'       => $self->o('mlss_id'),
             },
-            -input_ids  => [
-                { 'mlss_id' => 40066 },
-            ],
+            -wait_for  => [ 'innodbise_table_factory', 'innodbise_table' ],
             -flow_into => {
                 2 => [ 'load_genomedb' ],
+                1 => [ 'create_species_tree' ],
             },
-            -rc_id => 1,
         },
 
         {   -logic_name => 'load_genomedb',
@@ -104,14 +191,62 @@ sub pipeline_analyses {
                 'registry_dbs'  => [ $self->o('reg1'), $self->o('reg2'), ],
             },
             -flow_into => {
-                1 => [ 'dummy' ],   # each will flow into another one
+                1 => [ 'load_members_factory' ],   # each will flow into another one
             },
         },
 
-        {   -logic_name    => 'dummy',
-            -module        => 'Bio::EnsEMBL::Hive::RunnableDB::Dummy',
-            -hive_capacity => 10,       # allow several workers to perform identical tasks in parallel
+# ---------------------------------------------[load ncRNA and gene members and subsets]---------------------------------------------
+
+        {   -logic_name    => 'load_members_factory',
+            -module        => 'Bio::EnsEMBL::Compara::RunnableDB::GenomePrepareNCMembers',
+            -hive_capacity => 10,
+#temporary:
+-wait_for => [ 'create_species_tree', 'store_species_tree' ],
+            -flow_into => {
+                2 => [ 'load_members' ],   # per-genome fan
+            },
         },
+
+        {   -logic_name    => 'load_members',
+            -module        => 'Bio::EnsEMBL::Compara::RunnableDB::GeneStoreNCMembers',
+            -hive_capacity => 20,
+#temporary:
+-wait_for => [ 'create_species_tree', 'store_species_tree' ],
+            -flow_into => {
+                3 => [ 'mysql:////subset_member' ],   # every ncrna member is added to the corresponding subset
+                4 => [ 'mysql:////subset_member' ],   # every gene  member is added to the corresponding subset
+            },
+        },
+
+# ---------------------------------------------[load species tree]-------------------------------------------------------------------
+
+        {   -logic_name    => 'create_species_tree',
+            -module        => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+            -parameters    => {
+                'db_url'   => $self->dbconn_2_url('pipeline_db'),
+                'species_tree_file' => $self->o('work_dir').'/nctree_spec_tax.nh',
+                'cmd'      => $self->o('ensembl_cvs_root_dir').'/ensembl-compara/scripts/tree/testTaxonTree.pl -url #db_url# -create_species_tree -multifurcation_deletes_node 33316_129949_314146 -multifurcation_deletes_all_subnodes 9347_186625_32561 -njtree_output_filename #species_tree_file# -no_other_files 2>/dev/null',
+            },
+            -wait_for => [ 'load_genomedb_factory', 'load_genomedb' ],  # have to wait for both to complete (so is a funnel)
+            -flow_into => {
+                1 => { 'store_species_tree' => { 'species_tree_file' => '#species_tree_file#' } },
+            },
+        },
+
+        {   -logic_name    => 'store_species_tree',
+            -module        => 'Bio::EnsEMBL::Hive::RunnableDB::JobFactory',
+            -parameters => {
+                'inputcmd'        => 'cat #species_tree_file#',
+                'input_id'        => { 'node_id' => 1, 'tag' => 'species_tree_string', 'value' => '#_range_start#' },
+                'fan_branch_code' => 3,
+            },
+            -flow_into => {
+                3 => [ 'mysql:////nc_tree_tag' ],
+
+                # can continue the backbone from here (but make sure to sync with the double fan!)
+            },
+        },
+
     ];
 }
 
