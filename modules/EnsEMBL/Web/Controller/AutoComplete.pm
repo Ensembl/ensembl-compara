07044052466fbc18cde0906161837d6553fab24d
Simon Brent <sb23@sanger.ac.uk>
2010-10-20 09:48:34 +0000

    take the limiter off

--- modules/EnsEMBL/Web/Controller/AutoComplete.pm
+++ modules/EnsEMBL/Web/Controller/AutoComplete.pm
@@ -19,20 +19,11 @@ sub new {
   
   my $hub = new EnsEMBL::Web::Hub;
   my $dbh = new EnsEMBL::Web::DBSQL::WebsiteAdaptor($hub)->db;
-  my $q   = $hub->param('q');
-  my $all = 1;
-  my $sth = $dbh->prepare(sprintf 'select display_label, stable_id, db from gene_autocomplete where species = "%s" and display_label like %s', $hub->species, $dbh->quote("$q%"));
+  my $sth = $dbh->prepare(sprintf 'select display_label, stable_id, db from gene_autocomplete where species = "%s" and display_label like %s', $hub->species, $dbh->quote($hub->param('q') . '%'));
   
   $sth->execute;
   
-  my $results = $sth->fetchall_arrayref;
-  
-  if (scalar @$results > 10) {
-    splice @$results, 10;
-    $all = 0;
-  }
-  
-  print $self->jsonify({ results => $results, all => $all });
+  print $self->jsonify($sth->fetchall_arrayref);
   
   return $self;
 }
