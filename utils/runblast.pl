7ce9cb3b08bbc047b9c9c4f33dc664ce9766ccf7
Nick Langridge <nickl@ebi.ac.uk>
2010-10-22 08:08:38 +0000

    runblast.pl now loads plugins before running blast

--- utils/runblast.pl
+++ utils/runblast.pl
@@ -6,6 +6,7 @@ use Storable;
 
 use FindBin qw($Bin);
 use File::Basename qw( dirname );
+use File::Find;
 
 # --- load libraries needed for reading config ---
 use vars qw( $SERVERROOT );
@@ -17,13 +18,63 @@ BEGIN{
   map{ unshift @INC, $_ } @SiteDefs::ENSEMBL_LIB_DIRS;
 }
 
+use EnsEMBL::Web::SpeciesDefs;
+my $SPECIES_DEFS = EnsEMBL::Web::SpeciesDefs->new();
+
+#---- load plugins ---
+# Ensure that plugins are loaded before running the BLAST
+# This code copied from conf/perl.startup.pl
+my ($i, @plugins, @plugin_dirs);
+my @lib_dirs = @{$SPECIES_DEFS->ENSEMBL_LIB_DIRS};
+
+for (reverse @{$SPECIES_DEFS->ENSEMBL_PLUGINS||[]}) {
+  if (++$i % 2) {
+    push @plugin_dirs, "$_/modules" if -e "$_/modules";
+  } else {
+    unshift @plugins, $_;
+  }
+}
+
+unshift @INC, reverse @plugin_dirs; # Add plugin directories to INC so that EnsEMBL::PLUGIN modules can be used
+
+find(\&load_plugins, @plugin_dirs);
+
+# Loop through the plugin directories, requiring .pm files
+# The effect of this is that any plugin with an EnsEMBL::Web:: namespace is used to extend that module in the core directory
+# Functions that exist in both the core and the plugin will be overwritten, functions that exist only in the plugin will be added
+sub load_plugins {
+  if (/\.pm$/ && !/MetaDataBlast\.pm/) {
+    my $dir  = $File::Find::topdir;
+    my $file = $File::Find::name;
+    
+    (my $relative_file = $file) =~ s/^$dir\///;    
+    (my $package = $relative_file) =~ s/\//::/g;
+    $package =~ s/\.pm$//g;
+    
+    # Regex matches all namespaces which are EnsEMBL:: but not EnsEMBL::Web
+    # Therefore the if statement is true for EnsEMBL::Web:: and Bio:: packages, which are the ones we need to overload
+    if ($package !~ /^EnsEMBL::(?!Web)/) {      
+      no strict 'refs';
+      
+      # Require the base module first, unless it already exists
+      if (!exists ${"$package\::"}{'ISA'}) {
+        foreach (@lib_dirs) {
+          eval "require '$_/$relative_file'";
+          warn $@ if $@ && $@ !~ /^Can't locate/;
+          last if exists ${"$package\::"}{'ISA'};
+        }
+      }
+      
+      eval "require '$file'"; # Require the plugin module
+      warn $@ if $@;
+    }
+  }
+}
+#--- finished loading plugins ---
 
 use Bio::Tools::Run::Search;
 use Bio::Search::Result::EnsemblResult;
 use EnsEMBL::Web::DBSQL::DBConnection;
-use EnsEMBL::Web::SpeciesDefs;
-
-#warn( "Storable VERSION: $Storable::VERSION\n" );
 
 my $token = shift @ARGV;
 if( ! $token ){ die( "runblast.pl called with no token" ) }
@@ -31,7 +82,7 @@ my @bits = split( '/', $token );
 my $ticket = join( '',$bits[-3],$bits[-2] ); # Ticket = 3rd + 2nd to last dirs
 
 # Retrieve the runnable object
-my $SPECIES_DEFS = EnsEMBL::Web::SpeciesDefs->new();
+
 my $DBCONNECTION = EnsEMBL::Web::DBSQL::DBConnection->new( undef, $SPECIES_DEFS );
 
 my $blast_adaptor = $DBCONNECTION->get_databases_species( $SPECIES_DEFS->ENSEMBL_PRIMARY_SPECIES, 'blast')->{'blast'};
