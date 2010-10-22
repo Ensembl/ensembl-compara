70214780e0b39bfc50b7d10aa303cb22e40784e7
Simon Brent <sb23@sanger.ac.uk>
2010-10-22 13:14:58 +0000

    moved user to hub

--- modules/EnsEMBL/Web/Registry.pm
+++ modules/EnsEMBL/Web/Registry.pm
@@ -4,7 +4,6 @@ package EnsEMBL::Web::Registry;
 
 use strict;
 
-use EnsEMBL::Web::Data::User;
 use EnsEMBL::Web::SpeciesDefs;
 use EnsEMBL::Web::Timer;
 
@@ -12,9 +11,8 @@ sub new {
   my $class = shift;
   
   my $self = {
-    timer         => undef,
-    species_defs  => undef,
-    user          => undef,
+    timer        => undef,
+    species_defs => undef
   };
   
   bless $self, $class;
@@ -23,33 +21,6 @@ sub new {
 
 sub species_defs { return $_[0]{'species_defs'} ||= new EnsEMBL::Web::SpeciesDefs; }
 sub timer        { return $_[0]{'timer'}        ||= new EnsEMBL::Web::Timer;       }
-
 sub timer_push   { shift->timer->push(@_); }
-
-sub user :lvalue { $_[0]{'user'};  }
-
-sub initialize_user {
-  my ($self, $cookie, $r) = @_;
-  
-  $cookie->retrieve($r);
-
-  my $id = $cookie->get_value;
-
-  if ($id) {
-    my $user;
-    
-    # try to log in with user id from cookie
-    eval { 
-      $self->user = new EnsEMBL::Web::Data::User($id);
-    };
-      
-    if ($@) {
-      # login failed (because the connection to the used db has gone away)
-      # so log the user out by clearing the cookie
-      $cookie->clear($r);
-      $self->user = undef;
-    }
-  }
-}
 
 1;
