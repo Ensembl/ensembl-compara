d5a6d82c0cbd9c3ee232edada18561721e5f4868
Simon Brent <sb23@sanger.ac.uk>
2010-10-22 13:11:16 +0000

    changed SpeciesList

--- modules/EnsEMBL/Web/Command/Account/SaveFavourites.pm
+++ modules/EnsEMBL/Web/Command/Account/SaveFavourites.pm
@@ -1,25 +1,27 @@
+# $Id$
+
 package EnsEMBL::Web::Command::Account::SaveFavourites;
 
 use strict;
-use warnings;
 
-use EnsEMBL::Web::Data::User;
+use EnsEMBL::Web::Data::Record::SpeciesList;
 use EnsEMBL::Web::Document::HTML::SpeciesList;
 
 use base qw(EnsEMBL::Web::Command);
 
 sub process {
   my $self = shift;
-  my $user = $self->object->user;
+  my $hub  = $self->hub;
+  my $user = $hub->user;
   
   my ($species_list) = $user->specieslists;
   $species_list    ||= new EnsEMBL::Web::Data::Record::SpeciesList::User;
     
-  $species_list->favourites($self->object->param('favourites'));
+  $species_list->favourites($hub->param('favourites'));
   $species_list->user_id($user->id);
   $species_list->save;
 
-  print EnsEMBL::Web::Document::HTML::SpeciesList->render('fragment');
+  print new EnsEMBL::Web::Document::HTML::SpeciesList($hub)->render('fragment');
 }
 
 1;
