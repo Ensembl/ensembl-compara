5e99483e647fc4dcb532b3fae7d60928fe763850
Simon Brent <sb23@sanger.ac.uk>
2010-10-25 08:56:35 +0000

    fixed

--- modules/EnsEMBL/Web/Component/LRG/LRGSNPTable.pm
+++ modules/EnsEMBL/Web/Component/LRG/LRGSNPTable.pm
@@ -15,15 +15,10 @@ sub _init {
 sub content {
   my $self        = shift;
   my $hub         = $self->hub;
-  my $lrg         = $self->configure($hubparam('context') || 'FULL', $hub->get_imageconfig('lrgsnpview_transcript'));
+  my $lrg         = $self->configure($hub->param('context') || 'FULL', $hub->get_imageconfig('lrgsnpview_transcript'));
   my @transcripts = sort { $a->stable_id cmp $b->stable_id } @{$lrg->get_all_transcripts};
-  my $lrg_slice   = $lrg->Obj->feature_Slice;
-  my $tables      = {};
-
-###
-  my $table_rows = $self->variation_table(\@transcripts, $lrg_slice);
-  
-  my $table = $self->make_table($table_rows) if $table_rows;
+  my $table_rows  = $self->variation_table(\@transcripts, $lrg->Obj->feature_Slice);
+  my $table       = $table_rows ? $self->make_table($table_rows) : undef;
   
   return $self->render_content($table);
 }
@@ -32,18 +27,18 @@ sub make_table {
   my ($self, $table_rows) = @_;
   
   my $columns = [
-    { key => 'ID',        sort => 'html'                                                   },
-    { key => 'chr' ,      sort => 'position', title => 'Chr: bp'                           },
-    { key => 'Alleles',   sort => 'string',   align => 'center'                            },
-    { key => 'Ambiguity', sort => 'string',   align => 'center'                            },
-    { key => 'HGVS',      sort => 'string',   title => 'HGVS name(s)',   align => 'center' },
-    { key => 'class',     sort => 'string',   title => 'Class',          align => 'center' },
-    { key => 'Source',    sort => 'string'                                                 },
-    { key => 'status',    sort => 'string',   title => 'Validation',     align => 'center' },
-    { key => 'snptype',   sort => 'string',   title => 'Type',                             },
-    { key => 'aachange',  sort => 'string',   title => 'Amino Acid',     align => 'center' },
-    { key => 'aacoord',   sort => 'position', title => 'AA co-ordinate', align => 'center' },
-    { key => 'Transcript', sort => 'string'},
+    { key => 'ID',         sort => 'html'                                                   },
+    { key => 'chr' ,       sort => 'position', title => 'Chr: bp'                           },
+    { key => 'Alleles',    sort => 'string',                              align => 'center' },
+    { key => 'Ambiguity',  sort => 'string',                              align => 'center' },
+    { key => 'HGVS',       sort => 'string',   title => 'HGVS name(s)',   align => 'center' },
+    { key => 'class',      sort => 'string',   title => 'Class',          align => 'center' },
+    { key => 'Source',     sort => 'string'                                                 },
+    { key => 'status',     sort => 'string',   title => 'Validation',     align => 'center' },
+    { key => 'snptype',    sort => 'string',   title => 'Type',                             },
+    { key => 'aachange',   sort => 'string',   title => 'Amino Acid',     align => 'center' },
+    { key => 'aacoord',    sort => 'position', title => 'AA co-ordinate', align => 'center' },
+    { key => 'Transcript', sort => 'string'                                                 }
   ];
   
   return $self->new_table($columns, $table_rows, { data_table => 1, sorting => [ 'chr asc' ] });
