e40848577327aea22ecb7ccfff721983fb0ccdbd
Anne Parker <ap5@sanger.ac.uk>
2010-10-26 14:13:40 +0000

    Improved configuration parsing

--- modules/EnsEMBL/Web/Text/FeatureParser/WIG.pm
+++ modules/EnsEMBL/Web/Text/FeatureParser/WIG.pm
@@ -9,58 +9,59 @@ sub parse_row {
   my( $self, $row ) = @_;
   my $columns;
 
-  if ($row =~ /variableStep\s+chrom=(chr)?([^\s]+)(\s+span=)?(\d+)?/i) {
-    my $wigConfig = {
-		  'format' => 'v',
-		  'region' => $2,
-		  'span' => $4 || 1,
-	  };
-	  $self->{'tracks'}{ $self->current_key }->{'mode'} = $wigConfig;
-  } 
-  elsif ($row =~ /fixedStep\s+chrom=(chr)?(.+)\s+start=(\d+)\s+step=(\d+)(\s+span=)?(\d+)?/i) {
-	  my $wigConfig = {
-		  'format' => 'f',
-		  'region' => $2,
-		  'span' => $6 || 1,
-		  'start' => $3,
-		  'step' => $4,
-	  };
-	  $self->{'tracks'}{ $self->current_key }->{'mode'} = $wigConfig;
+  $row =~ s/^\s+//;
+  my @ws_delimited = split /\s+/, $row;
+  push @ws_delimited, $ws_delimited[0];
+
+  my $wigConfig = $self->{'tracks'}{ $self->current_key }->{'mode'};
+
+  if ($wigConfig->{format}) {
+    if ($wigConfig->{format} eq 'v') {
+      $columns = [
+                $wigConfig->{'region'}, 
+                $ws_delimited[0], 
+                $ws_delimited[0] + $wigConfig->{span} - 1, 
+                $ws_delimited[1], 
+                $ws_delimited[2]
+      ];
+    } 
+    elsif ($wigConfig->{format} eq 'f') {
+      $columns = [
+                $wigConfig->{'region'}, 
+                $wigConfig->{start}, 
+                $wigConfig->{start} + $wigConfig->{span} - 1, 
+                $ws_delimited[0], 
+                $ws_delimited[1]
+      ];
+      $self->{'tracks'}{ $self->current_key }->{'mode'}->{'start'} += $wigConfig->{step};
+    }
 	} 
   else {
-    ## Actual data row
-    $row =~ s/^\s+//;
-	  my @ws_delimited = split /\s+/, $row;
-	  push @ws_delimited, $ws_delimited[0];
-
-	  my $wigConfig = $self->{'tracks'}{ $self->current_key }->{'mode'};
-	  if ($wigConfig->{format}) {
-		  if ($wigConfig->{format} eq 'v') {
-		    $columns = [
-                    $wigConfig->{'region'}, 
-                    $ws_delimited[0], 
-                    $ws_delimited[0] + $wigConfig->{span} - 1, 
-                    $ws_delimited[1], 
-                    $ws_delimited[2]
-        ];
-		  } 
-      elsif ($wigConfig->{format} eq 'f') {
-		    $columns = [
-                    $wigConfig->{'region'}, 
-                    $wigConfig->{start}, 
-                    $wigConfig->{start} + $wigConfig->{span} - 1, 
-                    $ws_delimited[0], 
-                    $ws_delimited[1]
-        ];
-		    $self->{'tracks'}{ $self->current_key }->{'mode'}->{'start'} += $wigConfig->{step};
-		  }
-	  } 
-    else {
-		  $columns = \@ws_delimited;
-    }
-	}
+		$columns = \@ws_delimited;
+  }
   return $columns; 
 }
 
+sub set_wig_config {
+### Set additional parameters needed by WIG display
+  my $self = shift;
+  my $track = $self->{'tracks'}{ $self->current_key };
+  my $config = $track->{'config'};
+
+  my $wig_config = {
+		'region'  => $config->{'chrom'},
+		'span'    => $config->{'span'} || 1,
+  };
+
+  if (defined $config->{'variableStep'}) {
+    $wig_config->{'format'} = 'v';
+  } 
+  elsif (defined $config->{'fixedStep'}) {
+    $wig_config->{'format'} = 'f';
+	  $wig_config->{'start'}  = $config->{'start'};
+	  $wig_config->{'step'}   = $config->{'step'};
+	} 
+	$track->{'mode'} = $wig_config;
+}
 
 1;
