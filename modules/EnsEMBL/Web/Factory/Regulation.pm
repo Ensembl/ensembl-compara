2d14520d449fcdbf11ad45ff560064ad36b6735a
Beth Yates <bp1@sanger.ac.uk>
2010-11-01 09:40:35 +0000

    fixed error url param

--- modules/EnsEMBL/Web/Factory/Regulation.pm
+++ modules/EnsEMBL/Web/Factory/Regulation.pm
@@ -48,7 +48,7 @@ sub _help {
 
   my %sample    = %{$self->species_defs->SAMPLE_DATA || {}};
   my $help_text = $string ? sprintf '<p>%s</p>', encode_entities($string) : '';
-  my $url       = $self->hub->url({ __clear => 1, action => 'Cell_line', g => $sample{'REGULATION_PARAM'} });
+  my $url       = $self->hub->url({ __clear => 1, action => 'Cell_line', rf => $sample{'REGULATION_PARAM'} });
   
   $help_text .= sprintf('
   <p>
