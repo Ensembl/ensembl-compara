c60261dd3ad0f98ebf4682c23769e2b8c21dbf60
Beth Yates <bp1@sanger.ac.uk>
2010-11-01 09:30:19 +0000

    add in TFBS information

--- modules/EnsEMBL/Web/ZMenu/FeatureEvidence.pm
+++ modules/EnsEMBL/Web/ZMenu/FeatureEvidence.pm
@@ -7,11 +7,42 @@ use strict;
 use base qw(EnsEMBL::Web::ZMenu);
 
 sub content {
-  my $self        = shift;
-  my $hub         = $self->hub;
-  my $db_adaptor  = $hub->database($hub->param('fdb'));
-  my $feature_set = $db_adaptor->get_FeatureSetAdaptor->fetch_by_name($hub->param('fs'));
- 
+  my $self              = shift;
+  my $hub               = $self->hub;
+  my $db_adaptor        = $hub->database($hub->param('fdb'));
+  my $feature_set       = $db_adaptor->get_FeatureSetAdaptor->fetch_by_name($hub->param('fs')); 
+  my ($chr, $start, $end) = split (/\:|\-/g, $hub->param('pos'));
+  my $slice             = $hub->database('core')->get_SliceAdaptor->fetch_by_region('toplevel', $chr, $start, $end);
+
+  my @a_features = @{$db_adaptor->get_AnnotatedFeatureAdaptor->fetch_all_by_Slice($slice)};
+  my $annotated_feature;
+  foreach ( @a_features) {
+    if ($_->feature_set->display_label eq $feature_set->display_label) { $annotated_feature = $_; }
+  }
+
+  my $summit         = $annotated_feature->summit || 'undetermined';
+  my @features = @{$annotated_feature->get_associated_MotifFeatures};
+  my %motif_features;
+  foreach my $mf (@features){
+    my %assoc_ftype_names;
+    map {$assoc_ftype_names{$_->feature_type->name} = undef} @{$mf->associated_annotated_features};
+    my $bm_ftname = $mf->binding_matrix->feature_type->name;
+    my @other_ftnames;
+    foreach my $af_ftname(keys(%assoc_ftype_names)){
+      push @other_ftnames, $af_ftname if $af_ftname ne $bm_ftname;
+    }
+
+    my $other_names_txt = '';
+
+    if(@other_ftnames){
+      $other_names_txt = ' ('.join(' ', @other_ftnames).')';
+    }
+
+    $motif_features{$mf->start .':'. $mf->end} = [ $bm_ftname.$other_names_txt,  $mf->score];
+  }
+  
+
+
   $self->caption('Evidence');
   
   $self->add_entry({
@@ -27,7 +58,14 @@ sub content {
   if ($hub->param('ps') !~ /undetermined/) {
     $self->add_entry({
       type  => 'Peak summit',
-      label => $hub->param('ps')
+      label => $summit
+    });
+  }
+  foreach my $motif (sort keys %motif_features){
+    my ($name, $score) = @{$motif_features{$motif}};
+    $self->add_entry({
+      type  => $name,
+      label => $score
     });
   }
 }
--- modules/EnsEMBL/Web/ZMenu/Regulation.pm
+++ modules/EnsEMBL/Web/ZMenu/Regulation.pm
@@ -27,6 +27,9 @@ sub content {
       $core_reg_obj = $rf;
     }
   }
+
+  my $cell_line_specific_reg_object = $self->new_object('Regulation', $core_reg_obj, $object->__data);
+  my %motif_features      = %{$cell_line_specific_reg_object->get_motif_features};
   
   $self->caption('Regulatory Feature');
   
@@ -47,18 +50,38 @@ sub content {
     link  => $object->get_location_url
   });
 
-  unless ($object->bound_start == $object->seq_region_start && $object->bound_end == $object->seq_region_end) {
+  unless ($cell_line_specific_reg_object->bound_start == $object->seq_region_start && $cell_line_specific_reg_object->bound_end == $object->seq_region_end) {
     $self->add_entry({
       type  => 'Bounds bp',
-      label => $object->bound_location_string,
-      link  => $object->get_bound_location_url
+      label => $cell_line_specific_reg_object->bound_location_string,
+      link  => $cell_line_specific_reg_object->get_bound_location_url
     });
   }
   
   $self->add_entry({
     type  => 'Attributes',
-    label => $object->get_attribute_list
+    label => $cell_line_specific_reg_object->get_attribute_list
   });
+
+  if (scalar (keys %motif_features) >> 0  ){
+    $self->add_subheader('PWM Information');
+    $self->add_entry({
+      type  => 'ID:',
+      label => 'Score:'
+    });  
+ 
+    foreach my $motif (sort keys %motif_features){
+      my ($name, $score, $binding_matrix_name) = @{$motif_features{$motif}};
+      
+      $self->add_entry({
+        type  => $name,
+        label => $score,
+        link  => $self->hub->get_ExtURL_link($score, 'JASPAR', $binding_matrix_name),
+        extra => { abs_url => 1 },
+      });
+    } 
+  }
 }
+
 
 1;
