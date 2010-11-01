81085d5d4637d15e2b3f31931ad037ee12d4a788
Beth Yates <bp1@sanger.ac.uk>
2010-11-01 15:48:01 +0000

    added more info

--- modules/EnsEMBL/Web/Component/StructuralVariation/Summary.pm
+++ modules/EnsEMBL/Web/Component/StructuralVariation/Summary.pm
@@ -19,16 +19,51 @@ sub content {
   my $source              = $object->source;
   my $source_description  = $object->source_description;
  
-  $name = "$class ( $name source $source - $source_description)";
+  $name = "$class ($name)";
+  my $source_link = $hub->get_ExtURL_link($source, 'DGVA', $source);
+  if($source_description =~/PMID/){ 
+    my @temp = split('\s', $source_description);
+    foreach (@temp ){
+      if ($_ =~/PMID/){
+        my $pubmed_id = $_; 
+        my $id = $pubmed_id;  
+        $id =~s/PMID\://; 
+        my $pubmed_url = $hub->get_ExtURL_link($pubmed_id, 'PUBMED', $id); 
+        $source_description =~s/$_/$pubmed_url/;
+      }
+    }
+  }   
+
+  $source = "$source_link - $source_description";
+ 
+  my $location = $object->neat_sr_name($object->Obj->slice->seq_region_name .":" . $object->Obj->start ."-" . $object->Obj->end);
+  my $strand = $object->Obj->strand > 0 ? "forward" : "reverse";
+  my $location_url = $self->hub->url({
+    'type'   => 'Location',
+    'action' => 'View',
+    'r'      => $object->Obj->slice->seq_region_name.':'.$object->Obj->start.'-'.$object->Obj->end
+  });
+  $location_url .= "contigviewbottom=variation_feature_structural=normal";
+
+  my $location_html = sprintf( '<a href="%s">%s</a>.',
+    $location_url,
+    'View in location tab'
+  );
+
+  my $location_link = "This feature maps to $location ($strand strand) | $location_html";
  
   my $html = qq{
     <dl class="summary">
-      <dt> Variation class </dt>
+      <dt>Variation class</dt>
       <dd>$name</dd>
+      <dt>Source</dt>
+      <dd>$source</dd>
+      <dt>Location<dt>
+      <dd>$location_link</dd> 
+    </dl>
   };
  
 
-  $html .= "</dl>";
   return $html;
 }
 
