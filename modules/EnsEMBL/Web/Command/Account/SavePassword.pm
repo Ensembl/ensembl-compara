5c3e811ab65c02256302366964490f8787555830
Harpreet Riat <hr5@sanger.ac.uk>
2010-11-03 16:40:10 +0000

    Password validation added

--- modules/EnsEMBL/Web/Command/Account/SavePassword.pm
+++ modules/EnsEMBL/Web/Command/Account/SavePassword.pm
@@ -14,29 +14,40 @@ sub process {
 
   my $user = EnsEMBL::Web::Data::User->find(email => $object->param('email'));
 
-  $user->password(encryptPassword($object->param('new_password_1')));
-  $user->status('active');
-  $user->modified_by($user->id);
-  $user->save;
-
-  my $param = {
-    'email'     => $user->email,
-    'password'  => $object->param('new_password_1'),
-    'url'       => $object->param('url'),
-    'updated'   => 'yes',
-  };
-
-  ## Password change is already logged in, so stay in control panel 
-  my $new_url;
-  if ($object->param('code')) {
-    $new_url = '/Account/SetCookie';
-    $param->{'activated'} = 'yes';
-  } 
+  my $error = 0;
+  my $message;
+  my $new_password = $object->param('new_password_1');
+  if ($new_password ne $object->param('new_password_2')) {
+    $message = 'password_not_confirmed';
+    $error   = 1;
+  }
+  elsif ($new_password !~ /^\S{6,32}$/) {
+    $message = 'invalid_password';
+    $error   = 1;
+  }
   else {
-    $new_url = '/Account/QuickLinks';
+    $user->password(encryptPassword($new_password));
+    $user->status('active');
+    $user->modified_by($user->id);
+    $user->save;
+    $message = 'password_saved';
+  }
+  
+  if ($object->param('code')) { # if password reset after "password lost" request
+    $self->ajax_redirect('/Account/SetCookie', {
+      'email'     => $user->email,
+      'password'  => $object->param('new_password_1'),
+      'url'       => $object->param('url'),
+      'activated' => 'yes',
+      'updated'   => 'yes',
+    });
   }
 
-  $self->ajax_redirect($new_url, $param);
+  else {  # if password changed
+    my $params = { 'error' => $error, 'message' => $message };
+    $params->{'back'} = $object->param('backlink') if $error && $object->param('backlink');
+    $self->ajax_redirect('/Account/Message', $params);
+  }
 }
 
 1;
