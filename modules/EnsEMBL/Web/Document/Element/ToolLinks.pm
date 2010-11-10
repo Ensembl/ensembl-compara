8f81a9f9cc038c43edad32b411c20ffa68a4b8d2
Simon Brent <sb23@sanger.ac.uk>
2010-11-10 15:45:26 +0000

    fix account links on pages with no species

--- modules/EnsEMBL/Web/Document/Element/ToolLinks.pm
+++ modules/EnsEMBL/Web/Document/Element/ToolLinks.pm
@@ -13,24 +13,24 @@ sub blast   :lvalue { $_[0]{'blast'};   }
 sub biomart :lvalue { $_[0]{'biomart'}; }
 
 sub content {
-  my $self      = shift;
-  my $hub       = $self->hub;
-  my $species   = $hub->species;
-  my $blast_dir = !$species || $species eq 'Multi' || $species eq 'common' ? 'Multi' : $species;
+  my $self    = shift;
+  my $hub     = $self->hub;
+  my $species = $hub->species;
+     $species = !$species || $species eq 'Multi' || $species eq 'common' ? 'Multi' : $species;
   my @links;
   
   if ($self->logins) {
     if ($hub->user) {
-      push @links, sprintf '<a class="constant modal_link" style="display:none" href="%s">Account</a>',  $hub->url({ __clear => 1, type => 'Account', action => 'Links'  });
-      push @links, sprintf '<a class="constant" href="%s">Logout</a>',                                   $hub->url({ __clear => 1, type => 'Account', action => 'Logout' });
+      push @links, sprintf '<a class="constant modal_link" style="display:none" href="%s">Account</a>',  $hub->url({ __clear => 1, species => $species, type => 'Account', action => 'Links'  });
+      push @links, sprintf '<a class="constant" href="%s">Logout</a>',                                   $hub->url({ __clear => 1, species => $species, type => 'Account', action => 'Logout' });
     } else {
-      push @links, sprintf '<a class="constant modal_link" style="display:none" href="%s">Login</a>',    $hub->url({ __clear => 1, type => 'Account', action => 'Login' });
-      push @links, sprintf '<a class="constant modal_link" style="display:none" href="%s">Register</a>', $hub->url({ __clear => 1, type => 'Account', action => 'User', function => 'Add' });
+      push @links, sprintf '<a class="constant modal_link" style="display:none" href="%s">Login</a>',    $hub->url({ __clear => 1, species => $species, type => 'Account', action => 'Login' });
+      push @links, sprintf '<a class="constant modal_link" style="display:none" href="%s">Register</a>', $hub->url({ __clear => 1, species => $species, type => 'Account', action => 'User', function => 'Add' });
     }
   }
   
-  push @links, qq{<a class="constant" href="/$blast_dir/blastview">BLAST/BLAT</a>} if $self->blast;
-  push @links,   '<a class="constant" href="/biomart/martview">BioMart</a>'        if $self->biomart;
+  push @links, qq{<a class="constant" href="/$species/blastview">BLAST/BLAT</a>} if $self->blast;
+  push @links,   '<a class="constant" href="/biomart/martview">BioMart</a>'      if $self->biomart;
   push @links,   '<a class="constant" href="/tools.html">Tools</a>';
   push @links,   '<a class="constant" href="/downloads.html">Downloads</a>';
   push @links,   '<a class="constant" href="/help.html">Help</a>';
