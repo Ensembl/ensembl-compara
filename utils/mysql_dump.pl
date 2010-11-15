1bc4172f2ec04476e6c87bc9023d77954c1c97c0
ridwan <ma7@sanger.ac.uk>
2010-11-15 12:48:25 +0000

    adding path directory option

--- utils/mysql_dump.pl
+++ utils/mysql_dump.pl
@@ -1,27 +1,28 @@
 #!/usr/local/bin/perl
 
-# Script for dumping the mysql files for the release (run perl dump.pl 60)
-# Can be run for specific databasename, table name.
-# db= is for databasename (one at a time no comma separated list allowed)
-# table= is for tablename, can be a list of comma separated tablenames (no space) and should specify databasename (db=) for this to work
-# ex: perl dump.pl 60 db=ensembl_mart table=meta_conf__user__dm,meta_template__template__main
-#
-# Also Allowing script to be run for different hostname (one connection at a time ).
+# Script for dumping the mysql files for the release
+# script can be run for different hostname (one connection at a time ).
 # hostname= is for the hostname of the database server (one hostname at a time)
 # port= is for port number to listen to on the database server
 # username= is for username to login on the database server
 # pass= is for the password for the database server
+# path= is for path where to dump the mysql files. By default it will dump where the script is run
+# ex: perl dump.pl 60 hostname=xxx port=xxxx username=xxxx pass=xxxx
+#
+# Can be run for specific databasename, table name.
+# db= is for databasename (one at a time no comma separated list allowed)
+# table= is for tablename, can be a list of comma separated tablenames (no space) and should specify databasename (db=) for this to work
+# ex: perl dump.pl 60 hostname=xxx port=xxxx username=xxxx pass=xxxx db=ensembl_mart table=meta_conf__user__dm,meta_template__template__main
 
 
 use strict;
 use Time::HiRes qw(time);
-# use constant MAX => 2 * 1024 * 1024 * 1024;
 use constant MAX => 1024 * 1024 * 1024;
 use DBI;
 
 my $VERSION = shift @ARGV; #release version should always be the first argument when running the script
 
-my ($arg,$db,$table_name,$hostname,$PORT,$USER,$PASS);
+my ($arg,$db,$table_name,$hostname,$PORT,$USER,$PASS,$path);
 foreach my $row (@ARGV)
 {
   $row =~ s/,/|/g if($row =~ /table/i);                         #only argv table are allowed to have comma separated list
@@ -31,6 +32,7 @@ foreach my $row (@ARGV)
   ($arg,$PORT)      = split(/=/,$row) if($row =~ /port/i);
   ($arg,$USER)      = split(/=/,$row) if($row =~ /username/i);
   ($arg,$PASS)      = split(/=/,$row) if($row =~ /pass/i);
+  ($arg, $path)     = split(/=/,$row) if($row =~ /path/i);
   
 }
 if($table_name && !$db){
@@ -38,18 +40,23 @@ if($table_name && !$db){
   exit;
 }
 
-my $dumps_dir    = '/mysql/dumps';
-my $sub_dir      = "release-$VERSION";
-mkdir "$dumps_dir/$sub_dir";
+my $current_directory = `pwd`;
+chomp($current_directory);
+
+chomp($path);
+$current_directory = $path if($path);
+
+my $sub_dir      = "$current_directory/release-$VERSION";
+mkdir "$sub_dir";
 #my $hostname = `hostname`;
 
 our $ST = time;
 chomp $hostname;
-open( LH, ">$hostname.log" );
+open( LH, ">$current_directory/$hostname.log" );
 
 
-my $C = "/usr/local/ensembl/mysql/bin/mysql -h$hostname -P$PORT -u$USER -p$PASS -e 'show databases'";
-my @databases = `/usr/local/ensembl/mysql/bin/mysql -h$hostname -P$PORT -u$USER -p$PASS -e 'show databases'`; 
+my $C = "mysql -h$hostname -P$PORT -u$USER -p$PASS -e 'show databases'";
+my @databases = `mysql -h$hostname -P$PORT -u$USER -p$PASS -e 'show databases'`; 
 shift(@databases);
 if($db && !grep(/^($db)$/, @databases)){
   print "[ERROR]Could not find database::$db on server::$hostname!!! \n";
@@ -64,11 +71,12 @@ foreach my $db_name ( @databases ) {
   }
   log_msg( $db_name );
   my $dbh = DBI->connect("dbi:mysql:$db_name:$hostname:$PORT",$USER,$PASS);
-  my $DIR = "$dumps_dir/$sub_dir/$db_name";
+  my $DIR = "$sub_dir/$db_name";
   my $SQL = '';
  
-  system( "rm -r $dumps_dir/$sub_dir/$db_name")  if($db && !$table_name);  
+  system( "rm -r $sub_dir/$db_name")  if($db && !$table_name);  
   system( "mkdir $DIR" );
+
   system( "chmod 777 $DIR");
   chdir( $DIR );  
   my @tables = map {@$_} @{$dbh->selectall_arrayref('show tables')};
@@ -87,6 +95,7 @@ foreach my $db_name ( @databases ) {
     }
     system("rm $DIR/$table.txt.gz") if($table_name);   #got to remove the old file first 
     my $FN = "$DIR/$table.txt";
+
     log_msg( " |- dumping $table" );
     my($M,$T)=$dbh->selectrow_array("show create table $table;" );
     $SQL .= "$T;\n\n";
