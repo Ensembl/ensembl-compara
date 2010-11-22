daf3e890b5b2d723148650a4d79881e95d3f4ef9
Simon Brent <sb23@sanger.ac.uk>
2010-11-22 16:39:25 +0000

    use get_module_names

--- modules/EnsEMBL/Web/Controller/ZMenu.pm
+++ modules/EnsEMBL/Web/Controller/ZMenu.pm
@@ -6,57 +6,21 @@ package EnsEMBL::Web::Controller::ZMenu;
 
 use strict;
 
-use SiteDefs;
-
 use base qw(EnsEMBL::Web::Controller);
 
 sub init {
   my $self = shift;
   
   $self->builder->create_objects;
-  $self->build_menu;
-}
-
-sub build_menu {
-  ### Creates a ZMenu module based on the object type and action of the page (see below), and renders the menu
   
-  my $self   = shift;
-  my $hub    = $self->hub;
-  my $object = $self->object;
- 
-  # Force values of action and type because apparently require "EnsEMBL::Web::ZMenu::::Gene" (for eg) doesn't fail. Stupid perl.
-  my $type   = $self->type   || 'NO_TYPE';
-  my $action = $self->action || 'NO_ACTION';
+  my $hub     = $self->hub;
+  my $object  = $self->object;
+  my @modules = $self->get_module_names('ZMenu', $self->type, $self->action);
   my $menu;
   
-  my $i;
-  my @packages = (map({ ++$i % 2 ? $_ : () } @$ENSEMBL_PLUGINS), 'EnsEMBL::Web');
-  
-  ### Check for all possible module permutations.
-  ### This way we can have, for example, ZMenu::Contig and ZMenu::Contig::Gene (contig menu with Gene page specific functionality),
-  ### and also ZMenu::Gene and ZMenu::Gene::ComparaTree (has a similar menu to that of a gene, but has a different glyph in the drawing code)
-  my @modules = (
-    "::ZMenu::$type",
-    "::ZMenu::$action",
-    "::ZMenu::${type}::$action",
-    "::ZMenu::${action}::$type"
-  );
-  
-  foreach my $module_root (@packages) {
-    my $module_name = [ map { $self->dynamic_use("$module_root$_") ? "$module_root$_" : () } @modules ]->[-1];
-    
-    if ($module_name) {
-      $menu = $module_name->new($hub, $object, $menu);
-      
-      last;
-    } else {
-      my $error = $self->dynamic_use_failure("$module_root$modules[-1]");
-      warn $error unless $error =~ /^Can't locate/;
-    }
-  }
+  $menu = $_->new($hub, $object, $menu) for @modules;
   
   $self->r->content_type('text/plain');
-  
   $menu->render if $menu;
 }
 
