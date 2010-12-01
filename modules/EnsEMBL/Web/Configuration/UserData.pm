50d58bf526683bf04ad1f09351bd8771cd27e83c
Simon Brent <sb23@sanger.ac.uk>
2010-12-01 11:27:33 +0000

    merging changes from branch-ensembl-60

--- modules/EnsEMBL/Web/Configuration/UserData.pm
+++ modules/EnsEMBL/Web/Configuration/UserData.pm
@@ -118,10 +118,12 @@ sub populate_tree {
     'availability' => 1, 'no_menu_entry' => 1 }
   );
   $self->create_node( 'UrlFeedback', '',
-   [qw(url_feedback EnsEMBL::Web::Component::UserData::UrlFeedback)], 
+   [qw(
+      url_feedback  EnsEMBL::Web::Component::UserData::UrlFeedback
+      url_parsed    EnsEMBL::Web::Component::UserData::UploadParsed
+    )],
     { 'availability' => 1, 'no_menu_entry' => 1 }
   );
-
 
   ## BAM file attachment
   $self->create_node( 'SelectBAM', "Attach BAM File",
