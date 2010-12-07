4787cb43b38eb987e7795ef2e2dfdd90cff4fbd3
Anne Parker <ap5@sanger.ac.uk>
2010-12-07 15:28:26 +0000

    Changed to show full hierarchy of pages

--- modules/EnsEMBL/Web/Document/Element/BreadCrumbs.pm
+++ modules/EnsEMBL/Web/Document/Element/BreadCrumbs.pm
@@ -6,8 +6,7 @@ use strict;
 
 use base qw(EnsEMBL::Web::Document::Element);
 
-# Package to generate breadcrumb links (currently incorporated into masthead)
-# Limited to three levels in order to keep masthead neat :)
+# Package to generate breadcrumb links 
 
 sub title {
   my $self = shift;
@@ -17,29 +16,75 @@ sub title {
 
 sub content {
   my $self        = shift;
-  my $path        = $ENV{'SCRIPT_NAME'};
-  my @breadcrumbs = $path eq '/index.html' ? () : '<a class="home" href="/">Home</a>';
+  my $home        = $self->species_defs->ENSEMBL_BASE_URL;
+  my $tree        = $self->species_defs->STATIC_INFO;
+  ## Remove leading slash
+  (my $pathstring  = $ENV{'SCRIPT_NAME'}) =~ s/^\///;
+  my @path        = split('/', $pathstring);
+  my @breadcrumbs = qq(<a class="home" href="$home">Home</a>);
 
-  if ($path =~ /^\/info\//) {
-    # Level 2 link
-    if ($path eq '/info/' || $path eq '/info/index.html') {
-      push @breadcrumbs, 'Docs &amp; FAQs';
-    } else {
-      push @breadcrumbs, '<a href="/info/">Docs &amp; FAQs</a>';
-      push @breadcrumbs, $self->title if $self->title;
+  if ($path[0] eq 'info') {
+    ## Recurse into tree
+    my $html;
+    my $current_path = '/info/';
+    my $subtree = $tree->{$path[1]};
+
+    ## Top level link
+    if ($path[1] eq 'index.html') {
+      push @breadcrumbs, $tree->{'_title'};
     }
-    
-    return unless @breadcrumbs;
-    
+    else {
+      push @breadcrumbs, sprintf '<a href="%s">%s</a>', $current_path, $tree->{'_title'};
+    }
+
+    for (my $i = 1; $i < scalar(@path); $i++ ) {
+      $current_path .= $path[$i];
+      if ($path[$i] !~ /html$/) {
+        $current_path .= '/';
+      }
+      my $next = $self->_create_link($subtree, $path[$i+1], $current_path);
+      $subtree = $next->{'subtree'};
+      push @breadcrumbs, $next->{'link'} if $next->{'link'};
+    }
+
     my $last = pop @breadcrumbs;
-    
-    return sprintf qq{<ul class="breadcrumbs print_hide">%s<li class="last">$last</li></ul>}, join '', map "<li>$_</li>", @breadcrumbs;
+
+    $html = '<ul class="breadcrumbs print_hide">';
+    if (@breadcrumbs) {
+      $html .= join '', map "<li>$_</li>", @breadcrumbs;
+    }
+    $html .= qq(<li class="last">$last</li></ul>);
+    return $html;  
   }
 }
 
 sub init {
   my ($self, $controller) = @_;
   $self->title($controller->content =~ /<title>(.*?)<\/title>/sm ? $1 : 'Untitled: ' . $controller->r->uri) if $controller->request eq 'ssi';
+}
+
+sub _create_link {
+  my ($self, $subtree, $child, $path) = @_;
+  my ($link, $childtree);
+
+  my $title = $subtree->{'_title'};
+  my $url   = $subtree->{'_path'};
+  if ($child) {
+    if ($subtree->{'_show'}) {
+      if ($path =~ /\.html/) {
+        $link = $title;
+      }
+      else {
+        $link = qq(<a href="$path" title="$title">$title</a>);
+      }
+    }
+    $childtree = $subtree->{$child};
+  }
+  else {
+    $link = $title;
+  }
+
+  return {'link' => $link, 'subtree' => $childtree};
 }
 
 1;
