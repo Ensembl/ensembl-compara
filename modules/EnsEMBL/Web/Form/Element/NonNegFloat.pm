f6c09166fd9eab676560ed88c88e367436e213cb
Harpreet Riat <hr5@sanger.ac.uk>
2010-12-07 10:44:28 +0000

    inherited from NonNegInt

--- modules/EnsEMBL/Web/Form/Element/NonNegFloat.pm
+++ modules/EnsEMBL/Web/Form/Element/NonNegFloat.pm
@@ -2,7 +2,7 @@ package EnsEMBL::Web::Form::Element::NonNegFloat;
 
 use strict;
 
-use base qw(EnsEMBL::Web::Form::Element::String);
+use base qw(EnsEMBL::Web::Form::Element::NonNegInt);
 
 use constant {
   VALIDATION_CLASS =>  '_nonnegfloat',
