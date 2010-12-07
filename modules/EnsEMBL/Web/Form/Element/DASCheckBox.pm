964c1bdd4e1afb4a424aa38468daf2605e13b394
Harpreet Riat <hr5@sanger.ac.uk>
2010-12-07 16:30:57 +0000

    das checkbox configured properly

--- modules/EnsEMBL/Web/Form/Element/DASCheckBox.pm
+++ modules/EnsEMBL/Web/Form/Element/DASCheckBox.pm
@@ -16,24 +16,25 @@ use constant {
 sub configure {
   ## @overrides
   my ($self, $params) = @_;
+  
+  $params->{'name'}   ||= 'logic_name';
+  $params->{'value'}  ||= $params->{'das'}->logic_name;
+  $params->{'label'}  ||= $params->{'das'}->label;
 
   $self->set_attribute('id', $params->{'id'}) if exists $params->{'id'};
-  $self->set_attribute('class', $params->{'class'}) if exists $params->{'class'};
-  $self->set_attribute('class', $self->CSS_CLASS);
+  $self->set_attribute('class', $self->CSS_CLASS.' '.$params->{'class'});
   
-  my $wrapper  = $self->dom->create_element('p');
-  $self->append_child($wrapper);
-  $wrapper->set_attribute('class', $self->CSS_CLASS_CHECKBOX_WRAPPER);
-  my $checkbox = $self->dom->create_element('inputcheckbox');
-  $wrapper->append_child($checkbox);
-  $checkbox->set_attribute('name', $params->{'name'});
+  my $wrapper  = $self->dom->create_element('p', {'class' => $self->CSS_CLASS_CHECKBOX_WRAPPER});
+  my $checkbox = $self->dom->create_element('inputcheckbox', {'name' => $params->{'name'}, 'value', $params->{'value'}});
   $checkbox->checked($params->{'checked'} ? 1 : 0);
-  $checkbox->set_attribute('value', $params->{'value'});
+  $checkbox->disabled($params->{'disabled'} ? 1 : 0);
 
-  my $text = $self->dom->create_element('div');
-  $text->set_attribute('class', $self->CSS_CLASS_TEXT);
+  my $text = $self->dom->create_element('div', {'class' => $self->CSS_CLASS_TEXT});
+  $text->inner_HTML(qq(<p>$params->{label}</p><span>$params->{das}->{description} [<a href="$params->{das}->{homepage}">Homepage</a>]</span>));
+
+  $wrapper->append_child($checkbox);
+  $self->append_child($wrapper);
   $self->append_child($text);
-  $text->inner_HTML(qq(<p>$params->{das}->{label}</p><span>$params->{das}->{description} [<a href="$params->{das}->{homepage}">Homepage</a>]</span>));
 }
 
 1;
