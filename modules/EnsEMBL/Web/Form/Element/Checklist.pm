216bdc490523d70f7da3981905acf01f75da8e75
Harpreet Riat <hr5@sanger.ac.uk>
2010-12-15 17:20:19 +0000

    class name now goes to the input element not wrapper

--- modules/EnsEMBL/Web/Form/Element/Checklist.pm
+++ modules/EnsEMBL/Web/Form/Element/Checklist.pm
@@ -9,55 +9,50 @@ use base qw(
 
 use constant {
   CSS_CLASS_SUBHEADING    => 'optgroup',
-  CSS_CLASS_WRAPPER       => 'ff-checklist',
+  CSS_CLASS_INNER_WRAPPER => 'ff-checklist',
   SELECT_DESELECT_CAPTION => '<u><b>Select/deselect all</b></u>',
+  
+  _IS_MULTIPLE           => 1,               ## Override in child class if required
+  _ELEMENT_TYPE          => 'inputcheckbox'  ## Override in child class if required
 };
 
-sub __multiple {
-  ## Override in child class if required
-  return 1;
-}
-
-sub __input {
-  ## Override in child class if required
-  return 'inputcheckbox';
+sub render {
+  ## @overrides
+  my $self = shift;
+  eval { $_->get_elements_by_tag_name('label')->[0]->set_attribute('for', $_->get_elements_by_tag_name('input')->[0]->id); } for @{$self->get_elements_by_class_name($self->CSS_CLASS_INNER_WRAPPER)};
+  return $self->SUPER::render;
 }
 
 sub configure {
   ## @overrides
   my ($self, $params) = @_;
   
-  $self->set_attribute('id',    $params->{'id'})    if exists $params->{'id'};
-  $self->set_attribute('class', $params->{'class'}) if exists $params->{'class'};
+  $self->set_attribute('id',    $params->{'id'})            if exists $params->{'id'};
+  $self->set_attribute('class', $params->{'wrapper_class'}) if exists $params->{'wrapper_class'};
+
+  # default attributes for the checkboxs/radiobuttons
+  $self->{'__option_name'}      = $params->{'name'} || '';
+  $self->{'__option_disabled'}  = $params->{'disabled'} ? 1 : 0;
+  $self->{'__option_class'}     = $params->{'class'} if exists $params->{'class'};
   
-  $self->{'__name'}     = $params->{'name'} || '';
-  $self->{'__disabled'} = exists $params->{'disabled'} && $params->{'disabled'} == 1 ? 1 : 0;
-  $self->{'__inline'}   = exists $params->{'inline'} && $params->{'inline'} == 1 ? 1 : 0;
+  $self->{'__inline'}           = exists $params->{'inline'} && $params->{'inline'} == 1 ? 1 : 0;
 
   my $checked_values = {};
   if (exists $params->{'value'}) {
     $params->{'value'}  = [ $params->{'value'} ] unless ref($params->{'value'}) eq 'ARRAY';
-    $params->{'value'}  = [ shift @{$params->{'value'}} ] unless $self->__multiple;
+    $params->{'value'}  = [ shift @{$params->{'value'}} ] unless $self->_IS_MULTIPLE;
     $checked_values     = { map { $_ => 1 } @{$params->{'value'}} };
   }
   if (exists $params->{'selectall'}) {
-      $self->add_option({
-        'value'         => 'select_all',
-        'caption'       => $self->SELECT_DESELECT_CAPTION,
-        'checked'       => exists $_->{'value'} && exists $checked_values->{$_->{'value'}} ? 1 : 0,
-        'group'         => $_->{'group'}  || ''
-      });
+    $self->add_option({
+      'value'         => 'select_all',
+      'caption'       => $self->SELECT_DESELECT_CAPTION
+    });
   }
   if (exists $params->{'values'}) {
     for (@{$params->{'values'}}) {
-      my $args = {};
-      $args->{'value'}          =  $_->{'value'} if exists $_->{'value'};
-      $args->{'caption'}        =  $_->{'caption'} if exists $_->{'caption'};
-      $args->{'checked'}        =  exists $_->{'value'} && defined $_->{'value'} && exists $checked_values->{$_->{'value'}} ? 1 : 0;
-      $args->{'group'}          =  $_->{'group'} if exists $_->{'group'};
-      $args->{'name'}           =  $_->{'name'} if exists $_->{'name'};
-      $args->{'is_plain_text'}  =  $_->{'is_plain_text'} if $_->{'is_plain_text'};
-      $self->add_option($args);
+      $_->{'checked'} = exists $_->{'value'} && defined $_->{'value'} && exists $checked_values->{$_->{'value'}} ? 1 : 0;
+      $self->add_option($_);
     }
   }
 }
@@ -67,66 +62,42 @@ sub add_option {
   ## @params HashRef with following keys:
   ##  - id            Id attribute of <input>
   ##  - value         goes in value attribute of the option
-  ##  - caption       goes as innerText in <option> (is the actual name displayed)
+  ##  - caption       goes as innerText in <label>, appearing right side of the checkbox/radiobutton (is the actual name displayed)
   ##  - selected      flag to tell whether option is selected or not
-  ##  - group         (optional) Subheading caption - If subheading does not exist, a new one's created before adding it
-  ##  - name          (optional) Only needed to overwrite the default name attribute (one for the whole checklist)
+  ##  - group         Subheading caption - If subheading does not exist, a new one's created before adding it
+  ##  - class         Only needed to override the default class attribute for all options
+  ##  - name          Only needed to override the default name attribute for all options
+  ##  - disabled      Only needed to override the default enabled status for all options
   ##  - is_plain_text Flag to be on if caption is NOT HTML
   ## @return newly added Node::Element::P/Span object containg an input and a label
   my ($self, $params) = @_;
   
-  $params->{'value'}    = '' unless exists $params->{'value'} && defined $params->{'value'};
-  $params->{'caption'}  = '' unless exists $params->{'caption'} && defined $params->{'caption'};
-  my $is_plain_text     = $params->{'is_plain_text'} ? 1 : 0;
+  $params->{'value'}    = '' unless exists $params->{'value'};
+  $params->{'caption'}  = '' unless exists $params->{'caption'};
+  $params->{'class'}  ||= $self->{'__option_class'} if $self->{'__option_class'};
   
-  my $wrapper = $self->dom->create_element($self->{'__inline'} == 1 ? 'span' : 'p');
-  my $input   = $self->dom->create_element($self->__input);
-  my $label   = $self->dom->create_element('label');
+  my $wrapper = $self->dom->create_element($self->{'__inline'} ? 'span' : 'p', {'class' => $self->CSS_CLASS_INNER_WRAPPER});
+  my $input   = $self->dom->create_element($self->_ELEMENT_TYPE, {'value' => $params->{'value'}, 'name' => $params->{'name'} || $self->{'__option_name'}});
+
+  $input->set_attribute('id', $params->{'id'})        if $params->{'id'};
+  $input->set_attribute('class', $params->{'class'})  if $params->{'class'};
+  $input->disabled(exists $params->{'disabled'} ? ($params->{'disabled'} ? 1 : 0) : $self->{'__option_disabled'});
+  $input->checked(1) if exists $params->{'checked'} && $params->{'checked'} == 1;
   
   $wrapper->append_child($input);
-  $wrapper->set_attribute('class', $self->CSS_CLASS_WRAPPER);
-  $input->set_attribute('id', $params->{'id'} || $self->unique_id);
-  $input->set_attribute('value', $params->{'value'});
-  $input->set_attribute('name', $params->{'name'} || $self->{'__name'});
-  $input->disabled($self->{'__disabled'});
-  $input->checked(1) if exists $params->{'checked'} && $params->{'checked'} == 1;
-  if ($params->{'caption'} ne '') {
-    $wrapper->append_child($label);
-    $input->set_attribute('id', $self->unique_id) unless $input->id;
-    $label->set_attribute('for', $input->id);
-    if ($is_plain_text) {
-      $label->inner_text($params->{'caption'});
-    }
-    else {
-      $label->inner_HTML($params->{'caption'});
-    }
-  }
+  $wrapper->append_child($self->dom->create_element('label', {($params->{'is_plain_text'} ? 'inner_text' : 'inner_HTML') => $params->{'caption'}})) if $params->{'caption'} ne '';
   
   my $next_heading = undef;
   if (exists $params->{'group'} && defined $params->{'group'}) {
   
     my $match = 0;
     for (@{$self->get_elements_by_class_name($self->CSS_CLASS_SUBHEADING)}) {
-      if ($match) {
-        $next_heading = $_;
-        last;
-      }
+      $match and $next_heading = $_ and last;
       $match = 1 if $_->inner_HTML eq $params->{'group'};
     }
-    unless ($match) {
-      my $heading = $self->dom->create_element('p');
-      $heading->inner_HTML($params->{'group'});
-      $heading->set_attribute('class', $self->CSS_CLASS_SUBHEADING);
-      $self->append_child($heading);
-    }
+    $self->append_child($self->dom->create_element('p', {'inner_HTML' => $params->{'group'}, 'class' => $self->CSS_CLASS_SUBHEADING})) unless $match; #create new heading if no match found
   }
-  if (defined $next_heading) {
-    $self->insert_before($wrapper, $next_heading);
-  }
-  else {
-    $self->append_child($wrapper);
-  }
-  return $wrapper;
+  return defined $next_heading ? $self->insert_before($wrapper, $next_heading) : $self->append_child($wrapper);
 }
 
 1;
