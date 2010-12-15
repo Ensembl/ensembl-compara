bb843951b2601c149a3b515b404a7449997742db
Harpreet Riat <hr5@sanger.ac.uk>
2010-12-15 17:20:57 +0000

    class name can also be given to options + some code tidying

--- modules/EnsEMBL/Web/Form/Element/Dropdown.pm
+++ modules/EnsEMBL/Web/Form/Element/Dropdown.pm
@@ -7,18 +7,20 @@ use base qw(
   EnsEMBL::Web::Form::Element
 );
 
+sub render {
+  ## @overrides
+  my $self = shift;
+  $self->after($self->dom->create_element('span', {'inner_HTML' => ' '.$self->{'__shortnote'}})) if exists $self->{'__shortnote'};
+  return $self->SUPER::render;
+}
+
 sub configure {
   ## @overrides
   my ($self, $params) = @_;
   
-  $self->set_attribute('id',        $params->{'id'} || $self->unique_id);
-  $self->set_attribute('name',      $params->{'name'})            if exists $params->{'name'};
-  $self->set_attribute('class',     $params->{'class'})           if exists $params->{'class'};
-  $self->set_attribute('size',      $params->{'size'})            if exists $params->{'size'};
-  $self->set_attribute('class',     $self->CSS_CLASS_REQUIRED)    if exists $params->{'required'};
-
-  $self->disabled(1) if exists $params->{'disabled'} && $params->{'disabled'} == 1;
-  $self->multiple(1) if exists $params->{'multiple'} && $params->{'multiple'} == 1;
+  exists $params->{$_} and $self->set_attribute($_, $params->{$_}) for qw(id name class size);
+  exists $params->{$_} and $params->{$_} == 1 and $self->$_(1) for qw(disabled multiple);
+  $self->set_attribute('class', $self->CSS_CLASS_REQUIRED) if exists $params->{'required'};
   
   my $selected_values = {};
   if (exists $params->{'value'}) {
@@ -26,21 +28,13 @@ sub configure {
     $params->{'value'} = [ shift @{ $params->{'value'} } ] unless $self->multiple;
     $selected_values  = { map { $_ => 1 } @{ $params->{'value'} } };
   }
+  $self->{'__shortnote'} = $params->{'shortnote'} if exists $params->{'shortnote'};
+  $self->{'__option_class'} = $params->{'option_class'} if exists $params->{'option_class'};
   if (exists $params->{'values'}) {
-    for (@{ $params->{'values'} }) {
-      my $args = {};
-      $args->{'id'}       = $_->{'id'}        if exists $_->{'id'};
-      $args->{'value'}    = $_->{'value'}     if exists $_->{'value'};
-      $args->{'caption'}  = $_->{'caption'}   if exists $_->{'caption'};
-      $args->{'selected'} = $_->{'value'} && exists $selected_values->{ $_->{'value'} } ? 1 : 0;
-      $args->{'group'}    = $_->{'group'}     if $_->{'group'};
-    
-      $self->add_option($args);
+    for (@{$params->{'values'}}) {
+      $_->{'selected'} = $_->{'value'} && exists $selected_values->{ $_->{'value'} } ? 1 : 0;
+      $self->add_option($_);
     }
-  }
-  if (exists $params->{'shortnote'}) {
-    $self->parent_node->append_child($self->dom->create_element('span'));
-    $self->next_sibling->inner_HTML($params->{'shortnote'});
   }
 }
 
@@ -51,35 +45,25 @@ sub add_option {
   ##  - value     goes in value attribute of the option
   ##  - caption   goes as innerText in <option> (is the actual name displayed)
   ##  - selected  flag to tell whether option is selected or not
+  ##  - class     Class attribute - overrides the 
   ##  - group     (optional) Label attribute for the parent Optgroup for the option - If optgroup does not exist, a new one's created before adding it
   ## @return newly added Node::Element::Option object
   my ($self, $params) = @_;
   
-  $params->{'value'} = '' unless exists $params->{'value'} && defined $params->{'value'};
+  $params->{'value'}   = '' unless exists $params->{'value'} && defined $params->{'value'};
   $params->{'caption'} = '' unless exists $params->{'caption'} && defined $params->{'caption'};
+  $params->{'class'} ||= $self->{'__option_class'} if exists $params->{'__option_class'};
   
-  my $option = $self->dom->create_element('option');
-  $option->inner_HTML($params->{'caption'});
+  my $option = $self->dom->create_element('option', {'inner_HTML' => $params->{'caption'}, 'value' => $params->{'value'}});
   $option->set_attribute('id', $params->{'id'}) if $params->{'id'};
-  $option->set_attribute('value', $params->{'value'});
   $option->selected(1) if exists $params->{'selected'} && $params->{'selected'} == 1;
-  
-  my $parent_node = $self;
+
+  my $group = undef;
   if (exists $params->{'group'} && $params->{'group'} ne '') {
-  
-    my $group = undef;
-    for (@{ $self->get_elements_by_tag_name('optgroup') }) {
-      $group = $_ if $_->get_attribute('label') eq $params->{'group'};
-    }
-    unless (defined $group) {
-      $group = $self->dom->create_element('optgroup');
-      $group->set_attribute('label', $params->{'group'});
-      $self->append_child($group);
-    }
-    $parent_node = $group;
+    $_->get_attribute('label') eq $params->{'group'} and $group = $_ and last for @{$self->get_elements_by_tag_name('optgroup')}; #find any matching one
+    $group = $self->append_child($self->dom->create_element('optgroup', {'label' => $params->{'group'}})) unless $group;          #new optgroup if no match
   }
-  $parent_node->append_child($option);
-  return $option;
+  return ($group || $self)->append_child($option);
 }
 
 1;
