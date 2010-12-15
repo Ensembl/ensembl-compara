76271dbfe1128adf24cc7a56472b11adb0b24e76
Harpreet Riat <hr5@sanger.ac.uk>
2010-12-15 16:51:17 +0000

    code tidying

--- modules/EnsEMBL/Web/Form/Element/YesNo.pm
+++ modules/EnsEMBL/Web/Form/Element/YesNo.pm
@@ -7,24 +7,21 @@ use base qw(EnsEMBL::Web::Form::Element::Dropdown);
 sub configure {
   ## @overrides
   my ($self, $params) = @_;
+
   $params->{'multiple'} = 0;
   $params->{'value'} = exists $params->{'value'} && ref($params->{'value'}) eq 'ARRAY'
     ? shift @{ $params->{'value'} }
     : $params->{'value'} || '';
   
-  $params->{'values'} = 
-  [
-    {
-      'value'     => 'yes',
-      'caption'   => 'Yes',
-      'selected'  => $params->{'value'} eq 'yes' ? 1 : 0,
-    },
-    {
-      'value'     => 'no',
-      'caption'   => 'No',
-      'selected'  => $params->{'value'} eq 'no' ? 1 : 0,
-    }
-  ];
+  $params->{'values'} = [{
+    'value'     => 'yes',
+    'caption'   => 'Yes',
+    'selected'  => $params->{'value'} eq 'yes' ? 1 : 0,
+  },{
+    'value'     => 'no',
+    'caption'   => 'No',
+    'selected'  => $params->{'value'} eq 'no' ? 1 : 0,
+  }];
   $self->SUPER::configure($params);
 }
 
