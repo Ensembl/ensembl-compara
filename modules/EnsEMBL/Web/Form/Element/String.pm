24c7f8ff8cd26cd4c11c6c292c3f4deb7e2f9884
Harpreet Riat <hr5@sanger.ac.uk>
2010-12-15 11:03:25 +0000

    shortnote positioned properly

--- modules/EnsEMBL/Web/Form/Element/String.pm
+++ modules/EnsEMBL/Web/Form/Element/String.pm
@@ -14,10 +14,9 @@ use constant {
 sub render {
   ## @overrides
   my $self = shift;
-  $self->parent_node->insert_after($self->dom->create_element('span', {'inner_HTML' => ' '.$self->{'__shortnote'}}), $self);
-    if exists $self->{'__shortnote'};
+  $self->after($self->dom->create_element('span', {'inner_HTML' => ' '.$self->{'__shortnote'}})) if exists $self->{'__shortnote'};
   return $self->SUPER::render;
-};
+}
 
 sub configure {
   ## @overrides
