ca40902086c9a9c6cee54e95824f1d0a1a66b349
Simon Brent <sb23@sanger.ac.uk>
2011-01-04 12:28:13 +0000

    fix layout

--- modules/EnsEMBL/Web/Document/HTML/TOC.pm
+++ modules/EnsEMBL/Web/Document/HTML/TOC.pm
@@ -1,3 +1,5 @@
+# $Id$
+
 package EnsEMBL::Web::Document::HTML::TOC;
 
 ### Generates table of contents for documentation (/info/)
@@ -8,62 +10,57 @@ use EnsEMBL::Web::RegObj;
 use base qw(EnsEMBL::Web::Document::HTML);
 
 sub render {
-  my $self = shift;
-
-  my $tree = $ENSEMBL_WEB_REGISTRY->species_defs->STATIC_INFO;
+  my $self              = shift;
+  my $tree              = $ENSEMBL_WEB_REGISTRY->species_defs->STATIC_INFO;
   (my $location         = $ENV{'SCRIPT_NAME'}) =~ s/index\.html$//;
   my @toplevel_sections = map { ref $tree->{$_} eq 'HASH' ? $_ : () } keys %$tree;
-  my $html;
-  my $first_header = 1;
+  my %html              = ( left => '', right => '' );
   
   my @section_order = sort {
     $tree->{$a}{'_order'} <=> $tree->{$b}{'_order'} ||
     $tree->{$a}{'_title'} cmp $tree->{$b}{'_title'} ||
-    $tree->{$a} cmp $tree->{$b}
+    $tree->{$a}           cmp $tree->{$b}
   } @toplevel_sections;
   
-  my $count = 0;
   foreach my $dir (grep { !/^_/ && keys %{$tree->{$_}} } @section_order) {
-    my $section   = $tree->{$dir};
-    my $side      = $dir eq 'docs' ? 'right' : 'left';
-    my $title     = $section->{'_title'} || ucfirst $dir;
-    $html .= qq{<div class="twocol-$side plain-box"><h2 class="first">$title</h2>};
-    $first_header = 0;
-
-    my @second_level = @{$self->_create_links($section, ' style="font-weight:bold"')};
+    my $section      = $tree->{$dir};
+    my $side         = $dir eq 'docs' ? 'right' : 'left';
+    my $title        = $section->{'_title'} || ucfirst $dir;
+    my @second_level = @{$self->create_links($section, ' style="font-weight:bold"')};
+    
+    $html{$side} .= qq{<div class="plain-box" style="margin:0 0 2%;padding:2%"><h2 class="first">$title</h2>};
+    
     if (scalar @second_level) {
-      $html .= '<ul>';
+      $html{$side} .= '<ul>';
   
       foreach my $entry (@second_level) {
-      
-        my $link  = $entry->{'link'};
-        $html .= '<li>'.$link;
+        my $link = $entry->{'link'};
 
         ## One more level!
-        my $subsection = $entry->{'key'};
-        my @third_level = @{$self->_create_links($subsection)};
+        my $subsection  = $entry->{'key'};
+        my @third_level = @{$self->create_links($subsection)};
+        
         if (scalar @third_level) {
-          $html .= '<ul>';
-          foreach my $subentry (@third_level) {
-            my $sublink  = $subentry->{'link'};
-            $html .= qq{<li>$sublink</li>};
-          }
-          $html .= '</ul>';
+          $link .= '<ul>';
+          $link .= "<li>$_->{'link'}</li>" for @third_level;
+          $link .= '</ul>';
         }
 
-        $html .= '</li>';
+        $html{$side} .= "<li>$link</li>";
       }      
       
-      $html .= '</ul>';
+      $html{$side} .= '</ul>';
     }
-    $html .= '</div>';
-    $count++;  
+    
+    $html{$side} .= '</div>';
   }
-   
-  return $html;
+  
+  $html{$_} = qq{<div style="float:$_;clear:$_;width:49%">$html{$_}</div>} for grep $html{$_}, keys %html;
+  
+  return qq{<div style="width:100%">$html{'left'}$html{'right'}</div>};
 }
 
-sub _create_links {
+sub create_links {
   my ($self, $level, $style) = @_;
   my $links = [];
     
@@ -72,17 +69,17 @@ sub _create_links {
     
   if (scalar @sublevel) {
     my @sub_order = sort { 
-        $level->{$a}{'_order'} <=> $level->{$b}{'_order'} ||
-        $level->{$a}{'_title'} cmp $level->{$b}{'_title'} ||
-        $level->{$a} cmp $level->{$b}
-      } @sublevel;
-
+      $level->{$a}{'_order'} <=> $level->{$b}{'_order'} ||
+      $level->{$a}{'_title'} cmp $level->{$b}{'_title'} ||
+      $level->{$a}           cmp $level->{$b}
+    } @sublevel;
+    
     foreach my $sub (grep { !/^_/ && keys %{$level->{$_}} } @sub_order) {
       my $pages = $level->{$sub};
       my $path  = $pages->{'_path'} || "$level->{'_path'}$sub";
       my $title = $pages->{'_title'} || ucfirst $sub;
         
-      push @$links, {'key' => $pages, 'link' => qq(<a href="$path" title="$title"$style>$title</a>)};
+      push @$links, { key => $pages, link => qq(<a href="$path" title="$title"$style>$title</a>) };
     }
   }
 
