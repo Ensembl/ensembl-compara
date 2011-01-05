ddf1168b972910098175aafbbd50c24610cf95c2
Simon Brent <sb23@sanger.ac.uk>
2011-01-05 16:12:12 +0000

    moved img_url function to Root.pm

--- modules/EnsEMBL/Web/Document/Element.pm
+++ modules/EnsEMBL/Web/Document/Element.pm
@@ -16,17 +16,13 @@ sub new {
   return $self;
 }
 
-sub renderer :lvalue { $_[0]->{'renderer'};             }
-sub hub              { return $_[0]->{'hub'};           }
-sub species_defs     { return $_[0]->hub->species_defs; }
-
-sub home_url  { return $_[0]->{'home_url'} ||= $_[0]->species_defs->ENSEMBL_WEB_ROOT   || '/';   }
-sub img_url   { return $_[0]->{'img_url'}  ||= $_[0]->species_defs->ENSEMBL_IMAGE_ROOT || '/i/'; }
-
-sub printf { my $self = shift; $self->renderer->printf(@_) if $self->renderer; }
-sub print  { my $self = shift; $self->renderer->print(@_)  if $self->renderer; }
-
-sub content {}
+sub renderer :lvalue { $_[0]->{'renderer'};                                                         }
+sub hub              { return $_[0]->{'hub'};                                                       }
+sub species_defs     { return $_[0]->hub->species_defs;                                             }
+sub home_url         { return $_[0]->{'home_url'} ||= $_[0]->species_defs->ENSEMBL_WEB_ROOT || '/'; }
+sub printf           { my $self = shift; $self->renderer->printf(@_) if $self->renderer;            }
+sub print            { my $self = shift; $self->renderer->print(@_)  if $self->renderer;            }
+sub content          {}
 
 sub new_panel {
   my ($self, $panel_type, $controller, %params) = @_;
