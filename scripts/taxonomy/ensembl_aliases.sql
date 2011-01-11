fde1aa51180b58066632c5f5433ca7e7d4e9bee0
Bronwen Aken <ba1@sanger.ac.uk>
2011-01-11 19:53:47 +0000

    Added lamprey

--- scripts/taxonomy/ensembl_aliases.sql
+++ scripts/taxonomy/ensembl_aliases.sql
@@ -574,6 +574,17 @@ SET @this_value='Northern white-cheeked gibbon';
 SET @this_name_class='ensembl common name';
 insert into ncbi_taxa_name select @this_taxon_id,@this_value,@this_name_class from ncbi_taxa_name WHERE taxon_id=@this_taxon_id and name_class=@this_name_class having count(*)=0;
 
+SET @this_taxon_id=7757;
+SET @this_value='Lamprey';
+SET @this_name_class='ensembl alias name';
+insert into ncbi_taxa_name select @this_taxon_id,@this_value,@this_name_class from ncbi_taxa_name WHERE taxon_id=@this_taxon_id and name_class=@this_name_class having count(*)=0;
+
+SET @this_taxon_id=7757;
+SET @this_value='Sea lamprey';
+SET @this_name_class='ensembl common name';
+insert into ncbi_taxa_name select @this_taxon_id,@this_value,@this_name_class from ncbi_taxa_name WHERE taxon_id=@this_taxon_id and name_class=@this_name_class having count(*)=0;
+
+
 
 -- -- Use these to ADD new species
 
