5d68809e04d8466c36fea81e510dd00123d45676
Simon Brent <sb23@sanger.ac.uk>
2011-01-11 09:19:49 +0000

    add update_from_input function

--- modules/EnsEMBL/Web/ViewConfig/Location/Cell_line.pm
+++ modules/EnsEMBL/Web/ViewConfig/Location/Cell_line.pm
@@ -1,3 +1,5 @@
+# $Id$
+
 package EnsEMBL::Web::ViewConfig::Location::Cell_line;
 
 use base qw(EnsEMBL::Web::ViewConfig::Cell_line);
@@ -9,5 +11,7 @@ sub init {
   $self->SUPER::init(@_);
   $self->has_images = 0;
 }
+
+sub update_from_input { $_[0]->SUPER::update_from_input('contigviewbottom'); }
 
 1;
