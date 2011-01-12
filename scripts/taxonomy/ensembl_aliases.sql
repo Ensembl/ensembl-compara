739a534cb068ddde67271b8b8af6a7dfec87d928
Bronwen Aken <ba1@sanger.ac.uk>
2011-01-12 15:11:34 +0000

    change 'Giant Panda' to 'Panda'

--- scripts/taxonomy/ensembl_aliases.sql
+++ scripts/taxonomy/ensembl_aliases.sql
@@ -555,7 +555,7 @@ SET @this_name_class='ensembl common name';
 insert into ncbi_taxa_name select @this_taxon_id,@this_value,@this_name_class from ncbi_taxa_name WHERE taxon_id=@this_taxon_id and name_class=@this_name_class having count(*)=0;
 
 SET @this_taxon_id=9646;
-SET @this_value='Giant Panda';
+SET @this_value='Panda';
 SET @this_name_class='ensembl alias name';
 insert into ncbi_taxa_name select @this_taxon_id,@this_value,@this_name_class from ncbi_taxa_name WHERE taxon_id=@this_taxon_id and name_class=@this_name_class having count(*)=0;
 
