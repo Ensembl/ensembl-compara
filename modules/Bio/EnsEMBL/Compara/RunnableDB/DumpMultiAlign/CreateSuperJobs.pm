7158b1652fe8871cf444cd06d1c093cb305f3d5a
Kathryn Beal <kbeal@ebi.ac.uk>
2011-01-24 11:14:17 +0000

    Set coord_system_name with a parameter instead of hard coding it to supercontig. Cannot use parallelise this by using chunks (this is not supported by DumpMultiAlign.pm). Tidied up code.

--- modules/Bio/EnsEMBL/Compara/RunnableDB/DumpMultiAlign/CreateSuperJobs.pm
+++ modules/Bio/EnsEMBL/Compara/RunnableDB/DumpMultiAlign/CreateSuperJobs.pm
@@ -1,5 +1,20 @@
+=head1 LICENSE
 
-=pod 
+  Copyright (c) 1999-2011 The European Bioinformatics Institute and
+  Genome Research Limited.  All rights reserved.
+
+  This software is distributed under a modified Apache license.
+  For license details, please see
+
+    http://www.ensembl.org/info/about/code_licence.html
+
+=head1 CONTACT
+
+  Please email comments or questions to the public Ensembl
+  developers list at <dev@ensembl.org>.
+
+  Questions may also be sent to the Ensembl help desk at
+  <helpdesk@ensembl.org>.
 
 =head1 NAME
 
@@ -13,13 +28,6 @@ This RunnableDB module is part of the DumpMultiAlign pipeline.
 
 This RunnableDB module generates DumpMultiAlign jobs from genomic_align_blocks
 on the species supercontigs. The jobs are split into $split_size chunks
-
-=head1 CONTACT
-
- This modules is part of the EnsEMBL project (http://www.ensembl.org)
-
-Questions can be posted to the ensembl-dev mailing list:
-ensembl-dev@ebi.ac.uk
 
 =cut
 
@@ -45,15 +53,11 @@ sub strict_hash_format {
 
 sub fetch_input {
     my $self = shift;
-
-    
 }
 
 
 sub run {
     my $self = shift;
-    
-
 }
 
 sub write_output {
@@ -93,54 +97,32 @@ if ($self->param('reg_conf')) {
     FROM genomic_align 
     LEFT JOIN dnafrag 
     USING (dnafrag_id) 
-    WHERE coord_system_name = \"supercontig\" 
+    WHERE coord_system_name = ? 
     AND genome_db_id= ? 
     AND method_link_species_set_id=?";
 
-
     my $sth = $compara_dba->dbc->prepare($sql);
-    $sth->execute($self->param('genome_db_id'), $self->param('mlss_id'));
+    $sth->execute($self->param('coord_system_name'),$self->param('genome_db_id'), $self->param('mlss_id'));
     my ($total_blocks) = $sth->fetchrow_array;
     
-    my $super_blocks;
-    my $tag = "supercontig";
-
-    my $output_file = $self->param('output_dir') ."/" . $self->param('filename') . "." . $tag . "." . $self->param('format');
+    my $tag = $self->param('coord_system_name');
+    #my $output_file = $self->param('output_dir') ."/" . $self->param('filename') . "." . $tag . "." . $self->param('format');
+    my $output_file = $self->param('filename') . "." . $tag . "." . $self->param('format');
     
-    my $num_chunks = ceil($total_blocks/$self->param('split_size'));
-    #print "$total_blocks $num_chunks\n";
-
-    my $dump_program = $self->param('dump_program');
-    my $dump_mlss_id = $self->param('dump_mlss_id');
-    my $reg_conf = $self->param('reg_conf');
-    my $compara_dbname = $self->param('compara_dbname');
-    my $masked_seq = $self->param('masked_seq');
-    my $split_size = $self->param('split_size');
     my $format = $self->param('format');
-    my $species = $self->param('species');
-    my $emf2maf_program = $self->param('emf2maf_program');
-    my $maf_output_dir = $self->param('maf_output_dir');
+    my $coord_system_name = $self->param('coord_system_name');
+    #This doesn't work because DumpMultiAlignment adds _1 to the output file and can create more if there are lots of supercontigs.
+    #Since I create only one job, the compress will only start when all the chunks have been produced (if more than one) so I can use "*"
+    #my $this_suffix = "." . $format;
+    my $this_suffix = "*" . "." . $format;
+    my $dump_output_file = $output_file;
+    $dump_output_file =~ s/\.$format/$this_suffix/;
+	
+    #Write out cmd for DumpMultiAlign and a few other parameters 
+    #used in downstream analyses 
+    $output_ids = "{\"coord_system\"=> \"$coord_system_name\", \"output_file\"=> \"$output_file\", \"num_blocks\"=> $total_blocks, \"dumped_output_file\"=> \"$dump_output_file\", \"format\"=> \"$format\"}";
 
-    for (my $chunk = 1; $chunk <= $num_chunks; $chunk++) {
-
-	#Number of gabs in this chunk (used for healthcheck)
-	my $this_num_blocks = $split_size;
-	if ($chunk == $num_chunks) {
-	    $this_num_blocks = ($total_blocks - (($chunk-1)*$split_size));
-	}
-	#print "this_num_chunks $this_num_blocks\n";
-	
-	my $this_suffix = "_" . $chunk . "." . $format;
-	my $dump_output_file = $output_file;
-	$dump_output_file =~ s/\.$format/$this_suffix/;
-	
-	#Write out cmd from DumpMultiAlign and a few other parameters 
-	#used in downstream analyses 
-	my $output_ids = "{\"cmd\"=>\"perl $dump_program --reg_conf $reg_conf --dbname $compara_dbname --species $species --mlss_id $dump_mlss_id --coord_system supercontig --masked_seq $masked_seq --split_size $split_size --output_format $format --output_file $output_file --chunk_num $chunk\", \"num_blocks\"=>\"$this_num_blocks\", \"output_file\"=>\"$dump_output_file\", \"format\"=> \"$format\", \"emf2maf_program\"=>\"$emf2maf_program\", \"maf_output_dir\"=>\"$maf_output_dir\"}";
-	#print "$output_ids\n";
-	
-	$self->dataflow_output_id($output_ids, 2);
-    }
+    $self->dataflow_output_id($output_ids, 2);
 }
 
 1;
