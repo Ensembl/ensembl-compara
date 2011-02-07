27408ca101cc5c4ad24995299c125dff27139604
Stephen Searle <searle@sanger.ac.uk>
2011-02-07 11:18:11 +0000

    Fixes to DAS wizard (handling of no matching sources when filtering), and addition of new caching functionality to speed up response - from branch

--- modules/EnsEMBL/Web/Command/UserData/CheckServer.pm
+++ modules/EnsEMBL/Web/Command/UserData/CheckServer.pm
@@ -14,13 +14,23 @@ sub process {
 
   ## Catch any errors at the server end
   my $server  = $object->param('other_das') || $object->param('preconf_das');
+
+  # Hack for cache reloading for now - special text in filter box
+  if ($object->param('das_name_filter') eq 'reloadCache') {
+    $object->param('das_clear_cache','1');
+    $object->param('das_name_filter','');
+  }
+
   my $filter  = new EnsEMBL::Web::Filter::DAS({ object => $object });
   my $sources = $filter->catch($server);
 
   if ($sources) {
     $param->{'das_server'} = $server;
+    $param->{'das_name_filter'} = $object->param('das_name_filter');
     $url .= 'DasSources';
   } else {
+    $param->{'filter_module'} = 'DAS';
+    $param->{'filter_code'} = $filter->error_code;
     $url .= 'SelectServer';
   }
   
