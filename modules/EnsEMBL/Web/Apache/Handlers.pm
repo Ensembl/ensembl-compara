c5ed320fba013262b8efe6930c90c3558a82220d
Simon Brent <sb23@sanger.ac.uk>
2011-02-08 15:17:14 +0000

    remove trailing ; or & from REQUEST_URI and HTTP_REFERER - URL is used in memcacheing and was incorrect

--- modules/EnsEMBL/Web/Apache/Handlers.pm
+++ modules/EnsEMBL/Web/Apache/Handlers.pm
@@ -197,7 +197,7 @@ sub cleanURI {
   # Clean out the uri
   my $uri = $ENV{'REQUEST_URI'};
   
-  if ($uri =~ s/time=\d+\.\d+[;&]?//g + $uri =~ s!([^:])/{2,}!$1/!g) {
+  if ($uri =~ s/[;&]?time=\d+\.\d+//g + $uri =~ s!([^:])/{2,}!$1/!g) {
     $r->parse_uri($uri);
     $r->subprocess_env->{'REQUEST_URI'} = $uri;
   }
@@ -205,7 +205,7 @@ sub cleanURI {
   # Clean out the referrer
   my $referer = $ENV{'HTTP_REFERER'};
   
-  if ($referer =~ s/time=\d+\.\d+[;&]?//g + $referer =~ s!([^:])/{2,}!$1/!g) {
+  if ($referer =~ s/[;&]?time=\d+\.\d+//g + $referer =~ s!([^:])/{2,}!$1/!g) {
     $r->subprocess_env->{'HTTP_REFERER'} = $referer;
   }
   
