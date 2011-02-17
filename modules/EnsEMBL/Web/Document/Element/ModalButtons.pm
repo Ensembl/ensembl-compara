f3f5ee5f3b5f6df77412e95d662ae5863fafecff
Simon Brent <sb23@sanger.ac.uk>
2011-02-17 13:25:55 +0000

    make it work

--- modules/EnsEMBL/Web/Document/Element/ModalButtons.pm
+++ modules/EnsEMBL/Web/Document/Element/ModalButtons.pm
@@ -6,92 +6,43 @@ package EnsEMBL::Web::Document::Element::ModalButtons;
 
 use strict;
 
-use base qw(EnsEMBL::Web::Document::Element);
+use base qw(EnsEMBL::Web::Document::Element::ToolButtons);
 
-sub entries {
-  my $self = shift;
-  return $self->{'_entries'}||[];
-}
-
-sub add_entry {
-  my $self = shift;
-  push @{$self->{'_entries'}}, @_;
-}
-
-sub class {
-  my $self = shift;
-  $self->{'class'} = shift if @_;
-  return $self->{'class'};
-}
-
-sub get_json {
-  my $self = shift;
-  return { tools => $self->content };
-} 
-
-sub content {
-  my $self = shift;
-  
-  return unless @{$self->entries};
-  
-  my %classes = (
-    'Add custom track'  => 'data',
-    'Reset to defaults' => 'data',
-  );
-  
-  my $html = '<div id="modal-tools">';
-
-  foreach (@{$self->entries}) {
-    if ($_->{'class'} eq 'disabled') {
-      $html .= qq{<p class="disabled $classes{$_->{'caption'}}" title="$_->{'title'}">$_->{'caption'}</p>};
-    } else {
-      my $rel   = lc $_->{'rel'};
-      my $attrs = $_->{'class'};
-      $attrs   .= ($attrs ? ' ' : '') . 'external' if $rel eq 'external';
-      $attrs   .= ($attrs ? ' ' : '') . $classes{$_->{'caption'}} if $classes{$_->{'caption'}};
-      $attrs    = qq{class="$attrs"} if $attrs;
-      #$attrs   .= ' style="display:none"' if $attrs =~ /modal_link/;
-      $attrs   .= qq{ rel="$rel"} if $rel;
-
-      $html .= qq{
-        <p><a href="$_->{'url'}" $attrs>$_->{'caption'}</a></p>};
-    }
-  }
-  
-  $html .= '</div>';
-
-  return $html; 
+sub label_classes {
+  return {
+    'Add custom track'    => 'data',
+    'Reset configuration' => 'config'
+  };
 }
 
 sub init {
-  my $self        = shift;  
-  my $controller  = shift;
-  my $hub         = $controller->hub;
+  my $self       = shift;  
+  my $controller = shift;
+  my $hub        = $controller->hub;
   
-  $self->add_entry({
-    caption => 'Add custom track',
-    class   => 'modal_link',
-    url     => $hub->url({
-      time    => time,
-      type    => 'UserData',
-      action  => 'SelectFile',
-      __clear => 1 
-    })
-  });
-  
-  $self->add_entry({
-    caption => 'Reset to defaults',
-    class   => 'modal_link',
-    url     => $hub->url({
-      time      => time,
-      type      => 'Config',
-      action    => $hub->type,
-      function  => $hub->action,
-      reset     => 1,
-      __clear => 1 
-    })
-  });
-  
+  if ($hub->type eq 'Config') {
+    my $config = $hub->param('config');
+    
+    $self->add_entry({
+      caption => 'Add custom track',
+      class   => 'modal_link',
+      url     => $hub->url({
+        type    => 'UserData',
+        action  => 'SelectFile',
+        __clear => 1 
+      })
+    });
+
+    $self->add_entry({
+      caption => 'Reset configuration',
+      class   => 'modal_link',
+      url     => $hub->url('Config', {
+        reset   => 1,
+        config  => $config eq '_page' ? '' : $config,
+        __clear => 1 
+      })
+    });
+  }
 }
 
 1;
