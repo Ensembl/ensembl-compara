88f444364edff2062f46790f46ae191660394652
Leo Gordon <lg4@ebi.ac.uk>
2011-02-18 10:10:01 +0000

    gene_trees renamed to protein_trees

--- scripts/dumps/dumpTreeMSA_id.pl
+++ scripts/dumps/dumpTreeMSA_id.pl
@@ -68,7 +68,7 @@ my $dba = Bio::EnsEMBL::Hive::URLFactory->fetch( $url.';type=compara' );
 
 my ($prefix, $adaptor) = $nc
     ? ('ncrna_trees_.', $dba->get_NCTreeAdaptor)
-    : ('gene_trees_.', $dba->get_ProteinTreeAdaptor);
+    : ('protein_trees_.', $dba->get_ProteinTreeAdaptor);
 
 my @tree_ids;
 if($tree_id_file and -r $tree_id_file) {
