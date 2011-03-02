27ad773c535bef82d34b7aa0d5f9f59455cd6f4d
ridwan <ma7@sanger.ac.uk>
2011-03-02 11:30:05 +0000

    Making the output goes to the new export component

--- modules/EnsEMBL/Web/Configuration/Export.pm
+++ modules/EnsEMBL/Web/Configuration/Export.pm
@@ -9,14 +9,15 @@ use base qw(EnsEMBL::Web::Configuration);
 sub populate_tree {
   my $self = shift; 
   my %config = ( availability => 1, no_menu_entry => 1 );
-  
-  foreach ('Location', 'Gene', 'Transcript', 'LRG') {
+
+  foreach ('Location', 'Gene', 'Transcript', 'LRG', 'Variation') {
     $self->create_node("Configure/$_",  '', [ 'configure',   'EnsEMBL::Web::Component::Export::Configure' ], \%config);
-    $self->create_node("Form/$_",       '', [], { command => 'EnsEMBL::Web::Command::Export::Form',           %config});
+    $self->create_node("Form/$_",       '', [], { command => 'EnsEMBL::Web::Command::Export::Form',           %config});      #redirecting the form to the right url so that it can go to formats below
     $self->create_node("Formats/$_",    '', [ 'formats',     'EnsEMBL::Web::Component::Export::Formats' ],   \%config);
-    $self->create_node("Alignments/$_", '', [ 'alignments', 'EnsEMBL::Web::Component::Export::Alignments' ], \%config) unless $_ eq 'Transcript';
+    $self->create_node("Alignments/$_", '', [ 'alignments',  'EnsEMBL::Web::Component::Export::Alignments' ], \%config) unless $_ eq 'Transcript';
+    $self->create_node("Output/$_",     '', [ 'export',      'EnsEMBL::Web::Component::Export::Output' ], \%config);    
   }
-  
+
   $self->create_node('HaploviewFiles/Location', '', [], { command => 'EnsEMBL::Web::Command::Export::HaploviewFiles',  %config});
   $self->create_node('LDExcelFile/Location',    '', [], { command => 'EnsEMBL::Web::Command::Export::LDExcelFile',     %config});
   $self->create_node('LDFormats/Location',      '', [ 'ld_formats',  'EnsEMBL::Web::Component::Export::LDFormats' ],  \%config);
