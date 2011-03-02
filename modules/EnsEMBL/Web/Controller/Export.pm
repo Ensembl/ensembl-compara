9d9d4bc5a1b4561cff0b3651bc1c5b4c9fdd2b2b
ridwan <ma7@sanger.ac.uk>
2011-03-02 11:33:52 +0000

    having a proper controller for export

--- modules/EnsEMBL/Web/Controller/Export.pm
+++ modules/EnsEMBL/Web/Controller/Export.pm
@@ -5,7 +5,20 @@ package EnsEMBL::Web::Controller::Export;
 use strict;
 
 use base qw(EnsEMBL::Web::Controller::Page);
+ 
+sub page_type { return $_[0]->action eq 'Output' ? 'Dynamic' : 'Popup'; }
+sub request   { return $_[0]->action eq 'Output' ? 'Export'  : 'Modal'; }
 
-sub request { return 'export'; }
+sub init {
+  my $self = shift;
+  
+  $self->hub->type = $self->hub->function if $self->hub->action eq 'Output';    #this is to get the left nav and top nav according to the right object
+  $self->builder->create_objects('Export');
+  $self->renderer->{'_modal_dialog_'} = $self->r->headers_in->{'X-Requested-With'} eq 'XMLHttpRequest'; # Flag indicating that this is modal dialog panel, loaded by AJAX
+  $self->page->initialize; # Adds the components to be rendered to the page module  
+  $self->configure;
+  $self->page->remove_body_element('summary');
+  $self->render_page;  
+}
 
 1;
