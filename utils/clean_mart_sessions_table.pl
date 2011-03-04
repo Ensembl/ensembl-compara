78c4b519c9952d3d283b67f8bb29c10a012a2392
Simon Brent <sb23@sanger.ac.uk>
2011-03-04 09:23:57 +0000

    delete entries from the sessions table which are over 7 days old. run daily on crontab

---
+++ utils/clean_mart_sessions_table.pl
@@ -0,0 +1,33 @@
+#!/usr/local/bin/perl
+
+use strict;
+
+use DBI;
+use File::Basename qw(dirname);
+use FindBin qw($Bin);
+
+BEGIN {
+  my $serverroot = dirname($Bin);
+  unshift @INC, "$serverroot/conf", $serverroot;
+  
+  require SiteDefs;
+  
+  unshift @INC, $_ for @SiteDefs::ENSEMBL_LIB_DIRS;
+
+  require EnsEMBL::Web::Hub;
+}
+
+my $hub = new EnsEMBL::Web::Hub;
+my $sd  = $hub->species_defs;
+
+my $dbh = DBI->connect(
+  sprintf('DBI:mysql:database=%s;host=%s;port=%s', $sd->ENSEMBL_USERDB_NAME, $sd->ENSEMBL_USERDB_HOST, $sd->ENSEMBL_USERDB_PORT),
+  $sd->ENSEMBL_USERDB_USER, $sd->ENSEMBL_USERDB_PASS
+);
+
+# Optimise table on Sundays
+if (!(gmtime)[6]) {
+  $dbh->do('OPTIMIZE TABLE sessions');
+}
+  
+$dbh->disconnect;
