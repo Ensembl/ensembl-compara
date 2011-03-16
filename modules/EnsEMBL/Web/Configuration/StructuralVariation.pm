fac55400f1f12dd547aebb05bb509e8cf0292c8c
Laurent Gil <lgil@ebi.ac.uk>
2011-03-16 13:36:24 +0000

    Add a Genes/Transcripts link on the left panel

--- modules/EnsEMBL/Web/Configuration/StructuralVariation.pm
+++ modules/EnsEMBL/Web/Configuration/StructuralVariation.pm
@@ -15,13 +15,14 @@ sub set_default_action {
 sub populate_tree {
   my $self = shift;
 
-  $self->create_node('Summary', 'Context',
-    [qw(
-      context  EnsEMBL::Web::Component::StructuralVariation::Context
-    )],
+	$self->create_node('Summary', 'Genes/Transcripts',
+    [qw( summary EnsEMBL::Web::Component::StructuralVariation::Mappings )],
+    { 'availability' => 'structural_variation', 'concise' => 'Genes/Transcripts' }
+  );
+
+  $self->create_node('Context', 'Context',
+    [qw( summary  EnsEMBL::Web::Component::StructuralVariation::Context)],
     { 'availability' => 'structural_variation', 'concise' => 'Context' }
   );
-  
 }
-
 1;
