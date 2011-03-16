7bf28c118c8260dc6190aded6797e0ba20beaaac
Laurent Gil <lgil@ebi.ac.uk>
2011-03-16 13:34:10 +0000

    Add the SSV data and the ftp links to the DGVa studies

--- modules/EnsEMBL/Web/Component/StructuralVariation/Summary.pm
+++ modules/EnsEMBL/Web/Component/StructuralVariation/Summary.pm
@@ -14,34 +14,56 @@ sub content {
   my $self                = shift;
   my $hub                 = $self->hub;
   my $object              = $self->object;
+	my $sv_obj              = $object->Obj;
   my $name                = $object->name;
   my $class               = $object->class;
   my $source              = $object->source;
   my $source_description  = $object->source_description;
- 
+  my $study_name          = $object->study_name;
+  my $study_description   = $object->study_description;
+  my $study_url           = $object->study_url;
+  my $supporting_sv				= $object->supporting_sv;
   $name = "$class ($name)";
   my $source_link = $hub->get_ExtURL_link($source, 'DGVA', $source);
-  if($source_description =~/PMID/){ 
-    my @temp = split('\s', $source_description);
-    foreach (@temp ){
-      if ($_ =~/PMID/){
-        my $pubmed_id = $_; 
-        my $id = $pubmed_id;  
-        $id =~s/PMID\://; 
-        my $pubmed_url = $hub->get_ExtURL_link($pubmed_id, 'PUBMED', $id); 
-        $source_description =~s/$_/$pubmed_url/;
-      }
-    }
-  }   
-
-  $source = "$source_link - $source_description";
  
-  my $location = $object->neat_sr_name($object->Obj->slice->seq_region_name .":" . $object->Obj->start ."-" . $object->Obj->end);
-  my $strand = $object->Obj->strand > 0 ? "forward" : "reverse";
+  $source_description = add_pubmed_link($source_description, $hub);
+  
+	$source = "$source_link - $source_description";
+ 	
+	# Study line display
+	my $study_line = '';
+  if ($study_name ne '') {
+		$study_description = add_pubmed_link($study_description, $hub);
+    $study_line = sprintf ('<a href="%s">%s</a>',$study_url,$study_name);
+  	$study_line = "<dt>Study</dt><dd>$study_line - $study_description</dd>";
+  }
+	
+	# Supporting evidence
+	my $ssv_line = '';
+	if (scalar @{$supporting_sv} > 0) {
+		$ssv_line = "<dt>Supporting evidence</dt>\n<dd>";
+		my $ssv_sep = '';
+		my $ssv_count = 1;
+		my $ssv_limit = 15;
+		foreach my $ssv (@{$supporting_sv}) {
+			$ssv_line .= "$ssv_sep".$ssv->name;
+			if ($ssv_sep eq '') { $ssv_sep = ', '; }
+			if ($ssv_count==$ssv_limit) { 
+				$ssv_line .= $ssv_sep.'<br />';
+				$ssv_count = 0;
+				$ssv_sep = '';
+			}
+			$ssv_count++;
+		}
+		$ssv_line .= "</dd>";
+	} 
+ 
+  my $location = $object->neat_sr_name($sv_obj->slice->seq_region_name .":" . $sv_obj->start ."-" . $sv_obj->end);
+  my $strand = $sv_obj->strand > 0 ? "forward" : "reverse";
   my $location_url = $self->hub->url({
     'type'   => 'Location',
     'action' => 'View',
-    'r'      => $object->Obj->slice->seq_region_name.':'.$object->Obj->start.'-'.$object->Obj->end
+    'r'      => $sv_obj->slice->seq_region_name.':'.$sv_obj->start.'-'.$sv_obj->end
   });
   $location_url .= "contigviewbottom=variation_feature_structural=normal";
 
@@ -49,7 +71,7 @@ sub content {
     $location_url,
     'View in location tab'
   );
-
+	
   my $location_link = "This feature maps to $location ($strand strand) | $location_html";
  
   my $html = qq{
@@ -58,13 +80,35 @@ sub content {
       <dd>$name</dd>
       <dt>Source</dt>
       <dd>$source</dd>
-      <dt>Location<dt>
-      <dd>$location_link</dd> 
+	    $study_line
+      <dt>Location</dt>
+      <dd>$location_link</dd>
+			$ssv_line 
     </dl>
   };
- 
-
+	
   return $html;
+}
+
+
+# Method to add a pubmed link to the expression "PMID:xxxxxxx"
+# in the source or study description, if it is present.
+sub add_pubmed_link{
+	my $s_description = shift;
+	my $hub = shift;
+	if($s_description =~/PMID/){ 
+		my @temp = split('\s', $s_description);
+    foreach (@temp ){
+			if ($_ =~/PMID/){
+      	my $pubmed_id = $_; 
+        my $id = $pubmed_id;  
+        $id =~s/PMID\://; 
+        my $pubmed_url = $hub->get_ExtURL_link($pubmed_id, 'PUBMED', $id); 
+        $s_description =~s/$_/$pubmed_url/;
+			}
+		}
+ 	}
+	return $s_description;
 }
 
 1;
