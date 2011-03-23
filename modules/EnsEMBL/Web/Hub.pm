0ca1d62cf43986062958e4826a3ee4f8936bc83f
Harpreet Riat <hr5@sanger.ac.uk>
2011-03-23 12:32:41 +0000

    modified parse_referer to accept url with no or 'common' species

--- modules/EnsEMBL/Web/Hub.pm
+++ modules/EnsEMBL/Web/Hub.pm
@@ -337,10 +337,15 @@ sub parse_referer {
   $uri    =~ s/[;&]$//;
   
   my ($url, $query_string) = split /\?/, $uri;
-  my ($species, $type, $action, $function) = split /\//, $url;
   
-  return {} unless $species_defs->valid_species($species);
+  my @path = split /\//, $url;
   
+  unshift @path, 'common' unless $path[0] eq 'common' || $species_defs->valid_species($path[0]);
+
+  return {absolute_url => $ENV{'HTTP_REFERER'}} unless $self->valid_type($path[1]);
+
+  my ($species, $type, $action, $function) = @path;
+
   my @pairs  = split /[&;]/, $query_string;
   my $params = {};
   
@@ -385,6 +390,11 @@ sub parse_referer {
     uri              => "/$uri",
     absolute_url     => $ENV{'HTTP_REFERER'}
   };
+}
+
+sub valid_type {
+  ## Checks whether the given string is a valid 'Type'
+  return exists $SiteDefs::OBJECT_TO_SCRIPT->{pop @_};
 }
 
 sub filename {
