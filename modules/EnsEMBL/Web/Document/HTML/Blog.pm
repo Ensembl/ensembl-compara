e52549f08d52232f17a2745639e3fbcd2cb89061
Anne Parker <ap5@sanger.ac.uk>
2011-03-28 09:15:10 +0000

    Fix for RSS timeouts

--- modules/EnsEMBL/Web/Document/HTML/Blog.pm
+++ modules/EnsEMBL/Web/Document/HTML/Blog.pm
@@ -6,8 +6,6 @@ use strict;
 use warnings;
 
 use LWP::UserAgent;
-use XML::Atom::Feed;
-use XML::RSS;
 
 use EnsEMBL::Web::Hub;
 use EnsEMBL::Web::Cache;
@@ -29,7 +27,7 @@ sub render {
   my $rss_type = $blog_rss =~ /atom/ ? 'atom' : 'rss';
 
   my $html = '<h3>Latest blog posts</h3>';
-    
+   
   if ($hub->cookies->{'ENSEMBL_AJAX'}) {
     $html .= qq(<div class="js_panel ajax" id="blog"><input type="hidden" class="ajax_load" value="/blog.html" /><inpu
 t type="hidden" class="panel_type" value="Content" /></div>);
@@ -40,49 +38,51 @@ t type="hidden" class="panel_type" value="Content" /></div>);
     my $blog = $MEMD && $MEMD->get('::BLOG') || '';
   
     unless ($blog) {
+      my @items;
       my $ua = new LWP::UserAgent;
       my $proxy = $hub->species_defs->ENSEMBL_WWW_PROXY;
       $ua->proxy( 'http', $proxy ) if $proxy;
-  
+      $ua->timeout(5);
+
       my $response = $ua->get($blog_rss);
-
-      my @items;
-      my $count = 0;
-      if ($rss_type eq 'atom') {
-        my $feed = XML::Atom::Feed->new(\$response->decoded_content);
-        my @entries = $feed->entries;
-        foreach my $entry (@entries) {
-          my ($link) = grep { $_->rel eq 'alternate' } $entry->link;
-          my $date  = $self->pretty_date(substr($entry->published, 0, 10), 'daymon');
-          my $item = {
-            'title' => $entry->title,
-            'link'  => $link->href,
-            'date'  => $date,
-            };
-          push @items, $item;
-          $count++;
-          last if $count == 3;
+      
+      if ($response->is_success) {
+        my $count = 0;
+        if ($rss_type eq 'atom' && $self->dynamic_use('XML::Atom::Feed')) {
+          my $feed = XML::Atom::Feed->new(\$response->decoded_content);
+          my @entries = $feed->entries;
+          foreach my $entry (@entries) {
+            my ($link) = grep { $_->rel eq 'alternate' } $entry->link;
+            my $date  = $self->pretty_date(substr($entry->published, 0, 10), 'daymon');
+            my $item = {
+              'title' => $entry->title,
+              'link'  => $link->href,
+              'date'  => $date,
+              };
+            push @items, $item;
+            $count++;
+            last if $count == 3;
+          }
         }
-      }
-      elsif ($rss_type eq 'rss') {
-        my $rss = XML::RSS->new;
-        $rss->parse($response->decoded_content);
-        foreach my $entry (@{$rss->{'items'}}) {
-          my $date = substr($entry->{'pubDate'}, 5, 11);
-          my $item = {
-            'title' => $entry->{'title'},
-            'link'  => $entry->{'link'},,
-            'date'  => $date,
-            };
-          push @items, $item;
-          $count++;
-          last if $count == 3;
+        elsif ($rss_type eq 'rss' && $self->dynamic_use('XML::RSS')) {
+          my $rss = XML::RSS->new;
+          $rss->parse($response->decoded_content);
+          foreach my $entry (@{$rss->{'items'}}) {
+            my $date = substr($entry->{'pubDate'}, 5, 11);
+            my $item = {
+              'title' => $entry->{'title'},
+              'link'  => $entry->{'link'},,
+              'date'  => $date,
+              };
+            push @items, $item;
+            $count++;
+            last if $count == 3;
+          }
         }
+        else {
+          warn "!!! UNKNOWN RSS METHOD DEFINED";
+        } 
       }
-      else {
-        warn "!!! UNKNOWN RSS METHOD DEFINED";
-      } 
-
       if (@items) {
         $blog .= "<ul>";
         foreach my $item (@items) {
