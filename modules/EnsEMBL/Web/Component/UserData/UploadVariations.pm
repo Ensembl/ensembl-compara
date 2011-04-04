63c6e636d3f57a560e4b303d9b1926320f814c2f
William McLaren <wm2@ebi.ac.uk>
2011-04-04 14:01:09 +0000

    Added lots of new options and rejigged help text

--- modules/EnsEMBL/Web/Component/UserData/UploadVariations.pm
+++ modules/EnsEMBL/Web/Component/UserData/UploadVariations.pm
@@ -31,20 +31,23 @@ sub content {
   my $form = $self->modal_form('select', $action_url,);
   $form->add_notes({ 
     'heading'=>'Variant Effect Predictor:',
-    'text'=>qq(<p class="space-below">(Formerly SNP Effect Predictor). This tool takes a list of variant positions and alleles, and predicts the effects of each
-              of these on any overlapping features (transcripts, regulatory features) annotated in Ensembl.</p>
-              <p class="space-below">The tool will accept substitutions, insertions and deletions as input,
-              uploaded as a list of tab separated values - 
-              <a href="/info/website/upload/var.html" target="_blank">more about required format</a>.</p>
-              <p class="space-below">The tool will also accept VCF format input -
-              <a href="http://www.1000genomes.org/wiki/Analysis/Variant%20Call%20Format/vcf-variant-call-format-version-40" target="_blank">more about VCF</a>.</p>
-              <p>There is a limit of $variation_limit variations that can be processed at any one time. 
-              You can upload a file that contains more entries, however anything after the $variation_limit 
-              line will be ignored. If your file contains more than $variation_limit variations you can split 
-              your file into smaller chunks and process them one at a time, or you may wish to use the 
-              <a href="/info/docs/api/variation/variation_tutorial.html#Consequence" target="_blank">variation API</a> or a standalone 
-              <a href="ftp://ftp.ensembl.org/pub/misc-scripts/Variant_effect_predictor_1.0/" target="_blank">perl script</a> which you 
-              can run on your own machine to generate the same results as this web tool. </p>
+    'text'=>qq(
+      <p class="space-below">(Formerly SNP Effect Predictor). This tool takes a
+      list of variant positions and alleles, and predicts the effects of each
+      of these on any overlapping features annotated in Ensembl. The
+      tool accepts substitutions, insertions and deletions as input,
+      uploaded as a list of <a href="/info/website/upload/var.html"
+      target="_blank">tab separated values</a>, <a
+      href="http://www.1000genomes.org/wiki/Analysis/Variant%20Call%20Format/vcf-variant-call-format-version-40"
+      target="_blank">VCF</a> or Pileup format input.</p>
+      
+      <p>Upload is limited to $variation_limit variants; lines after the limit
+      will be ignored. Users with more than $variation_limit variations can
+      split files into smaller chunks, use the standalone <a
+      href="ftp://ftp.ensembl.org/pub/misc-scripts/Variant_effect_predictor_2.0/"
+      target="_blank">perl script</a> or the <a
+      href="/info/docs/api/variation/variation_tutorial.html#Consequence"
+      target="_blank">variation API</a>.</p>
   )});
   my $subheader = 'Upload file';
 
@@ -56,7 +59,6 @@ sub content {
   @species = sort {$a->{'name'} cmp $b->{'name'}} @species;
 
   $form->add_element( type => 'Hidden', name => 'consequence_mapper', 'value' => 1);
-  $form->add_element( type => 'Hidden', name => 'format', 'value' => 'snp');
   $form->add_element('type' => 'SubHeader', 'value' => $subheader);
   $form->add_element(
       'type'    => 'DropDown',
@@ -71,7 +73,132 @@ sub content {
   $form->add_element( type => 'Text', name => 'text', label => 'Paste file' );
   $form->add_element( type => 'File', name => 'file', label => 'Upload file' );
   $form->add_element( type => 'URL',  name => 'url',  label => 'or provide file URL', size => 30 );
- 
+  
+  $form->add_element(
+      'type'    => 'DropDown',
+      'name'    => 'format',
+      'label'   => "Input file format",
+      'values'  => [
+        { value => 'snp',     name => 'Ensembl default' },
+        { value => 'vep_vcf', name => 'VCF'             },
+        { value => 'pileup',  name => 'Pileup'          },
+      ],
+      'value'   => 'snp',
+      'select'  => 'select',
+  );
+  
+  
+  ## OPTIONS
+  $form->add_element('type' => 'SubHeader', 'value' => 'Options');
+  
+  $form->add_element(
+    type   => 'DropDown',
+    select =>, 'select',
+    label  => 'Type of consequences to display',
+    name   => 'consequence_format',
+    values => [
+      { value => 'display',  name => 'Ensembl terms'           },
+      { value => 'SO',       name => 'Sequence Ontology terms' },
+      { value => 'NCBI',     name => 'NCBI terms'              },
+    ],
+    value  => 'ensembl',
+    select => 'select',
+  );  
+  
+  $form->add_element(
+    type  => 'CheckBox',
+    name  => "check_existing",
+    label => "Check for existing co-located variants",
+    value => 'yes',
+    selected => 1
+  );
+  
+  $form->add_element(
+    type  => 'CheckBox',
+    name  => "coding_only",
+    label => "Return results for variants in coding regions only",
+    value => 'yes',
+    selected => 0
+  );
+  
+  $form->add_element(
+    type  => 'CheckBox',
+    name  => "hgnc",
+    label => "Show HGNC identifier for genes where available",
+    value => 'yes',
+    selected => 0
+  );
+  
+  $form->add_element(
+    type  => 'CheckBox',
+    name  => "protein",
+    label => "Show Ensembl protein identifiers where available",
+    value => 'yes',
+    selected => 0
+  );
+  
+  $form->add_element(
+    type   => 'DropDown',
+    select =>, 'select',
+    label  => 'Show HGVS identifiers for variants where available',
+    name   => 'hgvs',
+    values => [
+      { value => 'no',             name => 'No'                           },
+      { value => 'coding',         name => 'Coding sequence only'         },
+      { value => 'protein',        name => 'Protein sequence only'        },
+      { value => 'coding_protein', name => 'Coding and protein sequence'  },
+    ],
+    value  => 'no',
+    select => 'select',
+  );
+  
+  
+  $form->add_element('type' => 'SubHeader', 'value' => 'Non-synonymous SNP predictions (human only)');
+  
+  $form->add_element(
+    type   => 'DropDown',
+    select =>, 'select',
+    label  => 'SIFT predictions',
+    name   => 'sift',
+    values => [
+      { value => 'no',         name => 'No'                   },
+      { value => 'pred',       name => 'Prediction only'      },
+      { value => 'score',      name => 'Score only'           },
+      { value => 'pred_score', name => 'Prediction and score' },
+    ],
+    value  => 'no',
+    select => 'select',
+  );  
+  
+  $form->add_element(
+    type   => 'DropDown',
+    select =>, 'select',
+    label  => 'PolyPhen predictions',
+    name   => 'polyphen',
+    values => [
+      { value => 'no',         name => 'No'                   },
+      { value => 'pred',       name => 'Prediction only'      },
+      { value => 'score',      name => 'Score only'           },
+      { value => 'pred_score', name => 'Prediction and score' },
+    ],
+    value  => 'no',
+    select => 'select',
+  );
+  
+  $form->add_element(
+    type   => 'DropDown',
+    select =>, 'select',
+    label  => 'Condel consensus (SIFT/PolyPhen) predictions',
+    name   => 'condel',
+    values => [
+      { value => 'no',         name => 'No'                   },
+      { value => 'pred',       name => 'Prediction only'      },
+      { value => 'score',      name => 'Score only'           },
+      { value => 'pred_score', name => 'Prediction and score' },
+    ],
+    value  => 'no',
+    select => 'select',
+  );
 
   $html .= $form->render;
   return $html;
