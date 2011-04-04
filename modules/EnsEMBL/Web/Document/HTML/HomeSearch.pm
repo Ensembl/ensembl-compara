7cf28921251e3ca8827f548e5cd3e3f5f669050b
Simon Brent <sb23@sanger.ac.uk>
2011-04-04 12:25:55 +0000

    added favourite species at the top of the dropdown. tidied

--- modules/EnsEMBL/Web/Document/HTML/HomeSearch.pm
+++ modules/EnsEMBL/Web/Document/HTML/HomeSearch.pm
@@ -4,88 +4,90 @@ package EnsEMBL::Web::Document::HTML::HomeSearch;
 ### home pages, with sample search terms taken from ini files
 
 use strict;
+
 use base qw(EnsEMBL::Web::Document::HTML);
 
-use EnsEMBL::Web::RegObj;
+sub new {
+  my ($class, $hub) = @_;
+  
+  my $self = $class->SUPER::new(
+    species      => $hub->species || 'Multi',
+    species_defs => $hub->species_defs,
+    favourites   => $hub->get_favourite_species
+  );
+  
+  bless $self, $class;
+  return $self;
+}
+
+sub species    { return $_[0]->{'species'};  }
+sub favourites { return $_[0]{'favourites'}; }
 
 sub render {
+  my $self = shift;
+  
   return if $ENV{'HTTP_USER_AGENT'} =~ /Sanger Search Bot/;
   
-  my $species_defs = $ENSEMBL_WEB_REGISTRY->species_defs;
-  my $page_species = $ENV{'ENSEMBL_SPECIES'} || 'common';
-  my $species_name = '';
-  $species_name = $species_defs->DISPLAY_NAME if $page_species;
-  if( $species_name =~ /\./ ) {
-    $species_name = '<i>'.$species_name.'</i>'
-  }
-  my $html = q(
-<div class="center">);
-
-  my $search_url = $species_defs->ENSEMBL_WEB_ROOT;
-  $search_url .= $page_species if $page_species.'/';
-  $search_url .= '/psychic';
+  my $species_defs        = $self->species_defs;
+  my $page_species        = $self->species;
+  my $species_name        = $page_species eq 'Multi' ? '' : $species_defs->DISPLAY_NAME;
+  $species_name           = "<i>$species_name</i>" if $species_name =~ /\./;
+  my $search_url          = $species_defs->ENSEMBL_WEB_ROOT . "$page_species/psychic";
   my $default_search_code = $species_defs->ENSEMBL_DEFAULT_SEARCHCODE;
-
-  $html .= sprintf q(
-  <h2 class="first">Search %s %s</h2>), $species_defs->ENSEMBL_SITETYPE, $species_name unless $species_name;
-  $html .= sprintf q(
-  <form action="%s" method="get"><div>
-    <input type="hidden" name="site" value="%s" />),
-  $search_url, $default_search_code;
-
-  my $input_size = 50;
-
-  if ($page_species eq 'common') {
-    $html .= q(
+  my $input_size          = $page_species eq 'Multi' ? 30 : 50;
+  my $favourites          = $self->favourites;
+  
+  my $html = qq{
+  <div class="center">
+    <form action="$search_url" method="get"><div>
+      <input type="hidden" name="site" value="$default_search_code" />};
+  
+  if ($page_species eq 'Multi') {
+    my %species      = map { $species_defs->get_config($_, 'DISPLAY_NAME') => $_ } @{$species_defs->ENSEMBL_DATASETS};
+    my %common_names = reverse %species;
+    
+    $html .= '
     <label for="species">Search</label>: <select id="species" name="species">
       <option value="">All species</option>
-      <option value="">---</option>
-);
-    $input_size = 30;
-
-    my %species = map {
-      $species_defs->get_config($_, 'DISPLAY_NAME') => $_
-    } @{$species_defs->ENSEMBL_DATASETS};
-    foreach my $common_name (sort {uc($a) cmp uc($b)} keys %species) {
-      $html .= qq(<option value="$species{$common_name}">$common_name</option>);
+      <option value="">---</option>';
+    
+    if (scalar @$favourites) {
+      $html .= '<optgroup label="Favourite species">';
+      $html .= qq{<option value="$_">$common_names{$_}</option>} for grep $common_names{$_}, @$favourites;
+      $html .= '</optgroup>';
     }
-
-    $html .= q(
+    
+    $html .= '<option value="">---</option>';
+    $html .= qq{<option value="$species{$_}">$_</option>} for sort { uc $a cmp uc $b } keys %species;
+    $html .= '
     </select>
-    <label for="q">for</label>);
-  }
-  else {
-    $html .= q(
-    <label for="q">Search for</label>:);
+    <label for="q">for</label>';
+  } else {
+    $html .= '<label for="q">Search for</label>:';
   }
 
-  $html .= qq(
+  $html .= qq{
     <input id="q" name="q" size="$input_size" value="" />
-    <input type="submit" value="Go" class="input-submit" />);
+    <input type="submit" value="Go" class="input-submit" />};
 
   ## Examples
-  my @examples;
-  if ($page_species eq 'common') {
-    my $sample_data = $species_defs->get_config('MULTI', 'GENERIC_DATA') || {};
-    if (keys %$sample_data) {
-      @examples = grep {$_} ($sample_data->{'GENE_TEXT'}, $sample_data->{'LOCATION_TEXT'}, $sample_data->{'SEARCH_TEXT'});
-    }
+  my $sample_data;
+  
+  if ($page_species eq 'Multi') {
+    $sample_data = $species_defs->get_config('MULTI', 'GENERIC_DATA') || {};
+  } else {
+    $sample_data = $species_defs->SAMPLE_DATA;
+    $sample_data->{'GENE_TEXT'} = "gene $sample_data->{'GENE_TEXT'}" if $sample_data->{'GENE_TEXT'};
   }
-  else {
-    my $sample_data = $species_defs->SAMPLE_DATA;
-    if (keys %$sample_data) {
-      @examples = grep {$_} ('gene '.$sample_data->{'GENE_TEXT'}, $sample_data->{'LOCATION_TEXT'}, $sample_data->{'SEARCH_TEXT'});
-    }
-  }
-  if (scalar(@examples)) {
-    $html .= '<p>e.g. ' . join(' or ', map {'<strong><a href="'.$search_url.'?q='.$_.'" style="text-decoration:none">'.$_.'</a></strong>'} @examples) . '</p>';
-  }
-  $html .= qq(
-  </div></form>
-</div>);
+  
+  my @examples = map $sample_data->{$_} || (), qw(GENE_TEXT LOCATION_TEXT SEARCH_TEXT);
+  
+  $html .= sprintf '<p>e.g. %s</p>', join ' or ', map qq{<strong><a href="$search_url?q=$_" style="text-decoration:none">$_</a></strong>}, @examples if scalar @examples;
+  $html .= '
+    </div></form>
+  </div>';
 
   return $html;
-
 }
 
 1;
