4bba055337fc7edaf08877376598268da25d47e9
Steve Trevanion <st3@sanger.ac.uk>
2011-04-08 15:05:29 +0000

    no need to load generic_match_label glyphsets since the labels are added correctly, put features into proper menus since it's now configurable, tweak tracks and descriptions if we have a vega gene (ensembl or vega sites), make sure gene track is not in configuration menu

--- modules/EnsEMBL/Web/ImageConfig/supporting_evidence_transcript.pm
+++ modules/EnsEMBL/Web/ImageConfig/supporting_evidence_transcript.pm
@@ -20,56 +20,72 @@ sub init {
 
   $self->create_menus(
     'TSE_transcript'      => 'Genes',
+    'transcript_features' => 'Transcript features',
+    'evidence'            => 'Evidence',
   );
 
   ## Add in additional
   $self->load_tracks();
 
-  #switch off all transcript unwanted transcript tracks
-  foreach my $child ( $self->get_node('TSE_transcript')->descendants ) {
-    $child->set( 'display' => 'off' );
-  }
+  my $is_vega_gene = ($self->hub->param('db') eq 'vega') ? 1 : ($self->species_defs->ENSEMBL_SITETYPE eq 'Vega') ? 1 : 0;
 
-  $self->add_tracks( 'TSE_transcript', [
+  $self->add_tracks( 'transcript_features', [
     'non_can_intron', 'Non-canonical splicing', 'non_can_intron', {
      'display' => 'normal',
      'strand'  => 'r',
-     'colours' => $self->species_defs->colour('feature')
-    }
-  ], [
-    'TSE_generic_match_label', 'Transc. evidence:',   'TSE_generic_match_label', {
-      'display' => 'normal',
-      'strand'  => 'r'
-    }
-  ], [
-    'TSE_generic_match', '', 'TSE_generic_match', {
-      'display' => 'normal',
-      'strand'  => 'r',
-      'colours' => $self->species_defs->colour('feature')
-    }
-  ], [
-    'SE_generic_match_label',  'Exon evidence:', 'SE_generic_match_label', {
-      'display' => 'normal',
-      'strand'  => 'r'
-    }
-  ], [
-    'SE_generic_match', '', 'SE_generic_match', {
+     'colours' => $self->species_defs->colour('feature'),
+     'description' => 'Non-canonical splice sites (ie not GT/AG, GC/AG, AT/AC or NN/NN)',
+   }
+  ]);
+
+  my $transcript_evi_desc = $is_vega_gene ? 'Alignments from the Havana pipeline that support the transcript' :  'Alignments used to build this transcript model';
+  $self->add_tracks( 'evidence', [
+    'TSE_generic_match', 'Transcript supporting evidence', 'TSE_generic_match', {
       'display' => 'normal',
       'strand'  => 'r',
-      'colours' => $self->species_defs->colour('feature')
+      'colours' => $self->species_defs->colour('feature'),
+      'description' => $transcript_evi_desc,
     }
-  ], [
+  ]);
+  unless ($is_vega_gene) {
+    $self->add_tracks( 'evidence', [
+      'SE_generic_match', 'Exon supporting evidence (Ensembl)', 'SE_generic_match', {
+        'display' => 'normal',
+        'strand'  => 'r',
+        'colours' => $self->species_defs->colour('feature'),
+        'description' => 'Alignments from Ensembl pipeline that support the exons',
+        'logic_names_excluded' => '_havana',
+      }
+    ], [
+      'SE_generic_match_havana', 'Exon supporting evidence (Havana)', 'SE_generic_match', {
+        'display' => 'normal',
+        'strand'  => 'r',
+        'colours' => $self->species_defs->colour('feature'),
+        'description' => 'Alignments from Havana pipeline that support the exons',
+        'logic_names_only' => '_havana',
+      }
+    ]);
+  }
+  $self->add_tracks( 'evidence', [
     'TSE_background_exon', '', 'TSE_background_exon', {
       'display' => 'normal',
-      'strand'  => 'r'
+      'strand'  => 'r',
+      'menu'    => 'no',
     }
   ], [
     'TSE_legend', 'Legend', 'TSE_legend', {
       'display' => 'normal',
-      'strand' => 'r',
-      'colours'  => $self->species_defs->colour('feature')
+      'strand'  => 'r',
+      'colours' => $self->species_defs->colour('feature'),
+      'menu'    => 'no',
     }
   ]);
+
+  #remove genes from menu
+  $self->modify_configs(
+    [ 'TSE_transcript' ],
+    { menu => 'no' }
+  );
 }
 
 1;
