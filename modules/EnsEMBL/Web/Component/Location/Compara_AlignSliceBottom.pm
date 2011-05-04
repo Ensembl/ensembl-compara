73740e66d70edd7178c67ff15210d5f989dbdb0e
Simon Brent <sb23@sanger.ac.uk>
2011-05-04 14:33:43 +0000

    use hub instead of object. support changes to image config

--- modules/EnsEMBL/Web/Component/Location/Compara_AlignSliceBottom.pm
+++ modules/EnsEMBL/Web/Component/Location/Compara_AlignSliceBottom.pm
@@ -16,52 +16,48 @@ sub _init {
 }
 
 sub content {
-  my $self = shift;
-  my $object = $self->object;
-  my $threshold = 1000100 * ($object->species_defs->ENSEMBL_GENOME_SIZE||1);
-  my $align_params = $object->param('align');
-  
-  my ($align, undef, $target_slice_name) = split '--', $align_params;
+  my $self         = shift;
+  my $hub          = $self->hub;
+  my $species_defs = $hub->species_defs;
+  my $object       = $self->object;
+  my $threshold    = 1000100 * ($species_defs->ENSEMBL_GENOME_SIZE || 1);
+  my $align_params = $hub->param('align');
+  my ($align)      = split '--', $align_params;
   
   return $self->_warning('Region too large', '<p>The region selected is too large to display in this view - use the navigation above to zoom in...</p>') if $object->length > $threshold;
   return $self->_info('No alignment specified', '<p>Select the alignment you wish to display from the box above.</p>') unless $align;
   
-  my $align_details = $object->species_defs->multi_hash->{'DATABASE_COMPARA'}->{'ALIGNMENTS'}->{$align};
+  my $align_details = $species_defs->multi_hash->{'DATABASE_COMPARA'}->{'ALIGNMENTS'}->{$align};
 
   return $self->_error('Unknown alignment', '<p>The alignment you have select does not exist in the current database.</p>') unless $align_details;
   
-  my $primary_species = $object->species;
+  my $primary_species = $hub->species;
   
   if (!exists $align_details->{'species'}->{$primary_species}) {
     return $self->_error('Unknown alignment', sprintf(
       '<p>%s is not part of the %s alignment in the database.</p>', 
-      $object->species_defs->species_label($primary_species),
+      $species_defs->species_label($primary_species),
       encode_entities($align_details->{'name'})
     ));
   }
   
-  my $image_width = $self->image_width;
-  my $slice = $object->slice;
-  my ($slices) = $self->get_slices($slice, $align_params, $primary_species);
-  
-  my @skipped;
-  my @missing;
-  my @images;
-  my $i = 1;
-  my $html;
-  my $info;
-  
+  my $image_width     = $self->image_width;
+  my $slice           = $object->slice;
+  my ($slices)        = $self->get_slices($slice, $align_params, $primary_species);
   my %aligned_species = map { $_->{'name'} => 1 } @$slices;
+  my $i               = 1;
+  my (@skipped, @missing, @images, $html, $info);
   
   foreach (keys %{$align_details->{'species'}}) {
     next if $_ eq $primary_species;
     
-    push @skipped, $_ if $align_details->{'class'} !~ /pairwise/ && ($object->param(sprintf 'species_%d_%s', $align, lc)||'off') eq 'off';
+    push @skipped, $_ if $align_details->{'class'} !~ /pairwise/ && ($hub->param(sprintf 'species_%d_%s', $align, lc) || 'off') eq 'off';
     push @missing, $_ unless $aligned_species{$_} || $_ eq 'ancestral_sequences';
   }
   
   foreach (@$slices) {
-    my $image_config = $object->get_imageconfig('alignsliceviewbottom', "alignsliceviewbottom_$i", $_->{'name'});
+    my $species      = $_->{'name'} eq 'Ancestral_sequences' ? 'Multi' : $_->{'name'}; # Cheating: set species to Multi to stop errors due to invalid species.
+    my $image_config = $hub->get_imageconfig('alignsliceviewbottom', "alignsliceviewbottom_$i", $species);
     
     $image_config->set_parameters({
       container_width => $_->{'slice'}->length,
@@ -72,8 +68,8 @@ sub content {
     
     my ($species_name, $slice_name) = split ':', $_->{'name'};
     
-    my $panel_caption = $object->species_defs->get_config($species_name, 'SPECIES_COMMON_NAME') || 'Ancestral sequences';
-    $panel_caption .= " $slice_name" if $slice_name;
+    my $panel_caption = $species_defs->get_config($species_name, 'SPECIES_COMMON_NAME') || 'Ancestral sequences';
+    $panel_caption   .= " $slice_name" if $slice_name;
 
     $image_config->get_node('alignscalebar')->set('caption', $panel_caption);
     
@@ -95,18 +91,18 @@ sub content {
     $info .= sprintf(
       '<p>The following %d species in the alignment are not shown in the image. Use the "<strong>Configure this page</strong>" on the left to show them.<ul><li>%s</li></ul></p>', 
       scalar @skipped, 
-      join "</li>\n<li>", sort map $object->species_defs->species_label($_), @skipped
+      join "</li>\n<li>", sort map $species_defs->species_label($_), @skipped
     );
   }
   
   if (scalar @missing) {
     if ($align_details->{'class'} =~ /pairwise/) {
-      $info .= sprintf '<p>%s has no alignment in this region</p>', $object->species_defs->species_label($missing[0]);
+      $info .= sprintf '<p>%s has no alignment in this region</p>', $species_defs->species_label($missing[0]);
     } else {
       $info .= sprintf(
         '<p>The following %d species have no alignment in this region:<ul><li>%s</li></ul></p>', 
         scalar @missing, 
-        join "</li>\n<li>", sort map $object->species_defs->species_label($_), @missing
+        join "</li>\n<li>", sort map $species_defs->species_label($_), @missing
       );
     }
   }
