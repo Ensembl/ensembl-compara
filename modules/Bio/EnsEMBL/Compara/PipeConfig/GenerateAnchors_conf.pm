9c807ed53abe4e23a48f83578c822a0952991203
ens-stephenf <stephenf@ebi.ac.uk>
2011-05-05 15:49:27 +0000

    updated.

--- modules/Bio/EnsEMBL/Compara/PipeConfig/GenerateAnchors_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/GenerateAnchors_conf.pm
@@ -13,6 +13,7 @@ sub default_options {
     return {
         'pipeline_name' => 'compara_GenerateAnchors',
 	'ensembl_cvs_root_dir' => '/nfs/users/nfs_s/sf5/src',
+	'species_tree_file' => $self->o('ensembl_cvs_root_dir').'/ensembl-compara/scripts/pipeline/species_tree_blength.nh',
 	   # parameters that are likely to change from execution to another:
 	'release'               => '62',
 	'rel_suffix'            => '',    # an empty string by default, a letter otherwise
@@ -27,6 +28,14 @@ sub default_options {
 		-pass   => $self->o('password'),
 		-dbname => $ENV{'USER'}.'_compara_generate_anchors'.$self->o('rel_with_suffix'),
    	},
+	'compara_pairwise_db' => {
+		-user => 'ensro',
+		-port => 3306,
+		-host => 'ens-livemirror',
+		-pass => '',
+		-dbname => 'ensembl_compara_62',
+	},
+	'core_db_url' => 'mysql://anonymous@ensembldb.ensembl.org:5306/62',
      };
 }
 
@@ -42,15 +51,7 @@ sub pipeline_wide_parameters {
 	return {
 		%{$self->SUPER::pipeline_wide_parameters},
 
-		'compara_pairwise_db' => {
-				-user => 'ensro',
-				-port => '3306',
-				-group => 'compara',
-				-species => 'Multi',
-				-host => 'ens-livemirror',
-				-dbname => 'ensembl_compara_61',
-				},
-					
+		'compara_pairwise_db' => $self->o('compara_pairwise_db'),
 		'min_anchor_size'     => 50,
 		'min_number_of_org_hits_per_base' => 2,
 	        'reference_genome_db_id' => 90,
@@ -75,13 +76,15 @@ sub pipeline_analyses {
 	    {   -logic_name => 'innodbise_table_factory',
 		-module     => 'Bio::EnsEMBL::Hive::RunnableDB::JobFactory',
 		-parameters => {
-				'inputquery'      => "SELECT table_name FROM information_schema.tables WHERE table_schema ='".$self->o('pipeline_db','-dbname')."' AND table_name!='genome_db' AND engine='MyISAM' ",
+				'inputquery'      => "SELECT table_name FROM information_schema.tables WHERE table_schema ='" . 
+						$self->o('pipeline_db','-dbname') .
+						"' AND table_name!='genome_db' AND engine='MyISAM' ",
 				'input_id'        => { 'table_name' => '#_range_start#' },
 				'fan_branch_code' => 2,
 			       },
 		-input_ids => [{}],
 		-flow_into => {
-			       2 => [ 'innodbise_table'  ],
+			       2 => [ 'innodbise_table' ],
 			      },
 	    },
 
@@ -96,43 +99,81 @@ sub pipeline_analyses {
 	    {	-logic_name	=> 'chunk_reference_dnafrags',
 		-module		=> 'Bio::EnsEMBL::Compara::Production::EPOanchors::ChunkRefDnafrags',
 		-parameters	=> {
-				    'method_link_type' => 'BLASTZ_NET:LASTZ_NET',
+				    'compara_db' => $self->o('compara_pairwise_db'),
+				    'method_link_types' => [ 'BLASTZ_NET', 'LASTZ_NET'],
 				    'chunk_size' => '10000000',
 				    'consensus_overlap' => 0,
-				    'genome_db_ids' => [38, 101],
-				#    'genome_db_ids' => [],
+				    'genome_db_ids' => [39, 64, 93],
+#				    'genome_db_ids' => [],
 				   },
 		-input_ids 	=> [{}],
 		-wait_for       => [ 'innodbise_table_factory', 'innodbise_table' ],	
 		-flow_into	=> {
-					1 => [ 'find_pairwise_overlaps' ],
-					2 => [ 'mysql:////dnafrag?insertion_method=INSERT_IGNORE' ],
-					3 => [ 'mysql:////genome_db?insertion_method=INSERT_IGNORE'  ],
+					2 => [ 'find_pairwise_overlaps' ],
+					3 => [ 'import_entries' ],
+					4 => [ 'mysql:////meta' ],
 				   },
 				 
 	    },
-		
+
+	    {   -logic_name => 'import_entries',
+	        -module     => 'Bio::EnsEMBL::Hive::RunnableDB::MySQLTransfer',
+    	        -parameters => {
+	            'src_db_conn'   => $self->o('compara_pairwise_db'),
+	            'where'         => 'genome_db_id IN (#genome_dbs_csv#)',
+	        },
+		-flow_into      => {
+					2 => [ 'load_species_tree' ],
+					3 => [ 'set_genome_db_locator' ],
+		},
+	    },
 	    {
+		-logic_name     => 'set_genome_db_locator',
+		-module         => 'Bio::EnsEMBL::Compara::Production::EPOanchors::SetGenomeDBLocator',
+		-parameters     => { 'core_db_url' => $self->o('core_db_url') },
+		-input_ids => [{}],
+		-wait_for       => [ 'import_entries' ],
+		-flow_into => {
+				2 => [ 'mysql:////genome_db?insertion_method=REPLACE' ],
+		}
+	
+	    },
+	    {
+		-logic_name => 'load_species_tree',
+		-module        => 'Bio::EnsEMBL::Hive::RunnableDB::JobFactory',
+		-parameters => {
+			'inputcmd'        => 'cat ' . $self->o('species_tree_file') . ' | tr \'[A-Z]\' \'[a-z]\'',
+			'input_id'        => { 'meta_key' => 'tree_string', 'meta_value' => '#_range_start#' },
+			'fan_branch_code' => 2,
+		},
+		-input_ids      => [{}],
+		-flow_into => {
+				2 => [ 'mysql:////meta' ],
+		},
+		-wait_for       => [ 'import_entries' ],
+	   },
+		
+	   {
 		-logic_name	=> 'find_pairwise_overlaps',
 		-module		=> 'Bio::EnsEMBL::Compara::Production::EPOanchors::FindPairwiseOverlaps',
-		-wait_for	=> [ 'chunk_reference_dnafrags' ],
 		-parameters     => { 'method_link_species_set_id' => 300, },
+		-wait_for	=> [ 'import_entries' ],
 		-flow_into	=> {
 					1 => [ 'pecan' ],
 					3 => [ 'mysql:////dnafrag_region?insertion_method=INSERT_IGNORE' ],
 				   },
 		-hive_capacity => 100,
-	    },
-	    {
+	   },
+
+	   {
 		-logic_name    => 'pecan',
 		-module        => 'Bio::EnsEMBL::Compara::Production::GenomicAlignBlock::Pecan',
 		-wait_for      => 'find_pairwise_overlaps',
 		-parameters     => { 'method_link_species_set_id' => 400, },
 		-hive_capacity => 100,
-	    },
+	   },
 	];
 }	
 
 
 1;
-
