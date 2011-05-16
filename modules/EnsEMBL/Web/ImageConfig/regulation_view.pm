42ce22a46b540f04480a373fc5823461b7d1ed73
Simon Brent <sb23@sanger.ac.uk>
2011-05-16 13:54:57 +0000

    merge with reg_detail_by_cell_line and regulation_view_bottom

--- modules/EnsEMBL/Web/ImageConfig/regulation_view.pm
+++ modules/EnsEMBL/Web/ImageConfig/regulation_view.pm
@@ -7,16 +7,19 @@ use strict;
 use base qw(EnsEMBL::Web::ImageConfig);
 
 sub init {
-  my $self = shift;
+  my $self         = shift;
+  my @feature_sets = ('cisRED', 'VISTA', 'miRanda', 'NestedMICA', 'REDfly CRM', 'REDfly TFBS', 'search');
+  my @cell_lines   = sort keys %{$self->species_defs->databases->{'DATABASE_FUNCGEN'}{'tables'}{'cell_type'}{'ids'}};
+  
+  s/\:\d*// for @cell_lines;
   
   $self->set_parameters({
-    title          => 'Feature context',
-    show_labels    => 'yes',
-    label_width    => 113,
-    opt_lines      => 1,
-    opt_highllight => 'yes',
-  });  
-
+    title       => 'Details by cell line',
+    show_labels => 'yes',
+    label_width => 113,
+    opt_lines   => 1,
+  });
+  
   $self->create_menus(
     sequence       => 'Sequence',
     transcript     => 'Genes',
@@ -29,38 +32,24 @@ sub init {
     functional     => 'Functional Genomics', 
     multiple_align => 'Multiple alignments',
     variation      => 'Germline variation',
-    other          => 'Decorations',
+    other          => '',
     information    => 'Information'
   );
-
-  $self->add_tracks('other',
-    [ 'scalebar',                 '', 'scalebar',                 { display => 'normal', strand => 'f', menu => 'no', name => 'Scale bar' }],
-    [ 'ruler',                    '', 'ruler',                    { display => 'normal', strand => 'f', menu => 'no', name => 'Ruler'     }],
-    [ 'fg_background_regulation', '', 'fg_background_regulation', { display => 'normal', strand => 'r', menu => 'no', tag => 0            }],
-);
+  
+  $self->load_tracks;
   
   $self->add_tracks('sequence',
     [ 'contig', 'Contigs', 'stranded_contig', { display => 'normal', strand => 'r' }]
-  );
-
-  $self->load_tracks;
-  $self->load_configured_das;
-
-  $self->modify_configs(
-    [ 'functional' ],
-    { display => 'off', menu => 'no' }
-  );
-  
-  my @feature_sets = ('cisRED', 'VISTA', 'miRanda', 'NestedMICA', 'REDfly CRM', 'REDfly TFBS', 'search');
-  
-  $self->modify_configs(
-    [ map "regulatory_regions_funcgen_$_", @feature_sets ],
-    { menu => 'yes' }
   );
   
   $self->modify_configs(
     [ 'transcript_core_ensembl' ],
     { display => 'collapsed_nolabel' }
+  );
+  
+  $self->modify_configs(
+    [ map "regulatory_regions_funcgen_$_", @feature_sets ],
+    { menu => 'yes' }
   );
   
   $self->modify_configs(
@@ -70,7 +59,75 @@ sub init {
   
   $self->modify_configs(
     [ 'opt_empty_tracks' ],
-    { menu => 'yes', display => 'off' }
+    { menu => 'yes' }
+  );
+  
+  foreach my $cell_line (@cell_lines) {
+    my $display = $cell_line =~ /^(MultiCell|CD4)$/ ? 'tiling_feature' : 'compact';
+
+    # Turn on reg_feats track
+    $self->modify_configs(
+      [ "reg_feats_$cell_line" ],
+      { display => 'normal', menu => 'yes' }
+    );
+    
+    # Turn on core evidence track
+    $self->modify_configs(
+      [ "reg_feats_core_$cell_line" ],
+      { display => $display , menu => 'yes' }
+    );
+    
+    push @{$self->{'tracks_to_remove'}}, "reg_feats_$cell_line", "reg_feats_core_$cell_line";
+    
+    next if $cell_line eq 'MultiCell';
+    
+    # Turn on supporting evidence track
+    $self->modify_configs(
+      [ "reg_feats_other_$cell_line" ],
+      { display => 'compact', menu => 'yes' }
+    );
+    
+    push @{$self->{'tracks_to_remove'}}, "reg_feats_other_$cell_line";
+  }
+  
+  if ($self->{'code'} ne $self->{'type'}) {
+    my $func = "init_$self->{'code'}";
+    $self->$func if $self->can($func);
+  }
+}
+
+sub init_top {
+  my $self = shift;
+
+  $self->add_tracks('other',
+    [ 'scalebar',                 '', 'scalebar',                 { display => 'normal', strand => 'f', menu => 'no', name => 'Scale bar' }],
+    [ 'ruler',                    '', 'ruler',                    { display => 'normal', strand => 'f', menu => 'no', name => 'Ruler'     }],
+    [ 'fg_background_regulation', '', 'fg_background_regulation', { display => 'normal', strand => 'r', menu => 'no', tag => 0            }],
+  );
+  
+  $self->get_node($_)->remove for @{$self->{'tracks_to_remove'}};
+}
+
+sub init_cell_line {
+  my $self = shift;
+  $self->get_node($_)->remove for 'contig', 'transcript_core_ensembl';
+}
+
+sub init_bottom {
+  my $self = shift;
+  
+  $self->add_tracks('other',
+    [ 'fg_background_regulation', '', 'fg_background_regulation', { display => 'normal', strand => 'r', menu => 'no', tag => 0            }],
+    [ 'scalebar',                 '', 'scalebar',                 { display => 'normal', strand => 'r', menu => 'no', name => 'Scale bar' }],
+    [ 'ruler',                    '', 'ruler',                    { display => 'normal', strand => 'r', menu => 'no', name => 'Ruler'     }],
+  );
+  
+  $self->get_node($_)->remove for @{$self->{'tracks_to_remove'}}, 'contig', 'transcript_core_ensembl';
+  
+  $self->modify_configs(
+    [ 'fg_regulatory_features_legend' ],
+    { display => 'normal' }
   );
 }
+
 1;
