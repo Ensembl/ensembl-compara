5cc7e1908992b21ee1473e75cc5713a80898e284
Simon Brent <sb23@sanger.ac.uk>
2011-05-19 09:05:01 +0000

    fix internal server error, redirect loop

--- modules/EnsEMBL/Web/Command/Account/ResetFavourites.pm
+++ modules/EnsEMBL/Web/Command/Account/ResetFavourites.pm
@@ -1,16 +1,17 @@
+# $Id$
+
 package EnsEMBL::Web::Command::Account::ResetFavourites;
 
 use strict;
-use warnings;
 
 use base qw(EnsEMBL::Web::Command);
 
 sub process {
   my $self = shift;
-  my $user   = $self->object->user;
-  $user->specieslists->destroy;
-
-  $self->ajax_redirect($ENV{'ENSEMBL_BASE_URL'});
+  my $hub  = $self->hub;
+  my $user = $hub->user;
+  $user->specieslists->delete_all;
+  $hub->redirect($hub->species_defs->ENSEMBL_BASE_URL);
 }
 
 1;
