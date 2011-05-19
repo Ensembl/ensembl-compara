3662d30b3aefbfc40c644acf12bffd94d9fad787
Harpreet Riat <hr5@sanger.ac.uk>
2011-05-19 15:35:07 +0000

    changes for new Form module; fix for warning'Odd number of elements in anonymous hash'

--- modules/EnsEMBL/Web/Component/UserData/SelectRemote.pm
+++ modules/EnsEMBL/Web/Component/UserData/SelectRemote.pm
@@ -31,46 +31,40 @@ sub content {
   push @attachable, @{$object->species_defs->USERDATA_REMOTE_FORMATS};
   my @all_formats = sort {lc($a) cmp lc($b)} @attachable;
   my $formats = join(', ', @all_formats);
-  $form->add_notes({'heading'=>'Tip', 'text'=> qq(
-  Accessing data via a URL can be slow unless you use an indexed format such as BAM. However it has the advantage that you always see the same data as the file on your own machine.<br /><br />
-  We currently accept attachment of the following formats: $formats.
-  VCF files must be indexed prior to attachment.
-  )});
 
-  $form->add_element('type'  => 'URL',
-                     'name'  => 'url',
-                     'label' => 'File URL',
-                     'size'  => '30',
-                     'value' => $object->param('url'),
-                     'notes' => '( e.g. http://www.example.com/MyProject/mydata.gff )');
+  $form->add_notes({
+    'heading' => 'Tip',
+    'text'    => qq(
+                    Accessing data via a URL can be slow unless you use an indexed format such as BAM. However it has the advantage that you always see the same data as the file on your own machine.<br /><br />
+                    We currently accept attachment of the following formats: $formats.
+                    VCF files must be indexed prior to attachment.)
+  });
 
-  my $format_values = [{'name' => '-- Choose --', 'value' => ''}];
-  foreach my $f (@all_formats) {
-    push @$format_values, {'name' => $f, 'value' => uc($f)};
-  }
-
-  $form->add_element(
-      'type'    => 'DropDown',
-      'name'    => 'format',
-      'label'   => "Data format",
-      'values'  => $format_values,
-      'select'  => 'select',
-      'disabled'=> scalar @all_formats ? 0 : 1,
-  );
-
-  $form->add_element('type'  => 'String',
-                     'name'  => 'name',
-                     'label' => 'Name for this track',
-                     'size'  => '30',
-                     );
-
-  if ($user && $user->id) {
-    $form->add_element('type'    => 'CheckBox',
-                       'name'    => 'save',
-                       'label'   => 'Save URL to my account',
-                       'notes'   => 'N.B. Only the file address will be saved, not the data itself',
-                       );
-  }
+  $form->add_field([{
+    'type'      => 'url',
+    'name'      => 'url',
+    'label'     => 'File URL',
+    'size'      => '30',
+    'value'     => $object->param('url') || '',
+    'notes'     => '( e.g. http://www.example.com/MyProject/mydata.gff )'
+  }, {
+    'type'      => 'dropdown',
+    'name'      => 'format',
+    'label'     => "Data format",
+    'values'    => [{'caption' => '-- Choose --', 'value' => ''}, map {{'caption' => $_, 'value' => uc $_}} @all_formats ],
+    'select'    => 'select',
+    'disabled'  => scalar @all_formats ? 0 : 1,
+  }, {
+    'type'      => 'string',
+    'name'      => 'name',
+    'label'     => 'Name for this track',
+    'size'      => '30',
+  }, $user && $user->id ? {
+    'type'      => 'checkbox',
+    'name'      => 'save',
+    'label'     => 'Save URL to my account',
+    'notes'     => 'N.B. Only the file address will be saved, not the data itself',
+  } : ()]);
 
   return $form->render;
 }
