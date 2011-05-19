3ee8d20ee2ece828bf0612f3118b2bda65154b93
Simon Brent <sb23@sanger.ac.uk>
2011-05-19 10:04:15 +0000

    don't show info message below images - seems pretty useless. view configs are now linked to components

--- modules/EnsEMBL/Web/Component/Location/ViewBottom.pm
+++ modules/EnsEMBL/Web/Component/Location/ViewBottom.pm
@@ -1,10 +1,8 @@
+# $Id$
+
 package EnsEMBL::Web::Component::Location::ViewBottom;
 
 use strict;
-use warnings;
-no warnings 'uninitialized';
-
-use Time::HiRes qw(time);
 
 use base qw(EnsEMBL::Web::Component::Location);
 
@@ -18,21 +16,18 @@ sub _init {
 
 sub content {
   my $self        = shift;
+  my $hub         = $self->hub;
   my $object      = $self->object;
-  my $threshold   = 1000100 * ($object->species_defs->ENSEMBL_GENOME_SIZE||1);
+  my $threshold   = 1000100 * ($hub->species_defs->ENSEMBL_GENOME_SIZE || 1);
   my $image_width = $self->image_width;
   
   return $self->_warning('Region too large', '<p>The region selected is too large to display in this view - use the navigation above to zoom in...</p>') if $object->length > $threshold;
   
   my $slice        = $object->slice;
   my $length       = $slice->end - $slice->start + 1;
-  my $T            = time;
-  my $image_config = $object->get_imageconfig('contigviewbottom');
-  my $s            = $object->param('panel_top') eq 'yes' ? 3 : 2;
-  $T = sprintf "%0.3f", time - $T;
+  my $image_config = $hub->get_imageconfig('contigviewbottom');
+  my $s            = $hub->get_viewconfig('ViewTop')->get('show_panel') eq 'yes' ? 3 : 2;
   
-  $image_config->tree->dump("View Bottom configuration [ time to generate $T sec ]", '([[caption]])') if $object->species_defs->ENSEMBL_DEBUG_FLAGS & $object->species_defs->ENSEMBL_DEBUG_TREE_DUMPS;
-
   $image_config->set_parameters({
     container_width => $length,
     image_width     => $image_width || 800, # hack at the moment
@@ -40,7 +35,7 @@ sub content {
   });
 
   ## Force display of individual low-weight markers on pages linked to from Location/Marker
-  if (my $marker_id = $object->param('m')) {
+  if (my $marker_id = $hub->param('m')) {
     $image_config->modify_configs(
       [ 'marker' ],
       { marker_id => $marker_id }
@@ -48,7 +43,7 @@ sub content {
   }
 
   # Add multicell configuration
-  if (keys %{$object->species_defs->databases->{'DATABASE_FUNCGEN'}->{'tables'}{'cell_type'}{'ids'}}){
+  if (keys %{$hub->species_defs->databases->{'DATABASE_FUNCGEN'}{'tables'}{'cell_type'}{'ids'}}){
     my $web_slice_obj = $self->new_object( 'Slice', $slice, $object->__data );
     my $cell_line_data = $web_slice_obj->get_cell_line_data($image_config);
     $image_config->{'data_by_cell_line'} = $cell_line_data;
@@ -57,30 +52,17 @@ sub content {
   # Lets see if we have any das sources
   $self->_attach_das($image_config);
   
-  my $image_config_2 = $object->get_imageconfig('contigviewtop');
-  my $info           = $image_config->_update_missing($object);
-  my $info_2         = $image_config_2->_update_missing($object);
-  my $extra_message  = '';
-  
-  if ($object->param('panel_top') eq 'yes') {
-    $extra_message .= "You currently have $info_2->{'count'} tracks in the overview panel and $info->{'count'} tracks in the main panel turned off";
-  } else {
-    $extra_message .= "You currently have the overview panel and $info->{'count'} tracks on the main panel turned off";
-  }
+  $image_config->_update_missing($object);
   
   my $image = $self->new_image($slice, $image_config, $object->highlights);
   
 	return if $self->_export_image($image);
   
   $image->{'panel_number'} = 'bottom';
-  $image->imagemap = 'yes';
-
+  $image->imagemap         = 'yes';
   $image->set_button('drag', 'title' => 'Click or drag to centre display');
   
-  my $html = $image->render;
-  $html .= $self->_info('Configuring the display', qq{<p>$extra_message. To change the tracks you are displaying, use the "<strong>Configure this page</strong>" link on the left.</p>});
-  
-  return $html;
+  return $image->render;
 }
 
 
