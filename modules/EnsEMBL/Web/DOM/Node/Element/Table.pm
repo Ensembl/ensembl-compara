10075a29c52b11ac42f685763257230629f46abe
Harpreet Riat <hr5@sanger.ac.uk>
2011-05-27 09:25:21 +0000

    rows and cells methods added to table

--- modules/EnsEMBL/Web/DOM/Node/Element/Table.pm
+++ modules/EnsEMBL/Web/DOM/Node/Element/Table.pm
@@ -15,4 +15,14 @@ sub w3c_appendable {
   return $child->node_type == $self->ELEMENT_NODE && $child->node_name =~ /^(caption|colgroup|col|tbody|tfoot|thead|tr)$/ ? 1 : 0;
 }
 
+sub rows {
+  ## Gets an arrayref of all the rows inside the table
+  return [ map { $_->node_name =~ /^t(.*)$/ ? $1 eq 'r' ? $_ : (@{$_->child_nodes}) : () } @{shift->child_nodes} ];
+}
+
+sub cells {
+  ## Gets an arrayref of all the cells in the table
+  return [ map {(@{$_->cells})} @{shift->rows} ];
+}
+
 1;
