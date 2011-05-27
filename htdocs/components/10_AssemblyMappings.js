8e4e2110976b5a5276f24d0ad207d13868380618
Anne Parker <ap5@sanger.ac.uk>
2011-05-27 14:50:23 +0000

    New module used by Assembly Converter to update list of assembly mappings whenever species is changed in adjacent dropdown

---
+++ htdocs/components/10_AssemblyMappings.js
@@ -0,0 +1,20 @@
+Ensembl.Panel.AssemblyMappings = Ensembl.Panel.extend({
+  // Hides options in 'conversion' that don't match the current value in 'species
+  init: function () {
+    var panel = this;
+    this.base();
+
+    this.elLk.conversion = $('.conversion', this.el);
+    this.elLk.species    = $('.dropdown_remotecontrol', this.el).bind('change', function () {
+      panel.showBySpecies();  
+    });
+    
+    this.showBySpecies();
+  },
+
+  showBySpecies: function () {
+    this.elLk.conversion.children().hide().filter(':selected').removeAttr('selected').end()
+      .filter('.' + this.elLk.species.val()).show().first().attr('selected', 'selected');
+  }
+
+});
