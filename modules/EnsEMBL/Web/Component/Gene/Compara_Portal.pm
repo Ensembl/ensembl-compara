f847467a8ad037cfa0c0ae06954d8ecc95a2b2c3
Simon Brent <sb23@sanger.ac.uk>
2011-06-02 09:58:02 +0000

    use generic portal component as parent

--- modules/EnsEMBL/Web/Component/Gene/Compara_Portal.pm
+++ modules/EnsEMBL/Web/Component/Gene/Compara_Portal.pm
@@ -4,73 +4,26 @@ package EnsEMBL::Web::Component::Gene::Compara_Portal;
 
 use strict;
 
-use base qw(EnsEMBL::Web::Component);
-
-sub _init {
-  my $self = shift;
-  my $hub  = $self->hub;
-  
-  $self->cacheable(1);
-  $self->ajaxable(0);
-}
+use base qw(EnsEMBL::Web::Component::Portal);
 
 sub content {
-  my $self      = shift;
-  my $hub       = $self->hub;
-  my $avail     = $self->object->availability;
+  my $self         = shift;
+  my $hub          = $self->hub;
+  my $availability = $self->object->availability;
+  my $location     = $hub->url({ type => 'Location',  action => 'Compara' });
 
-  my ($align_url, $tree_url, $ortho_url, $para_url, $fam_url);
-  if ($avail->{'has_alignments'}) {
-    $align_url   = $hub->url({'action' => 'Compara_Alignments'});
-  }
-  if ($avail->{'has_gene_tree'}) {
-    $tree_url   = $hub->url({'action' => 'Compara_Tree'});
-  }
-  if ($avail->{'has_orthologs'}) {
-    $ortho_url  = $hub->url({'action' => 'Compara_Ortholog'});
-  }
-  if ($avail->{'has_paralogs'}) {
-    $para_url   = $hub->url({'action' => 'Compara_Paralog'});
-  }
-  if ($avail->{'family'}) {
-    $fam_url    = $hub->url({'action' => 'Family'});
-  }
+  $self->{'buttons'} = [
+    { title => 'Genomic alignments', img => 'compara_align', url => $availability->{'has_alignments'} ? $hub->url({ action => 'Compara_Alignments' }) : '' },
+    { title => 'Gene tree',          img => 'compara_tree',  url => $availability->{'has_gene_tree'}  ? $hub->url({ action => 'Compara_Tree'       }) : '' },
+    { title => 'Orthologues',        img => 'compara_ortho', url => $availability->{'has_orthologs'}  ? $hub->url({ action => 'Compara_Ortholog'   }) : '' },
+    { title => 'Paralogues',         img => 'compara_para',  url => $availability->{'has_paralogs'}   ? $hub->url({ action => 'Compara_Paralog'    }) : '' },
+    { title => 'Families',           img => 'compara_fam',   url => $availability->{'family'}         ? $hub->url({ action => 'Family'             }) : '' },
+  ];
 
-  my @buttons = (
-    {'title' => 'Genomic alignments', => 'img' => 'compara_align', 'url' => $align_url},
-    {'title' => 'Gene tree',          => 'img' => 'compara_tree',  'url' => $tree_url},
-    {'title' => 'Orthologues',        => 'img' => 'compara_ortho', 'url' => $ortho_url},
-    {'title' => 'Paralogues',         => 'img' => 'compara_para',  'url' => $para_url},
-    {'title' => 'Families',           => 'img' => 'compara_fam',   'url' => $fam_url},
-  );
-
-  my $html = qq(
-    <div class="centered">
-  );
-  foreach my $button (@buttons) {
-    my $title = $button->{'title'};
-    my $img = $button->{'img'};
-    my $url = $button->{'url'};
-    if ($url) {
-      $img  .= '.gif';
-      $html .= qq(<a href="$url" title="$title"><img src="/img/$img" class="portal" alt="" /></a>);
-    }
-    else {
-      $img   .= '_off.gif';
-      $title .= ' (NOT AVAILABLE)';
-      $html  .= qq(<img src="/img/$img" class="portal" alt="" title="$title" />);
-    }
-  }
-
-  my $url = $self->hub->url({'type'=>'Location', 'action'=>'Compara'});
-  $html .= qq(<p>More views of comparative genomics data, such as multiple alignments and synteny, are available on the <a href="$url">Location</a> page for this gene.</p>);
- 
-  $html .= qq(
-    </div>
-  );
+  my $html  = $self->SUPER::content;
+     $html .= qq{<div class="centered">More views of comparative genomics data, such as multiple alignments and synteny, are available on the <a href="$location">Location</a> page for this gene.</div>};
 
   return $html;
 }
-
 
 1;
--- modules/EnsEMBL/Web/Component/Location/Compara_Portal.pm
+++ modules/EnsEMBL/Web/Component/Location/Compara_Portal.pm
@@ -4,75 +4,33 @@ package EnsEMBL::Web::Component::Location::Compara_Portal;
 
 use strict;
 
-use base qw(EnsEMBL::Web::Component);
-
-sub _init {
-  my $self = shift;
-  my $hub  = $self->hub;
-  
-  $self->cacheable(1);
-  $self->ajaxable(0);
-}
+use base qw(EnsEMBL::Web::Component::Portal);
 
 sub content {
-  my $self      = shift;
-  my $hub       = $self->hub;
-  my $object    = $self->object;
-  my $avail     = $object->availability;
+  my $self         = shift;
+  my $hub          = $self->hub;
+  my $availability = $self->object->availability;
+  my $slice        = $availability->{'slice'};
+  my $alignments   = $availability->{'has_alignments'};
+  my $gene         = $hub->param('g') ? $hub->url({ type => 'Gene',  action => 'Compara' }) : '';
 
-  my ($text_url, $image_url, $multi_url, $syn_url);
-  if ($avail->{'slice'}) {
-    if ($avail->{'has_alignments'}) {
-      $image_url  = $hub->url({'action' => 'Compara_Alignments', 'function' => 'Image'});
-      $text_url   = $hub->url({'action' => 'Compara_Alignments'});
-    }
-    if ($avail->{'has_pairwise_alignments'}) {
-      $multi_url   = $hub->url({'action' => 'Multi'});
-    }
-  }
-  if ($avail->{'chromosome'} && $avail->{'has_synteny'}) {
-    $syn_url    = $hub->url({'action' => 'Synteny'});
-  }
+  $self->{'buttons'} = [
+    { title => 'Alignments (image)', img => 'compara_image', url => $slice && $alignments                                           ? $hub->url({ action => 'Compara_Alignments', function => 'Image' }) : '' },
+    { title => 'Alignments (text)',  img => 'compara_text',  url => $slice && $alignments                                           ? $hub->url({ action => 'Compara_Alignments'                      }) : '' },
+    { title => 'Multi-species view', img => 'compara_multi', url => $slice && $availability->{'has_pairwise_alignments'}            ? $hub->url({ action => 'Multi'                                   }) : '' },
+    { title => 'Synteny',            img => 'compara_syn',   url => $availability->{'chromosome'} && $availability->{'has_synteny'} ? $hub->url({ action => 'Synteny'                                 }) : '' },
+  ];
 
-  my @buttons = (
-    {'title' => 'Alignments (image)', 'img' => 'compara_image', 'url' => $image_url},
-    {'title' => 'Alignments (text)',  'img' => 'compara_text',   'url' => $text_url},
-    {'title' => 'Multi-species view', 'img' => 'compara_multi',  'url' => $multi_url},
-    {'title' => 'Synteny',            'img' => 'compara_syn',    'url' => $syn_url},
-  );
-
-  my $html = qq(
-    <div class="centered">
-  );
-  foreach my $button (@buttons) {
-    my $title = $button->{'title'};
-    my $img = $button->{'img'};
-    my $url = $button->{'url'};
-    if ($url) {
-      $img .= '.gif';
-      $html .= qq(<a href="$url" title="$title"><img src="/img/$img" class="portal" alt="" /></a>);
-    }
-    else {
-      $img   .= '_off.gif';
-      $title .= ' (NOT AVAILABLE)';
-      $html .= qq(<img src="/img/$img" class="portal" alt="" title="$title" />);
-    }
+  my $html  = $self->SUPER::content;
+     $html .= '<div class="centered">';
+  
+  if ($gene) {
+    $html .= qq{More views of comparative genomics data, such as orthologues and paralogues, are available on the <a href="$gene">Gene</a> page.};
+  } else {
+    $html .= 'Additional comparative genomics views are available for individual genes.';
   }
-
-  if ($hub->param('g')) {
-    my $url = $hub->url({'type'=>'Gene','action'=>'Compara'});
-    $html .= qq(<p>More views of comparative genomics data, such as orthologues and paralogues, are available on the <a href="$url">Gene</a> page.</p>);
-  }
-  else {
-    $html .= qq(<p>Additional comparative genomics views are available for individual genes.</p>);
-  }
- 
-  $html .= qq(
-    </div>
-  );
-
-  return $html;
+     
+  return "$html</div>";
 }
-
 
 1;
