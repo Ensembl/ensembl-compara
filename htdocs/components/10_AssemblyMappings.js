ec3adc8b000c34ea408f407f6b2428d3aa2fec44
Simon Brent <sb23@sanger.ac.uk>
2011-06-02 11:15:50 +0000

    $Rev

--- htdocs/components/10_AssemblyMappings.js
+++ htdocs/components/10_AssemblyMappings.js
@@ -1,20 +1,19 @@
+// $Revision$
+// Hides options in 'conversion' that don't match the current value in 'species'
+
 Ensembl.Panel.AssemblyMappings = Ensembl.Panel.extend({
-  // Hides options in 'conversion' that don't match the current value in 'species
   init: function () {
     var panel = this;
     this.base();
-
+    
     this.elLk.conversion = $('.conversion', this.el);
-    this.elLk.species    = $('.dropdown_remotecontrol', this.el).bind('change', function () {
-      panel.showBySpecies();  
-    });
+    this.elLk.species    = $('.dropdown_remotecontrol', this.el).bind('change', function () { panel.showBySpecies(); });
     
     this.showBySpecies();
   },
-
+  
   showBySpecies: function () {
     this.elLk.conversion.children().hide().filter(':selected').removeAttr('selected').end()
       .filter('.' + this.elLk.species.val()).show().first().attr('selected', 'selected');
   }
-
 });
