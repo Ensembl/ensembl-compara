66ad442735f0036b2e0d8775c0b79aaefb195fc2
Simon Brent <sb23@sanger.ac.uk>
2011-06-02 11:14:35 +0000

    jslint

--- htdocs/components/10_PopulationGraph.js
+++ htdocs/components/10_PopulationGraph.js
@@ -14,56 +14,49 @@ Ensembl.Panel.PopulationGraph = Ensembl.Panel.extend({
       });
     }
   },
-
+  
   getContent: function () {
+    var alleles   = [ 'A', 'T', 'G', 'C' ];
+    var acolours  = [ '#00FF00', '#FF0000', '#FFD700', '#0000FF' ];
+    var bcolours  = [ '#FF00FF', '#000000', '#008080', '#7B68EE' ]; // Other colours if the allele is not A, T, G or C
     var graphData = [];
-    var i, j, r, pie, pieData, pieLegend;
- 
+    var i, j, k, raphael, pieData, pieColors, piePercent, b, colourFlag;
+
+    
     $('input.population', this.el).each(function () {
       graphData.push(eval(this.value));
-    });
+    }); 
     
-		var alleles = ['A','T','G','C'];
-		var acolours = ['#00FF00','#FF0000','#FFD700','#0000FF'];
-		var bcolours = ['#FF00FF','#000000','#008080','#7B68EE']; // Other colours if the allele is not A, T, G or C
-		
-		// For each graph //
+    // For each graph
     for (i in graphData) {
-      r = Raphael('graphHolder' + i);
-
-      r.g.txtattr.font = "12px 'Luxi Sans','Helvetica', sans-serif";
-      
       pieData    = [];
-      pieLegend  = [];
-			pieColors  = [];
-			piePercent = [];
-      
-			var legend_flag = 0;
-			var data_flag   = 0;
-			var b_id = 0;
-			
-			// For each allele //
+      pieColors  = [];
+      piePercent = [];
+      raphael    = Raphael('graphHolder' + i);
+      raphael.g.txtattr.font = "12px 'Luxi Sans','Helvetica', sans-serif";
+            
+      // For each allele
       for (j in graphData[i]) {
-
         pieData.push(graphData[i][j][1]);
-				piePercent.push(graphData[i][j][0]+': %%');
-				
-				var cflag = 0;
-				// Normal allele colour
-				for (k in alleles) {
-					if (graphData[i][j][0] == alleles[k]) {
-						pieColors.push(acolours[k]);
-						cflag = 1;
-					}
-				}
-				// Other colour
-				if (cflag == 0 && b_id < bcolours.length) {
-					pieColors.push(bcolours[b_id]);
-					b_id ++;
-				}
+        piePercent.push(graphData[i][j][0] + ': %%');
+        colourFlag = 0;
+
+        // Normal allele colour
+        for (k in alleles) {
+          if (graphData[i][j][0] === alleles[k]) {
+            pieColors.push(acolours[k]);
+            colourFlag = 1;
+          }
+        }
+
+        // Other colour
+        if (colourFlag === 0 && b < bcolours.length) {
+          pieColors.push(bcolours[b]);
+          b++;
+        }
       }
-			// Create the pie graph
-			pie = r.g.piechart(25, 25, 20, pieData, {legend: piePercent, legendpos: "east", legendmark: 'arrow', colors: pieColors});
-		}
+      
+      raphael.g.piechart(25, 25, 20, pieData, { legend: piePercent, legendpos: "east", legendmark: 'arrow', colors: pieColors }); // Create the pie graph
+    }
   }
 });
