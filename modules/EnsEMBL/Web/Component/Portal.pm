750071e0eba2a77cded833bcbcf003392423ad01
Simon Brent <sb23@sanger.ac.uk>
2011-06-02 09:57:30 +0000

    generic portal component

---
+++ modules/EnsEMBL/Web/Component/Portal.pm
@@ -0,0 +1,33 @@
+# $Id$
+
+package EnsEMBL::Web::Component::Portal;
+
+use strict;
+
+use base qw(EnsEMBL::Web::Component);
+
+sub _init {
+  my $self = shift;
+  
+  $self->cacheable(1);
+  $self->ajaxable(0);
+  
+  $self->{'buttons'} = [];
+}
+
+sub content {
+  my $self = shift;
+  my $html;
+  
+  foreach (@{$self->{'buttons'}}) {
+    if ($_->{'url'}) {
+      $html .= qq{<a href="$_->{'url'}" title="$_->{'title'}"><img src="/img/$_->{'img'}.gif" class="portal" alt="" /></a>};
+    } else {
+      $html .= qq{<img src="/img/$_->{'img'}_off.gif" class="portal" alt="" title="$_->{'title'} (NOT AVAILABLE)" />};
+    }
+  }
+  
+  return qq{<div class="centered">$html</div>};
+}
+
+1;
