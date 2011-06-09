d06e6a8aa7a831832dfca5672349d7fa321abd89
Steve Trevanion <st3@sanger.ac.uk>
2011-06-09 17:32:34 +0000

    ZMenu for ENCODE supporting features

---
+++ modules/EnsEMBL/Web/ZMenu/Transcript_sf.pm
@@ -0,0 +1,40 @@
+package EnsEMBL::Web::ZMenu::Transcript_sf;
+
+use strict;
+
+use HTML::Entities qw(encode_entities);
+
+use base qw(EnsEMBL::Web::ZMenu);
+
+sub content {
+  my $self         = shift;
+  my $hub          = $self->hub;
+  my $object       = $self->object;
+  my $id           = $hub->param('id');
+  my $db           = $hub->param('fdb') || $hub->param('db') || 'core'; 
+  my $object_type  = $hub->param('ftype');
+  my $db_adaptor   = $hub->database(lc $db);
+  my $adaptor_name = "get_${object_type}Adaptor";
+  my $feat_adap    = $db_adaptor->$adaptor_name;
+  my $features     = $feat_adap->fetch_all_by_hit_name($id) || [];
+  my $click_start  = $hub->param('click_start');
+  my $click_end    = $hub->param('click_end');
+
+  $self->caption($id);
+
+  $self->add_entry({
+    type  => 'Location',
+    label => $object->seq_region_name . ':' . $object->seq_region_start . '-' . $object->seq_region_end,
+  });
+
+  $self->add_entry({
+    type  => 'Score',
+    label => $features->[0]->score,
+  });
+
+  $self->add_entry({
+    label_html => $features->[0]->analysis->description
+  });
+}
+
+1;
