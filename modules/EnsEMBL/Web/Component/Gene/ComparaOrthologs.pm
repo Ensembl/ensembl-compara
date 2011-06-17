88d9babe4893e5bf8779b74fbc4702afd9e7d07b
Simon Brent <sb23@sanger.ac.uk>
2011-06-17 12:33:50 +0000

    tidied and added row classes for filtering

--- modules/EnsEMBL/Web/Component/Gene/ComparaOrthologs.pm
+++ modules/EnsEMBL/Web/Component/Gene/ComparaOrthologs.pm
@@ -19,11 +19,11 @@ sub _init {
 sub _species_sets {}
 
 sub content {
-  my $self          = shift;
-  my $hub           = $self->hub;
-  my $object        = $self->object;
-  my $species_defs  = $hub->species_defs;
-  my $cdb           = shift || $hub->param('cdb') || 'compara';
+  my $self         = shift;
+  my $hub          = $self->hub;
+  my $object       = $self->object;
+  my $species_defs = $hub->species_defs;
+  my $cdb          = shift || $hub->param('cdb') || 'compara';
   
   my @orthologues = (
     $object->get_homology_matches('ENSEMBL_ORTHOLOGUES', undef, undef, $cdb), 
@@ -50,84 +50,59 @@ sub content {
 
   ##--------------------------- SUMMARY TABLE ----------------------------------------
 
-  my ($species_sets, $set_order) = $self->_species_sets(\%orthologue_list, \%skipped, \%orthologue_map);
+  my ($species_sets, $sets_by_species, $set_order) = $self->_species_sets(\%orthologue_list, \%skipped, \%orthologue_map);
 
   if ($species_sets) {
-
-    $html .= qq(<h3>Summary of orthologues of this gene</h3>
-                <p class="space-below">Click on 'Show' to display the orthologues for one or more groups, or click on 'Configure this page' to choose a custom list of species</p>);
+    $html .= qq{
+      <h3>Summary of orthologues of this gene</h3>
+      <p class="space-below">Click on 'Show' to display the orthologues for one or more groups, or click on 'Configure this page' to choose a custom list of species</p>
+    };
  
     $columns = [
-      { key => 'set',       'title' => 'Species set', align => 'left',    width => '20%', sort => 'none' },
-      { key => 'show',      'title' => '',                                width => '10%' },
-      { key => '1:1',       'title' => '1:1',         align => 'center',  width => '20%', sort => 'none' },
-      { key => '1:many',    'title' => '1:many',      align => 'center',  width => '20%', sort => 'none' },
-      { key => 'many:many', 'title' => 'many:many',   align => 'center',  width => '20%', sort => 'none' },
-      { key => 'none',      'title' => 'No orthologues', align => 'center',  width => '20%', sort => 'none' },
+      { key => 'set',       title => 'Species set',    align => 'left',    width => '20%' },
+      { key => 'show',      title => 'Show details',   align => 'center',  width => '10%' },
+      { key => '1:1',       title => '1:1',            align => 'center',  width => '20%' },
+      { key => '1:many',    title => '1:many',         align => 'center',  width => '20%' },
+      { key => 'many:many', title => 'many:many',      align => 'center',  width => '20%' },
+      { key => 'none',      title => 'No orthologues', align => 'center',  width => '20%' },
     ];
 
     foreach my $set (@$set_order) {
-      #next unless ($set && ref($set) eq 'HASH');
       my $set_info = $species_sets->{$set};
-
-      my $url = $self->ajax_url . ";set=$set;update_panel=1";
-      my $show = qq{
-        <a href="$url" class="ajax_update toggle closed" rel="">
-          <span class="closed">Show</span><span class="open">Hide</span>
-          <input type="hidden" class="set" value="$set" />
-        </a>
-      };
-
-      my $url = $hub->url({'set' => $set});
+      
       push @rows, {
-        'set'         => '<strong>'.$set_info->{'title'}.'</strong><br />'.$set_info->{'desc'},
-        'show'        => $show,
-        '1:1'         => $set_info->{'1-to-1'} || '0',
-        '1:many'      => $set_info->{'1-to-many'} || '0',
-        'many:many'   => $set_info->{'Many-to-many'} || '0',
-        'none'        => $set_info->{'none'} || '0',
+        'set'       => "<strong>$set_info->{'title'}</strong><br />$set_info->{'desc'}",
+        'show'      => qq{<input type="checkbox" class="table_control" title="Check to show these species in table below" name="orthologues" value="$set" />},
+        '1:1'       => $set_info->{'1-to-1'}       || 0,
+        '1:many'    => $set_info->{'1-to-many'}    || 0,
+        'many:many' => $set_info->{'Many-to-many'} || 0,
+        'none'      => $set_info->{'none'}         || 0,
       };
     }
-
-    my $summary = $self->new_table($columns, \@rows);
-    $html .= $summary->render;
+    
+    $html .= $self->new_table($columns, \@rows)->render;
   }
 
   ##----------------------------- FULL TABLE -----------------------------------------
 
-  my %selected_species;
-  if ($species_sets) {
-    my @selected_sets = $self->hub->param('set') || ('all');
-    warn ">>> SETS @selected_sets";
-    foreach my $set (@selected_sets) {
-      foreach my $species (@{$species_sets->{$set}{'species'}}) {
-        $selected_species{$species}++;
-      }
-    }
-    $html .= qq(<h3>Selected orthologues</h3>);
-  }
-  else {
-    %selected_species = %orthologue_list; 
-  }
+  $html .= '<h3>Selected orthologues</h3>' if $species_sets;
  
-  my $column_name =  $self->html_format ? 'Compare' : 'Description';
+  my $column_name = $self->html_format ? 'Compare' : 'Description';
   
   my $columns = [
-    { key => 'Species',            align => 'left', width => '10%', sort => 'html'          },
-    { key => 'Type',               align => 'left', width => '5%',  sort => 'string'        },
-    { key => 'dN/dS',              align => 'left', width => '5%',  sort => 'numeric'       },
-    { key => 'Ensembl identifier &amp; gene name', align => 'left', width => '15%', sort => 'html'},
-    { key => $column_name,         align => 'left', width => '10%', sort => 'none'          },
-    { key => 'Location',           align => 'left', width => '20%', sort => 'position_html' },
-    { key => 'Target %id',         align => 'left', width => '5%',  sort => 'numeric'       },
-    { key => 'Query %id',          align => 'left', width => '5%',  sort => 'numeric'       },
+    { key => 'Species',    align => 'left', width => '10%', sort => 'html'                                                },
+    { key => 'Type',       align => 'left', width => '5%',  sort => 'string'                                              },
+    { key => 'dN/dS',      align => 'left', width => '5%',  sort => 'numeric'                                             },
+    { key => 'identifier', align => 'left', width => '15%', sort => 'html', title => 'Ensembl identifier &amp; gene name' },
+    { key => $column_name, align => 'left', width => '10%', sort => 'none'                                                },
+    { key => 'Location',   align => 'left', width => '20%', sort => 'position_html'                                       },
+    { key => 'Target %id', align => 'left', width => '5%',  sort => 'numeric'                                             },
+    { key => 'Query %id',  align => 'left', width => '5%',  sort => 'numeric'                                             },
   ];
   
   @rows = ();
   
-  foreach my $species (
-      sort { ($a =~ /^<.*?>(.+)/ ? $1 : $a) cmp ($b =~ /^<.*?>(.+)/ ? $1 : $b) } 
-      keys %selected_species) {
+  foreach my $species (sort { ($a =~ /^<.*?>(.+)/ ? $1 : $a) cmp ($b =~ /^<.*?>(.+)/ ? $1 : $b) } keys %orthologue_list) {
     next if $skipped{$species};
     
     foreach my $stable_id (sort keys %{$orthologue_list{$species}}) {
@@ -153,9 +128,7 @@ sub content {
       });
 
       # Check the target species are on the same portal - otherwise the multispecies link does not make sense
-      my $local_species = ($link_url =~ /^\//) ? 1 : 0;      
-
-      my $target_links =  ($local_species && ($cdb eq 'compara')) ? sprintf(
+      my $target_links = ($link_url =~ /^\// && $cdb eq 'compara') ? sprintf(
         '<ul class="compact"><li class="first"><a href="%s" class="notext">Multi-species view</a></li>',
         $hub->url({
           type   => 'Location',
@@ -165,20 +138,19 @@ sub content {
           r      => undef
         })
       ) : '';
-   
       
       if ($orthologue_desc ne 'DWGA') {
         ($target, $query) = ($orthologue->{'target_perc_id'}, $orthologue->{'query_perc_id'});
        
         my $align_url = $hub->url({
             action   => 'Compara_Ortholog',
-            function => "Alignment". ($cdb=~/pan/ ? '_pan_compara' : ''),
+            function => 'Alignment' . ($cdb =~ /pan/ ? '_pan_compara' : ''),
             g1       => $stable_id,
           });
 
-        $target_links .= sprintf('<li><a href="%s" class="notext">Alignment (protein)</a></li>', $align_url);
-        $align_url .= ';seq=cDNA';
-        $target_links .= sprintf('<li><a href="%s" class="notext">Alignment (cDNA)</a></li>', $align_url);
+        $target_links .= sprintf '<li><a href="%s" class="notext">Alignment (protein)</a></li>', $align_url;
+        $align_url    .= ';seq=cDNA';
+        $target_links .= sprintf '<li><a href="%s" class="notext">Alignment (cDNA)</a></li>', $align_url;
         
         $alignview = 1;
       }
@@ -187,7 +159,7 @@ sub content {
         '<li><a href="%s" class="notext">Gene Tree (image)</a></li>',
         $hub->url({
           type   => 'Gene',
-          action => "Compara_Tree". ($cdb=~/pan/ ? '/pan_compara' : ''),
+          action => 'Compara_Tree' . ($cdb =~ /pan/ ? '/pan_compara' : ''),
           g1     => $stable_id,
           anc    => $orthologue->{'ancestor_node_id'},
           r      => undef
@@ -195,8 +167,6 @@ sub content {
       );
       
       # (Column 5) External ref and description
-      my $object_stable_id_link = qq{<p class="space-below"><a href="$link_url">$stable_id</a></p>};
-
       my $description = encode_entities($orthologue->{'description'});
          $description = 'No description' if $description eq 'NULL';
          
@@ -205,13 +175,17 @@ sub content {
         $description   .= sprintf '[Source: %s; acc: %s]', $edb, $hub->get_ExtURL_link($acc, $edb, $acc) if $acc;
       }
       
-      my @external = qq{<span class="small">$description</span>};
-      unshift @external, $orthologue->{'display_id'} if $orthologue->{'display_id'};
+      my @external = (qq{<span class="small">$description</span>});
+      
+      if ($orthologue->{'display_id'}) {
+        if ($orthologue->{'display_id'} eq 'Novel Ensembl prediction' && $description eq 'No description') {
+          @external = ('<span class="small">-</span>');
+        } else {
+          unshift @external, $orthologue->{'display_id'};
+        }
+      }
 
-      # Bug fix:  In othologues list, all orthologues with no description used to appear to be described as "novel ensembl predictions":
-      @external = qq{<span class="small">-</span>} if (($description eq 'No description') && ($orthologue->{'display_id'} eq 'Novel Ensembl prediction'));  
-
-      my $id_info = $object_stable_id_link.join('<br />', @external);
+      my $id_info = qq{<p class="space-below"><a href="$link_url">$stable_id</a></p>} . join '<br />', @external;
 
       ## (Column 6) Location - split into elements to reduce horizonal space
       my $location_link = $hub->url({
@@ -223,39 +197,42 @@ sub content {
         __clear => 1
       });
 
-      my ($chr, $coords, $strand) = split(/:/, $orthologue->{'location'});
-      my ($start, $end)           = split(/-/, $coords);
-
-      my $location_text = qq{Chr: <a href="$location_link">$chr</a><br />
+      my ($region, $coords, $strand) = split /:/, $orthologue->{'location'};
+      my ($start, $end)              = split /-/, $coords;
+      
+      my $region_string  = grep($region, @{$species_defs->get_config($species, 'ENSEMBL_CHROMOSOMES')}) ? 'Chromosome ' : '';
+         $region_string .= $region;
+         
+      my $location_text = qq{
+        Region: <a href="$location_link">$region_string</a><br />
         Start: $start<br />
         End: $end<br />
         Strand: $strand<br />
       };
-      my $label    = join('<br />(', split /\s*\(/, $species_defs->species_label($species));
-      
-      @external = qq{<span class="small">-</span>} if (($description eq 'No description') && ($orthologue->{'display_id'} eq 'Novel Ensembl prediction'));
       
       push @rows, {
-        'Species'            => $label,
-        'Type'               => ucfirst $orthologue_desc,
-        'dN/dS'              => $orthologue_dnds_ratio,
-        'Ensembl identifier &amp; gene name' => $id_info,
-        'Location'           => $location_text,
-        $column_name         => $self->html_format ? qq{<span class="small">$target_links</span>} : "$description",
-        'Target %id'         => $target,
-        'Query %id'          => $query,
+        'Species'    => join('<br />(', split /\s*\(/, $species_defs->species_label($species)),
+        'Type'       => ucfirst $orthologue_desc,
+        'dN/dS'      => $orthologue_dnds_ratio,
+        'identifier' => $id_info,
+        'Location'   => $location_text,
+        $column_name => $self->html_format ? qq{<span class="small">$target_links</span>} : $description,
+        'Target %id' => $target,
+        'Query %id'  => $query,
+        'options'    => { class => join(' ', 'all', @{$sets_by_species->{$species}}) }
       };      
     }
   }
   
-  my $table = $self->new_table($columns, \@rows, { data_table => 1, sorting => [ 'Species asc', 'Type asc' ] });
+  my $table = $self->new_table($columns, \@rows, { data_table => 1, sorting => [ 'Species asc', 'Type asc' ], id => 'orthologues' });
   
   if ($alignview && keys %orthologue_list) {
     $html .= sprintf(
       '<p><a href="%s">View sequence alignments of these homologues</a>.</p>', 
-      $hub->url({ action => "Compara_Ortholog", function => "Alignment". ($cdb=~/pan/ ? '_pan_compara' : ''), })
+      $hub->url({ action => 'Compara_Ortholog', function => 'Alignment' . ($cdb =~ /pan/ ? '_pan_compara' : ''), })
     );
   }
+  
   $html .= $table->render;
   
   if (scalar keys %skipped) {
