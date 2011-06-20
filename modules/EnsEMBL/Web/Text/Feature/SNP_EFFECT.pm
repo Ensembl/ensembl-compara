8988381607b519f1f6ab66dfdeb79454f20ad65c
William McLaren <wm2@ebi.ac.uk>
2011-06-20 10:23:31 +0000

    New field in VEP output

--- modules/EnsEMBL/Web/Text/Feature/SNP_EFFECT.pm
+++ modules/EnsEMBL/Web/Text/Feature/SNP_EFFECT.pm
@@ -39,14 +39,15 @@ sub location          { return $_[0]->{'__raw__'}[1];  }
 sub allele            { return $_[0]->{'__raw__'}[2];  }
 sub gene              { return $_[0]->{'__raw__'}[3];  }
 sub transcript        { return $_[0]->{'__raw__'}[4];  }
-sub consequence       { return $_[0]->{'__raw__'}[5];  }
-sub cdna_position     { return $_[0]->{'__raw__'}[6];  }
-sub cds_position      { return $_[0]->{'__raw__'}[7];  }
-sub protein_position  { return $_[0]->{'__raw__'}[8];  }
-sub aa_change         { return $_[0]->{'__raw__'}[9];  }
-sub codons            { return $_[0]->{'__raw__'}[10]; }
-sub snp               { return $_[0]->{'__raw__'}[11]; }
-sub extra_col         { return $_[0]->{'__raw__'}[12]; }
+sub feature_type      { return $_[0]->{'__raw__'}[5];  }
+sub consequence       { return $_[0]->{'__raw__'}[6];  }
+sub cdna_position     { return $_[0]->{'__raw__'}[7];  }
+sub cds_position      { return $_[0]->{'__raw__'}[8];  }
+sub protein_position  { return $_[0]->{'__raw__'}[9];  }
+sub aa_change         { return $_[0]->{'__raw__'}[10];  }
+sub codons            { return $_[0]->{'__raw__'}[11]; }
+sub snp               { return $_[0]->{'__raw__'}[12]; }
+sub extra_col         { return $_[0]->{'__raw__'}[13]; }
 
 sub external_data   { return $_[0]->{'__extra__'}; }
 sub extra           { return $_[0]->{'__extra__'}; }
