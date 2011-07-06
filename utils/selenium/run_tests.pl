9bfbf7349dbe917c8876b35140ce61620ed83918
ridwan <ma7@sanger.ac.uk>
2011-07-06 10:26:36 +0000

    selenium work

---
+++ utils/selenium/run_tests.pl
@@ -0,0 +1,98 @@
+#!/usr/local/bin/perl
+
+use strict;
+use Getopt::Long;
+use Class::Inspector;
+use FindBin qw($Bin);
+use vars qw( $SERVERROOT );
+
+BEGIN {
+  $SERVERROOT = "$Bin/../..";
+  unshift @INC,"$SERVERROOT/public-plugins/ensembl/modules";  
+  unshift @INC, "$SERVERROOT/conf";
+  eval{ require SiteDefs };
+  if ($@){ die "Can't use SiteDefs.pm - $@\n"; }
+  map{ unshift @INC, $_ } @SiteDefs::ENSEMBL_LIB_DIRS;    
+}
+
+my $module='Generic';
+my $url = 'http://www.ensembl.org';
+my $host = '172.20.10.187';#'localhost'; 
+my $port = '4444';
+my $browser = '*firefox';
+my @test;
+my @skip;
+my $verbose;
+my $species;
+my $timeout;
+
+GetOptions(
+  'module=s'  => \$module,
+  'url=s'     => \$url,
+  'host=s'    => \$host,
+  'port=s'    => \$port,
+  'browser=s' => \$browser,
+  'test=s'    => \@test,
+  'skip=s'    => \@skip,
+  'verbose'   => \$verbose,
+  'species=s' => \$species,
+  'timeout=s' => \$timeout,
+);
+
+die "You must specify a test module, eg. --module Generic" unless $module;
+die "You must specify a url to test against, eg. --url http://www.ensembl.org" unless $url;
+
+# hack: collect errors so that we can check for selenium failures
+my @errors;
+$SIG{'__DIE__'} = sub { push(@errors, $_[0]) };
+
+# try to use the package
+my $package = "EnsEMBL::Selenium::Test::$module";
+eval("use $package");
+die "can't use $package\n$@" if $@;
+
+# look for test methods
+no strict 'refs';
+my @methods = sort grep { /^test_/ } @{Class::Inspector->methods($package, 'public')};
+use strict 'refs';
+die "Module has no test methods (test methods must be named 'test_*')" unless @methods;
+# create test object
+my $object = $package->new(
+  url => $url,
+  host => $host,
+  port => $port,
+  browser => $browser,
+  conf => {
+    timeout => $timeout, 
+    species => $species,
+  },
+  verbose => $verbose,  
+);
+
+my $methods_called = 0;
+
+#FIRST CHECK IF WEBSITE IS UP.....
+$object->check_website;
+
+# run tests
+foreach my $method (@methods) {
+  next if @test and !grep {$method =~ /^(test_)?$_$/i} @test;
+  next if @skip and grep {$method =~ /^(test_)?$_$/i} @skip;
+  
+  print "\n****************************************\n";
+  print " $method\n";
+  print "****************************************\n";
+  $object->$method;
+  $methods_called++;
+  
+#   my $location = $object->get_location();
+#   print "\n URL:: $location \n" if ($verbose or grep {/^Error.*selenium/} @errors or $object->testmore_output =~ /not ok/m);
+}
+
+# check for problems
+if ($methods_called) {    
+  print "\nAll tests passed OK\n" 
+    unless ($verbose or grep {/^Error.*selenium/} @errors or $object->testmore_output =~ /not ok/m);
+} else {
+  print "No test methods to run\n";
+}
