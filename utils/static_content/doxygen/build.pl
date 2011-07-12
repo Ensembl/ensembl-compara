f1dc201287432b2474622c7519b8a254e3fce331
Simon Brent <sb23@sanger.ac.uk>
2011-07-12 12:02:29 +0000

    fix, add --apis and --dir options

--- utils/static_content/doxygen/build.pl
+++ utils/static_content/doxygen/build.pl
@@ -1,24 +1,41 @@
+# USAGE: perl utils/static_content/doxygen/build.pl [--dir root directory of server] [--apis comma-separated list of apis]
+
 use strict;
 
-my $root  = shift;
-my $dir   = "$root/utils/static_content/doxygen";
-my $html  = "$root/htdocs/info/docs/Doxygen";
-my @apis  = scalar @ARGV ? @ARGV : qw(core hive compara analysis external functgenomics pipeline variation);
+use Cwd;
+use Getopt::Long;
+
+Getopt::Long::Configure('bundling');
+
+my $root = getcwd();
+my @apis;
+
+my $flag = GetOptions(
+  'dir=s',  \$root,
+  'apis=s', \@apis,
+);
+
+my $dir  = "$root/utils/static_content/doxygen";
+my $html = "$root/htdocs/info/docs/Doxygen";
+
+@apis = split /,/, join ',', @apis;
+@apis = qw(core hive compara analysis external functgenomics pipeline variation) unless scalar @apis; # core and hive must always be first in order to generate links
 
 unshift @INC, "$root/conf", $root;
 require SiteDefs;
 
 my %config_by_api = (
   core => {
-    PROJECT_NAME    => 'Ensembl',
-    INPUT           => "$root/ensembl/modules/",
-    EXCLUDE         => "$root/modules/t $root/ensembl/misc-scripts/",
-    STRIP_FROM_PATH => "$root/ensembl/modules/",
-    TAGFILES        => '',
+    PROJECT_NAME     => 'Ensembl',
+    INPUT            => "$root/ensembl/modules/",
+    EXCLUDE          => "$root/modules/t $root/ensembl/misc-scripts/",
+    STRIP_FROM_PATH  => "$root/ensembl/modules/",
+    TAGFILES         => '',
+    GENERATE_TAGFILE => 'ensembl.tag'
   },
   compara => {
     STRIP_FROM_PATH => "$root/ensembl-compara/modules/",
-    TAGFILES        => "ensembl.tag=../core-api/ \ \nhive.tag=../hive-api/",
+    TAGFILES        => "ensembl.tag=core-api/ \\ \n                         hive.tag=hive-api/",
   },
   external => {
     STRIP_FROM_PATH => "$root/ensembl-external/modules/Bio/",
@@ -39,14 +56,15 @@ my %config_template = (
   STRIP_FROM_PATH   => "$root/ensembl-%s/modules/Bio/EnsEMBL/",
   INPUT             => "$root/ensembl-%s/modules/",
   EXCLUDE           => '',
-  TAGFILES          => 'ensembl.tag=../core-api/',
+  TAGFILES          => 'ensembl.tag=core-api/',
   GENERATE_TAGFILE  => '%s.tag',
 );
 
 my $template = `cat $dir/docbuild_template`;
-my @docbuilds;
+my @times;
 
 foreach my $api (@apis) {
+  # Make the docbuild file from the template
   my %config = map { $_ => exists $config_by_api{$api}{$_} ? $config_by_api{$api}{$_} : sprintf($config_template{$_}, /PROJECT_(NAME|BRIEF)/ ? ucfirst $api : $api) } keys %config_template;
   my $output = $template;
   
@@ -58,13 +76,15 @@ foreach my $api (@apis) {
   print FH $output;
   close FH;
   
+  # Check out the code if it doesn't exist
+  my $api_dir = 'ensembl' . ($api eq 'core' ? '' : "-$api");
+  system("cd $root; cvs co $api_dir") unless -e "$root/$api_dir";
+  
+  # Run doxygen
   system("
     cd $dir
     export PERL5LIB=\${PERL5LIB}:\${PWD}/lib/site_perl/5.8.8/
-    export PATH=\${PWD}/bin/:\${PATH}
+    export PATH=\${PWD}/bin:\${PATH}
     doxygen ${api}_docbuild 2> ${api}_error.log
   ");
-  
-  #unlink "$dir/${api}_docbuild";
 }
-
