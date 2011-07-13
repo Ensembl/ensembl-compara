9d48bd8d62faeb640b37f74974670d04ae5fb95f
Simon Brent <sb23@sanger.ac.uk>
2011-07-13 09:49:55 +0000

    add species selection as an extra tab

--- modules/EnsEMBL/Web/ViewConfig/Location/MultiBottom.pm
+++ modules/EnsEMBL/Web/ViewConfig/Location/MultiBottom.pm
@@ -17,6 +17,21 @@ sub init {
   $self->title = 'Multi-species Image';
 }
 
+sub extra_tabs {
+  my $self = shift;
+  my $hub  = $self->hub;
+  
+  return [
+    'Select species',
+    $hub->url('Component', {
+      action   => 'Web',
+      function => 'MultiSpeciesSelector/ajax',
+      time     => time,
+      %{$hub->multi_params}
+    })
+  ];
+}
+
 sub form {
   my $self = shift;
   $self->add_form_element({ type => 'YesNo', name => 'show_bottom_panel', select => 'select', label => 'Show panel' });
