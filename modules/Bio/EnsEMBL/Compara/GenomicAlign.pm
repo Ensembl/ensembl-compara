cd2a1b6a5efe293eb975b025e8e0e158b1ab235b
Miguel Pignatelli <mp@ebi.ac.uk>
2011-07-22 09:19:30 +0000

    Deleted throw

--- modules/Bio/EnsEMBL/Compara/GenomicAlign.pm
+++ modules/Bio/EnsEMBL/Compara/GenomicAlign.pm
@@ -391,7 +391,7 @@ sub genomic_align_block {
     ## Add adaptor to genomic_align_block object if possible and needed
     if (!defined($genomic_align_block->{'adaptor'}) and !defined($genomic_align_block->{'_adaptor'}) and defined($self->{'adaptor'})) {
 $DB::single=1;
-throw();
+#throw();
       $genomic_align_block->adaptor($self->adaptor->db->get_GenomicAlignBlockAdaptor);
     }
 
