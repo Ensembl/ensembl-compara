e11a63452259ea9687301a13f4e93ac61283fbba
Simon Brent <sb23@sanger.ac.uk>
2011-08-25 10:48:15 +0000

    can use Component.pm's ajax_url. Yay, an empty file.

--- modules/EnsEMBL/Web/Component/LRG.pm
+++ modules/EnsEMBL/Web/Component/LRG.pm
@@ -4,22 +4,7 @@ package EnsEMBL::Web::Component::LRG;
 
 use strict;
 
-use Digest::MD5 qw(md5_hex);
-
 use base qw(EnsEMBL::Web::Component);
 
-sub ajax_url {
-  my ($self, $function_name, $no_query_string) = @_;
-
-  my $hub = $self->hub;
-  my ($ensembl, $plugin, $component, $type, $module) = split '::', ref $self;
-
-  my $url = join '/', $hub->species_defs->species_path, 'Component', $type, $plugin, $module;
-  $url   .= "/$function_name" if $function_name && $self->can("content_$function_name");
-  $url   .= '?_rmd=' . substr md5_hex($ENV{'REQUEST_URI'}), 0, 4;
-  $url   .= ";$ENV{'QUERY_STRING'}" unless $no_query_string;
-
-  return $url;
-}
 
 1;
