47943a3815361c841045be9dee2e1a88176a857d
Leo Gordon <lg4@ebi.ac.uk>
2011-08-26 12:48:03 +0000

    added new patches provided by WormBase

--- scripts/taxonomy/web_name_patches.sql
+++ scripts/taxonomy/web_name_patches.sql
@@ -1,16 +1,21 @@
 -- This file might need to be edited before importing NCBI taxonomy into ncbi_taxonomy@ens-livemirror:
 
-update ncbi_taxa_name set name = 'Canis familiaris'         where taxon_id = 9615  and name_class = 'scientific name'   and name = 'Canis lupus familiaris';
-update ncbi_taxa_name set name = 'Canis lupus familiaris'   where taxon_id = 9615  and name_class = 'synonym'           and name = 'Canis familiaris';
-update ncbi_taxa_name set name = 'Vicugna pacos'            where taxon_id = 30538 and name_class = 'scientific name'   and name = 'Lama pacos';
-update ncbi_taxa_name set name = 'Lama pacos'               where taxon_id = 30538 and name_class = 'synonym'           and name = 'Vicugna pacos';
-update ncbi_taxa_name set name = 'Vicugna'                  where taxon_id=9839                                         and name ='Lama';
+UPDATE ncbi_taxa_name SET name = 'Canis familiaris'         WHERE taxon_id = 9615   AND name_class = 'scientific name'  AND name = 'Canis lupus familiaris';
+UPDATE ncbi_taxa_name SET name = 'Canis lupus familiaris'   WHERE taxon_id = 9615   AND name_class = 'synonym'          AND name = 'Canis familiaris';
+UPDATE ncbi_taxa_name SET name = 'Vicugna pacos'            WHERE taxon_id = 30538  AND name_class = 'scientific name'  AND name = 'Lama pacos';
+UPDATE ncbi_taxa_name SET name = 'Lama pacos'               WHERE taxon_id = 30538  AND name_class = 'synonym'          AND name = 'Vicugna pacos';
+UPDATE ncbi_taxa_name SET name = 'Vicugna'                  WHERE taxon_id=9839                                         AND name ='Lama';
 
-update ncbi_taxa_name set name = 'Xenopus tropicalis'       where taxon_id = 8364  and name_class = 'scientific name'   and name = 'Xenopus (Silurana) tropicalis';
-update ncbi_taxa_name set name = 'Xenopus (Silurana) tropicalis' where taxon_id = 8364 and name_class = 'synonym'       and name = 'Xenopus tropicalis';
+UPDATE ncbi_taxa_name SET name = 'Xenopus tropicalis'       WHERE taxon_id = 8364   AND name_class = 'scientific name'  AND name = 'Xenopus (Silurana) tropicalis';
+UPDATE ncbi_taxa_name SET name = 'Xenopus (Silurana) tropicalis' WHERE taxon_id = 8364 AND name_class = 'synonym'       AND name = 'Xenopus tropicalis';
 
 -- change this internal node name as requested by Dr. Brandon Menzies:
 
-update ncbi_taxa_name set name = 'Marsupialia'              where taxon_id = 9263  and name_class = 'scientific name'   and name = 'Metatheria';
-update ncbi_taxa_name set name = 'Metatheria'               where taxon_id = 9263  and name_class = 'synonym'           and name = 'Marsupialia';
+UPDATE ncbi_taxa_name SET name = 'Marsupialia'              WHERE taxon_id = 9263   AND name_class = 'scientific name'  AND name = 'Metatheria';
+UPDATE ncbi_taxa_name SET name = 'Metatheria'               WHERE taxon_id = 9263   AND name_class = 'synonym'          AND name = 'Marsupialia';
+
+-- these species were renamed by WormBase:
+
+UPDATE ncbi_taxa_name SET name = 'Caenorhabditis angaria'   WHERE taxon_id = 96668  AND name_class = 'scientific name';
+UPDATE ncbi_taxa_name SET name = 'Caenorhabditis csp11'     WHERE taxon_id = 886184 AND name_class = 'scientific name';
 
