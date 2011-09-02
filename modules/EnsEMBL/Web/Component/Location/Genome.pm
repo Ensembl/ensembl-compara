59b490890b942e036eaaf6bfd736b2e3ffefc091
Anne Parker <ap5@sanger.ac.uk>
2011-09-02 12:39:36 +0000

    Major refactoring so that it (hopefully) makes more sense and is more easily extensible. Also removed phenotype code to new modules

--- modules/EnsEMBL/Web/Component/Location/Genome.pm
+++ modules/EnsEMBL/Web/Component/Location/Genome.pm
@@ -18,20 +18,6 @@ sub _init {
   $self->ajaxable(1);
 }
 
-our $feature_display_name = {
-  'Xref'                => 'External Reference',
-  'DnaAlignFeature'     => 'Sequence Feature',
-  'ProteinAlignFeature' => 'Protein Feature',
-};
-
-our $column_info = {
-  'names'   => {'title' => 'Ensembl ID'},
-  'loc'     => {'title' => 'Genomic location (strand)'},
-  'extname' => {'title' => 'External names'},
-  'length'  => {'title' => 'Length'},
-  'lrg'     => {'title' => 'Name'},
-};
-
 sub content {
   my $self         = shift;
   my $hub          = $self->hub;
@@ -41,9 +27,11 @@ sub content {
   my $features = {};
  
   ## Get features to draw
-  my $object = $self->builder->create_objects('Feature', 'lazy') if $hub->param('id');
-  if ($object) {
-    $features = $object->convert_to_drawing_parameters;
+  if ($hub->param('id')) {
+    my $object = $self->object || $self->builder->create_objects('Feature', 'lazy');
+    if ($object) {
+      $features = $object->convert_to_drawing_parameters;
+    }
   }
 
   my $chromosomes  = $species_defs->ENSEMBL_CHROMOSOMES || [];
@@ -86,66 +74,83 @@ sub content {
     my $image    = $self->new_karyotype_image;
     my $config   = $hub->get_imageconfig('Vkaryotype'); ## Form with hidden elements for click-through
 
+    ## Map some user-friendly display names
+    my $feature_display_name = {
+      'Xref'                => 'External Reference',
+      'ProbeFeature'        => 'Oligoprobe',
+      'DnaAlignFeature'     => 'Sequence Feature',
+      'ProteinAlignFeature' => 'Protein Feature',
+    };
+    my ($xref_type, $xref_name);
+    while (my ($type, $feature_set) = each (%$features)) {    
+      if ($type eq 'Xref') {
+        my $sample = $feature_set->[0][0];
+        $xref_type = $sample->{'label'};
+        $xref_name = $sample->{'extname'};
+        $xref_name =~ s/ \[#\]//;
+        $xref_name =~ s/^ //;
+      }
+    }
+
     ## Create pointers to be drawn
     my $pointers = [];
+    my ($legend_info, $has_gradient);
 
     if ($mapped_features) {
       ## Title for image - a bit messy, but we want it to be human-readable!
       my $title = 'Location';
       $title .= 's' if $mapped_features > 1;
       $title .= ' of ';
-      my $data_type = $hub->param('ftype');
-      my (%names, $data_name, @other_names, $name_string);
+      my ($data_type, $assoc_name);
+      my $ftype = $hub->param('ftype');
+      if (grep (/$ftype/, keys %$features)) {
+        $data_type = $ftype;
+      }
+      else {
+        my @A = sort keys %$features;
+        $data_type = $A[0];
+        $assoc_name = $hub->param('name');
+        unless ($assoc_name) {
+          $assoc_name = $xref_type.' ';
+          $assoc_name .= $hub->param('id');
+          $assoc_name .= " ($xref_name)";
+        }
+      }
+
+      my %names;
       ## De-camelcase names
-      foreach (keys %$features) {
+      foreach (sort keys %$features) {
         my $pretty = $feature_display_name->{$_} || $self->decamel($_);
-        if ($_ eq $data_type) {
-          $data_name = $pretty;
-        }
-        else {
-          push @other_names, $pretty;
-        }
-        $names{$_} = $pretty.'s';
+        $pretty .= 's' if $mapped_features > 1;
+        $names{$_} = $pretty;
       }
-      my $feature_names;
 
-      if (scalar @other_names < 1) {
-          $title .= $data_name;
-      } else {
-        my $last_name = pop(@other_names);
-        if (scalar @other_names > 0) {
-          $name_string = join ', ', @other_names;
-          $name_string .= ' and '.$last_name;
-        }
-        $title .= "$name_string$last_name associated with $data_name";
+      my @feat_names = sort values %names;
+      my $last_name = pop(@feat_names);
+      if (scalar @feat_names > 0) {
+        $title .= join ', ', @feat_names;
+        $title .= ' and ';
       }
+      $title .= $last_name;
+      $title .= " associated with $assoc_name" if $assoc_name;
+      $html .= "<h3>$title</h3>";        
      
-      ## Deal with pointer colours
-      my %used_colour;
-      my %pointer_default = (
-        DnaAlignFeature     => [ 'red', 'rharrow' ],
-        ProteinAlignFeature => [ 'red', 'rharrow' ],
-        RegulatoryFactor    => [ 'red', 'rharrow' ],
-        ProbeFeature        => [ 'red', 'rharrow' ],
-        Xref                => [ 'red', 'rharrow' ],
-        Gene                => [ 'blue','lharrow' ],
-        Domain              => [ 'blue','lharrow' ], 
-      );
-      
-      $html .= "<h2>$title</h2>";        
-
       ## Create pointers for Ensembl features
       while (my ($feat_type, $set) = each (%$features)) {          
-        my $defaults    = $pointer_default{$set->[2]};
+        my $defaults    = $self->pointer_default($feat_type);
+        my $colour      = $hub->param('colour') || $defaults->[1];
+        my $gradient    = $defaults->[2];
         my $pointer_ref = $image->add_pointers($hub, {
           config_name  => 'Vkaryotype',
           features     => $set->[0],
           feature_type => $feat_type,
-          color        => $hub->param('colour') || $defaults->[0],
-          style        => $hub->param('style')  || $defaults->[1],            
+          color        => $colour,
+          style        => $hub->param('style')  || $defaults->[0],            
+          gradient     => $gradient,
         });
-          
+        $legend_info->{$feat_type} = {'colour' => $colour, 'gradient' => $gradient};  
         push @$pointers, $pointer_ref;
+        $has_gradient++ if $gradient;
       }
 
       ## Create pointers for userdata
@@ -167,6 +172,54 @@ sub content {
       
     $html .= $image->render;
  
+    ## Add colour key if required
+    if ($self->html_format && (scalar(keys %$legend_info) > 1 || $has_gradient)) { 
+      $html .= '<h3>Key</h3>';
+
+      my $columns = [
+        {'key' => 'ftype',  'title' => 'Feature type'},
+        {'key' => 'colour', 'title' => 'Colour'},
+      ];
+      my $rows;
+
+      foreach my $type (sort keys %$legend_info) {
+        my $type_name = $feature_display_name->{$type} || $type;
+        my $colour    = $legend_info->{$type}{'colour'};
+        my @gradient  = @{$legend_info->{$type}{'gradient'}||[]};
+        my $swatch_style = 'width:30px;height:20px;border:2px solid #999;text-align:center;padding:4px 0 0 0;'; 
+        my $swatch;
+        if ($colour eq 'gradient' && @gradient) {
+          $gradient[0] = '20';
+          my @colour_scale = $hub->colourmap->build_linear_gradient(@gradient);
+          my $i = 1;
+          foreach my $step (@colour_scale) {                
+            my $label;
+            if ($i == 1) {
+              $label = sprintf("%.1f", $i);
+            } 
+            elsif ($i == scalar @colour_scale) {
+              $label = '>'.$i/2;
+            }
+            else {
+              $label = $i % 3 ? '' : sprintf("%.1f", ($i/3 + 2));
+            }
+            $swatch .= qq{<div style="background:#$step;color:#fff;float:left;$swatch_style">$label</div>};
+            $i++;
+          }
+          $swatch .= '<br /><div style="clear:both;text-align:center">Less significant p-values &lt;-------------------------&gt; More significant p-values</div>';
+        }
+        else { 
+          $swatch = qq{<span style="background-color:$colour;display:block;$swatch_style" title="$colour"></span>};
+        }
+        push @$rows, {
+              'ftype'  => {'value' => $type_name, 'style' => $self->cell_style},
+              'colour' => {'value' => $swatch,    'style' => $self->cell_style},
+        };
+      }
+      my $legend = $self->new_table($columns, $rows); 
+      $html .= $legend->render;
+    }
+      
     if ($unmapped_features > 0) {
       my $message;
       if ($mapped_features) {
@@ -185,34 +238,51 @@ sub content {
   }
 
   ## Create HTML tables for features, if any
+  my $default_column_info = {
+    'names'   => {'title' => 'Ensembl ID'},
+    'loc'     => {'title' => 'Genomic location (strand)', 'sort' => 'position_html'},
+    'extname' => {'title' => 'External names'},
+    'length'  => {'title' => 'Length', 'sort' => 'numeric'},
+    'lrg'     => {'title' => 'Name'},
+    'xref'    => {'title' => 'Name(s)'},
+  };
+
   while (my ($feat_type, $feature_set) = each (%$features)) {
     my $method = '_configure_'.$feat_type.'_table';
     if ($self->can($method)) {
-      my ($header, $column_order, $rows) = $self->$method($feat_type, $feature_set);
+      my $table_info = $self->$method($feat_type, $feature_set);
+      my $column_info = $table_info->{'custom_columns'} || $default_column_info;
       my $columns = [];
-      my $table_style = {};
-      my $cell_style = $self->cell_style;
       my $col;
+      my $cell_style = $self->cell_style;
+      my $table_style = $table_info->{'table_style'} || {};
 
-      foreach $col (@$column_order) {
+      foreach $col (@{$table_info->{'column_order'}||[]}) {
         push @$columns, {'key' => $col, 'title' => $column_info->{$col}{'title'}, 'style' => $cell_style};
       }
 
+      ## Add "extra" columns (unique to particular table types)
       my $extras = $feature_set->[1];
       foreach $col (@$extras) {
-        push @$columns, {'key' => 'extra_'.lc($col), 'title' => $col, 'style' => $cell_style}; 
+        push @$columns, {
+                    'key'   => $col->{'key'}, 
+                    'title' => $col->{'title'}, 
+                    'sort'  => $col->{'sort'}, 
+                    'style' => $cell_style,
+                    }; 
       }
-
       $table_style->{'margin'}      = '1em 0px';
-      my $table = $self->new_table($columns, $rows, $table_style);
-      $html .= '<h2 style="margin-top:1em">'.$header.'</h2>';
+      $table_style->{'data_table'}  = 1;
+      my $table = $self->new_table($columns, $table_info->{'rows'}, $table_style);
+      $html .= '<h3 style="margin-top:1em">'.$table_info->{'header'}.'</h3>';
       $html .= $table->render;
     }
   }
 
   ## User table
   if (keys %$user_features) {
-    my ($header, $column_order, $rows) = $self->configure_UserData_table('UserData', $user_features);
+    my ($header, $column_order, $rows, $custom_columns) = $self->configure_UserData_table('UserData', $user_features);
+    my $column_info = $custom_columns || $default_column_info;
     my $columns = [];
     my $table_style = {};
     my $cell_style = $self->cell_style;
@@ -224,7 +294,7 @@ sub content {
 
     $table_style->{'margin'}      = '1em 0px';
     my $table = $self->new_table($columns, $rows, $table_style);
-    $html .= '<h2 style="margin-top:1em">'.$header.'</h2>';
+    $html .= '<h3 style="margin-top:1em">'.$header.'</h3>';
     $html .= $table->render;
   }
 
@@ -246,38 +316,31 @@ sub _configure_Gene_table {
     my $domain_id = $self->hub->param('id');
     my $count     = scalar @{$feature_set->[0]};
     my $plural    = $count > 1 ? 'genes' : 'gene';
-    $header       = "Domain $domain_id maps to $count $plural. The gene information is shown below:";
+    $header       = "Domain $domain_id maps to $count $plural:";
   }
 
   my $column_order = [qw(names loc extname)];
 
   my ($data, $extras) = @$feature_set;
   foreach my $feature ($self->_sort_features_by_coords($data)) {
-    my ($loc_link);
-
     my $row = {
               'extname' => {'value' => $feature->{'extname'}, 'style' => $self->cell_style},
               'names'   => {'value' => $self->_names_link($feature, $feature_type), 'style' => $self->cell_style},
               'loc'     => {'value' => $self->_location_link($feature), 'style' => $self->cell_style},
               };
-
-    my $i = 0;
-    foreach my $col (@$extras) {
-      $row->{'extra_'.lc($col)} = {'value' => $feature->{'extra'}[$i], 'style' => $self->cell_style};
-      $i++;
-    }
+    $self->add_extras($row, $feature, $extras);
     push @$rows, $row;
   }
 
-  return ($header, $column_order, $rows); 
+  return {'header' => $header, 'column_order' => $column_order, 'rows' => $rows}; 
 }
 
 sub _configure_Transcript_table {
   my ($self, $feature_type, $feature_set) = @_;
-  my ($header, $column_order, $rows) = $self->_configure_Gene_table($feature_type, $feature_set);
+  my $info = $self->_configure_Gene_table($feature_type, $feature_set);
   ## Override default header
-  $header = 'Transcript Information';
-  return ($header, $column_order, $rows);
+  $info->{'header'} = 'Transcript Information';
+  return $info; 
 }
 
 sub _configure_ProbeFeature_table {
@@ -286,63 +349,78 @@ sub _configure_ProbeFeature_table {
   
   my $column_order = [qw(loc length names)];
 
-  my $header = 'OligoProbe Information';
+  my $header = 'Oligoprobe Information';
  
   my ($data, $extras) = @$feature_set;
   foreach my $feature ($self->_sort_features_by_coords($data)) {
-    my ($loc_link);
-
     my $row = {
               'loc'     => {'value' => $self->_location_link($feature), 'style' => $self->cell_style},
               'length'  => {'value' => $feature->{'length'},            'style' => $self->cell_style}, 
               'names'   => {'value' => $feature->{'label'},             'style' => $self->cell_style},
               };
-
-    my $i = 0;
-    foreach my $col (@$extras) {
-      $row->{'extra_'.lc($col)} = {'value' => $feature->{'extra'}[$i], 'style' => $self->cell_style};
-      $i++;
-    }
+    $self->add_extras($row, $feature, $extras);
     push @$rows, $row;
   }
 
-  return ($header, $column_order, $rows); 
+  return {'header' => $header, 'column_order' => $column_order, 'rows' => $rows}; 
 }
 
 sub _configure_RegulatoryFeature_table {
   my ($self, $feature_type, $feature_set) = @_;
-  my ($header, $column_order, $rows) = $self->_configure_ProbeFeature_table($feature_type, $feature_set);
+  my $info = $self->_configure_ProbeFeature_table($feature_type, $feature_set);
   ## Override default header
   my $rf_id     = $self->hub->param('id');
   my $ids       = join(', ', $rf_id);
   my $count     = scalar @{$feature_set->[0]};
   my $plural    = $count > 1 ? 'Factors' : 'Factor';
-  $header = "Regulatory Features associated with Regulatory $plural $ids";
-  return ($header, $column_order, $rows);
+  $info->{'header'} = "Regulatory Features associated with Regulatory $plural $ids";
+  return $info;
 }
 
 sub _configure_Xref_table {
   my ($self, $feature_type, $feature_set) = @_;
-  my ($header, $column_order, $rows) = $self->_configure_ProbeFeature_table($feature_type, $feature_set);
-  ## Override default header
-  $header = 'External References';
-  return ($header, $column_order, $rows);
+  my $rows = [];
+  
+  my $column_order = [qw(loc length xref)];
+
+  my $header = 'External References';
+ 
+  my ($data, $extras) = @$feature_set;
+  foreach my $feature ($self->_sort_features_by_coords($data)) {
+    my $row = {
+              'loc'     => {'value' => $self->_location_link($feature), 'style' => $self->cell_style},
+              'length'  => {'value' => $feature->{'length'},            'style' => $self->cell_style}, 
+              'xref'    => {'value' => $feature->{'label'},             'style' => $self->cell_style},
+              };
+    $self->add_extras($row, $feature, $extras);
+    push @$rows, $row;
+  }
+
+  return {'header' => $header, 'column_order' => $column_order, 'rows' => $rows}; 
 }
 
 sub _configure_DnaAlignFeature_table {
   my ($self, $feature_type, $feature_set) = @_;
-  my ($header, $column_order, $rows) = $self->_configure_ProbeFeature_table($feature_type, $feature_set);
+  my $info = $self->_configure_ProbeFeature_table($feature_type, $feature_set);
   ## Override default header
-  $header = 'Sequence Feature Information';
-  return ($header, $column_order, $rows);
+  $info->{'header'} = 'Sequence Feature Information';
+  return $info; 
 }
 
 sub _configure_ProteinAlignFeature_table {
   my ($self, $feature_type, $feature_set) = @_;
-  my ($header, $column_order, $rows) = $self->_configure_ProbeFeature_table($feature_type, $feature_set);
+  my $info = $self->_configure_ProbeFeature_table($feature_type, $feature_set);
   ## Override default header
-  $header = 'Protein Feature Information';
-  return ($header, $column_order, $rows);
+  $info->{'header'} = 'Protein Feature Information';
+  return $info; 
+}
+
+sub add_extras {
+  my ($self, $row, $feature, $extras) = @_;
+  foreach my $col (@$extras) {
+    my $key = $col->{'key'};
+    $row->{$key} = {'value' => $feature->{'extra'}{$key}, 'style' => $self->cell_style};
+  }
 }
 
 sub _sort_features_by_coords {
