9b0c0b638125cc3f63d08021f22af5561f007515
Simon Brent <sb23@sanger.ac.uk>
2011-09-13 15:50:03 +0000

    added structrual_variation_table and toggleable_table functions

--- modules/EnsEMBL/Web/Component.pm
+++ modules/EnsEMBL/Web/Component.pm
@@ -852,6 +852,109 @@ sub transcript_table {
   return qq{<div class="summary_panel">$html</div>};
 }
 
+sub structrual_variation_table {
+  my ($self, $slice, $title, $table_id, $function, $open) = @_;
+  my $hub = $self->hub;
+  my $rows;
+  
+  my $columns = [
+     { key => 'id',          sort => 'string',        title => 'Name'   },
+     { key => 'location',    sort => 'position_html', title => 'Chr:bp' },
+     { key => 'size',        sort => 'string',        title => 'Genomic size (bp)' },
+     { key => 'class',       sort => 'string',        title => 'Class'  },
+     { key => 'source',      sort => 'string',        title => 'Source Study' },
+     { key => 'description', sort => 'string',        title => 'Study description', width => '50%' },
+  ];
+  
+  foreach my $svf (@{$slice->$function}) {
+    my $name        = $svf->variation_name;
+    my $sv          = $svf->structural_variation;
+    my $description = $sv->source_description;
+    my $sv_class    = $sv->var_class;
+    my $source      = $sv->source;
+    
+    if ($sv->study) {
+      my $ext_ref    = $sv->study->external_reference;
+      my $study_name = $sv->study->name;
+      my $study_url  = $sv->study->url;
+      
+      if ($study_name) {
+        $source      .= ":$study_name";
+        $source       = qq{<a rel="external" href="$study_url">$source</a>} if $study_url;
+        $description .= ': ' . $sv->study->description;
+      }
+      
+      if ($ext_ref =~ /pubmed\/(.+)/) {
+        my $pubmed_id   = $1;
+        my $pubmed_link = $hub->get_ExtURL('PUBMED', $pubmed_id);
+           $description =~ s/$pubmed_id/'<a href="'.$pubmed_link.'" target="_blank">'.$&.'<\/a>'/eg;
+      }
+    }
+    
+    # SV size (format the size with comma separations, e.g: 10000 to 10,000)
+    my $sv_size    = $svf->end - $svf->start + 1;
+    my $int_length = length $sv_size;
+    
+    if ($int_length > 3) {
+      my $nb         = 0;
+      my $int_string = '';
+      
+      while (length $sv_size > 3) {
+        $sv_size    =~ /(\d{3})$/;
+        $int_string = ",$int_string" if $int_string ne '';
+        $int_string = "$1$int_string";
+        $sv_size    = substr $sv_size, 0, (length($sv_size) - 3);
+      }
+      
+      $sv_size = "$sv_size,$int_string";
+    }  
+      
+    my $sv_link = $hub->url({
+      type   => 'StructuralVariation',
+      action => 'Summary',
+      sv     => $name
+    });      
+
+    my $loc_string = $svf->seq_region_name . ':' . $svf->seq_region_start . '-' . $svf->seq_region_end;
+        
+    my $loc_link = $hub->url({
+      type   => 'Location',
+      action => 'View',
+      r      => $loc_string,
+    });
+      
+    my %row = (
+      id          => qq{<a href="$sv_link">$name</a>},
+      location    => qq{<a href="$loc_link">$loc_string</a>},
+      size        => $sv_size,
+      class       => $sv_class,
+      source      => $source,
+      description => $description,
+    );
+    
+    push @$rows, \%row;
+  }
+  
+  return $self->toggleable_table($title, $table_id, $self->new_table($columns, $rows, { data_table => 1, sorting => [ 'location asc' ] }), $open);
+}
+
+sub toggleable_table {
+  my ($self, $title, $id, $table, $open) = @_;
+  my @state = $open ? qw(show open) : qw(hide closed);
+  
+  $table->add_option('data_table', "toggle_table $state[0]");
+  $table->add_option('id', "${id}_table");
+  
+  return sprintf('
+    <div class="toggleable_table">
+      <h2>%s</h2><span class="toggle_button" id="%s"><em class="%s"></em></span>
+      <p class="invisible">.</p>
+      %s
+    </div>',
+    $title, $id, $state[1], $table->render
+  ); 
+}
+
 # Simple subroutine to dump a formatted "warn" block to the error logs - useful when debugging complex
 # data structures etc... 
 # output looks like:
