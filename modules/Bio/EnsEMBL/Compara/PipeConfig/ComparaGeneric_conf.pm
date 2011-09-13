7f9a10d02347006e2aa67ac35ac45d162e830a4e
Leo Gordon <lg4@ebi.ac.uk>
2011-09-13 14:47:37 +0000

    let the user decide whether (s)he wants InnoDB or MyISAM schema

--- modules/Bio/EnsEMBL/Compara/PipeConfig/ComparaGeneric_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/ComparaGeneric_conf.pm
@@ -13,7 +13,8 @@ sub default_options {
     return {
         %{$self->SUPER::default_options},
 
-        'pipeline_name' => 'compara_generic',
+        'pipeline_name'         => 'compara_generic',
+        'compara_innodb_schema' => 1,
     };
 }
 
@@ -23,12 +24,13 @@ sub pipeline_create_commands {
     return [
         @{$self->SUPER::pipeline_create_commands},    # inheriting database and hive table creation
 
-            # Compara 'release' tables have to be turned from MyISAM into InnoDB:
-        "sed 's/ENGINE=MyISAM/ENGINE=InnoDB/g' ".$self->o('ensembl_cvs_root_dir').'/ensembl-compara/sql/table.sql'.' | mysql '.$self->dbconn_2_mysql('pipeline_db', 1),
+            # Compara 'release' tables will be turned from MyISAM into InnoDB on the fly by default:
+        ($self->o('compara_innodb_schema') ? "sed 's/ENGINE=MyISAM/ENGINE=InnoDB/g' " : 'cat ')
+            . $self->o('ensembl_cvs_root_dir').'/ensembl-compara/sql/table.sql | mysql '.$self->dbconn_2_mysql('pipeline_db', 1),
 
-            # Compara 'pipeline' tables are already InnoDB:
-        'mysql '.$self->dbconn_2_mysql('pipeline_db', 1).' <'.$self->o('ensembl_cvs_root_dir').'/ensembl-compara/sql/pipeline-tables.sql',
-                    
+            # Compara 'pipeline' tables are already InnoDB, but can be turned to MyISAM if needed:
+        ($self->o('compara_innodb_schema') ? 'cat ' : "sed 's/ENGINE=InnoDB/ENGINE=MyISAM/g' ")
+            . $self->o('ensembl_cvs_root_dir').'/ensembl-compara/sql/pipeline-tables.sql | mysql '.$self->dbconn_2_mysql('pipeline_db', 1),
     ];
 }
 
