87338f06a0757e1a6f8a9cd9f8b6c92105329368
Steve Trevanion <st3@sanger.ac.uk>
2011-10-07 14:52:32 +0000

    tweaks from vega45 run

--- modules/Bio/EnsEMBL/Compara/PipeConfig/Example/VegaProteinTrees_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/Example/VegaProteinTrees_conf.pm
@@ -37,53 +37,58 @@ use base ('Bio::EnsEMBL::Compara::PipeConfig::ProteinTrees_conf');
 
 
 sub default_options {
-    my ($self) = @_;
-    return {
-        %{$self->SUPER::default_options},   # inherit the generic ones
+  my ($self) = @_;
+  return {
+    %{$self->SUPER::default_options},   # inherit the generic ones
 
     # parameters that are likely to change from execution to another:
-#       'mlss_id'               => 15,   # it is very important to check that this value is current (commented out to make it obligatory to specify)
-#       'release'               => '63',
-
-        'rel_suffix'            => 'vega',
-        'work_dir'              => '/lustre/scratch103/ensembl/'.$ENV{'USER'}.'/protein_trees_'.$self->o('rel_with_suffix'),
-        'outgroups'             => [ ],   # affects 'hcluster_dump_input_per_genome'
-        'taxlevels'             => [ 'Theria' ],
-        'filter_high_coverage'  => 1,   # affects 'group_genomes_under_taxa'
+#    'mlss_id'               => 15,   # it is very important to check that this value is current (commented out to make it obligatory to specify)
+#    'release'               => '64',
+    'rel_suffix'            => 'vega',
+    'work_dir'              => '/lustre/scratch103/ensembl/'.$ENV{'USER'}.'/compara_generation/'.$ENV{'USER'}.'_vega_genetree_20110711_'.$self->o('rel_with_suffix'),
+    'outgroups'             => [ ],   # affects 'hcluster_dump_input_per_genome'
+    'taxlevels'             => [ 'Theria' ],
+    'filter_high_coverage'  => 1,   # affects 'group_genomes_under_taxa'
 
     # connection parameters to various databases:
 
-        'pipeline_db' => {                      # the production database itself (will be created)
-            -host   => 'vegabuild',
-            -port   => 5304,
-            -user   => 'ottadmin',
-            -pass   => $self->o('password'),
-            -dbname => $self->o('ENV', 'USER').'_compara_homology_'.$self->o('rel_with_suffix'),
-        },
+    # the production database itself (will be created)
+    'pipeline_db' => { 
+      -host   => 'vegabuild',
+      -port   => 5304,
+      -user   => 'ottadmin',
+      -pass   => $self->o('password'),
+      -dbname => $self->o('ENV', 'USER').'_compara_homology_'.$self->o('rel_with_suffix'),
+    },
 
-        'master_db' => {                        # the master database for synchronization of various ids
-            -host   => 'vegabuild',
-            -port   => 5304,
-            -user   => 'ottadmin',
-            -pass   => $self->o('password'),
-            -dbname => 'vega_compara_master',
-        },
+    # the master database for synchronization of various ids
+    'master_db' => {
+      -host   => 'vegabuild',
+      -port   => 5304,
+      -user   => 'ottadmin',
+      -pass   => $self->o('password'),
+      -dbname => 'vega_compara_master',
+#      -dbname => 'vega_compara_master_64',
+    },
 
     # switch off the reuse:
-        'reuse_core_sources_locs'   => [ ],
-        'prev_release'              => 0,   # 0 is the default and it means "take current release number and subtract 1"
-        'reuse_db'                  => 0,
+    'reuse_core_sources_locs'   => [ ],
+    'prev_release'              => 0,   # 0 is the default and it means "take current release number and subtract 1"
+    'reuse_db'                  => 0,
 
     # hive_capacity values for some analyses:
-        'store_sequences_capacity'  => 200,
-        'blastp_capacity'           => 450,
-        'mcoffee_capacity'          => 100,
-        'njtree_phyml_capacity'     => 100,
-        'ortho_tree_capacity'       => 100,
-        'build_hmm_capacity'        => 200,
-        'other_paralogs_capacity'   =>  50,
-        'homology_dNdS_capacity'    => 200,
-    };
+    'store_sequences_capacity'  => 200,
+    'blastp_capacity'           => 450,
+    'mcoffee_capacity'          => 100,
+    'njtree_phyml_capacity'     => 100,
+    'ortho_tree_capacity'       => 100,
+    'build_hmm_capacity'        => 200,
+    'other_paralogs_capacity'   =>  50,
+    'homology_dNdS_capacity'    => 200,
+
+    #if the 65 hive fails at wublast p and you can't work out what's wrong with the path then uncomment this
+    #'wublastp_exe'              => '/usr/local/ensembl/bin/wublastp',
+  };
 }
 
 1;
