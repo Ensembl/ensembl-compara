23fc86e5153b22c04bf1853079ab582612825403
Simon Brent <sb23@sanger.ac.uk>
2011-10-27 15:30:12 +0000

    tidied and removed the ability to share with groups, since it currently doesn't work

--- modules/EnsEMBL/Web/Component/UserData/SelectShare.pm
+++ modules/EnsEMBL/Web/Component/UserData/SelectShare.pm
@@ -12,104 +12,64 @@ sub _init {
   $self->ajaxable(0);
 }
 
-sub caption {
-  return 'Share Your Data';
-}
-
 sub content {
   my $self     = shift;
   my $hub      = $self->hub;
-  my $session  = $hub->session;
   my $sitename = $hub->species_defs->ENSEMBL_SITETYPE;
-  my $form     = $self->modal_form('share', $hub->species_path($hub->data_species).'/UserData/CheckShare', {'wizard' => 1, 'back_button' => 0});
+  my $form     = $self->modal_form('share', $hub->url({ action => 'CheckShare', __clear => 1 }), { wizard => 1, back_button => 0 });
+  my $session  = $hub->session;
   my $user     = $hub->user;
-  my ($html, $has_groups, @groups);
+  #my @groups   = $user && !$hub->param('code') ? $user->find_administratable_groups : (); ## Can't share temp data (code) with group
+  my @groups; # GROUP SHARING ISN'T WORKING
+  my ($info_text, @values);
   
-  if ($user && !$hub->param('code')) { ## Can't share temp data with group
-    @groups = $user->find_administratable_groups;
-    $has_groups = $#groups > -1 ? 1 : 0;
-  }
-  my $info_text;
-  if ($has_groups) {
+  if (scalar @groups) {
     $form->add_element('type' => 'SubHeader', 'value' => 'Share with');
-    $info_text = qq(You can share your saved data with one of the groups you administer,
-                  or any data with anyone else even if they don't have an account with $sitename. 
-                  Just select 'Anyone, via URL' to get a shareable link to your data.
-    );
+    $info_text = "
+      You can share your saved data with one of the groups you administer,
+      or any data with anyone else even if they don't have an account with $sitename. 
+      Just select 'Anyone, via URL' to get a shareable link to your data.
+    ";
+  } else {
+    $info_text = "
+      You can share your uploaded data with anyone, even if they don't have an
+      account with $sitename. Just select one or more of your uploads and click on 'Next'
+      to get a shareable link to your data.
+    ";
   }
-  else {
-    $info_text = qq(You can share your uploaded data with anyone, even if they don't have an
-                  account with $sitename. Just select one or more of your uploads and click on 'Next'
-                  to get a shareable link to your data.
-    );
-  }
 
-  $info_text .= qq(Please note that these URLs expire after 72 hours, but if you save the upload
-                  to your account, you can create a new shareable URL at any time.);
+  $info_text .= 'Please note that these URLs expire after 72 hours, but if you save the upload to your account, you can create a new shareable URL at any time.';
 
-  $form->add_notes({heading => 'How it works', text => $info_text});
+  $form->add_notes({ heading => 'How it works', text => $info_text });
 
-  if ($has_groups) {
-    my @ids = ({'value' => 0, 'name' => 'Anyone, via URL'});
-    foreach my $group (@groups) {
-      push @ids, {'value'=>$group->id, 'name'=>$group->name};
-    }
-    $form->add_element('type'  => 'RadioGroup', 'name'  => 'webgroup_id', 'values' => \@ids);
-    $form->add_element('type' => 'Hidden', 'name' => 'type', 'value' => $hub->param('type'));
+  if (scalar @groups) {
+    my @ids = ({ value => 0, name => 'Anyone, via URL' });
+    push @ids, { value => $_->id, name => $_->name } for @groups;
+    
+    $form->add_element(type => 'RadioGroup', name => 'webgroup_id', values => \@ids);
+    $form->add_element(type => 'Hidden',     name => 'source',      value  => $hub->param('source'));
   }
 
   $form->set_attribute('class', 'narrow-labels');
-  $form->add_element('type' => 'SubHeader', 'value' => 'Data to share');
-
-  my @values = ();
-
-  my @session_uploads = $session->get_data(type => 'upload');
-  foreach my $upload (@session_uploads) {
-    push @values, {
-      name  => 'Temporary upload: ' . $upload->{name},
-      value => $upload->{code},
-    };
-  }
-
+  $form->add_element(type => 'SubHeader', value => 'Data to share');
+  
   if ($user) {
-    foreach my $record ($user->uploads) {
-      push @values, {
-        name  => 'Saved upload: '. $record->name,
-        value => $record->id,
-      };
-    }
+    push @values, { name => "Saved upload: $_->{'name'}", value => $_->id } for $user->uploads;
+    push @values, { name => "Saved URL: $_->{'name'}",    value => $_->id } for $user->urls;
   }
-  my @session_urls = $session->get_data(type => 'url');
-  foreach my $url (@session_urls) {
-    push @values, {
-      name  => 'Temporary URL: ' . $url->{name},
-      value => $url->{code},
-    };
-  }
-
-  if ($user) {
-    foreach my $record ($user->urls) {
-      push @values, {
-        name  => 'Saved URL: '. $record->name,
-        value => $record->id,
-      };
-    }
-  }
-
-  ## If only one record, have the checkbox automatically checked
-  my @autoselect = $hub->param('id');
-  push @autoselect, $hub->param('code'); 
-
+  
+  push @values, { name => "Temporary upload: $_->{'name'}", value => $_->{'code'} } for $session->get_data(type => 'upload');
+  push @values, { name => "Temporary URL: $_->{'name'}",    value => $_->{'code'} } for $session->get_data(type => 'url');
+  
   $form->add_element(
     type   => 'MultiSelect',
     name   => 'share_id',
     label  => 'Uploaded files',
-    value  => \@autoselect,
+    value  => $hub->param('id') || $hub->param('code'),
     values => \@values
   );
 
-  $html .= $form->render;
-  return $html;
+  return $form->render;
 }
 
 1;
