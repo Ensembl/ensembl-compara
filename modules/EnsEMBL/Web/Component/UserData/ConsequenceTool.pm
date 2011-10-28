73aa5b996e60861b804d838e50b29e0598ad6544
Simon Brent <sb23@sanger.ac.uk>
2011-10-28 13:31:13 +0000

    tidied

--- modules/EnsEMBL/Web/Component/UserData/ConsequenceTool.pm
+++ modules/EnsEMBL/Web/Component/UserData/ConsequenceTool.pm
@@ -1,46 +1,34 @@
+# $Id$
+
 package EnsEMBL::Web::Component::UserData::ConsequenceTool;
 
 use strict;
-use warnings;
-no warnings "uninitialized";
-use base qw(EnsEMBL::Web::Component::UserData);
 
-use URI::Escape qw(uri_unescape);
+use base qw(EnsEMBL::Web::Component::UserData);
 
 sub _init {
   my $self = shift;
-  $self->cacheable( 0 );
-  $self->ajaxable(  1 );
-}
-
-sub caption {
-  return;
+  $self->cacheable(0);
+  $self->ajaxable(1);
 }
 
 sub content {
-  my $self = shift;
-  my $object = $self->object;
-  my $html = '<h2>Variant Effect Predictor  Results:</h2>';
-
-  my @files = ($object->param('code'));
-  my $size_limit =  $object->param('variation_limit');
-
+  my $self       = shift;
+  my $hub        = $self->hub;
+  my $object     = $self->object;
+  my $html       = '<h2>Variant Effect Predictor  Results:</h2>';
+  my $size_limit = $hub->param('variation_limit');
+  my ($file, $name, $gaps) = split ':', $hub->param('convert_file');
+  
   ## Tidy up user-supplied names
-  my $id = $object->param('convert_file');
-  my ($file, $name, $gaps) = split(':', $id);
-  $name =~ s/ /_/g;
-  if ($name !~ /\.txt$/i) {
-    $name .= '.txt';
-  }
-  my $newname = $name || 'converted_data.txt';
-  my $download_url = sprintf('/%s/download?file=%s;name=%s;prefix=user_upload;format=txt', $object->species, $file, $newname, $newname);
+  $name  =~ s/ /_/g;
+  $name .= '.txt' unless $name =~ /\.txt$/i;
+  
+  my $newname      = $name || 'converted_data.txt';
+  my $download_url = sprintf '/%s/download?file=%s;name=%s;prefix=user_upload;format=txt', $hub->species, $file, $newname, $newname;
 
-  $html .= qq(<p style="padding-top:1em"><a href="$download_url">Download text version</a></p>);
-  foreach my $code (@files) {
-    my $data = $object->consequence_data_from_file($code); 
-    my $table = $object->consequence_table($data);
-    $html .= $table->render;
-  }
+  $html .= qq{<p style="padding-top:1em"><a href="$download_url">Download text version</a></p>};
+  $html .= $object->consequence_table($object->consequence_data_from_file($_))->render for $hub->param('code');
 
   return $html;
 }
