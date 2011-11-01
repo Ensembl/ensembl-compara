f23a8690ef7029a152d49bb5e86a11e4be28bedb
Simon Brent <sb23@sanger.ac.uk>
2011-11-01 13:13:19 +0000

    tidied. added filetype parameter

--- modules/EnsEMBL/Web/Component/UserData/SelectFeatures.pm
+++ modules/EnsEMBL/Web/Component/UserData/SelectFeatures.pm
@@ -12,155 +12,104 @@ sub _init {
   $self->ajaxable(0);
 }
 
-sub caption {
-  return 'Select File to Upload';
-}
-
 sub content {
-  my $self            = shift;
-  my $hub             = $self->hub;
-  my $session         = $hub->session;
-  my $species_defs    = $hub->species_defs;
-  my $sitename        = $species_defs->ENSEMBL_SITETYPE;
-  my $species         = $hub->species;
-  my $current_species = $hub->data_species;
+  my $self              = shift;
+  my $hub               = $self->hub;
+  my $session           = $hub->session;
+  my $species_defs      = $hub->species_defs;
+  my $species           = $hub->data_species;
+  my @valid_species     = $species_defs->valid_species;
+  my %assembly_mappings = map { $_ => $species_defs->get_config($_, 'ASSEMBLY_MAPPINGS') } @valid_species;
+  my @mapping_species   = sort grep $assembly_mappings{$_}, @valid_species;
   my $html;
 
-  ## Get necessary data first, to ensure we can do assembly mapping!
-  my @species;
-  foreach my $sp ($species_defs->valid_species) {
-    if (scalar($species_defs->get_config($sp, 'ASSEMBLY_MAPPINGS')) > 0) {
-      push @species, $sp;
-    }
-  }
-  my %mappings;
-  foreach my $sp (@species) {
-    my $sp_map = $species_defs->get_config($sp, 'ASSEMBLY_MAPPINGS');
-    if ($sp_map) {
-      $mappings{$sp} = $sp_map;
-    }
-  }
-
-  if (keys %mappings) {
-
-    $html .= qq(
-      <input type="hidden" class="panel_type" value="AssemblyMappings" />
-    );
-
-    my $form = $self->modal_form('select', $hub->species_path($current_species) . "/UserData/CheckConvert");
-    $form->add_notes({'heading' => 'Tips',
-  'text' => qq(<p class="space-below">Map your data to the current assembly. 
-  The tool accepts a <a href="/info/website/upload/bed.html#required">list of simple coordinates</a>, 
-  or files in these formats: 
-  <a href="/info/website/upload/gff.html">GFF</a>,
-  <a href="/info/website/upload/gff.html">GTF</a>,
-  <a href="/info/website/upload/bed.html">BED</a>,
-  <a href="/info/website/upload/psl.html">PSL</a>
-</p>
-<p class="space-below">N.B. Export is currently in GFF only</p>
-<p>For large data sets, you may find it more efficient to use our <a href="ftp://ftp.ensembl.org/pub/misc-scripts/Assembly_mapper_1.0/">ready-made converter script</a>.</p>)
-  });
+  if (scalar @mapping_species) {
+    my $user      = $hub->user;
     my $subheader = 'Upload file';
-
-    ## Munge data needed for form elements
+    my $form      = $self->modal_form('select', $hub->url({ action => 'CheckConvert', __clear => 1 }));
     my (@forward, @backward, @species_values);
-    @species = sort {$species_defs->get_config($a, 'SPECIES_COMMON_NAME') 
-                      cmp $species_defs->get_config($b, 'SPECIES_COMMON_NAME')} 
-                @species;
-    foreach my $sp (sort @species) {
-      push @species_values, {'value' => $sp, 'name' => $species_defs->species_label($sp, 1)};
-      my $mappings = $mappings{$sp};
-      if ($mappings) {
-        foreach my $string (reverse sort @$mappings) {
-          my ($to, $from) = split('#', $string);
-          ## Which mapping set? Have to fetch all, for easy JS auto-changing with species
-          push @forward, {'name' => $from.' -> '.$to, 'value' => $from.':'.$to, 'class' => $sp};
-          push @backward, {'name' => $to.' -> '.$from, 'value' => $to.':'.$from, 'class' => $sp};
-        }
+    
+    $form->add_notes({
+      heading => 'Tips',
+      text    => qq{
+        <p class="space-below">
+          Map your data to the current assembly. 
+          The tool accepts a <a href="/info/website/upload/bed.html#required">list of simple coordinates</a>, 
+          or files in these formats: 
+          <a href="/info/website/upload/gff.html">GFF</a>,
+          <a href="/info/website/upload/gff.html">GTF</a>,
+          <a href="/info/website/upload/bed.html">BED</a>,
+          <a href="/info/website/upload/psl.html">PSL</a>
+        </p>
+        <p class="space-below">N.B. Export is currently in GFF only</p>
+        <p>For large data sets, you may find it more efficient to use our <a href="ftp://ftp.ensembl.org/pub/misc-scripts/Assembly_mapper_1.0/">ready-made converter script</a>.</p>
+      }
+    });
+    
+    ## Munge data needed for form elements
+    foreach (@mapping_species) {
+      push @species_values, { value => $_, name => $species_defs->species_label($_, 1) };
+      
+      foreach my $string (sort { $b cmp $a } @{$assembly_mappings{$_}}) {
+        my ($to, $from) = split '#', $string;
+        ## Which mapping set? Have to fetch all, for easy JS auto-changing with species
+        push @forward,  { name => "$from -> $to", value => "${from}:$to", class => $_ };
+        push @backward, { name => "$to -> $from", value => "${to}:$from", class => $_ };
       }
     }
 
     $form->add_element(
-      'type'    => 'DropDown',
-      'name'    => 'species',
-      'label'   => "Species",
-      'values'  => \@species_values,
-      'value'   => $current_species,
-      'select'  => 'select',
-      'class'   => 'dropdown_remotecontrol',
+      type   => 'DropDown',
+      name   => 'species',
+      label  => 'Species',
+      values => \@species_values,
+      value  => $species,
+      select => 'select',
+      class  => 'dropdown_remotecontrol',
     );
-
-    my @values = (@forward, @backward);
+    
     $form->add_element(
-      'type'    => 'DropDown',
-      'name'    => 'conversion',
-      'label'   => "Assembly/coordinates to convert",
-      'values'  => \@values,
-      'select'  => 'select',
-      'class'   => 'conversion',
+      type   => 'DropDown',
+      name   => 'conversion',
+      label  => 'Assembly/coordinates to convert',
+      values => [ @forward, @backward ],
+      select => 'select',
+      class  => 'conversion',
     );
 
     ## Check for uploaded data for this species
-    my $user = $hub->user;
-    if ($user) {
-      my (@data, @temp);
-      foreach my $upload ($user->uploads) {
-        next unless $upload->species eq $species;
-        push @data, $upload; 
-      } 
-      foreach my $upload ($session->get_data('type' => 'upload')) {
-        next unless $upload->{'species'} eq $species;
-        push @data, $upload;
-      } 
-      foreach my $url ($user->urls) {
-        next unless $url->species eq $species;
-        push @data, $url;
-      } 
-      foreach my $url ($session->get_data('type' => 'url')) {
-        next unless $url->{'species'} eq $species;
-        push @data, $url;
-      } 
-    
-      if (@data) {
-        $form->add_element('type' => 'SubHeader',
-          'value' => 'Select existing upload(s)',
+    my @data = grep { $_->{'species'} eq $species } map { $user ? $user->$_ : (), $session->get_data(type => substr $_, 0, -1) } qw(uploads urls);
+  
+    if (scalar @data) {
+      $form->add_element(type => 'SubHeader', value => 'Select existing upload(s)');
+      
+      $subheader = 'Or upload new file';
+      
+      foreach my $file (@data) {
+        my ($name, $id) = ref($file) =~ /Record/ ? ($file->name, join('-', 'user', $file->type, $file->id)) : ($file->{'name'}, "temp-$file->{'type'}-$file->{'code'}");
+        
+        $form->add_element(
+          type  => 'CheckBox',
+          name  => 'convert_file',
+          label => $name,
+          value => "${id}:$name",
         );
-        foreach my $file (@data) {
-          my ($name, $id, $species);
-          if (ref ($file) =~ /Record/) {
-            my $type = $file->type;
-            $name = $file->name;
-            $id   = 'user-'.$type.'-'.$file->id; 
-          }
-          else {
-            my $type = $file->{'type'};
-            $name = $file->{'name'};
-            $id   = 'temp-'.$type.'-'.$file->{'code'};
-          }
-          $form->add_element(
-            'type'    => 'CheckBox',
-            'name'    => 'convert_file',
-            'label'   => $name,
-            'value'   => $id.':'.$name,
-          );
-        }
-        $subheader = 'Or upload new file';
       }
     }
 
-    $form->add_element('type' => 'SubHeader', 'value' => $subheader);
-
-    $form->add_element( type => 'String', name => 'name', label => 'Name for this upload (optional)' );
-
-    $form->add_element( type => 'Text', name => 'text', label => 'Paste file' );
-    $form->add_element( type => 'File', name => 'file', label => 'Upload file' );
-    $form->add_element( type => 'URL',  name => 'url',  label => 'or provide file URL', size => 30 );
-
+    $form->add_element(type => 'SubHeader', value => $subheader);
+    $form->add_element(type => 'Hidden', name => 'filetype', value => 'Assembly Converter');
+    $form->add_element(type => 'String', name => 'name', label => 'Name for this upload (optional)');
+    $form->add_element(type => 'Text',   name => 'text', label => 'Paste file');
+    $form->add_element(type => 'File',   name => 'file', label => 'Upload file');
+    $form->add_element(type => 'URL',    name => 'url',  label => 'or provide file URL', size => 30);
+    
+    $html .= '<input type="hidden" class="panel_type" value="AssemblyMappings" />';
     $html .= $form->render;
-  }
-  else {
+  } else {
     $html .= $self->_info('No mappings', 'Sorry, no species currently have assembly mappings.');
   }
+  
   return $html;
 }
 
--- modules/EnsEMBL/Web/Component/UserData/UploadStableIDs.pm
+++ modules/EnsEMBL/Web/Component/UserData/UploadStableIDs.pm
@@ -1,68 +1,53 @@
+# $Id$
+
 package EnsEMBL::Web::Component::UserData::UploadStableIDs;
 
 use strict;
-use warnings;
-no warnings 'uninitialized';
 
 use base qw(EnsEMBL::Web::Component::UserData);
 
 sub _init {
   my $self = shift;
-  $self->cacheable( 0 );
-  $self->ajaxable(  0 );
-}
-
-sub caption {
-  my $self = shift;
-  return 'Select File to Upload';
+  $self->cacheable(0);
+  $self->ajaxable(0);
 }
 
 sub content {
-  my $self = shift;
-  my $object = $self->object;
-
-  my $sitename = $object->species_defs->ENSEMBL_SITETYPE;
-  my $current_species = $object->data_species;
-
-  ## Get assembly info
-  my $html;
-  my $id_limit = 30;
-
-  my $form = $self->modal_form('select', $object->species_path($current_species) . "/UserData/CheckConvert");
-  $form->add_notes({'heading'=>'IMPORTANT NOTE:', 'text' => qq(<p>Please note that we limit the number of ID's processed to $id_limit. If the uploaded file contains more entries than this only the first $id_limit will be mapped.</p>
-<p>If you would like to convert more IDs, please use our <a href="ftp://ftp.ensembl.org/pub/misc-scripts/ID_mapper_1.0/">api script</a>.</p>) });
-  my $subheader = 'Upload file';
-
-   ## Species now set automatically for the page you are on
-  my @species;
-  foreach my $sp ($object->species_defs->valid_species) {
-    push @species, {'value' => $sp, 'name' => $object->species_defs->species_label($sp, 1)};
-  }
-  @species = sort {$a->{'name'} cmp $b->{'name'}} @species;
+  my $self         = shift;
+  my $hub          = $self->hub;
+  my $species_defs = $hub->species_defs;
+  my $species      = $hub->data_species;
+  my $id_limit     = 30;
+  my $form         = $self->modal_form('select', $hub->url({ action => 'CheckConvert', __clear => 1 }));
+  
+  $form->add_notes({
+    heading => 'IMPORTANT NOTE:', 
+    text    => qq{
+      <p>Please note that we limit the number of ID's processed to $id_limit. If the uploaded file contains more entries than this only the first $id_limit will be mapped.</p>
+      <p>If you would like to convert more IDs, please use our <a href="ftp://ftp.ensembl.org/pub/misc-scripts/ID_mapper_1.0/">api script</a>.</p>
+    }
+  });
 
   $form->add_element(
-      'type'    => 'DropDown',
-      'name'    => 'species',
-      'label'   => "Species",
-      'values'  => \@species,
-      'value'   => $current_species,
-      'select'  => 'select',
+    type   => 'DropDown',
+    name   => 'species',
+    label  => 'Species',
+    values => [ sort { $a->{'name'} cmp $b->{'name'} } map { value => $_, name => $species_defs->species_label($_, 1) }, $species_defs->valid_species ],
+    value  => $species,
+    select => 'select',
   );
 
-  $form->add_element( type => 'Hidden', name => 'species', 'value' => $current_species);
-  $form->add_element( type => 'Hidden', name => 'id_mapper', 'value' => 1);
-  $form->add_element( type => 'Hidden', name => 'id_limit', 'value' => $id_limit);
-  $form->add_element('type' => 'SubHeader', 'value' => $subheader);
-
-  $form->add_element( type => 'String', name => 'name', label => 'Name for this upload (optional)' );
-
-  $form->add_element( type => 'Text', name => 'text', label => 'Paste file' );
-  $form->add_element( type => 'File', name => 'file', label => 'Upload file' );
-  $form->add_element( type => 'URL',  name => 'url',  label => 'or provide file URL', size => 30 );
+  $form->add_element(type => 'Hidden', name => 'species',   value => $species);
+  $form->add_element(type => 'Hidden', name => 'id_mapper', value => 1);
+  $form->add_element(type => 'Hidden', name => 'id_limit',  value => $id_limit);
+  $form->add_element(type => 'Hidden', name => 'filetype',  value => 'ID History Converter');
+  $form->add_element(type => 'SubHeader',                   value => 'Upload file');
+  $form->add_element(type => 'String', name => 'name', label => 'Name for this upload (optional)');
+  $form->add_element(type => 'Text',   name => 'text', label => 'Paste file');
+  $form->add_element(type => 'File',   name => 'file', label => 'Upload file');
+  $form->add_element(type => 'URL',    name => 'url',  label => 'or provide file URL', size => 30);
  
-
-  $html .= $form->render;
-  return $html;
+  return $form->render;
 }
 
 
