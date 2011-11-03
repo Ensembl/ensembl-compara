4e8519ac211ab1ccec9429adcb3a7446c84f783c
Matthieu Muffato <muffato@ebi.ac.uk>
2011-11-03 17:51:20 +0000

    rel.65 compatible statistics

--- sql/protein_tree-stats.sql
+++ sql/protein_tree-stats.sql
@@ -1,68 +1,109 @@
-/* SQL statements that provide a summary of the data in a compara GeneTree
-   database. The whole lot should run within 5 mins 
 
-  At the moment we have;
-  1. Count of total number of protein trees (2 methods)
-  2. Count of the total protein_members by species cf the number in trees 
-  3. Coverage of longest translation peptides that are in Trees
-  4. Breakdown of tree counts and sizes by species of the root node
+/*
+
+	Det of statistics for the protein tree pipeline
+	Requires a write access to the database to create
+	  temporary tables / keys
+
 */
 
-/* Number of Trees */
--- SELECT count(*) as protein_tree_count
--- FROM   protein_tree_node ptn
--- WHERE  ptn.node_id = ptn.root_id
--- AND    root_id != 0;
-# 1sec
+/*
+	Number of genes, peptides per species, proportion covered by the gene trees
+*/
 
-/* Alternative count of Trees */
-SELECT count(*) as protein_tree_count
-FROM   protein_tree_node ptn, protein_tree_tag ptt
-WHERE  ptn.node_id=ptt.node_id
-AND    ptt.tag='gene_count';
-# 2sec
+/* 5 min */
+CREATE TEMPORARY TABLE tmp_coverage
+	SELECT
+		member.taxon_id,
+		SUM(source_name='ENSEMBLGENE') AS nb_genes,
+		SUM(source_name='ENSEMBLPEP') AS nb_pep,
+		SUM(protein_tree_attr.taxon_id = member.taxon_id) AS nb_pep_spectree,
+		SUM(protein_tree_attr.taxon_id IS NOT NULL AND protein_tree_attr.taxon_id != member.taxon_id) AS nb_pep_anctree
+	FROM
+		member
+		LEFT JOIN protein_tree_member USING (member_id)
+		LEFT JOIN protein_tree_node ON (protein_tree_member.root_id = protein_tree_node.node_id) 
+		LEFT JOIN protein_tree_attr ON (protein_tree_node.node_id = protein_tree_attr.node_id)
+	GROUP BY
+		member.taxon_id;
+ALTER TABLE tmp_coverage ADD PRIMARY KEY (taxon_id);
+OPTIMIZE TABLE tmp_coverage;
 
-/* Number of Species and their Members */
-SELECT gdb.name as species_name, 
-       count(*) as pmember_cnt,
-       sum( if( tm.member_id, 1, 0 ) ) as in_tree,
-       round( sum( if( tm.member_id, 1, 0 ) ) * 100 
-         / count(*) ) as in_tree_pct 
-FROM   genome_db gdb, 
-       member m left join protein_tree_member tm on m.member_id=tm.member_id 
-WHERE  gdb.genome_db_id=m.genome_db_id 
-AND    m.source_name = 'ENSEMBLPEP'
-GROUP  BY gdb.name, m.source_name;
-# 20sec
+SELECT
+	genome_db.name,
+	nb_pep,
+	nb_genes AS nb_can_pep,
+	nb_pep_spectree,
+	nb_pep_anctree,
+	nb_genes-nb_pep_spectree-nb_pep_anctree AS nb_pep_orphan,
+	ROUND(100*nb_pep_spectree/nb_genes, 2) AS perc_pep_spectree,
+	ROUND(100*nb_pep_anctree/nb_genes, 2) AS perc_pep_anctree,
+	ROUND(100*(nb_genes-nb_pep_spectree-nb_pep_anctree)/nb_genes, 2) AS perc_pep_orphan
+FROM
+	genome_db
+	JOIN ncbi_taxa_node USING (taxon_id)
+	JOIN tmp_coverage USING (taxon_id)
+ORDER BY
+	left_index;
 
-/* Coverage of longest translation peptides that are in Trees */
-/* Uses subset_member which is not a release table */
 
-SELECT gdb.name as species_name, 
-       count(*) as member_cnt,
-       sum( if( tm.member_id, 1, 0 ) ) as in_tree,
-       sum( if( tm.member_id, 1, 0 ) ) * 100 
-         / count(*) as in_tree_pct 
-FROM   genome_db gdb,
-       member m, subset_member sm left join protein_tree_member tm on sm.member_id=tm.member_id 
-WHERE sm.member_id=m.member_id 
-AND   gdb.genome_db_id=m.genome_db_id 
-AND   m.source_name = 'ENSEMBLPEP' 
-GROUP  BY gdb.name, m.source_name 
-ORDER BY in_tree_pct DESC;
-# 20 sec
 
-/* Breakdown of tree counts and sizes by species of the root node */
-SELECT ptt.value as root_node_species, 
-       count(*) as protein_tree_count, 
-       round( avg( cast( ptt2.value as unsigned ) ) ) as avg_proteins,
-       min( cast( ptt2.value as unsigned ) ) as min_proteins,
-       max( cast( ptt2.value as unsigned ) ) as max_proteins
-FROM   protein_tree_tag ptt,
-       protein_tree_tag ptt2
-WHERE  ptt.node_id=ptt2.node_id
-AND    ptt.tag='taxon_name'
-AND    ptt2.tag='gene_count'
-GROUP  BY ptt.value
-ORDER  BY count(*) DESC;
-# 10sec
+/* 15 min */
+
+CREATE TEMPORARY TABLE tmp_root_properties
+	SELECT
+		protein_tree_attr.node_id,
+		protein_tree_attr.taxon_id,
+		protein_tree_attr.taxon_name,
+		COUNT(member_id) AS nb_prot,
+		COUNT(DISTINCT member.taxon_id) AS nb_spec
+	FROM
+		protein_tree_member
+		JOIN member USING (member_id)
+		JOIN protein_tree_attr ON protein_tree_attr.node_id=protein_tree_member.root_id
+	GROUP BY
+		protein_tree_attr.node_id;
+ALTER TABLE tmp_root_properties ADD KEY (taxon_id);
+OPTIMIZE TABLE tmp_root_properties;
+
+SELECT
+	taxon_id,
+	taxon_name,
+	COUNT(*) AS nb_trees,
+	SUM(nb_prot) AS tot_nb_prot,
+	ROUND(AVG(nb_prot),2) AS avg_nb_prot,
+	MAX(nb_prot) AS max_nb_prot,
+	ROUND(AVG(nb_spec),2) AS avg_nb_spec,
+	MAX(nb_spec) AS max_nb_spec,
+	ROUND(AVG(nb_prot/nb_spec),2) AS avg_nb_prot_per_spec
+FROM
+	ncbi_taxa_node
+	JOIN tmp_root_properties USING (taxon_id)
+GROUP BY
+	taxon_id
+ORDER BY
+	left_index;
+
+
+
+/*
+	Tree properties, grouped by taxon_id
+	rel64: 25s
+*/
+
+SELECT
+	taxon_id,
+	taxon_name,
+	COUNT(*) AS nb_nodes,
+	SUM(node_type="speciation") AS nb_spec_nodes,
+	SUM(node_type="duplication") AS nb_dup_nodes,
+	SUM(node_type="dubious") AS nb_dubious_nodes,
+	SUM(node_type="gene_split") AS nb_gene_splits,
+	ROUND(AVG(duplication_confidence_score),2) AS avg_dupscore,
+	ROUND(AVG(IF(node_type="duplication",duplication_confidence_score,NULL)),2) AS avg_dupscore_nondub
+FROM
+	protein_tree_attr
+	JOIN ncbi_taxa_node USING (taxon_id)
+GROUP BY taxon_id
+ORDER BY left_index ;
+
