d03e2a35592992c3d74cfaca3b35ee6a18aeb26c
Simon Brent <sb23@sanger.ac.uk>
2011-11-08 11:57:55 +0000

    add modal reload if reload param is set

--- modules/EnsEMBL/Web/Component/UserData/ShareURL.pm
+++ modules/EnsEMBL/Web/Component/UserData/ShareURL.pm
@@ -1,43 +1,35 @@
+# $Id$
+
 package EnsEMBL::Web::Component::UserData::ShareURL;
 
 use strict;
-use warnings;
-no warnings "uninitialized";
+
+use EnsEMBL::Web::Tools::Encryption qw(checksum);
 
 use base qw(EnsEMBL::Web::Component::UserData);
-use EnsEMBL::Web::Tools::Encryption qw(checksum);
 
 sub _init {
   my $self = shift;
-  $self->cacheable( 0 );
-  $self->ajaxable(  0 );
-}
-
-sub caption {
-  my $self = shift;
-  return 'Shareable URL';
+  $self->cacheable(0);
+  $self->ajaxable(0);
 }
 
 sub content {
-  my $self = shift;
-  my $object = $self->object;
-
-  my @shares = grep { $_ } ($self->object->param('share_id'));
-
-  my $share_ref = join ';', (
-    map { ($_ =~ /^\d+$/) ? "share_ref=000000$_-". checksum($_) : "share_ref=$_" } @shares
-  );
-
-  my $url = $self->hub->referer->{'absolute_url'};
-  $url .= $url =~ /\?/ ? ';' : '?' unless $url =~ /;$/;
-  $url .= $share_ref;
-
-  my $html = qq(<p class="space-below">To share this data, use the URL:</p>
-<p class="space-below"><a href="$url">$url</a></p>
-<p class="space-below">Please note that this link will expire after 72 hours.</p>
-);
-  
-  return $html;
+  my $self      = shift;
+  my $hub       = $self->hub;
+  my @shares    = grep $_, $hub->param('share_id');
+  my $share_ref = join ';', map { $_ =~ /^\d+$/ ? "share_ref=000000$_-" . checksum($_) : "share_ref=$_" } @shares;
+  my $reload    = $hub->param('reload') ? '<div class="modal_reload"></div>' : '';
+  my $url       = $hub->referer->{'absolute_url'};
+     $url      .= $url =~ /\?/ ? ';' : '?' unless $url =~ /;$/;
+     $url      .= $share_ref;
+     
+  return qq{
+    <p class="space-below">To share this data, use the URL:</p>
+    <p class="space-below"><a href="$url">$url</a></p>
+    <p class="space-below">Please note that this link will expire after 72 hours.</p>
+    $reload
+  };
 }
 
 1;
