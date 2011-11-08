4f5dac1584950eb99eae52a0a6bfe1a0c0035cfe
Simon Brent <sb23@sanger.ac.uk>
2011-11-08 15:57:46 +0000

    adding saved configs

--- modules/EnsEMBL/Web/Controller/Config.pm
+++ modules/EnsEMBL/Web/Controller/Config.pm
@@ -6,6 +6,8 @@ package EnsEMBL::Web::Controller::Config;
 
 use strict;
 
+use Encode qw(decode_utf8);
+
 use base qw(EnsEMBL::Web::Controller::Modal);
 
 sub page_type { return 'Configurator'; }
@@ -25,12 +27,54 @@ sub update_configuration {
   
   return unless $hub->param('submit') || $hub->param('reset');
   
-  my $r           = $self->r;
-  my $type        = $hub->type;
-  my $view_config = $hub->get_viewconfig($hub->action);
-  my $updated     = $view_config->update_from_input;
+  my $r            = $self->r;
+  my $session      = $hub->session;
+  my $view_config  = $hub->get_viewconfig($hub->action);
+  my $code         = $view_config->code;
+  my $image_config = $view_config->image_config;  
+  my $updated      = $view_config->update_from_input;
+  my $existing_config;
   
-  $hub->session->store;
+  $session->store;
+  
+  if ($hub->param('save_as')) {
+    my %params = map { $_ => decode_utf8($hub->param($_)) } qw(record_type name description);
+    $params{'record_type_id'} = $params{'record_type'} eq 'session' ? $session->create_session_id : $hub->user ? $hub->user->id : undef;
+    
+    if ($params{'record_type_id'}) {
+      my $adaptor   = $hub->config_adaptor;
+      my $configs   = $adaptor->all_configs;
+      my $overwrite = $hub->param('overwrite');
+         $overwrite = undef unless exists $configs->{$overwrite}; # check that the overwrite id belongs to this user
+      my (%existing, @links);
+      
+      if ($overwrite) {
+        foreach my $id ($overwrite, $configs->{$overwrite}{'link_id'} || ()) {
+          $existing{$configs->{$id}{'type'}} = { record_id => $id };
+          $params{$_} ||= $configs->{$id}{$_} for qw(record_type record_type_id name description);
+          push @{$params{'set_ids'}}, $adaptor->record_to_sets($id);
+        }
+      }
+      
+      foreach (qw(view_config image_config)) {
+        ($params{'code'}, $params{'link'}) = $_ eq 'view_config' ? ($code, [ 'image_config', $image_config ]) : ($image_config, [ 'view_config', $code ]);
+        
+        my ($saved, $deleted) = $adaptor->save_config(%params, %{$existing{$_}}, type => $_, data => $adaptor->get_config($_, $params{'code'}));
+        
+        push @links, { id => $saved, code => $params{'code'}, link => $params{'link'}, set_ids => $params{'set_ids'} };
+        
+        if ($deleted) {
+          push @{$existing_config->{'deleted'}}, $deleted;
+        } elsif ($saved) {
+          $existing_config->{'saved'} ||= { value => $saved, class => $saved, html => $configs->{$saved}{'name'} }; # only provide one saved entry for a linked pair
+        }
+      }
+      
+      $adaptor->link_configs(@links);
+      
+      delete $existing_config->{'saved'} if $overwrite && $configs->{$existing_config->{'saved'}{'value'}}{'link_id'};
+    }
+  }
   
   if ($hub->param('submit')) {
     if ($r->headers_in->{'X-Requested-With'} eq 'XMLHttpRequest') {
@@ -48,8 +92,10 @@ sub update_configuration {
         };
       } elsif ($updated || $hub->param('reload')) {
         $json = $updated if ref $updated eq 'HASH';
-        $json->{'updated'} = 1;
+        $json->{'updated'}  = 1;
       }
+      
+      $json->{'existingConfig'} = $existing_config if $existing_config;
       
       $r->content_type('text/plain');
       
