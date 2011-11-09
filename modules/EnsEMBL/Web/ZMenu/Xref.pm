a432e9119405962e6eea3a49c85ff67650c9bdc5
Anne Parker <ap5@sanger.ac.uk>
2011-11-09 10:46:48 +0000

    Fixed zmenu so it only shows the xref corresponding to the pointer location!

--- modules/EnsEMBL/Web/ZMenu/Xref.pm
+++ modules/EnsEMBL/Web/ZMenu/Xref.pm
@@ -15,33 +15,40 @@ use strict;
 use base qw(EnsEMBL::Web::ZMenu);
 
 sub content {
-  my $self   = shift;
-  my $hub    = $self->hub;
-  my $object = $self->object;
-  my $data = $object->features('Xref')->data_objects;
+  my $self    = shift;
+  my $hub     = $self->hub;
+  my $data    = $self->object->features('Xref')->data_objects;
   my $ftype = $hub->param('ftype');
+  my $id    = $hub->param('id');
+  my $xref = $self->object->get_feature_by_id('Xref', 'primary_id', $id);
 
-  foreach my $array (@$data) {
-    my $xref = shift @$array;
-      $self->caption('Xref: '.$xref->db_display_name);
+  if ($xref) {
+    my $caption = 'Xref: '.$xref->db_display_name;
+    my $name    = $hub->param('name');
+    $caption   .= " ($name)" if $name; 
+    $self->caption($caption);
     
-      foreach my $g (@$array) {        
-        my $r = 'Chromosome '.$g->seq_region_name.': '.$self->thousandify($g->start).'-'.$self->thousandify($g->end);        
-        
-        $self->add_entry({
-          type  => 'Linked Gene',
-          label => $g->stable_id,
-          link  => $hub->url({ type => 'Gene', action => 'Summary' })
-        });      
-      
-        $self->add_entry({
-          type  => 'Location',
-          label => $r,
-          link  => $hub->url({type   => 'Location', action => 'View', g => undef, r => ($g->seq_region_name . ':' . $g->seq_region_start . '-' . $g->seq_region_end), h => $xref->db_display_name }),
-        });
-      }
-    
-    } 
+    my $r = $hub->param('r');
+    my $g = $hub->param('g');
+
+    $self->add_entry({
+      type  => 'Linked Gene',
+      label => $g,
+      link  => $hub->url({ type => 'Gene', action => 'Matches', g => $g })
+    });      
+
+    $self->add_entry({
+      type  => 'Location',
+      label => 'Chromosome '.$r,
+      link  => $hub->url({type   => 'Location', action => 'View', g => undef, r => $r, h => $xref->db_display_name }),
+    });
+
+    $self->add_entry({
+      type  => 'Description',
+      label => $hub->param('desc'),
+    });      
+
+  } 
 }
 
 1;
