4d76414d851a970655ceded09d54a4acebff05c6
Bronwen Aken <ba1@sanger.ac.uk>
2011-11-09 13:11:34 +0000

    Added Chinese softshell turtle

--- scripts/taxonomy/ensembl_aliases.sql
+++ scripts/taxonomy/ensembl_aliases.sql
@@ -607,6 +607,16 @@ SET @this_name_class='ensembl common name';
 insert into ncbi_taxa_name select @this_taxon_id,@this_value,@this_name_class from ncbi_taxa_name WHERE taxon_id=@this_taxon_id and name_class=@this_name_class having count(*)=0;
 
 
+SET @this_taxon_id=13735;
+SET @this_value='Turtle';
+SET @this_name_class='ensembl alias name';
+insert into ncbi_taxa_name select @this_taxon_id,@this_value,@this_name_class from ncbi_taxa_name WHERE taxon_id=@this_taxon_id and name_class=@this_name_class having count(*)=0;
+
+SET @this_taxon_id=13735;
+SET @this_value='Chinese softshell turtle';
+SET @this_name_class='ensembl common name';
+insert into ncbi_taxa_name select @this_taxon_id,@this_value,@this_name_class from ncbi_taxa_name WHERE taxon_id=@this_taxon_id and name_class=@this_name_class having count(*)=0;
+
 -- -- Use these to ADD new species
 
 -- SET @this_taxon_id=;
