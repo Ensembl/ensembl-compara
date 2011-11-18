1c5f430ae818ab7d5cda97c9245e370b17423e64
Anne Parker <ap5@sanger.ac.uk>
2011-11-18 11:26:31 +0000

    Added method to get phenotype description from db, and updated caption methods accordingly

--- modules/EnsEMBL/Web/Object/Phenotype.pm
+++ modules/EnsEMBL/Web/Object/Phenotype.pm
@@ -7,13 +7,20 @@ use base qw(EnsEMBL::Web::Object::Feature);
 
 sub short_caption {
   my $self = shift;
-  return shift eq 'global' ? 'Phenotype: '.$self->hub->param('name') : 'Phenotype-based displays';
+  my $name = $self->get_phenotype_desc;
+  return shift eq 'global' ? "Phenotype: $name" : 'Phenotype-based displays';
 }
 
 sub caption {
   my $self = shift;
-  return 'Phenotype '.$self->hub->param('name');
+  return $self->get_phenotype_desc;
 }
 
+sub get_phenotype_desc {
+  my $self = shift;
+  my $vardb   = $self->hub->database('variation');
+  my $vaa     = $vardb->get_adaptor('VariationAnnotation');
+  return $vaa->fetch_phenotype_description_by_id($self->hub->param('ph'));
+};
 
 1;
