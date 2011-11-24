fb1c523548407e15c836f8dcb5a392b09acffa28
Kathryn Beal <kbeal@ebi.ac.uk>
2011-11-24 09:57:50 +0000

    Added the ability to define the database urls in the config file, making the setup much easier if using the defaults. Changed dump_mlss_id to the more usual mlss_id. General tidy up of unused parameters.

--- modules/Bio/EnsEMBL/Compara/PipeConfig/DumpMultiAlign_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/DumpMultiAlign_conf.pm
@@ -1,9 +1,27 @@
 ## Configuration file for DumpMultiAlign pipeline
+#Release 65
+#
+#epo 6 way
+#init_pipeline.pl Bio::EnsEMBL::Compara::PipeConfig::DumpMultiAlign_conf --password **** --mlss_id 548 --output_dir /lustre/scratch101/ensembl/kb3/scratch/hive/release_65/emf_dumps/epo_6_primate --species human -dbname dumpMultiAlign_6way_primate_65 -pipeline_name DUMP_6_65
+#3.4 hours
+#
+#epo 12 way
+#init_pipeline.pl Bio::EnsEMBL::Compara::PipeConfig::DumpMultiAlign_conf --password **** --mlss_id 547 --output_dir /lustre/scratch101/ensembl/kb3/scratch/hive/release_65/emf_dumps/epo_12_eutherian --species human -dbname dumpMultiAlign_12way_eutherian_65 -pipeline_name DUMP_12_65
+#2.7 hours
+#
+#mercator/pecan 19 way
+#init_pipeline.pl Bio::EnsEMBL::Compara::PipeConfig::DumpMultiAlign_conf --password **** --mlss_id 50035 --output_dir /lustre/scratch101/ensembl/kb3/scratch/hive/release_65/emf_dumps/pecan_19_amniota --species human -dbname dumpMultiAlign_19way_amniota_65 -pipeline_name DUMP_19_65
+#5.5 hours
+#
+#low coverage epo 35 way
+#init_pipeline.pl Bio::EnsEMBL::Compara::PipeConfig::DumpMultiAlign_conf --password **** --mlss_id 50036 --output_dir /lustre/scratch101/ensembl/kb3/scratch/hive/release_65/emf_dumps/epo_35_eutherian --species human --high_coverage_mlss_id 547 -dbname dumpMultiAlign_35way_eutherian_65 -pipeline_name DUMP_35_65
+#
 
 package Bio::EnsEMBL::Compara::PipeConfig::DumpMultiAlign_conf;
 
 use strict;
 use warnings;
+
 use base ('Bio::EnsEMBL::Hive::PipeConfig::HiveGeneric_conf');  # All Hive databases configuration files should inherit from HiveGeneric, directly or indirectly
 
 
@@ -11,7 +29,7 @@ sub default_options {
     my ($self) = @_;
     return {
         'ensembl_cvs_root_dir' => $ENV{'HOME'}.'/src/ensembl_main/', 
-	'release'       => 61,
+	'release'       => 65,
         'pipeline_name' => 'DUMP_'.$self->o('release'),  # name used by the beekeeper to prefix job names on the farm
 
         'dbname' => 'dumpMultiAlign'.$self->o('release'),  # database suffix (without user name prepended)
@@ -23,7 +41,41 @@ sub default_options {
             -pass   => $self->o('password'),
             -dbname => $ENV{USER}.'_'.$self->o('dbname'),
         },
-	'core_url' => "",
+
+        'staging_loc1' => {                     # general location of half of the current release core databases
+            -host   => 'ens-staging1',
+            -port   => 3306,
+            -user   => 'ensro',
+            -pass   => '',
+	    -driver => 'mysql',
+	    -dbname => $self->o('release'),
+        },
+
+        'staging_loc2' => {                     # general location of the other half of the current release core databases
+            -host   => 'ens-staging2',
+            -port   => 3306,
+            -user   => 'ensro',
+            -pass   => '',
+	    -driver => 'mysql',
+	    -dbname => $self->o('release'),
+        },
+
+        'livemirror_loc' => {                   # general location of the previous release core databases (for checking their reusability)
+            -host   => 'ens-livemirror',
+            -port   => 3306,
+            -user   => 'ensro',
+            -pass   => '',
+        },
+
+	#Location of core and, optionally, compara db
+	'db_urls' => [ $self->dbconn_2_url('staging_loc1'), $self->dbconn_2_url('staging_loc2') ],
+
+	#Alternative method of defining location of dbs
+	'reg_conf' => '',
+
+	#Default compara. Can be the database name (if loading via db_urls) or the url
+	'compara_db' => 'Multi',
+
 	'species'  => "human",
         'coord_system_name1' => "chromosome",
         'coord_system_name2' => "supercontig",
@@ -50,6 +102,7 @@ sub pipeline_create_commands {
 	#Store DumpMultiAlign healthcheck results
         'mysql ' . $self->dbconn_2_mysql('pipeline_db', 1) . " -e 'CREATE TABLE healthcheck (filename VARCHAR(400) NOT NULL, expected INT NOT NULL, dumped INT NOT NULL)'",
 	
+	'mkdir -p '.$self->o('output_dir'), #Make dump_dir directory
     ];
 }
 
@@ -77,15 +130,11 @@ sub pipeline_analyses {
 	 {  -logic_name => 'initJobs',
             -module     => 'Bio::EnsEMBL::Compara::RunnableDB::DumpMultiAlign::InitJobs',
             -parameters => {'species' => $self->o('species'),
-			    'format' => $self->o('format'),
-			    'dump_mlss_id' => $self->o('dump_mlss_id'),
+			    'dump_mlss_id' => $self->o('mlss_id'),
 			    'output_dir' => $self->o('output_dir'),
-			    'compara_dbname' => $self->o('compara_dbname'),
+			    'compara_db' => $self->o('compara_db'),
+			    'db_url'    =>  $self->o('db_urls'),
 			    'reg_conf' => $self->o('reg_conf'),
-			    'split_size' => $self->o('split_size'),
-			    'masked_seq' => $self->o('masked_seq'),
-			    'dump_program' => $self->o('dump_program'),
-			    'emf2maf_program' => $self->o('emf2maf_program'),
 			    'maf_output_dir' => $self->o('maf_output_dir'), #define if want to run emf2maf 
 			   },
             -input_ids => [ {} ],
@@ -101,8 +150,8 @@ sub pipeline_analyses {
             -module        => 'Bio::EnsEMBL::Compara::RunnableDB::DumpMultiAlign::CreateChrJobs',
             -parameters    => {'coord_system_name' => $self->o('coord_system_name1'),
 			       'format' => $self->o('format'),
-			       'output_dir' => $self->o('output_dir'),
-			       'compara_dbname' => $self->o('compara_dbname'),
+			       'compara_db' => $self->o('compara_db'),
+			       'db_url'    =>  $self->o('db_urls'),
 			       'reg_conf' => $self->o('reg_conf'),
 			       'split_size' => $self->o('split_size'),
 			      },
@@ -117,7 +166,8 @@ sub pipeline_analyses {
             -parameters    => {'coord_system_name' => $self->o('coord_system_name2'),
                                'format' => $self->o('format'),
 			       'output_dir' => $self->o('output_dir'),
-			       'compara_dbname' => $self->o('compara_dbname'),
+			       'compara_db' => $self->o('compara_db'),
+			       'db_url'    =>  $self->o('db_urls'),
 			       'reg_conf' => $self->o('reg_conf'),
 			      },
             -input_ids     => [
@@ -130,9 +180,9 @@ sub pipeline_analyses {
             -module        => 'Bio::EnsEMBL::Compara::RunnableDB::DumpMultiAlign::CreateOtherJobs',
             -parameters    => {'species' => $self->o('species'),
 			       'format' => $self->o('format'),
-			       'output_dir' => $self->o('output_dir'),
-			       'compara_dbname' => $self->o('compara_dbname'),
+			       'compara_db' => $self->o('compara_db'),
 			       'reg_conf' => $self->o('reg_conf'),
+			       'db_url'    =>  $self->o('db_urls'),
 			       'split_size' => $self->o('split_size'),
 			      },
             -input_ids     => [
@@ -144,13 +194,16 @@ sub pipeline_analyses {
         },
 	{  -logic_name    => 'dumpMultiAlign',
             -module        => 'Bio::EnsEMBL::Compara::RunnableDB::DumpMultiAlign::DumpMultiAlign',
-            -parameters    => {"cmd"=>"perl " . $self->o('dump_program') . " --reg_conf " .  $self->o('reg_conf') .  " --dbname " . $self->o('compara_dbname') . " --species " . $self->o('species') . " --mlss_id " . $self->o('dump_mlss_id') ." --coord_system " . "#coord_system# --masked_seq " . $self->o('masked_seq') . " --split_size " . $self->o('split_size') . " --output_format " . $self->o('format') . "  #extra_args#", 
+
+            -parameters    => {"cmd"=>"perl " . $self->o('dump_program') . " --species " . $self->o('species') . " --mlss_id " . $self->o('mlss_id') ." --coord_system " . "#coord_system# --masked_seq " . $self->o('masked_seq') . " --split_size " . $self->o('split_size') . " --output_format " . $self->o('format') . "  #extra_args#", 
+			       "reg_conf" => $self->o('reg_conf'),
+			       "db_urls" => $self->o('db_urls'),
+			       "compara_db" => $self->o('compara_db'),
 			       "num_blocks"=> "#num_blocks#",
 			       "output_dir"=> $self->o('output_dir'),
 			       "output_file"=>"#output_file#" , 
 			       "dumped_output_file"=>"#dumped_output_file#" , 
 			       "format" => $self->o('format'), 
-			       #"emf2maf_program" => $self->o('emf2maf_program'), 
 			       "maf_output_dir" => $self->o('maf_output_dir'),
 			      },
             -input_ids     => [
@@ -164,9 +217,12 @@ sub pipeline_analyses {
         },
 	{  -logic_name    => 'emf2maf',
             -module        => 'Bio::EnsEMBL::Compara::RunnableDB::DumpMultiAlign::Emf2Maf',
-            -parameters    => {"output_dir"=> $self->o('output_dir'), "emf2maf_program" => $self->o('emf2maf_program'), "maf_output_dir" => $self->o('maf_output_dir')},
+            -parameters    => {"output_dir"=> $self->o('output_dir'), 
+			       "emf2maf_program" => $self->o('emf2maf_program'), 
+			       "maf_output_dir" => $self->o('maf_output_dir')},
             -input_ids     => [
             ],
+	   -can_be_empty  => 1,
 	   -hive_capacity => 200,
 	   -flow_into => {
 	       2 => [ 'compress' ],
@@ -178,7 +234,6 @@ sub pipeline_analyses {
             -input_ids     => [
             ],
 	   -hive_capacity => 200,
-	   -wait_for => [ 'emf2maf' ],
         },
 	{  -logic_name    => 'md5sum',
             -module        => 'Bio::EnsEMBL::Compara::RunnableDB::DumpMultiAlign::MD5SUM',
@@ -190,9 +245,10 @@ sub pipeline_analyses {
 	{  -logic_name    => 'readme',
             -module        => 'Bio::EnsEMBL::Compara::RunnableDB::DumpMultiAlign::Readme',
             -parameters    => {'format' => $self->o('format'),
+			       'compara_db' => $self->o('compara_db'),
 			       'reg_conf' => $self->o('reg_conf'),
-			       'compara_dbname' => $self->o('compara_dbname'),
-			       'mlss_id' => $self->o('dump_mlss_id'),
+			       'db_url'    =>  $self->o('db_urls'),
+			       'mlss_id' => $self->o('mlss_id'),
 			       'output_dir' => $self->o('output_dir'),
 			       'split_size' => $self->o('split_size'),
 			       'species_tree_file' => $self->o('species_tree_file'),
