02084f1fb86f2aa5b4799960989b25d08a53c1e0
Simon Brent <sb23@sanger.ac.uk>
2011-11-28 11:42:57 +0000

    add set_format function

--- modules/EnsEMBL/Web/Command/UserData/UploadFile.pm
+++ modules/EnsEMBL/Web/Command/UserData/UploadFile.pm
@@ -9,8 +9,11 @@ use HTML::Entities qw(encode_entities);
 use base qw(EnsEMBL::Web::Command::UserData);
 
 sub process {
-  my $self       = shift;
-  my $hub        = $self->hub;
+  my $self = shift;
+  my $hub  = $self->hub;
+  
+  return $self->set_format if $hub->function eq 'set_format';
+  
   my $error      = $hub->input->cgi_error;
   my ($method)   = grep $hub->param($_), qw(text file url);
   my $url_params = { __clear => 1 };
@@ -28,6 +31,23 @@ sub process {
   }
 
   $self->file_uploaded($url_params);
+}
+
+sub set_format {
+  my $self    = shift;
+  my $hub     = $self->hub;
+  my $session = $hub->session;
+  my $code    = $hub->param('code');
+  my $format  = $hub->param('format');
+  
+  $session->set_data(%{$session->get_data(code => $code)}, format => $format) if $format;
+  
+  $self->ajax_redirect($hub->url({
+    action   => $format ? 'UploadFeedback' : 'MoreInput',
+    function => undef,
+    format   => $format,
+    code     => $code
+  }));
 }
 
 1;
