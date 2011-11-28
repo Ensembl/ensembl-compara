8d3d7f290c3028000c423274c985585b02d527fc
Simon Brent <sb23@sanger.ac.uk>
2011-11-28 09:25:09 +0000

    add format to upload in Command. Tidy.

--- modules/EnsEMBL/Web/Component/UserData/UploadFeedback.pm
+++ modules/EnsEMBL/Web/Component/UserData/UploadFeedback.pm
@@ -12,51 +12,28 @@ sub _init {
   $self->ajaxable(0);
 }
 
-sub caption {
-  return 'File Uploaded';
-}
 
 sub content {
   my $self   = shift;
   my $hub    = $self->hub;
-  my $upload = $hub->session->get_data('code' => $hub->param('code'));
+  my $upload = $hub->session->get_data(code => $hub->param('code'));
   my $html;
 
   if ($upload) {
-
-    ## Set format from CGI if necessary
-    my $format;
-    if ($hub->param('format')) {
-      $format = $hub->param('format');
-      if ($format =~ /bedgraph/i) {
-        $format = 'BED';
-      }
-      $upload->{'format'} = $format;
-      $hub->session->set_data(%$upload); 
-    } 
-    else {
-      $format = $upload->{'format'};
-    }
-
-    $html = '<p class="space-below">Thank you. Your file uploaded successfully</p><p class="space-below"><strong>File uploaded</strong>: '.$upload->{'name'}.' (';
-    if ($format) {
-      $html .= "$format file";
-    }
-    else {
-      $html .= 'Unknown format';
-    }
-    if (my $species = $upload->{'species'}) {
-      $species =~ s/_/ /;
-      $html .= ", <em>$species</em>";
-    }
-    else {
-      $html .= ', unknown species';
-    }
-    $html .= ')</p>';
+    my $format  = $upload->{'format'};
+    my $species = $upload->{'species'} ? $hub->species_defs->get_config($upload->{'species'}, 'SPECIES_SCIENTIFIC_NAME') : '';
+    
+    $html = sprintf('
+      <p class="space-below">Thank you. Your file uploaded successfully</p>
+      <p class="space-below"><strong>File uploaded</strong>: %s (%s, %s)</p>',
+      $upload->{'name'},
+      $format  ? "$format file"      : 'Unknown format',
+      $species ? "<em>$species</em>" : 'unknown species'
+    );
+  } else {
+    $html = 'Sorry, there was a problem uploading your file. Please try again.';
   }
-  else {
-    $html = qq('Sorry, there was a problem uploading your file. Please try again.');
-  }
+  
   return $html;
 }
 
