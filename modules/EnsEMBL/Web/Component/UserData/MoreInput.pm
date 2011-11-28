bcebb6e4cc54d903bbd0b729de6881d67629f0b9
Simon Brent <sb23@sanger.ac.uk>
2011-11-28 11:43:22 +0000

    redirect to UploadFile/set_format

--- modules/EnsEMBL/Web/Component/UserData/MoreInput.pm
+++ modules/EnsEMBL/Web/Component/UserData/MoreInput.pm
@@ -1,31 +1,25 @@
+# $Id$
+
 package EnsEMBL::Web::Component::UserData::MoreInput;
 
 use strict;
-use warnings;
-no warnings "uninitialized";
 
 use base qw(EnsEMBL::Web::Component::UserData);
 
 sub _init {
   my $self = shift;
-  $self->cacheable( 0 );
-  $self->ajaxable(  0 );
-}
-
-sub caption {
-  my $self = shift;
-  return 'File Details';
+  $self->cacheable(0);
+  $self->ajaxable(0);
 }
 
 sub content {
-  my $self = shift;
+  my $self    = shift;
+  my $hub     = $self->hub;
+  my $species = $hub->param('species') || $hub->data_species;
+  my $form    = $self->modal_form('more_input', $hub->species_path($species) . '/UserData/UploadFile/set_format', { wizard => 1 });
 
-  my $species = $self->hub->param('species') || $self->hub->data_species;
-  my $form = $self->modal_form('more_input', $self->hub->species_path($species) . "/UserData/UploadFeedback", {'wizard' => 1});
-
-  $form->add_element(type => 'Hidden', name => 'code', value => $self->hub->param('code'));
-  $form->add_element(type => 'Hidden', name => 'species', value => $species);
-  $form->add_element(type => 'Information', value => 'Your file format could not be identified - please select an option:');
+  $form->add_element(type => 'Hidden', name => 'code', value => $hub->param('code'));
+  $form->add_element(type => 'Information',            value => 'Your file format could not be identified - please select an option:');
   $self->add_file_format_dropdown($form, 'upload');
 
   return $form->render;
