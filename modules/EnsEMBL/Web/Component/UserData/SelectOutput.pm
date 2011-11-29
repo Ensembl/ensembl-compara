60c84342e59df72702e2920857a70ebccacd2b4d
Simon Brent <sb23@sanger.ac.uk>
2011-11-29 12:06:43 +0000

    tidied

--- modules/EnsEMBL/Web/Component/UserData/SelectOutput.pm
+++ modules/EnsEMBL/Web/Component/UserData/SelectOutput.pm
@@ -1,8 +1,8 @@
+# $Id$
+
 package EnsEMBL::Web::Component::UserData::SelectOutput;
 
 use strict;
-use warnings;
-use warnings 'uninitialized';
 
 use URI::Escape qw(uri_escape);
 
@@ -14,53 +14,57 @@ sub _init {
   $self->ajaxable(0);
 }
 
-sub caption { return ''; }
-
 sub content {
-  my $self = shift; 
-  my $object = $self->object;
-  my ($html_target, $text_target, $title, $convert_file, $data_format);
-  my $extra_param = "";
+  my $self  = shift; 
+  my $hub   = $self->hub;
+  my $code  = $hub->param('code');
+  my $count = $hub->param('count');
+  my ($html_action, $text_action, $title, $data_format);
 
-  if ($object->param('id_mapper') ) {
-    $title = 'Stable ID Mapper';
-    $html_target = 'IDConversion';
-    $text_target = 'MapIDs';       
+  if ($hub->param('id_mapper') ) {
+    $title       = 'Stable ID Mapper';
+    $html_action = 'IDConversion';
+    $text_action = 'MapIDs';       
     $data_format = 'id';
-  } elsif ($object->param('consequence_mapper')) {
-    $title = 'Variant Effect Predictor';  
-    $html_target = 'ConsequenceCalculator';
-    $text_target = 'PreviewConvertIDs';
+  } elsif ($hub->param('consequence_mapper')) {
+    $title       = 'Variant Effect Predictor';  
+    $html_action = 'ConsequenceCalculator';
+    $text_action = 'PreviewConvertIDs';
     $data_format = 'snp';
   }
 
-  my $html         = "<h2>$title</h2>";
+  my $html = "<h2>$title</h2>";
+  my $text = 'Please select the format you would like your output in:';
 
-  if ($object->param('consequence_mapper') && $object->param('count')) {
-    $html .= $self->_info('Too many features', 'Your file contained '.$object->param('count') .' features; however this web tool will only convert the first '. $object->param('size_limit') .' features in the file.');
+  if ($hub->param('consequence_mapper') && $count) {
+    $html .= $self->_info(
+      'Too many features',
+      sprintf('Your file contained %s features; however this web tool will only convert the first %s features in the file.', $count, $hub->param('size_limit'))
+    );
   }
-
-  my $text         = "Please select the format you would like your output in:";
-  my $species      = ';species=' . $object->param('species');
-  my $species_path = $object->species_path($object->data_species) || '/'.$object->param('species');
   
-  $extra_param .= ';_time=' . $object->param('_time') if $object->param('_time');
+  my %params = map { $hub->param($_) ? ($_ => $hub->param($_)) : () } qw(convert_file id_limit variation_limit _time);
+     $params{'data_format'} = $data_format;
   
-  $convert_file  = ';convert_file=' . $object->param('convert_file') if $object->param('convert_file');
-  $convert_file .= ';id_limit=' . $object->param('id_limit') if $object->param('id_limit');
-  $convert_file .= ';variation_limit=' . $object->param('variation_limit') if $object->param('variation_limit');
+  my $html_url = $hub->url({
+    action => $html_action,
+    format => 'html',
+    code   => $code || undef,
+    %params
+  });
   
-  my $html_url = "$species_path/UserData/$html_target?format=html;data_format=".$data_format . $convert_file . $species;
-  $html_url .= ';code='.$object->param('code') if $object->param('code');
-
-  my $text_url = "$species_path/UserData/$text_target?format=text;data_format=".$data_format . $convert_file . $species . $extra_param;
+  my $text_url = $hub->url({
+    action => $text_action,
+    format => 'text',
+    %params
+  });
 
   my $list = [
     qq{<a class="modal_link" href="$html_url">HTML</a>},
     qq{<a class="modal_link" href="$text_url">Text</a>}
   ];
   
-  my $form = $self->modal_form('select', "$species_path/UserData/IDMapper", { no_button => 1 });
+  my $form = $self->modal_form('select', $hub->url({ action => 'IDMapper' }, 1)->[0], { no_button => 1 });
   
   $form->add_fieldset;
   $form->add_notes({ class => undef, text => $text });
@@ -68,19 +72,18 @@ sub content {
   
   $html .= $form->render;
 
-  if ($object->param('code')) {
-    my $session_data = $self->hub->session->get_data('code' => $object->param('code'));
-    my $nearest = $session_data->{'nearest'};
+  if ($code) {
+    my $nearest = $self->hub->session->get_data(code => $code)->{'nearest'};
+    
     if ($nearest) {
-      $html .= qq(
-<p>or view a sample SNP in <a href="$species_path/Location/View?r=$nearest">Region in Detail</a></p>
-<p>(You can also view or download your converted file from 'Manage Your Data')</p>
-);
+      $html .= sprintf(qq{
+        <p>or view a sample SNP in <a href="%s">Region in Detail</a></p>
+        <p>(You can also view or download your converted file from 'Manage Your Data')</p>
+      }, $hub->url({ type => 'Location', action => 'View', r => $nearest, __clear => 1 }));
     }
   }
 
   return $html;
 }
-
 
 1;
