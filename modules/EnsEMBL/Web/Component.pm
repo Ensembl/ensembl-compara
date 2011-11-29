2c8e0959bb5680433473aae129fb70799b5d1d83
Simon Brent <sb23@sanger.ac.uk>
2011-11-29 13:56:22 +0000

    fix karyotype images when there's no image config

--- modules/EnsEMBL/Web/Component.pm
+++ modules/EnsEMBL/Web/Component.pm
@@ -361,7 +361,7 @@ sub new_vimage {
 
 sub new_karyotype_image {
   my ($self, $image_config) = @_;  
-  my $image = new EnsEMBL::Web::Document::Image($self->hub->species_defs, [ $image_config ]);
+  my $image = new EnsEMBL::Web::Document::Image($self->hub->species_defs, $image_config ? [ $image_config ] : undef);
   $image->{'object'} = $self->object;
   
   return $image;
