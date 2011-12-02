69c37fe285c090e0eb6b4333ef7bb442e3adcd02
Simon Brent <sb23@sanger.ac.uk>
2011-12-02 15:39:51 +0000

    replace space with _ in individual names

--- modules/EnsEMBL/Web/Command/Export/HaploviewFiles.pm
+++ modules/EnsEMBL/Web/Command/Export/HaploviewFiles.pm
@@ -53,8 +53,9 @@ sub make_files {
   }
   
   foreach my $individual (keys %ind_genotypes) {
-    my $i = $individuals{$individual};
-    my $output = join "\t", 'FAM' . $family++, $individual, $i->{'father'}, $i->{'mother'}, $i->{'gender'}, "0\t";
+    my $i      = $individuals{$individual};
+    (my $name  = $individual) =~ s/ /_/g;
+    my $output = join "\t", 'FAM' . $family++, $name, $i->{'father'}, $i->{'mother'}, $i->{'gender'}, "0\t";
     
     foreach (@snps) {
       my $snp = $ind_genotypes{$individual}->{$_} || '00';
