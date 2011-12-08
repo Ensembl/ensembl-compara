ece07375033d7b735b806ba8b245c657a8aa6ac7
Simon Brent <sb23@sanger.ac.uk>
2011-12-08 17:00:20 +0000

    render 1 black bar with white blocks on top

--- modules/Bio/EnsEMBL/GlyphSet/scalebar.pm
+++ modules/Bio/EnsEMBL/GlyphSet/scalebar.pm
@@ -8,22 +8,18 @@ use base qw(Bio::EnsEMBL::GlyphSet);
 
 sub render {
   my ($self, $y) = @_;
-  
-  my $container      = $self->{'container'};
-  my $length         = $container->length;
-  
-  my $contig_strand  = $container->can('strand') ? $container->strand : 1;
-  my $pix_per_bp     = $self->scalex;
-  
-  my $global_start   = $contig_strand < 0 ? -$container->end : $container->start;
-  my $global_end     = $contig_strand < 0 ? -$container->start : $container->end;
-  
-  my $register_line  = $self->get_parameter('opt_lines');
-  my $feature_colour = $self->get_parameter('col');
-  
+  my $container     = $self->{'container'};
+  my $length        = $container->length;
+  my $contig_strand = $container->can('strand') ? $container->strand : 1;
+  my $pix_per_bp    = $self->scalex;
+  my $global_start  = $contig_strand < 0 ? -$container->end : $container->start;
+  my $global_end    = $contig_strand < 0 ? -$container->start : $container->end;
+  my $register_line = $self->get_parameter('opt_lines');
+  my $filled        = 1;
+  my $last_text_x   = -1e20;
   my ($fontname, $fontsize) = $self->get_font_details('innertext');
   my ($major_unit, $minor_unit);
-
+  
   if ($length <= 51) {
     $major_unit = 10;
     $minor_unit = 1; 
@@ -46,65 +42,71 @@ sub render {
     }
   }
   
+  # Black bar all the way along
+  $self->push($self->Rect({
+    x            => 0, 
+    y            => $y,
+    width        => $length,
+    height       => 3,
+    colour       => 'black',
+    bordercolour => 'black',
+    absolutey    => 1
+  }));
+  
   my $start = floor($global_start / $minor_unit) * $minor_unit;
-  my $filled = 1;
-  my $last_text_x = -1e20;
   
   while ($start <= $global_end) { 
     my $end       = $start + $minor_unit - 1;
     my $box_start = $start < $global_start ? $global_start : $start;
     my $box_end   = $end   > $global_end   ? $global_end   : $end;
+       $filled    = 1 - $filled;
     
-    $filled = 1 - $filled;
-    
-    # Draw the glyph for this box
-    my $t = $self->Rect({
-      'x'         => $box_start - $global_start, 
-      'y'         => $y,
-      'width'     => abs($box_end - $box_start + 1),
-      'height'    => 3,
-      ( $filled == 1 ? 'colour' : 'bordercolour' ) => 'black',
-      'absolutey' => 1
-    });
+    if (!$filled) {
+      # white blocks on top of the black bar
+      my $t = $self->Rect({
+        x            => $box_start - $global_start, 
+        y            => $y,
+        width        => abs($box_end - $box_start + 1),
+        height       => 3,
+        colour       => 'white',
+        bordercolour => 'black',
+        absolutey    => 1
+      });
 
-    $self->push($t);
-    
-    # Vertical lines across all species
-    if ($register_line) {
-      # This is the end of the box
-      if ($start == $box_start) {
-        $self->join_tag($t, "ruler_$start", 0, 0 , $start % $major_unit ? 'grey90' : 'grey80');
-      } elsif (($box_end == $global_end) && !(($box_end + 1) % $minor_unit)) {
-        $self->join_tag($t, "ruler_$end", 1, 0 ,($global_end + 1) % $major_unit ? 'grey90' : 'grey80');
+      $self->push($t);
+      
+      # Vertical lines across all species
+      if ($register_line) {
+        $self->join_tag($t, "ruler_$start", 0, 0, $start            % $major_unit ? 'grey90' : 'grey80');
+        $self->join_tag($t, "ruler_$end",   1, 0, ($global_end + 1) % $major_unit ? 'grey90' : 'grey80') unless ($box_end + 1) % $minor_unit;
       }
     }
     
     # Draw the major unit tick 
     unless ($box_start % $major_unit) {
       $self->push($self->Rect({
-        'x'         => $box_start - $global_start,
-        'y'         => $y, 
-        'width'     => 0,
-        'height'    => 5,
-        'colour'    => 'black',
-        'absolutey' => 1
+        x         => $box_start - $global_start,
+        y         => $y, 
+        width     => 0,
+        height    => 5,
+        colour    => 'black',
+        absolutey => 1
       }));
       
       my $label = $minor_unit < 1000 ? $self->commify($box_start * $contig_strand): $self->bp_to_nearest_unit($box_start * $contig_strand, 2);
-      
-      my @res = $self->get_text_width(($box_start - $last_text_x) * $pix_per_bp * 1.5, $label, '', 'font' => $fontname, 'ptsize' => $fontsize);
+      my @res   = $self->get_text_width(($box_start - $last_text_x) * $pix_per_bp * 1.5, $label, '', font => $fontname, ptsize => $fontsize);
 
       if ($res[0]) {
         $self->push($self->Text({
-          'x'         => $box_start - $global_start,
-          'y'         => defined $y ? $y - $res[3] - 1 : 5,
-          'height'    => $res[3],
-          'font'      => $fontname,
-          'ptsize'    => $fontsize,
-          'halign'    => 'left',
-          'colour'    => $feature_colour,
-          'text'      => $label,
-          'absolutey' => 1
+          x         => $box_start - $global_start,
+          y         => defined $y ? $y - $res[3] - 1 : 5,
+          height    => $res[3],
+          font      => $fontname,
+          ptsize    => $fontsize,
+          halign    => 'left',
+          colour    => 'black',
+          text      => $label,
+          absolutey => 1
         }));
         
         $last_text_x = $box_start;
@@ -117,12 +119,12 @@ sub render {
   # Draw the major unit tick 
   unless (($global_end + 1) % $major_unit) {
     $self->push($self->Rect({
-      'x'         => $global_end - $global_start + 1,
-      'y'         => $y,
-      'width'     => 0,
-      'height'    => 5,
-      'colour'    => 'black',
-      'absolutey' => 1
+      x         => $global_end - $global_start + 1,
+      y         => $y,
+      width     => 0,
+      height    => 5,
+      colour    => 'black',
+      absolutey => 1
     }));
   }
   
@@ -134,14 +136,11 @@ sub bp_to_nearest_unit {
   
   $dp = 1 unless defined $dp;
    
-  my @units = qw( bp Kb Mb Gb Tb );
+  my @units = qw(bp Kb Mb Gb Tb);
   my $power = int((length(abs $bp) - 1) / 3);
-  
-  my $unit = $units[$power];
-
+  my $unit  = $units[$power];
   my $value = int($bp / (10 ** ($power * 3)));
-    
-  $value = sprintf "%.${dp}f", $bp / (10 ** ($power * 3)) if $unit ne 'bp';      
+     $value = sprintf "%.${dp}f", $bp / (10 ** ($power * 3)) if $unit ne 'bp';      
 
   return "$value $unit";
 }
