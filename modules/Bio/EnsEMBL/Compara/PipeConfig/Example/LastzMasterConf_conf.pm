628c1ec36df8fe6f6b3aadf2924aae84fa23d417
Kathryn Beal <kbeal@ebi.ac.uk>
2012-01-24 12:35:22 +0000

    Example pipeline configuarion file for running the pairaligner pipeline

---
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/Example/LastzMasterConf_conf.pm
@@ -0,0 +1,36 @@
+package Bio::EnsEMBL::Compara::PipeConfig::Example::LastzMasterConf_conf;
+
+#
+#Test with a master and pairwise alignment configuration file (lastz.conf)
+#human chr 22 vs mouse chr 16 and human chr 22 vs rat chr 11
+#Use lastz.conf to define the location of the core databases.
+#Set the master to the ensembl release for this test only.
+#
+
+use strict;
+use warnings;
+use base ('Bio::EnsEMBL::Compara::PipeConfig::PairAligner_conf');  # Inherit from base PairAligner class
+
+
+sub default_options {
+    my ($self) = @_;
+    return {
+	    %{$self->SUPER::default_options},   # inherit the generic ones
+	    'pipeline_name'         => 'LASTZ_CONF_'.$self->o('rel_with_suffix'),   # name the pipeline to differentiate the submitted processes
+	    #'master_db' => 'mysql://user@host/ensembl_compara_master',
+	    master_db => 'mysql://ensro@ens-livemirror:3306/ensembl_compara_65', #set to ensembl release for test only
+
+	    #Location of executables
+	    'pair_aligner_exe' => '/software/ensembl/compara/bin/lastz',
+
+	    #
+            #Skip pairaligner configuration module
+            #
+            'skip_pairaligner_config' => 1,
+            'bed_dir' => $self->o('dump_dir').'/bed_files', 
+            'output_dir' => $self->o('dump_dir').'/output', 
+
+ 	   };
+}
+
+1;
