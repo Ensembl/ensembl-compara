968964dc77f23124b38fee4d30857a589671c06f
Anne Parker <ap5@sanger.ac.uk>
2012-01-25 10:44:38 +0000

    Improved error handling and fixed problem with session records

--- modules/EnsEMBL/Web/Command/UserData/RunRegionTool.pm
+++ modules/EnsEMBL/Web/Command/UserData/RunRegionTool.pm
@@ -3,17 +3,15 @@ package EnsEMBL::Web::Command::UserData::RunRegionTool;
 use strict;
 
 use EnsEMBL::Web::TmpFile::Text;
+use Digest::MD5 qw(md5_hex);
 
-use base qw(EnsEMBL::Web::Command);
+use base qw(EnsEMBL::Web::Command::UserData);
 
 sub process {
   my $self    = shift;
   my $hub     = $self->hub;
   my $session = $hub->session;
   my $data    = $session->get_data(code => $hub->param('code'));
-
-  my $url     = $hub->species_path($hub->data_species) . '/UserData/RegionReportOutput';
-  my $param;
 
   my $root    = $hub->species_defs->ENSEMBL_SERVERROOT;
   my $script  = $root.'/ensembl-tools/scripts/region_reporter/region_report.pl';
@@ -49,27 +47,56 @@ sub process {
   $oneliner .= sprintf('--password=%s', $hub->species_defs->DATABASE_DBPASS) if $hub->species_defs->DATABASE_DBPASS;
 
   ## Pipe straight to output file, to conserve memory
-  my $filename  = $self->temp_file_name.'.txt';
-  my $path      = $hub->species_defs->ENSEMBL_TMP_DIR."/region_report";
+  my $output    = $self->temp_file_name;
+  my $extension = $hub->param('output_format') eq 'gff3' ? 'gff' : 'txt';
+  my $path      = $hub->species_defs->ENSEMBL_TMP_DIR.'/download';
   my $directory = $self->make_directory($path.'/'.$filename);
 
-  $oneliner .= " --output=$path/$filename";
+  $oneliner .= " --output=$path/$output.$extension";
 
   #warn ">>> $oneliner";
 
-  my $error = system($oneliner);
+  my $response = system($oneliner);
+  my %return = (
+                1 => 'no_features',
+                2 => 'location_unknown',
+              );
 
-  if ($error) {
-    $param->{'filter_module'} = 'Regions';
-    $param->{'filter_code'} = 'script_fail';
-    warn ">>> REGION REPORT ERROR $?";
+  my $param = {'action' => 'RegionReportOutput'};
+
+  if ($response) {
+    my $exitcode = $? >>8;
+    $param->{'error_code'} = $return{$exitcode};
+    $param->{'code'} = $hub->param('code');
+    warn "!!! REGION REPORT ERROR ".$param->{'error_code'};
   }
   else {
-    $param->{'code'} = $hub->param('code');
-  }  
-  warn ">>> URL $url";
+    ## Create new session record for output file 
+    my $session = $hub->session;
+    my $code    = join '_', md5_hex($output), $session->session_id;
 
-  $self->ajax_redirect($url, $param);
+    ## Attach data species to session
+    my $new_data = $session->add_data(
+          type      => 'upload',
+          filename  => $output,
+          code      => $code,
+          md5       => md5_hex($output),
+          name      => 'Region report',
+          species   => $data->{'species'},
+          format    => $hub->param('output_format'),
+          timestamp => time,
+          extension => $extension,
+          prefix    => 'download',
+    );
+
+    $session->configure_user_data('upload', $new_data);
+    $param->{'code'} = $code;
+
+    ## Remove uploaded file record
+    $session->purge_data('code' => $hub->param('code'));
+  }
+
+  $self->file_uploaded($param);
 }
 
 1;
