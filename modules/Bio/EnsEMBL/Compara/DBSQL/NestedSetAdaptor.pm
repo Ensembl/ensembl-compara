026d98b7b853806ee34ffdd3d7ded2e1f0da163e
Matthieu Muffato <muffato@ebi.ac.uk>
2012-02-02 14:38:12 +0000

    Fixed bug in fetch_subtree_under_node

--- modules/Bio/EnsEMBL/Compara/DBSQL/NestedSetAdaptor.pm
+++ modules/Bio/EnsEMBL/Compara/DBSQL/NestedSetAdaptor.pm
@@ -163,6 +163,7 @@ sub fetch_subtree_under_node {
   my $root_id = $node->_root_id;
   my $constraint = "WHERE ($alias.root_id = $root_id) AND ($alias.left_index >= $left_index) AND ($alias.right_index <= $right_index)";
   my $all_nodes = $self->_generic_fetch($constraint);
+  push @{$all_nodes}, $node;
   $self->_build_tree_from_nodes($all_nodes);
   return $node;
 }
