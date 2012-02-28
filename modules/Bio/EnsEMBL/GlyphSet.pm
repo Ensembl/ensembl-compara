92c3b02a109db2cb018c9b45c963dc88cd8a0078
Simon Brent <sb23@sanger.ac.uk>
2012-02-28 13:44:11 +0000

    fix updating config panel for primary species in multi species views

--- modules/Bio/EnsEMBL/GlyphSet.pm
+++ modules/Bio/EnsEMBL/GlyphSet.pm
@@ -225,7 +225,7 @@ sub init_label {
       header    => $name,
       desc      => $desc,
       class     => $class,
-      component => lc($component . ($config->multi_species ? '_' . $config->species : '')),
+      component => lc($component . ($config->multi_species && $config->species ne $hub->species ? '_' . $config->species : '')),
       renderers => \@r,
       fav       => [ $fav, "$url;$track=favourite_" ],
       off       => "$url;$track=off",
