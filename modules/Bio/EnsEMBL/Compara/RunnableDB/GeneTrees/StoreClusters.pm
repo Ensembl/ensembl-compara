f6f222d4aa75bd4340c952bd73623556ea299d64
Matthieu Muffato <muffato@ebi.ac.uk>
2012-03-05 16:37:59 +0000

    Base class to store clusterset and clusters

---
+++ modules/Bio/EnsEMBL/Compara/RunnableDB/GeneTrees/StoreClusters.pm
@@ -0,0 +1,226 @@
+=head1 LICENSE
+
+  Copyright (c) 1999-2012 The European Bioinformatics Institute and
+  Genome Research Limited.  All rights reserved.
+
+  This software is distributed under a modified Apache license.
+  For license details, please see
+
+   http://www.ensembl.org/info/about/code_licence.html
+
+=head1 CONTACT
+
+  Please email comments or questions to the public Ensembl
+  developers list at <dev@ensembl.org>.
+
+  Questions may also be sent to the Ensembl help desk at
+  <helpdesk@ensembl.org>.
+
+=head1 NAME
+
+Bio::EnsEMBL::Compara::RunnableDB::GeneTrees::StoreClusters
+
+=head1 DESCRIPTION
+
+This is a base RunnableDB to stores a set of clusters in the database.
+ProteinTrees::HclusterParseOutput and ncRNAtrees::RFAMClassify both
+inherit from it. The easiest way to use this class is by creating an
+array of arrays of member_id, and give it to store_and_dataflow_clusterset.
+This would create the c;usterset and create the subsequent jobs.
+
+=head1 AUTHORSHIP
+
+Ensembl Team. Individual contributions can be found in the CVS log.
+
+=head1 MAINTAINER
+
+$Author$
+
+=head VERSION
+
+$Revision$
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object methods.
+Internal methods are usually preceded with an underscore (_)
+
+=cut
+
+package Bio::EnsEMBL::Compara::RunnableDB::GeneTrees::StoreClusters;
+
+use strict;
+
+use Bio::EnsEMBL::Compara::GeneTree;
+use Bio::EnsEMBL::Compara::GeneTreeNode;
+use Bio::EnsEMBL::Compara::GeneTreeMember;
+
+use base ('Bio::EnsEMBL::Compara::RunnableDB::BaseRunnable');
+
+
+=head2 store_and_dataflow_clusterset
+
+  Description: Shortcut for all the individual steps. This function stores the
+               clusters and the clusterset, then flow the clusters into the
+               branch 2.
+  Arg [1]    : <arrayref> of <arrayref> of member_id
+  Returntype : none
+  Exceptions : none
+  Caller     : general
+
+=cut
+
+sub store_and_dataflow_clusterset {
+    my $self = shift;
+    my $allclusters = $self->param('allclusters');
+    $self->create_clusterset();
+    if ($self->param('sort_clusters')) {
+        foreach my $cluster_list (sort {scalar(@$b) <=> scalar(@$a)} @$allclusters) {
+            $self->add_cluster($cluster_list);
+        }
+    } else {
+        foreach my $cluster_list (@$allclusters) {
+            $self->add_cluster($cluster_list);
+        }
+    }
+    $self->finish_store_clusterset();
+    $self->dataflow_clusters();
+}
+
+
+=head2 create_clusterset
+
+  Description: Create an empty clusterset and store it in the database.
+  Returntype : GeneTree: the created clusterset
+  Exceptions : none
+  Caller     : general
+
+=cut
+
+sub create_clusterset {
+    my $self = shift;
+
+    my $mlss_id = $self->param('mlss_id') or die "'mlss_id' is an obligatory parameter";
+
+    # Create the clusterset and associate mlss
+    my $clusterset = new Bio::EnsEMBL::Compara::GeneTree;
+    $clusterset->tree_type($self->param('db_prefix').'clusterset');
+    $clusterset->method_link_species_set_id($mlss_id);
+    $self->param('clusterset', $clusterset);
+
+    # Create the clusterset root node
+    my $clusterset_root = new Bio::EnsEMBL::Compara::GeneTreeNode;
+    $clusterset->root($clusterset_root);
+    $self->compara_dba->get_GeneTreeAdaptor->store($clusterset);
+    
+    my @allcluster_ids;
+    $self->param('allcluster_ids', \@allcluster_ids);
+
+    return $clusterset;
+}
+
+
+=head2 add_cluster
+
+  Description: Create a new cluster (a root node linked to many leafes) and
+               store it in the database.
+  Arg [1]    : <arrayref> of member_id
+  Returntype : GeneTree: the created cluster
+  Exceptions : none
+  Caller     : general
+
+=cut
+
+sub add_cluster {
+    my $self = shift;
+    my $cluster_list = shift;
+    my $clusterset = $self->param('clusterset');
+
+    return if (2 > scalar(@$cluster_list));
+
+    # Every cluster maps to a leaf of the clusterset
+    my $clusterset_leaf = new Bio::EnsEMBL::Compara::GeneTreeNode;
+    $clusterset_leaf->no_autoload_children();
+    $clusterset->root->add_child($clusterset_leaf);
+
+    # The new cluster object
+    my $cluster = new Bio::EnsEMBL::Compara::GeneTree;
+    $cluster->tree_type($self->param('db_prefix').'tree');
+    $cluster->method_link_species_set_id($clusterset->method_link_species_set_id);
+    $cluster->clusterset_id($clusterset->root_id);
+
+    # The cluster root node
+    my $cluster_root = new Bio::EnsEMBL::Compara::GeneTreeNode;
+    $cluster_root->tree($cluster);
+    $cluster->root($cluster_root);
+    $clusterset_leaf->add_child($cluster_root);
+
+    # The cluster leaves
+    foreach my $member_id (@$cluster_list) {
+        my $leaf = new Bio::EnsEMBL::Compara::GeneTreeMember;
+        $leaf->member_id($member_id);
+        $cluster_root->add_child($leaf);
+    }
+
+    # Stores the cluster
+    $self->compara_dba->get_GeneTreeAdaptor->store($clusterset_leaf);
+    $cluster->store_tag('gene_count', $cluster_root->get_child_count);
+    print STDERR "cluster ", $cluster->root_id, " with ", $cluster_root->get_child_count, " leaves\n" if $self->debug;
+    
+    # Dataflows immediately or keep it for later
+    if ($self->param('immediate_dataflow')) {
+        $self->dataflow_output_id({ $self->param('hive_prefix').'_tree_id' => $cluster->root_id, }, 2);
+    } else {
+        push @{$self->param('allcluster_ids')}, $cluster->root_id;
+    }
+
+    # Frees memory
+    $cluster_root->disavow_parent();
+    $cluster_root->release_tree();
+}
+
+
+=head2 finish_store_clusterset
+
+  Description: Updates the left/right_index of the clusterset.
+  Returntype : none
+  Exceptions : none
+  Caller     : general
+
+=cut
+
+sub finish_store_clusterset {
+    my $self = shift;
+    my $clusterset = $self->param('clusterset');
+
+    # left/right_index for quicker clusterset retrieval
+    $clusterset->root->build_leftright_indexing(1);
+    $self->compara_dba->get_GeneTreeAdaptor->store($clusterset);
+    my $leafcount = scalar(@{$clusterset->root->get_all_leaves});
+    print STDERR "clusterset ", $clusterset->root_id, " with $leafcount leaves\n" if $self->debug;
+}
+
+
+=head2 dataflow_clusters
+
+  Description: Creates one job per cluster into branch 2.
+               Flows into branch 1 with the clusterset_id of the new clusterset
+  Arg [1]    : <arrayref> of member_id
+  Returntype : none
+  Exceptions : none
+  Caller     : general
+
+=cut
+
+sub dataflow_clusters {
+    my $self = shift;
+
+    # Loop on all the clusters that haven't been dataflown yet
+    foreach my $tree_id (@{$self->param('allcluster_ids')}) {
+        $self->dataflow_output_id({ $self->param('hive_prefix').'_tree_id' => $tree_id, }, 2);
+    }
+    $self->input_job->autoflow(0);
+    $self->dataflow_output_id({ 'clusterset_id' => $self->param('clusterset')->root_id }, 1);
+}
+
+1;
--- modules/Bio/EnsEMBL/Compara/RunnableDB/ProteinTrees/HclusterParseOutput.pm
+++ modules/Bio/EnsEMBL/Compara/RunnableDB/ProteinTrees/HclusterParseOutput.pm
@@ -22,8 +22,9 @@ Bio::EnsEMBL::Compara::RunnableDB::ProteinTrees::HclusterParseOutput
 
 =head1 DESCRIPTION
 
-This is the RunnableDB that parses the output of Hcluster, stores the clusters as trees without internal structure
-(each tree will have one root and several leaves) and dataflows the cluster_ids down branch #2.
+This is the RunnableDB that parses the output of Hcluster, stores the
+clusters as trees without internal structure (each tree will have one
+root and several leaves) and dataflows the cluster_ids down branch #2.
 
 =head1 SYNOPSIS
 
@@ -59,12 +60,16 @@ package Bio::EnsEMBL::Compara::RunnableDB::ProteinTrees::HclusterParseOutput;
 
 use strict;
 
-use Bio::EnsEMBL::Compara::GeneTree;
-use Bio::EnsEMBL::Compara::GeneTreeNode;
-use Bio::EnsEMBL::Compara::GeneTreeMember;
+use base ('Bio::EnsEMBL::Compara::RunnableDB::GeneTrees::StoreClusters');
 
-use base ('Bio::EnsEMBL::Compara::RunnableDB::BaseRunnable');
-
+sub param_defaults {
+    return {
+            'sort_clusters'         => 1,
+            'immediate_dataflow'    => 1,
+            'db_prefix'             => 'protein',
+            'hive_prefix'           => 'protein',
+    };
+}
 
 
 sub run {
@@ -77,7 +82,7 @@ sub run {
 sub write_output {
     my $self = shift @_;
 
-    $self->dataflow_clusters;
+    $self->store_and_dataflow_clusterset();
 }
 
 
@@ -90,43 +95,23 @@ sub write_output {
 sub parse_hclusteroutput {
     my $self = shift;
 
-    my $mlss_id = $self->param('mlss_id') or die "'mlss_id' is an obligatory parameter";
-
-    my $protein_tree_adaptor = $self->compara_dba->get_ProteinTreeAdaptor;
-
-    my $cluster_dir   = $self->param('cluster_dir');
-    my $filename      = $cluster_dir . '/hcluster.out';
-
-    # Create the clusterset and associate mlss
-    my $clusterset = new Bio::EnsEMBL::Compara::GeneTree;
-    $clusterset->tree_type('proteinclusterset');
-    $clusterset->method_link_species_set_id($mlss_id);
-    $self->param('clusterset', $clusterset);
-
-    my $clusterset_root = new Bio::EnsEMBL::Compara::GeneTreeNode;
-    $clusterset->root($clusterset_root);
-    $protein_tree_adaptor->store($clusterset);
+    my $filename      = $self->param('cluster_dir') . '/hcluster.out';
 
     my @allclusters;
-    my @allcluster_ids;
-    $self->param('allcluster_ids', \@allcluster_ids);
-
-    # FIXME: load the entire file in a hash and store in decreasing
-    # order by cluster size this will make big clusters go first in the
-    # alignment process, which makes sense since they are going to take
-    # longer to process anyway
+    $self->param('allclusters', \@allclusters);
+    
     open(FILE, $filename) or die "Could not open '$filename' for reading : $!";
     while (<FILE>) {
         # 0       0       0       1.000   2       1       697136_68,
         # 1       0       39      1.000   3       5       1213317_31,1135561_22,288182_42,426893_62,941130_38,
         chomp $_;
 
-        my ($cluster_id, $dummy1, $dummy2, $dummy3, $dummy4, $dummy5, $cluster_list) = split("\t",$_);
+        my ($cluster_id, $dummy1, $dummy2, $dummy3, $dummy4, $cluster_size, $cluster_list) = split("\t",$_);
 
-        next if ($dummy5 < 2);
-        $cluster_list =~ s/\,$//;
+        next if ($cluster_size < 2);
+        $cluster_list =~ s/\,^//;
         $cluster_list =~ s/_[0-9]*//g;
-        my @cluster_list = split(",",$cluster_list);
+        my @cluster_list = split(",", $cluster_list);
 
         # If it's a singleton, we don't store it as a protein tree
         next if (2 > scalar(@cluster_list));
@@ -134,58 +119,7 @@ sub parse_hclusteroutput {
     }
     close FILE;
 
-    # load the entire file in a hash and store in decreasing order by cluster
-    # size this will make big clusters go first in the alignment process,
-    # which makes sense since they are going to take longer to process anyway
-    foreach my $cluster_list (sort {scalar(@$b) <=> scalar(@$a)} @allclusters) {
-        my $clusterset_leaf = new Bio::EnsEMBL::Compara::GeneTreeNode;
-        $clusterset_leaf->no_autoload_children();
-        $clusterset_root->add_child($clusterset_leaf);
-
-        my $cluster = new Bio::EnsEMBL::Compara::GeneTree;
-        $cluster->tree_type('proteintree');
-        $cluster->method_link_species_set_id($mlss_id);
-
-        my $cluster_root = new Bio::EnsEMBL::Compara::GeneTreeNode;
-        $cluster->root($cluster_root);
-        $cluster->clusterset_id($clusterset_root->node_id);
-        $cluster_root->tree($cluster);
-        $clusterset_leaf->add_child($cluster_root);
-
-        foreach my $member_id (@$cluster_list) {
-            my $node = new Bio::EnsEMBL::Compara::GeneTreeMember;
-            $cluster_root->add_child($node);
-            $node->member_id($member_id);
-        }
-
-        # Store the cluster:
-        $protein_tree_adaptor->store($clusterset_leaf);
-        push @allcluster_ids, $cluster->root_id;
-
-        my $leafcount = scalar(@{$cluster->root->get_all_leaves});
-        print STDERR "cluster $cluster with $leafcount leaves\n" if $self->debug;
-        $cluster->store_tag('gene_count', $leafcount);
-        $cluster_root->disavow_parent();
-        $cluster_root->release_tree();
-
-    }
-    $clusterset_root->build_leftright_indexing(1);
-    $protein_tree_adaptor->store($clusterset);
-    $self->param('clusterset_id', $clusterset_root->node_id);
-    my $leafcount = scalar(@{$clusterset->root->get_all_leaves});
-    print STDERR "clusterset $clusterset with $leafcount leaves\n" if $self->debug;
-
 }
 
-
-sub dataflow_clusters {
-    my $self = shift;
-
-    foreach my $tree_id (@{$self->param('allcluster_ids')}) {
-        $self->dataflow_output_id({ 'protein_tree_id' => $tree_id, }, 2);
-    }
-    $self->input_job->autoflow(0);
-    $self->dataflow_output_id({ 'clusterset_id' => $self->param('clusterset_id') }, 1);
-}
 
 1;
