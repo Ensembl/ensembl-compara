967d7451847365004f9235052d82007ea413435c
Dan Sheppard <ds23@sanger.ac.uk>
2012-03-09 16:55:22 +0000

    This time to head.
    Update vega protein tree pipleine config to optimise to streamline vega
    builds.
    
    Reduces CPU time by 275hrs (47%) and clock time by 16h (64%).
    
    1. time limit and automate ramping of memory resources for mcoffee jobs so
    that it matches our old manual baby-sitting process, to mean pipe runs
    automatically;
    
    2. reduce parallelism in database-intensive tasks so that we don't kill our
    poor database server; so that we don't waste farm cpu-time waiting for the
    server; so that total clock-time is reduced due to less lock-furtling in
    database server; and so we don't get failures from connection timeouts;
    
    3. apply sensible cuttofs for tasks based on execution time to accurately
    distinguish jobs which will never finish (determined by analysis of earlier
    runs), so as not to waste CPU or wall time on endless;
    
    4. more accurately matches requested resources to those used to speed up
    allocation of slots on farm and to be nice to it when running and to deal
    with memory limit changes.
    
    Modifies the main protein pipeleine rather than duplicating it to
    increase chance that modifications to pipeline by compara will "just work".
    
    Slightly ugly is that without the ability to time-limit jobs, we have to
    time-limit workers (via LSF runlimit) which means there's a small chance
    that good jobs will fail leading to a suboptimal alignement. The probability
    is very samll, though, less than one protein slightly sub-optimally aligned
    in 10000 releases, way below the noise floor.

--- modules/Bio/EnsEMBL/Compara/PipeConfig/Example/VegaProteinTrees_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/Example/VegaProteinTrees_conf.pm
@@ -35,17 +35,32 @@ use strict;
 use warnings;
 use base ('Bio::EnsEMBL::Compara::PipeConfig::ProteinTrees_conf');
 
+use Storable qw(dclone);
 
+sub resource_classes {
+    my ($self) = @_;
+    return {
+         0 => { -desc => 'default',          'LSF' => '-M200000 -R"select[mem>260] rusage[mem=260]"' },
+         1 => { -desc => 'hcluster_run',     'LSF' => '-C0 -M1000000 -R"select[mycompara2<500 && mem>1000] rusage[mycompara2=10:duration=10:decay=1:mem=1000]"' },
+         2 => { -desc => 'mcoffee_himem',    'LSF' => '-W180 -C0 -M7500000 -R"select[mem>7500] rusage[mem=7500]"' },
+         3 => { -desc => 'mcoffee_veryhimem','LSF' => '-W180 -C0 -M15000000 -R"select[mem>15000] rusage[mem=15000]"' },
+         4 => { -desc => 'mcoffee',          'LSF' => '-W60 -M2500000 -R"select[mem>2600] rusage[mem=2600]"' },
+         5 => { -desc => 'bigish',           'LSF' => '-M1000000 -R"select[mem>1100] rusage[mem=1100]"' },
+         9 => { -desc => 'CAFE',             'LSF' => '-M15000000 -R"select[mem>15000] rusage[mem=15000]"'},
+    };
+}
+
+# each run you will need to specify and uncomment: mlss_id, release, work_dir, dbname
 sub default_options {
   my ($self) = @_;
   return {
     %{$self->SUPER::default_options},   # inherit the generic ones
 
     # parameters that are likely to change from execution to another:
-#    'mlss_id'               => 15,   # it is very important to check that this value is current (commented out to make it obligatory to specify)
-#    'release'               => '64',
+#    'mlss_id'               => 24,   # it is very important to check that this value is current (commented out to make it obligatory to specify)
+#    'release'               => '66', # specify and uncomment each run!
     'rel_suffix'            => 'vega',
-    'work_dir'              => '/lustre/scratch103/ensembl/'.$ENV{'USER'}.'/compara_generation/'.$ENV{'USER'}.'_vega_genetree_20110711_'.$self->o('rel_with_suffix'),
+#    'work_dir'              => '/lustre/scratch101/sanger/ds23/compara/compara-vega47/ds23_vega_genetree_20111219_66_faster', # specify and uncomment each run
     'outgroups'             => [ ],   # affects 'hcluster_dump_input_per_genome'
     'taxlevels'             => [ 'Theria' ],
     'filter_high_coverage'  => 1,   # affects 'group_genomes_under_taxa'
@@ -58,7 +73,7 @@ sub default_options {
       -port   => 5304,
       -user   => 'ottadmin',
       -pass   => $self->o('password'),
-      -dbname => $self->o('ENV', 'USER').'_compara_homology_'.$self->o('rel_with_suffix'),
+#      -dbname => 'ds23_vega_genetree_20111219_66_faster', # spcify and uncomment each run
     },
 
     # the master database for synchronization of various ids
@@ -80,15 +95,63 @@ sub default_options {
     'store_sequences_capacity'  => 50,
     'blastp_capacity'           => 450,
     'mcoffee_capacity'          => 100,
-    'njtree_phyml_capacity'     => 100,
-    'ortho_tree_capacity'       => 100,
-    'build_hmm_capacity'        => 200,
+    'njtree_phyml_capacity'     => 70,
+    'ortho_tree_capacity'       => 50,
+    'build_hmm_capacity'        => 50,
     'other_paralogs_capacity'   =>  50,
-    'homology_dNdS_capacity'    => 200,
+    'homology_dNdS_capacity'    => 100,
 
     #if the 65 hive fails at wublast p and you can't work out what's wrong with the path then uncomment this
-    #'wublastp_exe'              => '/usr/local/ensembl/bin/wublastp',
+    'wublastp_exe'              => '/usr/local/ensembl/bin/wublastp',
   };
+}
+
+#
+# We don't really want to have to maintain our own analysis pipeline, we just want to alter the existing one
+# to cope with our issues with exploding mcoffees. So we get the parent analysis and then tinker with it
+# rather than specifying it from scratch. This should make it clearer what we're changing and also make it
+# more robust to changes in unrelated parts of the analysis.
+#
+sub pipeline_analyses {
+  my ($self) = @_;
+
+  my $analyses = $self->SUPER::pipeline_analyses;
+
+  my %bigish = map { $_ => 1 } ('hcluster_parse_output','load_fresh_members','njtree_phyml',
+                                'store_sequences','store_sequences_factory');
+  # fix mcoffee
+  foreach $_ (@$analyses) {
+    my $name = $_->{'-logic_name'};
+    if($name eq 'mcoffee') {
+      $_->{'-rc_id'} = 4;
+    } elsif($name eq 'dummy_wait_alltrees') {
+      push @{$_->{'-wait_for'}},'mcoffee_veryhimem','mcoffee_mafft';
+    }
+    $_->{'-rc_id'} = 5 if(exists $bigish{$name});
+  }
+  # find mcoffee_himem and use as template for mcoffee_veryhimem and mcoffee_mafft
+  my $himem_i;
+  for(my $i=0;$i<@$analyses;$i++) {
+    $himem_i = $i if $analyses->[$i]->{'-logic_name'} eq 'mcoffee_himem';
+  }
+  die "No mcoffee_himem found" unless $himem_i;
+  my $himem = $analyses->[$himem_i];
+  # fix himem
+  $himem->{'-flow_into'}->{-2} = ['mcoffee_veryhimem'];
+  $himem->{'-flow_into'}->{-1} = ['mcoffee_veryhimem'];
+  # setup veryhimem
+  my $veryhimem = dclone($himem);
+  $veryhimem->{'-rc_id'} = 3;
+  $veryhimem->{'-logic_name'} = 'mcoffee_veryhimem';
+  $veryhimem->{'-flow_into'}->{-2} = ['mcoffee_mafft'];
+  $veryhimem->{'-flow_into'}->{-1} = ['mcoffee_mafft'];
+  # setup mafft
+  my $mafft = dclone($himem);
+  $mafft->{'-logic_name'} = 'mcoffee_mafft';
+  $mafft->{'-parameters'}->{'method'} = 'mafft';
+  # add them
+  splice(@$analyses,$himem_i+1,0,$veryhimem,$mafft);
+  return $analyses;
 }
 
 1;
