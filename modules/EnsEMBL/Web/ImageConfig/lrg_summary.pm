ea75b13d10441d0e29d68f1bc12a769c718cf9bf
Anne Parker <ap5@sanger.ac.uk>
2012-03-22 15:29:08 +0000

    Added 'hide_empty' flag on transcripts, to avoid confusing LRG users!

--- modules/EnsEMBL/Web/ImageConfig/lrg_summary.pm
+++ modules/EnsEMBL/Web/ImageConfig/lrg_summary.pm
@@ -11,6 +11,8 @@ sub init {
 
   $self->set_parameters({
     opt_lines => 1,  # draw registry lines
+    opt_empty_tracks => 1,     # include empty tracks
+    empty_track_exception => {'lrg'=> 1},
   });
 
   $self->create_menus(qw(
@@ -23,14 +25,14 @@ sub init {
     functional
     external_data
     user_data
-    other
+    information
   ));
   
   $self->get_node('transcript')->set('caption', 'Other genes');
   
-  $self->add_tracks('other',
+  $self->add_tracks('information',
     [ 'scalebar',  '', 'lrg_scalebar', { display => 'normal', strand => 'b', name => 'Scale bar', description => 'Shows the scalebar' }],
-    [ 'ruler',     '', 'ruler',        { display => 'normal', strand => 'b', name => 'Ruler',     description => 'Shows the length of the region being displayed' }],
+    [ 'information',     '', 'ruler',        { display => 'normal', strand => 'b', name => 'Ruler',     description => 'Shows the length of the region being displayed' }],
     [ 'draggable', '', 'draggable',    { display => 'normal', strand => 'b', menu => 'no' }],
   );
   
@@ -44,6 +46,7 @@ sub init {
   $self->add_tracks('lrg',
     [ 'lrg_transcript', 'LRG', '_transcript', {
       display     => 'normal',
+      hide_empty  => 1,
       name        => 'LRG transcripts', 
       description => 'Shows LRG transcripts',
       logic_names => [ 'LRG_import' ], 
@@ -54,7 +57,9 @@ sub init {
       zmenu       => 'LRG',
     }]
   );
-  
+ 
+  $self->modify_configs(['transcript'], { hide_empty => 1});
+ 
   $self->modify_configs(
     [ 'fg_regulatory_features_funcgen', 'transcript', 'prediction', 'variation' ],
     { display => 'off' }
