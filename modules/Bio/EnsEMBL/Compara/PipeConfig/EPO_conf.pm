f5588acce947d22572cfd9b66c8be7547974ea21
ens-stephenf <stephenf@ebi.ac.uk>
2012-03-29 10:58:17 +0000

    main_core_dbs is now an array of hashes, and default rc_id of 0 has been updated

--- modules/Bio/EnsEMBL/Compara/PipeConfig/EPO_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/EPO_conf.pm
@@ -15,11 +15,11 @@ sub default_options {
         'pipeline_name' => 'compara_EPO',
 
 	   # parameters that are likely to change from execution to another:
-	'release'               => '65',
+	'release'               => '67',
 	'rel_suffix'            => '',    # an empty string by default, a letter otherwise
 	   # dependent parameters:
 	'rel_with_suffix'       => $self->o('release').$self->o('rel_suffix'),
-	'species_tag' 		=> 'primates_',
+	'species_tag' 		=> '12mammals_',
 
 	   # connection parameters to various databases:
 	'pipeline_db' => { # the production database itself (will be created)
@@ -27,7 +27,7 @@ sub default_options {
 		-port   => 3306,
                 -user   => 'ensadmin',
 		-pass   => $self->o('password'),
-		-dbname => $self->o('ENV', 'USER').'_TEST_compara_epo_'.$self->o('species_tag').$self->o('rel_with_suffix'),
+		-dbname => $self->o('ENV', 'USER').'_compara_epo_'.$self->o('species_tag').$self->o('rel_with_suffix'),
    	},
 	 # ancestral seqs db
 	'ancestor_db' => {
@@ -36,7 +36,7 @@ sub default_options {
 		-port => 3306,
 		-pass => $self->o('password'),
 		-name => 'ancestral_sequences',
-		-dbname => $self->o('ENV', 'USER').'_TEST_ancestral_sequences_core_'.$self->o('rel_with_suffix'),
+		-dbname => $self->o('ENV', 'USER').'_ancestral_sequences_core_'.$self->o('rel_with_suffix'),
 	},
 	  # database containing the mapped anchors
 	'compara_mapped_anchor_db' => {
@@ -44,7 +44,7 @@ sub default_options {
 		-port => 3306,
 		-pass => '',
 		-host => 'compara3',
-		-dbname => 'sf5_compara64_anc_align',
+		-dbname => 'sf5_compara_template_anchor_align',
 	},
 	 # master db
 	'compara_master' => {
@@ -54,21 +54,30 @@ sub default_options {
 		-host => 'compara1',
 		-dbname => 'sf5_ensembl_compara_master',
 	},
-	'main_core_dbs' => {
-		-user => 'ensro',
-		-port => 3306,
-		-host => 'ens-livemirror',
-		-dbname => '',
-		-db_version => $self->o('release'),
-	},
+	'main_core_dbs' => [ 
+		{
+			-user => 'ensro',
+			-port => 3306,
+			-host => 'ens-staging1',
+			-dbname => '',
+			-db_version => $self->o('release'),
+		},
+		{
+			-user => 'ensro',
+			-port => 3306,
+			-host => 'ens-staging2',
+			-dbname => '',
+			-db_version => $self->o('release'),
+		},
+	],
 	other_core_dbs => {
 	},
 	  # mlssid of mappings to use
-	'mapping_mlssid' => 6,
+	'mapping_mlssid' => 12,
 	  # mlssid of ortheus alignments
-	'ortheus_mlssid' => 548,
+	'ortheus_mlssid' => 578,
 	  # species tree file
-	'species_tree_file' => '',
+	'species_tree_file' => '/lustre/scratch109/ensembl/sf5/EPO_DUMPS/species_tree_file',
 	  # data directories:
 	'mapping_file' => $self->o('ENV', 'EPO_DUMP_PATH').'/enredo_friendly.'.$self->o('rel_with_suffix'),
 	'enredo_output_file' => $self->o('ENV', 'EPO_DUMP_PATH').'/enredo.out.'.$self->o('rel_with_suffix'),
@@ -94,7 +103,7 @@ sub pipeline_create_commands {
 sub resource_classes {
     my ($self) = @_; 
     return {
-         0 => { -desc => 'default',  'LSF' => '' },
+         0 => { -desc => 'mem2000',  'LSF' => '-C0 -M2000000 -R"select[mem>2000] rusage[mem=2000]"' },
          1 => { -desc => 'mem3500',  'LSF' => '-C0 -M3500000 -R"select[mem>3500] rusage[mem=3500]"' },
          2 => { -desc => 'mem7500',  'LSF' => '-C0 -M7500000 -R"select[mem>7500] rusage[mem=7500]"' },  
          3 => { -desc => 'mem11400', 'LSF' => '-C0 -M11400000 -R"select[mem>11400] rusage[mem=11400]"' },  
