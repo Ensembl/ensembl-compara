9be5cf437c5a7608366f4dedbdfdd114a131b05a
Matthieu Muffato <muffato@ebi.ac.uk>
2012-03-30 15:59:20 +0000

    Now using Bio::EnsEMBL::Compara::DBSQL::BaseAdaptor

--- modules/Bio/EnsEMBL/Compara/DBSQL/BaseRelationAdaptor.pm
+++ modules/Bio/EnsEMBL/Compara/DBSQL/BaseRelationAdaptor.pm
@@ -3,86 +3,7 @@ package Bio::EnsEMBL::Compara::DBSQL::BaseRelationAdaptor;
 use strict;
 use Bio::EnsEMBL::Utils::Exception;
 
-use base ('Bio::EnsEMBL::DBSQL::BaseAdaptor');
-
-
-=head2 generic_fetch
-
-  Arg [1]    : (optional) string $constraint
-               An SQL query constraint (i.e. part of the WHERE clause)
-               e.g. "fm.family_id = $family_id"
-  Arg [2]    : (optional) arrayref $join
-               the arrayref $join should contain arrayrefs of this form
-               [['family_member', 'fm'],
-                # the table to join with synonym (mandatory) as an arrayref
-                'm.member_id = fm.member_id',
-                # the join condition (mandatory)
-                [qw(fm.family_id fm.member_id fm.cigar_line)]]
-                # any additional columns that the join could imply (optional)
-                # as an arrayref 
-  Example    : $arrayref = $a->generic_fetch($constraint, $join);
-  Description: Performs a database fetch and returns BaseRelation-inherited objects
-  Returntype : arrayref of Bio::EnsEMBL::Compara::BaseRelation-inherited objects
-  Exceptions : none
-  Caller     : various adaptors' specific fetch_ subroutines
-
-=cut
-  
-sub generic_fetch {
-  my ($self, $constraint, $join) = @_;
-  
-  my @tables = $self->_tables;
-  my $columns = join(', ', $self->_columns());
-  
-  if ($join) {
-    foreach my $single_join (@{$join}) {
-      my ($tablename, $condition, $extra_columns) = @{$single_join};
-      if ($tablename && $condition) {
-        push @tables, $tablename;
-        
-        if($constraint) {
-          $constraint .= " AND $condition";
-        } else {
-          $constraint = " $condition";
-        }
-      } 
-      if ($extra_columns) {
-        $columns .= ", " . join(', ', @{$extra_columns});
-      }
-    }
-  }
-      
-  #construct a nice table string like 'table1 t1, table2 t2'
-  my $tablenames = join(', ', map({ join(' ', @$_) } @tables));
-
-  my $sql = "SELECT $columns FROM $tablenames";
-
-  my $default_where = $self->_default_where_clause;
-  my $final_clause = $self->_final_clause;
-
-  #append a where clause if it was defined
-  if($constraint) { 
-    $sql .= " WHERE $constraint ";
-    if($default_where) {
-      $sql .= " AND $default_where ";
-    }
-  } elsif($default_where) {
-    $sql .= " WHERE $default_where ";
-  }
-
-  #append additional clauses which may have been defined
-  $sql .= " $final_clause";
-
-  my $sth = $self->prepare($sql);
-
-#  warn $sql;
-
-  $sth->execute;
-  my $obj_list = $self->_objs_from_sth($sth);
-  $sth->finish();
-
-  return $obj_list;
-}
+use base ('Bio::EnsEMBL::Compara::DBSQL::BaseAdaptor');
 
 
 =head2 fetch_by_dbID
@@ -184,26 +105,5 @@ sub store_relation {
   $attribute->member_id($member->dbID);
 
 }
-
-
-=head2 _join
-
-  Arg [1]    : none
-  Example    : none
-  Description: Can be overridden by a subclass to specify any left joins
-               which should occur. The table name specigfied in the join
-               must still be present in the return values of
-  Returntype : a {'tablename' => 'join condition'} pair
-  Exceptions : none
-  Caller     : general
-
-=cut
-
-sub _join {
-  my $self = shift;
-
-  return '';
-}
-
 
 1;
