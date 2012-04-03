4b911ae456c82a6a9de795d2ee12bad4598905c0
Simon Brent <sb23@sanger.ac.uk>
2012-04-03 11:16:00 +0000

    added colour_key. tidied.

--- modules/Bio/EnsEMBL/GlyphSet/P_feature.pm
+++ modules/Bio/EnsEMBL/GlyphSet/P_feature.pm
@@ -4,29 +4,38 @@ use strict;
 
 use base  qw(Bio::EnsEMBL::GlyphSet);
 
+sub colour_key { return $_[1]->analysis->logic_name; }
+
 sub _init {
-  my $self      = shift;
-  my $protein   = $self->{'container'};
+  my $self    = shift;
+  my $protein = $self->{'container'};
+  
   return unless $protein->dbID;
+  
   my $caption   = $self->my_config('caption');
   my $h         = $self->my_config('height') || 4;
   my $y         = 0;
-  foreach my $logic_name ( @{$self->my_config( 'logic_names' )||[]} ) {
-    my $colour = $self->my_colour( lc($logic_name) );
-    my $text   = $self->my_colour( lc($logic_name), 'text', $caption );
+  my $colourmap = $self->{'config'}->colourmap;
+  
+  foreach my $logic_name (@{$self->my_config('logic_names') || []}) {
+    my $colour;
+    
     foreach my $pf (@{$protein->get_all_ProteinFeatures($logic_name)}) {
-      my $x = $pf->start();
+      my $x = $pf->start;
       my $w = $pf->end - $x;
+      
       $self->push($self->Rect({
-        'x'       => $x,
-        'y'       => $y,
-        'width'   => $w,
-        'height'  => $h,
-	'title'   => "$caption; Position: ".$pf->start.'-'.$pf->end,
-        'colour'  => $colour,
+        x      => $x,
+        y      => $y,
+        width  => $w,
+        height => $h,
+        title  => "$caption; Position: " . $pf->start . '-' . $pf->end,
+        colour => $colour ||= $self->get_colour($pf),
       }));
     }
-    $y+= $h + 2; ## slip down a line for subsequent analyses...
+    
+    $y += $h + 2; ## slip down a line for subsequent analyses
   }
 }
+
 1;
