7837756782d3b09f3d5d1d65b7f383a4158fd924
Simon Brent <sb23@sanger.ac.uk>
2012-04-03 12:04:57 +0000

    added href, colour_key, feature_label, canvas_attributes, features functions

--- modules/Bio/EnsEMBL/GlyphSet/chr_band.pm
+++ modules/Bio/EnsEMBL/GlyphSet/chr_band.pm
@@ -14,29 +14,21 @@ sub _init {
   ########## only draw contigs once - on one strand
   
   my ($fontname, $fontsize) = $self->get_font_details('innertext');
-  my $bands      = $self->{'container'}->get_all_KaryotypeBands; # fetch the chromosome bands that cover this VC.
+  my $bands      = $self->features;
   my $h          = [ $self->get_text_width(0, 'X', '', font => $fontname, ptsize => $fontsize) ]->[3];
-  my $pix_per_bp = $self->{'config'}->transform->{'scalex'};
+  my $pix_per_bp = $self->scalex;
   my @t_colour   = qw(gpos25 gpos75);
-  my $chr        = scalar @$bands ? $bands->[0]->slice->seq_region_name : '';
-  my @bands      = sort { $a->start <=> $b->start } @$bands;
+  my $length     = $self->{'container'}->length;
   
-  foreach my $band (@bands) {
-    my $bandname      = $band->name;
-       $bandname      =~ /(\d+)\w?/;
-    my $band_no       = $1;
-    my $start         = $band->start;
-    my $end           = $band->end;
-    my $stain         = $band->stain;
-    my $vc_band_start = $start;
-       $vc_band_start = 1 if $vc_band_start < 1;
-    my $vc_band_end   = $end;
-       $vc_band_end   = $self->{'container'}->length if $vc_band_end > $self->{'container'}->length;
-    my $vc_adjust     = 1 - $self->{'container'}->start;
-    my $band_start    = $start - $vc_adjust;
-    my $band_end      = $end   - $vc_adjust;
-    my $col           = $self->my_colour($stain);
-    my $fontcolour    = $self->my_colour($stain, 'label') || 'black';
+  foreach my $band (@$bands) {
+    my $bandname   = $band->name;
+    my $stain      = $band->stain;
+    my $start      = $band->start;
+    my $end        = $band->end;
+       $start      = 1       if $start < 1;
+       $end        = $length if $end   > $length;
+    my $col        = $self->my_colour($stain);
+    my $fontcolour = $self->my_colour($stain, 'label') || 'black';
     
     if (!$stain) {
       $stain      = shift @t_colour;
@@ -47,24 +39,24 @@ sub _init {
     }
     
     $self->push($self->Rect({
-      x            => $vc_band_start - 1 ,
+      x            => $start - 1 ,
       y            => 0,
-      width        => $vc_band_end - $vc_band_start + 1 ,
+      width        => $end - $start + 1 ,
       height       => $h + 4,
       colour       => $col || 'white',
       absolutey    => 1,
       title        => "Band: $bandname",
-      href         => $self->_url({ r => "$chr:$band_start-$band_end" }),
+      href         => $self->href($band),
       bordercolour => 'black'
     }));
     
     if ($fontcolour ne 'invisible') {
-      my @res = $self->get_text_width(($vc_band_end - $vc_band_start + 1) * $pix_per_bp, $bandname, '', font => $fontname, ptsize => $fontsize);
+      my @res = $self->get_text_width(($end - $start + 1) * $pix_per_bp, $bandname, '', font => $fontname, ptsize => $fontsize);
       
       # only add the lable if the box is big enough to hold it
       if ($res[0]) {
         $self->push($self->Text({
-          x         => ($vc_band_end + $vc_band_start - 1 - $res[2]/$pix_per_bp) / 2,
+          x         => ($end + $start - 1 - $res[2]/$pix_per_bp) / 2,
           y         => 1,
           width     => $res[2] / $pix_per_bp,
           textwidth => $res[2],
@@ -79,15 +71,14 @@ sub _init {
     }
   }
   
-  $self->no_features unless @bands;
+  $self->no_features unless scalar @$bands;
 }
 
 sub render_text {
-  my $self  = shift;
-  my @bands = sort { $a->start <=> $b->start } @{$self->{'container'}->get_all_KaryotypeBands||[]};
+  my $self = shift;
   my $export;
   
-  foreach (@bands) {
+  foreach (@{$self->features}) {
     $export .= $self->_render_text($_, 'Chromosome band', { 
       headers => [ 'name' ], 
       values  => [ $_->name ] 
@@ -96,5 +87,25 @@ sub render_text {
   
   return $export;
 }
+
+sub features {
+  my $self = shift;
+  return [ sort { $a->start <=> $b->start } @{$self->{'container'}->get_all_KaryotypeBands || []} ];
+}
+
+sub href {
+  my ($self, $band) = @_;
+  my $slice = $band->project('toplevel')->[0]->to_Slice;
+  return $self->_url({ r => sprintf('%s:%s-%s', map $slice->$_, qw(seq_region_name start end)) });
+}
+
+sub colour_key { return $_[1]->stain; }
+
+sub feature_label {
+  my ($self, $f) = @_;
+  return $self->my_colour($self->colour_key($f), 'label') eq 'invisible' ? '' : $f->name;
+}
+
+sub canvas_attributes { return ( borderColor => '#000000' ); }
 
 1;
