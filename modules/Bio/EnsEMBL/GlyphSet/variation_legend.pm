8dcf11b555d9e91f74a17dccafc31e343603b29b
Simon Brent <sb23@sanger.ac.uk>
2012-04-03 10:50:54 +0000

    sort by consequence type priority

--- modules/Bio/EnsEMBL/GlyphSet/variation_legend.pm
+++ modules/Bio/EnsEMBL/GlyphSet/variation_legend.pm
@@ -2,85 +2,79 @@ package Bio::EnsEMBL::GlyphSet::variation_legend;
 
 use strict;
 
+use Bio::EnsEMBL::Variation::Utils::Constants;
+
 use base qw(Bio::EnsEMBL::GlyphSet);
 
 sub _init {
-  my ($self) = @_;
+  my $self = shift;
 
-  return unless ($self->strand() == -1);
-
-  my $BOX_WIDTH     = 20;
-  my $NO_OF_COLUMNS = 3;
-
-  my $vc            = $self->{'container'};
-  my $Config        = $self->{'config'}; 
-  my $im_width      = $Config->image_width();
-  my $type          = 'all'; #$Config->get('variation_legend', 'src');
-
-  my @colours;
-  return unless $Config->{'variation_legend_features'};
-  my %features = %{$Config->{'variation_legend_features'}}; 
+  return unless $self->strand == -1;
+  
+  my $config   = $self->{'config'}; 
+  my %features = %{$config->{'variation_legend'} || {}};
+  
   return unless %features;
- 
-  my ($x,$y) = (0,0);
-  my( $fontname, $fontsize ) = $self->get_font_details( 'legend' );
-  my @res = $self->get_text_width( 0, 'X', '', 'font'=>$fontname, 'ptsize' => $fontsize );
-  my $th = $res[3];
-  my $pix_per_bp = $self->{'config'}->transform()->{'scalex'};
-
-  my $FLAG = 0;
-  foreach (sort { $features{$b}->{'priority'} <=> $features{$a}->{'priority'} } keys %features) {
-    @colours = @{$features{$_}->{'legend'}};
-    $y++ unless $x==0;
-    $x=0;
-    while( my ($legend, $colour) = splice @colours, 0, 2 ) {
-      $FLAG = 1;
-      my $tocolour='';
-      ($tocolour,$colour) = ($1,$2) if $colour =~ /(.*):(.*)/;
-      $self->push($self->Rect({
-        'x'         => $im_width * $x/$NO_OF_COLUMNS,
-        'y'         => $y * ( $th + 3 ) + 2,
-        'width'     => $BOX_WIDTH,
-        'height'    => $th-2,
-        $tocolour.'colour'    => $colour,
-        'absolutey' => 1,
-        'absolutex' => 1,'absolutewidth'=>1,
-      }));
-      $self->push($self->Text({
-        'x'         => $im_width * $x/$NO_OF_COLUMNS + $BOX_WIDTH + 3,
-        'y'         => $y * ( $th + 3 ),
-        'height'    => $th,
-        'valign'    => 'center',
-        'halign'    => 'left',
-        'ptsize'    => $fontsize,
-        'font'      => $fontname,
-        'colour'    => 'black',
-        'text'      => " $legend",
-        'absolutey' => 1,
-        'absolutex' => 1,'absolutewidth'=>1
-      }));
-      $x++;
-      if($x==$NO_OF_COLUMNS) {
-        $x=0;
-        $y++;
-      }
+  
+  my $im_width              = $config->image_width;
+  my $pix_per_bp            = $config->transform->{'scalex'};
+  my ($fontname, $fontsize) = $self->get_font_details('legend');
+  my $text_height           = [ $self->get_text_width(0, 'X', '', font => $fontname, ptsize => $fontsize) ]->[3];
+  my %labels                = map { $_->display_term => [ $_->rank, $_->label ] } values %Bio::EnsEMBL::Variation::Utils::Constants::OVERLAP_CONSEQUENCES;
+  my $type                  = 'all';
+  my $box_width             = 20;
+  my $cols                  = 3;
+  my ($x, $y)               = (0, 0);
+  
+  foreach (sort { $labels{$a}[0] <=> $labels{$b}[0] } keys %features) {
+    my $colour = $features{$_};
+    my $tocolour;
+    
+    ($tocolour, $colour) = ($1, $2) if $colour =~ /(.*):(.*)/;
+    
+    $self->push($self->Rect({
+      x                   => $im_width * $x / $cols,
+      y                   => $y * ($text_height + 3) + 2,
+      width               => $box_width,
+      height              => $text_height - 2,
+      "${tocolour}colour" => $colour,
+      absolutey           => 1,
+      absolutex           => 1,
+      absolutewidth       => 1,
+    }));
+    
+    $self->push($self->Text({
+      x             => $im_width * $x / $cols + $box_width + 3,
+      y             => $y * ($text_height + 3),
+      height        => $text_height,
+      valign        => 'center',
+      halign        => 'left',
+      ptsize        => $fontsize,
+      font          => $fontname,
+      colour        => 'black',
+      text          => " $labels{$_}[1]",
+      absolutey     => 1,
+      absolutex     => 1,
+      absolutewidth => 1
+    }));
+    
+    if (++$x == $cols) {
+      $x = 0;
+      $y++;
     }
   }
-# Set up a separating line...
-  my $rect = $self->Rect({
-    'x'         => 0,
-    'y'         => 0,
-    'width'     => $im_width,
-    'height'    => 0,
-    'colour'    => 'grey50',
-    'absolutey' => 1,
-    'absolutex' => 1,'absolutewidth'=>1,
-  });
-  $self->push($rect);
-  unless( $FLAG ) {
-    $self->errorTrack( "No SNPs in this panel" );
-  }
+  
+  # separating line
+  $self->push($self->Rect({
+    x             => 0,
+    y             => 0,
+    width         => $im_width,
+    height        => 0,
+    colour        => 'grey50',
+    absolutey     => 1,
+    absolutex     => 1,
+    absolutewidth => 1,
+  }));
 }
 
 1;
-      
