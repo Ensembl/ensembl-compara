e6894f68feecda27e899780efde1608b31827f79
Simon Brent <sb23@sanger.ac.uk>
2012-04-03 12:08:04 +0000

    tidied. added feature_label, colour_key, features, href, canvas_attributes functions

--- modules/Bio/EnsEMBL/GlyphSet/_marker.pm
+++ modules/Bio/EnsEMBL/GlyphSet/_marker.pm
@@ -4,131 +4,141 @@ use strict;
 
 use base qw(Bio::EnsEMBL::GlyphSet);
 
-our $MAP_WEIGHT = 2;
-our $PRIORITY   = 50;
+sub feature_label { return $_[1]->{'drawing_id'}; }
+sub colour_key    { return lc $_[1]->marker->type; }
 
 sub _init {
   my $self = shift;
-
+  
+  return unless $self->strand == -1;
   return $self->render_text if $self->{'text_export'};
   
-  my $slice         = $self->{'container'};
-  my $Config        = $self->{'config'};
-
-  $self->_init_bump(); ## Initialize bumping (set max depth to "infinity"! 
-
-  my $L             = $slice->length();
-  my $pix_per_bp    = $Config->transform->{'scalex'};
-
-  return unless $self->strand() == -1;
-
-  my( $fontname, $fontsize ) = $self->get_font_details( 'outertext' );
-  my @res = $self->get_text_width( 0, 'X', '', 'font'=>$fontname, 'ptsize' => $fontsize );
-  my $h = $res[3];
-
-  my $row_height     = 8;
-
-  my $labels         = ($self->my_config('labels' ) ne 'off') && ($L<1e7);
-  if( $L > 5e7 ) {
-    $self->errorTrack( "Markers only displayed for less than 50Mb.");
+  $self->_init_bump;
+  
+  my $slice  = $self->{'container'};
+  my $length = $slice->length;
+  
+  if ($length > 5e7) {
+    $self->errorTrack('Markers only displayed for less than 50Mb.');
     return;
   }
-
-  my $priority       = $self->my_config( 'priority' );
-
-  my $previous_start = $L + 1e10;
-  my $previous_end   = -1e10;
-  my $previous_fid   = '';
-
-  my @features = @{$slice->get_all_MarkerFeatures(undef,$priority,$MAP_WEIGHT)};
-
-  ## Force drawing of specific marker regardless of weight
-  if (my $marker_id = $self->my_config('marker_id')) {
-    my @extra_features = @{$slice->get_MarkerFeatures_by_Name($marker_id)};
-    push @features, @extra_features;
-  }
-  my $base_url = $self->_url( { 'action' => 'Marker' } );
   
-  foreach my $f (sort { $a->seq_region_start <=> $b->seq_region_start } @features){
-    my $ms   = $f->marker->display_MarkerSynonym;
-    my $fid  = $ms ? $ms->name : '';
-      ($fid) = grep { $_ ne '-' } map { $_->name } @{$f->marker->get_all_MarkerSynonyms||[]} if $fid eq '-' || $fid eq '';
+  my $pix_per_bp     = $self->scalex;
+  my %font_params    = $self->get_font_details('outertext', 1);
+  my $text_height    = [$self->get_text_width(0, 'X', '', %font_params)]->[3];
+  my $labels         = $self->my_config('labels') ne 'off' && $length < 1e7;
+  my $row_height     = 8;
+  my $previous_start = $length + 1e10;
+  my $previous_end   = -1e10;
+  my $previous_id    = '';
+  my $features       = $self->features;
+  
+  foreach my $f (@$features) {
+    my $id = $f->{'drawing_id'};
 
     ## Remove duplicates
-    next if $fid == $previous_fid && $f->start == $previous_start && $f->end == $previous_end;
+    next if $id == $previous_id && $f->start == $previous_start && $f->end == $previous_end;
 
-    my $feature_colour = $self->my_colour( $f->marker->type );
-    my $zmenu = {
-    	'type'   => 'Location',
-    	'action' => 'Marker',
-    	'm'      => $fid,
-    };
-
-    my $S = $f->start()-1; next if $S>$L; $S = 0 if $S<0;
-    my $E = $f->end()    ; next if $E<0;  $E = $L if $E>$L;
+    my $feature_colour = $self->my_colour($self->colour_key($f));
+    my $start          = $f->start - 1;
+    my $end            = $f->end;
+    
+    next if $start > $length || $end < 0;
+    
+    $start = 0       if $start < 0;
+    $end   = $length if $end > $length;
 
     # Draw feature
-    unless( $slice->strand < 0 ? $previous_start - $S < 0.5/$pix_per_bp : $E - $previous_end < 0.5/$pix_per_bp ) {
-      $self->push( $self->Rect({
-        'x' => $S,
-        'y' => 0,
-        'height' => $row_height, 
-        'width' => ($E-$S),
-        'colour' => $feature_colour, 
-        'absolutey' => 1,
-        'href' => $self->_url($zmenu)
+    unless ($slice->strand < 0 ? $previous_start - $start < 0.5 / $pix_per_bp : $end - $previous_end < 0.5 / $pix_per_bp) {
+      $self->push($self->Rect({
+        x         => $start,
+        y         => 0,
+        height    => $row_height, 
+        width     => $end - $start,
+        colour    => $feature_colour, 
+        absolutey => 1,
+        href      => $self->href($f)
       }));
-      $previous_end   = $E;
-      $previous_start = $E;
+      
+      $previous_end   = $end;
+      $previous_start = $end;
     }
-    $previous_fid = $fid;
+    
+    $previous_id = $id;
+    
     next unless $labels;
-    my @res = $self->get_text_width( 0, $fid, '', 'font'=>$fontname, 'ptsize' => $fontsize );
+    
+    my $text_width = [$self->get_text_width(0, $id, '', %font_params)]->[2];
+    
     my $glyph = $self->Text({
-      'x'         => $S,
-      'y'         => $row_height,
-      'height'    => $h,
-      'width'     => $res[2] / $pix_per_bp,
-      'halign'    => 'left',
-      'font'      => $fontname,
-      'ptsize'    => $fontsize,
-      'colour'    => $feature_colour,
-      'absolutey' => 1,
-      'text'      => $fid,
-      'href'      => $self->_url($zmenu),
+      x         => $start,
+      y         => $row_height,
+      height    => $text_height,
+      width     => $text_width / $pix_per_bp,
+      halign    => 'left',
+      colour    => $feature_colour,
+      absolutey => 1,
+      text      => $id,
+      href      => $self->href($f),
+      %font_params
     });
 
-    my $bump_start = int($glyph->x() * $pix_per_bp);
+    my $bump_start = int($glyph->x * $pix_per_bp);
        $bump_start = 0 if $bump_start < 0;
-    my $bump_end = $bump_start + $res[2];
-    my $row = $self->bump_row( $bump_start, $bump_end, 1 ); # don't display if falls off RHS.. 
-    next if $row < 0;
-    $glyph->y($glyph->y() + (1.2 * $row * $h));
+    my $bump_end   = $bump_start + $text_width;
+    my $row        = $self->bump_row($bump_start, $bump_end, 1);
+    
+    next if $row < 0; # don't display if falls off RHS
+    
+    $glyph->y($glyph->y + (1.2 * $row * $text_height));
     $self->push($glyph);
-  }    
-  ## No features show "empty track line" if option set....  ##
-  if( (scalar(@features) == 0 ) && $Config->get_option('opt_empty_tracks') == 1) {
-    $self->errorTrack( "No markers in this region" )
   }
+  
+  ## No features show "empty track line" if option set
+  $self->errorTrack('No markers in this region') if !scalar @$features && $self->{'config'}->get_option('opt_empty_tracks') == 1;
 }
 
 sub render_text {
   my $self = shift;
+  return join '', map $self->_render_text($_, 'Marker', { headers => [ 'id' ], values => [ $_->{'drawing_id'} ] }), @{$self->features};
+}
 
-  return unless $self->strand == -1;
+sub features {
+  my $self  = shift;
+  my $slice = $self->{'container'};
+  my @features;
   
-  my $export;
-  
-  foreach my $f (sort { $a->seq_region_start <=> $b->seq_region_start } @{$self->{'container'}->get_all_MarkerFeatures}) {
-    my $ms = $f->marker->display_MarkerSynonym;
-    my $fid = $ms ? $ms->name : '';
-    
-    ($fid) = grep { $_ ne '-' } map { $_->name } @{$f->marker->get_all_MarkerSynonyms||[]} if $fid eq '-' || $fid eq '';    
-    
-    $export .= $self->_render_text($f, 'Marker', { 'headers' => [ 'id' ], 'values' => [ $fid ] });
+  if ($self->{'text_export'}) {
+    @features = @{$slice->get_all_MarkerFeatures};
+  } else {
+    my $priority   = $self->my_config('priority');
+    my $marker_id  = $self->my_config('marker_id');
+    my $map_weight = 2;
+       @features   = (@{$slice->get_all_MarkerFeatures(undef, $priority, $map_weight)}, $marker_id ? @{$slice->get_MarkerFeatures_by_Name($marker_id)} : ()); ## Force drawing of specific marker regardless of weight
   }
   
-  return $export;
+  foreach my $f (@features) {
+    my $ms  = $f->marker->display_MarkerSynonym;
+    my $id  = $ms ? $ms->name : '';
+      ($id) = grep $_ ne '-', map $_->name, @{$f->marker->get_all_MarkerSynonyms || []} if $id eq '-' || $id eq '';
+    
+    $f->{'drawing_id'} = $id;
+  }
+  
+  return [ sort { $a->seq_region_start <=> $b->seq_region_start } @features ];
 }
+
+sub href {
+  my ($self, $f) = @_;
+  
+  return $self->_url({
+    species => $self->species,
+    type    => 'Location',
+    action  => 'Marker',
+    m       => $f->{'drawing_id'},
+  });
+}
+
+sub canvas_attributes { return ( id => $_[1]->{'drawing_id'} ); }
 
 1;
