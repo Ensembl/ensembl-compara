64fd937b60842eb9d2f6678719c2549c759d5237
Simon Brent <sb23@sanger.ac.uk>
2012-04-03 12:09:16 +0000

    added colour_key. tidied.

--- modules/Bio/EnsEMBL/GlyphSet/P_domain.pm
+++ modules/Bio/EnsEMBL/GlyphSet/P_domain.pm
@@ -2,117 +2,125 @@ package Bio::EnsEMBL::GlyphSet::P_domain;
 
 use strict;
 
-no warnings 'uninitialized';
-
 use base qw(Bio::EnsEMBL::GlyphSet);
 
 ## Variables defined in UserConfig.pm 
 ## 'caption'   -> Track label
 ## 'logicname' -> Logic name
 
+sub colour_key { return $_[1]->analysis->logic_name; }
+
 sub _init {
-  my ($self) = @_;
+  my $self = shift;
   
   return $self->render_text if $self->{'text_export'};
   
-  my $protein       = $self->{'container'};
+  my $protein = $self->{'container'};
 
   $self->_init_bump;
 
-  my $label         = $self->my_config( 'caption'    );
-  my $depth         = $self->my_config( 'depth'      );
-  my $h             = $self->my_config( 'height'     ) || 4;
-  my $font_details  = $self->get_text_simple( undef, 'innertext' );
-  my $pix_per_bp    = $self->scalex;
+  my $label        = $self->my_config('caption');
+  my $depth        = $self->my_config('depth');
+  my $h            = $self->my_config('height') || 4;
+  my $font_details = $self->get_text_simple(undef, 'innertext');
+  my $pix_per_bp   = $self->scalex;
 
-  foreach my $logic_name ( @{$self->my_config( 'logic_names' )||[]} ) {
-    my %hash;
-    my @ps_feat = @{$protein->get_all_ProteinFeatures( $logic_name )};
-    push @{$hash{$_->hseqname}},$_ foreach @ps_feat;
-
-    my $colour = $self->my_colour( lc($logic_name) );
+  foreach my $logic_name (@{$self->my_config('logic_names') || []}) {
+    my (%hash, $colour);
+    
+    push @{$hash{$_->hseqname}}, $_ for @{$protein->get_all_ProteinFeatures($logic_name)};
+    
     foreach my $key (keys %hash) {
-      my( @rect, $prsave, $minx, $maxx );
+      my (@rect, $prsave, $minx, $maxx);
+      
       foreach my $pr (@{$hash{$key}}) {
-        my $x  = $pr->start();
-        $minx  = $x if ($x < $minx || !defined($minx));
-        my $w  = $pr->end() - $x;
-        $maxx  = $pr->end() if ($pr->end() > $maxx || !defined($maxx));
-        my $id = $pr->hseqname();
+        my $x    = $pr->start;
+           $minx = $x if $x < $minx || !defined $minx;
+        my $w    = $pr->end - $x;
+           $maxx = $pr->end if $pr->end > $maxx || !defined $maxx;
+        my $id   = $pr->hseqname;
+        
         push @rect, $self->Rect({
-          'x'        => $x,
-          'y'        => 0,
-          'width'    => $w,
-          'height'   => $h,
-          'colour'   => $colour,
+          x      => $x,
+          y      => 0,
+          width  => $w,
+          height => $h,
+          colour => $colour ||= $self->get_colour($pr),
         });
+        
         $prsave ||= $pr;
       }
-      my $title =  sprintf '%s %s; Positions: %d-%d', $label, $key, $minx, $maxx;
-         $title .= '; Interpro: '. $prsave->interpro_ac if $prsave->interpro_ac;
-         $title .= '; '.$prsave->idesc                  if $prsave->idesc;
-      my $dbID = $prsave->dbID;
-      my $Composite = $self->Composite({
-        'x'     => $minx,
-        'y'     => 0,
-        'href'  => $self->_url({ 'type' => 'Transcript', 'action' => 'ProteinSummary', 'pf_id' => $dbID }),
-        'title' => $title
+      
+      my $title  = "$label $key; Positions: $minx-$maxx";
+         $title .= '; Interpro: ' . $prsave->interpro_ac if $prsave->interpro_ac;
+         $title .= '; '. $prsave->idesc                  if $prsave->idesc;
+      my $dbID   = $prsave->dbID;
+      
+      my $composite = $self->Composite({
+        x     => $minx,
+        y     => 0,
+        href  => $self->_url({ type => 'Transcript', action => 'ProteinSummary', pf_id => $dbID }),
+        title => $title
       });
-      $Composite->push(@rect,
+      
+      $composite->push(@rect,
         $self->Rect({
-          'x'        => $minx,
-          'y'        => $h/2,
-          'width'    => $maxx - $minx,
-          'height'   => 0,
-          'colour'   => $colour,
-          'absolutey' => 1,
+          x         => $minx,
+          y         => $h / 2,
+          width     => $maxx - $minx,
+          height    => 0,
+          colour    => $colour,
+          absolutey => 1,
         })
       );
-    #### add a label
-      my $desc = $prsave->idesc() || $key;
-      my @res = $self->get_text_width( 0, $desc, '', 'font'=>$font_details->{'font'}, 'ptsize' => $font_details->{'fontsize'} );
-      $Composite->push($self->Text({
-        'font'   => $font_details->{'font'},
-        'ptsize' => $font_details->{'fontsize'},
-        'halign' => 'left',
-        'text'   => $desc,
-        'x'      => $Composite->x(),
-        'y'      => $h,
-        'height' => $font_details->{'height'},
-        'width'  => $res[2]/$pix_per_bp,
-        'colour' => $colour,
-        'absolutey' => 1
+      
+      #### add a label
+      my $desc = $prsave->idesc || $key;
+      my @res  = $self->get_text_width(0, $desc, '', font => $font_details->{'font'}, ptsize => $font_details->{'fontsize'});
+      
+      $composite->push($self->Text({
+        font      => $font_details->{'font'},
+        ptsize    => $font_details->{'fontsize'},
+        halign    => 'left',
+        text      => $desc,
+        x         => $composite->x,
+        y         => $h,
+        height    => $font_details->{'height'},
+        width     => $res[2] / $pix_per_bp,
+        colour    => $colour,
+        absolutey => 1
       }));
-
-      if($depth>0) {
-        my $bump_start = int($Composite->x() * $pix_per_bp);
-        my $bump_end   = $bump_start + int( $Composite->width / $pix_per_bp );
-        my $row        = $self->bump_row( $bump_start, $bump_end );
-        $Composite->y( $Composite->y + ( $row * ( 4 + $h + $font_details->{'height'}))) if $row;
+      
+      if ($depth > 0) {
+        my $bump_start = int($composite->x * $pix_per_bp);
+        my $bump_end   = $bump_start + int($composite->width / $pix_per_bp);
+        my $row        = $self->bump_row($bump_start, $bump_end);
+        
+        $composite->y($composite->y + ($row * (4 + $h + $font_details->{'height'}))) if $row;
       }
-      $self->push($Composite);
+      
+      $self->push($composite);
     }
   }
 }
 
 sub render_text {
-  my $self = shift;
-  
+  my $self      = shift;
   my $container = $self->{'container'};
-  my $label = $self->my_config('caption');
+  my $label     = $self->my_config('caption');
   my $export;
   
-  foreach my $logic_name (@{$self->my_config('logic_names')||[]}) {
+  foreach my $logic_name (@{$self->my_config('logic_names') || []}) {
     my @features = map { $_->[1] } sort { $a->[0] cmp $b->[0] } map { [ $_->hseqname, $_ ] } @{$container->get_all_ProteinFeatures($logic_name)};
     
     foreach (@features) {
       my $analysis = $_->analysis;
       
       $export .= $self->_render_text($_, $analysis->gff_feature, { 
-         'headers' => [ 'id', 'description' ],
-         'values'  => [ $_->hseqname, $_->idesc ]
+        headers => [ 'id', 'description' ],
+        values  => [ $_->hseqname, $_->idesc ]
       }, {
-        'source'  => $analysis->gff_source,
+        source  => $analysis->gff_source,
       });
     }
   }
