690699e427b2d7abb1e396f6039a85259610e944
Matthieu Muffato <muffato@ebi.ac.uk>
2012-04-12 09:25:11 +0000

    Now compliant with member.source_name, better reuse of the transcripts, stores the new subset entries

--- scripts/pipeline/convert_patch_to_compara_homologies.pl
+++ scripts/pipeline/convert_patch_to_compara_homologies.pl
@@ -9,6 +9,8 @@ use Bio::EnsEMBL::Slice;
 use Bio::EnsEMBL::DBSQL::DBAdaptor;
 use Bio::EnsEMBL::DBSQL::SliceAdaptor;
 
+use Bio::EnsEMBL::Compara::Subset;
+use Bio::EnsEMBL::Compara::MethodLinkSpeciesSet;
 use Bio::EnsEMBL::Compara::DBSQL::DBAdaptor;
 use Bio::EnsEMBL::Compara::RunnableDB::LoadMembers;
 
@@ -27,6 +29,7 @@ my $of_dbname = '';
 my $comp_url = '';
 my $species_name = '';
 
+my $no_store = 0;
 
 my $description = q'
 	PROGRAM: convert_patch_to_compara_homologies.pl
@@ -58,6 +61,8 @@ unless(@ARGV){
 
         'comp_url:s'    => \$comp_url,
         'species:s'     => \$species_name,
+
+        'no_store:i'    => \$no_store,
         );
 
 unless(defined $core_host && defined $of_host && defined $comp_url) {
@@ -80,24 +85,32 @@ my $of_db = new Bio::EnsEMBL::DBSQL::DBAdaptor( -dnadb  => $core_db,
         -dbname => $of_dbname );
 
 # This needs to be run first
-# INSERT INTO method_link VALUES (205, 'ENSEMBL_PROJECTIONS', 'Homology.homology');
-# INSERT INTO method_link_species_set VALUES(28937, 205, 32416, 'H.sap patch projections', 'ensembl', '');
 # ALTER TABLE member   AUTO_INCREMENT=300000001
 # ALTER TABLE sequence AUTO_INCREMENT=300000001
-# ALTER TABLE homology AUTO_INCREMENT=200000001
-
-# not necessary
-# ALTER TABLE member MODIFY COLUMN `source_name` enum('ENSEMBLGENE','ENSEMBLPEP','Uniprot/SPTREMBL','Uniprot/SWISSPROT','ENSEMBLTRANS','EXTERNALCDS','ENSEMBLPROJ') NOT NULL;
-# ALTER TABLE homology MODIFY COLUMN `description` enum('ortholog_one2one','apparent_ortholog_one2one','ortholog_one2many','ortholog_many2many','within_species_paralog','other_paralog','putative_gene_split','contiguous_gene_split','between_species_paralog','possible_ortholog','UBRH','BRH','MBRH','RHS','projection_unchanged','projection_altered') DEFAULT NULL;
+# ALTER TABLE subset   AUTO_INCREMENT=300000001
+# ALTER TABLE homology AUTO_INCREMENT=300000001
 
 my $compara_dba = new Bio::EnsEMBL::Compara::DBSQL::DBAdaptor( -url => $comp_url );
 my $member_adaptor = $compara_dba->get_MemberAdaptor();
 my $homology_adaptor = $compara_dba->get_HomologyAdaptor();
 my $human_genome_db = $compara_dba->get_GenomeDBAdaptor()->fetch_by_name_assembly($species_name);
-my $mlss_id = $compara_dba->get_MethodLinkSpeciesSetAdaptor()->fetch_by_method_link_type_GenomeDBs('ENSEMBL_PROJECTIONS', [$human_genome_db])->dbID;
+
+# MLSS
+my $mlss = new Bio::EnsEMBL::Compara::MethodLinkSpeciesSet(
+    -method => $compara_dba->get_MethodAdaptor->fetch_by_type('ENSEMBL_PROJECTIONS'),
+    -species_set_obj => $compara_dba->get_SpeciesSetAdaptor->fetch_by_GenomeDBs([$human_genome_db]),
+);
+$compara_dba->get_MethodLinkSpeciesSetAdaptor->store($mlss);
+
+# Subsets
+my $subset_peps  = Bio::EnsEMBL::Compara::Subset->new(-name => sprintf("gdb:%d %s projected canonical translations", $human_genome_db->dbID, $human_genome_db->name) );
+my $subset_genes = Bio::EnsEMBL::Compara::Subset->new(-name => sprintf("gdb:%d %s projected canonical genes",        $human_genome_db->dbID, $human_genome_db->name) );
+$compara_dba->get_SubsetAdaptor->store($subset_peps) unless $no_store;
+$compara_dba->get_SubsetAdaptor->store($subset_genes) unless $no_store;
 
 print "FOUND genome_db ", $human_genome_db->dbID, "\n";
-print "FOUND mlss_id $mlss_id\n";
+print "FOUND/STORED mlss ", $mlss->dbID, "\n";
+print "FOUND/STORED subsets ", $subset_peps->dbID, " ", $subset_genes->dbID, "\n";
 
 #get adaptors
 my $of_ga = $of_db->get_GeneAdaptor();
@@ -133,6 +146,84 @@ my $count_orig_trans = 0;
 my $count_proj_gene = 0;
 my $count_proj_trans = 0;
 my $count_homology = 0;
+
+my %compara_to_core;
+
+sub fetch_or_store_gene {
+    my $gene = shift;
+    my $gene_member = $member_adaptor->fetch_by_source_stable_id('ENSEMBLGENE', $gene->stable_id);
+    if (defined $gene_member) {
+        print "REUSE: $gene_member "; $gene_member->print_member();
+    } else {
+        $gene_member = Bio::EnsEMBL::Compara::Member->new_from_gene(-gene=>$gene, -genome_db=>$human_genome_db);
+        print "NEW: $gene_member "; $gene_member->print_member();
+        $member_adaptor->store($gene_member) unless $no_store;
+        $subset_genes->add_member($gene_member);
+        my $counter = shift;
+        ${$counter} ++;
+    }
+    $compara_to_core{$gene_member} = $gene;
+    return $gene_member;
+}
+ 
+
+sub fetch_or_store_transcript {
+    my $transcript = shift;
+
+    my $trans_member = $member_adaptor->fetch_by_source_stable_id('ENSEMBLTRANS', $transcript->stable_id);
+       $trans_member = $member_adaptor->fetch_by_source_stable_id('ENSEMBLPEP', $transcript->translation->stable_id) unless $trans_member or not $transcript->translation;
+    if (defined $trans_member) {
+        print "REUSE: $trans_member ";  $trans_member->print_member();
+    } else {
+        my $gene_member = shift;
+        my $gene = shift;
+        $trans_member = Bio::EnsEMBL::Compara::Member->new_from_transcript(
+                -transcript     => $transcript,
+                -genome_db      => $human_genome_db,
+                -description    => Bio::EnsEMBL::Compara::RunnableDB::LoadMembers::fasta_description(undef, $gene, $transcript),
+                -translate      => ($transcript->translation ? 'yes' : 'ncrna'),
+                );
+        $trans_member->gene_member($gene_member);
+        print "NEW: $trans_member "; $trans_member->print_member();
+        $member_adaptor->store($trans_member) unless $no_store;
+        $member_adaptor->store_gene_peptide_link($gene_member->dbID, $trans_member->dbID) unless $no_store;
+        my $counter = shift;
+        ${$counter} ++;
+    }
+    $compara_to_core{$trans_member} = $transcript;
+    return $trans_member;
+}
+
+
+sub fetch_or_store_homology {
+    my $trans_member1 = shift;
+    my $trans_member2 = shift;
+    my $homology_type = shift;
+
+    my $homology = new Bio::EnsEMBL::Compara::Homology;
+    $homology->description($homology_type);
+    $homology->subtype('');
+    $homology->ancestor_node_id(0);
+    $homology->tree_node_id(0);
+    $homology->method_link_species_set_id($mlss->dbID);
+
+    my $attribute;
+    $attribute = new Bio::EnsEMBL::Compara::Attribute;
+    $attribute->peptide_member_id($trans_member1->dbID);
+    $homology->add_Member_Attribute([$trans_member1->gene_member, $attribute]);
+
+    $attribute = new Bio::EnsEMBL::Compara::Attribute;
+    $attribute->peptide_member_id($trans_member2->dbID);
+    $homology->add_Member_Attribute([$trans_member2->gene_member, $attribute]);
+
+    print "NEW: $homology "; $homology->print_homology();
+    $homology_adaptor->store($homology) unless $no_store;
+    $count_homology ++;
+
+    return $homology;
+}
+
+
 
 #work out the relationships
 foreach my $proj_gene (@projected_genes){
@@ -172,82 +263,19 @@ TRANSCRIPT:
                 #print $proj_transcript->stable_id()." ".$feat_pair->hseqname."\n";
 
                 my $orig_gene = $core_ga->fetch_by_transcript_stable_id($orig_transcript_id);
-
-                # Create the original gene member if necessary
-                my $orig_gene_member = $member_adaptor->fetch_by_source_stable_id('ENSEMBLGENE', $orig_gene->stable_id);
-                if (defined $orig_gene_member) {
-                    print "REUSE: $orig_gene_member "; $orig_gene_member->print_member();
-                } else {
-                    $orig_gene_member = Bio::EnsEMBL::Compara::Member->new_from_gene(-gene=>$orig_gene, -genome_db=>$human_genome_db);
-                    print "NEW: $orig_gene_member "; $orig_gene_member->print_member();
-                    $member_adaptor->store($orig_gene_member);
-                    $count_orig_gene ++;
-                }
-
-                # Create the patch gene member if necessary
-                my $proj_gene_member = $member_adaptor->fetch_by_source_stable_id('ENSEMBLPROJ', $proj_gene->stable_id);
-                if (defined $proj_gene_member) {
-                    print "REUSE: $proj_gene_member "; $proj_gene_member->print_member();
-                } else {
-                    $proj_gene_member = Bio::EnsEMBL::Compara::Member->new_from_gene(-gene=>$proj_gene, -genome_db=>$human_genome_db);
-                    $proj_gene_member->source_name('ENSEMBLPROJ');
-                    print "NEW: $proj_gene_member "; $proj_gene_member->print_member();
-                    $member_adaptor->store($proj_gene_member);
-                    $count_proj_gene ++;
-                }
-
                 my $orig_transcript = $core_ta->fetch_by_stable_id($orig_transcript_id);
 
+                # Create the original gene member if necessary
+                my $orig_gene_member = fetch_or_store_gene($orig_gene, \$count_orig_gene);
                 # Create the original transcript member if necessary
-                my $orig_trans_member =  $member_adaptor->fetch_by_source_stable_id('ENSEMBLTRANS', $orig_transcript_id);
-                if (defined $orig_trans_member) {
-                    print "REUSE: $orig_trans_member ";  $orig_trans_member->print_member();
-                } else {
-                    $orig_trans_member = Bio::EnsEMBL::Compara::Member->new_from_transcript(-transcript=>$orig_transcript, -genome_db=>$human_genome_db, -description=>Bio::EnsEMBL::Compara::RunnableDB::LoadMembers::fasta_description(undef, $orig_gene, $orig_transcript), -translate=>'ncrna');
-                    print "NEW: $orig_trans_member "; $orig_trans_member->print_member();
-                    $member_adaptor->store($orig_trans_member);
-                    $member_adaptor->store_gene_peptide_link($orig_gene_member->dbID, $orig_trans_member->dbID);
-                    $count_orig_trans ++;
-                }
-
+                my $orig_trans_member = fetch_or_store_transcript($orig_transcript, $orig_gene_member, $orig_gene, \$count_orig_trans);
+                # Create the patch gene member if necessary
+                my $proj_gene_member = fetch_or_store_gene($proj_gene, \$count_proj_gene);
                 # Create the patch transcript member if necessary
-                my $proj_trans_member = $member_adaptor->fetch_by_source_stable_id('ENSEMBLTRANS', $proj_transcript->stable_id);
-                if (defined $proj_trans_member) {
-                    print "REUSE: $proj_trans_member ";  $proj_trans_member->print_member();
-                } else {
-                    $proj_trans_member = Bio::EnsEMBL::Compara::Member->new_from_transcript(-transcript=>$proj_transcript, -genome_db=>$human_genome_db, -description=>Bio::EnsEMBL::Compara::RunnableDB::LoadMembers::fasta_description(undef, $proj_gene, $proj_transcript), -translate=>'ncrna');
-                    print "NEW: $proj_trans_member "; $proj_trans_member->print_member;
-                    $member_adaptor->store($proj_trans_member);
-                    $member_adaptor->store_gene_peptide_link($proj_gene_member->dbID, $proj_trans_member->dbID);
-                    $count_proj_trans ++;
-                }
+                my $proj_trans_member = fetch_or_store_transcript($proj_transcript, $proj_gene_member, $proj_gene, \$count_proj_trans);
 
                 # Create the homology link id necessary
-                my $stored_homology = $homology_adaptor->fetch_by_Member_id_Member_id($orig_gene_member->dbID, $proj_gene_member->dbID);
-                if (defined $stored_homology) {
-                    print "CONFLICT: $stored_homology "; $stored_homology->print_homology;
-                } else {
-                    my $homology = new Bio::EnsEMBL::Compara::Homology;
-                    $homology->description($homology_type);
-                    $homology->subtype('');
-                    $homology->ancestor_node_id(0);
-                    $homology->tree_node_id(0);
-                    $homology->method_link_species_set_id($mlss_id);
-                    
-                    my $attribute;
-                    $attribute = new Bio::EnsEMBL::Compara::Attribute;
-                    $attribute->peptide_member_id($orig_trans_member->dbID);
-                    $homology->add_Member_Attribute([$orig_gene_member, $attribute]);
-
-                    $attribute = new Bio::EnsEMBL::Compara::Attribute;
-                    $attribute->peptide_member_id($proj_trans_member->dbID);
-                    $homology->add_Member_Attribute([$proj_gene_member, $attribute]);
-
-                    print "NEW: $homology "; $homology->print_homology();
-                    $homology_adaptor->store($homology);
-                    $count_homology ++;
-                }
-
+                #fetch_or_store_homology($orig_trans_member, $proj_trans_member, $homology_type);
                 print $proj_transcript->stable_id." ".$orig_transcript_id." ".$patch_type." ".$alt_seq."\n";
 
                 next TRANSCRIPT;
@@ -258,10 +286,21 @@ TRANSCRIPT:
 
 print "total transcripts fetched: ".$transcript_count."\n";
 
+# canonical translations
+my $count_can_trans = 0;
+foreach my $gene_member (@{$subset_genes->member_list}) {
+    print "CANONICAL ", $gene_member->stable_id, " ";
+    my $can_trans = $compara_to_core{$gene_member}->canonical_transcript;
+    my $can_member = fetch_or_store_transcript($can_trans, $gene_member, $compara_to_core{$gene_member}, \$count_can_trans);
+    $subset_peps->add_member($can_member);
+}
+
+
 print "new compara entries:\n";
 print $count_orig_gene, " ref genes\n";
 print $count_orig_trans, " ref transcripts\n";
 print $count_proj_gene, " projected genes\n";
-print $count_proj_trans, " projected trasncripts\n";
+print $count_proj_trans, " projected transcripts\n";
 print $count_homology, " new homologies\n";
+print $count_can_trans, " new canonical transcripts\n";
 
