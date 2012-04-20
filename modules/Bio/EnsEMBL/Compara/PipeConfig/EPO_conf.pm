8c04f885b3ed480c012e526895c844ffac3b4573
ens-stephenf <stephenf@ebi.ac.uk>
2012-04-20 10:15:42 +0000

    A species_tree is now generated from the ~ensembl-compara/scripts/pipeline/species_tree_blength.nh file and added to the method_link_species_set_tag table

--- modules/Bio/EnsEMBL/Compara/PipeConfig/EPO_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/EPO_conf.pm
@@ -14,12 +14,15 @@ sub default_options {
 
         'pipeline_name' => 'compara_EPO',
 
+	   # version number of core dbs to access for sequence
+	'core_db_version' => 67,
+
 	   # parameters that are likely to change from execution to another:
-	'release'               => '67',
+	'release'               => '101',
 	'rel_suffix'            => '',    # an empty string by default, a letter otherwise
 	   # dependent parameters:
 	'rel_with_suffix'       => $self->o('release').$self->o('rel_suffix'),
-	'species_tag' 		=> '12mammals_',
+	'species_tag' 		=> 'test2_13mammals_',
 
 	   # connection parameters to various databases:
 	'pipeline_db' => { # the production database itself (will be created)
@@ -44,7 +47,7 @@ sub default_options {
 		-port => 3306,
 		-pass => '',
 		-host => 'compara3',
-		-dbname => 'sf5_compara_template_anchor_align',
+		-dbname => 'sf5_epo_test_anc_template_101',
 	},
 	 # master db
 	'compara_master' => {
@@ -54,31 +57,32 @@ sub default_options {
 		-host => 'compara1',
 		-dbname => 'sf5_ensembl_compara_master',
 	},
-	'main_core_dbs' => [ 
+	'main_core_dbs' => [
 		{
 			-user => 'ensro',
 			-port => 3306,
 			-host => 'ens-staging1',
 			-dbname => '',
-			-db_version => $self->o('release'),
+			-db_version => $self->o('core_db_version') || $self->o('release'),
 		},
+	
 		{
 			-user => 'ensro',
 			-port => 3306,
 			-host => 'ens-staging2',
 			-dbname => '',
-			-db_version => $self->o('release'),
+			-db_version => $self->o('core_db_version') || $self->o('release'),
 		},
 	],
 	# dbs thay may be on genebuild dbs etc
 	other_core_dbs => {
 	},
 	  # mlssid of mappings to use
-	'mapping_mlssid' => 12,
+	'mapping_mlssid' => 13,
 	  # mlssid of ortheus alignments
 	'ortheus_mlssid' => 578,
 	  # species tree file
-	'species_tree_file' => '/lustre/scratch109/ensembl/sf5/EPO_DUMPS/species_tree_file',
+	'species_tree_file' => $self->o('ensembl_cvs_root_dir').'/ensembl-compara/scripts/pipeline/species_tree_blength.nh',
 	  # data directories:
 	'mapping_file' => $self->o('ENV', 'EPO_DUMP_PATH').'/enredo_friendly.'.$self->o('rel_with_suffix'),
 	'enredo_output_file' => $self->o('ENV', 'EPO_DUMP_PATH').'/enredo.out.'.$self->o('rel_with_suffix'),
@@ -107,8 +111,7 @@ sub resource_classes {
          0 => { -desc => 'mem2500',  'LSF' => '-C0 -M2500000 -R"select[mem>2500] rusage[mem=2500]"' },
          1 => { -desc => 'mem3500',  'LSF' => '-C0 -M3500000 -R"select[mem>3500] rusage[mem=3500]"' },
          2 => { -desc => 'mem7500',  'LSF' => '-C0 -M7500000 -R"select[mem>7500] rusage[mem=7500]"' },  
-         3 => { -desc => 'mem11400', 'LSF' => '-C0 -M11400000 -R"select[mem>11400] rusage[mem=11400]"' },  
-         4 => { -desc => 'mem14000', 'LSF' => '-C0 -M14000000 -R"select[mem>14000] rusage[mem=14000]"' },  
+         3 => { -desc => 'mem14000', 'LSF' => '-C0 -M14000000 -R"select[mem>14000] rusage[mem=14000]"' },  
     };  
 }
 
@@ -126,8 +129,10 @@ sub pipeline_wide_parameters {
 		'enredo_output_file' => $self->o('enredo_output_file'),
 		'ancestor_db' => $self->o('ancestor_db'),
 		'core_cvs_sql_schema' => $self->o('core_cvs_sql_schema'),
+		'core_db_version' => $self->o('core_db_version'),
 		'bl2seq' => $self->o('bl2seq'),
 		'addMT' => $self->o('addMT'),
+		'species_tree_file' => $self->o('species_tree_file'),
 	};
 }
 
@@ -137,9 +142,9 @@ sub pipeline_analyses {
 
 return [
 		# dump mapping info from mapping db
-	    {   -logic_name => 'dump_mappings_to_file',
-		-module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
-		-parameters => {
+	    {  -logic_name => 'dump_mappings_to_file',
+	       -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+               -parameters => {
 				'db_conn' => $self->dbconn_2_mysql('compara_mapped_anchor_db', 1),
 				'mapping_file' => $self->o('mapping_file'),
 				'cmd' => "mysql #db_conn# -NB -e \'SELECT aa.anchor_id, gdb.name, df.name, aa.dnafrag_start, aa.dnafrag_end, CASE aa.dnafrag_strand WHEN 1 THEN \"+\" ELSE \"-\" END, aa.num_of_organisms, aa.score FROM anchor_align aa INNER JOIN dnafrag df ON aa.dnafrag_id = df.dnafrag_id INNER JOIN genome_db gdb ON gdb.genome_db_id = df.genome_db_id WHERE aa.method_link_species_set_id = " . $self->o('mapping_mlssid') . " ORDER BY gdb.name, df.name, aa.dnafrag_start\' >#mapping_file#",
@@ -159,33 +164,64 @@ return [
 				1 => [ 'load_genomeDB_dnafrag_dnafragRegion' ],
 			      },
 	   },
-	   
-	   {   -logic_name => 'load_genomeDB_dnafrag_dnafragRegion',
+		# populate the genome_db, species_set, method_link, method_link_species_set and dnafrag tables
+		# create an ancestral core db
+		# parse the enredo file - add the info to the dnafrag_region and synteny_region tables		
+	    {  -logic_name => 'load_genomeDB_dnafrag_dnafragRegion',
 	       -module    => 'Bio::EnsEMBL::Compara::Production::EPOanchors::ParseEnredo',
 	       -parameters => {
 				compara_master => $self->o('compara_master'),
 				other_core_dbs => $self->o('other_core_dbs'),
 				ortheus_mlssid => $self->o('ortheus_mlssid'),
 				ancestor_db => $self->o('ancestor_db'),
+				main_core_dbs => $self->o('main_core_dbs'),
 			      },
 		-flow_into => {
 				1 => [ 'find_dnafrag_region_strand' ],
 				2 => [ 'Ortheus' ],
 			},
-	  },
+	    },
 	
-	  {	-logic_name => 'find_dnafrag_region_strand',
+		# add a newick species tree to the method_link_species_set_tag table
+	   {   -logic_name    => 'make_species_tree',
+               -module        => 'Bio::EnsEMBL::Compara::RunnableDB::MakeSpeciesTree',
+               -parameters    => { 
+                                   'mlss_id' => $self->o('ortheus_mlssid'),
+                                  },  
+               -input_ids     => [
+                                 { 'blength_tree_file' => $self->o('species_tree_file'), 'newick_format' => 'simple' }, #species_tree
+                            ],  
+               -hive_capacity => -1,   # to allow for parallelization
+               -wait_for => [ 'load_genomeDB_dnafrag_dnafragRegion' ],
+               -flow_into => {
+                         4 => { 'mysql:////method_link_species_set_tag' => { 'method_link_species_set_id' => '#mlss_id#', 'tag' => 'species_tree', 'value' => '#species_tree_string#' } },
+                         },  
+            },
+	  	# find the most likely strand orientation for genomic regions which enredo was unable to determine orientation (set to '0' in the enredo output file 
+	    {	-logic_name => 'find_dnafrag_region_strand',
 		-module    => 'Bio::EnsEMBL::Compara::Production::EPOanchors::FindStrand',
 		-parameters => {
 				bl2seq => $self->o('bl2seq'),
 				bl2seq_file => $self->o('bl2seq_file'),
 			       },
 		-hive_capacity => 10,
-		-failed_job_tolerance => 5, # can start ortheus if a few jobs fail 
-		-rc_id => 3, # there should not be too many of these jobs, most of which do not require much memory, but a small number  will
-	  },
+		-flow_into => {
+				-1 => [ 'find_dnafrag_region_strand_more_mem' ],
+			},
+       	   },
+	
+	   {
+		-logic_name => 'find_dnafrag_region_strand_more_mem',
+		-module    => 'Bio::EnsEMBL::Compara::Production::EPOanchors::FindStrand',
+		-parameters => {
+				bl2seq => $self->o('bl2seq'),
+				bl2seq_file => $self->o('bl2seq_file'),
+			       },
+		-can_be_empty => 1,
+		-rc_id => 1,
+	   },
 
-	  {	-logic_name => 'set_internal_ids',
+	   {	-logic_name => 'set_internal_ids',
 		-module => 'Bio::EnsEMBL::Hive::RunnableDB::SqlCmd',
 		-input_ids => [{}],
 		-wait_for => [ 'load_genomeDB_dnafrag_dnafragRegion' ],
@@ -198,9 +234,9 @@ return [
 						'ALTER TABLE dnafrag AUTO_INCREMENT=#expr(($ortheus_mlssid * 10**10) + 1)expr#',
 					],
 			},
-	  },
-
-	  {	-logic_name => 'Ortheus',
+	   },
+		# run ortheus
+	   {	-logic_name => 'Ortheus',
 		-parameters => {
 				max_block_size=>1000000,
 				java_options=>'-server -Xmx1000M',
@@ -208,21 +244,32 @@ return [
 		-module => 'Bio::EnsEMBL::Compara::RunnableDB::Ortheus',
 		-hive_capacity => 100,
 		-flow_into => {
-                               1 => [ 'update_max_alignment_length' ],
-                     },
-		-wait_for => [ 'set_internal_ids', 'find_dnafrag_region_strand' ],
-	  },
+			-1 => [ 'Ortheus_high_mem' ],
+		},
+		-wait_for => [ 'set_internal_ids', 'find_dnafrag_region_strand', 'find_dnafrag_region_strand_more_mem', 'make_species_tree' ],
+	   },
+		
+		# increase compute memory and java memory if default settings fail 
+	   {	-logic_name => 'Ortheus_high_mem',
+		-parameters => {
+			max_block_size=>1000000,
+			java_options=>'-server -Xmx6000M',
+		},
+		-module => 'Bio::EnsEMBL::Compara::RunnableDB::Ortheus',
+		-hive_capacity => 10,
+		-can_be_empty => 1,
+		-rc_id => 3,
+	   },
 
-	  {  -logic_name => 'update_max_alignment_length',
-             -module     => 'Bio::EnsEMBL::Compara::Production::EPOanchors::UpdateMaxAlignmentLength',
+	   {  -logic_name => 'update_max_alignment_length',
+             -module     => 'Bio::EnsEMBL::Compara::RunnableDB::GenomicAlignBlock::UpdateMaxAlignmentLength',
+	     -input_ids => [{}],
 	     -parameters => {
 				'method_link_species_set_id' => $self->o('ortheus_mlssid'),
 		},
-             -flow_into => {
-                              1 => [ 'create_neighbour_nodes_jobs_alignment' ],
-                     },  
-	     -wait_for => [ 'Ortheus' ],
+	     -wait_for => [ 'Ortheus', 'Ortheus_high_mem' ],
             },  
+		# set up jobs for updating left_index and right_index values in genomic_align_tree table
 	    {	-logic_name => 'create_neighbour_nodes_jobs_alignment',
 		-parameters => {
 				'inputquery' => 'SELECT root_id FROM genomic_align_tree WHERE parent_id = 0',
@@ -233,16 +280,16 @@ return [
 		-flow_into => {
 				1 => [ 'set_neighbour_nodes' ],
 			},
-	},
+	    },
 	
-	{	-logic_name => 'set_neighbour_nodes',
+       	    { 	-logic_name => 'set_neighbour_nodes',
 		-module => 'Bio::EnsEMBL::Compara::RunnableDB::EpoLowCoverage::SetNeighbourNodes',
 		-parameters => {
 				'method_link_species_set_id' => $self->o('ortheus_mlssid'),
 			},
 		-batch_size    => 10,
 		-hive_capacity => 15,
-	},
+	    },
 	
      ];
 }	
