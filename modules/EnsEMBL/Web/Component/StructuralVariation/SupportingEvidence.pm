2a95c05ddebe7916c460e2647f7d03e75a16cfd1
Laurent Gil <lgil@ebi.ac.uk>
2012-04-20 10:46:28 +0000

    Added new column to the table (Phenotype, Breakpoint order)

--- modules/EnsEMBL/Web/Component/StructuralVariation/SupportingEvidence.pm
+++ modules/EnsEMBL/Web/Component/StructuralVariation/SupportingEvidence.pm
@@ -31,37 +31,35 @@ sub supporting_evidence_table {
   
   my $columns = [
      { key => 'ssv',    sort => 'string',        title => 'Supporting evidence'   },
-     { key => 'pos',    sort => 'position_html', title => 'Chr:bp'                },
-     { key => 'class',  sort => 'string',        title => 'Allele type'           },
-     { key => 'clin',   sort => 'string',        title => 'Clinical significance' },
-     { key => 'sample', sort => 'string',        title => 'Sample name'           }, 
+     { key => 'pos',    sort => 'position_html', title => 'Chr:bp'                }, 
   ];
-  
-  if ($self->hub->species ne 'Homo_sapiens') {  
-     push(@$columns,{ key => 'strain', sort => 'string', title => 'Strain' });
-  };
+  my $sorting = 'pos';
 
   my $rows = ();
+  my $has_bp;
+  my $has_phen;
   
   # Supporting evidences list
   if (scalar @{$ssvs}) {
     my $ssv_names = {};
     foreach my $ssv (@$ssvs){
-      my $name = $ssv->name;
+      my $name = $ssv->variation_name;
       $name =~ /(\d+)$/;
       my $ssv_nb = $1;
       $ssv_names->{$1} = $ssv;
     }
-    
+   
     foreach my $ssv_n (sort {$a <=> $b} (keys(%$ssv_names))) {
       my $ssv_obj = $ssv_names->{$ssv_n};
       my $name = $ssv_obj->variation_name;
       my $loc;
+      my $bp_order;
       
       # Location(s)
       foreach my $svf (@{$ssv_obj->get_all_StructuralVariationFeatures}) {
         my $sv_name;
-        my $chr_bp = $svf->seq_region_name . ':' . $svf->seq_region_start . '-' . $svf->seq_region_end;
+        my $chr_bp  = $svf->seq_region_name.':';
+           $chr_bp .= $svf->seq_region_start ==  $svf->seq_region_end ? $svf->seq_region_start : $svf->seq_region_start.'-'.$svf->seq_region_end;
         my $loc_url;
         
         my $loc_hash = {
@@ -72,8 +70,11 @@ sub supporting_evidence_table {
         $loc_hash->{sv} = $name if ($ssv_obj->is_evidence == 0);
           
         my $loc_url = $hub->url($loc_hash);
-        $loc .= <br /> if ($loc);
+        $loc .= "<br />" if ($loc);
         $loc .= qq{<a href="$loc_url">$chr_bp</a>};
+        
+        $bp_order = $svf->breakpoint_order;
+        $has_bp = 1 if ($bp_order && $bp_order > 1);
       }
       $loc = '-' if (!$loc);
   
@@ -92,11 +93,12 @@ sub supporting_evidence_table {
       my $sv_class = '<table style="border-spacing:0px"><tr><td style="background-color:'.$colour.';width:5px"></td><td style="margin:0px;padding:0px">&nbsp;'.$ssv_obj->var_class.'</td></tr></table>';
        
       # Annotation(s)
-      my ($clin, $sample, $strain);
+      my ($clin, $sample, $strain, $phen);
       foreach my $annot (@{$ssv_obj->get_all_StructuralVariationAnnotations}) {
         my $aclin = $annot->clinical_significance;
         my $asample = $annot->sample_name;
         my $astrain = $annot->strain_name;
+        my $aphen   = $annot->phenotype_description;
         
         if ($aclin) {
           $clin .= '<br />' if ($clin);
@@ -110,20 +112,48 @@ sub supporting_evidence_table {
           $strain .= '<br />' if ($strain);
           $strain = $astrain;
         }
+        if ($aphen) {
+          $phen .= '<br />' if ($phen);
+          $phen = $aphen;
+          $has_phen = 1;
+        }    
       }
       
       my %row = (
-                  ssv    => $name,
-                  class  => $sv_class,
-                  pos    => $loc,
-                  clin   => $clin ? $clin : '-',
-                  sample => $sample ? $sample : '-',
-                  strain => $strain ? $strain : '-',
+                  ssv      => $name,
+                  class    => $sv_class,
+                  pos      => $loc,
+                  clin     => $clin ? $clin : '-',
+                  sample   => $sample ? $sample : '-',
+                  strain   => $strain ? $strain : '-',
+                  bp_order => $bp_order ? $bp_order : '-',
+                  phen     => $phen ? $phen : '-',
                 );
         
       push @$rows, \%row;
     }
-    return $self->new_table($columns, $rows, { data_table => 1, sorting => [ 'location asc' ] })->render;
+    
+    if (defined($has_bp)) {  
+     push(@$columns,{ key => 'bp_order', sort => 'numeric', title => 'Breakpoint order' });
+     $sorting = 'bp_order';
+    };
+    
+    push @$columns, (
+     { key => 'class',  sort => 'string',        title => 'Allele type'           },
+     { key => 'clin',   sort => 'string',        title => 'Clinical significance' },
+     { key => 'sample', sort => 'string',        title => 'Sample name'           },
+    );
+    
+    if (defined($has_phen)) {  
+     push(@$columns,{ key => 'phen', sort => 'string', title => 'Phenotype' });
+    };
+    
+    if ($self->hub->species ne 'Homo_sapiens') {  
+     push(@$columns,{ key => 'strain', sort => 'string', title => 'Strain' });
+    };
+    
+    
+    return $self->new_table($columns, $rows, { data_table => 1, sorting => [ "$sorting asc" ] })->render;
   }
 }
 1;
