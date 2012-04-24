fb88677933656963391e6315d92b7bb1003e63bc
Simon Brent <sb23@sanger.ac.uk>
2012-04-24 08:12:21 +0000

    set track height in ImageConfig. Get the maths for positioning the tags correct finally

--- modules/Bio/EnsEMBL/GlyphSet/structural_variation.pm
+++ modules/Bio/EnsEMBL/GlyphSet/structural_variation.pm
@@ -22,10 +22,6 @@ sub features {
     $var_features = $slice->get_all_StructuralVariationFeatures; # All structural variations
   }
   
-  if ($self->my_config('display') eq 'normal') {
-    $self->get_render_normal;
-  }
-  
   return $var_features;  
 }
 
@@ -37,25 +33,25 @@ sub colour_key  {
 sub tag {
   my ($self, $f) = @_;
   my $colour         = $self->my_colour($self->colour_key($f), 'tag');
+  my $border         = 'dimgray';
   my $inner_crossing = $f->inner_start && $f->inner_end && $f->inner_start >= $f->inner_end ? 1 : 0;
   my @g_objects;
-
+  
   if ($inner_crossing && $f->inner_start == $f->seq_region_end) {
-    push @g_objects, {
-        style  => 'rect',
-        colour => $colour,
-        start  => $f->start,
-        end    => $f->end
-     };
-     return @g_objects;
+    return {
+      style  => 'rect',
+      colour => $colour,
+      border => $border,
+      start  => $f->start,
+      end    => $f->end
+    };
   }
-
+  
   # start of feature
   if ($f->outer_start && $f->inner_start) {
     if ($f->outer_start != $f->inner_start && !$inner_crossing) {
       push @g_objects, {
         style  => 'rect',
-        colour => $colour,
         start  => $f->start,
         end    => $f->inner_start - $f->seq_region_start + $f->start
       };
@@ -64,7 +60,6 @@ sub tag {
     if ($f->outer_start == $f->seq_region_start || $inner_crossing) {
       push @g_objects, {
         style  => 'bound_triangle_right',
-        colour => $colour,
         start  => $f->start,
         out    => 1
       };
@@ -72,9 +67,8 @@ sub tag {
   } elsif ($f->inner_start) {
     if ($f->inner_start == $f->seq_region_start && !$inner_crossing) {
       push @g_objects, {
-        style  => 'bound_triangle_left',
-        colour => $colour,
-        start  => $f->start
+        style => 'bound_triangle_left',
+        start => $f->start
       };
     }
   }
@@ -84,7 +78,6 @@ sub tag {
     if ($f->outer_end != $f->inner_end && !$inner_crossing) {
       push @g_objects, {
         style  => 'rect',
-        colour => $colour,
         start  => $f->end - $f->seq_region_end + $f->inner_end,
         end    => $f->end
       };
@@ -93,7 +86,6 @@ sub tag {
     if ($f->outer_end == $f->seq_region_end || $inner_crossing) {
       push @g_objects, {
         style  => 'bound_triangle_left',
-        colour => $colour,
         start  => $f->end,
         out    => 1
       };
@@ -102,24 +94,18 @@ sub tag {
     if ($f->inner_end == $f->seq_region_end && !$inner_crossing) {
       push @g_objects, {
         style  => 'bound_triangle_right',
-        colour => $colour,
         start  => $f->end
       };
     }
   }
   
-  return @g_objects;
-} 
-
-
-sub get_render_normal {
-  my $self = shift;
+  foreach (@g_objects) {
+    $_->{'colour'} = $colour;
+    $_->{'border'} = $border;
+  }
   
-  my ($font, $fontsize) = $self->get_font_details($self->my_config('font') || 'innertext');
-  my $height            = $self->my_config('height') || [$self->get_text_width(0, 'X', '', font => $font, ptsize => $fontsize)]->[3] + 2;
-  $self->{'extras'}{'height'} = $height/=2;
+  return @g_objects;
 }
-
 
 sub render_tag {
   my ($self, $tag, $composite, $slice_length, $width, $start, $end, $img_start, $img_end) = @_;
@@ -127,18 +113,18 @@ sub render_tag {
   
   if ($tag->{'style'} =~ /^bound_triangle_(\w+)$/ && $img_start < $tag->{'start'} && $img_end > $tag->{'end'}) {
     my $pix_per_bp = $self->scalex;
-    my $x          = $tag->{'start'} + ($tag->{'out'} == ($1 eq 'left') ? 1 : -1) * ($tag->{'out'} ? $width  / 4 / $pix_per_bp : ($width + 2) / 2 / $pix_per_bp);
+    my $x          = $tag->{'start'} + ($tag->{'out'} == ($1 eq 'left') ? 1 : -1) * (($tag->{'out'} ? 1 : ($width / 2) + 1) / $pix_per_bp);
     my $y          = $width / 2;
     
     # Triangle returns an array: the triangle, and an invisible rectangle behind it for clicking purposes
     @glyph = $self->Triangle({
       mid_point    => [ $x, $y ],
       colour       => $tag->{'colour'},
+      bordercolour => $tag->{'border'},
       absolutey    => 1,
       width        => $width,
       height       => $y / $pix_per_bp,
       direction    => $1,
-      bordercolour => 'dimgray',
     });
   }
   
@@ -171,14 +157,12 @@ sub highlight {
   my ($self, $f, $composite, $pix_per_bp, $h) = @_;
   my $id = $f->variation_name;
   my %highlights;
-  my $extra = 0;
   @highlights{$self->highlights} = (1);
 
   my $length = ($f->end - $f->start) + 1; 
   
   return unless $highlights{$id};
   
-  # First a black box
   $self->unshift($self->Rect({
     x         => $composite->x - 2/$pix_per_bp,
     y         => $composite->y - 2, # + makes it go down
