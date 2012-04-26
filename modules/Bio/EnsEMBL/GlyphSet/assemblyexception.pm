050bcc1d5628a4c17730e4f84655e8284568d9b1
Simon Brent <sb23@sanger.ac.uk>
2012-04-26 14:17:32 +0000

    added align_r to zmenu url. tidied.

--- modules/Bio/EnsEMBL/GlyphSet/assemblyexception.pm
+++ modules/Bio/EnsEMBL/GlyphSet/assemblyexception.pm
@@ -4,70 +4,63 @@ use strict;
 
 use base qw(Bio::EnsEMBL::GlyphSet_simple);
 
-sub readable_strand {
-  my ($self, $strand) = @_;
-  return $strand < 0 ? 'rev' : 'fwd';
-}
-
-sub my_label { return undef; }
-
-sub features {
-  my ($self) = @_;
-  return $self->{'container'}->get_all_AssemblyExceptionFeatures();
-}
+sub readable_strand { return $_[1] < 0 ? 'rev' : 'fwd'; }
+sub my_label        { return undef; }
+sub features        { return $_[0]->{'container'}->get_all_AssemblyExceptionFeatures; }
 
 sub colour_key {
-  my( $self, $f ) = @_;
-  ( my $key = lc($f->type) ) =~ s/ /_/g;
+  my ($self, $f) = @_;
+  (my $key = lc $f->type) =~ s/ /_/g;
   return $key;
 }
 
 sub feature_label {
   my ($self, $f) = @_;
-  return undef if $self->my_config( 'label' ) eq 'off';
-  if( $self->my_config( 'short_labels') ) {
-    return( $f->{'alternate_slice'}->seq_region_name, 'under' ) ;
-  }
+  
+  return undef if $self->my_config('label') eq 'off';
+  return ($f->{'alternate_slice'}->seq_region_name, 'under') if $self->my_config('short_labels');
+  
   return(
-    sprintf( "%s: %s:%d-%d (%s)",
+    sprintf('%s: %s:%d-%d (%s)',
       $f->type,
       $f->{'alternate_slice'}->seq_region_name,
       $f->{'alternate_slice'}->start,
       $f->{'alternate_slice'}->end,
-      $self->readable_strand( $f->{'alternate_slice'}->strand )
+      $self->readable_strand($f->{'alternate_slice'}->strand)
     ), 'undef'
   );
 }
 
 sub title {
-  my ($self, $f ) = @_;
+  my ($self, $f) = @_;
 
-  return sprintf "%s; %s:%d-%d (%s); %s:%d-%d (%s)",
-    $self->my_colour($self->colour_key($f),'text'),
+  return sprintf('%s; %s:%d-%d (%s); %s:%d-%d (%s)',
+    $self->my_colour($self->colour_key($f), 'text'),
     $f->{'slice'}->seq_region_name,
-    $f->{'slice'}->start+$f->{'start'}-1,
-    $f->{'slice'}->start+$f->{'end'}-1,
-    $self->readable_strand( $f->{'slice'}->strand ),
+    $f->{'slice'}->start + $f->{'start'} - 1,
+    $f->{'slice'}->start + $f->{'end'}   - 1,
+    $self->readable_strand($f->{'slice'}->strand),
     $f->{'alternate_slice'}->seq_region_name,
     $f->{'alternate_slice'}->start,
     $f->{'alternate_slice'}->end,
-    $self->readable_strand( $f->{'alternate_slice'}->strand );
+    $self->readable_strand($f->{'alternate_slice'}->strand)
+  );
 }
 
 
 sub href {
-  my ($self, $f ) = @_;
-  my $c2 = $f->{'alternate_slice'}->seq_region_name;
-  my $s2 = $f->{'alternate_slice'}->start;
-  my $e2 = $f->{'alternate_slice'}->end;
-  my $o2 = $f->{'alternate_slice'}->strand;
+  my ($self, $f) = @_;
+  my $c2    = $f->{'alternate_slice'}->seq_region_name;
+  my $s2    = $f->{'alternate_slice'}->start;
+  my $e2    = $f->{'alternate_slice'}->end;
+  my $o2    = $f->{'alternate_slice'}->strand;
   my $class = $self->colour_key($f);
-  my $script = $ENV{'ENSEMBL_SCRIPT'} eq 'multicontigview' ? 'contigview' : $ENV{'ENSEMBL_SCRIPT'};
   
   return $self->_url({
     species => $f->adaptor->db->species,
     action  => 'View',
     r       => "$c2:$s2-$e2",
+    align_r => $self->{'config'}->hub->action eq 'Multi' ? '' : $self->{'config'}->hub->param('r'),
     class   => $class,
   });
 }
@@ -76,10 +69,10 @@ sub tag {
   my ($self, $f) = @_;
   
   return {
-    'style' => 'join',
-    'tag' => $f->{'start'}.'-'.$f->{'end'},
-    'colour' => $self->my_colour( $self->colour_key($f),'join' ),
-    'zindex' => -20
+    style  => 'join',
+    tag    => "$f->{'start'}-$f->{'end'}",
+    colour => $self->my_colour($self->colour_key($f), 'join'),
+    zindex => -20
   };
 }
 
@@ -88,8 +81,8 @@ sub export_feature {
   my ($feature, $feature_type) = @_;
   
   return $self->_render_text($feature, $feature_type, { 
-    'headers' => [ 'alternate_slice' ],
-    'values' => [ $feature->{'alternate_slice'}->seq_region_name ]
+    headers => [ 'alternate_slice' ],
+    values  => [ $feature->{'alternate_slice'}->seq_region_name ]
   });
 }
 
