d1d02ae6249a5bdebe4dd1ccc421cd5acff32422
Simon Brent <sb23@sanger.ac.uk>
2012-04-26 08:34:11 +0000

    switched to using key of help for help mouseovers on table headers

--- modules/EnsEMBL/Web/Component/Gene/GeneSNPTable.pm
+++ modules/EnsEMBL/Web/Component/Gene/GeneSNPTable.pm
@@ -4,8 +4,9 @@ package EnsEMBL::Web::Component::Gene::GeneSNPTable;
 
 use strict;
 
-use Bio::EnsEMBL::Variation::Utils::Constants;
 use EnsEMBL::Web::DBSQL::WebsiteAdaptor;
+
+use Bio::EnsEMBL::Variation::Utils::Constants;
 
 use base qw(EnsEMBL::Web::Component::Gene);
 
@@ -54,25 +55,25 @@ sub content {
   return $ret;
 }
 
+
+
 sub make_table {
   my ($self, $table_rows, $consequence_type) = @_;
-    
+  
+  my $glossary = new EnsEMBL::Web::DBSQL::WebsiteAdaptor($self->hub)->fetch_glossary_text_lookup;
+  
   my $columns = [
-    { key => 'ID',       sort => 'html'                                                    },
-    { key => 'chr' ,     sort => 'position',  title => 'Chr: bp'                           },
-    { key => 'Alleles',  sort => 'string',                               align => 'center' },
-    { key => 'class',    sort => 'string',    title => 'Class',          align => 'center' },
-    { key => 'Source',   sort => 'string'                                                  },
-    { key => 'status',   sort => 'string',    label => 'Validation', title => $glossary->{'Validation status'}, align => 'center' },
-    { key => 'snptype',  sort => 'string',    title => 'Type',                             },
-    { key => 'aachange', sort => 'string',    title => 'Amino Acid',     align => 'center' },
-    { key => 'aacoord',  sort => 'position',  title => 'AA coord',       align => 'center' },
+    { key => 'ID',       sort => 'html'                                                                                         },
+    { key => 'chr' ,     sort => 'position', title => 'Chr: bp'                                                                 },
+    { key => 'Alleles',  sort => 'string',                          align => 'center'                                           },
+    { key => 'class',    sort => 'string',   title => 'Class',      align => 'center'                                           },
+    { key => 'Source',   sort => 'string'                                                                                       },
+    { key => 'status',   sort => 'string',   title => 'Validation', align => 'center', help => $glossary->{'Validation status'} },
+    { key => 'snptype',  sort => 'string',   title => 'Type',                                                                   },
+    { key => 'aachange', sort => 'string',   title => 'Amino Acid', align => 'center'                                           },
+    { key => 'aacoord',  sort => 'position', title => 'AA coord',   align => 'center'                                           },
   ];
- 
-  ## Glossary
-  my $adaptor = EnsEMBL::Web::DBSQL::WebsiteAdaptor->new($self->hub);
-  my $glossary = $adaptor->fetch_glossary_text_lookup;
- 
+  
   # HGVS
   if($self->hub->param('hgvs') eq 'on') {
     splice(@$columns, 3, 0, 
@@ -83,8 +84,8 @@ sub make_table {
   # add GMAF, SIFT and PolyPhen for human
   if ($self->hub->species eq 'Homo_sapiens') {
     push @$columns, (
-      { key => 'sift',     sort => 'position_html', label => 'SIFT',     title => $glossary->{'SIFT'}, align => 'center' },
-      { key => 'polyphen', sort => 'position_html', label => 'PolyPhen', title => $glossary->{'PolyPhen'}, align => 'center' },
+      { key => 'sift',     sort => 'position_html', title => 'SIFT',     align => 'center', help => $glossary->{'SIFT'},     },
+      { key => 'polyphen', sort => 'position_html', title => 'PolyPhen', align => 'center', help => $glossary->{'PolyPhen'}, },
     );
     
     splice(@$columns, 3, 0, { key => 'gmaf', sort => 'numerical', title => 'Frequency', align => 'center' });
