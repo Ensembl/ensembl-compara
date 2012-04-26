6f6926d58201912cb0a023dd9206ea49574bad7a
Simon Brent <sb23@sanger.ac.uk>
2012-04-26 10:19:13 +0000

    use Document::Table instead of Document::SpreadSheet. tidied.

--- modules/EnsEMBL/Web/Document/HTML/FTPtable.pm
+++ modules/EnsEMBL/Web/Document/HTML/FTPtable.pm
@@ -1,3 +1,5 @@
+# $Id$
+
 package EnsEMBL::Web::Document::HTML::FTPtable;
 
 ### This module outputs a table of links to the FTP site
@@ -7,208 +9,153 @@ use strict;
 use HTML::Entities qw(encode_entities);
 
 use EnsEMBL::Web::RegObj;
-use EnsEMBL::Web::Document::SpreadSheet;
+use EnsEMBL::Web::Document::Table;
 use base qw(EnsEMBL::Web::Document::HTML);
 
 sub render {
-  my $self = shift;
-
-  my $species_defs = $ENSEMBL_WEB_REGISTRY->species_defs;
-
-  # Always set to use the release number rather than current to get around the delay in FTP site links updating 
-  my $rel = 'release-'.$species_defs->ENSEMBL_VERSION;
-
+  my $self            = shift;
+  my $species_defs    = $ENSEMBL_WEB_REGISTRY->species_defs;
+  my $rel             = 'release-' . $species_defs->ENSEMBL_VERSION; # Always set to use the release number rather than current to get around the delay in FTP site links updating
+  my $required_lookup = $self->required_types_for_species;
+  my $table           = new EnsEMBL::Web::Document::Table([], [], { data_table => 1, exportable => 0 });
+  my $rows;
+  
   my %title = (
-    'dna'     => 'Masked and unmasked genome sequences associated with the assembly (contigs, chromosomes etc.)',
-    'cdna'       => 'cDNA sequences for Ensembl or "ab initio" predicted genes',
-    'prot'       => 'Protein sequences for Ensembl or "ab initio" predicted genes',
-    'rna'        => 'Non-coding RNA gene predictions',
-    'embl'       => 'Ensembl database dumps in EMBL nucleotide sequence database format',
-    'genbank'    => 'Ensembl database dumps in GenBank nucleotide sequence database format',
-    'gtf'        => 'Gene sets for each species. These files include annotations of both coding and non-coding genes',
-    'mysql'      => 'All Ensembl MySQL databases are available in text format as are the SQL table definition files',
-    'emf'        => 'Alignments of resequencing data from the ensembl_compara database',
-    'gvf'        => 'Variation data in GVF format',
-    'vep'        => 'Cache files for use with the VEP script',
-    'funcgen'    => 'Regulation data in GFF format',
-    'coll'       => 'Additional regulation data (not in database)',
-    'bed'        => 'Constrained elements calculated using GERP',
-    'extra'      => 'Additional release data stored as flat files rather than MySQL for performance reasons',
-    'ancestral'  => 'Ancestral Allele data in FASTA format',
-    'bam'        => 'Alignments against the genome',
+    dna       => 'Masked and unmasked genome sequences associated with the assembly (contigs, chromosomes etc.)',
+    cdna      => 'cDNA sequences for Ensembl or "ab initio" predicted genes',
+    prot      => 'Protein sequences for Ensembl or "ab initio" predicted genes',
+    rna       => 'Non-coding RNA gene predictions',
+    embl      => 'Ensembl database dumps in EMBL nucleotide sequence database format',
+    genbank   => 'Ensembl database dumps in GenBank nucleotide sequence database format',
+    gtf       => 'Gene sets for each species. These files include annotations of both coding and non-coding genes',
+    mysql     => 'All Ensembl MySQL databases are available in text format as are the SQL table definition files',
+    emf       => 'Alignments of resequencing data from the ensembl_compara database',
+    gvf       => 'Variation data in GVF format',
+    vep       => 'Cache files for use with the VEP script',
+    funcgen   => 'Regulation data in GFF format',
+    coll      => 'Additional regulation data (not in database)',
+    bed       => 'Constrained elements calculated using GERP',
+    files     => 'Additional release data stored as flat files rather than MySQL for performance reasons',
+    ancestral => 'Ancestral Allele data in FASTA format',
+    bam       => 'Alignments against the genome',
   );
   
   $title{$_} = encode_entities($title{$_}) for keys %title;
-
-  my $EMF = $title{'emf'};
-  my $BED = $title{'bed'};
-  my $XML = $title{'xml'};
-  my $ANC = $title{'ancestral'};
-
-  my $html = qq(
-<h3>Multi-species data</h3>
-<table class="ss tint" cellpadding="4">
-<tr>
-<th>Database</th>
-<th></th>
-<th></th>
-<th></th>
-<th></th>
-<th></th>
-</tr>
-<tr class="bg1">
-<td>Comparative genomics</td>
-<td style="text-align:center"><a rel="external" title="$title{'mysql'}" href="ftp://ftp.ensembl.org/pub/).$rel.qq(/mysql/">MySQL</a></td>
-<td style="text-align:center"><a rel="external" title="$EMF" href="ftp://ftp.ensembl.org/pub/).$rel.qq(/emf/ensembl-compara/">EMF</a></td>
-<td style="text-align:center"><a rel="external" title="$BED" href="ftp://ftp.ensembl.org/pub/).$rel.qq(/bed/">BED</a></td>
-<td style="text-align:center"><a rel="external" title="$XML" href="ftp://ftp.ensembl.org/pub/).$rel.qq(/xml/ensembl-compara/homologies/">XML</a></td>
-<td style="text-align:center"><a rel="external" title="$ANC" href="ftp://ftp.ensembl.org/pub/).$rel.qq(/fasta/ancestral_alleles">Ancestral Alleles</a></td>
-
-</tr>
-<tr class="bg2">
-<td>BioMart</td>
-<td style="text-align:center"><a rel="external" title="$title{'mysql'}" href="ftp://ftp.ensembl.org/pub/).$rel.qq(/mysql/">MySQL</a></td>
-<td style="text-align:center">-</td>
-<td style="text-align:center">-</td>
-<td style="text-align:center">-</td>
-<td style="text-align:center">-</td>
-</tr>
-</table>
-);
-
-  ## Main table
-  $html .= qq(
-<div class="js_panel" id="ftp-table">
-<input type="hidden" class="panel_type" value="Content">
-<h3>Single species data</h3>
-);
-
-  my $table = new EnsEMBL::Web::Document::SpreadSheet([], [], {data_table => 1});
-  $table->add_columns(
-    { key => 'species', title => 'Species',             align => 'left',   width => '10%', sort => 'html' },
-    { key => 'dna',     title => 'DNA (FASTA)',         align => 'center', width => '10%', sort => 'none' },
-    { key => 'cdna',    title => 'cDNA (FASTA)',        align => 'center', width => '10%', sort => 'none' },
-    { key => 'ncrna',   title => 'ncRNA (FASTA)',       align => 'center', width => '10%', sort => 'none' },
-    { key => 'protseq', title => 'Protein sequence (FASTA)',  align => 'center', width => '10%', sort => 'none' },
-    { key => 'embl',    title => 'Annotated sequence (EMBL)', align => 'center', width => '10%', sort => 'none' },
-    { key => 'genbank', title => 'Annotated sequence (GenBank)',  align => 'center', width => '10%', sort => 'none' },
-    { key => 'genes',   title => 'Gene sets',           align => 'center', width => '10%', sort => 'none' },
-    { key => 'mysql',   title => 'Whole databases',     align => 'center', width => '10%', sort => 'none' },
-    { key => 'var1',    title => 'Variation (EMF)',     align => 'center', width => '10%', sort => 'html' },
-    { key => 'var2',    title => 'Variation (GVF)',     align => 'center', width => '10%', sort => 'html' },
-    { key => 'var3',    title => 'Variation (VEP)',     align => 'center', width => '10%', sort => 'html' },
-    { key => 'funcgen', title => 'Regulation (GFF)',    align => 'center', width => '10%', sort => 'html' },
-    { key => 'files',   title => 'Data files',          align => 'center', width => '10%', sort => 'html' },
-    { key => 'bam',     title => 'BAM',                 align => 'center', width => '10%', sort => 'html' },
-  );
-
-  my @species = $species_defs->ENSEMBL_DATASETS;
-  my $rows;
   
-  my $required_lookup = $self->required_types_for_species();
+  $table->add_columns(
+    { key => 'species', title => 'Species',                      align => 'left',   width => '10%', sort => 'html' },
+    { key => 'dna',     title => 'DNA (FASTA)',                  align => 'center', width => '10%', sort => 'none' },
+    { key => 'cdna',    title => 'cDNA (FASTA)',                 align => 'center', width => '10%', sort => 'none' },
+    { key => 'ncrna',   title => 'ncRNA (FASTA)',                align => 'center', width => '10%', sort => 'none' },
+    { key => 'protseq', title => 'Protein sequence (FASTA)',     align => 'center', width => '10%', sort => 'none' },
+    { key => 'embl',    title => 'Annotated sequence (EMBL)',    align => 'center', width => '10%', sort => 'none' },
+    { key => 'genbank', title => 'Annotated sequence (GenBank)', align => 'center', width => '10%', sort => 'none' },
+    { key => 'genes',   title => 'Gene sets',                    align => 'center', width => '10%', sort => 'none' },
+    { key => 'mysql',   title => 'Whole databases',              align => 'center', width => '10%', sort => 'none' },
+    { key => 'var1',    title => 'Variation (EMF)',              align => 'center', width => '10%', sort => 'html' },
+    { key => 'var2',    title => 'Variation (GVF)',              align => 'center', width => '10%', sort => 'html' },
+    { key => 'var3',    title => 'Variation (VEP)',              align => 'center', width => '10%', sort => 'html' },
+    { key => 'funcgen', title => 'Regulation (GFF)',             align => 'center', width => '10%', sort => 'html' },
+    { key => 'files',   title => 'Data files',                   align => 'center', width => '10%', sort => 'html' },
+    { key => 'bam',     title => 'BAM',                          align => 'center', width => '10%', sort => 'html' },
+  );
   
   foreach my $spp (sort @{$species_defs->ENSEMBL_DATASETS}) {
-    (my $sp_name = $spp) =~ s/_/ /;
-    my $sp_dir =lc($spp);
-    my $sp_var = lc($spp).'_variation';
-    my $common = $species_defs->get_config($spp, 'DISPLAY_NAME');
-  
-    my $emf   = '-';
-    if($required_lookup->{var1}->{$sp_dir}) {
-      $emf = sprintf '<a rel="external" title="%s" href="ftp://ftp.ensembl.org/pub/%s/emf/%s/">EMF</a>', $title{'emf'}, $rel, $sp_var;
-    }
+    (my $sp_name  = $spp) =~ s/_/ /;
+    my $sp_dir    = lc $spp;
+    my $sp_var    = lc $spp . '_variation';
+    my $files_dir = $species_defs->get_config(ucfirst $sp_dir, 'databases')->{'DATABASE_FUNCGEN'}{'NAME'};
     
-    my $variation = '-';
-    if($required_lookup->{var2}->{$sp_dir}) {
-      $variation = sprintf '<a rel="external" title="%s" href="ftp://ftp.ensembl.org/pub/%s/variation/gvf/%s/">GVF</a>', $title{'gvf'}, $rel, $sp_dir;
-    }
-    
-    my $vep = '-';
-    if($required_lookup->{var3}->{$sp_dir}) {
-      $vep = sprintf '<a rel="external" title="%s" href="ftp://ftp.ensembl.org/pub/%s/variation/VEP/%s/">VEP</a>', $title{'vep'}, $rel, $sp_dir;
-    }
-    
-    my $funcgen = '-';
-    if($required_lookup->{funcgen}->{$sp_dir}) {
-      $funcgen = sprintf '<a rel="external" title="%s" href="ftp://ftp.ensembl.org/pub/%s/regulation/%s/">Regulation</a> (GFF)', $title{'funcgen'}, $rel, $sp_dir;      
-    }
-    
-    my $extra = '-';
-    if($required_lookup->{files}->{$sp_dir}) {
-      my $dbs = $species_defs->get_config(ucfirst($sp_dir), 'databases');
-      my $coll_dir = $dbs->{'DATABASE_FUNCGEN'}{'NAME'};
-      $extra = sprintf '<a rel="external" title="%s" href="ftp://ftp.ensembl.org/pub/%s/data_files/%s/">Regulation data files</a>', $title{'extra'}, $rel, $coll_dir;
-    }
-    
-    my $bam = '-';
-    if($required_lookup->{bam}->{$sp_dir}) {
-      $bam = sprintf '<a rel="external" title="%s" href="ftp://ftp.ensembl.org/pub/%s/bam/%s/">BAM</a>', $title{'bam'}, $rel, $sp_dir;
-    }
-
     $table->add_row({
-      'species'       => sprintf('<strong><i>%s</i></strong> (%s)', $sp_name, $common),
-      'dna'           => sprintf('<a rel="external" title="%s" href="ftp://ftp.ensembl.org/pub/%s/fasta/%s/dna/">FASTA</a>', $title{'dna'}, $rel, $sp_dir),
-      'cdna'          => sprintf('<a rel="external" title="%s" href="ftp://ftp.ensembl.org/pub/%s/fasta/%s/cdna/">FASTA</a>', $title{'cdna'}, $rel, $sp_dir),
-      'ncrna'         => sprintf('<a rel="external" title="%s" href="ftp://ftp.ensembl.org/pub/%s/fasta/%s/ncrna/">FASTA</a>', $title{'rna'}, $rel, $sp_dir),
-      'protseq'       => sprintf('<a rel="external" title="%s" href="ftp://ftp.ensembl.org/pub/%s/fasta/%s/pep/">FASTA</a>', $title{'prot'}, $rel, $sp_dir),
-      'embl'          => sprintf('<a rel="external" title="%s" href="ftp://ftp.ensembl.org/pub/%s/embl/%s/">EMBL</a>', $title{'embl'}, $rel, $sp_dir),
-      'genbank'       => sprintf('<a rel="external" title="%s" href="ftp://ftp.ensembl.org/pub/%s/genbank/%s/">GenBank</a>', $title{'genbank'}, $rel, $sp_dir),
-      'genes'         => sprintf('<a rel="external" title="%s" href="ftp://ftp.ensembl.org/pub/%s/gtf/">GTF</a>', $title{'gtf'}, $rel),
-      'mysql'         => sprintf('<a rel="external" title="%s" href="ftp://ftp.ensembl.org/pub/%s/mysql/">MySQL</a>', $title{'mysql'}, $rel),
-      'var1'          => $emf,
-      'var2'          => $variation,
-      'var3'          => $vep,
-      'funcgen'       => $funcgen,
-      'files'         => $extra,
-      'bam'           => $bam,
+      species => sprintf('<strong><i>%s</i></strong> (%s)', $sp_name, $species_defs->get_config($spp, 'DISPLAY_NAME')),
+      dna     => sprintf('<a rel="external" title="%s" href="ftp://ftp.ensembl.org/pub/%s/fasta/%s/dna/">FASTA</a>',   $title{'dna'},     $rel, $sp_dir),
+      cdna    => sprintf('<a rel="external" title="%s" href="ftp://ftp.ensembl.org/pub/%s/fasta/%s/cdna/">FASTA</a>',  $title{'cdna'},    $rel, $sp_dir),
+      ncrna   => sprintf('<a rel="external" title="%s" href="ftp://ftp.ensembl.org/pub/%s/fasta/%s/ncrna/">FASTA</a>', $title{'rna'},     $rel, $sp_dir),
+      protseq => sprintf('<a rel="external" title="%s" href="ftp://ftp.ensembl.org/pub/%s/fasta/%s/pep/">FASTA</a>',   $title{'prot'},    $rel, $sp_dir),
+      embl    => sprintf('<a rel="external" title="%s" href="ftp://ftp.ensembl.org/pub/%s/embl/%s/">EMBL</a>',         $title{'embl'},    $rel, $sp_dir),
+      genbank => sprintf('<a rel="external" title="%s" href="ftp://ftp.ensembl.org/pub/%s/genbank/%s/">GenBank</a>',   $title{'genbank'}, $rel, $sp_dir),
+      genes   => sprintf('<a rel="external" title="%s" href="ftp://ftp.ensembl.org/pub/%s/gtf/">GTF</a>',              $title{'gtf'},     $rel),
+      mysql   => sprintf('<a rel="external" title="%s" href="ftp://ftp.ensembl.org/pub/%s/mysql/">MySQL</a>',          $title{'mysql'},   $rel),
+      var1    => $required_lookup->{'var1'}{$sp_dir}    ? sprintf('<a rel="external" title="%s" href="ftp://ftp.ensembl.org/pub/%s/emf/%s/">EMF</a>',                          $title{'emf'},     $rel, $sp_var)    : '-',
+      var2    => $required_lookup->{'var2'}{$sp_dir}    ? sprintf('<a rel="external" title="%s" href="ftp://ftp.ensembl.org/pub/%s/variation/gvf/%s/">GVF</a>',                $title{'gvf'},     $rel, $sp_dir)    : '-',
+      var3    => $required_lookup->{'var3'}{$sp_dir}    ? sprintf('<a rel="external" title="%s" href="ftp://ftp.ensembl.org/pub/%s/variation/VEP/%s/">VEP</a>',                $title{'vep'},     $rel, $sp_dir)    : '-',
+      funcgen => $required_lookup->{'funcgen'}{$sp_dir} ? sprintf('<a rel="external" title="%s" href="ftp://ftp.ensembl.org/pub/%s/regulation/%s/">Regulation</a> (GFF)',      $title{'funcgen'}, $rel, $sp_dir)    : '-',
+      bam     => $required_lookup->{'bam'}{$sp_dir}     ? sprintf('<a rel="external" title="%s" href="ftp://ftp.ensembl.org/pub/%s/bam/%s/">BAM</a>',                          $title{'bam'},     $rel, $sp_dir)    : '-',
+      files   => $required_lookup->{'files'}{$sp_dir}   ? sprintf('<a rel="external" title="%s" href="ftp://ftp.ensembl.org/pub/%s/data_files/%s/">Regulation data files</a>', $title{'files'},   $rel, $files_dir) : '-',
     });
   }
-
-  $html .= $table->render;
-  $html .= '</div>';
-
-  return $html;
+  
+  return sprintf(qq{
+    <h3>Multi-species data</h3>
+    %s
+    <div class="js_panel" id="ftp-table">
+      <input type="hidden" class="panel_type" value="Content">
+      <h3>Single species data</h3>
+      %s
+    </div>
+  }, new EnsEMBL::Web::Document::Table([
+    { key => 'database',  title => 'Database' },
+    { key => 'mysql',     title => '', align => 'center' },
+    { key => 'emf',       title => '', align => 'center' },
+    { key => 'bed',       title => '', align => 'center' },
+    { key => 'xml',       title => '', align => 'center' },
+    { key => 'ancestral', title => '', align => 'center' }
+  ], [{
+    database  => 'Comparative genomics',
+    mysql     => qq{<a rel="external" title="$title{'mysql'}" href="ftp://ftp.ensembl.org/pub/$rel/mysql/">MySQL</a>},
+    emf       => qq{<a rel="external" title="$title{'emf'}" href="ftp://ftp.ensembl.org/pub/$rel/emf/ensembl-compara/">EMF</a>},
+    bed       => qq{<a rel="external" title="$title{'bed'}" href="ftp://ftp.ensembl.org/pub/$rel/bed/">BED</a>},
+    xml       => qq{<a rel="external" title="$title{'xml'}" href="ftp://ftp.ensembl.org/pub/$rel/xml/ensembl-compara/homologies/">XML</a>},
+    ancestral => qq{<a rel="external" title="$title{'ancestral'}" href="ftp://ftp.ensembl.org/pub/$rel/fasta/ancestral_alleles">Ancestral Alleles</a>},
+  }, {
+    database  => 'BioMart',
+    mysql     => qq{<a rel="external" title="$title{'mysql'}" href="ftp://ftp.ensembl.org/pub/$rel/mysql/">MySQL</a>},
+    emf       => '-',
+    bed       => '-',
+    xml       => '-',
+    ancestral => '-',
+  }], { cellpadding => 4, cellspacing => 2 })->render, $table->render);
 }
 
-#Lookup for the types we need for species
+# Lookup for the types we need for species
 sub required_types_for_species {
-  my ($self) = @_;
+  my $self = shift;
   my %required_lookup;
   
-  #EMF
-  $required_lookup{var1} = { map { $_ => 1} qw/
+  # EMF
+  $required_lookup{'var1'} = { map { $_ => 1 } qw(
     homo_sapiens mus_musculus rattus_norvegicus
-  / };
+  )};
   
-  #GVF
-  $required_lookup{var2} = { map { $_ => 1 } qw/
+  # GVF
+  $required_lookup{'var2'} = { map { $_ => 1 } qw(
     bos_taurus canis_familiaris danio_rerio drosophila_melanogaster 
     equus_caballus felis_catus gallus_gallus homo_sapiens 
     saccharomyces_cerevisiae monodelphis_domestica mus_musculus 
     ornithorhynchus_anatinus pan_troglodytes pongo_pygmaeus 
     rattus_norvegicus sus_scrofa taeniopygia_guttata tetraodon_nigroviridis 
     pongo_abelii
-  / };
+  )};
   
-  #VEP
-  $required_lookup{var3} = { map { $_ => 1 } qw/
+  # VEP
+  $required_lookup{'var3'} = { map { $_ => 1 } qw(
     bos_taurus danio_rerio homo_sapiens mus_musculus rattus_norvegicus
-  / };
+  )};
   
-  #Funcgen
-  $required_lookup{funcgen} = { map { $_ => 1 } qw/
+  # Funcgen
+  $required_lookup{'funcgen'} = { map { $_ => 1 } qw(
     homo_sapiens mus_musculus
-  / };
+  )};
   
-  #Funcgen files
-  $required_lookup{files} = { map { $_ => 1 } qw/
+  # Funcgen files
+  $required_lookup{'files'} = { map { $_ => 1 } qw(
     homo_sapiens mus_musculus
-  / };
+  )};
   
-  #BAM
-  $required_lookup{bam} = { map { $_ => 1 } qw/
+  # BAM
+  $required_lookup{'bam'} = { map { $_ => 1 } qw(
     pan_troglodytes
-  / };
+  )};
   
   return \%required_lookup;
 }
--- modules/EnsEMBL/Web/Document/HTML/MovieList.pm
+++ modules/EnsEMBL/Web/Document/HTML/MovieList.pm
@@ -1,51 +1,40 @@
+# $Id$
+
 package EnsEMBL::Web::Document::HTML::MovieList;
 
 ### This module outputs a selection of news headlines for the home page, 
 ### based on the user's settings or a default list
 
 use strict;
-use warnings;
 
 use EnsEMBL::Web::Hub;
 use EnsEMBL::Web::DBSQL::WebsiteAdaptor;
-use EnsEMBL::Web::Document::SpreadSheet;
+use EnsEMBL::Web::Document::Table;
 
 use base qw(EnsEMBL::Web::Document::HTML);
 
 sub render {
-  my $self = shift;
-  my $html;
-
-  my $hub           = EnsEMBL::Web::Hub->new;
-  my $adaptor       = EnsEMBL::Web::DBSQL::WebsiteAdaptor->new($hub);
-  my $static_server = $hub->species_defs->ENSEMBL_STATIC_SERVER;
-  my @movies        = @{$adaptor->fetch_movies};
+  my $self    = shift;
+  my $hub     = new EnsEMBL::Web::Hub;
+  my $adaptor = new EnsEMBL::Web::DBSQL::WebsiteAdaptor($hub);
   
-  $html .= qq{<p class="space-below">The tutorials listed below are Flash animations of some of our training presentations.
-              We are gradually adding to the list, so please check back regularly.</p>
-              <p><a href="http://www.youtube.com/user/EnsemblHelpdesk"><img src="$static_server/img/youtube.png" height="54"
-              width="85" alt="YouTube" title="Youtube" style="float:left;padding:0px 10px 10px 0px;" /></a>Note that we are
-              now hosting all our tutorials on <a href="http://www.youtube.com/user/EnsemblHelpdesk">YouTube</a> (and
-              <a href="http://u.youku.com/Ensemblhelpdesk" title="YouKu">&#20248;&#37239;&#32593;</a> for users in China)
-              for ease of maintenance. A selection of tutorials is also available on the
-              <a href="http://www.ebi.ac.uk/2can/evideos/index.html">EBI E-Video website</a>.</p>};
+  my $table = new EnsEMBL::Web::Document::Table([
+    { key => 'title', title => 'Title',                  width => '60%', align => 'left' },
+    { key => 'mins',  title => 'Running time (minutes)', width => '20%', align => 'left' },
+  ]);
 
-  my $table = EnsEMBL::Web::Document::SpreadSheet->new();
-
-  $table->add_columns(
-      {'key' => "title", 'title' => 'Title', 'width' => '60%', 'align' => 'left' },
-      {'key' => "mins", 'title' => 'Running time (minutes)', 'width' => '20%', 'align' => 'left' },
-  );
-
-  foreach my $movie (@movies) {
-    next unless $movie->{'youtube_id'};
-    my $title_link = sprintf(qq(<a href="/Help/Movie?id=%s" class="popup">%s</a>\n), $movie->{'id'}, $movie->{'title'});
-    $table->add_row( { 'title'  => $title_link, 'mins' => $movie->{'length'} } );
-
-  }
-  $html .= $table->render;
-
-  return $html;
+  $table->add_row({ title => sprintf('<a href="/Help/Movie?id=%s" class="popup">%s</a>', $_->{'id'}, $_->{'title'}), mins => $_->{'length'} }) for grep $_->{'youtube_id'}, @{$adaptor->fetch_movies};
+  
+  return sprintf(qq{
+    <p class="space-below">The tutorials listed below are Flash animations of some of our training presentations. We are gradually adding to the list, so please check back regularly.</p>
+    <p>
+      <a href="http://www.youtube.com/user/EnsemblHelpdesk"><img src="%s/img/youtube.png" height="54" width="85" alt="YouTube" title="Youtube" style="float:left;padding:0px 10px 10px 0px;" /></a>
+      Note that we are now hosting all our tutorials on <a href="http://www.youtube.com/user/EnsemblHelpdesk">YouTube</a> 
+      (and <a href="http://u.youku.com/Ensemblhelpdesk" title="YouKu">&#20248;&#37239;&#32593;</a> for users in China) for ease of maintenance. 
+      A selection of tutorials is also available on the <a href="http://www.ebi.ac.uk/2can/evideos/index.html">EBI E-Video website</a>.
+    </p>
+    %s
+  }, $hub->species_defs->ENSEMBL_STATIC_SERVER, $table->render);
 }
 
 1;
