921df0b940290d348160dc38a54d905a56ff91ec
Simon Brent <sb23@sanger.ac.uk>
2012-04-26 11:24:25 +0000

    get correct labels for patch alignments in the URL once you move out of the region they exist in

--- modules/EnsEMBL/Web/Component/Location/MultiSpeciesSelector.pm
+++ modules/EnsEMBL/Web/Component/Location/MultiSpeciesSelector.pm
@@ -26,21 +26,39 @@ sub content_ajax {
   my $params          = $hub->multi_params; 
   my $alignments      = $species_defs->multi_hash->{'DATABASE_COMPARA'}->{'ALIGNMENTS'} || {};
   my $primary_species = $hub->species;
+  my $species_label   = $species_defs->species_label($primary_species, 1);
   my %shown           = map { $params->{"s$_"} => $_ } grep s/^s(\d+)$/$1/, keys %$params; # get species (and parameters) already shown on the page
   my $object          = $self->object;
+  my $chr             = $object->seq_region_name;
   my $start           = $object->seq_region_start;
   my $end             = $object->seq_region_end;
-  my $intra_species   = ($hub->species_defs->multi_hash->{'DATABASE_COMPARA'}{'INTRA_SPECIES_ALIGNMENTS'} || {})->{'REGION_SUMMARY'}{$primary_species}{$object->seq_region_name};
+  my $intra_species   = ($hub->species_defs->multi_hash->{'DATABASE_COMPARA'}{'INTRA_SPECIES_ALIGNMENTS'} || {})->{'REGION_SUMMARY'}{$primary_species};
   my $chromosomes     = $species_defs->ENSEMBL_CHROMOSOMES;
-  my %species;
+  my (%species, %included_regions);
   
-  foreach my $alignment (grep $start < $_->{'end'} && $end > $_->{'start'}, @$intra_species) {
+  foreach my $alignment (grep $start < $_->{'end'} && $end > $_->{'start'}, @{$intra_species->{$object->seq_region_name}}) {
     my $type = lc $alignment->{'type'};
     my ($s)  = grep /--$alignment->{'target_name'}$/, keys %{$alignment->{'species'}};
     my ($sp, $target) = split '--', $s;
     s/_/ /g for $type, $target;
     
     $species{$s} = $species_defs->species_label($sp, 1) . (grep($target eq $_, @$chromosomes) ? ' chromosome' : '') . " $target - $type";
+  }
+  
+  foreach (grep !$species{$_}, keys %shown) {
+    my ($sp, $target) = split '--';
+    $included_regions{$target} = $intra_species->{$target} if $sp eq $primary_species;
+  }
+  
+  foreach my $target (keys %included_regions) {
+    my $s     = "$primary_species--$target";
+    my $label = $species_label . (grep($target eq $_, @$chromosomes) ? ' chromosome' : '');
+    
+    foreach (grep $_->{'target_name'} eq $chr, @{$included_regions{$target}}) {
+      (my $type = lc $_->{'type'}) =~ s/_/ /g;
+      (my $t    = $target)         =~ s/_/ /g;
+      $species{$s} = "$label $t - $type";
+    }
   }
   
   foreach my $alignment (grep { $_->{'species'}{$primary_species} && $_->{'class'} =~ /pairwise/ } values %$alignments) {
@@ -61,7 +79,7 @@ sub content_ajax {
   
   if ($shown{$primary_species}) {
     my ($chr) = split ':', $params->{"r$shown{$primary_species}"};
-    $species{$primary_species} = $species_defs->species_label($primary_species, 1) . " - chromosome $chr";
+    $species{$primary_species} = "$species_label - chromosome $chr";
   }
   
   $self->{'all_options'}      = \%species;
