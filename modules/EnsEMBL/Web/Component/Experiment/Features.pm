a9d8fca47b97fc9c4d59328080194e5b90ede0d7
Harpreet Riat <hr5@sanger.ac.uk>
2012-04-27 15:32:44 +0000

    experiments can be viewed wrt feature type name; little apply/remove filter links added to the table

--- modules/EnsEMBL/Web/Component/Experiment/Features.pm
+++ modules/EnsEMBL/Web/Component/Experiment/Features.pm
@@ -35,17 +35,44 @@ sub content {
     my $project_name  = encode_entities($feature_set_info->{'project_name'});
     my $project_link  = encode_entities($feature_set_info->{'project_url'} || '');
     my $evidence_type = encode_entities($feature_set_info->{'evidence_label'});
+    my $ctype_name    = encode_entities($feature_set_info->{'cell_type_name'});
+    my $ftype_name    = encode_entities($feature_set_info->{'feature_type_name'});
 
     $source_link    ||= $self->srx_link($source_label) if $source_label =~ /^SRX/;
     $evidence_type    =~ s/\s/&nbsp;/g;
     $project_name     =~ s/\s/&nbsp;/g;
 
+    my $filters       = {
+      'project_name'    => $object->is_filter_applied('project', $feature_set_info->{'project_name'}),
+      'evidence_label'  => $object->is_filter_applied('evidence_type', $feature_set_info->{'evidence_label'}),
+      'cell_type'       => $object->is_filter_applied('cell_type', $feature_set_info->{'cell_type_name'}),
+      'feature_type'    => $object->is_single_feature_type_name_view
+    };
+
     $table->add_row({
       'source'        => $source_link  ? sprintf('<a href="%s">%s</a>', $source_link, $source_label) : $source_label,
-      'project'       => $project_link ? sprintf('<a href="%s">%s</a>', $project_link, $project_name) : $project_name,
-      'evidence_type' => $evidence_type,
-      'cell_type'     => sprintf('<a href="%s">%s</a>', $self->efo_link(encode_entities($feature_set_info->{'efo_id'})), encode_entities($feature_set_info->{'cell_type_name'})),
-      'feature_type'  => encode_entities($feature_set_info->{'feature_type_name'}),
+      'project'       => !$project_name ? '' : sprintf('%s <a class="hover_show" href="%s" title="View all experiments with project: %s">%s filter</a>',
+        $project_link ? sprintf('<a href="%s">%s</a>', $project_link, $project_name) : $project_name,
+        $hub->url({'ex' => $object->get_url_param({'project' => $feature_set_info->{'project_name'}}, $filters->{'project_name'} ? -1 : 0)}),
+        $project_name,
+        $filters->{'project_name'} ? 'remove' : 'apply'
+      ),
+      'evidence_type' => sprintf('%s <a class="hover_show" href="%s" title="View all experiments with evidence type: %1$s">%s filter</a>',
+        $evidence_type,
+        $hub->url({'ex' => $object->get_url_param({'evidence_type' => $feature_set_info->{'evidence_label'}}, $filters->{'evidence_type'} ? -1 : 0)}),
+        $filters->{'evidence_type'} ? 'remove' : 'apply'
+      ),
+      'cell_type'     => sprintf('%s <a class="hover_show" href="%s" title="View all experiments with cell type: %s">%s filter</a>',
+        sprintf('<a href="%s">%s</a>', $self->efo_link(encode_entities($feature_set_info->{'efo_id'})), $ctype_name),
+        $hub->url({'ex' => $object->get_url_param({'cell_type' => $feature_set_info->{'cell_type_name'}}, $filters->{'cell_type'} ? -1 : 0)}),
+        $ctype_name,
+        $filters->{'cell_type'} ? 'remove' : 'apply'
+      ),
+      'feature_type'  => sprintf('%s <a class="hover_show" href="%s" title="View all experiments with feature type name: %1$s">%s filter</a>',
+        $ftype_name,
+        $hub->url({'ex' => $object->get_url_param($filters->{'feature_type'} ? {} : {'ftname' => $feature_set_info->{'feature_type_name'}})}),
+        $filters->{'feature_type'} ? 'remove' : 'apply'
+      ),
       'gene'          => join(', ', map {sprintf('<a href="%s">%s</a>', $hub->url({'type' => 'Gene', 'action' => 'Summary', 'g' => $_}), $_)} @{$feature_set_info->{'xref_genes'}} ),
       'motif'         => join(', ', map {sprintf('<a href="%s">%s</a>', $self->motif_link($_), $_)} @{$feature_set_info->{'binding_motifs'}} ),
     });
@@ -56,6 +83,14 @@ sub content {
   my $html;
   if ($object->is_single_feature_view) {
     $html = "Showing a single experiment out of $total_experiments experiments";
+  }
+  elsif ($object->is_single_feature_type_name_view) {
+    $html = sprintf('%s&nbsp;(<a href="%s" title="Show all experiments">%s</a>)',
+      $shown_experiments
+        ? sprintf('Showing %s experiments with feature type name: %s', $shown_experiments, encode_entities($feature_sets_info->[0]{'feature_type_name'}))
+        : 'No experiment found for the given feature type name.',
+      $hub->url({'ex' => $object->get_url_param({})}),
+      $shown_experiments ? 'Remove Filter' : 'Show all');
   }
   elsif ($total_experiments == $shown_experiments) {
     $html = "Showing all  $total_experiments experiments";
