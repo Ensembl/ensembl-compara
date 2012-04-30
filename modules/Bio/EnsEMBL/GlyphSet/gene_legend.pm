ace324cac0edf7fbd522ecac7bf86a9cf455e756
Simon Brent <sb23@sanger.ac.uk>
2012-04-30 14:14:11 +0000

    changed the way join legends are retrieved and checked

--- modules/Bio/EnsEMBL/GlyphSet/gene_legend.pm
+++ modules/Bio/EnsEMBL/GlyphSet/gene_legend.pm
@@ -13,7 +13,7 @@ sub _init {
   my $columns   = 2;
   my $config    = $self->{'config'};
   my $im_width  = $config->get_parameter('panel_width');
-  my @colours;
+  my ($join, @colours);
   
   return unless $config->{'legend_features'};
   
@@ -22,16 +22,14 @@ sub _init {
   return unless %features;
  
   my ($x, $y) = (0, 0);
-  my ($fontname, $fontsize) = $self->get_font_details('legend');
-  my @res = $self->get_text_width(0, 'X', '', 'font' => $fontname, 'ptsize' => $fontsize);
-  my $th = $res[3];
-  my $pix_per_bp = $self->scalex;
+  my %font        = $self->get_font_details('legend', 1);
+  my $text_height = [ $self->get_text_width(0, 'X', '', %font) ]->[3];
+  my $pix_per_bp  = $self->scalex;
   my %seen;
   
-  foreach my $type (sort { $features{$a}->{'priority'} <=> $features{$b}->{'priority'} } keys %features) {
-    @colours = $type eq 'joins' ? 
-      map { $_, $features{$type}->{'legend'}->{$_} } sort keys %{$features{$type}->{'legend'}} : 
-      @{$features{$type}->{'legend'}};
+  foreach my $type (sort { $features{$a}{'priority'} <=> $features{$b}{'priority'} } keys %features) {
+    $join    = $type eq 'joins';
+    @colours = $join ? map { $_, $features{$type}{'legend'}{$_} } sort keys %{$features{$type}{'legend'}} : @{$features{$type}{'legend'}};
     
     $y++ unless $x == 0;
     $x = 0;
@@ -41,13 +39,11 @@ sub _init {
       
       $seen{"$legend:$colour"} = 1;
       
-      my $join = $legend =~ /orthologue|paralogue|alleles/i;
-      
       $self->push($self->Rect({
         x             => $im_width * $x/$columns,
-        y             => $y * ($th + 3) + ($join ? ($th/2) : 2),
+        y             => $y * ($text_height + 3) + ($join ? $text_height / 2 : 2),
         width         => $box_width, 
-        height        => $join ? 0.5 : $th-2,
+        height        => $join ? 0.5 : $text_height - 2,
         colour        => $colour,
         absolutey     => 1,
         absolutex     => 1,
@@ -56,17 +52,16 @@ sub _init {
       
       $self->push($self->Text({
         x             => $im_width * $x/$columns + $box_width,
-        y             => $y * ($th + 3),
-        height        => $th,
+        y             => $y * ($text_height + 3),
+        height        => $text_height,
         valign        => 'center',
         halign        => 'left',
-        ptsize        => $fontsize,
-        font          => $fontname,
         colour        => 'black',
         text          => " $legend",
         absolutey     => 1,
         absolutex     => 1,
-        absolutewidth => 1
+        absolutewidth => 1,
+        %font
       }));
       
       $x++;
