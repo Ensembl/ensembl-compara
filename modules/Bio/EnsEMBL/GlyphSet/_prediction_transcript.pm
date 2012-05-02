424d666789f4b68b10a7bae3a1971d5fbc202527
Simon Brent <sb23@sanger.ac.uk>
2012-05-02 12:39:50 +0000

    fix features for new Glyphset_transcript methodology

--- modules/Bio/EnsEMBL/GlyphSet/_prediction_transcript.pm
+++ modules/Bio/EnsEMBL/GlyphSet/_prediction_transcript.pm
@@ -6,41 +6,44 @@ use Bio::EnsEMBL::Gene;
 
 use base qw(Bio::EnsEMBL::GlyphSet::_transcript);
 
-sub _das_type {
-  return "prediction_transcript";
-}
+sub _das_type { return 'prediction_transcript'; }
 
 sub _make_gene {
-  my( $self, $transcript ) = @_;
-  my $gene = new Bio::EnsEMBL::Gene();
-  $gene->add_Transcript( $transcript );
+  my ($self, $transcript) = @_;
+  my $gene = new Bio::EnsEMBL::Gene;
+  
+  $gene->add_Transcript($transcript);
+  $gene->stable_id($transcript->stable_id); # fake a stable id so that the data structures returned by features are correct.
+  
   return $gene;
 }
 
 sub features {
-  my $self = shift;
-  my $slice = $self->{'container'};
+  my $self     = shift;
+  my $slice    = $self->{'container'};
   my $db_alias = $self->my_config('db');
-  my $analyses = $self->my_config('logic_names');
 
-  my @genes = map { $self->_make_gene($_) }
-              map { @{$slice->get_all_PredictionTranscripts( $_, $db_alias )||[]} }
-              @$analyses;
-  return \@genes;
+  return $self->SUPER::features(map $self->_make_gene($_), map @{$slice->get_all_PredictionTranscripts($_, $db_alias) || []}, @{$self->my_config('logic_names')});
 }
 
 ## Hacked url for prediction transcripts pt=xxx
 sub href {
   my ($self, $gene, $transcript) = @_;
-  my $db_alias = $self->my_config('db');
-  return $self->_url({'type'=>'Transcript','action'=>'Summary','pt'=>$transcript->stable_id,'g'=>undef,'db'=>$db_alias});
+  
+  return $self->_url({
+    type   => 'Transcript',
+    action => 'Summary',
+    pt     => $transcript->stable_id,
+    g      => undef,
+    db     => $self->my_config('db')
+  });
 }
 
 sub export_feature {
   my $self = shift;
   my ($feature, $transcript_id, $source) = @_;
   
-  return $self->_render_text($feature, "$source prediction", { 'headers' => [ 'transcript_id' ], 'values' => [ $transcript_id ] });
+  return $self->_render_text($feature, "$source prediction", { headers => [ 'transcript_id' ], values => [ $transcript_id ] });
 }
 
 1;
