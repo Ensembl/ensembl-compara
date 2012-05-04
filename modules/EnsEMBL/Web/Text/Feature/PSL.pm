6d01f9f24aa6d8f7a6aeddce1bc9d2c57b293b00
Dan Sheppard <ds23@sanger.ac.uk>
2012-05-04 17:12:05 +0000

    Better PSL CIGAR code (general tidying and simplifying); fix for out-by-factor-of-3 bug when UCSC hack is in place on CIGAR string deletion length.

--- modules/EnsEMBL/Web/Text/Feature/PSL.pm
+++ modules/EnsEMBL/Web/Text/Feature/PSL.pm
@@ -7,6 +7,7 @@ no warnings 'uninitialized';
 use base qw(EnsEMBL::Web::Text::Feature);
 
 use List::Util qw(min);
+use List::MoreUtils qw(pairwise);
 
 sub new {
   my( $class, $args ) = @_;
@@ -72,37 +73,36 @@ sub external_data { my $self = shift; return $self->{'__extra__'} ? $self->{'__e
 sub cigar_string {
   my $self = shift;
   return $self->{'_cigar'} if $self->{'_cigar'};
-  my @raw = @{$self->{'__raw__'}};
+  my $raw = $self->{'__raw__'}; # readonly alias to simplify code
   # extract lists
-  my @qst = split /,/,$raw[19];
-  my @tst = split /,/,$raw[20];
-  my @len = split /,/,$raw[18];
-  my $num_blocks = min($raw[17],scalar @qst,scalar @tst,scalar @len); 
-  # rearrange into (q,t,l),(q,t,l),.... Easier to process.
-  my @aligns = map { { qst => $qst[$_]+0, 
-                       tst => $tst[$_]+0, 
-                       len => $len[$_]+0} } (0 .. $num_blocks-1);
-  return "" unless @aligns;
-  # Sort into tst order (may be in any order) (paranoia)
-  @aligns = sort { $a->{'tst'} <=> $b->{'tst'} } @aligns;
+  my ($len,$qst,$tst) = map [ split /,/, $raw->[$_] ], 18..20;
+  my $num_blocks = min($raw->[17], map { scalar @$_ } ($len,$qst,$tst)); 
+  splice(@$_,$num_blocks) for ($len,$qst,$tst);
+  # Sort all three into tst order (may be in any order) (paranoia)
+  # Uses modified Schwartzian Transform
+  $qst = [ map { $_->[1] } sort { $a->[0] <=> $b->[0] } pairwise { [$a,$b] } @$tst,@$qst ];
+  $len = [ map { $_->[1] } sort { $a->[0] <=> $b->[0] } pairwise { [$a,$b] } @$tst,@$len ];
+  $tst = [ sort @$tst ];
+  return "" unless $num_blocks;
   # If multiplying size of last block by three would take us to the end,
   # do so (assuming AA-block sizes rather than bases). Same hack at UCSC.
-  my $multiplier = 1;
-  my $block_end = $raw[16]; # +ve strand
-  $block_end = $raw[14]-$raw[15] if($self->strand < 0); # -ve strand, uses a == x-b iff x-a == b
-  $multiplier = 3 if(($aligns[-1]->{'tst'}+$aligns[-1]->{'len'}*3) == $block_end);
+  my $block_end = $raw->[16]; # +ve strand
+  $block_end = $raw->[14]-$raw->[15] if($self->strand < 0); # -ve strand, uses a == x-b iff x-a == b
+  if(($tst->[-1]+$len->[-1]*3) == $block_end) {
+    $_*= 3 for(@$qst,@$len);
+  }
   # Roll cigar
   my @cigar;
   foreach (0..$num_blocks-1) {
-    push @cigar,[$aligns[$_]->{'len'} * $multiplier,"M"];
+    push @cigar,[$len->[$_],"M"];
     if($_ < $num_blocks-1) {
-      push @cigar,[$aligns[$_+1]->{'tst'} - $aligns[$_]->{'tst'} - $aligns[$_]->{'len'} * $multiplier,"I"];
-      push @cigar,[$aligns[$_+1]->{'qst'} - $aligns[$_]->{'qst'} - $aligns[$_]->{'len'},"D"];
+      push @cigar,[$tst->[$_+1] - $tst->[$_] - $len->[$_],"I"];
+      push @cigar,[$qst->[$_+1] - $qst->[$_] - $len->[$_],"D"];
     }
   }
   @cigar = grep { $_->[0] } @cigar; # delete 0X
   for(@cigar) { $_->[0]='' if($_->[0] == 1); } # map 1X to X
-  return $self->{'_cigar'} = join("",map { $_->[0].$_->[1] } @cigar);
+  return $self->{'_cigar'} = join("",map { join("",@$_) } @cigar);
 }
 
 1;
