0223962bdded9fbe4c4d25b779452a6bf02fea4d
Leo Gordon <lg4@ebi.ac.uk>
2012-05-04 16:09:12 +0000

    switching to new Runnable API

--- modules/Bio/EnsEMBL/Compara/Production/EPOanchors/HMMerAnchors.pm
+++ modules/Bio/EnsEMBL/Compara/Production/EPOanchors/HMMerAnchors.pm
@@ -43,40 +43,30 @@ Internal methods are usually preceded with a _
 package Bio::EnsEMBL::Compara::Production::EPOanchors::HMMerAnchors;
 
 use strict;
-use Data::Dumper;
-use Bio::EnsEMBL::Compara::Production::DBSQL::DBAdaptor;
-use Bio::EnsEMBL::Hive::Process;
-use Bio::EnsEMBL::Utils::Exception qw(throw warning);
-use Bio::EnsEMBL::Analysis;
 use Bio::AlignIO;
+use Bio::EnsEMBL::Utils::Exception qw(throw warning);
 
-our @ISA = qw(Bio::EnsEMBL::Hive::Process);
+use base('Bio::EnsEMBL::Compara::RunnableDB::BaseRunnable');
 
-
-sub configure_defaults {
- 	my $self = shift;
-	my $nhmmer = "/software/ensembl/compara/hmmer3.1_nhmmer_beta2/src/nhmmer";
-	my $hmmbuild = "/software/ensembl/compara/hmmer3.1_nhmmer_beta2/src/hmmbuild";
-	$self->analysis->program("{ nhmmer=>\"$nhmmer\", hmmbuild=>\"$hmmbuild\" }") unless $self->analysis->program;
-  	return 1;
-}
 
 sub fetch_input {
 	my ($self) = @_;
-	$self->configure_defaults();
-	$self->get_parameters($self->parameters);
-	#create a Compara::DBAdaptor which shares the same DBI handle with $self->db (Hive DBAdaptor)
-	$self->{'comparaDBA'} = Bio::EnsEMBL::Compara::Production::DBSQL::DBAdaptor->new(-DBCONN=>$self->db->dbc);
-	$self->{'comparaDBA'}->dbc->disconnect_if_idle();
-	$self->get_input_id($self->input_id);
-	my $genomic_align_block_adaptor = $self->{comparaDBA}->get_GenomicAlignBlockAdaptor();
-	my $align_slice_adaptor = $self->{comparaDBA}->get_AlignSliceAdaptor();
+
+	my $nhmmer = "/software/ensembl/compara/hmmer3.1_nhmmer_beta2/src/nhmmer";
+	my $hmmbuild = "/software/ensembl/compara/hmmer3.1_nhmmer_beta2/src/hmmbuild";
+	$self->analysis->program("{ nhmmer=>\"$nhmmer\", hmmbuild=>\"$hmmbuild\" }") unless $self->analysis->program;
+
+	$self->compara_dba->dbc->disconnect_if_idle();
+
+	my $genomic_align_block_adaptor = $self->compara_dba->get_GenomicAlignBlockAdaptor();
+	my $align_slice_adaptor = $self->compara_dba->get_AlignSliceAdaptor();
 	my $analysis_data_adaptor = $self->db->get_AnalysisDataAdaptor();
-	my $target_genome_files = eval $analysis_data_adaptor->fetch_by_dbID($self->analysis_data_id);
-	$self->target_file( $target_genome_files->{target_genomes}->{ $self->target_genome_db_id } );
-	my $genomic_align_block_id = $self->genomic_align_block_id;
+
+	my $target_genome_files = eval $analysis_data_adaptor->fetch_by_dbID($self->param('analysis_data_id') );
+	$self->param('target_file', $target_genome_files->{target_genomes}->{ $self->param('target_genome_db_id') } );
+	my $genomic_align_block_id = $self->param('genomic_align_block_id');
 	my $genomic_align_block = $genomic_align_block_adaptor->fetch_by_dbID($genomic_align_block_id);
-	$self->anchor_id( $genomic_align_block_id );
+	$self->param('anchor_id', $genomic_align_block_id );
 	my $align_slice = $align_slice_adaptor->fetch_by_GenomicAlignBlock($genomic_align_block, 1, 0);
 	my $simple_align = $align_slice->get_SimpleAlign(); 
 	my $stockholm_file = $self->worker_temp_directory . "stockholm." . $genomic_align_block_id;
@@ -90,28 +80,26 @@ sub fetch_input {
 	close(F);
 	#build the hmm from the stockholm format file
 	my $hmm_programs = eval $self->analysis->program;
-	$self->nhmmer($hmm_programs->{nhmmer});
+	$self->param('nhmmer', $hmm_programs->{nhmmer});
 	my $hmmbuild_outfile = $self->worker_temp_directory . "$genomic_align_block_id.hmm";
 	my $hmmbuild = $hmm_programs->{hmmbuild}; 
 	my $hmmbuild_command = "$hmmbuild --dna $hmmbuild_outfile $stockholm_file";
 	system($hmmbuild_command);
-	$self->query_file($hmmbuild_outfile);
-	return 1;
+	$self->param('query_file', $hmmbuild_outfile);
 }
 
 sub run {
 	my ($self) = @_;
-	my $nhmmer_command = join(" ", $self->nhmmer, "--noali", $self->query_file, $self->target_file); 
+	my $nhmmer_command = join(" ", $self->param('nhmmer'), "--noali", $self->param('query_file'), $self->param('target_file') ); 
 	my $exo_fh;
 	open( $exo_fh, "$nhmmer_command |" ) or throw("Error opening nhmmer command: $? $!"); #run nhmmer	
-	$self->exo_file($exo_fh);
-	return 1;
+	$self->param('exo_file', $exo_fh);
 }
 
 sub write_output {
 	my ($self) = @_;
-	my $anchor_align_adaptor = $self->{'comparaDBA'}->get_AnchorAlignAdaptor();
-	my $exo_fh = $self->exo_file;
+	my $anchor_align_adaptor = $self->compara_dba->get_AnchorAlignAdaptor();
+	my $exo_fh = $self->param('exo_file');
 	my (@hits, %hits);
 	{ local $/ = ">>";
 		while(my $mapping = <$exo_fh>){ 
@@ -125,134 +113,13 @@ sub write_output {
 		my($score, $evalue, $alifrom, $alito) = (split(/ +/, $mapping_info))[2,4,8,9];
 		my $strand = $alifrom > $alito ? "-1" : "1";
 		($alifrom, $alito) = ($alito, $alifrom) if $strand eq "-1";
-		my $dnafrag_adaptor = $self->{comparaDBA}->get_DnaFragAdaptor();
-		my $dnafrag_id = $dnafrag_adaptor->fetch_by_GenomeDB_and_name($self->target_genome_db_id, $seq_region_name)->dbID;
-		push(@{ $hits{$self->anchor_id} }, [ $dnafrag_id, $alifrom, $alito, $strand, $score, $evalue ]);
+		my $dnafrag_adaptor = $self->compara_dba->get_DnaFragAdaptor();
+		my $dnafrag_id = $dnafrag_adaptor->fetch_by_GenomeDB_and_name($self->param('target_genome_db_id'), $seq_region_name)->dbID;
+		push(@{ $hits{$self->param('anchor_id')} }, [ $dnafrag_id, $alifrom, $alito, $strand, $score, $evalue ]);
 	}
 #	$anchor_align_adaptor->store_exonerate_hits($records);
-	return 1;
 }
 
-
-sub anchor_id {
-	my $self = shift;
-	if (@_) {
-		$self->{anchor_id} = shift;
-	}
-	return $self->{anchor_id};
-}
-
-sub nhmmer {
-	my $self = shift;
-	if (@_) {
-		$self->{nhmmer} = shift;
-	}
-	return $self->{nhmmer};
-}
-
-sub exo_file {
-	my $self = shift;
-	if (@_) {
-		$self->{_exo_file} = shift;
-	}
-	return $self->{_exo_file};
-}
-
-sub analysis_id {
-	my $self = shift;
-	if (@_) {
-		$self->{_analysis_id} = shift;
-	}
-	return $self->{_analysis_id};
-}
-
-sub analysis_data_id {
-	my $self = shift;
-	if (@_) {
-		$self->{_analysis_data_id} = shift;
-	}
-	return $self->{_analysis_data_id};
-}
-
-sub query_file {
-	my $self = shift;
-	if (@_) {
-		$self->{_query_file} = shift;
-	}
-	return $self->{_query_file};
-}		
-
-sub genomic_align_block_id {
-	my $self = shift;
-	if (@_) {
-		$self->{_genomic_align_block_id} = shift;
-	}
-	return $self->{_genomic_align_block_id};
-}
-
-sub target_file {
-	my $self = shift;
-	if (@_){
-		$self->{_target_file} = shift;
-	}
-	return $self->{_target_file};
-}
-
-sub target_genome_db_id {
-	my $self = shift;
-	if (@_){
-		$self->{_target_genome_db_id} = shift;
-	}
-	return $self->{_target_genome_db_id};
-}
-
-sub anchor_sequences_mlssid {
-	my $self = shift;
-	if (@_){
-		$self->{_anchor_sequences_mlssid} = shift;
-	}
-	$self->{_anchor_sequences_mlssid};
-}
-
-sub get_parameters {
-	my $self = shift;
-	my $param_string = shift;
-	
-	return unless($param_string);
-	my $params = eval($param_string);
-	$self->exonerate_options($params);
-}
-
-sub get_input_id {
-	my $self = shift;
-	my $input_id_string = shift;
-
-	return unless($input_id_string);
-	print("parsing input_id string : ",$input_id_string,"\n");
-	
-	my $params = eval($input_id_string);
-	return unless($params);
-	
-	if(defined($params->{'genomic_align_block_id'})) {
-		$self->genomic_align_block_id($params->{'genomic_align_block_id'});
-	}
-	if(defined($params->{'target_genome_db_id'})) {
-		$self->target_genome_db_id($params->{'target_genome_db_id'});
-	}
-	if(defined($params->{'analysis_data_id'})) {
-		$self->analysis_data_id($params->{'analysis_data_id'});
-	}
-	if(defined($params->{'analysis_id'})) {
-		$self->analysis_id($params->{'analysis_id'});
-	}
-	if(defined($params->{'exonerate_mlssid'})) {
-		$self->exonerate_mlssid($params->{'exonerate_mlssid'});
-	}
-	if(defined($params->{'anchor_sequences_mlssid'})) {
-		$self->anchor_sequences_mlssid($params->{'anchor_sequences_mlssid'});
-	}
-	return 1;
-}
 
 1;
 
