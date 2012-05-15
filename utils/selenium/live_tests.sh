8c6f795e17e6cf07a2dddcee800a3a16dd4017de
ridwan <ma7@sanger.ac.uk>
2012-05-15 08:19:44 +0000

    new file for running selenium test on live only

---
+++ utils/selenium/live_tests.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+perl run_tests.pl --module FTPtable --url http://www.ensembl.org --host 172.20.10.187 --port 4444 > "test_reports/FTPtable.txt" 2>&1
+perl run_tests.pl --module Blast --url http://www.ensembl.org --host 172.20.10.187 --port 4444 > "test_reports/Blast.txt" 2>&1
