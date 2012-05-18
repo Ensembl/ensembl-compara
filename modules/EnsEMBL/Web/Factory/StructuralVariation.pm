028163d93e30239861df0bdfceae5a2a4ced26e3
Simon Brent <sb23@sanger.ac.uk>
2012-05-18 13:37:08 +0000

    tidied

--- modules/EnsEMBL/Web/Factory/StructuralVariation.pm
+++ modules/EnsEMBL/Web/Factory/StructuralVariation.pm
@@ -9,35 +9,25 @@ use HTML::Entities qw(encode_entities);
 use base qw(EnsEMBL::Web::Factory);
 
 sub createObjects {
-  my $self      = shift; 
-  my $structural_variation = shift; 
-  my $identifier;
+  my $self                 = shift;
+  my $structural_variation = shift;
+  my $db                   = $self->database('variation');
+  my $identifier           = $self->param('sv');
   
-  my $db = $self->species_defs->databases->{'DATABASE_VARIATION'};
   return $self->problem ('fatal', 'Database Error', 'There is no variation database for this species.') unless $db;
    
   if (!$structural_variation) {
-    my $dbs  = $self->hub->get_databases(qw(core variation));;
-    
-    return $self->problem('fatal', 'Database Error', 'Could not connect to the core database.') unless $dbs;
-    
-    my $variation_db = $dbs->{'variation'};
-    $variation_db->include_failed_variations(1);
-     
-    return $self->problem('fatal', 'Database Error', 'Could not connect to the variation database.') unless $variation_db;
-    
-    $identifier = $self->param('sv');
-    
     return $self->problem('fatal', 'Structural Variation ID required', $self->_help('A structural variation ID is required to build this page.')) unless $identifier;
     
-    $structural_variation = $variation_db->get_StructuralVariationAdaptor->fetch_by_name($identifier);
+    $db->include_failed_variations(1);
+    
+    $structural_variation = $db->get_StructuralVariationAdaptor->fetch_by_name($identifier);
   }
   
   if ($structural_variation) { 
     $self->DataObjects($self->new_object('StructuralVariation', $structural_variation, $self->__data));
     
-    my @svf           = $self->param('svf');
-    
+    my @svf          = $self->param('svf');
     my @sv_features  = @{$structural_variation->get_all_StructuralVariationFeatures};
     my ($sv_feature) = scalar @sv_features == 1 ? $sv_features[0] : $svf[0] ? grep $_->dbID eq $svf[0], @sv_features : undef;
     
@@ -51,13 +41,13 @@ sub createObjects {
     
     $self->param('vdb', 'variation');
     $self->param('sv', $structural_variation->variation_name) unless $self->param('sv'); # For same reason as svf check above;
-  } else { 
-    my $dbsnp_version = "";
-    if ( $self->species_defs->databases->{'DATABASE_VARIATION'}->{'dbSNP_VERSION'}){
-      $dbsnp_version = "which includes data from dbSNP ". $self->species_defs->databases->{'DATABASE_VARIATION'}->{'dbSNP_VERSION'} .',';
-    }
-    my $help_message = "Either $identifier does not exist in the current Ensembl database, $dbsnp_version or there was a problem retrieving it.";
-    return $self->problem('fatal', "Could not find structural variation $identifier", $self->_help($help_message));
+  } else {
+    my $db_version = $self->species_defs->databases->{'DATABASE_VARIATION'}{'dbSNP_VERSION'};
+    
+    return $self->problem('fatal', "Could not find structural variation $identifier", $self->_help(sprintf(
+      'Either %s does not exist in the current Ensembl database, %sor there was a problem retrieving it.',
+      $identifier, $db_version ? "which includes data from dbSNP $db_version, " : ''
+    )));
   }
 }
 
