7a2a98fde5a631f28a5d7d4de8316490fed1599e
Matthieu Muffato <muffato@ebi.ac.uk>
2012-05-21 15:24:10 +0000

    parent() returns undef by default

--- modules/Bio/EnsEMBL/Compara/NestedSet.pm
+++ modules/Bio/EnsEMBL/Compara/NestedSet.pm
@@ -228,6 +228,7 @@ sub parent {
     my $self = shift;
     return $self->adaptor->fetch_parent_for_node($self) if defined $self->adaptor;
     return $self->{'_parent_link'}->get_neighbor($self) if defined $self->{'_parent_link'};
+    return undef;
 }
 
 
