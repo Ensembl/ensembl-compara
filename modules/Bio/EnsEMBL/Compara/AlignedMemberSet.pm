4cdd7457b43f4534ec6185101435c070d445be2f
Matthieu Muffato <muffato@ebi.ac.uk>
2012-06-03 23:35:08 +0000

    New method print_sequences_to_fasta()

--- modules/Bio/EnsEMBL/Compara/AlignedMemberSet.pm
+++ modules/Bio/EnsEMBL/Compara/AlignedMemberSet.pm
@@ -841,6 +841,30 @@ sub has_species_by_name {
 ######################
 
 
+sub print_sequences_to_fasta {
+    my ($self, $pep_file) = @_;
+    my $pep_counter = 0;
+    open PEP, ">$pep_file";
+    foreach my $member (@{$self->get_all_AlignedMember}) {
+        next if $member->source_name eq 'ENSEMBLGENE';
+        my $member_stable_id = $member->stable_id;
+        my $seq = $member->sequence;
+
+        print PEP ">$member_stable_id\n";
+        $seq =~ s/(.{72})/$1\n/g;
+        chomp $seq;
+        unless (defined($seq)) {
+            my $family_id = $self->dbID;
+            die "member $member_stable_id in family $family_id doesn't have a sequence";
+        }
+        print PEP $seq,"\n";
+        $pep_counter++;
+    }
+    close PEP;
+    return $pep_counter;
+}
+
+
 =head2 read_clustalw
 
   Arg [1]    : string $file 
