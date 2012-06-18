e19f337a7c617d1b3959e7b85d5cb4579843927d
Matthieu Muffato <muffato@ebi.ac.uk>
2012-06-18 12:05:36 +0000

    Faster + only uses the clusterset_id "default"

--- sql/tree-stats.sql
+++ sql/tree-stats.sql
@@ -13,32 +13,37 @@
 	rel65/nc: 15 sec
 */
 
-CREATE TEMPORARY TABLE tmp_coverage
+CREATE TEMPORARY TABLE tmp_ngenes
+	SELECT
+		taxon_id,
+		SUM(source_name='ENSEMBLGENE') AS nb_genes,
+		SUM(source_name='ENSEMBLPEP') AS nb_pep
+	FROM
+		member
+	GROUP BY
+		taxon_id
+	WITH ROLLUP;
+ALTER TABLE tmp_ngenes ADD KEY(taxon_id);
+OPTIMIZE TABLE tmp_ngenes;
+
+CREATE TEMPORARY TABLE tmp_ntrees
 	SELECT
 		member.taxon_id,
-		SUM(source_name='ENSEMBLGENE') AS nb_genes,
-		SUM(source_name='ENSEMBLPEP') AS nb_pep,
 		SUM(gene_tree_node_attr.taxon_id = member.taxon_id) AS nb_pep_spectree,
 		SUM(gene_tree_node_attr.taxon_id IS NOT NULL AND gene_tree_node_attr.taxon_id != member.taxon_id) AS nb_pep_anctree
 	FROM
 		member
-		LEFT JOIN gene_tree_member USING (member_id)
-		LEFT JOIN gene_tree_node USING (node_id)
-		LEFT JOIN gene_tree_node_attr ON (gene_tree_node.root_id = gene_tree_node_attr.node_id)
+		JOIN gene_tree_member USING (member_id)
+		JOIN gene_tree_node USING (node_id)
+		JOIN gene_tree_root USING (root_id)
+		JOIN gene_tree_node_attr ON (gene_tree_node.root_id = gene_tree_node_attr.node_id)
+	WHERE
+		clusterset_id = 'default'
 	GROUP BY
-		member.taxon_id;
-ALTER TABLE tmp_coverage ADD PRIMARY KEY (taxon_id);
-OPTIMIZE TABLE tmp_coverage;
-
-CREATE TEMPORARY TABLE tmp_coverage_sum
-	SELECT
-		NULL AS taxon_id,
-		SUM(nb_genes) AS nb_genes,
-		SUM(nb_pep) AS nb_pep,
-		SUM(nb_pep_spectree) AS nb_pep_spectree,
-		SUM(nb_pep_anctree) AS nb_pep_anctree
-	FROM
-		tmp_coverage;
+		member.taxon_id
+	WITH ROLLUP;
+ALTER TABLE tmp_ntrees ADD KEY(taxon_id);
+OPTIMIZE TABLE tmp_ntrees;
 
 SELECT
 	IF(taxon_id, taxon_id, "") AS taxon_id,
@@ -51,20 +56,19 @@ SELECT
 	ROUND(100*nb_pep_spectree/nb_genes, 2) AS perc_pep_spectree,
 	ROUND(100*nb_pep_anctree/nb_genes, 2) AS perc_pep_anctree,
 	ROUND(100*(nb_genes-nb_pep_spectree-nb_pep_anctree)/nb_genes, 2) AS perc_pep_orphan
+
 FROM
-	(
-		SELECT * FROM tmp_coverage
-		UNION ALL
-		SELECT * FROM tmp_coverage_sum
-	) tt 
+	tmp_ngenes
+	JOIN tmp_ntrees USING (taxon_id)
 	LEFT JOIN ncbi_taxa_name USING (taxon_id)
 	LEFT JOIN ncbi_taxa_node USING (taxon_id)
 WHERE
 	name_class IS NULL
 	OR name_class = "scientific name"
 ORDER BY
-	IF(left_index, left_index, 1e7)
-;
+	IF(left_index, left_index, 1e7);
+
+
 
 /*
 	Tree properties, grouped by root species
@@ -83,7 +87,10 @@ CREATE TEMPORARY TABLE tmp_root_properties
 		gene_tree_member
 		JOIN member USING (member_id)
 		JOIN gene_tree_node USING (node_id)
+		JOIN gene_tree_root USING (root_id)
 		JOIN gene_tree_node_attr ON gene_tree_node_attr.node_id=gene_tree_node.root_id
+	WHERE
+		clusterset_id = 'default'
 	GROUP BY
 		gene_tree_node_attr.node_id;
 ALTER TABLE tmp_root_properties ADD KEY (taxon_id);
@@ -131,8 +138,37 @@ ORDER BY
 	rel65/nc: <5 sec
 */
 
+CREATE TEMPORARY TABLE tmp_taxa
+	(taxon_id INT NOT NULL PRIMARY KEY, taxon_name VARCHAR(255))
+	SELECT
+		DISTINCT taxon_id, taxon_name
+	FROM
+		gene_tree_node_attr
+	UNION ALL
+		SELECT 1e7+1, "Total (ancestral species)"
+	UNION ALL
+		SELECT 1e7+2, "Total (extant species)"
+	UNION ALL
+		SELECT 1e7+3, "Total";
+OPTIMIZE TABLE tmp_taxa;
+
+
+CREATE TEMPORARY TABLE tmp_stats
+	SELECT
+		taxon_id, taxon_name AS ref_taxon_name, node_type, duplication_confidence_score, bootstrap
+	FROM
+		gene_tree_node_attr
+		JOIN gene_tree_node USING (node_id)
+		JOIN gene_tree_root USING (root_id)
+	WHERE
+		tree_type = 'tree'
+		AND clusterset_id = 'default';
+ALTER TABLE tmp_stats ADD KEY (taxon_id);
+OPTIMIZE TABLE tmp_stats;
+
+
 SELECT
-	IF(taxon_id<1e7, taxon_id, '') AS taxon_id,
+	IF(sort_taxon_id<1e7, sort_taxon_id, '') AS sort_taxon_id,
 	taxon_name,
 	COUNT(*) AS nb_nodes,
 	SUM(node_type="speciation") AS nb_spec_nodes,
@@ -142,20 +178,26 @@ SELECT
 	ROUND(AVG(duplication_confidence_score),2) AS avg_dupscore,
 	ROUND(AVG(IF(node_type="duplication",duplication_confidence_score,NULL)),2) AS avg_dupscore_nondub
 FROM
-	( 
-	(SELECT taxon_id, taxon_name, node_type, duplication_confidence_score FROM gene_tree_node_attr)
-	UNION ALL
-	(SELECT 1e7+1, "Total (ancestral species)", node_type, duplication_confidence_score FROM gene_tree_node_attr WHERE taxon_name NOT LIKE "% %")
-	UNION ALL
-	(SELECT 1e7+2, "Total (extant species)", node_type, duplication_confidence_score FROM gene_tree_node_attr WHERE taxon_name LIKE "% %")
-	UNION ALL
-	(SELECT 1e7+3, "Total", node_type, duplication_confidence_score FROM gene_tree_node_attr)
+	(
+		SELECT
+			tmp_taxa.taxon_id AS sort_taxon_id,
+			tmp_taxa.taxon_name,
+			tmp_stats.*
+		FROM
+			tmp_taxa
+			JOIN tmp_stats ON (
+				tmp_taxa.taxon_id = tmp_stats.taxon_id
+				OR (tmp_taxa.taxon_id = 1e7+1 AND ref_taxon_name NOT LIKE "% %")
+				OR (tmp_taxa.taxon_id = 1e7+2 AND ref_taxon_name LIKE "% %")
+				OR (tmp_taxa.taxon_id = 1e7+3)
+			)
+
 	) tt
-	LEFT JOIN ncbi_taxa_node USING (taxon_id)
+	LEFT JOIN ncbi_taxa_node ON tt.sort_taxon_id = ncbi_taxa_node.taxon_id
 GROUP BY
-	tt.taxon_id
+	tt.sort_taxon_id
 ORDER BY
-	IF(left_index, left_index, tt.taxon_id)
+	IF(left_index, left_index, tt.sort_taxon_id)
 ;
 
 
