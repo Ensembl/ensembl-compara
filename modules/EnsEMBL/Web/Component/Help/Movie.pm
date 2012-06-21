8c18b48d1fb74a7b2b0c554f9294d3dc1a4d62c8
Harpreet Riat <hr5@sanger.ac.uk>
2012-06-21 12:58:24 +0000

    table based layout html removed

--- modules/EnsEMBL/Web/Component/Help/Movie.pm
+++ modules/EnsEMBL/Web/Component/Help/Movie.pm
@@ -45,14 +45,13 @@ sub content {
       $html .= $self->help_feedback($style, $movie->{'id'}, return_url => '/Help/Movie', type => 'Movie');
 
       ## Link to movie-specific feedback form
-      $html .= qq{
-        <div class="info-box" style="float:right;width:50%;padding:10px;margin:5px">
+      $html .= qq(
+        <div class="info-box" style="padding:10px;">
           If you have problems viewing this movie, we would be grateful if you could <a href="/Help/MovieFeedback?title=$movie->{'title'}" class="popup">provide feedback</a> that will help us improve our service. Thank you.
         </div>
-      };
+      );
     }
-    
-    $html .= '</td></tr></table>';
+
   } elsif (scalar @movies > 0 && $movies[0]) {
     $html .= EnsEMBL::Web::Document::HTML::MovieList::render;
   } else {
