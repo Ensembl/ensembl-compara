9ccae5d638478b9eb1cac6e53a7bc44ffbc307c5
Simon Brent <sb23@sanger.ac.uk>
2012-07-05 09:08:16 +0000

    allow the addition of javascript recursively through sub-directories. allow directories other than components to be merged

--- modules/EnsEMBL/Web/Tools/DHTMLmerge.pm
+++ modules/EnsEMBL/Web/Tools/DHTMLmerge.pm
@@ -17,61 +17,38 @@ use CSS::Minifier;
 use JavaScript::Minifier;
 
 sub merge_all {
-  my ($species_defs) = @_;
-  
-  my $css_file = merge($species_defs, 'css');
-  my $js_file  = merge($species_defs, 'js');
+  my $species_defs = shift;
   
   $species_defs->{'_storage'}{'ENSEMBL_JSCSS_TYPE'} = 'minified';
-  $species_defs->{'_storage'}{'ENSEMBL_JS_NAME'}    = $js_file;
-  $species_defs->{'_storage'}{'ENSEMBL_CSS_NAME'}   = $css_file;
+  $species_defs->{'_storage'}{'ENSEMBL_CSS_NAME'}   = merge($species_defs, 'css');
+  $species_defs->{'_storage'}{'ENSEMBL_JS_NAME'}    = merge($species_defs, 'js');
   
   $species_defs->store;
 }
 
 sub merge {
-  my ($species_defs, $type) = @_;
+  my ($species_defs, $type, @subdirs) = @_;
+  my ($contents, $jquery);
   
-  my $contents;
-  my $jquery;
+  unshift @subdirs, 'components'; # Always add the contents of the components directory to be minified, else global variables (jQuery, $, Ensembl) won't be correct
   
-  foreach my $root (reverse @{$species_defs->ENSEMBL_HTDOCS_DIRS||[]}) {
+  foreach my $root (reverse @{$species_defs->ENSEMBL_HTDOCS_DIRS || []}) {
     next if $root =~ /biomart/; # Not part of Ensembl template system
     
-    my $dir = "$root/components";
-    
-    if (-e $dir && -d $dir) {
-      opendir DH, $dir;
-      my @T = readdir DH;
-      my @files = sort grep { /^\d/ && -f "$dir/$_" && /\.$type$/ } @T;
-      closedir DH;
-      
-      foreach my $fn (@files) {      
-        open I, "$dir/$fn";
-        local $/ = undef;
-        my $file_contents = <I>;
-        close I;
-        
-        if ($fn =~ /jquery\.js/) {
-          $jquery = $file_contents;
-        } else {
-          $contents .= $file_contents;
-        }       
-      }
+    foreach (@subdirs) {
+      my @content   = get_contents($type, $root, $_);
+         $contents .= $content[0];
+         $jquery    = $content[1] if $content[1];
     }
   }
   
   # Convert style placeholders to actual colours
-  my %colours = %{$species_defs->ENSEMBL_STYLE||{}};
-  
-  # Add sequence markup colours to ENSEMBL_STYLE - they are used in CSS. This smells a lot like a hack.
-  my $sequence_markup = $species_defs->colour('sequence_markup') || {};
-  my %styles = map { $_ => $sequence_markup->{$_}->{'default'} } keys %$sequence_markup;
-  
-  %colours = (%colours, %styles);
-  $colours{$_} =~ s/^([0-9A-F]{6})$/#$1/i for keys %colours;
-  
-  $contents =~ s/\[\[(\w+)\]\]/$colours{$1}||"\/* ARG MISSING DEFINITION $1 *\/"/eg; 
+  if ($type eq 'css') {
+    my $sequence_markup = $species_defs->colour('sequence_markup') || {}; # Add sequence markup colours to ENSEMBL_STYLE - they are used in CSS. This smells a lot like a hack.
+    my %colours         = (%{$species_defs->ENSEMBL_STYLE || {}}, map { $_ => $sequence_markup->{$_}{'default'} } keys %$sequence_markup);
+       $colours{$_}     =~ s/^([0-9A-F]{6})$/#$1/i for keys %colours;
+       $contents        =~ s/\[\[(\w+)\]\]/$colours{$1}||"\/* ARG MISSING DEFINITION $1 *\/"/eg; 
+  }
   
   my $root_dir        = $species_defs->ENSEMBL_SERVERROOT;
   my $compression_dir = "$root_dir/utils/compression/";
@@ -107,6 +84,42 @@ sub merge {
   }
   
   return $filename;
+}
+
+sub get_contents {
+  my ($type, $root, $subdir) = @_;
+  my $dir        = "$root/$subdir";
+  my $components = $dir =~ /components/;
+  my ($contents, $jquery);
+  
+  if (-e $dir && -d $dir) {
+    opendir DH, $dir;
+    my @files = readdir DH;
+    closedir DH;
+    
+    foreach (sort { -d "$dir/$a" <=> -d "$dir/$b" || lc $a cmp lc $b } grep /\w/, @files) {
+      if (-d "$dir/$_") {
+        my @content   = get_contents($type, $root, "$subdir/$_");
+           $contents .= $content[0];
+           $jquery    = $content[1] if $content[1];
+      } elsif (-f "$dir/$_" && /\.$type$/) {
+        next if $components && !/^\d/;
+        
+        open I, "$dir/$_";
+        local $/ = undef;
+        my $file_contents = <I>;
+        close I;
+        
+        if (/jquery\.js/) {
+          $jquery = $file_contents;
+        } else {
+          $contents .= $file_contents;
+        }
+      }
+    }
+  }
+  
+  return ($contents, $jquery);
 }
 
 1;
