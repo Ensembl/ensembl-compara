8a33dcb02dbba5a290b7f2ef27272946b881398d
Anne Parker <ap5@sanger.ac.uk>
2012-07-09 08:53:38 +0000

    Removed hyperlink from share URL, because we want people to use that as-is, not follow the link and copy from the browser address bar

--- modules/EnsEMBL/Web/Component/UserData/ShareURL.pm
+++ modules/EnsEMBL/Web/Component/UserData/ShareURL.pm
@@ -26,7 +26,7 @@ sub content {
      
   return qq{
     <p class="space-below">To share this data, use the URL:</p>
-    <p class="space-below"><a href="$url">$url</a></p>
+    <p class="space-below">$url</p>
     <p class="space-below">Please note that this link will expire after 72 hours.</p>
     $reload
   };
