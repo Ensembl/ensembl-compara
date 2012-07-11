ddb0a3bccd99103806d413c2d388707de17e95db
Beth Yates <bp1@sanger.ac.uk>
2012-07-11 15:53:30 +0000

    renamed from GeneSNPImage to reflect change in name of component, code moved to parent module to allow sharing across components

---
+++ modules/EnsEMBL/Web/ViewConfig/Gene/VariationImage.pm
@@ -0,0 +1,13 @@
+package EnsEMBL::Web::ViewConfig::Gene::VariationImage;
+
+use strict;
+
+use base qw(EnsEMBL::Web::ViewConfig::Gene::VariationTable);
+
+sub init {
+  my $self = shift;
+  $self->SUPER::init;
+  $self->add_image_config('gene_variation', 'nodas');
+}
+
+1;
