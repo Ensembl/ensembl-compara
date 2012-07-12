d40ed79acc67b802f5c92825b0b922bb7d6b3ca6
Anne Parker <ap5@sanger.ac.uk>
2012-07-12 08:42:27 +0000

    Code is ready, but database needs fixing as it contains redundant entries

--- modules/EnsEMBL/Web/Component/Phenotype/All.pm
+++ modules/EnsEMBL/Web/Component/Phenotype/All.pm
@@ -15,10 +15,30 @@ sub _init {
 sub content {
   my $self = shift;
   my $object = $self->object;
-  my $html;
 
-  $html = "Content goes here";  
+  my @phenotypes = @{$object->get_all_phenotypes};
+  my (%index, @toc, %list);
 
+  foreach my $phen (@phenotypes) {
+    my $desc    = $phen->description;
+    $desc =~ s/A(n?) //; ## get rid of leading indefinite article!
+    my $initial = uc(substr($desc, 0, 1));
+    ## NB - descriptions are a nasty mix of uppercase and mixed case,
+    ## so we need to be able to sort them in a case-insensitive manner
+    unless ($index{$initial}) {
+      push @toc, sprintf('<a href="#phenotypes-%s">%s</a>', $initial, $initial);
+      $list{$initial}{uc($desc)} .= sprintf('<h2 id="phenotypes-%s" style="margin-top:16px">%s</h2>', $initial, $initial);
+    }
+    $list{$initial}{uc($desc)} .= sprintf('<p><a href="/%s/Phenotype/Locations?ph=%s">%s</a></p>', $self->hub->species, $phen->id, ucfirst($desc));
+    $index{$initial}++;
+  } 
+
+  my $html = '<p style="margin:16px">'.join(' | ', sort @toc).'</p>';
+  foreach my $i (sort keys %list) {
+    foreach my $j (sort keys %{$list{$i}}) {
+      $html .= $list{$i}{$j};
+    }
+  }
   return $html;
 }
 
