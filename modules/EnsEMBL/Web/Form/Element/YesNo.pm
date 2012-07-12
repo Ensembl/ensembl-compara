2caa0cbe2337494716860e1927bb2e5745309fce
Harpreet Riat <hr5@sanger.ac.uk>
2012-07-12 08:50:48 +0000

    rolled back last change

--- modules/EnsEMBL/Web/Form/Element/YesNo.pm
+++ modules/EnsEMBL/Web/Form/Element/YesNo.pm
@@ -11,14 +11,16 @@ sub configure {
   $params->{'multiple'} = 0;
   $params->{'value'} = exists $params->{'value'} && ref($params->{'value'}) eq 'ARRAY'
     ? shift @{ $params->{'value'} }
-    : $params->{'value'} || 0;
+    : $params->{'value'} || '';
   
   $params->{'values'} = [{
-    'value'     => 1,
-    'caption'   => 'Yes'
-  }, {
-    'value'     => 0,
-    'caption'   => 'No'
+    'value'     => 'yes',
+    'caption'   => 'Yes',
+    'selected'  => $params->{'value'} eq 'yes' ? 1 : 0,
+  },{
+    'value'     => 'no',
+    'caption'   => 'No',
+    'selected'  => $params->{'value'} eq 'no' ? 1 : 0,
   }];
   $self->SUPER::configure($params);
 }
