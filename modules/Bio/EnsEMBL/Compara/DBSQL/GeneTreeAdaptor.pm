2757d0982835237255f4398d2d014afef6d99ea2
Matthieu Muffato <muffato@ebi.ac.uk>
2012-07-13 16:00:50 +0000

    Keeps the links between the subtrees and their super-tree

--- modules/Bio/EnsEMBL/Compara/DBSQL/GeneTreeAdaptor.pm
+++ modules/Bio/EnsEMBL/Compara/DBSQL/GeneTreeAdaptor.pm
@@ -288,7 +288,7 @@ sub fetch_subtrees {
 
     my $tree_id = (ref($tree) ? $tree->root_id : $tree);
 
-    my $join = [[['gene_tree_node', 'gtn2'], 'gtn2.node_id = gtr.root_id'], [['gene_tree_node', 'gtn1'], 'gtn1.node_id = gtn2.parent_id']];
+    my $join = [[['gene_tree_node', 'gtn2'], 'gtn2.node_id = gtr.root_id', ['gtn2.parent_id']], [['gene_tree_node', 'gtn1'], 'gtn1.node_id = gtn2.parent_id']];
     my $constraint = "(gtn1.root_id = ?) AND (gtn1.left_index = (gtn1.right_index - 1))";
 
     $self->bind_param_generic_fetch($tree_id, SQL_INTEGER);
