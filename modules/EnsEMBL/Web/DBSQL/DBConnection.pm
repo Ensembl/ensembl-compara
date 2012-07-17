5c7274b7bb3a44a6ce71463f4f8ea2cecd2a4bad
Simon Brent <sb23@sanger.ac.uk>
2012-07-17 08:27:19 +0000

    ok, finally decided on the file path. this has been totally ridiculous.

--- modules/EnsEMBL/Web/DBSQL/DBConnection.pm
+++ modules/EnsEMBL/Web/DBSQL/DBConnection.pm
@@ -84,7 +84,7 @@ sub get_DBAdaptor {
 
   # Funcgen Database Files Overwrite
   if ($database eq 'funcgen' && $self->{'species_defs'}->databases->{'DATABASE_FUNCGEN'}{'NAME'}) {
-    my $file_path = join '/', $self->{'species_defs'}->DATAFILE_BASE_PATH, lc $species, $self->{'species_defs'}->ASSEMBLY_NAME, 'regulation_' . $self->{'species_defs'}->ENSEMBL_VERSION;
+    my $file_path = join '/', $self->{'species_defs'}->DATAFILE_BASE_PATH, lc $species, $self->{'species_defs'}->ASSEMBLY_NAME;
     $dba->get_ResultSetAdaptor->dbfile_data_root($file_path) if -e $file_path && -d $file_path;
   }  
   
