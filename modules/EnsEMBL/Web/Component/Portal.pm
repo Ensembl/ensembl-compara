9497da3daf19f53f662de9941fb2846d67f865c9
Harpreet Riat <hr5@sanger.ac.uk>
2012-07-19 11:56:11 +0000

    e68 style modifications; qq{} changed to qq()

--- modules/EnsEMBL/Web/Component/Portal.pm
+++ modules/EnsEMBL/Web/Component/Portal.pm
@@ -21,13 +21,13 @@ sub content {
   
   foreach (@{$self->{'buttons'}}) {
     if ($_->{'url'}) {
-      $html .= qq{<a href="$_->{'url'}" title="$_->{'title'}"><img src="/img/$_->{'img'}.gif" class="portal" alt="" /></a>};
+      $html .= qq(<a href="$_->{'url'}" title="$_->{'title'}"><img src="/img/$_->{'img'}.gif" class="portal" alt="" /></a>);
     } else {
-      $html .= qq{<img src="/img/$_->{'img'}_off.gif" class="portal" alt="" title="$_->{'title'} (NOT AVAILABLE)" />};
+      $html .= qq|<img src="/img/$_->{'img'}_off.gif" class="portal" alt="" title="$_->{'title'} (NOT AVAILABLE)" />|;
     }
   }
   
-  return qq{<div class="centered">$html</div>};
+  return qq{<div>$html</div>};
 }
 
 1;
