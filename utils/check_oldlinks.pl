d73c6d734d9eb7804de8d8d3cdaa1e9fe172b2f5
Simon Brent <sb23@sanger.ac.uk>
2012-07-19 15:44:09 +0000

    skip things we don't care about

--- utils/check_oldlinks.pl
+++ utils/check_oldlinks.pl
@@ -13,18 +13,18 @@ use strict;
 use warnings;
 
 use FindBin qw($Bin);
-use File::Basename qw( dirname );
+use File::Basename qw(dirname);
 
 use vars qw( $SERVERROOT $SCRIPT_ROOT );
 
-BEGIN{
-  $SCRIPT_ROOT = dirname( $Bin );
+BEGIN {
+  $SCRIPT_ROOT = dirname($Bin);
   ($SERVERROOT = $SCRIPT_ROOT) =~ s#/utils##;
 
   unshift @INC, "$SERVERROOT/conf";
-  eval{ require SiteDefs };
-  if ($@){ die "Can't use SiteDefs.pm - $@\n"; }
-  map{ unshift @INC, $_ } @SiteDefs::ENSEMBL_LIB_DIRS;
+  eval { require SiteDefs };
+  die "Can't use SiteDefs.pm - $@\n" if $@;
+  unshift @INC, $_ for @SiteDefs::ENSEMBL_LIB_DIRS;
 }
 
 use EnsEMBL::Web::Root;
@@ -34,29 +34,28 @@ use EnsEMBL::Web::Builder;
 use EnsEMBL::Web::Document::Page::Dynamic;
 use EnsEMBL::Web::OldLinks;
 
-my $hub = EnsEMBL::Web::Hub->new();
-my $builder = EnsEMBL::Web::Builder->new({'hub'=>$hub});
+my $hub     = new EnsEMBL::Web::Hub;
+my $builder = new EnsEMBL::Web::Builder({ hub => $hub });
+my (@object_types, %old_links);
 
-my @object_types;
-while (my($k, $v) = each (%{$hub->species_defs->OBJECT_TO_SCRIPT})) {
-  push @object_types, $k if ($v eq 'Page');
+while (my ($k, $v) = each (%{$hub->species_defs->OBJECT_TO_SCRIPT})) {
+  push @object_types, $k if $v eq 'Page' && $k !~ /^(Info|Blast)$/;
 } 
 
-my $old_links;
-while (my($k, $v) = each (%EnsEMBL::Web::OldLinks::mapping)) {
-  foreach my $mapping (@$v) {
-    $old_links->{$mapping->{'type'}}{$mapping->{'action'}}++;
-  }
+while (my ($k, $v) = each (%EnsEMBL::Web::OldLinks::mapping)) {
+  $old_links{$_->{'type'}}{$_->{'action'}}++ for @$v;
 }
 
 foreach my $type (@object_types) {
-  my $conf_module = 'EnsEMBL::Web::Configuration::'.$type;
+  my $conf_module = "EnsEMBL::Web::Configuration::$type";
+  
   if (EnsEMBL::Web::Root::dynamic_use(undef, $conf_module)) {
     ## We need to fake a web page so that we can get the LH menu
     my $page = EnsEMBL::Web::Document::Page::Dynamic->new({
       hub          => $hub,
       species_defs => $hub->species_defs,
-    });;
+    });
+    
     my $data = {
       tree         => new EnsEMBL::Web::Tree,
       default      => undef,
@@ -64,18 +63,25 @@ foreach my $type (@object_types) {
       configurable => 0,
       page_type    => 'Dynamic',
     };
-    my $conf = $conf_module->new($page, $hub, $builder, $data);
+    
+    my $conf  = $conf_module->new($page, $hub, $builder, $data);
     my @nodes = $conf->tree->nodes;
+    
     foreach my $node (@nodes) {
       my $action = $node->id;
-      if ($old_links->{$type}{$action}) {
+      
+      next unless $node->data->{'components'};
+      next if $node->data->{'no_menu_entry'};
+      next if $node->data->{'external'};
+      next if $action eq 'Output';
+      
+      if ($old_links{$type}{$action}) {
         #warn "... PAGE $type/$action has a mapping";
-      }
-      else {
-        warn "!!! NO MAPPING FOR PAGE $type/$action";
+      } else {
+        warn "!!! NO MAPPING FOR PAGE $type/$action\n";
       }
     }
-    warn ".........";
+    warn "\n";
   }
 }
 
