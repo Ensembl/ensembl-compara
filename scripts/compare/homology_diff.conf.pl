faa4a6208e3cc900c84986a68ea3015b3615626b
Matthieu Muffato <muffato@ebi.ac.uk>
2012-08-10 17:41:51 +0000

    Useful values for recent releases

--- scripts/compare/homology_diff.conf.pl
+++ scripts/compare/homology_diff.conf.pl
@@ -1,17 +1,21 @@
 [
 # homology description for set1, referred by --url1
 { 'ortholog_one2one' => 1,
-  'ortholog_one2many' => 2,
-  'ortholog_many2many' => 3,
-  'inparalog' => 4 }
+  'apparent_ortholog_one2one' => 2,
+  'ortholog_one2many' => 3,
+  'ortholog_many2many' => 4,
+#  'within_species_paralog' => 5,
+#  'inparalog' => 4 }
+}
 ,
 # homology description for set2, referred by --url2
 { 'ortholog_one2one' => 1,
   'apparent_ortholog_one2one' => 2,
   'ortholog_one2many' => 3,
   'ortholog_many2many' => 4,
-  'within_species_paralog' => 5,
-  'between_species_paralog' => 6 }
+#  'within_species_paralog' => 5,
+ # 'between_species_paralog' => 6
+  }
 #{ 'UBRH' => 1,
 #  'MBRH' => 2,
 #  'RHS' => 3,
