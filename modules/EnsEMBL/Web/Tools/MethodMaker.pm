1314d1e04cf77bfd342eff28ad62aeaa055aa0a9
Harpreet Riat <hr5@sanger.ac.uk>
2012-08-13 15:02:50 +0000

    ignore copying the method if both methods exist, or none of them exists

--- modules/EnsEMBL/Web/Tools/MethodMaker.pm
+++ modules/EnsEMBL/Web/Tools/MethodMaker.pm
@@ -61,9 +61,9 @@ sub copy_method {
 
   while (@_) {
     my ($old_method, $new_method) = splice @_, 0, 2;
-    
-    next if $class->can($old_method) && $class->can($new_method);
-    
+
+    next unless $class->can($old_method) xor $class->can($new_method); # ignore if both methods exist, or none of them exists
+
     no strict qw(refs);
     *{"${class}::${new_method}"} = \&{"${class}::${old_method}"};
   }
