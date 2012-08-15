075b862d257d2d57b9c1a01e9943a2176d97203b
Thomas Maurel <maurel@ebi.ac.uk>
2012-08-15 09:08:09 +0000

    added Ferret and platyfish for release 69, updated turtle to Chinese softshell turtle

--- scripts/taxonomy/ensembl_aliases.sql
+++ scripts/taxonomy/ensembl_aliases.sql
@@ -306,7 +306,7 @@ SET @this_name_class='ensembl alias name';
 insert into ncbi_taxa_name select @this_taxon_id,@this_value,@this_name_class from ncbi_taxa_name WHERE taxon_id=@this_taxon_id and name_class=@this_name_class having count(*)=0;
 
 SET @this_taxon_id=13735;
-SET @this_value='Turtle';
+SET @this_value='Chinese softshell turtle';
 SET @this_name_class='ensembl alias name';
 insert into ncbi_taxa_name select @this_taxon_id,@this_value,@this_name_class from ncbi_taxa_name WHERE taxon_id=@this_taxon_id and name_class=@this_name_class having count(*)=0;
 
@@ -315,6 +315,15 @@ SET @this_value='Coelacanth';
 SET @this_name_class='ensembl alias name';
 insert into ncbi_taxa_name select @this_taxon_id,@this_value,@this_name_class from ncbi_taxa_name WHERE taxon_id=@this_taxon_id and name_class=@this_name_class having count(*)=0;
 
+SET @this_taxon_id=8083;
+SET @this_value='Platyfish';
+SET @this_name_class='ensembl alias name';
+insert into ncbi_taxa_name select @this_taxon_id,@this_value,@this_name_class from ncbi_taxa_name WHERE taxon_id=@this_taxon_id and name_class=@this_name_class having count(*)=0;
+
+SET @this_taxon_id=9669;
+SET @this_value='Ferret';
+SET @this_name_class='ensembl alias name';
+insert into ncbi_taxa_name select @this_taxon_id,@this_value,@this_name_class from ncbi_taxa_name WHERE taxon_id=@this_taxon_id and name_class=@this_name_class having count(*)=0;
 
 -- -- Use these to ADD new species
 
