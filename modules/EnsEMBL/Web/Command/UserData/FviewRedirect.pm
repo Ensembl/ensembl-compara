169bd61072bbbc8d8f270ee4c56799cf2192a809
Anne Parker <ap5@sanger.ac.uk>
2012-08-15 10:57:31 +0000

    Now saves your chosen features as a BED file so that they can be reconfigured, deleted, etc like other user tracks

--- modules/EnsEMBL/Web/Command/UserData/FviewRedirect.pm
+++ modules/EnsEMBL/Web/Command/UserData/FviewRedirect.pm
@@ -5,25 +5,52 @@ package EnsEMBL::Web::Command::UserData::FviewRedirect;
 use strict;
 use warnings;
 
-use base qw(EnsEMBL::Web::Command);
+use EnsEMBL::Web::Builder;
+
+use base qw(EnsEMBL::Web::Command::UserData);
 
 sub process {
   my $self = shift;
-  my $object = $self->object;
+  my $hub = $self->hub;
 
-  my $url = '/'.$object->param('species').'/Location/Genome';
-  my @ids = split(',', $object->param('id'));
-  for (@ids) {
-    s/^\s+//;
-    s/\s+$//;
+  my @input_ids = split /\n|\r/, $hub->param('id');
+  my @output_ids;
+  foreach (@input_ids) {
+    my @A = split(/\t|\s+|, /, $_);
+    push @output_ids, @A;
   }
-  my $params = {
-    'ftype' => $object->param('ftype'), 
-    'id' => \@ids,
-    'colour' => $object->param('colour'), 
-    'style' => $object->param('style'), 
-    'reload' => 1,
-  };
+
+  ## This bit is basically replicating the old featureview URL-based functionality!
+  my $builder = new EnsEMBL::Web::Builder({hub => $hub});
+  my $features = [];
+  $hub->param('id', @output_ids);
+  my $object = $builder->create_objects('Feature', 'lazy');
+  if ($object && $object->can('convert_to_drawing_parameters')) {
+    $features = $object->convert_to_drawing_parameters;
+  }
+
+  ## Write out features as BED file
+  my $desc = $hub->param('name') || 'Selected '.$hub->param('ftype').'s';
+  my $content = sprintf('track name=%s description="%s" useScore=1 color=%s style=%s', $hub->param('ftype'), $desc, $hub->param('colour'), $hub->param('style'));
+  $content .= "\n";
+
+  while (my ($type, $feat) = each (%{$features||{}})) {
+    foreach my $f (@{$feat->[0]||[]}) {
+      my @A = ($f->{'region'}, $f->{'start'}, $f->{'end'}, $f->{'label'}, '.', $f->{'strand'});
+      $content .= join('  ', @A);
+      $content .= "\n";
+    }
+  }
+
+  $hub->param('text', $content);
+  $hub->param('format', 'BED');
+  $hub->param('name', $desc);
+
+  ## Upload munged data
+  $self->upload('text');
+
+  my $url = '/'.$hub->param('species').'/Location/Genome';
+  my $params = {'reload' => 1};
   
   $self->ajax_redirect($url, $params, undef, 'page'); 
 }
