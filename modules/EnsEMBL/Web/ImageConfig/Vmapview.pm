9bf2afc61fce7e94fb7a1233177327a5af88fb1d
Steve Trevanion <st3@sanger.ac.uk>
2012-09-05 16:02:12 +0000

    new logic_names for density features. Scale all genes across the three tracks

--- modules/EnsEMBL/Web/ImageConfig/Vmapview.pm
+++ modules/EnsEMBL/Web/ImageConfig/Vmapview.pm
@@ -17,6 +17,10 @@ sub init {
     top_margin   => 40,
     band_links   => 'yes',
     spacing      => 10,
+    scale_values => [qw(
+                        codingdensity
+                        noncodingdensity
+                        pseudogenedensity)],
     active_menu  => 'other'
   });
 
@@ -32,39 +36,64 @@ sub init {
       menu      => 'no',
       renderers => [ 'normal', 'normal' ],
     }],
-    [ 'Vgenes', 'Genes', 'Vdensity_features', {
-      same_scale => 1,
+    [ 'Vcodgenes', 'CodGenes', 'Vdensity_features', {
+      scale_all  => 1,
       display    => 'density_outline',
       colourset  => 'densities',
-      keys       => [ 'geneDensity', 'knownGeneDensity' ],
+      keys       => [ 'codingdensity' ],
       renderers  => [
         'off',             'Off',
-        'density_outline', 'Bar chart',
-        'density_graph',   'Lines'
-      ],
-    }],
-    [ 'Vpercents', 'Percent GC/Repeats', 'Vdensity_features', {
-      same_scale => 1,
-      display    => 'density_mixed',
-      colourset  => 'densities',
-      keys       => [ 'PercentGC', 'PercentageRepeat' ],
-      renderers  => [
-        'off',           'Off', 
-        'density_mixed', 'Histogram and line'
-      ],
-    }],
-    $self->hub->species_defs->databases->{'DATABASE_VARIATION'} ? [ 'Vsnps', 'Variations', 'Vdensity_features', {
-      display   => 'density_outline',
-      colourset => 'densities',
-      maxmin    => 1,
-      keys      => [ 'snpDensity' ],
-      renderers => [
-        'off',             'Off', 
-        'density_line',    'Line graph', 
-        'density_bar',     'Bar chart - filled',
         'density_outline', 'Bar chart - outline',
+        'density_bar',     'Bar chart - filled',
+        'density_line',    'Line graph'
       ],
-    }] : (),
+    }],
+    [ 'VNonCodgenes', 'NonCodGenes', 'Vdensity_features', {
+      scale_all  => 1,
+      display    => 'density_outline',
+      colourset  => 'densities',
+      keys       => [ 'noncodingdensity' ],
+      renderers  => [
+        'off',             'Off',
+        'density_outline', 'Bar chart - outline',
+        'density_bar',     'Bar chart - filled',
+        'density_line',    'Line graph',
+      ],
+    }],
+     [ 'VPseudogenes', 'PseudoGenes', 'Vdensity_features', {
+       scale_all  => 1,
+       display    => 'density_outline',
+       colourset  => 'densities',
+       keys       => [ 'pseudogenedensity' ],
+       renderers  => [
+         'off',             'Off',
+         'density_outline', 'Bar chart - outline',
+         'density_bar',     'Bar chart - filled',
+         'density_line',    'Line graph'],
+     }],
+     [ 'Vpercents', 'Percent GC/Repeats', 'Vdensity_features', {
+       same_scale => 1,
+       display    => 'density_mixed',
+       colourset  => 'densities',
+       keys       => [ 'percentgc', 'percentagerepeat' ],
+       renderers  => [
+         'off',           'Off', 
+         'density_mixed', 'Histogram and line'
+       ],
+     }],
+     $self->hub->species_defs->databases->{'DATABASE_VARIATION'} ? [ 'Vsnps', 'Variations', 'Vdensity_features', {
+       same_scale => 1,
+       display    => 'density_outline',
+       colourset  => 'densities',
+       maxmin     => 1,
+       keys       => [ 'snpdensity' ],
+       renderers  => [
+         'off',             'Off', 
+         'density_line',    'Line graph', 
+         'density_bar',     'Bar chart - filled',
+         'density_outline', 'Bar chart - outline',
+       ],
+     }] : (),
     [ 'drag_right', '', 'Vdraggable', { display => 'normal', part => 1, menu => 'no' }],
   );
   
