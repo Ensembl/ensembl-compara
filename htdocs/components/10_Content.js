26388e7437d5c3fca9a08ac3c2158fb44c27b8d6
Harpreet Riat <hr5@sanger.ac.uk>
2012-09-07 15:44:29 +0000

    JS for helptips

--- htdocs/components/10_Content.js
+++ htdocs/components/10_Content.js
@@ -10,7 +10,8 @@ Ensembl.Panel.Content = Ensembl.Panel.extend({
       ajaxLoad:  $('.ajax', this.el),
       hideHints: $('.hint', this.el),
       glossary:  $('.glossary_mouseover', this.el),
-      dataTable: $('table.data_table', this.el)
+      dataTable: $('table.data_table', this.el),
+      helpTips:  $('._ht', this.el)
     };
     
     if (this.el.hasClass('ajax')) {
@@ -261,5 +262,52 @@ Ensembl.Panel.Content = Ensembl.Panel.extend({
   dataTable: function () {
     $.extend(this, Ensembl.DataTable);
     this.dataTableInit();
+  },
+
+  helpTips: function () {
+    var eventHandlers = {
+      mouseover: function(e) {
+        var el      = $(this);
+        var helptip = el.data('helptip');
+
+        if (!helptip) {
+          if (this.title) {
+            helptip = $('<div class="helptip"><div class="ht-inner">' + this.title + '</div></div>').appendTo(document.body);
+            el.data('helptip', helptip).removeAttr('title');
+          } else {
+            el.off(eventHandlers);
+            return;
+          }
+        }
+
+        helptip[0].className = 'helptip'; // this removes any 'helptip-left' or 'helptip-top' class added previously
+        var pos = {x: e.pageX, y: e.pageY};
+
+        if ($(window).height() < (e.pageY * 2)) {
+          helptip.addClass('helptip-top');
+          pos.y -= helptip.height();
+        }
+
+        if ($(window).width() < (e.pageX * 2)) {
+          helptip.addClass('helptip-left');
+          pos.x -= helptip.width();
+        }
+
+        helptip.css({left: pos.x + 'px', top: pos.y + 'px'}).show();
+        el.data('helptip_offset', {x: pos.x - e.pageX, y: pos.y - e.pageY});
+
+      },
+
+      mouseout: function() {
+        $(this).data('helptip').hide();
+      },
+
+      mousemove: function(e) {
+        var offset = $(this).data('helptip_offset');
+        $(this).data('helptip').css({left: (offset.x + e.pageX) + 'px', top: (offset.y + e.pageY) + 'px'});
+      }
+    }
+
+    this.elLk.helpTips.on(eventHandlers);
   }
 });
