369d6e0d079f33215781346541519a15d7bfc4f4
Harpreet Riat <hr5@sanger.ac.uk>
2012-09-11 14:55:25 +0000

    static option added - ie. if movent on mousemove is not required; ie6 doesnt understand css classes staring with hiphen

--- htdocs/components/00_jquery_helptip.js
+++ htdocs/components/00_jquery_helptip.js
@@ -7,29 +7,29 @@
   $.helptip = function (
     el,       // element itself
     tip,      // text to be displayed on hover
-    options   // options - delay, width (TODO)
+    options   // options - delay, width, static (TODO)
   ) {
 
-    $(el).on({
+    el = $(el).on({
       mouseover: function(e) {
         var el      = $(this);
         var helptip = el.data('helptip');
 
         if (!helptip) {
-          helptip = $('<div class="-helptip"><div class="-ht-inner">' + tip + '</div></div>').appendTo(document.body);
+          helptip = $('<div class="helptip"><div class="ht-inner">' + tip + '</div></div>').appendTo(document.body);
           el.data('helptip', helptip);
         }
 
-        helptip[0].className = '-helptip'; // this removes any '-helptip-left' or '-helptip-top' class added previously
+        helptip[0].className = 'helptip'; // this removes any 'helptip-left' or 'helptip-top' class added previously
         var pos = {x: e.pageX, y: e.pageY};
 
         if ($(window).height() < ((e.pageY - $(window).scrollTop()) * 2)) {
-          helptip.addClass('-helptip-top');
+          helptip.addClass('helptip-top');
           pos.y -= helptip.height();
         }
 
         if ($(window).width() < ((e.pageX - $(window).scrollLeft()) * 2)) {
-          helptip.addClass('-helptip-left');
+          helptip.addClass('helptip-left');
           pos.x -= helptip.width();
         }
 
@@ -39,14 +39,18 @@
 
       mouseout: function() {
         $(this).data('helptip').hide();
-      },
-
-      mousemove: function(e) {
-        var el = $(this);
-        var offset = el.data('helptip_offset');
-        el.data('helptip').css({left: (offset.x + e.pageX) + 'px', top: (offset.y + e.pageY) + 'px'});
       }
     });
+
+    if (!options.static) {
+      el.on({
+        mousemove: function(e) {
+          var el = $(this);
+          var offset = el.data('helptip_offset');
+          el.data('helptip').css({left: (offset.x + e.pageX) + 'px', top: (offset.y + e.pageY) + 'px'});
+        }
+      });
+    }
   };
 
   $.fn.helptip = function (tip, options) {
