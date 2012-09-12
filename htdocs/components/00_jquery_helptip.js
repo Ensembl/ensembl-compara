5d0adb006cf163a9671cd887edc6e7f9aceb1819
Simon Brent <sb23@sanger.ac.uk>
2012-09-12 09:02:40 +0000

    fix bug with positioning after window resize - helptip near the right edge of the window can have the wrong width until it is repositioned. If static option is set, position based on the middle of the element

--- htdocs/components/00_jquery_helptip.js
+++ htdocs/components/00_jquery_helptip.js
@@ -1,15 +1,12 @@
 /**
  * helptip - Displays a helptip on hovering an element
  **/
-
-(function($) {
-
+(function ($) {
   $.helptip = function (
     el,       // element itself
     tip,      // text to be displayed on hover
     options   // options - delay, width, static (TODO)
   ) {
-  
     el = $(el);
     
     if (typeof tip === 'undefined' || tip === '') {
@@ -23,60 +20,59 @@
     
     el.on({
       mouseover: function(e) {
-        var el = $(this);
+        var el      = $(this);
         var helptip = el.data('helptip');
-
+        var pos;
+        
+        if (options['static']) {
+          var offset = el.offset();
+              pos    = { x: offset.left + el.outerWidth() / 2, y: offset.top + el.outerHeight() / 2 };
+        } else {
+          pos = { x: e.pageX, y: e.pageY };
+        }
+        
         if (!helptip) {
           helptip = $('<div class="helptip"><div class="ht-inner">' + tip + '</div></div>').appendTo(document.body);
           el.data('helptip', helptip);
 
           if (!options['static']) {
-            el.on({mousemove: function(e) {
-              var el = $(this);
-              var offset = el.data('helptip_offset');
-              el.data('helptip').css({left: (offset.x + e.pageX) + 'px', top: (offset.y + e.pageY) + 'px'});
-            }});
+            el.on('mousemove', function (e) {
+              var el     = $(this);
+              var offset = el.data('helptipPos');
+              el.data('helptip').css({ left: offset.x + e.pageX, top: offset.y + e.pageY });
+            });
           }
         }
-
+        
         helptip[0].className = 'helptip'; // this removes any 'helptip-left' or 'helptip-top' class added previously
-        var pos = {x: e.pageX, y: e.pageY};
-
-        if ($(window).height() < ((e.pageY - $(window).scrollTop()) * 2)) {
+        
+        if ($(window).height() < ((pos.y - $(window).scrollTop()) * 2)) {
           helptip.addClass('helptip-top');
           pos.y -= helptip.height();
         }
-
-        if ($(window).width() < ((e.pageX - $(window).scrollLeft()) * 2)) {
+        
+        if ($(window).width() < ((pos.x - $(window).scrollLeft()) * 2)) {
           helptip.addClass('helptip-left');
-          pos.x -= helptip.width();
+          pos.x -= (el.data('helptipPos') || {}).width || helptip.width();
         }
-
-        helptip.css({left: pos.x + 'px', top: pos.y + 'px'}).show();
-
-        if (!options['static']) {
-          el.data('helptip_offset', {x: pos.x - e.pageX, y: pos.y - e.pageY});
-        }
+        
+        helptip.css({ left: pos.x, top: pos.y }).show();
+        el.data('helptipPos', { x: pos.x - e.pageX, y: pos.y - e.pageY, width: helptip.width() });
       },
-
-      mouseout: function() {
+      
+      mouseout: function () {
         $(this).data('helptip').hide();
       }
     });
   };
 
   $.fn.helptip = function (tip, options) {
-
     if (typeof tip === 'object') {
       options = tip;
-      tip = '';
+      tip     = '';
     }
-
-    options = $.extend({ 'static': $(document.body).hasClass('ie67') }, options || {});
-
-    return this.each(function() {
-      new $.helptip(this, tip, options);
-    });
+    
+    options = $.extend({ 'static': $(document.body).hasClass('ie6') }, options || {});
+    return this.each(function () { $.helptip(this, tip, options); });
   };
-
 })(jQuery);
