282e4427d8cc84d7c991975aaf115f69d1f9f522
Anne Parker <ap5@sanger.ac.uk>
2012-09-12 10:31:05 +0000

    Moved sample identifiers to title attribute, as they screw with alignment if shown as text (widths too variable), and moved search and news into separate column wrapper for neater vertical layout

--- modules/EnsEMBL/Web/Component/Info/HomePage.pm
+++ modules/EnsEMBL/Web/Component/Info/HomePage.pm
@@ -41,21 +41,6 @@ sub content {
   my $search = EnsEMBL::Web::Document::HTML::HomeSearch->new($hub);
   $html .= $search->render;
 
-  $html .= '<div class="round-box tinted-box unbordered">'.$self->_assembly_text.'</div>';
-
-  $html .= '<p style="height:1px;clear:both">&nbsp;</p>';
-
-  $html .= '<div class="round-box tinted-box unbordered">'.$self->_compara_text.'</div>';  
-
-  $html .= '<p style="height:1px;clear:both">&nbsp;</p>';
-
-  ## Only show regulation box if we have a full regulatory build 
-  ## (most funcgen dbs only contain oligoprobe data) 
-  my $sample_data = $species_defs->SAMPLE_DATA;
-  if ($sample_data->{'REGULATION_PARAM'}) {
-    $html .= '<div class="round-box tinted-box unbordered">'.$self->_funcgen_text.'</div>';
-  }
-
   $html .= '
     </div>
   </div>
@@ -66,6 +51,30 @@ sub content {
     $html .= '<div class="round-box tinted-box unbordered">'.$self->_whatsnew_text.'</div>';  
     $html .= '<p style="height:1px;clear:both">&nbsp;</p>';
   }
+
+  $html .= '
+    </div>
+  </div>
+</div>
+<div class="column-wrapper">  
+  <div class="column-two">
+    <div class="column-padding no-left-margin">';
+
+  $html .= '<div class="round-box tinted-box unbordered">'.$self->_assembly_text.'</div>';
+
+  $html .= '<p style="height:1px;clear:both">&nbsp;</p>';
+
+  $html .= '<div class="round-box tinted-box unbordered">'.$self->_compara_text.'</div>';  
+
+  $html .= '<p style="height:1px;clear:both">&nbsp;</p>';
+
+  $html .= '<div class="round-box tinted-box unbordered">'.$self->_funcgen_text.'</div>';
+
+  $html .= '
+    </div>
+  </div>
+  <div class="column-two">
+    <div class="column-padding">';
 
   $html .= '<div class="round-box tinted-box unbordered">'.$self->_genebuild_text.'</div>';  
  
@@ -91,7 +100,7 @@ sub _whatsnew_text {
     
   my $news_url = $hub->url({'action'=>'WhatsNew'});
 
-  my $html = sprintf(qq(<h2><a href="%s"><img src="/i/24/announcement.png" style="vertical-align:middle" alt="" /></a> What's New in %s release %s</h2>),
+  my $html = sprintf(qq(<h2><a href="%s" title="More release news"><img src="/i/24/announcement.png" style="vertical-align:middle" alt="" /></a> What's New in %s release %s</h2>),
                         $news_url,
                         $species_defs->SPECIES_COMMON_NAME,
                         $species_defs->ENSEMBL_VERSION,
@@ -108,7 +117,7 @@ sub _whatsnew_text {
       my $record_url = $news_url.'#change_'.$record->{'id'};
       $html .= sprintf('<li><a href="%s">%s</a></li>', $record_url, $record->{'title'});
     }
-    $html .= qq(</ul><p class="right"><a href="$news_url">More release news &gt;&gt;</a></p>);
+    $html .= '</ul>';
   }
 
   return $html;
@@ -147,8 +156,8 @@ sub _assembly_text {
   my $region_url  = $species_defs->species_path.'/Location/View?r='.$sample_data->{'LOCATION_PARAM'};
 
   $html .= qq{
-    <a href="$region_url"><img src="$img_url/96/region.png" class="bordered" /></a>
-    <p><a href="$region_url" class="nodeco">Example region<br />($region_text)</a></p>
+    <a href="$region_url" title="$region_text"><img src="$img_url/96/region.png" class="bordered" /></a>
+    <p><a href="$region_url" class="nodeco" title="$region_text">Example region</a></p>
   };
 
   $html .= '
@@ -230,15 +239,15 @@ sub _genebuild_text {
   my $gene_text = $sample_data->{'GENE_TEXT'}; 
   my $gene_url  = $species_defs->species_path.'/Gene/Summary?g='.$sample_data->{'GENE_PARAM'};
   $html .= qq{
-    <a href="$gene_url"><img src="$img_url/96/gene.png" class="bordered" /></a>
-    <p><a href="$gene_url" class="nodeco">Example gene<br />($gene_text)</a></p>
+    <a href="$gene_url" title="$gene_text"><img src="$img_url/96/gene.png" class="bordered" /></a>
+    <p><a href="$gene_url" class="nodeco" title="$gene_text">Example gene</a></p>
   };
 
   my $trans_text = $sample_data->{'TRANSCRIPT_TEXT'}; 
   my $trans_url  = $species_defs->species_path.'/Transcript/Summary?g='.$sample_data->{'TRANSCRIPT_PARAM'};
   $html .= qq{
-    <a href="$trans_url"><img src="$img_url/96/transcript.png" class="bordered" /></a>
-    <p><a href="$trans_url" class="nodeco">Example transcript<br />($trans_text)</a></p>
+    <a href="$trans_url" title="$trans_text"><img src="$img_url/96/transcript.png" class="bordered" /></a>
+    <p><a href="$trans_url" class="nodeco" title="$trans_text">Example transcript</a></p>
   };
 
   $html .= '
@@ -287,8 +296,8 @@ sub _compara_text {
   my $tree_text = $sample_data->{'GENE_TEXT'}; 
   my $tree_url  = $species_defs->species_path.'/Gene/Compara_Tree?g='.$sample_data->{'GENE_PARAM'};
   $html .= qq{
-    <a href="$tree_url"><img src="$img_url/96/compara.png" class="bordered" /></a>
-    <p><a href="$tree_url" class="nodeco">Example gene tree<br />($tree_text)</a></p>
+    <a href="$tree_url" title="$tree_text"><img src="$img_url/96/compara.png" class="bordered" /></a>
+    <p><a href="$tree_url" class="nodeco" title="$tree_text">Example gene tree</a></p>
   };
   $html .= '
   </div>
@@ -379,34 +388,46 @@ sub _funcgen_text {
   my $img_url         = $self->img_url;
   my $sample_data     = $species_defs->SAMPLE_DATA;
   my $ensembl_version = $species_defs->ENSEMBL_VERSION;
+  my $site            = $species_defs->ENSEMBL_SITETYPE;
+  my $html;
 
-  my $html = '
+  my $sample_data = $species_defs->SAMPLE_DATA;
+  if ($sample_data->{'REGULATION_PARAM'}) {
+    $html = '
 <div class="homepage-icon">
   <div class="center">
+    <h2>Regulation</h2>
   ';
-  my $reg_url  = $species_defs->species_path.'/Regulation/Cell_line?db=funcgen;rf='.$sample_data->{'REGULATION_PARAM'};
-  $html .= qq{
+
+    my $reg_url  = $species_defs->species_path.'/Regulation/Cell_line?db=funcgen;rf='.$sample_data->{'REGULATION_PARAM'};
+    $html .= qq{
     <a href="$reg_url"><img src="$img_url/96/regulation.png" class="bordered" /></a>
     <p><a href="$reg_url" class="nodeco">Example regulatory<br />feature</a></p>
   };
-  $html .= '
+
+    $html .= '
+<p><strong>What can I find?</strong> DNA methylation, transcription factor binding sites, histone modifications, and regulatory features such as enhancers and repressors, and microarray annotations.</p>';
+
+    $html .= qq(<p><a href="/info/docs/funcgen/"><img src="/i/24/info.png" alt="" style="vertical-align:middle" /></a> <a href="/info/docs/funcgen/">More about the $site regulatory build</a> and <a href="/info/docs/microarry_probe_set_mapping.html">microarray annotation</a></li>);
+
+    $html .= '</ul>';
+
+    if ($species_defs->ENSEMBL_FTP_URL) {
+      my $ftp_url = sprintf '%s/release-%s/regulation/%s/', $species_defs->ENSEMBL_FTP_URL, $ensembl_version, lc $species;
+      $html   .= qq{
+<p><a href=$ftp_url"><img src="/i/24/download.png" alt="" style="vertical-align:middle" /></a> <a href="$ftp_url">Download all regulatory features</a> (GFF)</p>};
+    } 
+    $html .= '
   </div>
 </div>
 ';
+  }
+  else {
+    $html .= '<h2>Regulation</h2>
+<p><strong>What can I find?</strong> Microarray annotations.</p>';
+    $html .= qq(<p><a href="/info/docs/microarry_probe_set_mapping.html"><img src="/i/24/info.png" alt="" style="vertical-align:middle" /></a> <a href="/info/docs/microarry_probe_set_mapping.html">More about the $site microarray annotation strategy</li>);
+  }
 
-  $html .= '<h2>Regulation</h2>
-<p><strong>What can I find?</strong> DNA methylation, transcription factor binding sites, histone modifications, and regulatory features such as enhancers and repressors.</p>';
-
-  my $site = $species_defs->ENSEMBL_SITETYPE;
-  $html .= qq(<p><a href="/info/docs/funcgen/"><img src="/i/24/info.png" alt="" style="vertical-align:middle" /></a> <a href="/info/docs/funcgen/">More about the $site regulatory build</li>);
-
-  $html .= '</ul>';
-
-  if ($species_defs->ENSEMBL_FTP_URL) {
-    my $ftp_url = sprintf '%s/release-%s/regulation/%s/', $species_defs->ENSEMBL_FTP_URL, $ensembl_version, lc $species;
-    $html   .= qq{
-<p><a href=$ftp_url"><img src="/i/24/download.png" alt="" style="vertical-align:middle" /></a> <a href="$ftp_url">Download all regulatory features</a> (GFF)</p>};
-  } 
   return $html;
 }
 
