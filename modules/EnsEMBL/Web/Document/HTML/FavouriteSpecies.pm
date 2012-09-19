711742378e81eb2377749d3329570d6008da9cde
Anne Parker <ap5@sanger.ac.uk>
2012-09-19 09:46:16 +0000

    Added star icon to 'customise this list' link

--- modules/EnsEMBL/Web/Document/HTML/FavouriteSpecies.pm
+++ modules/EnsEMBL/Web/Document/HTML/FavouriteSpecies.pm
@@ -91,12 +91,13 @@ sub render_species_list {
   return $fav_html if $fragment;
   
   # output list
+  my $star = '<img src="/i/16/star.png" style="vertical-align:middle;margin-right:4px" />';
   my $html = sprintf qq{<div class="static_favourite_species"><h3>%s genomes</h3><div class="species_list_container species-list">$fav_html</div>%s</div>}, 
     $logins && $user && scalar(@ok_faves) ? 'Favourite' : 'Popular',
     $logins
-      ? sprintf('<p class="customise-species-list">(%s)</p>', $user
-        ? '<span class="link toggle_link">Change favourites</span>'
-        : '<a href="/Account/Login" class="modal_link modal_title_Login/Register">Log in to customize this list</a>'
+      ? sprintf('<p class="customise-species-list">%s</p>', $user
+        ? qq(<span class="link toggle_link">${star}Change favourites</span>)
+        : qq(<a href="/Account/Login" class="modal_link modal_title_Login/Register">${star}Log in to customize this list</a>)
       )
     : ''
   ;
