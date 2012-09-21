41bf7164a9436ca072266a3537dff1930957351b
Beth Yates <bp1@sanger.ac.uk>
2012-09-21 15:37:22 +0000

    fix bug that breaks track if only a sinlge feature is available

--- modules/Bio/EnsEMBL/GlyphSet/simple_histogram.pm
+++ modules/Bio/EnsEMBL/GlyphSet/simple_histogram.pm
@@ -13,12 +13,21 @@ sub draw_features  {
   my $colour_key             = $self->colour_key($features->[0]);
   my $feature_colour         = $self->my_colour($colour_key);
   my $axis_colour            = $self->my_colour($colour_key, 'join') || 'black';
- 
-  my @sorted_features   = sort { $a->score <=> $b->score  } @$features;
+  my (@sorted_features, $min_score, $max_score); 
+
+  if (scalar @$features >> 1) {
+    @sorted_features   = sort { $a->score <=> $b->score  } @$features;
+    $min_score = $sorted_features[1]->score;
+    $max_score = $sorted_features[-1]->score;
+  } else { 
+    @sorted_features = @$features;
+    $min_score = $sorted_features[0]->score;
+    $max_score = $min_score;
+  }
 
   $self->draw_wiggle_plot(\@sorted_features, {
-    min_score            => $sorted_features[1]->score,
-    max_score            => $sorted_features[-1]->score,
+    min_score            => $min_score,
+    max_score            => $max_score,
     score_colour         => $feature_colour,
     axis_colour          => $axis_colour,
     axis_label           => 'off',
