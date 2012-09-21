a30245c562dbe92c92b8f407a53e2c637bcdf41b
Kathryn Beal <kbeal@ebi.ac.uk>
2012-09-21 10:51:28 +0000

    Added ability to write the core test databases on a different server from the server of the original core databases.

--- modules/t/CreateCoreTestDatabase.pl
+++ modules/t/CreateCoreTestDatabase.pl
@@ -1,9 +1,9 @@
-#!/usr/local/ensembl/bin/perl -w
+#!/usr/bin/env perl
 
 # File name: CreateCoreTestDatabase.pl
 #
 # Given a source and destination database this script will create 
-# a ensembl core database and perform 2Mbases of test data insertion 
+# a ensembl core database and populate it with data from a region defined in the seq_region_file
 #
 
 
@@ -11,20 +11,27 @@ use strict;
 
 use Getopt::Long;
 use DBI;
+use Bio::EnsEMBL::DBSQL::DBAdaptor;
 
-my ($help, $srcDB, $destDB, $host, $user, $pass, $port, $seq_region_file);
+my ($help, $srcDB, $destDB, $host, $user, $pass, $port, $seq_region_file, $dest_host, $dest_user, $dest_pass, $dest_port, $source_url, $dest_url);
 
 GetOptions('help' => \$help,
+	   'source_url=s' => \$source_url,
+	   'destination_url=s' => \$dest_url,
            's=s' => \$srcDB,
 	   'd=s' => \$destDB,
 	   'h=s' => \$host,
 	   'u=s' => \$user,
 	   'p=s' => \$pass,
 	   'port=i' => \$port,
+	   'dest_h=s' => \$dest_host,
+	   'dest_u=s' => \$dest_user,
+	   'dest_p=s' => \$dest_pass,
+	   'dest_port=i' => \$dest_port,
            'seq_region_file=s' => \$seq_region_file);
 
 my $usage = "Usage:
-CreateCoreTestDatabase.pl -s srcDB -d destDB -h host -u user -p pass [--port port]\n";
+CreateCoreTestDatabase.pl -s srcDB -d destDB -h host -u user -p pass [--port port] -dest_h dest_host -dest_u dest_user -dest_p dest_pass [--dest_port dest_port] --seq_region_file seq_region_file \n";
 
 if ($help) {
   print $usage;
@@ -35,39 +42,60 @@ unless($port) {
   $port = 3306;
 }
 
-my @seq_regions = @{do $seq_region_file};
-
+unless($dest_port) {
+  $dest_port = 3306;
+}
 
 # If needed command line args are missing print the usage string and quit
-$srcDB and $destDB and $host and $user and $pass and $seq_region_file or die $usage;
+$srcDB and $destDB and $host and $user and $dest_host and $dest_user and $dest_pass and $seq_region_file or die $usage;
 
-my $dsn = "DBI:mysql:host=$host;port=$port";
+#Get seq_regions
+my @seq_regions = @{do $seq_region_file};
 
-# Connect to the mySQL host
-my $dbh = DBI->connect( $dsn, $user, $pass, {RaiseError => 1})
+my $from_dsn = "DBI:mysql:host=$host;port=$port";
+my $to_dsn = "DBI:mysql:host=$dest_host;port=$dest_port";
+
+#Need this to create the core database
+my $to_dbh = DBI->connect( $to_dsn, $dest_user, $dest_pass, {RaiseError => 1})
   or die "Could not connect to database host : " . DBI->errstr;
 
-print "\nWARNING: If the $destDB database already exists the existing copy \n"
+my $from_dba = new Bio::EnsEMBL::DBSQL::DBAdaptor(
+						-host => $host,
+						-user => $user,
+						-port => $port,
+						-group => 'core',
+						-dbname => $srcDB);
+
+my $to_dba = new Bio::EnsEMBL::DBSQL::DBAdaptor(
+						-host => $dest_host,
+						-user => $dest_user,
+						-pass => $dest_pass,
+						-port => $dest_port,
+						-group => 'core',
+						-dbname => $destDB);
+
+print "\nWARNING: If the $destDB database on $dest_host already exists the existing copy \n"
   . "will be destroyed. Proceed (y/n)? ";
 
 my $key = lc(getc());
 
 unless( $key =~ /y/ ) {
-  $dbh->disconnect();
+  $from_dba->dbc->disconnect();
+  $to_dba->dbc->disconnect();
   print "Test Genome Creation Aborted\n";
   exit;
 }
 
-print "Proceeding with test genome database $destDB creation\n";  
+print "Proceeding with test genome database $destDB on $dest_host creation\n";  
 
 # dropping any destDB database if there
-my $array_ref = $dbh->selectall_arrayref("SHOW DATABASES LIKE '$destDB'");
+my $array_ref = $to_dbh->selectall_arrayref("SHOW DATABASES LIKE '$destDB'");
 if (scalar @{$array_ref}) {
-  $dbh->do("DROP DATABASE $destDB");
+  $to_dba->dbc->do("DROP DATABASE $destDB");
 }
 # creating destination database
-$dbh->do( "CREATE DATABASE " . $destDB )
-  or die "Could not create database $destDB: " . $dbh->errstr;
+$to_dbh->do( "CREATE DATABASE " . $destDB )
+  or die "Could not create database $destDB: " . $to_dbh->errstr;
 
 # Dump the source database table structure (w/o data) and use it to create
 # the new database schema
@@ -75,85 +103,256 @@ $dbh->do( "CREATE DATABASE " . $destDB )
 # May have to eliminate the -p pass part... not sure
 
 my $rc = 0xffff & system(
-  "mysqldump -p$pass -u $user -h $host -P $port --no-data $srcDB | " .
-  "mysql -p$pass -u $user -h $host -P $port $destDB");
+  "mysqldump -u $user -h $host -P $port --no-data $srcDB | " .
+  "mysql -p$dest_pass -u $dest_user -h $dest_host -P $dest_port $destDB");
 
 if($rc != 0) {
   $rc >>= 8;
   die "mysqldump and insert failed with return code: $rc";
 }
-$dbh->do("use $destDB");
+$to_dba->dbc->do("use $destDB");
 
 # populate coord_system table
-$dbh->do("insert into coord_system select * from $srcDB.coord_system");
-$array_ref = $dbh->selectcol_arrayref("select coord_system_id from coord_system where rank=1");
-my $coord_system_id = $array_ref->[0];
+my $query = "SELECT * FROM $srcDB.coord_system";
+my $table_name = "coord_system";
+copy_data_in_text_mode($from_dba, $to_dba, $table_name, $query);
 
+#Store available coord_systems
+my %coord_systems;
+my $sql = "select coord_system_id, name, rank from coord_system";
+my $sth = $to_dba->dbc->prepare($sql);
+$sth->execute();
+while (my $row = $sth->fetchrow_hashref) {
+    $coord_systems{$row->{name}} = $row->{rank};
+}
+$sth->finish;
+
+#$array_ref = $from_dba->db_handle->selectcol_arrayref("select coord_system_id from coord_system where rank=1");
+#my $coord_system_id = $array_ref->[0];
+
+my $slice_adaptor = $from_dba->get_adaptor("Slice");
 # populate assembly and assembly_exception tables
 my $all_seq_region_names = {};
 foreach my $seq_region (@seq_regions) {
   my ($seq_region_name, $seq_region_start, $seq_region_end) = @{$seq_region};
-  $all_seq_region_names->{$seq_region_name} = 1;
-  $dbh->do("insert into assembly select a.* from $srcDB.seq_region s,$srcDB.assembly a where s.coord_system_id=$coord_system_id and s.name='$seq_region_name' and s.seq_region_id=a.asm_seq_region_id and a.asm_start<$seq_region_end and a.asm_end>$seq_region_start");
+  #$all_seq_region_names->{$seq_region_name} = 1;
+
+  #This query doesn't work with gorilla (e!68) because we cannot go from chromosome to contig but must go via supercontig
+  #$query = "SELECT a.* FROM $srcDB.seq_region s,$srcDB.assembly a WHERE s.coord_system_id=$coord_system_id AND s.name='$seq_region_name' AND s.seq_region_id=a.asm_seq_region_id AND a.asm_start<$seq_region_end AND a.asm_end>$seq_region_start";
+
+  #Make the assumption that the core API is OK and that the 3 levels of assembly are chromosome, supercontig and contig
+
+  my $slice = $slice_adaptor->fetch_by_region('toplevel', $seq_region_name, $seq_region_start, $seq_region_end);
+
+  my $supercontigs;
+  my $seq_region_list;
+
+  #May not always have supercontigs
+  if ($coord_systems{'supercontig'}) {
+      $supercontigs = $slice->project('supercontig');
+      foreach my $supercontig (@$supercontigs) {
+          my $supercontig_slice = $supercontig->[2];
+          $seq_region_list .= $supercontig_slice->get_seq_region_id . ",";
+      }
+  }
+
+  #Assume always have contigs
+  my $contigs = $slice->project('contig');
+
+  foreach my $contig (@$contigs) {
+      my $contig_slice = $contig->[2];
+      $seq_region_list .= $contig_slice->get_seq_region_id . ",";
+  }
+  chop $seq_region_list;
+
+  $query = "SELECT a.* FROM $srcDB.seq_region s JOIN $srcDB.assembly a ON (s.seq_region_id = a.cmp_seq_region_id) WHERE seq_region_id IN ($seq_region_list)";
+
+  copy_data_in_text_mode($from_dba, $to_dba, "assembly", $query);
+
+  #convert seq_region_name to seq_region_id
+  my $sql = "SELECT seq_region_id FROM seq_region WHERE name = \"$seq_region_name\"";
+  my $sth = $from_dba->dbc->prepare($sql);
+  $sth->execute();
+  my ($seq_region_id) = $sth->fetchrow_arrayref->[0];
+  $sth->finish;
+  print "$seq_region_name seq_region_id $seq_region_id\n";
+  $all_seq_region_names->{$seq_region_name} = $seq_region_id;
 }
-$dbh->do("insert ignore into assembly_exception select ax.* from $srcDB.assembly_exception ax,assembly a where ax.seq_region_id=a.asm_seq_region_id");
+
+my ($asm_seq_region_ids,$asm_seq_region_ids_str)  = get_ids($to_dba,"asm_seq_region_id", "assembly") ;
+
+my $all_seq_region_ids;
+my ($cmp_seq_region_ids,$cmp_seq_region_ids_str)  = get_ids($to_dba,"cmp_seq_region_id", "assembly") ;
+
+$query = "SELECT ax.* FROM assembly_exception ax WHERE ax.seq_region_id in ($asm_seq_region_ids_str)";
+copy_data_in_text_mode($from_dba, $to_dba, "assembly_exception", $query);
 
 # populate attrib_type table
-$dbh->do("insert into attrib_type select * from $srcDB.attrib_type");
+$query = "SELECT * FROM attrib_type";
+copy_data_in_text_mode($from_dba, $to_dba, "attrib_type", $query);
 
 # populate seq_region and seq_region_attrib tables
-$dbh->do("insert ignore into seq_region select s.* from assembly a,$srcDB.seq_region s where a.asm_seq_region_id=s.seq_region_id");
-$dbh->do("insert ignore into seq_region select s.* from assembly a,$srcDB.seq_region s where a.cmp_seq_region_id=s.seq_region_id");
-$dbh->do("insert into seq_region_attrib select sa.* from $srcDB.seq_region_attrib sa, seq_region s where s.seq_region_id=sa.seq_region_id");
+foreach my $id (@$asm_seq_region_ids) {
+    push @$all_seq_region_ids, $id;
+}
+foreach my $id (@$cmp_seq_region_ids) {
+    push @$all_seq_region_ids, $id;
+}
+my $all_seq_region_ids_str = join ",", @$all_seq_region_ids;
+
+$query = "SELECT s.* from seq_region s where s.seq_region_id in ($all_seq_region_ids_str)";
+copy_data_in_text_mode($from_dba, $to_dba, "seq_region", $query);
+
+$query = "SELECT sa.* FROM seq_region_attrib sa WHERE sa.seq_region_id in ($all_seq_region_ids_str)";
+copy_data_in_text_mode($from_dba, $to_dba, "seq_region_attrib", $query);
 
 # populate dna table
-$dbh->do("insert into dna select d.* from $srcDB.dna d,seq_region s where s.seq_region_id=d.seq_region_id");
+$query = "SELECT d.* FROM dna d WHERE d.seq_region_id in ($all_seq_region_ids_str)";
+copy_data_in_text_mode($from_dba, $to_dba, "dna", $query);
 
 # populate repeat_feature and repeat_consensus tables
 # repeat_features are stored at sequence_level
-$dbh->do("insert into repeat_feature select rf.* from seq_region s, $srcDB.repeat_feature rf where s.seq_region_id=rf.seq_region_id and s.name not in (\"".join("\", \"", keys(%$all_seq_region_names))."\")");
+$query = "SELECT rf.* FROM repeat_feature rf WHERE rf.seq_region_id in ($cmp_seq_region_ids_str)";
+copy_data_in_text_mode($from_dba, $to_dba, "repeat_feature", $query);
+
 foreach my $seq_region (@seq_regions) {
   my ($seq_region_name, $seq_region_start, $seq_region_end) = @{$seq_region};
-  $dbh->do("insert into repeat_feature select rf.* from seq_region s, $srcDB.repeat_feature rf where s.seq_region_id=rf.seq_region_id and s.name='$seq_region_name' and rf.seq_region_start<$seq_region_end and rf.seq_region_end>$seq_region_start");
+  my $seq_region_id = $all_seq_region_names->{$seq_region_name};
+
+  $query = "SELECT rf.* FROM repeat_feature rf WHERE rf.seq_region_id=$seq_region_id AND rf.seq_region_start<$seq_region_end AND rf.seq_region_end>$seq_region_start";
+  copy_data_in_text_mode($from_dba, $to_dba, "repeat_feature", $query);
 }
-$dbh->do("insert ignore into repeat_consensus select rc.* from repeat_feature rf, $srcDB.repeat_consensus rc where rf.repeat_consensus_id=rc.repeat_consensus_id");
+
+my ($repeat_consensus_ids, $repeat_consensus_ids_str) = get_ids($to_dba, "repeat_consensus_id", "repeat_feature");
+$query = "SELECT rc.* FROM repeat_consensus rc WHERE rc.repeat_consensus_id in ($repeat_consensus_ids_str)";
+copy_data_in_text_mode($from_dba, $to_dba, "repeat_consensus", $query);
+
 
 # populate transcript, transcript_attrib and transcript_stable_id tables
 # transcripts are stored at top_level
 foreach my $seq_region (@seq_regions) {
   my ($seq_region_name, $seq_region_start, $seq_region_end) = @{$seq_region};
-  $dbh->do("insert into transcript select t.* from seq_region s, $srcDB.transcript t where s.seq_region_id=t.seq_region_id and s.coord_system_id=$coord_system_id and s.name='$seq_region_name' and t.seq_region_end<$seq_region_end and t.seq_region_end>$seq_region_start;");
+  my $seq_region_id = $all_seq_region_names->{$seq_region_name};
+
+  $query = "SELECT t.* FROM transcript t WHERE t.seq_region_id=$seq_region_id AND t.seq_region_end<$seq_region_end AND t.seq_region_end>$seq_region_start";
+  copy_data_in_text_mode($from_dba, $to_dba, "transcript", $query);
 }
-$dbh->do("insert into transcript_attrib select ta.* from transcript t, $srcDB.transcript_attrib ta where t.transcript_id=ta.transcript_id");
-$dbh->do ("insert into transcript_stable_id select ts.* from transcript t, $srcDB.transcript_stable_id ts where t.transcript_id=ts.transcript_id");
+
+my ($transcript_ids, $transcript_ids_str) = get_ids($to_dba, "transcript_id", "transcript");
+my ($gene_ids, $gene_ids_str) = get_ids($to_dba, "gene_id", "transcript");
+
+if ($transcript_ids_str) {
+    $query = "SELECT ta.* FROM transcript_attrib ta WHERE ta.transcript_id in ($transcript_ids_str)";
+    copy_data_in_text_mode($from_dba, $to_dba, "transcript_attrib", $query);
+}
 
 # populate translation, translation_attrib and translation_stable_id tables
-$dbh->do("insert into translation select tl.* from transcript t, $srcDB.translation tl where t.transcript_id=tl.transcript_id");
-$dbh->do("insert into translation_attrib select tla.* from translation tl, $srcDB.translation_attrib tla where tl.translation_id=tla.translation_id");
-$dbh->do ("insert into translation_stable_id select tls.* from translation tl, $srcDB.translation_stable_id tls where tl.translation_id=tls.translation_id");
+if ($transcript_ids_str) {
+    $query = "SELECT tl.* FROM translation tl WHERE tl.transcript_id in ($transcript_ids_str)";
+    copy_data_in_text_mode($from_dba, $to_dba, "translation", $query);
+}
+
+my ($translation_ids, $translation_ids_str) = get_ids($to_dba, "translation_id", "translation");
+
+if ($translation_ids_str) {
+    $query = "SELECT tla.* FROM translation_attrib tla WHERE tla.translation_id in ($translation_ids_str)";
+    copy_data_in_text_mode($from_dba, $to_dba, "translation_attrib", $query);
+}
 
 # populate exon_transcript, exon and exon_stable_id tables
-$dbh->do("insert into exon_transcript select et.* from transcript t, $srcDB.exon_transcript et where t.transcript_id=et.transcript_id");
-$dbh->do("insert ignore into exon select e.* from exon_transcript et, $srcDB.exon e where et.exon_id=e.exon_id");
-$dbh->do("insert into exon_stable_id select es.* from exon e, $srcDB.exon_stable_id es where e.exon_id=es.exon_id");
+if ($transcript_ids_str) {
+    $query = "SELECT et.* FROM exon_transcript et WHERE et.transcript_id in ($transcript_ids_str)";
+    copy_data_in_text_mode($from_dba, $to_dba, "exon_transcript", $query);
+}
+
+my ($exon_ids, $exon_ids_str) = get_ids($to_dba, "exon_id", "exon_transcript");
+
+if ($exon_ids_str) {
+    $query = "SELECT e.* FROM exon e WHERE e.exon_id in ($exon_ids_str)";
+    copy_data_in_text_mode($from_dba, $to_dba, "exon", $query);
+}
 
 # populate gene, gene_stable_id and alt_allele tables
-$dbh->do("insert ignore into gene select g.* from transcript t, $srcDB.gene g where t.gene_id=g.gene_id");
-$dbh->do("insert into gene_stable_id select gs.* from gene g, $srcDB.gene_stable_id gs where g.gene_id=gs.gene_id");
-$dbh->do("insert into alt_allele select alt.* from gene g, $srcDB.alt_allele alt where g.gene_id=alt.gene_id");
+if ($gene_ids_str) {
+    $query = "SELECT g.* FROM gene g WHERE g.gene_id in ($gene_ids_str)";
+    copy_data_in_text_mode($from_dba, $to_dba, "gene", $query);
+}
+
+if ($gene_ids_str) {
+    $query = "SELECT alt.* FROM alt_allele alt WHERE alt.gene_id in ($gene_ids_str)";
+    copy_data_in_text_mode($from_dba, $to_dba, "alt_allele", $query);
+}
+
 # populate meta and meta_coord table
-$dbh->do("insert into meta select * from $srcDB.meta");
-$dbh->do("insert into meta_coord select * from $srcDB.meta_coord");
+$query = "SELECT * FROM meta";
+copy_data_in_text_mode($from_dba, $to_dba, "meta", $query);
 
-# populate analysis table
-# I don't if this one is needed for test runs
-#$dbh->do("insert into analysis select * from $srcDB.analysis");
-
-$dbh->disconnect();
+$query = "SELECT * FROM meta_coord";
+copy_data_in_text_mode($from_dba, $to_dba, "meta_coord", $query);
 
 print "Test genome database $destDB created\n";
 
-# cmd to dump .sql and .txt files
-# /usr/local/ensembl/mysql/bin/mysqldump -hia64f -uensadmin -pensembl -P3306 --socket=/mysql/data_3306/mysql.sock -T ./ abel_core_test
+sub copy_data_in_text_mode {
+  my ($from_dba, $to_dba, $table_name, $query, $step) = @_;
+   print "start copy_data_in_text_mode $table_name\n";
+
+  my $user = $to_dba->dbc->username;
+  my $pass = $to_dba->dbc->password;
+  my $host = $to_dba->dbc->host;
+  my $port = $to_dba->dbc->port;
+  my $dbname = $to_dba->dbc->dbname;
+  my $use_limit = 1;
+  my $start = 0;
+
+  if (!defined $step) {
+      $step = 100000;
+  }
+
+  while (1) {
+    my $start_time = time();
+    my $end = $start + $step - 1;
+    my $sth;
+    print "$query start $start end $end\n";
+    $sth = $from_dba->dbc->prepare($query." LIMIT $start, $step");
+
+    $start += $step;
+    $sth->execute();
+    my $all_rows = $sth->fetchall_arrayref;
+    $sth->finish;
+    ## EXIT CONDITION
+    return if (!@$all_rows);
+    my $time=time(); 
+    my $filename = "/tmp/$table_name.copy_data.$$.$time.txt";
+    open(TEMP, ">$filename") or die "could not open the file '$filename' for writing";
+    foreach my $this_row (@$all_rows) {
+      print TEMP join("\t", map {defined($_)?$_:'\N'} @$this_row), "\n";
+    }
+    close(TEMP);
+    #print "time " . ($start-$min_id) . " " . (time - $start_time) . "\n";
+
+    system("mysqlimport -h$host -P$port -u$user ".($pass ? "-p$pass" : '')." -L -l -i $dbname $filename");
+
+    unlink("$filename");
+    #print "total time " . ($start-$min_id) . " " . (time - $start_time) . "\n";
+  }
+}
+
+sub get_ids {
+    my ($dba, $id, $table) = @_;
+    my $ids;
+    my $sql = "SELECT distinct($id) FROM $table";
+    my $sth = $dba->dbc->prepare($sql);
+    $sth->execute();
+
+    while (my $row = $sth->fetchrow_arrayref) {
+	push @$ids, $row->[0];
+    }
+    $sth->finish();
+
+    return (undef, "") unless ($ids);
+    my $ids_str = join ",", @$ids;
+
+    return ($ids, $ids_str);
+}
 
 exit 0;
