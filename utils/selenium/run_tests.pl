b37669ad8cc12d1e421ae2fe615b4c31680b6e1e
ridwan <ma7@sanger.ac.uk>
2012-09-26 10:06:39 +0000

    commented out my ip address

--- utils/selenium/run_tests.pl
+++ utils/selenium/run_tests.pl
@@ -21,6 +21,7 @@ my $module;
 my $url = 'http://test.ensembl.org';
 my $host = `host mib20062i`; #get the IP address of the selenium server which is on the macbook pro.
 $host =~ s/mib20062i.internal.sanger.ac.uk has address //;#'172.20.11.204';
+#$host = '172.20.10.187';
 my $port = '4444';
 my $browser = '*firefox';
 my $test;
