463ebdb7b0d2e091e012dc94a907f9ed2700f76e
Dan Sheppard <ds23@sanger.ac.uk>
2012-10-01 14:30:58 +0000

    Excel export fix.

--- modules/EnsEMBL/Web/Controller/Ajax.pm
+++ modules/EnsEMBL/Web/Controller/Ajax.pm
@@ -127,7 +127,7 @@ sub table_export {
   my $clean = sub {
     my ($str,$opts) = @_;
     # Remove summaries, ugh.
-    $str =~ s!<div class="toggle_summary">.*?</div>!!g;
+    $str =~ s!<span class="toggle_summary[^"]*">.*?</span>!!g;
     # split multiline columns
     for (2..$opts->{'split_newline'}) {
       unless($str =~ s/<br.*?>/\0/) {
