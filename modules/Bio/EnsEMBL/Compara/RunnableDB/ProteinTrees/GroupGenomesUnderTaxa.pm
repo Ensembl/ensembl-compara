bef3908f988386854625194ddf351b0c26c207cf
Matthieu Muffato <muffato@ebi.ac.uk>
2012-10-08 10:09:51 +0000

    now using the API call fetch_all_by_ancestral_taxon_id()

--- modules/Bio/EnsEMBL/Compara/RunnableDB/ProteinTrees/GroupGenomesUnderTaxa.pm
+++ modules/Bio/EnsEMBL/Compara/RunnableDB/ProteinTrees/GroupGenomesUnderTaxa.pm
@@ -42,7 +42,7 @@ sub fetch_input {
 
     my $filter_high_coverage = $self->param('filter_high_coverage');
 
-    my @selected_gdb_ids = ();
+    my %selected_gdb_ids = ();
 
     foreach my $genome_db (@$genome_dbs) {
         if($filter_high_coverage) {
@@ -53,26 +53,26 @@ sub fetch_input {
                     or die "'assembly.coverage_depth' is not defined in core database's meta table". $core_adaptor->dbc->dbname; 
 
             if( ($coverage_depth eq 'high') or ($coverage_depth eq '6X')) {
-                push @selected_gdb_ids, $genome_db->dbID();
+                $selected_gdb_ids{$genome_db->dbID} = 1;
             }
         } else {    # take all of them
-            push @selected_gdb_ids, $genome_db->dbID();
+            $selected_gdb_ids{$genome_db->dbID} = 1;
         }
     }
 
-    my $selected_gdb_id_string = join(',', @selected_gdb_ids);
-
     ###
-
-    my $dbc         = $self->compara_dba()->dbc();
 
     my $taxlevels   = $self->param('taxlevels')
                         or die "'taxlevels' is an obligatory parameter";
 
     my @species_sets = ();
 
+    my $gdb_a = $self->compara_dba()->get_GenomeDBAdaptor;
+    my $ncbi_a = $self->compara_dba()->get_NCBITaxonAdaptor;
     foreach my $taxlevel (@$taxlevels) {
-        push @species_sets, filter_genomes_by_taxlevel($dbc, $selected_gdb_id_string, $taxlevel);
+        my $taxon = $ncbi_a->fetch_node_by_name($taxlevel);
+        my $all_gdb_ids = [map {$_->dbID} @{$gdb_a->fetch_all_by_ancestral_taxon_id($taxon->dbID)}];
+        push @species_sets, [grep {exists $selected_gdb_ids{$_}} @$all_gdb_ids];
     }
 
     $self->param('species_sets', \@species_sets);
@@ -87,37 +87,6 @@ sub write_output {      # dataflow the results
     foreach my $ss (@$species_sets) {
         $self->dataflow_output_id( { 'species_set' => $ss }, 2);
     }
-}
-
-
-# ------------------------- non-interface subroutines -----------------------------------
-
-
-sub filter_genomes_by_taxlevel {    # not a method
-    my ($dbc, $selected_gdb_id_string, $taxlevel) = @_;
-
-    my $sql = qq{
-        SELECT DISTINCT g.genome_db_id
-          FROM ncbi_taxa_name parent_name, ncbi_taxa_node parent_node, ncbi_taxa_node child_node, genome_db g
-         WHERE parent_name.name='$taxlevel'
-           AND parent_name.taxon_id=parent_node.taxon_id
-           AND parent_node.left_index<child_node.left_index
-           AND child_node.right_index<=parent_node.right_index
-           AND child_node.taxon_id=g.taxon_id
-           AND g.genome_db_id in ($selected_gdb_id_string)
-      ORDER BY g.genome_db_id
-    };
-
-    my @species_subset = ();
-
-    my $sth = $dbc->prepare($sql);
-    $sth->execute();
-
-    while(my ($genome_db_id) = $sth->fetchrow()) {
-        push @species_subset, $genome_db_id;
-    }
-
-    return \@species_subset;
 }
 
 1;
