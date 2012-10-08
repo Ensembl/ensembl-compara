1d09c242572d45e9a65b3f5dcebb1cb2abba8d97
Dan Sheppard <ds23@sanger.ac.uk>
2012-10-08 14:25:46 +0000

    Bizarre Perl bytecode issue.

--- modules/EnsEMBL/Web/DBSQL/DirectDBConnection.pm
+++ modules/EnsEMBL/Web/DBSQL/DirectDBConnection.pm
@@ -3,17 +3,16 @@ package EnsEMBL::Web::DBSQL::DirectDBConnection;
 use strict;
 use warnings;
 
-use EnsEMBL::Web::Controller;
-
 my %handles;
 
 sub direct_connection {
   my ($class,$caller,$name,$host,$port,$user,$pass) = @_;
   no strict 'refs';
   *{$caller.'::db_Main'} = sub {
+    require EnsEMBL::Web::Controller;
     use strict 'refs';
     my $dbh = $handles{$caller};
-    return $dbh if defined $dbh && $dbh->ping;
+    return $dbh if (defined $dbh) and $dbh->ping;
     my $dsn = join(':','dbi','mysql',$name,$host,$port);
     $dbh = DBI->connect_cached($dsn,$user,$pass,
       {
