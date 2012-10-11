cf90581a79f5baf27d14cf89068738b634ef455a
Simon Brent <sb23@sanger.ac.uk>
2012-10-11 16:01:38 +0000

    use new modularised TextSequence code

--- modules/EnsEMBL/Web/Component/Variation/Compara_Alignments.pm
+++ modules/EnsEMBL/Web/Component/Variation/Compara_Alignments.pm
@@ -8,184 +8,41 @@ use base qw(EnsEMBL::Web::Component::Compara_Alignments);
 
 sub get_sequence_data {
   my ($self, $slices, $config) = @_;
-  my $hub         = $self->hub;
-  my @consequence = $hub->param('consequence_filter');
-  my (@sequence, @markup, @temp_slices, @pos);
+  my (@sequence, @markup, @temp_slices);
   
-  my $focus = $config->{'focus_variant_feature'};
-  $config->{'consequence_filter'} = { map { $_ => 1 } @consequence } if join('', @consequence) ne 'off';
+  $self->set_variation_filter($config);
   
   foreach my $sl (@$slices) {
-    my $mk    = {};
-    my $slice = $sl->{'slice'};
-    my $name  = $sl->{'name'};
-    my $seq   = uc $slice->seq(1);
-
-    my $vfa = Bio::EnsEMBL::Registry->get_adaptor($name, 'variation', 'variationfeature');
-    
-    my ($slice_start, $slice_end, $slice_length, $slice_strand) = ($slice->start, $slice->end, $slice->length, $slice->strand);
+    my $mk            = {};
+    my $slice         = $sl->{'slice'};
+    my $name          = $sl->{'name'};
+    my $seq           = uc $slice->seq(1);
     my @variation_seq = map ' ', 1..length $seq;
     
-    $config->{'length'} ||= $slice_length;
+    $config->{'length'} ||= $slice->length;
     
-    # Markup inserts on comparisons
-    if ($config->{'align'}) {
-      while ($seq =~  m/(\-+)[\w\s]/g) {
-        my $ins_length = length $1;
-        my $ins_end    = pos ($seq) - 1;
-        
-        $mk->{'comparisons'}->{$ins_end-$_}->{'insert'} = "$ins_length bp" for 1..$ins_length;
-      }
-    }
+    $self->set_alignments($config, $sl, $mk, $seq) if $config->{'align'};
+    $self->set_variations($config, $sl, $mk, \@variation_seq);
     
-    # Get variations
-    if ($config->{'snp_display'}) {
-      my $snps   = [];
-      my $u_snps = {};
-    
-      my $old_ifv; 
-      eval {
-        if($vfa and $focus) {
-          $old_ifv = $vfa->db->include_failed_variations;
-          $vfa->db->include_failed_variations(1);
-        }
-        $snps = $slice->get_all_VariationFeatures;
-        $snps = [ map { my $snp = $_; grep($config->{'consequence_filter'}{$_}, @{$snp->consequence_type}) || $config->{'v'} eq $snp->variation_name ? $snp : () } @$snps ] if $snps && $config->{'consequence_filter'};
-      };
-      
-      if (scalar @$snps) {
-        foreach my $u_slice (@{$sl->{'underlying_slices'}||[]}) {
-          next if $u_slice->seq_region_name eq 'GAP';
-          
-          if (!$u_slice->adaptor) {
-            my $slice_adaptor = Bio::EnsEMBL::Registry->get_adaptor($name, $config->{'db'}, 'slice');
-            $u_slice->adaptor($slice_adaptor);
-          }
-          
-          eval {
-            map { $u_snps->{$_->variation_name} = $_ } @{$u_slice->get_all_VariationFeatures};
-          };
-        }
-      }
-      
-      # Put deletes second, so that they will overwrite the markup of other variations in the same location
-      my @ordered_snps = map { $_->[1] } sort { $a->[0] <=> $b->[0] } map {[ $_->end < $_->start ? 1 : 0, $_ ]} @$snps;
-      
-      foreach (@ordered_snps) {
-        my $variation_name = $_->variation_name;
-        my $var_class      = $_->can('var_class') ? $_->var_class : $_->can('variation') && $_->variation ? $_->variation->var_class : '';
-        my $failed         = $_->variation ? $_->variation->is_failed : 0;
-        my $dbID           = $_->dbID;
-        my $start          = $_->start;
-        my $end            = $_->end;
-        my $alleles        = $_->allele_string;
-        my $ambigcode      = $var_class =~ /in\-?del|insertion|deletion/ ? '*' : $_->ambig_code;
-        my $url            = $hub->url({ species => $name, r => undef, v => $variation_name, vf => $dbID });
-        my $var            = $variation_name eq $config->{'v'} ? $ambigcode : qq{<a href="$url">$ambigcode</a>};
-        my $snp_type       = $config->{'consequence_filter'} ? lc [ grep $config->{'consequence_filter'}{$_}, @{$_->consequence_type} ]->[0] : undef;
-           $snp_type     ||= lc $_->display_consequence;
-        $snp_type = 'failed' if($failed);
-        my $focus_snp = ( $dbID == $focus and 
-                          $name eq $config->{'species'});     
-        next if $failed and not $focus_snp;
- 
-        # If gene is reverse strand we need to reverse parts of allele, i.e AGT/- should become TGA/-
-        if ($slice_strand < 0) {
-          my @al = split /\//, $alleles;
-          
-          $alleles  = '';
-          $alleles .= reverse($_) . '/' for @al;
-          $alleles  =~ s/\/$//;
-        }
-      
-        # If the variation is on reverse strand, flip the bases
-        $alleles =~ tr/ACGTacgt/TGCAtgca/ if $_->strand < 0;
-        
-        # Use the variation from the underlying slice if we have it.
-        my $snp = scalar keys %$u_snps ? $u_snps->{$variation_name} : $_;
-        
-        # Co-ordinates relative to the sequence - used to mark up the variation's position
-        my $s = $start - 1;
-        my $e = $end   - 1;
-        
-        # Co-ordinates relative to the region - used to determine if the variation is an insert or delete
-        my $seq_region_start = $snp->seq_region_start;
-        my $seq_region_end   = $snp->seq_region_end;
-        
-        if ($var_class =~ /in\-del|insertion|deletion/) {
-          ($s, $e)  = ($e, $s) if $s > $e;
-        }
-        
-        $s = 0 if $s < 0;
-        $e = $#variation_seq if $e > $#variation_seq;
-        
-        for ($s..$e) {          
-          $mk->{'variations'}->{$_}->{'type'}     = $snp_type;
-          $mk->{'variations'}->{$_}->{'v'}        = $variation_name;
-          $mk->{'variations'}->{$_}->{'alleles'} .= ($mk->{'variations'}->{$_}->{'alleles'} ? '; ' : '') . $alleles;
-          $mk->{'variations'}->{$_}->{'href'}   ||= {
-            species     => $name,
-            type        => 'ZMenu',
-            action      => 'TextSequence',
-            factorytype => 'Location'
-          };
-          
-          push @{$mk->{'variations'}->{$_}->{'href'}->{'v'}},  $variation_name;
-          push @{$mk->{'variations'}->{$_}->{'href'}->{'vf'}}, $dbID;
-          
-          $variation_seq[$_] = $var if $ambigcode;
-        }
-        
-        @pos = ($s..$e) if $variation_name eq $config->{'v'};
-      }
-      $vfa->db->include_failed_variations($old_ifv) if(defined $old_ifv);
-    }
-    
-    $mk->{'variations'}->{$_}->{'align'} = 1 for @pos;
+    $mk->{'variations'}{$_}{'align'} = 1 for @{$config->{'focus_position'} || []};
     
     if (!$sl->{'no_variations'} && grep /\S/, @variation_seq) {
       push @temp_slices, {};
-      push @markup, {};
-      push @sequence, [ map {{ letter => $_ }} @variation_seq ];
+      push @markup,      {};
+      push @sequence,    [ map {{ letter => $_ }} @variation_seq ];
     }
     
     push @temp_slices, $sl;
-    push @markup, $mk;
-    push @sequence, [ map {{ letter => $_ }} split //, $seq ];
+    push @markup,      $mk;
+    push @sequence,    [ map {{ letter => $_ }} split '', $seq ];
     
     $config->{'ref_slice_seq'} ||= $sequence[-1];
   }
   
-  $config->{'slices'} = \@temp_slices;
+  $config->{'display_width'} = $config->{'length'};
+  $config->{'slices'}        = \@temp_slices;
   
   return (\@sequence, \@markup);
-}
-
-sub markup_variation {
-  my $self = shift;
-  my ($sequence, $markup, $config) = @_;
-
-  my $hub = $self->hub;
-  my ($snps, $inserts, $deletes, $seq, $variation, $ambiguity);
-  my $i = 0;
-
-  foreach my $data (@$markup) {
-    $seq = $sequence->[$i];
-    
-    foreach (sort { $a <=> $b } keys %{$data->{'variations'}}) {      
-      $variation = $data->{'variations'}->{$_};
-      
-      $seq->[$_]->{'title'} .= ($seq->[$_]->{'title'} ? '; ' : '') . $variation->{'alleles'};
-      $seq->[$_]->{'class'} .= "$variation->{'type'} ";
-      $seq->[$_]->{'class'} .= 'bold ' if $variation->{'align'};
-      $seq->[$_]->{'class'} .= 'var '  if $config->{'v'} eq $variation->{'v'}; # The page's variation
-      $seq->[$_]->{'href'}   = $hub->url($variation->{'href'}) if $variation->{'href'};
-      
-      $config->{'key'}->{'variations'}->{$variation->{'type'}} = 1 if $variation->{'type'};
-    }
-    
-    $i++;
-  }
 }
 
 sub markup_conservation {
@@ -219,41 +76,37 @@ sub content {
   my $species_defs = $hub->species_defs;
   my $width        = 20;
   my %mappings     = %{$object->variation_feature_mapping}; 
-  my $v            = keys %mappings == 1 ? [values %mappings]->[0] : $mappings{$hub->param('vf')};
+  my $v            = keys %mappings == 1 ? [ values %mappings ]->[0] : $mappings{$hub->param('vf')};
   
   return $self->_info('Unable to draw SNP neighbourhood', $object->not_unique_location) if $object->not_unique_location;
   
   my $defaults = { 
     snp_display          => 1, 
-    title_display        => 1, 
     conservation_display => 1,
-    v                    => $hub->param('v')
+    variation_sequence   => 1,
+    v                    => $hub->param('v'),
+    focus_variant        => $hub->param('vf'),
+    failed_variant       => $object->Obj->is_failed,
+    ambiguity            => 0,
   };
   
   my $seq_type   = $v->{'type'}; 
   my $seq_region = $v->{'Chr'};
   my $start      = $v->{'start'} - ($width/2);  
   my $end        = $v->{'start'} + abs($v->{'end'} - $v->{'start'}) + ($width / 2);
-  
-  my $slice   = $hub->get_adaptor('get_SliceAdaptor')->fetch_by_region($seq_type, $seq_region, $start, $end, 1);
-  my $align   = $hub->param('align');
-  my ($error) = $self->check_for_errors($align, $species);
+  my $slice      = $hub->get_adaptor('get_SliceAdaptor')->fetch_by_region($seq_type, $seq_region, $start, $end, 1);
+  my $align      = $hub->param('align');
+  my ($error)    = $self->check_for_errors($align, $species);
   
   return $error if $error;
   
-  my ($html, $info);
- 
   # Get all slices for the gene
   my ($slices, $slice_length) = $self->get_slices($slice, $align, $species);
-  
-  my @aligned_slices;
-  my %non_aligned_slices;
-  my %no_variation_slices;  
-  my $ancestral_seq;
+  my ($html, $info, @aligned_slices, %non_aligned_slices, %no_variation_slices, $ancestral_seq);
   
   foreach my $s (@$slices) {
     my $other_species_dbs = $species_defs->get_config($s->{'name'}, 'databases');
-    my $name = $species_defs->species_label($s->{'name'});
+    my $name              = $species_defs->species_label($s->{'name'});
     
     if ($s->{'name'} eq 'ancestral_sequences') {
       $ancestral_seq = $name;
@@ -282,7 +135,7 @@ sub content {
     }
   }
   
-  my $align_species = $species_defs->multi_hash->{'DATABASE_COMPARA'}->{'ALIGNMENTS'}->{$align}->{'species'};
+  my $align_species = $species_defs->multi_hash->{'DATABASE_COMPARA'}{'ALIGNMENTS'}{$align}{'species'};
   my %aligned_names = map { $_->{'name'} => 1 } @aligned_slices;
   
   foreach (keys %$align_species) {
--- modules/EnsEMBL/Web/Component/Variation/FlankingSequence.pm
+++ modules/EnsEMBL/Web/Component/Variation/FlankingSequence.pm
@@ -1,264 +1,77 @@
+# $Id$
+
 package EnsEMBL::Web::Component::Variation::FlankingSequence;
 
 use strict;
-use warnings;
-no warnings "uninitialized";
+
 use base qw(EnsEMBL::Web::Component::Variation EnsEMBL::Web::Component::TextSequence);
 
-use Bio::EnsEMBL::Variation::Utils::Sequence qw(align_seqs);
-sub _init {
-  my $self = shift;
-  $self->cacheable( 0 );
-  $self->ajaxable( 1 );
-}
-
 sub content {
-  my $self    = shift;
-  my $object  = $self->object;
-  my $table   = $self->new_twocol;
-  my $hub     = $self->hub;
-  my @consequence = $hub->param('consequence_filter');
+  my $self   = shift;
+  my $object = $self->object;
   
   ## first check we have uniquely determined variation
   return $self->_info('A unique location can not be determined for this Variation', $object->not_unique_location) if $object->not_unique_location;
   
-  ## count locations
-  my $mapping_count = scalar keys %{$object->variation_feature_mapping};
-
-  ## Add flanking sequence
-  my $f_label;
-  my $f_html;
+  my $hub               = $self->hub;
+  my $variation         = $object->Obj;
+  my $vf                = $hub->param('vf');
+  my $flanking          = $hub->param('select_sequence') || 'both';
+  my $flank_size        = $hub->param('flank_size') || 400;
+  my @flank             = $flanking eq 'both' ? ($flank_size, $flank_size) : $flanking eq 'up' ? ($flank_size) : (undef, $flank_size);
+  my %mappings          = %{$object->variation_feature_mapping}; 
+  my $v                 = keys %mappings == 1 ? [ values %mappings ]->[0] : $mappings{$vf};
+  my $variation_feature = $variation->get_VariationFeature_by_dbID($vf);
+  my $align_quality     = $variation_feature->flank_match;
+  my $chr_end           = $variation_feature->slice->end;
+  my $slice_start       = $v->{'start'} - $flank[0] > 1        ? $v->{'start'} - $flank[0] : 1;
+  my $slice_end         = $v->{'end'}   + $flank[1] > $chr_end ? $chr_end                  : $v->{'end'} + $flank[1];
+  my $slice             = $hub->get_adaptor('get_SliceAdaptor')->fetch_by_region(undef, $v->{'Chr'}, $slice_start, $slice_end, $v->{'strand'});
+  my @sequence          = [ map {{ letter => $_ }} split '', $slice->seq ];
+  my @markup            = ({});
+  my $html;
   
-  my $f_info_msg = qq{ 
-    <p>
-      The sequence below is from the <b>reference genome</b> flanking the variant location.
-      The variant is shown in <span class="alt_allele"><u>red</u></span> text.
-      Neighbouring variants are shown with highlighted letters and ambiguity codes.<br />
-      To change the display of the flanking sequence (e.g. hide the other variants, change the length of the flanking sequence), 
-      use the "<b>Configure this page</b>" link on the left.
-    </p>
+  my $config = {
+    display_width  => $hub->param('display_width') || 60,
+    species        => $hub->species,
+    snp_display    => $hub->param('snp_display') eq 'no' ? 0 : 1,
+    v              => $hub->param('v'),
+    focus_variant  => $vf,
+    failed_variant => 1,
+    ambiguity      => 1,
+    length         => $slice->length,
   };
-  my $f_info = $self->_info('Flanking sequence', $f_info_msg, 'auto');
-
-  my $status   = 'status_ambig_sequence';
-
-  my $ambig_code = $object->vari->ambig_code;
-  unless ($ambig_code) {
-    $ambig_code = "[".$object->alleles."]";
-  }
-  
-  my $config = { 
-    display_width   => $hub->param('display_width') || 60,
-    species         => $hub->species,
-    maintain_colour => 1,
-    snp_display     => $hub->param('snp_display'),
-    select_sequence => $hub->param('select_sequence') || 'both',
-  };
-  
-  $config->{'consequence_filter'} = { map { $_ => 1 } @consequence } if $config->{'snp_display'} && join('', @consequence) ne 'off';
-
-  # first determine correct SNP location 
-  my %mappings = %{ $object->variation_feature_mapping }; 
-  my $loc;
-  if( keys %mappings == 1 ) {
-    ($loc) = values %mappings;
-  } else { 
-    $loc = $mappings{$object->param('vf')};
-  }
   
   # check if the flanking sequences match the reference sequence
-  my $align_info;
-  my $vf = $object->vari->get_VariationFeature_by_dbID($object->param('vf'));
-  my $align_quality = $vf->flank_match;
-  if (defined($align_quality) && $align_quality < 1) {
-    my $source_link = $hub->get_ExtURL_link("here", uc($object->source), $object->param('v')."#submission");
-    $source_link =~ s/%23/#/;
-    $align_info = $self->_warning('Alignment quality', "<p>The longest flanking sequence submitted to dbSNP for this variant doesn't match the reference sequence displayed below.<br />For more information about the submitted sequences, please click on the dbSNP link $source_link.</p>", 'auto');
+  if (defined $align_quality && $align_quality < 1) {
+    my $source_link = $hub->get_ExtURL_link('here', uc($variation->source), "$config->{'v'}#submission");
+       $source_link =~ s/%23/#/;
+       
+    $html .= $self->_warning('Alignment quality', "
+      The longest flanking sequence submitted to dbSNP for this variant doesn't match the reference sequence displayed below.<br />
+      For more information about the submitted sequences, please click on the dbSNP link $source_link.
+    ", 'auto');
   }
   
-  my $flank_size = $object->param('flank_size') || 400;
-  $flank_size ++;
-  # get a slice adaptor
-  my $sa = $object->vari->adaptor->db->dnadb->get_SliceAdaptor(); 
+  # The variation_feature has coords relative to the whole chromosome.
+  # If snp_display is off, we only want to display this feature, so hack its start and end to be relative
+  # to the slice, so that it can be used in the parent module (all variations there are relative to the slice)
+  $variation_feature->$_($variation_feature->$_ - $slice->start + 1) for qw(start end);
   
-  my $chr_slice = $sa->fetch_by_region(undef,$loc->{Chr});
-  my $chr_end = $chr_slice->end;
+  $self->set_variations($config, { name => $config->{'species'}, slice => $slice }, $markup[0], undef, $config->{'snp_display'} ? undef : $variation_feature);
+  $self->markup_variation(\@sequence, \@markup, $config); 
   
-  my $slice_start = ($loc->{start} - $flank_size > 1) ? $loc->{start} - $flank_size : 1;
-  my $slice_end   = ($loc->{end} + $flank_size > $chr_end) ? $chr_end : $loc->{end} + $flank_size;
-
-  # get slice
-  my $slice = $sa->fetch_by_region(
-      undef,
-      $loc->{Chr},
-      $slice_start,
-      $slice_end,
-      $loc->{strand}
-  );
+  $html .= $self->tool_buttons($slice->seq, $config->{'species'});
+  $html .= sprintf '<div class="sequence_key">%s</div>', $self->get_key($config);
+  $html .= $self->build_sequence(\@sequence, $config);
   
-  # get up slice
-  my $up_end = ($loc->{start} - 1 > 1) ? $loc->{start} - 1 : 1;
-  my $up_slice = $sa->fetch_by_region(
-      undef,
-      $loc->{Chr},
-      $slice_start,
-      $up_end,
-      $loc->{strand}
-  );
-    
-  # get down slice
-  my $down_start = ($loc->{end} + 1 > $chr_end) ? $chr_end : $loc->{end} + 1;
-  my $down_slice = $sa->fetch_by_region(
-      undef,
-      $loc->{Chr},
-      $down_start,
-      $slice_end,
-      $loc->{strand}
-  );
-  
-  my $mk = {};
-  my @markup;
-  my @sequence;
-  
-  my @reference_seq;
-  my @reference_seq_up   = map {{ letter => $_ }} split //, $up_slice->seq;
-  my @reference_seq_down = map {{ letter => $_ }} split //, $down_slice->seq;
-
-  
-  my $v_name = $object->param('v');
-  my $v_dbID = $object->param('vf');
-  
-  if ($config->{'snp_display'} eq 'yes') {
-
-    # Upstream sequence
-    if ($config->{'select_sequence'} ne 'down') {
-      @reference_seq = @reference_seq_up;
-      $self->find_variations(0, $up_slice, $mk);
-    }
-    # Variation
-    my $length = @reference_seq;
-    foreach my $v_letter (split //, $ambig_code) {
-      push @reference_seq, {letter => $v_letter, is_main_variation => 1};
-      $mk->{'variations'}->{$length}->{'alleles'}   .= ($mk->{'variations'}->{$_}->{'alleles'} ? ', ' : '') . $ambig_code;
-      $mk->{'variations'}->{$length}->{'ambigcode'} = $v_letter;
-      $length++;
-    }
-    
-    # Downstream sequence
-    if ($config->{'select_sequence'} ne 'up'){
-  
-     foreach my $down (@reference_seq_down) {
-       push @reference_seq, $down;
-     }
-     $self->find_variations($length, $down_slice, $mk);
-    }
-    push @sequence, \@reference_seq;
-    push @markup, $mk;
-
-    $self->markup_variation(\@sequence, \@markup, $config); 
-    $f_html .= $self->tool_buttons($slice->seq, $config->{'species'});
-    $f_html .= sprintf('<div class="sequence_key">%s</div>', $self->get_key($config));
-    $f_html .= $self->build_sequence(\@sequence, $config);
-  
-  }
-  elsif(defined($up_slice) && defined($down_slice)) {
-    $f_html .= uc( $up_slice->seq ) if ($config->{'select_sequence'} ne 'down');
-    $f_html .= lc( $ambig_code );
-    $f_html .= uc( $down_slice->seq ) if ($config->{'select_sequence'} ne 'up');
-
-    $f_html =~ s/(.{60})/$1\n/g;
-    $f_html =~ s/(([a-z]|\/|-|\[|\])+)/'<span class="alt_allele"><u>'.uc("$1").'<\/u><\/span>'/eg;
-    $f_html =~ s/\n/\n/g;
-  }
-  return  qq{$f_info\n$align_info\n<pre>$f_html</pre>};
+  return $self->_info('Flanking sequence', qq{ 
+    The sequence below is from the <b>reference genome</b> flanking the variant location.
+    The variant is shown in <u style="color:red;font-weight:bold">red</u> text.
+    Neighbouring variants are shown with highlighted letters and ambiguity codes.<br />
+    To change the display of the flanking sequence (e.g. hide the other variants, change the length of the flanking sequence), 
+    use the "<b>Configure this page</b>" link on the left.
+  }, 'auto') . $html;
 }
 
-
-sub markup_variation {
-  my $self = shift;
-  my ($sequence, $markup, $config) = @_;
-
-  my ($snps, $inserts, $deletes, $seq, $variation, $ambiguity);
-  my $hub = $self->hub;
-  my $i   = 0;
-  
-  my $class = {
-    snp    => 'sn',
-    insert => 'si',
-    delete => 'sd'
-  };
-
-  foreach my $data (@$markup) {
-    $seq = $sequence->[$i];
-    
-    foreach (sort {$a <=> $b} keys %{$data->{'variations'}}) {
-      $variation  = $data->{'variations'}->{$_};
-      my $ambigcode = $variation->{ambigcode};
-      
-      if ($seq->[$_]->{'is_main_variation'}) {
-        my $letter = $seq->[$_]->{'letter'};
-        $seq->[$_]->{'letter'} = qq{<span class="alt_allele"><u>$letter</u></span>};
-        $seq->[$_]->{'class'} = undef;
-      }
-      else {
-        $seq->[$_]->{'letter'} = $ambigcode if $ambigcode;
-        #$seq->[$_]->{'letter'} = $ambiguity if $ambiguity;
-        $seq->[$_]->{'title'} .= ($seq->[$_]->{'title'} ? '; ' : '') . $variation->{'alleles'} if $config->{'title_display'};
-        $seq->[$_]->{'class'} .= ($class->{$variation->{'type'}} || $variation->{'type'}) . ' ';
-        $seq->[$_]->{'href'}   = $hub->url($variation->{'href'});
-        $seq->[$_]->{'post'}   = join '', @{$variation->{'link_text'}} if $config->{'snp_display'} eq 'snp_link' && $variation->{'link_text'};
-     
-        $config->{'key'}->{'variations'}->{$variation->{'type'}} = 1 if ($variation->{'type'});
-      }
-    }
-    $i++;
-  }
-}
-
-
-sub find_variations {
-  my $self   = shift;
-  my $length = shift;
-  my $slice  = shift;
-  my $mk     = shift;
-  
-  my $hub = $self->hub;
-  
-  foreach my $var (reverse @{$slice->get_all_VariationFeatures()}) {
-    my $dbID           = $var->dbID;
-    my $variation_name = $var->variation_name;
-    my $alleles        = $var->allele_string;
-    my $ambigcode      = $var->ambig_code || undef;
-    my $start          = $var->start - 1 + $length; # -1 because of the sequence index starts at 0
-    my $end            = $var->end - 1 + $length;   # -1 because of the sequence index starts at 0 
-    my $type           = lc($var->display_consequence);
-    if ($var) {
-      if ($var->strand == -1) {
-        $ambigcode =~ tr/acgthvmrdbkynwsACGTDBKYHVMRNWS\//tgcadbkyhvmrnwsTGCAHVMRDBKYNWS\//;
-        $alleles   =~ tr/acgthvmrdbkynwsACGTDBKYHVMRNWS\//tgcadbkyhvmrnwsTGCAHVMRDBKYNWS\//;
-      }
-    }
-      
-    # Variation is an insert if start > end
-    ($start, $end) = ($end, $start) if $start > $end;
-      
-    foreach ($start..$end) {
-      $mk->{'variations'}->{$_}->{'alleles'}   .= ($mk->{'variations'}->{$_}->{'alleles'} ? ', ' : '') . $alleles;
-      $mk->{'variations'}->{$_}->{'url_params'} = { v => $variation_name, vf => $dbID, vdb => 'variation' };
-      $mk->{'variations'}->{$_}->{'ambigcode'} = $ambigcode;
-        
-      my $url = $mk->{'variations'}->{$_}->{'url_params'} ? $hub->url({ type => 'Variation', action => 'Summary', %{$mk->{'variations'}->{$_}->{'url_params'}} }) : '';
-        
-      $mk->{'variations'}->{$_}->{'type'} = $type;
-      $mk->{'variations'}->{$_}->{'href'} ||= {
-         type        => 'ZMenu',
-         action      => 'TextSequence',
-         factorytype => 'Location'
-      };
-        
-      push @{$mk->{'variations'}->{$_}->{'href'}->{'v'}},  $variation_name;
-      push @{$mk->{'variations'}->{$_}->{'href'}->{'vf'}}, $dbID;
-    }
-  }
-}
 1;
