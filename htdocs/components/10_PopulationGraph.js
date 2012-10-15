b2a1567cf4ad05f05c2eb7c4109dcc71ca1dcdde
Simon Brent <sb23@sanger.ac.uk>
2012-10-15 11:52:21 +0000

    make sub populations toggleable

--- htdocs/components/10_PopulationGraph.js
+++ htdocs/components/10_PopulationGraph.js
@@ -1,10 +1,13 @@
 // $Revision$
 
-Ensembl.Panel.PopulationGraph = Ensembl.Panel.extend({  
+Ensembl.Panel.PopulationGraph = Ensembl.Panel.Content.extend({  
   init: function () {
     var panel = this;
     
     this.base();
+    
+    this.graphData = [];
+    this.graphEls  = {};
     
     if (typeof Raphael === 'undefined') {
       $.getScript('/raphael/raphael-min.js', function () {
@@ -16,34 +19,51 @@ Ensembl.Panel.PopulationGraph = Ensembl.Panel.extend({
   },
   
   getContent: function () {
-    var alleles   = [ 'A', 'T', 'G', 'C' ];
-    var acolours  = [ '#00BB00', '#FF0000', '#FFD700', '#0000FF' ];
-    var bcolours  = [ '#222222', '#FF00FF', '#008080', '#7B68EE' ]; // Other colours if the allele is not A, T, G or C
-    var graphData = [];
-    var i, j, k, raphael, pieData, pieColors, piePercent, b, colourFlag;
+    var panel   = this;
+    var visible = [];
     
     $('input.population', this.el).each(function () {
-      graphData.push(eval(this.value));
-    }); 
+      panel.graphData.push(eval(this.value));
+    });
     
-    // For each graph
-    for (i in graphData) {
+    for (i in this.graphData) {
+      this.graphEls[i] = $('#graphHolder' + i);
+      
+      if (this.graphEls[i].is(':visible')) {
+        visible.push(i);
+      }
+    }
+    
+    this.makeGraphs(visible);
+  },
+  
+  makeGraphs: function (index) {
+    var alleles  = [ 'A', 'T', 'G', 'C' ];
+    var acolours = [ '#00BB00', '#FF0000', '#FFD700', '#0000FF' ];
+    var bcolours = [ '#222222', '#FF00FF', '#008080', '#7B68EE' ]; // Other colours if the allele is not A, T, G or C
+    var i, j, k, raphael, pieData, pieColors, piePercent, b, colourFlag;
+    
+    for (i in index) {
+      if (this.graphEls[index[i]].data('done')) {
+        continue;
+      }
+      
       pieData    = [];
       pieColors  = [];
       piePercent = [];
-      raphael    = Raphael('graphHolder' + i);
+      raphael    = Raphael('graphHolder' + index[i]);
       raphael.g.txtattr.font = "12px 'Luxi Sans','Helvetica', sans-serif";
       b = 0;      
       
       // For each allele
-      for (j in graphData[i]) {
-        pieData.push(graphData[i][j][1]);
-        piePercent.push(graphData[i][j][0] + ': %%');
+      for (j in this.graphData[index[i]]) {
+        pieData.push(this.graphData[index[i]][j][1]);
+        piePercent.push(this.graphData[index[i]][j][0] + ': %%');
         colourFlag = 0;
 
         // Normal allele colour
         for (k in alleles) {
-          if (graphData[i][j][0] === alleles[k]) {
+          if (this.graphData[index[i]][j][0] === alleles[k]) {
             pieColors.push(acolours[k]);
             colourFlag = 1;
           }
@@ -57,6 +77,20 @@ Ensembl.Panel.PopulationGraph = Ensembl.Panel.extend({
       }
       
       raphael.g.piechart(25, 25, 20, pieData, { legend: piePercent, legendpos: "east", legendmark: 'arrow', colors: pieColors }); // Create the pie graph
+      
+      this.graphEls[index[i]].data('done', true);
     }
+  },
+  
+  toggleContent: function (el) {
+    if (el.hasClass('open') && !el.data('done')) {
+      this.base(el);
+      this.makeGraphs($('.pie_chart > div', '.' + el.attr('rel')).map(function () { return this.id.replace('graphHolder', ''); }).toArray());
+      el.data('done', true);
+    } else {
+      this.base(el);
+    }
+    
+    el = null;
   }
 });
--- modules/EnsEMBL/Web/Component/Variation/PopulationGraphs.pm
+++ modules/EnsEMBL/Web/Component/Variation/PopulationGraphs.pm
@@ -1,3 +1,5 @@
+# $Id$
+
 package EnsEMBL::Web::Component::Variation::PopulationGraphs;
 
 use strict;
@@ -11,114 +13,124 @@ sub _init {
 }
 
 sub content {
-  my $self = shift;
-  my $object = $self->object;
-  my $hub = $self->hub;
+  my $self      = shift;
+  my $hub       = $self->hub;
+  my $freq_data = $self->object->freqs;
+  my $pop_freq  = $self->format_frequencies($freq_data);
   
-  my $freq_data = $object->freqs;
+  return unless defined $pop_freq;
   
-  my $pop_freq = $self->format_frequencies($freq_data);
-  return '' unless (defined($pop_freq));
+  my @pop_phase1 = grep /phase_1/, keys %$pop_freq;
   
-  my @pop_phase1 = grep{ /phase_1/} keys(%$pop_freq);
-  return '' unless (scalar @pop_phase1);
-
-  my @graphs;
-  my @inputs;
+  return unless scalar @pop_phase1;
+  
   my $graph_id = 0;
   my $height   = 50;
-
-  my $pop_tree;                 
-  my %sub_pops;                 
-  my @alleles;
+  my (@graphs, @inputs, $pop_tree, %sub_pops, @alleles);
+  
   # Get alleles list
-  foreach my $pop_name (sort(keys(%$pop_freq))) {
-    my $values = '';
-    my $p_name = (split(':',$pop_name))[1];
+  foreach my $pop_name (sort keys %$pop_freq) {
+    my $values   = '';
+    my $p_name   = (split ':', $pop_name)[1];
+       $pop_tree = $self->update_pop_tree($pop_tree, $pop_name, $pop_freq->{$pop_name}{'sub_pop'}) if defined $pop_freq->{$pop_name}{'sub_pop'};
     
-    $pop_tree = $self->update_pop_tree($pop_tree,$pop_name,$pop_freq->{$pop_name}{sub_pop}) if (defined($pop_freq->{$pop_name}{sub_pop}));
-    
-    foreach my $ssid (keys %{$pop_freq->{$pop_name}{freq}}) {
-      foreach my $allele (keys %{$pop_freq->{$pop_name}{freq}{$ssid}}) {
-        my $freq = $pop_freq->{$pop_name}{freq}{$ssid}{$allele};
-        push (@alleles, $allele) if ((!grep {$allele eq $_} @alleles) && $freq>0);
-       }
+    foreach my $ssid (keys %{$pop_freq->{$pop_name}{'freq'}}) {
+      foreach my $allele (keys %{$pop_freq->{$pop_name}{'freq'}{$ssid}}) {
+        my $freq = $pop_freq->{$pop_name}{'freq'}{$ssid}{$allele};
+        push (@alleles, $allele) if $freq > 0 && !(grep $allele eq $_, @alleles);
+      }
     }  
   }
   
-  my $nb_alleles = scalar(@alleles);
-  if ($nb_alleles>2) {
+  my $nb_alleles = scalar @alleles;
+  
+  if ($nb_alleles > 2) {
     while ($nb_alleles != 2) {
       $height += 5;
-      $nb_alleles --;
+      $nb_alleles--;
     }
   }
- 
+  
   # Create graphs
-  foreach my $pop_name (sort {($a !~ /ALL/) cmp ($b !~ /ALL/) || $a cmp $b} (@pop_phase1)) {
+  foreach my $pop_name (sort { ($a !~ /ALL/ cmp $b !~ /ALL/) || $a cmp $b } @pop_phase1) {
+    my $values    = '';
+    my @pop_names = split ':', $pop_name;
     
-    my $values = '';
-    my @pop_names = (split(':',$pop_name));
     shift @pop_names;
-    my $p_name = join(':',@pop_names);
+    
+    my $p_name     = join ':', @pop_names;
     my $short_name = $self->get_short_name($p_name);
-    my $pop_desc = $pop_freq->{$pop_name}{desc};
+    my $pop_desc   = $pop_freq->{$pop_name}{'desc'};
     
     # Constructs the array for the pie charts: [allele,frequency]
     foreach my $al (@alleles) {
-      foreach my $ssid (keys %{$pop_freq->{$pop_name}{freq}}) {
-
-        next if (!$pop_freq->{$pop_name}{freq}{$ssid}{$al});
-          
-        my $freq = $pop_freq->{$pop_name}{freq}{$ssid}{$al};
-          
-        $values .= ',' if ($values ne '');
-        $freq = 0.5 if ($freq < 0.5); # Fixed bug if freq between 0 and 0.5
+      foreach my $ssid (keys %{$pop_freq->{$pop_name}{'freq'}}) {
+        my $freq = $pop_freq->{$pop_name}{'freq'}{$ssid}{$al};
+        
+        next unless $freq;
+        
+        $values .= ',' if $values ne '';
+        $freq    = 0.5 if $freq < 0.5; # Fixed bug if freq between 0 and 0.5
         $values .= "['$al',$freq]";
         last;
       }
     }
+    
     push @inputs, qq{<input type="hidden" class="population" value="[$values]" />};
     
     if ($short_name =~ /ALL/) {
-      push @graphs, sprintf qq{<div class="pie-chart%s" title="$pop_desc"><span>$short_name</span><div id="graphHolder$graph_id" style="width:118px;height:$height\px;"></div></div>}, $short_name eq 'ALL' ? ' all-population' : '';
-    }
-    elsif ($pop_tree->{$short_name}) {
-      my $show = $self->hub->get_cookie_value("toggle_population_freq_$short_name") eq 'open';
-      push @graphs, sprintf qq{<div class="pie-chart" title="$pop_desc">
-                                 <span>$short_name</span>
-                                 <div id="graphHolder$graph_id" style="width:118px;height:$height\px;"></div>
-                                 <a class="toggle set_cookie %s" href="#" rel="population_freq_$short_name" title="Click to toggle subpopulation frequencies">Sub-populations</a>
-                               </div>
-                               }, $show ? 'open' : 'closed';
-    }
-    else {
-      foreach my $sp (keys(%{$pop_tree})) {
-        if ($pop_tree->{$sp}{$short_name}) {
-          push @{$sub_pops{$sp}}, qq{<div class="pie-chart" title="$pop_desc"><span>$short_name</span><div id="graphHolder$graph_id" style="width:118px;height:$height\px;"></div></div>};
-        }
+      push @graphs, sprintf('
+        <div class="pie_chart_holder">
+          <div class="pie_chart%s" title="%s">
+            <h4>%s</h4>
+            <div id="graphHolder%s" style="width:118px;height:%spx"></div>
+          </div>
+        </div>
+      ', $short_name eq 'ALL' ? ' all_population' : '', $pop_desc, $short_name, $graph_id, $height);
+    } elsif ($pop_tree->{$short_name}) {
+      push @graphs, sprintf('
+        <div class="pie_chart_holder">
+          <div class="pie_chart" title="%s">
+            <h4>%s</h4>
+            <div id="graphHolder%s" style="width:118px;height:%spx"></div>
+          </div>
+          <a class="toggle set_cookie %s" href="#" rel="population_freq_%s" title="Click to toggle sub-population frequencies">Sub-populations</a>
+        </div>
+      ', $pop_desc, $short_name, $graph_id, $height, $hub->get_cookie_value("toggle_population_freq_$short_name") eq 'open' ? 'open' : 'closed', $short_name);
+    } else {
+      foreach (grep $pop_tree->{$_}{$short_name}, keys %$pop_tree) {
+        push @{$sub_pops{$_}}, qq{
+          <div class="pie_chart_holder">
+            <div class="pie_chart" title="$pop_desc">
+              <h4>$short_name</h4>
+              <div id="graphHolder$graph_id" style="width:118px;height:${height}px"></div>
+            </div>
+          </div>
+        };
       }
     }
     
-    $graph_id ++;
+    $graph_id++;
   }
   
-  my $html = sprintf q{<h2>1000 Genomes allele frequencies</h2><div><input type="hidden" class="panel_type" value="PopulationGraph" />%s</div><div class="population-genetics-pie">%s</div>},
+  my $html = sprintf(
+    '<h2>1000 Genomes allele frequencies</h2><div><input type="hidden" class="panel_type" value="PopulationGraph" />%s</div><div class="population_genetics_pie">%s</div>',
     join('', @inputs),
     join('', @graphs)
-  ;
+  );
   
-  foreach my $sp (keys(%sub_pops)) {
-    my $sub_html;
-    foreach my $pop (@{$sub_pops{$sp}}) {
-      $sub_html .= $pop;
-    }
-    my $show = $self->hub->get_cookie_value("toggle_population_freq_$sp") eq 'open';
-    $html .= sprintf(q{<div class="population-genetics-pie population_freq_%s"><div class="toggleable" %s><div><p><b>%s sub-populations</b></p></div>%s</div></div>},
-        $sp,
-        $show ? '' : 'style="display:none"',
-        $sp,
-        $sub_html);
+  foreach my $sp (sort keys %sub_pops) {
+    my $sub_html = join '', @{$sub_pops{$sp}};
+    my $show     = $hub->get_cookie_value("toggle_population_freq_$sp") eq 'open';
+    
+    $html .= sprintf('
+      <div class="population_genetics_pie population_freq_%s">
+        <div class="toggleable" %s>
+          <div><p><b>%s sub-populations</b></p></div>
+          %s
+        </div>
+      </div>
+    ', $sp, $show ? '' : 'style="display:none"', $sp, $sub_html);
   }
 
   return $html;
@@ -132,27 +144,25 @@ sub format_frequencies {
    foreach my $pop_id (keys %$freq_data) {
     foreach my $ssid (keys %{$freq_data->{$pop_id}}) {
       my $pop_name = $freq_data->{$pop_id}{$ssid}{'pop_info'}{'Name'};
-      next if($freq_data->{$pop_id}{$ssid}{'pop_info'}{'Name'} !~ /^1000genomes\:.*/i);
-      next if($freq_data->{$pop_id}{$ssid}{failed_desc});
-      # Freqs alleles ---------------------------------------------
+      
+      next if $freq_data->{$pop_id}{$ssid}{'failed_desc'};
+      next if $freq_data->{$pop_id}{$ssid}{'pop_info'}{'Name'} !~ /^1000genomes\:.*/i;
+      
       my @allele_freq = @{$freq_data->{$pop_id}{$ssid}{'AlleleFrequency'}};
       
-      $pop_freq->{$pop_name}{desc} = $freq_data->{$pop_id}{$ssid}{'pop_info'}{'Description'};
-      
-      if (scalar(keys %{$freq_data->{$pop_id}{$ssid}{'pop_info'}{'Sub-Population'}})) {
-        $pop_freq->{$pop_name}{sub_pop} = $freq_data->{$pop_id}{$ssid}{'pop_info'}{'Sub-Population'};
-      }
+      $pop_freq->{$pop_name}{'desc'}    = $freq_data->{$pop_id}{$ssid}{'pop_info'}{'Description'};
+      $pop_freq->{$pop_name}{'sub_pop'} = $freq_data->{$pop_id}{$ssid}{'pop_info'}{'Sub-Population'} if scalar keys %{$freq_data->{$pop_id}{$ssid}{'pop_info'}{'Sub-Population'}};
       
       foreach my $gt (@{$freq_data->{$pop_id}{$ssid}{'Alleles'}}) {
         next unless $gt =~ /(\w|\-)+/;
         
         my $freq = $self->format_number(shift @allele_freq);
-        if ($freq ne 'unknown') {
-          $pop_freq->{$pop_name}{freq}{$ssid}{$gt} = $freq;
-        }
+        
+        $pop_freq->{$pop_name}{'freq'}{$ssid}{$gt} = $freq if $freq ne 'unknown';
       }
     }
   }
+  
   return $pop_freq;
 }
 
@@ -163,32 +173,30 @@ sub format_number {
   ### Returns "unknown" if null or formats the number to 3 decimal places
 
   my ($self, $number) = @_;
-  $number = $number*100 if (defined $number);
+  $number = $number * 100 if defined $number;
   return defined $number ? sprintf '%.2f', $number : 'unknown';
 }
 
-
 sub update_pop_tree {
-  my $self = shift;
-  my $p_tree = shift;
-  my $p_name = shift;
-  my $sub_list = shift;
-  
+  my ($self, $p_tree, $p_name, $sub_list) = @_;
   my $p_short_name = $self->get_short_name($p_name);
-  foreach my $sub_pop (keys(%{$sub_list})) {
-    my $sub_name = $sub_list->{$sub_pop}{Name};
+  
+  foreach my $sub_pop (keys %$sub_list) {
+    my $sub_name       = $sub_list->{$sub_pop}{'Name'};
     my $sub_short_name = $self->get_short_name($sub_name);
+    
     $p_tree->{$p_short_name}{$sub_short_name} = 1;
   }
+  
   return $p_tree;
 }
-
 
 sub get_short_name {
   my $self   = shift;
   my $p_name = shift;
-  $p_name =~ /phase_1_(.+)/; # Gets a shorter name for the display
-  return ($1) ? $1 : $p_name;
+     $p_name =~ /phase_1_(.+)/; # Gets a shorter name for the display
+  
+  return $1 || $p_name;
 }
 
 1;
