5f305811e54ea562849994e65109356d10b07794
Simon Brent <sb23@sanger.ac.uk>
2012-10-16 09:20:05 +0000

    tidied

--- modules/EnsEMBL/Web/Constants.pm
+++ modules/EnsEMBL/Web/Constants.pm
@@ -6,12 +6,13 @@ no warnings 'uninitialized';
 
 sub ICON_MAPPINGS {
   my $component = shift || 'page';
+  
   return {
-      'config'    => {'file' => 'setting.png', 'alt' => 'config', 'title' => "Configure this $component"},
-      'search'    => {'file' => 'search.png', 'alt' => 'search', 'title' => "Search this $component"},
-      'download'  => {'file' => 'download.png', 'alt' => 'download', 'title' => "Download data from this $component"},
-      'image'     => {'file' => 'picture.png', 'alt' => 'image', 'title' => "Export this image"},
-      'userdata'  => {'file' => 'page-user.png', 'alt' => 'data', 'title' => "Manage your custom tracks"},
+    'config'    => { 'file' => 'setting.png',   'alt' => 'config',   'title' => "Configure this $component"          },
+    'search'    => { 'file' => 'search.png',    'alt' => 'search',   'title' => "Search this $component"             },
+    'download'  => { 'file' => 'download.png',  'alt' => 'download', 'title' => "Download data from this $component" },
+    'image'     => { 'file' => 'picture.png',   'alt' => 'image',    'title' => "Export this image"                  },
+    'userdata'  => { 'file' => 'page-user.png', 'alt' => 'data',     'title' => "Manage your custom tracks"          },
   };
 }
 
@@ -22,12 +23,12 @@ sub FORMATS {
 
 sub EXPORT_FORMATS {
   return (
-	  'png'  => { 'name' => 'PNG', 'longname' => 'Portable Network Graphics',   'extn' => 'png', 'mime' => 'image/png' },
-    'gif'  => { 'name' => 'GIF', 'longname' => 'Graphics Interchange Format', 'extn' => 'gif', 'mime' => 'image/gif' },
-	  'svg'  => { 'name' => 'SVG', 'longname' => 'Scalable Vector Graphics',    'extn' => 'svg', 'mime' => 'image/svg+xml' },
+	  'png'  => { 'name' => 'PNG', 'longname' => 'Portable Network Graphics',   'extn' => 'png', 'mime' => 'image/png'              },
+    'gif'  => { 'name' => 'GIF', 'longname' => 'Graphics Interchange Format', 'extn' => 'gif', 'mime' => 'image/gif'              },
+	  'svg'  => { 'name' => 'SVG', 'longname' => 'Scalable Vector Graphics',    'extn' => 'svg', 'mime' => 'image/svg+xml'          },
 	  'eps'  => { 'name' => 'EPS', 'longname' => 'Encapsulated Postscript',     'extn' => 'eps', 'mime' => 'application/postscript' },
-	  'pdf'  => { 'name' => 'PDF', 'longname' => 'Portable Document Format',    'extn' => 'pdf', 'mime' => 'application/pdf' },
-	  'gff'  => { 'name' => 'GFF', 'longname' => 'General Feature Format',      'extn' => 'txt', 'mime' => 'text/plain' }
+	  'pdf'  => { 'name' => 'PDF', 'longname' => 'Portable Document Format',    'extn' => 'pdf', 'mime' => 'application/pdf'        },
+	  'gff'  => { 'name' => 'GFF', 'longname' => 'General Feature Format',      'extn' => 'txt', 'mime' => 'text/plain'             }
   );
 }
 
@@ -158,9 +159,9 @@ sub GENERAL_MARKUP_OPTIONS {
       'name'   => 'line_numbering',
       'label'  => 'Line numbering',
       'values' => [
-        { 'value' => 'sequence', 'name' => 'Relative to this sequence' },
+        { 'value' => 'sequence', 'name' => 'Relative to this sequence'      },
         { 'value' => 'slice',    'name' => 'Relative to coordinate systems' },
-        { 'value' => 'off',      'name' => 'None' },
+        { 'value' => 'off',      'name' => 'None'                           },
       ]
     },
     'exon_ori' => {
@@ -169,9 +170,9 @@ sub GENERAL_MARKUP_OPTIONS {
       'name'   => 'exon_ori',
       'label'  => 'Orientation of additional exons',
       'values' => [
-        { 'value' => 'fwd', 'name' => 'Display same orientation exons only' },
+        { 'value' => 'fwd', 'name' => 'Display same orientation exons only'    },
         { 'value' => 'rev', 'name' => 'Display reverse orientation exons only' },
-        { 'value' => 'all', 'name' => 'Display exons in both orientations' },
+        { 'value' => 'all', 'name' => 'Display exons in both orientations'     },
       ],
     },
     'pop_filter' => {
@@ -223,9 +224,9 @@ sub GENE_MARKUP_OPTIONS {
       'name'   => 'exon_display',
       'label'  => 'Additional exons to display',
       'values' => [
-        { 'value' => 'off',       'name' => 'No exon markup' },
+        { 'value' => 'off',       'name' => 'No exon markup'  },
         { 'value' => 'Ab-initio', 'name' => 'Ab-initio exons' },
-        { 'value' => 'core',      'name' => 'Core exons' },
+        { 'value' => 'core',      'name' => 'Core exons'      },
       ],
     },
   );
@@ -259,7 +260,7 @@ sub OTHER_MARKUP_OPTIONS {
       'name'   => 'codons_display',
       'label'  => 'Codons',
       'values' => [
-        { 'value' => 'all', 'name' => 'START/STOP codons' },
+        { 'value' => 'all', 'name' => 'START/STOP codons'  },
         { 'value' => 'off', 'name' => 'Do not show codons' },
       ],
     },
@@ -270,7 +271,7 @@ sub OTHER_MARKUP_OPTIONS {
       'label'  => 'Display pop-up information on mouseover',
       'values' => [
         { 'value' => 'all', 'name' => 'Yes' },
-        { 'value' => 'off', 'name' => 'No' },
+        { 'value' => 'off', 'name' => 'No'  },
       ],
     },
   );
@@ -280,59 +281,59 @@ sub OTHER_MARKUP_OPTIONS {
 sub VARIATION_OPTIONS {
   return (
     'variation' =>  {
-      'opt_freq'       =>  [ 'on', 'By frequency' ],
-      'opt_cluster'    =>  [ 'on', 'By Cluster' ],
-      'opt_doublehit'  =>  [ 'on', 'By doublehit' ],
-      'opt_submitter'  =>  [ 'on', 'By submitter' ],
-      'opt_hapmap'     =>  [ 'on', 'Hapmap' ],
-      'opt_1000Genome' =>  [ 'on', '1000 genomes' ],
+      'opt_freq'       =>  [ 'on', 'By frequency'      ],
+      'opt_cluster'    =>  [ 'on', 'By Cluster'        ],
+      'opt_doublehit'  =>  [ 'on', 'By doublehit'      ],
+      'opt_submitter'  =>  [ 'on', 'By submitter'      ],
+      'opt_hapmap'     =>  [ 'on', 'Hapmap'            ],
+      'opt_1000Genome' =>  [ 'on', '1000 genomes'      ],
       'opt_precious'   =>  [ 'on', 'Precious variants' ],
-      'opt_noinfo'     =>  [ 'on', 'No information' ],
+      'opt_noinfo'     =>  [ 'on', 'No information'    ],
     }, 
     'class' =>  {
-      'opt_class_insertion'              =>  [ 'on', 'Insertions' ],
-      'opt_class_deletion'               =>  [ 'on', 'Deletions' ],
-      'opt_class_indel'                  =>  [ 'on', 'In-dels' ],
-      'opt_class_snp'                    =>  [ 'on', 'SNPs' ],
+      'opt_class_insertion'              =>  [ 'on', 'Insertions'             ],
+      'opt_class_deletion'               =>  [ 'on', 'Deletions'              ],
+      'opt_class_indel'                  =>  [ 'on', 'In-dels'                ],
+      'opt_class_snp'                    =>  [ 'on', 'SNPs'                   ],
       'opt_class_cnv'                    =>  [ 'on', 'Copy number variations' ],
-      'opt_class_substitution'           =>  [ 'on', 'Substitutions' ],
-      'opt_class_tandem_repeat'          =>  [ 'on', 'Tandem repeats' ],
-      'opt_class_'                       =>  [ 'on', 'Unclassified' ],
+      'opt_class_substitution'           =>  [ 'on', 'Substitutions'          ],
+      'opt_class_tandem_repeat'          =>  [ 'on', 'Tandem repeats'         ],
+      'opt_class_'                       =>  [ 'on', 'Unclassified'           ],
 	  
-      'opt_class_somatic_insertion'      =>  [ 'on', 'Somatic insertions' ],
-      'opt_class_somatic_deletion'       =>  [ 'on', 'Somatic deletions' ],
-      'opt_class_somatic_indel'          =>  [ 'on', 'Somatic in-dels' ],
-      'opt_class_somatic_snv'            =>  [ 'on', 'Somatic SNVs' ],
+      'opt_class_somatic_insertion'      =>  [ 'on', 'Somatic insertions'             ],
+      'opt_class_somatic_deletion'       =>  [ 'on', 'Somatic deletions'              ],
+      'opt_class_somatic_indel'          =>  [ 'on', 'Somatic in-dels'                ],
+      'opt_class_somatic_snv'            =>  [ 'on', 'Somatic SNVs'                   ],
       'opt_class_somatic_cnv'            =>  [ 'on', 'Somatic copy number variations' ],
-      'opt_class_somatic_substitution'   =>  [ 'on', 'Somatic substitutions' ],
-      'opt_class_somatic_tandem_repeat'  =>  [ 'on', 'Somatic tandem repeats' ],
+      'opt_class_somatic_substitution'   =>  [ 'on', 'Somatic substitutions'          ],
+      'opt_class_somatic_tandem_repeat'  =>  [ 'on', 'Somatic tandem repeats'         ],
       'opt_class_somatic_'               =>  [ 'on', 'Unclassified somatic mutations' ],
     }, 
     'type' => {
-      'opt_transcript_ablation'               =>  [ 'on', 'Essential splice site', 1 ],
-      'opt_splice_donor_variant'              =>  [ 'on', 'Splice donor', 3 ],
-      'opt_splice_acceptor_variant'           =>  [ 'on', 'Splice acceptor', 3 ],
-      'opt_stop_gained'                       =>  [ 'on', 'Stop gained', 4 ],
-      'opt_frameshift_variant'                =>  [ 'on', 'Frameshift', 5 ],
-      'opt_stop_lost'                         =>  [ 'on', 'Stop lost', 6 ],
-      'opt_initiator_codon_variant'           =>  [ 'on', 'Initiator codon', 7 ],
-      'opt_inframe_insertion'                 =>  [ 'on', 'Inframe insertion', 10 ],
-      'opt_inframe_deletion'                  =>  [ 'on', 'Inframe deletion', 11 ],
-      'opt_missense_variant'                  =>  [ 'on', 'Missense', 12 ],
-      'opt_splice_region_variant'             =>  [ 'on', 'Splice region', 13 ],
-      'opt_incomplete_terminal_codon_variant' =>  [ 'on', 'Incomplete terminal codon', 14],
-      'opt_synonymous_variant'                =>  [ 'on', 'Synonymous', 15 ],
-      'opt_stop_retained'                     =>  [ 'on', 'Stop retained', 15 ],
-      'opt_coding_sequence_variant'           =>  [ 'on', 'Coding sequence', 16 ],
-      'opt_mature_mirna_variant'              =>  [ 'on', 'Mature miRNA', 17 ],
-      'opt_5_prime_utr_variant'               =>  [ 'on', "5' UTR", 18 ],
-      'opt_3_prime_utr_variant'               =>  [ 'on', "3' UTR", 19 ],
-      'opt_intron_variant'                    =>  [ 'on', 'Intron', 20 ],
-      'opt_nmd_transcript_variant'            =>  [ 'on', 'NMD transcript', 21 ],
-      'opt_non_coding_exon_variant'           =>  [ 'on', 'Non-coding exon', 22 ],
-      'opt_nc_transcript_variant'             =>  [ 'on', 'Non-coding transcript', 23 ],
-      'opt_upstream_gene_variant'             =>  [ 'on', 'Upstream', 24 ],
-      'opt_downstream_gene_variant'           =>  [ 'on', 'Downstream', 25 ],
+      'opt_transcript_ablation'               =>  [ 'on', 'Essential splice site',       1  ],
+      'opt_splice_donor_variant'              =>  [ 'on', 'Splice donor',                3  ],
+      'opt_splice_acceptor_variant'           =>  [ 'on', 'Splice acceptor',             3  ],
+      'opt_stop_gained'                       =>  [ 'on', 'Stop gained',                 4  ],
+      'opt_frameshift_variant'                =>  [ 'on', 'Frameshift',                  5  ],
+      'opt_stop_lost'                         =>  [ 'on', 'Stop lost',                   6  ],
+      'opt_initiator_codon_variant'           =>  [ 'on', 'Initiator codon',             7  ],
+      'opt_inframe_insertion'                 =>  [ 'on', 'Inframe insertion',           10 ],
+      'opt_inframe_deletion'                  =>  [ 'on', 'Inframe deletion',            11 ],
+      'opt_missense_variant'                  =>  [ 'on', 'Missense',                    12 ],
+      'opt_splice_region_variant'             =>  [ 'on', 'Splice region',               13 ],
+      'opt_incomplete_terminal_codon_variant' =>  [ 'on', 'Incomplete terminal codon',   14 ],
+      'opt_synonymous_variant'                =>  [ 'on', 'Synonymous',                  15 ],
+      'opt_stop_retained'                     =>  [ 'on', 'Stop retained',               15 ],
+      'opt_coding_sequence_variant'           =>  [ 'on', 'Coding sequence',             16 ],
+      'opt_mature_mirna_variant'              =>  [ 'on', 'Mature miRNA',                17 ],
+      'opt_5_prime_utr_variant'               =>  [ 'on', "5' UTR",                      18 ],
+      'opt_3_prime_utr_variant'               =>  [ 'on', "3' UTR",                      19 ],
+      'opt_intron_variant'                    =>  [ 'on', 'Intron',                      20 ],
+      'opt_nmd_transcript_variant'            =>  [ 'on', 'NMD transcript',              21 ],
+      'opt_non_coding_exon_variant'           =>  [ 'on', 'Non-coding exon',             22 ],
+      'opt_nc_transcript_variant'             =>  [ 'on', 'Non-coding transcript',       23 ],
+      'opt_upstream_gene_variant'             =>  [ 'on', 'Upstream',                    24 ],
+      'opt_downstream_gene_variant'           =>  [ 'on', 'Downstream',                  25 ],
       'opt_sara'                              =>  [ 'on', 'SARA (same as ref.assembly)', 40 ],
     },
   );
@@ -349,74 +350,71 @@ sub MESSAGE_PRIORITY {
 
 sub USERDATA_MESSAGES {
   return (
-    no_url        => {
-                      'type'    => 'error', 
-                      'title'   => 'Input problem',
-                      'message' => 'No URL was entered. Please try again.',
-                      },
-    no_response   => {
-                      'type'    => 'error', 
-                      'title'   => 'File system error',
-                      'message' => 'We were unable to access your data file. If you continue to get this message, there may be an network issue, or your file may be too large for us to upload.',
-                      },
-    file_format   => {
-                      'type'    => 'error', 
-                      'title'   => 'Input problem',
-                      'message' => 'Your file does not appear to be in a valid format. Please try again.',
-                      },
-    file_empty    => {
-                      'type'    => 'error', 
-                      'title'   => 'Input problem',
-                      'message' => 'Your file appears to be empty. Please check that it contains correctly-formatted data.',
-                      },
-    file_size     => {
-                      'type'    => 'error', 
-                      'title'   => 'File system error',
-                      'message' => 'Your file is too big to upload. Please select a smaller file.',
-                      },
-    file_save     => {
-                      'type'    => 'error', 
-                      'title'   => 'File system error',
-                      'message' => 'Your data could not be saved. Please check the file contents and try again.'
-                      },
-
-    load_file     => {
-                      'type'    => 'error', 
-                      'title'   => 'File system error',
-                      'message' => 'There was an error retrieving your data from disk.'
-                      },
-
-    save_file     => {
-                      'type'    => 'error', 
-                      'title'   => 'Database error',
-                      'message' => 'Unable to save uploaded file contents to your account',
-                      },
-    save_das      => {
-                      'type'    => 'error', 
-                      'title'   => 'Database error',
-                      'message' => 'Unable to save DAS details to your account'}
-                      ,
-    save_url      => {
-                      'type'    => 'error', 
-                      'title'   => 'Database error',
-                      'message' => 'Unable to save remote URL to your account'
-                      },
-
-    no_features   => {
-                      'type'    => 'warning', 
-                      'title'   => 'Script warning',
-                      'message' => 'The script returned no features.',
-                      },
-    location_unknown  => {
-                            'type'    => 'warning', 
-                            'title'   => 'Input problem',
-                            'message' => "The selected region(s) lie outside the scope of this species' chromosomes.",
-                          },
+    no_url => {
+      'type'    => 'error', 
+      'title'   => 'Input problem',
+      'message' => 'No URL was entered. Please try again.',
+    },
+    no_response => {
+      'type'    => 'error', 
+      'title'   => 'File system error',
+      'message' => 'We were unable to access your data file. If you continue to get this message, there may be an network issue, or your file may be too large for us to upload.',
+    },
+    file_format => {
+      'type'    => 'error', 
+      'title'   => 'Input problem',
+      'message' => 'Your file does not appear to be in a valid format. Please try again.',
+    },
+    file_empty => {
+      'type'    => 'error', 
+      'title'   => 'Input problem',
+      'message' => 'Your file appears to be empty. Please check that it contains correctly-formatted data.',
+    },
+    file_size => {
+      'type'    => 'error', 
+      'title'   => 'File system error',
+      'message' => 'Your file is too big to upload. Please select a smaller file.',
+    },
+    file_save => {
+      'type'    => 'error', 
+      'title'   => 'File system error',
+      'message' => 'Your data could not be saved. Please check the file contents and try again.'
+    },
+    load_file => {
+      'type'    => 'error', 
+      'title'   => 'File system error',
+      'message' => 'There was an error retrieving your data from disk.'
+    },
+    save_file => {
+      'type'    => 'error', 
+      'title'   => 'Database error',
+      'message' => 'Unable to save uploaded file contents to your account',
+    },
+    save_das => {
+      'type'    => 'error', 
+      'title'   => 'Database error',
+      'message' => 'Unable to save DAS details to your account'
+    },
+    save_url => {
+      'type'    => 'error', 
+      'title'   => 'Database error',
+      'message' => 'Unable to save remote URL to your account'
+    },
+    no_features => {
+      'type'    => 'warning', 
+      'title'   => 'Script warning',
+      'message' => 'The script returned no features.',
+    },
+    location_unknown => {
+      'type'    => 'warning', 
+      'title'   => 'Input problem',
+      'message' => "The selected region(s) lie outside the scope of this species' chromosomes.",
+    },
     location_toolarge => {
-                            'type'    => 'warning', 
-                            'title'   => 'Script aborted',
-                            'message' => 'The region(s) you selected are too large and will return too much data for the web interface to cope with.'
-                          },
+      'type'    => 'warning', 
+      'title'   => 'Script aborted',
+      'message' => 'The region(s) you selected are too large and will return too much data for the web interface to cope with.'
+    },
   );
 }
 
