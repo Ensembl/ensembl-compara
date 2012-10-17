7378b4188f426860cdf9c8cf29b12a4340a3c46b
Dan Sheppard <ds23@sanger.ac.uk>
2012-10-17 16:19:39 +0000

    Avoid letter soup from superimposed error messages when empty tracks is turned on and methylation is zoomed out.

--- modules/Bio/EnsEMBL/GlyphSet/fg_methylation.pm
+++ modules/Bio/EnsEMBL/GlyphSet/fg_methylation.pm
@@ -16,6 +16,7 @@ sub features {
   if($slice->length > 200000) {
     if(not $self->{'config'}->{'_sent_ch3_error_track'}) {
       $self->{'config'}->{'_sent_ch3_error_track'} = 1;
+      $self->{'no_empty_track_message'} = 1;
       return $self->errorTrack("Methylation data is only viewable on images  200kb in size");
     } else {
       return undef;
