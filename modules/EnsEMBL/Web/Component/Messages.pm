fd415829f6d03f153eb14cedda7933a9022ba060
Simon Brent <sb23@sanger.ac.uk>
2012-10-23 12:57:10 +0000

    allow us to set order for messages to be displayed

--- modules/EnsEMBL/Web/Component/Messages.pm
+++ modules/EnsEMBL/Web/Component/Messages.pm
@@ -28,7 +28,8 @@ sub content {
   my $html;
   
   # Group messages by type
-  push @{$messages{$_->{'function'} || '_info'}}, $_->{'message'} for $session->get_data(type => 'message');
+  # Set a default order of 100 - we probably aren't going to have 100 messages on the page at once, and this allows us to force certain messages to the bottom by giving order > 100
+  push @{$messages{$_->{'function'} || '_info'}}, $_->{'message'} for sort { $a->{'order'} || 100 <=> $b->{'order'} || 100 } $session->get_data(type => 'message');
   
   $session->purge_data(type => 'message');
   
