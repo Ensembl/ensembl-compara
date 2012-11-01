d400e67deef2e6b46b6844942004e24bb7e33ae3
Simon Brent <sb23@sanger.ac.uk>
2012-11-01 15:15:10 +0000

    added delay, container, choosing events

--- htdocs/components/00_jquery_helptip.js
+++ htdocs/components/00_jquery_helptip.js
@@ -7,93 +7,135 @@
  **/
 (function ($) {
   $.helptip = function (el, tip, options) {
-    el = $(el);
+    el      = $(el);
+    options = $.extend({
+      eventIn: 'mouseenter',
+      eventOut: 'mouseleave',
+      delay: 100
+    }, options);
+    
+    var container = options.container || $(document.body);
     
     if (typeof tip === 'undefined' || tip === '') {
-      tip = el[0].title;
+      tip = el[0].title.replace(/\n/g, '<br />');
       el.removeAttr('title');
     }
     
     var methods = {
-      init: function() {
-        this.data('helptip').popup = $('<div class="helptip"><div class="ht-inner"></div></div>').appendTo(document.body);
+      init: function () {
+        this.data('helptip').popup = $('<div class="helptip"><div class="ht-inner"></div></div>').appendTo(container);
       },
       
-      setTip: function(popup, html) {
+      setTip: function (popup, html) {
         popup.children().html(html);
       },
       
-      setOption: function(popup, options) {
+      setOption: function (popup, options) {
         this.data('helptip').options = options;
-        if (this.data('helptip')['class']) { //previously added class
+        
+        if (this.data('helptip')['class']) { // previously added class
           popup.removeClass(this.data('helptip')['class']);
         }
+        
         if (options['class']) {
           popup.addClass(options['class']);
           this.data('helptip')['class'] = options['class'];
         }
+        
         popup.toggleClass('helptip-static', options['static']);
       },
       
-      setEventHandlers: function() {
-        this.on({
-          'mouseover.helptip': function(e) {
-            var el      = $(this);
-            var popup   = el.data('helptip').popup;
-            var options = el.data('helptip').options;
-            var pos;
-            
-            if (options['static']) {
-              var offset = el.offset();
-                  pos    = { x: offset.left + el.outerWidth() / 2, y: offset.top + el.outerHeight() / 2 };
-            } else {
-              pos = { x: e.pageX, y: e.pageY };
-              
-              if (!el.data('helptip').mousemove) {
-                el.on('mousemove.helptip', function (e) {
-                  var el = $(this);
-                  if (!el.data('helptip').hidden) {
-                    var offset = el.data('helptip').offset;
-                    el.data('helptip').popup.css({ left: offset.x + e.pageX, top: offset.y + e.pageY }).show();
-                  }
-                });
-                el.data('helptip').mousemove = true;
-              }
-            }
-            
-            popup.removeClass('helptip-left helptip-top');
-            
-            if ($(window).height() < ((pos.y - $(window).scrollTop()) * 2)) {
-              popup.addClass('helptip-top');
-              pos.y -= popup.height();
-            }
-            
-            if ($(window).width() < ((pos.x - $(window).scrollLeft()) * 2)) {
-              popup.addClass('helptip-left');
-              pos.x -= (el.data('helptip').offset || {}).width || popup.width();
-            }
-            if (!el.data('helptip').hidden) {
-              popup.css({ left: pos.x, top: pos.y }).show();
-            }
-            el.data('helptip').offset = { x: pos.x - e.pageX, y: pos.y - e.pageY, width: popup.width() };
-          },
+      setEventHandlers: function () {
+        var handlers = {};
+        var func;
+        
+        if (options.delay) {
+          handlers = { over: methods.mouseIn, out: methods.mouseOut, interval: options.delay };
+          func     = 'hoverIntent';
           
-          'mouseout.helptip': function () {
-            $(this).data('helptip').popup.hide();
+          this.on('mousemove.helptip-track', function (e) {
+            var data = $(this).data('helptip');
+            
+            if (!data.mousemove) {
+              data.coords = { x: e.pageX, y: e.pageY };
+            }
+          });
+        } else {
+          handlers[options.eventIn  + '.helptip'] = methods.mouseIn;
+          handlers[options.eventOut + '.helptip'] = methods.mouseOut;
+          func = 'on';
+        }
+        
+        this[func](handlers).data('helptip').initiated = true;
+      },
+      
+      mouseIn: function (e) {
+        var el      = $(this);
+        var $window = $(window);
+        var popup   = el.data('helptip').popup;
+        var options = el.data('helptip').options;
+        var pos, originalPos;
+        
+        if (options['static']) {
+          var offset = options.container ? el.position() : el.offset();
+          originalPos = pos = { x: offset.left + el.outerWidth() / 2, y: offset.top + el.outerHeight() / 2 };
+        } else {
+          originalPos = pos = el.data('helptip').coords || { x: e.pageX, y: e.pageY };
+          
+          el.data('helptip').coords = false;
+          
+          if (!el.data('helptip').mousemove) {
+            el.on('mousemove.helptip', methods.mouseMove).data('helptip').mousemove = true;
           }
-        }).data('helptip').initiated = true;
+        }
+        
+        popup.removeClass('helptip-left helptip-top');
+        
+        if ($window.height() < ((pos.y - $window.scrollTop()) * 2)) {
+          popup.addClass('helptip-top');
+          pos.y -= popup.height();
+        }
+        
+        if ($window.width() < ((pos.x - $window.scrollLeft()) * 2)) {
+          popup.addClass('helptip-left');
+          pos.x -= (el.data('helptip').offset || {}).width || popup.width();
+        }
+        
+        if (!el.data('helptip').hidden) {
+          popup.css({ left: pos.x, top: pos.y }).show();
+        }
+        
+        el.data('helptip').offset = { x: pos.x - originalPos.x, y: pos.y - originalPos.y, width: popup.width() };
+        
+        el = popup = $window = null;
+      },
+      
+      mouseOut: function () {
+        $(this).data('helptip').popup.hide();
+        $(this).off('mousemove.helptip').data('helptip').mousemove = false;
+      },
+      
+      mouseMove: function (e) {
+        var el = $(this);
+        
+        if (!el.data('helptip').hidden) {
+          var offset = el.data('helptip').offset;
+          el.data('helptip').popup.css({ left: offset.x + e.pageX, top: offset.y + e.pageY });
+        }
       },
       
       hide: function (popup) {
         this.data('helptip').hidden = true;
-        popup.hide()
+        popup.hide();
       },
       
       destroy: function (popup) {
-        this.off('.helptip');
+        this.off('.helptip .helptip-track');
+        
         if (popup) {
           popup.remove();
         }
+        
         this.removeData('helptip');
       }
     };
@@ -107,13 +149,13 @@
     
     // destroy any existing helptip instance if first argument is set false
     if (tip === false) {
-      methods.destroy.apply(el, [popup]);
+      methods.destroy.call(el, popup);
       return;
     }
     
     // if not already, instantiate helptip
     if (!popup) {
-      methods.init.apply(el);
+      methods.init.call(el);
       popup = el.data('helptip').popup;
     } else {
       el.data('helptip').hidden = false;
@@ -121,19 +163,19 @@
     
     // set tip
     if (tip) {
-      methods.setTip.apply(el, [popup, tip]);
+      methods.setTip.call(el, popup, tip);
     } else {
-      methods.hide.apply(el, [popup]);
+      methods.hide.call(el, popup);
     }
     
     // set options
     if (!$.isEmptyObject(options)) {
-      methods.setOption.apply(el, [popup, options]);
+      methods.setOption.call(el, popup, options);
     }
     
     // set event handlers
     if (!el.data('helptip').initiated) {
-      methods.setEventHandlers.apply(el);
+      methods.setEventHandlers.call(el);
     }
   };
 
