c0a79ec979488664f269a7f40d2271d4a31eb1b9
Dan Sheppard <ds23@sanger.ac.uk>
2012-11-08 17:28:50 +0000

    Steve Wilder's suggested methylation legend improvements.

--- modules/Bio/EnsEMBL/GlyphSet/meth_legend.pm
+++ modules/Bio/EnsEMBL/GlyphSet/meth_legend.pm
@@ -23,26 +23,27 @@ sub _init {
   my ($x, $y) = (0, 0);
     
   my %font        = $self->get_font_details('legend', 1);
-  my $text_height = [ $self->get_text_width(0, 'X', '', %font) ]->[3];
-
+  my ($t1,$t2,$text_width,$text_height) =
+    $self->get_text_width(0, 'X', '', %font);
   
   my @cg = $self->{'config'}->colourmap
-                ->build_linear_gradient(11, [ qw(yellow green blue) ]);
+                ->build_linear_gradient(10, [ qw(yellow green blue) ]);
   my $bw = 20;
-  for my $i (0..$#cg) {
-    $self->push($self->Rect({
-      x             => $i * $bw,
-      y             => 2,
-      width         => $bw, 
-      height        => $text_height - 2,
-      colour        => $cg[$i],
-      absolutey     => 1,
-      absolutex     => 1,
-      absolutewidth => 1
-    }));
-
+  for my $i (0..@cg) {
+    if($i<@cg) {
+      $self->push($self->Rect({
+        x             => $i * $bw,
+        y             => 2,
+        width         => $bw, 
+        height        => $text_height - 2,
+        colour        => $cg[$i],
+        absolutey     => 1,
+        absolutex     => 1,
+        absolutewidth => 1
+      }));
+    }
     $self->push($self->Text({
-      x             => $i* $bw,
+      x             => $i* $bw - ($text_width*($i?2:0)/3) ,
       y             => $text_height,
       height        => $text_height,
       valign        => 'center',
