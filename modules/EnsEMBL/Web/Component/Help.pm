08d82f3dcbf2f11e23dbd07b86fdad1cf260e737
Harpreet Riat <hr5@sanger.ac.uk>
2012-11-20 16:03:30 +0000

    tidy; parse_help_html method modified to parse images too; inline css removed

--- modules/EnsEMBL/Web/Component/Help.pm
+++ modules/EnsEMBL/Web/Component/Help.pm
@@ -5,35 +5,32 @@ package EnsEMBL::Web::Component::Help;
 use base qw( EnsEMBL::Web::Component);
 use strict;
 
-sub kw_hilite {
-  ### Highlights the search keyword(s) in the text, omitting HTML tag contents
-  my ($self, $content) = @_;
-  my $kw = $self->hub->param('string');
-  return $content unless $kw;
-
-  $content =~ s/($kw)(?![^<]*?>)/<span class="hilite">$1<\/span>/img;
-  return $content;
-}
-
 sub embed_movie {
   my ($self, $movie) = @_;
 
   return sprintf '
     <h3>%s</h3>
-    <p class="space-below"><iframe width="640" height="480" src="http://www.youtube-nocookie.com/embed/%s" frameborder="0" allowfullscreen="allowfullscreen" style="margin:0 auto"></iframe></p>
-    <p class="space-below"><a href="http://www.youtube.com/user/EnsemblHelpdesk"><img alt="" src="%s/img/youtube.png" /></a></p>',
+    <p><iframe width="640" height="480" src="http://www.youtube-nocookie.com/embed/%s" frameborder="0" allowfullscreen="allowfullscreen" style="margin:0 auto"></iframe></p>
+    <p><a href="http://www.youtube.com/user/EnsemblHelpdesk"><img alt="" src="%s/img/youtube.png" /></a></p>',
     $movie->{'title'}, $movie->{'youtube_id'}, $self->hub->species_defs->ENSEMBL_STATIC_SERVER;
 }
 
 sub parse_help_html {
+  ## Parses help content looking for embedded movie and images placeholders
   my ($self, $content, $adaptor) = @_;
 
+  my $sd      = $self->hub->species_defs;
+  my $img_url = $sd->ENSEMBL_STATIC_SERVER.$sd->ENSEMBL_HELP_IMAGE_ROOT;
   my $html;
 
-  ### Parse help looking for embedded movie placeholders
-  foreach my $line (split('\n', $content)) {
+  foreach my $line (split '\n', $content) {
+
     if ($line =~ /\[\[movie=(\d+)/i) {
       $line = $self->embed_movie(@{$adaptor->fetch_help_by_ids([$1]) || []});
+    }
+
+    while ($line =~ /\[\[image=([^\s]+)\s+([^\]]+)\s*\]\]/ig) {
+      substr $line, $-[0], $+[0] - $-[0], qq(<img src="$img_url$1" $2 \/>); # replace square bracket tag with actual image
     }
 
     $html .= $line;
@@ -43,7 +40,7 @@ sub parse_help_html {
 }
 
 sub help_feedback {
-  my ($self, $style, $id, %args) = @_;
+  my ($self, $id, %args) = @_;
   return ''; ## FIXME - this needs to be reenabled when we have time
   
   my $html = qq{
