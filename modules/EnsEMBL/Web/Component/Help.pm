ad1ee4489c8a7f5e147a9753a434483a114ad70f
Anne Parker <ap5@sanger.ac.uk>
2012-11-23 12:07:24 +0000

    Extended embed_movie to support YouKu where appropriate

--- modules/EnsEMBL/Web/Component/Help.pm
+++ modules/EnsEMBL/Web/Component/Help.pm
@@ -8,11 +8,27 @@ use strict;
 sub embed_movie {
   my ($self, $movie) = @_;
 
+  ## Select YouKu (if possible) for visitors within China
+  my $country = $self->requesting_country;
+  my ($source, $channel, $logo, $alt);
+  if ($country eq 'CN' && $movie->{'youku_id'}) {    
+    $source   = sprintf('http://v.youku.com/v_show/id_%s.html', $movie->{'youku_id'});
+    $channel  = 'http://u.youku.com/Ensemblhelpdesk';
+    $logo     = $self->hub->species_defs->ENSEMBL_STATIC_SERVER.'/img/youku.png';
+    $alt      = 'Youku &#20248;&#37239;&#32593; channel';
+  }  
+  else {    
+    $source   = sprintf('http://www.youtube-nocookie.com/embed/', $movie->{'youtube_id'});
+    $channel  = '"http://www.youtube.com/user/EnsemblHelpdesk';
+    $logo     = $self->hub->species_defs->ENSEMBL_STATIC_SERVER.'/img/youtube.png';
+    $alt      = 'YouTube channel';
+  }
+
   return sprintf '
     <h3>%s</h3>
-    <p><iframe width="640" height="480" src="http://www.youtube-nocookie.com/embed/%s" frameborder="0" allowfullscreen="allowfullscreen" style="margin:0 auto"></iframe></p>
-    <p><a href="http://www.youtube.com/user/EnsemblHelpdesk"><img alt="" src="%s/img/youtube.png" /></a></p>',
-    $movie->{'title'}, $movie->{'youtube_id'}, $self->hub->species_defs->ENSEMBL_STATIC_SERVER;
+    <p><iframe width="640" height="480" src="%s" frameborder="0" allowfullscreen="allowfullscreen" style="margin:0 auto"></iframe></p>
+    <p><a href="%s"><img alt="%s" src="%s" /></a></p>',
+    $movie->{'title'}, $source, $channel, $alt, $logo;
 }
 
 sub parse_help_html {
