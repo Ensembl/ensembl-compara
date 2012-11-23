00cd8a3d35a95a39c6680efb5328ef41947b0de4
Harpreet Riat <hr5@sanger.ac.uk>
2012-11-23 14:26:49 +0000

    embed code changed for youku

--- modules/EnsEMBL/Web/Component/Help.pm
+++ modules/EnsEMBL/Web/Component/Help.pm
@@ -8,17 +8,15 @@ use strict;
 sub embed_movie {
   my ($self, $movie) = @_;
 
-  ## Select YouKu (if possible) for visitors within China
-  my $country = $self->requesting_country;
-  my ($source, $channel, $logo, $alt);
-  if ($country eq 'CN' && $movie->{'youku_id'}) {    
-    $source   = sprintf('http://v.youku.com/v_show/id_%s.html', $movie->{'youku_id'});
+  my ($embed, $channel, $logo, $alt);
+  if ($movie->{'youku_id'} && $self->requesting_country eq 'CN') {  ## Select YouKu (if possible) for visitors within China
+    $embed    = sprintf('<embed src="http://player.youku.com/player.php/sid/%s/v.swf" allowFullScreen="true" quality="high" width="640" height="480" align="middle" allowScriptAccess="always" type="application/x-shockwave-flash"></embed>', $movie->{'youku_id'});
     $channel  = 'http://u.youku.com/Ensemblhelpdesk';
     $logo     = $self->hub->species_defs->ENSEMBL_STATIC_SERVER.'/img/youku.png';
     $alt      = 'Youku &#20248;&#37239;&#32593; channel';
-  }  
-  else {    
-    $source   = sprintf('http://www.youtube-nocookie.com/embed/%s', $movie->{'youtube_id'});
+  }
+  else {
+    $embed    = sprintf('<iframe width="640" height="480" src="http://www.youtube-nocookie.com/embed/%s" frameborder="0" allowfullscreen="allowfullscreen" style="margin:0 auto"></iframe>', $movie->{'youtube_id'});
     $channel  = '"http://www.youtube.com/user/EnsemblHelpdesk';
     $logo     = $self->hub->species_defs->ENSEMBL_STATIC_SERVER.'/img/youtube.png';
     $alt      = 'YouTube channel';
@@ -26,9 +24,9 @@ sub embed_movie {
 
   return sprintf '
     <h3>%s</h3>
-    <p><iframe width="640" height="480" src="%s" frameborder="0" allowfullscreen="allowfullscreen" style="margin:0 auto"></iframe></p>
+    <p>%s</p>
     <p><a href="%s"><img alt="%s" src="%s" /></a></p>',
-    $movie->{'title'}, $source, $channel, $alt, $logo;
+    $movie->{'title'}, $embed, $channel, $alt, $logo;
 }
 
 sub parse_help_html {
