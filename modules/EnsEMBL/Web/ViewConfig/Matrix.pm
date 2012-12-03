b28499c26c8e2bc17704569c033a1522ca6c4bca
Simon Brent <sb23@sanger.ac.uk>
2012-12-03 15:06:27 +0000

    added default renderer. optimized so that subtrack popups aren't added to the html initially. moved tutorials into specific cells, so we don't need to do it in javascript

--- modules/EnsEMBL/Web/ViewConfig/Matrix.pm
+++ modules/EnsEMBL/Web/ViewConfig/Matrix.pm
@@ -11,6 +11,8 @@ use base qw(EnsEMBL::Web::ViewConfig);
 # TODO: Support z dimension - popup with extra options on each square
 # TODO: Support other datahub dimensions as filters?
 
+sub matrix_image_config :lvalue { $_[0]->{'matrix_image_config'}; }
+
 sub init {
   my $self = shift;
   my $defaults;
@@ -51,7 +53,10 @@ sub add_image_config {
 sub reset {
   my $self = shift;
   
-  $_->{'columns'} = [] for map values %$_, values %{$self->{'matrix_config'}};
+  foreach (map values %$_, values %{$self->{'matrix_config'}}) {
+    $_->{'columns'}  = [];
+    $_->{'features'} = {};
+  }
   
   $self->SUPER::reset(@_);
   $self->set_columns(@_);
@@ -59,34 +64,37 @@ sub reset {
 
 sub set_columns {
   my ($self, $image_config) = @_;
-  my $tree = (ref $image_config ? $image_config : $self->hub->get_imageconfig($image_config))->tree;
   
-  foreach (grep $_->data->{'label_x'}, $tree->nodes) {
-    my $set       = $_->data->{'set'};
-    my $label_x   = $_->data->{'label_x'};
-    my $menu      = $tree->clean_id($_->data->{'menu_key'});
-    my $renderers = $_->get('renderers');
+  $self->matrix_image_config = ref $image_config ? $image_config : $self->hub->get_imageconfig($image_config);
+  
+  my $tree = $self->matrix_image_config->tree;
+  
+  foreach my $node (grep $_->data->{'label_x'}, $tree->nodes) {
+    my $data      = $node->data;
+    my $set       = $data->{'set'};
+    my $label_x   = $data->{'label_x'};
+    my $menu      = $tree->clean_id($data->{'menu_key'});
+    my $renderers = $node->get('renderers');
     my %renderers = @$renderers;
     my $conf      = $self->{'matrix_config'}{$menu}{$set} ||= {
       menu         => $set,
-      track_prefix => $set,
+      track_prefix => $menu,
       section      => $tree->get_node($menu)->get('caption'),
-      header       => $_->data->{'header'},
-      description  => $_->data->{'description'},
-      axes         => $_->data->{'axes'},
+      header       => $data->{'header'},
+      description  => $data->{'info'},
+      axes         => $data->{'axes'},
     };
     
-    push @{$conf->{'columns'}}, { display => $_->get('display'), renderers => $renderers, x => $label_x };
-    
-    $conf->{'features'}{$label_x} = $_->data->{'features'};
+    push @{$conf->{'columns'}}, { name => $tree->clean_id(join '_', $conf->{'track_prefix'}, $set, $label_x), x => $label_x, display => $node->get('display'), renderers => $renderers };
+    push @{$conf->{'features'}{$label_x}{$_}}, @{$data->{'features'}{$_}} for keys %{$data->{'features'}};
     $conf->{'renderers'}{$_}++ for keys %renderers;
   }
 }
 
 sub matrix_data {
   my ($self, $menu, $set) = @_;
-  my %data = map { $_ => 1 } map keys %$_, values %{$self->{'matrix_config'}{$menu}{$set}{'features'}};
-  return map { id => $_ }, sort keys %data;
+  my %data = map { lc $_ => $_ } map keys %$_, values %{$self->{'matrix_config'}{$menu}{$set}{'features'}};
+  return map { id => $data{$_} }, sort keys %data;
 }
 
 sub build_imageconfig_form {
@@ -104,12 +112,36 @@ sub build_imageconfig_form {
       
       next unless $node;
       
-      $counts{$set}{'total'} = scalar @{$conf->{'columns'}};
-      $counts{$set}{'on'}    = scalar grep { $_->{'display'} ne 'off' } @{$conf->{'columns'}}; 
+      my ($total, $on);
+      
+      foreach (grep ref, map values %$_, values %{$conf->{'features'}}) {
+        foreach (@$_) {
+          $total++;
+          
+          if ($self->get($_->{'option_key'}) eq 'on') {
+            my $d = $image_config->get_node($_->{'name'})->get('display');
+            $on++ if $d ne 'off' || ($d eq 'default' && $image_config->get_node($_->{'column_key'})->get('display') ne 'off');
+          }
+        }
+      }
+      
+      $counts{$set} = {
+        node  => $node,
+        #total => $total || scalar @{$conf->{'columns'}}, # FIXME: get counts working properly in JS
+        total => $total ? 0 : scalar @{$conf->{'columns'}},
+        on    => $total ? $on : scalar grep $_->{'display'} ne 'off', @{$conf->{'columns'}}
+      };
+      
+      if ($total) {
+        my $parent = $node->parent_key;
+        $counts{$parent}{'node'} ||= $node->parent_node;
+        $counts{$parent}{'total'} = 0; # FIXME: get counts working properly in JS
+        #$counts{$parent}{'total'} += $total;
+        #$counts{$parent}{'on'}    += $on;
+      }
       
       my $menu_data = {
         url          => $hub->url('Config', { function => 'Matrix', partial => 1, set => $set, menu => $menu }),
-        count        => qq{(<span class="on">$counts{$set}{'on'}</span>/$counts{$set}{'total'})},
         class        => $conf->{'subset'},
         class        => "${menu}_$set",
         availability => 1,
@@ -118,20 +150,25 @@ sub build_imageconfig_form {
       $node->set($_, $menu_data->{$_}) for keys %$menu_data;
     }
   }
+  
+  #$_->{'node'}->set('count', sprintf '(<span class="on">%s</span>/%s)', $_->{'on'} || 0, $_->{'total'}) for values %counts; # FIXME: get counts working properly in JS
+  $_->{'node'}->set('count', $_->{'total'} ? sprintf '(<span class="on">%s</span>/%s)', $_->{'on'} || 0, $_->{'total'} : '') for values %counts;
 }
 
 sub form_matrix {
-  my $self      = shift;
-  my $hub       = $self->hub;
-  my $set       = $hub->param('set');
-  my $menu      = $hub->param('menu');
-  my $img_url   = $self->img_url;
-  my $conf      = $self->{'matrix_config'}{$menu}{$set};
-  my @columns   = @{$conf->{'columns'}};
-  my $width     = (scalar @columns * 26) + 107; # Each td is 25px wide + 1px border. The first cell (th) is 90px + 1px border + 16px padding-right
-  my %filters   = ( '' => 'All classes' );
-  my $tick      = qq{<img class="tick" src="${img_url}tick.png" alt="Selected" title="Selected" />};
-  my (@rows, $rows_html, @headers_html, $last_class);
+  my $self          = shift;
+  my $hub           = $self->hub;
+  my $set           = $hub->param('set');
+  my $menu          = $hub->param('menu');
+  my $image_config  = $self->matrix_image_config;
+  my $user_settings = $image_config->get_user_settings;
+  my $img_url       = $self->img_url;
+  my $conf          = $self->{'matrix_config'}{$menu}{$set};
+  my @columns       = @{$conf->{'columns'}};
+  my $width         = (scalar @columns * 26) + 107; # Each td is 25px wide + 1px border. The first cell (th) is 90px + 1px border + 16px padding-right
+  my %filters       = ( '' => 'All classes' );
+  my $tick          = qq{<img class="tick" src="${img_url}tick.png" alt="Selected" title="Selected" />};
+  my (@rows, $rows_html, @headers_html, $last_class, %gaps, $track_style_header);
   
   $self->{'panel_type'} = 'ConfigMatrix';
   
@@ -154,7 +191,7 @@ sub form_matrix {
   
   my @renderer_template = (
     qq{<ul class="popup_menu"><li class="header">Change track style<img class="close" src="${img_url}close.png" title="Close" alt="Close" /></li>},
-    qq{<li class="%s"><img title="%s" alt="%s" src="${img_url}render/%s.gif" class="$conf->{'menu'}_$set" />%s<span%s>%s</span></li>},
+    qq{<li class="%s"><img title="%s" alt="%s" src="${img_url}render/%s.gif" class="${menu}_$set" />%s<span%s>%s</span></li>},
     '</ul>'
   );
   
@@ -179,24 +216,85 @@ sub form_matrix {
     my @row      = ("$y_class $cls", { tag => 'th', class => 'first', html => sprintf("$y$select_all_row", $y) });
     my $exists;
     
-    push @rows, [ 'gap', { tag => 'td' }] if $last_class && $class ne $last_class;
+    if ($last_class && $class ne $last_class) {
+      push @rows, [ 'gap', { tag => 'td' }];
+      $gaps{$#rows} = 1;
+    }
     
     $last_class    = $class;
     $filters{$cls} = $class if $class;
     
     foreach (@columns) {
-      my $x    = $_->{'x'};
-      my $cell = { tag => 'td', html => '<p></p>' };
+      my $x            = $_->{'x'};
+      (my $x_class     = lc $x) =~ s/[^\w-]/_/g;
+      my $cell         = { tag => 'td' };
+      my $col_renderer = $_->{'display'};
       
       if (exists $conf->{'features'}{$x}{$id}) {
+        my $on = $self->get("opt_matrix_${menu}_${set}_$x:$id") eq 'on';
+        
         $cell->{'title'}  = "$x:$y";
-        $cell->{'class'}  = "option $x $y_class";
-        $cell->{'class'} .= ' on'      if $self->get("opt_matrix_${menu}_${set}_$x:$id") eq 'on';
+        $cell->{'class'}  = "opt $x_class $y_class";
+        $cell->{'class'} .= ' on'      if $on;
         $cell->{'class'} .= ' default' if $self->{'options'}{"opt_matrix_${menu}_${set}_$x:$id"}{'default'} eq 'on';
         
+        if (ref $conf->{'features'}{$x}{$id} eq 'ARRAY') {
+          # TODO: renderers. Currently assuming that subtrack renderers match parent renderers.
+          my @r = @{$self->deepcopy($_->{'renderers'})};
+          
+          splice @r, 2, 0, 'default', 'Default';
+          
+          my %renderers = @r;
+          my $total     = scalar @{$conf->{'features'}{$x}{$id}};
+          my $on        = 0;
+          my ($subtracks, $select_all);
+          
+          foreach my $feature (@{$conf->{'features'}{$x}{$id}}) {
+            my $display  = $user_settings->{$feature->{'name'}}{'display'} || 'default';
+            my $renderer = $user_settings->{$feature->{'name'}}{'display'} || $col_renderer;
+            my $li_class = $renderer eq 'off' ? '' : ' on';
+            my $menu;
+            
+            for (my $i = 0; $i < scalar @r; $i += 2) {
+              $menu .= sprintf $renderer_template[1], $r[$i], $r[$i+1], $r[$i+1], $r[$i] eq 'default' ? $col_renderer : $r[$i], $r[$i] eq $display ? ($tick, ' class="current"') : ('', ''), $r[$i+1];
+            }
+            
+            $subtracks .= qq{
+              <li class="$x_class$li_class track">
+                $renderer_template[0]
+                $menu
+                $renderer_template[2]
+                <input type="hidden" class="track_name" name="$feature->{'name'}" value="$display" />
+                <img class="menu_option" title="$renderers{$renderer}" alt="$renderers{$renderer}" src="${img_url}render/$renderer.gif" />
+                <span class="menu_option">$feature->{'source_name'}</span>
+              </li>
+            };
+            
+            $on++ if $renderer ne 'off';
+            $self->{'json_params'}{'defaultRenderers'}{"$x:$id"}++ if $display eq 'default';
+            $select_all ||= "$renderer_template[0]$menu$renderer_template[2]";
+          }
+          
+          $self->{'json_params'}{'defaultRenderers'}{"$x:$id"} ||= 0;
+          $self->{'json_params'}{'cellTracks'}{"$x:$id"}        = sprintf('
+            <div class="subtracks info_popup">
+              <span class="close"></span>
+              %s
+              <ul class="config_menu">%s</ul>
+            </div>',
+            $total > 1 ? qq{<div class="select_all">$select_all<strong class="menu_option">Enable/disable all $cell->{'title'}</strong></div>} : '',
+            $subtracks
+          );
+          
+          $cell->{'html'}   = qq{<p><span class="on">$on</span>$total</p>$cell->{'html'}};
+          $cell->{'class'} .= ' st';
+          
+          $track_style_header ||= 'Default';
+        } else {
+          $cell->{'html'} = '<p></p>';
+        }
+        
         $exists = 1;
-      } else {
-        $cell->{'class'} = 'disabled';
       }
       
       push @row, $cell;
@@ -204,17 +302,28 @@ sub form_matrix {
     
     push @rows, \@row if $exists;
   }
-    
+  
+  my $cols           = scalar @{$rows[0]} - 1;
+  my $tutorial_col   = $cols > 5 ? 6 : $cols;
+  my ($tutorial_row) = sort { $a <=> $b } 5, scalar grep { $_->[0] ne 'gap' } @rows;
+  my $wrapper_class  = scalar @rows - $tutorial_row < 3 ? ' short' : '';
+  
+  $tutorial_row++ for grep { $_ < $tutorial_row } sort { $a <=> $b } keys %gaps;
+  
+  $rows[$tutorial_row][1]{'html'}                 .= '<b class="tutorial col"></b>';
+  $rows[$tutorial_row - 1][$tutorial_col]{'html'} .= '<b class="tutorial drag"></b>';
+  
+  $track_style_header ||= 'Track';
+  
   foreach (@rows) {
     my $row_class = shift @$_;
     my $row_html;
     
     foreach (@$_) {
-      $row_html .= sprintf('<%s%s%s%s>%s</%s>',
+      $row_html .= sprintf('<%s%s%s>%s</%s>',
         $_->{'tag'},
-        $_->{'class'}   ? qq{ class="$_->{'class'}"}     : '',
-        $_->{'title'}   ? qq{ title="$_->{'title'}"}     : '',
-        $_->{'colspan'} ? qq{ colspan="$_->{'colspan'}"} : '',
+        $_->{'class'} ? qq{ class="$_->{'class'}"} : '',
+        $_->{'title'} ? qq{ title="$_->{'title'}"} : '',
         $_->{'html'},
         $_->{'tag'}
       );
@@ -223,33 +332,36 @@ sub form_matrix {
     $rows_html .= qq{<tr class="$row_class">$row_html</tr>};
   }
   
+  my $i = 0;
+  
   foreach (@columns) {
     my $x         = $_->{'x'};
+    (my $x_class  = lc $x) =~ s/[^\w-]/_/g;
     my $display   = $_->{'display'};
+    my $name      = $_->{'name'};
     my %renderers = @{$_->{'renderers'}};
-    my $name      = join '_', $conf->{'track_prefix'} eq $set ? () : $conf->{'track_prefix'}, $set, $x;
-       $name      =~ s/[^\w-]/_/g;
     my $menu      = $renderer_template[0];
-       $menu     .= sprintf $renderer_template[1], $k, $v, $v, $k, $k eq $display ? ('', '') : ($tick, ' class="current"'), $v while ($k, $v) = splice @{$_->{'renderers'}}, 0, 2;
+       $menu     .= sprintf $renderer_template[1], $k, $v, $v, $k, $k eq $display ? ($tick, ' class="current"') : ('', ''), $v while ($k, $v) = splice @{$_->{'renderers'}}, 0, 2;
        $menu     .= $renderer_template[2];
     
-    $headers_html[0] .= sprintf qq{<th class="$x"><p>$x</p>$select_all_col</th>}, $x, $x, $x, $x;
-    $headers_html[2] .= qq{
-      <th class="$x $name">
-        <img class="menu_option" title="$renderers{$display}" alt="$renderers{$display}" src="${img_url}render/$display.gif">
-        <input type="hidden" class="track_name" name="$name" value="$display">
+    $headers_html[0] .= sprintf qq{<th class="$x_class"><p>$x</p>$select_all_col%s</th>}, $x, $x_class, $x_class, $x_class, $i == $tutorial_col - 2 ? '<b class="tutorial row"></b>' : '';
+    $headers_html[2] .= sprintf(qq{
+      <th class="$x_class $name%s">
         $menu
+        <input type="hidden" class="track_name" name="$name" value="$display" />
+        <img class="menu_option" title="$renderers{$display}" alt="$renderers{$display}" src="${img_url}render/$display.gif" />
+        %s
       </th>
-    };
+    }, $display eq 'off' ? '' : ' on', $i++ ? '' : '<b class="tutorial track"></b>');
   }
   
-  my $html = sprintf('
-    <h1>%s</h1>
+  my $html = sprintf(qq{
+    <h1>$conf->{'section'}</h1>
     <div class="toggle_tutorial"></div>
     <div class="header_wrapper">
       <h2>%s</h2> 
       <div class="sprite info_icon help" title="Click for more information">&nbsp;</div>
-      <div class="desc">%s</div>
+      <div class="desc">$conf->{'description'}</div>
     </div>
     <div class="filter_wrapper">
       <h2>Filter by</h2>
@@ -263,43 +375,32 @@ sub form_matrix {
       <div class="key"><div>Off</div><div class="cell off"></div></div>
       <div class="key"><div>No Data</div><div class="cell disabled"></div></div>
       <div class="key"><h2>Filtered:</h2></div>
-      <div class="key"><div>On</div><div class="cell on filter"><p></p></div></div>
+      <div class="key"><div>On</div><div class="cell filter on"><p></p></div></div>
       <div class="key"><div>Off</div><div class="cell off filter"><p></p></div></div>
     </div>
     <div class="config_matrix_scroller"><div></div></div>
-    <div class="config_matrix_wrapper">
-      <div class="tutorial track"></div>
-      <div class="tutorial all_track"></div>
-      <div class="tutorial col"></div>
-      <div class="tutorial row"></div>
-      <div class="tutorial drag"></div>
-      <div style="width:%spx; margin-top: 50px">
+    <div class="config_matrix_wrapper$wrapper_class">
+      <div class="table_wrapper" style="width:${width}px; margin-top: 50px">
         <table class="config_matrix" cellspacing="0" cellpadding="0">
           <thead>
             <tr><th class="first"></th>%s</tr>
-            <tr class="renderers"><th class="first select_all"><div class="menu_option"><h2>Track style:</h2><em>Enable/disable all</em></div>%s</th>%s</tr>
+            <tr class="renderers"><th class="first select_all"><div class="menu_option"><h2>$track_style_header style:</h2><em>Enable/disable all</em></div>%s<b class="tutorial all_track"></b></th>%s</tr>
           </thead>
           <tbody>
-            %s
+            $rows_html
           </tbody>
         </table>
       </div>
       <div class="no_results">No results found</div>
     </div>
-    ',
-    $conf->{'section'},
+    },
     encode_entities($conf->{'header'}),
-    $conf->{'description'},
     scalar keys %filters > 1 ? sprintf('<select class="filter">%s</select>', join '', map qq{<option value="$_">$filters{$_}</option>}, sort keys %filters) : '',
     map({ s/([a-z])([A-Z])([a-z])/$1_$2$3/g; s/_/ /g; lc; } $conf->{'axes'}{'x'}, $conf->{'axes'}{'y'}),
-    $width,
-    @headers_html,
-    $rows_html
+    @headers_html
   );
   
-  $self->get_form->append_children(
-    [ 'div', { inner_HTML => $html, class => 'js_panel config_matrix', id => "${menu}_$set" }]
-  );
+  $self->get_form->append_child('div', { inner_HTML => $html, class => 'js_panel config_matrix', id => "${menu}_$set" });
 }
 
 1;
