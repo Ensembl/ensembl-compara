163fcdcf441a45cd2bd736bd8a05c47f7eb8e2a0
Laurent Gil <lgil@ebi.ac.uk>
2012-12-12 14:02:59 +0000

    Update the display of the somatic structural variation with a breakpoint value.

--- modules/Bio/EnsEMBL/GlyphSet/structural_variation.pm
+++ modules/Bio/EnsEMBL/GlyphSet/structural_variation.pm
@@ -16,92 +16,128 @@ sub features {
   my $set_name = $self->my_config('set_name');
   my $func     = $self->somatic ? 'get_all_somatic_StructuralVariationFeatures' : 'get_all_StructuralVariationFeatures';
   
-  return $slice->get_all_StructuralVariationFeatures_by_VariationSet($self->{'config'}->hub->get_adaptor('get_VariationSetAdaptor', 'variation')->fetch_by_name($set_name)) if $set_name;
-  return $slice->$func($source =~ /^\w/ ? $source : undef);  
+  my $features;
+  
+  if ($set_name) {
+    $features = $slice->get_all_StructuralVariationFeatures_by_VariationSet($self->{'config'}->hub->get_adaptor('get_VariationSetAdaptor', 'variation')->fetch_by_name($set_name)) if $set_name;
+  } else {
+    $features = $slice->$func($source =~ /^\w/ ? $source : undef);
+  }
+  
+  my @display_features;    
+  for (my $i=0;$i<scalar(@$features);$i++) {
+    if (!$features->[$i]->{breakpoint_order}) {
+      push (@display_features,$features->[$i]);
+      next;
+    }
+    my $seq_start = $features->[$i]->seq_region_start;
+    my $seq_end   = $features->[$i]->seq_region_end;
+    if (($seq_start >= $slice->start && $seq_start <= $slice->end) || 
+          ($seq_end >= $slice->start && $seq_end <= $slice->end)) 
+    {
+      push (@display_features, $features->[$i]);
+    }
+     
+  }
+  return \@display_features;
 }
+
 
 sub tag {
   my ($self, $f) = @_;
-  
-  if ($f->is_somatic && $f->start == $f->end) {
-    return ({
-      style  => 'breakpoint',
-      colour => 'gold',
-      border => 'goldenrod',
-      start  => $f->start,
-    });
-  }
-  
-  my $colour         = $self->my_colour($self->colour_key($f), 'tag');
-  my $border         = 'dimgray';
-  my $inner_crossing = $f->inner_start && $f->inner_end && $f->inner_start >= $f->inner_end ? 1 : 0;
+  my $colour = $self->my_colour($self->colour_key($f), 'tag');
   my @tags;
   
-  if ($inner_crossing && $f->inner_start == $f->seq_region_end) {
-    return {
-      style  => 'rect',
-      colour => $colour,
-      border => $border,
-      start  => $f->start,
-      end    => $f->end
-    };
-  }
+  if ($f->is_somatic && $f->breakpoint_order) {
+    my $slice = $self->{'container'};
+    my $seq_start = $f->seq_region_start;
+    my $seq_end   = $f->seq_region_end;
+      
+    my @coords;
+    push (@coords, $f->start) if ($seq_start >= $slice->start && $seq_start <= $slice->end);
+    push (@coords, $f->end) if ($f->start!=$f->end && ($seq_end >= $slice->start && $seq_end <= $slice->end));
+   
+     foreach my $coord (@coords) {
+      push @tags,{
+        style  => 'breakpoint',
+        colour => 'gold',
+        border => 'goldenrod',
+        start  => $coord,
+        end    => $coord+10
+      };
+    }
+  } 
+  else {
+    my $border         = 'dimgray';
+    my $inner_crossing = $f->inner_start && $f->inner_end && $f->inner_start >= $f->inner_end ? 1 : 0;
+
   
-  # start of feature
-  if ($f->outer_start && $f->inner_start) {
-    if ($f->outer_start != $f->inner_start && !$inner_crossing) {
-      push @tags, {
-        style => 'rect',
-        start => $f->start,
-        end   => $f->inner_start - $f->seq_region_start + $f->start
+    if ($inner_crossing && $f->inner_start == $f->seq_region_end) {
+      return {
+        style  => 'rect',
+        colour => $colour,
+        border => $border,
+        start  => $f->start,
+        end    => $f->end
       };
     }
-  } elsif ($f->outer_start) {
-    if ($f->outer_start == $f->seq_region_start || $inner_crossing) {
-      push @tags, {
-        style => 'bound_triangle_right',
-        start => $f->start,
-        out   => 1
-      };
-    }
-  } elsif ($f->inner_start) {
-    if ($f->inner_start == $f->seq_region_start && !$inner_crossing) {
-      push @tags, {
-        style => 'bound_triangle_left',
-        start => $f->start
-      };
-    }
-  }
   
-  # end of feature
-  if ($f->outer_end && $f->inner_end) {
-    if ($f->outer_end != $f->inner_end && !$inner_crossing) {
-      push @tags, {
-        style => 'rect',
-        start => $f->end - $f->seq_region_end + $f->inner_end,
-        end   => $f->end
-      };
+    # start of feature
+    if ($f->outer_start && $f->inner_start) {
+      if ($f->outer_start != $f->inner_start && !$inner_crossing) {
+        push @tags, {
+          style => 'rect',
+          start => $f->start,
+          end   => $f->inner_start - $f->seq_region_start + $f->start
+        };
+      }
+    } elsif ($f->outer_start) {
+      if ($f->outer_start == $f->seq_region_start || $inner_crossing) {
+        push @tags, {
+          style => 'bound_triangle_right',
+          start => $f->start,
+          out   => 1
+        };
+      }
+    } elsif ($f->inner_start) {
+      if ($f->inner_start == $f->seq_region_start && !$inner_crossing) {
+        push @tags, {
+          style => 'bound_triangle_left',
+          start => $f->start
+        };
+      }
     }
-  } elsif ($f->outer_end) {
-    if ($f->outer_end == $f->seq_region_end || $inner_crossing) {
-      push @tags, {
-        style => 'bound_triangle_left',
-        start => $f->end,
-        out   => 1
-      };
-    }
-  } elsif ($f->inner_end) {
-    if ($f->inner_end == $f->seq_region_end && !$inner_crossing) {
-      push @tags, {
-        style => 'bound_triangle_right',
-        start => $f->end
-      };
-    }
-  }
   
-  foreach (@tags) {
-    $_->{'colour'} = $colour;
-    $_->{'border'} = $border;
+    # end of feature
+    if ($f->outer_end && $f->inner_end) {
+      if ($f->outer_end != $f->inner_end && !$inner_crossing) {
+        push @tags, {
+          style => 'rect',
+          start => $f->end - $f->seq_region_end + $f->inner_end,
+          end   => $f->end
+        };
+      }
+    } elsif ($f->outer_end) {
+      if ($f->outer_end == $f->seq_region_end || $inner_crossing) {
+        push @tags, {
+          style => 'bound_triangle_left',
+          start => $f->end,
+          out   => 1
+        };
+      }
+    } elsif ($f->inner_end) {
+      if ($f->inner_end == $f->seq_region_end && !$inner_crossing) {
+        push @tags, {
+          style => 'bound_triangle_right',
+          start => $f->end
+        };
+       }
+    }
+  
+    foreach (@tags) {
+      $_->{'colour'} = $colour;
+      $_->{'border'} = $border;
+    }
   }
   
   return @tags;
@@ -109,6 +145,7 @@ sub tag {
 
 sub render_tag {
   my ($self, $tag, $composite, $slice_length, $width, $start, $end, $img_start, $img_end) = @_;
+  my $pix_per_bp = $self->scalex;
   my @glyph;
   
   if ($tag->{'style'} =~ /^bound_triangle_(\w+)$/ && $img_start < $tag->{'start'} && $img_end > $tag->{'end'}) {
@@ -131,27 +168,48 @@ sub render_tag {
     my $x     = $tag->{'start'};
     my $y     = $h / 2;
     my $width = 10 / $self->scalex;
-    
-    # Triangle returns an array: the triangle, and an invisible rectangle behind it for clicking purposes
-    @glyph = ($self->Triangle({
-      mid_point    => [ $x, $y ],
-      colour       => $tag->{'colour'},
-      bordercolour => $tag->{'border'},
-      absolutey    => 1,
-      width        => $width,
-      height       => $y,
-      direction    => 'up',
-      z            => 12,
-    }), $self->Triangle({
-      mid_point    => [ $x, ($y / 2) + 2 ],
-      colour       => $tag->{'colour'},
-      bordercolour => $tag->{'border'},
-      absolutey    => 1,
-      width        => $width,
-      height       => $y,
-      direction    => 'down',
-      z            => 12,
-    }));
+    $y = $h*2;
+    my $scale = ($y/4)/$pix_per_bp;
+
+    my $points = [ $x,            1,              # 1
+                   $x+$scale,     1,              # 2
+                   $x+$scale/10,  $y/2-$y/6+1,    # 3
+                   $x+$scale*1.2, $y/2-$y/6+1,    # 4
+                   $x,            $y-2,           # 5
+                   $x,            $y-$y/2+$y/6-1, # 6
+                   $x-$scale/2,   $y-$y/2+$y/6-1  # 7
+                 ];
+                          
+    my $points2 = [ $x-$scale/5,   0,             # 1
+                     $x+$scale*1.6, 0,            # 2
+                     $x+$scale*0.8, $y/2-$y/6,    # 3
+                     $x+$scale*1.9, $y/2-$y/6,    # 4
+                     $x+$scale/10,  $y,           # 5a
+                     $x-$scale/10,  $y,           # 5b 
+                     $x-$scale/5,   $y-$y/2+$y/6, # 6
+                     $x-$scale*1.2, $y-$y/2+$y/6  # 7
+                   ];  
+    @glyph = (
+             $self->Poly({
+               'points'    => $points2,
+               'colour'    => 'black',
+               'z'         => 5,
+             }),
+             $self->Poly({
+               'points'    => $points,
+               'colour'    => $tag->{'colour'},
+               'z'         => 10,
+             }) 
+    );
+    $composite->push(
+             $self->Rect({
+               'z'         => 1,
+               'x'         => $x-$scale*1.2,
+               'width'     => $scale*3.1,
+               'height'    => $y,
+               'absolutey' => 1,
+             })
+    );
   }
   
   return @glyph;
@@ -187,14 +245,25 @@ sub highlight {
   my $width = max(map $_->width, $composite, @tags);
   my $x     = min(map $_->x,     $composite, @tags);
   
-  $self->unshift($self->Rect({
-    x            => $x - 1/$pix_per_bp,
-    y            => $composite->y - 1,
-    width        => $width + 2/$pix_per_bp,
-    height       => $h + 2,
-    bordercolour => 'green',
-    absolutey    => 1,
-  }));
+  if ($f->is_somatic and $f->breakpoint_order) {
+    $self->unshift($self->Rect({
+      x            => $x - 1/$pix_per_bp,
+      y            => $composite->y - 1,
+      width        => $width + 2/$pix_per_bp,
+      height       => $h*2 + 2,
+      bordercolour => 'green',
+      absolutey    => 1,
+    }));
+  } else {
+    $self->unshift($self->Rect({
+      x            => $x - 1/$pix_per_bp,
+      y            => $composite->y - 1,
+      width        => $width + 2/$pix_per_bp,
+      height       => $h + 2,
+      bordercolour => 'green',
+      absolutey    => 1,
+    }));
+  }
 }
 
 1;
