e5456a96255daf0eb6b5b41c42f8a9d0cfe5103b
Matthieu Muffato <muffato@ebi.ac.uk>
2012-12-19 16:11:22 +0000

    The "canonical" transcript is not always the "longest" one

--- scripts/examples/mixed_getInterproAlignments.pl
+++ scripts/examples/mixed_getInterproAlignments.pl
@@ -30,14 +30,14 @@ my $human_genes = $human_gene_adaptor->fetch_all_by_domain($interpro_domain);
 
 ## Loop through all the genes. Although we know the gene, we need to get the actual
 ## protein to get the coordinates of the domain. Also, we only want the proteins
-## for the longest translation for each gene as these are the one used for building
+## for the canonical translation for each gene as these are the one used for building
 ## the gene trees
 foreach my $this_human_gene (@$human_genes) {
   print "GENE: ", $this_human_gene->stable_id, " - ", $this_human_gene->external_name, "\n";
   ## Get the gene_member for this human gene
   my $gene_member = $member_adaptor->fetch_by_source_stable_id(
       "ENSEMBLGENE", $this_human_gene->stable_id);
-  ## Get the longest peptide Member, i.e. the one used for building the trees
+  ## Get the canonical peptide Member, i.e. the one used for building the trees
   my $peptide_member = $gene_member->get_canonical_Member;
 
   # Go through all the possible transcripts
@@ -45,7 +45,7 @@ foreach my $this_human_gene (@$human_genes) {
     my $translation = $this_transcript->translation;
     # Skip if no translation (i.e. pseudogene)
     next if (!$translation);
-    # Skip if not the longest protein
+    # Skip if not the canonical protein
     next if ($translation->stable_id ne $peptide_member->stable_id);
     # Find the coordinates of this feature on the protein (start and end coord.)
     foreach my $domain_feature (@{$translation->get_all_DomainFeatures}) {
