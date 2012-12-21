eadf3ea6cc0cdbce515e52460ee988c690b5ba59
Rishi Nag <rn6@sanger.ac.uk>
2012-12-21 11:22:10 +0000

    removed hashbangs, added source/version/strict/warnings

--- modules/Bio/EnsEMBL/Analysis/Runnable/AlignmentNets.pm
+++ modules/Bio/EnsEMBL/Analysis/Runnable/AlignmentNets.pm
@@ -41,8 +41,11 @@ Internal methods are usually preceded with a _
 
 # Let the code begin...
 
+# $Source: /tmp/ENSCOPY-ENSEMBL-ANALYSIS/modules/Bio/EnsEMBL/Analysis/Runnable/AlignmentNets.pm,v $
+# $Version: $
 package Bio::EnsEMBL::Analysis::Runnable::AlignmentNets;
 
+use warnings ;
 use vars qw(@ISA);
 use strict;
 
