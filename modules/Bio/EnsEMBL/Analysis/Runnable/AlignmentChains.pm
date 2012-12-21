1cbe9dbd0054e4547037baff0ec4aceaa3e944db
Rishi Nag <rn6@sanger.ac.uk>
2012-12-21 15:33:49 +0000

    Version tag renamed Revision

--- modules/Bio/EnsEMBL/Analysis/Runnable/AlignmentChains.pm
+++ modules/Bio/EnsEMBL/Analysis/Runnable/AlignmentChains.pm
@@ -42,7 +42,7 @@ Internal methods are usually preceded with a _
 # Let the code begin...
 
 # $Source: /tmp/ENSCOPY-ENSEMBL-ANALYSIS/modules/Bio/EnsEMBL/Analysis/Runnable/AlignmentChains.pm,v $
-# $Version: $
+# $Revision: 1.14 $
 package Bio::EnsEMBL::Analysis::Runnable::AlignmentChains;
 
 use warnings ;
