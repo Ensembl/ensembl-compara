0eacb1adec4fb2f56f85fa108417b54155b1daeb
Harpreet Riat <hr5@sanger.ac.uk>
2013-01-11 13:55:08 +0000

    formSubmit method added to override the inherited one; 'initialize' changed back to 'init'

--- htdocs/components/20_UserData.js
+++ htdocs/components/20_UserData.js
@@ -10,30 +10,17 @@ Ensembl.Panel.UserData = Ensembl.Panel.ModalContent.extend({
     
     this.base();
     
-    // Hack this.elLk.content and this.elLk.link to get formSubmit to correctly reload content when the form doesn't have a class of upload
+    // in case this panel is added by addSubPanel method, it misses getting references to content and links
     if (!this.elLk.content.length) {
       this.elLk.content = this.el.parents('.modal_wrapper');
+      this.elLk.links   = this.elLk.content.siblings('.modal_nav').find('ul.local_context li');
     }
-
-    this.elLk.link = this.elLk.content.siblings('.modal_nav').find('ul.local_context li.active');    
-    this.elLk.form = this.el.find('form').validate().on('submit.UserData', function () {
-      var visibleInps = panel.elLk.requiredInputs.filter(':visible');
-      
-      // if neither of the multiple inputs have any value
-      if (visibleInps.length > 1 && visibleInps.filter(function () { return !!this.value; }).length === 0) {
-        panel.elLk.errorMessage.show();
-        return false;
-      }
-      
-      panel.elLk.link.removeClass('active');
-      
-      return panel.formSubmit($(this));
-    });
     
-    this.elLk.requiredInputs = this.elLk.form.find(':input.required');
-    this.elLk.errorMessage   = this.elLk.form.find('label._userdata_upload_error').addClass('invalid');
-    this.elLk.actionInputs   = this.elLk.form.find(':input._action').each(function () {
-      $(this).on('change.UserData', function () {
+    this.elLk.form            = this.el.find('form').validate().off('.UserData').on('submit.UserData', function () { return panel.formSubmit($(this)); });
+    this.elLk.requiredInputs  = this.elLk.form.find(':input.required');
+    this.elLk.errorMessage    = this.elLk.form.find('label._userdata_upload_error').addClass('invalid');
+    this.elLk.actionInputs    = this.elLk.form.find(':input._action').each(function () {
+      $(this).off('.UserData').on('change.UserData', function () {
         // change the form action according to the dropdown/radio buttons (if <select> is changed, give priority to radio buttons if they are visible)
         var action = this.nodeName === 'SELECT' ? panel.elLk.actionInputs.filter('input:visible:checked')[0] || $(this).find('option:selected')[0] : this;
             action = action ? (action.className.match(/(?:\s+|^)_action_([^\s]+)/) || []).pop() || '' : '';
@@ -68,5 +55,20 @@ Ensembl.Panel.UserData = Ensembl.Panel.ModalContent.extend({
         }
       });
     }).filter('select').validate(true).end(); // not to forget validating the dropdown to select the format
+  },
+  
+  formSubmit: function(form, data) {
+    
+    var visibleInps = this.elLk.requiredInputs.filter(':visible');
+    
+    // if neither of the multiple inputs have any value
+    if (visibleInps.length > 1 && visibleInps.filter(function () { return !!this.value; }).length === 0) {
+      this.elLk.errorMessage.show();
+      return false;
+    }
+    
+    this.elLk.links.removeClass('active');
+    
+    return this.base(form, data);
   }
 });
