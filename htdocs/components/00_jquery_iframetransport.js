44b56344c9944bcd313384f653a5fdcbd45a6d94
Harpreet Riat <hr5@sanger.ac.uk>
2013-01-18 10:57:47 +0000

    modification to make the plugin compatiable with IE and webkit

--- htdocs/components/00_jquery_iframetransport.js
+++ htdocs/components/00_jquery_iframetransport.js
@@ -14,45 +14,50 @@
   
   // Register a transport for the 'iframe' data type.
   $.ajaxTransport('iframe', function(options, origOptions, jqXHR) {
-    var form      = options.form;
     var iFrame    = null;
-    var cloneForm = null;
+    var form      = null;
+    var files     = { originals: options.form.find('input:file:enabled:not([value=""])') };
     var name      = 'iframe' + $.now();
     
     // Function to revert all changes made to the page
     function cleanUp() {
-      cloneForm.remove();
-      iFrame.off('load').attr('src', 'javascript:false;').remove();
-      cloneForm = iFrame = null;
+      if (iFrame) {
+        files.clones.replaceWith(function(i) { return files.originals[i]; });
+        form.remove();
+        iFrame.off('load').attr('src', 'javascript:false;').remove();
+        form = iFrame = null;
+      }
     }
     
     // Remove 'iframe' from the datatypes list added by ajaxPrefilter so that further processing is based upon the dataType specified in options
     options.dataTypes.shift();
     
     // Use the iframe transport iff there are files that need to be uploaded
-    if (form.find('input:file:enabled:not([value=""])').length) {
+    if (files.originals.length) {
     
-      // Clone the actual form (that will be submitted through iFrame), add required attributes to it, and add a hidden input for the server side to know about the request type
-      cloneForm = form.clone().hide()
-        .attr({action: options.url || form.attr('href'), target: name, enctype: 'multipart/form-data', method: 'POST'})
-        .append($('<input>', {type: 'hidden', name: 'X-Requested-With', value: 'iframe'}));
-
-      // In case there's an input in the form with name 'submit', it won't let the form to be submited via JS
-      if (typeof cloneForm[0].submit !== 'function') {
-        throw "Default submit() method seems to be missing for the form being submitted. Please make sure there's no field with name 'submit' in the form.";
-      }
+      // Create a new form (that will be submitted through iFrame) and add required attributes to it
+      form = $('<form>', {action: options.url || options.form.attr('href'), name: name, target: name, enctype: 'multipart/form-data', method: 'POST'});
+      
+      // Add hidden inputs corresponding to each input in original form and an extra one for the server side to know about the request type
+      $.each($.merge([{name: 'X-Requested-With', value: 'iframe'}], options.form.serializeArray()), function(i, field) {
+        $('<input type="hidden" />').attr(field).appendTo(form);
+      });
+      
+      // Copy the actual file inputs to the new form, and leave the cloned file inputs in the actual form
+      files.clones = files.originals.after(function() { return this.cloneNode(true); }).next();
+      files.originals.appendTo(form);
       
       // return the 'send' and 'abort' functions for this transport
       return {
       
         send: function(headers, completeCallback) {
-          iFrame = $('<iframe>', {src :'javascript:false;', name: name, id: name, style: 'display:none'});
-          
+        
           // The first load event gets fired after the iframe has been injected into the DOM, and is used to prepare the actual submission.
-          iFrame.on('load', function() {
+          iFrame = $('<iframe name="' + name + '" id="' + name + '" src="javascript:false;" style="display:none">').on('load', function() {
           
             // The second load event gets fired when the response to the form submission is received.
             iFrame.off('load').on('load', function() {
+            
               var doc   = this.contentWindow ? this.contentWindow.document : (this.contentDocument ? this.contentDocument : this.document);
               var root  = doc.documentElement || doc.body;
               cleanUp();
@@ -60,17 +65,15 @@
             });
             
             // Now that the load handler has been set up, submit the form.
-            cloneForm[0].submit();
+            form[0].submit();
           });
           
-          // After everything has been set up correctly, the form and iframe get injected into the DOM so that the submission can be initiated.
-          $('body').append(cloneForm, iFrame);
+          // After everything has been set up correctly, the form and the iframe get injected into the DOM so that the submission can be initiated.
+          $('body').append(form, iFrame);
         },
         
         abort: function() {
-          if (iFrame !== null) {
-            cleanUp();
-          }
+          cleanUp();
         }
       };
     }
