8f258f605f00fce65038357edcc5345de232447b
Kathryn Beal <kbeal@ebi.ac.uk>
2013-01-29 14:23:05 +0000

    Added back the code to store the member sequence in the sequence table.

--- modules/Bio/EnsEMBL/Compara/DBSQL/MemberAdaptor.pm
+++ modules/Bio/EnsEMBL/Compara/DBSQL/MemberAdaptor.pm
@@ -655,6 +655,15 @@ sub store {
 
   $member->adaptor($self);
 
+  # insert in sequence table to generate new
+  # sequence_id to insert into member table;
+  if(defined($member->sequence) and $member->sequence_id == 0) {
+    $member->sequence_id($self->db->get_SequenceAdaptor->store($member->sequence,1)); # Last parameter induces a check for redundancy
+
+    my $sth3 = $self->prepare("UPDATE member SET sequence_id=? WHERE member_id=?");
+    $sth3->execute($member->sequence_id, $member->dbID);
+    $sth3->finish;
+  }
 
   return $member->dbID;
 }
