f0f2c4e42e1fd11fb16be0c7b14a817ea1d562dc
Matthieu Muffato <muffato@ebi.ac.uk>
2013-02-01 10:22:26 +0000

    Fixed title and description

--- sql/patch_70_71_a.sql
+++ sql/patch_70_71_a.sql
@@ -1,8 +1,8 @@
 # patch_70_71_a.sql
 #
-# Title: Update keys and column types in other_member_sequence
+# Title: other_member_sequence_keys
 #
-# Description:
+# Description: Update keys and column types in other_member_sequence
 
 ALTER TABLE other_member_sequence DROP KEY seq_type, DROP KEY sequence, MODIFY COLUMN sequence mediumtext not null;
 
