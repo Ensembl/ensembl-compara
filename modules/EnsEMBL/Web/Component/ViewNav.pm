10b3d5b018e9719ab3a5fce2ceeebd0a5616f5d5
Anne Parker <ap5@sanger.ac.uk>
2013-02-04 16:20:16 +0000

    Generic component with link to Region in Detail

---
+++ modules/EnsEMBL/Web/Component/ViewNav.pm
@@ -0,0 +1,27 @@
+package EnsEMBL::Web::Component::ViewNav;
+
+use strict;
+
+use HTML::Entities qw(encode_entities);
+
+use base qw(EnsEMBL::Web::Component);
+
+sub _init {
+  my $self = shift;
+  $self->ajaxable(0);
+}
+
+sub content {
+  my $self             = shift;
+
+  my $hub              = $self->hub;
+  my $image_width      = $self->image_width . 'px';
+  my $url              = $hub->url({'type' => 'Location', 'action' => 'View'});
+
+  return qq{
+      <div class="navbar print_hide" style="width:$image_width">
+        <a href="$url"><img src="/i/48/region_thumb.png" title="Go to Region in Detail for more options" style="border:1px solid #ccc;margin:0 16px;vertical-align:middle" /></a> Go to <a href="$url">Region in Detail</a> for more tracks and navigation options
+      </div>};
+}
+
+1;
