cf08489238198d2a425652eb3ecf857d276cddbb
Simon Brent <sb23@sanger.ac.uk>
2013-02-07 13:17:44 +0000

    turn the uploaded data on in the configuration of the Genome page. tidied

--- modules/EnsEMBL/Web/Command/UserData/FviewRedirect.pm
+++ modules/EnsEMBL/Web/Command/UserData/FviewRedirect.pm
@@ -1,78 +1,66 @@
+# $Id$
+
 package EnsEMBL::Web::Command::UserData::FviewRedirect;
 
-### Redirects from the 'FeatureView' form to either Location/Chromosome or Location/Genome
+### Redirects from the 'FeatureView' form to Location/Genome
 
 use strict;
 use warnings;
 
 use URI::Escape qw(uri_escape);
+
 use EnsEMBL::Web::Builder;
 
 use base qw(EnsEMBL::Web::Command::UserData);
 
 sub process {
-  my $self = shift;
-  my $hub = $self->hub;
-
-  my @input_ids = split /\n|\r/, $hub->param('id');
-  my @output_ids;
-  foreach (@input_ids) {
-    my @A = split(/\t|\s+|, /, $_);
-    push @output_ids, @A;
-  }
-
-  ## This bit is basically replicating the old featureview URL-based functionality!
-  my $builder = EnsEMBL::Web::Builder->new({hub => $hub});
-  my $features = [];
-  $hub->param('id', @output_ids);
-  my $object = $builder->create_objects('Feature', 'lazy');
-  if ($object && $object->can('convert_to_drawing_parameters')) {
-    $features = $object->convert_to_drawing_parameters;
-  }
-
+  my $self      = shift;
+  my $hub       = $self->hub;
+  my $site_type = $hub->species_defs->ENSEMBL_SITETYPE;
+  my $ftype     = $hub->param('ftype');
+  my $builder   = EnsEMBL::Web::Builder->new({ hub => $hub }); ## This bit is basically replicating the old featureview URL-based functionality
+  my $object    = $builder->create_objects('Feature', 'lazy');
+  my $features  = $object && $object->can('convert_to_drawing_parameters') ? $object->convert_to_drawing_parameters : {};
+  my $desc      = $hub->param('name') || "Selected ${ftype}s";
+  my $content   = sprintf qq{track name=%s description="%s" useScore=1 color=%s style=%s\n}, $ftype, $desc, $hub->param('colour'), $hub->param('style');
+  
   ## Write out features as GFF file
-  my $desc = $hub->param('name') || 'Selected '.$hub->param('ftype').'s';
-  my $content = sprintf('track name=%s description="%s" useScore=1 color=%s style=%s', $hub->param('ftype'), $desc, $hub->param('colour'), $hub->param('style'));
-  $content .= "\n";
-
-  while (my ($type, $feat) = each (%{$features||{}})) {
-    foreach my $f (@{$feat->[0]||[]}) {
+  while (my ($type, $feat) = each %$features) {
+    foreach my $f (@{$feat->[0] || []}) {
       my $strand = $f->{'strand'} == 1 ? '+' : '-';
       my @attribs;
-      if ($hub->param('ftype') eq 'Gene') {
+      
+      if ($ftype eq 'Gene') {
         @attribs = (
-                    'ID='.$f->{'gene_id'}[0], 
-                    'extname='.$f->{'extname'}, 
-                    'description='.uri_escape($f->{'extra'}{'description'})
-                    );
-      }
-      else {
+          'ID='          . $f->{'gene_id'}[0],
+          'extname='     . $f->{'extname'}, 
+          'description=' . uri_escape($f->{'extra'}{'description'})
+        );
+      }  else {
         @attribs = (
-                    'length='   .$f->{'length'},
-                    'label='    .uri_escape($f->{'label'}),
-                    'align='    .$f->{'extra'}{'align'},
-                    'ori='      .$f->{'extra'}{'ori'},
-                    'id='       .$f->{'extra'}{'id'},
-                    'score='    .$f->{'extra'}{'score'},
-                    'p-value='  .$f->{'extra'}{'p-value'},
-                    );
+          'length='  . $f->{'length'},
+          'label='   . uri_escape($f->{'label'}),
+          'align='   . $f->{'extra'}{'align'},
+          'ori='     . $f->{'extra'}{'ori'},
+          'id='      . $f->{'extra'}{'id'},
+          'score='   . $f->{'extra'}{'score'},
+          'p-value=' . $f->{'extra'}{'p-value'},
+        );
       }
-      my $attrib_string = join('; ', @attribs);
-      $content .= join("\t", $f->{'region'}, $hub->species_defs->ENSEMBL_SITETYPE, $hub->param('ftype'),
-                              $f->{'start'}, $f->{'end'}, '.', $strand, '.', $attrib_string);
+      
+      $content .= join "\t", $f->{'region'}, $site_type, $ftype, $f->{'start'}, $f->{'end'}, '.', $strand, '.', join('; ', @attribs);
       $content .= "\n";
     }
   }
-
-  $hub->param('text', $content);
+  
+  $hub->param('id',     split /\s+|, /, $hub->param('id'));
+  $hub->param('text',   $content);
   $hub->param('format', 'GFF');
-  $hub->param('name', $desc);
-
-  ## Upload munged data
-  $self->upload('text');
-
-  my $url = '/'.$hub->param('species').'/Location/Genome';
-  my $params = {'reload' => 1};
+  $hub->param('name',   $desc);
+  
+  my $upload = $self->upload('text'); ## Upload munged data
+  my $url    = $hub->url({ species => $hub->param('species'), type => 'Location', action => 'Genome', function => undef, __clear => 1 });
+  my $params = { Vkaryotype => "upload_$upload->{'code'}=on" };
   
   $self->ajax_redirect($url, $params, undef, 'page'); 
 }
