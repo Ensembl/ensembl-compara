c60291a7beecadd3b7520d8cfd65bc24790a9646
Simon Brent <sb23@sanger.ac.uk>
2013-03-05 16:23:15 +0000

    added select by. fixed resizing

--- htdocs/components/10_MultiSelector.js
+++ htdocs/components/10_MultiSelector.js
@@ -6,7 +6,7 @@ Ensembl.Panel.MultiSelector = Ensembl.Panel.extend({
     this.urlParam = params.urlParam;
     
     Ensembl.EventManager.register('updateConfiguration', this, this.updateSelection);
-    Ensembl.EventManager.register('modalPanelResize', this, this.style);
+    Ensembl.EventManager.register('modalPanelResize',    this, this.style);
   },
   
   init: function () {
@@ -17,7 +17,7 @@ Ensembl.Panel.MultiSelector = Ensembl.Panel.extend({
     this.initialSelection = '';
     this.selection        = [];
     
-    this.elLk.content = $('.modal_wrapper', this.el);
+    this.elLk.content = $('.modal_wrapper',       this.el);
     this.elLk.list    = $('.multi_selector_list', this.elLk.content);
     
     var ul    = $('ul', this.elLk.list);
@@ -31,8 +31,8 @@ Ensembl.Panel.MultiSelector = Ensembl.Panel.extend({
     this.setSelection(true);
     
     this.elLk.included.sortable({
-      containment: panel.elLk.included.parent(),
-      stop: function () { panel.setSelection(); }
+      containment: this.elLk.included.parent(),
+      stop: $.proxy(this.setSelection, this)
     });
     
     this.buttonWidth = spans.filter('.switch').on('click', function () {
@@ -68,22 +68,51 @@ Ensembl.Panel.MultiSelector = Ensembl.Panel.extend({
     
     this.style();
     
+    $('.select_by select', this.el).on('change', function () {
+      var toAdd, toRemove;
+      
+      switch (this.value) {
+        case ''    : break;
+        case 'all' : toAdd    = panel.elLk.excluded.children(); break;
+        case 'none': toRemove = panel.elLk.included.children(); break;
+        default    : toAdd    = panel.elLk.excluded.children(':contains(' + this.options[this.selectedIndex].innerHTML + ')'); 
+                     toRemove = panel.elLk.included.children().not(':contains(' + this.options[this.selectedIndex].innerHTML + ')'); break;
+      }
+      
+      if (toAdd) {
+        panel.elLk.included.append(toAdd);
+      }
+      
+      if (toRemove) {
+        toRemove.add(panel.elLk.excluded.children()).detach().sort(function (a, b) { return $(a).text() > $(b).text(); }).appendTo(panel.elLk.excluded);
+      }
+      
+      panel.setSelection();
+      
+      toAdd = toRemove = null;
+    });
+    
+    
     ul = null;
   },
   
   style: function () {
     var width = 0;
+    var initialWidth = this.elLk.list.removeClass('multi_selector_list_wide').width();
+    
+    this.elLk.list.addClass('multi_selector_list_wide');
     
     this.elLk.spans.each(function () {
       var w = $(this).width();
-
+      
       if (w > width) {
         width = w;
       }
     });
     
-    this.elLk.list.width('');
-    this.elLk.list.width(this.elLk.list.width() <= width + this.buttonWidth ? '100%' : '');
+    if (initialWidth > width + this.buttonWidth) {
+      this.elLk.list.removeClass('multi_selector_list_wide');
+    }
   },
   
   setSelection: function (init) {
