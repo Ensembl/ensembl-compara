11840ebe6cd7b21e178cb41b1454e13f0fc55178
Steve Trevanion <st3@sanger.ac.uk>
2013-03-08 18:09:06 +0000

    get objet type (ie rnaseq or vega) from core params (db doesn't work); tweak display for rnaseq

--- modules/EnsEMBL/Web/ImageConfig/supporting_evidence_transcript.pm
+++ modules/EnsEMBL/Web/ImageConfig/supporting_evidence_transcript.pm
@@ -17,60 +17,76 @@ sub init {
   $self->create_menus('TSE_transcript', 'splice_sites', 'evidence');
   
   $self->load_tracks;
-  
-  my $is_vega_gene = $self->hub->param('db') eq 'vega' ? 1 : $self->species_defs->ENSEMBL_SITETYPE eq 'Vega' ? 1 : 0;
+
+  my $logic_name = '';;
+  if ($self->hub->core_objects->{'transcript'}) {
+    $logic_name = $self->hub->core_objects->{'transcript'}->Obj->analysis->logic_name;
+  }
+  my $is_vega_gene   = $self->hub->get_db eq 'vega'   ? 1 : $self->species_defs->ENSEMBL_SITETYPE eq 'Vega' ? 1 : 0;
+  my $is_rnaseq_gene = $self->hub->get_db eq 'rnaseq' ? 1 : 0;
 
   $self->add_tracks('splice_sites', [ 'non_can_intron', 'Non-canonical splicing', 'non_can_intron', {
-      display     => 'normal',
-      strand      => 'r',
-      colours     => $self->species_defs->colour('feature'),
-      description => 'Non-canonical splice sites (ie not GT/AG, GC/AG, AT/AC or NN/NN)',
-   }]
-  );
+    display     => 'normal',
+    strand      => 'r',
+    colours     => $self->species_defs->colour('feature'),
+    description => 'Non-canonical splice sites (ie not GT/AG, GC/AG, AT/AC or NN/NN)',
+  }]);
 
   my $transcript_evi_desc = $is_vega_gene ? 'Alignments from the Havana pipeline that support the transcript' :  'Alignments used to build this transcript model';
-  
+
   $self->add_tracks('evidence', [ 'TSE_generic_match', 'Transcript supporting evidence', 'TSE_generic_match', {
-      display     => 'normal',
-      strand      => 'r',
-      colours     => $self->species_defs->colour('feature'),
-      description => $transcript_evi_desc,
-    }
-  ]);
-  
+    display     => 'normal',
+    strand      => 'r',
+    colours     => $self->species_defs->colour('feature'),
+    description => $transcript_evi_desc,
+  }]);
+
   if (!$is_vega_gene) {
-    $self->add_tracks('evidence', [ 'SE_generic_match', 'Exon supporting evidence (Ensembl)', 'SE_generic_match', {
+    if ($is_rnaseq_gene) {
+
+      #configure intron supporting feature display
+      $self->add_tracks('evidence', [ 'Supported_introns', 'Intron supporting evidence', 'TSE_intron_sf', {
+        display              => 'normal',
+        strand               => 'r',
+        colours              => $self->species_defs->colour('feature'),
+        description          => 'RNASeq reads that support the introns',
+      }]);
+    }
+    else {
+      $self->add_tracks('evidence', [ 'SE_generic_match', 'Exon supporting evidence (Ensembl)', 'SE_generic_match', {
         display              => 'normal',
         strand               => 'r',
         colours              => $self->species_defs->colour('feature'),
         description          => 'Alignments from the Ensembl pipeline that support the exons',
         logic_names_excluded => '_havana',
-      }
-    ]);
+      }]);
+    }
 
     if ($self->species_defs->HAVANA_DATAFREEZE_DATE) {
       $self->add_tracks('evidence', [ 'SE_generic_match_havana', 'Exon supporting evidence (Havana)', 'SE_generic_match', {
-          display          => 'normal',
-          strand           => 'r',
-          colours          => $self->species_defs->colour('feature'),
-          description      => 'Alignments from the Havana pipeline that support the exons',
-          logic_names_only => '_havana',
-        }
-      ]);
+        display          => 'normal',
+        strand           => 'r',
+        colours          => $self->species_defs->colour('feature'),
+        description      => 'Alignments from the Havana pipeline that support the exons',
+        logic_names_only => '_havana',
+      }]);
     }
   }
-  
-  $self->add_tracks('evidence', [ 'TSE_background_exon', '', 'TSE_background_exon', {
-      display => 'normal',
-      strand  => 'r',
-      menu    => 'no',
-    }], [ 'TSE_legend', 'Legend', 'TSE_legend', {
-      display => 'normal',
-      strand  => 'r',
-      colours => $self->species_defs->colour('feature'),
-      menu    => 'no',
-    }
-  ]);
+  $self->add_tracks('evidence',
+                    [ 'TSE_background_exon', '', 'TSE_background_exon', {
+                      display => 'normal',
+                      strand  => 'r',
+                      menu    => 'no',
+                    }],
+                    [ 'TSE_legend', 'Legend', 'TSE_legend', {
+                      display => 'normal',
+                      strand  => 'r',
+                      colours => $self->species_defs->colour('feature'),
+                      menu    => 'no',
+                    }],
+                  );
+
+
 }
 
 1;
