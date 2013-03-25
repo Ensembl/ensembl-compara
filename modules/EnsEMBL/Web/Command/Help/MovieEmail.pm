f728ba95783b4137d676d8dcbbdcbc5bf88e926a
Harpreet Riat <hr5@sanger.ac.uk>
2013-03-25 12:33:04 +0000

    email-sending part moved to Mailer::Help

--- modules/EnsEMBL/Web/Command/Help/MovieEmail.pm
+++ modules/EnsEMBL/Web/Command/Help/MovieEmail.pm
@@ -5,81 +5,18 @@ package EnsEMBL::Web::Command::Help::MovieEmail;
 use strict;
 use warnings;
 
-# use EnsEMBL::Web::Filter::Spam;
-use EnsEMBL::Web::Mailer;
+use EnsEMBL::Web::Mailer::Help;
 
 use base qw(EnsEMBL::Web::Command);
 
 sub process {
-  my $self = shift;
-  
-  my $hub = $self->hub;
-  my $spam;
+  my $self  = shift;
+  my $hub   = $self->hub;
+  my $url   = {qw(type Help action EmailSent result 1)};
 
-   # Check honeypot fields first
-  if ($hub->param('honeypot_1') || $hub->param('honeypot_2')) {
-    $spam = 1;
-  }  else {
-    # Check the user's input for spam _before_ we start adding all our crap!
-#    my $filter = EnsEMBL::Web::Filter::Spam->new;
-#    $spam = $filter->check($hub->param('message'), 1);
-  }
+  $url->{'result'} = EnsEMBL::Web::Mailer::Help->new($hub)->send_movie_feedback_email($self->object->movie_problems) unless $hub->param('honeypot_1') || $hub->param('honeypot_2'); # check honeypot fields before sending email
 
-  
-  if (!$spam) {
-    my @mail_attributes;
-    
-    my $species_defs = $hub->species_defs;
-    my $subject      = ($hub->param('subject') || $species_defs->ENSEMBL_SITETYPE . ' Helpdesk') . ' - ' . $species_defs->ENSEMBL_SERVERNAME;
-    my @T            = localtime;
-    my $date         = sprintf '%04d-%02d-%02d %02d:%02d:%02d', $T[5]+1900, $T[4]+1, $T[3], $T[2], $T[1], $T[0];
-    my $url          = 'http://' . $species_defs->ENSEMBL_SERVERNAME . ':' . $species_defs->ENSEMBL_PORT if $species_defs->ENSEMBL_PORT != '80';
-    
-    $url = undef if $url =~ m#Help/SendEmail#; # Compensate for auto-filling of _referer
-    
-    push @mail_attributes, (
-      [ 'Date',       $date ],
-      [ 'Name',       $hub->param('name') ],
-      [ 'Referer',    $url || '-none-' ],
-      [ 'User agent', $ENV{'HTTP_USER_AGENT'} ]
-    );
-    
-    my $message = 'Feedback from ' . $species_defs->ENSEMBL_SERVERNAME . "\n\n";
-    $message .= join "\n", map { sprintf '%-16.16s %s', "$_->[0]:", $_->[1] } @mail_attributes;
-
-    my $problems = {
-      no_load   => 'Movie did not appear',
-      playback  => 'Playback was jerky',
-      no_sound  => 'No sound',
-      bad_sound => 'Poor quality sound',
-      other     => 'Other (please describe below)'
-    };
-    
-    my @problems  = $hub->param('problem');
-    my $title     = $hub->param('title');
-    my $problem_text = "\n\nProblem with video $title:\n\n";
-    
-    if (@problems) {
-      foreach my $p (@problems) {
-        $problem_text .= "* $problems->{$p}\n";
-      }
-    }
-    
-    $message .= $problem_text;
-    $message .= "\n\nComments:\n\n" . $hub->param('message') . "\n\n";
-    
-    my $mailer = EnsEMBL::Web::Mailer->new({
-      mail_server => 'localhost',
-      from        => $hub->param('email'),
-      to          => $species_defs->ENSEMBL_HELPDESK_EMAIL,
-      subject     => $subject,
-      message     => $message
-    });
-
-    $mailer->send({ spam_check => 0 });
-  }
-  
-  $self->ajax_redirect($self->url('/Help/EmailSent'));
+  return $self->ajax_redirect($hub->url($url));
 }
 
 1;
--- modules/EnsEMBL/Web/Command/Help/SendEmail.pm
+++ modules/EnsEMBL/Web/Command/Help/SendEmail.pm
@@ -5,78 +5,30 @@ package EnsEMBL::Web::Command::Help::SendEmail;
 use strict;
 use warnings;
 
-# use EnsEMBL::Web::Filter::Spam;
-use EnsEMBL::Web::Mailer;
+use EnsEMBL::Web::Mailer::Help;
 
 use base qw(EnsEMBL::Web::Command);
 
 sub process {
-  my $self = shift;
-  my $hub = $self->hub;
+  my $self    = shift;
+  my $hub     = $self->hub;
   my $url;
 
   if ($hub->param('submit') eq 'Back') {
-    my $param = {
-      name    => $hub->param('name'),
-      address => $hub->param('address'),
-      subject => $hub->param('subject'),
-      message => $hub->param('message')
+
+    $url = {
+      'type'    => 'Help',
+      'action'  => 'Contact',
+      map {$_   => $hub->param($_) || ''} qw(name address subject message)
     };
-    
-    $url = $self->url('/Help/Contact', $param);
+
   } else {
-    my $spam;
 
-    # Check honeypot fields first
-    #will prob need a list of these blacklisted addresses, but do this for now to fix Vega spam
-    $spam = 1 if $hub->param('address') eq 'neverdiespike@hanmail.net';
-
-    if ($hub->param('honeypot_1') || $hub->param('honeypot_2')) {
-      $spam = 1;
-    }  else {
-      # Check the user's input for spam _before_ we start adding all our crap!
-#     my $filter = EnsEMBL::Web::Filter::Spam->new;
-#     $spam = $filter->check($hub->param('message'), 1);
-    }
-
-    if (!$spam) {
-      my @mail_attributes;
-      
-      my $species_defs = $hub->species_defs;
-      my $subject      = ($hub->param('subject') || $species_defs->ENSEMBL_SITETYPE . ' Helpdesk') . ' - ' . $species_defs->ENSEMBL_SERVERNAME;
-      my @T            = localtime;
-      my $date         = sprintf '%04d-%02d-%02d %02d:%02d:%02d', $T[5]+1900, $T[4]+1, $T[3], $T[2], $T[1], $T[0];
-      my $url          = $species_defs->ENSEMBL_BASE_URL;
-      
-      $url = undef if $url =~ m#Help/SendEmail#; # Compensate for auto-filling of _referer
-      
-      push @mail_attributes, (
-        [ 'Date',        $date ],
-        [ 'Name',        $hub->param('name') ],
-        [ 'Referer',     $url || '-none-' ],
-        [ 'Last Search', $hub->param('string')||'-none-' ],
-        [ 'User agent',  $ENV{'HTTP_USER_AGENT'} ]
-      );
-      
-      my $message = 'Support question from ' . $species_defs->ENSEMBL_SERVERNAME . "\n\n";
-      $message .= join "\n", map { sprintf '%-16.16s %s', "$_->[0]:", $_->[1] } @mail_attributes;
-      $message .= "\n\nComments:\n\n" . $hub->param('message') . "\n\n";
-
-      my $mailer = EnsEMBL::Web::Mailer->new({
-        mail_server => 'localhost',
-        from        => $hub->param('address'),
-        to          => $species_defs->ENSEMBL_HELPDESK_EMAIL,
-        subject     => $subject,
-        message     => $message
-      });
-      
-      $mailer->send({ spam_check => 0 });
-    }
-
-    $url = $self->url('/Help/EmailSent');
+    $url              = {qw(type Help action EmailSent result 1)};
+    $url->{'result'}  = EnsEMBL::Web::Mailer::Help->new($hub)->send_help_contact_email unless $hub->param('honeypot_1') || $hub->param('honeypot_2'); # check honeypot fields before sending email
   }
 
-  $self->ajax_redirect($url);
+  return $self->ajax_redirect($hub->url($url));
 }
 
 1;
