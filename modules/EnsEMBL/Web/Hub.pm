89439e2a499607281683b3d84e0b37a7398e7031
Beth Yates <bp1@sanger.ac.uk>
2013-03-26 21:09:01 +0000

    bug fix - broken dbc retrieval

--- modules/EnsEMBL/Web/Hub.pm
+++ modules/EnsEMBL/Web/Hub.pm
@@ -195,7 +195,7 @@ sub database {
   if ($_[0] =~ /compara/) {
     return Bio::EnsEMBL::Registry->get_DBAdaptor('multi', $_[0], 1);
   } else {
-    return $self->species eq 'common' ? undef : $self->databases->get_DBAdaptor(@_);
+    return $self->databases->get_DBAdaptor(@_);
   }
 }
 
