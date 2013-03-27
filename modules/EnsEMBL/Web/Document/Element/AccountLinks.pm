89f3ce799cd5ead7a12dc44457fc36bc98d1a399
Harpreet Riat <hr5@sanger.ac.uk>
2013-03-27 15:37:25 +0000

    content method moved to user pugins

--- modules/EnsEMBL/Web/Document/Element/AccountLinks.pm
+++ modules/EnsEMBL/Web/Document/Element/AccountLinks.pm
@@ -1,31 +1,9 @@
-# $Id$
-
 package EnsEMBL::Web::Document::Element::AccountLinks;
-
-### Generates links to user account (currently in masthead)
 
 use strict;
 
 use base qw(EnsEMBL::Web::Document::Element);
 
-sub content {
-  my $self    = shift;
-  my $hub     = $self->hub;
-  my $species = $hub->species;
-     $species = !$species || $species eq 'Multi' || $species eq 'common' ? 'Multi' : $species;
-  my $html;
-  my $species_defs = $self->species_defs;
-  if ($species_defs->has_userdb and $species_defs->ENSEMBL_LOGINS) {
-    if ($hub->user) {
-      $html .= sprintf '<a class="constant modal_link" style="display:none" href="%s">Account</a>',            $hub->url({ __clear => 1, species => $species, type => 'Account', action => 'Links'  });
-      $html .= sprintf ' &middot; <a class="constant" href="%s">Logout</a>',                                   $hub->url({ __clear => 1, species => $species, type => 'Account', action => 'Logout' });
-    } else {
-      $html .= sprintf '<a class="constant modal_link" style="display:none" href="%s">Login</a>',              $hub->url({ __clear => 1, species => $species, type => 'Account', action => 'Login' });
-      $html .= sprintf ' &middot; <a class="constant modal_link" style="display:none" href="%s">Register</a>', $hub->url({ __clear => 1, species => $species, type => 'Account', action => 'User', function => 'Add' });
-    }
-  }
-
-  return $html;
-}
+sub content {} # stub for users plugin
 
 1;
