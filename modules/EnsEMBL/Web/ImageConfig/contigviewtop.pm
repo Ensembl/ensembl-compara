495d386f4a13655c8fb1b92fc4dd79813ef71d8e
Simon Brent <sb23@sanger.ac.uk>
2013-04-02 09:51:32 +0000

    remove structural_variation_legend too

--- modules/EnsEMBL/Web/ImageConfig/contigviewtop.pm
+++ modules/EnsEMBL/Web/ImageConfig/contigviewtop.pm
@@ -33,8 +33,8 @@ sub init {
   $self->load_tracks;
   $self->image_resize = 1;
   
-  $self->modify_configs([ 'transcript'                     ], { render => 'gene_label', strand => 'r' });
-  $self->modify_configs([ 'variation',  'variation_legend' ], { display => 'off', menu => 'no'        });
+  $self->modify_configs([ 'transcript' ], { render => 'gene_label', strand => 'r' });
+  $self->modify_configs([ 'variation',  'variation_legend', 'structural_variation_legend' ], { display => 'off', menu => 'no' });
   
   $self->add_tracks('decorations',
     [ 'scalebar',  '', 'scalebar',  { display => 'normal', menu => 'no'                }],
