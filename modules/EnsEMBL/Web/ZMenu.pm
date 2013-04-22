11dbe055034ebf7d3bbdb1dc38f109716e69b28b
Simon Brent <sb23@sanger.ac.uk>
2013-04-22 13:05:48 +0000

    support for multiple features to be shown in 1 zmenu, by calling ->new_feature for each feature

--- modules/EnsEMBL/Web/ZMenu.pm
+++ modules/EnsEMBL/Web/ZMenu.pm
@@ -12,21 +12,21 @@ sub new {
   my ($class, $hub, $object) = @_;
   
   my $self = {
-    hub            => $hub,
-    object         => $object,
-    entries        => [],
-    stored_entries => {},
-    order          => 1,
-    caption        => '',
-    pagination     => {},
+    hub      => $hub,
+    object   => $object,
+    features => [],
   };
   
   bless $self, $class;
   
+  $self->new_feature;
   $self->content;
   
   # stored_entries keeps all entries of all plugins in a hash, keyed by order
-  $self->{'stored_entries'}->{$_->{'order'}} = $_ for @{$self->{'entries'}};
+  
+  foreach my $feature (@{$self->{'features'}}) {
+    $feature->{'stored_entries'}{$_->{'order'}} = $_ for @{$feature->{'entries'}};
+  }
   
   return $self;
 }
@@ -43,20 +43,29 @@ sub object {
 
 sub caption {
   my $self = shift;
-  $self->{'caption'} = shift if @_;
-  return $self->{'caption'};
+  $self->{'feature'}{'caption'}      = shift if @_;
+  $self->{'feature'}{'caption_link'} = shift if @_;
+  return $self->{'feature'}{'caption'};
 }
 
 sub highlight {
   my $self = shift;
-  $self->{'highlight'} = shift if @_;
-  return $self->{'highlight'};
+  $self->{'feature'}{'highlight'} = shift if @_;
+  return $self->{'feature'}{'highlight'};
 }
 
-sub pagination {
+sub new_feature {
   my $self = shift;
-  $self->{'pagination'} = shift if @_;
-  return $self->{'pagination'};
+  
+  $self->{'feature'} = {
+    entries        => [],
+    stored_entries => {},
+    order          => 1,
+    caption        => '',
+    highlight      => '',
+  };
+  
+  push @{$self->{'features'}}, $self->{'feature'};
 }
 
 # When adding an entry you can specify ORDER or POSITION.
@@ -70,59 +79,48 @@ sub add_entry {
   my ($self, $entry) = @_;
   
   if ($entry->{'position'}) {
-    $_->{'order'}++ for grep $_->{'order'} >= $entry->{'position'}, @{$self->{'entries'}}; # increment order for each entry after the given position
+    $_->{'order'}++ for grep $_->{'order'} >= $entry->{'position'}, @{$self->{'feature'}{'entries'}}; # increment order for each entry after the given position
     $entry->{'order'} = $entry->{'position'};
-    $self->{'order'}++;
+    $self->{'feature'}{'order'}++;
   } else {
-    $entry->{'order'} ||= $self->{'order'}++;
+    $entry->{'order'} ||= $self->{'feature'}{'order'}++;
   }
   
-  push @{$self->{'entries'}}, $entry;
+  push @{$self->{'feature'}{'entries'}}, $entry;
 }
 
 sub add_subheader {
   my ($self, $label) = @_;
-  
-  return unless defined $label;
-  
-  $self->add_entry({
-    type       => 'subheader',
-    label_html => $label
-  });
+  $self->add_entry({ type => 'subheader', label_html => $label }) if defined $label;
 }
 
 # Can be used from plugins to remove entries from previous plugins' menu.
 # Requires a list of entry positions to remove
 sub remove_entries {
   my $self = shift;
-  delete $self->{'stored_entries'}->{$_} for @_;
+  delete $self->{'feature'}{'stored_entries'}{$_} for @_;
 }
 
 # Generic code to grab hold of an existing entry and modify it
 sub modify_entry_by {
-  my ($self, $type, $entry) = @_;
-  for my $i (0..$#{$self->{'entries'}}) {
-    if ($self->{'entries'}[$i]{$type} eq $entry->{$type}) {
-      $self->{'entries'}[$i]{$_} = $entry->{$_} for keys %$entry;
+  my ($self, $key, $entry) = @_;
+  
+  foreach (@{$self->{'feature'}{'entries'}}) {
+    if ($_->{$key} eq $entry->{$key}) {
+      $_ = $entry;
       last;
     }
   }
 }
 
-sub modify_entry_by_type {
-  my ($self, $entry) = @_;
-  warn "DEPRECATED, use modify_entry_by";
-  $self->modify_entry_by($entry->{'type'},$entry);
-}
-
 # Delete an entry by its value
 sub delete_entry_by_value {
   my ($self, $value) = @_;
-
-  for my $i (0..$#{$self->{'entries'}}) {
-    foreach my $key (keys %{$self->{'entries'}[$i]}) {
-      if ($self->{'entries'}[$i]{$key} eq $value) {
-        $self->{'entries'}[$i] = undef;
+  
+  foreach my $entry (@{$self->{'feature'}{'entries'}}) {
+    foreach (keys %$entry) {
+      if ($entry->{$_} eq $value) {
+        $entry = undef;
         last;
       }
     }
@@ -132,55 +130,61 @@ sub delete_entry_by_value {
 # Delete an entry by type
 sub delete_entry_by_type {
   my ($self, $type) = @_;
-  my $i;
   
-  for my $i (0..$#{$self->{'entries'}}) {
-    foreach my $key (keys %{$self->{'entries'}[$i]}) {
-      if ($self->{'entries'}[$i]{'type'} eq $type) {
-        $self->{'entries'}[$i] = undef;
-        last;
-      }
+  foreach (@{$self->{'feature'}{'entries'}}) {
+    if ($_->{'type'} eq $type) {
+      $_ = undef;
+      last;
     }
   }
 }
 
 # Build and print the JSON response
 sub render {
-  my $self = shift;
+  my $self          = shift;
+  my %reserved_keys = map { $_ => 1 } qw(type link link_class label label_html order abs_url external update_params); # These keys will not be added to the entries hash as part of %extra
+  my @features;
   
-  my @entries;
-  
-  foreach (sort { $a <=> $b } keys %{$self->{'stored_entries'}}) {
-    my $entry = $self->{'stored_entries'}->{$_};
-    my $type  = encode_entities($entry->{'type'});
-    my $link;
+  foreach my $feature (@{$self->{'features'}}) {
+    my @entries;
     
-    if ($entry->{'link'}) {
-      if ($entry->{'extra'}{'abs_url'}) {
-        $link = $entry->{'link'};
+    foreach (sort { $a <=> $b } keys %{$feature->{'stored_entries'}}) {
+      my $entry = $feature->{'stored_entries'}{$_};
+      my $type  = encode_entities($entry->{'type'});
+      my %extra = map { $reserved_keys{$_} ? () : ($_ => $entry->{$_}) } keys %$entry;
+      my $value;
+      
+      if ($entry->{'link'}) {
+        if ($entry->{'abs_url'}) {
+          $value = $entry->{'link'};
+        } else {
+          $value = sprintf(
+            '<a href="%s"%s%s>%s%s%s</a>',
+            encode_entities($entry->{'link'}),
+            $entry->{'external'} ? ' rel="external"' : '',
+            $entry->{'link_class'} ? qq{ class="$entry->{'link_class'}"} : '',
+            encode_entities($entry->{'label'}),
+            $entry->{'label_html'},
+            $entry->{'update_params'},
+          );
+        }
       } else {
-        $link = sprintf(
-          '<a href="%s"%s %s>%s%s</a>',
-          encode_entities($entry->{'link'}),
-          $entry->{'extra'}{'external'} ? ' rel="external"' : '',
-          $entry->{'class'} ? qq{ class="$entry->{'class'}"} : '',
-          encode_entities($entry->{'label'} . $entry->{'label_html'}),
-          $entry->{'extra'}{'update_params'},
-        );
+        $value = encode_entities($entry->{'label'}) . $entry->{'label_html'};
       }
-    } else {
-      $link = encode_entities($entry->{'label'}) . $entry->{'label_html'};
+      
+      push @entries, { key => $type, value => $value, %extra };
     }
     
-    push @entries, { link => $link, type => $type };
+    if (scalar @entries) {
+      push @features, {
+        caption   => $feature->{'caption_link'} ? $feature->{'caption'} : encode_entities($feature->{'caption'}),
+        highlight => $feature->{'highlight'},
+        entries   => \@entries,
+      };
+    }
   }
   
-  print $self->jsonify({
-    caption    => encode_entities($self->{'caption'}),
-    entries    => \@entries,
-    highlight  => encode_entities($self->{'highlight'}),
-    pagination => encode_entities($self->{'pagination'}),
-  });
+  print $self->jsonify(\@features);
 }
 
 1;
