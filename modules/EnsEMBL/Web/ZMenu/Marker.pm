bc505e8738961bc8ea31e6e2e5e44d032ea51430
Simon Brent <sb23@sanger.ac.uk>
2013-04-25 15:21:58 +0000

    support for multiple features

--- modules/EnsEMBL/Web/ZMenu/Marker.pm
+++ modules/EnsEMBL/Web/ZMenu/Marker.pm
@@ -4,21 +4,42 @@ package EnsEMBL::Web::ZMenu::Marker;
 
 use strict;
 
+use Bio::EnsEMBL::GlyphSet::_marker;
+
 use base qw(EnsEMBL::Web::ZMenu);
 
 sub content {
-  my $self = shift;
-  my $hub  = $self->hub;
-  my $m    = $hub->param('m');
+  my $self       = shift;
+  my $hub        = $self->hub;
+  my $m          = $hub->param('m');
+  my $click_data = $self->click_data;
+  my @features;
   
-  $self->caption($m);
+  if ($click_data) {
+    my $glyphset = Bio::EnsEMBL::GlyphSet::_marker->new($click_data);
+    $glyphset->{'text_export'} = 1;
+    @features = @{$glyphset->features};
+    @features = () unless grep $_->{'drawing_id'} eq $m, @features;
+  }
+  
+  @features = { drawing_id => $m } unless scalar @features;
+  
+  $self->feature_content($_) for @features;
+}
+
+sub feature_content {
+  my ($self, $f) = @_;
+  my $hub = $self->hub;
+  
+  $self->new_feature;
+  $self->caption($f->{'drawing_id'});
   
   $self->add_entry({
     label => 'Marker info.',
     link  => $hub->url({
       type   => 'Marker',
       action => 'Details',
-      m      => $m
+      m      => $f->{'drawing_id'}
     })
   });
 }
