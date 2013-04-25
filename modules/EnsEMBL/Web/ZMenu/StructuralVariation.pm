3690e0e36a6b3545b8e82bc800835cc39795b1d0
Simon Brent <sb23@sanger.ac.uk>
2013-04-25 15:22:58 +0000

    added support for multiple features

--- modules/EnsEMBL/Web/ZMenu/StructuralVariation.pm
+++ modules/EnsEMBL/Web/ZMenu/StructuralVariation.pm
@@ -2,209 +2,191 @@ package EnsEMBL::Web::ZMenu::StructuralVariation;
 
 use strict;
 
+use Bio::EnsEMBL::GlyphSet::structural_variation;
+
 use base qw(EnsEMBL::Web::ZMenu);
 
 sub content {
-  my $self = shift; 
-  my $hub  = $self->hub;
-  my $sv_id = $hub->param('sv');
+  my $self       = shift;
+  my $hub        = $self->hub;
+  my $svf        = $hub->param('svf');
+  my $click_data = $self->click_data;
+  my @features;
   
-  return unless $sv_id;
+  if ($click_data) {
+    @features = @{Bio::EnsEMBL::GlyphSet::structural_variation->new($click_data)->features};
+    @features = () unless grep $_->dbID eq $svf, @features;
+  }
   
-  my $db_adaptor      = $hub->database('variation');
-  my $var_adaptor     = $db_adaptor->get_StructuralVariation;
-  my $variation       = $var_adaptor->fetch_by_name($sv_id); 
-  my $svf             = $variation->get_all_StructuralVariationFeatures();
-  my $max_length      = ($hub->species_defs->ENSEMBL_GENOME_SIZE || 1) * 1e6; 
-  my $class           = $variation->var_class;
-  my $vstatus         = $variation->get_all_validation_states;
-  my $pubmed_link     = '';
-  my $location_link;
-  my $feature;
-  my $study_name;
-  my $description;
-  my $study_url;
-  my $is_breakpoint;
-  my $study = $variation->study;
+  @features = $hub->database('variation')->get_StructuralVariationFeatureAdaptor->fetch_by_dbID($svf) unless scalar @features;
   
-  if (defined($study)) {
-    $study_name   = $study->name;
+  $self->{'feature_count'} = scalar @features;
+  
+  $self->feature_content($_) for @features;
+}
+
+sub feature_content {
+  my ($self, $feature) = @_;
+  my $hub         = $self->hub;
+  my $variation   = $feature->structural_variation;
+  my $sv_id       = $feature->variation_name;
+  my $seq_region  = $feature->seq_region_name;
+  my $start       = $feature->seq_region_start;
+  my $end         = $feature->seq_region_end;
+  my $class       = $variation->var_class;
+  my $vstatus     = $variation->get_all_validation_states;
+  my $study       = $variation->study;
+  my $ssvs        = $variation->get_all_SupportingStructuralVariants;
+  my $description = $variation->source_description;
+  my $position    = $start;
+  my $length      = $end - $start;
+  my $max_length  = ($hub->species_defs->ENSEMBL_GENOME_SIZE || 1) * 1e6; 
+  my ($pubmed_link, $location_link, $study_name, $study_url, $is_breakpoint, @allele_types);  
+  
+  $self->new_feature;
+  
+  if (defined $study) {
+    $study_name  = $study->name;
     $description = $study->description;
     $study_url   = $study->url; 
   }
-
-  if (scalar @$svf == 1) {
-    $feature = $svf->[0];
-  } else {
-    foreach (@$svf) {
-      $feature = $_ if $_->dbID eq $hub->param('svf');
-    }
-  }
-  
-  my $start      = $feature->start;
-  my $end        = $feature->end;
-  my $seq_region = $feature->seq_region_name;
-  my $position   = "$seq_region:$start";
-  my $length     = $end - $start;
   
   if ($end < $start) {
-    $position = "$seq_region: between $end & $start";
+    $position = "between $end & $start";
   } elsif ($end > $start) {
-    $position = "$seq_region:$start-$end";
-  }
-	
-	
-  if (! $description) {
-    $description = $variation->source_description;
+    $position = "$start-$end";
   }
   
-	if (defined($feature->breakpoint_order) && $feature->is_somatic == 1) {
-	  $is_breakpoint = " (breakpoint)";
-	  $position = $self->get_locations($sv_id);
-	
+	if (defined $feature->breakpoint_order && $feature->is_somatic == 1) {
+	  $is_breakpoint = 1;
 	} elsif ($length > $max_length) {
-    my $track_name = $variation->is_somatic ? 'somatic_sv_feature' : 'variation_feature_structural';  
     $location_link = $hub->url({
       type     => 'Location',
       action   => 'Overview',
-      r        => $seq_region . ':' . $start . '-' . $end,
-      cytoview => "$track_name=normal",
+      r        => "$start-$end",
+      cytoview => sprintf('%s=normal', $variation->is_somatic ? 'somatic_sv_feature' : 'variation_feature_structural'),
     });
   } else {
     $location_link = $hub->url({
       type   => 'Location',
       action => 'View',
-      r      => $seq_region . ':' . $start . '-' . $end,
+      r      => "$seq_region:$start-$end",
     });
   }
   
-  if ($description =~/PMID/) {
+  if ($description =~ /PMID/) {
     my @description_string = split (':', $description);
-    my $pubmed_id = pop @description_string;
-    $pubmed_id =~ s/\s+.+//g;
-    $pubmed_link = $hub->get_ExtURL('PUBMED', $pubmed_id);  
+    my $pubmed_id          = pop @description_string;
+       $pubmed_id          =~ s/\s+.+//g;
+       $pubmed_link        = $hub->get_ExtURL('PUBMED', $pubmed_id);  
   }    
-
-  my $ssvs   = $self->object->supporting_sv;
-  my @allele_types;
+  
   foreach my $ssv (@$ssvs) {
     my $a_type = $ssv->var_class;
-    
-    next if (grep {$a_type eq $_} @allele_types);
-    push @allele_types, $a_type;
+    push @allele_types, $a_type unless grep $a_type eq $_, @allele_types;
   }
-  @allele_types = sort(@allele_types);
-
-  my $sv_caption = 'Structural variation: ';
-  if ($class eq 'CNV_PROBE') {
-    $sv_caption = 'CNV probe: ';
-  }
-  $self->caption($sv_caption . $sv_id);
-
+  
+  @allele_types = sort @allele_types;
+  
+  $self->caption($class eq 'CNV_PROBE' ? 'CNV probe: ' : 'Structural variation: ' . $sv_id);
+  
   $self->add_entry({
-    label_html => $sv_id.' properties',
+    label_html => "$sv_id properties",
     link       => $hub->url({
       type     => 'StructuralVariation',
       action   => 'Summary',
       sv       => $sv_id,
     })
   });
-
+  
+  if ($is_breakpoint) {
+    $self->add_entry({
+      type       => 'Location',
+      label_html => $self->get_locations($sv_id),
+    });
+	} else {
+    $self->add_entry({
+      type  => 'Location',
+      label => sprintf('%s: %s', $self->neat_sr_name($feature->slice->coord_system->name, $seq_region), $position),
+      link  => $location_link,
+    });
+	}
+  
   $self->add_entry({
     type  => 'Source',
     label => $variation->source,
   });
   
-  
-  if (defined($study_name)) {
+  if (defined $study_name) {
     $self->add_entry({
       type  => 'Study',
       label => $study_name,
       link  => $study_url, 
     });
   }
-
+  
   $self->add_entry({
     type  => 'Description',
     label => $description,
     link  => $pubmed_link, 
   });
-
+  
   $self->add_entry({
     type  => 'Class',
     label => $class,
   });
-
-  if (scalar(@allele_types)) {
-    my $s = (scalar(@allele_types) > 1) ? 's' : '';
+  
+  if (scalar @allele_types) {
     $self->add_entry({
-      type  => "Allele type$s",
-      label => join(', ',@allele_types),
+      type  => 'Allele type' . (scalar @allele_types > 1 ? 's' : ''),
+      label => join(', ', @allele_types),
     });
   }
-
-  if (scalar(@$vstatus) and $vstatus->[0]) {
+  
+  if (scalar @$vstatus && $vstatus->[0]) {
     $self->add_entry({
       type  => 'Validation',
-      label => join(',',@$vstatus),
+      label => join(',', @$vstatus),
     });    
   }
-
-  if ($is_breakpoint) {
-	 $self->add_entry({
-      type  => 'Location',
-      label_html => $position,
-    });
-	} else {
-    $self->add_entry({
-      type  => 'Location',
-      label => $position.$is_breakpoint,
-      link  => $location_link,
-    });
-	}
 }
 
-
 sub get_locations {
-  my $self  = shift;
-	my $sv_id = shift;
-	my $hub   = $self->hub;
-	my $mappings = $self->object->variation_feature_mapping;
-	
+  my ($self, $sv_id) = @_;
+	my $hub       = $self->hub;
+	my $mappings  = $self->object->variation_feature_mapping;
   my $params    = $hub->core_params;
   my @locations = ({ value => 'null', name => 'None selected' });
-    
-  my $location_info;
-  my $global_location_info;
-	  
+  my ($global_location_info, $location_info);
+	
   # add locations for each mapping
   foreach (sort { $mappings->{$a}{'Chr'} cmp $mappings->{$b}{'Chr'} || $mappings->{$a}{'start'} <=> $mappings->{$b}{'start'}} keys %$mappings) {
     my $region   = $mappings->{$_}{'Chr'}; 
     my $start    = $mappings->{$_}{'start'};
     my $end      = $mappings->{$_}{'end'};
     my $str      = $mappings->{$_}{'strand'};
-    my $bp_order = $mappings->{$_}{'breakpoint_order'};
-       
-    if (defined($bp_order)) {
+    
+    if (defined $mappings->{$_}{'breakpoint_order'}) {
       my @coords = ($start);
-			push (@coords, $end) if ($start!=$end);
+			push @coords, $end if $start != $end;
 			
 			foreach my $coord (@coords) {
         my $loc_text = "<b>$region:$coord</b>";
         my $loc_link = sprintf(
           '<a href="%s" class="constant">%s</a>',
-            $hub->url({
-              type              => 'Location',
-              action            => 'View',
-              r                 => $region . ':' . ($coord - 500) . '-' . ($coord + 500),
-              sv                => $sv_id,
-              svf               => $_,
-              contigviewbottom  => 'somatic_sv_feature=normal'
-            }), $loc_text
+          $hub->url({
+            type             => 'Location',
+            action           => 'View',
+            r                => $region . ':' . ($coord - 500) . '-' . ($coord + 500),
+            sv               => $sv_id,
+            svf              => $_,
+            contigviewbottom => 'somatic_sv_feature=normal'
+          }), $loc_text
         );
-        $loc_link .= '<br />('.($str > 0 ? 'forward' : 'reverse').' strand)';
         
-        if (!defined($location_info)) {
+        $loc_link .= '<br />(' . ($str > 0 ? 'forward' : 'reverse') . ' strand)';
+        
+        if (!$location_info) {
           $location_info = "Breakpoints<br />FROM $loc_link";
         } elsif ($location_info =~ /<br \/>TO/) {
 				  $global_location_info .= $location_info;
@@ -215,8 +197,8 @@ sub get_locations {
       } 
     }
   }
-    
-  return ($global_location_info) ? $global_location_info.$location_info : $location_info;
+  
+  return "$global_location_info$location_info";
 }
 
 1;
