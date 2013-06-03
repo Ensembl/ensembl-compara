26c54dee20a166825df1ba807a55e35b381eb41b
Matthieu Muffato <muffato@ebi.ac.uk>
2013-06-03 11:08:16 +0000

    Updated tutorials for the Gene/SeqMember API

--- docs/tutorial_examples/tut_member1.pl
+++ docs/tutorial_examples/tut_member1.pl
@@ -1,9 +1,9 @@
 # get the MemberAdaptor
-my $member_adaptor = Bio::EnsEMBL::Registry->get_adaptor(
-    'Multi','compara','Member');
+my $genemember_adaptor = Bio::EnsEMBL::Registry->get_adaptor(
+    'Multi','compara','GeneMember');
 
 # fetch a Member
-my $member = $member_adaptor->fetch_by_source_stable_id(
+my $member = $genemember_adaptor->fetch_by_source_stable_id(
     'ENSEMBLGENE','ENSG00000004059');
 
 # print out some information about the Member
