f66738f1bec216f5b2a375b731a1ab0d106dcbc4
Matthieu Muffato <muffato@ebi.ac.uk>
2013-07-08 15:56:18 +0000

    We can use ncbi-blast-2.2.27+

--- modules/Bio/EnsEMBL/Compara/PipeConfig/ProteinTrees_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/ProteinTrees_conf.pm
@@ -122,7 +122,7 @@ sub default_options {
         #'quicktree_exe'             => '/software/ensembl/compara/quicktree_1.1/bin/quicktree',
         #'buildhmm_exe'              => '/software/ensembl/compara/hmmer3/hmmer-3.0/src/hmmbuild',
         #'codeml_exe'                => '/usr/local/ensembl/bin/codeml',
-        #'blast_exe_dir'             => '/software/ensembl/compara/ncbi-blast-2.2.26+/bin',
+        #'blast_exe_dir'             => '/software/ensembl/compara/ncbi-blast-2.2.27+/bin',
 
     # HMM specific parameters
         #'hmm_clustering'            => 0, ## by default run blastp clustering
@@ -130,7 +130,7 @@ sub default_options {
         #'hmm_library_basedir'       => '/lustre/scratch109/sanger/fs9/treefam8_hmms',
         ##'cm_file_or_directory'      => '/lustre/scratch110/ensembl/mp12/panther_hmms/PANTHER7.2_ascii', ## Panther DB
         ##'hmm_library_basedir'       => '/lustre/scratch110/ensembl/mp12/Panther_hmms',
-        #'blast_path'                => '/software/ensembl/compara/ncbi-blast-2.2.26+/bin/',
+        #'blast_path'                => '/software/ensembl/compara/ncbi-blast-2.2.27+/bin/',
         #'pantherScore_path'         => '/software/ensembl/compara/pantherScore1.03',
         #'hmmer_path'                => '/software/ensembl/compara/hmmer-2.3.2/src/',
 
