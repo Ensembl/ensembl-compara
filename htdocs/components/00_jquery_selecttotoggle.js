9e5faab8968162c26ddcd50b4eac4e6d526dd7ec
Harpreet Riat <hr5@sanger.ac.uk>
2013-09-11 13:01:24 +0000

    selectToToggle can now be called as el.selectToToggle('trigger') to manually trigger the 'change' event in case change is triggered by somewhere else JavaScript instead of the user

--- htdocs/components/00_jquery_selecttotoggle.js
+++ htdocs/components/00_jquery_selecttotoggle.js
@@ -5,12 +5,12 @@
  * Note: Be careful if there are more than one selectToToggle elements on a page with one or more options having same values (use className on the option tags in those cases)
  **/
 (function ($) {
-  $.selectToToggle = function (el, toggleMap, wrapper) {
+  $.selectToToggle = function (el, options, wrapper) {
     
     var toggle = function() {
-      for (var val in toggleMap) {
+      for (var val in options) {
         if (val != this.value) {
-          wrapper.find(toggleMap[val]).hide().removeAttr('selected checked').filter('option').each(function() { // if hiding an option element, also disable it to make it work in webkit
+          wrapper.find(options[val]).hide().removeAttr('selected checked').filter('option').each(function() { // if hiding an option element, also disable it to make it work in webkit
             var option = $(this);
             if (typeof option.data('_stt_disabled') === 'undefined') {
               option.data('_stt_disabled', !!this.disabled); // remember original disabled attribute
@@ -19,7 +19,7 @@
         }
       }
 
-      wrapper.find(toggleMap[this.value]).show().filter('option').prop('disabled', function() {
+      wrapper.find(options[this.value]).show().filter('option').prop('disabled', function() {
         return $(this).data('_stt_disabled');
       }).filter('select option').parent().each(function() { //show the requried html block
         var dropdown = $(this);
@@ -29,29 +29,37 @@
       });
     };
 
-    el.on('change.selectToToggle', toggle);
-    if (el[0].nodeName == 'SELECT' || el[0].checked) {
-      toggle.apply(el[0]);
+    if (options === 'trigger') {
+      el.trigger('change.selectToToggle');
+    } else {
+      el.on('change.selectToToggle', toggle);
+      if (el[0].nodeName == 'SELECT' || el[0].checked) {
+        toggle.apply(el[0]);
+      }
     }
   };
 
   $.fn.selectToToggle = function (
-    toggleMap,  // map of select element's option value to corresponding jquery selectors strings (as accepted by find() method) (Optional - defaults to '._stt_[className]' if class name uses prefix _stt__, or '._stt_[value]' otherwise)
+    options,    // string 'trigger' to trigger toggling for an existing element, or map of select element's option value to corresponding jquery selectors strings (as accepted by find() method) (Optional - defaults to '._stt_[className]' if class name uses prefix _stt__, or '._stt_[value]' otherwise)
     wrapper     // wrapper element to call method 'find(selectors)' on - defaults to $(document.body)
   ) {
     
     return this.each(function () {
       var input = $(this);
-      var tMap  = $.extend({}, toggleMap);
-      wrapper   = wrapper || $(document.body);
-      if ($.isEmptyObject(tMap)) {
-        (this.nodeName == 'SELECT' ? input.find('option') : wrapper.find('input[name=' + this.name + ']')).each(function() {
-          if (this.value) {
-            tMap[this.value] = '._stt_' + ((this.className.match(/(?:\s+|^)_stt__([^\s]+)/) || []).pop() || this.value);
-          }
-        });
+      if (options === 'trigger') {
+        $.selectToToggle(input, 'trigger', wrapper);
+      } else {
+        var tMap  = $.extend({}, options);
+        wrapper   = wrapper || $(document.body);
+        if ($.isEmptyObject(tMap)) {
+          (this.nodeName == 'SELECT' ? input.find('option') : wrapper.find('input[name=' + this.name + ']')).each(function() {
+            if (this.value) {
+              tMap[this.value] = '._stt_' + ((this.className.match(/(?:\s+|^)_stt__([^\s]+)/) || []).pop() || this.value);
+            }
+          });
+        }
+        $.selectToToggle(input, tMap, wrapper);
       }
-      $.selectToToggle(input, tMap, wrapper);
     });
   };
 })(jQuery);
