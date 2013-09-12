bc8bee06d35ade349e27c07df7cc76efc35e8fc0
Dan Sheppard <ds23@sanger.ac.uk>
2013-09-12 15:02:23 +0000

    New scalebar code.

--- modules/Bio/EnsEMBL/GlyphSet/scalebar.pm
+++ modules/Bio/EnsEMBL/GlyphSet/scalebar.pm
@@ -1,3 +1,4 @@
+# See end of file for docs
 package Bio::EnsEMBL::GlyphSet::scalebar;
 
 use strict;
@@ -5,6 +6,59 @@ use strict;
 use POSIX qw(floor);
 
 use base qw(Bio::EnsEMBL::GlyphSet);
+  
+# Least number of pixels per written number. Feel free to tweak.
+my $min_pix_per_major = 80;
+
+sub scale {
+  my ($from,$to,$max) = @_; 
+
+  my $div = 5;
+  my $scale = 1;
+  my $skip = 1;
+
+  my @from_d = split(//,"".$from);
+  my @to_d = split(//,"".$to);
+  push @from_d,"0" for(@from_d..@to_d-1);
+  push @to_d,"0" for(@to_d..@from_d-1);
+
+  my $d; 
+  my $unit = -1; 
+  foreach my $i (0..@from_d-1) {
+    my $a_v = join("",@from_d[0..@from_d-$i-1])+0;
+    my $b_v = join("",@to_d[0..@from_d-$i-1])+0;
+    $d = $b_v-$a_v;
+    if($d) {
+      if($d <= $max) {
+        $unit = ("1".("0" x $i))+0;
+        last;
+      }   
+    }   
+  }
+  return [("1".("0" x @from_d)),1,1] if $unit == -1; 
+  if($d*5 <= $max) { $unit /=5; $div = 2; }
+  elsif($d*4 <= $max) { $unit /=4; $skip = 2; }
+  elsif($d*2 <= $max) { $unit /=2; $skip = 2; }
+  $skip = 1 unless $d > 2;
+  return [$unit,$div,$unit*$skip];
+}
+
+sub fmt_number {
+  my ($self,$num,$step) = @_;
+
+  $step =~ /0+$/;
+  my $strip = length $&;
+  my @lhs = split(//,"".$num);
+  if($strip>3) {
+    my $mul = int((@lhs-1)/3);
+    my @rhs = splice(@lhs,@lhs-$mul*3);
+    pop @rhs for(1..$strip);
+    unshift @rhs,"." if @rhs;
+    return join("",@lhs,@rhs,['','k','M','G']->[$mul],"b");
+  } else {
+    return $self->commify($num);
+  }
+}
 
 sub render {
   my ($self, $y) = @_;
@@ -20,28 +74,14 @@ sub render {
   my ($fontname, $fontsize) = $self->get_font_details('innertext');
   my ($major_unit, $minor_unit);
   
-  if ($length <= 51) {
-    $major_unit = 10;
-    $minor_unit = 1; 
-  } else {
-    my $exponent = 10 ** int(log($length) / log(10));
-    my $mantissa  = $length / $exponent;
-    
-    if ($mantissa < 1.2) {
-      $major_unit = $exponent / 10;
-      $minor_unit = $major_unit / 5;
-    } elsif ($mantissa < 2.5) {
-      $major_unit = $exponent / 5;
-      $minor_unit = $major_unit / 4;
-    } elsif ($mantissa < 5) {
-      $major_unit = $exponent / 2;
-      $minor_unit = $major_unit / 5;
-    } else {
-      $major_unit = $exponent;
-      $minor_unit = $major_unit / 5;
-    }
-  }
+
+  my $max_major_divs = $length*$pix_per_bp/$min_pix_per_major;
   
+  my ($major,$div,$labmaj) = @{scale($container->start,$container->end,$max_major_divs)};
+
+  $major_unit = $major;
+  $minor_unit = $major_unit / $div;
+
   # Black bar all the way along
   $self->push($self->Rect({
     x            => 0, 
@@ -96,9 +136,10 @@ sub render {
       }));
       
       my $label = $minor_unit < 1000 ? $self->commify($box_start * $contig_strand): $self->bp_to_nearest_unit($box_start * $contig_strand, 2);
+      $label = $self->fmt_number($box_start*$contig_strand,$major_unit);
       my @res   = $self->get_text_width(($box_start - $last_text_x) * $pix_per_bp * 1.5, $label, '', font => $fontname, ptsize => $fontsize);
 
-      if ($res[0]) {
+      if ($res[0] && !(($box_start*$contig_strand) % $labmaj)) {
         $self->push($self->Text({
           x         => $box_start - $global_start,
           y         => defined $y ? $y - $res[3] - 1 : 5,
@@ -134,3 +175,30 @@ sub render {
 }
 
 1;
+
+# Scalebar divisions should be
+#  SMALL ENOUGH
+#    to allow a user to be precise
+#  BUT BIG ENOUGH
+#    to not confuse the eye
+#    to allow room for the number underneath.
+#
+# They should be of a nice size and offset for decimal mental calculations.
+#
+# We work on arrays of digits, not numbers. That
+#  avoids rounding errors etc in floats
+#  us to easily track SF, nicely format numbers, etc
+#  to write more directly what we intend to do (which is display digits).
+#
+# We define a minimum size of scalebar major division (ie one with a number)
+# in pixels. That should be enough pixels to write a number and leave some
+# space, and also big enough not to confuse the eye. We then search through
+# all the decimal truncations to find the greatest one which comes in over
+# the minimum division size. Finally, we try multiplying by "nice" decimal
+# divisors, -- 2, 4, 5 -- to see if we can bump the number up and yet stay
+# under the limit.
+#
+# Number writing uses units if that would truncate more than three digits
+# off the number, otherwise it's written literally (takes up no more space
+# and no more confusing because there will be lots of digits on rhs of the
+# unit based rep at this point).
