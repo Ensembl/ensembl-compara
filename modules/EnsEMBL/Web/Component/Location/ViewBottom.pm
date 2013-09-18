c430b22a64bab31be092d9e8ea21868e60c711b8
Simon Brent <sb23@sanger.ac.uk>
2013-09-18 13:10:56 +0000

    hardcode slice number - javascript doesn't need it to vary. tidied.

--- modules/EnsEMBL/Web/Component/Location/ViewBottom.pm
+++ modules/EnsEMBL/Web/Component/Location/ViewBottom.pm
@@ -14,53 +14,23 @@ sub _init {
   $self->has_image(1);
 }
 
-sub _add_object_track {
-  my $self = shift;
-
-  my $extra = '';
-  my $hub = $self->hub;
-  my $image_config = $hub->get_imageconfig('contigviewbottom');
-  # Add track for gene if not on by default
-  if (my $gene = $hub->core_objects->{'gene'}) {
-    my $key  = $image_config->get_track_key('transcript', $gene);
-    my $node = $image_config->get_node(lc $key);
- 
-    if($node) {
-      my $current = $node->get('display');
-      my $default = $node->data->{'display'};
-      if($current eq 'off' and $default eq 'off') {
-        my $flag = $hub->session->get_data(type => 'auto_add', code => lc $key);
-        unless($flag->{'data'}) {             # haven't done this before
-          $image_config->update_track_renderer(lc $key,'transcript_label');
-          $extra .= $self->_info("Information","<p>The track containing the highlighted gene has been added to your display.</p>")."<br/>";
-          $hub->session->set_data(type => 'auto_add' , code => lc $key, data => 1); 
-          $hub->session->store();
-        }
-      }
-    }
-  }
-  return $extra;
-}
-
 sub content {
   my $self        = shift;
   my $hub         = $self->hub;
   my $object      = $self->object;
   my $threshold   = 1000100 * ($hub->species_defs->ENSEMBL_GENOME_SIZE || 1);
   my $image_width = $self->image_width;
-  my $info = '';
   
   return $self->_warning('Region too large', '<p>The region selected is too large to display in this view - use the navigation above to zoom in...</p>') if $object->length > $threshold;
   
   my $slice        = $object->slice;
   my $length       = $slice->end - $slice->start + 1;
   my $image_config = $hub->get_imageconfig('contigviewbottom');
-  my $s            = $hub->get_viewconfig('ViewTop')->get('show_panel') eq 'yes' ? 3 : 2;
   
   $image_config->set_parameters({
     container_width => $length,
     image_width     => $image_width || 800, # hack at the moment
-    slice_number    => "1|$s"
+    slice_number    => '1|3'
   });
 
   ## Force display of individual low-weight markers on pages linked to from Location/Marker
@@ -70,18 +40,12 @@ sub content {
       { marker_id => $marker_id }
     );
   }
-
-  $info .= $self->_add_object_track();
-
+  
   # Add multicell configuration
-  if (keys %{$hub->species_defs->databases->{'DATABASE_FUNCGEN'}{'tables'}{'cell_type'}{'ids'}}){
-    my $web_slice_obj = $self->new_object( 'Slice', $slice, $object->__data );
-    my $cell_line_data = $web_slice_obj->get_cell_line_data($image_config);
-    $image_config->{'data_by_cell_line'} = $cell_line_data;
-  } 
-
+  $image_config->{'data_by_cell_line'} = $self->new_object('Slice', $slice, $object->__data)->get_cell_line_data($image_config) if keys %{$hub->species_defs->databases->{'DATABASE_FUNCGEN'}{'tables'}{'cell_type'}{'ids'}};
   $image_config->_update_missing($object);
   
+  my $info  = $self->_add_object_track($image_config);
   my $image = $self->new_image($slice, $image_config, $object->highlights);
   
 	return if $self->_export_image($image);
@@ -90,8 +54,39 @@ sub content {
   $image->imagemap         = 'yes';
   $image->set_button('drag', 'title' => 'Click or drag to centre display');
   
-  return $info.$image->render;
+  return $info . $image->render;
 }
 
+sub _add_object_track {
+  my ($self, $image_config) = @_;
+  my $hub     = $self->hub;
+  my $session = $hub->session;
+  my $extra;
+  
+  # Add track for gene if not on by default
+  if (my $gene = $hub->core_objects->{'gene'}) {
+    my $key  = $image_config->get_track_key('transcript', $gene);
+    my $node = $image_config->get_node(lc $key);
+ 
+    if ($node) {
+      my $current = $node->get('display');
+      my $default = $node->data->{'display'};
+      
+      if ($current eq 'off' && $default eq 'off') {
+        my $flag = $session->get_data(type => 'auto_add', code => lc $key);
+        
+        if (!$flag->{'data'}) { # haven't done this before
+          $image_config->update_track_renderer(lc $key, 'transcript_label');
+          $session->set_data(type => 'auto_add' , code => lc $key, data => 1); 
+          $session->store;
+          
+          $extra = $self->_info('Information', '<p>The track containing the highlighted gene has been added to your display.</p>');
+        }
+      }
+    }
+  }
+  
+  return $extra;
+}
 
 1;
