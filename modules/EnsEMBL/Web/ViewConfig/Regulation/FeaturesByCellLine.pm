0e432c5dd0e3c1df92dc2be876df0f5eeca8a586
Simon Brent <sb23@sanger.ac.uk>
2013-09-20 14:31:03 +0000

    not a child of Cell_line any more

--- modules/EnsEMBL/Web/ViewConfig/Regulation/FeaturesByCellLine.pm
+++ modules/EnsEMBL/Web/ViewConfig/Regulation/FeaturesByCellLine.pm
@@ -4,21 +4,19 @@ package EnsEMBL::Web::ViewConfig::Regulation::FeaturesByCellLine;
 
 use strict;
 
-use base qw(EnsEMBL::Web::ViewConfig::Cell_line);
+use base qw(EnsEMBL::Web::ViewConfig);
 
 sub init {
   my $self = shift;
-  
-  $self->title         = 'Details by cell line';
-  $self->{'form_func'} = 'form_context';
-  
+  $self->title = 'Details by cell line';
   $self->add_image_config('regulation_view');
-  $self->SUPER::init;
   $self->set_defaults({ opt_highlight => 'yes', context => 200 });
 }
 
-sub form_context {
+sub form {
   my $self = shift;
+  
+  $self->add_fieldset('Display options');
   
   $self->add_form_element({
     type   => 'DropDown',
@@ -26,17 +24,17 @@ sub form_context {
     name   => 'context',
     label  => 'Context',
     values => [
-      { value => '20',   name => '20bp'   },
-      { value => '50',   name => '50bp'   },
-      { value => '100',  name => '100bp'  },
-      { value => '200',  name => '200bp'  },
-      { value => '500',  name => '500bp'  },
-      { value => '1000', name => '1000bp' },
-      { value => '2000', name => '2000bp' },
-      { value => '5000', name => '5000bp' }
+      { value => 20,   name => '20bp'   },
+      { value => 50,   name => '50bp'   },
+      { value => 100,  name => '100bp'  },
+      { value => 200,  name => '200bp'  },
+      { value => 500,  name => '500bp'  },
+      { value => 1000, name => '1000bp' },
+      { value => 2000, name => '2000bp' },
+      { value => 5000, name => '5000bp' }
     ]
   });
-
+  
   $self->add_form_element({ type => 'YesNo', name => 'opt_highlight', select => 'select', label => 'Highlight core region' });
 }
 
