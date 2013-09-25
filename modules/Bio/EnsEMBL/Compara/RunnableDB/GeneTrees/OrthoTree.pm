82b0fb0eb296d61245d099c0a4de94ff9f2074ec
Matthieu Muffato <muffato@ebi.ac.uk>
2013-09-25 10:07:09 +0100

    More genome_dbs than usual: the key must be computed accordingly

--- modules/Bio/EnsEMBL/Compara/RunnableDB/GeneTrees/OrthoTree.pm
+++ modules/Bio/EnsEMBL/Compara/RunnableDB/GeneTrees/OrthoTree.pm
@@ -135,7 +135,7 @@ sub fetch_input {
         my $gdbs = $mlss->species_set_obj->genome_dbs;
         # This should be unique to a pair of genome_db_ids, and equal if we switch the two genome_dbs
         # WARNING: This trick is valid for genome_db_id up to 10000
-        my $mlss_key = sprintf("%s_%d", $mlss_type, $gdbs->[0]->dbID * $gdbs->[-1]->dbID + 100000000*($gdbs->[0]->dbID + $gdbs->[-1]->dbID));
+        my $mlss_key = sprintf("%s_%d", $mlss_type, $gdbs->[0]->dbID * $gdbs->[-1]->dbID + 100000*100000*($gdbs->[0]->dbID + $gdbs->[-1]->dbID));
         $mlss_hash{$mlss_key} = $mlss;
     }
 }
@@ -864,7 +864,7 @@ sub store_gene_link_as_homology {
       }
     # This should be unique to a pair of genome_db_ids, and equal if we switch the two genome_dbs
     # WARNING: This trick is valid for genome_db_id up to 10000
-      my $mlss_key = sprintf("%s_%d", $mlss_type, $gene1->genome_db->dbID * $gene2->genome_db->dbID + 100000000*($gene1->genome_db->dbID+$gene2->genome_db->dbID));
+      my $mlss_key = sprintf("%s_%d", $mlss_type, $gene1->genome_db->dbID * $gene2->genome_db->dbID + 100000*100000*($gene1->genome_db->dbID+$gene2->genome_db->dbID));
 
       if (exists $self->param('mlss_hash')->{$mlss_key}) {
           $mlss = $self->param('mlss_hash')->{$mlss_key};
@@ -879,6 +879,7 @@ sub store_gene_link_as_homology {
                   -method => ($self->compara_dba->get_MethodAdaptor->fetch_by_type($mlss_type) || new Bio::EnsEMBL::Compara::Method(-type => $mlss_type, -class => 'Homology.homology')),
                   -species_set_obj => ($self->compara_dba->get_SpeciesSetAdaptor->fetch_by_GenomeDBs($gdbs) || new Bio::EnsEMBL::Compara::SpeciesSet(-genome_dbs => $gdbs)),
                   );
+          $self->warning("Create new for key $mlss_key: $mlss_type, ". $gene1->genome_db->dbID . " " . $gene2->genome_db->dbID);
           $self->compara_dba->get_MethodLinkSpeciesSetAdaptor->store($mlss) unless ($self->param('_readonly'));
           $self->param('mlss_hash')->{$mlss_key} = $mlss;
       }
