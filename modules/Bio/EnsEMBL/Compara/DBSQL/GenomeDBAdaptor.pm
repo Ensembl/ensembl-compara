10f0cc87529bbaf919fc0e071a27dd161e077751
Matthieu Muffato <muffato@ebi.ac.uk>
2013-10-03 23:43:35 +0000

    assembly_default should be reset in the objects as well

--- modules/Bio/EnsEMBL/Compara/DBSQL/GenomeDBAdaptor.pm
+++ modules/Bio/EnsEMBL/Compara/DBSQL/GenomeDBAdaptor.pm
@@ -345,6 +345,10 @@ sub store {
         my $sth = $self->prepare('UPDATE genome_db SET assembly_default = 0 WHERE name = ? AND genome_db_id != ?');
         my $nrows = $sth->execute($gdb->name, $gdb->dbID);
         $sth->finish();
+        # in the cache as well
+        foreach my $other_gdb (@{$self->fetch_all}) {
+            $other_gdb->assembly_default($other_gdb->dbID == $gdb->dbID ? 1 : 0) if $other_gdb->name eq $gdb->name;
+        }
     }
 
     #make sure the id_cache has been fully populated
