4111de21aa750929b58e24b5705288ec1be46366
Sarah Hunt <seh@ebi.ac.uk>
2013-10-04 09:12:09 +0000

    Add UCSC link for human

--- modules/EnsEMBL/Web/Component/Variation/Publication.pm
+++ modules/EnsEMBL/Web/Component/Variation/Publication.pm
@@ -19,23 +19,27 @@ sub content {
   
   my $data = $object->get_citation_data;
   
-  return $self->_info('We do not have any citation data for this variation') unless scalar @$data;
+  return $self->_info('No citation data is available') unless scalar @$data;
   
+  my $html = ('<b>' . $object->name() .' is mentioned in the following publications</b>'); 
 
   my ($table_rows ) = $self->table_data($data);
   my $table         = $self->new_table([], [], { data_table => 1 });
    
 
   $table->add_columns(   
-    { key => 'pmid', title => 'PMID', align => 'left', sort => 'html' },  
-    { key => 'title', title => 'Title', align => 'left', sort => 'html' },  
-    { key => 'author',  title => 'Author(s)',     align => 'left', sort => 'html' },
-    { key => 'text', title => 'Full text', align => 'left', sort => 'html' },  
-    { key => 'year', title => 'Year', align => 'left', sort => 'html' },  
+    { key => 'pmid',   title => 'PMID',      align => 'left', sort => 'html' },  
+    { key => 'title',  title => 'Title',     align => 'left', sort => 'html' },  
+    { key => 'author', title => 'Author(s)', align => 'left', sort => 'html' },
+    { key => 'text',   title => 'Full text', align => 'left', sort => 'html' },  
+    { key => 'year',   title => 'Year',      align => 'left', sort => 'html' },  
   );
-  
+
+  $table->add_columns( { key => 'ucsc',   title => 'UCSC', align => 'left', sort => 'html' }) if $self->hub->species eq 'Homo_sapiens';
   foreach my $row (@{$table_rows}){  $table->add_rows($row);}
-  return $table->render;
+
+  $html .=  $table->render;
+  return $html;
 };
 
 
@@ -45,23 +49,24 @@ sub table_data {
   my $hub        = $self->hub;
   my $object     = $self->object;
 
-  my $epmc_url = 'http://europepmc.org/articles/';
-  my $pm_url   = 'http://www.ncbi.nlm.nih.gov/pubmed/';
+  my $ucsc_url = 'http://genome.ucsc.edu/cgi-bin/hgc?r=0&l=0&c=0&o=-1&t=0&g=pubsMarkerSnp&i='; ## TESTURL
 
   my @data_rows;
                  
                  
   foreach my $cit (@$citation_data) { 
+      
+      my $row = {
+	  year    => $cit->year(),
+	  pmid    => defined $cit->pmid() ? $hub->get_ExtURL_link($cit->pmid(), "PUBMED", $cit->pmid()) : undef,
+	  title   => $cit->title(),
+	  author  => $cit->authors(),
+	  text    => defined $cit->pmcid() ? $hub->get_ExtURL_link($cit->pmcid(), "EPMC", $cit->pmcid()) : undef,
+	  ucsc    => defined $cit->ucsc_id() ? "<a href=\"" . $ucsc_url . $object->name() ."&pubsFilterExtId=". $cit->ucsc_id() . "\">View</a>" : undef 
 
-    my $row = {
-      year    => $cit->year(),
-      pmid    => defined $cit->pmid() ? "<a href=\"" . $pm_url . $cit->pmid() . "\">" .$cit->pmid() . "</a>" : undef,
-      title   => $cit->title(),
-      author  => $cit->authors(),
-      text    => defined $cit->pmcid() ? "<a href=\"" . $epmc_url . $cit->pmcid() . "\">" . $cit->pmcid() . "</a>" : undef,
+     
     };
   
-    
     push @data_rows, $row;
 
   } 
