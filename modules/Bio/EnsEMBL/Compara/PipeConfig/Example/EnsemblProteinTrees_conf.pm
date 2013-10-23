a5c3a1b02a00f849d68b2f363c6be87310a2a118
Matthieu Muffato <muffato@ebi.ac.uk>
2013-10-23 16:52:36 +0000

    reuse parameters for e74

--- modules/Bio/EnsEMBL/Compara/PipeConfig/Example/EnsemblProteinTrees_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/Example/EnsemblProteinTrees_conf.pm
@@ -165,7 +165,7 @@ sub default_options {
         #'prev_core_sources_locs'   => [ $self->o('staging_loc1'), $self->o('staging_loc2') ],
 
         # Add the database location of the previous Compara release. Use "undef" if running the pipeline without reuse
-        'prev_rel_db' => 'mysql://ensro@compara2:3306/lg4_compara_homology_73'
+        'prev_rel_db' => 'mysql://ensro@compara2:3306/lg4_ensembl_compara_73'
 
         # To run without a master database
         #'master_db'                 => undef,
