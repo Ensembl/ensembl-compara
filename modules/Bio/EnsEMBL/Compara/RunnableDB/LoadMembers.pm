a177408c5dd6b96ff8fa21734192ccc7ddce2183
Matthieu Muffato <muffato@ebi.ac.uk>
2013-10-23 16:52:33 +0000

    Warns if a gene could not be stored

--- modules/Bio/EnsEMBL/Compara/RunnableDB/LoadMembers.pm
+++ modules/Bio/EnsEMBL/Compara/RunnableDB/LoadMembers.pm
@@ -339,6 +339,8 @@ sub store_gene_and_all_transcripts {
     $seq_member_adaptor->_set_member_as_canonical($canonicalPeptideMember);
     # print("     LONGEST " . $canonicalPeptideMember->stable_id . "\n");
   }
+
+  $self->warning($gene->stable_id." is not stored") if $gene_member_not_stored;
   return 1;
 }
 
