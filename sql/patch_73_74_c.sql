250aec474d5575cfb18eb9acb6f437c2d9084a23
Miguel Pignatelli <mp@ebi.ac.uk>
2013-10-28 13:39:46 +0000

    Added final ;

--- sql/patch_73_74_c.sql
+++ sql/patch_73_74_c.sql
@@ -19,9 +19,8 @@ ALTER TABLE species_tree_root DROP COLUMN `pvalue_lim`;
 
 ALTER TABLE species_tree_node ADD COLUMN `taxon_id` int(10) unsigned DEFAULT NULL, ADD COLUMN `genome_db_id` int(10) unsigned DEFAULT NULL, ADD COLUMN `node_name` varchar(255) DEFAULT NULL;
 UPDATE species_tree_node JOIN CAFE_species_gene USING(node_id) SET species_tree_node.taxon_id = CAFE_species_gene.taxon_id;
-ALTER TABLE CAFE_species_gene DROP COLUMN taxon_id
+ALTER TABLE CAFE_species_gene DROP COLUMN taxon_id;
 UPDATE species_tree_node JOIN genome_db using(taxon_id) SET species_tree_node.genome_db_id=genome_db.genome_db_id;
-
 
 INSERT INTO meta (species_id, meta_key, meta_value)
   VALUES (NULL, 'patch', 'patch_73_74_c.sql|species_tree');
