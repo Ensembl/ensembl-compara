26bc6b3c46c949b84bc2e08002c49f1275f3737f
Harpreet Riat <hr5@sanger.ac.uk>
2013-10-28 11:17:35 +0000

    this package provides method to inspect the loaded plugins

---
+++ modules/EnsEMBL/Web/Tools/PluginInspector.pm
@@ -0,0 +1,21 @@
+package EnsEMBL::Web::PluginInspector;
+
+## TODO
+
+use strict;
+use warnings;
+
+use Exporter qw(import);
+our @EXPORT_OK = qw(get_all_plugins get_file_plugins previous_plugin next_plugin); # And more?
+
+use SiteDefs;
+
+sub get_all_plugins {}
+
+sub get_file_plugins {}
+
+sub previous_plugin {}
+
+sub next_plugin {}
+
+1;
