04a5d0426cf7cc8ee202f3070091bc2f9f29561a
Miguel Pignatelli <mp@ebi.ac.uk>
2013-10-28 13:48:56 +0000

    bugfix -- removed dup in addition of species_tree_node_id in the homology table

--- sql/patch_73_74_f.sql
+++ sql/patch_73_74_f.sql
@@ -19,7 +19,7 @@ ALTER TABLE homology
 INSERT INTO species_tree_node SELECT 500000000+taxon_id, 500000000+parent_id, 500000000+root_id, left_index, right_index, NULL, taxon_id, NULL, name FROM ncbi_taxa_node JOIN ncbi_taxa_name USING (taxon_id) WHERE name_class = "scientific name";
 UPDATE species_tree_node stn JOIN genome_db gdb USING (taxon_id) SET stn.genome_db_id = gdb.genome_db_id;
 
-ALTER TABLE homology ADD species_tree_node_id INT UNSIGNED DEFAULT NULL AFTER lnl;
+## ALTER TABLE homology ADD species_tree_node_id INT UNSIGNED DEFAULT NULL AFTER lnl;
 UPDATE homology JOIN gene_tree_node_attr ON node_id = ancestor_node_id SET species_tree_node_id = 500000000+taxon_id;
 ALTER TABLE homology DROP COLUMN subtype;
 
