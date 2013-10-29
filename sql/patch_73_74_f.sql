9fd9a4d4fc9fbf5f24fcb37963eea498004cd0d1
Miguel Pignatelli <mp@ebi.ac.uk>
2013-10-29 11:34:16 +0000

    Added patch for foreign keys in the homology table

--- sql/patch_73_74_f.sql
+++ sql/patch_73_74_f.sql
@@ -11,10 +11,14 @@
 SET session sql_mode='TRADITIONAL';
 
 ALTER TABLE homology
+  DROP KEY `ancestor_node_id`,
+  DROP KEY `tree_node_id`,
   CHANGE COLUMN ancestor_node_id gene_tree_node_id INT(10) UNSIGNED,
   CHANGE COLUMN tree_node_id     gene_tree_root_id INT(10) UNSIGNED,
-  ADD COLUMN species_tree_node_id INT(10) UNSIGNED AFTER lnl;
-
+  ADD COLUMN species_tree_node_id INT(10) UNSIGNED AFTER lnl,
+  ADD FOREIGN KEY (species_tree_node_id) REFERENCES species_tree_node(node_id),
+  ADD FOREIGN KEY (gene_tree_node_id) REFERENCES gene_tree_node(node_id),
+  ADD FOREIGN KEY (gene_tree_root_id) REFERENCES gene_tree_root(root_id);
 
 INSERT INTO species_tree_node SELECT 500000000+taxon_id, 500000000+parent_id, 500000000+root_id, left_index, right_index, NULL, taxon_id, NULL, name FROM ncbi_taxa_node JOIN ncbi_taxa_name USING (taxon_id) WHERE name_class = "scientific name";
 UPDATE species_tree_node stn JOIN genome_db gdb USING (taxon_id) SET stn.genome_db_id = gdb.genome_db_id;
