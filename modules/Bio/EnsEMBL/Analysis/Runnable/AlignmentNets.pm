9288d97e05f7a094db4c481e3bd2026afc1107f0
Rishi Nag <rn6@sanger.ac.uk>
2013-11-01 11:04:33 +0000

    source/revision cvs tag removal

--- modules/Bio/EnsEMBL/Analysis/Runnable/AlignmentNets.pm
+++ modules/Bio/EnsEMBL/Analysis/Runnable/AlignmentNets.pm
@@ -41,8 +41,6 @@ Internal methods are usually preceded with a _
 
 # Let the code begin...
 
-# $Source: /tmp/ENSCOPY-ENSEMBL-ANALYSIS/modules/Bio/EnsEMBL/Analysis/Runnable/AlignmentNets.pm,v $
-# $Revision: 1.19 $
 package Bio::EnsEMBL::Analysis::Runnable::AlignmentNets;
 
 use warnings ;
