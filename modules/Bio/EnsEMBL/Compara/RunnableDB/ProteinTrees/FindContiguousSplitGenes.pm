c556a02181fa8556fad99e20e6bbda303a17f922
Matthieu Muffato <muffato@ebi.ac.uk>
2013-11-05 16:03:42 +0000

    Reverted to the e73 code

--- modules/Bio/EnsEMBL/Compara/RunnableDB/ProteinTrees/FindContiguousSplitGenes.pm
+++ modules/Bio/EnsEMBL/Compara/RunnableDB/ProteinTrees/FindContiguousSplitGenes.pm
@@ -157,18 +157,25 @@ sub check_for_split_genes {
         my ($protein1, $protein2) = @$genepairlink;
 
         # Compute the sequence overlap
-        my $aln = 0;
-        my $len1 = 0;
-        my @aln1 = split(//, $protein1->alignment_string);
-        my $len2 = 0;
-        my @aln2 = split(//, $protein2->alignment_string);
+        #my $aln = 0;
+        #my $len1 = 0;
+        #my @aln1 = split(//, $protein1->alignment_string);
+        #my $len2 = 0;
+        #my @aln2 = split(//, $protein2->alignment_string);
 
-        for (my $i=0; $i <= $#aln1; $i++) {
-            $len1++ if ($aln1[$i] ne '-');
-            $len2++ if ($aln2[$i] ne '-');
-            $aln++ if ($aln1[$i] ne '-' && $aln2[$i] ne '-');
-        }
+        #for (my $i=0; $i <= $#aln1; $i++) {
+        #    $len1++ if ($aln1[$i] ne '-');
+        #    $len2++ if ($aln2[$i] ne '-');
+        #    $aln++ if ($aln1[$i] ne '-' && $aln2[$i] ne '-');
+        #}
  
+        #printf("Pair: %s-%s: %d out of %d-%d\n", $protein1->stable_id, $protein2->stable_id, $aln, $len1, $len2) if ($self->debug);
+        my $pair = new Bio::EnsEMBL::Compara::AlignedMemberSet;
+        my $protein1_copy = $protein1->Bio::EnsEMBL::Compara::AlignedMember::copy;
+        my $protein2_copy = $protein2->Bio::EnsEMBL::Compara::AlignedMember::copy;
+        $pair->add_Member($protein1_copy);
+        $pair->add_Member($protein2_copy);
+        $pair->update_alignment_stats;
         print "Pair: ", $protein1->stable_id, " - ", $protein2->stable_id, "\n" if ($self->debug);
 
         my $gene_member1 = $protein1->gene_member; my $gene_member2 = $protein2->gene_member;
@@ -178,8 +185,10 @@ sub check_for_split_genes {
         my $taxon_id1 = $gene_member1->taxon_id; my $taxon_id2 = $gene_member2->taxon_id;
         my $name1 = $gene_member1->chr_name; my $name2 = $gene_member2->chr_name;
 
+        printf("%%Id: %d/%d, %%Pos: %d/%d\n", $protein1_copy->perc_id, $protein2_copy->perc_id, $protein1_copy->perc_pos, $protein2_copy->perc_pos);
         # Checking for gene_split cases
-        if ($aln == 0) {
+        #if ($aln == 0)
+        if (0 == $protein1_copy->perc_id && 0 == $protein2_copy->perc_id && 0 == $protein1_copy->perc_pos && 0 == $protein2_copy->perc_pos) {
 
             # Condition A1: If same seq region and less than 1MB distance
             if ($name1 eq $name2 && ($self->param('max_dist_no_overlap') > abs($start1 - $start2)) && $strand1 eq $strand2 ) {
@@ -215,8 +224,10 @@ sub check_for_split_genes {
         # "skid marks" in the alignment view.
 
         # Condition B1: all 4 percents below 10
-        } elsif (100*$aln < $len1*$self->param('small_overlap_percentage')
-                && 100*$aln < $len2*$self->param('small_overlap_percentage')) {
+        #} elsif (100*$aln < $len1*$self->param('small_overlap_percentage')
+        #        && 100*$aln < $len2*$self->param('small_overlap_percentage')) {
+        } elsif ($protein1_copy->perc_id < $self->param('small_overlap_percentage') && $protein2_copy->perc_id < $self->param('small_overlap_percentage')
+              && $protein1_copy->perc_pos < $self->param('small_overlap_percentage') && $protein2_copy->perc_pos < $self->param('small_overlap_percentage')) {
 
             # Condition B2: If non-overlapping and smaller than 500kb start and 500kb end distance
             if ($name1 eq $name2 
