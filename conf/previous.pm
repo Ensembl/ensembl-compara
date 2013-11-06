b24098f3f90ba6e990cdd9efb99976b960b9a7a7
Harpreet Riat <hr5@sanger.ac.uk>
2013-11-06 13:05:39 +0000

    some documentation

--- conf/previous.pm
+++ conf/previous.pm
@@ -1,8 +1,9 @@
 package previous;
 
-### Provides the PREV psudo-class for calling a method from the
-### core web code (or a previous plugin) from  inside a plugin that
-### is overrdding the method
+### Provides the PREV psudo-class syntax for calling a method,
+### a class function or a sub-routine from the core web code (or
+### a previous plugin) from  inside a plugin that is overrdding
+### the method/sub-routine.
 ###
 ### Usage:
 ###
@@ -11,15 +12,32 @@ package previous;
 ### use previous qw(new);
 ### sub new {
 ###   my $self = shift->PREV::new(@_);
-###   $self->do_something;
+###   // do something to the object before returning it
 ###   return $self;
 ### }
 ###
-### If PREV::method is called on an object in a file (possibly
-### not the object's class file itself) inside a plugin, it  will
-### transfer the call to a method with the given name that is
-### found first declared in the list of previous plugins or the
-### core web code in the same class or any of it's parents.
+### Just like SUPER, PREV is resolved from the package where the
+### call is made. It is not resolved based on the object's class.
+###
+### Unlike SUPER, PREV can also be used on a class function or
+### a sub-routine, and usage is like below:
+###
+### package EnsEMBL::Web::SomeUtility; # inside plugins
+### use strict;
+### use previous qw(my_sub);
+### sub my_sub {
+###   PREV::my_sub();
+###   // do something else
+###   return $self;
+### }
+### sub my_sub {
+###   // override the old some_sub here
+### }
+###
+### The call to the method gets transfered to a method with the
+### given name that is found first declared in the list of
+### previous plugins or the core web code in the same class or
+### any of it's parents.
 ###
 
 use strict;
@@ -28,10 +46,9 @@ use warnings;
 use Digest::MD5 qw(md5_hex);
 
 sub import {
-  my ($self, @methods) = @_;
-  my @caller = caller;
+  my $self = shift;
 
-  if (@methods) {
+  if (@_) {
 
     # For all the methods that are intended to be used
     # with PREV keyword, we need to make a copy of them so
@@ -39,24 +56,27 @@ sub import {
     # which will be declared further in the plugin file (the
     # one that is calling 'use previous qw(methods)').
     # Since we need to remember these old methods' names to
-    # call them in future, each method is renamed by giving
-    # it a prefix that corresponds to the plugin it's being
+    # call them in future, the copied method is given
+    # a prefix that corresponds to the plugin it's being
     # modified in. Thus all the methods in all the packages
     # that are being renamed in one plugin will have a same
     # prefix, but different from methods being renamed in
     # some other plugin.
 
-    my $target_package = $caller[0];
-    my $method_prefix  = _method_prefix(@caller);
-    
+    my @caller          = caller;
+    my $target_package  = $caller[0];
+    my $method_prefix   = _method_prefix(@caller);
+
     no strict qw(refs);
-    
-    for (@methods) {
-    
-      # If the method being renamed is not defined in the package,
-      # we won't create an empty method to avoid causing any other
-      # problems.
-    
+
+    for (@_) {
+
+      # If the requesting class, or one of its parents has the
+      # method defined in their packages, it gets copied with
+      # the prefix to the requesting class. If none of them has
+      # the method defined, an empty method won't get created.
+      # This is to avoid causing any other problems.
+
       if (my $coderef = $target_package->can($_)) {
         *{"${target_package}::${method_prefix}_$_"} = $coderef;
       } else {
