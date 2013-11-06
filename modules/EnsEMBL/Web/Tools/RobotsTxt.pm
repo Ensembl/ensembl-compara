91214fe7eae02f990aaa3583417045b1499cc689
Web Group <www-ens@sanger.ac.uk>
2013-11-06 15:27:05 +0000

    Nutch, and others I assume, doesn't see the Allow syntax. Therefore extend the Ensembl approach of explicitly disallowing pages to all sites (eg Vega / Pre)

--- modules/EnsEMBL/Web/Tools/RobotsTxt.pm
+++ modules/EnsEMBL/Web/Tools/RobotsTxt.pm
@@ -24,7 +24,7 @@ sub create {
   my $sd      = shift;
   my $root    = $sd->ENSEMBL_SERVERROOT . '/htdocs';
   my @allowed = @{$sd->ENSEMBL_EXTERNAL_SEARCHABLE||[]};
-  
+
   #check if directory for creating .cvsignore and robots.txt exist
   make_path($root) unless(-e $root);
 
@@ -47,57 +47,60 @@ sub create {
     warn _box("UNABLE TO CREATE ROBOTS.TXT FILE IN $root/");
     return;
   }
-  if(-e "$server_root/htdocs/sitemaps/sitemap-index.xml") {
-    # If we have a sitemap we need a less-restrictive robots.txt, so
-    # that the crawler can use the sitemap.
-    warn _box("Creating robots.txt for google sitemap");    
 
-    print FH _lines("User-agent","*");
-    print FH _lines("Disallow",qw(
-      /Multi/  /biomart/  /Account/  /ExternalData/  /UserAnnotation/
-      */Ajax/  */Config/  */blastview/  */Export/  */Experiment/  
-      */Location/  */LRG/  */Phenotype/  */Regulation/  */Search/
-      */UserConfig/  */UserData/  */Variation/
-    ));
+  print FH _lines("User-agent","*");
+  print FH _lines("Disallow",qw(
+				 /Multi/  /biomart/  /Account/  /ExternalData/  /UserAnnotation/
+				 */Ajax/  */Config/  */blastview/  */Export/  */Experiment/ */Experiment*
+				 */Location/  */LRG/  */Phenotype/  */Regulation/  */Search/ */Share
+				 */UserConfig/  */UserData/  */Variation/
+			      ));
 
-    #old views
-    print FH _lines("Disallow",qw(*/*view));
+  #old views
+  print FH _lines("Disallow",qw(*/*view));
 
-    #other misc views google bot hits
-    print FH _lines("Disallow",qw(/id/));
-    print FH _lines("Disallow",qw(/*/psychic));
+  #other misc views google bot hits
+  print FH _lines("Disallow",qw(/id/));
+  print FH _lines("Disallow",qw(/*/psychic));
 
-    foreach my $row (('A'..'Z','a'..'z')){
-      next if lc $row eq 's';
-      print FH _lines("Disallow","*/Gene/$row*","*/Transcript/$row*");
-    }
-    
-    print FH _lines("Sitemap","http://www.ensembl.org/sitemap-index.xml");
-  } else {
-    # No sitemap, use old, restrictive robots.txt.
-    if( @allowed ) {      
-      print FH _lines("User-agent","*");
-      print FH _lines("Disallow",qw(/Multi/  /biomart/));
-      foreach my $sp ( @{$species||[]} ) {
-        print FH _lines("Disallow","/$sp/");
-        print FH _lines("Allow",map { "/$sp/$_" } @allowed);
-      }
-    } else {
-      ## Allowed list is empty so we only allow access to the main
-      ## index page... /index.html...
-      print FH _lines("User-agent","*");
-      print FH _lines("Disallow","/");
-    }
-    print FH _lines("User-agent","W3C-checklink");
-    print FH _lines("Allow","/info");
+  foreach my $row (('A'..'Z','a'..'z')){
+    next if lc $row eq 's';
+    print FH _lines("Disallow","*/Gene/$row*","*/Transcript/$row*");
   }
-  # stop Nutch indexing Doxygen pages
-  print FH _lines("User-agent","Sanger Search Bot/Nutch-1.1 (Nutch Spider; http://www.sanger.ac.uk; webmaster at sanger dot ac dot uk)");
-  print FH _lines("Disallow",qw(/info/docs/Doxygen));
-  print FH _lines("Allow",qw(/info/* /index.html /info/docs/Doxygen/index.html));
+
+  # a bunch of others that are being bypassed
+  foreach my $row (qw(SpeciesTree Similarity SupportingEvidence Sequence_Protein Sequence_cDNA Sequence StructuralVariation_Gene Splice)) {
+    print FH _lines("Disallow","*/Gene/$row*","*/Transcript/$row*");
+  }
+
+  # links from ChEMBL
+  print FH _lines("Disallow","/Gene/Summary");
+  print FH _lines("Disallow"," /Transcript/Summary");
+
+  # Doxygen
+  print FH _lines("Disallow","/info/docs/Doxygen");
+
+  if (-e "$server_root/htdocs/sitemaps/sitemap-index.xml") {
+    print FH _lines("Sitemap","http://www.ensembl.org/sitemap-index.xml");
+  }
+
+  print FH _lines("User-agent","W3C-checklink");
+  print FH _lines("Allow","/info");
+
   # Limit Blekkobot's crawl rate to only one page every 20 seconds.
   print FH _lines("User-agent","Blekkobot");
   print FH _lines("Crawl-delay","20");
+
+  # stop AhrefsBot indexing us (https://ahrefs.com/robot/)
+  print FH _lines("User-agent","AhrefsBot");
+  print FH _lines("Disallow","/");
+
+  if(-e "$server_root/htdocs/sitemaps/sitemap-index.xml") {
+    # If we have a sitemap let google know about it.
+    warn _box("Creating robots.txt for google sitemap");
+    print FH _lines("Sitemap","http://www.ensembl.org/sitemap-index.xml");
+  }
+
   close FH;
   return;
 }
