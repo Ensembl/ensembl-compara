7fa6139b1801a60a29b0554f2026ddaf00178426
Anne Parker <ap5@sanger.ac.uk>
2013-11-22 13:25:05 +0000

    New component to display large R2R output file

---
+++ modules/EnsEMBL/Web/Component/Gene/RnaSecondaryStructure.pm
@@ -0,0 +1,50 @@
+# $Id$
+
+package EnsEMBL::Web::Component::Gene::RnaSecondaryStructure;
+
+use strict;
+
+use base qw(EnsEMBL::Web::Component::Gene);
+
+sub _init {
+  my $self = shift;
+  $self->cacheable(0);
+  $self->ajaxable(1);
+}
+
+sub content {
+  my $self         = shift;
+  my $hub          = $self->hub;
+  my $object       = $self->object;
+  my $species_defs = $hub->species_defs;
+  my $html;
+
+  my ($display_name) = $object->display_xref;
+
+  my $svg_path = $self->draw_structure($display_name);
+  if ($svg_path) {
+    $html = qq(<object data="$svg_path" type="image/svg+xml"></object>);
+  }
+
+  return $html;
+}
+
+
+sub get_synonyms {
+  my ($self, $match_id, @matches) = @_;
+  my ($ids, $syns);
+  foreach my $m (@matches) {
+    my $dbname = $m->db_display_name;
+    my $disp_id = $m->display_id;
+    if ($dbname =~/(HGNC|ZFIN)/ && $disp_id eq $match_id) {
+      my $synonyms = $m->get_all_synonyms;
+      $ids = '';
+      $ids = $ids . ', ' . (ref $_ eq 'ARRAY' ? "@$_" : $_) for @$synonyms;
+    }
+  }
+  $ids  =~ s/^\,\s*//;
+  $syns = $ids if $ids =~ /^\w/;
+  return $syns;
+}
+
+1;
