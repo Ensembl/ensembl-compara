772472cd4fdcb0176445c36e057812ce3fd07774
Anne Parker <ap5@sanger.ac.uk>
2013-11-25 13:34:20 +0000

    Added legend image

--- modules/EnsEMBL/Web/Component/Gene/RnaSecondaryStructure.pm
+++ modules/EnsEMBL/Web/Component/Gene/RnaSecondaryStructure.pm
@@ -19,32 +19,16 @@ sub content {
   my $species_defs = $hub->species_defs;
   my $html;
 
+  $html .= '<h4>Key</h4><img src="/img/r2r_legend.png" />';
+
   my ($display_name) = $object->display_xref;
 
   my $svg_path = $self->draw_structure($display_name);
   if ($svg_path) {
-    $html = qq(<object data="$svg_path" type="image/svg+xml"></object>);
+    $html .= qq(<object data="$svg_path" type="image/svg+xml"></object>);
   }
 
   return $html;
-}
-
-
-sub get_synonyms {
-  my ($self, $match_id, @matches) = @_;
-  my ($ids, $syns);
-  foreach my $m (@matches) {
-    my $dbname = $m->db_display_name;
-    my $disp_id = $m->display_id;
-    if ($dbname =~/(HGNC|ZFIN)/ && $disp_id eq $match_id) {
-      my $synonyms = $m->get_all_synonyms;
-      $ids = '';
-      $ids = $ids . ', ' . (ref $_ eq 'ARRAY' ? "@$_" : $_) for @$synonyms;
-    }
-  }
-  $ids  =~ s/^\,\s*//;
-  $syns = $ids if $ids =~ /^\w/;
-  return $syns;
 }
 
 1;
