5952ec1f3549fc267f29c377dce3dc3ecf79e60b
Simon Brent <sb23@sanger.ac.uk>
2013-11-27 13:42:00 +0000

    SO OLD

--- utils/indexing/dumpsnps.bash
+++
@@ -1,4 +0,0 @@
-/mysqld/current/bin/mysql -S /mysqld/ecs3d_3307/ecs3d_3307.sock -uensro homo_sapiens_variation_42_36d -e 'select variation_id,source_id,name from variation order by variation_id' > hsv.txt
-/mysqld/current/bin/mysql -S /mysqld/ecs3d_3307/ecs3d_3307.sock -uensro homo_sapiens_variation_42_36d -e 'select variation_id,source_id,name from variation_synonym order by variation_id' > hsvs.txt
-/mysqld/current/bin/mysql -S /mysqld/ecs3d_3307/ecs3d_3307.sock -uensro mus_musculus_variation_42_36c -e 'select variation_id,source_id,name from variation order by variation_id' > msv.txt
-/mysqld/current/bin/mysql -S /mysqld/ecs3d_3307/ecs3d_3307.sock -uensro mus_musculus_variation_42_36c -e 'select variation_id,source_id,name from variation_synonym order by variation_id' > msvs.txt
