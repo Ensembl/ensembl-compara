7487aee1e5f92512e40b1a5ccd5e0803f0ace83a
Matthieu Muffato <muffato@ebi.ac.uk>
2013-12-03 12:28:45 +0000

    Update Jalview to the latest version

--- modules/EnsEMBL/Web/ZMenu/Jalview.pm
+++ modules/EnsEMBL/Web/ZMenu/Jalview.pm
@@ -31,11 +31,18 @@ sub content {
   my $hub      = $self->hub;
   my $url_site = $hub->species_defs->ENSEMBL_BASE_URL;
   my $html     = sprintf(
-    '<applet code="jalview.bin.JalviewLite" width="140" height="35" archive="%s/jalview/jalviewAppletOld.jar">
-      <param name="file" value="%s">
-      <param name="treeFile" value="%s">
-      <param name="sortByTree" value="true">
-      <param name="defaultColour" value="clustal">
+    '<applet code="jalview.bin.JalviewLite" width="140" height="35" archive="%s/jalview/jalviewApplet.jar">
+
+      <param name="file" value="%s" />
+      <param name="treeFile" value="%s" />
+
+      <param name="sortByTree" value="true" />
+      <param name="showFullId" value="false" />
+      <param name="defaultColour" value="clustal" />
+      <param name="showSequenceLogo" value="true" />
+      <param name="showGroupConsensus" value="true" />
+
+      <param name="application_url" value="http://www.jalview.org/services/launchApp" />
     </applet>', 
     $url_site, 
     $url_site . uri_unescape($hub->param('file')),
