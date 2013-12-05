d8fac9137a2f0adda4a9728222deee628512ae4c
Anne Parker <ap5@sanger.ac.uk>
2013-12-05 10:58:12 +0000

    Revised version from ayates - fixed to be compatible with new seqdumper utility

--- modules/EnsEMBL/Web/SeqDumper.pm
+++ modules/EnsEMBL/Web/SeqDumper.pm
@@ -20,54 +20,35 @@ package EnsEMBL::Web::SeqDumper;
 
 use strict;
 use Bio::EnsEMBL::Utils::SeqDumper;
+use IO::String;
 
 our @ISA = qw(Bio::EnsEMBL::Utils::SeqDumper);
 
 =head2 EnsEMBL::Web::SeqDumper
 
 This package is an extension of the Bio::EnsEMBL::Utils::SeqDumper
-written so that it can print to a EnsEMBL::Web::Document::Panel
-object (this requires that we override the print module to print
-to a panel handle), and also override the dump function so that
-it passes an EnsEMBL::Web::Document::Panel object around rather
-than the "tied" file handle that the Bio::EnsEMBL::Utils::SeqDumper
-does
-
-=head3 sub print( $panel, $string );
-
-This method overrides the parent class method so that "print"
-print uses the panel's print function.
-
-=cut
-
-sub print {
-  my ($self, $panel, $string) = @_;
-  
-  $self->{'string'} .= $string;
-}
-
-=head3 sub dump( $slice, $format, $panel );
-
-This method overrides the parent class dump method so that
-it passes the $panel around rather than the file handle that
-the parent class passes around
+written so that it can print to a IO::String
 
 =cut
 
 sub dump {
-  my ($self, $slice, $format, $panel) = @_;
+  my ($self, $slice, $format) = @_;
+
+  $format || throw("format arg is required");
+  $slice  || throw("slice arg is required");
 
   my $dump_handler = 'dump_' . lc($format);
   
-  $self->{'string'} = '';
+  my $fh = IO::String->new();
   
   if ($self->can($dump_handler)) {
-    $self->$dump_handler($slice, $panel);
+    $self->$dump_handler($slice, $fh);
   }
   
-  $self->{'string'} =~ s/\n/\r\n/g;
+  my $ref = $fh->string_ref();
+  ${$ref} =~ s/\n/\r\n/g;
   
-  return $self->{'string'};
+  return ${$ref};
 }
 
 1;
