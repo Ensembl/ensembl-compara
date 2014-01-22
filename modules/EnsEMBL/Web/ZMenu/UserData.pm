dddae7a76445e36029e0e1c29ce4d4c48d8a47ac
Simon Brent <sb23@sanger.ac.uk>
2014-01-22 17:08:56 +0000

    Fixed UserData zmenu for new bigwig code

--- modules/EnsEMBL/Web/ZMenu/UserData.pm
+++ modules/EnsEMBL/Web/ZMenu/UserData.pm
@@ -38,9 +38,15 @@ sub content {
   
   $glyphset = $glyphset->new($click_data);
   
-  my %feats    = $glyphset->features; # FIXME: this works for bigwig/bigbed, but won't for other types where features returns a sensible data structure
-  my @features = grep !($_->can('score') && $_->score == 0), map { ref $_->[0] eq 'ARRAY' ? @{$_->[0]} : @$_ } values %feats;
-  my $i        = 0;
+  my $i = 0;
+  my @features;
+  
+  if ($type eq 'bigbed') {
+    my %feats    = $glyphset->features; # bigbed returns a stupid data structure
+       @features = grep !($_->can('score') && $_->score == 0), map { ref $_->[0] eq 'ARRAY' ? @{$_->[0]} : @$_ } values %feats;
+  } else {
+    @features = @{$glyphset->features};
+  }
   
   if (scalar @features > 5) {
     $self->summary_content(\@features);
@@ -56,14 +62,14 @@ sub content {
 
 sub feature_content {
   my ($self, $feature, $i) = @_;
-  my %extra  = $feature->can('extra_data') && ref $feature->extra_data eq 'HASH' ? %{$feature->extra_data} : ();
-  my $start  = $feature->seq_region_start;
-  my $end    = $feature->seq_region_end;
+  my %extra  = ref $feature ne 'HASH' && $feature->can('extra_data') && ref $feature->extra_data eq 'HASH' ? %{$feature->extra_data} : ();
+  my $start  = $feature->{'start'};
+  my $end    = $feature->{'end'};
   my $single = $start == $end;
   
   $self->new_feature;
   
-  $self->caption($feature->id || ($single ? $start : ''));
+  $self->caption(ref $feature eq 'HASH' ? $single ? $start : "$start-$end" : $feature->id || ($single ? $start : ''));
   
   my @entries = (
     $single ? (
@@ -72,11 +78,11 @@ sub feature_content {
       { type => 'Start', label => $start },
       { type => 'End',   label => $end   },
     ),
-    { type => 'Strand',     label => ('-', 'Forward', 'Reverse')[$feature->seq_region_strand] }, # remember, [-1] = at end
-    { type => 'Hit start',  label => $feature->can('hstart')  ? $feature->hstart  : ''        },
-    { type => 'Hit end',    label => $feature->can('hend')    ? $feature->hend    : ''        },
-    { type => 'Hit strand', label => $feature->can('hstrand') ? $feature->hstrand : ''        },
-    { type => 'Score',      label => $feature->can('score')   ? $feature->score   : ''        },
+    { type => 'Strand',     label => ('-', 'Forward', 'Reverse')[$feature->{'strand'}] }, # remember, [-1] = at end
+    { type => 'Hit start',  label => $feature->{'hstart'}  },
+    { type => 'Hit end',    label => $feature->{'hend'}    },
+    { type => 'Hit strand', label => $feature->{'hstrand'} },
+    { type => 'Score',      label => $feature->{'score'}   },
   );
   
   push @entries, { type => $self->format_type($_), label => join(', ', @{$extra{$_}}) } for sort grep !/^(_type|item_colour)$/, keys %extra;
@@ -88,15 +94,14 @@ sub summary_content {
   my ($self, $features) = @_;
   my $min = 9e99;
   my $max = -9e99;
-  my ($mean, $score, $i);
+  my ($mean, $i);
   
   foreach (@$features) {
-    next unless $_->can('score');
+    next unless $_->{'score'};
     
-    $score = $_->score;
-    $min   = min($min, $score);
-    $max   = max($max, $score);
-    $mean += $score;
+    $min   = min($min, $_->{'score'});
+    $max   = max($max, $_->{'score'});
+    $mean += $_->{'score'};
     $i++;
   }
   
