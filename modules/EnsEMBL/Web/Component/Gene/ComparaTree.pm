4c26c54b8f341493dc9e736281bd7ad2d3eed08e
Anne Parker <ap5@sanger.ac.uk>
2014-02-05 15:32:52 +0000

    Updated link to Multi/GeneTree - action is now mandatory

--- modules/EnsEMBL/Web/Component/Gene/ComparaTree.pm
+++ modules/EnsEMBL/Web/Component/Gene/ComparaTree.pm
@@ -124,7 +124,7 @@ sub content {
   my @collapsed_clades     = grep { $_ =~ /^group_/ && $hub->param($_) eq 'collapse' } $hub->param;
   my @highlights           = $gene && $member ? ($gene->stable_id, $member->genome_db->dbID) : (undef, undef);
   my $hidden_genes_counter = 0;
-  my $link                 = $hub->type eq 'GeneTree' ? '' : sprintf ' <a href="%s">%s</a>', $hub->url({ species => 'Multi', type => 'GeneTree', action => undef, gt => $tree_stable_id, __clear => 1 }), $tree_stable_id;
+  my $link                 = $hub->type eq 'GeneTree' ? '' : sprintf ' <a href="%s">%s</a>', $hub->url({ species => 'Multi', type => 'GeneTree', action => 'Image', gt => $tree_stable_id, __clear => 1 }), $tree_stable_id;
   my ($hidden_genome_db_ids, $highlight_species, $highlight_genome_db_id);
 
   my $html                 = sprintf '<h3>GeneTree%s</h3>%s', $link, $self->new_twocol(
