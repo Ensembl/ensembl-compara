1ba4ada7a9392302c7adbc16afe300430f9ce5e3
Harpreet Riat <harpreet@ebi.ac.uk>
2014-02-05 16:50:00 +0000

    Old Blast files removed

--- utils/google_sitemaps/make_google_sitemap.pl
+++ utils/google_sitemaps/make_google_sitemap.pl
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# TODO - change this according to new Blast code
 
 use strict;
 use FindBin qw($Bin);
--- utils/test_blast_api.pl
+++ utils/test_blast_api.pl
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# TODO - change it according to new Blast code
+
 use strict;
 use warnings;
 use Carp;
