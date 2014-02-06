be1a608b076a20cad7407ac259828ec7c9f59349
Matthieu Muffato <muffato@ebi.ac.uk>
2014-02-06 17:13:01 +0000

    No references to the mailing-lists

--- modules/Bio/EnsEMBL/Compara/RunnableDB/ComparaHMM/PantherLoadModels.pm
+++ modules/Bio/EnsEMBL/Compara/RunnableDB/ComparaHMM/PantherLoadModels.pm
@@ -37,12 +37,11 @@ alignment process.
 
 =head1 CONTACT
 
-  Please email comments or questions to the public Ensembl
-  developers list at <dev@ensembl.org>.
+Please email comments or questions to the public Ensembl
+developers list at <http://lists.ensembl.org/mailman/listinfo/dev>.
 
-  Questions may also be sent to the Ensembl help desk at
-  <helpdesk@ensembl.org>.
-
+Questions may also be sent to the Ensembl help desk at
+<http://www.ensembl.org/Help/Contact>.
 
 =head1 APPENDIX
 
