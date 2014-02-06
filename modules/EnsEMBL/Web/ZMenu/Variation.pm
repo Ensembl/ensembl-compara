a8107b17db8aa324444452e0134d7ad19c108731
Laurent Gil <lgil@ebi.ac.uk>
2014-02-06 14:15:16 +0000

    Use external link for private data instead of internal 'properties' link

--- modules/EnsEMBL/Web/ZMenu/Variation.pm
+++ modules/EnsEMBL/Web/ZMenu/Variation.pm
@@ -131,17 +131,35 @@ sub feature_content {
   }
   
   $self->caption(sprintf '%s: %s', $feature->variation->is_somatic ? 'Somatic mutation' : 'Variation', $name);
-  
-  $self->add_entry({
-    label_html => "$name properties",
-    link       => $hub->url({
-      type   => 'Variation', 
-      action => 'Summary',
-      v      => $name,
-      vf     => $dbID,
-      source => $source
-    })
-  });
+
+  if ($db eq 'variation') {
+    $self->add_entry({
+      label_html => "$name properties",
+      link       => $hub->url({
+        type   => 'Variation',
+        action => 'Explore',
+        v      => $name,
+        vf     => $dbID,
+        source => $source
+      })
+    });
+
+  } else {
+    if ($source eq 'LOVD') {
+      # http://varcache.lovd.nl/redirect/hg19.chr###ID### , e.g. for ID: 1:808922_808922(FAM41C:n.1101+570C>T)
+      my $tmp_chr_end = ($chr_start>$chr_end) ? $chr_start+1 : $chr_end;
+      my $external_url = $hub->get_ExtURL_link("View in $source", 'LOVD', { ID => "$chr:$chr_start\_$tmp_chr_end($name)" });
+      $self->add_entry({
+        label_html => $external_url
+      });
+    }
+    else {
+      my $external_url = $hub->get_ExtURL_link("View in $source", uc($source));
+      $self->add_entry({
+         label_html => $external_url
+      });
+    }
+  }
   
   $self->add_entry({ type => $_->[0], label => $_->[1] }) for grep $_->[1], @entries;
   $self->add_entry({ label_html => $codon_change }) if $codon_change;
