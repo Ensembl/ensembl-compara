6aa2524ea0c05ccb7a4b2d75fa7b8bb99e1a1105
Matthieu Muffato <muffato@ebi.ac.uk>
2014-02-10 11:01:00 +0000

    bugfix: get_short_name cannot work on Uniprot members

--- modules/Bio/EnsEMBL/Compara/AlignedMemberSet.pm
+++ modules/Bio/EnsEMBL/Compara/AlignedMemberSet.pm
@@ -373,11 +373,11 @@ sub get_SimpleAlign {
         $seqID .= "_" . $member->taxon_id if($append_taxon_id);
         $seqID .= "_" . $member->genome_db_id if ($append_genomedb_id);
 
-        ## Append $seqID with Speciae short name, if required
-        if ($append_sp_short_name) {
+        ## Append $seqID with species short name, if required
+        if ($append_sp_short_name and $member->genome_db_id) {
             my $species = $member->genome_db->get_short_name;
             $species =~ s/\s/_/g;
-            $seqID .= "_" . $species . "_";
+            $seqID .= "_" . $species;
         }
 
         # Sequence length
