db151db3b03c847b904d99d8e77317ef1ca121cd
Harpreet Riat <harpreet@ebi.ac.uk>
2014-02-13 14:59:36 +0000

    Method embed_html_file added
    A new method embed_html_file added that modifies the relative links in the html file to make them work while the file is embedded

--- modules/EnsEMBL/Web/Component/Help.pm
+++ modules/EnsEMBL/Web/Component/Help.pm
@@ -21,8 +21,10 @@ package EnsEMBL::Web::Component::Help;
 use strict;
 
 use List::Util qw(first);
-use EnsEMBL::Web::Tools::FileHandler qw(file_get_contents);
+
 use EnsEMBL::Web::Document::HTML::Movie;
+use EnsEMBL::Web::Tools::FileHandler qw(file_get_contents);
+use EnsEMBL::Web::Exceptions;
 
 use base qw( EnsEMBL::Web::Component);
 
@@ -42,7 +44,6 @@ sub parse_help_html {
 
   my $sd      = $self->hub->species_defs;
   my $img_url = $sd->ENSEMBL_STATIC_SERVER.$sd->ENSEMBL_HELP_IMAGE_ROOT;
-  my @htdocs  = map "$_/htdocs", grep(!m/\:\:/, @{$sd->ENSEMBL_PLUGINS}), $sd->ENSEMBL_WEBROOT;
   my $html;
 
   foreach my $line (split '\n', $content) {
@@ -54,8 +55,7 @@ sub parse_help_html {
     if ($line =~ /\[\[MOVIE::(\d+)/i) {
       $line = $self->embed_movie(@{$adaptor->fetch_help_by_ids([$1]) || []});
     } elsif ($line =~ /\[\[HTML::(.+)\]\]/) {
-      my $html_file = first { -e $_ } map "$_/$1", @htdocs;
-      $line = $html_file ? join '', file_get_contents($html_file) : "<p><i>Error: File $1 not found.</i>";
+      $line = $self->embed_html_file($1, $adaptor);
     }
 
     $html .= $line;
@@ -87,6 +87,55 @@ sub help_feedback {
     </div>';
   
   return $html;
+}
+
+sub embed_html_file {
+  my ($self, $file_name, $adaptor) = @_;
+
+  if (!$self->{'_htdocs'}) {
+    my $sd = $self->hub->species_defs;
+    $self->{'_htdocs'} = [ map "$_/htdocs", grep(!m/\:\:/, @{$sd->ENSEMBL_PLUGINS}), $sd->ENSEMBL_WEBROOT ];
+  }
+
+  my $file_path = first { -e $_ } map "$_$file_name", @{$self->{'_htdocs'}};
+
+  return "<p><i>Error: File $file_name not found.</i></p>" unless $file_path;
+
+  my $file  = $self->parse_help_html(join('', file_get_contents($file_path)), $adaptor); # this will parse any tags in the html doc itself
+  my $div   = $self->dom->create_element('div');
+
+  my @replacements;
+
+  try {
+    while ($file =~ /(<a\s+[^>]+>)/g) {
+      $div->inner_HTML("$1</a>", 1);
+
+      my $link  = $div->first_child;
+      my $href  = $link->get_attribute('href');
+
+      unshift @replacements, { 'offset' => $-[1], 'length' => $+[1] - $-[1], 'link' => $link };
+
+      if ($href !~ /^(\/|#)/) {
+        if ($href =~ /^(ht|f)tp(s)?:\/\//) {
+          $link->set_attribute('rel', 'external') unless $link->get_attribute('rel');
+        } else {
+          my @relative_path = split /\//, $href;
+          my @absolute_path = split /\//, $file_name;
+          pop @absolute_path;                                                           # remove the actual file name from the path
+          shift @relative_path and pop @absolute_path while $relative_path[0] eq '..';  # adjust the path considering the '..' to step down one level
+          $link->set_attribute('href', join '/', @absolute_path, @relative_path);       # change the href now to the new absolute path
+        }
+      } elsif ($href =~ /^$file_name(#.+)/) {
+        $link->set_attribute('href', $1);
+      }
+    }
+  } catch {
+    $file = undef;
+  };
+
+  substr $file, $_->{'offset'}, $_->{'length'}, $_->{'link'}->render =~ s/<[^<]+>$//r for @replacements; # render return closing tag too - remove the closing tag before replacing the link
+
+  return defined $file ? $file : "<p><i>Error: File $file_name contains one or more invalid links.</i></p>";
 }
 
 1;
