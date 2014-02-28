7b69f6d409d27eddde473a8440c5f3140e6721a0
William McLaren <wm2@ebi.ac.uk>
2014-02-28 16:56:32 +0000

    fix when no gene name available to look up associated variations

--- modules/EnsEMBL/Web/Component/Gene/GenePhenotypeVariation.pm
+++ modules/EnsEMBL/Web/Component/Gene/GenePhenotypeVariation.pm
@@ -111,7 +111,7 @@ sub stats_table {
   }
   push @$columns,  { key => 'source',  title => 'Source(s)',  sort => 'string', width => '11%'};
   
-  foreach my $pf (@{$pf_adaptor->fetch_all_by_associated_gene($gene_name)}) {
+  foreach my $pf ($gene_name ? @{$pf_adaptor->fetch_all_by_associated_gene($gene_name)} : ()) {
     my $var_name   = $pf->object->name;  
     my $phe        = $pf->phenotype->description;
     my $phe_source = $pf->source;
