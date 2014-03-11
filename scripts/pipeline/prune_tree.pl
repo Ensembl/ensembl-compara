94aad646ac095721d6bbc6995e19a72c59efbbbd
Matthieu Muffato <muffato@ebi.ac.uk>
2014-03-11 01:15:38 +0000

    create_species_tree() and prune_tree() are now in Utils::SpeciesTree

--- scripts/pipeline/prune_tree.pl
+++ scripts/pipeline/prune_tree.pl
@@ -17,6 +17,7 @@
 use warnings;
 use strict;
 use Bio::EnsEMBL::Compara::DBSQL::DBAdaptor;
+use Bio::EnsEMBL::Compara::Utils::SpeciesTree;
 use Bio::EnsEMBL::Compara::Graph::NewickParser;
 use Getopt::Long;
 
@@ -46,7 +47,7 @@ my $compara_dba = new Bio::EnsEMBL::Compara::DBSQL::DBAdaptor(-url => $url)
     or die "Must define a url";
 
 if (defined $output_taxon_file) {
-    my $species_tree    = $compara_dba->get_SpeciesTreeAdaptor()->create_species_tree();
+    my $species_tree    = Bio::EnsEMBL::Compara::Utils::SpeciesTree::create_species_tree(-COMPARA_DBA => $compara_dba);
 
     open  TF, ">$output_taxon_file" or die "$!";
     print TF $species_tree->newick_format( 'njtree' );
@@ -56,7 +57,7 @@ if (defined $output_taxon_file) {
 if (defined $output_tree_file) {
 
     my $blength_tree = Bio::EnsEMBL::Compara::Graph::NewickParser::parse_newick_into_tree( `cat $tree_file` );
-    my $pruned_tree  = $compara_dba->get_SpeciesTreeAdaptor()->prune_tree( $blength_tree, $species_set_id );
+    my $pruned_tree  = Bio::EnsEMBL::Compara::Utils::SpeciesTree::prune_tree( $blength_tree, $compara_dba, $species_set_id );
 
     open FH, ">$output_tree_file" or die "$!";
     print FH $pruned_tree->newick_format('simple') . "\n";
