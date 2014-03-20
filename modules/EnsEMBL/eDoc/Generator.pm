733f9ba1c285309eb2fca085f8ce1e9a60c4351b
Anne Lyle <annelyle@ebi.ac.uk>
2014-03-20 13:44:25 +0000

    Added remaining object and more methods

--- modules/EnsEMBL/eDoc/Generator.pm
+++ modules/EnsEMBL/eDoc/Generator.pm
@@ -95,12 +95,16 @@ sub find_modules {
   my $self = shift;
   my $code_ref = sub { $self->wanted(@_) };
   find($code_ref, @{$self->get_directories});
-  return;
 
   # map inheritance (super and sub classes), and replace module
   # names with module objects.
   my %subclasses = ();
-  foreach my $module (@{ $self->get_modules }) {
+  while (my ($class, $modules) = each(%{$self->get_module_info})) {
+  }
+  return;
+
+=pod
+ # foreach my $module (@{ $self->get_modules }) {
     if ($module->inheritance) {
       my @class_cache = ();
       foreach my $classname (@{ $module->get_inheritance }) {
@@ -136,14 +140,15 @@ sub find_modules {
   }
 
   return $self->get_modules;
+=cut
 }
 
 sub wanted {
   ### Callback method used to find packages in the directory of
   ### interest.
   my $self = shift;
-  if ($File::Find::name!~ /CVS/ && $_ =~ /pm$/) {
-    print "Indexing " . $_ . "\n";
+  if ($File::Find::name =~ /pm$/) {
+    #print "Indexing " . $_ . "\n";
     #warn "CHECKING: " . $File::Find::name;
     my $class = $self->_package_name($File::Find::name);
     $self->_add_module($class, $File::Find::name);
@@ -174,14 +179,14 @@ sub _add_module {
   ### The new module will find methods within that package on
   ### instantiation.
   my ($self, $class, $location) = @_;
-  my $module = EnsEMBL::eDoc::Module->new((
-                     name => $class,
-                     location => $location,
-                     identifier => $self->get_identifier,
-                     keywords => $self->get_keywords,
-                     find_methods => "yes"
-                   ));
-  #warn "ADDING: " . $module->name;
+  #print "ADDING: $class\n";
+  my $module = EnsEMBL::eDoc::Module->new(
+                     name         => $class,
+                     location     => $location,
+                     identifier   => $self->get_identifier,
+                     keywords     => $self->get_keywords,
+                     find_methods => 1,
+                   );
   my $arrayref = $self->get_modules_by_name($class);
   if ($arrayref) {
     push @$arrayref, $module;
--- modules/EnsEMBL/eDoc/Module.pm
+++ modules/EnsEMBL/eDoc/Module.pm
@@ -37,7 +37,30 @@ sub new {
     'keywords'    => $params{'keywords'}    || $default_keywords,
   };
   bless $self, $class;
+  if ($params{'find_methods'}) {
+    $self->find_methods;
+  }
   return $self;
+}
+
+sub get_methods {
+  my $self = shift;
+  return $self->{'methods'};
+}
+
+sub get_name {
+  my $self = shift;
+  return $self->{'name'};
+}
+
+sub get_inheritance {
+  my $self = shift;
+  return $self->{'inheritance'};
+}
+
+sub get_subclasses {
+  my $self = shift;
+  return $self->{'subclasses'};
 }
 
 sub get_location {
@@ -45,24 +68,195 @@ sub get_location {
   return $self->{'location'};
 }
 
-sub get_base {
+sub get_lines {
   my $self = shift;
-  return $self->{'base'};
+  return $self->{'lines'};
 }
 
-sub get_serverroot {
+sub set_lines {
   my $self = shift;
-  return $self->{'serverroot'};
+  $self->{'lines'} = shift;
 }
 
-sub set_serverroot {
+sub get_overview {
   my $self = shift;
-  $self->{'serverroot'} = shift;
+  return $self->{'overview'};
 }
 
-sub get_support {
+sub get_identifier {
   my $self = shift;
-  return $self->{'support'};
+  return $self->{'identifier'};
+}
+
+sub get_keywords {
+  my $self = shift;
+  return $self->{'keywords'};
+}
+
+sub add_subclass {
+  ### Adds a subclass to the subclass array.
+  my ($self, $subclass) = @_;
+  push @{$self->get_subclasses}, $subclass;
+}
+
+sub add_superclass {
+  ### Adds a superclass to the inheritance array.
+  my ($self, $superclass) = @_;
+  push @{$self->get_inheritance}, $superclass;
+}
+
+sub find_methods {
+  ### Scans package files for method definitions, and creates
+  ### new method objects for each one found. Method object 
+  ### references are stored in the methods array.
+  my $self = shift;
+  my %documentation = %{ $self->_parse_package_file };
+  
+  foreach my $method (keys %{ $documentation{methods} }) {
+    my $new_method = EnsEMBL::eDoc::Method->new((
+                       name           => $method,
+                       documentation  => $documentation{methods}{$method}->{comment},
+                       type           => $documentation{methods}{$method}->{type},
+                       result         => $documentation{methods}{$method}->{return},
+                       class          => $self
+                     ));
+    if ($documentation{table}{$method}) {
+      $new_method->set_table($documentation{table}{$method});
+    }
+    $self->add_method($new_method);
+  }
+  if ($documentation{isa}) {
+    my @superclasses = split /\s+/, $documentation{isa};
+    foreach my $class (@superclasses) {
+      $self->add_superclass($class);
+    }
+  }
+
+  if ($documentation{overview}) {
+     $self->set_overview($documentation{overview});
+  }
+
+}
+
+sub _parse_package_file {
+  ### Opens and parses Perl package files for methods and comments
+  ### in e! doc format.
+  my $self = shift;
+  my %docs = ();
+  open (my $fh, $self->get_location);
+  my $sub = "";
+  my $package = "";
+  my $lines = "";
+  my $comment_code = $self->get_identifier;
+  my $table = 0;
+  my $block_table = 0;
+  while (<$fh>) {
+    my $block = 0;
+    $lines++;
+
+    ## Get parent(s)
+    if (/\@ISA/) {
+      my ($nothing, $isa) = split /=/;
+      if ($isa) {
+        $isa =~ s/qw|\(|\)|;//g;
+        chomp $isa;
+        $isa =~ s/\s+//g;
+        $docs{isa} = [$isa];
+      }
+    }
+    elsif (/^use base qw\(([a-zA-Z:\s]+)\);/) {
+      my @isa = split(/\s+/, $1);
+      $docs{isa} = \@isa;
+    }
+
+    ## Get package name and introductory documentation
+    if (/^package/) {
+      $package = $_;
+      $docs{overview} = "";
+    }
+
+    if ($package && $sub eq "" && /^$comment_code /) {
+      my $temp = $_;
+      $temp =~ s/$comment_code//g;
+      $docs{overview} .= $temp;
+    }
+
+    ## Get method documentation
+    if (/^sub /) {
+      $package = "";
+      $sub = $_;
+      $sub =~ s/^sub |{.*//g;
+      $sub =~ s/:lvalue//; ## REALLY NEED TO SET A FLAG HERE FOR LVALUE FUNCTIONS....
+      $sub =~ s/\W+//g;
+      if (!$docs{methods}) {
+        $docs{methods} = {};
+      }
+      $table = "";
+      $docs{methods}{$sub} = {};
+      $docs{table}{$sub} = {};
+    }
+    if ($sub && /$comment_code/) {
+      my ($trash, $comment) = split /$comment_code/;
+      $comment =~ s/^\s+|\s+$//g;
+      chomp $comment;
+      if ($comment eq "") {
+        $comment .= "<br /><br />";
+        $table = "";
+      }
+
+      if ($comment eq "___") {
+         if ($block_table) {
+           $block_table = 0;
+         } else {
+           $block_table = 1;
+         }
+      }
+
+      if ($comment =~ /[A-Z].*\s*:\s+\w+/) {
+        if (!$block_table) {
+          ($table, $trash) = split(/:/, $comment);
+        }
+      }
+      if ($table) {
+        if ($comment !~ /^.eturns:/) {
+          my $table_content = $comment;
+          $table_content =~ s/$table\s*://;
+          if (!$docs{table}{$sub}->{$table}) {
+            $docs{table}{$sub}->{$table} = "";
+          }
+          $docs{table}{$sub}->{$table} .= $table_content . " ";
+          $block = 1;
+        }
+      }
+
+      my @elements = split /\s+/, $comment;
+      if (!$docs{methods}{$sub}{type}) {
+        $docs{methods}{$sub}{type} = "method";
+      }
+      if ($#elements == 0 and $comment ne '___') {
+        $comment = ucfirst($self->convert_keyword($comment));
+        $docs{methods}{$sub}{type} = lc($comment);
+        $comment .= ". ";
+      } else {
+        if ($elements[0] =~ /^.eturns/) {
+          $docs{methods}{$sub}{return} = "@elements";
+          $table = "";
+          $block = 1;
+        }
+      }
+      $docs{methods}{$sub}{comment} .= " " . $comment if !$block;
+      $block = 0;
+    }
+    if (/SUPER::/) {
+      if (/->(.*)::(.*)\(/) {
+        $docs{methods}{$sub}{super} = $2;
+      } elsif (/->(.*)::(.*)\s+;/) {
+        $docs{methods}{$sub}{super} = $2;
+      }
+    }
+  }
+  $self->set_lines($lines);
+  return \%docs;
 }
 
 1;
