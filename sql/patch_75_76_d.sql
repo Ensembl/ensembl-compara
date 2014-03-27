969003cf86e98787b93bb487e93122b00cd749c4
Matthieu Muffato <muffato@ebi.ac.uk>
2014-03-27 10:55:08 +0000

    Insert the missing dnafrags

--- sql/patch_75_76_d.sql
+++ sql/patch_75_76_d.sql
@@ -21,6 +21,25 @@
 #   Split member into seq_member and gene_member
 
 
+-- We first need to insert the missing dnafrags
+
+ALTER TABLE dnafrag AUTO_INCREMENT=200000000000001;
+
+INSERT INTO dnafrag (length, name, genome_db_id, coord_system_name, is_reference)
+SELECT MAX(chr_end), chr_name, genome_db_id, "lrg", 0
+FROM member LEFT JOIN dnafrag ON member.genome_db_id = dnafrag.genome_db_id AND member.chr_name = dnafrag.name
+WHERE chr_name LIKE "LRG%" and dnafrag.name IS NULL
+GROUP BY (chr_name, genome_db_id);
+
+INSERT INTO dnafrag (length, name, genome_db_id, coord_system_name, is_reference)
+SELECT MAX(chr_end), chr_name, genome_db_id, "unknown", 0
+FROM member LEFT JOIN dnafrag ON member.genome_db_id = dnafrag.genome_db_id AND member.chr_name = dnafrag.name
+WHERE dnafrag.name IS NULL
+GROUP BY (chr_name, genome_db_id);
+
+
+-- At this stage, all the chr_name are registered in dnafrag
+
 DROP TABLE IF EXISTS gene_member;
 CREATE TABLE gene_member (
   gene_member_id              int(10) unsigned NOT NULL AUTO_INCREMENT, # unique internal id
@@ -78,7 +97,7 @@ AS SELECT
 	IFNULL(orthologues, 0) AS orthologues,
 	IFNULL(paralogues, 0) AS paralogues,
 	IFNULL(homoeologues, 0) AS homoeologues
-FROM member LEFT JOIN dnafrag ON member.genome_db_id = dnafrag.genome_db_id AND member.chr_name = dnafrag.name LEFT JOIN member_production_counts USING (stable_id)
+FROM member JOIN dnafrag ON member.genome_db_id = dnafrag.genome_db_id AND member.chr_name = dnafrag.name LEFT JOIN member_production_counts USING (stable_id)
 WHERE source_name = "ENSEMBLGENE";
 
 
@@ -132,7 +151,7 @@ AS SELECT
 	chr_end AS dnafrag_end,
 	chr_strand AS dnafrag_strand,
 	display_label
-FROM member LEFT JOIN dnafrag ON member.genome_db_id = dnafrag.genome_db_id AND member.chr_name = dnafrag.name
+FROM member JOIN dnafrag ON member.genome_db_id = dnafrag.genome_db_id AND member.chr_name = dnafrag.name
 WHERE source_name != "ENSEMBLGENE";
 
 
