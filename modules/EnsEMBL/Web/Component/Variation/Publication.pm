cb8aa79720c21873c6dc77a92bdc009309346a3d
Laurent Gil <lgil@ebi.ac.uk>
2014-05-02 11:35:07 +0100

    Create a new 'Phenotype' column, update column headers and add new phenotype and ucsc icons

--- modules/EnsEMBL/Web/Component/Variation/Publication.pm
+++ modules/EnsEMBL/Web/Component/Variation/Publication.pm
@@ -41,19 +41,26 @@ sub content {
   
   my $html = ('<h3>' . $object->name() .' is mentioned in the following publications</h3>'); 
 
-  my ($table_rows ) = $self->table_data($data);
-  my $table         = $self->new_table([], [], { data_table => 1, sorting => [ 'year desc' ] });
+  my ($table_rows,  $column_flags) = $self->table_data($data);
+  my $table = $self->new_table([], [], { data_table => 1, sorting => [ 'year desc' ] });
    
 
-  $table->add_columns(   
-    { key => 'year',   title => 'Year',      align => 'left', sort => 'numeric' },
-    { key => 'pmid',   title => 'PMID',      align => 'left', sort => 'html'    },  
+  $table->add_columns(  
+    { key => 'year',   title => 'Year',      align => 'left', sort => 'hidden_numeric', help => 'Year of publication'},
+    { key => 'pmid',   title => 'PMID',      align => 'left', sort => 'html'          , help => 'PubMed Identifier'  }
+  );
+
+  if ($column_flags->{'phen_asso'}) {
+    $table->add_columns({ key => 'phen', title => 'Phenotype', align => 'center', sort => 'hidden_string', help => 'Significant phenotype(s) association(s) to the variant found in the publication' });
+  }
+
+  $table->add_columns(
     { key => 'title',  title => 'Title',     align => 'left', sort => 'html'    },  
     { key => 'author', title => 'Author(s)', align => 'left', sort => 'html'    },
     { key => 'text',   title => 'Full text', align => 'left', sort => 'html'    },  
   );
 
-  $table->add_columns( { key => 'ucsc',   title => 'UCSC', align => 'left', sort => 'html' }) if $self->hub->species eq 'Homo_sapiens';
+  $table->add_columns( { key => 'ucsc',   title => 'UCSC', align => 'center', sort => 'html', help => 'View publication data in USCS website' }) if $self->hub->species eq 'Homo_sapiens';
   foreach my $row (@{$table_rows}){  $table->add_rows($row);}
 
   $html .=  $table->render;
@@ -70,24 +77,61 @@ sub table_data {
   my $ucsc_url = 'http://genome.ucsc.edu/cgi-bin/hgc?r=0&l=0&c=0&o=-1&t=0&g=pubsMarkerSnp&i='; ## TESTURL
 
   my @data_rows;
-                 
+  my %column_flags; 
+
+  # Find phenotype entry with the same publication
+  my $pfs = $object->Obj->get_all_PhenotypeFeatures;
+  my %pf_publication;
+  foreach my $pf (@$pfs) {
+    if ($pf->study && $pf->is_significant==1) {
+      if ($pf->study->external_reference =~ /^pubmed\/(\d+)/) {
+        $pf_publication{$1}{$pf->phenotype->description} = 1;
+      }
+    }
+  }
+
+  my $url = my $url = $hub->url({
+    type   => 'Variation',
+    action => 'Phenotype',
+  });
                  
   foreach my $cit (@$citation_data) { 
-      
+    
+    my $has_phen_asso = 0;
+    my $phenotypes = '';
+    my $export_phenotypes = '';
+    if (%pf_publication) {
+      $has_phen_asso = defined $cit->pmid() ? ($pf_publication{$cit->pmid()} ? 1 : 0) : 0;
+      if ($has_phen_asso == 1) {
+        $column_flags{'phen_asso'} = 1 if (!$column_flags{'phen_asso'});
+        
+        $phenotypes = "'".join("'; '",keys(%{$pf_publication{$cit->pmid}}))."'";
+        $export_phenotypes = join(";",keys(%{$pf_publication{$cit->pmid}}));
+      }
+    }
+
+    my $has_phenotype_html = qq{
+      <span class="hidden export">$export_phenotypes</span>
+      <a href="$url" title="This variant has significant associated phenotype(s) in this paper: $phenotypes">
+        <img class="_ht" src="/i/val/var_phenotype_data_small.png" style="border-radius:5px;border:1px solid #000" alt="Phenotype"/>
+      </a>
+    };
+
     my $row = {
 	  year   => $cit->year(),
 	  pmid   => defined $cit->pmid() ? $hub->get_ExtURL_link($cit->pmid(), "PUBMED", $cit->pmid()) : undef,
+          phen   => $has_phen_asso == 1 ? $has_phenotype_html : '',
 	  title  => $cit->title(),
 	  author => $cit->authors(),
 	  text   => defined $cit->pmcid() ? $hub->get_ExtURL_link($cit->pmcid(), "EPMC", $cit->pmcid()) : undef,
-	  ucsc   => defined $cit->ucsc_id() ? "<a href=\"" . $ucsc_url . $object->name() ."&pubsFilterExtId=". $cit->ucsc_id() . "\">View</a>" : undef 
+	  ucsc   => defined $cit->ucsc_id() ? "<a class=\"_ht\" href=\"" . $ucsc_url . $object->name() ."&pubsFilterExtId=". $cit->ucsc_id() . "\" title=\"View in UCSC\"><img src=\"/i/val/ucsc_logo_small.png\" style=\"border-radius:5px;border:1px solid #000;padding:1px;background-color:#FFF\" alt=\"View\" /><span class=\"hidden export\">" . $ucsc_url . $object->name() ."&pubsFilterExtId=". $cit->ucsc_id() . "</span></a>" : undef 
     };
  
     push @data_rows, $row;
 
   } 
 
-  return \@data_rows;
+  return \@data_rows, \%column_flags;
 }
 
 
