c9b0c4bf071437ff860915c847e61baa26a7feb3
Anne Lyle <annelyle@ebi.ac.uk>
2014-05-20 15:36:38 +0100

    Don't need species directory if using random dir names

--- modules/EnsEMBL/Web/Component/Gene.pm
+++ modules/EnsEMBL/Web/Component/Gene.pm
@@ -21,7 +21,7 @@ package EnsEMBL::Web::Component::Gene;
 use strict;
 
 use EnsEMBL::Web::TmpFile::Text;
-use EnsEMBL::Web::TmpFile::Image;
+use EnsEMBL::Web::Tools::RandomString qw(random_ticket);
 
 use base qw(EnsEMBL::Web::Component::Shared);
 
@@ -29,14 +29,19 @@ sub draw_structure {
   my ($self, $display_name, $is_thumbnail) = @_;
   my $html = '';
 
-  my $r2r_path  = $self->hub->species_defs->ENSEMBL_TMP_DIR_IMG.'/r2r';
+  ## We want to control where the temp files go, otherwise R2R screws up the image captions!
+  my $random_dir  = random_ticket;
+
+  my $img_dir   = sprintf('%s/r2r/%s', 
+                              $self->hub->species_defs->ENSEMBL_TMP_DIR_IMG,
+                              $random_dir);
   my $name      = $display_name.'-'.$self->hub->param('g');
   my $filename  = $name.'-thumbnail' if $is_thumbnail;
   $filename    .= '.svg';
-  my $svg_path  = $r2r_path.'/'.$filename;
+  my $svg_path  = $img_dir.'/'.$filename;
 
   unless (-e $svg_path) { 
-    $self->make_directory($r2r_path.'/');
+    $self->make_directory($img_dir);
     my $database = $self->hub->database('compara');
     if ($database) {
       my $gma = $database->get_GeneMemberAdaptor();
@@ -53,26 +58,25 @@ sub draw_structure {
       my $ss_cons     = $gene_tree->get_tagvalue('ss_cons');
       return unless $ss_cons;
       my $input_aln   = $gene_tree->get_SimpleAlign( -id => 'MEMBER' );
-      my $aln_file    = $self->_dump_multiple_alignment($input_aln, $model_name, $ss_cons);
-      my ($thumbnail, $plot) = $self->_draw_structure($aln_file, $gene_tree, $peptide->stable_id,$model_name);
+      my $aln_file    = $self->_dump_multiple_alignment($input_aln, $random_dir, $model_name, $ss_cons);
+      my ($thumbnail, $plot) = $self->_draw_structure($aln_file, $gene_tree, $peptide->stable_id, $random_dir, $model_name);
       $filename = $is_thumbnail ? $thumbnail : $plot;
-      $svg_path = $r2r_path.'/'.$filename;
+      $svg_path = $img_dir.'/'.$filename;
     }
   }
   return $svg_path;
 }
 
 sub _dump_multiple_alignment {
-    my ($self, $aln, $model_name, $ss_cons) = @_;
+    my ($self, $aln, $random_dir, $model_name, $ss_cons) = @_;
     if ($ss_cons =~ /^\.+$/) {
       warn "The tree has no structure\n";
       return undef;
     }
 
     my $aln_file  = EnsEMBL::Web::TmpFile::Text->new(
-                        path_pattern  => 'XXXXXXXXXXXXXXX',
+                        prefix        => "r2r/$random_dir",
                         filename      => $model_name,
-                        prefix        => 'r2r/'.$self->hub->species,
                         extension     => ".aln",
                     );
 
@@ -97,7 +101,7 @@ sub _create_cons_file {
 }
 
 sub _draw_structure {
-    my ($self, $aln_file, $tree, $peptide_id, $model_name) = @_;
+    my ($self, $aln_file, $tree, $peptide_id, $random_dir, $model_name) = @_;
 
     $self->_create_cons_file($aln_file, $model_name);
     ## Get random directory name being used by meta files
@@ -108,8 +112,8 @@ sub _draw_structure {
 
     my $thumbnail = $model_name.'-thumbnail.svg';
     my $th_meta = EnsEMBL::Web::TmpFile::Text->new(
-                        prefix   => 'r2r/'.$self->hub->species,
-                        path_pattern => 'XXXXXXXXXXXXXXX',
+                        prefix    => "r2r/$random_dir",
+                        filename  => $model_name.'-thumbnail',
                         extension => ".meta",
                     );
     my $th_content = "$meta_path/$model_name.cons\tskeleton-with-pairbonds\n";
@@ -117,8 +121,8 @@ sub _draw_structure {
     $self->_run_r2r_and_check("", $th_meta->{'full_path'}, $img_path, $thumbnail, "");
 
     my $meta_file  = EnsEMBL::Web::TmpFile::Text->new(
-                        prefix   => 'r2r/'.$self->hub->species,
-                        path_pattern => 'XXXXXXXXXXXXXXX',
+                        prefix    => "r2r/$random_dir",
+                        filename  => $model_name,
                         extension => ".meta",
                     );
     my $content = "$meta_path/$model_name.cons\n";
@@ -128,7 +132,7 @@ sub _draw_structure {
     my $plot_file = $model_name.'.svg';
     $self->_run_r2r_and_check("", $meta_file->{'full_path'}, $img_path, $plot_file, "");
 
-    return ($random_dir.'/'.$thumbnail, $random_dir.'/'.$plot_file);
+    return ($thumbnail, $plot_file);
 }
 
 sub _run_r2r_and_check {
