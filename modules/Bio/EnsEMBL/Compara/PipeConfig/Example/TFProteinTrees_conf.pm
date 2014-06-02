cfccde33cfae1cb00381eadfecb29e9e97058888
Matthieu Muffato <muffato@ebi.ac.uk>
2014-06-02 17:17:27 +0100

    Included the TreeFam v10 species tree (with 347 species)

--- modules/Bio/EnsEMBL/Compara/PipeConfig/Example/TFProteinTrees_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/Example/TFProteinTrees_conf.pm
@@ -102,7 +102,7 @@ sub default_options {
 
     # species tree reconciliation
         # you can define your own species_tree for 'treebest'. It can contain multifurcations
-        'species_tree_input_file'   => $self->o('ensembl_cvs_root_dir').'/ensembl-compara/scripts/pipeline/tf10_347_species',
+        'species_tree_input_file'   => $self->o('ensembl_cvs_root_dir').'/ensembl-compara/scripts/pipeline/tf10_347_species.nh',
         # you can define your own species_tree for 'notung'. It *has* to be binary
 
     # homology_dnds parameters:
