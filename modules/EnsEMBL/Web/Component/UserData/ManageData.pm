7677cdc939794c0d1dace3cbf8b29798a87cb5dd
Anne Lyle <annelyle@ebi.ac.uk>
2014-07-21 11:40:31 +0100

    Fix link on config icon

--- modules/EnsEMBL/Web/Component/UserData/ManageData.pm
+++ modules/EnsEMBL/Web/Component/UserData/ManageData.pm
@@ -238,7 +238,7 @@ sub table_row {
   ## Link for valid datahub  
   my ($config_link, $conf_template);
   if ($file->{'format'} eq 'DATAHUB' && $hub->species_defs->get_config($file->{'species'}, 'ASSEMBLY_NAME') eq $file->{'assembly'}) {
-    $conf_template  = $self->_icon({ link_class => 'modal_link',  class => 'config_icon', 'title' => 'Configure hub tracks for '.$hub->species_defs->get_config($file->{'species'}, 'SPECIES_COMMON_NAME') });
+    $conf_template  = $self->_icon({ class => 'config_icon', 'title' => 'Configure hub tracks for '.$hub->species_defs->get_config($file->{'species'}, 'SPECIES_COMMON_NAME') });
     my $sample_data = $hub->species_defs->get_config($file->{'species'}, 'SAMPLE_DATA') || {};
     my $default_loc = $sample_data->{'LOCATION_PARAM'};
     (my $menu_name = $file->{'name'}) =~ s/ /_/g;
@@ -249,9 +249,10 @@ sub table_row {
         function => undef,
         r        => $hub->param('r') || $default_loc,
     });
-    $name .= sprintf('<br /><a href="%s#modal_config_viewbottom-%s">Configure hub</a>',
+    ## Add menu name here, as we need it in icon link, below this block
+    $config_link .= '#modal_config_viewbottom-'.$menu_name;
+    $name .= sprintf('<br /><a href="%s">Configure hub</a>',
       $config_link,
-      $menu_name,
     );
   }
 
