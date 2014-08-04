Ensembl/ensembl-compara

commit 68a544b31fc8e994b5509d536c22be356db238b9

Only one cell line by default in cell line view.

modules/EnsEMBL/Web/ImageConfig/regulation_view.pm
@@ -100,11 +100,13 @@ sub init {
 
   $self->get_node('opt_empty_tracks')->set('display', 'normal');	
 
-  foreach my $cell_line (@cell_lines) {
-    $cell_line = EnsEMBL::Web::Tree->clean_id($cell_line);
-    $_->set('display', 'normal') for map $self->get_node("${_}_$cell_line") || (), 'reg_feats', 'seg';
-    
-    $self->{'reg_feats_tracks'}{$_} = 1 for "reg_feats_$cell_line", "reg_feats_core_$cell_line", "reg_feats_non_core_$cell_line", "seg_$cell_line";
+  my $cell_line = $self->hub->species_defs->get_config($self->species, 'REGULATION_DEFAULT_CELL');
+  EnsEMBL::Web::Tree->clean_id($cell_line); # Eugh, modifies arg.
+  foreach my $type (qw(reg_feats seg reg_feats_non_core reg_feats_core)) {
+    my $node = $self->get_node("${type}_$cell_line");
+    next unless $node;
+    $node->set('display',$type =~ /_core/ ? 'compact' : 'normal');
+    $self->{'reg_feats_tracks'}{$node->id} = 1;
   }
 
   if ($self->{'code'} ne $self->{'type'}) {    

commit 2645011eeddda14fb0722c730912b074f103103b

Bugfix from 68a544b3 broke cell line views.

modules/EnsEMBL/Web/ImageConfig/regulation_view.pm
@@ -107,7 +107,10 @@ sub init {
     my $node = $self->get_node("${type}_$cell_line");
     next unless $node;
     $node->set('display',$type =~ /_core/ ? 'compact' : 'normal');
-    $self->{'reg_feats_tracks'}{$node->id} = 1;
+  }
+  foreach my $cell_line (@cell_lines) {
+    EnsEMBL::Web::Tree->clean_id($cell_line); # Eugh, modifies arg.
+    $self->{'reg_feats_tracks'}{$_} = 1 for "reg_feats_$cell_line", "reg_feats_core_$cell_line", "reg_feats_non_core_$cell_line", "seg_$cell_line";
   }
 
   if ($self->{'code'} ne $self->{'type'}) {    