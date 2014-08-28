8d1e33f2fd6faad17ce018c0b157e064d26c3fd3
Dan Sheppard <dan@ebi.ac.uk>
2014-08-28 12:11:18 +0100

    Fix cell and evidence selectors on Location views.

--- modules/EnsEMBL/Draw/GlyphSet/fg_multi_wiggle.pm
+++ modules/EnsEMBL/Draw/GlyphSet/fg_multi_wiggle.pm
@@ -51,13 +51,11 @@ sub draw_features {
 
   my $hub = $self->{'config'}->hub;
   my $cell_type_url = $hub->url('Component', {
-    type => 'Regulation',
     action   => 'Web',
     function    => 'CellTypeSelector/ajax',
     image_config => $self->{'config'}->type,
   });
   my $evidence_url = $hub->url('Component', {
-    type => 'Regulation',
     action => 'Web',
     function => 'EvidenceSelector/ajax',
     image_config => $self->{'config'}->type,
