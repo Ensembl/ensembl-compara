f9484179d7c4fe37765baa81454a997b66da2539
Laurent Gil <lgil@ebi.ac.uk>
2014-08-29 13:31:21 +0100

    Fix a display issue with the population description when its length is greater than 75 characters

--- modules/EnsEMBL/Web/Component/Variation/IndividualGenotypes.pm
+++ modules/EnsEMBL/Web/Component/Variation/IndividualGenotypes.pm
@@ -152,8 +152,8 @@ sub summary_tables {
       while ($description =~ m/^.{75}.*?(\s|\,|\.)/g) {
         my $extra_desc =  substr($description, (pos $description));
            $extra_desc =~ s/,/ /g;
-        $description  = qq{<span class="hidden export">$full_desc</span>};
-        $description .= substr($description, 0, (pos $description) - 1) . qq{... <span class="_ht" title="... $extra_desc">(more)</span>};
+           $extra_desc = $self->strip_HTML($extra_desc);
+        $description = qq{<span class="hidden export">$full_desc</span>} . substr($description, 0, (pos $description) - 1) . qq{... <span class="_ht" title="... $extra_desc">(more)</span>};
         last;
       }
     }
