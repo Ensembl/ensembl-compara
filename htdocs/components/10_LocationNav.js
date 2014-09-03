137e1e502673fe6768b24ade9a5edad0f08d07d9
Dan Sheppard <dan@ebi.ac.uk>
2014-09-03 13:01:59 +0100

    Move slider code to JS, make continuous.

--- htdocs/components/10_LocationNav.js
+++ htdocs/components/10_LocationNav.js
@@ -26,7 +26,79 @@ Ensembl.Panel.LocationNav = Ensembl.Panel.extend({
       Ensembl.EventManager.register('ajaxComplete', this, function () { this.enabled = true; });
     }
   },
-  
+
+  currentLocation: function() { // extract r=, in easy to use format
+    var url = window.location[Ensembl.locationURL];
+    var url_r = url.match(Ensembl.locationMatch)[1];
+    var r_parts = url_r.match(/^(.*):(.*)-(.*)/);
+    return [r_parts[1],parseInt(r_parts[2]),parseInt(r_parts[3])];
+  },
+
+  urlNewLocation: function(r) { // current url as string, but with new r=
+    var url = window.location.href;
+    return url.replace(/([#?;&])r=([^;&]+)/,'$1r='+r);
+  },
+
+  arrow: function(step) { // href for arrow buttons at cur pos. as string
+    var r = this.currentLocation();
+    return this.urlNewLocation(r[0]+':'+(r[1]+step)+'-'+(r[2]+step));  
+  },
+
+  zoom: function(factor) { // href for +/- buttons at cur pos. as string
+    var r = this.currentLocation();
+    var centre = (r[1] + r[2])/2;
+    var width = r[2] - r[1];
+    var start = Math.round(centre-width*factor/2);
+    var end = Math.round(start + width*factor);
+    if(start == end && factor > 1) { end += 1; } // enable zoom out from 1bp
+    return this.urlNewLocation(r[0]+':'+start+'-'+end);
+  },
+
+  updateButtons: function() { // update button hrefs (and loc) at cur. pos
+    var panel = this;
+
+    var r = this.currentLocation();
+    var width = r[2]-r[1]+1;
+    $('.left_2',panel.el).attr('href',panel.arrow(-1e6));
+    $('.left_1',panel.el).attr('href',panel.arrow(-width));
+    $('.zoom_in',panel.el).attr('href',panel.zoom(0.5));
+    $('.zoom_out',panel.el).attr('href',panel.zoom(2));
+    $('.right_1',panel.el).attr('href',panel.arrow(width));
+    $('.right_2',panel.el).attr('href',panel.arrow(1e6));
+    $('#loc_r',panel.el).val(r[0]+':'+r[1]+'-'+r[2]);
+  },
+
+  val2pos: function () { // from 0-100 on UI slider to bp
+    var panel = this;
+
+    var r = this.currentLocation();
+    var input = r[2]-r[1]+1;
+    var sliderConfig = $.parseJSON($('span.ramp', panel.el).text());
+    var slide_min = sliderConfig.min;
+    var slide_max = sliderConfig.max;
+    var slide_mul = ( Math.log(slide_max) - Math.log(slide_min) ) / 100;
+    var slide_off = Math.log(slide_min);
+    var out = (Math.log(input)-slide_off)/slide_mul;
+    if(out < 0) { return 0; }
+    if(out > 100) { return 100; }
+    return out;
+  }, 
+
+  pos2val: function(pos) { // from bp to 0-100 on UI slider
+    var panel = this;
+
+    var sliderConfig = $.parseJSON($('span.ramp', panel.el).text());
+    var slide_min = sliderConfig.min;
+    var slide_max = sliderConfig.max;
+    var slide_mul = ( Math.log(slide_max) - Math.log(slide_min) ) / 100;
+    var slide_off = Math.log(slide_min);
+    var raw_value = Math.exp(pos * slide_mul + slide_off);
+    // To 2sf
+    var mag = Math.pow(10,2 - Math.ceil(Math.log(raw_value)/Math.LN10));
+    var value = Math.round(Math.round(raw_value*mag)/mag);
+    return value;
+  },
+ 
   init: function () {
     var panel = this;
     
@@ -34,12 +106,12 @@ Ensembl.Panel.LocationNav = Ensembl.Panel.extend({
     
     this.enabled = this.params.enabled || false;
     
-    var sliderConfig = $('span.ramp', this.el).hide().children();
+    $('span.ramp', this.el).hide();
+    var sliderConfig = $.parseJSON($('span.ramp', this.el).text());
     var sliderLabel  = $('.slider_label', this.el);
     
     $('.slider_wrapper', this.el).children().css('display', 'inline-block');
     
-    this.elLk.updateURL     = $('.update_url', this.el);
     this.elLk.locationInput = $('.location_selector', this.el);
     this.elLk.navbar        = $('.navbar', this.el);
     this.elLk.imageNav      = $('.image_nav', this.elLk.navbar);
@@ -64,27 +136,31 @@ Ensembl.Panel.LocationNav = Ensembl.Panel.extend({
       }
     });
     
+    var slide_min = sliderConfig.min;
+    var slide_max = sliderConfig.max;
+    var slide_mul = ( Math.log(slide_max) - Math.log(slide_min) ) / 100;
+    var slide_off = Math.log(slide_min);
+    var r = panel.currentLocation();
+    var chr = r[0];
+    var r_2centre = Math.round(r[1]+r[2]);
     this.elLk.slider = $('.slider', this.el).slider({
-      value: sliderConfig.filter('.selected').index(),
+      value: panel.val2pos(),
       step:  1,
       min:   0,
-      max:   sliderConfig.length - 1,
+      max:   100,
       force: false,
       slide: function (e, ui) {
-        sliderLabel.html(sliderConfig[ui.value].name + ' bp').show();
+        var value = panel.pos2val(ui.value);
+        sliderLabel.html(value + ' bp').show();
       },
       change: function (e, ui) {
-        var input = sliderConfig[ui.value];
-        var url   = input.href;
-        var r     = url.match(Ensembl.locationMatch)[1];
+        if(panel.elLk.slider.slider('option','fake')) { return; }
+        var input = panel.pos2val(ui.value);
+        var r_start = Math.round((r_2centre-input)/2);
+        var r = chr+':'+r_start+'-'+(r_start+input+1);
+        var url = panel.urlNewLocation(r);
         
-        sliderLabel.html(input.name + ' bp');
-        
-        input = null;
-        
-        if (panel.elLk.slider.slider('option', 'force') === true) {
-          return false;
-        } else if (panel.enabled === false) {
+        if (panel.enabled === false) {
           Ensembl.redirect(url);
           return false;
         } else if (Ensembl.locationURL === 'hash' && !window.location.hash.match(Ensembl.locationMatch) && window.location.search.match(Ensembl.locationMatch)[1] === r) {
@@ -100,37 +176,17 @@ Ensembl.Panel.LocationNav = Ensembl.Panel.extend({
         $('.ui-slider-handle', panel.elLk.slider).trigger('blur'); // Force the blur event to remove the highlighting for the handle
       }
     });
-    
+    this.updateButtons(); 
     this.resize();
   },
   
   getContent: function () {
     var panel = this;
-    
-    if (!this.elLk.updateURL.length) {
-      return;
-    }
-    
-    $.ajax({
-      url: Ensembl.urlFromHash(this.elLk.updateURL.val() + ';update_panel=1'),
-      dataType: 'json',
-      success: function (json) {
-        var sliderValue = json.shift();
-        
-        if (panel.elLk.slider.slider('value') !== sliderValue) {
-          panel.elLk.slider.slider('option', 'force', true);
-          panel.elLk.slider.slider('value', sliderValue);
-          panel.elLk.slider.slider('option', 'force', false);
-        }
-      
-        panel.elLk.updateURL.val(json.shift());
-        panel.elLk.locationInput.val(json.shift());
-        
-        panel.elLk.navLinks.attr('href', function () {
-          return this.href.replace(Ensembl.locationReplace, '$1$2' + json.shift() + '$3');
-        });
-      }
-    });
+  
+    panel.elLk.slider.slider('option','fake',true); 
+    panel.elLk.slider.slider('value',panel.val2pos());
+    panel.elLk.slider.slider('option','fake',false); 
+    panel.updateButtons();
   },
   
   resize: function () {
@@ -147,5 +203,6 @@ Ensembl.Panel.LocationNav = Ensembl.Panel.extend({
     } else {
       this.elLk.navbar.removeClass('narrow1 narrow2');
     }
+    this.updateButtons();
   }
 });
--- modules/EnsEMBL/Web/Component/Location/ViewBottomNav.pm
+++ modules/EnsEMBL/Web/Component/Location/ViewBottomNav.pm
@@ -33,60 +33,21 @@ sub _init {
 sub set_cache_key {
   my $self = shift;
   $self->SUPER::set_cache_key;
-  $ENV{'CACHE_KEY'} .= ':JSON' if $self->hub->param('update_panel');
 }
 
 sub content_region {
-  return $_[0]->content([ [4,1e4], [6,5e4], [8,1e5], [10,5e5], [12,1e6], [14,2e6], [16,5e6], [18,1e7] ], 'region')
+  return $_[0]->content(1e4,1e7);
 }
 
 sub content {
-  my $self             = shift;
-  my $ramp_entries     = shift || [ [4,1e3], [6,5e3], [8,1e4], [10,5e4], [12,1e5], [14,2e5], [16,5e5], [18,1e6] ];
-  my $hub              = $self->hub;
-  my $object           = $self->object;
-  my $image_width      = $self->image_width . 'px';
-  my $seq_region_start = $object->seq_region_start;
-  my $seq_region_end   = $object->seq_region_end;
-  my $cp               = int(($seq_region_end + $seq_region_start) / 2);
-  my $wd               = $seq_region_end - $seq_region_start + 1;
-  my $r                = $hub->param('r');
+  my ($self,$min,$max) = @_;
   
-  $self->{'update'} = $hub->param('update_panel');
-  
-  my $values = [
-    $self->ajax_url(shift, { __clear => 1, r => $r }),
-    $r,
-    $self->nav_url($seq_region_start - 1e6, $seq_region_end - 1e6),
-    $self->nav_url($seq_region_start - $wd, $seq_region_end - $wd),
-    $self->nav_url($cp - int($wd/4) + 1, $cp + int($wd/4)),
-    $self->nav_url($cp - $wd + 1, $cp + $wd),
-    $self->nav_url($seq_region_start + $wd, $seq_region_end + $wd),
-    $self->nav_url($seq_region_start + 1e6, $seq_region_end + 1e6)
-  ];
-  
-  my $ramp = $self->ramp($ramp_entries, $wd, $cp);
-  
-  if ($self->{'update'}) {
-    my $i = 0;
-    my @ramp_values;
-    
-    foreach (@$ramp) {
-      push @ramp_values, $_->[1];
-      unshift @$values, $i if $_->[3];
-      $i++;
-    }
-    
-    splice @$values, 6, 0, @ramp_values;
-    
-    return $self->jsonify($values);
-  }
-
-  return $self->navbar($ramp, $wd, $values);
+  my $ramp = $self->ramp($min||1e3,$max||1e6);
+  return $self->navbar($ramp);
 }
 
 sub navbar {
-  my ($self, $ramp, $wd, $values) = @_;
+  my ($self, $ramp) = @_;
   
   my $hub          = $self->hub;
   my $img_url      = $self->img_url;
@@ -97,16 +58,15 @@ sub navbar {
   my $g            = $hub->param('g');
   my $g_input      = $g ? qq{<input name="g" value="$g" type="hidden" />} : '';
   
-  return sprintf (qq{
+  return qq(
       <div class="navbar print_hide" style="width:$image_width">
         <input type="hidden" class="panel_type" value="LocationNav" />
-        <input type="hidden" class="update_url" value="%s" />
         <div class="relocate">
           <form action="$url->[0]" method="get">
             <label for="loc_r">Location:</label>
             $extra_inputs
             $g_input
-            <input name="r" id="loc_r" class="location_selector" value="%s" type="text" />
+            <input name="r" id="loc_r" class="location_selector" value="" type="text" />
             <a class="go-button" href="">Go</a>
           </form>
           <div class="js_panel" style="float: left; margin: 0">
@@ -121,86 +81,32 @@ sub navbar {
           </div>
         </div>
         <div class="image_nav">
-          <a href="%s" class="move left_2" title="Back 2 windows"></a>
-          <a href="%s" class="move left_1" title="Back 1 window"></a>
-          <a href="%s" class="zoom_in" title="Zoom in"></a>
+          <a href="#" class="move left_2" title="Back 1Mb"></a>
+          <a href="#" class="move left_1" title="Back 1 window"></a>
+          <a href="#" class="zoom_in" title="Zoom in"></a>
           <span class="ramp">$ramp</span>
           <span class="slider_wrapper">
             <span class="slider_left"></span>
-            <span class="slider"><span class="slider_label floating_popup">$wd</span></span>
+            <span class="slider"><span class="slider_label floating_popup"></span></span>
             <span class="slider_right"></span>
           </span>
-          <a href="%s" class="zoom_out" title="Zoom out"></a>
-          <a href="%s" class="move right_1" title="Forward 1 window"></a>
-          <a href="%s" class="move right_2" title="Forward 2 windows"></a>
+          <a href="#" class="zoom_out" title="Zoom out"></a>
+          <a href="#" class="move right_1" title="Forward 1 window"></a>
+          <a href="#" class="move right_2" title="Forward 1Mb"></a>
         </div>
         <div class="invisible"></div>
-      </div>},
-    @$values
-  );
+      </div>);
 }
 
 sub ramp {
-  my ($self, $ramp_entries, $wd, @url_params) = @_;
+  my ($self,$min,$max) = @_;
   
   my $scale = $self->hub->species_defs->ENSEMBL_GENOME_SIZE || 1;
-  my $x     = 0;
-  my (@ramp, @mp);
-  
-  foreach (@$ramp_entries) {
-    $_->[1] *= $scale;
-    push @mp, sqrt($x * $_->[1]);
-    $x = $_->[1];
-  }
-  
-  push @mp, 1e30;
-  
-  my $l = shift @mp;
-  
-  my $img_url = $self->img_url;
-  
-  foreach (@$ramp_entries) {
-    my $r = shift @mp; 
-    
-    push @ramp, [
-      '<a href="%s" name="%d" class="ramp%s" title="%d bp" style="height:%dpx"></a>',
-      $self->ramp_url($_->[1], @url_params),
-      $_->[1], 
-      $wd > $l && $wd <= $r ? ' selected' : '',
-      $_->[1],
-      $_->[0]
-    ];
-    
-    $l = $r;
-  }
-  
-  return $self->{'update'} ? \@ramp : join '', map { sprintf shift @$_, @$_ } @ramp;
-}
-
-sub ramp_url {
-  my ($self, $entry, $cp) = @_;
-  return $self->nav_url($cp - ($entry/2) + 1, $cp + $entry/2);
-}
-
-sub nav_url {
-  my ($self, $s, $e) = @_;
-  my $hub    = $self->hub;
-  my $object = $self->object;
-  my $max    = $object->seq_region_length;
-  
-  ($s, $e) = (1, $e - $s || 1) if $s < 1;
-  ($s, $e) = ($max - ($e - $s), $max) if $e > $max;
-  
-  $s = 1  if $s < 1;
-  $s = $e if $s > $e;
-  
-  return $object->seq_region_name . ":$s-$e" if $self->{'update'};
-  
-  return $hub->url({ 
-    %{$hub->multi_params(0)},
-    r        => $object->seq_region_name . ":$s-$e",
-    function => undef
+  my $json = $self->jsonify({
+    min => $min * $scale,
+    max => $max * $scale,
   });
+  return $json;
 }
 
 1;
