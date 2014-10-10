Ensembl/ensembl-compara

commit 3c513d37b83f636ddc74e0d757605928810b3d1d

Removing click event from track description
The click event that displays the renderer popup was registered with the li.track element. Thus anything inside this li.track was displaying this popup when clicked, and this included the track description box.
To fix this behavior, a new element div.track_name containing just the track label is added inside li.track and click event and all the required CSS are moved to this new element.
Now, the popup is displayed only when clicked on the track name or the renderer icon.

htdocs/components/20_Configurator.js
@@ -130,7 +130,7 @@ Ensembl.Panel.Configurator = Ensembl.Panel.ModalContent.extend({
     }
     
     this.elLk.configDivs.not('.view_config')
-    .on('click', 'ul.config_menu > li.track, .select_all', $.proxy(this.showConfigMenu, this)) // Popup menus - displaying
+    .on('click', 'ul.config_menu > li.track div.track_name, .select_all', $.proxy(this.showConfigMenu, this)) // Popup menus - displaying
     .on('click', '.popup_menu li',                         $.proxy(this.setTrackConfig, this)) // Popup menus - setting values
     .on('click', '.config_header', function () {                                               // Header on search results and active tracks sections will act like the links on the left
       $('a.' + this.parentNode.className.replace(/\s*config\s*/, ''), panel.elLk.links).trigger('click');
@@ -287,7 +287,7 @@ Ensembl.Panel.Configurator = Ensembl.Panel.ModalContent.extend({
       return true;
     }
     
-    var el        = $(e.currentTarget);
+    var el        = $(e.currentTarget.parentNode);
     var selectAll = el.hasClass('select_all');
     
     if (selectAll && e.target === e.currentTarget) {

htdocs/components/90_configurator.css
@@ -47,12 +47,15 @@ div.config_wrapper div.config ul                                    { margin: 0;
 div.config_wrapper div.config ul li                                 { margin: 0; list-style-image: none; background-repeat: no-repeat; background-position: 2px 2px; }
 
 div.config_wrapper div.config ul.config_menu li                     { font-weight: normal; clear: right; background-color: #FFF; }
-div.config_wrapper div.config ul.config_menu li.track               { padding: 3px 0 2px 25px; cursor: pointer; }
+div.config_wrapper div.config ul.config_menu li div.track_name      { padding: 3px 0 2px 25px; cursor: pointer; background: inherit; }
 div.config_wrapper div.config ul.config_menu li div.desc            { margin: 5px 0; padding: 5px 10px; display: none; background-color: [[TINT_BG]]; border: 1px solid [[PANEL_BORDER]]; }
 div.config_wrapper div.config ul.config_menu li div.desc ul         { padding-left: 26px; }
 div.config_wrapper div.config ul.config_menu li div.desc ul li      { padding: 0 0 2px; background-color: inherit; list-style-image: url(/i/bullet_1.gif); }
 div.config_wrapper div.config ul.config_menu li div.desc ul li p    { margin: 4px 0 16px 8px; }
-div.config_wrapper div.config ul.config_menu li .controls           { margin: -1px -5px 0 0; float: right; height: 17px; }
+div.config_wrapper div.config ul.config_menu li div.desc h1,
+div.config_wrapper div.config ul.config_menu li div.desc h2         { font-size: 14px; display: block; border: none; cursor: default; color: [[PAGE_TEXT]] }
+div.config_wrapper div.config ul.config_menu li div.desc h2         { font-size: 12px; }
+div.config_wrapper div.config ul.config_menu li .controls           { margin-right: -5px; float: right; height: 17px; padding: 2px; }
 div.config_wrapper div.config ul.config_menu li .controls div       { height: 100%; width: 17px; float: left; margin-right: 7px; }
 div.config_wrapper div.config ul.config_menu li .controls .empty    { cursor: default; }
 div.config_wrapper div.config ul.config_menu li .menu_help.open,
@@ -60,9 +63,9 @@ div.config_wrapper div.config ul.config_menu li.fav .fave_icon      { background
 div.config_wrapper div.config ul.config_menu                        { margin-left: 5px; }
 div.config_wrapper div.config ul.config_menu li .strand             { background: url(/i/strand.png) no-repeat left top; cursor: default!important; }
 div.config_wrapper div.config ul.config_menu li.r .strand           { background-position: 0 -19px; }
-div.config_wrapper div.config ul.config_menu li.external:before     { background: url(/i/track-external.gif) no-repeat; width: 48px; height: 100%; margin: 0 6px 0 2px; float: left; content: '\0000a0'; }
+div.config_wrapper div.config ul.config_menu li.external:before     { background: url(/i/track-external.gif) no-repeat; width: 48px; height: 100%; margin: 2px 6px 0 24px; float: left; content: '\0000a0'; }
 div.config_wrapper div.config ul.config_menu li.user:before         { background-image: url(/i/track-user.gif); }
-div.config_wrapper div.config ul.popup_menu                         { background: [[TINT_BG]]; position: absolute; display: none; margin: 10px 0 0 -14px; padding: 0; z-index: 3000; border: 1px solid; cursor: pointer; }
+div.config_wrapper div.config ul.popup_menu                         { background: [[TINT_BG]]; position: absolute; display: none; margin: 10px 0 0 10px; padding: 0; z-index: 3000; border: 1px solid; cursor: pointer; }
 div.config_wrapper div.config ul.popup_menu li                      { background-color: [[TINT_BG]]!important; color: #000; height: 22px; line-height: 22px; padding: 0 6px 0 28px; font-weight: bold; }
 div.config_wrapper div.config ul.popup_menu li.header               { background: [[BUTTON_BG]]!important; color: #FFF; text-align: center; padding: 2px 10px; border-bottom: 1px solid [[PAGE_TEXT]]; height: auto; line-height: 16px; }
 div.config_wrapper div.config ul.popup_menu li.header img           { position: absolute; top: -10px; right: -10px; border: 0; padding: 0; height: 20px; width: 20px; margin: 0; }
@@ -74,12 +77,11 @@ div.config_wrapper div.config div.subset.first,
 div.config_wrapper div.config div.subset.active                     { margin-top: -4px; }
 div.config_wrapper div.config div.subset.active h4                  { display: block; margin-top: 10px; }
 div.config_wrapper div.config div.select_all strong:hover,
-div.config_wrapper div.config li.track:hover,
-div.config_wrapper div.track_order ul.config_menu li:hover,
+div.config_wrapper div.config li.track div.track_name:hover,
+div.config_wrapper div.track_order ul.config_menu li div.track_name:hover,
 div.config_wrapper div.config ul.popup_menu li:hover                { background-color: [[CONFIG_HOVER]]; }
-div.config_wrapper div.config li.track:hover div.desc               { background-color: [[CONFIG_HOVER]]; border-color: [[CONFIG_HOVER]]; }
 div.config_wrapper div.favourite_tracks img                         { vertical-align: top; }
-div.config_wrapper div.track_order ul.config_menu li.track          { cursor: n-resize; }
+div.config_wrapper div.track_order ul.config_menu li div.track_name { cursor: n-resize; }
 
 form.configuration.single div.config ul.config_menu li.hidden       { display: none!important; }
 
@@ -102,7 +104,7 @@ body.ie6 div.config_wrapper div.config ul.popup_menu li img         { margin: 2p
 body.ie6 div.config_wrapper div.config div.select_all               { margin-bottom: 4px; }
 body.ie6 div.config_wrapper div.config ul.config_menu li .controls  { margin-top: -16px; }
 body.ie7 div.config_wrapper div.config ul.config_menu li .controls  { margin-top: -18px; }
-body.mac div.config_wrapper div.track_order ul.config_menu li.track { cursor: ns-resize; }
+body.mac div.config_wrapper div.track_order ul.config_menu li div.track_name { cursor: ns-resize; }
 
 /* renderers */
 

modules/EnsEMBL/Web/ViewConfig.pm
@@ -676,13 +676,14 @@ sub build_imageconfig_menus {
     
     my $child = $parent->append_child('li', {
       id         => $id,
-      class      => join(' ', @classes),
-      inner_HTML => qq{$name
+      class      => \@classes,
+      inner_HTML => qq{
         <div class="controls">
           $controls
           <div class="favourite sprite fave_icon _ht" title="Favorite this track"></div>
           $help
         </div>
+        <div class="track_name">$name</div>
         $desc
       }
     });

commit fa6edcf20cf1cfa6a8161769378b5a9497af5aba

Get parentNode only for div.track_name
Fixing a bug introduced in 3c513d3 that breaks 'Enable/Disable all' functionality.
parentNode (i.e. li.track) should only be considered if clicked on a div.track_name.

htdocs/components/20_Configurator.js
@@ -287,9 +287,13 @@ Ensembl.Panel.Configurator = Ensembl.Panel.ModalContent.extend({
       return true;
     }
     
-    var el        = $(e.currentTarget.parentNode);
+    var el        = $(e.currentTarget);
     var selectAll = el.hasClass('select_all');
-    
+
+    if (el.hasClass('track_name')) {
+      el = el.parent();
+    }
+
     if (selectAll && e.target === e.currentTarget) {
       return false; // Stop clicks for select all firing unless you click on the actual text, not the div
     }