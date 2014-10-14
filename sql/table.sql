4e880c69c43d499c465cb3a4c7d87a05d768ef7f
Matthieu Muffato <muffato@ebi.ac.uk>
2014-10-14 22:48:47 +0100

    Scheme patch for v78

--- sql/table.sql
+++ sql/table.sql
@@ -1694,12 +1694,12 @@ CREATE TABLE `CAFE_species_gene` (
 
 # Auto add schema version to database (this will override whatever hive puts there)
 DELETE FROM meta WHERE meta_key='schema_version';
-INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'schema_version', '77');
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'schema_version', '78');
 
 #Add schema type
 INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'schema_type', 'compara');
 
 # Patch identifier
 INSERT INTO meta (species_id, meta_key, meta_value)
-  VALUES (NULL, 'patch', 'patch_76_77_a.sql|schema_version');
+  VALUES (NULL, 'patch', 'patch_77_78_a.sql|schema_version');
 
