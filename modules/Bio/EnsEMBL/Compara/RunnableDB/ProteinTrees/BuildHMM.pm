c87db8117932f5e1f7f1bb59f07586057f204dfb
Matthieu Muffato <muffato@ebi.ac.uk>
2014-10-21 12:11:05 +0100

    Fixed the inheritance path

--- modules/Bio/EnsEMBL/Compara/RunnableDB/ProteinTrees/BuildHMM.pm
+++ modules/Bio/EnsEMBL/Compara/RunnableDB/ProteinTrees/BuildHMM.pm
@@ -55,7 +55,7 @@ use strict;
 
 use Bio::EnsEMBL::Compara::AlignedMemberSet;
 
-use base ('Bio::EnsEMBL::Compara::RunnableDB::ComparaHMM::MultiHMMLoadModels', 'Bio::EnsEMBL::Compara::RunnableDB::GeneTrees::StoreTree', 'Bio::EnsEMBL::Compara::RunnableDB::RunCommand');
+use base ('Bio::EnsEMBL::Compara::RunnableDB::ComparaHMM::LoadModels', 'Bio::EnsEMBL::Compara::RunnableDB::GeneTrees::StoreTree', 'Bio::EnsEMBL::Compara::RunnableDB::RunCommand');
 
 sub param_defaults {
     return {
