bb8de6d4662528da5fdd972bf3c8807605bd4495
Anne Lyle <annelyle@ebi.ac.uk>
2014-11-04 10:25:14 +0000

    Added new message on disabled export button, directing users to alternative download options

--- modules/EnsEMBL/Web/Document/Element/ToolButtons.pm
+++ modules/EnsEMBL/Web/Document/Element/ToolButtons.pm
@@ -134,6 +134,7 @@ sub init {
       caption => 'Export data',
       class   => 'disabled',
       url     => undef,
+      title   => 'Generic export has been disabled on this page. Check individual images, tables, etc for download buttons',
     });
   }
   
