44a66fefb10f1af413d53e79e6705029a5d98f12
Anne Lyle <annelyle@ebi.ac.uk>
2014-11-20 11:17:23 +0000

    Extensively refactored xref table to use hidden columns instead of viewconfig

--- modules/EnsEMBL/Web/Component/Gene/SimilarityMatches.pm
+++ modules/EnsEMBL/Web/Component/Gene/SimilarityMatches.pm
@@ -43,57 +43,78 @@ sub matches_to_html {
   my @dbtypes          = @_;
   my $hub            = $self->hub;
   my $count_ext_refs = 0;
-  my $table          = $self->new_table([], [], { data_table => 'no_col_toggle', sorting => [ 'transcriptid asc' ], exportable => 1 });
-  my (%existing_display_names, @rows, $html);
+  my (@rows, $html);
 
-  my @columns = ({
-    key        => 'transcriptid' ,
-    title      => 'Transcript ID',
-    align      => 'left',
-    sort       => 'string',
-    priority   => 2147483647, # Give transcriptid the highest priority as we want it to be the 1st colum
-    display_id => '',
-    link_text  => ''
-  });
+  my @columns = ({ 
+                  key        => 'transcriptid',
+                  title      => 'Transcript ID',
+                  align      => 'left',
+                  sort       => 'string',
+                });
 
-  my %options = map { $_ => 1 } $self->view_config->options;
-  
-  foreach (@{$self->object->Obj->get_all_Transcripts}) {
-    my $url = sprintf '<a href="%s">%s</a>', $hub->url({ type => 'Transcript', action => 'Summary', function => undef, t => $_->stable_id }), $_->stable_id;
-    my $row = { transcriptid => $url };
+  my (%seen, %hidden_columns, %columns_with_data);
 
-    foreach ($self->get_matches_by_transcript($_, @dbtypes)) {
-
-      #switch off rows that should be off
-      next unless $options{$_->db_display_name} && $hub->param($_->db_display_name) ne 'off';
-
-      my %similarity_links = $self->get_similarity_links_hash($_);
-      my $ext_db_entry     = $similarity_links{'link'} ? qq{<a href="$similarity_links{'link'}">$similarity_links{'link_text'}</a>}  : $similarity_links{'link_text'};
-      $row->{$_->db_display_name} .= ' ' if defined $row->{$_->db_display_name};
-      $row->{$_->db_display_name} .= $ext_db_entry;
-      $count_ext_refs++;
-
-      if (!defined $existing_display_names{$_->db_display_name}) {
-        push @columns, {
-          key        => $_->db_display_name, 
-          title      => $self->format_column_header($_->db_display_name),
-          align      => 'left', 
-          sort       => 'string', 
-          priority   => $_->priority, 
-          display_id => $_->display_id, 
-          link_text  => $similarity_links{'link_text'}
-        };
-        $existing_display_names{$_->db_display_name} = 1;
-      }
-    }
-    push @rows, $row if (1 < keys %$row);
+  my @other_columns = @{$hub->species_defs->DEFAULT_XREFS};
+  foreach (@other_columns) {
+    $_ =~ s/_/ /g;
+    push @columns, {
+                   key    => $_,
+                   title  => $self->format_column_header($_),
+                   align  => 'left',
+                   sort   => 'string',
+                  };
+    $seen{$_} = 1;
+    $hidden_columns{$_} = 0;
   }
-  @columns = sort { $b->{'priority'} <=> $a->{'priority'} || $a->{'title'} cmp $b->{'title'} || $a->{'link_text'} cmp $b->{'link_text'} } @columns;
-  #@columns = sort { default_on($a) <=> default_on($b) || $a->{'title'} cmp $b->{'title'}} @columns;
-  @rows    = sort { keys %{$b} <=> keys %{$a} } @rows; # show rows with the most information first
 
-  $table->add_columns(@columns);
-  $table->add_rows(@rows);
+  my @all_xref_types = keys %{$hub->species_defs->XREF_TYPES||{}};
+  foreach (sort @all_xref_types) {
+    next if $seen{$_};  
+    push @columns, {
+                    key    => $_,
+                    title  => $self->format_column_header($_),
+                    align  => 'left',
+                    sort   => 'string',
+                   };
+    $hidden_columns{$_} = 1;
+  }
+  
+
+  foreach my $transcript (@{$self->object->Obj->get_all_Transcripts}) {
+    my $url = sprintf '<a href="%s">%s</a>', $hub->url({ type => 'Transcript', action => 'Summary', function => undef, t => $transcript->stable_id }), $transcript->stable_id;
+    my $row = { 'transcriptid' => $url };
+    $columns_with_data{'transcriptid'} = 1;
+
+    foreach my $db_entry ($self->get_matches_by_transcript($transcript, @dbtypes)) {
+      my $key = $db_entry->db_display_name;
+      my %matches = $self->get_similarity_links_hash($db_entry);
+
+      $row->{$key} .= ' ' if defined $row->{$key};
+      $row->{$key} .=  $matches{'link'} ? sprintf('<a href="%s">%s</a>', $matches{'link'}, $matches{'link_text'})  : $matches{'link_text'};
+      $count_ext_refs++;
+      $columns_with_data{$key}++;
+    }
+    if (keys %$row) {
+      push @rows, $row;
+    }
+  }
+  @rows = sort { keys %{$b} <=> keys %{$a} } @rows; # show rows with the most information first
+
+  ## Hide columns with no values, as well as those not shown by default
+  my @hidden_cols;
+  my $i = 0;
+  foreach (@columns) {
+    if ($hidden_columns{$_->{'key'}} || !$columns_with_data{$_->{'key'}}) {
+      push @hidden_cols, $i;
+    }
+    $i++; 
+  }  
+
+  my $table = $self->new_table(\@columns, \@rows, { 
+                                                data_table => 1, 
+                                                exportable => 1, 
+                                                hidden_columns => \@hidden_cols, 
+                                              });
 
   if ($count_ext_refs == 0) {
     $html.= '<p><strong>No (selected) external database contains identifiers which correspond to the transcripts of this gene.</strong></p>';
@@ -200,5 +221,6 @@ sub get_similarity_links_hash {
     $similarity_links{'all_locations_url'} = $all_locations_url;
     return %similarity_links;
 }
+
 
 1;
