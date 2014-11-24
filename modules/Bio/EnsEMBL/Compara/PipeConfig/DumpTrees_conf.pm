5b072f584c238e8f775a948cb078b1b0dd9b3816
Matthieu Muffato <muffato@ebi.ac.uk>
2014-11-24 18:54:19 +0000

    typo

--- modules/Bio/EnsEMBL/Compara/PipeConfig/DumpTrees_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/DumpTrees_conf.pm
@@ -183,7 +183,7 @@ sub pipeline_analyses {
                 |,
             },
             -input_ids => [
-                {'cmd' => 'echo "#query#" | db_cmd.pl -url #db_conn# -extra -N -extra -q" > #target_dir#/#file_name#',},
+                {'cmd' => 'echo "#query#" | db_cmd.pl -url #db_conn# -extra -N -extra -q > #target_dir#/#file_name#',},
             ],
             -flow_into => {
                 1 => { 'archive_long_files' => { 'full_name' => '#target_dir#/#file_name#' } },
