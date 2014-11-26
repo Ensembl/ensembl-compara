cca98642abf78bf7451d8f9a8de6e5de8d0433ca
Anne Lyle <annelyle@ebi.ac.uk>
2014-11-26 16:39:51 +0000

    Bring in the remaining glyph modules with actual content

---
+++ modules/EnsEMBL/Draw/Output.pm
@@ -0,0 +1,35 @@
+=head1 LICENSE
+
+Copyright [1999-2014] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+package EnsEMBL::Draw::Output;
+
+### Base package for drawing a discreet section of a genomic image,
+### such as a section of assembly, feature track, scalebar or track legend
+### Uses GD and the EnsEMBL::Draw::Glyph codebase
+
+use strict;
+
+use GD;
+use GD::Simple;
+use URI::Escape qw(uri_escape);
+use POSIX qw(floor ceil);
+
+use Ensembl::Draw::Glyph::Circle;
+use Ensembl::Draw::Glyph::Composite;
+use Ensembl::Draw::Glyph::Poly;
+use Ensembl::Draw::Glyph::Triangle;
