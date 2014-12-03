359d438f81306a42e617c59093959986539d505a
Anne Lyle <annelyle@ebi.ac.uk>
2014-12-03 11:58:39 +0000

    Error message should be inside an arrayref

--- modules/EnsEMBL/Web/Tools/RemoteURL.pm
+++ modules/EnsEMBL/Web/Tools/RemoteURL.pm
@@ -8,27 +8,39 @@ use Exporter qw(import);
 our @EXPORT_OK = qw(chase_redirects);
 
 use HTTP::Tiny;
+use LWP::UserAgent;
 
 sub chase_redirects {
   my ($self, $url, $max_follow) = @_;
 
   $max_follow = 10 unless defined $max_follow;
 
-  my %args = (
+  if ($url =~ /^ftp/) {
+    my $ua = LWP::UserAgent->new( max_redirect => $max_follow );
+    $ua->timeout(10);
+    $ua->env_proxy;
+    $ua->proxy([qw(http https)], $self->{'hub'}->species_defs->ENSEMBL_WWW_PROXY) || ();
+    my $response = $ua->head($url);
+    return $response->is_success ? $response->request->uri->as_string 
+                                    : {'error' => [$response->status_line]};
+  }
+  else {
+    my %args = (
               'timeout'       => 10,
               'max_redirect'  => $max_follow,
               'http_proxy'    => $self->{'hub'}->species_defs->ENSEMBL_WWW_PROXY,
               'https_proxy'   => $self->{'hub'}->species_defs->ENSEMBL_WWW_PROXY,
               );
 
-  my $http = HTTP::Tiny->new(%args);
+    my $http = HTTP::Tiny->new(%args);
 
-  my $response = $http->request('HEAD', $url);
-  if ($response->{'success'}) {
-    return $response->{'url'};
-  }
-  else {
-    return {'error' => $response->{'status'}.': '.$response->{'reason'}};
+    my $response = $http->request('HEAD', $url);
+    if ($response->{'success'}) {
+      return $response->{'url'};
+    }
+    else {
+      return {'error' => $response->{'status'}.': '.$response->{'reason'}};
+    }
   }
 }
 
