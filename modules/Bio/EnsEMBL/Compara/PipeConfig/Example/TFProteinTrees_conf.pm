094ce81f39f0826b2f73977f2536011d42b8cb7b
Mateus Patricio <mateus@ebi.ac.uk>
2014-12-09 10:16:12 +0000

    New capacity levels, in order to avoid max number of connection errors.

--- modules/Bio/EnsEMBL/Compara/PipeConfig/Example/TFProteinTrees_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/Example/TFProteinTrees_conf.pm
@@ -165,11 +165,11 @@ sub default_options {
         'hc_capacity'               =>   4,
         'HMMer_classify_capacity'   => 400,
 		'HMMer_classifyPantherScore_capacity'=> 1000,
-        'loadmembers_capacity'      =>  30,
-        'copy_trees_capacity'       => 100,
-		'copy_alignments_capacity'  => 100,
-		'mafft_update_capacity'     => 100,
-		'raxml_update_capacity'     => 100,
+        'loadmembers_capacity'      => 30,
+        'copy_trees_capacity'       => 50,
+        'copy_alignments_capacity'  => 50,
+        'mafft_update_capacity'     => 50,
+        'raxml_update_capacity'     => 50,
 
     # hive priority for non-LOCAL health_check analysis:
 
