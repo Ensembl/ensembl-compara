a9c6c63aea89e837d66ae37bc013a700d0a7a32d
Anne Lyle <annelyle@ebi.ac.uk>
2015-01-27 16:45:01 +0000

    Fixed missing variable declaration

--- modules/EnsEMBL/Web/Document/Image/R2R.pm
+++ modules/EnsEMBL/Web/Document/Image/R2R.pm
@@ -129,6 +129,7 @@ sub _create_svg {
     ## Path to the files we dumped earlier
     my $path    = $aln_file->base_read_path;
 
+    my $sub_dir = 'r2r_'.$self->hub->species;
     my $cons_filename  = $model_name.'.cons';
     ## For information about these options, check http://breaker.research.yale.edu/R2R/R2R-manual-1.0.3.pdf
     $self->_run_r2r_and_check("--GSC-weighted-consensus", $aln_file->write_location, $path, $cons_filename, "3 0.97 0.9 0.75 4 0.97 0.9 0.75 0.5 0.1");
