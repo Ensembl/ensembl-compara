68628716372c5420d8c83245b6d03c7f3a5157ef
Anne Lyle <annelyle@ebi.ac.uk>
2015-02-06 11:35:20 +0000

    Assembly lookup needs to return a flag to show if assembly is old or not

--- modules/EnsEMBL/Web/SpeciesDefs.pm
+++ modules/EnsEMBL/Web/SpeciesDefs.pm
@@ -1042,17 +1042,17 @@ sub assembly_lookup {
   foreach ($self->valid_species) {
     my $assembly = $self->get_config($_, 'ASSEMBLY_VERSION');
     ## A bit clunky, but it makes code cleaner in use
-    $lookup->{$assembly} = [$_, $assembly];
+    $lookup->{$assembly} = [$_, $assembly, 0];
     ## Now look up UCSC assembly names
     if ($self->get_config($_, 'UCSC_GOLDEN_PATH')) {
-      $lookup->{$self->get_config($_, 'UCSC_GOLDEN_PATH')} = [$_, $assembly];
+      $lookup->{$self->get_config($_, 'UCSC_GOLDEN_PATH')} = [$_, $assembly, 0];
     }
     if ($old_assemblies) {
       ## Include past UCSC assemblies
       if ($self->get_config($_, 'UCSC_ASSEMBLIES')) {
         my %ucsc = @{$self->get_config($_, 'UCSC_ASSEMBLIES')||[]};
         while (my($k, $v) = each(%ucsc)) {
-          $lookup->{$k} = [$_, $v];
+          $lookup->{$k} = [$_, $v, 1];
         }
       }
     }
