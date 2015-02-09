Ensembl/ensembl-production

Simple pipeline for calculating synteny stats.

SyntenyStats_conf.pm
@@ -1,0 +1,94 @@
+=head1 LICENSE
+
+Copyright [1999-2014] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+=head1 NAME
+
+Bio::EnsEMBL::EGPipeline::PipeConfig::SyntenyStats_conf
+
+=head1 DESCRIPTION  
+
+Calculate synteny coverage statistics.
+
+=cut
+
+package Bio::EnsEMBL::EGPipeline::PipeConfig::SyntenyStats_conf;
+
+use strict;
+use warnings;
+
+use base ('Bio::EnsEMBL::EGPipeline::PipeConfig::EGGeneric_conf');
+use File::Spec::Functions qw(catdir);
+
+sub default_options {
+  my ($self) = @_;
+  return {
+    %{$self->SUPER::default_options},
+    
+    pipeline_name => 'synteny_stats_'.$self->o('ensembl_release'),
+    division      => undef,
+    mlss_id       => undef,
+  };
+}
+
+# Force an automatic loading of the registry in all workers.
+sub beekeeper_extra_cmdline_options {
+  my ($self) = @_;
+  
+  my $options = join(' ',
+    $self->SUPER::beekeeper_extra_cmdline_options,
+    "-reg_conf ".$self->o('registry')
+  );
+  
+  return $options;
+}
+
+sub pipeline_wide_parameters {
+  my ($self) = @_;
+  return {
+    %{ $self->SUPER::pipeline_wide_parameters() },
+    division => $self->o('division'),
+  };
+}
+
+sub pipeline_analyses {
+  my ($self) = @_;
+  
+  return [
+    {
+      -logic_name      => 'FetchMLSS',
+      -module          => 'Bio::EnsEMBL::EGPipeline::SyntenyStats::FetchMLSS',
+      -max_retry_count => 0,
+      -parameters      => {
+                            mlss_id  => $self->o('mlss_id'),
+                          },
+      -input_ids       => [ {} ],
+      -rc_name         => 'normal',
+      -flow_into       => ['SyntenyStats'],
+    },
+    
+    {
+      -logic_name      => 'SyntenyStats',
+      -module          => 'Bio::EnsEMBL::EGPipeline::SyntenyStats::SyntenyStats',
+      -max_retry_count => 0,
+      -rc_name         => 'normal',
+    },
+    
+  ];
+}
+
+1;

Ensembl/ensembl-compara

modules/Bio/EnsEMBL/Compara/PipeConfig/SyntenyStats_conf.pm
@@ -1,0 +1,94 @@
+=head1 LICENSE
+
+Copyright [1999-2014] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+=head1 NAME
+
+Bio::EnsEMBL::Compara::PipeConfig::SyntenyStats_conf
+
+=head1 DESCRIPTION  
+
+Calculate synteny coverage statistics.
+
+=cut
+
+package Bio::EnsEMBL::Compara::PipeConfig::SyntenyStats_conf;
+
+use strict;
+use warnings;
+
+use base ('Bio::EnsEMBL::Compara::PipeConfig::EGGeneric_conf');
+use File::Spec::Functions qw(catdir);
+
+sub default_options {
+  my ($self) = @_;
+  return {
+    %{$self->SUPER::default_options},
+    
+    pipeline_name => 'synteny_stats_'.$self->o('ensembl_release'),
+    division      => undef,
+    mlss_id       => undef,
+  };
+}
+
+# Force an automatic loading of the registry in all workers.
+sub beekeeper_extra_cmdline_options {
+  my ($self) = @_;
+  
+  my $options = join(' ',
+    $self->SUPER::beekeeper_extra_cmdline_options,
+    "-reg_conf ".$self->o('registry')
+  );
+  
+  return $options;
+}
+
+sub pipeline_wide_parameters {
+  my ($self) = @_;
+  return {
+    %{ $self->SUPER::pipeline_wide_parameters() },
+    division => $self->o('division'),
+  };
+}
+
+sub pipeline_analyses {
+  my ($self) = @_;
+  
+  return [
+    {
+      -logic_name      => 'FetchMLSS',
+      -module          => 'Bio::EnsEMBL::Compara::RunnableDB::SyntenyStats::FetchMLSS',
+      -max_retry_count => 0,
+      -parameters      => {
+                            mlss_id  => $self->o('mlss_id'),
+                          },
+      -input_ids       => [ {} ],
+      -rc_name         => 'normal',
+      -flow_into       => ['SyntenyStats'],
+    },
+    
+    {
+      -logic_name      => 'SyntenyStats',
+      -module          => 'Bio::EnsEMBL::Compara::RunnableDB::SyntenyStats::SyntenyStats',
+      -max_retry_count => 0,
+      -rc_name         => 'normal',
+    },
+    
+  ];
+}
+
+1;