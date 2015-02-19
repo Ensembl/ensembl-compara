a5fae18283b4f49dd11ccedd6e40db5bddcd0811
Harpreet Riat <harpreet@ebi.ac.uk>
2015-02-19 12:55:36 +0000

    Initialise only once

--- htdocs/components/00_jquery_keepOnPage.js
+++ htdocs/components/00_jquery_keepOnPage.js
@@ -29,20 +29,24 @@
      */
 
     el = $(el);
-    el.css('width', el.css('width'));
 
-    $(window).on('load.keepOnTop scroll.keepOnTop', {
-      el        : el,
-      options   : options,
-      clone     : options.spaced ? el.clone().hide().empty().css({ height: el.height() - 2, width: el.width(), visibility: 'hidden' }).insertAfter(el) : false, // -2 is just to prevent an possible jumpy effect
-      defaults  : {
-        top       : el.offset().top,
-        position  : el.css('position')
-    }}, function (e) {
-      var fixed = e.data.defaults.top - e.data.options.marginTop <= $(window).scrollTop();
-      e.data.el.css(fixed ? { position: 'fixed', top: e.data.options.marginTop } : { position: e.data.defaults.position, top: e.data.defaults.top });
-      if (e.data.clone) e.data.clone.toggle(fixed);
-    });
+    if (!el.data('keepOnTop')) {
+
+      el.css('width', el.css('width')).data('keepOnTop', true);
+
+      $(window).on('load.keepOnTop scroll.keepOnTop', {
+        el        : el,
+        options   : options,
+        clone     : options.spaced ? el.clone().hide().empty().css({ height: el.height() - 2, width: el.width(), visibility: 'hidden' }).insertAfter(el) : false, // -2 is just to prevent an possible jumpy effect
+        defaults  : {
+          top       : el.offset().top,
+          position  : el.css('position')
+      }}, function (e) {
+        var fixed = e.data.defaults.top - e.data.options.marginTop <= $(window).scrollTop();
+        e.data.el.css(fixed ? { position: 'fixed', top: e.data.options.marginTop } : { position: e.data.defaults.position, top: e.data.defaults.top });
+        if (e.data.clone) e.data.clone.toggle(fixed);
+      });
+    }
   };
 
   $.fn.keepOnPage = function (options) {
@@ -52,7 +56,7 @@
 
     this.each(function() {
 
-      new $.keepOnPage(this, options);
+      $.keepOnPage(this, options);
     });
 
     return this;
