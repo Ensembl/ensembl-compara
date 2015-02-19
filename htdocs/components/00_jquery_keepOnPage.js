ae7356d5bef31dc8bb9f1a96d8b419ece95fa27c
Harpreet Riat <harpreet@ebi.ac.uk>
2015-02-19 14:26:20 +0000

    Update clone's height and width when displaying it

--- htdocs/components/00_jquery_keepOnPage.js
+++ htdocs/components/00_jquery_keepOnPage.js
@@ -34,12 +34,12 @@
 
     if (!el.data('keepOnTop')) {
 
-      el.css('width', el.css('width')).data('keepOnTop', {fixed: false});
+      el.data('keepOnTop', {fixed: false});
 
       $(window).on('load.keepOnTop scroll.keepOnTop', {
         el        : el,
         options   : options,
-        clone     : options.spaced ? el.clone().hide().empty().css({ height: el.height() - 2, width: el.width(), visibility: 'hidden' }).insertAfter(el) : false, // -2 is just to prevent an possible jumpy effect
+        clone     : options.spaced ? el.clone().hide().empty().css({ visibility: 'hidden' }).insertAfter(el) : false,
         defaults  : {
           top       : el.offset().top,
           position  : el.css('position')
@@ -51,7 +51,7 @@
         e.data.el.data('keepOnTop', {fixed: fixed});
         e.data.el.css(fixed ? { position: 'fixed', top: e.data.options.marginTop } : { position: e.data.defaults.position, top: e.data.defaults.top });
         if (e.data.clone) {
-          e.data.clone.toggle(fixed);
+          e.data.clone.css(fixed ? {height: e.data.el.height() - 2, width: e.data.el.width()} : {}).toggle(fixed);  // -2 is just to prevent an possible jumpy effect
         }
         ((fixed ? e.data.options.onfix : e.data.options.onreset) || $.noop).apply(e.data.el[0]);
       });
