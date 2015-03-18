5866eac7fc0723318b28cd7c10b572bbfca77f06
Ridwan Amode <ridwan@ebi.ac.uk>
2015-03-18 16:01:14 +0000

    move twitter feed in a function so that it can be overwritten in the mobile plugin

--- modules/EnsEMBL/Web/Document/HTML/WhatsNew.pm
+++ modules/EnsEMBL/Web/Document/HTML/WhatsNew.pm
@@ -61,16 +61,26 @@ sub render {
   $html .= $blog if $blog;
 
   return if ($species_defs->ENSEMBL_SITETYPE eq 'Archive');
+  
+  $html .= $self->show_twitter();
+
+  return $html;
+}
+
+sub show_twitter {
+  my $self          = shift;
+  my $species_defs  = $self->hub->species_defs;
+  my $twitter_html  = '';
 
   my $twitter_user = $species_defs->ENSEMBL_TWITTER_ACCOUNT;
   my $widget_id    = $species_defs->TWITTER_FEED_WIDGET_ID;
   if ($twitter_user && $widget_id) {
-    $html .= sprintf(qq(<a class="twitter-timeline" href="https://twitter.com/%s" height="400" data-widget-id="%s">Recent tweets from @%s</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>), 
+    $twitter_html = sprintf(qq(<a class="twitter-timeline" href="https://twitter.com/%s" height="400" data-widget-id="%s">Recent tweets from @%s</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>),
                 $twitter_user, $widget_id, $twitter_user);
   }
 
-  return $html;
+  return $twitter_html;
 }
 
 sub show_headlines {
