e3ba332d07ea0d3c5210b48b813edbfa19c50a1e
Harpreet Riat <harpreet@ebi.ac.uk>
2015-04-08 11:45:22 +0100

    Hub is provided to Document::HTML as an argument

--- modules/EnsEMBL/Web/Document/HTML/DidYouKnow.pm
+++ modules/EnsEMBL/Web/Document/HTML/DidYouKnow.pm
@@ -26,27 +26,20 @@ use warnings;
 use LWP::UserAgent;
 use Encode qw(encode_utf8);
 
-use EnsEMBL::Web::Hub;
-use EnsEMBL::Web::Cache;
-
-use base qw(EnsEMBL::Web::Document::HTML);
-
-our $MEMD = EnsEMBL::Web::Cache->new(
-  enable_compress    => 1,
-  compress_threshold => 10_000,
-);
+use parent qw(EnsEMBL::Web::Document::HTML);
 
 sub render {
   my $self           = shift;
-  my $hub            = EnsEMBL::Web::Hub->new;
+  my $hub            = $self->hub;
   my $sd             = $hub->species_defs;
+  my $cache          = $hub->cache;
   my $static_server  = $sd->ENSEMBL_STATIC_SERVER;
   my $img_url        = $sd->img_url;
   my $sitename       = $sd->ENSEMBL_SITETYPE;
   my $html           = ''; 
 
   my $rss_url = $sd->ENSEMBL_TIPS_RSS;
-  my $tips = $MEMD && $MEMD->get('::TIPS') || {};
+  my $tips = $cache && $cache->get('::TIPS') || {};
   
   my %categories = (
                     'new' => 'New!',
@@ -63,7 +56,7 @@ sub render {
         $_->{'content'} = encode_utf8($_->{'content'}) for @{$tips->{$cat}};
       }
     }
-    $MEMD->set('::TIPS', $tips, 3600, qw(STATIC TIPS)) if $MEMD;
+    $cache->set('::TIPS', $tips, 3600, qw(STATIC TIPS)) if $cache;
   }
 
   $html .= '<div class="did-you-know"><ul class="bxslider">';
