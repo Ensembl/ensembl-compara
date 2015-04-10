886c3ec91ced582c7e4e0ee5392f51bea70c3186
Matthieu Muffato <muffato@ebi.ac.uk>
2015-04-10 15:33:05 +0100

    table.sql should have been updated when the patches were introduced

--- sql/table.sql
+++ sql/table.sql
@@ -1768,14 +1768,14 @@ CREATE TABLE `CAFE_species_gene` (
 
 # Auto add schema version to database (this will override whatever hive puts there)
 DELETE FROM meta WHERE meta_key='schema_version';
-INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'schema_version', '79');
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'schema_version', '80');
 
 #Add schema type
 INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'schema_type', 'compara');
 
 # Patch identifier
 INSERT INTO meta (species_id, meta_key, meta_value)
-  VALUES (NULL, 'patch', 'patch_78_79_a.sql|schema_version');
+  VALUES (NULL, 'patch', 'patch_79_80_a.sql|schema_version');
 INSERT INTO meta (species_id, meta_key, meta_value)
-  VALUES (NULL, 'patch', 'patch_78_79_b.sql|genome_component');
+  VALUES (NULL, 'patch', 'patch_79_80_b.sql|genebuild_unique');
 
