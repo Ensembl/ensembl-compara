45092128e0326b1bf4dcf7ca7d5188a246ac6f8e
Anne Lyle <annelyle@ebi.ac.uk>
2015-04-14 14:07:25 +0100

    Added remote attachment functionality from AttachRemote

--- modules/EnsEMBL/Web/Command/UserData/AddFile.pm
+++ modules/EnsEMBL/Web/Command/UserData/AddFile.pm
@@ -37,7 +37,7 @@ sub process {
 
   if ($method eq 'url') {
     ## Attach the file from the remote URL
-    
+    $url_params = $self->attach_data($hub->param('url'), $format); 
   }
   else {
     ## Upload the data
@@ -63,6 +63,121 @@ sub process {
   }
 
   return $self->ajax_redirect($self->hub->url($url_params));
+}
+
+sub attach_data {
+  my ($self, $url, $format_name) = @_;
+  my $hub     = $self->hub;
+
+  my $format_package = 'EnsEMBL::Web::File::AttachedFormat::' . uc $format_name;
+  my $trackline      = $self->hub->param('trackline');
+  my $url_params     = {};
+  my $format;
+
+  if ($self->dynamic_use($format_package)) {
+    $format = $format_package->new($self->hub, $format_name, $url, $trackline);
+  } else {
+    $format = EnsEMBL::Web::File::AttachedFormat->new($self->hub, $format_name, $url, $trackline);
+  }
+
+  ## For datahubs, pass assembly info so we can check if there's suitable data
+  my $assemblies = $species_defs->assembly_lookup;
+
+  my ($url, $error, $options) = $format->check_data($assemblies);
+
+  if ($error) {
+    $url_params->{'restart'} = 1;
+
+    $session->add_data(
+      type     => 'message',
+      code     => 'AttachURL',
+      message  => $error,
+      function => '_error'
+    );
+  } 
+  else {
+    ## This next bit is a hack - we need to implement userdata configuration properly! 
+    my $extra_config_page   = $format->extra_config_page;
+    my $name                = $hub->param('name') || $options->{'name'} || $filename;
+    $url_params->{'action'} = $extra_config_page || 'RemoteFeedback';
+
+    delete $options->{'name'};
+
+    my $assemblies = $options->{'assemblies'} || [$hub->species_defs->get_config($hub->data_species, 'ASSEMBLY_VERSION')];
+    my %ensembl_assemblies = %{$hub->species_defs->assembly_lookup};
+
+    my ($flag_info, $code);
+
+    foreach (@$assemblies) {
+
+      my ($current_species, $assembly, $is_old) = @{$ensembl_assemblies{$_}||[]};
+
+      ## This is a bit messy, but there are so many permutations!
+      if ($assembly) {
+        if ($current_species eq $hub->param('species')) {
+          $flag_info->{'species'}{'this'} = 1;
+          if ($is_old) {
+            $flag_info->{'assembly'}{'this_old'} = 1;
+          }
+          else {
+            $flag_info->{'assembly'}{'this_new'} = 1;
+          }
+        }
+        else {
+          $flag_info->{'species'}{'other'}++;
+          if ($is_old) {
+            $flag_info->{'assembly'}{'other_old'} = 1;
+          }
+          else {
+            $flag_info->{'assembly'}{'other_new'} = 1;
+          }
+          unless ($is_old) {
+            my $data = $session->add_data(
+              type        => 'url',
+              code        => join('_', md5_hex($name . $current_species . $assembly . $url), $session->session_id),
+              url         => $url,
+              name        => $name,
+              format      => $format->name,
+              style       => $format->trackline,
+              species     => $current_species,
+              assembly    => $assembly,
+              timestamp   => time,
+              %$options,
+            );
+
+            $session->configure_user_data('url', $data);
+
+            if ($current_species eq $hub->param('species')) {
+              $code = $data->{'code'};
+            }
+
+            $object->move_to_user(type => 'url', code => $data->{'code'}) if $hub->param('save');
+          }
+        }
+      }
+      ## For datahubs, work out what feedback we need to give the user
+      my ($species_flag, $assembly_flag);
+      if ($flag_info->{'species'}{'other'} && !$flag_info->{'species'}{'this'}) {
+        $species_flag = 'other_only';
+      }
+
+      if ($flag_info->{'assembly'}{'this_new'} && $flag_info->{'assembly'}{'this_old'}) {
+        $assembly_flag = 'old_and_new';
+      }
+      elsif (!$flag_info->{'assembly'}{'this_new'} && !$flag_info->{'assembly'}{'other_new'}) {
+        $assembly_flag = 'old_only';
+      }
+
+      $url_params->{'format'}         = $format->name;
+      $url_params->{'type'}           = 'url';
+      $url_params->{'name'}           = $name;
+      $url_params->{'species'}        = $hub->param('species');
+      $url_params->{'species_flag'}   = $species_flag;
+      $url_params->{'assembly_flag'}  = $assembly_flag;
+      $url_params->{'code'}           = $code;
+    }
+  }          
+  return $url_params;
 }
 
 sub set_format {
