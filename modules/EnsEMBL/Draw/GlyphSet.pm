thomasmaurel/ensembl-webcode

Load new Arc glyph

modules/EnsEMBL/Draw/GlyphSet.pm
@@ -42,6 +42,7 @@ use Sanger::Graphics::Glyph::Rect;
 use Sanger::Graphics::Glyph::Space;
 use Sanger::Graphics::Glyph::Sprite;
 use Sanger::Graphics::Glyph::Text;
+use Sanger::Graphics::Glyph::Arc;
 
 use Bio::EnsEMBL::Registry;
 
@@ -131,6 +132,7 @@ sub Space      { my $self = shift; return Sanger::Graphics::Glyph::Space->new(@_
 sub Sprite     { my $self = shift; return Sanger::Graphics::Glyph::Sprite->new(@_);     }
 sub Text       { my $self = shift; return Sanger::Graphics::Glyph::Text->new(@_);       }
 sub Triangle   { my $self = shift; return Sanger::Graphics::Glyph::Triangle->new(@_);   }
+sub Arc        { my $self = shift; return Sanger::Graphics::Glyph::Arc->new(@_);   }
 
 sub render {
   my $self   = shift;

Ensembl/ensembl-compara

modules/EnsEMBL/Draw/GlyphSet.pm
@@ -41,6 +41,7 @@ use EnsEMBL::Draw::Glyph::Rect;
 use EnsEMBL::Draw::Glyph::Space;
 use EnsEMBL::Draw::Glyph::Sprite;
 use EnsEMBL::Draw::Glyph::Text;
+use EnsEMBL::Draw::Glyph::Arc;
 
 use Bio::EnsEMBL::Registry;
 use Bio::EnsEMBL::DnaDnaAlignFeature;
@@ -240,6 +241,7 @@ sub Space      { my $self = shift; return EnsEMBL::Draw::Glyph::Space->new(@_); 
 sub Sprite     { my $self = shift; return EnsEMBL::Draw::Glyph::Sprite->new(@_);     }
 sub Text       { my $self = shift; return EnsEMBL::Draw::Glyph::Text->new(@_);       }
 sub Triangle   { my $self = shift; return EnsEMBL::Draw::Glyph::Triangle->new(@_);   }
+sub Arc        { my $self = shift; return EnsEMBL::Draw::Glyph::Arc->new(@_);   }
 
 sub _init {
 ### _init creates masses of Glyphs from a data source.