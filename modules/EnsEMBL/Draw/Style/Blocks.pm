34dd34d733ae94b3f0b08e3ba110d71fd98a71e4
Anne Lyle <annelyle@ebi.ac.uk>
2015-04-23 16:49:31 +0100

    Implement bumping - probably needs some factoring out, but will do for now!

--- modules/EnsEMBL/Draw/Style/Blocks.pm
+++ modules/EnsEMBL/Draw/Style/Blocks.pm
@@ -32,6 +32,7 @@ This module expects data in the following format:
             {
               'start'         => 123456,
               'end'           => 123789,
+              'strand'        => 1,
               'colour'        => 'red',
               'href'          => '/Location/View?r=123456-124789',  # optional  
               'label'         => 'Feature 1',                       # optional
@@ -40,6 +41,7 @@ This module expects data in the following format:
             {
               'start'         => 123654,
               'end'           => 123987,
+              'strand'        => 1,
               'colour'        => 'blue',
               'href'         => '/Location/View?r=123654-124987',   # optional  
               'label'         => 'Feature 2',                       # optional
@@ -50,6 +52,8 @@ This module expects data in the following format:
 
 use strict;
 use warnings;
+
+use EnsEMBL::Draw::Utils::Bump qw(bump);
 
 use parent qw(EnsEMBL::Draw::Style);
 
@@ -63,21 +67,37 @@ sub create_glyphs {
   
   foreach my $block (@$data) {
     my $text_info = $self->get_text_info($block->{'label'});
+    my $row = 0;
+    my $new_y;
 
-    ## Feature (non-bumped)
-    my $height = $self->track_config->{'height'} || ($text_info->{'height'} + 2);
-    $self->draw_block($block, {'height' => $height});
-
-    ## Optional label (needs to be bumped)
-    if ($track_config->get('has_labels') && $block->{'label'}) {
-      my $position = {
-                        'y'       => $height + 4, 
-                        'width'   => $text_info->{'width'}, 
-                        'height'  => $text_info->{'height'},
-                      };
-      $self->add_label($block, $position);
+    if ($track_config->get('bumped')) {
+      ## Set bumping based on longest of feature and label
+      my $end       = $block->{'end'};
+      my $text_end  = $block->{'start'} + ($text_info->{'width'} / $self->{'pix_per_bp'});
+      $end          = $text_end if $text_end > $end;
+      my @coords    = $block->{'strand'} == 1 ? ($block->{'start'}, $end)
+                                              : ($end, $block->{'start'});
+      $row = bump($self->bump_tally, @coords);
     }
 
+    ## Feature
+    my $height    = $track_config->get('height') || ($text_info->{'height'} + 2);
+    my $position  = {
+                    'y'       => ($row + 1) * ($height + 4) + ($row * $text_info->{'height'}),
+                    'height'  => $height,
+                    };
+    $self->draw_block($block, $position);
+
+    ## Optional label
+    if ($track_config->get('has_labels') && $block->{'label'}) {
+      $new_y = $position->{'y'} + $height;
+      $position = {
+                    'y'       => $new_y,
+                    'width'   => $text_info->{'width'}, 
+                    'height'  => $text_info->{'height'},
+                  };
+      $self->add_label($block, $position);
+    }
   }
 
   return @{$self->glyphs||[]};
@@ -93,7 +113,7 @@ sub draw_block {
   ## Set parameters
   my $params = {
                   x            => $block->{'start'},
-                  y            => 0,
+                  y            => $position->{'y'},
                   width        => $block->{'end'} - $block->{'start'} + 1,
                   height       => $position->{'height'},
                   colour       => $block->{'colour'},
