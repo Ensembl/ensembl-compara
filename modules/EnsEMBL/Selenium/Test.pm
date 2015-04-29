d2741f9c0f5ece3bd3b8b1db65353f7b85dcbf57
Anne Lyle <annelyle@ebi.ac.uk>
2015-04-29 10:58:33 +0100

    Don't instantiate the Selenium object here - create on in test script and pass it in

--- modules/EnsEMBL/Selenium/Test.pm
+++ modules/EnsEMBL/Selenium/Test.pm
@@ -24,9 +24,10 @@ use strict;
 
 use LWP::UserAgent;
 use Time::HiRes;
-use EnsEMBL::Selenium;
 use Test::Exception;
 use Test::More "no_plan";
+
+use EnsEMBL::Selenium;
 
 my $TESTMORE_OUTPUT;
 
@@ -37,24 +38,12 @@ sub new {
   
   my $self = {
     _url      => $args{url},
-    _host     => $args{host},
-    _port     => $args{port},
-    _browser  => $args{browser},
-    _ua       => $args{ua} || LWP::UserAgent->new(keep_alive => 5, env_proxy => 1),
-    _conf     => $args{conf},
-    _timeout  => $args{conf}{timeout} || 50000,
+    _sel      => $args{selenium},
+    _timeout  => $args{timeout} || 50000,
     _verbose  => $args{verbose},
     _species  => $args{species},
   };
     
-  $self->{_sel} = EnsEMBL::Selenium->new( 
-    _ua         => $self->{_ua},
-    host        => $self->{_host},
-    port        => $self->{_port},
-    browser     => $self->{_browser},
-    browser_url => $self->{_url},
-  );
-
   bless $self, $class;
   
   # redirect Test::More output unless we're in verbose mode
@@ -64,19 +53,11 @@ sub new {
 }
 
 sub url     {$_[0]->{_url}};
-sub host    {$_[0]->{_host}};
-sub port    {$_[0]->{_port}};
-sub browser {$_[0]->{_browser}};
-sub ua      {$_[0]->{_ua}};
 sub sel     {$_[0]->{_sel}};
 sub verbose {$_[0]->{_verbose}};
 sub species {$_[0]->{_species}};
+sub timeout {$_[0]->{_timeout}};
 
-
-sub conf {
-  my ($self, $key) = @_;
-  return $self->{_conf}->{$key};
-}
 
 sub testmore_output {
   # test builder output (this will be empty if we are in verbose mode)
