bef6c2a4a83a624586ace8c992fa93b0d20d5cd9
Dan Bolser <dbolser@ebi.ac.uk>
2015-04-30 12:30:46 +0100

    adding capacities from EnsemblProteinTrees_conf.pm

--- modules/Bio/EnsEMBL/Compara/PipeConfig/Example/EGProteinTrees_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/Example/EGProteinTrees_conf.pm
@@ -169,6 +169,11 @@ sub default_options {
         'decision_capacity'         =>   4,
         'HMMer_classify_capacity'   => 400,
         'loadmembers_capacity'      =>  30,
+        'HMMer_classifyPantherScore_capacity'   => 1000,
+        'copy_trees_capacity'       => 50,
+        'copy_alignments_capacity'  => 50,
+        'mafft_update_capacity'     => 50,
+        'raxml_update_capacity'     => 50,
 
     # hive priority for non-LOCAL health_check analysis:
 
