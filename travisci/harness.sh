2a1f647cd1b20a2cd98a49e9fce08320cf27fc91
Matthieu Muffato <muffato@ebi.ac.uk>
2015-05-12 00:39:32 +0100

    Let's run CoverAlls on 5.14 to include ensembl-rest's tests in the coverage report

--- travisci/harness.sh
+++ travisci/harness.sh
@@ -16,14 +16,13 @@ fi
 
 echo "We are running Perl '$TRAVIS_PERL_VERSION'"
 if [[ "$TRAVIS_PERL_VERSION" < "5.14" ]]; then
-  echo "Skipping ensembl-rest test suite on Perl $TRAVIS_PERL_VERSION"
+  echo "Skipping ensembl-rest test suite"
 else
   export PERL5LIB=$PERL5LIB:$PWD/ensembl-variation/modules:$PWD/ensembl-funcgen/modules:$PWD/ensembl-io/modules:$PWD/ensembl-rest/lib
   echo "Running ensembl-rest test suite using $PERL5LIB"
   REST_SCRIPTS=("$PWD/ensembl-rest/t/genomic_alignment.t" "$PWD/ensembl-rest/t/info.t" "$PWD/ensembl-rest/t/taxonomy.t")
   if [ "$COVERALLS" = 'true' ]; then
-    #PERL5OPT='-MDevel::Cover=+ignore,bioperl,+ignore,ensembl,+ignore,ensembl-test,+ignore,ensembl-variation,+ignore,ensembl-io,+ignore,ensembl-funcgen' perl $PWD/ensembl-test/scripts/runtests.pl -verbose "${REST_SCRIPTS[@]}"
-    perl $PWD/ensembl-test/scripts/runtests.pl "${REST_SCRIPTS[@]}"
+    PERL5OPT='-MDevel::Cover=+ignore,bioperl,+ignore,ensembl,+ignore,ensembl-test,+ignore,ensembl-variation,+ignore,ensembl-io,+ignore,ensembl-funcgen' perl $PWD/ensembl-test/scripts/runtests.pl -verbose "${REST_SCRIPTS[@]}"
   else
     perl $PWD/ensembl-test/scripts/runtests.pl "${REST_SCRIPTS[@]}"
   fi
