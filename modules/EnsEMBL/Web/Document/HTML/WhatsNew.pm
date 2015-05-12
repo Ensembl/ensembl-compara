ff5719d7ce21ecdc75b24240695fd3bb3ff36bfa
Anne Lyle <annelyle@ebi.ac.uk>
2015-05-12 14:58:01 +0100

    'Archive' is now a subtype, not site type

--- modules/EnsEMBL/Web/Document/HTML/WhatsNew.pm
+++ modules/EnsEMBL/Web/Document/HTML/WhatsNew.pm
@@ -60,7 +60,7 @@ sub render {
   }
   $html .= $blog if $blog;
 
-  return if ($species_defs->ENSEMBL_SITETYPE eq 'Archive');
+  return if ($species_defs->ENSEMBL_SUBTYPE eq 'Archive');
   
   $html .= $self->show_twitter();
 
