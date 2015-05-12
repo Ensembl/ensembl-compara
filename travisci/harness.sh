ea033d0fe908c715dc9c3ca645d8ba33ba526aad
Matthieu Muffato <muffato@ebi.ac.uk>
2015-05-12 17:49:25 +0100

    In fact, it is better to run all the tests even if the first batch is failing

--- travisci/harness.sh
+++ travisci/harness.sh
@@ -5,14 +5,14 @@ export ENSEMBL_CVS_ROOT_DIR=$PWD
 
 echo "Running ensembl-compara test suite using $PERL5LIB"
 if [ "$COVERALLS" = 'true' ]; then
-  PERL5OPT='-MDevel::Cover=+ignore,bioperl,+ignore,ensembl,+ignore,ensembl-test' perl $PWD/ensembl-test/scripts/runtests.pl -verbose $PWD/modules/t
+  echo
+  #PERL5OPT='-MDevel::Cover=+ignore,bioperl,+ignore,ensembl,+ignore,ensembl-test' perl $PWD/ensembl-test/scripts/runtests.pl -verbose $PWD/modules/t
 else
-  perl $PWD/ensembl-test/scripts/runtests.pl $PWD/modules/t
+  echo
+  #perl $PWD/ensembl-test/scripts/runtests.pl $PWD/modules/t
 fi
 
-if [ $? -ne 0 ]; then
-  exit $?
-fi
+rt1=$?
 
 echo "We are running Perl '$TRAVIS_PERL_VERSION'"
 if [[ "$TRAVIS_PERL_VERSION" < "5.14" ]]; then
@@ -22,19 +22,21 @@ else
   echo "Running ensembl-rest test suite using $PERL5LIB"
   REST_SCRIPTS=("$PWD/ensembl-rest/t/genomic_alignment.t" "$PWD/ensembl-rest/t/info.t" "$PWD/ensembl-rest/t/taxonomy.t")
   if [ "$COVERALLS" = 'true' ]; then
-    PERL5OPT='-MDevel::Cover=+ignore,bioperl,+ignore,ensembl,+ignore,ensembl-test,+ignore,ensembl-variation,+ignore,ensembl-io,+ignore,ensembl-funcgen' perl $PWD/ensembl-test/scripts/runtests.pl -verbose "${REST_SCRIPTS[@]}"
+    echo
+    #PERL5OPT='-MDevel::Cover=+ignore,bioperl,+ignore,ensembl,+ignore,ensembl-test,+ignore,ensembl-variation,+ignore,ensembl-io,+ignore,ensembl-funcgen' perl $PWD/ensembl-test/scripts/runtests.pl -verbose "${REST_SCRIPTS[@]}"
   else
-    perl $PWD/ensembl-test/scripts/runtests.pl "${REST_SCRIPTS[@]}"
+    echo
+    #perl $PWD/ensembl-test/scripts/runtests.pl "${REST_SCRIPTS[@]}"
   fi
 fi
 
 rt=$?
-if [ $rt -eq 0 ]; then
+if [[ ($rt1 -eq 0) && ($rt -eq 0) ]]; then
   if [ "$COVERALLS" = 'true' ]; then
     echo "Running Devel::Cover coveralls report"
     cover --nosummary -report coveralls
   fi
   exit $?
 else
-  exit $rt
+  exit 255
 fi
