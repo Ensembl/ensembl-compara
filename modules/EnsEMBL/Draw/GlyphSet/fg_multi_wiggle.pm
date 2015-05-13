0b295e9cb13440cabc5c42f774e624b4c120b89c
Dan Sheppard <dan@ebi.ac.uk>
2015-05-13 18:05:32 +0100

    ENSWEB-1303: Don't show two wiggle&block miniboxes.
    Reinstate regression of mysteriously disappeared if-statement.

--- modules/EnsEMBL/Draw/GlyphSet/fg_multi_wiggle.pm
+++ modules/EnsEMBL/Draw/GlyphSet/fg_multi_wiggle.pm
@@ -123,8 +123,10 @@ sub draw_blocks {
     $self->draw_track_name($label, $colour, -108, 0, 'no_offset');
     $self->draw_block_features ($features, $colour, $f_set, 1, 1);
   }
-  $self->_add_sublegend(undef,"More","Links",$zmenu_extra_content,
-                        $self->_offset+2);
+  if(defined $zmenu_extra_content) {
+    $self->_add_sublegend(undef,"More","Links",$zmenu_extra_content,
+                          $self->_offset+2);
+  }
 
   $self->draw_space_glyph;
 }
