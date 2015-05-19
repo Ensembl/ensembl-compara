878065e04b428b6c53de3230ac5ec052346a4a6d
Anne Lyle <annelyle@ebi.ac.uk>
2015-05-19 14:56:08 +0100

    Tidied up error log format

--- utils/selenium/run_tests.pl
+++ utils/selenium/run_tests.pl
@@ -325,11 +325,16 @@ sub write_to_log {
 
   my ($sec, $min, $hour, $day, $month, $year) = gmtime;
   my $timestamp = sprintf('at %02d:%02d:%02d on %02d-%02d-%s', $hour, $min, $sec, $day, $month+1, $year+1900);
+  
+  my $message = uc($code);
+  $message    .= " in $module" if $module;
+  $message    .= "::$method" if $method;
+  $message    .= "- $message $timestamp\n";
   if ($log) {
-    print $log uc($code)." in $module::$method - $message $timestamp\n";
+    print $log $message;
   }
   else {
-    print uc($code)." in $module::$method - $message $timestamp\n";
+    print $message; 
   }
 }
 
