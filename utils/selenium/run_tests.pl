c3a743f558fbe495502de1eb97701a14de1fde11
Anne Lyle <annelyle@ebi.ac.uk>
2015-05-19 14:52:43 +0100

    Tweaked if statement to ensure correct reports get written to log

--- utils/selenium/run_tests.pl
+++ utils/selenium/run_tests.pl
@@ -297,7 +297,7 @@ sub run_test {
         my @response = ($object->$method(@params));
         foreach (@response) {
           if (ref($_) eq 'ARRAY') {
-            if ($_->[0] eq 'fail' || $config->{'verbose'}) {
+            if ($config->{'verbose'} || $_->[0] ne 'pass') { 
               write_to_log(@$_);
               $_->[0] eq 'pass' ? $pass++ : $fail++;
             }
