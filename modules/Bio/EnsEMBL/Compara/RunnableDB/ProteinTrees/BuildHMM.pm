bb1d2a4624b7476de30665574146ccaf95d0a4db
Matthieu Muffato <muffato@ebi.ac.uk>
2015-06-18 18:40:08 +0100

    The name of the HMM is the stable ID, or the root_id_

--- modules/Bio/EnsEMBL/Compara/RunnableDB/ProteinTrees/BuildHMM.pm
+++ modules/Bio/EnsEMBL/Compara/RunnableDB/ProteinTrees/BuildHMM.pm
@@ -179,7 +179,7 @@ sub run_buildhmm {
             $self->param('hmmbuild_exe'),
             ($self->param('cdna') ? ($self->param_required('hmmer_version') eq '2' ? '--nucleic' : '--dna') : '--amino'),
             $self->param_required('hmmer_version') eq '2' ? '-F' : '',
-            '-n', $self->param('gene_tree_id'),
+            '-n', $self->param('protein_tree')->stable_id || $self->param('gene_tree_id'),
             $hmm_file,
             $aln_file
     );
