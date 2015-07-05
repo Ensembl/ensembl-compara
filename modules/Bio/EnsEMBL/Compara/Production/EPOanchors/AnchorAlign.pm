72796a780895de313caedcd84da28baab6a33828
Matthieu Muffato <muffato@ebi.ac.uk>
2015-07-05 21:35:43 +0100

    AnchorAlign is now a Locus

--- modules/Bio/EnsEMBL/Compara/Production/EPOanchors/AnchorAlign.pm
+++ modules/Bio/EnsEMBL/Compara/Production/EPOanchors/AnchorAlign.pm
@@ -38,7 +38,7 @@ use warnings;
 use Bio::EnsEMBL::Utils::Argument qw(rearrange);
 use Bio::EnsEMBL::Compara::DnaFrag;
 
-use base ('Bio::EnsEMBL::Storable');        # inherit dbID(), adaptor() and new() methods
+use base qw(Bio::EnsEMBL::Compara::Locus Bio::EnsEMBL::Storable);
 
 
 sub new {
@@ -46,24 +46,17 @@ sub new {
 
     my $self = $class->SUPER::new(@args);       # deal with Storable stuff
 
-    ## First lines are for backward compatibility, middle one is for both versions and
-    ## last ones are for the new schema
+    ## First line is for backward compatibility with an old schema
+    ## last is are for the new schema
     my ($method_link_species_set, $method_link_species_set_id, $anchor_id,
-        $dnafrag, $dnafrag_id, $dnafrag_start, $dnafrag_end, $dnafrag_strand,
         $score, $num_of_organisms, $num_of_sequences, $evalue, $anchor_status) =
       rearrange([qw(
           METHOD_LINK_SPECIES_SET METHOD_LINK_SPECIES_SET_ID ANCHOR_ID
-          DNAFRAG DNAFRAG_ID DNAFRAG_START DNAFRAG_END DNAFRAG_STRAND
           SCORE NUM_OF_ORGANISMS NUM_OF_SEQUENCES EVALUE ANCHOR_STATUS)], @args);
 
     $self->method_link_species_set($method_link_species_set) if (defined($method_link_species_set));
     $self->method_link_species_set_id($method_link_species_set_id) if (defined($method_link_species_set_id));
     $self->anchor_id($anchor_id) if (defined($anchor_id));
-    $self->dnafrag($dnafrag) if (defined($dnafrag));
-    $self->dnafrag_id($dnafrag_id) if (defined($dnafrag_id));
-    $self->dnafrag_start($dnafrag_start) if (defined($dnafrag_start));
-    $self->dnafrag_end($dnafrag_end) if (defined($dnafrag_end));
-    $self->dnafrag_strand($dnafrag_strand) if (defined($dnafrag_strand));
     $self->score($score) if (defined($score));
     $self->num_of_organisms($num_of_organisms) if (defined($num_of_organisms));
     $self->num_of_sequences($num_of_sequences) if (defined($num_of_sequences));
@@ -107,51 +100,6 @@ sub evalue {
   return $self->{_evalue};
 }
 
-sub dnafrag {
-  my $self = shift;
-  if (@_) {
-    $self->{_dnafrag} = shift;
-  } elsif (!$self->{_dnafrag} and $self->{_dnafrag_id} and $self->{adaptor}) {
-    $self->{_dnafrag} = $self->{adaptor}->db->get_DnaFragAdaptor->fetch_by_dbID($self->{_dnafrag_id});
-  }
-  return $self->{_dnafrag};
-}
-
-sub dnafrag_id {
-  my $self = shift;
-  if (@_) {
-    $self->{_dnafrag_id} = shift;
-  }
-  if (!defined($self->{_dnafrag_id}) and defined($self->{_dnafrag})) {
-    $self->{_dnafrag_id} = $self->{_dnafrag}->dbID;
-  }
-  return $self->{_dnafrag_id};
-}
-
-sub dnafrag_start {
-  my $self = shift;
-  if (@_) {
-    $self->{_dnafrag_start} = shift;
-  }
-  return $self->{_dnafrag_start};
-}
-
-sub dnafrag_end {
-  my $self = shift;
-  if (@_) {
-    $self->{_dnafrag_end} = shift;
-  }
-  return $self->{_dnafrag_end};
-}
-
-sub dnafrag_strand {
-  my $self = shift;
-  if (@_) {
-    $self->{_dnafrag_strand} = shift;
-  }
-  return $self->{_dnafrag_strand};
-}
-
 sub score {
   my $self = shift;
   if (@_) {
@@ -188,8 +136,7 @@ sub seq {
   my $self = shift;
   return $self->{'_seq'} if ($self->{'_seq'});
 
-  my $seq = $self->dnafrag->slice()->subseq($self->{'_dnafrag_start'},
-      $self->{'_dnafrag_end'}, $self->{'_dnafrag_strand'});
+  my $seq = $self->get_Slice()->seq;
   $self->{'_seq'} = $seq;
 
   return $self->{'_seq'};
