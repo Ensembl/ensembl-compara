aea5e0a6ca9da83ba7b45146fb119d4d9edd24ed
Dan Sheppard <dan@ebi.ac.uk>
2015-07-09 16:58:51 +0100

    ENSWEB-1409: run togglewrap on pager changes.

--- htdocs/components/05_DataTable.js
+++ htdocs/components/05_DataTable.js
@@ -113,6 +113,7 @@ Ensembl.DataTable = {
         parent = null;
       },
       fnDrawCallback: function (tableSettings) {
+        this.togglewrap('redo');
         $('.dataTables_info, .dataTables_paginate, .dataTables_bottom', tableSettings.nTableWrapper)[tableSettings._iDisplayLength === -1 ? 'hide' : 'show']();
         
         var data          = this.data();
