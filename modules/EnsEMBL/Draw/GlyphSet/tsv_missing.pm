2c0e3faa1bbb3eb4e1daecdad370f74f0606be28
Fiona Cunningham <fcunningham@users.noreply.github.com>
2015-07-13 10:21:15 +0100

    Update tsv_missing.pm
    Changed variations to variants

--- modules/EnsEMBL/Draw/GlyphSet/tsv_missing.pm
+++ modules/EnsEMBL/Draw/GlyphSet/tsv_missing.pm
@@ -45,7 +45,7 @@ sub _init {
   return unless ($self->strand() == -1);
 
 
-  my $configure_text = "The 'Configure this page' link in the menu on the left hand side of this page can be used to customise the exon context and types of variations displayed above.";
+  my $configure_text = "The 'Configure this page' link in the menu on the left hand side of this page can be used to customise the exon context and types of variants displayed above.";
   $self->errorTrack( $configure_text);
 
   my $counts = $self->{'config'}->{'snp_counts'};
@@ -55,11 +55,11 @@ sub _init {
   if ($counts->[0]==0 ) {
     $text .= "There are no SNPs within the context selected for this transcript.";
   } elsif ($counts->[1] ==0 ) {
-    $text .= "The options set in the page configuration have filtered out all $counts->[0] variations in this region.";
+    $text .= "The options set in the page configuration have filtered out all $counts->[0] variants in this region.";
   } elsif ($counts->[0] == $counts->[1] ) {
-    $text .= "None of the variations are filtered out by the Source, Class and Type filters.";
+    $text .= "None of the variants are filtered out by the Source, Class and Type filters.";
   } else {
-    $text .= ($counts->[0]-$counts->[1])." of the $counts->[0] variations in this region have been filtered out by the Source, Class and Type filters.";
+    $text .= ($counts->[0]-$counts->[1])." of the $counts->[0] variants in this region have been filtered out by the Source, Class and Type filters.";
   }
   $self->errorTrack( $text, 0, 14  );
 
@@ -68,13 +68,13 @@ sub _init {
 
   my $context_text;
   if ($counts->[2]==0) {
-    $context_text = "None of the intronic variations are removed by the Context filter.";
+    $context_text = "None of the intronic variants are removed by the Context filter.";
   }
   elsif ($counts->[2]==1) {
-    $context_text = $counts->[2]." intronic variation has been removed by the Context filter.";
+    $context_text = $counts->[2]." intronic variants have been removed by the Context filter.";
   }
  else {
-    $context_text = $counts->[2]." intronic variations are removed by the Context filter.";
+    $context_text = $counts->[2]." intronic variants are removed by the Context filter.";
   }
   $self->errorTrack( $context_text, 0, 28 );
 
