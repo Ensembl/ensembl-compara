409c26eb107add3cd5619e822260629715a53d7e
Anne Lyle <annelyle@ebi.ac.uk>
2015-07-24 10:18:52 +0100

    Bump features by default and start using metadata

--- modules/EnsEMBL/Draw/GlyphSet/flat_file.pm
+++ modules/EnsEMBL/Draw/GlyphSet/flat_file.pm
@@ -72,15 +72,18 @@ sub features {
 sub render_normal {
   my $self = shift;
 
-  #$self->{'my_config'}->set('bumped', 1);
-
-  $self->{'my_config'}->set('default_colour', $self->{'_default_colour'});
+  $self->{'my_config'}->set('bumped', 1);
 
   my $subtracks = $self->features;
   my $config    = $self->track_style_config;
+  #$config->set('default_colour', $self->{'_default_colour'});
 
   foreach (@$subtracks) {
     my $features  = $_->{'features'};
+    my $metadata  = $_->{'metadata'};
+    my $name      = $metadata->{'name'};
+    $config->{'track_config'}->set('caption', $name) if $name;
+
     my $style     = EnsEMBL::Draw::Style::Blocks->new($config, $features);
     $self->push($style->create_glyphs);
   }
