16ace63a559be9006c0de9eb1315fb3ce6c81705
Anne Lyle <annelyle@ebi.ac.uk>
2015-07-29 11:38:30 +0100

    Fixed package name, and added create_glyphs method

--- modules/EnsEMBL/Draw/Style/Feature/Interaction.pm
+++ modules/EnsEMBL/Draw/Style/Feature/Interaction.pm
@@ -16,7 +16,7 @@ limitations under the License.
 
 =cut
 
-package EnsEMBL::Draw::Style::Feature::Structured;
+package EnsEMBL::Draw::Style::Feature::Interaction;
 
 ### Renders a track as a series of features with internal structure
 ### Blocks may be joined with horizontal lines, semi-transparent
@@ -24,101 +24,107 @@ package EnsEMBL::Draw::Style::Feature::Structured;
 
 use parent qw(EnsEMBL::Draw::Style::Feature);
 
+
+sub create_glyphs {
+### A much-simplified version of the parent method, but necessary
+### because arc-rendering is very different from normal tracks
+  my $self = shift;
+
+  my $data            = $self->data;
+  my $image_config    = $self->image_config;
+  my $track_config    = $self->track_config;
+  ## Set some track-wide variables
+  my $default_colour  = $track_config->get('default_colour');
+  my $feature_height  = $track_config->get('height');
+  my $slice_width     = $image_config->container_width;
+
+  foreach my $feature (@$data) {
+
+    ## Set default colour if there is one
+    my $colour      = $feature->{'colour'} || $default_colour;
+
+    my %defaults = {
+                    'y'           => 0,
+                    'height'      => $feature_height,
+                    'colour'      => $colour,
+                    'absolute_y'  => 1,
+                    };
+
+    $self->draw_feature($feature, %defaults);
+
+  }
+  return @{$self->glyphs||[]};
+}
+
 sub draw_feature {
 ### Create each "feature" as a set of glyphs: blocks plus joins
 ### @param feature Hashref - data for a single feature
 ### @param position Hashref - information about the feature's size and position
-  my ($self, $feature, $position) = @_;
+  my ($self, $feature, %defaults) = @_;
 
-  ## In case we're trying to draw a feature with no internal structure,
-  ## revert to parent method, which is much simpler!
   my $structure = $feature->{'structure'};
-  if (!$structure) {
-    $self->SUPER::draw_feature($feature, $position);
-  }
+  return unless $structure;
 
   ## Basic parameters for all parts of the feature
+  my $image_width   = $self->image_config->container_width; 
   my $feature_start = $feature->{'start'};
   my $current_x     = $feature_start;
   $current_x        = 0 if $current_x < 0;
 
-  my $colour      = $feature->{'colour'};
-  my $join_colour = $feature->{'join_colour'} || $feature->{'bordercolour'} || $colour;
+  my ($block_1, $block_2) = @{$structure};
 
-  my $track_config  = $self->track_config;
-  my $join          = $track_config->get('no_join') ? 0 : 1;
-
-  my %defaults = (
-                  y            => $position->{'y'},
-                  height       => $position->{'height'},
-                  href         => $feature->{'href'},
-                  title        => $feature->{'title'},
-                  absolutey    => 1,
-                );
-
-  my $image_width = $position->{'image_width'};
-  my %previous;
-
-  foreach (@$structure) {
-
-    my $last_element = 0;
-
-    ## Join this block to the previous one
-    if ($join && keys %previous) {
-      my %params        = %defaults;
-      $params{'join_colour'}  = $join_colour;
-
-      my $start         = $previous{'x'} + $previous{'width'};
-      $params{'x'}      = $start;
-      my $end           = $_->{'start'};
-      my $width         = $end - $start;
-      if ($end > $image_width) {
-        $width          = $image_width - $start;
-        $last_element   = 1;
-      }
-      $params{'width'}        = $width;
-
-      $self->draw_join(%params);      
-    }
-    last if $last_element;
-
-    ## Now draw the next chunk of structure
-    my %params = %defaults;
-
-    my $start = $_->{'start'};
-    my $end   = $_->{'end'};
-    $params{'x'}      = $start;
-    $params{'width'}  = $end - $start;
-
-    ## Only draw blocks that appear on the image!
-    unless ($end < 0 || $start > $position->{'image_width'}) {
-      $params{'colour'} = $colour;
-      $self->draw_block($feature, %params);
-    }
-    $current_x += $width;
-    %previous = %params;
+  ## Draw first block
+  unless ($block_1->{'end'} < 0) { 
+    my %params        = %defaults;
+    $params{'x'}      = $block_1->{'start'};
+    $params{'width'}  = $block_1->{'end'} - $block_1->{'start'};
+    $self->draw_block(%params);
   }
 
+=pod
+  ## Draw arc
+
+  ## Default behaviour is to draw arc from middles of features
+  ## Of course for the arcs we have to use the real coordinates, 
+  ## not the ones constrained to the viewport
+  my $arc_start       = $s1 == $e1 ? $s1 - 0.5
+                                : $s1 + ceil(($e1 - $s1) / 2);
+  my $arc_end         = $s2 == $e2 ? $s2 - 0.5
+                                : $s2 + floor(($e2 - $s2) / 2);
+
+  my $direction_1     = $f->direction_1;
+  my $direction_2     = $f->direction_2;
+  if ($direction_1 || $direction_2) {
+    if ($direction_1 =~ /\+/) {
+       $arc_start  = $s1 == $e1 ? $s1 - 1 : $s1;
+       $arc_end    = $s2 == $e2 ? $s2 - 1 : $s2;
+    }
+    else {
+      $arc_start = $e1;
+      $arc_end   = $e2;
+    }
+  }
+  ## Don't show arcs if both ends lie outside viewport
+  next if ($arc_start < 0 && $arc_end > $length);
+=cut
+
+  ## Draw second block
+  unless ($block_2->{'start'} > $image_width) {
+    my %params        = %defaults;
+    $params{'x'}      = $block_2->{'start'};
+    $params{'width'}  = $block_2->{'end'} - $block_2->{'start'};
+    $self->draw_block(%params);
+  }
+}
+
+sub draw_block {
+  my ($self, %params) = @_;
+  push @{$self->glyphs}, $self->Rect(\%params);
 }
 
 sub draw_join {
   my ($self, %params) = @_;
-  my $alpha = $self->track_config->get('alpha');
-
-  if ($alpha) {
-    $params{'colour'} = $params{'join_colour'};
-    $params{'alpha'}  = $alpha;
-  }
-  else {
-    $params{'bordercolour'} = $params{'join_colour'};
-  }
-  delete $params{'join_colour'};
-  push @{$self->glyphs}, $self->Rect(\%params);
-}
-
-sub draw_block {
-  my ($self, $feature, %params) = @_;
-  push @{$self->glyphs}, $self->Rect(\%params);
+  push @{$self->glyphs}, $self->Arc(\%params);
 }
 
 1;
