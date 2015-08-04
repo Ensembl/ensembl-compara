8b2859777e519b983b329f4dd645e748a75e6ba2
Anne Lyle <annelyle@ebi.ac.uk>
2015-08-04 10:44:52 +0100

    New wrapper module - needed by upload validation

---
+++ modules/EnsEMBL/Web/IOWrapper/GFF3.pm
@@ -0,0 +1,31 @@
+=head1 LICENSE
+
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+package EnsEMBL::Web::IOWrapper::GFF3;
+
+### Wrapper for Bio::EnsEMBL::IO::Parser::GFF3, which builds
+### simple hash features suitable for use in the drawing code 
+
+use strict;
+use warnings;
+no warnings 'uninitialized';
+
+use parent qw(EnsEMBL::Web::IOWrapper);
+
+
+1;
