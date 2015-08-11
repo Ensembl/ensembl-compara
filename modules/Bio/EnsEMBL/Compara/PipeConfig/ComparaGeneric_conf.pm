7d51d72dca4004c456d2939a43f72c13453d9c49
Matthieu Muffato <muffato@ebi.ac.uk>
2015-08-11 12:50:59 +0100

    The source table is given below as an analysis-parameter

--- modules/Bio/EnsEMBL/Compara/PipeConfig/ComparaGeneric_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/ComparaGeneric_conf.pm
@@ -79,7 +79,7 @@ sub init_basic_tables_analyses {
             },
             -input_ids => $input_ids,
             -flow_into => {
-                ($target_analysis ? '2->A' : 2) => { 'copy_table' => { 'src_db_conn' => '#db_conn#', 'table' => '#table#' } },
+                ($target_analysis ? '2->A' : 2) => { 'copy_table' => { 'table' => '#table#' } },
                 ($target_analysis ? ( 'A->1' => [ $target_analysis ] ) : ()),
             },
         },
