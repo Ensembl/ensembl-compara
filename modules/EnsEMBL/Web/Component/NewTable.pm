aa9d0f41532b0a3aec9ad085bb63ff4825f868e2
Dan Sheppard <dan@ebi.ac.uk>
2015-08-17 14:21:48 +0100

    Other types of sort.

--- modules/EnsEMBL/Web/Component/NewTable.pm
+++ modules/EnsEMBL/Web/Component/NewTable.pm
@@ -21,15 +21,115 @@ package EnsEMBL::Web::Component::NewTable;
 use strict;
 
 use JSON qw(from_json);
+use Scalar::Util qw(looks_like_number);
+use List::MoreUtils qw(each_array);
 
 # XXX move all this stuff to somewhere it's more suited
 
-sub server_sort {
-  my ($self,$data,$sort,$cols) = @_;
+# XXX sort to end
 
-  my %col_idx;
+sub html_cleaned {
+  my ($x) = @_;
+
+  $x =~ s/<.*?>//g;
+  return $x; 
+}
+
+sub html_hidden {
+  my ($x) = @_;
+
+  return $1 if $x =~ m!<span class="hidden">(.*?)</span>!;
+  return $x;
+}
+
+sub num_if_num {
+  my ($a,$b) = @_;
+
+  if(looks_like_number($a)) {
+    if(looks_like_number($b)) {
+      return $a <=> $b;
+    } else {
+      return -1;
+    }
+  } elsif(looks_like_number($b)) {
+    return 1;
+  } else {
+    return $a cmp $b;
+  }
+}
+
+sub server_sort_numeric {
+  my ($self,$a,$b) = @_;
+
+  return $a <=> $b;
+}
+
+sub server_sort_position {
+  my ($self,$a,$b) = @_;
+
+  my @a = split(/:-/,$a);
+  my @b = split(/:-/,$b);
+  my $it = each_array(@a,@b);
+  while(my ($aa,$bb) = $it->()) {
+    my $c = num_if_num($aa,$bb);
+    return $c if $c; 
+  }
+  return 0;
+}
+
+sub server_sort_position_html {
+  my ($self,$a,$b) = @_;
+
+  return $self->server_sort_position(html_cleaned($a),html_cleaned($b));
+}
+
+sub server_sort_html_numeric {
+  my ($self,$a,$b) = @_;
+
+  return $self->server_sort_numeric(html_cleaned($a),html_cleaned($b));
+}
+
+sub server_sort_html {
+  my ($self,$a,$b) = @_;
+
+  return $self->server_sort_string(html_cleaned($a),html_cleaned($b));
+}
+
+sub server_sort_hidden_position {
+  my ($self,$a,$b) = @_;
+
+  return $self->server_sort_position(html_hidden($a),html_hidden($b));
+}
+
+sub server_sort_string {
+  my ($self,$a,$b) = @_;
+
+  return lc $a cmp lc $b;
+}
+
+sub server_sort_string_hidden {
+  my ($self,$a,$b) = @_;
+
+  return $self->server_sort_string(html_hidden($a),html_hidden($b));
+}
+
+sub server_sort_numeric_hidden {
+  my ($self,$a,$b) = @_;
+
+  return $self->server_sort_numeric(html_hidden($a),html_hidden($b));
+}
+
+sub server_sort {
+  my ($self,$data,$sort,$iconfig) = @_;
+
+  my (%col_idx,%sort_fn);
+  my $cols = $iconfig->{'columns'};
   foreach my $i (0..(@$cols-1)) {
     $col_idx{$cols->[$i]{'key'}} = $i;
+    ( my $fn = $cols->[$i]{'sort'} ) =~ s/[^A-Za-z_-]//g;
+    my $sort_fn = $self->can("server_sort_$fn");
+    $sort_fn = $self->can("server_sort_string") unless defined $sort_fn;
+    $sort_fn{$cols->[$i]{'key'}} = $sort_fn;
   }
   @$data = sort {
     my $c = 0;
@@ -38,7 +138,7 @@ sub server_sort {
       ($aa,$bb) = ($b,$a) if $col->{'dir'} < 0;
       my $av = $aa->[$col_idx{$col->{'key'}}];
       my $bv = $bb->[$col_idx{$col->{'key'}}];
-      $c = ( $av cmp $bv );
+      $c = $sort_fn{$col->{'key'}}->($self,$av,$bv);
       last if $c;
     }
     $c;
@@ -58,8 +158,6 @@ sub ajax_table_content {
   my $iconfig = from_json($hub->param('config'));
 
   my $view = from_json($hub->param('data'));
-  use Data::Dumper;
-  warn Dumper('view',$view);
 
   # Check if we need to request all rows due to sorting
   my $all_data = 0;
@@ -99,7 +197,7 @@ sub ajax_table_content {
 
   # Sort it, if necessary
   if($view->{'sort'} and @{$view->{'sort'}}) {
-    $self->server_sort(\@data_out,$view->{'sort'},$iconfig->{'columns'});
+    $self->server_sort(\@data_out,$view->{'sort'},$iconfig);
     splice(@data_out,0,$irows->[0]);
     splice(@data_out,$irows->[1]) if $irows->[1] >= 0;
   }
