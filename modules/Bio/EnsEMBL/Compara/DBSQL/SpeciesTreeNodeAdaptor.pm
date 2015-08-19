6884bec966ec256ea0b8f9f177a643afa7c3dd59
Matthieu Muffato <muffato@ebi.ac.uk>
2015-08-19 10:32:24 +0100

    If there is already a genome_db_id, it is much simpler :)

--- modules/Bio/EnsEMBL/Compara/DBSQL/SpeciesTreeNodeAdaptor.pm
+++ modules/Bio/EnsEMBL/Compara/DBSQL/SpeciesTreeNodeAdaptor.pm
@@ -89,7 +89,9 @@ sub new_from_NestedSet {
             }
 
             my $genomeDB;
-            if (defined $node->taxon_id) {
+            if ($node->genome_db_id) {
+                $genomeDB = $genomeDB_Adaptor->fetch_by_dbID($node->genome_db_id);
+            } elsif (defined $node->taxon_id) {
                 $genomeDB = $genomeDB_Adaptor->fetch_by_taxon_id($node->taxon_id);
             } elsif (defined $node->node_name) {
                 $genomeDB = $genomeDB_Adaptor->fetch_by_name_assembly($node->node_name);
