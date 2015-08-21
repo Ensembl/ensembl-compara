b38fc250844958ed8e86972281c30c9b4c4e6358
Harpreet Riat <harpreet@ebi.ac.uk>
2015-08-21 16:46:05 +0100

    $href should be a hash ref (linked to a5e1d6cb)

--- modules/EnsEMBL/Web/Document/Image/GD.pm
+++ modules/EnsEMBL/Web/Document/Image/GD.pm
@@ -181,18 +181,18 @@ sub add_pointers {
                     : $p_value_sorted->{sprintf("%.1f", $row->{'p_value'})};
     my $href = $row->{'href'};
     if (!$href && $extra->{'zmenu'}) {
-      $href = $hub->url({'type' => 'ZMenu', 'action' => $extra->{'zmenu'},
+      $href = {'type' => 'ZMenu', 'action' => $extra->{'zmenu'},
                         'config' => $extra->{'config_name'}, 'track' => $extra->{'track'},
                         'fake_click_chr' => $chr, 'fake_click_start' => $row->{'start'}, 
                         'fake_click_end' => $row->{'end'},
-                        });
+                        };
     }
     my $point = {
       start     => $row->{'start'},
       end       => $row->{'end'},
       id        => $row->{'label'},
       col       => $item_colour || $grad_colour || $default_colour,
-      href      => $hub->url({$config_name ? ('config' => $config_name) : (), %$href}),
+      href      => $href ? $hub->url({$config_name ? ('config' => $config_name) : (), %$href}) : '',
       html_id   => $row->{'html_id'} || '',
     };
 
