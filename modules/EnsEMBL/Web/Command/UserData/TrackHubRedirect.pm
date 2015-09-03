6de7190832813e5a7871b8027144806bf915302c
Anne Lyle <annelyle@ebi.ac.uk>
2015-09-03 13:49:37 +0100

    Refine redirect - open control panel if hub is usable on chosen species

--- modules/EnsEMBL/Web/Command/UserData/TrackHubRedirect.pm
+++ modules/EnsEMBL/Web/Command/UserData/TrackHubRedirect.pm
@@ -36,63 +36,74 @@ sub process {
   my @bits          = split /\./, $filename;
   my $extension     = $bits[-1] eq 'gz' ? $bits[-2] : $bits[-1];
   my $pattern       = "^$extension\$";
+  my ($redirect, $anchor);
+  my $params        = {};
 
   ## Allow for manually-created URLs with capitalisation, and 
   ## also validate any user-provided species name
   my $species       = $hub->param('species') || $hub->param('Species');
-  if (!$species || !$hub->species_defs->valid_species($species)) {
+  if (!$species) {
     $species = $species_defs->ENSEMBL_PRIMARY_SPECIES;
   }
-  my $location      = $hub->param('r') || $hub->param('location') || $hub->param('Location');
+  elsif (!$hub->species_defs->valid_species($species)) {
+    $redirect = '/Help/Trackhub';
+    $params->{'species'}  = $species;
+    $params->{'error'}    = 'not_valid_species';
+    $anchor               = 'modal';
+    $species              = undef;
+  }
 
-  my $redirect      = $hub->species_path($species) . '/UserData/';
-  my $new_action    = '';
-  my $params        = {};
+  if ($species) {
+    my $location    = $hub->param('r') || $hub->param('location') || $hub->param('Location');
 
+    $redirect       = $hub->species_path($species) . '/UserData/';
+    $anchor         = 'modal_config_viewbottom';
+    my $new_action  = '';
 
-  if ($url) {
-    ## Is this file already attached?
-    ($new_action, $params)  = $self->check_attachment($url);
+    if ($url) {
+      ## Is this file already attached?
+      ($new_action, $params)  = $self->check_attachment($url);
 
-    unless ($new_action) {
-      my $trackhub = EnsEMBL::Web::File::AttachedFormat::TRACKHUB->new('hub' => $self->hub, 'url' => $url);
-    
-      ($new_action, $params) = $self->attach($trackhub, $filename); 
-   }
+      unless ($new_action) {
+        my $trackhub = EnsEMBL::Web::File::AttachedFormat::TRACKHUB->new('hub' => $self->hub, 'url' => $url);
+     
+        ($new_action, $params) = $self->attach($trackhub, $filename); 
+      }
 
-    ## Override standard redirect with sample location
-    $redirect           = sprintf('/%s/Location/View', $species);
-    unless ($location) {
-      my $sample_links  = $species_defs->get_config($species, 'SAMPLE_DATA');
-      $location         = $sample_links->{'LOCATION_PARAM'} if $sample_links;
-    }
-    $params->{'r'} = $location;
+      ## Override standard redirect with sample location
+      $redirect           = sprintf('/%s/Location/View', $species);
+      unless ($location) {
+        my $sample_links  = $species_defs->get_config($species, 'SAMPLE_DATA');
+        $location         = $sample_links->{'LOCATION_PARAM'} if $sample_links;
+      }
+      $params->{'r'} = $location;
 
-    my %messages  = EnsEMBL::Web::Constants::USERDATA_MESSAGES;
-    my $p         = $params->{'reattach'} || $params->{'species_flag'} 
-                      || $params->{'assembly_flag'} || 'ok';
-    my $key       = sprintf('hub_%s', $p);
+      my %messages  = EnsEMBL::Web::Constants::USERDATA_MESSAGES;
+      my $p         = $params->{'reattach'} || $params->{'species_flag'} 
+                        || $params->{'assembly_flag'} || 'ok';
+      my $key       = sprintf('hub_%s', $p);
 
-    if ($messages{$key}) {
-      $hub->session->add_data(
-        type     => 'message',
-        code     => 'AttachURL',
-        message  => $messages{$key}{'message'},
-        function => '_'.$messages{$key}{'type'},
-      );
-    }
-
-  } else {
+      if ($messages{$key}) {
+        $anchor = 'modal_user_data' unless $p eq 'ok';
+        $hub->session->add_data(
+          type     => 'message',
+          code     => 'AttachURL',
+          message  => $messages{$key}{'message'},
+          function => '_'.$messages{$key}{'type'},
+        );
+      }
+    } else {
       $session->add_data(
         type     => 'message',
         code     => 'AttachURL',
         message  => 'No URL was provided',
         function => '_error'
       );
-    $redirect .= 'SelectFile';
+      $redirect .= 'SelectFile';
+    }
   }
   
-  $self->ajax_redirect($redirect, $params);  
+  $self->ajax_redirect($redirect, $params, $anchor);  
 }
 
 1;
