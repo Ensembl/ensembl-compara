fe46cbe13c71614e3429d2f48d91374291b323d8
Matthieu Muffato <muffato@ebi.ac.uk>
2015-09-07 21:20:53 +0100

    This taxon_id does not exist any more since it has been merged into taxon_id 1561998

--- scripts/taxonomy/web_name_patches.sql
+++ scripts/taxonomy/web_name_patches.sql
@@ -22,7 +22,3 @@ UPDATE ncbi_taxa_name SET name = 'Xenopus (Silurana) tropicalis' WHERE taxon_id 
 UPDATE ncbi_taxa_name SET name = 'Marsupialia'              WHERE taxon_id = 9263   AND name_class = 'scientific name'  AND name = 'Metatheria';
 UPDATE ncbi_taxa_name SET name = 'Metatheria'               WHERE taxon_id = 9263   AND name_class = 'synonym'          AND name = 'Marsupialia';
 
--- these species were renamed by WormBase:
-
-UPDATE ncbi_taxa_name SET name = 'Caenorhabditis csp11'     WHERE taxon_id = 886184 AND name_class = 'scientific name';
-
