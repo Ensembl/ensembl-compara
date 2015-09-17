cc2012f5e0781996db90b3b60e9949f4018ec4a8
Anne Lyle <annelyle@ebi.ac.uk>
2015-09-17 15:25:27 +0100

    First pass at an upload tester

--- modules/EnsEMBL/Selenium/Test/UserData.pm
+++ modules/EnsEMBL/Selenium/Test/UserData.pm
@@ -25,16 +25,16 @@ use strict;
 use parent 'EnsEMBL::Selenium::Test';
 
 sub test_upload_file {
-  my ($self, $args) = @_;
+  my $self          = shift;
   my $sel           = $self->sel;
   my $species       = $self->species;
-  my $upload_text   = 'Display your data in Ensembl';
 
   $self->no_mirrors_redirect;
 
   my @responses;
 
   my $species_name = $species->{'name'};
+  my $files        = $species->{'files'} || {};
 
   my $home_page = sprintf('%s/Info/Index', $species_name);
   my $error = eval { $self->sel->open($home_page); };
@@ -43,15 +43,52 @@ sub test_upload_file {
   }
   else { 
     $error = $sel->ensembl_wait_for_page_to_load;
-    if ($error && $error->[0] eq 'fail') {
+    if ($error && $error ne 'OK') {
       push @responses, $error;
     }
     else {
+      my $upload_text = 'Display your data in Ensembl';
       $error = $sel->ensembl_click_links(["link=$upload_text"]); 
-      push @responses, $error;
+      if ($error && $error ne 'OK') {
+        push @responses, $error;
+      }
+      else {
+        while (my($format, $file_url) = each(%$files)) {
+          $error = $self->_upload_file($format, $file_url);
+          push @responses, $error;
+        }
+      }
     }
   }
   return @responses;
+}
+
+sub _upload_file {
+### Attempt to upload a file from a URL
+### @param $format String - format of the test file
+### @param $url String - URL of the test file
+  my ($self, $format, $url) = @_;
+  my $sel = $self->sel;
+  my $error;
+
+  ## Interact with form
+  my $form = "dom=document.forms['select']";
+
+  ## Type file name into textarea
+  my $textarea = $form.'.textareas[0]';
+  $error = $sel->type($textarea, $url);
+  warn ">>> ERROR 1: $error";
+
+  ## Select the format
+  my $dropdown = 'name=format';
+  $error = $sel->type($dropdown, $format);
+  warn ">>> ERROR 2: $error";
+
+  ## Submit the form
+  $error = $sel->submit($form);
+  warn ">>> ERROR 3: $error";
+
+  return $error;
 }
 
 1;
