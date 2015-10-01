e7cf973e2b31d713458404e61c27397f00c42df4
Anne Lyle <annelyle@ebi.ac.uk>
2015-10-01 16:58:02 +0100

    New modules for image export

---
+++ modules/EnsEMBL/Web/Configuration/ImageExport.pm
@@ -0,0 +1,43 @@
+=head1 LICENSE
+
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+package EnsEMBL::Web::Configuration::ImageExport;
+
+use strict;
+
+use EnsEMBL::Web::Constants;
+
+use base qw(EnsEMBL::Web::Configuration);
+
+sub caption { 
+  my $self = shift;
+  return 'Export Image'; 
+}
+
+sub populate_tree {
+  my $self  = shift;
+
+  ## Input nodes
+  $self->create_node('SelectFormat', 'Select Format', ['select_format', 'EnsEMBL::Web::Component::ImageExport::SelectFormat']);
+
+  ## Output nodes
+  $self->create_node('Output',  '', [], { 'command' => 'EnsEMBL::Web::Command::ImageExport::Output'});
+  $self->create_node('Error', 'Output Error', ['error', 'EnsEMBL::Web::Component::ImageExport::Error']);
+}
+
+1;
---
+++ modules/EnsEMBL/Web/Object/ImageExport.pm
@@ -0,0 +1,43 @@
+=head1 LICENSE
+
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+package EnsEMBL::Web::Object::ImageExport;
+
+### Object for ImageExport pages
+
+### STATUS: Under development
+
+### DESCRIPTION: Unlike most other EnsEMBL::Web::Object children,
+### this module is not a wrapper around a specific API object.
+### Instead it uses the individual components to fetch and munge 
+### data via their own Objects, and does any additional 
+### export-specific munging as required. 
+
+use EnsEMBL::Web::Controller;
+use EnsEMBL::Web::Builder;
+use EnsEMBL::Web::File::User;
+
+use strict;
+use warnings;
+
+use parent qw(EnsEMBL::Web::Object);
+
+sub caption       { return 'Export';  }
+sub short_caption { return 'Export';  }
+
+1;
