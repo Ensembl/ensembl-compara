167f3350b767aef6e2eb9b6357dfeeeb96ef4597
Anne Lyle <annelyle@ebi.ac.uk>
2015-10-05 14:33:02 +0100

    New module to redirect to standard image output URL

---
+++ modules/EnsEMBL/Web/Command/ImageExport/ImageOutput.pm
@@ -0,0 +1,54 @@
+=head1 LICENSE
+
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+package EnsEMBL::Web::Command::ImageExport::ImageOutput;
+
+### Redirects to the view's standard image export mode
+
+use strict;
+use warnings;
+
+use List::Util qw(first);
+
+use parent qw(EnsEMBL::Web::Command);
+
+sub process {
+  my $self      = shift;
+  my $hub       = $self->hub;
+
+  my $url = sprintf('%s/Component/%s/Web/%s', $hub->param('data_type'), $hub->param('component'));
+
+  my $format = $hub->param('format') || 'png';
+
+  my $canned = {
+                'journal'   => '-c-2-s-2',
+                'poster'    => '-c-2-s-5',
+                'projector' => '-c-2-s-1.00',
+                };
+  my ($extra) = first { $hub->param($_) } qw(journal projector poster);
+  $format .= $extra if $extra;
+ 
+  my $params = {
+                'export'    => $format,
+                'download'  => $hub->param('download') || 0,
+                };
+ 
+  $self->ajax_redirect($url, $params); 
+}
+
+1;
