640b5ab676565c490322e1ef4a2d77a1abbff530
Matthieu Muffato <muffato@ebi.ac.uk>
2015-10-08 00:58:33 +0100

    There are no between-species paralogues any more

--- scripts/examples/homology_getAllHomologuesBetween2Species.pl
+++ scripts/examples/homology_getAllHomologuesBetween2Species.pl
@@ -45,13 +45,8 @@ my $sp2 = "mus_musculus";
 my $sp1_gdb = $genomedb_adaptor->fetch_by_name_assembly($sp1);
 my $sp2_gdb = $genomedb_adaptor->fetch_by_name_assembly($sp2);
 
-my $mlss_orth = $mlss_adaptor->fetch_by_method_link_type_GenomeDBs
-  ('ENSEMBL_ORTHOLOGUES', [$sp1_gdb, $sp2_gdb]);
-my $mlss_para = $mlss_adaptor->fetch_by_method_link_type_GenomeDBs
-  ('ENSEMBL_PARALOGUES', [$sp1_gdb, $sp2_gdb]);
+my $mlss_orth = $mlss_adaptor->fetch_by_method_link_type_GenomeDBs('ENSEMBL_ORTHOLOGUES', [$sp1_gdb, $sp2_gdb]);
 my @orthologies = @{$homology_adaptor->fetch_all_by_MethodLinkSpeciesSet($mlss_orth)};
-my @paralogies = @{$homology_adaptor->fetch_all_by_MethodLinkSpeciesSet($mlss_para)};
 
 print scalar(@orthologies), " orthologies between $sp1 and $sp2\n";
-print scalar(@paralogies), " paralogies between $sp1 and $sp2\n";
 
