fd5bcb550ebbf466bf8be18e73999ce8b368045c
Dan Sheppard <dan@ebi.ac.uk>
2015-10-07 17:18:50 +0100

    Maximum memoized cache size.

--- modules/EnsEMBL/Web/Memoize.pm
+++ modules/EnsEMBL/Web/Memoize.pm
@@ -44,18 +44,33 @@ sub clear_old_boots {
   remove_tree("$base/$_") for @files;
 }
 
-my $MAX_FILES = 8; # $MAX_FILES*65536 in total
+my $MULT = 3; # 2^(4*$MULT) buckets
+my $MAX_FILES = 32; # $MAX_FILES*$MULT in total
+my $MAX_BUCKET_SIZE = 1024*1024*2;
+# Max total cache size = $MAX_BUCKET_SIZE*number of buckets
 sub prune_partners {
   my ($dir,$live) = @_;
 
   opendir(DIR,$dir) || return;
   my @files = grep { -f "$dir/$_" and $_ ne $live } readdir(DIR);
   closedir DIR;
+  my (%size,%age);
+  foreach my $file (@files) {
+    my @s = stat("$dir/$file");
+    $size{$file} = $s[7];
+    $age{$file} = $s[9];
+  }
   if(@files>$MAX_FILES) {
-    my @age =
-      map { $_->[0] } sort { $a->[1] <=> $b->[1] }
-      map { [$_,[stat("$dir/$_")]->[9]] } @files;
+    my @age = sort { $age{$a} <=> $age{$b} } @files;
     unlink "$dir/$age[0]";
+  }
+  my $size = 0;
+  my @size = sort { $size{$a} <=> $size{$b} } @files;
+  foreach my $file (@size) {
+    $size += $size{$file};
+    if($size>$MAX_BUCKET_SIZE) {
+      unlink "$dir/$file";
+    }
   }
 }
 
@@ -66,7 +81,10 @@ sub filebase {
   my $mach = join('/',$SiteDefs::ENSEMBL_TMP_DIR,'procedure',
                   substr(md5_hex($SiteDefs::ENSEMBL_BASE_URL),0,8));
   my $boot = join('/',$mach,$boottime);
-  my $dir = join('/',$boot,substr($hex,0,2),substr($hex,2,2));
+  my $mult = substr($hex,0,$MULT);
+  my @mult;
+  while(length $mult) { push @mult,substr($mult,0,2,''); }
+  my $dir = join('/',$boot,@mult);
   make_path($dir);
   clear_old_boots($mach,$boottime);
   prune_partners($dir,$hex);
