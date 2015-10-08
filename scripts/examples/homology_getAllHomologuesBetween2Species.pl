e6cc8b52653698b61c17a5c5b1aa8f2eb7994d97
Matthieu Muffato <muffato@ebi.ac.uk>
2015-10-08 01:01:12 +0100

    Allow registry aliases

--- scripts/examples/homology_getAllHomologuesBetween2Species.pl
+++ scripts/examples/homology_getAllHomologuesBetween2Species.pl
@@ -37,15 +37,11 @@ my $human_gene_adaptor = $reg->get_adaptor("Homo sapiens", "core", "Gene");
 my $comparaDBA = Bio::EnsEMBL::Registry-> get_DBAdaptor('Multi', 'compara');
 my $homology_adaptor = $comparaDBA->get_HomologyAdaptor;
 my $mlss_adaptor = $comparaDBA->get_MethodLinkSpeciesSetAdaptor;
-my $genomedb_adaptor = $comparaDBA->get_GenomeDBAdaptor;
 
-my $sp1 = "homo_sapiens";
-my $sp2 = "mus_musculus";
+my $sp1 = "human";
+my $sp2 = "mouse";
 
-my $sp1_gdb = $genomedb_adaptor->fetch_by_name_assembly($sp1);
-my $sp2_gdb = $genomedb_adaptor->fetch_by_name_assembly($sp2);
-
-my $mlss_orth = $mlss_adaptor->fetch_by_method_link_type_GenomeDBs('ENSEMBL_ORTHOLOGUES', [$sp1_gdb, $sp2_gdb]);
+my $mlss_orth = $mlss_adaptor->fetch_by_method_link_type_registry_aliases('ENSEMBL_ORTHOLOGUES', [$sp1, $sp2]);
 my @orthologies = @{$homology_adaptor->fetch_all_by_MethodLinkSpeciesSet($mlss_orth)};
 
 print scalar(@orthologies), " orthologies between $sp1 and $sp2\n";
