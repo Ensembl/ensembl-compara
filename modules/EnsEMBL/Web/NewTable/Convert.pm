6380d1e21f1afc5c79a0c5dab5bdd41282a100ef
Dan Sheppard <dan@ebi.ac.uk>
2015-10-08 11:58:01 +0100

    Split CompressedArray from convert for future sort.

--- modules/EnsEMBL/Web/NewTable/Convert.pm
+++ modules/EnsEMBL/Web/NewTable/Convert.pm
@@ -25,9 +25,7 @@ use JSON;
 use MIME::Base64;
 use Compress::Zlib;
 
-sub compress_block {
-  return encode_base64(compress(JSON->new->encode($_[0])));
-}
+use EnsEMBL::Web::NewTable::CompressedArray;
 
 sub uncompress_block {
   return JSON->new->decode(uncompress(decode_base64($_[0])));
@@ -39,7 +37,7 @@ sub new {
   my $self = {
     series => [],
     rseries => {},
-    outblock => [],
+    out => EnsEMBL::Web::NewTable::CompressedArray->new,
   };
   bless $self,$class;
   return $self;
@@ -48,9 +46,6 @@ sub new {
 sub add_response {
   my ($self,$response) = @_; 
 
-  my $ob_size = 10000;
-  my $outblock = -1; 
-  my $rows = []; 
   # Columns
   foreach my $col (@{$response->{'series'}}) {
     next if exists $self->{'rseries'}{$col};
@@ -63,28 +58,14 @@ sub add_response {
     my $null = uncompress_block($response->{'nulls'}[$block]);
     foreach my $row (0..$response->{'len'}[$block]-1) {
       my $rownum = $response->{'start'}+$row;
-      my $new_ob = int($rownum/$ob_size);
-      my $offset = $rownum-($new_ob*$ob_size);
-      if($outblock != $new_ob) {
-        if($outblock!=-1) {
-          $self->{'outblock'}[$outblock] = compress_block($rows);
-        }
-        $outblock = $new_ob;
-        if($self->{'outblock'}[$outblock]) {
-          $rows = uncompress_block($self->{'outblock'}[$outblock]);
-        } else {
-          $rows = []; 
-        }   
-      }   
       my @row;
       foreach my $i (0..$#{$response->{'series'}}) {
         next if $null->[$i][$row];
         $row[$self->{'rseries'}{$response->{'series'}[$i]}] = $data->[$i][$row];
-      }   
-      $rows->[$offset] = \@row;
+      }
+      $self->{'out'}->row($rownum,\@row);
     }   
   }   
-  $self->{'outblock'}[$outblock] = compress_block($rows) if $outblock != -1;
 }
 
 sub series { return $_[0]->{'series'}; }
@@ -92,9 +73,8 @@ sub series { return $_[0]->{'series'}; }
 sub run {
   my ($self,$fn) = @_;
 
-  foreach my $outblock (@{$self->{'outblock'}}) {
-    my $rows = uncompress_block($outblock);
-    $fn->($_) for @$rows;
+  foreach my $i (0..$self->{'out'}->max) {
+    $fn->($self->{'out'}->row($i));
   }
 }
 
