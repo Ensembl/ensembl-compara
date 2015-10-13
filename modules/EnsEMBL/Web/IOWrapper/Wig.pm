6a44733e8df0ce93b44648c1ff6f4b6fc8496cb4
Anne Lyle <annelyle@ebi.ac.uk>
2015-10-13 14:56:11 +0100

    Get hash of feature info from wig file

--- modules/EnsEMBL/Web/IOWrapper/Wig.pm
+++ modules/EnsEMBL/Web/IOWrapper/Wig.pm
@@ -27,5 +27,36 @@ no warnings 'uninitialized';
 
 use parent qw(EnsEMBL::Web::IOWrapper);
 
+sub create_hash {
+### Create a hash of feature information in a format that
+### can be used by the drawing code
+### @param metadata - Hashref of information about this track
+### @param slice - Bio::EnsEMBL::Slice object
+### @return Hashref
+  my ($self, $metadata, $slice) = @_;
+  $metadata ||= {};
+  return unless $slice;
+
+  ## Start and end need to be relative to slice,
+  ## as that is how the API returns coordinates
+  my $seqname       = $self->parser->get_seqname;
+  my $feature_start = $self->parser->get_start;
+  my $feature_end   = $self->parser->get_end;
+
+  ## Only set colour if we have something in file, otherwise
+  ## we will override the default colour in the drawing code
+  my $colour;
+  if ($metadata->{'color'}) {
+    $colour = $metadata->{'color'};
+  }
+
+  return {
+    'start'       => $feature_start - $slice->start,
+    'end'         => $feature_end - $slice->start,
+    'seq_region'  => $seqname,
+    'score'       => $self->parser->get_score,
+    'colour'      => $colour,
+  };
+}
 
 1;
