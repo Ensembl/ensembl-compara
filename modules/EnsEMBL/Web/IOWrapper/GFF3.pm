46d8a83958bc1e76ba072ecac7d8eeb6cd5bb82b
Anne Lyle <annelyle@ebi.ac.uk>
2015-10-19 15:35:21 +0100

    Add necessary code to turn parser content into drawable features

--- modules/EnsEMBL/Web/IOWrapper/GFF3.pm
+++ modules/EnsEMBL/Web/IOWrapper/GFF3.pm
@@ -25,7 +25,121 @@ use strict;
 use warnings;
 no warnings 'uninitialized';
 
-use parent qw(EnsEMBL::Web::IOWrapper);
+use parent qw(EnsEMBL::Web::IOWrapper::GXF);
 
+sub post_process {
+### Reassemble sub-features back into features
+
+  while (my ($track_key, $content) = each (%$data)) {
+    my $tree = {};
+    foreach (@{$content->{'features'}}) {
+      if ($_->{'parent'}) {
+        $self->_add_to_parent($tree, $_);
+      }
+      else {
+        $tree->{$_->{'id'}} = $_;
+      }
+    }
+
+    my $args;
+
+    my @ok_features;
+    while (my($id, $f) = each(%$tree)) {
+      my %transcript;
+      if ($f->{'type'} =~ /gene/ && !$f->{'children'}) {
+        ## Ignore genes unless they don't have any transcript structure
+        push @ok_features, $f;
+      }
+      else {
+        if ($f->{'children'}) { ## Probably a transcript
+          
+          ## Push previous transcript onto array
+          if (keys %transcript) {
+            push @ok_features, \%transcript;
+            %transcript = ();
+          }
+          ## Create a new transcript from the current feature
+          %transcript = %$f;
+          ## GFF3 shouldn't have a bunch of exons with no parent
+          $args = {'seen' => {}, 'no_separate_transcript' => 0};
+        }
+        else { ## exon or similar
+          if ($f->{'parent'}) {
+            ($args, %transcript) = $self->add_to_transcript($f, $args, %transcript);  
+          }
+          else { ## Singleton
+            push @ok_features, $f;
+          }
+        }
+      }
+    }
+
+    $content->{'features'} = \@ok_features;
+  }
+
+}
+
+sub _add_to_parent {
+  my ($self, $node, $feature) = @_;
+  ## Is this a child of the current level?
+  if ($node->{$feature->{'parent'}}) {
+    $node->{$feature->{'parent'}}{'children'}{$feature->{'id'}} = $feature;
+    return;
+  }
+  else {
+    while (my($k, $v) = each(%$node)) {
+      $self->_add_to_parent($v, $feature);
+    }
+  }
+}
+
+
+sub create_hash {
+### Create a hash of feature information in a format that
+### can be used by the drawing code
+### @param metadata - Hashref of information about this track
+### @param slice - Bio::EnsEMBL::Slice object
+### @return Hashref
+  my ($self, $metadata, $slice) = @_;
+  $metadata ||= {};
+  return unless $slice;
+
+  ## Start and end need to be relative to slice,
+  ## as that is how the API returns coordinates
+  my $seqname       = $self->parser->get_seqname;
+  my $feature_start = $self->parser->get_start;
+  my $feature_end   = $self->parser->get_end;
+  my $strand        = $self->parser->get_strand;
+  my $score         = $self->parser->get_score;
+
+  my $id    = $self->parser->get_attribute_by_name('ID');
+  my $label = $self->parser->get_attribute_by_name('Name') || $id; 
+  my $alias = $self->parser->get_attribute_by_name('Alias');
+  $label .= sprintf(' (%s)', $alias) if $alias; 
+
+  my $href = $self->href({
+                        'id'          => $id,
+                        'seq_region'  => $seqname,
+                        'start'       => $feature_start,
+                        'end'         => $feature_end,
+                        });
+
+  return {
+    'id'            => $id,
+    'type'          => $self->parser->get_type,
+    'parent'        => $self->parser->get_attribute_by_name('Parent'),
+    'start'         => $feature_start - $slice->start,
+    'end'           => $feature_end - $slice->start,
+    'seq_region'    => $seqname,
+    'strand'        => $strand,
+    'score'         => $score,
+    'colour'        => $colour,
+    'label'         => $label,
+    'href'          => $href,
+  };
+}
+
+
+}
 
 1;
