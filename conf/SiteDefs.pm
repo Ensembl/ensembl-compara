865a8acf9730b3874738d1593a20d1e674842118
Dan Sheppard <dan@ebi.ac.uk>
2015-10-30 17:24:43 +0000

    Emergency hotfix for RSS down.

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -162,6 +162,8 @@ our $ENSEMBL_IMAGE_WIDTH       = 800;
 our $ENSEMBL_MINIFIED_FILES_PATH = '/minified'; # path for saving the minified files
 our $ENSEMBL_DEBUG_JS            = 0; # change these to 1 to prevent js minification
 our $ENSEMBL_DEBUG_CSS           = 0; # change these to 1 to prevent css minification
+our $ENSEMBL_DEBUG_IMAGES        = 0; # change these to 1 to prevent css minification
+our $ENSEMBL_SKIP_RSS            = 0; # set to 1 in sandboxes to avoid overloading blog
 
 our $ENSEMBL_EXTERNAL_SEARCHABLE = 0; # No external bots allowed by default
 
--- modules/EnsEMBL/Web/Document/HTML/DidYouKnow.pm
+++ modules/EnsEMBL/Web/Document/HTML/DidYouKnow.pm
@@ -45,6 +45,8 @@ sub render {
   my $sitename       = $sd->ENSEMBL_SITETYPE;
   my $html           = ''; 
 
+  return if $SiteDefs::ENSEMBL_SKIP_RSS;
+
   my $rss_url = $sd->ENSEMBL_TIPS_RSS;
   my $tips = $MEMD && $MEMD->get('::TIPS') || {};
   
@@ -54,16 +56,24 @@ sub render {
                     );
 
   ## Check the cache, then fetch new tips
-  foreach my $cat (keys %categories) {
-    (my $cat_url = $rss_url) =~ s/feed\/$/category\/$cat\/feed\//;
-    unless (scalar(@{$tips->{$cat}||[]}) && $cat_url) {
-      $tips->{$cat} = $self->get_rss_feed($hub, $cat_url);
-
-      if (scalar(@{$tips->{$cat}||[]})) {
-        $_->{'content'} = encode_utf8($_->{'content'}) for @{$tips->{$cat}};
+  unless($MEMD && $MEMD->get('::TIPS-FAILED')) {
+    my $got = 0;
+    foreach my $cat (keys %categories) {
+      (my $cat_url = $rss_url) =~ s/feed\/$/category\/$cat\/feed\//;
+      unless (scalar(@{$tips->{$cat}||[]}) && $cat_url) {
+        warn "TIPS $cat\n";
+        $tips->{$cat} = $self->get_rss_feed($hub, $cat_url);
+        $got += @{$tips->{$cat}} if $tips->{$cat};
+        if (scalar(@{$tips->{$cat}||[]})) {
+          $_->{'content'} = encode_utf8($_->{'content'}) for @{$tips->{$cat}};
+        }
       }
+      $MEMD->set('::TIPS', $tips, 3600, qw(STATIC TIPS)) if $MEMD;
     }
-    $MEMD->set('::TIPS', $tips, 3600, qw(STATIC TIPS)) if $MEMD;
+    if(!$got) { # No tips, probably failed
+      warn "MARKING RSS AS FAILED\n";
+      $MEMD->set('::TIPS-FAILED',1,3600,qw(STATIC TIPS)) if $MEMD;
+    }
   }
 
   $html .= '<ul class="bxslider">';
