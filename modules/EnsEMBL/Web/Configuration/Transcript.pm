e8d501fe2c7299e2c703c0798cdbcb9526ed92ee
Ridwan Amode <ridwan@ebi.ac.uk>
2015-11-10 12:09:31 +0000

    ENSWEB-1826: remove GO completely from transcript

--- modules/EnsEMBL/Web/Configuration/Transcript.pm
+++ modules/EnsEMBL/Web/Configuration/Transcript.pm
@@ -90,23 +90,6 @@ sub populate_tree {
     { 'availability' => 'transcript database:funcgen has_oligos', 'concise' => 'Oligo probes' }
   ));
  
-  $self->create_node('Ontology/Table', 'Ontology',
-    [qw( go EnsEMBL::Web::Component::Transcript::Go )],
-    { 'availability' => 'transcript has_go', 'concise' => 'GO table' }
-  );
-=pod 
-  my $go_menu = $self->create_submenu('GO', 'Ontology');
-  $go_menu->append($self->create_node('Ontology/Image', 'GO graph',
-    [qw( go EnsEMBL::Web::Component::Transcript::Goimage )],
-    { 'availability' => 'transcript has_go', 'concise' => 'GO graph' }
-  ));
-
-  $go_menu->append($self->create_node('Ontology/Table', 'GO table',
-    [qw( go EnsEMBL::Web::Component::Transcript::Go )],
-    { 'availability' => 'transcript has_go', 'concise' => 'GO table' }
-  ));
-=cut
-
   my $var_menu = $self->create_submenu('Variation', 'Genetic Variation');
 
   $var_menu->append($self->create_node('Variation_Transcript/Table', 'Variant table',
