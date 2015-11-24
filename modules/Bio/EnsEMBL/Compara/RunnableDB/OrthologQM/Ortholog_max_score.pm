3b1d0253616978fd16946cb79229617b4bb44ad6
Wasiu Akanni <waakanni@ebi.ac.uk>
2015-11-24 15:41:25 +0000

    add the input options to the example

--- modules/Bio/EnsEMBL/Compara/RunnableDB/OrthologQM/Ortholog_max_score.pm
+++ modules/Bio/EnsEMBL/Compara/RunnableDB/OrthologQM/Ortholog_max_score.pm
@@ -14,7 +14,7 @@
 
     Example run
 
-  standaloneJob.pl Bio::EnsEMBL::Compara::RunnableDB::OrthologQM::Ortholog_max_score 
+  standaloneJob.pl Bio::EnsEMBL::Compara::RunnableDB::OrthologQM::Ortholog_max_score  -mlss_ID <100021> -db_conn <DB_url>
 
 =cut
 
@@ -32,8 +32,7 @@ sub fetch_input {
 	my $self = shift;
 	my $mlss_ID = $self->param_required('mlss_ID');
 	my $query = "SELECT homology_id, percent_conserved_score, method_link_species_set_id FROM ortholog_quality_metric where method_link_species_set_id = $mlss_ID ORDER BY homology_id";
-	my $quality_data = $self->dbc->db_handle->selectall_arrayref(
-	$query, {});
+	my $quality_data = $self->data_dbc->db_handle->selectall_arrayref($query, {});
 	$self->param('quality_data', $quality_data);
 #	print Dumper($quality_data);
 }
@@ -42,10 +41,11 @@ sub run {
 	my $self = shift;
 	my $orth_results = {};
 	while (my $result =shift @{ $self->param('quality_data')}) {
-		print $result->[0], "  ", $result->[1], "\n";
+#		print $result->[0], "  ", $result->[1], "\n";
 		if (defined($orth_results->{$result->[0]})) {
 			#grab the one with the higher percent score
 			$orth_results->{$result->[0]} = $orth_results->{$result->[0]} >= $result->[1] ? $orth_results->{$result->[0]} : $result->[1] ; 
+#			print "method_link_species_set_id ", $self->param_required('mlss_ID'), ' homology_id ' , $result->[0], ' percent_conserved_score ' , $orth_results->{$result->[0]}, " \n\n";
 			$self->dataflow_output_id( {'method_link_species_set_id' => $self->param_required('mlss_ID'), 'homology_id' => $result->[0], 'percent_conserved_score' => $orth_results->{$result->[0]} }, 2 );
 			
 		} else {
