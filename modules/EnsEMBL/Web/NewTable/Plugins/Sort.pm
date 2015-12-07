b1a23bceb84181cad86215740ca1762d06a721a5
Dan Sheppard <dan@ebi.ac.uk>
2015-12-07 17:16:43 +0000

    Don't save column sort order.

--- modules/EnsEMBL/Web/NewTable/Plugins/Sort.pm
+++ modules/EnsEMBL/Web/NewTable/Plugins/Sort.pm
@@ -7,6 +7,12 @@ use parent qw(EnsEMBL::Web::NewTable::Plugin);
 sub children { return [qw(ClientSort ServerSort)]; }
 sub requires { return children(); }
 
+sub filter_saved {
+  my ($config,$data) = @_;
+
+  delete $data->{'sort'};
+}
+
 package EnsEMBL::Web::NewTable::Plugins::ClientSort;
 use parent qw(EnsEMBL::Web::NewTable::Plugin);
 
