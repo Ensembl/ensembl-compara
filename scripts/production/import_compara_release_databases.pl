774b5ba4697b723f96afaefdebc7c9cd946fb5f1
Matthieu Muffato <muffato@ebi.ac.uk>
2015-12-10 10:02:52 +0900

    myisamchk is now under /software/ensembl/central

--- scripts/production/import_compara_release_databases.pl
+++ scripts/production/import_compara_release_databases.pl
@@ -159,7 +159,7 @@ print $fh join("\n", map {join("\t", @$_)} @data), "\n";
 close($fh);
 
 # This is the location of myisamchk, which may not be in the user's PATH
-$ENV{PATH} = $ENV{PATH}.':/usr/local/ensembl/mysql/bin';
+$ENV{PATH} = $ENV{PATH}.':/software/ensembl/central/bin/';
 
 ## Run CopyDBoverServer.pl and remove the configuration file
 print STDERR ">> $copydboverserver -pass $ensadmin_psw -noflush $filename\n";
