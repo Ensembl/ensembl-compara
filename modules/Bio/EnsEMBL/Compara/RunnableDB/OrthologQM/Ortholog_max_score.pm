c03a650a622ec552adb7938d660600df23678532
Wasiu Akanni <waakanni@ebi.ac.uk>
2016-01-21 17:42:07 +0000

    now properly updating the goc column on the homology table

--- modules/Bio/EnsEMBL/Compara/RunnableDB/OrthologQM/Ortholog_max_score.pm
+++ modules/Bio/EnsEMBL/Compara/RunnableDB/OrthologQM/Ortholog_max_score.pm
@@ -31,7 +31,7 @@ use Bio::EnsEMBL::Registry;
 sub fetch_input {
 	my $self = shift;
 	my $mlss_ID = $self->param_required('mlss_ID');
-	my $query = "SELECT homology_id, percent_conserved_score, method_link_species_set_id FROM ortholog_quality_metric where method_link_species_set_id = $mlss_ID ORDER BY homology_id";
+	my $query = "SELECT homology_id, goc_score, method_link_species_set_id FROM ortholog_goc_metric where method_link_species_set_id = $mlss_ID ORDER BY homology_id";
 	my $quality_data = $self->data_dbc->db_handle->selectall_arrayref($query, {});
 	$self->param('quality_data', $quality_data);
 #	print Dumper($quality_data);
@@ -39,15 +39,25 @@ sub fetch_input {
 
 sub run {
 	my $self = shift;
+	my $homology_adaptor  = $self->compara_dba->get_HomologyAdaptor;
 	my $orth_results = {};
 	while (my $result =shift @{ $self->param('quality_data')}) {
 #		print $result->[0], "  ", $result->[1], "\n";
+
 		if (defined($orth_results->{$result->[0]})) {
+
 			#grab the one with the higher percent score
+
 			$orth_results->{$result->[0]} = $orth_results->{$result->[0]} >= $result->[1] ? $orth_results->{$result->[0]} : $result->[1] ; 
+
 #			print "method_link_species_set_id ", $self->param_required('mlss_ID'), ' homology_id ' , $result->[0], ' percent_conserved_score ' , $orth_results->{$result->[0]}, " \n\n";
-			$self->dataflow_output_id( {'method_link_species_set_id' => $self->param_required('mlss_ID'), 'homology_id' => $result->[0], 'percent_conserved_score' => $orth_results->{$result->[0]} }, 2 );
+#			$self->dataflow_output_id( {'method_link_species_set_id' => $self->param_required('mlss_ID'), 'homology_id' => $result->[0], 'goc_score' => $orth_results->{$result->[0]} }, 2 );
+
+			print "Updating homology table goc score\n" if ( $self->debug );
+			my $homology = $homology_adaptor->fetch_by_dbID($result->[0]);
+			$homology->goc_score($orth_results->{$result->[0]});
 			
+			$homology_adaptor->update_goc_score($homology);
 		} else {
 			$orth_results->{$result->[0]} = $result->[1];
 		}
