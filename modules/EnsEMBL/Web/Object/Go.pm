486843d39a1ed1d42797375f1d0f7a8d2ee9476c
Matthieu Muffato <muffato@ebi.ac.uk>
2016-02-05 19:12:34 +0000

    FamilyAdaptor::fetch_by_Member_source_stable_id() is deprecated

--- modules/EnsEMBL/Web/Object/Go.pm
+++ modules/EnsEMBL/Web/Object/Go.pm
@@ -61,8 +61,8 @@ sub load_genes {
       my $gene_obj = $ga->fetch_by_dbID($gene);
       push (@$subarray_ref, $gene_obj);
       if($self->param('display')) {
-        my $fam_obj = $fa->fetch_by_Member_source_stable_id( 'ENSEMBLGENE', $gene_obj->stable_id ); 
-        if( $fam_obj ) {
+        my $fam_obj = $fa->fetch_all_by_Gene($gene_obj);
+        if( @$fam_obj ) {
           push (@$subarray_ref, $fam_obj->[0]);
         } else {
           warn "NO FAMILY OBJ ", $gene_obj->stable_id ;
