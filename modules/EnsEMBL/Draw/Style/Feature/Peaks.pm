ce56a57b526fde85c86f48248a0654e6c6919863
Anne Lyle <annelyle@ebi.ac.uk>
2016-02-10 16:55:24 +0000

    New drawing style, mainly used for regulatory features

---
+++ modules/EnsEMBL/Draw/Style/Feature/Peaks.pm
@@ -0,0 +1,62 @@
+=head1 LICENSE
+
+Copyright [1999-2016] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+package EnsEMBL::Draw::Style::Feature::Peaks;
+
+=pod
+
+Renders a track as a series of rectangular blocks with
+the peaks marked in a different colour
+
+=cut
+
+use parent qw(EnsEMBL::Draw::Style::Feature);
+
+
+sub draw_feature {
+### Create a glyph that's a filled rectangle with triangles indicating peaks
+### and an optional "centre" marked in black
+### @param feature Hashref - data for a single feature
+### @param position Hashref - information about the feature's size and position
+  my ($self, $feature, $position) = @_;
+
+  return unless ($feature->{'colour'});
+
+  ## Set parameters
+  my $x = $feature->{'start'};
+  $x    = 0 if $x < 0;
+  my $params = {
+                  x           => $x,
+                  y           => $position->{'y'},
+                  width       => $position->{'width'},
+                  height      => $position->{'height'},
+                  href        => $feature->{'href'},
+                  title       => $feature->{'title'},
+                  colour      => $feature->{'colour'},
+                  absolutey   => 1,
+                };
+
+  push @{$self->glyphs}, $self->Rect($params);
+}
+
+
+sub draw_label {
+### This style of track is labelled in the sidebar, not in the body of the image
+}
+
+1;
