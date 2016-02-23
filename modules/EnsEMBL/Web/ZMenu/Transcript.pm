af390289d16be0f17f27a072c59eda4714792645
Ridwan Amode <ridwan@ebi.ac.uk>
2016-02-23 12:10:49 +0000

    ENSWEB-1849: added exon rank, code for intron rank also in place but waiting for core for intron rank API to be added for 85

--- modules/EnsEMBL/Web/ZMenu/Transcript.pm
+++ modules/EnsEMBL/Web/ZMenu/Transcript.pm
@@ -35,14 +35,41 @@ sub content {
   my $gene_desc   = $gene ? $object->gene_description =~ s/No description//r =~ s/\[.+\]\s*$//r : '';
   my @xref        = $object->display_xref;
   my @click       = $self->click_location;
+  my @all_exons   = @{$transcript->get_all_Exons};
+  my @all_introns = @{$transcript->get_all_Introns};
+  my (@exons, @introns); 
   
   $translation = undef if $transcript->isa('Bio::EnsEMBL::PredictionTranscript'); 
 
   $self->caption($xref[0] ? "$xref[3]: $xref[0]" : !$gene ? $stable_id : 'Novel transcript');
+    
+  if (scalar @click) {
+    ## Has user clicked on an exon (or exons)? If yes find out the exon rank to display in zmenu
+    foreach (@all_exons) {
+      my ($s, $e) = ($_->start, $_->end);
+      
+      if (
+        ($s <= $click[1] && $e >= $click[2]) || # click is completely inside exon
+        ($s >= $click[1] && $e <= $click[2])    # click completely contains exon
+      ) {      
+        push @exons, $_->rank($transcript);
+      }
+    }
+    ## Has user clicked on an intron (or introns)? If yes find out the intron rank to display in zmenu
+    foreach (@all_introns) {
+      my ($st, $en) = ($_->start, $_->end);
+      
+      if (
+        ($st <= $click[1] && $en >= $click[2]) || # click is completely inside exon
+        ($st >= $click[1] && $en <= $click[2])    # click completely contains exon
+      ) {      
+        #push @introns, $_->rank($transcript);   #TO uncomment once core added the rank call for intron      
+      }
+    }    
+  }
   
   # Only if there is a gene (not Prediction transcripts)
-  if ($gene) {
-  
+  if ($gene) {  
     if($gene_desc) {
       $self->add_entry({
         type  => 'Gene',
@@ -78,6 +105,16 @@ sub content {
       })
     });
   }
+
+  $self->add_entry({
+    type  => 'Exon',
+    label => $exons[0]." of ". scalar(@all_exons)
+  }) if(scalar @exons);
+  
+  $self->add_entry({
+    type  => 'Intron',
+    label => $introns[0]." of ". scalar(@all_introns)
+  }) if(scalar @introns);  
   
   $self->add_entry({
     type  => 'Transcript',
