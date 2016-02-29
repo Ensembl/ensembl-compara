0b02f32b1552e182561108f47f2cef7af83a8677
Wasiu Akanni <waakanni@ebi.ac.uk>
2016-02-29 17:53:25 +0000

    print statements

--- modules/Bio/EnsEMBL/Compara/RunnableDB/OrthologQM/Prepare_Per_Chr_Jobs.pm
+++ modules/Bio/EnsEMBL/Compara/RunnableDB/OrthologQM/Prepare_Per_Chr_Jobs.pm
@@ -40,7 +40,7 @@ sub param_defaults {
 #	'mlss_ID'=>'100021',
 #        'ref_species_dbid' => 155,
 #        'non_ref_species_dbid' => 31,
-#		'ortholog_info_hashref'	=>	{ '1045569' => {
+#		'ortholog_info_hashref'	=>	{     '1045569' => {
 #                         '46043' => '83505425',
 #                         '14469' => '83531457',
 #                         '14803' => '83531457',
@@ -69,7 +69,7 @@ sub run {
 	my $self = shift;
 
 	my $ortholog_hashref = $self->param('ortholog_info_hashref');
-#	print " -------------------------------------------------------------Bio::EnsEMBL::Compara::RunnableDB::OrthologQM::Prepare_Per_Chr_Jobs \n\n";
+	print "Bio::EnsEMBL::Compara::RunnableDB::OrthologQM::Prepare_Per_Chr_Jobs ---------------------------------------------START \n\n" if ( $self->debug );
 #	print  $self->param('mlss_ID'), "\n";
 #	print Dumper($ortholog_hashref);
 	while (my ($ref_dnafragID, $chr_orth_hashref) = each(%$ortholog_hashref)){
@@ -85,7 +85,7 @@ sub run {
 
         my $chr_job = {};
         $chr_job->{$ref_dnafragID} = \@orth_sorted;
-        print Dumper($chr_job);
+        print Dumper($chr_job) if ( $self->debug );
 #        $self->param( 'chr_job', {'chr_job' => $chr_job} );
         $self->dataflow_output_id( {'chr_job' => $chr_job, 'ref_species_dbid' => $self->param('ref_species_dbid'), 'non_ref_species_dbid' => $self->param('non_ref_species_dbid'), 'mlss_ID' => $self->param('mlss_ID')}, 2 );
 
