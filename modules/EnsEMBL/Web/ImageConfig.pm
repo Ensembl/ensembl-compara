f333ff085f863c225215855e934cc62217511767
Anne Lyle <annelyle@ebi.ac.uk>
2016-03-03 14:41:15 +0000

    Remove message now that we support default tracks on trackhubs

--- modules/EnsEMBL/Web/ImageConfig.pm
+++ modules/EnsEMBL/Web/ImageConfig.pm
@@ -1655,9 +1655,6 @@ sub update_from_url {
       }
       # We have to create a URL upload entry in the session
       my $message  = sprintf('Data has been attached to your display from the following URL: %s', encode_entities($p));
-      if (uc $format eq 'TRACKHUB') {
-          $message .= " Please go to '<b>Configure this page</b>' to choose which tracks to show (we do not turn on tracks automatically in case they overload our server).";
-      }
       $session->add_data(
           type     => 'message',
           function => '_info',
