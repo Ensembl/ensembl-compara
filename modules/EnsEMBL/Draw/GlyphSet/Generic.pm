3cf8374345952e16e1c4f29bc1ff0b5c67db4c9d
Anne Lyle <annelyle@ebi.ac.uk>
2016-03-03 15:33:53 +0000

    Use default track colour for gradient, rather than hard-coding it

--- modules/EnsEMBL/Draw/GlyphSet/Generic.pm
+++ modules/EnsEMBL/Draw/GlyphSet/Generic.pm
@@ -267,7 +267,8 @@ sub convert_to_pvalues {
   ## Set parameters
   my $max_score        = $params->{max_score} || 1000;
   my $min_score        = $params->{min_score} || 0;
-  my @gradient_colours = @{ $params->{gradient_colours} || [qw(white red)] };
+  my $default_colour   = $self->{my_config}->get('colour') || 'red';
+  my @gradient_colours = @{ $params->{gradient_colours} || ['white', $default_colour] };
   my $transform        = $transforms{ $params->{transform} || 'default' };
   my $decimal_places   = $params->{decimal_places} || 2;
 
