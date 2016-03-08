b92dd71ce4dd75c724b5f4343be6706048c685a4
Mateus Patricio <mateus@ebi.ac.uk>
2016-03-08 09:42:37 +0000

    Declaring use_timetree_times tag.

--- modules/Bio/EnsEMBL/Compara/PipeConfig/Example/EnsemblProteinTrees_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/Example/EnsemblProteinTrees_conf.pm
@@ -260,6 +260,8 @@ sub default_options {
         # Do we want to initialise the CAFE part now ?
         'initialise_cafe_pipeline'  => 1,
 
+        #Use Timetree divergence times for the GeneTree internal nodes
+        'use_timetree_times'        => 1,
     };
 }
 
