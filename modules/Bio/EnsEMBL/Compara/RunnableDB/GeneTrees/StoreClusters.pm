331d68549429e8530e6594a39c229434bae59632
Mateus Patricio <mateus@ebi.ac.uk>
2016-04-06 11:00:23 +0100

    Adding stable_ids to the clusters.
    This is an important change since it will allow us to refer to the same
    families across different releases.
    It will add the HMM model name to the gene_tree_root_table->stable_id,
    also allowing us to fetch trees from previous releases under the same
    family.

--- modules/Bio/EnsEMBL/Compara/RunnableDB/GeneTrees/StoreClusters.pm
+++ modules/Bio/EnsEMBL/Compara/RunnableDB/GeneTrees/StoreClusters.pm
@@ -193,6 +193,7 @@ sub add_cluster {
         -tree_type => 'tree',
         -method_link_species_set_id => $clusterset->method_link_species_set_id,
         -clusterset_id => $clusterset->clusterset_id,
+        -stable_id => $cluster_def->{'model_name'},
     );
 
     # The cluster root node
