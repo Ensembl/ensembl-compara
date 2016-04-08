155a8521dee247bb9d3dbea0f8e262520306828d
Mateus Patricio <mateus@ebi.ac.uk>
2016-04-08 16:01:13 +0100

    Fixing indentation.

--- modules/Bio/EnsEMBL/Compara/RunnableDB/ProteinTrees/ClusterTagging.pm
+++ modules/Bio/EnsEMBL/Compara/RunnableDB/ProteinTrees/ClusterTagging.pm
@@ -41,11 +41,12 @@ sub fetch_input {
     my $gene_tree_id = $self->param_required('gene_tree_id');
     $self->param( 'tree_adaptor', $self->compara_dba->get_GeneTreeAdaptor );
     my $gene_tree = $self->param('tree_adaptor')->fetch_by_dbID($gene_tree_id) or die "Could not fetch gene_tree with gene_tree_id='$gene_tree_id'";
-    my $species_tree = $self->compara_dba->get_SpeciesTreeAdaptor->fetch_by_method_link_species_set_id_label( $self->param('mlss_id'), 'default' ) || die "Could not fetch species tree";
+    my $species_tree = $self->compara_dba->get_SpeciesTreeAdaptor->fetch_by_method_link_species_set_id_label( $self->param('mlss_id'), 'default' ) ||
+      die "Could not fetch species tree";
 
     #print Dumper $gene_tree;
-    $self->param( 'gene_tree', $gene_tree );
-    $self->param( 'species_tree', $species_tree);
+    $self->param( 'gene_tree',    $gene_tree );
+    $self->param( 'species_tree', $species_tree );
 }
 
 sub run {
@@ -66,8 +67,8 @@ sub run {
 
 sub write_output {
     my $self = shift;
-    $self->param('gene_tree')->store_tag( 'taxonomic_coverage', $self->param('taxonomic_coverage') );
     $self->param('gene_tree')->store_tag( 'lca_node_id',         $self->param('lca_node_id')->dbID );
+    $self->param('gene_tree')->store_tag( 'taxonomic_coverage',  $self->param('taxonomic_coverage') );
     $self->param('gene_tree')->store_tag( 'ratio_species_genes', $self->param('ratio_species_genes') );
 }
 
@@ -85,20 +86,20 @@ sub _get_lca_node_id {
     my $gene_tree_leaves = $self->param('gene_tree')->get_all_Members() || die "Could not get_all_Members for genetree: " . $self->param_required('gene_tree_id');
 
     #get all the genomes in the tree, store in a hash to avoid duplications.
-    foreach my $leaf (@{$gene_tree_leaves}) {
+    foreach my $leaf ( @{$gene_tree_leaves} ) {
         my $genomeDbId = $leaf->genome_db_id();
         $genomes_list->{$genomeDbId} = 1;
     }
 
     #storing refences in order to avoid multiple calls of the same functions.
-    $self->param('genomes_list',$genomes_list);
-    $self->param('gene_tree_leaves',$gene_tree_leaves);
+    $self->param( 'genomes_list',     $genomes_list );
+    $self->param( 'gene_tree_leaves', $gene_tree_leaves );
 
     #store the list of species_tree nodes, in order to get the mrca.
     my @species_tree_node_list;
-    foreach my $genomeDbId (keys %{$genomes_list}) {
+    foreach my $genomeDbId ( keys %{$genomes_list} ) {
         my $species_tree_node = $self->param('species_tree')->root->find_leaves_by_field( 'genome_db_id', $genomeDbId )->[0];
-        push( @species_tree_node_list, $species_tree_node);
+        push( @species_tree_node_list, $species_tree_node );
     }
 
     my $lca_node_id = $self->param('species_tree')->Bio::EnsEMBL::Compara::NestedSet::find_first_shared_ancestor_from_leaves( [@species_tree_node_list] );
@@ -110,13 +111,13 @@ sub _get_taxonomic_coverage {
     my $self = shift;
 
     #get all genomes
-    my $genomes_list = scalar($self->param('genomes_list'));
+    my $genomes_list = scalar( $self->param('genomes_list') );
 
     #get all leaves from MRCA
-    $self->param('leaves_ancestral',\@leaves_ancestral);
     my @leaves_ancestral = @{ $self->param('lca_node_id')->get_all_leaves() };
+    $self->param( 'leaves_ancestral', \@leaves_ancestral );
 
-    my $taxonomic_coverage = sprintf( "%.5f", ( keys(%{$genomes_list})/scalar(@leaves_ancestral) ) );
+    my $taxonomic_coverage = sprintf( "%.5f", ( keys( %{$genomes_list} )/scalar(@leaves_ancestral) ) );
 
     return $taxonomic_coverage;
 }
@@ -124,7 +125,7 @@ sub _get_taxonomic_coverage {
 sub _get_ratio_species_genes {
     my $self = shift;
 
-    my $ratio_species_genes = sprintf( "%.5f", scalar(@{$self->param('leaves_ancestral')})/scalar(@{$self->param('gene_tree_leaves')}) );
+    my $ratio_species_genes = sprintf( "%.5f", scalar( @{ $self->param('leaves_ancestral') } )/scalar( @{ $self->param('gene_tree_leaves') } ) );
 
     return $ratio_species_genes;
 }
