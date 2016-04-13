0925cc881c3c0b5a65fb1aa1499059338d58e86a
Harpreet Riat <harpreet@ebi.ac.uk>
2016-04-13 15:39:07 +0100

    Set correct Content-Length header and ignore logging request url

--- modules/EnsEMBL/Web/Controller/CSS.pm
+++ modules/EnsEMBL/Web/Controller/CSS.pm
@@ -27,13 +27,20 @@ use parent qw(EnsEMBL::Web::Controller);
 
 sub process {
   my $self          = shift;
+  my $r             = $self->r;
   my $species_defs  = $self->species_defs;
   my $filename      = $self->query;
   my $file          = $filename ? first { $filename eq $_->url_path } map { @{$_->files} } @{$species_defs->ENSEMBL_JSCSS_FILES->{'css'} || []} : undef;
+  my $content       = $filename ? $file ? $file->get_contents($species_defs, 'css') : "/* ERROR: file $filename is not present in the document root */\n" : "/* ERROR: No file name provided */\n";
 
-  $self->r->content_type('text/css');
+  # don't pollute logs with CSS requests
+  $r->subprocess_env('LOG_REQUEST_IGNORE', 1);
 
-  print $filename ? $file ? $file->get_contents($species_defs, 'css') : "/* ERROR: file $filename is not present in the document root */\n" : "/* ERROR: No file name provided */\n";
+  # set correct headers
+  $r->content_type('text/css');
+  $r->headers_out->set('Content-Length', length $content);
+
+  $r->print($content);
 }
 
 1;
