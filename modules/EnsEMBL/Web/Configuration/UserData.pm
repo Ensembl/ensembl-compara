215ceb5ef96f91d515f67db15510ffc171c012cf
Anne Lyle <annelyle@ebi.ac.uk>
2016-06-03 17:04:40 +0100

    Revert "Consolidate userdata interface to always use 'Custom tracks' as label"
    This reverts commit ac3955d1e8afc4320e7292463c4640a5c4d79648.

--- modules/EnsEMBL/Web/Configuration/UserData.pm
+++ modules/EnsEMBL/Web/Configuration/UserData.pm
@@ -37,7 +37,7 @@ sub populate_tree {
   my $config_menu = $self->create_submenu('Configurations', 'Manage Configurations');
 
   ## Upload "wizard"
-  $data_menu->append($self->create_node('SelectFile',     'Add your data', [qw(select_file  EnsEMBL::Web::Component::UserData::SelectFile)], {'no_menu_entry' => 1}));
+  $data_menu->append($self->create_node('SelectFile',     'Add your data', [qw(select_file  EnsEMBL::Web::Component::UserData::SelectFile)]));
   $data_menu->append($self->create_node('MoreInput',      '',              [qw(more_input   EnsEMBL::Web::Component::UserData::MoreInput)]));
   $data_menu->append($self->create_node('UploadFeedback', '',              [qw(feedback     EnsEMBL::Web::Component::UserData::UploadFeedback   parsed EnsEMBL::Web::Component::UserData::UploadParsed)]));
   $data_menu->append($self->create_node('RemoteFeedback', '',              [qw(feedback     EnsEMBL::Web::Component::UserData::RemoteFeedback   parsed EnsEMBL::Web::Component::UserData::UploadParsed)]));
@@ -48,7 +48,7 @@ sub populate_tree {
   $data_menu->append($self->create_node('RefreshUpload',   '', [], { command => 'EnsEMBL::Web::Command::UserData::RefreshUpload'   }));
   $data_menu->append($self->create_node('CheckShare',   '', [], { command => 'EnsEMBL::Web::Command::UserData::CheckShare'   }));
   ## Data management
-  $data_menu->append($self->create_node('ManageData',            'Custom tracks', [qw(
+  $data_menu->append($self->create_node('ManageData',            'Manage Data', [qw(
                     manage_remote EnsEMBL::Web::Component::UserData::ManageData
                     select_file   EnsEMBL::Web::Component::UserData::SelectFile
     )]));
