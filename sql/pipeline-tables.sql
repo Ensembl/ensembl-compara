5c4a11db8f24910db114bb4a3020f8e8e85aebea
Mateus Patricio <mateus@ebi.ac.uk>
2016-06-16 14:25:15 +0100

    Adding index to method_link_species_set_id column.

--- sql/pipeline-tables.sql
+++ sql/pipeline-tables.sql
@@ -276,7 +276,9 @@ CREATE TABLE IF NOT EXISTS ortholog_goc_metric (
   left1 INT,
   left2 INT,
   right1 INT,
-  right2 INT
+  right2 INT,
+
+  KEY method_link_species_set_id (method_link_species_set_id)
             
 )  ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
