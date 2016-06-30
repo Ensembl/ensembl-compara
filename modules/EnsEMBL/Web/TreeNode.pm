660351dab8f339831af1bc9ff48166a5a5594332
Harpreet Riat <harpreet@ebi.ac.uk>
2016-06-30 11:36:42 +0100

    Few methods deprecated and few new added

--- modules/EnsEMBL/Web/TreeNode.pm
+++ modules/EnsEMBL/Web/TreeNode.pm
@@ -27,7 +27,6 @@ use EnsEMBL::Web::Attributes;
 use EnsEMBL::Web::Utils::EqualityComparator qw(is_same);
 
 sub id    :Accessor;
-sub data  :Accessor;
 sub tree  :Accessor;
 
 sub new {
@@ -36,6 +35,7 @@ sub new {
   ## @param (Optional) DOM object as required by the parent constructor
   ## @param (Optional) Id of the node
   ## @param (Optional) Node specific data
+  ## @caller EnsEMBL::Web::Tree::create_node
   my ($class, $tree, $dom, $id, $data) =@_;
 
   my $self = $class->SUPER::new($dom);
@@ -46,14 +46,6 @@ sub new {
 
   return $self;
 }
-
-sub get_node    { return shift->tree->get_node(@_);   }
-sub nodes       { return shift->tree->nodes(@_);      }
-sub is_leaf     { return !$_[0]->has_child_nodes;     }
-sub previous    { return $_[0]->previous_sibling;     }
-sub next        { return $_[0]->next_sibling;         }
-sub append      { return $_[0]->append_child($_[1]);  }
-sub prepend     { return $_[0]->prepend_child($_[1]); }
 
 sub descendants {
   ## Gets all the descendant nodes for this node
@@ -71,19 +63,32 @@ sub leaves {
   return map { $_->has_child_nodes ? $_->leaves : $_ } @{$self->child_nodes};
 }
 
+sub get_data {
+  ## Returns value from node specific data
+  my ($self, $key)  = @_;
+  return $self->{'data'}{$key};
+}
+
+sub set_data {
+  ## Adds/modifies the given key-value pair in the 'data' key (not user_data key - use set_user_setting for that)
+  my ($self, $key, $value) = @_;
+  $self->{'data'}{$key} = $value;
+}
+
+sub data_keys {
+  ## Gets a list of all the data keys for the node
+  ## @return List of keys (Strings)
+  my $self = shift;
+  return keys %{$self->{'data'}};
+}
+
 sub get {
   ## Returns shared user data value for the given key if present, otherwise returns value from node specific data
   my ($self, $key)  = @_;
   my $user_data     = $self->user_data;
   my $node_id       = $self->id;
 
-  return $user_data && exists $user_data->{$node_id} && exists $user_data->{$node_id}{$key} ? $user_data->{$node_id}{$key} : $self->data->{$key};
-}
-
-sub set {
-  ## Adds/modifies the given key-value pair in the 'data' key (not user_data key - use set_user_setting for that)
-  my ($self, $key, $value) = @_;
-  $self->data->{$key} = $value;
+  return $user_data && exists $user_data->{$node_id} && exists $user_data->{$node_id}{$key} ? $user_data->{$node_id}{$key} : $self->get_data($key);
 }
 
 sub set_user_setting {
@@ -95,7 +100,7 @@ sub set_user_setting {
   my $user_data = $self->tree->user_data;
 
   # If same as default value - remove data
-  if (is_same($value, $self->data->{$key})) {
+  if (is_same($value, $self->get_data($key))) {
     delete $user_data->{$node_id}{$key};
     delete $user_data->{$node_id} unless scalar %{$user_data->{$node_id}};
     return 1;
@@ -126,6 +131,23 @@ sub reset_user_settings {
   return 0;
 }
 
+sub insert_alphabetically {
+  ## Inserts a child node among the existing children alphabetically according to a given key
+  ## @param Node to be inserted
+  ## @param Data key that needs to be compared alphabetically
+  my ($self, $node, $key) = @_;
+
+  $key          ||= 'caption';
+  my $node_name   = $node->get_data($key);
+  my $next_child  = $self->first_child;
+
+  while ($next_child && ($next_child->get_data($key) || '') lt $node_name) { # empty string lt anything is true
+    $next_child = $next_child->next_sibling;
+  }
+
+  return $next_child ? $self->insert_before($node, $next_child) : $self->append_child($node);
+}
+
 sub clear_references {
   ## Clean interlinked references to make sure object gets destroyed properly after we are done with it
   my $self  = shift;
@@ -134,6 +156,15 @@ sub clear_references {
   $_->clear_references for @{$self->remove_children};
 }
 
-sub set_user :Deprecated('Use set_user_setting') { return shift->set_user_setting(@_); }
+sub data        :Deprecated('Use get_data/set_data/data_keys')  { return $_[0]->{'data'};             }
+sub get_node    :Deprecated('Call get_nodes on tree')           { return shift->tree->get_node(@_);   }
+sub nodes       :Deprecated('Call nodes on tree')               { return shift->tree->nodes(@_);      }
+sub is_leaf     :Deprecated('Use has_child_nodes')              { return !$_[0]->has_child_nodes;     }
+sub previous    :Deprecated('Use previous_sibling')             { return $_[0]->previous_sibling;     }
+sub next        :Deprecated('Use next_sibling')                 { return $_[0]->next_sibling;         }
+sub append      :Deprecated('Use append_child')                 { return $_[0]->append_child($_[1]);  }
+sub prepend     :Deprecated('Use prepend_child')                { return $_[0]->prepend_child($_[1]); }
+sub set_user    :Deprecated('Use set_user_setting')             { return shift->set_user_setting(@_); }
+sub set         :Depreceted('Use set_data')                     { return shift->set_data(@_);         }
 
 1;
