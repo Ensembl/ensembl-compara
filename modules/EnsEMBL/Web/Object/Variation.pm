Ensembl/ensembl-compara

commit 2230a3540ac5a2fb49809cad4aca738fc2b81a67

fix(variation view): display the number of unique transcripts that a variant overlaps
For multi allelic variants, the Gene and Transcript consequences table presents every overlapping transcripts for each allele. In the variation summary section we should show a variant overlaps X (unique) transcript(s).

Fix ENSEMBL-4480.

modules/EnsEMBL/Web/Object/Variation.pm
@@ -58,7 +58,7 @@ sub availability {
       
       $availability->{'variation'} = 1;
       
-      $availability->{"has_$_"} = $counts->{$_} for qw(transcripts regfeats features populations population_freqs samples ega citation locations);
+      $availability->{"has_$_"} = $counts->{$_} for qw(uniq_transcripts transcripts regfeats features populations population_freqs samples ega citation locations);
       if($self->param('vf')){
         ## only show these if a mapping available
         $availability->{"has_$_"} = $counts->{$_} for qw(alignments ldpops);
@@ -89,6 +89,7 @@ sub counts {
 
   unless ($counts) {
     $counts = {};
+    $counts->{'uniq_transcripts'} = $self->count_uniq_transcripts;
     $counts->{'transcripts'} = $self->count_transcripts;
     $counts->{'regfeats'}    = $self->count_regfeats;
     $counts->{'features'}    = $counts->{'transcripts'} + $counts->{'regfeats'};
@@ -149,6 +150,24 @@ sub count_regfeats {
   # $counts += scalar map {@{$_->get_all_RegulatoryFeatureVariations}, @{$_->get_all_MotifFeatureVariations}} @{$self->get_variation_features};
   $counts += scalar map {@{$_->get_all_RegulatoryFeatureVariations}} @{$self->get_variation_features};
   return $counts;
+}
+
+sub count_uniq_transcripts {
+  my $self = shift;
+  my %mappings = %{ $self->variation_feature_mapping };
+  my $count = 0;
+
+  foreach my $varif_id (keys %mappings) {
+    next unless ($varif_id  eq $self->param('vf'));
+    my %transcriptnames;
+    for ( $mappings{$varif_id}{transcript_vari} ) {
+        $transcriptnames{$_{transcriptname}} += 1;
+    }
+    $count = scalar keys %transcriptnames;
+    last;
+  }
+
+  return $count;
 }
 
 sub count_populations {

modules/EnsEMBL/Web/Component/Variation/Summary.pm
@@ -84,11 +84,11 @@ sub feature_summary {
   
   push @str_array, sprintf('overlaps <a class="dynamic-link" href="%s">%s %s</a>', 
                       $transcript_url, 
-                      $avail->{has_transcripts}, 
-                      $avail->{has_transcripts} eq "1" ? "transcript" : "transcripts"
-                  ) if($avail->{has_transcripts});
+                      $avail->{has_uniq_transcripts}, 
+                      $avail->{has_uniq_transcripts} eq "1" ? "transcript" : "transcripts"
+                  ) if($avail->{has_uniq_transcripts});
   push @str_array, sprintf('%s<a class="dynamic-link" href="%s">%s %s</a>',
-                      $avail->{has_transcripts} ? '' : 'overlaps ',
+                      $avail->{has_uniq_transcripts} ? '' : 'overlaps ',
                       $transcript_url,
                       $avail->{has_regfeats},
                       $avail->{has_regfeats} eq "1" ? "regulatory feature" : "regulatory features"

commit 24b11e431be4b03814fab3fd98b02e86086f72ae

Fix a bug introduced in change 2230a35.
Use correct way to iterate over and access elements in an array of hashes.

modules/EnsEMBL/Web/Object/Variation.pm
@@ -161,8 +161,8 @@ sub count_uniq_transcripts {
   foreach my $varif_id (keys %mappings) {
     next unless ($varif_id  eq $self->param('vf'));
     my %transcriptnames;
-    for ( $mappings{$varif_id}{transcript_vari} ) {
-        $transcriptnames{$_{transcriptname}} += 1;
+    for ( @{ $mappings{$varif_id}{transcript_vari} } ) {
+        $transcriptnames{$_->{transcriptname}} += 1;
     }
     $count = scalar keys %transcriptnames;
     last;