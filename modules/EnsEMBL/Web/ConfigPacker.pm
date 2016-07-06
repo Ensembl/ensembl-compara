f41259f56f9ad1eb2275a9ce04683ece75ea7dac
Anne Lyle <annelyle@ebi.ac.uk>
2016-07-06 15:11:44 +0100

    Revert "Don't restrict cell lines to ones in regulatory build, or we ignore data"
    This reverts commit 72a31db6123dab6db72e9e60b1ac5a9143e2e2b8.

--- modules/EnsEMBL/Web/ConfigPacker.pm
+++ modules/EnsEMBL/Web/ConfigPacker.pm
@@ -742,8 +742,13 @@ sub _summarise_funcgen_db {
 
 ### Find details of epigenomes that are present in the current regulatory build
   my $c_aref =  $dbh->selectall_arrayref(
-    'select epigenome.name, epigenome.epigenome_id, epigenome.display_label 
-      from epigenome'
+    'select
+      distinct epigenome.name, epigenome.epigenome_id, epigenome.display_label 
+        from regulatory_build 
+      join regulatory_build_epigenome using (regulatory_build_id) 
+      join epigenome using (epigenome_id)
+     where regulatory_build.is_current=1
+     '
   );
   foreach my $row (@$c_aref) {
     my $cell_type_key =  $row->[0] .':'. $row->[1];
