90d6d0777bcbe59a88a7a6cc46f940c5971d1309
Matthieu Muffato <muffato@ebi.ac.uk>
2016-07-30 23:22:44 +0100

    Already defined in EnsemblGeneric_conf

--- modules/Bio/EnsEMBL/Compara/PipeConfig/PairAligner_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/PairAligner_conf.pm
@@ -77,9 +77,6 @@ sub default_options {
     return {
         %{$self->SUPER::default_options},   # inherit the generic ones
 
-        #'ensembl_cvs_root_dir' => $ENV{'HOME'}.'/src/ensembl_main/', 
-        'ensembl_cvs_root_dir' => $ENV{'ENSEMBL_CVS_ROOT_DIR'}, 
-
              # dependent parameters:
         'host'        => 'compara1',                        #separate parameter to use the resources aswell
 
