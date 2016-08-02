10dc694803aa619ae7c3ba7a5cc840c0fd48cf72
Matthieu Muffato <muffato@ebi.ac.uk>
2016-08-02 23:04:15 +0100

    set_internal_ids is not used any more

--- modules/Bio/EnsEMBL/Compara/PipeConfig/PairAligner_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/PairAligner_conf.pm
@@ -660,14 +660,6 @@ sub pipeline_analyses {
  	       -wait_for => [ 'update_max_alignment_length_after_chain', 'remove_inconsistencies_after_chain' ],
 	       -rc_name => '1Gb',
  	    },
- 	    {  -logic_name => 'set_internal_ids',
- 	       -module     => 'Bio::EnsEMBL::Compara::RunnableDB::PairAligner::SetInternalIds',
- 	       -parameters => {
-			       'tables' => [ 'genomic_align_block', 'genomic_align' ],
-			       'skip' => $self->o('patch_alignments'),
-			      },
-	       -rc_name => '1Gb',
- 	    },
  	    {  -logic_name => 'alignment_nets',
  	       -hive_capacity => $self->o('net_hive_capacity'),
  	       -batch_size => $self->o('net_batch_size'),
@@ -676,7 +668,6 @@ sub pipeline_analyses {
 	       -flow_into => {
 			      -1 => [ 'alignment_nets_himem' ],  # MEMLIMIT
 			     },
-#	       -wait_for => [ 'set_internal_ids' ],                                     # lg4, 1Apr2015: skipping set_internal_ids to see if set_internal_ids_collection is any faster
 	       -rc_name => '1Gb',
  	    },
 	    {  -logic_name => 'alignment_nets_himem',
