2010e69569e5eca01a89cb91511e65f20de61c2d
Harpreet Riat <harpreet@ebi.ac.uk>
2016-08-03 14:42:15 +0100

    single_transcript image config rewrite

--- modules/EnsEMBL/Web/ImageConfig/single_transcript.pm
+++ modules/EnsEMBL/Web/ImageConfig/single_transcript.pm
@@ -20,11 +20,15 @@ limitations under the License.
 package EnsEMBL::Web::ImageConfig::single_transcript;
 
 use strict;
+use warnings;
 
-use base qw(EnsEMBL::Web::ImageConfig);
+use parent qw(EnsEMBL::Web::ImageConfig);
 
-sub init {
+sub init_cacheable {
+  ## @override
   my $self = shift;
+
+  $self->SUPER::init_cacheable(@_);
 
   $self->set_parameters({
     no_labels => 1,
@@ -47,4 +51,3 @@ sub init {
 }
 
 1;
-
