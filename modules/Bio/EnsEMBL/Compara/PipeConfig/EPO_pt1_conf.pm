515514a4e36cad8e92d7ed73477d88ef99921054
Matthieu Muffato <muffato@ebi.ac.uk>
2016-08-08 09:51:58 +0100

    bugfix: typo in the hive param macro

--- modules/Bio/EnsEMBL/Compara/PipeConfig/EPO_pt1_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/EPO_pt1_conf.pm
@@ -371,7 +371,7 @@ return [
  -logic_name => 'load_anchor_sequence_factory',
  -module     => 'Bio::EnsEMBL::Hive::RunnableDB::JobFactory',
  -parameters => {
-	'inputquery'  => 'SELECT DISTINCT(anchor_id) AS anchor_id FROM anchor_align WHERE method_link_species_set_id = #overlaps_mlssid',
+	'inputquery'  => 'SELECT DISTINCT(anchor_id) AS anchor_id FROM anchor_align WHERE method_link_species_set_id = #overlaps_mlssid#',
   },
  -flow_into => {
 	2 => [ 'load_anchor_sequence' ],	
