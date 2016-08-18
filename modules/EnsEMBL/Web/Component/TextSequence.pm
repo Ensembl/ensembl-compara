1ff53d26000a4f7f24828ab9d9482550cbc3fc49
Dan Sheppard <dan@ebi.ac.uk>
2016-08-18 12:02:56 +0100

    Less.

--- modules/EnsEMBL/Web/Component/TextSequence.pm
+++ modules/EnsEMBL/Web/Component/TextSequence.pm
@@ -176,30 +176,6 @@ sub get_sequence_data_new {
   return ([@{$view->sequences}], \@markup);
 }
 
-sub set_sequence {
-  my ($self, $config, $sequence, $markup, $seq, $name) = @_;
-  
-  if ($config->{'match_display'}) {
-    if ($name eq $config->{'ref_slice_name'}) {
-      push @$sequence, [ map {{ letter => $_ }} @{$config->{'ref_slice_seq'}} ];
-    } else {
-      my $i       = 0;
-      my @cmp_seq = map {{ letter => ($config->{'ref_slice_seq'}[$i++] eq $_ ? '|' : ($config->{'ref_slice_seq'}[$i-1] eq uc($_) ? '.' : $_)) }} split '', $seq;
-
-      while ($seq =~ m/([^~]+)/g) {
-        my $reseq_length = length $1;
-        my $reseq_end    = pos $seq;
-        
-        $markup->{'comparisons'}{$reseq_end - $_}{'resequencing'} = 1 for 1..$reseq_length;
-      }
-      
-      push @$sequence, \@cmp_seq;
-    }
-  } else {
-    push @$sequence, [ map {{ letter => $_ }} split '', $seq ];
-  }
-}
-
 sub set_variation_filter {
   my ($self, $config) = @_;
   my $hub = $self->hub;
