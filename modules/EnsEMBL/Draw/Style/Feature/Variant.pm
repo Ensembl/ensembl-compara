fc51d36b3ff1eb0213e3f293fac4976df5374f25
Anne Lyle <annelyle@ebi.ac.uk>
2016-08-24 14:40:05 +0100

    To keep drawing stuff out of cached data, make this style variant-specific

---
+++ modules/EnsEMBL/Draw/Style/Feature/Variant.pm
@@ -0,0 +1,99 @@
+=head1 LICENSE
+
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+Copyright [2016] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+package EnsEMBL::Draw::Style::Feature::Variant;
+
+=pod
+
+Renders a variation track with variants drawn differently
+depending on what type they are 
+
+=cut
+
+use List::Util qw(min);
+
+use parent qw(EnsEMBL::Draw::Style::Feature);
+
+sub draw_feature {
+### Draw a block with optional tags
+  my ($self, $feature, $position) = @_;
+
+  return unless ($feature->{'colour'} || $feature->{'bordercolour'});
+
+  if ($feature->{'type'}) {
+    my $method = 'draw_'.$feature->{'type'};
+    $self->$method($feature, $position) if $self->can($method); 
+  }
+  else {
+    $self->SUPER::draw_feature($feature, $position);
+  }
+}
+
+sub draw_insertion {
+### Draw a variant of type 'insertion'
+### @param feature  - hashref describing the feature 
+### @param position - hashref describing the position of the main feature
+  my ($self, $feature, $position) = @_;
+
+  my $composite = $self->Composite;
+
+  ## Draw a narrow line to mark the insertion point
+  my $x = $feature->{'start'};
+  $x    = 1 if $x < 1;
+  my $params = {
+                  x         => $x - 1,
+                  y         => $position->{'y'},
+                  width     => $position->{'width'} / (2 * $self->{'pix_per_bp'}),
+                  height    => $position->{'height'},
+                  href      => $feature->{'href'},
+                  colour    => $feature->{'colour'},
+                  title     => $feature->{'title'},
+                  absolutey => 1,
+                };
+  $composite->push($self->Rect($params));
+
+  ## invisible box to make inserts more clickable
+  my $width = min(1, 16 / $self->{'pix_per_bp'});
+  $composite->push($self->Rect({
+                                  x         => $x - 1 - $position->{'width'} / 2,
+                                  y         => 0,
+                                  width     => $width,
+                                  height    => $position->{'height'} + 2,
+                                  href      => $feature->{'href'},
+                                  absolutey => 1,
+                                }));
+
+  ## Draw a triangle below the line to identify it as an insertion
+  $params = {
+              width         => int(6 / $self->{'pix_per_bp'}),
+              height        => 4,
+              direction     => 'up',
+              mid_point     => [ $x - 1, $position->{'height'} - 1 ],
+              colour        => $feature->{'colour'},
+              absolutey     => 1,
+              no_rectangle  => 1,
+             };
+  my $triangle = $self->Triangle($params);
+
+  ## OK, all done!
+  ## Note that we can't add the triangle to the composite, for Reasons
+  push @{$self->glyphs}, $composite, $triangle;
+}
+
+1;
