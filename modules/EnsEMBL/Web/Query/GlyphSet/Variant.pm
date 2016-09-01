86e0592114da48f1db8bf64a72e279f0497bcbab
Anne Lyle <annelyle@ebi.ac.uk>
2016-09-01 14:00:37 +0100

    Comment out unused params

--- modules/EnsEMBL/Web/Query/GlyphSet/Variant.pm
+++ modules/EnsEMBL/Web/Query/GlyphSet/Variant.pm
@@ -121,7 +121,8 @@ sub type {
   my $type;
 
   if (($args->{'config'}{'style'}||'') eq 'box') {
-    $type = $f->start > $f->end ? 'left-snp' : $f->var_class eq 'in-del' ? 'delta' : 'box';
+    ## We don't actually do anything with these styles, so ignore them for now
+    #$type = $f->start > $f->end ? 'left-snp' : $f->var_class eq 'in-del' ? 'delta' : '';
   }
   elsif  ($f->start > $f->end) {
     $type = 'insertion';
