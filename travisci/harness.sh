42099e6226331fcfb32839960e6efe3dcddc680c
Anne Lyle <annelyle@ebi.ac.uk>
2016-09-05 15:50:59 +0100

    Need ensembl-io

--- travisci/harness.sh
+++ travisci/harness.sh
@@ -2,7 +2,7 @@
 
 ENSDIR="${ENSDIR:-$PWD}"
 
-export PERL5LIB=$ENSDIR/bioperl-live:$ENSDIR/ensembl-test/modules:$ENSDIR/ensembl-webcode/modules:$PWD/modules:$ENSDIR/ensembl/modules:$ENSDIR/ensembl-compara/modules:$ENSDIR/ensembl-funcgen/modules:$ENSDIR/ensembl-variation/modules
+export PERL5LIB=$ENSDIR/ensembl-test/modules:$ENSDIR/ensembl-io/modules::$ENSDIR/ensembl-webcode/modules:$PWD/modules
 export TEST_AUTHOR=$USER
 
 echo "Running test suite"
