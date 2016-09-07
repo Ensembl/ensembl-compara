b85e7d84841e02db5c3fc2b3f5ababca3d01612f
Anne Lyle <annelyle@ebi.ac.uk>
2016-09-07 14:38:23 +0100

    Try some code found on github

--- travisci/harness.sh
+++ travisci/harness.sh
@@ -3,21 +3,21 @@
 ENSDIR="${ENSDIR:-$PWD}"
 
 ## Install mod_perl manually
-cd $ENSDIR
-curl http://mirrors.muzzy.org.uk/apache//httpd/httpd-2.2.31.tar.gz > apache.tar.gz
-curl http://apache.org/dist/perl/mod_perl-2.0.9.tar.gz > mod_perl.tar.gz
-tar zxf apache.tar.gz
-tar zxf mod_perl.tar.gz | tar xvf -
-cd httpd-2.2.31
-./configure --enable-deflate --prefix=$ENSDIR/apache2
-cd ..
-echo '########## READY TO INSTALL MOD_PERL ##############'
-ls -l httpd-2.2.31
-cd mod_perl-2.0.9
+#cd $ENSDIR
+#curl http://mirrors.muzzy.org.uk/apache//httpd/httpd-2.2.31.tar.gz > apache.tar.gz
+#curl http://apache.org/dist/perl/mod_perl-2.0.9.tar.gz > mod_perl.tar.gz
+#tar zxf apache.tar.gz
+#tar zxf mod_perl.tar.gz | tar xvf -
+#cd httpd-2.2.31
+#./configure --enable-deflate --prefix=$ENSDIR/apache2
+#cd ..
+#echo '########## READY TO INSTALL MOD_PERL ##############'
+#ls -l httpd-2.2.31
+#cd mod_perl-2.0.9
 
-perl Makefile.PL PREFIX=$ENSDIR/apache2 MP_APXS=$ENSDIR/apache2/bin/apxs
-make
-make install
+#perl Makefile.PL PREFIX=$ENSDIR/apache2 MP_APXS=$ENSDIR/apache2/bin/apxs
+#make
+#make install
 
 ## Proceed with test harness
 
