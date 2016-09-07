b1d8e05a2e37b4360582aa30183152229b512e78
Anne Lyle <annelyle@ebi.ac.uk>
2016-09-07 13:54:00 +0100

    Try apt-get again for apache + mod_perl

--- travisci/harness.sh
+++ travisci/harness.sh
@@ -2,25 +2,6 @@
 
 ENSDIR="${ENSDIR:-$PWD}"
 
-## Install mod_perl manually
-cd $ENSDIR
-curl http://mirrors.muzzy.org.uk/apache//httpd/httpd-2.2.31.tar.gz > apache.tar.gz
-curl http://apache.org/dist/perl/mod_perl-2.0.9.tar.gz > mod_perl.tar.gz
-tar zxf apache.tar.gz
-tar zxf mod_perl.tar.gz | tar xvf -
-cd httpd-2.2.31
-./configure --enable-deflate --prefix=$ENSDIR/apache2
-cd ..
-echo '########## READY TO INSTALL MOD_PERL ##############'
-ls -l httpd-2.2.31
-cd mod_perl-2.0.9
-
-perl Makefile.PL PREFIX=$ENSDIR/apache2 MP_APXS=$ENSDIR/apache2/bin/apxs
-make
-make install
-
-## Proceed with test harness
-
 export PERL5LIB=$ENSDIR/bioperl-live:$ENSDIR/ensembl/modules:$ENSDIR/ensembl-variation/modules:$ENSDIR/ensembl-test/modules:$ENSDIR/ensembl-io/modules::$ENSDIR/ensembl-webcode/modules:$PWD/modules
 export TEST_AUTHOR=$USER
 
