68ef72ad702cb9c5965fe76c658921627c9a02cd
Harpreet Riat <harpreet@ebi.ac.uk>
2016-09-09 16:30:16 +0100

    Hub gets created implicitly

--- modules/EnsEMBL/Web/Document/HTML.pm
+++ modules/EnsEMBL/Web/Document/HTML.pm
@@ -22,7 +22,6 @@ package EnsEMBL::Web::Document::HTML;
 use strict;
 
 use EnsEMBL::Web::Document::Panel;
-use EnsEMBL::Web::Hub;
 use EnsEMBL::Web::DBSQL::ArchiveAdaptor;
 use EnsEMBL::Web::File::Utils::IO qw/file_exists read_file/;
 use LWP::UserAgent;
