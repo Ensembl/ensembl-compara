26234aaf03d5e00770d3bba181064ceffe68c664
Mateus Patricio <mateus@ebi.ac.uk>
2016-09-21 10:55:06 +0100

    Resetting resource classes to more reasonable values.

--- modules/Bio/EnsEMBL/Compara/PipeConfig/ProteinTrees_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/ProteinTrees_conf.pm
@@ -1569,7 +1569,7 @@ sub core_pipeline_analyses {
             -parameters => {
                 'division'                  => $self->o('division'),
             },
-            -rc_name => '8Gb_job',
+            -rc_name => '2Gb_job',
         },
 
         {   -logic_name     => 'cluster_tagging',
