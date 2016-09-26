25a3191c23d02b497847af2ac402e53c826fc6b1
Steve Trevanion <st3@sanger.ac.uk>
2016-09-26 17:53:21 +0100

    new release month

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -20,8 +20,8 @@ use Text::Wrap;
 $Text::Wrap::columns = 75;
 
 our $ENSEMBL_VERSION           = 86;
-our $ARCHIVE_VERSION           = 'Sep2016';    # Change this to the archive site for this version
-our $ENSEMBL_RELEASE_DATE      = 'Sept 2016';
+our $ARCHIVE_VERSION           = 'Oct2016';    # Change this to the archive site for this version
+our $ENSEMBL_RELEASE_DATE      = 'Oct 2016';
 
 #### START OF VARIABLE DEFINITION #### DO NOT REMOVE OR CHANGE THIS COMMENT ####
 
