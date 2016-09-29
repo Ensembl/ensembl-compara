01a742dbc0cadd25d81d805cd699d9cd83279009
Dan Sheppard <dan@ebi.ac.uk>
2016-09-29 14:48:28 +0100

    ENSWEB-3126

--- modules/EnsEMBL/Web/Object/Location.pm
+++ modules/EnsEMBL/Web/Object/Location.pm
@@ -91,7 +91,7 @@ sub has_strainpop {
   my ($self) = @_;
 
   my $hub = $self->hub;
-  my $pop_adaptor = $hub->databases->{'DATABASE_VARIATION'} ? $hub->get_adaptor('get_PopulationAdaptor','variation') : undef;
+  my $pop_adaptor = $hub->species_defs->databases->{'DATABASE_VARIATION'} ? $hub->get_adaptor('get_PopulationAdaptor','variation') : undef;
   my $pop = $pop_adaptor && $pop_adaptor->fetch_by_name('Mouse Genomes Project');
   return defined $pop;
 }
