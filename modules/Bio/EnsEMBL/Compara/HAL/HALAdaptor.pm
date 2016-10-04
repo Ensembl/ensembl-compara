318a4221067af8b16f4b706a554092a892a9c4f1
Matthieu Muffato <muffato@ebi.ac.uk>
2016-10-04 12:30:25 +0100

    Use /**/ to comment-out a big block

--- modules/Bio/EnsEMBL/Compara/HAL/HALAdaptor.pm
+++ modules/Bio/EnsEMBL/Compara/HAL/HALAdaptor.pm
@@ -350,27 +350,29 @@ void _get_multiple_aln_blocks( int halfileHandle, char *querySpecies, char *targ
 
     //printf("%s\n", "MSA 1");
 
+    /*
     // create a hal_species_t struct for querySpecies
-    // struct hal_species_t* head = NULL;
-    // struct hal_species_t* prev = NULL;
-    // struct hal_species_t* cur  = NULL;
+    struct hal_species_t* head = NULL;
+    struct hal_species_t* prev = NULL;
+    struct hal_species_t* cur  = NULL;
 
-    // char *str_copy = strdup(querySpecies);
-    // char *str_copy_ptr = str_copy;
-    // char *token;
-    // while ((token = strsep(&str_copy_ptr, ","))) {
-    //     cur = (struct hal_species_t*) calloc(1, sizeof(struct hal_species_t));
-    //     cur->name = strdup(token);
-    //     cur->next = NULL;
-    //     if ( head == NULL ){ //struct start
-    //         head = cur;
-    //     }
-    //     else {
-    //         prev->next = cur;
-    //     }
-    //     prev = cur;
-    // }
-    // free(str_copy);
+    char *str_copy = strdup(querySpecies);
+    char *str_copy_ptr = str_copy;
+    char *token;
+    while ((token = strsep(&str_copy_ptr, ","))) {
+        cur = (struct hal_species_t*) calloc(1, sizeof(struct hal_species_t));
+        cur->name = strdup(token);
+        cur->next = NULL;
+        if ( head == NULL ){ //struct start
+            head = cur;
+        }
+        else {
+            prev->next = cur;
+        }
+        prev = cur;
+    }
+    free(str_copy);
+    */
 
     // only way seems to be to fetch all and split structure into 2
     struct hal_species_t *hal_genomes = halGetSpecies(halfileHandle, NULL);
