264ec65c6a2367f0d1b44f59acfabb04ff634f7b
Mateus Patricio <mateus@ebi.ac.uk>
2016-10-11 11:11:45 +0100

    New features to load files from different sources.
    This commit only adds new features previous capabilities are preserved.

--- modules/Bio/EnsEMBL/Compara/RunnableDB/LoadMembersFromFiles.pm
+++ modules/Bio/EnsEMBL/Compara/RunnableDB/LoadMembersFromFiles.pm
@@ -89,69 +89,81 @@ sub write_output {
       my $seq_member_adaptor = $compara_dba->get_SeqMemberAdaptor();
 
       print Dumper($self->param('genome_content')) if $self->debug;
-      my $prot_seq = $self->param('genome_content')->get_protein_sequences;
-      my $cds_seq = $self->param('genome_content')->get_cds_sequences;
-      my $gene_coordinates = $self->param('genome_content')->get_gene_coordinates;
-      my $cds_coordinates = $self->param('genome_content')->get_cds_coordinates;
+      my ($prot_seq,$cds_seq) = $self->param('genome_content')->get_sequences;
+
+      my $prot_count = keys(%{$prot_seq});
+      my $cds_count  = keys(%{$cds_seq});
+
+      print "PROT:$prot_count\tCDS:$cds_count\n";
+
+      my ($gene_coordinates,$cds_coordinates) = $self->param('genome_content')->get_coordinates();
+
       my $taxon_id = $self->param('genome_content')->get_taxonomy_id;
 
       my %cached_dnafrags = ();
 
-	my $count = 0;
-      foreach my $gene_name (keys %$prot_seq) {
-		
-		$count++;
-            my $sequence = $prot_seq->{$gene_name};
+	  my $count = 0;
 
-		print "sequence $count: name ", $sequence->id, "\n" if ($self->debug > 1);
+      foreach my $prot_id (keys %$prot_seq) {
+
+		$count++;
+        my $sequence = $prot_seq->{$prot_id}->{'seq_obj'};
+        my $display_name = $prot_seq->{$prot_id}->{'display_name'} if ($prot_seq->{$prot_id}->{'display_name'});
+
+		print "sequence $count: name ", $prot_id, "\n" if ($self->debug > 1);
 		print "sequence $count: description ", $sequence->desc, "\n" if ($self->debug > 1);
 		print "sequence $count: length ", $sequence->length, "\n" if ($self->debug > 1);
 
 		my $gene_member = Bio::EnsEMBL::Compara::GeneMember->new(
-                -stable_id      => $gene_name,
+                -stable_id      => $prot_id,
                 -source_name    => 'EXTERNALGENE',
                 -taxon_id       => $taxon_id,
                 -description    => $sequence->desc,
                 -genome_db_id   => $genome_db_id,
             );
 
-		$gene_member->display_label($sequence->id);
-            if (exists $gene_coordinates->{$sequence->id}) {
-                my $coord = $gene_coordinates->{$sequence->id};
+		$gene_member->display_label($display_name);
+            if (exists $gene_coordinates->{$prot_id}) {
+               my $coord = $gene_coordinates->{$prot_id};
                 if (not $cached_dnafrags{$coord->[0]}) {
                     $cached_dnafrags{$coord->[0]} = Bio::EnsEMBL::Compara::DnaFrag->new(-GENOME_DB => $self->param('genome_db'), -NAME => $coord->[0]);
+                    my $dnafrag_adaptor = $compara_dba->get_DnaFragAdaptor();
+		            $dnafrag_adaptor->store($cached_dnafrags{$coord->[0]}) || die "Could not store dnafrags";
                 }
                 $gene_member->dnafrag($cached_dnafrags{$coord->[0]});
                 $gene_member->dnafrag_start($coord->[1]);
                 $gene_member->dnafrag_end($coord->[2]);
                 $gene_member->dnafrag_strand($coord->[3]);
             } else {
-                warn $sequence->id, " does not have gene coordinates\n";
+                warn $prot_id, " does not have gene coordinates\n";
+                die $prot_id, " does not have gene coordinates\n";
             }
 
-            #print Dumper($gene_member);
 		$gene_member_adaptor->store($gene_member);
 
 		my $pep_member = Bio::EnsEMBL::Compara::SeqMember->new(
-                -stable_id      => $gene_name,
+                -stable_id      => $prot_id,
                 -source_name    => 'EXTERNALPEP',
                 -taxon_id       => $taxon_id,
                 -description    => $sequence->desc,
                 -genome_db_id   => $genome_db_id,
             );
-		$pep_member->display_label($sequence->id);
+		$pep_member->display_label($prot_id);
             $pep_member->gene_member_id($gene_member->dbID);
-            if (exists $cds_coordinates->{$sequence->id}) {
-                my $coord = $cds_coordinates->{$sequence->id};
+            if (exists $cds_coordinates->{$prot_id}) {
+                my $coord = $cds_coordinates->{$prot_id};
                 if (not $cached_dnafrags{$coord->[0]}) {
                     $cached_dnafrags{$coord->[0]} = Bio::EnsEMBL::Compara::DnaFrag->new(-GENOME_DB => $self->param('genome_db'), -NAME => $coord->[0]);
+                    my $dnafrag_adaptor = $compara_dba->get_DnaFragAdaptor();
+		            $dnafrag_adaptor->store($cached_dnafrags{$coord->[0]}) || die "Could not store dnafrag";
                 }
                 $pep_member->dnafrag($cached_dnafrags{$coord->[0]});
                 $pep_member->dnafrag_start($coord->[1]);
                 $pep_member->dnafrag_end($coord->[2]);
                 $pep_member->dnafrag_strand($coord->[3]);
             } else {
-                warn $sequence->id, " does not have cds coordinates\n";
+                warn $prot_id, " does not have cds coordinates\n";
+                die $prot_id, " does not have cds coordinates\n";
             }
 		my $seq = $sequence->seq;
 		$seq =~ s/O/X/g;
@@ -159,12 +171,12 @@ sub write_output {
 		$seq_member_adaptor->store($pep_member);
             $seq_member_adaptor->_set_member_as_canonical($pep_member);
 
-            if (exists $cds_seq->{$sequence->id}) {
-                $sequence_adaptor->store_other_sequence($pep_member, $cds_seq->{$sequence->id}->seq, 'cds');
+            if (exists $cds_seq->{ $prot_id }) {
+                $sequence_adaptor->store_other_sequence($pep_member, $cds_seq->{ $prot_id }->{'seq_obj'}->seq, 'cds');
             } elsif ($self->param('need_cds_seq')) {
-                die $sequence->id, " does not have cds sequence\n";
+                die $prot_id, " does not have cds sequence\n";
             } else {
-                warn $sequence->id, " does not have cds sequence\n";
+                warn $prot_id, " does not have cds sequence\n";
             } 
       };
 
