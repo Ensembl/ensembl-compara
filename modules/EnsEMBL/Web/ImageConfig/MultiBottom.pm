a408075fc70d3496596a09130f570248d5ac1414
Ridwan Amode <ridwan@ebi.ac.uk>
2016-10-24 16:58:54 +0100

    move gencode basic track from each imageconfig to parent imageconfigextension/tracks.pm

--- modules/EnsEMBL/Web/ImageConfig/MultiBottom.pm
+++ modules/EnsEMBL/Web/ImageConfig/MultiBottom.pm
@@ -68,27 +68,6 @@ sub init_cacheable {
     information
   ));
 
-  my $gencode_version = $self->hub->species_defs->GENCODE_VERSION ? $self->hub->species_defs->GENCODE_VERSION : '';
-  $self->add_track('transcript', 'gencode', "Basic Gene Annotations from $gencode_version", '_gencode', {
-    labelcaption => "Genes (Basic set from $gencode_version)",
-    display     => 'off',
-    description => 'The GENCODE set is the gene set for human and mouse. GENCODE Basic is a subset of representative transcripts (splice variants).',
-    sortable    => 1,
-    colours     => $self->species_defs->colour('gene'),
-    label_key  => '[biotype]',
-    logic_names => ['proj_ensembl',  'proj_ncrna', 'proj_havana_ig_gene', 'havana_ig_gene', 'ensembl_havana_ig_gene', 'proj_ensembl_havana_lincrna', 'proj_havana', 'ensembl', 'mt_genbank_import', 'ensembl_havana_lincrna', 'proj_ensembl_havana_ig_gene', 'ncrna', 'assembly_patch_ensembl', 'ensembl_havana_gene', 'ensembl_lincrna', 'proj_ensembl_havana_gene', 'havana'],
-    renderers   =>  [
-      'off',                     'Off',
-      'gene_nolabel',            'No exon structure without labels',
-      'gene_label',              'No exon structure with labels',
-      'transcript_nolabel',      'Expanded without labels',
-      'transcript_label',        'Expanded with labels',
-      'collapsed_nolabel',       'Collapsed without labels',
-      'collapsed_label',         'Collapsed with labels',
-      'transcript_label_coding', 'Coding transcripts only (in coding genes)',
-    ],
-  }) if($gencode_version);
-
   # Add in additional tracks
   $self->load_tracks;
 
