eb2cce570033068ac5d140dc3289aee72d25caf0
Matthieu Muffato <muffato@ebi.ac.uk>
2016-11-15 07:32:50 +0000

    For some clusterset_ids the trees are not rooted, so we expect a trifurcation

--- modules/Bio/EnsEMBL/Compara/RunnableDB/GeneTrees/SqlHealthChecks.pm
+++ modules/Bio/EnsEMBL/Compara/RunnableDB/GeneTrees/SqlHealthChecks.pm
@@ -310,7 +310,7 @@ our $config = {
         tests => [
             {
                 description => 'Checks that the gene tree is binary (and minimized)',
-                query => 'SELECT gtn1.node_id FROM gene_tree_node gtn1 JOIN gene_tree_node gtn2 ON gtn1.node_id = gtn2.parent_id WHERE gtn1.root_id = #gene_tree_id# GROUP BY gtn1.node_id HAVING COUNT(*) != 2',
+                query => 'SELECT gtn1.node_id FROM gene_tree_root gtr JOIN gene_tree_node gtn1 ON gtr.root_id=gtn1.node_id JOIN gene_tree_node gtn2 ON gtn1.node_id = gtn2.parent_id WHERE gtn1.root_id = #gene_tree_id# GROUP BY clusterset_id, gtn1.root_id, gtn1.node_id HAVING COUNT(*) != IF(gtn1.node_id!=gtn1.root_id OR clusterset_id="default" OR clusterset_id LIKE "nj%" OR clusterset_id LIKE "phyml%" OR clusterset_id LIKE "rax%" OR clusterset_id LIKE "pg%",2,3)',
             },
 
             {
