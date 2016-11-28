5cf0ea5bea868b3f958b1f2249c7887cf9e32b27
Matthieu Muffato <muffato@ebi.ac.uk>
2016-11-28 17:38:10 +0000

    When the GenomeDB is about to be stored, it doesn't have an adaptor, so this was failing. Print the taxon_id instead

--- modules/Bio/EnsEMBL/Compara/GenomeDB.pm
+++ modules/Bio/EnsEMBL/Compara/GenomeDB.pm
@@ -653,7 +653,7 @@ sub display_name {
 
 sub get_scientific_name {
     my $self = shift;
-    my $n = $self->taxon_id ? $self->taxon->scientific_name : $self->name;
+    my $n = $self->taxon_id ? (($self->{'_taxon'} || $self->adaptor) ? $self->taxon->scientific_name : $self->taxon_id) : $self->name;
     $n .= " " . $self->strain_name if $self->strain_name;
     $n .= sprintf(' (component %s)', $self->genome_component) if $self->genome_component;
     return $n;
