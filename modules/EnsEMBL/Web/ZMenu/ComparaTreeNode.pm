ba662f429ee69b1b3975c9dcb744723faaa5f519
Jyothish <joyjoy.nt@gmail.com>
2016-12-06 10:52:45 +0000

    [ENSWEB-3019] adjust REST url for strain trees

--- modules/EnsEMBL/Web/ZMenu/ComparaTreeNode.pm
+++ modules/EnsEMBL/Web/ZMenu/ComparaTreeNode.pm
@@ -368,7 +368,8 @@ sub content {
     if ($hub->referer->{ENSEMBL_ACTION} ne 'Strain_Compara_Tree') {
 
       # Get wasabi files if found in session store
-      my $gt_id               = $node->tree->stable_id;
+      my $is_strain = $hub->species_defs->IS_STRAIN_OF ? 1 : 0;
+      my $gt_id               = $is_strain ? $gene->stable_id : $node->tree->stable_id;
       my $wasabi_session_key  = $gt_id . "_" . $node_id;
       my $wasabi_session_data = $hub->session->get_data(type=>'tree_files', code => 'wasabi');
 
@@ -391,9 +392,11 @@ sub content {
 
         my $is_success = head($rest_url);
         if ($is_success) {
-          $rest_url .= sprintf('/genetree/id/%s?content-type=text/javascript&aligned=1&subtree_node_id=%s',
-                       $gt_id,
-                       $node_id);
+          $rest_url .= sprintf('/genetree/%s/id/%s?content-type=text/javascript&aligned=1&subtree_node_id=%s&%s',
+                        $is_strain ? 'member' : '',
+                        $gt_id,
+                        $node_id,
+                        $is_strain ? 'clusterset_id=murinae' : '');
 
           if ($hub->wasabi_status) {
             $link = $hub->get_ExtURL('WASABI_ENSEMBL', {
