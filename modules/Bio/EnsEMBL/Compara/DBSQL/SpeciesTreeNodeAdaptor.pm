ff9adcb08fe0f249c48c8c4c4d058c6278768caa
Matthieu Muffato <muffato@ebi.ac.uk>
2016-12-12 13:15:41 +0000

    Die if the row we've just inserted could not be inserted

--- modules/Bio/EnsEMBL/Compara/DBSQL/SpeciesTreeNodeAdaptor.pm
+++ modules/Bio/EnsEMBL/Compara/DBSQL/SpeciesTreeNodeAdaptor.pm
@@ -127,7 +127,7 @@ sub store_node {
         $node->node_id( $node_id );
     }
 
-    $self->generic_update('species_tree_node',
+    my $rc = $self->generic_update('species_tree_node',
         {
             'parent_id'             => $node->parent ? $node->parent->node_id : undef,
             'root_id'               => $node->root->node_id,
@@ -140,6 +140,9 @@ sub store_node {
         }, {
             'node_id'               => $node->node_id,
         } );
+    if ($rc == 0) {
+        die "Could not update the newly-created species_tree_node row node_id=".$node->node_id.". Please investigate\n";
+    }
 
     return $node->node_id;
 }
