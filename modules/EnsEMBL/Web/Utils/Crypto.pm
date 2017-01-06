1dbd7c20a883ef7988a0ba53d05e0e82190c64b6
Dan Sheppard <dan@ebi.ac.uk>
2017-01-06 16:26:55 +0000

    Honeypot (missing file from last commit).

---
+++ modules/EnsEMBL/Web/Utils/Crypto.pm
@@ -0,0 +1,27 @@
+package EnsEMBL::Web::Utils::Crypto;
+
+use strict;
+use warnings;
+
+use Exporter qw(import);
+
+our @EXPORT_OK = qw(random_key);
+
+sub random_key {
+  my ($bytes) = @_;
+
+  my $data;
+  if(open(RANDOM,'<:raw','/dev/urandom')) {
+    local $/ = \$bytes;
+    my $data = <RANDOM>;
+    close RANDOM;
+  } else {
+    warn "Using weak crypto\n";
+    for(my $i=0;$i<$bytes;$i++) {
+      substr($data,$i,$i) = int rand 256;
+    }
+  }
+  return unpack("H[32]",$data);
+}
+
+1;
