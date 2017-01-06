11d950b4157e098e3632cac4dcebb16534e11709
Guy Naamati <gnaamati@ebi.ac.uk>
2017-01-06 10:05:25 +0000

    dnafrag name is 255

--- sql/table.sql
+++ sql/table.sql
@@ -604,7 +604,7 @@ CREATE TABLE synteny_region (
 CREATE TABLE dnafrag (
   dnafrag_id                  bigint unsigned NOT NULL AUTO_INCREMENT, # unique internal id
   length                      int(11) DEFAULT 0 NOT NULL,
-  name                        varchar(40) DEFAULT '' NOT NULL,
+  name                        varchar(255) DEFAULT '' NOT NULL,
   genome_db_id                int(10) unsigned NOT NULL, # FK genome_db.genome_db_id
   coord_system_name           varchar(40) DEFAULT '' NOT NULL,
   is_reference                tinyint(1) DEFAULT 1 NOT NULL,
@@ -2087,4 +2087,6 @@ INSERT INTO meta (species_id, meta_key, meta_value)
   VALUES (NULL, 'patch', 'patch_86_87_b.sql|gene_member_qc');
 INSERT INTO meta (species_id, meta_key, meta_value)
   VALUES (NULL, 'patch', 'patch_86_87_c.sql|msa_stats_to_stn_tag');
+INSERT INTO meta (species_id, meta_key, meta_value)
+  VALUES (NULL, 'patch', 'patch_87_88_b.sql|dnafrag_name_255');
 
