7dbc2104b2e1ad1dd665f1efff44501cbcbde00d
Matthieu Muffato <muffato@ebi.ac.uk>
2017-01-30 18:16:33 +0000

    Minimal factorization

--- modules/Bio/EnsEMBL/Compara/DBSQL/GeneTreeNodeAdaptor.pm
+++ modules/Bio/EnsEMBL/Compara/DBSQL/GeneTreeNodeAdaptor.pm
@@ -264,8 +264,6 @@ sub delete_node {
   my $node_id = $node->node_id;
   $self->dbc->do("UPDATE gene_tree_node dn, gene_tree_node n SET ".
             "n.parent_id = dn.parent_id WHERE n.parent_id=dn.node_id AND dn.node_id=$node_id");
-  $self->dbc->do("DELETE from gene_tree_node_tag    WHERE node_id = $node_id");
-  $self->dbc->do("DELETE from gene_tree_node_attr   WHERE node_id = $node_id");
   $self->dbc->do("UPDATE gene_tree_node SET root_id = NULL WHERE node_id = $node_id");
   $self->dbc->do("DELETE homology_member from homology_member JOIN homology using(homology_id) WHERE gene_tree_node_id = $node_id");
   $self->dbc->do("DELETE from homology WHERE gene_tree_node_id = $node_id");
@@ -277,7 +275,7 @@ sub delete_node {
     $self->dbc->do("DELETE FROM gene_tree_root WHERE root_id = $node_id");
   }
 
-  $self->dbc->do("DELETE from gene_tree_node   WHERE node_id = $node_id");
+  $self->delete_flattened_leaf($node);
 }
 
 sub delete_nodes_not_in_tree
