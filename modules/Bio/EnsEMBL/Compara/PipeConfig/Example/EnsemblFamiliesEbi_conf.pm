7346aaa68076664b882310e035e100aa34d987a5
Matthieu Muffato <muffato@ebi.ac.uk>
2017-02-01 14:55:32 +0000

    bugfix: "mysql://" was missing

--- modules/Bio/EnsEMBL/Compara/PipeConfig/Example/EnsemblFamiliesEbi_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/Example/EnsemblFamiliesEbi_conf.pm
@@ -124,7 +124,7 @@ sub default_options {
         'protein_trees_db' => 'mysql://ensadmin:' . $self->o('password') . '@mysql-ens-compara-prod-2:4522/mateus_protein_trees_87',
 
         # Production database (for the biotypes)
-        'production_db_url'     => 'ensro@mysql-ens-sta-1:4519/ensembl_production',
+        'production_db_url'     => 'mysql://ensro@mysql-ens-sta-1:4519/ensembl_production',
 
         # used by the StableIdMapper as the location of the master 'mapping_session' table:
         'master_db' => 'mysql://ensadmin:' . $self->o('password') . '@mysql-ens-compara-prod-1:4485/ensembl_compara_master', };
