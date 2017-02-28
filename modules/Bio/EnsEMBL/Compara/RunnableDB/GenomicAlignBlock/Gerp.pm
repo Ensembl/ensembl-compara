818fa2b67cff45abdaa13a932c037284889113b8
Matthieu Muffato <muffato@ebi.ac.uk>
2017-02-28 09:30:32 +0000

    complete_early() is a better solution when there is nothing to do

--- modules/Bio/EnsEMBL/Compara/RunnableDB/GenomicAlignBlock/Gerp.pm
+++ modules/Bio/EnsEMBL/Compara/RunnableDB/GenomicAlignBlock/Gerp.pm
@@ -150,8 +150,7 @@ sub fetch_input {
 	  #check still have enough species
 	  #print "NUM SPP $num_spp\n";
 	  if ($num_spp < 3) {
-	      $self->param('run_gerp', 0);
-	      return 1;
+              $self->complete_early('Less than 3 species in this block. Cannot run GERP. (have you asked to skip too many species ?)');
 	  }
 	  $tree_string = $gat->newick_format("simple");
           $tree_string=~s/:0;/;/; # Remove unused node at end
@@ -207,9 +206,8 @@ sub fetch_input {
 	  }
 	  close (PARAM_FILE_TMP);
       }
-      $self->param('run_gerp', 1);
   } else {
-      $self->param('run_gerp', 0);
+      $self->complete_early('Less than 3 sequences aligned in this block. Cannot run GERP');
   }
   return 1;
 }
@@ -227,10 +225,6 @@ sub fetch_input {
 sub run {
     my $self = shift;
 
-    #only run gerp if there are sufficient species present in the genomic align block
-    if (!$self->param('run_gerp')) {
-	return;
-    }
     $self->compara_dba->dbc->disconnect_if_idle();
     if ($self->param('program_version') == 1) { 
 	$self->run_gerp;
@@ -267,11 +261,6 @@ sub write_output {
 sub _write_output {
     my ($self) = @_;
   
-    #if haven't run gerp, don't try to store any results!
-    if (!$self->param('run_gerp')) { 
-	return 1;
-    }
-
     print STDERR "Write Output\n";
 
     #parse results and store constraints and conserved elements in database
