cf53e8aea69afc5ee8c5f2e9d7c7669726a64767
Harpreet Riat <harpreet@ebi.ac.uk>
2017-03-02 15:54:40 +0000

    Config used in the main webcode

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -88,6 +88,7 @@ our $ENSEMBL_REGISTRY                 = undef;  # Set this to a valid config fil
 our $ENSEMBL_SITE_DIR                 = '';     # URL Path if site is served from a sub path i.e www.example.org/$ENSEMBL_SITE_DIR/
 our $ENSEMBL_STATIC_SERVER            = '';     # Static server address - if static content (js/css/images) is served from a different server
 our $SYSLOG_COMMAND                   = sub { warn "$_[0]\n"; }  # command/subroutine called by `syslog` - check EnsEMBL::Web::Utils::Syslog
+our $TIDY_USERDB_CONNECTIONS          = 1;      # Clear user/session db connections after request is finished
 ###############################################################################
 
 
