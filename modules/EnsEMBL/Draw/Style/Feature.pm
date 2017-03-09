818f6a5bae9b8b544ba16b5d30780771aafc6aff
Anne Lyle <annelyle@ebi.ac.uk>
2017-03-09 10:49:09 +0000

    Use section height when calculating track height

--- modules/EnsEMBL/Draw/Style/Feature.pm
+++ modules/EnsEMBL/Draw/Style/Feature.pm
@@ -272,9 +272,12 @@ sub create_glyphs {
   }
 
   ## Check if overall track label (in LH margin) is taller than track itself
-  ## (Use stanaard font size plus padding)
+  ## (Use standard font size plus padding)
   my $track_label_height = $track_config->get('track_label_rows') * ($self->{'font_size'} + 6);
   $total_height = $track_label_height if $track_label_height > $total_height;
+  ## Also allow for tracks with complex track labels, e.g. regulation
+  my $section_height = $track_config->get('section_height'); 
+  $total_height += $section_height if $section_height;
 
   $self->draw_hidden_bgd($total_height);
   my $track_height = $track_config->get('total_height') || 0;
