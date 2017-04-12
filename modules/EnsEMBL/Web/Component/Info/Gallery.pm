7fb0606c6aec34b7a625894947167ad3eb2cbe22
Jyothish <joyjoy.nt@gmail.com>
2017-04-12 12:43:56 +0100

    Event tracking on plot gallery pages
    Signed-off-by: Jyothish <joyjoy.nt@gmail.com>

--- modules/EnsEMBL/Web/Component/Info/Gallery.pm
+++ modules/EnsEMBL/Web/Component/Info/Gallery.pm
@@ -95,7 +95,7 @@ sub format_gallery {
     ## Template for each entry
     my $entry_template = '<div class="gallery-preview">
                             <div class="page-preview">%s</div>
-                            <h3%s>%s</h3>
+                            <h3 class="%s">%s</h3>
                               <p class="preview-caption">%s</p>
                               <p%s>%s</p>
                           </div>';
@@ -115,7 +115,7 @@ sub format_gallery {
       my ($img_disabled, $img_title, $next_action);
       my $action_class  = '';
       my $link_class    = '';
-      my $title_class   = '';
+      my $title_class   = '_title';
       my ($img_link, $multi_form);
 
       if ($page->{'disabled'}) {
@@ -126,7 +126,7 @@ sub format_gallery {
           $next_action .= ': '.$page->{'message'};
         }
         $img_title    = $next_action;
-        $title_class  = ' class="disabled"';
+        $title_class  = ' disabled';
       }
       elsif ($page->{'multi'}) {
         my $multi_type = $page->{'multi'}{'type'};
