17d25f6b01b5bba46778a473f5e85027fa64c915
Anne Lyle <annelyle@ebi.ac.uk>
2017-05-02 16:13:03 +0100

    Simplify logic

--- conf/AutoIdentities.pm
+++ conf/AutoIdentities.pm
@@ -52,7 +52,8 @@ sub add_symlinks {
       foreach my $link (grep { !$done->{$_} } grep -l, map "$here/$_", readdir($dir)) {
         my $dest = readlink($link);
            $dest = abs_path($dest =~ m!^/! ? $dest : "$here/$dest");
-	      next if !$dest || $here = $dest;
+        next unless $dest;
+	      next if $here = $dest;
         foreach my $matching_dest (grep { $_ eq $dest } @$out) {
           push @$out, map { $_ =~ s/^\Q$matching_dest\E/$link/r } grep { m/^\Q$matching_dest\E/ } @$out;
           $done->{$link} = 1;
