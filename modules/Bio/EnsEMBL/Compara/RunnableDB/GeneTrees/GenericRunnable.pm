ca30efafe76d26450f36f2d1a9fb2dc90bc6a13e
Matthieu Muffato <muffato@ebi.ac.uk>
2017-06-29 18:17:02 +0100

    On recent versions of eHive, max_retry_count can be defined globally for all analyses

--- modules/Bio/EnsEMBL/Compara/RunnableDB/GeneTrees/GenericRunnable.pm
+++ modules/Bio/EnsEMBL/Compara/RunnableDB/GeneTrees/GenericRunnable.pm
@@ -125,7 +125,7 @@ sub param_defaults {
 sub fetch_input {
     my $self = shift @_;
 
-    if (defined $self->param('escape_branch') and $self->input_job->retry_count >= $self->input_job->analysis->max_retry_count) {
+    if (defined $self->param('escape_branch') and $self->input_job->retry_count >= ($self->input_job->analysis->max_retry_count // $self->db->hive_pipeline->hive_default_max_retry_count)) {
         $self->dataflow_output_id(undef, $self->param('escape_branch'));
         $self->input_job->autoflow(0);
         $self->complete_early(sprintf("The job is being tried for the %dth time: escaping to branch #%d\n", $self->input_job->retry_count, $self->param('escape_branch')));
