6f8221f88bd03d4b11ab21a4d5ff821fe56785a3
Harpreet Riat <harpreet@ebi.ac.uk>
2017-08-03 13:10:41 +0100

    moved PERL_RLIMIT_AS to SiteDefs to allow overriding the value

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -95,6 +95,7 @@ our $ENSEMBL_SERVER_SIGNATURE         = "$ENSEMBL_SERVER-$ENSEMBL_SERVERROOT" =~
 our $ENSEMBL_SITETYPE                 = 'Ensembl';
 our $ENSEMBL_HELPDESK_EMAIL           = defer { $ENSEMBL_SERVERADMIN };   # Email address for contact form and help pages
 our $ENSEMBL_REST_URL                 = 'http://rest.mydomain.org';       # url to your REST service
+our $PERL_RLIMIT_AS                   = '2048:4096';                      # linux does not honor RLIMIT_DATA, RLIMIT_AS (address space) will work to limit the size of a process
 our $CGI_POST_MAX                     = 20 * 1024 * 1024; # 20MB file upload max limit
 our $UPLOAD_SIZELIMIT_WITHOUT_INDEX   = 10 * 1024 * 1024; # 10MB max allowed for url uploads that don't have index files in the same path
 our $TRACKHUB_TIMEOUT                 = 60 * 60 * 24;     # Timeout for outgoing trackhub requests
@@ -255,6 +256,7 @@ $ENSEMBL_SETENV->{'http_proxy'}       = 'HTTP_PROXY';
 $ENSEMBL_SETENV->{'https_proxy'}      = 'HTTP_PROXY';
 $ENSEMBL_SETENV->{'COMPARA_HAL_DIR'}  = 'COMPARA_HAL_DIR';
 $ENSEMBL_SETENV->{'UDC_CACHEDIR'}     = 'UDC_CACHEDIR';
+$ENSEMBL_SETENV->{'PERL_RLIMIT_AS'}   = 'PERL_RLIMIT_AS';
 ###############################################################################
 
 
