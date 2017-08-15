f3e84d9e0eab98cd4bcca914f89e371a7af28f27
Matthieu Muffato <muffato@ebi.ac.uk>
2017-08-15 11:04:07 +0100

    Small schema change to allow the foreign key to seq_member

---
+++ sql/patch_90_91_b.sql
@@ -0,0 +1,28 @@
+-- Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+-- Copyright [2016-2017] EMBL-European Bioinformatics Institute
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+# patch_90_91_b.sql
+#
+# Title: Change the type of gene_member_qc.seq_member_id to allow a foreign key
+#
+# Description:
+# Foreign keys are only allowed between columns of identical types, but
+# gene_member_qc.seq_member_id was not set as "unsigned"
+
+ALTER TABLE gene_member_qc MODIFY COLUMN seq_member_id int(10) unsigned;
+
+# Patch identifier
+INSERT INTO meta (species_id, meta_key, meta_value)
+  VALUES (NULL, 'patch', 'patch_90_91_b.sql|gene_member_qc.seq_member_id.unsigned');
--- sql/table.sql
+++ sql/table.sql
@@ -1761,14 +1761,14 @@ CREATE TABLE gene_tree_node_attr (
 CREATE TABLE gene_member_qc (
   gene_member_stable_id       varchar(128) NOT NULL,
   genome_db_id                int(10) unsigned NOT NULL,
-  seq_member_id               int(10),
+  seq_member_id               int(10) unsigned,
   n_species                   INT,
   n_orth                      INT,
   avg_cov                     FLOAT,
   status                      varchar(50) NOT NULL,
 
   FOREIGN KEY (gene_member_stable_id) REFERENCES gene_member(stable_id),
-  -- FOREIGN KEY (seq_member_id) REFERENCES seq_member(seq_member_id),
+  FOREIGN KEY (seq_member_id) REFERENCES seq_member(seq_member_id),
   FOREIGN KEY (genome_db_id) REFERENCES genome_db(genome_db_id),
 
   KEY (gene_member_stable_id)
