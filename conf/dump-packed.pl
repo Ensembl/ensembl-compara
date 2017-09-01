7b1aa693958134a27da6a5ef3b48062055c18d4a
Dan Sheppard <dan@ebi.ac.uk>
2017-09-01 14:36:39 +0100

    Fix perl location.

--- conf/dump-packed.pl
+++ conf/dump-packed.pl
@@ -1,4 +1,4 @@
-#!/localsw/bin/perl
+#!/usr/bin/env perl
 # Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
 # Copyright [2016-2017] EMBL-European Bioinformatics Institute
 # 
