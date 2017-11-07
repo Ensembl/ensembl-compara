Ensembl/ensembl-compara

commit e1a360be4fc63314392bdd627916b402c1f2315f

Simplified the interface since the adaptor knows the file handle

modules/Bio/EnsEMBL/Compara/DBSQL/GenomicAlignBlockAdaptor.pm
@@ -1289,7 +1289,6 @@ sub _get_GenomicAlignBlocks_from_HAL {
         $mlss->{'_hal_adaptor'} = Bio::EnsEMBL::Compara::HAL::HALXS::HALAdaptor->new($hal_file);
     }
     my $hal_adaptor = $mlss->{'_hal_adaptor'};
-    my $hal_fh = $hal_adaptor->hal_filehandle;
     my $e2u_mappings = $Bio::EnsEMBL::Compara::HAL::UCSCMapping::e2u_mappings->{ $dnafrag->genome_db_id };
     my $hal_seq_reg = $e2u_mappings->{ $dnafrag->name } || $dnafrag->name;
 
@@ -1306,7 +1305,7 @@ sub _get_GenomicAlignBlocks_from_HAL {
       my $targets_str = join(',', @hal_targets);
 
       my $max_ref_gap = 50;
-      my $maf_file_str = $hal_adaptor->msa_blocks( $hal_fh, $targets_str, $ref, $hal_seq_reg, $start-1, $end, $max_ref_gap );
+      my $maf_file_str = $hal_adaptor->msa_blocks( $targets_str, $ref, $hal_seq_reg, $start-1, $end, $max_ref_gap );
 
       # check if MAF is empty
       unless ( $maf_file_str =~ m/[A-Za-z]/ ){
@@ -1445,7 +1444,7 @@ sub _get_GenomicAlignBlocks_from_HAL {
           # print "end is $end\n";
 
           my $t_hal_seq_reg = $Bio::EnsEMBL::Compara::HAL::UCSCMapping::e2u_mappings->{ $target_dnafrag->genome_db_id }->{ $target_dnafrag->name } || $target_dnafrag->name;
-          my $blocks = Bio::EnsEMBL::Compara::HAL::HALXS::HALAdaptor->pairwise_blocks($hal_fh, $target, $ref, $hal_seq_reg, $start, $end, $t_hal_seq_reg);
+          my $blocks = Bio::EnsEMBL::Compara::HAL::HALXS::HALAdaptor->pairwise_blocks($target, $ref, $hal_seq_reg, $start, $end, $t_hal_seq_reg);
           
           foreach my $entry (@$blocks) {
   	        if (defined $entry) {

modules/Bio/EnsEMBL/Compara/HAL/HALXS/HALAdaptor.pm
@@ -109,20 +109,20 @@ sub genomes {
 }
 
 sub msa_blocks {
-    my ( $self, $hal_fh, $targets_str, $ref, $hal_seq_reg, $start, $end, $max_ref_gap ) = @_;
+    my ( $self, $targets_str, $ref, $hal_seq_reg, $start, $end, $max_ref_gap ) = @_;
     $max_ref_gap ||= 0;
-    return HALXS::_get_multiple_aln_blocks( $hal_fh, $targets_str, $ref, $hal_seq_reg, $start, $end, $max_ref_gap );
+    return HALXS::_get_multiple_aln_blocks( $self->{'hal_fd'}, $targets_str, $ref, $hal_seq_reg, $start, $end, $max_ref_gap );
 }
 
 sub pairwise_blocks {
-    my ( $self, $hal_fh, $target, $ref, $hal_seq_reg, $start, $end, $target_seq_reg ) = @_;
+    my ( $self, $target, $ref, $hal_seq_reg, $start, $end, $target_seq_reg ) = @_;
 
     my @blocks;
     if ( $target_seq_reg ){
-        @blocks = HALXS::_get_pairwise_blocks_filtered($hal_fh, $target, $ref, $hal_seq_reg, $start, $end, $target_seq_reg);
+        @blocks = HALXS::_get_pairwise_blocks_filtered($self->{'hal_fd'}, $target, $ref, $hal_seq_reg, $start, $end, $target_seq_reg);
     }
     else {
-        @blocks = HALXS::_get_pairwise_blocks($hal_fh, $target, $ref, $hal_seq_reg, $start, $end);
+        @blocks = HALXS::_get_pairwise_blocks($self->{'hal_fd'}, $target, $ref, $hal_seq_reg, $start, $end);
     }
 
     return \@blocks;

commit 72deaa4484fe7caf5f38d6b566618df290c14b3d

Fixed a bug introduced in e1a360be4fc63314392bdd627916b402c1f2315f. We need to pass the object reference

modules/Bio/EnsEMBL/Compara/DBSQL/GenomicAlignBlockAdaptor.pm
@@ -1446,7 +1446,7 @@ sub _get_GenomicAlignBlocks_from_HAL {
           # print "end is $end\n";
 
           my $t_hal_seq_reg = $Bio::EnsEMBL::Compara::HAL::UCSCMapping::e2u_mappings->{ $target_dnafrag->genome_db_id }->{ $target_dnafrag->name } || $target_dnafrag->name;
-          my $blocks = Bio::EnsEMBL::Compara::HAL::HALXS::HALAdaptor->pairwise_blocks($target, $ref, $hal_seq_reg, $start-1, $end, $t_hal_seq_reg);
+          my $blocks = $hal_adaptor->pairwise_blocks($target, $ref, $hal_seq_reg, $start-1, $end, $t_hal_seq_reg);
           
           foreach my $entry (@$blocks) {
   	        if (defined $entry) {