14b9d5f62dd80202b70e6ed0af8a0d73a0027b92
Jyothish <joyjoy.nt@gmail.com>
2017-11-10 14:09:42 +0000

    ENSWEB-4184 show data regardless of whether transcript/reg_feat data is available

--- modules/EnsEMBL/Web/Configuration/Variation.pm
+++ modules/EnsEMBL/Web/Configuration/Variation.pm
@@ -58,7 +58,7 @@ sub populate_tree {
   
   $context_menu->append($self->create_node('Mappings', 'Genes and regulation',
     [qw( mappings EnsEMBL::Web::Component::Variation::Mappings )],
-    { 'availability' => 'variation has_features', 'concise' => 'Genes and regulation' }
+    { 'availability' => 'variation', 'concise' => 'Genes and regulation' }
   ));
   $context_menu->append($self->create_node('Sequence', 'Flanking sequence',
     [qw( flanking EnsEMBL::Web::Component::Variation::FlankingSequence )],
