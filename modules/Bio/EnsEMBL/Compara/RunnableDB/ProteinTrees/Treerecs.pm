7da86530c41733264fb9185dd3b23128c5a372ac
Mateus Patricio <mateus@ebi.ac.uk>
2018-01-12 17:06:56 +0000

    New module to run Treerecs.
    Treerecs is another tool for reconciliation os gene trees with species
    trees. It rearranges the gene trees with information from a given rooted
    species tree.
    
    More info:
    https://gitlab.inria.fr/Phylophile/Treerecs

---
+++ modules/Bio/EnsEMBL/Compara/RunnableDB/ProteinTrees/Treerecs.pm
@@ -0,0 +1,49 @@
+=head1 LICENSE
+
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+Copyright [2016-2018] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+
+=head1 CONTACT
+
+  Please email comments or questions to the public Ensembl
+  developers list at <http://lists.ensembl.org/mailman/listinfo/dev>.
+
+  Questions may also be sent to the Ensembl help desk at
+  <http://www.ensembl.org/Help/Contact>.
+
+=cut
+
+package Bio::EnsEMBL::Compara::RunnableDB::ProteinTrees::Treerecs;
+
+use strict;
+use warnings;
+
+use base ('Bio::EnsEMBL::Compara::RunnableDB::GeneTrees::GenericRunnable');
+
+sub param_defaults {
+    my $self = shift;
+    return {
+        %{$self->SUPER::param_defaults},
+        'cmd'                   => '#treerecs_exe# -g #gene_tree_file# -s #species_tree_file# -V --nhx -t 0.8 -o treerecs_result.#gene_tree_id# --output-without-description',
+        'runtime_tree_tag'      => 'treerecs_runtime',
+        'output_file'           => 'treerecs_result.#gene_tree_id#',
+        'run_treebest_sdi'      => 1,
+        'species_tree_label'    => 'binary',
+    };
+}
+1;
