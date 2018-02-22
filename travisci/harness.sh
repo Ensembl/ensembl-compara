1756c3f4c9b6b6074a930805fadd9a149087d183
Matthieu Muffato <muffato@ebi.ac.uk>
2018-02-22 12:38:00 +0000

    eHive 2.5 requires $EHIVE_ROOT_DIR

--- travisci/harness.sh
+++ travisci/harness.sh
@@ -20,6 +20,7 @@ echo "We are running Perl '$TRAVIS_PERL_VERSION', Coveralls status is set to '$C
 
 # Setup the environment variables
 export ENSEMBL_CVS_ROOT_DIR=$PWD
+export EHIVE_ROOT_DIR=$PWD/ensembl-hive
 export TEST_AUTHOR=$USER
 export PERL5LIB=$PWD/bioperl-live
 export PERL5LIB=$PERL5LIB:$PWD/modules
