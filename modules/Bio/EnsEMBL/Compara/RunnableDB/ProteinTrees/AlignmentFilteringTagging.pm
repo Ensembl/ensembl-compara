2500028e9dde7e7b5e5fd6694d61a8bd6acebaf4
Mateus Patricio <mateus@ebi.ac.uk>
2018-03-02 17:05:40 +0000

    New method to compute the alignment depth.
    This algorithm checks for each column of an alignment and checks the
    amount of sequences that are aligned for that particular position.
    
    e.g.:
    Sequence 1: (2+2+2)/3=2
    Sequence 2: (0+1+1+2+2+2+0+0)/8=1
    Sequence 3: (1+1+2+2+2)/5=1.6
    These values can themselves be averaged: (2+1+1.6)/3=1.53

--- modules/Bio/EnsEMBL/Compara/RunnableDB/ProteinTrees/AlignmentFilteringTagging.pm
+++ modules/Bio/EnsEMBL/Compara/RunnableDB/ProteinTrees/AlignmentFilteringTagging.pm
@@ -66,6 +66,9 @@ sub run {
     my $aligned_proportion = 1-$gappiness;
     $self->param( 'gappiness', $gappiness );
     $self->param( 'aligned_proportion', $aligned_proportion);
+
+    my $alignment_depth = $self->_get_alignment_depth();
+    $self->param( 'alignment_depth', $alignment_depth);
 }
 
 sub write_output {
@@ -76,6 +79,7 @@ sub write_output {
     $self->param('gene_tree')->store_tag( 'gene_count',              $self->param('gene_count') );
     $self->param('gene_tree')->store_tag( 'gappiness',               $self->param('gappiness') );
     $self->param('gene_tree')->store_tag( 'aligned_proportion',      $self->param('aligned_proportion') );
+    $self->param('gene_tree')->store_tag( 'alignment_depth',         $self->param('alignment_depth') );
 }
 
 ##########################################
@@ -182,5 +186,91 @@ sub _get_gappiness {
 
     return $gappiness;
 }
+
+sub _get_alignment_depth {
+    my $self = shift;
+
+    my $aln_length = $self->param('gene_tree')->get_value_for_tag('aln_length') || die "Could not fetch tag aln_length for root_id=" . $self->param_required('gene_tree_id');
+
+    my @cigar_lines_arrays;
+    my $member_counter = 0;
+
+    #COLLAPSED VERSION
+    print "expanded:\n";
+    foreach my $member ( @{ $self->param('cigar_lines') } ) {
+
+        #get cigar line
+        my $cigar_line          = $member->cigar_line;
+        $cigar_lines_arrays[$member_counter] = $member->get_cigar_array;
+        $member_counter++;
+    }
+
+    #cigar_lines_arrays => [
+    #           member_1    [[D,3], [M,3], [D2]],
+    #           member_2    [[M,8]],
+    #           member_3    [[D,1], [M,5], [D,2]]
+    #                      ]
+
+    # We could use this example to test the code:
+    # We need to comment the declarations of cigar_lines_arrays and member_counter above and redefine them like this:
+    #my @v1 = ('D',3);
+    #my @v2 = ('M',3);
+    #my @v3 = ('D',2);
+    #my @v4 = ('M',8);
+    #my @v5 = ('D',1);
+    #my @v6 = ('M',5);
+    #my @v7 = ('D',2);
+
+    #my @m1 = (\@v1,\@v2,\@v3);
+    #my @m2 = (\@v4);
+    #my @m3 = (\@v5,\@v6,\@v7);
+
+    #$cigar_lines_arrays[0] = \@m1;
+    #$cigar_lines_arrays[1] = \@m2;
+    #$cigar_lines_arrays[2] = \@m3;
+    #$member_counter = 3;
+
+    #print Dumper @cigar_lines_arrays;
+    #print scalar(@cigar_lines_arrays)."\n";
+    #print "$cigar_lines_arrays[0]->[0]->[0]\n";
+
+    #load
+
+    #Contains the sum of the aligned sequences per alignment column (iteration)
+    my %sum;
+    my @seq_num_of_aligned_positions;
+
+    #while number of memebers > 0
+    while (scalar(@{ $cigar_lines_arrays[0] })) {
+
+        my @aligned_members;
+        for ( my $member = 0; $member < $member_counter; $member++ ) {
+            #We always read the first element, No need to iterate in the position we always read from position 0
+            if ( $cigar_lines_arrays[$member]->[0]->[0] eq 'M' ) {
+                push(@aligned_members, $member);
+            }
+            $cigar_lines_arrays[$member]->[0]->[1]--;
+            if ( $cigar_lines_arrays[$member]->[0]->[1] == 0 ) {
+                shift(@{ $cigar_lines_arrays[$member] });
+            }
+        }
+
+        foreach my $aligned_member (@aligned_members) {
+            $sum{$aligned_member} += scalar(@aligned_members) - 1;
+            $seq_num_of_aligned_positions[$aligned_member]++;
+        }
+
+    } ## end while ( scalar(@cigar_lines_arrays...))
+
+    #average
+    my $total = 0;
+    foreach my $member ( keys %sum ) {
+        my $seq_avg = $sum{$member}/$seq_num_of_aligned_positions[$member];
+        #print $sum{$member} . "/" . $seq_num_of_aligned_positions[$member] . " = $seq_avg\n" if ($self->debug);
+        $total += $seq_avg;
+    }
+    my $avg = $total/scalar(keys(%sum));
+
+} ## end sub _get_alignment_depth
 
 1;
