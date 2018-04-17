Ensembl/ensembl-compara

commit 2829859ae40a78a779f51056f3ca860aea3e46f2

ENSWEB-4340: Improve the gene tree highlighting messages

modules/EnsEMBL/Web/Component/Gene/ComparaTree.pm
@@ -178,22 +178,26 @@ sub content {
     }
   }
 
-  if ($highlight_gene) {
-    my $highlight_gene_display_label;
-    
-    foreach my $this_leaf (@$leaves) {    
-      if ($highlight_gene && $this_leaf->gene_member->stable_id eq $highlight_gene) {
-        $highlight_gene_display_label = $this_leaf->gene_member->display_label || $highlight_gene;
-        $highlight_species            = $hub->species_defs->production_name_mapping($this_leaf->gene_member->genome_db->name);
-        $highlight_genome_db_id       = $this_leaf->gene_member->genome_db_id;
-        last;
-      }
+  # store g1 param in a different param as $highlight_gene can be undef if highlighting is disabled
+  my $gene_to_highlight = $hub->param('g1');
+  my $highlight_gene_display_label;  
+      
+  foreach my $this_leaf (@$leaves) {    
+    if ($gene_to_highlight && $this_leaf->gene_member->stable_id eq $gene_to_highlight) {
+      $highlight_gene_display_label = $this_leaf->gene_member->display_label || $gene_to_highlight;
+      $highlight_species            = $hub->species_defs->production_name_mapping($this_leaf->gene_member->genome_db->name);
+      $highlight_genome_db_id       = $this_leaf->gene_member->genome_db_id;
+      last;
     }
+  }
 
+  # use $highlight_gene to check if highlight is enabled or not
+  # $gene_to_highlight will be used for getting info necessary to display the highlighting message
+  if ($highlight_gene) {
     if ($member && $gene && $highlight_species) {
       $html .= $self->_info('Highlighted genes',
         sprintf(
-          '<p>The <i>%s</i> %s gene, its paralogues, its orthologue in <i>%s</i>, and paralogues of the <i>%s</i> gene, have all been highlighted. <a href="#" class="switch_highlighting on">Click here to switch off highlighting</a>.</p>',
+          '<p>The <i>%s</i> %s gene, its paralogues, its orthologue in <i>%s</i>, and paralogues of the <i>%s</i> gene, have all been highlighted. <a href="#" class="switch_highlighting on">Click here to disable highlighting</a>.</p>',
           $hub->species_defs->get_config($hub->species_defs->production_name_mapping($member->genome_db->name), 'SPECIES_COMMON_NAME'),
           $highlight_gene_display_label,
           $hub->species_defs->get_config($highlight_species, 'SPECIES_COMMON_NAME'),
@@ -205,7 +209,14 @@ sub content {
       $highlight_gene = undef;
     }
   } else {
-    $html .= $self->_info('Highlighted genes', '<p>Highlighting is turned off. <a href="#" class="switch_highlighting off">Click here to switch on highlighting</a>.</p>');
+    $html .= $self->_info('Highlighted genes', 
+      sprintf(
+        '<p><i>%s</i> %s gene and its paralogues are highlighted. <a href="#" class="switch_highlighting off">Click here to enable highlighting of %s homologues</a>.</p>',
+        $hub->species_defs->get_config($hub->species_defs->production_name_mapping($member->genome_db->name), 'SPECIES_COMMON_NAME'),
+        $highlight_gene_display_label,
+        $hub->species_defs->get_config($highlight_species, 'SPECIES_COMMON_NAME')
+      )
+    );
   }
   
   # Get all the genome_db_ids in each clade

commit 97f687321fa0f919a283f739b4cfc2aabf661b0a

Fix bug introduced in commit 2829859ae

modules/EnsEMBL/Web/Component/Gene/ComparaTree.pm
@@ -210,14 +210,7 @@ sub content {
       $highlight_gene = undef;
     }
   } else {
-    $html .= $self->_info('Highlighted genes', 
-      sprintf(
-        '<p><i>%s</i> %s gene and its paralogues are highlighted. <a href="#" class="switch_highlighting off">Click here to enable highlighting of %s homologues</a>.</p>',
-        $hub->species_defs->get_config($hub->species_defs->production_name_mapping($member->genome_db->name), 'SPECIES_COMMON_NAME'),
-        $highlight_gene_display_label,
-        $hub->species_defs->get_config($highlight_species, 'SPECIES_COMMON_NAME')
-      )
-    );
+    $html .= $self->_info('Highlighted genes', '<p>Highlighting is turned off. <a href="#" class="switch_highlighting off">Click here to switch on highlighting</a>.</p>');
   }
   
   # Get all the genome_db_ids in each clade