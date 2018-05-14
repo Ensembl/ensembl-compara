f3cd7e4f3a79a8bb31a8a1331c975b78dacaf8d0
Matthieu Muffato <muffato@ebi.ac.uk>
2018-05-14 12:19:41 +0100

    Need INPUT_PLUS to propagate all the parameters, incl. the original goc_mlss_id

--- modules/Bio/EnsEMBL/Compara/PipeConfig/Parts/GOC.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/Parts/GOC.pm
@@ -168,7 +168,7 @@ sub pipeline_analyses_goc {
             -logic_name => 'get_genetic_distance',
             -module => 'Bio::EnsEMBL::Compara::RunnableDB::OrthologQM::Fetch_genetic_distance',
             -flow_into => {
-                1 =>    ['threshold_calculator'],
+                1 =>    { 'threshold_calculator' => INPUT_PLUS },
                -1 =>    [ 'get_genetic_distance_himem' ],  # MEMLIMIT
                 },
             -hive_capacity  =>  $self->o('goc_stats_capacity'),
@@ -178,7 +178,7 @@ sub pipeline_analyses_goc {
             -logic_name => 'get_genetic_distance_himem',
             -module => 'Bio::EnsEMBL::Compara::RunnableDB::OrthologQM::Fetch_genetic_distance',
             -flow_into => {
-                1 =>    ['threshold_calculator'],
+                1 =>    { 'threshold_calculator' => INPUT_PLUS },
                 },
             -hive_capacity  =>  $self->o('goc_stats_capacity'),
             -rc_name        => '2Gb_job',
