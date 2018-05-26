ba3db6ba16015eaffe6967b1bc3c8d84a2d018ba
Matthieu Muffato <muffato@ebi.ac.uk>
2018-05-26 17:42:35 +0100

    Joined EPO parts 2 and 3 into one pipeline

---
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/EBI/EPO_conf.pm
@@ -0,0 +1,143 @@
+=head1 LICENSE
+
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+Copyright [2016-2018] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+=head1 NAME
+
+Bio::EnsEMBL::Compara::PipeConfig::EBI::EPO_conf
+
+=head1 SYNOPSIS
+
+    EBI-specific configuration of EPO pipeline (anchor mapping). Options that
+    may need to be checked include:
+
+       'species_set_name'  - used in the naming of the database
+       'compara_anchor_db' - database containing the anchor sequences (entered in the anchor_sequence table)
+       'mlss_id'       - mlss_id for the epo alignment (in master)
+
+    #4. Run init_pipeline.pl script:
+        Using command line arguments:
+        init_pipeline.pl Bio::EnsEMBL::Compara::PipeConfig::EPO_conf.pm
+
+    #5. Run the "beekeeper.pl ... -sync" and then " -loop" command suggested by init_pipeline.pl
+
+    #6. Fix the code when it crashes
+
+=head1 DESCRIPTION
+
+    This configuaration file gives defaults for mapping (using exonerate at the moment) anchors to a set of target genomes (dumped text files)
+
+=head1 CONTACT
+
+Please email comments or questions to the public Ensembl
+developers list at <http://lists.ensembl.org/mailman/listinfo/dev>.
+
+Questions may also be sent to the Ensembl help desk at
+<http://www.ensembl.org/Help/Contact>.
+
+=cut
+
+package Bio::EnsEMBL::Compara::PipeConfig::EBI::EPO_conf;
+
+use strict;
+use warnings;
+
+use Bio::EnsEMBL::Hive::Version 2.4;
+
+use base ('Bio::EnsEMBL::Compara::PipeConfig::EPO_conf');
+
+sub default_options {
+    my ($self) = @_;
+
+    return {
+        %{$self->SUPER::default_options},
+
+        'species_set_name' => 'mammals',
+        #'rel_suffix' => 'b',
+
+        # Where the pipeline lives
+        'host' => 'mysql-ens-compara-prod-1.ebi.ac.uk',
+        'port' => 4485,
+
+        'reg_conf' => $self->o('ensembl_cvs_root_dir').'/ensembl-compara/scripts/pipeline/production_reg_ebi_conf.pl',
+
+
+        # Capacities
+        'low_capacity'                  => 10,
+        'map_anchors_batch_size'        => 20,
+        'map_anchors_capacity'          => 1000,
+        'trim_anchor_align_batch_size'  => 20,
+        'trim_anchor_align_capacity'    => 150,
+
+        'work_dir'  => '/hps/nobackup/production/ensembl/' . $ENV{USER} . '/' . $self->o('pipeline_name') . '/',
+
+        'species_tree_file' => $self->o('ensembl_cvs_root_dir').'/ensembl-compara/scripts/pipeline/species_tree.ensembl.branch_len.nw',
+
+        'bl2seq_exe'        => undef,   # We use blastn instead
+        'blastn'            => $self->check_exe_in_cellar('blast/2.2.30/bin/blastn'),
+        'enredo_exe'        => $self->check_exe_in_cellar('enredo/0.5.0/bin/enredo'),
+        'exonerate_exe'     => $self->check_exe_in_cellar('exonerate22/2.2.0/bin/exonerate'),
+        'server_exe'        => $self->check_exe_in_cellar('exonerate22/2.2.0/bin/exonerate-server'),
+        'fasta2esd_exe'     => $self->check_exe_in_cellar('exonerate22/2.2.0/bin/fasta2esd'),
+        'esd2esi_exe'       => $self->check_exe_in_cellar('exonerate22/2.2.0/bin/esd2esi'),
+        'gerp_exe_dir'      => $self->check_dir_in_cellar('gerp/20080211/bin'),
+        'java_exe'          => $self->check_exe_in_linuxbrew_opt('jdk@8/bin/java'),
+        'ortheus_bin_dir'   => $self->check_dir_in_cellar('ortheus/0.5.0_1/bin'),
+        'ortheus_c_exe'     => $self->check_exe_in_cellar('ortheus/0.5.0_1/bin/ortheus_core'),
+        'ortheus_lib_dir'   => $self->check_dir_in_cellar('ortheus/0.5.0_1'),
+        'pecan_exe_dir'     => $self->check_dir_in_cellar('pecan/0.8.0/libexec'),
+        'semphy_exe'        => $self->check_exe_in_cellar('semphy/2.0b3/bin/semphy'),
+
+        'gerp_version' => '2.1', #gerp program version
+
+        'epo_stats_report_email' => $ENV{'USER'} . '@ebi.ac.uk',
+
+        # Databases
+        'compara_master' => 'mysql://ensro@mysql-ens-compara-prod-1.ebi.ac.uk:4485/ensembl_compara_master',
+        # database containing the anchors for mapping
+        'compara_anchor_db' => 'mysql://ensro@mysql-ens-compara-prod-3.ebi.ac.uk:4523/sf5_TEST_gen_anchors_mammals_cat_100',
+        # the previous database to reuse the anchor mappings
+        'reuse_db' => 'mysql://ensro@mysql-ens-compara-prod-2.ebi.ac.uk:4522/waakanni_mammals_epo_anchor_mapping_93',
+        # The ancestral_db is created on the same server as the pipeline_db
+        'ancestral_db' => { # core ancestral db
+            -driver   => $self->o('pipeline_db', '-driver'),
+            -host     => $self->o('pipeline_db', '-host'),
+            -port     => $self->o('pipeline_db', '-port'),
+            -species  => $self->o('ancestral_sequences_name'),
+            -user     => $self->o('pipeline_db', '-user'),
+            -pass     => $self->o('pipeline_db', '-pass'),
+            -dbname   => $self->o('ENV', 'USER').'_'.$self->o('species_set_name').'_ancestral_core_'.$self->o('rel_with_suffix'),
+        },
+    };
+}
+
+sub resource_classes {
+    my ($self) = @_;
+    return {
+        %{$self->SUPER::resource_classes},  # inherit 'default' from the parent class
+        'default'   => {'LSF' => '-C0 -M2500 -R"select[mem>2500] rusage[mem=2500]"' }, # farm3 lsf syntax
+        'mem3500'   => {'LSF' => '-C0 -M3500 -R"select[mem>3500] rusage[mem=3500]"' },
+        '3.5Gb'     => {'LSF' => '-C0 -M3500 -R"select[mem>3500] rusage[mem=3500]"' },
+        'mem7500'   => {'LSF' => '-C0 -M7500 -R"select[mem>7500] rusage[mem=7500]"' },
+        'mem14000'  => {'LSF' => '-C0 -M14000 -R"select[mem>14000] rusage[mem=14000]"' },
+        'hugemem'   => {'LSF' => '-q hugemem -C0 -M30000 -R"select[mem>30000] rusage[mem=30000]"' },
+
+    };
+}
+
+1;
---
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/EPO_conf.pm
@@ -0,0 +1,254 @@
+=head1 LICENSE
+
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+Copyright [2016-2018] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+=head1 NAME
+
+Bio::EnsEMBL::Compara::PipeConfig::EPO_conf
+
+=head1 SYNOPSIS
+
+    #1. Update ensembl-hive, ensembl and ensembl-compara GIT repositories before each new release
+
+    #3. Check all default_options, you will probably need to change the following :
+        pipeline_db (-host)
+        resource_classes
+
+    'ensembl_cvs_root_dir' - the path to the compara/hive/ensembl GIT checkouts - set as an environment variable in your shell
+        'password' - your mysql password
+    'compara_anchor_db' - database containing the anchor sequences (entered in the anchor_sequence table)
+    'compara_master' - location of your master db containing relevant info in the genome_db, dnafrag, species_set, method_link* tables
+        The dummy values - you should not need to change these unless they clash with pre-existing values associated with the pairwise alignments you are going to use
+
+    #4. Run init_pipeline.pl script:
+        Using command line arguments:
+        init_pipeline.pl Bio::EnsEMBL::Compara::PipeConfig::EPO_conf.pm
+
+    #5. Run the "beekeeper.pl ... -sync" and then " -loop" command suggested by init_pipeline.pl
+
+    #6. Fix the code when it crashes
+
+=head1 DESCRIPTION
+
+    This configuaration file gives defaults for mapping (using exonerate at the moment) anchors to a set of target genomes (dumped text files)
+
+=head1 CONTACT
+
+Please email comments or questions to the public Ensembl
+developers list at <http://lists.ensembl.org/mailman/listinfo/dev>.
+
+Questions may also be sent to the Ensembl help desk at
+<http://www.ensembl.org/Help/Contact>.
+
+=cut
+
+package Bio::EnsEMBL::Compara::PipeConfig::EPO_conf;
+
+use strict;
+use warnings;
+
+use Bio::EnsEMBL::Hive::Version 2.4;
+use Bio::EnsEMBL::Hive::PipeConfig::HiveGeneric_conf;   # For INPUT_PLUS
+
+use Bio::EnsEMBL::Compara::PipeConfig::Parts::EPOMapAnchors;
+use Bio::EnsEMBL::Compara::PipeConfig::Parts::EPOAlignment;
+
+use base ('Bio::EnsEMBL::Compara::PipeConfig::ComparaGeneric_conf');
+
+sub default_options {
+    my ($self) = @_;
+
+    return {
+        %{$self->SUPER::default_options},
+
+        'pipeline_name' => $self->o('species_set_name').'_epo_'.$self->o('rel_with_suffix'),
+
+        # mlss_ids & co. Don't touch and define "mlss_id" on the command line
+        'anchors_mlss_id' => 10000, # this should correspond to the mlss_id in the anchor_sequence table of the compara_anchor_db database (from EPO_pt1_conf.pm)
+        # 'mlss_id' => 825, # epo mlss from master
+        'mapping_method_link_id' => 10000, # dummy value - should not need to change
+        'mapping_method_link_name' => 'MAP_ANCHORS', 
+        'mapping_mlssid' => 10000, # dummy value - should not need to change
+        'trimmed_mapping_mlssid' => 11000, # dummy value - should not need to change
+        'ancestral_sequences_name' => 'ancestral_sequences',
+
+        # Executable parameters
+        'mapping_params'    => { bestn=>11, gappedextension=>"no", softmasktarget=>"no", percent=>75, showalignment=>"no", model=>"affine:local", },
+        'enredo_params'     => ' --min-score 0 --max-gap-length 200000 --max-path-dissimilarity 4 --min-length 10000 --min-regions 2 --min-anchors 3 --max-ratio 3 --simplify-graph 7 --bridges -o ',
+        'gerp_window_sizes' => [1,10,100,500], #gerp window sizes
+
+        # Dump directory
+        'enredo_output_file'    => $self->o('work_dir').'/enredo_output.txt',
+        'bed_dir'               => $self->o('work_dir').'/bed',
+        'feature_dir'           => $self->o('work_dir').'/feature_dump',
+        'seq_dump_loc'          => $self->o('work_dir').'/genome_seq',
+        'enredo_mapping_file'   => $self->o('work_dir').'/enredo_input.txt',
+        'bl2seq_dump_dir'       => $self->o('work_dir').'/bl2seq', # location for dumping sequences to determine strand (for bl2seq)
+        'bl2seq_file_stem'      => '#bl2seq_dump_dir#/bl2seq',
+        'output_dir'            => '#feature_dir#', # alias
+
+        # Options
+        #skip this module if set to 1
+        'skip_multiplealigner_stats' => 0,
+        # dont dump the MT sequence for mapping
+        'only_nuclear_genome' => 1,
+        # add MT dnafrags separately (1) or not (0) to the dnafrag_region table
+        'add_non_nuclear_alignments' => 1,
+         # batch size of grouped anchors to map
+        'anchor_batch_size' => 10,
+         # max number of sequences to allow in an anchor
+        'anc_seq_count_cut_off' => 15,
+        # Usually set to 0 because we run Gerp on the EPO2X alignment instead
+        'run_gerp' => 0,
+
+        # Scripts
+        'dump_features_exe'     => $self->o('ensembl_cvs_root_dir')."/ensembl-compara/scripts/dumps/dump_features.pl",
+        'compare_beds_exe'      => $self->o('ensembl_cvs_root_dir')."/ensembl-compara/scripts/pipeline/compare_beds.pl",
+        'epo_stats_report_exe'  => $self->o('ensembl_cvs_root_dir')."/ensembl-compara/scripts/production/epo_stats.pl",
+    };
+}
+
+sub pipeline_create_commands {
+    my ($self) = @_;
+    return [
+        @{$self->SUPER::pipeline_create_commands},  # inheriting database and hive tables' creation
+        $self->pipeline_create_commands_rm_mkdir(['work_dir', 'bed_dir', 'feature_dir', 'seq_dump_loc', 'bl2seq_dump_dir']),
+        $self->pipeline_create_commands_lfs_setstripe('seq_dump_loc'),
+    ];
+}
+
+
+sub pipeline_wide_parameters {
+    my $self = shift @_;
+    return {
+        %{$self->SUPER::pipeline_wide_parameters},
+
+        # mlss_id & co
+        'mlss_id'                   => $self->o('mlss_id'),
+        'anchors_mlss_id'           => $self->o('anchors_mlss_id'),
+        'mapping_method_link_id'    => $self->o('mapping_method_link_id'),
+        'mapping_method_link_name'  => $self->o('mapping_method_link_name'),
+        'mapping_mlssid'            => $self->o('mapping_mlssid'),
+        'trimmed_mapping_mlssid'    => $self->o('trimmed_mapping_mlssid'),
+
+        # directories
+        'work_dir'              => $self->o('work_dir'),
+        'feature_dir'           => $self->o('feature_dir'),
+        'enredo_output_file'    => $self->o('enredo_output_file'),
+        'bed_dir'               => $self->o('bed_dir'),
+        'seq_dump_loc'          => $self->o('seq_dump_loc'),
+        'enredo_mapping_file'   => $self->o('enredo_mapping_file'),
+        'bl2seq_dump_dir'       => $self->o('bl2seq_dump_dir'),
+        'bl2seq_file_stem'      => $self->o('bl2seq_file_stem'),
+
+        # databases
+        'compara_anchor_db' => $self->o('compara_anchor_db'),
+        'master_db'         => $self->o('compara_master'),
+        'reuse_db'          => $self->o('reuse_db'),
+        'ancestral_db'      => $self->o('ancestral_db'),
+
+        # options
+        'run_gerp' => $self->o('run_gerp'),
+    };
+
+}
+
+sub core_pipeline_analyses {
+    my ($self) = @_;
+
+    return [
+
+        {   -logic_name => 'backbone_prepare_databases',
+            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::Dummy',
+            -input_ids => [{}],
+            -flow_into  => {
+                '1->A' => [ 'copy_table_factory', 'set_internal_ids', 'drop_ancestral_db' ],
+                'A->1' => 'backbone_align_anchors',
+            }
+        },
+
+        {   -logic_name => 'backbone_align_anchors',
+            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::Dummy',
+            -flow_into  => {
+                '1->A' => [ 'reuse_anchor_align_factory' ],
+                'A->1' => 'backbone_build_syntenic_regions',
+            }
+        },
+
+        {   -logic_name => 'backbone_build_syntenic_regions',
+            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::Dummy',
+            -flow_into  => {
+                '1->A' => [ 'remove_overlaps' ],
+                'A->1' => 'backbone_ortheus_alignments',
+            }
+        },
+
+        {   -logic_name => 'backbone_ortheus_alignments',
+            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::Dummy',
+            -flow_into  => {
+                '1->A' => [ 'load_dnafrag_region' ],
+                'A->1' => 'backbone_alignment_post_processing',
+            }
+        },
+
+        {   -logic_name => 'backbone_alignment_post_processing',
+            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::Dummy',
+            -flow_into  => {
+                '1->A' => [ 'remove_dodgy_ancestral_blocks' ],
+                'A->1' => 'backbone_end',
+            }
+        },
+
+        {   -logic_name => 'backbone_end',
+            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::Dummy',
+        },
+
+        @{ Bio::EnsEMBL::Compara::PipeConfig::Parts::EPOMapAnchors::pipeline_analyses_epo_anchor_mapping($self) },
+        @{ Bio::EnsEMBL::Compara::PipeConfig::Parts::EPOAlignment::pipeline_analyses_epo_alignment($self) },
+    ];
+}
+
+sub tweak_analyses {
+    my $self = shift;
+    my $analyses_by_name = shift;
+
+    # Build the species-tree once all the GenomeDBs have been loaded
+    $analyses_by_name->{'create_mlss_ss'}->{'-flow_into'} = [ 'make_species_tree', 'find_ancestral_seq_gdb' ];
+
+    # Remove the semaphore as "remove_overlaps" is in another part of the pipeline
+    $analyses_by_name->{'map_anchor_align_genome_factory'}->{'-flow_into'} = { 2 => $analyses_by_name->{'map_anchor_align_genome_factory'}->{'-flow_into'}->{'2->A'} };
+
+    # Move "dump_mappings_to_file" to after having trimmed the anchors
+    delete $analyses_by_name->{'make_species_tree'}->{'-flow_into'};
+    $analyses_by_name->{'trim_anchor_align_factory'}->{'-flow_into'} = {
+        '2->A' => $analyses_by_name->{'trim_anchor_align_factory'}->{'-flow_into'}->{2},
+        'A->1' => [ 'dump_mappings_to_file' ],
+    };
+    delete $analyses_by_name->{'store_ancestral_species_name'}->{'-flow_into'};
+
+    # Remove the dataflow as "load_dnafrag_region" is in another part of the pipeline
+    delete $analyses_by_name->{'run_enredo'}->{'-flow_into'};
+
+    # Remove the semaphore as "remove_dodgy_ancestral_blocks" is in another part of the pipeline
+    $analyses_by_name->{'load_dnafrag_region'}->{'-flow_into'} = {
+        '2' => $analyses_by_name->{'load_dnafrag_region'}->{'-flow_into'}->{'2->A'},
+        '3' => $analyses_by_name->{'load_dnafrag_region'}->{'-flow_into'}->{'3->A'},
+    };
+}
+
+1;
