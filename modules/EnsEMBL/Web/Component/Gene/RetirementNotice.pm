418d396d7551f6a67fbf4e8743d33f539900612b
Anne Lyle <annelyle@ebi.ac.uk>
2018-06-05 15:58:09 +0100

    Reworded notice to make it clear it only affects human

--- modules/EnsEMBL/Web/Component/Gene/RetirementNotice.pm
+++ modules/EnsEMBL/Web/Component/Gene/RetirementNotice.pm
@@ -36,8 +36,10 @@ sub content {
   my $hub            = $self->hub;
   return unless $hub->species eq 'Homo_sapiens';
 
-  my $message = qq(<p>As of Ensembl release 93 this view will be retired for human. We have elected to retire this page because we feel that the density of known genetic variation is too great for this view to be informative in its current form.</p>
-<p>For more information on how to find variation data for a gene, please see <a href="http://www.ensembl.info/2018/06/05/gene-variant-image-retirement-for-human-e93/">our blog post</a>.</p>
+  my $message = qq(<h4>Human only - retirement of this view</h4>
+<p>As of Ensembl release 93 this view will no longer be available for human, because we feel that the density of known human genetic variation is too great for the display to be informative in its current form.</p>
+<p>Other species will not be affected, as they have less variation data.</p>
+<p>For more information about the decision and on how to find variation data for a gene, please see <a href="http://www.ensembl.info/2018/06/05/gene-variant-image-retirement-for-human-e93/">our blog post</a>.</p>
                     );
   return $self->_warning('Retirement notice', $message);
 }
