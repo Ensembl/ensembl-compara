8c74ad670a0c31cc6aa3b06524d17ca97e0d447c
Anne Lyle <annelyle@ebi.ac.uk>
2018-06-25 16:42:36 +0100

    Update release date

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -35,8 +35,8 @@ use Sys::Hostname::Long;
 ###############################################################################
 ## Ensembl Version and release dates (these get updated every release)
 our $ENSEMBL_VERSION        = 93;            # Ensembl release number
-our $ARCHIVE_VERSION        = 'Jun2018';     # Archive site for this version
-our $ENSEMBL_RELEASE_DATE   = 'June 2018'; # As it would appear in the copyright/footer
+our $ARCHIVE_VERSION        = 'Jul2018';     # Archive site for this version
+our $ENSEMBL_RELEASE_DATE   = 'July 2018'; # As it would appear in the copyright/footer
 ###############################################################################
 
 
