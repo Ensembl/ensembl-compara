8ede11bc65a5258a386da39e1c1819ad728d7f2c
Matthieu Muffato <muffato@ebi.ac.uk>
2018-07-02 22:50:09 +0100

    Fixed the capitalization

--- sql/patch_93_94_c.sql
+++ sql/patch_93_94_c.sql
@@ -24,29 +24,29 @@ ALTER TABLE method_link ADD COLUMN display_name varchar(255) DEFAULT '' NOT NULL
 
 UPDATE method_link SET display_name = "BlastZ" WHERE type = "BLASTZ_NET";
 UPDATE method_link SET display_name = "LastZ" WHERE type = "LASTZ_NET";
-UPDATE method_link SET display_name = "Translated Blat (no nets)" WHERE type = "TRANSLATED_BLAT";
-UPDATE method_link SET display_name = "Translated Blat" WHERE type = "TRANSLATED_BLAT_NET";
+UPDATE method_link SET display_name = "translated Blat (no nets)" WHERE type = "TRANSLATED_BLAT";
+UPDATE method_link SET display_name = "translated Blat" WHERE type = "TRANSLATED_BLAT_NET";
 UPDATE method_link SET display_name = "EPO" WHERE type = "EPO";
 UPDATE method_link SET display_name = "EPO-Low-Coverage" WHERE type = "EPO_LOW_COVERAGE";
 UPDATE method_link SET display_name = "Mercator-Pecan" WHERE type = "PECAN";
 UPDATE method_link SET display_name = "Ortheus alignment" WHERE type = "ORTHEUS";
 UPDATE method_link SET display_name = "Cactus" WHERE type = "CACTUS_HAL";
 UPDATE method_link SET display_name = "Cactus (restricted)" WHERE type = "CACTUS_HAL_PW";
-UPDATE method_link SET display_name = "Synteny" WHERE type = "SYNTENY";
-UPDATE method_link SET display_name = "Families" WHERE type = "FAMILY";
-UPDATE method_link SET display_name = "Protein-trees" WHERE type = "PROTEIN_TREES";
+UPDATE method_link SET display_name = "synteny" WHERE type = "SYNTENY";
+UPDATE method_link SET display_name = "families" WHERE type = "FAMILY";
+UPDATE method_link SET display_name = "protein-trees" WHERE type = "PROTEIN_TREES";
 UPDATE method_link SET display_name = "ncRNA-trees" WHERE type = "NC_TREES";
-UPDATE method_link SET display_name = "Species-tree" WHERE type = "SPECIES_TREE";
-UPDATE method_link SET display_name = "Homologues" WHERE type = "ENSEMBL_HOMOLOGUES";
-UPDATE method_link SET display_name = "Orthologues" WHERE type = "ENSEMBL_ORTHOLOGUES";
-UPDATE method_link SET display_name = "Paralogues" WHERE type = "ENSEMBL_PARALOGUES";
-UPDATE method_link SET display_name = "Patch projections" WHERE type = "ENSEMBL_PROJECTIONS";
-UPDATE method_link SET display_name = "Gerp Constrained Elements" WHERE type = "GERP_CONSTRAINED_ELEMENT";
-UPDATE method_link SET display_name = "Gerp Conservation Scores" WHERE type = "GERP_CONSERVATION_SCORE";
+UPDATE method_link SET display_name = "species-tree" WHERE type = "SPECIES_TREE";
+UPDATE method_link SET display_name = "homologues" WHERE type = "ENSEMBL_HOMOLOGUES";
+UPDATE method_link SET display_name = "orthologues" WHERE type = "ENSEMBL_ORTHOLOGUES";
+UPDATE method_link SET display_name = "paralogues" WHERE type = "ENSEMBL_PARALOGUES";
+UPDATE method_link SET display_name = "patch projections" WHERE type = "ENSEMBL_PROJECTIONS";
+UPDATE method_link SET display_name = "GERP Constrained Elements" WHERE type = "GERP_CONSTRAINED_ELEMENT";
+UPDATE method_link SET display_name = "GERP Conservation Scores" WHERE type = "GERP_CONSERVATION_SCORE";
 UPDATE method_link SET display_name = "LastZ-path" WHERE type = "LASTZ_PATCH";
-UPDATE method_link SET display_name = "Homoeologues" WHERE type = "ENSEMBL_HOMOEOLOGUES";
+UPDATE method_link SET display_name = "homoeologues" WHERE type = "ENSEMBL_HOMOEOLOGUES";
 UPDATE method_link SET display_name = "ATAC Assembly mapping" WHERE type = "ATAC";
-UPDATE method_link SET display_name = "Polyploidy-aware self-alignment" WHERE type = "POLYPLOID";
+UPDATE method_link SET display_name = "polyploidy-aware self-alignment" WHERE type = "POLYPLOID";
 
 # Patch identifier
 INSERT INTO meta (species_id, meta_key, meta_value)
