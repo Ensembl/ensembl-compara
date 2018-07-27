3231b181f49821b3d35a2e687a6d440bbee7486b
Anne Lyle <annelyle@ebi.ac.uk>
2018-07-27 13:38:59 +0100

    Code needed to support BigPsl format - ENSWEB-4634

--- modules/EnsEMBL/Web/ConfigPacker.pm
+++ modules/EnsEMBL/Web/ConfigPacker.pm
@@ -1757,6 +1757,7 @@ sub _munge_file_formats {
     'bam'       => {'ext' => 'bam', 'label' => 'BAM',       'display' => 'graph', 'remote' => 1},
     'bigwig'    => {'ext' => 'bw',  'label' => 'BigWig',    'display' => 'graph', 'remote' => 1},
     'bigbed'    => {'ext' => 'bb',  'label' => 'BigBed',    'display' => 'graph', 'remote' => 1},
+    'bigpsl'    => {'ext' => 'bb',  'label' => 'BigPsl',    'display' => 'graph', 'remote' => 1},
     'cram'      => {'ext' => 'cram','label' => 'CRAM',      'display' => 'graph', 'remote' => 1},
     'trackhub'  => {'ext' => 'txt', 'label' => 'Track Hub', 'display' => 'graph', 'remote' => 1},
     ## Export only
---
+++ modules/EnsEMBL/Web/IOWrapper/BigPsl.pm
@@ -0,0 +1,33 @@
+=head1 LICENSE
+
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+Copyright [2016-2018] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+package EnsEMBL::Web::IOWrapper::BigPsl;
+
+### Wrapper around Bio::EnsEMBL::IO::Parser::BigBed
+
+use strict;
+use warnings;
+no warnings 'uninitialized';
+
+use parent qw(EnsEMBL::Web::IOWrapper::BigBed);
+
+## BigPsl is basically a BigBed file with custom AutoSQL, so we don't need any special
+## functionality in this wrapper
+
+1;
