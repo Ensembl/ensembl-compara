169c2c0b6fa73f138563306bf934ffb9b3d8197c
Anne Lyle <annelyle@ebi.ac.uk>
2019-01-22 14:08:51 +0000

    Format hub data into structure needed by JavaScript

--- modules/EnsEMBL/Web/JSONServer/TrackHubData.pm
+++ modules/EnsEMBL/Web/JSONServer/TrackHubData.pm
@@ -46,18 +46,19 @@ sub json_data {
     last if ($record && keys %$record);
   }
   my $url = $record->{'url'};
-  warn ">>> URL $url";
+  #warn ">>> URL $url";
 
   my $trackhub  = EnsEMBL::Web::Utils::TrackHub->new('hub' => $self->hub, 'url' => $url);
   my $hub_info = $trackhub->get_hub({'parse_tracks' => 1}); ## Do we have data for this species?
   $self->{'th_default_count'} = 0;
 
+  ## Get the track data for this genome
   my $data;
   my $assemblies = $hub->species_defs->get_config($hub->param('species'), 'TRACKHUB_ASSEMBLY_ALIASES');
   $assemblies ||= [];
   $assemblies = [ $assemblies ] unless ref($assemblies) eq 'ARRAY';
   foreach (qw(UCSC_GOLDEN_PATH ASSEMBLY_VERSION)) {
-    my $assembly = $self->hub->species_defs->get_config($hub->param('species'), $_);
+    my $assembly = $hub->species_defs->get_config($hub->param('species'), $_);
     next unless $assembly;
     push @$assemblies,$assembly;
   }
@@ -66,27 +67,49 @@ sub json_data {
     last if $data;
   }
 
-  use Data::Dumper; 
-  $Data::Dumper::Sortkeys = 1;
-  $Data::Dumper::Maxdepth = 2;
-  warn Dumper($data);
+  #use Data::Dumper; 
+  #$Data::Dumper::Sortkeys = 1;
+  #$Data::Dumper::Maxdepth = 2;
+  #warn Dumper($data);
 
-  $final->{data}->{evidence}->{'name'}    = 'evidence';
-  $final->{data}->{evidence}->{'label'}   = 'Evidence';
-  $final->{data}->{evidence}->{'data'}    = {};
-  $final->{data}->{evidence}->{'subtabs'} = 1;
+  my ($dimX, $dimY);
+  my $dimLabels = {};
+  foreach my $track (@{$data||[]}) {
+    if ($track->{'dimensions'} && !@{$final->{'dimensions'}||[]}) {
+      ## We only need this information once
+      $dimX = $track->{'dimensions'}{'x'};
+      $dimY = $track->{'dimensions'}{'y'};
+      ## Create a lookup for all the dimensions' individual values 
+      my %combined_hash = (%{$track->{'subGroup1'}}, %{$track->{'subGroup2'}}); 
+      while (my($k, $v) = each (%combined_hash)) {
+        ## Skip the information about the dimensions themselves
+        next if ($k eq 'name' || $k eq 'label'); 
+        (my $pretty_label = $v) =~ s/_/ /g;
+        $dimLabels->{$k} = $pretty_label;
+      }
 
-  $final->{data}->{epigenome}->{'name'}   = 'epigenome';
-  $final->{data}->{epigenome}->{'label'}  = 'Epigenome';
-  $final->{data}->{epigenome}->{'data'}   = {};
-  $final->{data}->{epigenome}->{'listType'} = 'alphabetRibbon';
+      $final->{'dimensions'} = [$dimX, $dimY];
+      $final->{'data'}{$dimX}{'name'}    = $dimX;
+      $final->{'data'}{$dimX}{'label'}   = $track->{'subGroup1'}{'label'};
+      $final->{'data'}{$dimX}{'data'}    = {};
+      $final->{'data'}{$dimY}{'name'}    = $dimY;
+      $final->{'data'}{$dimY}{'label'}   = $track->{'subGroup2'}{'label'};
+      $final->{'data'}{$dimY}{'data'}    = {};
+    }
+    elsif ($track->{'bigDataUrl'}) {
+      ## Only add tracks that are displayable, i.e. not superTracks/composites/etc 
+      my $keyX    = $track->{'subGroups'}{$dimX};
+      my $keyY    = $track->{'subGroups'}{$dimY};
+      my $labelX  = $dimLabels->{$keyX};
+      my $labelY  = $dimLabels->{$keyY};
+      my $isOn    = $track->{'on_off'} eq 'on' ? 1 : 0;
 
-  $final->{dimensions} = ['epigenome', 'evidence'];
+      push @{$final->{'data'}{$dimX}{'data'}{$keyX}}, {'rel' => $dimY, 'val' => $keyY, 'defaultOn' => $isOn}; 
+      push @{$final->{'data'}{$dimY}{'data'}{$keyY}}, $keyX;
+    }
+  }
+
   return $final;
-}
-
-sub _add_trackhub_node {
-  my ($self, $node) = @_;
 }
 
 1;
