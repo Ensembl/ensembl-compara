cc97d09c0642f0d6e84a14f95a9b7ae71cbc409c
Mateus Patricio <mateus@ebi.ac.uk>
2019-02-01 14:47:45 +0000

    Allowing the script to retire and delete both genome_dbs and mlss_ids.

--- scripts/pipeline/retire_genome.pl
+++ scripts/pipeline/retire_genome.pl
@@ -40,8 +40,10 @@ This script's main purpose is to retire a genome:
   perl retire_genome.pl --help
     [--reg_conf registry_configuration_file]
     --compara compara_db_name_or_alias
-    --genome_db_ids_to_retire 1234
-    --file_of_genome_db_ids_to_retire path/to/file]
+    --mode genome_db|mlss_id
+    --gdb_id_to_change 1234
+    --mlss_id_to_change 1234
+    --file_of_ids_to_change path/to/file]
 
 =head1 OPTIONS
 
@@ -76,26 +78,44 @@ the one set in ENSEMBL_REGISTRY will be used if defined, if not
 The compara database to update. You can use either the original name or any of the
 aliases given in the registry_configuration_file
 
-=item B<genome_db_ids_to_retire genome_db_ids_name>
-
-The genome_db_id of that need to be retired.
-
 =back
 
 =head2 OPTIONS
 
 =over
 
-=item B<[--file_of_genome_db_ids_to_retire path/to/file]>
+=item B<[--file_of_ids_to_change path/to/file]>
 
 File that contains a list of genome_db_ids to be retired.
 In this mode, --genome_db_ids_to_retire is ignored.
+
+=item B<[--mode delete or retire]>
+
+This scripts run in two modes: retire or delete ids
+
+=item B<[--id_type genome_db or mlss_id]>
+
+This scripts deals with two types of ids: genome_db_id or mlss_ids
+
+=item B<gdb_id_to_change genome_db_id>
+
+The genome_db_id of that need to be retired.
+
+=item B<mlss_id_to_change mlss_id>
+
+The genome_db_id of that need to be retired.
 
 =back
 
 =head1 INTERNAL METHODS
 
 =cut
+
+#Delete a mlss_id:
+#perl $ENSEMBL_CVS_ROOT_DIR/ensembl-compara/scripts/pipeline/retire_genome.pl --reg_conf $ENSEMBL_CVS_ROOT_DIR/ensembl-compara/scripts/pipeline/production_reg_${COMPARA_DIV}_conf.pl --compara compara_curr --mlss_id_to_change 1234 --mode delete --id_type mlss_id
+
+#Retire a mlss_id:
+#perl $ENSEMBL_CVS_ROOT_DIR/ensembl-compara/scripts/pipeline/retire_genome.pl --reg_conf $ENSEMBL_CVS_ROOT_DIR/ensembl-compara/scripts/pipeline/production_reg_${COMPARA_DIV}_conf.pl --compara compara_curr --mlss_id_to_change 1234 --mode retire --id_type mlss_id
 
 use Bio::EnsEMBL::Registry;
 use Bio::EnsEMBL::ApiVersion;
@@ -111,7 +131,10 @@ use Getopt::Long;
 my $help;
 my $reg_conf;
 my $compara;
-my $gdb_id_to_retire = "";
+my $mode;
+my $id_type;
+my $gdb_id_to_change = "";
+my $mlss_id_to_change = "";
 my $file;
 my $release;
 my $collection;
@@ -119,13 +142,16 @@ my $collection;
 GetOptions( "help"                                 => \$help,
             "reg_conf=s"                           => \$reg_conf,
             "compara=s"                            => \$compara,
-            "gdb_id_to_retire=s"                   => \$gdb_id_to_retire,
-            'file_of_gdb_ids_to_retire=s'          => \$file,);
+            "mode=s"                               => \$mode,
+            "id_type=s"                            => \$id_type,
+            "gdb_id_to_change=s"                   => \$gdb_id_to_change,
+            "mlss_id_to_change=s"                  => \$mlss_id_to_change,
+            'file_of_ids_to_change=s'              => \$file,);
 
 $| = 0;
 
 # Print Help and exit if help is requested
-if ($help or (!$gdb_id_to_retire and !$file) or !$compara) {
+if ($help or ((!$gdb_id_to_change and !$file) and (!$mlss_id_to_change and !$file)) or !$compara or ($mode !~ m/(\bretire\b|\bdelete\b)/) or ($id_type !~ m/(\bmlss_id\b|\bgenome_db\b)/) ) {
     use Pod::Usage;
     pod2usage({-exitvalue => 0, -verbose => 2});
 }
@@ -141,25 +167,59 @@ if ($compara =~ /mysql:\/\//) {
 throw ("Cannot connect to database [$compara]") if (!$compara_dba);
 my $genome_db_adaptor = $compara_dba->get_GenomeDBAdaptor();
 
+if ($id_type eq "genome_db") {
+    # create the list of gdb_ids_to_retire
+    my @species_list;
+    if ($gdb_id_to_change) {
+        die "--gdb_id_to_change and --file_of_ids_to_change cannot be given at the same time.\n" if $file;
+        push @species_list, $gdb_id_to_change;
+    }
+    else {
+        my $gdb_ids = slurp_to_array( $file, "chomp" );
+        foreach my $gdbid (@$gdb_ids) {
+            #left and right trim for unwanted spaces
+            $gdbid =~ s/^\s+|\s+$//g;
+            push @species_list, $gdbid;
+        }
+    }
 
-# create the list of gdb_ids_to_retire
-my @species_list;
-if ($gdb_id_to_retire) {
-    die "--gdb_id_to_retire and --file_of_gdb_ids_to_retire cannot be given at the same time.\n" if $file;
-    push @species_list, $gdb_id_to_retire;
-} else {
-    my $gdb_ids = slurp_to_array($file, "chomp");
-    foreach my $gdbid (@$gdb_ids) {
-        #left and right trim for unwanted spaces
-        $gdbid =~ s/^\s+|\s+$//g;
-        push @species_list, $gdbid;
+    foreach my $this_gdb_id (@species_list) {
+        my $gdba = $compara_dba->get_GenomeDBAdaptor;
+        my $gdb  = $gdba->fetch_by_dbID($this_gdb_id);
+        if ($mode eq "retire"){
+            $gdba->retire_object($gdb);
+        }
+        elsif ($mode eq "delete"){
+            $gdba->delete_by_dbID($gdb);
+        }
     }
 }
+elsif ($id_type = "mlss_id") {
+    # create the list of mlss_ids_to_retire
+    my @mlss_ids_list;
+    if ($mlss_id_to_change) {
+        die "--mlss_id_to_change and --file_of_ids_to_change cannot be given at the same time.\n" if $file;
+        push @mlss_ids_list, $mlss_id_to_change;
+    }
+    else {
+        my $mlss_ids = slurp_to_array( $file, "chomp" );
+        foreach my $mlssid (@$mlss_ids) {
+            #left and right trim for unwanted spaces
+            $mlssid =~ s/^\s+|\s+$//g;
+            push @mlss_ids_list, $mlssid;
+        }
+    }
 
-foreach my $this_gdb_id (@species_list) {
-    my $gdba = $compara_dba->get_GenomeDBAdaptor;
-    my $gdb  = $gdba->fetch_by_dbID($this_gdb_id);
-    $gdba->retire_object($gdb);
+    foreach my $this_mlss_id (@mlss_ids_list) {
+        my $mlssa = $compara_dba->get_MethodLinkSpeciesSetAdaptor;
+        my $mlss  = $mlssa->fetch_by_dbID($this_mlss_id);
+        if ($mode eq "retire"){
+            $mlssa->retire_object($mlss);
+        }
+        elsif ($mode eq "delete"){
+            $mlssa->delete($this_mlss_id);
+        }
+    }
 }
 
 exit(0);
