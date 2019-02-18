4f2f004a5a303911ddb52a97a5c66f6e82f7108c
Mateus Patricio <mateus@ebi.ac.uk>
2019-02-18 12:14:29 +0000

    Adding some documentation.
    Adding some usefull information for the RELCO.

--- modules/Bio/EnsEMBL/Compara/PipeConfig/EBI/Ensembl/HighConfidenceOrthologs_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/EBI/Ensembl/HighConfidenceOrthologs_conf.pm
@@ -61,6 +61,10 @@ sub default_options {
 
         'division'   => 'ensembl',
         'reg_conf'   => $self->o('ensembl_cvs_root_dir').'/ensembl-compara/scripts/pipeline/production_reg_'.$self->o('division').'_conf.pl',
+
+        # This pipeline will faster if it's run against the production ProteinTree database:
+        # In this case it needs to be run *before* the merge, otherwise the data will not be in the release database.
+        #'compara_db' => 'compara_ptrees',
         'compara_db' => 'compara_curr',
 
         # In this structure, the "thresholds" are for resp. the GOC score, the WGA coverage and %identity
