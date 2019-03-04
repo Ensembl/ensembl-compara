65c4f99ee2a925c998388ddf9756b6c1e1295509
Anne Lyle <annelyle@ebi.ac.uk>
2019-03-04 13:54:05 +0000

    Use up-to-date parser to remove dependency on Vcf.pm - ENSWEB-4858

--- modules/EnsEMBL/Draw/GlyphSet/vcf.pm
+++ modules/EnsEMBL/Draw/GlyphSet/vcf.pm
@@ -28,12 +28,9 @@ no warnings 'uninitialized';
 use List::Util qw(max);
 
 use Role::Tiny::With;
-with 'EnsEMBL::Draw::Role::Wiggle';
 with 'EnsEMBL::Draw::Role::Default';
 
-use Bio::EnsEMBL::IO::Adaptor::VCFAdaptor;
-use Bio::EnsEMBL::Variation::DBSQL::VariationFeatureAdaptor;
-use Bio::EnsEMBL::Variation::Utils::Constants;
+use EnsEMBL::Web::IOWrapper::Indexed;
 
 use parent qw(EnsEMBL::Draw::GlyphSet::UserData);
 
@@ -71,7 +68,6 @@ sub render_simple {
       $self->{'my_config'}->set('show_overlay', 1);
       $self->{'my_config'}->set('default_strand', 1);
       $self->{'my_config'}->set('drawing_style', ['Feature::Variant']);
-      $self->{'data'}[0]{'features'} = $self->consensus_features;
       $self->draw_features;
     }
   }
@@ -105,185 +101,83 @@ sub render_density_bar {
 ############# DATA ACCESS & PROCESSING ########################
 
 sub get_data {
-### Fetch and cache raw features - we'll process them later as needed
-  my $self = shift;
-  $self->{'my_config'}->set('show_subtitle', 1);
-  $self->{'data'} ||= [];
+  my ($self, $url) = @_;
+  return $self->{'data'} if scalar @{$self->{'data'}||[]};
+  my $hub         = $self->{'config'}->hub;
+  $url          ||= $self->my_config('url');
+  my $container   = $self->{'container'};
+  my $data        = [];
 
-  unless (scalar @{$self->{'data'}}) {
-    my $slice       = $self->{'container'};
-    my $start       = $slice->start;
+  my ($skip, $strand_to_omit) = $self->get_strand_filters;
+  return $data if $skip == $self->strand;
 
-    ## Allow for seq region synonyms
-    my $seq_region_names = [$slice->seq_region_name];
-    if ($self->{'config'}->hub->species_defs->USE_SEQREGION_SYNONYMS) {
-      push @$seq_region_names, map {$_->name} @{ $slice->get_all_synonyms };
-    }
+  my $args            = { 'options' => {
+                                  'hub'         => $hub,
+                                  'config_type' => $self->{'config'}{'type'},
+                                  'track'       => $self->{'my_config'}{'id'},
+                                  },
+                        };
 
-    my $vcf_adaptor = $self->vcf_adaptor;
-    my $consensus;
-    foreach my $seq_region_name (@$seq_region_names) {
-      $consensus = eval { $self->vcf_adaptor->fetch_variations($seq_region_name, $slice->start, $slice->end); };
-      warn $@ if $@;
-      return [] if $@;
-      last if $consensus and @$consensus;
-    } 
+  my $iow = $self->get_iow($url, $args);
 
+  if ($iow) {
+    ## We need to pass 'faux' metadata to the ensembl-io wrapper, because
+    ## most files won't have explicit colour settings
     my $colours = $self->species_defs->colour('variation');
     my $colour  = $colours->{'default'}->{'default'}; 
-      
-    $self->{'data'} = [{'metadata' => {
-                                        'name'    => $self->{'my_config'}->get('name'),
-                                        'colour'  => $colour,
-                                       }, 
-                        'features' => $consensus
-                            }];
+    ## Don't try and scale if we're just doing a zmenu!
+    my $pix_per_bp = $self->{'display'} eq 'text' ? '' : $self->scalex;
+    my $metadata = {
+                    'action'          => $self->{'my_config'}->get('zmenu_action'),
+                    'colour'          => $colour,
+                    'display'         => $self->{'display'},
+                    'drawn_strand'    => $self->strand,
+                    'strand_to_omit'  => $strand_to_omit,
+                    'pix_per_bp'      => $pix_per_bp,
+                    'use_synonyms'    => $hub->species_defs->USE_SEQREGION_SYNONYMS,
+                    };
+
+    ## No colour defined in ImageConfig, so fall back to defaults
+    unless ($colour) {
+      my $colourset_key           = $self->{'my_config'}->get('colourset') || 'userdata';
+      my $colourset               = $hub->species_defs->colour($colourset_key);
+      my $colours                 = $colourset->{'url'} || $colourset->{'default'};
+      $metadata->{'colour'}       = $colours->{'default'};
+      $metadata->{'label_colour'} = $colours->{'text'} || $colours->{'default'};
+    }
+
+    my $style = $self->my_config('style') || $self->my_config('display') || '';
+
+    $data = $iow->create_tracks($container, $metadata);
+
+    ## Final fallback, in case we didn't set this in the individual parser
+    $metadata->{'label_colour'} ||= $colour;
+
+    ## Can we actually render this many features?
+    my $total;
+    foreach (@$data) {
+      $total += scalar @{$_->{'features'}||[]};
+    }
+
+    my $limit = $self->{'my_config'}->get('bigbed_limit') || 500;
+    if ($total > $limit) {
+      $self->{'data'} = [];
+      $self->{'no_empty_track_message'}  = 1;
+      return $self->errorTrack('This track has too many features to show at this scale. Please zoom in.');
+    }
+  } else {
+    $self->{'data'} = [];
+    return $self->errorTrack(sprintf 'Could not read file %s', $self->my_config('caption'));
   }
-  return $self->{'data'};
+  #use Data::Dumper; 
+  #$Data::Dumper::Sortkeys = 1;
+  #warn Dumper($data->[0]{'features'});
+  return $data;
 }
 
-sub consensus_features {
-### Turn raw features into consensus features for drawing
-### @return Arrayref of hashes
-  my $self = shift;
-  my $raw_features  = $self->{'data'}[0]{'features'};
-  my $config        = $self->{'config'};
-  my $slice         = $self->{'container'};
-  my $start         = $slice->start;
-  my $species       = $self->{'config'}->hub->species;
-  my $features      = [];
-
-  my $vfa = $config->hub->database('variation')->get_VariationFeatureAdaptor;
-  my %overlap_cons = %Bio::EnsEMBL::Variation::Utils::Constants::OVERLAP_CONSEQUENCES;
-  my $colours = $self->species_defs->colour('variation');
-   
-  foreach my $f (@$raw_features) {
-    my $type         = undef;
-    my $unknown_type = 1;
-    my $vs           = $f->{'POS'} - $start + 1;
-    my $ve           = $vs;
-    my $sv           = $f->{'INFO'}{'SVTYPE'};
-    my $info_string;
-    $info_string .= ";  $_: $a->{'INFO'}{$_}" for sort keys %{$a->{'INFO'} || {}};
-
-    ## N.B. Compensate for VCF indel start including the bp before the variant
-    if ($sv) {
-      $unknown_type = 0;
-
-      if ($sv eq 'DEL') {
-        $type = 'deletion';
-        my $svlen = $f->{'INFO'}{'SVLEN'} || 0;
-        $vs++;
-        $ve       = $vs + abs $svlen;
-
-        $f->{'REF'} = substr($f->{'REF'}, 0, 30) . ' ...' if length $f->{'REF'} > 30;
-      } 
-      elsif ($sv eq 'TDUP') {
-        my $svlen = $f->{'INFO'}{'SVLEN'} || 0;
-        $ve       = $vs + $svlen + 1;
-      } 
-      elsif ($sv eq 'INS') {
-        $type = 'insertion';
-        $vs++;
-        $ve = $vs -1;
-      }
-    } 
-    else {
-      my ($reflen, $altlen) = (length $f->{'REF'}, length $f->{'ALT'}[0]);
-
-      if ($altlen > $reflen) {
-        $vs++;
-        $type = 'insertion';
-      }
-      elsif ($altlen < $reflen) {
-        $vs++;
-        $type = 'deletion';
-      }
-
-      if ($reflen > 1) {
-        $ve = $vs + $reflen - 1;
-      } 
-      elsif ($altlen > 1) {
-        $ve = $vs - 1;
-      }
-      $sv = 'OTHER';
-    }
-
-    my $allele_string = join '/', $f->{'REF'}, @{$f->{'ALT'} || []};
-    my $vf_name       = $f->{'ID'} eq '.' ? "$f->{'CHROM'}_$f->{'POS'}_$allele_string" : $f->{'ID'};
-
-    ## Flip for drawing
-    if ($slice->strand == -1) {
-      my $flip = $slice->length + 1;
-      ($vs, $ve) = ($flip - $ve, $flip - $vs);
-    }
-
-    ## Zmenu
-    my %lookup = (
-                  'INS'   => 'Insertion',
-                  'DEL'   => 'Deletion',
-                  'TDUP'  => 'Duplication',
-                  );
-    my $location = sprintf('%s:%s', $slice->seq_region_name, $vs + $slice->start - 1);
-    $location   .= '-'.($ve + $slice->start - 2) if ($ve && $ve != $vs);
-
-    my $title = "$vf_name; Location: $location; Allele: $allele_string";
-    $title .= 'Type: '.$lookup{$sv}.'; ' if $lookup{$sv};
-    if (keys %{$f->{'INFO'}||{}}) {
-      $title .= '; INFO: --------------------------';
-      foreach (sort keys %{$f->{'INFO'}}) {
-        $title .= sprintf('; %s: %s', $_, $f->{'INFO'}{$_} || '');
-      }
-    }
-
-    my $colour  = $colours->{'default'}->{'default'};
-
-    ## Get consequence type
-    my ($consequence, $ambig_code);
-    if (defined($f->{'INFO'}->{'VE'})) {
-      $consequence = (split /\|/, $f->{'INFO'}->{'VE'})[0];
-    }
-    elsif ($vfa) {
-      ## Not defined in file, so look up in database
-      my $snp = {
-        start            => $vs, 
-        end              => $ve, 
-        strand           => 1, 
-        slice            => $slice,
-        allele_string    => $allele_string,
-        variation_name   => $vf_name,
-        map_weight       => 1, 
-        adaptor          => $vfa, 
-        seqname          => $info_string ? "; INFO: --------------------------$info_string" : '',
-        consequence_type => $unknown_type ? ['INTERGENIC'] : ['COMPLEX_INDEL']
-      };
-      bless $snp, 'Bio::EnsEMBL::Variation::VariationFeature';
-
-      $snp->get_all_TranscriptVariations;
-
-      $consequence = $snp->display_consequence;
-      $ambig_code  = $snp->ambig_code;
-    }
-      
-    ## Set colour by consequence
-    if ($consequence && defined($overlap_cons{$consequence})) {
-      $colour = $colours->{lc $consequence}->{'default'};
-    }
-
-    my $fhash = {
-                  start         => $vs,
-                  end           => $ve,
-                  strand        => 1,
-                  colour        => $colour,       
-                  label         => $vf_name, 
-                  text_overlay  => $ambig_code,
-                  title         => $title,
-                  type          => $type,
-                };
-
-    push @$features, $fhash;
-  }
-  return $features;
+sub get_iow {
+  my ($self, $url, $args) = @_;
+  return EnsEMBL::Web::IOWrapper::Indexed::open($url, 'VCF4Tabix', $args);
 }
 
 sub density_features {
