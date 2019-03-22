875d348bc988c1d8faa6c171f1a460d553e2e1c4
Jyothish <joyjoy.nt@gmail.com>
2019-03-22 14:12:01 +0000

    no image minification for showhide icon

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -34,7 +34,7 @@ use Sys::Hostname::Long;
 
 ###############################################################################
 ## Ensembl Version and release dates (these get updated every release)
-our $ENSEMBL_VERSION        = 95;            # Ensembl release number
+our $ENSEMBL_VERSION        = 96;            # Ensembl release number
 our $ARCHIVE_VERSION        = 'Jan2019';     # Archive site for this version
 our $ENSEMBL_RELEASE_DATE   = 'January 2019'; # As it would appear in the copyright/footer
 ###############################################################################
