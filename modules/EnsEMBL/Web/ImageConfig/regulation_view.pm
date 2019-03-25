3f9e0a5e973fa1e050d76ce267098c409f25fe85
Jyothish <joyjoy.nt@gmail.com>
2019-03-25 15:45:28 +0000

    More tracks turned off by default on regulation views

--- modules/EnsEMBL/Web/ImageConfig/regulation_view.pm
+++ modules/EnsEMBL/Web/ImageConfig/regulation_view.pm
@@ -89,16 +89,17 @@ sub init_cacheable {
 
   $self->get_node('opt_empty_tracks')->set('display', 'normal');	
 
-  my $cell_line = clean_id($self->hub->species_defs->get_config($self->species, 'REGULATION_DEFAULT_CELL')); # Eugh, modifies arg.
-  foreach my $type (qw(reg_feats seg reg_feats_non_core reg_feats_core)) {
-    my $node = $self->get_node("${type}_$cell_line");
-    next unless $node;
-    $node->set('display',$type =~ /_core/ ? 'compact' : 'normal');
-  }
-  foreach my $cell_line (@cell_lines) {
-    $cell_line = clean_id($cell_line); # Eugh, modifies arg.
-    $self->{'reg_feats_tracks'}{$_} = 1 for "reg_feats_$cell_line", "reg_feats_core_$cell_line", "reg_feats_non_core_$cell_line", "seg_$cell_line";
-  }
+  # my $cell_line = clean_id($self->hub->species_defs->get_config($self->species, 'REGULATION_DEFAULT_CELL')); # Eugh, modifies arg.
+  # foreach my $type (qw(reg_feats seg reg_feats_non_core reg_feats_core)) {
+  #   my $node = $self->get_node("${type}_$cell_line");
+  #   next unless $node;
+  #   $node->set('display',$type =~ /_core/ ? 'compact' : 'normal');
+  # }
+
+  # foreach my $cell_line (@cell_lines) {
+  #   $cell_line = clean_id($cell_line); # Eugh, modifies arg.
+  #   $self->{'reg_feats_tracks'}{$_} = 1 for "reg_feats_$cell_line", "reg_feats_core_$cell_line", "reg_feats_non_core_$cell_line", "seg_$cell_line";
+  # }
 
   if ($self->cache_code ne $self->type) {
     my $func = "init_".$self->cache_code;
