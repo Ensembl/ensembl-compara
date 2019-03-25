2f76b25a79a0ecb8acc9e07b491f1adc22af6eb4
Matthieu Muffato <muffato@ebi.ac.uk>
2019-03-25 16:53:04 +0000

    Schema version 97

--- sql/table.sql
+++ sql/table.sql
@@ -2248,12 +2248,12 @@ CREATE TABLE `CAFE_species_gene` (
 
 -- Add schema version to database
 DELETE FROM meta WHERE meta_key='schema_version';
-INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'schema_version', '96');
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'schema_version', '97');
 -- Add schema type to database
 DELETE FROM meta WHERE meta_key='schema_type';
 INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'schema_type', 'compara');
 
 # Patch identifier
 INSERT INTO meta (species_id, meta_key, meta_value)
-  VALUES (NULL, 'patch', 'patch_95_96_a.sql|schema_version');
+  VALUES (NULL, 'patch', 'patch_96_97_a.sql|schema_version');
 
