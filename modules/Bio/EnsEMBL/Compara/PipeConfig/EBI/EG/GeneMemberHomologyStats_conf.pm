4fad32dcd54c6d16bb120b8988672f0c3587e1d3
Matthieu Muffato <muffato@ebi.ac.uk>
2019-03-27 12:34:43 +0000

    Restored the EG PipeConfig for compatibility

---
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/EBI/EG/GeneMemberHomologyStats_conf.pm
@@ -0,0 +1,60 @@
+=head1 LICENSE
+
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+Copyright [2016-2019] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=head1 NAME
+
+Bio::EnsEMBL::Compara::PipeConfig::EBI::EG::GeneMemberHomologyStats_conf
+
+=head1 SYNOPSIS
+
+    init_pipeline.pl 
+      Bio::EnsEMBL::Compara::PipeConfig::EGGeneMemberHomologyStats_conf 
+      -curr_rel_db mysql://ensrw:XXX\@server/merged_compara_db 
+      -collection collection_name
+
+=head1 DESCRIPTION
+
+    A simple pipeline to populate the gene_member_hom_stats table
+    for a single collection.
+
+=cut
+
+package Bio::EnsEMBL::Compara::PipeConfig::EBI::EG::GeneMemberHomologyStats_conf;
+
+use strict;
+use warnings;
+
+use base ('Bio::EnsEMBL::Compara::PipeConfig::GeneMemberHomologyStats_conf');
+
+sub default_options {
+    my ($self) = @_;
+    return {
+        %{ $self->SUPER::default_options() },
+
+        'compara_db'    => $self->o('curr_rel_db'),     # For backwards compatibility
+    }
+}
+
+sub resource_classes {
+  my ($self) = @_;
+  return {
+    %{$self->SUPER::resource_classes},
+    'default' => { 'LSF' => '-q production-rh7' },
+  };
+}
+
+1;
