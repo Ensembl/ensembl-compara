5cd27034158f1ae03103b86cc30e5fa80d0f56f6
Carla Cummins <carlac@ebi.ac.uk>
2019-04-02 10:13:26 +0100

    Updated vertebrates registry config

--- scripts/pipeline/production_reg_vertebrates_conf.pl
+++ scripts/pipeline/production_reg_vertebrates_conf.pl
@@ -1,13 +1,13 @@
 #!/usr/bin/env perl
 # Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
 # Copyright [2016-2019] EMBL-European Bioinformatics Institute
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #      http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -31,7 +31,7 @@ use Bio::EnsEMBL::DBSQL::DBAdaptor;
 use Bio::EnsEMBL::Compara::DBSQL::DBAdaptor;
 use Bio::EnsEMBL::Taxonomy::DBSQL::TaxonomyDBAdaptor;
 
-my $curr_release = 96;
+my $curr_release = 97;
 my $prev_release = $curr_release - 1;
 
 # ---------------------- CURRENT CORE DATABASES---------------------------------
@@ -56,19 +56,20 @@ Bio::EnsEMBL::Registry->load_registry_from_url(
 # ---------------------- PREVIOUS CORE DATABASES---------------------------------
 
 # previous release core databases will ONLY be required by:
+#   * PrepareMasterDatabaseForRelease_conf
 #   * LoadMembers_conf
 #   * MercatorPecan_conf
 # !!! COMMENT THIS SECTION OUT FOR ALL OTHER PIPELINES (for speed) !!!
 
-# my $suffix_separator = '__cut_here__';
-# Bio::EnsEMBL::Registry->load_registry_from_db(
-#     -host           => 'mysql-ens-mirror-1',
-#     -port           => 4240,
-#     -user           => 'ensro',
-#     -pass           => '',
-#     -db_version     => $prev_release,
-#     -species_suffix => $suffix_separator.$prev_release,
-# );
+my $suffix_separator = '__cut_here__';
+Bio::EnsEMBL::Registry->load_registry_from_db(
+    -host           => 'mysql-ens-mirror-1',
+    -port           => 4240,
+    -user           => 'ensro',
+    -pass           => '',
+    -db_version     => $prev_release,
+    -species_suffix => $suffix_separator.$prev_release,
+);
 
 #------------------------COMPARA DATABASE LOCATIONS----------------------------------
 
@@ -76,35 +77,26 @@ Bio::EnsEMBL::Registry->load_registry_from_url(
 my $compara_dbs = {
     # general compara dbs
     'compara_master' => [ 'mysql-ens-compara-prod-1', 'ensembl_compara_master' ],
-    'compara_curr'   => [ 'mysql-ens-compara-prod-1', 'ensembl_compara_96' ],
-    'compara_prev'   => [ 'mysql-ens-compara-prod-1', 'ensembl_compara_95' ],
+    'compara_curr'   => [ 'mysql-ens-compara-prod-1', "ensembl_compara_$curr_release" ],
+    'compara_prev'   => [ 'mysql-ens-compara-prod-1', "ensembl_compara_$prev_release" ],
 
     # homology dbs
-    'compara_members'  => [ 'mysql-ens-compara-prod-6', 'mateus_ensembl_load_members_96'  ],
-    'compara_ptrees'   => [ 'mysql-ens-compara-prod-1', 'mateus_ensembl_protein_trees_96' ],
-    'ptrees_prev'      => [ 'mysql-ens-compara-prod-4', 'mateus_protein_trees_95' ],
-    'compara_families' => [ 'mysql-ens-compara-prod-2', 'mateus_ensembl_families_96'  ],
-    'compara_nctrees'  => [ 'mysql-ens-compara-prod-3', 'muffato_ensembl_compara_nctrees_96' ],
-    'murinae_ptrees'   => [ 'mysql-ens-compara-prod-5', 'muffato_murinae_protein_trees_96b' ],
-    'murinae_nctrees'  => [ 'mysql-ens-compara-prod-5', 'muffato_murinae_nctrees_96' ],
+    # 'compara_members'  => [ 'mysql-ens-compara-prod-6', '' ],
+    # 'compara_ptrees'   => [ 'mysql-ens-compara-prod-1', '' ],
+    'ptrees_prev'      => [ 'mysql-ens-compara-prod-1', 'mateus_ensembl_protein_trees_96' ],
+    # 'compara_families' => [ 'mysql-ens-compara-prod-2', '' ],
+    # 'compara_nctrees'  => [ 'mysql-ens-compara-prod-3', '' ],
+    # 'murinae_ptrees'   => [ 'mysql-ens-compara-prod-5', '' ],
+    # 'murinae_nctrees'  => [ 'mysql-ens-compara-prod-5', '' ],
 
     # LASTZ dbs
-    'lastz_batch_1'    => [ 'mysql-ens-compara-prod-1', 'mateus_lastz_ensembl_batch_1_96' ],
-    'lastz_batch_2'  => [ 'mysql-ens-compara-prod-1', 'mateus_lastz_ensembl_batch_2_96' ],
-    'lastz_batch_3'  => [ 'mysql-ens-compara-prod-2', 'mateus_lastz_ensembl_batch_3_96' ],
-    'lastz_batch_4'  => [ 'mysql-ens-compara-prod-2', 'mateus_lastz_ensembl_batch_4_96' ],
-    'lastz_batch5'   => [ 'mysql-ens-compara-prod-3', 'muffato_lastz_ensembl_batch_5_96' ],
-    'lastz_batch6'   => [ 'mysql-ens-compara-prod-3', 'muffato_lastz_ensembl_batch_6_96' ],
-    'lastz_batch7'   => [ 'mysql-ens-compara-prod-4', 'carlac_vertebrates_batch7_lastz_96' ],
-    'lastz_batch8'   => [ 'mysql-ens-compara-prod-6', 'carlac_lastz_ensembl_batch_8_96' ],
-    'lastz_batch_9'  => [ 'mysql-ens-compara-prod-5', 'waakanni_lastz_ensembl_batch_9_96' ],
-    'lastz_batch_10' => [ 'mysql-ens-compara-prod-5', 'waakanni_lastz_ensembl_batch_10_96' ],
+    # 'lastz_batch_1'    => [ 'mysql-ens-compara-prod-1', '' ],
 
     # EPO dbs
     ## mammals
-    'mammals_epo'         => [ 'mysql-ens-compara-prod-2', 'mateus_mammals_epo_96' ],
-    'mammals_epo_prev'    => [ 'mysql-ens-compara-prod-1', 'muffato_mammals_epo_95' ],
-    'mammals_epo_low'     => [ 'mysql-ens-compara-prod-3', 'muffato_mammals_epo_low_coverage_96' ],
+    # 'mammals_epo'         => [ 'mysql-ens-compara-prod-2', '' ],
+    'mammals_epo_prev'    => [ 'mysql-ens-compara-prod-2', 'mateus_mammals_epo_96' ],
+    # 'mammals_epo_low'     => [ 'mysql-ens-compara-prod-3', '' ],
     'mammals_epo_anchors' => [ 'mysql-ens-compara-prod-2', 'waakanni_generate_anchors_mammals_93' ],
 
     ## sauropsids
@@ -114,9 +106,9 @@ my $compara_dbs = {
     'sauropsids_epo_anchors' => [ 'mysql-ens-compara-prod-1', 'mm14_4saur_gen_anchors_hacked_86' ],
 
     ## fish
-    'fish_epo'         => [ 'mysql-ens-compara-prod-3', 'muffato_fish_epo_96' ],
-    'fish_epo_prev'    => [ 'mysql-ens-compara-prod-1', 'muffato_fish_epo_95' ],
-    'fish_epo_low'     => [ 'mysql-ens-compara-prod-3', 'muffato_fish_epo_low_coverage_96' ],
+    # 'fish_epo'         => [ 'mysql-ens-compara-prod-3', '' ],
+    'fish_epo_prev'    => [ 'mysql-ens-compara-prod-3', 'muffato_fish_epo_96' ],
+    # 'fish_epo_low'     => [ 'mysql-ens-compara-prod-3', '' ],
     'fish_epo_anchors' => [ 'mysql-ens-compara-prod-5', 'muffato_generate_anchors_fish_96' ],
 
     ## primates
@@ -126,13 +118,13 @@ my $compara_dbs = {
     'primates_epo_anchors' => [ 'mysql-ens-compara-prod-2', 'waakanni_generate_anchors_mammals_93' ],
 
     # other alignments
-    'amniotes_pecan'      => [ 'mysql-ens-compara-prod-5', 'muffato_amniotes_mercator_pecan_96b' ],
-    'amniotes_pecan_prev' => [ 'mysql-ens-compara-prod-6', 'carlac_amniotes_mercator_pecan_95' ],
+    # 'amniotes_pecan'      => [ 'mysql-ens-compara-prod-5', '' ],
+    'amniotes_pecan_prev' => [ 'mysql-ens-compara-prod-5', 'muffato_amniotes_mercator_pecan_96b' ],
 
-    'compara_syntenies'   => [ 'mysql-ens-compara-prod-8', 'mateus_synteny_96' ],
+    # 'compara_syntenies'   => [ 'mysql-ens-compara-prod-8', '' ],
 
     # miscellaneous
-    'alt_allele_projection' => [ 'mysql-ens-compara-prod-1', 'mateus_ensembl_alt_allele_import_96' ],
+    # 'alt_allele_projection' => [ 'mysql-ens-compara-prod-1', '' ],
 };
 
 add_compara_dbs( $compara_dbs ); # NOTE: by default, '%_prev' dbs will have a read-only connection
@@ -156,7 +148,7 @@ Bio::EnsEMBL::DBSQL::DBAdaptor->new(
     -pass => $ENV{'ENSADMIN_PSW'},
     -port => 4485,
     -group => 'core',
-    -species => 'ancestral_sequences',     # FIXME: this needs to be renamed to ancestral_sequences when we run the dumps
+    -species => 'ancestral_sequences_for_dumps',     # FIXME: this needs to be renamed to ancestral_sequences when we run the dumps
     -dbname => "ensembl_ancestral_$curr_release",
 );
 
