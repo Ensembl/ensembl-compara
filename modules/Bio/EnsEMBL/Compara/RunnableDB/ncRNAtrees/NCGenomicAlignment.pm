f99d1a27ff5668df9a24209911d9a6836ac6974d
Matthieu Muffato <muffato@ebi.ac.uk>
2019-04-26 19:03:43 +0100

    No need to open the file if we are not going to write anything

--- modules/Bio/EnsEMBL/Compara/RunnableDB/ncRNAtrees/NCGenomicAlignment.pm
+++ modules/Bio/EnsEMBL/Compara/RunnableDB/ncRNAtrees/NCGenomicAlignment.pm
@@ -119,11 +119,11 @@ sub dump_sequences_to_workdir {
     $self->param('tag_gene_count', scalar (@{$member_list}) );
     Bio::EnsEMBL::Compara::Utils::Preloader::load_all_DnaFrags($cluster->adaptor->db->get_DnaFragAdaptor, $member_list);
 
-    open (OUTSEQ, ">$fastafile") or $self->throw("Error opening $fastafile for writing: $!");
     if (scalar @{$member_list} < 2) {
         $self->param('single_peptide_tree', 1);
         return 1;
     }
+    open (OUTSEQ, ">$fastafile") or $self->throw("Error opening $fastafile for writing: $!");
 
     my $residues = 0;
     my $count = 0;
