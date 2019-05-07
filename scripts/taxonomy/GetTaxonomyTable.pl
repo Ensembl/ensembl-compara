3d0cceecc30ed6003ea09e2f92e57e24265b0a23
jalvarezjarreta <jalvarez@ebi.ac.uk>
2019-05-07 14:27:43 +0100

    Applied Matthieu Muffato's requested changes

--- scripts/taxonomy/GetTaxonomyTable.pl
+++ scripts/taxonomy/GetTaxonomyTable.pl
@@ -17,99 +17,95 @@
 use strict;
 use warnings;
 use Getopt::Long;
-use Cwd qw(abs_path);
-use File::Spec::Functions qw(catfile);
 use Bio::EnsEMBL::Compara::DBSQL::DBAdaptor;
 
-$| = 1;
-
 # Parameters:
-my $help = 0;
 # URL to the Compara database
 my $compara_url;
-# Output directory where the taxonomy table will be saved
-my $out_dir = Cwd::abs_path();
+# Group ranks in taxonomy table?
+# E.g.:
+#   CLASS     SUPERORDER        ORDER     SUBORDER     FAMILY   SUBFAMILY       SPECIES             GENOMEDB NAME
+#   Mammalia  Euarchontoglires  Primates  Haplorrhini  Cebidae  Callitrichinae  Callithrix jacchus  callithrix_jacchus
+#   Mammalia  Euarchontoglires  Primates  Haplorrhini  Cebidae  Cebinae         Cebus capucinus     cebus_capucinus
+# will be printed like
+#   CLASS     SUPERORDER        ORDER     SUBORDER     FAMILY   SUBFAMILY       SPECIES             GENOMEDB NAME
+#   Mammalia  Euarchontoglires  Primates  Haplorrhini  Cebidae  Callitrichinae  Callithrix jacchus  callithrix_jacchus
+#   ''        ''                ''        ''           ''       Cebinae         Cebus capucinus     cebus_capucinus
+my $group_ranks = 0;
 # Handle input arguments
-GetOptions("help"          => \$help,
-           "compara_url=s" => \$compara_url,
-           "outdir=s"      => \$out_dir
+GetOptions("compara_url=s" => \$compara_url,
+           "group" => \$group_ranks
           ) or die("Error in command line arguments\n");
-die "Error in command line arguments -compara_url " .
-    "mysql://user:pass\@host:port/db_name [-outdir /your/directory/]"
+die "Error in command line arguments: -compara_url mysql://user:pass\@host:port/db_name"
     if (!$compara_url);
 
 # Create new Compara database adaptor and get the GenomeDB adaptor
-my $compara_dba = Bio::EnsEMBL::Compara::DBSQL::DBAdaptor->new(
-    -url => $compara_url, -species => 'Multi');
+my $compara_dba = Bio::EnsEMBL::Compara::DBSQL::DBAdaptor->new(-url => $compara_url);
 my $genome_db_adaptor = $compara_dba->get_GenomeDBAdaptor();
 # Get all the Genome DBs
-my $list_ref_of_gdbs = $genome_db_adaptor->fetch_all();
+my $list_of_gdbs = $genome_db_adaptor->fetch_all();
 # Declare the taxonomic ranks of interest (sorted)
 my @target_ranks = qw(class superorder order suborder family subfamily species);
+# Number of ranks (plus the default addition of "GenomeDB name" as last rank)
+my $num_ranks = $#target_ranks + 1;
 # Create the taxonomy template: if a rank is missing, assign "N/A"
-my %rank_template = map {$_ => "N/A"} @target_ranks;
-# Default output file name
-my $out_file = "taxonomy_table.tsv";
-# Recursive function to flatten the taxonomy hash into an array with one string
-# per species' taxonomy (ranks separated by "\t"), and each rank sorted in
-# alphabetical order
-sub flatten {
-    my ($in_hash, $out_array, $already_flatted) = @_;
-    # The first call will have only two arguments
-    $already_flatted = "" unless(defined $already_flatted);
-    for my $key (sort keys %$in_hash) {
-        my $value = $in_hash->{$key};
-        if (ref $value eq 'HASH') {
-            flatten($value, $out_array, $already_flatted . $key . "\t");
-        } else {
-            push(@{$out_array}, $already_flatted . $key);
-        }
-    }
-}
-# Store all the taxonomic information in a mutidimensional hash, following the
-# same order as in @target_ranks
-my $taxonomy_db = {};
-foreach my $ref (@{$list_ref_of_gdbs}) {
-    my $node;
-    # There are some $ref that may not have 'taxon_id', so we need to handle the
-    # exception, report the troubling $ref and move to the next one
-    eval {
-        $node = $ref->taxon();
-    } or do {
-        warn "Undefined taxon_id for '", $ref->name(), "'.\n";
+my %rank_template = map {$_ => 'N/A'} @target_ranks;
+# @taxonomy_table will have one string per species' taxonomy in tab-separated
+# values (TSV) format
+my @taxonomy_table;
+foreach my $gdb (@{$list_of_gdbs}) {
+    # There are some $gdb that may not have 'taxon_id', so we need to handle the
+    # exception, report the troubling $gdb and move to the next one
+    if (!$gdb->taxon_id() || !defined($gdb->taxon())) {
+        warn "Undefined taxon_id for '", $gdb->name(), "'.\n";
         next;
-    };
+    }
+    my $node = $gdb->taxon();
     # Copy taxonomy template and fill it in
     my %taxonomy = %rank_template;
     while ($node->name() ne 'root') {
         my $rank = $node->rank();
-        if (defined $rank && exists $taxonomy{$rank}) {
+        if (exists($taxonomy{$rank})) {
             $taxonomy{$rank} = $node->name();
         }
         $node = $node->parent();
     }
-    # Replace species name by GenomeDB name
-    if (grep(/^species$/, @target_ranks)) {
-        $taxonomy{'species'} = $ref->name();
-    }
-    # Push the template into the taxonomy hash
-    my $level = $taxonomy_db;
+    # Get the taxonomy in a TSV-like string and append it to the taxonomy table
+    my $taxonomy_str = "";
     foreach my $rank (@target_ranks) {
-        my $classification = $taxonomy{$rank};
-        if (! exists($level->{$classification})) {
-            # Each rank has a hash as value except "species" (this has a 1)
-            $level->{$classification} = ($rank eq $target_ranks[-1]) ? 1 : {};
+        $taxonomy_str .= $taxonomy{$rank} . "\t";
+    }
+    # Add GenomeDB name as the last level/rank of the taxonomy
+    $taxonomy_str .= $gdb->name();
+    push(@taxonomy_table, $taxonomy_str);
+}
+# Sort the rows in alphabetical order
+@taxonomy_table = sort(@taxonomy_table);
+if ($group_ranks) {
+    # Group ranks in taxonomy table, replacing duplicates by "''"
+    # Get the first row as reference to see how many ranks are the same
+    my @prev_taxonomy = split(/\t/, $taxonomy_table[0]);
+    foreach my $i (1 .. $#taxonomy_table) {
+        my @taxonomy = split(/\t/, $taxonomy_table[$i]);
+        # Grouped version of the taxonomy (string)
+        my $grouped_taxonomy = "";
+        # Loop over each rank to compare it with the value in the previous row
+        foreach my $j (0 .. $num_ranks) {
+            if ($prev_taxonomy[$j] eq $taxonomy[$j]) {
+                $grouped_taxonomy .= "''\t";
+            } else {
+                # As soon as one rank is different, the remaining will be too
+                $grouped_taxonomy .= join("\t", @taxonomy[$j .. $num_ranks]);
+                last;
+            }
         }
-        $level = $level->{$classification};
+        # Replace the string by its grouped version
+        $taxonomy_table[$i] = $grouped_taxonomy;
+        # Get the current taxonomy as reference for the next step
+        @prev_taxonomy = @taxonomy;
     }
 }
-# Flatten the taxonomy hash into an array of one element per species
-my @taxonomy_table;
-flatten($taxonomy_db, \@taxonomy_table);
-# Write the array into the output file with @target_ranks as headers
-my $out_abs_path = catfile($out_dir, $out_file);
-open(my $file, ">$out_abs_path") or
-    die "Couldn't create file '$out_abs_path', $!";
-print $file join("\t", map(ucfirst, @target_ranks)), "\n";
-print $file join("\n", @taxonomy_table), "\n";
-close($file);
+# Write the array into STDOUT with @target_ranks as headers
+print join("\t", map(ucfirst, @target_ranks), 'GenomeDB name'), "\n",
+      join("\n", @taxonomy_table), "\n";
+
