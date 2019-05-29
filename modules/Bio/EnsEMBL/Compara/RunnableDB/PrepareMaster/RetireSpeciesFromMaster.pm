61e4d7b868822ec160ec4925190ede58f52314e8
Jorge Alvarez Jarreta <jalvarez@ebi.ac.uk>
2019-05-29 16:32:57 +0100

    fixed bug at line 44 (missing $)

--- modules/Bio/EnsEMBL/Compara/RunnableDB/PrepareMaster/RetireSpeciesFromMaster.pm
+++ modules/Bio/EnsEMBL/Compara/RunnableDB/PrepareMaster/RetireSpeciesFromMaster.pm
@@ -41,7 +41,7 @@ use base ('Bio::EnsEMBL::Compara::RunnableDB::BaseRunnable');
 sub fetch_input {
   my $self = shift;
   my $master_dba = $self->get_cached_compara_dba('master_db');
-  my $genome_db_adaptor = master_dba->get_GenomeDBAdaptor;
+  my $genome_db_adaptor = $master_dba->get_GenomeDBAdaptor;
   my $genome_db  = $genome_db_adaptor->fetch_by_name_assembly($self->param_required('species_name'));
   $genome_db_adaptor->retire_object($genome_db);
 }
