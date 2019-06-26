2e76691beb719040519387427b2a6218bee4000b
Jorge Alvarez Jarreta <jalvarez@ebi.ac.uk>
2019-06-26 10:36:10 +0100

    docs and params updated; get master information from init reg conf file

--- modules/Bio/EnsEMBL/Compara/RunnableDB/BuildMaster/ReconfigPipeline.pm
+++ modules/Bio/EnsEMBL/Compara/RunnableDB/BuildMaster/ReconfigPipeline.pm
@@ -27,15 +27,18 @@ Bio::EnsEMBL::Compara::RunnableDB::BuildMaster::ReconfigPipeline
 
 Modifies the registry configuration file to add the information of each species'
 cloned core database, and updates the information in 'database.default.properties'
-file (used by Java healthchecks) to point to the right host.
+file (used by Java healthchecks) to point to the right hosts.
 
 Requires several inputs:
-    'cloned_dbs'      : hash accumulator of "species" => "database_name"
+    'cloned_dbs'      : hash accumulator of 'species' => 'database_name'
+    'init_reg_conf'   : full path to the initial registry configuration file
     'reg_conf'        : full path to the registry configuration file
     'reg_conf_tmpl'   : full path to the registry configuration template file
-    'java_hc_db_prop' : full path to 'database.default.properties' file (used by Java healthchecks)
+    'master_db'       : new master database
+    'java_hc_db_prop' : full path to 'database.default.properties' file (used by
+                        Java healthchecks)
     'backups_dir'     : full path to the pipeline's backup directory
-    'dst_host'        : host name where the cloned core and master databases have been created
+    'dst_host'        : host name where the cloned core databases have been created
     'dst_port'        : host port
 
 
@@ -45,6 +48,7 @@ package Bio::EnsEMBL::Compara::RunnableDB::BuildMaster::ReconfigPipeline;
 
 use warnings;
 use strict;
+use Bio::EnsEMBL::Registry;
 use File::Slurp;
 
 use base ('Bio::EnsEMBL::Compara::RunnableDB::BaseRunnable');
@@ -60,38 +64,49 @@ sub fetch_input {
     my $self = shift;
     my $dst_host = $self->param_required('dst_host');
     my $cloned_dbs = $self->param_required('cloned_dbs');
-    # Create the cloned core databases hash with the format:
+    # Create the cloned core databases hash content with the format:
     #     species/alias name => [ host, db_name ]
-    my $dbs_hash = "\n";
+    my $core_dbs_hash = "\n";
     foreach my $species (keys %{ $cloned_dbs }) {
         my $dbname = $cloned_dbs->{$species};
-        $dbs_hash .= "    '$species' => [ '$dst_host', '$dbname' ],\n";
+        $core_dbs_hash .= "    '$species' => [ '$dst_host', '$dbname' ],\n";
     }
-    $self->param('dbs_hash', $dbs_hash);
+    $self->param('core_dbs_hash', $core_dbs_hash);
+    # Create the [host, db_name] array content for 'compara_master', getting the
+    # information from the initial registry configuration file
+    my $compara = $self->param_required('master_db');
+    my $init_reg_conf = $self->param_required('init_reg_conf');
+    Bio::EnsEMBL::Registry->load_all($init_reg_conf, 0, 0, 0, "throw_if_missing");
+    my $compara_db = Bio::EnsEMBL::Registry->get_DBAdaptor($compara, "compara");
+    my $master_db_info = " '" . $compara_db->dbc->host . "', '" . $compara_db->dbc->dbname . "' ";
+    $self->param('master_db_info', $master_db_info);
 }
 
 sub run {
     my $self = shift;
     my $reg_conf = $self->param_required('reg_conf');
     my $reg_conf_tmpl = $self->param_required('reg_conf_tmpl');
-    my $dbs_hash = $self->param_required('dbs_hash');
-    # Find the spot (labelled "<dbs_hash>") where the cloned core databases must
-    # be stored and add the required information
+    my $core_dbs_hash = $self->param_required('core_dbs_hash');
+    my $master_db_info = $self->param_required('master_db_info');
+    my $java_hc_db_prop = $self->param_required('java_hc_db_prop');
+    my $dst_host = $self->param_required('dst_host');
+    my $dst_port = $self->param_required('dst_port');
+    # Find the tag '<core_dbs_hash>' in the registry configuration file template
+    # and replace it by the cloned core databases hash content
     my $content = read_file($reg_conf_tmpl);
-    $content =~ s/<dbs_hash>/$dbs_hash/;
-    # Modify the registry conf file
+    $content =~ s/<core_dbs_hash>/$core_dbs_hash/;
+    # Find the tag '<master_db_info>' in the registry configuration file
+    # template and replace it by the new master database array content
+    $content =~ s/<master_db_info>/$master_db_info/;
+    # Uncomment the line where add_compara_dbs() is executed
+    $content =~ s/\#add_compara_dbs\(/add\_compara\_dbs\(/;
+    # Modify the registry configuration file
     open(my $file, '>', $reg_conf) or die "Could not open file '$reg_conf' $!";
     print $file $content;
     close $file;
-    # Make a backup copy of the Java healthchecks database properties file
-    my $java_hc_db_prop = $self->param_required('java_hc_db_prop');
-    my $backups_dir = $self->param_required('backups_dir');
-    $self->run_command("cp $java_hc_db_prop $backups_dir", {die_on_failure => 1});
-    # All cloned core and new master databases are in the same host, so replace
-    # that information in the Java healthchecks database properties file
-    # ('host', 'host1' and 'host2', and 'port', 'port1' and 'port2')
-    my $dst_host = $self->param_required('dst_host');
-    my $dst_port = $self->param_required('dst_port');
+    # All cloned core databases are in the same host, so replace that
+    # information in the Java healthchecks database properties file ('host',
+    # 'host1' and 'host2', and 'port', 'port1' and 'port2')
     $content = read_file($java_hc_db_prop);
     $content =~ s/(^)(host[12]?[ ]*=)[ ]*[\w\.-]+/$1$2 $dst_host/gm;
     $content =~ s/(^)(port[12]?[ ]*=)[ ]*\d+/$1$2 $dst_port/gm;
