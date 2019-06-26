1a10c4ef4272ac3c7c22f87853ba7f2fa83cd452
Jorge Alvarez Jarreta <jalvarez@ebi.ac.uk>
2019-06-26 17:19:50 +0100

    Pipeline updated and documentation improved
    The pipeline has been changed to create a new master database from
    complete core databases (defined in the registry configuration file),
    but includes the option to do the cloning of core regions if the
    division includes the word "test". The documentation has been improved
    and the parameters updated according to these changes.

--- modules/Bio/EnsEMBL/Compara/PipeConfig/BuildNewMasterDatabase_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/BuildNewMasterDatabase_conf.pm
@@ -32,15 +32,22 @@ Bio::EnsEMBL::Compara::PipeConfig::BuildNewMasterDatabase_conf
 
 =head1 DESCRIPTION
 
-    Create a new master database from scratch
+    Create a new master database from scratch via a predefined registry
+    configuration file with the desired core database(s) from where the species/
+    genome information will be copied.
 
+    WARNING: the previous reports and backups will be removed if the pipeline is
+    initialised again for the same division and release.
 
 =head1 SYNOPSIS
 
-    init_pipeline.pl Bio::EnsEMBL::Compara::PipeConfig::BuildNewMasterDatabase_conf -input <path_to_regions_file(s)> -division <division> -dst_host <host> -dst_port <port>
+    Usage for non-test divisions:
+    init_pipeline.pl Bio::EnsEMBL::Compara::PipeConfig::BuildNewMasterDatabase_conf -division <division>
 
-    #1. Clone data regions from JSON file(s) (one per species)
-    #2. Create a new master_db
+    #1. Create a new master database
+    #2. Populate it through PrepareMasterDatabaseForRelease pipeline
+
+    For test divisions see Bio::EnsEMBL::Compara::PipeConfig::EBI::Test::BuildTestMasterDatabase_conf
 
 =head1 AUTHORSHIP
 
@@ -70,34 +77,41 @@ sub default_options {
         # Inherit the generic default options
         %{$self->SUPER::default_options},
 
-        'pipeline_name' => 'build_master_for_' . $self->o('division'),
+        'pipeline_name' => 'build_new_master_for_' . $self->o('division'),
         'work_dir'      => $self->o('pipeline_dir'),
         'backups_dir'   => $self->o('work_dir') . '/backups/',
-        'dst_host'      => $self->o('dst_host'),
-        'dst_port'      => $self->o('dst_port'),
 
         'master_db'        => 'compara_master',
         'schema_file'      => $self->check_file_in_ensembl('ensembl-compara/sql/table.sql'),
         'method_link_dump' => $self->check_file_in_ensembl('ensembl-compara/sql/method_link.txt'),
         'clone_core_db'    => $self->check_exe_in_ensembl('ensembl-test/scripts/clone_core_database.pl'),
 
+        'java_hc_dir'     => $self->check_dir_in_ensembl('ensj-healthcheck/'),
+        'java_hc_db_prop' => $self->check_file_in_ensembl('ensj-healthcheck/database.defaults.properties'),
+
+        'init_reg_conf' => $self->o('reg_conf'), # needed to create the new master database
+        # Parameters required for 'test' division only
+        'config_dir'    => undef,
+        'reg_conf_tmpl' => undef,
+        'dst_host'      => undef,
+        'dst_port'      => undef,
+
         # PrepareMasterDatabaseForRelease pipeline configuration:
         'taxonomy_db'             => 'ncbi_taxonomy',
         'incl_components'         => 1, # let's default this to 1 - will have no real effect if there are no component genomes (e.g. in vertebrates)
         'create_all_mlss_exe'     => $self->check_exe_in_ensembl('ensembl-compara/scripts/pipeline/create_all_mlss.pl'),
-        'xml_file'                => $self->o('input') . '/compara_' . $self->o('division') . '.xml',
+        'xml_file'                => $self->o('config_dir') . '/compara_' . $self->o('division') . '.xml',
         'report_file'             => $self->o('work_dir') . '/mlss_ids_' . $self->o('division') . '.list',
         'master_backup_file'      => $self->o('backups_dir') . '/new_master_' . $self->o('division') . '.sql',
         'patch_dir'               => $self->check_dir_in_ensembl('ensembl-compara/sql/'),
         'alias_file'              => $self->check_file_in_ensembl('ensembl-compara/scripts/taxonomy/ensembl_aliases.sql'),
-        'java_hc_dir'             => $self->check_dir_in_ensembl('ensj-healthcheck/'),
         'list_genomes_script'     => undef, # required but not used: do_update_from_metadata = 0
         'report_genomes_script'   => undef, # required but not used: do_update_from_metadata = 0
         'update_metadata_script'  => $self->check_exe_in_ensembl('ensembl-compara/scripts/pipeline/update_master_db.pl'),
-        'assembly_patch_species'  => undef,
-        'additional_species'      => undef,
+        'assembly_patch_species'  => [], # by default, skip this step
+        'additional_species'      => {}, # by default, skip this step
         'do_update_from_metadata' => 0,
-        'do_load_lrg_dnafrags'    => 0,
+        'do_load_lrg_dnafrags'    => 1,
         'do_load_timetree'        => 1,
     };
 }
@@ -109,11 +123,6 @@ sub pipeline_create_commands {
         @{$self->SUPER::pipeline_create_commands},
 
         $self->pipeline_create_commands_rm_mkdir(['work_dir', 'backups_dir']),
-        # Make a backup of the current registry configuration file
-        'cp ' . $self->o('reg_conf') . ' ' . $self->o('backups_dir') . '/production_reg_' . $self->o('division') . '_conf.pl',
-        # Replace the registry configuration file by the one provided for the
-        # first part of the pipeline
-        'cp ' . $self->o('pre_reg_conf') . ' ' . $self->o('reg_conf'),
     ];
 }
 
@@ -126,7 +135,6 @@ sub pipeline_wide_parameters {
 
         'master_db'     => $self->o('master_db'),
         'division'      => $self->o('division'),
-        'reg_conf_tmpl' => $self->o('reg_conf_tmpl'),
         'release'       => $self->o('ensembl_release'),
         'hc_version'    => 1,
     };
@@ -135,17 +143,47 @@ sub pipeline_wide_parameters {
 sub pipeline_analyses {
     my ($self) = @_;
     return [
+        {   -logic_name => 'create_new_master',
+            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+            -input_ids  => [{}],
+            -parameters => {
+                'init_reg_conf' => $self->o('init_reg_conf'),
+                'cmd'          => 'db_cmd.pl -reg_conf #init_reg_conf# -reg_type compara -reg_alias #master_db# -sql "CREATE DATABASE"',
+            },
+            -flow_into  => ['load_schema_master'],
+        },
+
+        {   -logic_name => 'load_schema_master',
+            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+            -parameters => {
+                'init_reg_conf' => $self->o('init_reg_conf'),
+                'schema_file'   => $self->o('schema_file'),
+                'cmd'           => 'db_cmd.pl -reg_conf #init_reg_conf# -reg_type compara -reg_alias #master_db# < #schema_file#',
+            },
+            -flow_into  => ['init_method_link_table'],
+        },
+
+        {   -logic_name => 'init_method_link_table',
+            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+            -parameters => {
+                'init_reg_conf'    => $self->o('init_reg_conf'),
+                'method_link_dump' => $self->o('method_link_dump'),
+                'cmd'              => 'db_cmd.pl -reg_conf #init_reg_conf# -reg_type compara -reg_alias #master_db# -executable mysqlimport #method_link_dump#',
+            },
+            -flow_into  => WHEN(
+                '#division# =~ m/test/' => 'seed_species_to_clone',
+                ELSE 'patch_master_db'
+            ),
+        },
+
         {   -logic_name => 'seed_species_to_clone',
             -module     => 'Bio::EnsEMBL::Hive::RunnableDB::JobFactory',
-            -input_ids  => [{
-                'input' => $self->o('input'),
-            }],
             -parameters => {
-                'inputcmd' => 'find #input# -type f -name "*.json"',
+                'config_dir'    => $self->o('config_dir'),
+                'inputcmd' => 'find #config_dir# -type f -name "*.json"',
             },
             -flow_into  => {
-                '2->A' => {'clone_core_regions' => {'json_file' => '#_0#'},
-                           'create_new_master'  => {}},
+                '2->A' => {'clone_core_regions' => {'json_file' => '#_0#'}},
                 'A->1' => ['reconfigure_pipeline'],
             },
         },
@@ -154,7 +192,7 @@ sub pipeline_analyses {
             -module            => 'Bio::EnsEMBL::Compara::RunnableDB::BuildMaster::CloneCoreRegions',
             -parameters        => {
                 'clone_core_db' => $self->o('clone_core_db'),
-                'reg_conf'      => $self->o('reg_conf'),
+                'init_reg_conf' => $self->o('init_reg_conf'),
                 'dst_host'      => $self->o('dst_host'),
                 'dst_port'      => $self->o('dst_port'),
                 # Get species name from JSON file path
@@ -166,39 +204,14 @@ sub pipeline_analyses {
             -rc_name           => '500Mb_job',
         },
 
-        {   -logic_name => 'create_new_master',
-            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
-            -parameters => {
-                'cmd' => 'db_cmd.pl $COMPARA_REG #master_db# -sql "CREATE DATABASE"',
-            },
-            -flow_into  => ['load_schema_master'],
-        },
-
-        {   -logic_name => 'load_schema_master',
-            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
-            -parameters => {
-                'schema_file' => $self->o('schema_file'),
-                'cmd'         => 'db_cmd.pl $COMPARA_REG #master_db# < #schema_file#',
-            },
-            -flow_into  => ['init_method_link_table'],
-        },
-
-        {   -logic_name => 'init_method_link_table',
-            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
-            -parameters => {
-                'method_link_dump' => $self->o('method_link_dump'),
-                'cmd'              => 'db_cmd.pl $COMPARA_REG #master_db# -executable mysqlimport #method_link_dump#',
-            },
-        },
-
         {   -logic_name => 'reconfigure_pipeline',
             -module     => 'Bio::EnsEMBL::Compara::RunnableDB::BuildMaster::ReconfigPipeline',
             -parameters => {
+                'init_reg_conf'   => $self->o('init_reg_conf'),
                 'reg_conf'        => $self->o('reg_conf'),
                 'reg_conf_tmpl'   => $self->o('reg_conf_tmpl'),
-                'java_hc_dir'     => $self->o('java_hc_dir'),
-                'java_hc_db_prop' => '#java_hc_dir#/database.defaults.properties',
-                'backups_dir'     => $self->o('backups_dir'),
+                'master_db'       => $self->o('master_db'),
+                'java_hc_db_prop' => $self->o('java_hc_db_prop'),
                 'dst_host'        => $self->o('dst_host'),
                 'dst_port'        => $self->o('dst_port'),
             },
