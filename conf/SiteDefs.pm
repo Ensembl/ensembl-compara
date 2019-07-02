4b5152a795acb1a11910c918d66f05edb14e8cea
Kamal Dodiya <kamal@ebi.ac.uk>
2019-07-02 16:09:20 +0100

    Version++

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -34,9 +34,9 @@ use Sys::Hostname::Long;
 
 ###############################################################################
 ## Ensembl Version and release dates (these get updated every release)
-our $ENSEMBL_VERSION        = 97;            # Ensembl release number
-our $ARCHIVE_VERSION        = 'Jul2019';     # Archive site for this version
-our $ENSEMBL_RELEASE_DATE   = 'July 2019'; # As it would appear in the copyright/footer
+our $ENSEMBL_VERSION        = 98;            # Ensembl release number
+our $ARCHIVE_VERSION        = 'Sep2019';     # Archive site for this version
+our $ENSEMBL_RELEASE_DATE   = 'September 2019'; # As it would appear in the copyright/footer
 ###############################################################################
 
 
