18b8aa48f575a1d51698a3f29ca5794b41c72ca0
Jorge Alvarez Jarreta <jalvarez@ebi.ac.uk>
2019-07-10 17:11:54 +0100

    changed default value of do_load_lrg_dnafrags flag to 0

--- modules/Bio/EnsEMBL/Compara/PipeConfig/PrepareMasterDatabaseForRelease_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/PrepareMasterDatabaseForRelease_conf.pm
@@ -98,7 +98,7 @@ sub default_options {
         'additional_species'     => undef,
 
         'do_update_from_metadata' => 1,
-        'do_load_lrg_dnafrags'    => 1,
+        'do_load_lrg_dnafrags'    => 0,
         'do_load_timetree'        => 0,
     };
 }
