d94aeaf35efc11c07d4ad230842dc22748c0ac78
nicklangridge <nickl@ebi.ac.uk>
2019-08-12 10:27:01 +0100

    Improvemens for farm use

--- ctrl_scripts/precache-farm.pl
+++ ctrl_scripts/precache-farm.pl
@@ -14,8 +14,10 @@ use Parallel::Forker;
 use List::Util qw(shuffle);
 
 my ($list,@subparts);
+my $workers = 10;
+my $verbose = 0;
 
-GetOptions('l' => \$list,'s=s' => \@subparts);
+GetOptions('l' => \$list,'s=s' => \@subparts,'workers=i' => \$workers, 'verbose|v' => \$verbose);
 if($list) {
   print qx($Bin/precache.pl --mode=list);
   exit 0;
@@ -46,19 +48,29 @@ die unless $jobs;
   
 my $forker = Parallel::Forker->new(
   use_sig_chld => 1,
-  max_proc => 100
+  max_proc => $workers,
 );
 $SIG{CHLD} = sub { Parallel::Forker::sig_child($forker); };
 $SIG{TERM} = sub { $forker->kill_tree_all('TERM') if $forker && $forker->in_parent; die "Quitting...\n"; };
 
 my $njobs=@$jobs;
 my $ndone=0;
+
+my @lib_dirs;
+my @plugins = reverse @{$SiteDefs::ENSEMBL_PLUGINS};
+while (my ($dir, $name) = splice @plugins, 0, 2) {
+ push @lib_dirs, "$dir/modules";
+}
+push @lib_dirs, @$SiteDefs::ENSEMBL_API_LIBS;
+my $libs =  join(' ', map {"-I $_"} @lib_dirs);
+
 sub schedule {
   my ($i) = @_;
 
   $forker->schedule(
     run_on_start => sub {
-      qx(bsub -I export PERL5LIB='/nfs/public/release/ensweb-software/sharedsw/2019_02_07/linuxbrew/opt/bioperl-169/libexec'; $Bin/precache.pl --mode=index --index=$i);
+      $verbose && warn qq{bsub -I "perl $libs $Bin/precache.pl --mode=index --index=$i"};
+      qx(bsub -I "perl $libs $Bin/precache.pl --mode=index --index=$i");
       exit $?;
     },
     run_on_finish => sub {
