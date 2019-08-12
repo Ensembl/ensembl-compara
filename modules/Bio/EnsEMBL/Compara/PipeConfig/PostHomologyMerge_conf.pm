f1ae984a861d04974896fdb0f6c79614a03b499f
Matthieu Muffato <muffato@ebi.ac.uk>
2019-08-12 11:00:36 +0100

    This pipeline doesn't need the Compara tables since it operates on another database

--- modules/Bio/EnsEMBL/Compara/PipeConfig/PostHomologyMerge_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/PostHomologyMerge_conf.pm
@@ -63,6 +63,7 @@ sub default_options {
     };
 }
 
+sub no_compara_schema {}    # Tell the base class not to create the Compara tables in the database
 
 sub hive_meta_table {
     my ($self) = @_;
