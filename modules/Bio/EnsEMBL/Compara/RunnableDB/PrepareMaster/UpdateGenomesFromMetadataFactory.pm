d76ff4a6551f40d9167b9bf0c6b9ab7814289df1
Matthieu Muffato <muffato@ebi.ac.uk>
2019-08-22 18:14:53 +0100

    Use get_command_output
    Co-Authored-By: Carla Cummins <carlac@ebi.ac.uk>

--- modules/Bio/EnsEMBL/Compara/RunnableDB/PrepareMaster/UpdateGenomesFromMetadataFactory.pm
+++ modules/Bio/EnsEMBL/Compara/RunnableDB/PrepareMaster/UpdateGenomesFromMetadataFactory.pm
@@ -74,7 +74,7 @@ sub fetch_input {
 	# check there are no seq_region changes in the existing species
 	my $list_cmd = "perl $list_genomes_script $metadata_script_options";
 	my $list_out = $self->get_command_output($list_cmd);
-	my @release_genomes = split( /\s+/, $list_out );
+	my @release_genomes = $self->get_command_output($list_cmd);
 	chomp @release_genomes;
     die "No genomes reported for release" unless @release_genomes;
 
