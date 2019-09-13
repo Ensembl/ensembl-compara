91a526875601c27b288cf912190792fe2fe13219
Jorge Alvarez Jarreta <jalvarez@ebi.ac.uk>
2019-09-13 17:38:40 +0100

    These paths are mandatory in the context of these pipelines.
    Use the check* methods to guarantee they exist.

--- modules/Bio/EnsEMBL/Compara/PipeConfig/EBI/CITest/BuildCITestMasterDatabase_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/EBI/CITest/BuildCITestMasterDatabase_conf.pm
@@ -85,9 +85,9 @@ sub default_options {
         'compara_dir'   => $self->check_dir_in_ensembl('ensembl-compara/'),
         # NOTE: 'config_dir' is already in ENV but the check* method is not called on it
         #       Since it is essential for this pipeline, redefining it here with the checks
-        'config_dir'    => $self->check_dir_in_ensembl('ensembl-compara/conf/citest'),
-        'init_reg_conf' => $self->check_file_in_ensembl('ensembl-compara/conf/citest/production_init_reg_conf.pl'),
-        'reg_conf_tmpl' => $self->check_file_in_ensembl('ensembl-compara/conf/citest/production_reg_conf_tmpl.pl'),
+        'config_dir'    => $self->check_dir_in_ensembl('ensembl-compara/conf/' . $self->o('division')),
+        'init_reg_conf' => $self->check_file_in_ensembl('ensembl-compara/conf/' . $self->o('division') . '/production_init_reg_conf.pl'),
+        'reg_conf_tmpl' => $self->check_file_in_ensembl('ensembl-compara/conf/' . $self->o('division') . '/production_reg_conf_tmpl.pl'),
 
         # Change working directory
         'pipeline_name' => 'build_master',
--- modules/Bio/EnsEMBL/Compara/PipeConfig/EBI/Plants/ProteinTrees_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/EBI/Plants/ProteinTrees_conf.pm
@@ -82,7 +82,7 @@ sub default_options {
 
     # species tree reconciliation
         # you can define your own species_tree for 'treebest'. It can contain multifurcations
-        'species_tree_input_file'   => $self->check_file_in_ensembl('ensembl-compara/conf/plants/species_tree.topology.nw'),
+        'species_tree_input_file'   => $self->check_file_in_ensembl('ensembl-compara/conf/' . $self->o('division') . '/species_tree.topology.nw'),
         'binary_species_tree_input_file'    => undef,
 
     # homology_dnds parameters:
