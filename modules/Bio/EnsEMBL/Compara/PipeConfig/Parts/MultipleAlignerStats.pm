756bf00561ad370a88ea96af0976975eb7f49994
Matthieu Muffato <muffato@ebi.ac.uk>
2019-09-14 03:04:48 +0100

    Connected back the stats analyses to the pipeline
    They are fast enough to compute in production

--- modules/Bio/EnsEMBL/Compara/PipeConfig/Parts/MultipleAlignerStats.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/Parts/MultipleAlignerStats.pm
@@ -63,7 +63,7 @@ sub pipeline_analyses_multiple_aligner_stats {
             -flow_into  => {
                 '2->A' => [ 'multiplealigner_stats' ],
                 'A->1' => [ 'block_size_distribution' ],
-                    #1  => ['gab_stats_semaphore_holder'],
+                    1  => ['gab_stats_semaphore_holder'],
             },
         },
 
