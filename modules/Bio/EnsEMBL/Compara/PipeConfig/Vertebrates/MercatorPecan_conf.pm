fbfc01151221842cf0c7e258536dba72c6c73b0a
Jorge Alvarez Jarreta <jalvarez@ebi.ac.uk>
2019-10-30 14:38:44 +0000

    removed EBI layer at MercatorPecan pipeline
    - The EBI-specific parameters have been transferred either to the
      PipeConfig or the corresponding division layer
    - Documentation updated

--- modules/Bio/EnsEMBL/Compara/PipeConfig/Vertebrates/MercatorPecan_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/Vertebrates/MercatorPecan_conf.pm
@@ -22,7 +22,7 @@ limitations under the License.
 
 =head1 NAME
 
-Bio::EnsEMBL::Compara::PipeConfig::EBI::MercatorPecan_conf
+Bio::EnsEMBL::Compara::PipeConfig::Vertebrates::MercatorPecan_conf
 
 =head1 SYNOPSIS
 
@@ -31,16 +31,16 @@ Bio::EnsEMBL::Compara::PipeConfig::EBI::MercatorPecan_conf
     #3. make sure that all default_options are set correctly
 
     #4. Run init_pipeline.pl script:
-        init_pipeline.pl Bio::EnsEMBL::Compara::PipeConfig::EBI::MercatorPecan_conf -password <your_password> -mlss_id <your_current_Pecan_mlss_id>
+        init_pipeline.pl Bio::EnsEMBL::Compara::PipeConfig::Vertebrates::MercatorPecan_conf -host mysql-ens-compara-prod-X -port XXXX \
+            -mlss_id <curr_pecan_mlss_id>
 
     #5. Sync and loop the beekeeper.pl as shown in init_pipeline.pl's output
 
 
 =head1 DESCRIPTION  
 
-    The PipeConfig file for MercatorPecan pipeline that should automate most of the pre-execution tasks.
-
-    FYI: it took (3.7 x 24h) to perform the full production run for EnsEMBL release 62.
+    The Vertebrates PipeConfig file for MercatorPecan pipeline that should
+    automate most of the pre-execution tasks.
 
 =head1 CONTACT
 
@@ -52,7 +52,7 @@ Questions may also be sent to the Ensembl help desk at
 
 =cut
 
-package Bio::EnsEMBL::Compara::PipeConfig::EBI::MercatorPecan_conf;
+package Bio::EnsEMBL::Compara::PipeConfig::Vertebrates::MercatorPecan_conf;
 
 use strict;
 use warnings;
@@ -67,17 +67,11 @@ sub default_options {
     return {
         %{$self->SUPER::default_options},   # inherit the generic ones
 
-
     # parameters that are likely to change from execution to another:
-	#pecan mlss_id
-#       'mlss_id'               => 522,   # it is very important to check that this value is current (commented out to make it obligatory to specify)
-        'work_dir'              => $self->o('pipeline_dir'),
         'species_set_name'      => 'amniotes',
         'division'              => 'vertebrates',
         'do_not_reuse_list'     => [ ],
 
-    # master database
-    'master_db' => 'compara_master',
     # previous release data location for reuse
     'reuse_db'  => 'amniotes_pecan_prev',   # Cannot be the release db because we need exon members and the peptide_align_feature tables
     };
