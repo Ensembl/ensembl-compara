da26cd1036ec2df3f0730192816199c9e194ac7c
Jorge Alvarez Jarreta <jalvarez@ebi.ac.uk>
2019-10-30 18:10:14 +0000

    updated layers for OrthologQM_Alignment pipeline
    - The EBI-specific parameters have been transferred either to the
      PipeConfig or the corresponding division layer
    - The Strains-specific pipeline has been deleted as the configuration
      was the same as the Vertebrates division
    - Documentation updated

--- modules/Bio/EnsEMBL/Compara/PipeConfig/Vertebrates/OrthologQM_Alignment_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/Vertebrates/OrthologQM_Alignment_conf.pm
@@ -22,61 +22,23 @@ limitations under the License.
 
 =head1 NAME
 
-	Bio::EnsEMBL::Compara::PipeConfig::EBI::Vertebrates::OrthologQM_Alignment_conf;
+Bio::EnsEMBL::Compara::PipeConfig::Vertebrates::OrthologQM_Alignment_conf
 
 =head1 SYNOPSIS
 
-    init_pipeline.pl Bio::EnsEMBL::Compara::PipeConfig::EBI::Vertebrates::OrthologQM_Alignment_conf
+    For Vertebrates:
+        init_pipeline.pl Bio::EnsEMBL::Compara::PipeConfig::Vertebrates::OrthologQM_Alignment_conf -host mysql-ens-compara-prod-X -port XXXX \
+            -member_type <protein_or_ncrna>
 
-    To run on a species_set_name:
-        init_pipeline.pl Bio::EnsEMBL::Compara::PipeConfig::EBI::Vertebrates::OrthologQM_Alignment_conf -species_set_name <species_set_name>
-        or
-        init_pipeline.pl Bio::EnsEMBL::Compara::PipeConfig::EBI::Vertebrates::OrthologQM_Alignment_conf -species_set_id <species_set dbID>
-
-    To run on a pair of species:
-        init_pipeline.pl Bio::EnsEMBL::Compara::PipeConfig::EBI::Vertebrates::OrthologQM_Alignment_conf -species1 homo_sapiens -species2 gallus_gallus
-
-    In release mode:
-        # On an ncRNA-trees or protein-trees database, after all the alignments have been merged to the final database
-        init_pipeline.pl Bio::EnsEMBL::Compara::PipeConfig::EBI::Vertebrates::OrthologQM_Alignment_conf -compara_db $(mysql-ens-compara-prod-1-ensadmin details url muffato_compara_nctrees_94) -alt_aln_dbs $(mysql-ens-compara-prod-1 details url ensembl_compara_94) $(mysql-ens-compara-prod-1-ensadmin details hive)
+    For a specific strain/breed:
+        init_pipeline.pl Bio::EnsEMBL::Compara::PipeConfig::Vertebrates::OrthologQM_Alignment_conf -host mysql-ens-compara-prod-X -port XXXX \
+            -member_type <protein_or_ncrna> -collection <murinae_or_sus>
 
 =head1 DESCRIPTION
 
-    This pipeline uses whole genome alignments to calculate the coverage of homologous pairs.
-    The coverage is calculated on both exonic and intronic regions seperately and summarised using a quality_score calculation
-    The average quality_score between both members of the homology will be written to the homology table (in compara_db option)
-
-    http://www.ebi.ac.uk/seqdb/confluence/display/EnsCom/Quality+metrics+for+the+orthologs
-
-    Additional options:
-    -compara_db         database containing relevant data (this is where final scores will be written)
-    -alt_aln_dbs        take alignment objects from a different source
-    -alt_homology_db    take homology objects from a different source
-    -previous_rel_db    reuse scores from a previous release (requires a homology_id_mapping table in compara_db)
-
-    Note: If you wish to use homologies from one database, but the alignments live in a different database,
-    remember that final scores will be written to the homology table of the appointed compara_db. So, if you'd
-    like the final scores written to the homology database, assign this as compara_db and use the alt_aln_dbs option
-    to specify the location of the alignments. Likewise, if you want the scores written to the alignment-containing
-    database, assign it as compara_db and use the alt_homology_db option.
-
-    Examples:
-    ---------
-    # scores go to homology db, alignments come from afar
-    init_pipeline.pl Bio::EnsEMBL::Compara::PipeConfig::EBI::Vertebrates::OrthologQM_Alignment_conf -compara_db mysql://user:pass@host/homologies
-        -alt_aln_dbs mysql://ro_user@hosty_mchostface/alignments
-
-    # scores go to alignment db
-    init_pipeline.pl Bio::EnsEMBL::Compara::PipeConfig::EBI::Vertebrates::OrthologQM_Alignment_conf -compara_db mysql://user:pass@host/alignments
-        -alt_homology_db mysql://ro_user@hostess_with_the_mostest/homologies
-
-
-    # standard production run:
-    init_pipeline.pl Bio::EnsEMBL::Compara::PipeConfig::EBI::Vertebrates::OrthologQM_Alignment_conf -compara_db <current protein tree db> -alt_aln_dbs
-        <previous release database (unless new alignments were run)> -previous_rel_db <previous release database>
-        -species_set_name "collection-default"
-
-    (note: compara_db is supplied here in the pipeconfig, so may not be needed at init step)
+    This pipeline uses whole genome alignments to calculate the coverage of homologous pairs
+    for Vertebrates. See parent class Bio::EnsEMBL::Compara::PipeConfig::OrthologQM_Alignment_conf
+    for additional information.
 
 =head1 CONTACT
 
@@ -88,7 +50,7 @@ limitations under the License.
 
 =cut
 
-package Bio::EnsEMBL::Compara::PipeConfig::EBI::Vertebrates::OrthologQM_Alignment_conf;
+package Bio::EnsEMBL::Compara::PipeConfig::Vertebrates::OrthologQM_Alignment_conf;
 
 use strict;
 use warnings;
@@ -111,17 +73,7 @@ sub default_options {
         'collection'    => 'default',
         # 'member_type'   => undef, # should be 'protein' or 'ncrna'
 
-        'master_db'  => 'compara_master',
-
-        # location of homology data. note: wga_score will be written here
-        'compara_db' => '#expr( (#member_type# eq "protein") ? "compara_ptrees" : "compara_nctrees" )expr#',
-        # if alignments are not all present in compara_db, define alternative db locations
-        'alt_aln_dbs' => [
-            # list of databases with EPO or LASTZ data
-            'compara_curr',
-        ],
-        'previous_rel_db'  => 'compara_prev',
-        'species_set_name' => 'collection-#collection#',
+        'species_set_name' => 'collection-' . $self->o('collection'),
     };
 }
 
