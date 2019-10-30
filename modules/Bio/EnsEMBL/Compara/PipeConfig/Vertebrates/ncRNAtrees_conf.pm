a50736f6a99c8df161879e97c412b09e932c6789
Jorge Alvarez Jarreta <jalvarez@ebi.ac.uk>
2019-10-30 17:14:34 +0000

    removed EBI layer at ncRNAtrees pipeline
    - The EBI-specific parameters have been transferred either to the
      PipeConfig or the Vertebrates layer
    - Documentation updated

--- modules/Bio/EnsEMBL/Compara/PipeConfig/Vertebrates/ncRNAtrees_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/Vertebrates/ncRNAtrees_conf.pm
@@ -29,27 +29,26 @@ limitations under the License.
 
 =head1 NAME
 
-Bio::EnsEMBL::Compara::PipeConfig::EBI::Vertebrates::ncRNAtrees_conf
+Bio::EnsEMBL::Compara::PipeConfig::Vertebrates::ncRNAtrees_conf
 
 =head1 SYNOPSIS
 
-    init_pipeline.pl Bio::EnsEMBL::Compara::PipeConfig::EBI::Vertebrates::ncRNAtrees_conf -mlss_id <your_MLSS_id> -member_db <url_of_new_member_database> -prev_rel_db <last_production_database_of_this_mlss> -epo_db <most_recent_epo_low_coverage_database>
-
--epo_db should ideally contain EPO-2X alignments of all the genomes used in the ncRNA-trees. However, due to release coordination considerations, this may not be possible. In this case, you can use the one from the previous release
+    init_pipeline.pl Bio::EnsEMBL::Compara::PipeConfig::Vertebrates::ncRNAtrees_conf -host mysql-ens-compara-prod-X -port XXXX \
+        -mlss_id <curr_ncrna_mlss_id>
 
 =head1 EXAMPLES
 
+e99
+    init_pipeline.pl Bio::EnsEMBL::Compara::PipeConfig::Vertebrates::ncRNAtrees_conf -mlss_id 40139 $(mysql-ens-compara-prod-7-ensadmin details hive)
+
 e96
     # All the databases are defined in the production_reg_conf so the command-line is much simpler
-    init_pipeline.pl Bio::EnsEMBL::Compara::PipeConfig::EBI::Vertebrates::ncRNAtrees_conf -mlss_id 40130 $(mysql-ens-compara-prod-3-ensadmin details hive)
-
-e94
-    -mlss_id 40122 -member_db $(mysql-ens-compara-prod-2 details url waakanni_load_members_94) -prev_rel_db $(mysql-ens-compara-prod-1 details url ensembl_compara_93) -epo_db $(mysql-ens-compara-prod-1 details url ensembl_compara_93)
+    init_pipeline.pl Bio::EnsEMBL::Compara::PipeConfig::Vertebrates::ncRNAtrees_conf -mlss_id 40130 $(mysql-ens-compara-prod-3-ensadmin details hive)
 
 
 =head1 DESCRIPTION
 
-This is the Vertebrates PipeConfig for the ncRNAtree pipeline.
+This is the Vertebrates PipeConfig for the ncRNAtrees pipeline.
 
 =head1 AUTHORSHIP
 
@@ -62,11 +61,12 @@ Internal methods are usually preceded with an underscore (_)
 
 =cut
 
-package Bio::EnsEMBL::Compara::PipeConfig::EBI::Vertebrates::ncRNAtrees_conf;
+package Bio::EnsEMBL::Compara::PipeConfig::Vertebrates::ncRNAtrees_conf;
 
 use strict;
 use warnings;
-use base ('Bio::EnsEMBL::Compara::PipeConfig::EBI::ncRNAtrees_conf');
+
+use base ('Bio::EnsEMBL::Compara::PipeConfig::ncRNAtrees_conf');
 
 sub default_options {
     my ($self) = @_;
@@ -76,63 +76,15 @@ sub default_options {
 
             # Must be given on the command line
             #'mlss_id'          => 40100,
-            # Found automatically if the Core API is in PERL5LIB
-            #'ensembl_release'          => '76',
-            #'rel_suffix' => '',
 
             'division'      => 'vertebrates',
             'collection'    => 'default',       # The name of the species-set within that division
             'pipeline_name' => $self->o('collection') . '_' . $self->o('division').'_ncrna_trees_'.$self->o('rel_with_suffix'),
 
-            # tree break
-            'treebreak_gene_count'     => 400,
-
-            # capacity values for some analysis:
-            'load_members_capacity'           => 10,
-            'quick_tree_break_capacity'       => 100,
-            'msa_chooser_capacity'            => 200,
-            'other_paralogs_capacity'         => 200,
-            'aligner_for_tree_break_capacity' => 200,
-            'infernal_capacity'               => 200,
-            'orthotree_capacity'              => 200,
-            'treebest_capacity'               => 400,
-            'genomic_tree_capacity'           => 300,
-            'genomic_alignment_capacity'      => 700,
-            'fast_trees_capacity'             => 400,
-            'raxml_capacity'                  => 700,
-            'recover_capacity'                => 150,
-            'ss_picts_capacity'               => 200,
-            'ortho_stats_capacity'            => 10,
-            'homology_id_mapping_capacity'    => 10,
-            'cafe_capacity'                   => 50,
-            'decision_capacity'               => 4,
-
-            # Setting priorities
-            'genomic_alignment_priority'       => 35,
-            'genomic_alignment_himem_priority' => 40,
-
-
-            # Params for healthchecks;
-            'hc_priority'                     => 10,
-            'hc_capacity'                     => 40,
-            'hc_batch_size'                   => 10,
-
-            # RFAM parameters
-            'rfam_ftp_url'           => 'ftp://ftp.ebi.ac.uk/pub/databases/Rfam/12.0/',
-            'rfam_remote_file'       => 'Rfam.cm.gz',
-            'rfam_expanded_basename' => 'Rfam.cm',
-            'rfam_expander'          => 'gunzip ',
-
-            # miRBase database
-            'mirbase_url'           => 'mysql://ensro@mysql-ens-compara-prod-1.ebi.ac.uk:4485/mirbase_22',
-
             # CAFE parameters
             'initialise_cafe_pipeline'  => 1,
             # Use production names here
             'cafe_species'          => ['danio_rerio', 'taeniopygia_guttata', 'callithrix_jacchus', 'pan_troglodytes', 'homo_sapiens', 'mus_musculus'],
-
-            # Other parameters
-            'infernal_mxsize'       => 10000,
     };
 } 
 
