e596f86c539ea63f818a579289313816ddfffb06
Jorge Alvarez Jarreta <jalvarez@ebi.ac.uk>
2019-10-31 16:24:48 +0000

    updated documentation

--- modules/Bio/EnsEMBL/Compara/PipeConfig/Vertebrates/SusNcRNAtrees_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/Vertebrates/SusNcRNAtrees_conf.pm
@@ -27,7 +27,7 @@ limitations under the License.
 
 =head1 NAME
 
-Bio::EnsEMBL::Compara::PipeConfig::EBI::Vertebrates::SusNcRNAtrees_conf
+Bio::EnsEMBL::Compara::PipeConfig::Vertebrates::SusNcRNAtrees_conf
 
 =head1 SYNOPSIS
 
