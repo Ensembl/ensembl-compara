56e9a0a4286457e0f03204aaa537f83afbe18693
Matthieu Muffato <muffato@ebi.ac.uk>
2019-11-24 09:45:37 +0000

    $current_chunkset is always defined

--- modules/Bio/EnsEMBL/Compara/RunnableDB/FTPDumps/ChunkAndGroupDnaFrags.pm
+++ modules/Bio/EnsEMBL/Compara/RunnableDB/FTPDumps/ChunkAndGroupDnaFrags.pm
@@ -134,7 +134,7 @@ sub create_chunks_and_write_chromsize_file {
 
     close $fh;
 
-    push @all_chunksets, $current_chunkset if $current_chunkset and @$current_chunkset;
+    push @all_chunksets, $current_chunkset if @$current_chunkset;
 
     # Healthcheck: the sizes must match
     my $tot_chunk_size = 0;
