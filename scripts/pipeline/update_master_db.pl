2aa7c6c80631ff682463e321fb4f8ae1e8c7ec11
Matthieu Muffato <muffato@ebi.ac.uk>
2020-01-15 16:51:57 +0000

    Report some errors when checking the master database
    - missing genome_dbs
    - unavailable core databases

--- scripts/pipeline/update_master_db.pl
+++ scripts/pipeline/update_master_db.pl
@@ -148,6 +148,7 @@ Bio::EnsEMBL::Registry->load_all($reg_conf, 0, 0, 0, "throw_if_missing");
 my $compara_db = Bio::EnsEMBL::Registry->get_DBAdaptor($compara, "compara");
 my $genome_db_adaptor = $compara_db->get_GenomeDBAdaptor();
 my %found_genome_db_ids = ();
+my $has_errors = 0;
 
 foreach my $db_adaptor (@{Bio::EnsEMBL::Registry->get_all_DBAdaptors(-GROUP => 'core')}) {
 
@@ -197,7 +198,9 @@ foreach my $db_adaptor (@{Bio::EnsEMBL::Registry->get_all_DBAdaptors(-GROUP => '
             $proper_genome_db->adaptor($genome_db_adaptor);
             warn "> Differences for '$that_species' (assembly '$that_assembly')\n\t".($proper_genome_db->toString)."\n\t".($master_genome_db->toString)."\n$diffs\n";
             $proper_genome_db->dbID($master_genome_db->dbID);
-            unless ($dry_run) {
+            if ($dry_run) {
+                $has_errors = 1;
+            } else {
                 $genome_db_adaptor->update($proper_genome_db);
                 warn "\t> Successfully updated the master database\n";
             }
@@ -207,6 +210,7 @@ foreach my $db_adaptor (@{Bio::EnsEMBL::Registry->get_all_DBAdaptors(-GROUP => '
             warn "> '$that_species' (assembly '$that_assembly') is in the master database, but is not yet 'current' (i.e. first/last_release are not properly set). It should be fixed after running edit_collection.pl\n";
         }
     } elsif ($check_species_missing_from_compara) {
+        $has_errors = 1;
         warn "> Could not find the species '$that_species' (assembly '$that_assembly') in the genome_db table. You should probably add it.\n";
     }
 
@@ -222,6 +226,7 @@ if ($check_species_with_no_core) {
         # the ancestral database is only ready towards the end of the release
         next if $master_genome_db->name eq 'ancestral_sequences';
         if ($master_genome_db->is_current and not $found_genome_db_ids{$master_genome_db->dbID}) {
+            $has_errors = 1;
             if ($master_genome_db->locator) {
                 warn "> The following genome_db entry has a locator in the master database. You should check that it really needs it.\n";
             } else {
@@ -231,4 +236,5 @@ if ($check_species_with_no_core) {
     }
 }
 
+exit $has_errors;
 
