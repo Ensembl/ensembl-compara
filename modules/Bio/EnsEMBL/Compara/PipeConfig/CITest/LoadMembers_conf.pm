79abb3b488115fadd6967b74fef4841dd741a112
Jorge Alvarez Jarreta <jalvarez@ebi.ac.uk>
2020-01-16 17:35:43 +0000

    updated pipeconfig file

--- modules/Bio/EnsEMBL/Compara/PipeConfig/CITest/LoadMembers_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/CITest/LoadMembers_conf.pm
@@ -15,43 +15,25 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 
-=cut
-
-
-=head1 CONTACT
-
-  Please email comments or questions to the public Ensembl
-  developers list at <http://lists.ensembl.org/mailman/listinfo/dev>.
-
-  Questions may also be sent to the Ensembl help desk at
-  <http://www.ensembl.org/Help/Contact>.
-
 =head1 NAME
 
-Bio::EnsEMBL::Compara::PipeConfig::EBI::Vertebrates::LoadMembers_conf
-
-=head1 DESCRIPTION
-
-Specialized version of the LoadMembers pipeline for Vertebrates
+Bio::Bio::EnsEMBL::Compara::PipeConfig::CITest::LoadMembers_conf
 
 =head1 SYNOPSIS
 
-    init_pipeline.pl Bio::EnsEMBL::Compara::PipeConfig::EBI::CITest::LoadMembers_conf
+    init_pipeline.pl Bio::EnsEMBL::Compara::PipeConfig::LoadMembers_conf -host mysql-ens-compara-prod-X -port XXXX
 
-=head1 AUTHORSHIP
+=head1 DESCRIPTION
 
-Ensembl Team. Individual contributions can be found in the GIT log.
-
-=head1 APPENDIX
-
+This is a CITest configuration file for LastZ pipeline. Please, refer to the
+parent class for further information.
 
 =cut
 
-package Bio::EnsEMBL::Compara::PipeConfig::EBI::CITest::LoadMembers_conf;
+package Bio::EnsEMBL::Compara::PipeConfig::CITest::LoadMembers_conf;
 
 use strict;
 use warnings;
-
 
 use base ('Bio::EnsEMBL::Compara::PipeConfig::LoadMembers_conf');
 
@@ -62,39 +44,21 @@ sub default_options {
     return {
         %{$self->SUPER::default_options},   # inherit the generic ones
 
-    # parameters inherited from EnsemblGeneric_conf and very unlikely to be redefined:
-        # It defaults to Bio::EnsEMBL::ApiVersion::software_version()
-        # 'ensembl_release'       => 68,
-
-    # parameters that are likely to change from execution to another:
-        # It is very important to check that this value is current (commented out to make it obligatory to specify)
-        # Change this one to allow multiple runs
-        #'rel_suffix'            => 'b',
         'division'   => 'citest',
-        # 'collection' => $self->o('division'),
         'collection' => 'citest',
 
-        # names of species we don't want to reuse this time
-        #'do_not_reuse_list'     => [ 'homo_sapiens', 'mus_musculus', 'rattus_norvegicus', 'mus_spretus_spreteij', 'danio_rerio', 'sus_scrofa' ],
-        'do_not_reuse_list'     => [ ],
+        # Names of species we don't want to reuse this time
+        #'do_not_reuse_list' => [ 'homo_sapiens', 'mus_musculus', 'rattus_norvegicus', 'mus_spretus_spreteij', 'danio_rerio', 'sus_scrofa' ],
+        'do_not_reuse_list' => [ ],
 
-    # "Member" parameters:
-        # Store protein-coding genes
-        'store_coding'              => 1,
-        # Store ncRNA genes
-        'store_ncrna'               => 1,
-        # Store other genes
-        'store_others'              => 1,
-        # Only needed in e100
-        'fix_ncrna_members'         => 1,
-
-    #load uniprot members for family pipeline
-        'load_uniprot_members'      => 0,
+        # "Member" parameters:
+        'fix_ncrna_members' => 1,  # Only needed in e100
 
         # Load non reference sequences and patches for fresh members
-        'include_nonreference' => 1,
-        'include_patches'      => 1,
+        'include_nonreference' => 0,
+        'include_patches'      => 0,
 
+        # Run the pipeline without reuse
         'reuse_member_db' => undef,
     };
 }
