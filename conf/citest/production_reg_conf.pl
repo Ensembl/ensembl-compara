8dc7d0319c6a9454a09138b5a390f87ccad32488
Jorge Alvarez Jarreta <jalvarez@ebi.ac.uk>
2020-02-07 16:25:36 +0000

    updated registries based on XML config file

--- conf/citest/production_reg_conf.pl
+++ conf/citest/production_reg_conf.pl
@@ -51,8 +51,8 @@ Bio::EnsEMBL::Compara::Utils::Registry::add_core_dbas( $core_dbs );
 my $compara_dbs = {
     # General compara dbs
     'compara_master' => [ 'mysql-ens-compara-prod-8', 'jalvarez_compara_master_citest' ],
-    # 'compara_curr'   => [ 'mysql-ens-compara-prod-1', "ensembl_compara_$curr_release" ],
-    # 'compara_prev'   => [ 'mysql-ens-compara-prod-1', "ensembl_compara_$prev_release" ],
+    # 'compara_curr'   => [ 'mysql-ens-compara-prod-', "compara_citest_$curr_release" ],
+    # 'compara_prev'   => [ 'mysql-ens-compara-prod-', "compara_citest_$prev_release" ],
 
     # Homology dbs
     # 'compara_members'      => [ 'mysql-ens-compara-prod-', '' ],
@@ -61,50 +61,16 @@ my $compara_dbs = {
     # 'compara_families'     => [ 'mysql-ens-compara-prod-', '' ],
     # 'compara_nctrees'      => [ 'mysql-ens-compara-prod-', '' ],
     # 'nctrees_prev'         => [ 'mysql-ens-compara-prod-', '' ],
-    # 'murinae_ptrees'       => [ 'mysql-ens-compara-prod-', '' ],
-    # 'murinae_nctrees'      => [ 'mysql-ens-compara-prod-', '' ],
-    # 'murinae_ptrees_prev'  => [ 'mysql-ens-compara-prod-', '' ],
-    # 'murinae_nctrees_prev' => [ 'mysql-ens-compara-prod-', '' ],
-    # 'sus_ptrees'           => [ 'mysql-ens-compara-prod-', '' ],
-    # 'sus_nctrees'          => [ 'mysql-ens-compara-prod-', '' ],
-    # 'sus_ptrees_prev'      => [ 'mysql-ens-compara-prod-', '' ],
-    # 'sus_nctrees_prev'     => [ 'mysql-ens-compara-prod-', '' ],
 
     # LastZ dbs
     # 'lastz_batch_1'  => [ 'mysql-ens-compara-prod-', '' ],
-    # 'lastz_batch_2'  => [ 'mysql-ens-compara-prod-', '' ],
 
     # EPO dbs
-    ## Mammals
-    # 'mammals_epo'         => [ 'mysql-ens-compara-prod-', '' ],
-    # 'mammals_epo_prev'    => [ 'mysql-ens-compara-prod-', '' ],
-    # 'mammals_epo_low'     => [ 'mysql-ens-compara-prod-', '' ],
-    # 'mammals_epo_anchors' => [ 'mysql-ens-compara-prod-', '' ],
-
-    ## Sauropsids
-    # 'sauropsids_epo'         => [ 'mysql-ens-compara-prod-', '' ],
-    # 'sauropsids_epo_prev'    => [ 'mysql-ens-compara-prod-', '' ],
-    # 'sauropsids_epo_low'     => [ 'mysql-ens-compara-prod-', '' ],
-    # 'sauropsids_epo_anchors' => [ 'mysql-ens-compara-prod-', '' ],
-
-    ## Fish
-    # 'fish_epo'         => [ 'mysql-ens-compara-prod-', '' ],
-    # 'fish_epo_prev'    => [ 'mysql-ens-compara-prod-', '' ],
-    # 'fish_epo_low'     => [ 'mysql-ens-compara-prod-', '' ],
-    # 'fish_epo_anchors' => [ 'mysql-ens-compara-prod-', '' ],
-
-    ## Primates
-    # NOTE: Primates are reused from mammals of the *same release* (same anchors and subset of species)
-    # 'primates_epo'         => [ 'mysql-ens-compara-prod-', '' ],
-    # 'primates_epo_prev'    => [ 'mysql-ens-compara-prod-', '' ],
-    # 'primates_epo_low'     => [ 'mysql-ens-compara-prod-', '' ],
-    # 'primates_epo_anchors' => [ 'mysql-ens-compara-prod-', '' ],
-
-    ## Pig strains
-    # 'pig_strains_epo'         => [ 'mysql-ens-compara-prod-', '' ],
-    # 'pig_strains_epo_prev'    => [ 'mysql-ens-compara-prod-', '' ],
-    # 'pig_strains_epo_low'     => [ 'mysql-ens-compara-prod-', '' ],
-    # 'pig_strains_epo_anchors' => [ 'mysql-ens-compara-prod-', '' ],
+    ## Mammals with feathers
+    # 'mammals_with_feathers_epo'         => [ 'mysql-ens-compara-prod-', '' ],
+    # 'mammals_with_feathers_prev'        => [ 'mysql-ens-compara-prod-', '' ],
+    # 'mammals_with_feathers_epo_low'     => [ 'mysql-ens-compara-prod-', '' ],
+    # 'mammals_with_feathers_epo_anchors' => [ 'mysql-ens-compara-prod-', '' ],
 
     # Other alignments
     # 'amniotes_pecan'      => [ 'mysql-ens-compara-prod-', '' ],
@@ -124,10 +90,7 @@ my $ancestral_dbs = {
     'ancestral_curr' => [ 'mysql-ens-compara-prod-1', "ensembl_ancestral_$curr_release" ],
     'ancestral_prev' => [ 'mysql-ens-compara-prod-1', "ensembl_ancestral_$prev_release" ],
 
-    # 'mammals_ancestral'    => [ 'mysql-ens-compara-prod-5', 'jalvarez_mammals_ancestral_core_99' ],
-    # 'primates_ancestral'   => [ 'mysql-ens-compara-prod-3', 'mateus_primates_ancestral_core_98' ],
-    # 'sauropsids_ancestral' => [ 'mysql-ens-compara-prod-8', 'dthybert_sauropsids_ancestral_core_99' ],
-    # 'fish_ancestral'       => [ 'mysql-ens-compara-prod-1', 'cristig_fish_ancestral_core_99' ],
+    # 'mammals_with_feathers_ancestral' => [ 'mysql-ens-compara-prod-', '' ],
 };
 
 Bio::EnsEMBL::Compara::Utils::Registry::add_core_dbas( $ancestral_dbs );
