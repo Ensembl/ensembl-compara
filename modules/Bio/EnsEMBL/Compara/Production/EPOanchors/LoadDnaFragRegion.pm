e2761e984d298cca34adba3049a06041e54a88aa
David Thybert <dthybert@ebi.ac.uk>
2020-02-17 16:03:18 +0000

    Taking into account code review feedbacks

--- modules/Bio/EnsEMBL/Compara/Production/EPOanchors/LoadDnaFragRegion.pm
+++ modules/Bio/EnsEMBL/Compara/Production/EPOanchors/LoadDnaFragRegion.pm
@@ -166,12 +166,12 @@ sub write_output {
 	# add the MTs to the dnafrag_region table
 	if($self->param('add_non_nuclear_alignments')) {
         my $sth_mt = $self->dbc->prepare("SELECT dnafrag_id, length FROM dnafrag JOIN species_set USING (genome_db_id) JOIN method_link_species_set USING (species_set_id) WHERE cellular_component =\"MT\" AND method_link_species_set_id = ?");
-        $sth_mt->execute;
+        $sth_mt->execute($self->param('mlss_id'));
         my @mt_dna_frag= @{ $sth_mt->fetchall_arrayref };
         if ( scalar   @mt_dna_frag > 1){
             my $max_synteny_region_id = $synteny_region_ids[-1]->{'synteny_region_id'} + 1;
             $sth2->execute($max_synteny_region_id, $self->param('mlss_id'));
-            foreach my $dnafrag_region ( @{ $sth_mt->fetchall_arrayref } ) {
+            foreach my $dnafrag_region ( @mt_dna_frag ) {
                 $sth1->execute($max_synteny_region_id, $dnafrag_region->[0], 1, $dnafrag_region->[1], 1);
             }
             push(@synteny_region_ids, {synteny_region_id => $max_synteny_region_id});
