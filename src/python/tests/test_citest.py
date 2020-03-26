7c5c0b294523747c308b6af20a74afbbc155671a
Jorge Alvarez Jarreta <jalvarez@ebi.ac.uk>
2020-03-26 14:03:03 +0000

    reduced code complexity
    - Remove fixtures from "Args" docstring field
    - Replace exception control by ContextManager to simply the code

--- src/python/tests/test_citest.py
+++ src/python/tests/test_citest.py
@@ -16,10 +16,12 @@ limitations under the License.
 # Disable redefined-outer-name rule in pylint to avoid warning due to how pytest fixtures work
 # pylint: disable=redefined-outer-name
 
+from contextlib import ExitStack as does_not_raise
 from pathlib import Path
-from typing import Callable, Dict
+from typing import Callable, ContextManager, Dict
 
 import pytest
+from pytest import raises
 from _pytest.fixtures import FixtureRequest
 
 from ensembl.compara.citest import CITestDBItem, CITestDBContentError, CITestDBGroupingError, \
@@ -56,125 +58,110 @@ class TestCITestDBItem:
     """Tests CITest's :class:`CITestDBItem` class."""
 
     @pytest.mark.parametrize(
-        "kwargs, exception",
+        "kwargs, expectation",
         [
-            ({}, None),
-            ({'group_by': 'grp'}, CITestDBNumRowsError),
-            ({'variation': 0.5, 'group_by': 'grp'}, None),
-            ({'group_by': ['grp', 'value']}, CITestDBGroupingError),
-            ({'filter_by': 'value < 30'}, None),
-            ({'filter_by': ['value < 30', 'grp = "grp2"']}, CITestDBNumRowsError),
-            ({'variation': 0.25, 'filter_by': ['value < 30', 'grp = "grp2"']}, None),
-            ({'group_by': 'grp', 'filter_by': 'value < 24'}, None),
+            ({}, does_not_raise()),
+            ({'group_by': 'grp'}, raises(CITestDBNumRowsError)),
+            ({'variation': 0.5, 'group_by': 'grp'}, does_not_raise()),
+            ({'group_by': ['grp', 'value']}, raises(CITestDBGroupingError)),
+            ({'filter_by': 'value < 30'}, does_not_raise()),
+            ({'filter_by': ['value < 30', 'grp = "grp2"']}, raises(CITestDBNumRowsError)),
+            ({'variation': 0.25, 'filter_by': ['value < 30', 'grp = "grp2"']}, does_not_raise()),
+            ({'group_by': 'grp', 'filter_by': 'value < 24'}, does_not_raise()),
         ],
         ids=pytest.get_param_repr
     )
-    def test_num_rows_test(self, db_item: CITestDBItem, kwargs: Dict, exception: Exception) -> None:
+    def test_num_rows_test(self, db_item: CITestDBItem, kwargs: Dict, expectation: ContextManager) -> None:
         """Tests CITest's :meth:`CITestDBItem.test_num_rows()` method.
 
         Args:
-            db_item: CITest object to compare a table in reference and target unit test databases.
             kwargs: Named arguments to be passed to the method.
-            exception: If `None`, this test will pass if no exception is raised. If an exception is given,
-                the test will pass only if that specific exception is raised.
+            expectation: Context manager for the expected exception, i.e. the test will only pass if that
+                exception is raised. Use :class:`contextlib.nullcontext` if no exception is expected.
 
         """
-        if not exception:
+        with expectation:
             db_item.test_num_rows(**kwargs)
-        else:
-            with pytest.raises(exception):
-                db_item.test_num_rows(**kwargs)
 
     @pytest.mark.parametrize(
-        "kwargs, exception",
+        "kwargs, expectation",
         [
-            ({}, CITestDBContentError),
-            ({'columns': 'value'}, None),
-            ({'columns': ['value', 'comment']}, CITestDBContentError),
-            ({'ignore_columns': 'grp'}, CITestDBContentError),
-            ({'ignore_columns': ['id', 'grp', 'comment']}, None),
-            ({'columns': 'value', 'ignore_columns': 'grp'}, TypeError),
-            ({'filter_by': 'grp = "grp2"'}, CITestDBNumRowsError),
-            ({'filter_by': ['value < 23', 'comment = "Second group"']}, None),
-            ({'columns': 'grp', 'filter_by': 'grp = "grp3"'}, CITestDBNumRowsError),
-            ({'ignore_columns': 'id', 'filter_by': 'value != 24'}, None),
+            ({}, raises(CITestDBContentError)),
+            ({'columns': 'value'}, does_not_raise()),
+            ({'columns': ['value', 'comment']}, raises(CITestDBContentError)),
+            ({'ignore_columns': 'grp'}, raises(CITestDBContentError)),
+            ({'ignore_columns': ['id', 'grp', 'comment']}, does_not_raise()),
+            ({'columns': 'value', 'ignore_columns': 'grp'}, raises(TypeError)),
+            ({'filter_by': 'grp = "grp2"'}, raises(CITestDBNumRowsError)),
+            ({'filter_by': ['value < 23', 'comment = "Second group"']}, does_not_raise()),
+            ({'columns': 'grp', 'filter_by': 'grp = "grp3"'}, raises(CITestDBNumRowsError)),
+            ({'ignore_columns': 'id', 'filter_by': 'value != 24'}, does_not_raise()),
         ],
         ids=pytest.get_param_repr
     )
-    def test_content_test(self, db_item: CITestDBItem, kwargs: Dict, exception: Exception) -> None:
+    def test_content_test(self, db_item: CITestDBItem, kwargs: Dict, expectation: ContextManager) -> None:
         """Tests CITest's :meth:`CITestDBItem.test_content()` method.
 
         Args:
-            db_item: CITest object to compare a table in reference and target unit test databases.
             kwargs: Named arguments to be passed to the method.
-            exception: If `None`, this test will pass if no exception is raised. If an exception is given,
-                the test will pass only if that specific exception is raised.
+            expectation: Context manager for the expected exception, i.e. the test will only pass if that
+                exception is raised. Use :class:`contextlib.nullcontext` if no exception is expected.
 
         """
-        if not exception:
+        with expectation:
             db_item.test_content(**kwargs)
-        else:
-            with pytest.raises(exception):
-                db_item.test_content(**kwargs)
 
 
 class TestCITestFilesItem:
     """Tests CITest's :class:`CITestFilesItem` class."""
 
     @pytest.mark.parametrize(
-        "kwargs, exception",
+        "kwargs, expectation",
         [
-            ({}, CITestFilesSizeError),
-            ({'variation': 1.0}, CITestFilesTreeError),
-            ({'patterns': 'c*'}, None),
-            ({'patterns': ['a*', 'c*']}, CITestFilesSizeError),
-            ({'paths': '1'}, CITestFilesSizeError),
-            ({'paths': ['0', '2']}, CITestFilesSizeError),
-            ({'patterns': 'c*', 'paths': '1'}, None),
-            ({'variation': 1.0, 'patterns': 'b*', 'paths': '1'}, None),
+            ({}, raises(CITestFilesSizeError)),
+            ({'variation': 1.0}, raises(CITestFilesTreeError)),
+            ({'patterns': 'c*'}, does_not_raise()),
+            ({'patterns': ['a*', 'c*']}, raises(CITestFilesSizeError)),
+            ({'paths': '1'}, raises(CITestFilesSizeError)),
+            ({'paths': ['0', '2']}, raises(CITestFilesSizeError)),
+            ({'patterns': 'c*', 'paths': '1'}, does_not_raise()),
+            ({'variation': 1.0, 'patterns': 'b*', 'paths': '1'}, does_not_raise()),
         ],
         ids=pytest.get_param_repr
     )
-    def test_size_test(self, files_item: CITestFilesItem, kwargs: Dict, exception: Exception) -> None:
+    def test_size_test(self, files_item: CITestFilesItem, kwargs: Dict, expectation: ContextManager) -> None:
         """Tests CITest's :meth:`CITestFilesItem.test_size()` method.
 
         Args:
-            files_item: CITest object to compare reference and target test directory trees.
             kwargs: Named arguments to be passed to the method.
-            exception: If `None`, this test will pass if no exception is raised. If an exception is given,
-                the test will pass only if that specific exception is raised.
+            expectation: Context manager for the expected exception, i.e. the test will only pass if that
+                exception is raised. Use :class:`contextlib.nullcontext` if no exception is expected.
 
         """
-        if not exception:
+        with expectation:
             files_item.test_size(**kwargs)
-        else:
-            with pytest.raises(exception):
-                files_item.test_size(**kwargs)
 
     @pytest.mark.parametrize(
-        "kwargs, exception",
+        "kwargs, expectation",
         [
-            ({}, CITestFilesContentError),
-            ({'patterns': 'a*'}, CITestFilesTreeError),
-            ({'patterns': ['a*', 'c*']}, CITestFilesContentError),
-            ({'paths': '0'}, CITestFilesTreeError),
-            ({'paths': ['0', '2']}, CITestFilesContentError),
-            ({'patterns': 'a*', 'paths': '0'}, None),
+            ({}, raises(CITestFilesContentError)),
+            ({'patterns': 'a*'}, raises(CITestFilesTreeError)),
+            ({'patterns': ['a*', 'c*']}, raises(CITestFilesContentError)),
+            ({'paths': '0'}, raises(CITestFilesTreeError)),
+            ({'paths': ['0', '2']}, raises(CITestFilesContentError)),
+            ({'patterns': 'a*', 'paths': '0'}, does_not_raise()),
         ],
         ids=pytest.get_param_repr
     )
-    def test_content_test(self, files_item: CITestFilesItem, kwargs: Dict, exception: Exception) -> None:
+    def test_content_test(self, files_item: CITestFilesItem, kwargs: Dict, expectation: ContextManager
+                         ) -> None:
         """Tests CITest's :meth:`CITestFilesItem.test_content()` method.
 
         Args:
-            files_item: CITest object to compare reference and target test directory trees.
             kwargs: Named arguments to be passed to the method.
-            exception: If `None`, this test will pass if no exception is raised. If an exception is given,
-                the test will pass only if that specific exception is raised.
+            expectation: Context manager for the expected exception, i.e. the test will only pass if that
+                exception is raised. Use :class:`contextlib.nullcontext` if no exception is expected.
 
         """
-        if not exception:
+        with expectation:
             files_item.test_content(**kwargs)
-        else:
-            with pytest.raises(exception):
-                files_item.test_content(**kwargs)
