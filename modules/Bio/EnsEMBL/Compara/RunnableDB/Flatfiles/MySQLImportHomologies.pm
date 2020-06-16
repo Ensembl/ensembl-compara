526e992fab69315a74a1f104c31024ff534bf677
Carla Cummins <carlac@ebi.ac.uk>
2020-06-16 15:55:07 +0100

    Runnable (+ unit test) to use mysqlimport to load homologies

---
+++ modules/Bio/EnsEMBL/Compara/RunnableDB/Flatfiles/MySQLImportHomologies.pm
@@ -0,0 +1,161 @@
+=head1 LICENSE
+
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+Copyright [2016-2020] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+=pod
+
+=head1 NAME
+
+Bio::EnsEMBL::Compara::RunnableDB::Flatfiles::MySQLImportHomologies
+
+=head1 SYNOPSIS
+
+
+
+=cut
+
+package Bio::EnsEMBL::Compara::RunnableDB::Flatfiles::MySQLImportHomologies;
+
+use warnings;
+use strict;
+use Bio::EnsEMBL::Registry;
+use Bio::EnsEMBL::Compara::DBSQL::DBAdaptor;
+
+use Bio::EnsEMBL::Compara::Utils::FlatFile qw(map_row_to_header);
+use Data::Dumper;
+
+use base ('Bio::EnsEMBL::Compara::RunnableDB::BaseRunnable');
+
+sub fetch_input {
+    my $self = shift;
+
+    # handle any attrib files that may have been passed
+    $self->param('attribs', {});
+    my $attrib_files = $self->param('attrib_files');
+    return unless ( $attrib_files );
+
+    # fetch all attributes from file list
+    my %attribs;
+    my $primary_key = 'homology_id';
+    foreach my $f ( @$attrib_files ) {
+        open( my $fh, '<', $f ) or die "Cannot open $f for reading";
+        my $header = <$fh>;
+        my @header_cols = split( /\s+/, $header );
+        die "No $primary_key found in $f - please check file header line\n" unless grep {$_ eq $primary_key} @header_cols;
+        while ( my $line = <$fh> ) {
+            my $row = map_row_to_header($line, \@header_cols);
+            my $primary_id = $row->{$primary_key};
+            delete $row->{$primary_key};
+            foreach my $attrib_name ( keys %$row ) {
+                $attribs{$primary_id}->{$attrib_name} = $row->{$attrib_name};
+            }
+        }
+        close $fh;
+    }
+
+    $self->param('attribs', \%attribs);
+}
+
+sub write_output {
+    my $self = shift;
+
+    my $filename = $self->param_required('homology_flatfile');
+    my $attribs  = $self->param('attribs');
+
+    # open homology file
+    open(my $hom_fh, '<', $filename) or die "Cannot open $filename for reading";
+    my $header_line = <$hom_fh>;
+    my @header_cols = split(/\s+/, $header_line);
+
+    # open output files - one per table in tmp space
+    my $homology_csv = $self->worker_temp_directory . 'homology.txt';
+    open(my $h_csv, '>', $homology_csv) or die "Cannot open $homology_csv for writing";
+    my $homology_member_csv = $self->worker_temp_directory . 'homology_member.txt';
+    open(my $hm_csv, '>', $homology_member_csv) or die "Cannot open $homology_member_csv for writing";
+
+    # iterate over homology input and format it for later mysqlimport
+    my $h_count;
+    while ( my $line = <$hom_fh> ) {
+        my $row = map_row_to_header($line, \@header_cols);
+
+        # add the attribs
+        foreach my $attrib_name ( 'goc_score', 'wga_coverage', 'is_high_confidence' ) {
+            my $this_attrib = $attribs->{$row->{homology_id}}->{$attrib_name};
+            $row->{$attrib_name} = defined $this_attrib ? $this_attrib : '\N';
+        }
+
+        my ( $homology_row, $homology_member_rows ) = $self->split_row_for_homology_tables($row);
+        print $h_csv $homology_row;
+        print $hm_csv $homology_member_rows;
+        $h_count++;
+    }
+    close $h_csv;
+    close $hm_csv;
+    print "Files written to: $homology_csv & $homology_member_csv\n" if $self->debug;
+
+    # mysqlimport the data
+    my $this_dbc = $self->compara_dba->dbc;
+    my $user = $this_dbc->username;
+    my $pass = $this_dbc->password;
+    my $host = $this_dbc->host;
+    my $port = $this_dbc->port;
+    my $dbname = $this_dbc->dbname;
+    my $import_cmd = join(' ', 
+        "mysqlimport -h$host -P$port -u$user -p$pass",
+        "--local --lock-tables --ignore --fields-terminated-by=','",
+        $dbname
+    );
+    $self->run_command("$import_cmd $homology_csv", { die_on_failure => 1, debug => $self->debug });
+    $self->run_command("$import_cmd $homology_member_csv", { die_on_failure => 1, debug => $self->debug });
+}
+
+sub split_row_for_homology_tables {
+    my ( $self, $row ) = @_;
+
+    my $homology_row = join(",",
+        $row->{homology_id},
+        $row->{mlss_id},
+        $row->{homology_type},
+        $row->{is_tree_compliant},
+        '\N','\N','\N','\N','\N',
+        $row->{species_tree_node_id},
+        $row->{gene_tree_node_id},
+        $row->{gene_tree_root_id},
+        $row->{goc_score},
+        $row->{wga_coverage},
+        $row->{is_high_confidence},
+    ) . "\n";
+
+    my $homology_member_rows;
+    foreach my $prefix ( '', 'homology_' ) {
+        my $this_hm_row = join(",",
+            $row->{homology_id},
+            $row->{"${prefix}gene_member_id"},
+            $row->{"${prefix}seq_member_id"},
+            $row->{"${prefix}cigar_line"},
+            $row->{"${prefix}perc_cov"},
+            $row->{"${prefix}perc_id"},
+            $row->{"${prefix}perc_pos"},
+        );
+        $homology_member_rows .= "$this_hm_row\n";
+    }
+
+    return ( $homology_row, $homology_member_rows );
+}
+
+1;
---
+++ modules/t/MySQLImportHomologies.t
@@ -0,0 +1,110 @@
+#!/usr/bin/env perl
+# Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+# Copyright [2016-2020] EMBL-European Bioinformatics Institute
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+use strict;
+use warnings;
+
+use Data::Dumper;
+use Bio::EnsEMBL::Hive::Utils::Test qw(standaloneJob);
+use Bio::EnsEMBL::Hive::DBSQL::DBConnection;
+use Bio::EnsEMBL::Test::MultiTestDB;
+
+use Test::Most;
+
+BEGIN {
+    # check module can be seen and compiled
+    use_ok('Bio::EnsEMBL::Compara::RunnableDB::Flatfiles::MySQLImportHomologies');
+}
+
+##################################################################################
+#                              Test homologies                                   #
+##################################################################################
+subtest "Test Bio::EnsEMBL::Compara::RunnableDB::Flatfiles::MySQLImportHomologies", sub {
+    # Load test DB #
+    my $multi_db = Bio::EnsEMBL::Test::MultiTestDB->new('update_homologies_test');
+    my $dba = $multi_db->get_DBAdaptor('compara');
+    my $dbc = Bio::EnsEMBL::Hive::DBSQL::DBConnection->new(-dbconn => $dba->dbc);
+    my $compara_db = $dbc->url;
+    $dbc->do("TRUNCATE TABLE homology");
+
+    # find absolute path to the test input
+    # important for travis-ci
+    use Cwd 'abs_path';
+    my $test_flatfile_dir = abs_path($0);
+    $test_flatfile_dir =~ s!MySQLImportHomologies\.t!homology_flatfiles/mysqlimport_test!;
+
+    # run in standalone
+    standaloneJob(
+        'Bio::EnsEMBL::Compara::RunnableDB::Flatfiles::MySQLImportHomologies',
+        {
+            'compara_db'   => $compara_db,
+            'homology_flatfile' => "$test_flatfile_dir/homologies.tsv",
+            'attrib_files' => [
+                "$test_flatfile_dir/wga.tsv",
+                "$test_flatfile_dir/goc.tsv",
+                "$test_flatfile_dir/high_conf.tsv",
+            ],
+        },
+    );
+
+    # check main homology data loading
+    my $hom_sth = $dba->dbc->prepare("SELECT 
+        homology_id, method_link_species_set_id, description, is_tree_compliant,
+        species_tree_node_id, gene_tree_node_id, gene_tree_root_id
+        FROM homology"
+    );
+    $hom_sth->execute();
+    my $homology_data = $hom_sth->fetchall_arrayref;
+
+    my $exp_hom_data = [
+        ['1', '1234', 'ortholog_one2one',  1, '40133018', '14624903', '1377777'],
+        ['2', '1234', 'ortholog_one2many', 0, '40133016', '20937479', '2304777'],
+        ['3', '1234', 'ortholog_one2one',  1, '40133018', '15053143', '1631777'],
+    ];
+    is_deeply($homology_data, $exp_hom_data, 'homology data loaded correctly');
+
+    # check homology attribute data loading
+    my $attr_sth = $dba->dbc->prepare("SELECT 
+        goc_score, wga_coverage, is_high_confidence
+        FROM homology"
+    );
+    $attr_sth->execute();
+    my $attrib_data = $attr_sth->fetchall_arrayref;
+
+    my $exp_attr_data = [
+        ['0',   undef,   1],
+        ['50',  '95.50', 1],
+        [undef, '0.00',  0],
+    ];
+    is_deeply($attrib_data, $exp_attr_data, 'homology attributes loaded correctly');
+
+    # check homology_member data
+    my $hom_mem_sth = $dba->dbc->prepare("SELECT * FROM homology_member");
+    $hom_mem_sth->execute();
+    my $hom_mem_data = $hom_mem_sth->fetchall_arrayref;
+
+    my $exp_hom_mem_data = [
+        ['1', '607122', '759483',  '20M',     97, 83, 86],
+        ['1', '806596', '1021296', '10M5D5M', 99, 85, 87],
+        ['2', '694925', '875992',  '20M',     99, 84, 89],
+        ['2', '681661', '858054',  '10M5D5M', 99, 84, 89],
+        ['3', '213046', '256120',  '20M',     99, 82, 88],
+        ['3', '735836', '929326',  '10M5D5M', 76, 63, 67],
+    ];
+    is_deeply($hom_mem_data, $exp_hom_mem_data, 'homology members loaded correctly');
+};
+
+done_testing();
