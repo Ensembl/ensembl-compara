e042951a3a9f2339f2ad40c585d4cb003582eca1
Manoj <manojpandian@ebi.ac.uk>
2020-07-20 07:29:52 +0000

    Change release date to August for 101

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -35,8 +35,8 @@ use Sys::Hostname::Long;
 ###############################################################################
 ## Ensembl Version and release dates (these get updated every release)
 our $ENSEMBL_VERSION        = 101;            # Ensembl release number
-our $ARCHIVE_VERSION        = 'Jul2020';     # Archive site for this version
-our $ENSEMBL_RELEASE_DATE   = 'July 2020'; # As it would appear in the copyright/footer
+our $ARCHIVE_VERSION        = 'Aug2020';     # Archive site for this version
+our $ENSEMBL_RELEASE_DATE   = 'August 2020'; # As it would appear in the copyright/footer
 ###############################################################################
 
 
