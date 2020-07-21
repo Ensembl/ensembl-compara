10b55ddb88cbb258ff196d6e2a9f19635e2ef9c6
Matthieu Muffato <muffato@ebi.ac.uk>
2020-07-21 14:59:36 +0100

    Added a table to remember which ranges have been associated to which requestors

--- modules/Bio/EnsEMBL/Compara/Utils/IDGenerator.pm
+++ modules/Bio/EnsEMBL/Compara/Utils/IDGenerator.pm
@@ -35,6 +35,8 @@ package Bio::EnsEMBL::Compara::Utils::IDGenerator;
 use strict;
 use warnings;
 
+use Bio::EnsEMBL::Compara::Utils::CopyData qw(:insert);
+
 use base qw(Exporter);
 
 our %EXPORT_TAGS;
@@ -49,10 +51,29 @@ our @EXPORT_OK;
 
 
 sub get_id_range {
-    my ($dbc, $label, $n_ids) = @_;
+    my ($dbc, $label, $n_ids, $requestor) = @_;
 
     $n_ids //= 1;
     die "Cannot request 0 IDs" unless $n_ids;
+
+    # Check whether we have already seen this requestor
+    if ($requestor) {
+        my $existing_row = $dbc->db_handle->selectrow_arrayref(
+            'SELECT assigned_id, size FROM id_assignments WHERE label = ? AND requestor = ?',
+            undef,
+            $label, $requestor,
+        );
+        if ($existing_row) {
+            # This requestor has already placed its request, let's check if the interval was big enough
+            return $existing_row->[0] if $existing_row->[1] >= $n_ids;
+            # If not, forget about the previous request
+            $dbc->do(
+                'DELETE FROM id_assignments WHERE label = ? AND requestor = ?',
+                undef,
+                $label, $requestor,
+            );
+        }
+    }
 
     # First insert the initial value if needed
     $dbc->do(
@@ -70,6 +91,11 @@ sub get_id_range {
 
     # Recall the value before it got incremented
     my $next_id = $dbc->db_handle->last_insert_id();
+
+    # Register the attempt
+    if ($requestor) {
+        single_insert($dbc, 'id_assignments', [ $label, $requestor, $next_id, $n_ids, ]);
+    }
 
     return $next_id;
 }
