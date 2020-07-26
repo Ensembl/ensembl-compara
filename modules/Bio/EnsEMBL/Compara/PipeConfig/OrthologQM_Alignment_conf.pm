7c2e1d9594addf85e75399d4d18e620bc2cf708d
Matthieu Muffato <muffato@ebi.ac.uk>
2020-07-26 12:58:28 +0100

    This pipeline doesn't need the compara tables, since it connects to "compara_db"

--- modules/Bio/EnsEMBL/Compara/PipeConfig/OrthologQM_Alignment_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/OrthologQM_Alignment_conf.pm
@@ -140,6 +140,8 @@ sub default_options {
     };
 }
 
+sub no_compara_schema {}    # Tell the base class not to create the Compara tables in the database
+
 =head2 pipeline_create_commands
 
 	Description: create tables for writing data to
