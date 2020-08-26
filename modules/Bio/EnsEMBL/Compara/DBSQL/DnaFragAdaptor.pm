d4153408b692862024bb4509dc380dcb8c9186e2
Matthieu Muffato <muffato@ebi.ac.uk>
2020-08-26 19:35:43 +0100

    Added a method to fetch a bunch of dnafrags by their names

--- modules/Bio/EnsEMBL/Compara/DBSQL/DnaFragAdaptor.pm
+++ modules/Bio/EnsEMBL/Compara/DBSQL/DnaFragAdaptor.pm
@@ -309,6 +309,61 @@ sub fetch_all_karyotype_DnaFrags_by_GenomeDB {
 }
 
 
+=head2 fetch_all_by_GenomeDB_and_names
+
+  Arg [1]    : integer $genome_db_id
+                  - or -
+               Bio::EnsEMBL::Compara::GenomeDB
+  Arg [2]    : Arrayref of strings $names
+  Example    : my $dnafrags = $dnafrag_adaptor->fetch_all_by_GenomeDB_and_names($human_genome_db, ['X', 'Y']);
+  Description: Returns the Bio::EnsEMBL::Compara::DnaFrag objects corresponding to the
+               Bio::EnsEMBL::Compara::GenomeDB and names given. $genome_db can
+               be a valid $genome_db_id instead. Note that:
+               1) The dnafrags may be returned in a different order than
+                  the names.
+               2) There could be fewer dnafrags than names, if some names
+                  are not found.
+  Returntype : Arrayref of Bio::EnsEMBL::Compara::DnaFrag
+  Exceptions : throw when genome_db_id cannot be retrieved
+  Caller     : general
+
+=cut
+
+sub fetch_all_by_GenomeDB_and_names {
+    my ($self, $genome_db, $names) = @_;
+
+    my $genome_db_id;
+    if ($genome_db and ($genome_db =~ /^\d+$/)) {
+        $genome_db_id = $genome_db;
+    } else {
+        assert_ref($genome_db, 'Bio::EnsEMBL::Compara::GenomeDB', 'genome_db');
+        $genome_db_id = $genome_db->dbID;
+        if (!$genome_db_id) {
+            throw("[$genome_db] does not have a dbID");
+        }
+    }
+
+    my @dnafrags;
+    my @names_to_fetch;
+    foreach my $name (@$names) {
+        my $cache_key = $genome_db_id . '//' . $name;
+        if (my $cached_df = $self->{'_lru_cache_gdb_id_name'}->{$cache_key}) {
+            push @dnafrags, $cached_df;
+        } else {
+            push @names_to_fetch, $name;
+        }
+    }
+    return \@dnafrags unless @names_to_fetch;
+
+    my $sql_in = $self->generate_in_constraint(\@names_to_fetch, 'name', SQL_VARCHAR, 1);
+    $self->bind_param_generic_fetch($genome_db_id, SQL_INTEGER);
+    my $other_dnafrags = $self->generic_fetch('df.genome_db_id = ? AND ' . $sql_in);
+
+    push @dnafrags, @$other_dnafrags;
+    return \@dnafrags;
+}
+
+
 =head2 fetch_by_Slice
 
   Arg [1]    : Bio::EnsEMBL::Slice $slice
