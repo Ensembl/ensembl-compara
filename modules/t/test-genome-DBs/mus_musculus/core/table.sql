63c76901e01148674af89d4ab1eb676ebf2e4bd9
Matthieu Muffato <muffato@ebi.ac.uk>
2020-09-04 12:23:29 +0100

    The external_db_bak has never been part of the Core schema

--- modules/t/test-genome-DBs/mus_musculus/core/table.sql
+++ modules/t/test-genome-DBs/mus_musculus/core/table.sql
@@ -301,21 +301,6 @@ CREATE TABLE `external_db` (
   UNIQUE KEY `db_name_db_release_idx` (`db_name`,`db_release`)
 ) ENGINE=MyISAM AUTO_INCREMENT=50692 DEFAULT CHARSET=latin1;
 
-CREATE TABLE `external_db_bak` (
-  `external_db_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `db_name` varchar(100) NOT NULL,
-  `db_release` varchar(255) DEFAULT NULL,
-  `status` enum('KNOWNXREF','KNOWN','XREF','PRED','ORTH','PSEUDO') NOT NULL,
-  `priority` int(11) NOT NULL,
-  `db_display_name` varchar(255) DEFAULT NULL,
-  `type` enum('ARRAY','ALT_TRANS','ALT_GENE','MISC','LIT','PRIMARY_DB_SYNONYM','ENSEMBL') DEFAULT NULL,
-  `secondary_db_name` varchar(255) DEFAULT NULL,
-  `secondary_db_table` varchar(255) DEFAULT NULL,
-  `description` text,
-  PRIMARY KEY (`external_db_id`),
-  UNIQUE KEY `db_name_db_release_idx` (`db_name`,`db_release`)
-) ENGINE=MyISAM AUTO_INCREMENT=50692 DEFAULT CHARSET=latin1;
-
 CREATE TABLE `external_synonym` (
   `xref_id` int(10) unsigned NOT NULL,
   `synonym` varchar(100) NOT NULL,
