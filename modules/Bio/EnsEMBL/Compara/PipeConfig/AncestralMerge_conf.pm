8a97c64eb86b2e00e3f2e7fa2f9423d8af18272d
Jorge Alvarez Jarreta <jalvarez@ebi.ac.uk>
2020-11-04 17:52:36 +0000

    bugfix: remove 'collection-' from species set name

--- modules/Bio/EnsEMBL/Compara/PipeConfig/AncestralMerge_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/AncestralMerge_conf.pm
@@ -15,11 +15,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 
-=cut
-
-
-=pod
-
 =head1 NAME
 
 Bio::EnsEMBL::Compara::PipeConfig::AncestralMerge_conf
@@ -32,14 +27,6 @@ Bio::EnsEMBL::Compara::PipeConfig::AncestralMerge_conf
 =head1 DESCRIPTION
 
     A pipeline to create the EnsEMBL core database with ancestral sequences merged from different sources.
-
-=head1 CONTACT
-
-Please email comments or questions to the public Ensembl
-developers list at <http://lists.ensembl.org/mailman/listinfo/dev>.
-
-Questions may also be sent to the Ensembl help desk at
-<http://www.ensembl.org/Help/Contact>.
 
 =cut
 
@@ -122,7 +109,7 @@ sub pipeline_analyses {
                 # Get each EPO mlss id and its corresponding database alias from
                 # ensembl_compara_${CURR_ENSEMBL_RELEASE}
                 'db_conn'      => $self->o('compara_db'),
-                'inputquery'   => 'SELECT mlss.method_link_species_set_id, CONCAT(ssh.name, "_ancestral") FROM method_link_species_set mlss JOIN species_set_header ssh USING (species_set_id) WHERE mlss.method_link_id = 13 AND mlss.first_release IS NOT NULL AND mlss.last_release IS NULL',
+                'inputquery'   => 'SELECT mlss.method_link_species_set_id, CONCAT(TRIM(LEADING "collection-" FROM ssh.name), "_ancestral") FROM method_link_species_set mlss JOIN species_set_header ssh USING (species_set_id) WHERE mlss.method_link_id = 13 AND mlss.first_release IS NOT NULL AND mlss.last_release IS NULL',
                 'column_names' => [ 'mlss_id', 'from_alias' ],
             },
             -flow_into => {
