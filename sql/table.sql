e7b657ea6c5611c2ee33371c36a3975cfcd96adf
Jorge Alvarez Jarreta <jalvarez@ebi.ac.uk>
2020-11-18 18:12:11 +0000

    updated sql schema doc

--- sql/table.sql
+++ sql/table.sql
@@ -1381,9 +1381,6 @@ CREATE TABLE peptide_align_feature (
 @desc  This table contains all the group homologies found. There are several family_member entries for each family entry.
 @colour   #BC5CEC
 
-@example   The following query retrieves families with "DEGRADATION" in the description and a description_score of 100
-    @sql                                SELECT * FROM family WHERE description like '%DEGRADATION%' AND description_score = 100;
-
 @column family_id                    Internal unique ID
 @column stable_id                    Stable family ID. NOTE: stable_id are currently not stable. We are working in getting IDs stable between releases.
 @column version                      Version of the stable_id (may only change when members migrate between this family and another one; stays the same otherwise)
@@ -1415,9 +1412,6 @@ CREATE TABLE family (
 @table family_member
 @desc  This table contains the proteins corresponding to protein family relationship found. There are several family_member entries for each family entry
 @colour   #BC5CEC
-
-@example    The following query refers to the members of the protein family PTHR12675. The proteins can be retieved using the member_ids. The multiple alignment can be restored using the cigar_lines.
-    @sql    SELECT family_member.* FROM family_member JOIN family USING (family_id) WHERE stable_id = "PTHR12675";
 
 @column family_id      External reference to family_id in the @link family table
 @column seq_member_id  External reference to the seq_member_id in the @link seq_member table
@@ -1918,7 +1912,7 @@ CREATE TABLE hmm_curated_annot (
 @column is_high_confidence             Whether the homology is considered "high-confidence"
 
 @example    See species_names that participate in this particular homology entry
-    @sql    SELECT homology_id, description, GROUP_CONCAT(genome_db.name) AS species FROM homology JOIN method_link_species_set USING (method_link_species_set_id) JOIN species_set USING (species_set_id) JOIN genome_db USING(genome_db_id) WHERE method_link_id=201 AND homology_id<5000000  GROUP BY homology_id LIMIT 4;
+    @sql    SELECT homology_id, description, GROUP_CONCAT(genome_db.name) AS species FROM homology JOIN method_link_species_set USING (method_link_species_set_id) JOIN species_set USING (species_set_id) JOIN genome_db USING(genome_db_id) WHERE method_link_id=201 AND homology_id<200000000  GROUP BY homology_id LIMIT 4;
 
 @see homology_member
 @see method_link_species_set
