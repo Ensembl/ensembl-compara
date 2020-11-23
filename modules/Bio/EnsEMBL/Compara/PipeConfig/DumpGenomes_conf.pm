69c32b90527a6c3216dbde2f35fce829c420b931
Matthieu Muffato <muffato@ebi.ac.uk>
2020-11-23 13:48:09 +0000

    Typo
    Co-authored-by: CristiGuijarro <cristig@ebi.ac.uk>

--- modules/Bio/EnsEMBL/Compara/PipeConfig/DumpGenomes_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/DumpGenomes_conf.pm
@@ -132,7 +132,7 @@ sub pipeline_analyses {
             },
         },
 
-        # NOTE: DumpUnmaskedGenomeSequence creates two files (soft- and hard-masked),
+        # NOTE: DumpMaskedGenomeSequence creates two files (soft- and hard-masked),
         # so dataflows on branch #2, whereas DumpUnmaskedGenomeSequence creates a single
         # file and is allowed to amend the dataflow on branch #1
 
