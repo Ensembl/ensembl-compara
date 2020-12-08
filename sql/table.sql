55a7b819ee071a4ced9e54bfe59752a7cddbfc42
Carla Cummins <carlac@ebi.ac.uk>
2020-12-08 12:52:47 +0000

    Bump version to 105

--- sql/table.sql
+++ sql/table.sql
@@ -2276,15 +2276,11 @@ CREATE TABLE `CAFE_species_gene` (
 
 -- Add schema version to database
 DELETE FROM meta WHERE meta_key='schema_version';
-INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'schema_version', '104');
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'schema_version', '105');
 -- Add schema type to database
 DELETE FROM meta WHERE meta_key='schema_type';
 INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'schema_type', 'compara');
 
 # Patch identifier
 INSERT INTO meta (species_id, meta_key, meta_value)
-  VALUES (NULL, 'patch', 'patch_103_104_a.sql|schema_version');
-INSERT INTO meta (species_id, meta_key, meta_value)
-  VALUES (NULL, 'patch', 'patch_103_104_b.sql|dnafrag_alt_region');
-INSERT INTO meta (species_id, meta_key, meta_value)
-  VALUES (NULL, 'patch', 'patch_103_104_c.sql|fix_int_types');
+  VALUES (NULL, 'patch', 'patch_104_105_a.sql|schema_version');
