9ce13fad39184109525e274b3436caa55c16d29c
Matthieu Muffato <muffato@ebi.ac.uk>
2020-12-10 16:18:44 +0000

    Throw a clearer error message instead of letting Perl die because of an undefined value

--- scripts/pipeline/populate_new_database.pl
+++ scripts/pipeline/populate_new_database.pl
@@ -467,6 +467,7 @@ sub get_all_default_genome_dbs {
         unless ($method_link_species_set_adaptor);
     foreach my $this_mlss_id (@$mlss_ids) {
       my $this_mlss = $method_link_species_set_adaptor->fetch_by_dbID($this_mlss_id);
+      throw("mlss_id $this_mlss_id could not be found in ".$compara_dba->dbc->dbname) unless $this_mlss;
       foreach my $this_genome_db (@{$this_mlss->species_set->genome_dbs}) {
         $all_species->{$this_genome_db->dbID} = $this_genome_db;
         # Copy also the components of polyploid genomes
