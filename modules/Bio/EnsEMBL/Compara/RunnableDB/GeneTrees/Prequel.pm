6174fd6d8f42d5c59a90e62107ce766cdb44d092
Matthieu Muffato <muffato@ebi.ac.uk>
2020-12-15 10:31:04 +0000

    Up-to-date POD

--- modules/Bio/EnsEMBL/Compara/RunnableDB/GeneTrees/Prequel.pm
+++ modules/Bio/EnsEMBL/Compara/RunnableDB/GeneTrees/Prequel.pm
@@ -1,40 +1,33 @@
-#
-# You may distribute this module under the same terms as perl itself
-#
-# POD documentation - main docs before the code
+=head1 LICENSE
 
-=pod 
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+Copyright [2016-2020] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 
 =head1 NAME
 
 Bio::EnsEMBL::Compara::RunnableDB::GeneTrees::Prequel
-
-
-=head1 SYNOPSIS
-
-
 
 =head1 DESCRIPTION
 
 This Analysis/RunnableDB is designed to calculate ancestral sequences for a given gene tree
 via the PHAST's prequel tool (http://compgen.bscb.cornell.edu/phast/)
 
-
-=head1 CONTACT
-
-  Please email comments or questions to the public Ensembl
-  developers list at <dev@ensembl.org>.
-
-  Questions may also be sent to the Ensembl help desk at
-  <helpdesk@ensembl.org>.
-
-
-
 =head1 APPENDIX
 
 The rest of the documentation details each of the object methods. 
 Internal methods are usually preceded with a _
-
 
 =cut
 
