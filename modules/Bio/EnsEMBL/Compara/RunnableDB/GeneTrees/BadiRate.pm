546a7d5267d2d41ecbcede22f78693d2a42f401f
Matthieu Muffato <muffato@ebi.ac.uk>
2020-12-15 10:31:04 +0000

    Up-to-date POD

--- modules/Bio/EnsEMBL/Compara/RunnableDB/GeneTrees/BadiRate.pm
+++ modules/Bio/EnsEMBL/Compara/RunnableDB/GeneTrees/BadiRate.pm
@@ -1,22 +1,19 @@
 =head1 LICENSE
 
-  Copyright (c) 1999-2012 The European Bioinformatics Institute and
-  Genome Research Limited.  All rights reserved.
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+Copyright [2016-2020] EMBL-European Bioinformatics Institute
 
-  This software is distributed under a modified Apache license.
-  For license details, please see
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-    http://www.ensembl.org/info/about/code_licence.html
+     http://www.apache.org/licenses/LICENSE-2.0
 
-=head1 CONTACT
-
-  Please email comments or questions to the public Ensembl
-  developers list at <dev@ensembl.org>.
-
-  Questions may also be sent to the Ensembl help desk at
-  <helpdesk@ensembl.org>.
-
-=cut
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 
 =head1 NAME
 
@@ -41,6 +38,8 @@ Internal methods are usually preceded with an underscore (_)
 package Bio::EnsEMBL::Compara::RunnableDB::GeneTrees::BadiRate;
 
 use strict;
+use warnings;
+
 use Data::Dumper;
 use Bio::EnsEMBL::Compara::Graph::NewickParser;
 use base ('Bio::EnsEMBL::Compara::RunnableDB::GeneTrees::GeneGainLossCommon', 'Bio::EnsEMBL::Compara::RunnableDB::BaseRunnable');
