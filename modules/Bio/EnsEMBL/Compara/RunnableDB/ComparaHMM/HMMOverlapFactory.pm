237bfa6b21fa9425bcb80acae6c42895c2f145b1
Matthieu Muffato <muffato@ebi.ac.uk>
2020-12-18 10:55:27 +0000

    Added the license and a POD

--- modules/Bio/EnsEMBL/Compara/RunnableDB/ComparaHMM/HMMOverlapFactory.pm
+++ modules/Bio/EnsEMBL/Compara/RunnableDB/ComparaHMM/HMMOverlapFactory.pm
@@ -1,3 +1,35 @@
+=head1 LICENSE
+
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+Copyright [2016-2020] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+=head1 NAME
+
+Bio::EnsEMBL::Compara::RunnableDB::ComparaHMM::HMMOverlapFactory
+
+=head1 DESCRIPTION
+
+Runnable to create jobs that will recursively parse the output files and
+compute overlap between clusters.
+"create_jobs" and "rec_store_jobs" use the internal eHive API to create
+jobs and semaphores in an optimal way (no further factories or Dummy jobs
+needed).
+
+=cut
 
 package Bio::EnsEMBL::Compara::RunnableDB::ComparaHMM::HMMOverlapFactory;
 
