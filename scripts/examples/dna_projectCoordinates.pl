f56acdfac0120e7143ec06a173695e57310eb9a1
Matthieu Muffato <muffato@ebi.ac.uk>
2020-12-29 12:45:43 +0000

    Renamed "quer" to "query" and "reference" to "target"

--- scripts/examples/dna_projectCoordinates.pl
+++ scripts/examples/dna_projectCoordinates.pl
@@ -27,9 +27,9 @@ use Bio::AlignIO;
 use Bio::EnsEMBL::Utils::Exception qw(throw);
 
 # input and output
-my $quer_name;
-my $quer_bed;
-my $ref_name;
+my $query_name;
+my $query_bed;
+my $target_name;
 my $out;
 
 # global variables
@@ -40,39 +40,39 @@ my $slice_adaptor;
 
 
 ######################## usage and options ###############################
-my $USAGE=("Given input bed files for a query species, will project the query species' genomic coordinates to a chosen reference genome.
+my $USAGE=("Given input bed files for a query species, will project the query species' genomic coordinates to a chosen target genome.
     Valid species names are Ensembl database species names (eg. homo_sapiens).
     Takes into account only 1-to-1 pairwise alignments using the EPO alignment.
 
     Output file is a bed file of projected positions, and a column corresponding to original query positions:
-    referenceSpecies_projected_chromosome\treferenceSpecies_projected_start\treferenceSpecies_projected_end\tquerySpecies_position
+    targetSpecies_projected_chromosome\treferenceSpecies_projected_start\treferenceSpecies_projected_end\tquerySpecies_position
 
     where querSpecies_position is in format chromosome:start-end
 
     Run with following options:
-    scripts/examples/dna_projectCoordinates.pl -quer_name query_species -quer_bed query.bed -ref_name reference_species -out out_file\n\n");
+    scripts/examples/dna_projectCoordinates.pl -query_name query_species -query_bed query.bed -target_name target_species -out out_file\n\n");
 
-GetOptions('quer_name=s' => \$quer_name, 'quer_bed=s' => \$quer_bed,
-    'ref_name=s' => \$ref_name, 'out=s' => \$out);
+GetOptions('query_name=s' => \$query_name, 'query_bed=s' => \$query_bed,
+    'target_name=s' => \$target_name, 'out=s' => \$out);
 
-die $USAGE unless ($quer_name and $quer_bed and $ref_name and $out);
+die $USAGE unless ($query_name and $query_bed and $target_name and $out);
 
 
 
 ########################### subroutines ##########################
  # get the number of alignments in restricted genomic block
  # arg0: query species name
- # arg1: reference species name
+ # arg1: target species name
  # arg2: query species slice
  # returns the genomic align blocks
 
 sub blocks {
     my $query = $_[0];
-    my $reference = $_[1];
+    my $target = $_[1];
     my $slice = $_[2];
 
     my $querydb = $genomedb_adaptor->fetch_by_name_assembly($query);
-    my $referencedb = $genomedb_adaptor->fetch_by_name_assembly($reference);
+    my $targetdb = $genomedb_adaptor->fetch_by_name_assembly($target);
     my $alignment = $methodlink_adaptor->fetch_by_method_link_type_species_set_name("EPO", "mammals");
 
     my $genomicalign_block = $genomicalign_adaptor->fetch_all_by_MethodLinkSpeciesSet_Slice( $alignment, $slice);
@@ -100,21 +100,21 @@ $genomedb_adaptor = $registry->get_adaptor("Multi", "Compara", "GenomeDB");
 $methodlink_adaptor = $registry->get_adaptor("Multi", "Compara", "MethodLinkSpeciesSet");
 
 
-print "Projecting query species: ", $quer_name, "\tto reference: ",
-    $ref_name, "\n\tquery file: ", $quer_bed, "\n";
+print "Projecting query species: ", $query_name, "\tto target: ",
+    $target_name, "\n\tquery file: ", $query_bed, "\n";
 
 open (my $out_fh, '>', $out) or die "Cannot make outfile: $out\n";
 print "\tmaking output: $out\n\n";
 
-# make header for output file, including reference (projected) and query  positions
-print $out_fh "#", $ref_name,"_projected_chr\t", $ref_name, "_projected_start\t",
-$ref_name, "_projected_end\t", $quer_name, "_position\n";
+# make header for output file, including target (projected) and query  positions
+print $out_fh "#", $target_name,"_projected_chr\t", $target_name, "_projected_start\t",
+$target_name, "_projected_end\t", $query_name, "_position\n";
 
 # open in bed file
-open (my $in_fh, '<', $quer_bed) or die "cannot open $quer_bed\n";
+open (my $in_fh, '<', $query_bed) or die "cannot open $query_bed\n";
 
  # get slice adaptor using query species
-$slice_adaptor = Bio::EnsEMBL::Registry->get_adaptor($quer_name, "Core", "Slice");
+$slice_adaptor = Bio::EnsEMBL::Registry->get_adaptor($query_name, "Core", "Slice");
 
 while (<$in_fh>) {
     chomp;
@@ -126,7 +126,7 @@ while (<$in_fh>) {
     throw("No Slice can be created with coordinates $col[0]:$col[1]-$col[2]") if (!$query_slice);
 
     # pass to subroutine to get all possible alignment blocks
-    my $all_blocks = blocks($quer_name,$ref_name,$query_slice);
+    my $all_blocks = blocks($query_name,$target_name,$query_slice);
 
     # check that there is only one alignment (block)
     if ( scalar @$all_blocks == 1 ) {
@@ -145,10 +145,10 @@ while (<$in_fh>) {
             # go thorugh the alignments
             foreach my $align (@$align_list) {
                 next if (!$align);
-                #get the alignment from reference species
-                if ($align->genome_db()->name() eq $ref_name) {
+                #get the alignment from target species
+                if ($align->genome_db()->name() eq $target_name) {
 
-                    # print out reference (projected) and query coordinates
+                    # print out target (projected) and query coordinates
                     print $out_fh $align->dnafrag->name(), "\t", $align->dnafrag_start(),
                     "\t", $align->dnafrag_end(), "\t",
                     $query_slice->seq_region_name(), ":",
