41617b6f44a579407267632d5e9eebdc107b116e
Ivana Pilizota <ivana@ebi.ac.uk>
2021-01-05 14:52:59 +0000

    Plant registry 104: correct cores for species that overlap with metazoa

--- conf/plants/production_reg_conf.pl
+++ conf/plants/production_reg_conf.pl
@@ -51,6 +51,15 @@ Bio::EnsEMBL::Registry->load_registry_from_url("mysql://ensro\@mysql-ens-vertann
 # before loading the Vertebrates version
 #Bio::EnsEMBL::Registry->load_registry_from_url("mysql://ensro\@mysql-ens-sta-1:4519/$curr_release");
 
+# Ensure we're using the correct cores for species that overlap with metazoa
+my @metazoa_overlap_species = qw(drosophila_melanogaster caenorhabditis_elegans);
+Bio::EnsEMBL::Compara::Utils::Registry::remove_species(\@metazoa_overlap_species);
+my $metazoa_overlap_cores = {
+    'drosophila_melanogaster' => [ 'mysql-ens-vertannot-staging', "drosophila_melanogaster_core_104_9" ],
+    'caenorhabditis_elegans'  => [ 'mysql-ens-vertannot-staging', "caenorhabditis_elegans_core_104_269" ],
+};
+Bio::EnsEMBL::Compara::Utils::Registry::add_core_dbas( $metazoa_overlap_cores );
+
 # ---------------------- PREVIOUS CORE DATABASES---------------------------------
 
 # previous release core databases will be required by PrepareMasterDatabaseForRelease and LoadMembers only
