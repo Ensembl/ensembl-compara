b0deb3edcc8259cb06f50203c493c663155797b1
Andrey Azov <andrey@ebi.ac.uk>
2021-01-13 17:17:01 +0000

    Update 103 release month

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -35,8 +35,8 @@ use Sys::Hostname::Long;
 ###############################################################################
 ## Ensembl Version and release dates (these get updated every release)
 our $ENSEMBL_VERSION        = 103;            # Ensembl release number
-our $ARCHIVE_VERSION        = 'Jan2021';     # Archive site for this version
-our $ENSEMBL_RELEASE_DATE   = 'January 2021'; # As it would appear in the copyright/footer
+our $ARCHIVE_VERSION        = 'Feb2021';     # Archive site for this version
+our $ENSEMBL_RELEASE_DATE   = 'February 2021'; # As it would appear in the copyright/footer
 ###############################################################################
 
 
