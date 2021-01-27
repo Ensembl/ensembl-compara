526f0cde4c203dff2e153be859be3a3f43a23aee
Carla Cummins <carlac@ebi.ac.uk>
2021-01-27 15:47:08 +0000

    Copyright updates

--- src/python/lib/ensembl/compara/runnable/__init__.py
+++ src/python/lib/ensembl/compara/runnable/__init__.py
@@ -1,5 +1,5 @@
-# Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
-# Copyright [2016-2020] EMBL-European Bioinformatics Institute
+# See the NOTICE file distributed with this work for additional information
+# regarding copyright ownership.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,3 +12,4 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Runnable namespace"""
