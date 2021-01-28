01fc1cfd5ee2327416e462e5c835fc5d64b19720
Carla Cummins <carlac@ebi.ac.uk>
2021-01-28 16:15:11 +0000

    Ignore id_* tables in projection db too

--- modules/Bio/EnsEMBL/Compara/PipeConfig/MergeDBsIntoRelease_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/MergeDBsIntoRelease_conf.pm
@@ -97,8 +97,9 @@ sub default_options {
         # In these databases, ignore these tables
         'ignored_tables'    => {
             # Mapping 'db_alias' => Arrayref of table names
-            'ncrna_db'   => [qw(ortholog_quality id_generator id_assignments)],
-            'protein_db' => [qw(ortholog_quality id_generator id_assignments)],
+            'ncrna_db'      => [qw(ortholog_quality id_generator id_assignments)],
+            'protein_db'    => [qw(ortholog_quality id_generator id_assignments)],
+            'projection_db' => [qw(id_generator id_assignments)],
         },
 
    };
@@ -377,4 +378,3 @@ sub pipeline_analyses {
 =back
 
 =cut
-
