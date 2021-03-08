60230d9b226f9d0b5186a7fa0b8bc73e67461e95
Cristina Guijarro <cristig@ebi.ac.uk>
2021-03-08 18:17:55 +0000

    Split Datacheck pipeline into integratable parts

---
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/Parts/DataCheckFan.pm
@@ -0,0 +1,79 @@
+=head1 LICENSE
+
+See the NOTICE file distributed with this work for additional information
+regarding copyright ownership.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=head1 NAME
+
+Bio::EnsEMBL::Compara::PipeConfig::Parts::DataCheckFan
+
+=head1 DESCRIPTION
+
+    This PipeConfig part runs datachecks in parallel
+
+=cut
+
+package Bio::EnsEMBL::Compara::PipeConfig::Parts::DataCheckFan;
+
+use strict;
+use warnings;
+
+use Bio::EnsEMBL::Hive::Version 2.4;
+use Bio::EnsEMBL::Hive::PipeConfig::HiveGeneric_conf; # For WHEN and INPUT_PLUS
+
+sub pipeline_analyses_datacheck_fan {
+    my ($self) = @_;
+
+    return [
+        {
+            -logic_name        => 'datacheck_fan',
+            -module            => 'Bio::EnsEMBL::Compara::RunnableDB::DataCheckFan',
+            -analysis_capacity => 100,
+            -max_retry_count   => 0,
+            -rc_name           => '500Mb_job',
+            -flow_into         => {
+                '1'  => [ '?accu_name=results&accu_address=[]' ],
+                '-1' => [ 'datacheck_fan_high_mem' ],
+                '2'  => [ 'jira_ticket_creation' ],
+            },
+        },
+
+        {
+            -logic_name        => 'datacheck_fan_high_mem',
+            -module            => 'Bio::EnsEMBL::Compara::RunnableDB::DataCheckFan',
+            -analysis_capacity => 100,
+            -max_retry_count   => 0,
+            -rc_name           => '8Gb_job',
+            -flow_into         => {
+                '1' => [ '?accu_name=results&accu_address=[]' ],
+                '2' => [ 'jira_ticket_creation' ],
+            },
+        },
+
+        {
+            -logic_name        => 'jira_ticket_creation',
+            -module            => 'Bio::EnsEMBL::Compara::RunnableDB::CreateDCJiraTickets',
+            -parameters        => {
+                release                      => '#ensembl_release#',
+                update                       => 1,
+                create_datacheck_tickets_exe => $self->o('create_datacheck_tickets_exe'),
+            },
+            -analysis_capacity => 1,
+            -max_retry_count   => 0,
+        },
+    ];
+}
+
+1;
