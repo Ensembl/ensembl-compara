95bbfb7c56e349a2d1b0f2a359f2901748476f6b
Thomas Walsh <twalsh@ebi.ac.uk>
2021-06-09 09:43:58 +0100

    Add option to input source region BED file

--- scripts/hal_alignment/hal_gene_liftover.py
+++ scripts/hal_alignment/hal_gene_liftover.py
@@ -20,7 +20,12 @@
 Examples:
     # Do a liftover from GRCh38 to CHM13 of the human INS gene
     # along with 5 kb upstream and downstream flanking regions.
-    python hal_gene_liftover.py --region chr11:2159779-2161221:-1 \
+    python hal_gene_liftover.py --src-region chr11:2159779-2161221:-1 \
+        --flank 5000 input.hal GRCh38 CHM13 output.psl
+
+    # Do a liftover from GRCh38 to CHM13 of the
+    # features specified in an input BED file.
+    python hal_gene_liftover.py --src-bed-file input.bed \
         --flank 5000 input.hal GRCh38 CHM13 output.psl
 """
 
@@ -29,17 +34,46 @@ from pathlib import Path
 import re
 from subprocess import PIPE, Popen
 from tempfile import TemporaryDirectory
-from typing import Tuple
+from typing import Iterable, NamedTuple, Union
+
+import pybedtools  # type: ignore
 
 
-def parse_region(region: str) -> Tuple[str, int, int, str]:
+class SimpleRegion(NamedTuple):
+    """A simple region."""
+    chrom: str
+    start: int
+    end: int
+    strand: str
+
+
+def make_src_region_file(regions: Iterable[Union[pybedtools.cbedtools.Interval, SimpleRegion]],
+                         bed_file: Union[Path, str], flank_length: int = 0):
+    """Make source region file.
+
+    Args:
+        regions: Regions to write to output file.
+        bed_file: Path of BED file to output.
+        flank_length: Length of upstream/downstream flanking regions to request.
+    """
+    with open(bed_file, 'w') as f:
+        name = '.'
+        score = 0  # halLiftover requires an integer score in BED input
+        for region in regions:
+            flanked_start = region.start - flank_length
+            flanked_end = region.end + flank_length
+            fields = [region.chrom, flanked_start, flanked_end, name, score, region.strand]
+            print('\t'.join(str(x) for x in fields), file=f)
+
+
+def parse_region(region: str) -> SimpleRegion:
     """Parse a region string.
 
     Args:
         region: Region string.
 
     Returns:
-        A tuple of parsed region elements.
+        A SimpleRegion object.
 
     Raises:
         ValueError: If `region` is an invalid region string.
@@ -64,7 +98,7 @@ def parse_region(region: str) -> Tuple[str, int, int, str]:
     if region_start >= region_end:
         raise ValueError(f"region '{region}' has inverted/empty interval")
 
-    return region_chrom, region_start, region_end, region_strand
+    return SimpleRegion(region_chrom, region_start, region_end, region_strand)
 
 
 if __name__ == '__main__':
@@ -74,7 +108,11 @@ if __name__ == '__main__':
     parser.add_argument('src_genome', help="Source genome name.")
     parser.add_argument('dest_genome', help="Destination genome name.")
     parser.add_argument('output_file', help="Output file.")
-    parser.add_argument('--region', required=True, help="Region to liftover.")
+
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--src-region', help="Region to liftover.")
+    group.add_argument('--src-bed-file', help="BED file containing regions to liftover.")
+
     parser.add_argument('--flank', default=0, type=int,
                         help="Requested length of upstream/downstream"
                              " flanking regions to include in query.")
@@ -85,32 +123,27 @@ if __name__ == '__main__':
     src_genome = args.src_genome
     dest_genome = args.dest_genome
     output_file = args.output_file
-    region_string = args.region
-    flank_length = args.flank
+    src_region = args.src_region
+    src_bed_file = args.src_bed_file
+    flank = args.flank
 
 
-    if flank_length < 0:
-        raise ValueError(f'invalid flank length: {flank_length}')
+    if flank < 0:
+        raise ValueError(f'invalid flank length: {flank}')
 
     with TemporaryDirectory() as tmp_dir_name:
         tmp_dir = Path(tmp_dir_name)
 
-        src_bed_file = tmp_dir / 'src_regions.bed'
+        query_bed_file = tmp_dir / 'src_regions.bed'
 
-		# TODO: validate chromosome coords
-        chrom, chrom_start, chrom_end, strand = parse_region(region_string)
-        
-        # TODO: clip flanked region at chromosome ends
-        flanked_start = chrom_start - flank_length
-        flanked_end = chrom_end + flank_length
+        if src_region is not None:
+            src_regions = [parse_region(src_region)]
+        else:  # i.e. bed_file is not None
+            src_regions = pybedtools.BedTool(src_bed_file)
 
-        with open(src_bed_file, 'w') as f:
-            name = '.'
-            score = 0  # halLiftover requires an integer score in BED input
-            fields = [chrom, flanked_start, flanked_end, name, score, strand]
-            print('\t'.join(str(x) for x in fields), file=f)
+        make_src_region_file(src_regions, query_bed_file, flank_length=flank)
 
-        cmd1 = ['halLiftover', '--outPSL', hal_file, src_genome, src_bed_file, dest_genome,
+        cmd1 = ['halLiftover', '--outPSL', hal_file, src_genome, query_bed_file, dest_genome,
                 'stdout']
         cmd2 = ['pslPosTarget', 'stdin', output_file]
         p1 = Popen(cmd1, stdout=PIPE)
