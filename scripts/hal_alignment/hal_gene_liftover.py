fcbe3ab2a1d7556818013e957522b1fdea43ba69
twalsh-ebi <84317604+twalsh-ebi@users.noreply.github.com>
2021-06-14 15:26:37 +0100

    Update license notice
    Co-authored-by: J. Alvarez-Jarreta <jalvarez@ebi.ac.uk>

--- scripts/hal_alignment/hal_gene_liftover.py
+++ scripts/hal_alignment/hal_gene_liftover.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
-# Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
-# Copyright [2016-2020] EMBL-European Bioinformatics Institute
+# See the NOTICE file distributed with this work for additional information
+# regarding copyright ownership.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
