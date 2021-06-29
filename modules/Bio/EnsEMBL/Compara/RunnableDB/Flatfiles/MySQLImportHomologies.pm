162b306855347c2d739b6f896e8f83cd2ede2790
Cristina Guijarro <cristig@ebi.ac.uk>
2021-06-29 09:11:41 +0100

    Rewrite print to suppress warnings

--- modules/Bio/EnsEMBL/Compara/RunnableDB/Flatfiles/MySQLImportHomologies.pm
+++ modules/Bio/EnsEMBL/Compara/RunnableDB/Flatfiles/MySQLImportHomologies.pm
@@ -152,7 +152,8 @@ sub write_output {
         # Check what has happened
         if ($command->err =~ /Lock wait timeout exceeded/) {
             # Try importing the data again but in replace mode, just in case some rows were half-copied
-            print "Received 'Lock wait timeout exceeded'. Retrying...\n" if $self->{debug};
+            my $desc = $hc_passed ? "Received 'Lock wait timeout exceeded'." : "The imported data appeared corrupted.";
+            print $desc . " Retrying...\n" if $self->debug;
             if (! $replace) {
                 $import_query =~ s/  INTO / REPLACE INTO /g;
                 $replace = 1;
