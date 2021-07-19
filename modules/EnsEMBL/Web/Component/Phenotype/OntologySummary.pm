ec51aecaa8c3f237e9b48b21847910e5ad8e5c49
Jyothish <joyjoy.nt@gmail.com>
2021-07-19 11:41:08 +0100

    Yearly copyright update (#840)

--- modules/EnsEMBL/Web/Component/Phenotype/OntologySummary.pm
+++ modules/EnsEMBL/Web/Component/Phenotype/OntologySummary.pm
@@ -1,5 +1,5 @@
 =head1 LICENSE
-Copyright [2017-2018] EMBL-European Bioinformatics Institute
+Copyright [2017-2021] EMBL-European Bioinformatics Institute
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
