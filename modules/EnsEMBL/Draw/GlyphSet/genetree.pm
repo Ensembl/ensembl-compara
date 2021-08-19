Ensembl/ensembl-compara

commit 34e718b92bdabe316cc3636c66ab0c20f8ad141d

Rationalisation of species meta keys - ENSINT-507

modules/EnsEMBL/Draw/GlyphSet/genetree.pm
@@ -767,7 +767,7 @@ sub features {
       $f->{'_species'} = $self->species_defs->production_name_mapping($tree->genome_db->name); # This will be used in URLs
 
       # This will be used for display
-      $f->{'_species_label'} = $self->species_defs->get_config($f->{'_species'}, 'DISPLAY_NAME') || $self->species_defs->species_label($f->{'_species'}) || $f->{'_species'}; 
+      $f->{'_species_label'} = $self->species_defs->get_config($f->{'_species'}, 'GROUP_DISPLAY_NAME') || $self->species_defs->species_label($f->{'_species'}) || $f->{'_species'}; 
       $f->{'_genome_dbs'} ||= {};
       $f->{'_genome_dbs'}->{$tree->genome_db->dbID}++;
     }
@@ -845,7 +845,7 @@ sub features {
     if ($cigar_line =~ /M/) {
       $f->{'_cigar_line'} = $cigar_line;
     }
-    $f->{'label'} = $self->species_defs->get_config($self->species_defs->production_name_mapping($name), 'SPECIES_COMMON_NAME');
+    $f->{'label'} = $self->species_defs->get_config($self->species_defs->production_name_mapping($name), 'SPECIES_DISPLAY_NAME');
     if ($low_coverage_species && $low_coverage_species->{$genomic_align->genome_db->dbID}) {
      $f->{'_gat'}{'colour'} = 'brown';
     }

modules/EnsEMBL/Draw/GlyphSet.pm
@@ -657,10 +657,7 @@ sub init_label {
   }
  
   my $ch = $self->my_config('caption_height') || 0;
-  my $tooltip = sprintf '%s (%s)',
-                $config->species_defs->get_config($config->species, 'SPECIES_COMMON_NAME'),
-                $config->species_defs->get_config($config->species, 'SPECIES_SCIENTIFIC_NAME');
-
+  my $tooltip = $config->species_defs->get_config($config->species, 'SPECIES_DISPLAY_NAME'),
 
   $self->label($self->Text({
     text      => $text,

modules/EnsEMBL/Draw/GlyphSet/alignscalebar.pm
@@ -201,8 +201,8 @@ sub render_align_bar {
     my $ref_species   = $map->{$species};
     my $other_species = $map->{$s2sp}; 
 
-    my $ref_species_common_name = lc $species_defs->get_config($ref_species, 'SPECIES_COMMON_NAME');
-    my $other_species_common_name = lc $species_defs->get_config($other_species, 'SPECIES_COMMON_NAME') || '';
+    my $ref_species_common_name = lc $species_defs->get_config($ref_species, 'SPECIES_DISPLAY_NAME');
+    my $other_species_common_name = lc $species_defs->get_config($other_species, 'SPECIES_DISPLAY_NAME') || '';
     
     # This happens when we have two slices following each other
     if (defined $last_end and ($last_end <= $ss - 1)) {

modules/EnsEMBL/Web/Command/Info/CheckGallery.pm
@@ -61,7 +61,7 @@ sub process {
   $url_params->{$core_params->{$data_type}} =  $id; 
 
   my $error;
-  my $common_name = $hub->species_defs->get_config($species, 'SPECIES_COMMON_NAME');
+  my $display_name = $hub->species_defs->get_config($species, 'SPECIES_DISPLAY_NAME');
 
   my $builder   = $hub->{'_builder'};
   ## Don't use the create_factory method, as its error-handling 
@@ -84,7 +84,7 @@ sub process {
   else {
     my $object = $factory->object;
     unless ($object) {
-      $error = sprintf('%s "%s" could not be found in species %s. Please try again.', $data_type, $id, $common_name);
+      $error = sprintf('%s "%s" could not be found in species %s. Please try again.', $data_type, $id, $display_name);
     }
   }
 

modules/EnsEMBL/Web/Component/Compara_AlignSliceSelector.pm
@@ -110,7 +110,7 @@ sub content {
 sub getLabelHtml {
   my $self = shift;
   my $species = shift;
-  my $species_label = $self->hub->species_defs->get_config($species, 'SPECIES_COMMON_NAME');
+  my $species_label = $self->hub->species_defs->get_config($species, 'SPECIES_DISPLAY_NAME');
   my $img_url = $self->hub->species_defs->ENSEMBL_IMAGE_ROOT . '/species/' . $self->hub->species_defs->IMAGE_PATH;
   my $species_img = sprintf '<img class="nosprite" src="%s.png">', $img_url;
   my $common_name = '';

modules/EnsEMBL/Web/Component/Compara_Alignments.pm
@@ -247,7 +247,7 @@ sub _get_sequence {
     display_width   => $self->param('display_width'),
     site_type       => ucfirst lc $species_defs->ENSEMBL_SITETYPE || 'Ensembl',
     species         => $hub->species,
-    display_species => $species_defs->SPECIES_COMMON_NAME,
+    display_species => $species_defs->SPECIES_DISPLAY_NAME,
     comparison      => 1,
     ambiguity       => 1,
     db              => $object->can('get_db') ? $object->get_db : 'core',

modules/EnsEMBL/Web/Component/DataExport/Alignments.pm
@@ -82,9 +82,9 @@ sub default_file_name {
     if ($align) {
       my $align_name;
       if ($align->{'class'} =~ /pairwise/) {
-        $name = $species_defs->SPECIES_COMMON_NAME;
+        $name = $species_defs->SPECIES_DISPLAY_NAME;
         my ($other_species) = grep { $_ ne $self->hub->species } keys %{$align->{'species'}};
-        $name .= '_'.$species_defs->get_config($other_species, 'SPECIES_COMMON_NAME');
+        $name .= '_'.$species_defs->get_config($other_species, 'SPECIES_DISPLAY_NAME');
         my $type = lc($align->{'type'});
         $type =~ s/_net//;
         $name .= '_'.$type;

modules/EnsEMBL/Web/Component/DataExport/Homologs.pm
@@ -60,7 +60,7 @@ sub content {
 
 sub default_file_name {
   my $self = shift;
-  my $name = $self->hub->species_defs->SPECIES_COMMON_NAME;
+  (my $name = $self->hub->species_defs->SPECIES_DISPLAY_NAME) =~ s/ /_/g;
 
   $name .= '_'.$self->hub->param('gene_name').'_ortholog_alignment';
   return $name;

commit 9730717da6244efc06e8d49a0898bd9b31260ed7

Fix bug introduced in commit 34e718b9

modules/EnsEMBL/Draw/GlyphSet/genetree.pm
@@ -767,7 +767,7 @@ sub features {
       $f->{'_species'} = $self->species_defs->production_name_mapping($tree->genome_db->name); # This will be used in URLs
 
       # This will be used for display
-      $f->{'_species_label'} = $self->species_defs->get_config($f->{'_species'}, 'GROUP_DISPLAY_NAME') || $self->species_defs->species_label($f->{'_species'}) || $f->{'_species'}; 
+      $f->{'_species_label'} = $self->species_defs->get_config($f->{'_species'}, 'DISPLAY_NAME') || $self->species_defs->species_label($f->{'_species'}) || $f->{'_species'}; 
       $f->{'_genome_dbs'} ||= {};
       $f->{'_genome_dbs'}->{$tree->genome_db->dbID}++;
     }