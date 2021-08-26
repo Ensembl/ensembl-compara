b271b6ba17c4de631b68bd2d22f10b14b02b45fd
Anne Lyle <annelyle@ebi.ac.uk>
2021-08-26 14:09:07 +0000

    Fix missing bracket

--- modules/EnsEMBL/Web/SpeciesDefs.pm
+++ modules/EnsEMBL/Web/SpeciesDefs.pm
@@ -1580,8 +1580,9 @@ sub production_name_mapping {
   
   foreach ($self->valid_species) {
     if ($self->get_config($_, 'SPECIES_PRODUCTION_NAME') eq lc($production_name)) {
-    $mapping_name = $self->get_config($_, 'SPECIES_URL');
-    last;
+      $mapping_name = $self->get_config($_, 'SPECIES_URL');
+      last;
+    }
   }
 
   return $mapping_name;
