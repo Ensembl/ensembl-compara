63bbfdbc8a73b7a8f014f05e4ba8a1330f916b66
Thiago Genez <thiagogenez@ebi.ac.uk>
2021-09-02 17:22:17 +0100

    query gca number if missing and refactoring

--- scripts/dumps/dump_genome_from_core.py
+++ scripts/dumps/dump_genome_from_core.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python3
-
-import fileinput
+""" Wrapper of dump_genome_from_core.pl to dump a list of FASTA file
+"""
 import argparse
 import subprocess
 import os
-
+import sys
 
 try:
     import yaml
+    from yaml.loader import SafeLoader
 except ModuleNotFoundError as err:
     # Error handling
     print(err)
     print('Please, run "pip install PyYAML" to install PyYAML module')
-    exit(1)
-
-from yaml.loader import SafeLoader
 
 
 def subprocess_call(command, work_dir=None, shell=False, ibsub=False):
@@ -27,7 +25,7 @@ def subprocess_call(command, work_dir=None, shell=False, ibsub=False):
         ibsub: if ibsub is True, the command will be called via ibsub
 
     Returns:
-        The subprocess output
+        The subprocess output or None otherwise
 
     """
 
@@ -36,28 +34,29 @@ def subprocess_call(command, work_dir=None, shell=False, ibsub=False):
 
     call = command
     print("Running: {}".format(" ".join(call)))
-    process = subprocess.Popen(
+    with subprocess.Popen(
         call,
         shell=shell,
         encoding="ascii",
         cwd=work_dir,
         stdout=subprocess.PIPE,
         universal_newlines=True,
-    )
+    ) as process:
 
-    output, stderr = process.communicate()
+        output, stderr = process.communicate()
 
-    process.wait()
-    if process.returncode != 0:
-        out = "stdout={}".format(output)
-        out += ", stderr={}".format(stderr)
-        raise RuntimeError(
-            "Command {} exited {}: {}".format(call, process.returncode, out)
-        )
-    else:
+        process.wait()
+        if process.returncode != 0:
+            out = "stdout={}".format(output)
+            out += ", stderr={}".format(stderr)
+            raise RuntimeError(
+                "Command {} exited {}: {}".format(call, process.returncode, out)
+            )
         print("Successfully ran: {}".format(" ".join(call)))
 
-    return output.strip()
+        return output.strip()
+
+    return None
 
 
 def download_file(host, port, core_db, fasta_filename, mask="soft"):
@@ -97,7 +96,7 @@ def download_file(host, port, core_db, fasta_filename, mask="soft"):
     return subprocess_call(command=perl_call, ibsub=True, shell=False)
 
 
-def get_name(host, core_db):
+def query_coredb(host, core_db, query):
     """Get the correct meta production name
 
     Args:
@@ -113,7 +112,7 @@ def get_name(host, core_db):
         "{}".format(core_db),
         "-ss",
         "-e",
-        'SELECT meta_value FROM meta WHERE meta_key="species.production_name";',
+        "{}".format(query),
     ]
     return subprocess_call(command=mysql_call)
 
@@ -133,14 +132,29 @@ def parse_yaml(file, dest):
         port = data["port"]
 
         for core_db in data["core_db"]:
-            name = get_name(host=host, core_db=core_db)
-            name = name.replace("_", ".")
-            download_file(
+            specie_name = query_coredb(
                 host=host,
-                port=port,
                 core_db=core_db,
-                fasta_filename="{}/{}.fa".format(dest, name),
+                query='SELECT meta_value FROM meta WHERE meta_key="species.production_name";',
             )
+
+            # in case gca is presented in the specie name
+            if "gca" not in specie_name:
+                gca_number = query_coredb(
+                    host=host,
+                    core_db=core_db,
+                    query='SELECT meta_value FROM meta WHERE meta_key="assembly.accession";',
+                )
+                gca_number = gca_number.replace(".", "v").lower()
+                specie_name = "{}_{}".format(specie_name, gca_number)
+
+            if specie_name is not None:
+                download_file(
+                    host=host,
+                    port=port,
+                    core_db=core_db,
+                    fasta_filename="{}/{}.fa".format(dest, specie_name),
+                )
 
 
 if __name__ == "__main__":
@@ -151,11 +165,19 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    with open(args.yaml, mode="r") as f:
+    with open(args.yaml, mode="r", encoding="utf-8") as f:
 
         if args.output is None:
             args.output = os.path.dirname(os.path.realpath(f.name))
         else:
             args.output = os.path.abspath(args.output)
+
+        if not os.path.isdir(args.output):
+            print(
+                "{} does not exist for output, please create it first".format(
+                    args.output
+                )
+            )
+            sys.exit(1)
 
         parse_yaml(file=f, dest=args.output)
