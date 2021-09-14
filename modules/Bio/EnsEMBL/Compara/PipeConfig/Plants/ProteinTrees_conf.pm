8a4ecdbfdbc90147b0fe1eb8170ee3a7471bf150
Jorge Alvarez Jarreta <jalvarez@ebi.ac.uk>
2021-09-14 12:49:43 +0100

    set 'default' collection for prot trees
    For plants, metazoa and pan. In most cases this new collection is just a
    copy of the division one, but leaves it ready for future changes as it
    has happened with vertebrates and plants.
    
    These changes eases the run of the protein trees pipelines, specially
    when some parts needs to be rerun after it has completed (before, the
    results were renamed from 'default' to the division name, having to
    rename them to 'default' before running the analyses again).

--- modules/Bio/EnsEMBL/Compara/PipeConfig/Plants/ProteinTrees_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/Plants/ProteinTrees_conf.pm
@@ -46,7 +46,6 @@ sub default_options {
         %{$self->SUPER::default_options},   # inherit the generic ones
 
     'division'   => 'plants',
-    'collection' => $self->o('division'),
 
     # clustering parameters:
         # affects 'hcluster_dump_input_per_genome'
