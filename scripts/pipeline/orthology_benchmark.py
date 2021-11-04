bb712cc26b298362c019ab5ddd75fa65a2ff2e3f
Ivana Pilizota <ivana@ebi.ac.uk>
2021-11-04 10:35:05 +0000

    Implementation of a step that dumps genomes from cores

--- scripts/pipeline/orthology_benchmark.py
+++ scripts/pipeline/orthology_benchmark.py
@@ -17,18 +17,133 @@
 
 Typical usage example::
 
-    $ python orthology_benchmark.py --mlss_conf /path/to/mlss_conf.xml \
-        --species_set default
+    $ python orthology_benchmark.py --mlss_conf /path/to/mlss_conf.xml --species_set default \
+    --host mysql-ens-compara-prod-10 --port 4648 --out_dir $HPS_HOME
 
 """
 
 import argparse
+import os
+import subprocess
+from typing import Dict, List
+
+import MySQLdb
+import numpy
 
 from ensembl.compara.config import get_species_set_by_name
 
 
-def dump_genomes():
-    """Docstring"""
+def dump_genomes(species_list: List[str], species_set_name: str, host: str, port: int,
+                 out_dir: str) -> None:
+    """Dumps canonical peptides of protein-coding genes for a specified list of species
+    from the latest available core databases to FASTA files using `dump_gene_set_from_core.pl`.
+
+    Args:
+        species_list: A list of species (genome) names to dump.
+        species_set_name: Species set (collection) name.
+        host: Database host.
+        port: Host port.
+        out_dir: Directory to place `species_set_name/core_name.fasta` dumps.
+
+    Raises:
+        FileExistsError: If directory `out_dir/species_set_name` already exists.
+        MySQLdb.OperationalError: If connection to `host:port` cannot be established.
+        RuntimeError: If no cores are found for any species in `species_list` (`species_set_name`)
+                on `host:port`.
+
+    """
+    # Check access to the host & port
+    MySQLdb.connect(host=host, port=port, user="ensro")
+
+    cores = get_core_names(species_list, host)
+    dump_cores = [core for species, core in cores.items() if core != ""]
+
+    if len(dump_cores) == 0:
+        raise RuntimeError(f"No cores found for the species set '{species_set_name}' on host '{host}'.")
+
+    dumps_dir = os.path.join(out_dir, species_set_name)
+    os.mkdir(dumps_dir)
+
+    script = os.path.join(os.environ["ENSEMBL_ROOT_DIR"], "ensembl-compara", "scripts", "dumps",
+                          "dump_gene_set_from_core.pl")
+
+    for core in dump_cores:
+        out_file = os.path.join(dumps_dir, core + ".fasta")
+        subprocess.run([script, "-core-db", core, "-host", host, "-port", str(port),
+                       "-outfile", out_file], capture_output=True, check=True)
+
+
+def find_latest_core(core_names: List[str]) -> str:
+    """Returns the latest core database among the available ones.
+
+    Args:
+        core_names: A list of cores for a species of interest.
+
+    Raises:
+        RuntimeError: If `core_names` is empty.
+
+    Returns:
+        Name of the latest core database (the latest Ensembl release, the latest version).
+
+    """
+    if len(core_names) == 0:
+        raise RuntimeError("Empty list of core databases. Cannot determine the latest one.")
+
+    rel_ver = [name.split("_core_")[1].split("_") for name in core_names]
+    rel_ver_int = [list(map(int, i)) for i in rel_ver]
+    rel_ver_arr = numpy.array(rel_ver_int)
+
+    n_cols = rel_ver_arr.shape[1]
+    i = 0
+    while i < n_cols:
+        # Find max value in the i-th column
+        # For the next iteration (i+1) consider only rows where i-th column == max value
+        max_col = numpy.amax(rel_ver_arr[:, i])
+        rows_ind = numpy.where(rel_ver_arr[:, i] == max_col)[0]
+        rel_ver_arr = rel_ver_arr[[rows_ind], :][0]
+        i += 1
+
+    latest_rel_ver = '_'.join(map(str, rel_ver_arr[0]))
+    core_name = [core for core in core_names if latest_rel_ver in core][0]
+
+    return core_name
+
+
+def get_core_names(species_names: List[str], host: str) -> Dict[str, str]:
+    """Returns the latest core database names for a list of species.
+
+    Args:
+        species_names: Species (genome) names.
+        host: Host for core databases.
+
+    Raises:
+        RuntimeError: If `species_list` is empty.
+        FileNotFoundError: If cannot connect to `host`.
+
+    Returns:
+        Dictionary mapping species (genome) names to the latest version of available core names.
+
+    """
+    if len(species_names) == 0:
+        raise RuntimeError("Empty list of species names. Cannot search for core databases.")
+
+    core_names = {}
+
+    sql = "SHOW DATABASES LIKE '%_core_%'"
+    cmd = subprocess.run([host, "-Ne", sql], capture_output=True, check=True)
+    out_decoded = cmd.stdout.decode('utf-8')
+    all_cores = out_decoded.split('\n')[:-1]
+
+    for species in species_names:
+        core_name = [core for core in all_cores if species + "_core_" in core]
+        if len(core_name) == 0:
+            core_names[species] = ""
+        elif len(core_name) == 1:
+            core_names[species] = core_name[0]
+        else:
+            core_names[species] = find_latest_core(core_name)
+
+    return core_names
 
 
 def prep_input_for_orth_tools():
@@ -52,11 +167,15 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--mlss_conf", required=True, type=str, help="Path to MLSS configuration XML file")
     parser.add_argument("--species_set", required=True, type=str, help="Species set (collection) name")
+    parser.add_argument("--host", required=True, type=str, help="Database host")
+    parser.add_argument("--port", required=True, type=int, help="Database port")
+    parser.add_argument("--out_dir", required=True, type=str, help="Location for"
+                                                                   "'species_set/core_name.fasta' dumps")
 
     args = parser.parse_args()
 
-    species_list = get_species_set_by_name(args.mlss_conf, args.species_set)
-    # dump_genomes(species_list, ...)
+    genome_list = get_species_set_by_name(args.mlss_conf, args.species_set)
+    dump_genomes(genome_list, args.species_set, args.host, args.port, args.out_dir)
     # prep_input_for_orth_tools()
     # run_orthology_tools()
     # prep_input_for_goc()
---
+++ src/python/tests/test_orthology_benchmark.py
@@ -0,0 +1,222 @@
+# See the NOTICE file distributed with this work for additional information
+# regarding copyright ownership.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Unit testing of `orthology_benchmark.py` script.
+
+Typical usage example::
+
+    $ pytest test_orthology_benchmark.py
+
+"""
+
+from contextlib import nullcontext as does_not_raise
+from importlib.abc import Loader
+from importlib.machinery import ModuleSpec
+from importlib.util import module_from_spec, spec_from_file_location
+import os
+from pathlib import Path
+import sys
+from typing import ContextManager, Dict, List
+
+import MySQLdb
+
+import pytest
+from pytest import raises
+
+from ensembl.compara.filesys import file_cmp
+
+
+script_path = Path(__file__).parents[3] / "scripts" / "pipeline" / "orthology_benchmark.py"
+script_name = script_path.stem
+script_spec = spec_from_file_location(script_name, script_path)
+
+if not isinstance(script_spec, ModuleSpec):
+    raise ImportError(f"ModuleSpec not created for module file '{script_path}'")
+if not isinstance(script_spec.loader, Loader):
+    raise ImportError(f"no loader found for module file '{script_path}'")
+
+orthology_benchmark_module = module_from_spec(script_spec)
+sys.modules[script_name] = orthology_benchmark_module
+script_spec.loader.exec_module(orthology_benchmark_module)
+
+# pylint: disable=import-error,wrong-import-position
+
+import orthology_benchmark  # type: ignore
+
+# pylint: enable=import-error,wrong-import-position
+
+
+@pytest.mark.parametrize(
+    "multi_dbs",
+    [
+        [{'src': 'core/gallus_gallus_core_99_6', 'name': 'gallus_gallus_core_99_6'},
+         {'src': 'core/homo_sapiens_core_99_38', 'name': 'homo_sapiens_core_99_38'}]
+    ],
+    indirect=True
+)
+class TestDumpGenomes:
+    """Tests :func:`orthology_benchmark.dump_genomes()` function.
+
+    Attributes:
+        core_dbs: A set of test core databases.
+
+    """
+
+    core_dbs = {} # type: Dict
+
+    # autouse=True makes this fixture be executed before any test_* method of this class, and scope='class' to
+    # execute it only once per class parametrization
+    @pytest.fixture(scope='class', autouse=True)
+    def setup(self, multi_dbs: Dict) -> None:
+        """Loads the required fixtures and values as class attributes.
+
+        Args:
+            multi_dbs: Dictionary of unit test databases (fixture).
+
+        """
+        type(self).core_dbs = multi_dbs
+
+    @pytest.mark.parametrize(
+        "species_list, species_set_name, expectation",
+        [
+            (["gallus_gallus", "homo_sapiens"], "default", does_not_raise()),
+            (["homo_sapiens", "zea_mays"], "default", raises(FileExistsError)),
+            (["felis_catus", "zea_mays"], "test", raises(RuntimeError,
+                    match=r"No cores found for the species set 'test' on host 'mysql-ens-compara-prod-1'."))
+        ]
+    )
+    def test_dump_genomes(self, species_list: List[str], species_set_name: str,
+                          tmp_dir: Path, expectation: ContextManager) -> None:
+        """Tests :func:`orthology_benchmark.dump_genomes()` function
+        when connection to the server host and port can be established.
+
+        Args:
+            species_list: A list of species (genome names).
+            species_set_name: Species set (collection) name.
+            tmp_dir: Unit test temp directory (fixture).
+            expectation: Context manager for the expected exception, i.e. the test will only pass if that
+                    exception is raised. Use :class:`~contextlib.nullcontext` if no exception is expected.
+
+        """
+        with expectation:
+            host = self.core_dbs["gallus_gallus_core_99_6"].dbc.host
+            #port = self.core_dbs["gallus_gallus_core_99_6"].dbc.port
+            port = 4485 # hardcoded until .dbc.port is merged into ensembl-py
+            orthology_benchmark.dump_genomes(species_list, species_set_name, host, port, tmp_dir)
+
+            out_files = tmp_dir / species_set_name
+            exp_out = pytest.files_dir / "orth_benchmark"
+            for db_name in self.core_dbs:
+                assert file_cmp(Path(out_files, os.environ['USER'] + "_" + db_name + ".fasta"),
+                                Path(exp_out, db_name + ".fasta"))
+
+    def test_dump_genomes_fake_server(self, tmp_dir: Path) -> None:
+        """Tests :func:`orthology_benchmark.dump_genomes()` function when provided fake server host and port.
+
+        Args:
+            tmp_dir: Unit test temp directory (fixture).
+
+        """
+        with raises(MySQLdb.OperationalError):
+            orthology_benchmark.dump_genomes(["mus_musculus", "naja_naja"], "default",
+                                             "fake-host", 666, tmp_dir)
+
+
+@pytest.mark.parametrize(
+    "core_names, exp_output, expectation",
+    [
+        (["mus_musculus_core_105_1", "mus_musculus_core_105_3", "mus_musculus_core_104_4"],
+         "mus_musculus_core_105_3", does_not_raise()),
+        ([], None, raises(RuntimeError,
+                          match=r"Empty list of core databases. Cannot determine the latest one."))
+    ]
+)
+def test_find_latest_core(core_names: List[str], exp_output: str, expectation: ContextManager) -> None:
+    """Tests :func:`orthology_benchmark.find_latest_core()` function.
+
+    Args:
+        core_names: A list of core database names.
+        exp_output: Expected return value of the function.
+        expectation: Context manager for the expected exception, i.e. the test will only pass if that
+                exception is raised. Use :class:`~contextlib.nullcontext` if no exception is expected.
+
+    """
+    with expectation:
+        assert orthology_benchmark.find_latest_core(core_names) == exp_output
+
+
+@pytest.mark.parametrize(
+    "multi_dbs",
+    [
+        [{'src': 'core/danio_rerio_core_105_11', 'name': 'danio_rerio_core_105_11'},
+         {'src': 'core/mus_musculus_cbaj_core_107_1', 'name': 'mus_musculus_cbaj_core_107_1'},
+         {'src': 'core/mus_musculus_core_106_39', 'name': 'mus_musculus_core_106_39'}]
+    ],
+    indirect=True
+)
+class TestGetCoreNames:
+    """Tests :func:`orthology_benchmark.get_core_names()` function.
+
+    Attributes:
+        core_dbs: A set of test core databases.
+
+    """
+
+    core_dbs = {} # type: Dict
+
+    # autouse=True makes this fixture be executed before any test_* method of this class, and scope='class' to
+    # execute it only once per class parametrization
+    @pytest.fixture(scope='class', autouse=True)
+    def setup(self, multi_dbs: Dict) -> None:
+        """Loads the required fixtures and values as class attributes.
+
+        Args:
+            multi_dbs: Dictionary of unit test databases (fixture).
+
+        """
+        type(self).core_dbs = multi_dbs
+
+    @pytest.mark.parametrize(
+        "species_names, exp_output, expectation",
+        [
+            (["danio_rerio", "mus_musculus", "zea_mays"],
+             {"danio_rerio": os.environ['USER'] + "_" + "danio_rerio_core_105_11",
+             "mus_musculus": os.environ['USER'] + "_" + "mus_musculus_core_106_39", "zea_mays": ""},
+             does_not_raise()),
+            ([], None, raises(RuntimeError,
+                              match=r"Empty list of species names. Cannot search for core databases."))
+        ]
+    )
+    def test_get_core_names(self, species_names: List[str], exp_output: Dict[str, str],
+                            expectation: ContextManager) -> None:
+        """Tests :func:`orthology_benchmark.get_core_names()` function
+        when connection to the server host can be established.
+
+        Args:
+            species_names: Species (genome) names.
+            exp_output: Expected return value of the function.
+            expectation: Context manager for the expected exception, i.e. the test will only pass if that
+                    exception is raised. Use :class:`~contextlib.nullcontext` if no exception is expected.
+
+        """
+        with expectation:
+            host = self.core_dbs["mus_musculus_core_106_39"].dbc.host
+            assert orthology_benchmark.get_core_names(species_names, host) == exp_output
+
+    # Not necessary for this script (it would throw an error earlier)
+    # but in case the function is used for other purposes:
+    def test_get_core_names_fake_host(self) -> None:
+        """Tests :func:`orthology_benchmark.get_core_names()` function when provided fake server host."""
+        with raises(FileNotFoundError):
+            orthology_benchmark.get_core_names(["danio_rerio", "mus_musculus"], "fake-host")
