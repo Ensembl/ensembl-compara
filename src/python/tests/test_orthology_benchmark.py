7f868a8501c13f1a2378872da9bb84984c715d19
Ivana Pilizota <ivana@ebi.ac.uk>
2021-11-23 10:57:23 +0000

    Grammar

--- src/python/tests/test_orthology_benchmark.py
+++ src/python/tests/test_orthology_benchmark.py
@@ -96,7 +96,7 @@ class TestDumpGenomes:
         type(self).username = "ensro" if server_url.username == "ensadmin" else server_url.username
 
     @pytest.mark.skipif(os.environ['USER'] == 'travis',
-                        reason="This test requires Python and Perl and this is not supported by Travis")
+                        reason="The test requires both Perl and Python which is not supported by Travis.")
     @pytest.mark.parametrize(
         "species_list, species_set_name, expectation",
         [
