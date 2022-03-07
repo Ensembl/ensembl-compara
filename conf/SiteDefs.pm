7b865d6f6479fe08a8f79cf39e42105f0c047edd
Manoj Pandian Sakthivel <manojpandian@ebi.ac.uk>
2022-03-07 17:13:52 +0000

    Update ARCHIVE_VERSION to Mar

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -35,8 +35,8 @@ use Sys::Hostname::Long;
 ###############################################################################
 ## Ensembl Version and release dates (these get updated every release)
 our $ENSEMBL_VERSION        = 106;            # Ensembl release number
-our $ARCHIVE_VERSION        = 'Feb2022';     # Archive site for this version
-our $ENSEMBL_RELEASE_DATE   = 'Feb 2022'; # As it would appear in the copyright/footer
+our $ARCHIVE_VERSION        = 'Mar2022';     # Archive site for this version
+our $ENSEMBL_RELEASE_DATE   = 'Mar 2022'; # As it would appear in the copyright/footer
 ###############################################################################
 
 
