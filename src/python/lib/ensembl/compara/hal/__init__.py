82f4cf4c5f8fc15a162ed015f24368b3701614e7
Thomas Walsh <twalsh@ebi.ac.uk>
2022-03-11 14:47:45 +0000

    Add wildcard import to ensembl.compara.hal
    Co-authored-by: J. Alvarez-Jarreta <jalvarez@ebi.ac.uk>

--- src/python/lib/ensembl/compara/hal/__init__.py
+++ src/python/lib/ensembl/compara/hal/__init__.py
@@ -13,3 +13,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Module for working with HAL alignment data."""
+
+from .maf_to_fasta import *
