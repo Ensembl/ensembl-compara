2202334dbcef469333c6f2463600d447095f2d08
twalsh-ebi <twalsh@ebi.ac.uk>
2022-05-12 08:44:06 +0100

    Add initial Fungi config and pipeconfig files (#429)
    * Add initial Fungi config and pipeconfig files
    
    * Apply feedback from PR #429

---
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/Fungi/PrepareMasterDatabaseForRelease_conf.pm
@@ -0,0 +1,60 @@
+=head1 LICENSE
+
+See the NOTICE file distributed with this work for additional information
+regarding copyright ownership.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+=head1 NAME
+
+Bio::EnsEMBL::Compara::PipeConfig::Fungi::PrepareMasterDatabaseForRelease_conf
+
+=head1 SYNOPSIS
+
+    init_pipeline.pl Bio::EnsEMBL::Compara::PipeConfig::Fungi::PrepareMasterDatabaseForRelease_conf \
+    -host mysql-ens-compara-prod-X -port XXXX
+
+=head1 DESCRIPTION
+
+Prepare Fungi master database for next release. Please, refer to the parent
+class for further information.
+
+WARNING: the previous reports and backups will be removed if the pipeline is
+initialised again for the same division and release.
+
+=cut
+
+package Bio::EnsEMBL::Compara::PipeConfig::Fungi::PrepareMasterDatabaseForRelease_conf;
+
+use strict;
+use warnings;
+
+use Bio::EnsEMBL::Hive::Version 2.4;
+use Bio::EnsEMBL::Hive::PipeConfig::HiveGeneric_conf;
+
+use base ('Bio::EnsEMBL::Compara::PipeConfig::PrepareMasterDatabaseForRelease_conf');
+
+
+sub default_options {
+    my ($self) = @_;
+
+    return {
+        %{$self->SUPER::default_options},  # inherit the generic ones
+
+        'division' => 'fungi',
+    };
+}
+
+1;
