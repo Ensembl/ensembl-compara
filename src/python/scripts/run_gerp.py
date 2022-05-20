8a0d80e2bb22ddfaf71aa52a7654f395dd328a28
dthybert <dthybert@ebi.ac.uk>
2022-05-20 12:56:19 +0100

    replace agrschema with agrparse and add constraint elements in the json files (#414)
    * replace agrschema with agrparse and add constraint elements in the json file
    
    * PR feedback - right format of comment and script description
    
    * Integrate feedback from PR and corect mypy issues
    
    * Update src/python/scripts/run_gerp.py
    
    Co-authored-by: twalsh-ebi <twalsh@ebi.ac.uk>
    
    * Update src/python/scripts/run_gerp.py
    
    docstring update
    
    Co-authored-by: twalsh-ebi <twalsh@ebi.ac.uk>
    
    * Update src/python/scripts/run_gerp.py
    
    creating typed dictionary mypy compatible
    
    Co-authored-by: twalsh-ebi <twalsh@ebi.ac.uk>
    
    * specifying ignore type to mypy
    
    * switching python 3.7 to 3.7.9 in Travis conf file
    
    * Thomas code review feedbacks
    
    * typos correction
    
    * Update src/python/tests/conftest.py
    
    Co-authored-by: twalsh-ebi <twalsh@ebi.ac.uk>
    
    * Update src/python/scripts/run_gerp.py
    
    Co-authored-by: twalsh-ebi <twalsh@ebi.ac.uk>
    
    * Removing attr-defined mypy ignore tag
    
    * typo correction
    
    * Corectiontypos
    
    * Update src/python/scripts/run_gerp.py
    
    Co-authored-by: twalsh-ebi <twalsh@ebi.ac.uk>
    
    * Update src/python/scripts/run_gerp.py
    
    Co-authored-by: twalsh-ebi <twalsh@ebi.ac.uk>
    
    * Update src/python/scripts/run_gerp.py
    
    Co-authored-by: twalsh-ebi <twalsh@ebi.ac.uk>
    
    * Update src/python/scripts/run_gerp.py
    
    Co-authored-by: twalsh-ebi <twalsh@ebi.ac.uk>
    
    Co-authored-by: David Thybert <dthybert@codon-login-02.ebi.ac.uk>
    Co-authored-by: twalsh-ebi <twalsh@ebi.ac.uk>
    Co-authored-by: Thiago Genez <thiagogenez@ebi.ac.uk>
    Co-authored-by: Cristina Guijarro-Clarke <cristig@ebi.ac.uk>

--- src/python/scripts/run_gerp.py
+++ src/python/scripts/run_gerp.py
@@ -22,51 +22,86 @@ Typical usage example::
 
     $ python run_gerp.py --msa_file alignment.mfa --tree_file tree.nw
 
-If ``gerpcol`` and/or ``gerpelem`` are not accesible from ``$PATH``, you will need to provide the
+If ``gerpcol`` and/or ``gerpelem`` are not accessible from ``$PATH``, you will need to provide the
 path where they can be found::
 
     $ python run_gerp.py --msa_file alignment.mfa --tree_file tree.nw --gerp_exe_dir path/to/gerp/
 
 """
-
+import argparse
+import json
 import os
 import subprocess
 
-import argschema
+
+def add_ce_to_json(ce_file: str, json_file: str) -> None:
+    """Enrich the json file with the constrained elements.
+
+    Args:
+        ce_file: file containing the constrained elements (space-delimited format).
+        json_file: json file with genome coordinate level.
+
+    """
+    with open(json_file, "r") as json_file_obj:
+        align_set = json.load(json_file_obj)
+
+    constrained_elems = []
+    with open(ce_file) as ce_file_obj:
+        # enrich json file with constrained elements info
+        for cons_elem in ce_file_obj:
+            if cons_elem.rstrip() == "":
+                continue
+            ce = cons_elem.split()
+            constrained_elem = {
+                "start": int(ce[0]),
+                "end": int(ce[1]),
+                "length": int(ce[2]),
+                "score": float(ce[3]),
+                "p-val": float(ce[4])
+            }
+            constrained_elems.append(constrained_elem)
+    align_set["constrained_elems"] = constrained_elems
+
+    with open(json_file, "w") as json_file_obj:
+        json.dump(align_set, json_file_obj)
 
 
-class InputSchema(argschema.ArgSchema):
-    """Calculates the Genomic Evolutionary Rate Profiling (GERP) of a multiple sequence alignment (MSA)."""
+def main(param: argparse.Namespace) -> None:
+    """Main function of the run_gerp.py script
 
-    msa_file = argschema.fields.InputFile(
-        required=True, description="MSA file (MFA format)"
-    )
-    tree_file = argschema.fields.InputFile(
-        required=True, description="Tree file (Newick format). Must include every species in the MSA."
-    )
-    depth_threshold = argschema.fields.Float(
-        required=False,
-        description="Constrained elements' depth threshold for shallow columns, in substitutions "
-                    "per site. By default, 0.5."
-    )
-    gerp_exe_dir = argschema.fields.InputDir(
-        required=False,
-        description="Path where 'gerpcol' and 'gerpelem' binaries can be found. By default, resort to $PATH."
-    )
+    This function runs gerpcol to define a GERP score for every column of the genomic alignment block
+    and gerpelem to identify constrained elements across the genomic alignment block.
 
+    Args:
+        param: argparse.Namespace storing all the script parameters
 
-if __name__ == "__main__":
-    mod = argschema.ArgSchemaParser(schema_type=InputSchema)
+    """
 
-    cmd = ["gerpcol", "-t", mod.args["tree_file"], "-f", mod.args["msa_file"]]
-    if "gerp_exe_dir" in mod.args:
-        cmd[0] = os.path.join(mod.args['gerp_exe_dir'], cmd[0])
+    cmd = ["gerpcol", "-t", param.tree_file, "-f", param.msa_file]
+    cmd[0] = os.path.join(param.gerp_exe_dir, cmd[0])
     subprocess.run(cmd, check=True)
 
     # By default, gerpcol's ouput filename has the MSA filename plus ".rates" suffix
-    cmd = ["gerpelem", "-f", f"{mod.args['msa_file']}.rates"]
-    if "gerp_exe_dir" in mod.args:
-        cmd[0] = os.path.join(mod.args['gerp_exe_dir'], cmd[0])
-    if "depth_threshold" in mod.args:
-        cmd += ["-d", str(mod.args["depth_threshold"])]
+    cmd = ["gerpelem", "-f", f"{param.msa_file}.rates"]
+    cmd[0] = os.path.join(param.gerp_exe_dir, cmd[0])
+    cmd += ["-d", str(param.depth_threshold)]
     subprocess.run(cmd, check=True)
+
+    add_ce_to_json(f"{param.msa_file}.rates.elems", f"{os.path.splitext(param.msa_file)[0]}.json")
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Calculate GERP score and identify constrained elements.")
+    parser.add_argument("--msa_file", type=str, required=True, help="MSA file (MFA format) (REQUIRED)")
+    parser.add_argument("--tree_file", type=str, required=True, help="Tree file (Newick format). Must "
+                                                "include every species in the MSA. (REQUIRED)")
+    parser.add_argument("--depth_threshold", default=0.5, type=float, help="Constrained elements depth "
+                                                "threshold for shallow columns, in substitutions per site. "
+                                                "The default is 0.5.")
+    parser.add_argument("--gerp_exe_dir", default="", type=str,
+                        help="Path where 'gerpcol' and 'gerpelem' executable binaries can be found."
+                             " The default is $PATH.")
+
+    args = parser.parse_args()
+    main(args)
