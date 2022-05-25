ab835958e7548d9885464fc86425ce5cf8486e24
Botond Sipos/Ensembl Compara <103430062+ens-sb@users.noreply.github.com>
2022-05-25 17:28:59 +0100

    Update src/python/tests/test_collate_busco_results.py
    Co-authored-by: Cristina Guijarro-Clarke <cristig@ebi.ac.uk>

--- src/python/tests/test_collate_busco_results.py
+++ src/python/tests/test_collate_busco_results.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Testing of `` script.
+"""Testing of `collate_busco_results.py` script.
 
 Typical usage example::
 
