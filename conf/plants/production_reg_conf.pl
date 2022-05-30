c911407d4ee2f611f35dd3c8cb36c776870f3e46
Cristina Guijarro-Clarke <cristig@ebi.ac.uk>
2022-05-30 16:19:12 +0100

    Update plants e108 regsistry with members and lastz pipelines

--- conf/plants/production_reg_conf.pl
+++ conf/plants/production_reg_conf.pl
@@ -93,12 +93,12 @@ my $compara_dbs = {
     'compara_prev'   => [ 'mysql-ens-compara-prod-5', "ensembl_compara_plants_${prev_eg_release}_${prev_release}" ],
 
     # homology dbs
-    #'compara_members'        => [ 'mysql-ens-compara-prod-X', 'cristig_plants_load_members_108'],
+    'compara_members'        => [ 'mysql-ens-compara-prod-5', 'cristig_plants_load_members_108'],
     #'compara_ptrees'         => [ 'mysql-ens-compara-prod-X', 'cristig_default_plants_protein_trees_108' ],
     'wheat_cultivars_ptrees' => [ 'mysql-ens-compara-prod-5', 'ensembl_compara_plants_${prev_eg_release}_${prev_release}' ],
 
     # LASTZ dbs
-    #'lastz_batch_1'  => [ 'mysql-ens-compara-prod-X', 'cristig_plants_lastz_batch1_108' ],
+    'lastz_batch_1'  => [ 'mysql-ens-compara-prod-5', 'cristig_plants_lastz_batch1_108' ],
 
     # synteny
     #'compara_syntenies' => [ 'mysql-ens-compara-prod-X', 'cristig_plants_synteny_108' ],
