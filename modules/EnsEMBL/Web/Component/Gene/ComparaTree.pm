1c52f93fe4df51b8ef3b7c06ab88220ca7bf2230
Anne Lyle <annelyle@ebi.ac.uk>
2022-07-14 14:27:25 +0000

    Add sanity check for presence of gene - ENSWEB-6659

--- modules/EnsEMBL/Web/Component/Gene/ComparaTree.pm
+++ modules/EnsEMBL/Web/Component/Gene/ComparaTree.pm
@@ -98,6 +98,7 @@ sub content {
 
   my $type   = $self->param('data_type') || $hub->type;
   my $vc = $self->view_config($type);
+  warn ">>> GENE TREE? $is_genetree";
 
   if ($is_genetree) {
     $tree   = $object->Obj;
@@ -183,7 +184,7 @@ sub content {
   my $gene_to_highlight = $hub->param('g1');
   my $current_gene_display_name;
 
-  if (defined $gene->Obj->display_xref) {
+  if ($gene && defined $gene->Obj->display_xref) {
     $current_gene_display_name = $gene->Obj->display_xref->display_id;
   } else {
     $current_gene_display_name = $hub->param('g'); 
