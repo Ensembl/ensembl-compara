44f88d63f29c46a559e285e2ce4aca907083ab40
Thiago Genez <thiagogenez@ebi.ac.uk>
2022-08-12 20:28:53 +0100

    fixing auto-merged files
    given preference to release/107 branch

--- src/python/tests/conftest.py
+++ src/python/tests/conftest.py
@@ -70,11 +70,10 @@ def dir_cmp(request: FixtureRequest, tmp_dir: PathLike) -> DirCmp:
     """
     # Get the source and temporary absolute paths for reference and target root directories
     ref = Path(request.param['ref'])  # type: ignore[attr-defined]
-    ref_src = ref if ref.is_absolute() else pytest.files_dir / ref  # type: ignore[attr-defined,operator]
+    ref_src = ref if ref.is_absolute() else pytest.files_dir / ref  # type: ignore
     ref_tmp = Path(tmp_dir) / str(ref).replace(os.path.sep, '_')
     target = Path(request.param['target'])  # type: ignore[attr-defined]
-    # type: ignore[attr-defined]
-    target_src = target if target.is_absolute() else pytest.files_dir / target # type: ignore[operator]
+    target_src = target if target.is_absolute() else pytest.files_dir / target  # type: ignore
     target_tmp = Path(tmp_dir) / str(target).replace(os.path.sep, '_')
     # Copy directory trees (if they have not been copied already) ignoring file metadata
     if not ref_tmp.exists():
