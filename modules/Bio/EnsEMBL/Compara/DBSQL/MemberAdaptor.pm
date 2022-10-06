948aef8d6ed899df7fd2526947f822c2db3170fe
Thomas Walsh <twalsh@ebi.ac.uk>
2022-10-06 13:44:19 +0100

    Update fetch_by_stable_id deprecation text again

--- modules/Bio/EnsEMBL/Compara/DBSQL/MemberAdaptor.pm
+++ modules/Bio/EnsEMBL/Compara/DBSQL/MemberAdaptor.pm
@@ -80,7 +80,7 @@ sub fetch_by_stable_id { ## DEPRECATED
     my ($self, $stable_id) = @_;
 
     deprecate(
-        "MemberAdaptor::fetch_by_stable_id() is deprecated and will be removed in e109 or a subsequent release. Please use fetch_by_stable_id_GenomeDB instead."
+        "MemberAdaptor::fetch_by_stable_id() is deprecated and will be removed in e109. Please use fetch_by_stable_id_GenomeDB instead."
     );
 
     throw("MemberAdaptor::fetch_by_stable_id() must have an stable_id") unless $stable_id;
