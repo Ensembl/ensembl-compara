903af421b04bb05b82ad127e2ce6a17bf192e6e5
Thiago Genez <thiagogenez@ebi.ac.uk>
2022-10-10 16:51:03 +0100

    Supertree did not stay expanded
    ENSCOMPARASW-4276

--- modules/Bio/EnsEMBL/Compara/AlignedMemberSet.pm
+++ modules/Bio/EnsEMBL/Compara/AlignedMemberSet.pm
@@ -306,6 +306,7 @@ sub load_cigars_from_file {
     my %unseen = (map {$_ => 1} (keys %aln_string_hash));
 
     ## Then we associate each member with its alignment, based on the required $id_type
+    $tree->expand_subtrees;  # TODO: remove this additional call once ENSCOMPARASW-4276 is resolved
     foreach my $member (@{$self->get_all_Members}) {
 
         my $seqID = $member->seq_member_id;
