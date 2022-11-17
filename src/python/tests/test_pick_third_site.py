25d5e6009b8e94c0ef3d3e4b448017e22204e723
Botond Sipos/Compara <Botond.Sipos@ebi.ac.uk>
2022-11-17 14:31:08 +0000

    Species tree pipeline: flatfile fixes after moving to centralised location.

--- src/python/tests/test_pick_third_site.py
+++ src/python/tests/test_pick_third_site.py
@@ -50,7 +50,7 @@ class TestPickThirdSite:
         subprocess.check_call(cmd, cwd=location)
 
         # Compare with expected output:
-        expected_fas = str(Path(
-            __file__).parents[2] / 'test_data' / "flatfiles" / "SpeciesTreeFromBusco" / "pick_third_site_expected.fas")
+        expected_fas = str(Path(__file__).parents[2] / 'test_data' / "flatfiles" /
+                           "SpeciesTreeFromBusco" / "pick_third_site_expected.fas")
 
         assert file_cmp(output_fas, expected_fas)
