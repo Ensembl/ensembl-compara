efa9a4bf5b7a3ec3f9ec6c906205822cc3bd832c
twalsh-ebi <twalsh@ebi.ac.uk>
2022-11-17 09:45:40 +0000

    Restore MemberAdaptor::fetch_by_stable_id (#525)
    * Restore MemberAdaptor::fetch_by_stable_id
    
    * Update POD

--- modules/Bio/EnsEMBL/Compara/DBSQL/MemberAdaptor.pm
+++ modules/Bio/EnsEMBL/Compara/DBSQL/MemberAdaptor.pm
@@ -15,17 +15,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 
-=cut
-
-
-=head1 CONTACT
-
-  Please email comments or questions to the public Ensembl
-  developers list at <http://lists.ensembl.org/mailman/listinfo/dev>.
-
-  Questions may also be sent to the Ensembl help desk at
-  <http://www.ensembl.org/Help/Contact>.
-
 =head1 NAME
 
 Bio::EnsEMBL::Compara::DBSQL::MemberAdaptor
@@ -38,15 +27,6 @@ Base adaptor for Member objects.  This adaptor cannot be used directly.
 
   Bio::EnsEMBL::Compara::DBSQL::MemberAdaptor
   +- Bio::EnsEMBL::Compara::DBSQL::BaseAdaptor
-
-=head1 AUTHORSHIP
-
-Ensembl Team. Individual contributions can be found in the GIT log.
-
-=head1 APPENDIX
-
-The rest of the documentation details each of the object methods.
-Internal methods are usually preceded with an underscore (_)
 
 =cut
 
@@ -75,6 +55,33 @@ use base qw(Bio::EnsEMBL::Compara::DBSQL::BaseAdaptor);
 # GLOBAL METHODS
 #
 #####################
+
+sub fetch_by_stable_id { ## DEPRECATED
+    my ($self, $stable_id) = @_;
+
+    deprecate(
+        "MemberAdaptor::fetch_by_stable_id() is deprecated and will be removed in e110. Please use fetch_by_stable_id_GenomeDB instead."
+    );
+
+    throw("MemberAdaptor::fetch_by_stable_id() must have an stable_id") unless $stable_id;
+
+    my $constraint = 'm.stable_id = ?';
+    $self->bind_param_generic_fetch($stable_id, SQL_VARCHAR);
+    my $m = $self->generic_fetch_one($constraint);
+    return $m if $m;
+
+    my $vindex = rindex($stable_id, '.');
+    return undef if $vindex <= 0;  # bail out if there is no dot, or if the string starts with a dot (since that would make the stable_id part empty)
+    my $version = substr($stable_id,$vindex+1);
+    if (looks_like_number($version)) {  # to avoid DBI complains
+        $constraint = 'm.stable_id = ? AND m.version = ?';
+        $self->bind_param_generic_fetch(substr($stable_id,0,$vindex), SQL_VARCHAR);
+        $self->bind_param_generic_fetch($version, SQL_INTEGER);
+        return $self->generic_fetch_one($constraint);
+    } else {
+        return undef;
+    }
+}
 
 =head2 fetch_by_stable_id_GenomeDB
   Arg [1]       : string $stable_id
