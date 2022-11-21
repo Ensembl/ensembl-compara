a34d6e83c84ffc34e170b2ee23750b8e816413b2
Thomas Walsh <twalsh@ebi.ac.uk>
2022-11-21 19:46:53 +0000

    Update Compara dumps registry files for e109

--- conf/fungi/dumps_reg_conf.pl
+++ conf/fungi/dumps_reg_conf.pl
@@ -28,7 +28,7 @@ Bio::EnsEMBL::Registry->load_registry_from_url("mysql://ensro\@mysql-ens-sta-3-b
 
 # Compara databases:
 Bio::EnsEMBL::Compara::Utils::Registry::add_compara_dbas({
-    'compara_curr'   => [ 'mysql-ens-compara-prod-8', "ensembl_compara_fungi_${curr_eg_release}_${curr_release}" ],
+    'compara_curr'   => [ 'mysql-ens-compara-prod-4', "ensembl_compara_fungi_${curr_eg_release}_${curr_release}" ],
 });
 
 # Other databases:
