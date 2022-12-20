bc27a4262432b8707d0d59f4f1146e02b80913af
Simarpreet-Kaur-Bhurji <sbhurji@ebi.ac.uk>
2022-12-20 14:37:35 +0000

    Adding metazoa collection pipeline config files

---
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/Metazoa/ComplementaryProteinTrees_conf.pm
@@ -0,0 +1,117 @@
+=head1 LICENSE
+
+See the NOTICE file distributed with this work for additional information
+regarding copyright ownership.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+=head1 NAME
+
+Bio::EnsEMBL::Compara::PipeConfig::Metazoa::ComplementaryProteinTrees_conf
+
+=head1 SYNOPSIS
+
+    init_pipeline.pl Bio::EnsEMBL::Compara::PipeConfig::Metazoa::ComplementaryProteinTrees_conf -host mysql-ens-compara-prod-X -port XXXX \
+        -collection <complementary_collection> -ref_collection 'default'
+
+=head1 DESCRIPTION
+
+The Metazoa Complementary Collection PipeConfig file for ProteinTrees pipeline.
+This automates relevant pre-execution tasks.
+
+=cut
+
+package Bio::EnsEMBL::Compara::PipeConfig::Metazoa::ComplementaryProteinTrees_conf;
+
+use strict;
+use warnings;
+
+use Bio::EnsEMBL::ApiVersion ();
+
+use base ('Bio::EnsEMBL::Compara::PipeConfig::Metazoa::ProteinTrees_conf');
+
+
+sub default_options {
+    my ($self) = @_;
+
+    return {
+        %{$self->SUPER::default_options},
+
+    # complementary collection parameters:
+
+        # Collection in master that may have overlapping data. Potentially overlapping clusters
+        # and homologies are removed during the complementary protein-trees pipeline. This should be
+        # defined in the PipeConfig file for each complementary collection (or on the command line).
+        'ref_collection'   => undef,
+	'do_stable_id_mapping' => 0,
+	'do_treefam_xref' => 0,
+    };
+}
+
+
+sub pipeline_wide_parameters {
+    my ($self) = @_;
+    return {
+        %{$self->SUPER::pipeline_wide_parameters},
+
+        'ref_collection' => $self->o('ref_collection'),
+    }
+}
+
+
+sub core_pipeline_analyses {
+    my ($self) = @_;
+    return [
+        @{$self->SUPER::core_pipeline_analyses},
+
+        # include complementary collection-specific analyses
+        {   -logic_name => 'check_strains_cluster_factory',
+            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::JobFactory',
+            -parameters => {
+                'inputquery' => 'SELECT root_id AS gene_tree_id FROM gene_tree_root WHERE tree_type = "tree" AND clusterset_id="default"',
+            },
+            -flow_into  => {
+                '2->A' => [ 'cleanup_strains_clusters' ],
+                'A->1' => [ 'hc_clusters' ],
+            },
+            -rc_name    => '1Gb_job',
+        },
+
+        {   -logic_name => 'cleanup_strains_clusters',
+            -module     => 'Bio::EnsEMBL::Compara::RunnableDB::ProteinTrees::RemoveOverlappingClusters',
+        },
+
+        {
+             -logic_name => 'remove_overlapping_homologies',
+             -module     => 'Bio::EnsEMBL::Compara::RunnableDB::GeneTrees::RemoveOverlappingHomologies',
+        },
+    ]
+}
+
+
+sub tweak_analyses {
+    my $self = shift;
+
+    $self->SUPER::tweak_analyses(@_);
+
+    my $analyses_by_name = shift;
+
+    # wire up complementary collection-specific analyses
+    $analyses_by_name->{'remove_blocklisted_genes'}->{'-flow_into'} = ['check_strains_cluster_factory'];
+    push @{$analyses_by_name->{'backbone_pipeline_finished'}->{'-flow_into'}}, 'remove_overlapping_homologies';
+}
+
+
+1;
