1eaea8d0de9491b3e7d37b1d423cb6c7552bed90
Botond Sipos/Compara <Botond.Sipos@ebi.ac.uk>
2023-01-24 16:45:21 +0000

    Registry update for Vertebrates 110: compara_members

--- conf/vertebrates/production_reg_conf.pl
+++ conf/vertebrates/production_reg_conf.pl
@@ -74,7 +74,7 @@ my $compara_dbs = {
     'compara_prev'   => [ 'mysql-ens-compara-prod-1', "ensembl_compara_$prev_release" ],
 
     # homology dbs
-    #'compara_members'         => [ 'mysql-ens-compara-prod-X',  '' ],
+    'compara_members'         => [ 'mysql-ens-compara-prod-1',  'sbotond_vertebrates_load_members_110' ],
     #'compara_ptrees'          => [ 'mysql-ens-compara-prod-X',  '' ],
     'compara_ptrees_prev'      => [ 'mysql-ens-compara-prod-3',  'twalsh_default_vertebrates_protein_trees_109' ],
     #'compara_nctrees'         => [ 'mysql-ens-compara-prod-X',  '' ],
