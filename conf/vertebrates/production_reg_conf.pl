536221a48b2d5247bb749059d1bfed7aabbd44d8
Botond Sipos/Compara <Botond.Sipos@ebi.ac.uk>
2023-01-25 13:59:47 +0000

    Update Vertebrates registry with e110 lastz batch

--- conf/vertebrates/production_reg_conf.pl
+++ conf/vertebrates/production_reg_conf.pl
@@ -88,7 +88,7 @@ my $compara_dbs = {
     'pig_breeds_nctrees_prev' => [ 'mysql-ens-compara-prod-9',  'twalsh_vertebrates_pig_breeds_ncrna_reindexed_trees_109' ],
 
     # LASTZ dbs
-    #'lastz_batch_1'    => [ 'mysql-ens-compara-prod-X', '' ],
+    'lastz_batch_1'    => [ 'mysql-ens-compara-prod-3', 'sbotond_vertebrates_lastz_batch1_110' ],
     'unidir_lastz'     => [ 'mysql-ens-compara-prod-1', 'ensembl_vertebrates_unidirectional_lastz' ],
 
     # EPO dbs
