0cb003f9cbc8b7c60e1998ca2c6b8de242f2e29f
Thiago Genez <thiagogenez@ebi.ac.uk>
2023-05-18 14:34:03 +0100

    Update modules/Bio/EnsEMBL/Compara/PipeConfig/ProteinTrees_conf.pm
    Co-authored-by: twalsh-ebi <twalsh@ebi.ac.uk>

--- modules/Bio/EnsEMBL/Compara/PipeConfig/ProteinTrees_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/ProteinTrees_conf.pm
@@ -2487,7 +2487,7 @@ sub core_pipeline_analyses {
                     '(#raxml_cores# >  16) && (#raxml_cores# <= 32)'    => 'examl_8_cores',
                     '(#raxml_cores# >  32) && (#raxml_cores# <= 48)'    => 'examl_16_cores',
                     '(#raxml_cores# >  48) && (#raxml_cores# <= 128)'   => 'examl_32_cores',
-                    '(#raxml_cores# >  96)'                             => 'examl_64_cores',
+                    '(#raxml_cores# >  128)'                            => 'examl_64_cores',
                 ),
             },
         },
