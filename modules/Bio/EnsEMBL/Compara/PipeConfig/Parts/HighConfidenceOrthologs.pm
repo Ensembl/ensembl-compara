3f4627ab9e0ccc02661226eed9964d5cc31b778a
Thiago Genez <thiagogenez@ebi.ac.uk>
2023-06-26 13:25:26 +0100

    make 16Gb as default memory for import_homology_table_himem analysis

--- modules/Bio/EnsEMBL/Compara/PipeConfig/Parts/HighConfidenceOrthologs.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/Parts/HighConfidenceOrthologs.pm
@@ -159,7 +159,7 @@ sub pipeline_analyses_high_confidence {
                 homology_flatfile => '#homology_dumps_dir#/#hashed_mlss_id#/#mlss_id#.#member_type#.homologies.tsv',
                 replace      => 0,
             },
-            -rc_name         => '8Gb_job',
+            -rc_name         => '16Gb_job',
             -hive_capacity   => $self->o('import_homologies_capacity'),
             -max_retry_count => 0,
         },
