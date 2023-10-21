8ca98999db6262069d1a0f8cf45f7f538689d75f
Thomas Walsh <twalsh@ebi.ac.uk>
2023-10-21 16:15:52 +0100

    Update Pan registry with e112 members

--- conf/pan/production_reg_conf.pl
+++ conf/pan/production_reg_conf.pl
@@ -103,7 +103,7 @@ my $compara_dbs = {
     'compara_prev'   => [ 'mysql-ens-compara-prod-7', "ensembl_compara_pan_homology_${prev_eg_release}_${prev_release}" ],
 
     # homology dbs
-    #'compara_members'  => [ 'mysql-ens-compara-prod-7', '' ],
+    'compara_members'  => [ 'mysql-ens-compara-prod-7', 'twalsh_pan_load_members_112' ],
     #'compara_ptrees'   => [ 'mysql-ens-compara-prod-7', '' ],
 };
 
