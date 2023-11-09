d48ca0052f19eaacad46d6eae94770c37cd0a5c9
Thomas Walsh <twalsh@ebi.ac.uk>
2023-11-09 14:13:45 +0000

    Update protein-tree resource classes for Slurm

--- modules/Bio/EnsEMBL/Compara/PipeConfig/Vertebrates/ProteinTrees_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/Vertebrates/ProteinTrees_conf.pm
@@ -108,11 +108,10 @@ sub tweak_analyses {
     ## Extend this section to redefine the resource names of some analysis
     my %overriden_rc_names = (
         'CAFE_table'                => '24Gb_job',
-        'hcluster_run'              => '1Gb_job',
-        'hcluster_parse_output'     => '1Gb_job',
+        'hcluster_run'              => '1Gb_1_hour_job',
+        'hcluster_parse_output'     => '1Gb_1_hour_job',
         'split_genes'               => 'default',   # This is 250Mb
-        'CAFE_species_tree'         => '24Gb_job',
-        'stable_id_mapping'         => '4Gb_job',
+        'CAFE_species_tree'         => '24Gb_1_hour_job',
         'get_long_short_orth_genes_himem'  => '4Gb_job',
     );
     foreach my $logic_name (keys %overriden_rc_names) {
