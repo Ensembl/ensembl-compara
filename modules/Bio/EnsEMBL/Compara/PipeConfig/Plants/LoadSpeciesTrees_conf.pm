5809ebd38ff972266800cddaa600ceaaf79db2d4
Thomas Walsh <twalsh@ebi.ac.uk>
2023-11-12 07:02:43 +0000

    Revert binary requirement on e112 Plants species tree

--- modules/Bio/EnsEMBL/Compara/PipeConfig/Plants/LoadSpeciesTrees_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/Plants/LoadSpeciesTrees_conf.pm
@@ -47,7 +47,7 @@ sub default_options {
         'compara_alias_name'    => 'compara_curr',
         'species_tree'          => $self->o('config_dir') . '/species_tree.topology.nw',
 
-        'binary'    => 1,  # The tree shared by Plants is expected to be binary
+        'binary'    => 0,  # The tree shared by Plants is not binary
         'n_missing_species_in_tree' => 0,
     };
 }
