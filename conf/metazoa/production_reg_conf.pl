189ee4c95d35e1abd4017849de06378a7c40f05d
Thomas Walsh <twalsh@ebi.ac.uk>
2024-03-22 10:29:24 +0000

    Update Metazoa registry for e112

--- conf/metazoa/production_reg_conf.pl
+++ conf/metazoa/production_reg_conf.pl
@@ -58,8 +58,8 @@ Bio::EnsEMBL::Compara::Utils::Registry::add_core_dbas( $overlap_cores );
 # previous release core databases will be required by PrepareMasterDatabaseForRelease and LoadMembers only
 *Bio::EnsEMBL::Compara::Utils::Registry::load_previous_core_databases = sub {
     Bio::EnsEMBL::Registry->load_registry_from_db(
-        -host   => 'mysql-ens-sta-3',
-        -port   => 4160,
+        -host   => 'mysql-ens-sta-3-b',
+        -port   => 4686,
         -user   => 'ensro',
         -pass   => '',
         -db_version     => $prev_release,
@@ -76,11 +76,11 @@ my $compara_dbs = {
     'compara_prev'   => [ 'mysql-ens-compara-prod-6', "ensembl_compara_metazoa_${prev_eg_release}_${prev_release}" ],
 
     # homology dbs
-    'compara_members'  => [ 'mysql-ens-compara-prod-6', 'twalsh_metazoa_load_members_111' ],
-    'compara_ptrees'   => [ 'mysql-ens-compara-prod-6', 'sbhurji_default_metazoa_protein_trees_110' ],
-    'protostomes_ptrees' => [ 'mysql-ens-compara-prod-6', 'twalsh_protostomes_metazoa_protein_trees_20230209_110' ],
+    'compara_members'  => [ 'mysql-ens-compara-prod-6', 'thiagogenez_metazoa_load_members_112' ],
+    'compara_ptrees'   => [ 'mysql-ens-compara-prod-6', 'thiagogenez_default_metazoa_protein_trees_112' ],
+    'protostomes_ptrees' => [ 'mysql-ens-compara-prod-9', 'thiagogenez_protostomes_metazoa_protein_trees_112' ],
     'insects_ptrees'   => [ 'mysql-ens-compara-prod-8', 'twalsh_insects_metazoa_protein_trees_20230624_111' ],
-    #'drosophila_ptrees'  => [ 'mysql-ens-compara-prod-X', '' ],
+    'drosophila_ptrees'  => [ 'mysql-ens-compara-prod-9', 'thiagogenez_pangenome_drosophila_metazoa_protein_trees_112' ],
 
     # LastZ dbs
     # 'lastz_batch_1' => [ 'mysql-ens-compara-prod-X', '' ],
@@ -95,7 +95,7 @@ Bio::EnsEMBL::Compara::Utils::Registry::add_compara_dbas( $compara_dbs );
 # ----------------------NON-COMPARA DATABASES------------------------
 # NCBI taxonomy database (maintained by production team):
 Bio::EnsEMBL::Compara::Utils::Registry::add_taxonomy_dbas({
-    'ncbi_taxonomy' => [ 'mysql-ens-sta-3-b', "ncbi_taxonomy_$curr_release" ],
+    'ncbi_taxonomy' => [ 'mysql-ens-sta-3', "ncbi_taxonomy_$curr_release" ],
 });
 
 # -------------------------------------------------------------------
