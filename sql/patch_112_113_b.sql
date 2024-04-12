22b2586ce061d3569afb9fc7ba0e270595bb0399
Botond Sipos/Ensembl Compara <103430062+ens-sb@users.noreply.github.com>
2024-04-12 12:19:15 +0100

    Update sql/patch_112_113_b.sql
    Co-authored-by: twalsh-ebi <twalsh@ebi.ac.uk>

--- sql/patch_112_113_b.sql
+++ sql/patch_112_113_b.sql
@@ -13,7 +13,7 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
-# patch_109_110_b.sql
+# patch_112_113_b.sql
 #
 # Title: Update meta key in the meta table.
 #
