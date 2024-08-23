ce2c0d737e6bff05e5f57de73ac1dc91af0bc6c1
Thomas Walsh <twalsh@ebi.ac.uk>
2024-08-23 15:29:37 +0100

    Fix hc_params variable name

--- modules/Bio/EnsEMBL/Compara/PipeConfig/ncRNAtrees_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/ncRNAtrees_conf.pm
@@ -693,7 +693,7 @@ sub core_pipeline_analyses {
 
             { -logic_name => 'hc_supertree',
               -module     => 'Bio::EnsEMBL::Compara::RunnableDB::GeneTrees::HCOneSupertree',
-              %hc_analysis_params,
+              %hc_params,
             },
 
             { -logic_name => 'fire_cafe',
