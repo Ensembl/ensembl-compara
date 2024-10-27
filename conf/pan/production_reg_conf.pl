ebc050b4e8eaf5e892d4fd7a7883a1a5edbf3e86
Thomas Walsh <twalsh@ebi.ac.uk>
2024-10-27 07:53:05 +0000

    Update Pan registry with e114 release DB

--- conf/pan/production_reg_conf.pl
+++ conf/pan/production_reg_conf.pl
@@ -137,7 +137,7 @@ Bio::EnsEMBL::Compara::Utils::Registry::load_collection_core_database(
 my $compara_dbs = {
     # general compara dbs
     'compara_master' => [ 'mysql-ens-compara-prod-7', 'ensembl_compara_master_pan' ],
-    #'compara_curr'   => [ 'mysql-ens-compara-prod-7', "ensembl_compara_pan_homology_${curr_eg_release}_${curr_release}" ],
+    'compara_curr'   => [ 'mysql-ens-compara-prod-7', "ensembl_compara_pan_homology_${curr_eg_release}_${curr_release}" ],
     'compara_prev'   => [ 'mysql-ens-compara-prod-7', "ensembl_compara_pan_homology_${prev_eg_release}_${prev_release}" ],
 
     # homology dbs
