c3fc0a015519f9ee4589a82b8fc27fde34f14920
twalsh-ebi <twalsh@ebi.ac.uk>
2024-11-25 16:11:15 +0000

    Update CACTUS_DB pipelines and config for e114 (#875)
    * Update CACTUS_DB pipelines and config for e114
    
    * Refactor overhang mask creation into function
    
    * Omit component genomes from multiplealigner_stats jobs
    
    * Add some info on software used in Cactus-loading pipeline
    
    * Fix return type of trimming_maf_iterator

--- modules/Bio/EnsEMBL/Compara/RunnableDB/HAL/LoadHalMapping.pm
+++ modules/Bio/EnsEMBL/Compara/RunnableDB/HAL/LoadHalMapping.pm
@@ -27,13 +27,15 @@ use strict;
 use warnings;
 
 use Bio::EnsEMBL::Hive::Utils qw(destringify stringify);
-use Bio::EnsEMBL::Compara::HAL::HALXS::HALAdaptor;
+use Bio::EnsEMBL::Utils::Exception qw(throw);
 
 use base ('Bio::EnsEMBL::Compara::RunnableDB::BaseRunnable');
 
 
 sub run {
     my $self = shift;
+
+    my $hal_stats_exe = $self->require_executable('halStats_exe');
 
     my $mlss_adaptor = $self->compara_dba->get_MethodLinkSpeciesSetAdaptor();
     my $gdb_adaptor = $self->compara_dba->get_GenomeDBAdaptor();
@@ -48,10 +50,15 @@ sub run {
     } else {
 
         my @genome_dbs = map { $gdb_adaptor->fetch_by_dbID($_->dbID) } @{$mlss->species_set->genome_dbs};
+
+        # Keep only principal GenomeDBs; these will be expanded to include components as appropriate.
+        @genome_dbs = grep { !$_->genome_component } @genome_dbs;
+
         my $hal_file = $mlss->url;
 
-        my $hal_adaptor = Bio::EnsEMBL::Compara::HAL::HALXS::HALAdaptor->new($hal_file);
-        my @hal_genome_names = $hal_adaptor->genomes();
+        my $cmd_args = [$hal_stats_exe, '--genomes', $hal_file];
+        my ($output) = $self->get_command_output($cmd_args, { die_on_failure => 1 });  # We only need the first line of output.
+        my @hal_genome_names = split(/ /, $output);
         my @hal_leaf_genome_names = grep { $_ !~ /^Anc[0-9]+$/ } @hal_genome_names;
 
         foreach my $genome_db (@genome_dbs) {
