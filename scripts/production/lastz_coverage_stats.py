24cd355a0ba5907f986b58302a337d2e484eb979
Simarpreet-Kaur-Bhurji <sbhurji@ebi.ac.uk>
2025-03-12 15:54:57 +0000

    Updating the script to address feedback

--- scripts/production/lastz_coverage_stats.py
+++ scripts/production/lastz_coverage_stats.py
@@ -1,126 +1,167 @@
+#!/usr/bin/env python3
+# See the NOTICE file distributed with this work for additional information
+# regarding copyright ownership.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""This script obtains lastz coverage statistics for a release or lastz db. To get relevant 
+statistics from release DB use the --release flag. 
+
+Example command:
+Lastz db:
+    ./lastz_coverage_stats.py url output_file
+Release db:
+   ./lastz_coverage_stats.py url output_file --release release_version
+"""
+
 import argparse
 import csv
+
 from sqlalchemy import create_engine, text
 
-def execute_query(connection, query):
-    """
-    Execute a SQL query and return the result.
 
-    Args:
-        connection: The database connection object.
-        query: The SQL query to execute.
-
-    Returns:
-        The result of the executed query.
-    """
-    return connection.execute(text(query))
-
-def get_lastz_mlss_from_release_db(release):
-    """
-    Generate the SQL query to retrieve method_link_species_set_ids from the release database.
-
-    Args:
-        release: The release version.
-
-    Returns:
-        The SQL query string.
-    """
-    return f"""
+get_lastz_mlss_from_first_release = """
     SELECT 
-        method_link_species_set_tag.`method_link_species_set_id`
+        method_link_species_set_id
     FROM 
-        method_link_species_set_tag 
-    INNER JOIN
-        method_link_species_set
-    ON 
-        method_link_species_set_tag.method_link_species_set_id = method_link_species_set.method_link_species_set_id
+        method_link_species_set As mlss
     WHERE 
-        (method_link_species_set_tag.tag IN ('reference_species', 'ref_genome_coverage', 'ref_genome_length', 
-          'non_reference_species', 'non_ref_genome_coverage', 'non_ref_genome_length') 
-        AND 
-        method_link_species_set.first_release={release})
-    OR
-        method_link_species_set_tag.tag IN ('rerun_in_{release}','patched_in_{release}') 
+        mlss.method_link_id = 16
     AND 
-        method_link_species_set.method_link_id = 16;
+        first_release = :release;
     """
 
-def get_mlss_from_lastz_db():
+#The method_link_id 16 is intended to match LASTZ_NET method_link_type in this script.
+get_mlss_for_rerun_tags = """
+    SELECT 
+        mlss_tag.method_link_species_set_id
+    FROM 
+        method_link_species_set_tag AS mlss_tag
+    INNER JOIN
+        method_link_species_set AS mlss
+    USING 
+        (method_link_species_set_id)
+    WHERE 
+        mlss_tag.tag IN (:rerun_tag, :patched_tag) 
+    AND 
+        mlss.method_link_id = 16;
     """
-    Generate the SQL query to retrieve distinct method_link_species_set_ids from the lastz database.
 
-    Returns:
-        The SQL query string.
-    """
-    return """
-    SELECT DISTINCT method_link_species_set_id FROM method_link_species_set_tag 
-    WHERE tag IN ('reference_species','ref_genome_coverage', 'ref_genome_length', 
-                  'non_reference_species', 'non_ref_genome_coverage', 'non_ref_genome_length');
+
+get_mlss_from_lastz_db = """
+    SELECT 
+        method_link_species_set_id 
+    FROM 
+        method_link_species_set 
+    WHERE 
+        method_link_id = 16;
     """
 
-def get_result_query(ids_str):
+
+get_query_for_results = """
+    SELECT 
+        method_link_species_set_id,
+        tag,
+        value
+    FROM
+        method_link_species_set_tag 
+    WHERE
+        method_link_species_set_id IN :mlss_ids
+    AND
+        tag IN ('reference_species', 'ref_genome_coverage', 'ref_genome_length', 'non_reference_species', 
+        'non_ref_genome_coverage', 'non_ref_genome_length');
     """
-    Generate the SQL query to retrieve coverage ratios for the given method_link_species_set_ids.
+
+
+def calculate_coverage_ratios(results):
+    """
+    Calculate the coverage ratios from the query results and create a nested dictionary.
 
     Args:
-        ids_str: A comma-separated string of method_link_species_set_ids.
+        results: The query results.
 
     Returns:
-        The SQL query string.
+        A nested dictionary with the structure {mlss_id: {mlss_tag: value}} and 
+        a list of dictionaries with the calculated coverage ratios.
     """
-    return f"""
-    SELECT 
-        mlss,
-        reference_species,
-        ref_genome_coverage / ref_genome_length AS ref_coverage_ratio,
-        non_reference_species,
-        non_ref_genome_coverage / non_ref_genome_length AS non_ref_coverage_ratio
-    FROM (
-        SELECT 
-            method_link_species_set_id AS mlss,
-            (SELECT value FROM method_link_species_set_tag WHERE method_link_species_set_id = mlss 
-             AND tag = 'reference_species') AS reference_species,
-            (SELECT value FROM method_link_species_set_tag WHERE method_link_species_set_id = mlss 
-             AND tag = 'ref_genome_coverage') AS ref_genome_coverage,
-            (SELECT value FROM method_link_species_set_tag WHERE method_link_species_set_id = mlss 
-             AND tag = 'ref_genome_length') AS ref_genome_length,
-            (SELECT value FROM method_link_species_set_tag WHERE method_link_species_set_id = mlss 
-             AND tag = 'non_reference_species') AS non_reference_species,
-            (SELECT value FROM method_link_species_set_tag WHERE method_link_species_set_id = mlss 
-             AND tag = 'non_ref_genome_coverage') AS non_ref_genome_coverage,
-            (SELECT value FROM method_link_species_set_tag WHERE method_link_species_set_id = mlss 
-             AND tag = 'non_ref_genome_length') AS non_ref_genome_length               
-        FROM 
-            method_link_species_set_tag
-        WHERE 
-            method_link_species_set_id IN ({ids_str})
-        GROUP BY 
-            method_link_species_set_id
-    ) AS subquery
-    """
+    data = {}
+    for row in results:
+        mlss = row[0]
+        tag = row[1]
+        value = row[2]
+        if mlss not in data:
+            data[mlss] = {}
+        try:
+            data[mlss][tag] = int(value)
+        except ValueError:
+            data[mlss][tag] = value
 
-def write_to_tsv(filename, result):
+    calculated_results = []
+    for mlss, tags in data.items():
+        # print(mlss,tags.get('reference_species'))
+        ref_coverage_ratio = (
+            tags.get("ref_genome_coverage") / tags.get("ref_genome_length")
+        ) * 100
+        non_ref_coverage_ratio = (
+            tags.get("non_ref_genome_coverage") / tags.get("non_ref_genome_length")
+        ) * 100
+        calculated_results.append(
+            {
+                "mlss": mlss,
+                "reference_species": tags.get("reference_species"),
+                "ref_genome_coverage": tags.get("ref_genome_coverage"),
+                "ref_coverage_ratio": round(ref_coverage_ratio, 4),
+                "non_reference_species": tags.get("non_reference_species"),
+                "non_ref_genome_coverage": tags.get("non_ref_genome_coverage"),
+                "non_ref_coverage_ratio": round(non_ref_coverage_ratio, 4),
+            }
+        )
+
+    return calculated_results
+
+
+def write_to_tsv(filename, results):
     """
-    Write the query result to a TSV file.
+    Write the calculated results to a TSV file.
 
     Args:
         filename: The name of the TSV file.
-        result: The query result to write.
+        results: The calculated results to write.
     """
-    with open(filename, 'w', newline='') as tsvfile:
-        writer = csv.writer(tsvfile, delimiter='\t')
-        writer.writerow(['mlss', 'reference_species', 'ref_coverage_ratio', 'non_reference_species', 
-                         'non_ref_coverage_ratio'])
-        for row in result:
-            ref_coverage_ratio = row['ref_coverage_ratio']
-            non_ref_coverage_ratio = row['non_ref_coverage_ratio']
-            writer.writerow([
-                row['mlss'], 
-                row['reference_species'], 
-                round((ref_coverage_ratio if ref_coverage_ratio is not None else 0) * 100, 4), 
-                row['non_reference_species'], 
-                round((non_ref_coverage_ratio if non_ref_coverage_ratio is not None else 0) * 100, 4)
-            ])
+    with open(filename, "w", newline="", encoding="utf-8") as tsvfile:
+        writer = csv.writer(tsvfile, delimiter="\t")
+        writer.writerow(
+            [
+                "mlss",
+                "reference_species",
+                "ref_genome_coverage",
+                "ref_coverage_ratio",
+                "non_reference_species",
+                "non_ref_genome_coverage",
+                "non_ref_coverage_ratio",
+            ]
+        )
+        for row in results:
+            writer.writerow(
+                [
+                    row["mlss"],
+                    row["reference_species"],
+                    row["ref_genome_coverage"],
+                    row["ref_coverage_ratio"],
+                    row["non_reference_species"],
+                    row["non_ref_genome_coverage"],
+                    row["non_ref_coverage_ratio"],
+                ]
+            )
+
 
 def process_database(url, output_file, release=None):
     """
@@ -133,39 +174,60 @@ def process_database(url, output_file, release=None):
     """
     engine = create_engine(url)
     with engine.connect() as connection:
-        if release is None: 
-            initial_query = get_mlss_from_lastz_db()
-            initial_result = execute_query(connection, initial_query)
-            ids = [row[0] for row in initial_result]
+        if release is None:
+            initial_result = connection.execute(text(get_mlss_from_lastz_db))
+            ids = [str(row[0]) for row in initial_result]
 
             if ids:
-                ids_str = ', '.join(map(str, ids))
-                result_query = get_result_query(ids_str)
-                result = execute_query(connection, result_query)
-                write_to_tsv(output_file, result)
-        
-        elif release is not None:
-            initial_query = get_lastz_mlss_from_release_db(release)
-            initial_result = execute_query(connection, initial_query)
-            method_link_species_set_ids = [row[0] for row in initial_result]
+                param_ids = {"mlss_ids": ids}
+                result = connection.execute(text(get_query_for_results), param_ids)
+                calculated_results = calculate_coverage_ratios(result)
+                write_to_tsv(output_file, calculated_results)
 
-            if method_link_species_set_ids:
-                ids_str = ', '.join(map(str, method_link_species_set_ids))
-                result_query = get_result_query(ids_str)
-                result = execute_query(connection, result_query)
-                write_to_tsv(output_file, result)
+        elif release is not None:
+            param_release = {"release": release}
+
+            get_first_release_ids = connection.execute(
+                text(get_lastz_mlss_from_first_release), param_release
+            )
+            mlss_ids = [str(row[0]) for row in get_first_release_ids]
+
+            params_rerun_tags = {
+                "rerun_tag": f"rerun_in_{release}",
+                "patched_tag": f"patched_in_{release}",
+            }
+            get_rerun_mlsses = connection.execute(
+                text(get_mlss_for_rerun_tags), params_rerun_tags
+            )
+            mlss_ids.extend([str(row[0]) for row in get_rerun_mlsses])
+
+            if mlss_ids:
+                param_ids = {"mlss_ids": mlss_ids}
+                result_query = connection.execute(
+                    text(get_query_for_results), param_ids
+                )
+                calculated_results = calculate_coverage_ratios(result_query)
+                write_to_tsv(output_file, calculated_results)
+
 
 def main():
     """
     Main function to parse user input and process the database.
     """
-    parser = argparse.ArgumentParser(description='Process database and write coverage ratios to TSV file.')
-    parser.add_argument('url', type=str, help='The database URL.')
-    parser.add_argument('output_file', type=str, help='The name of the output TSV file.')
-    parser.add_argument('--release', type=int, default=None, help='The release version (optional).')
+    parser = argparse.ArgumentParser(
+        description="Process database and write coverage ratios to TSV file."
+    )
+    parser.add_argument("url", type=str, help="The database URL.")
+    parser.add_argument(
+        "output_file", type=str, help="The name of the output TSV file."
+    )
+    parser.add_argument(
+        "--release", type=int, default=None, help="The release version (optional)."
+    )
     args = parser.parse_args()
 
     process_database(args.url, args.output_file, args.release)
+
 
 if __name__ == "__main__":
     main()
