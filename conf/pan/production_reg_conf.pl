54b685adaa71cd8304a46eb474122f19e83022cc
Thomas Walsh <twalsh@ebi.ac.uk>
2025-09-23 18:42:12 +0100

    Prepare Pan registry for e116

--- conf/pan/production_reg_conf.pl
+++ conf/pan/production_reg_conf.pl
@@ -138,12 +138,12 @@ Bio::EnsEMBL::Compara::Utils::Registry::load_collection_core_database(
 my $compara_dbs = {
     # general compara dbs
     'compara_master' => [ 'mysql-ens-compara-prod-7', 'ensembl_compara_master_pan' ],
-    'compara_curr'   => [ 'mysql-ens-compara-prod-7', "ensembl_compara_pan_homology_${curr_eg_release}_${curr_release}" ],
+    #'compara_curr'   => [ 'mysql-ens-compara-prod-7', "ensembl_compara_pan_homology_${curr_eg_release}_${curr_release}" ],
     'compara_prev'   => [ 'mysql-ens-compara-prod-7', "ensembl_compara_pan_homology_${prev_eg_release}_${prev_release}" ],
 
     # homology dbs
-    'compara_members'  => [ 'mysql-ens-compara-prod-7', 'jitender_pan_load_members_115' ],
-    'compara_ptrees'   => [ 'mysql-ens-compara-prod-7', 'jitender_default_pan_protein_trees_115'],
+    #'compara_members'  => [ 'mysql-ens-compara-prod-X', '' ],
+    #'compara_ptrees'   => [ 'mysql-ens-compara-prod-x', ''],
 };
 
 Bio::EnsEMBL::Compara::Utils::Registry::add_compara_dbas( $compara_dbs );
