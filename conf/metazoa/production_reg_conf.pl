18af73c69e51cdc3bda0951c272ec37b61ea4a38
Botond Sipos/Compara <Botond.Sipos@ebi.ac.uk>
2025-10-10 15:05:30 +0100

    116 metazoa registry: added memebers and release databases.

--- conf/metazoa/production_reg_conf.pl
+++ conf/metazoa/production_reg_conf.pl
@@ -87,11 +87,11 @@ Bio::EnsEMBL::Compara::Utils::Registry::add_core_dbas( $overlap_cores );
 my $compara_dbs = {
     # general compara dbs
     'compara_master' => [ 'mysql-ens-compara-prod-6', 'ensembl_compara_master_metazoa' ],
-    #'compara_curr'   => [ 'mysql-ens-compara-prod-6', "ensembl_compara_metazoa_${curr_eg_release}_${curr_release}" ],
+    'compara_curr'   => [ 'mysql-ens-compara-prod-6', "ensembl_compara_metazoa_${curr_eg_release}_${curr_release}" ],
     'compara_prev'   => [ 'mysql-ens-compara-prod-6', "ensembl_compara_metazoa_${prev_eg_release}_${prev_release}" ],
 
     # homology dbs
-    #'compara_members'    => [ 'mysql-ens-compara-prod-6', '' ],
+    'compara_members'    => [ 'mysql-ens-compara-prod-6', 'sbotond_metazoa_load_members_116' ],
     #'compara_ptrees'     => [ 'mysql-ens-compara-prod-6', '' ],
     #'protostomes_ptrees' => [ 'mysql-ens-compara-prod-X', '' ],
     'insects_ptrees'     => [ 'mysql-ens-compara-prod-6', 'sbotond_insects_metazoa_protein_trees_115' ],
