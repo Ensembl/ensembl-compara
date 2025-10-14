bf50badfb77fc2e2a6c5d0cea3086fb98fad1623
Thomas Walsh <twalsh@ebi.ac.uk>
2025-10-14 11:34:51 +0100

    Update Vertebrates registry with e116 alt-alleles

--- conf/vertebrates/production_reg_conf.pl
+++ conf/vertebrates/production_reg_conf.pl
@@ -147,7 +147,7 @@ my $compara_dbs = {
     #'compara_syntenies'   => [ 'mysql-ens-compara-prod-X', '' ],
 
     # miscellaneous
-    #'alt_allele_projection' => [ 'mysql-ens-compara-prod-X', '' ],
+    'alt_allele_projection' => [ 'mysql-ens-compara-prod-9', 'twalsh_vertebrates_alt_allele_import_116' ],
 };
 
 Bio::EnsEMBL::Compara::Utils::Registry::add_compara_dbas( $compara_dbs );
