c2aa55f188563141fab9d856c2d1f848d3a6d509
Thomas Walsh <twalsh@ebi.ac.uk>
2025-10-20 09:43:25 +0100

    Update Vertebrates registry with e116 Pig trees

--- conf/vertebrates/production_reg_conf.pl
+++ conf/vertebrates/production_reg_conf.pl
@@ -100,8 +100,8 @@ my $compara_dbs = {
     'murinae_nctrees'         => [ 'mysql-ens-compara-prod-8',  'twalsh_vertebrates_murinae_ncrna_reindexed_trees_116' ],
     'murinae_ptrees_prev'     => [  'mysql-ens-compara-prod-1',  'sbhurji_vertebrates_murinae_protein_reindexed_trees_115'],
     'murinae_nctrees_prev'    => [ 'mysql-ens-compara-prod-6',  'sbhurji_vertebrates_murinae_ncrna_reindexed_trees_115' ],
-    #'pig_breeds_ptrees'       => [ 'mysql-ens-compara-prod-X',  '' ],
-    #'pig_breeds_nctrees'      => [ 'mysql-ens-compara-prod-X', '' ],
+    'pig_breeds_ptrees'       => [ 'mysql-ens-compara-prod-9',  'twalsh_pig_breeds_vertebrates_protein_trees_116' ],
+    'pig_breeds_nctrees'      => [ 'mysql-ens-compara-prod-9', 'twalsh_pig_breeds_vertebrates_ncrna_trees_116' ],
     'pig_breeds_ptrees_prev'  => [ 'mysql-ens-compara-prod-1',  'sbhurji_pig_breeds_vertebrates_protein_trees_115' ],
     'pig_breeds_nctrees_prev' => [ 'mysql-ens-compara-prod-4',  'twalsh_pig_breeds_vertebrates_ncrna_trees_take3_115' ],
 
