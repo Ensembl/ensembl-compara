dca9b2186b8473fc8ea445ca72b7c05f437fde1f
Thomas Walsh <twalsh@ebi.ac.uk>
2025-11-06 15:36:09 +0000

    Update Vertebrates registry with e116 ancestral DB

--- conf/vertebrates/production_reg_conf.pl
+++ conf/vertebrates/production_reg_conf.pl
@@ -156,7 +156,7 @@ Bio::EnsEMBL::Compara::Utils::Registry::add_compara_dbas( $compara_dbs );
 
 my $ancestral_dbs = {
     'ancestral_prev' => [ 'mysql-ens-compara-prod-1', "ensembl_ancestral_$prev_release" ],
-    #'ancestral_curr' => [ 'mysql-ens-compara-prod-9', "ensembl_ancestral_$curr_release" ],
+    'ancestral_curr' => [ 'mysql-ens-compara-prod-1', "ensembl_ancestral_$curr_release" ],
 
     'mammals_ancestral'    => ['mysql-ens-compara-prod-4', 'sbhurji_mammals_ancestral_core_115' ],
     'primates_ancestral'   => [ 'mysql-ens-compara-prod-2', 'twalsh_primates_ancestral_core_106' ],
