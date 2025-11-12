01f28b21db0a0f9e6d102b590a6793d75936494f
Thomas Walsh <twalsh@ebi.ac.uk>
2025-11-12 08:35:51 +0000

    Update Pan registry with e116 protein trees

--- conf/pan/production_reg_conf.pl
+++ conf/pan/production_reg_conf.pl
@@ -143,7 +143,7 @@ my $compara_dbs = {
 
     # homology dbs
     'compara_members'  => [ 'mysql-ens-compara-prod-7', 'twalsh_pan_load_members_116' ],
-    #'compara_ptrees'   => [ 'mysql-ens-compara-prod-x', ''],
+    'compara_ptrees'   => [ 'mysql-ens-compara-prod-7', 'twalsh_default_pan_protein_trees_116'],
 };
 
 Bio::EnsEMBL::Compara::Utils::Registry::add_compara_dbas( $compara_dbs );
