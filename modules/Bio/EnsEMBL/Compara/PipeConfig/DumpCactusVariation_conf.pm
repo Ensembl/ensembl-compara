36c91fc0cb911baf4c87bbee6b792ebc1d1c7521
Thomas Walsh <twalsh@ebi.ac.uk>
2026-05-15 16:20:27 +0100

    Update DumpCactusVariation references section

--- modules/Bio/EnsEMBL/Compara/PipeConfig/DumpCactusVariation_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/DumpCactusVariation_conf.pm
@@ -76,21 +76,23 @@ The main steps of the DumpCactusVariation pipeline are as follows:
 
 8. Finally, validateFiles validates each output bigChain file.
 
-=head2 Software/references
+=head2 References
 
-This pipeline makes use of various tools for processing alignments, including:
+This pipeline makes use of various tools and formats for processing alignments, including:
+- bigChain.as autoSql file (Kent & Brumbaugh 2002; https://www.linuxjournal.com/article/5949 )
+  hosted by the UCSC Genome browser website (Casper et al. 2026; https://doi.org/10.1093/nar/gkaf1250 )
 - bedToBigBed v. 2.8 ( Kent et al. 2010; https://doi.org/10.1093/bioinformatics/btq351 )
 - Biopython 1.81 ( Cock et al. 2009; https://doi.org/10.1093/bioinformatics/btp163 )
 - Cactus 3.0.0 ( Armstrong et al. 2020; https://doi.org/10.1038/s41586-020-2871-y )
-- chainSwap, part of kent v415 (Casper et al. 2025; https://doi.org/10.1093/nar/gkaf1250 )
-- chainToBigChain, part of kent v479 (Casper et al. 2025; https://doi.org/10.1093/nar/gkaf1250 )
+- chainSwap, part of kent v415 (Casper et al. 2026; https://doi.org/10.1093/nar/gkaf1250 )
+- chainToBigChain, part of kent v479 (Casper et al. 2026; https://doi.org/10.1093/nar/gkaf1250 )
 - hal2maf v2.2 ( Hickey et al. 2013; https://doi.org/10.1093/bioinformatics/btt128 )
 - halStats v2.2 ( Hickey et al. 2013; https://doi.org/10.1093/bioinformatics/btt128 )
 - maf-convert, part of LAST 1642 ( Kielbasa et al. 2011; https://doi.org/10.1101/gr.113985.110 )
 - mafDuplicateFilter, commit c101dedb2c1c8339bc284e3a16000bc4523f5da3 ( Earl et al. 2014; https://doi.org/10.1101/gr.174920.114 )
 - NumPy 1.24.3 ( Harris et al. 2020; https://doi.org/10.1038/s41586-020-2649-2 )
-- taffy, commit 1329d999948ad4acc10116276fa7a9752a749595 ( https://github.com/ComparativeGenomicsToolkit/taffy )
-- validateFiles v4.7 ( Casper et al. 2025; https://doi.org/10.1093/nar/gkaf1250 )
+- taffy, commit 1329d999948ad4acc10116276fa7a9752a749595 ( Paten & Hickey 2022; https://github.com/ComparativeGenomicsToolkit/taffy )
+- validateFiles v4.7 ( Casper et al. 2026; https://doi.org/10.1093/nar/gkaf1250 )
 
 Additionally, the pipeline implements variant normalisation along the lines
 described in Tan et al. 2015 ( https://doi.org/10.1093/bioinformatics/btv112 ).
