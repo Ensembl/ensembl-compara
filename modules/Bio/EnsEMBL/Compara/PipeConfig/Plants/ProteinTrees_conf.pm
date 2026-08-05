93a81d0c966e1365c0c35d12554a7a7dccb389ba
Thomas Walsh <twalsh@ebi.ac.uk>
2026-08-05 14:39:12 +0100

    Switch off shared homology dumps in Vertebrates and Plants

--- modules/Bio/EnsEMBL/Compara/PipeConfig/Plants/ProteinTrees_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/Plants/ProteinTrees_conf.pm
@@ -94,6 +94,11 @@ sub default_options {
 
         # WGA OrthologQM homology method_link_species_set
         'homology_method_link_types' => ['ENSEMBL_ORTHOLOGUES', 'ENSEMBL_HOMOEOLOGUES'],
+
+        # Do we need a mapping between homology_ids of this database to another database?
+        'do_homology_id_mapping' => 0,
+        # Do we expect to need shared homology dumps in a future release to facilitate reuse of WGA coverage data ?
+        'homology_dumps_shared_dir' => undef,
     };
 }
 
